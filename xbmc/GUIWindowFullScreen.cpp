ba7210a5cf25cdf821da548f5a9b6506ab4d1968
yamp <yamp@svn>
2003-08-31 19:14:43 +0000

    in fullscreenmode use remote display or controller START to switch between zoom/normal/stretch
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@86 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowFullScreen.cpp
+++ xbmc/GUIWindowFullScreen.cpp
@@ -1,5 +1,6 @@
 
 #include "GUIWindowFullScreen.h"
+#include "settings.h"
 
 CGUIWindowFullScreen::CGUIWindowFullScreen(void)
 :CGUIWindow(0)
@@ -13,11 +14,34 @@ CGUIWindowFullScreen::~CGUIWindowFullScreen(void)
 
 void CGUIWindowFullScreen::OnKey(const CKey& key)
 {
+	if ( key.GetButtonCode() == KEY_BUTTON_START  || key.GetButtonCode() == KEY_REMOTE_DISPLAY)
+	{
+		// zoom->stretch
+		if (g_stSettings.m_bZoom)
+		{
+			g_stSettings.m_bZoom=false;
+			g_stSettings.m_bStretch=true;
+			return;
+		}
+		// stretch->normal
+		if (g_stSettings.m_bStretch)
+		{
+			g_stSettings.m_bZoom=false;
+			g_stSettings.m_bStretch=false;
+			return;
+		}
+		// normal->zoom
+		g_stSettings.m_bZoom=true;
+		g_stSettings.m_bStretch=false;
+		return;
+
+	}
 
   CGUIWindow::OnKey(key);
 }
 
 bool CGUIWindowFullScreen::OnMessage(CGUIMessage& message)
 {
- return CGUIWindow::OnMessage(message);
+
+	return CGUIWindow::OnMessage(message);
 }
