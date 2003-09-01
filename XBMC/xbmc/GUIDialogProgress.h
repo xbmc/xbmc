ca1c14e55096f3c09ebaf5684c11652f929e63b0
yamp <yamp@svn>
2003-09-01 17:42:31 +0000

    added cancel button for my music scan & search
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@92 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIDialogProgress.h
+++ XBMC/xbmc/GUIDialogProgress.h
@@ -9,6 +9,7 @@ public:
 	virtual ~CGUIDialogProgress(void);
 
 	void						StartModal(DWORD dwParentId);
+  virtual bool    OnMessage(CGUIMessage& message);
 	void						Progress();
 	void					  SetLine(int iLine, const wstring& strLine);
 	void					  SetLine(int iLine, const string& strLine);
@@ -16,4 +17,7 @@ public:
 	void						SetHeading(const wstring& strLine);
 	void						SetHeading(int iString);
 	virtual void		Close();
+	bool						IsCanceled() const;
+protected:
+	bool						m_bCanceled;
 };
