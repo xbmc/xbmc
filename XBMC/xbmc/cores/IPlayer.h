1aac6e50ac5912468abee7b45acba974efe39849
yamp <yamp@svn>
2003-09-03 17:27:05 +0000

    added several keymappings for step forward/backward, turn osd on/off etc
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@96 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/IPlayer.h
+++ XBMC/xbmc/cores/IPlayer.h
@@ -18,9 +18,20 @@ public:
 	virtual bool		IsPlaying() const {return false;} ;
 	virtual void		Pause()=0;
 	virtual bool		IsPaused() const=0;
-	virtual __int64	GetPTS() =0;
 	virtual bool		HasVideo()=0;
 	virtual bool		HasAudio()=0;
+	virtual void		ToggleOSD()=0;
+
+	virtual __int64	GetPTS() =0;
+	virtual void		ToggleSubtitles()=0;
+	virtual void		ToggleFrameDrop()=0;
+	virtual void		SubtitleOffset(bool bPlus=true)=0;	
+	virtual void	  Seek(bool bPlus=true, bool bLargeStep=false)=0;
+	virtual void		SetVolume(bool bPlus=true)=0;	
+	virtual void		SetContrast(bool bPlus=true)=0;	
+	virtual void		SetBrightness(bool bPlus=true)=0;	
+	virtual void		SetHue(bool bPlus=true)=0;	
+	virtual void		SetSaturation(bool bPlus=true)=0;	
 protected:
 	IPlayerCallback& m_callback;
 };
--- XBMC/xbmc/cores/mplayer.cpp
+++ XBMC/xbmc/cores/mplayer.cpp
@@ -1,8 +1,66 @@
 #include "mplayer.h"
 #include "mplayer/mplayer.h"
 #include "../util.h"
-
 extern "C" void dllReleaseAll( );
+
+#define KEY_ENTER 13
+#define KEY_TAB 9
+
+#define KEY_BASE 0x100
+
+/*  Function keys  */
+#define KEY_F (KEY_BASE+64)
+
+/* Control keys */
+#define KEY_CTRL (KEY_BASE)
+#define KEY_BACKSPACE (KEY_CTRL+0)
+#define KEY_DELETE (KEY_CTRL+1)
+#define KEY_INSERT (KEY_CTRL+2)
+#define KEY_HOME (KEY_CTRL+3)
+#define KEY_END (KEY_CTRL+4)
+#define KEY_PAGE_UP (KEY_CTRL+5)
+#define KEY_PAGE_DOWN (KEY_CTRL+6)
+#define KEY_ESC (KEY_CTRL+7)
+
+/* Control keys short name */
+#define KEY_BS KEY_BACKSPACE
+#define KEY_DEL KEY_DELETE
+#define KEY_INS KEY_INSERT
+#define KEY_PGUP KEY_PAGE_UP
+#define KEY_PGDOWN KEY_PAGE_DOWN
+#define KEY_PGDWN KEY_PAGE_DOWN
+
+/* Cursor movement */
+#define KEY_CRSR (KEY_BASE+16)
+#define KEY_RIGHT (KEY_CRSR+0)
+#define KEY_LEFT (KEY_CRSR+1)
+#define KEY_DOWN (KEY_CRSR+2)
+#define KEY_UP (KEY_CRSR+3)
+
+/* XF86 Multimedia keyboard keys */
+#define KEY_XF86_BASE (0x100+384)
+#define KEY_XF86_PAUSE (KEY_XF86_BASE+1)
+#define KEY_XF86_STOP (KEY_XF86_BASE+2)
+#define KEY_XF86_PREV (KEY_XF86_BASE+3)
+#define KEY_XF86_NEXT (KEY_XF86_BASE+4)
+  
+/* Keypad keys */
+#define KEY_KEYPAD (KEY_BASE+32)
+#define KEY_KP0 (KEY_KEYPAD+0)
+#define KEY_KP1 (KEY_KEYPAD+1)
+#define KEY_KP2 (KEY_KEYPAD+2)
+#define KEY_KP3 (KEY_KEYPAD+3)
+#define KEY_KP4 (KEY_KEYPAD+4)
+#define KEY_KP5 (KEY_KEYPAD+5)
+#define KEY_KP6 (KEY_KEYPAD+6)
+#define KEY_KP7 (KEY_KEYPAD+7)
+#define KEY_KP8 (KEY_KEYPAD+8)
+#define KEY_KP9 (KEY_KEYPAD+9)
+#define KEY_KPDEC (KEY_KEYPAD+10)
+#define KEY_KPINS (KEY_KEYPAD+11)
+#define KEY_KPDEL (KEY_KEYPAD+12)
+#define KEY_KPENTER (KEY_KEYPAD+13)
+
 
 CMPlayer::CMPlayer(IPlayerCallback& callback)
 :IPlayer(callback)
@@ -58,7 +116,7 @@ bool CMPlayer::openfile(const CStdString& strFile)
 		char *argv[] = {"xbmc.xbe", "1.avi",NULL};
 		OutputDebugString("cmplayer::openfile() call init\n");
 		mplayer_init(argc,argv);
-		mplayer_put_key('o');
+		
 		Create();
 	}
 
@@ -220,3 +278,83 @@ bool CMPlayer::HasAudio()
 	return (mplayer_HasAudio()==TRUE);
 }
 
+void CMPlayer::ToggleOSD()
+{
+	mplayer_put_key('o');
+}
+
+
+void CMPlayer::ToggleSubtitles()
+{
+}
+
+
+void CMPlayer::SubtitleOffset(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('x');
+	else
+		mplayer_put_key('z');
+}
+
+void CMPlayer::Seek(bool bPlus, bool bLargeStep)
+{
+	if (bLargeStep)
+	{
+		if (bPlus)
+			mplayer_put_key(KEY_PAGE_UP);
+		else
+			mplayer_put_key(KEY_PAGE_DOWN);
+	}
+	else
+	{
+		if (bPlus)
+			mplayer_put_key(KEY_UP);
+		else
+			mplayer_put_key(KEY_DOWN);
+	}
+}
+
+void CMPlayer::ToggleFrameDrop()
+{
+	mplayer_put_key('d');
+}
+
+
+void CMPlayer::SetVolume(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('0');
+	else
+		mplayer_put_key('9');
+}
+
+void CMPlayer::SetContrast(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('2');
+	else
+		mplayer_put_key('1');
+}
+
+void CMPlayer::SetBrightness(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('4');
+	else
+		mplayer_put_key('3');
+}
+void CMPlayer::SetHue(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('6');
+	else
+		mplayer_put_key('5');
+}
+void CMPlayer::SetSaturation(bool bPlus)
+{
+	if (bPlus)
+		mplayer_put_key('8');
+	else
+		mplayer_put_key('7');
+}
