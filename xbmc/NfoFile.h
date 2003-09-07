37f0b0d01a554dd6d56115074e10d2e8602cffbc
yamp <yamp@svn>
2003-09-07 14:04:35 +0000

    no message
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@114 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/NfoFile.h
@@ -0,0 +1,35 @@
+// NfoFile.h: interface for the CNfoFile class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_NfoFile_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_)
+#define AFX_NfoFile_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <xtl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+class CNfoFile
+{
+public:
+	CNfoFile();
+	virtual ~CNfoFile();
+	
+	HRESULT Create(LPSTR szPath);
+
+	string m_strImDbUrl;
+private:
+	HRESULT Load(char* szFile);
+	void	Close();
+private:
+	char*	m_doc;
+	int		m_size;
+};
+
+#endif // !defined(AFX_NfoFile_H__641CCF68_6D2A_426E_9204_C0E4BEF12D00__INCLUDED_)
