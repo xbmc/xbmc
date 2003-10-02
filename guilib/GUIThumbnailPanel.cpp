09c33e376449c319fc8508821b26bcd6930a1d5e
yamp <yamp@svn>
2003-10-02 16:00:49 +0000

    fixed: cursor now jumps to the next item when adding a song to the playlist with Y
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@171 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIThumbnailPanel.cpp
+++ guilib/GUIThumbnailPanel.cpp
@@ -299,13 +299,13 @@ bool CGUIThumbnailPanel::OnMessage(CGUIMessage& message)
 				m_iCursorX=0;
 				m_iCursorY=0;
 				m_iOffset=0;
-				while (iItem > (m_iRows*m_iColumns) )
+				while (iItem >= (m_iRows*m_iColumns) )
 				{
 					m_iOffset +=(m_iRows*m_iColumns);
 					iItem -= (m_iRows*m_iColumns);
 					iPage++;
 				}
-				while (iItem > m_iColumns)
+				while (iItem >= m_iColumns)
 				{
 					m_iCursorY++;
 					iItem -= m_iColumns;
