402a966e73a7385cff8efbf13885063e157ce625
yamp <yamp@svn>
2003-10-11 16:21:01 +0000

    use english strings if tuples are not present in other language files
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@206 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/LocalizeStrings.cpp
+++ guilib/LocalizeStrings.cpp
@@ -15,31 +15,72 @@ CLocalizeStrings::~CLocalizeStrings(void)
 bool CLocalizeStrings::Load(const CStdString& strFileName)
 {
   m_vecStrings.erase(m_vecStrings.begin(),m_vecStrings.end());
-  TiXmlDocument xmlDoc;
-  if ( !xmlDoc.LoadFile(strFileName.c_str()) )
-  {
-    return false;
-  }
-  TiXmlElement* pRootElement =xmlDoc.RootElement();
-  CStdString strValue=pRootElement->Value();
-  if (strValue!=CStdString("strings")) return false;
-  const TiXmlNode *pChild = pRootElement->FirstChild();
-  while (pChild)
-  {
-    CStdString strValue=pChild->Value();
-    if (strValue=="string")
-    {
-      const TiXmlNode *pChildID = pChild->FirstChild("id");
-      const TiXmlNode *pChildText = pChild->FirstChild("value");
-      DWORD dwID=atoi(pChildID->FirstChild()->Value());
-      WCHAR wszText[1024];
-      swprintf(wszText,L"%S", pChildText->FirstChild()->Value() );
-      wstring strText=wszText;
-      m_vecStrings[dwID]=strText;
-    }
-    pChild=pChild->NextSibling();
-  }
-  return true;
+	{
+		TiXmlDocument xmlDoc;
+		if ( !xmlDoc.LoadFile(strFileName.c_str()) )
+		{
+			return false;
+		}
+		TiXmlElement* pRootElement =xmlDoc.RootElement();
+		CStdString strValue=pRootElement->Value();
+		if (strValue!=CStdString("strings")) return false;
+		const TiXmlNode *pChild = pRootElement->FirstChild();
+		while (pChild)
+		{
+			CStdString strValue=pChild->Value();
+			if (strValue=="string")
+			{
+				const TiXmlNode *pChildID = pChild->FirstChild("id");
+				const TiXmlNode *pChildText = pChild->FirstChild("value");
+				DWORD dwID=atoi(pChildID->FirstChild()->Value());
+				WCHAR wszText[1024];
+				swprintf(wszText,L"%S", pChildText->FirstChild()->Value() );
+				if (wcslen(wszText) > 0)
+				{
+					wstring strText=wszText;
+					m_vecStrings[dwID]=strText;
+				}
+			}
+			pChild=pChild->NextSibling();
+		}
+
+	}
+	{
+		// load the original english file
+		// and copy any missing texts
+		TiXmlDocument xmlDoc;
+		if ( !xmlDoc.LoadFile("Q:\\language\\english\\strings.xml") )
+		{
+			return true;
+		}
+		TiXmlElement* pRootElement =xmlDoc.RootElement();
+		CStdString strValue=pRootElement->Value();
+		if (strValue!=CStdString("strings")) return false;
+		const TiXmlNode *pChild = pRootElement->FirstChild();
+		while (pChild)
+		{
+			CStdString strValue=pChild->Value();
+			if (strValue=="string")
+			{
+				const TiXmlNode *pChildID = pChild->FirstChild("id");
+				const TiXmlNode *pChildText = pChild->FirstChild("value");
+				DWORD dwID=atoi(pChildID->FirstChild()->Value());
+				ivecStrings i;
+				i = m_vecStrings.find(dwID);
+				if (i==m_vecStrings.end())
+				{
+					WCHAR wszText[1024];
+					swprintf(wszText,L"%S", pChildText->FirstChild()->Value() );
+					wstring strText=wszText;
+					m_vecStrings[dwID]=strText;
+				}
+			}
+			pChild=pChild->NextSibling();
+		}
+	}
+	
+	
+	return true;
 }
 
 static wstring wszEmptyString=L"";
