53820461dc2988a9a36bb4378ca5572f7e901c0d
yamp <yamp@svn>
2003-12-20 17:56:21 +0000

    renamed util.cpp to pyutil.cpp to solve some #include conflicts
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@471 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/lib/libPython/xbmcmodule/pyutil.cpp
@@ -0,0 +1,29 @@
+#include "pyutil.h"
+#include <wchar.h>
+#include "stdstring.h"
+
+#pragma code_seg("PY_TEXT")
+#pragma data_seg("PY_DATA")
+#pragma bss_seg("PY_BSS")
+#pragma const_seg("PY_RDATA")
+
+namespace PYXBMC
+{
+	int PyGetUnicodeString(wstring& buf, PyObject* pObject, int pos)
+	{
+		if(PyUnicode_Check(pObject))
+		{
+			buf = PyUnicode_AsUnicode(pObject);
+			return 1;
+		}
+		if(PyString_Check(pObject))
+		{
+			buf = CStdStringW(PyString_AsString(pObject));
+			return 1;
+		}
+		// object is not an unicode ar normal string
+		buf = L"";
+		if (pos != -1) PyErr_Format(PyExc_TypeError, "argument %.200i must be unicode or str", pos);
+		return 0;
+	}
+}
