101920cd1a9ce3eefd3dfb612e70c15d8a3c53cc
jwnmulder <jwnmulder@svn>
2004-01-07 00:14:35 +0000

    fixed: samba filenames character bug
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@501 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/SMBDirectory.cpp
+++ xbmc/FileSystem/SMBDirectory.cpp
@@ -2,6 +2,8 @@
  * know bugs:
  * - doing smbc_stat on for example an IPC_SHARE leaves an open socket
  *   this happens when listing all shares from a pc ("smb://pc-name")
+ *
+ * debugging is off for release builds (see local.h)
  */
 
 #include "smbdirectory.h"
@@ -11,14 +13,12 @@
 
 CSMBDirectory::CSMBDirectory(void)
 {
-	
 } 
 
 CSMBDirectory::~CSMBDirectory(void)
 {
-	
 }
-	
+
 bool  CSMBDirectory::GetDirectory(const CStdString& strPath,VECFILEITEMS &items)
 {
 /*
@@ -35,14 +35,12 @@ bool  CSMBDirectory::GetDirectory(const CStdString& strPath,VECFILEITEMS &items)
 	    SMBC_DIR=7,
 	    SMBC_FILE=8,
 	    SMBC_LINK=9,
-	unsigned int dirlen;
-	unsigned int commentlen;
-	char *comment;
-	unsigned int namelen;
-	char name[1];
-}; dirent
 */
 
+	// note, samba uses UTF8 strings internal, that's why we have to convert strings
+	// and wstrings to UTF8.
+	char strUtfPath[1024];
+	size_t strlen;
 	CStdString strRoot = strPath;
 
 	if (!CUtil::HasSlashAtEnd(strPath))
@@ -50,29 +48,62 @@ bool  CSMBDirectory::GetDirectory(const CStdString& strPath,VECFILEITEMS &items)
 
 	smb.Init();
 
+	// convert from string to UTF8
+	strlen = convert_string(CH_DOS, CH_UTF8, strPath, strPath.length(), strUtfPath, 1024);
+	strUtfPath[strlen] = 0;
+
 	smb.Lock();
-	int fd = smbc_opendir(strPath);
+	int fd = smbc_opendir(strUtfPath);
 	smb.Unlock();
 
 	if (fd < 0) return false;
 	else
 	{
 		struct smbc_dirent* dirEnt;
+		wchar_t wStrFile[1024];
+		char strUtfFile[1024];
+
 		smb.Lock();
 		dirEnt = smbc_readdir(fd);
 		smb.Unlock();
+
 		while (dirEnt) 
 		{
-			if (dirEnt->name && strcmp(dirEnt->name,".") && strcmp(dirEnt->name,".."))
+			if (dirEnt->name && strcmp(dirEnt->name,".") && strcmp(dirEnt->name,"..") &&
+				 (dirEnt->name[dirEnt->namelen - 2] != '$'))
 			{
-				CStdString strFile=dirEnt->name;
-				struct stat info;
+				long lSize = 0;
+				bool bIsDir = true;
+				__int64 lTimeDate = 0;
 
-				smb.Lock();
-				smbc_stat(strRoot + strFile, &info);
-				smb.Unlock();
 				
-				__int64 lTimeDate= info.st_ctime;
+				// convert from UTF8 to wide string
+				strlen = convert_string(CH_UTF8, CH_UCS2, dirEnt->name, dirEnt->namelen, wStrFile, 1024);
+				wStrFile[strlen] = 0;
+
+				// doing stat on one of these types of shares leaves an open session
+				// so just skip them and only stat real dirs / files.
+				if( dirEnt->smbc_type != SMBC_FILE_SHARE &&
+						dirEnt->smbc_type != SMBC_PRINTER_SHARE &&
+						dirEnt->smbc_type != SMBC_COMMS_SHARE &&
+						dirEnt->smbc_type != SMBC_WORKGROUP &&
+						dirEnt->smbc_type != SMBC_SERVER)
+				{
+					struct stat info;
+					CStdString strFile = strRoot + wStrFile;
+
+					// convert from string to UTF8
+					strlen = convert_string(CH_DOS, CH_UTF8, strFile, strFile.length(), strUtfFile, 1024);
+					strUtfFile[strlen] = 0;
+
+					smb.Lock();
+					smbc_stat(strUtfFile, &info);
+					smb.Unlock();
+
+					bIsDir = (info.st_mode & S_IFDIR) ? true : false;
+					lTimeDate = info.st_ctime;
+					lSize = info.st_size;
+				}
 				
 				FILETIME fileTime,localTime;
 				LONGLONG ll = Int32x32To64(lTimeDate, 10000000) + 116444736000000000;
@@ -81,24 +112,24 @@ bool  CSMBDirectory::GetDirectory(const CStdString& strPath,VECFILEITEMS &items)
 
 				FileTimeToLocalFileTime(&fileTime,&localTime); 
 
-				if (info.st_mode & S_IFDIR)
+				if (bIsDir)
 				{
-					CFileItem *pItem = new CFileItem(strFile);
+					CFileItem *pItem = new CFileItem(wStrFile);
 					pItem->m_strPath=strRoot;
-					pItem->m_strPath+=strFile;
+					pItem->m_strPath+=wStrFile;
 					pItem->m_bIsFolder=true;
 					FileTimeToSystemTime(&localTime, &pItem->m_stTime);  
 					items.push_back(pItem);
 				}
 				else
 				{
-					if ( IsAllowed( strFile) )
+					if (IsAllowed(wStrFile))
 					{
-						CFileItem *pItem = new CFileItem(strFile);
+						CFileItem *pItem = new CFileItem(wStrFile);
 						pItem->m_strPath=strRoot;
-						pItem->m_strPath+=strFile;
+						pItem->m_strPath+=wStrFile;
 						pItem->m_bIsFolder=false;
-						pItem->m_dwSize = info.st_size;
+						pItem->m_dwSize = lSize;
 						FileTimeToSystemTime(&localTime, &pItem->m_stTime);
 		        
 						items.push_back(pItem);
--- xbmc/lib/libsmb/xbLibSmb.h
+++ xbmc/lib/libsmb/xbLibSmb.h
@@ -2,6 +2,10 @@
 #define XB_LIB_SMB_H
 
 #include "../lib/libsmb/config.h"
+//#include "../lib/libsmb/includes.h"
+#define uint16 unsigned short
+#define int32 INT32
+#include "../lib/libsmb/charset.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,6 +16,10 @@ extern "C" {
 
 	BOOL lp_do_parameter(int snum, const char *pszParmName, const char *pszParmValue);
 	void set_xbox_interface(char* ip, char* subnet);
+
+	size_t convert_string(charset_t from, charset_t to,
+		void const *src, size_t srclen, 
+		void *dest, size_t destlen);
 
 #ifdef __cplusplus
 }
