233970e96076a22dd4a17fb5fcaf0c1fe48ad22b
yamp <yamp@svn>
2004-01-11 11:53:40 +0000

    added: show more info in video section
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@514 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowSystemInfo.h
+++ xbmc/GUIWindowSystemInfo.h
@@ -13,7 +13,9 @@ public:
 protected:
 	void						GetValues();
 	DWORD						m_dwlastTime ;
-
+  DWORD           m_dwFPSTime;
+  DWORD           m_dwFrames;
+  float           m_fFPS;
 	unsigned short cputemp;
 	unsigned short cpudec;
 	unsigned short mbtemp;
