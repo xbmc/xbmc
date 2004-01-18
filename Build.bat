d6df51e8e36e85df0fb1dfa28a693164ac133237
yamp <yamp@svn>
2004-01-18 21:07:30 +0000

    no message
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@563 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ Build.bat
@@ -0,0 +1,50 @@
+@ECHO OFF
+
+rem ----PURPOSE----
+rem - Create a working XBMC build with a single click
+rem ---------------------------------------------
+rem Config
+rem Set your path for VSNET main executable
+rem Set the path for xbepatch
+rem Set the path for WinRARs rar.exe (freeware)
+rem and finally set the options for the final rar.
+rem ---------------------------------------------
+
+TITLE XBMC Build Prepare Script
+ECHO Wait while preparing the build.
+ECHO ------------------------------
+rem	CONFIG START
+	set NET=C:\Progra~1\Micros~2.NET\Common7\IDE\devenv.exe
+	set OPTS=xbmc.sln /build release
+	set CLEAN=xbmc.sln /clean release
+	set XBE=xbepatch.exe
+	set RAR=C:\Progra~1\Winrar\rar.exe
+	set RAROPS=a -r -idp -inul -m5 XBMC.rar BUILD
+	set SKINS=..\Skins
+rem	CONFIG END
+rem ---------------------------------------------
+ECHO Compiling Solution...
+rem %NET% %CLEAN%
+rem %NET% %OPTS%
+ECHO Done!
+ECHO ------------------------------
+ECHO Copying files...
+%XBE% release\default.xbe
+rmdir BUILD /S /Q
+md BUILD
+copy release\default.xbe BUILD
+copy *.xml BUILD
+copy *.txt BUILD
+xcopy mplayer BUILD\mplayer /E /Q /I /Y
+xcopy skin BUILD\skin /E /Q /I /Y
+xcopy language BUILD\language /E /Q /I /Y
+xcopy xbmc\keyboard\media BUILD\media /E /Q /I /Y
+xcopy visualisations BUILD\visualisations /E /Q /I /Y
+rem xcopy web BUILD\web /E /Q /I /Y
+rem xcopy python BUILD\python /E /Q /I /Y
+xcopy %SKINS% Build\Skin /E /Q /I /Y
+ECHO ------------------------------
+ECHO Rarring...
+%RAR% %RAROPS%
+ECHO ------------------------------
+ECHO finished!
