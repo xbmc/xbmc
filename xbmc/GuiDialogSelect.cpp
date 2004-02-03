af6b45af5425f6b64094ccf8edef0bb41f16b45b
yamp <yamp@svn>
2004-02-03 07:18:43 +0000

    added: if (correct) IMDB is not found you can do a manual lookup now.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@657 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GuiDialogSelect.cpp
+++ xbmc/GuiDialogSelect.cpp
@@ -3,13 +3,15 @@
 #include "localizestrings.h"
 #include "application.h"
 
-#define CONTROL_HEADING	4
-#define CONTROL_LIST 3
+#define CONTROL_HEADING	      4
+#define CONTROL_LIST          3
 #define CONTROL_NUMBEROFFILES 2
+#define CONTROL_BUTTON        5
 
 CGUIDialogSelect::CGUIDialogSelect(void)
 :CGUIDialog(0)
 {
+  m_bButtonEnabled=false;
 }
 
 CGUIDialogSelect::~CGUIDialogSelect(void)
@@ -37,8 +39,10 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 			g_application.EnableOverlay();			
 		}
 		break;
+
     case GUI_MSG_WINDOW_INIT:
     {
+      m_bButtonPressed=false;
 			CGUIDialog::OnMessage(message);
 			g_application.DisableOverlay();
 			m_iSelected=-1;
@@ -56,6 +60,17 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 			swprintf(wszText,L"%i %s", m_vecList.size(),szText);
 
 			SET_CONTROL_LABEL(GetID(), CONTROL_NUMBEROFFILES,wszText);
+
+      if (m_bButtonEnabled)
+      {
+        CGUIMessage msg2(GUI_MSG_ENABLED,GetID(),CONTROL_BUTTON,0,0,NULL);
+			  g_graphicsContext.SendMessage(msg2);  
+      }
+      else
+      {
+        CGUIMessage msg2(GUI_MSG_DISABLED,GetID(),CONTROL_BUTTON,0,0,NULL);
+			  g_graphicsContext.SendMessage(msg2);  
+      }
 			return true;
 		}
 		break;
@@ -76,6 +91,12 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 					Close();
 				}
 			}
+      if (CONTROL_BUTTON==iControl)
+      {
+        m_iSelected=-1;
+        m_bButtonPressed=true;
+        Close();
+      }
     }
     break;
   }
@@ -92,6 +113,8 @@ void CGUIDialogSelect::Reset()
 		delete pItem;
 	}
 	m_vecList.erase(m_vecList.begin(),m_vecList.end());
+  m_bButtonEnabled=false;
+
 }
 
 void CGUIDialogSelect::Add(const CStdString& strLabel)
@@ -119,7 +142,25 @@ void  CGUIDialogSelect::SetHeading(const string& strLine)
 
 void CGUIDialogSelect::SetHeading(int iString)
 {
-	CGUIMessage msg(GUI_MSG_LABEL_SET,GetID(),1);
+	CGUIMessage msg(GUI_MSG_LABEL_SET,GetID(),CONTROL_HEADING);
 	msg.SetLabel(iString);
 	OnMessage(msg);
+}
+
+
+void CGUIDialogSelect::EnableButton(bool bOnOff)
+{
+  m_bButtonEnabled=bOnOff;
+}
+
+void CGUIDialogSelect::SetButtonLabel(int iString)
+{
+	CGUIMessage msg(GUI_MSG_LABEL_SET,GetID(),CONTROL_BUTTON);
+	msg.SetLabel(iString);
+	OnMessage(msg);
+}
+
+bool CGUIDialogSelect::IsButtonPressed()
+{
+  return m_bButtonPressed;
 }
