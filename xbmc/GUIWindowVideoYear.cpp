0b3c70509df427082eef7c824b7ff3b37a6151da
yamp <yamp@svn>
2004-02-05 09:14:18 +0000

    added: SF patch 'step a few seconds back to see missed subtitles'. new action defined with number 76 (see key.h)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@679 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoYear.cpp
+++ xbmc/GUIWindowVideoYear.cpp
@@ -215,12 +215,7 @@ bool CGUIWindowVideoYear::OnMessage(CGUIMessage& message)
       else if (iControl==CONTROL_PLAY_DVD)
       {
           // play movie...
-		    g_TextureManager.Flush();
-		    g_graphicsContext.SetFullScreenVideo(true);
-		    m_gWindowManager.ActivateWindow(WINDOW_FULLSCREEN_VIDEO);
-        CIoSupport helper;
-        helper.Remount("D:","Cdrom0");
-        g_application.PlayFile("dvd://1");
+        CUtil::PlayDVD();
       }
       else if (iControl==CONTROL_LIST||iControl==CONTROL_THUMBS)  // list/thumb control
       {
@@ -509,9 +504,6 @@ void CGUIWindowVideoYear::OnClick(int iItem)
     }
 
     // play movie...
-		g_TextureManager.Flush();
-		g_graphicsContext.SetFullScreenVideo(true);
-		m_gWindowManager.ActivateWindow(WINDOW_FULLSCREEN_VIDEO);
 		g_playlistPlayer.PlayNext();
 	}
 }
