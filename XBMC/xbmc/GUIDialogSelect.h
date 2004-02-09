bd2c86041029c10a1f6e5d2a1d55a5aa2259bca1
bobbin007 <bobbin007@svn>
2004-02-09 00:46:09 +0000

    added: settings window remembers last selected control added: my music: delete album info option added: my music: delete CDDB  info option
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@722 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIDialogSelect.h
+++ XBMC/xbmc/GUIDialogSelect.h
@@ -19,15 +19,18 @@ public:
 	void						Reset();
 	void						Add(const CStdString& strLabel);
 	int							GetSelectedLabel() const;
+	const CStdString& GetSelectedLabelText();
 	void						SetHeading(const wstring& strLine);
 	void						SetHeading(const string& strLine);
 	void						SetHeading(int iString);
   void            EnableButton(bool bOnOff);
   void            SetButtonLabel(int iString);
   bool            IsButtonPressed();
+	void						Sort(bool bSortAscending=true);
 protected:
   bool            m_bButtonEnabled;
   bool            m_bButtonPressed;
 	int							m_iSelected;
+	CStdString			m_strSelected;
 	vector<CGUIListItem*> m_vecList;
 };
--- XBMC/xbmc/GUIWindowSettings.cpp
+++ XBMC/xbmc/GUIWindowSettings.cpp
@@ -4,24 +4,45 @@
 CGUIWindowSettings::CGUIWindowSettings(void)
 :CGUIWindow(0)
 {
+	m_iLastControl=-1;
 }
 
 CGUIWindowSettings::~CGUIWindowSettings(void)
 {
 }
 
-
 void CGUIWindowSettings::OnAction(const CAction &action)
 {
 	if (action.wID == ACTION_PREVIOUS_MENU)
-    {
+	{
 		m_gWindowManager.PreviousWindow();
 		return;
-    }
+	}
+
 	CGUIWindow::OnAction(action);
 }
 
 bool CGUIWindowSettings::OnMessage(CGUIMessage& message)
 {
- return CGUIWindow::OnMessage(message);
+  switch ( message.GetMessage() )
+  {
+    case GUI_MSG_WINDOW_DEINIT:
+		{
+			m_iLastControl=GetFocusedControl();
+		}
+		break;
+
+    case GUI_MSG_WINDOW_INIT:
+		{
+			CGUIWindow::OnMessage(message);
+
+	    if (m_iLastControl>-1)
+				SET_CONTROL_FOCUS(GetID(), m_iLastControl);
+
+			return true;
+		}
+		break;
+	}
+
+	return CGUIWindow::OnMessage(message);
 }
