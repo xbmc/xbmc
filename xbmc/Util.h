63b7d9a2d90864cf0d6509630d0236b17a1c286f
yamp <yamp@svn>
2004-02-10 19:06:29 +0000

    10-02-2004 added: imdb thumbnails are now stored in thumbs\imdb by using IMDB numbers
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@749 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.h
+++ xbmc/Util.h
@@ -103,5 +103,6 @@ public:
   static void PlayDVD();
   static DWORD SetUpNetwork( bool resetmode, struct network_info& networkinfo );
   static bool  GetFolderThumb(const CStdString& strFolder, CStdString& strThumb);
+  static void GetVideoThumbnail(const CStdString& strIMDBID, CStdString& strThumb);
 
 };
