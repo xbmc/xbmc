2392187eacd42636e7c63b6c8facdf22609e05dc
yamp <yamp@svn>
2004-02-12 11:49:50 +0000

    fixed: caching .m3u files didnt work
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@779 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Util.cpp
+++ XBMC/xbmc/Util.cpp
@@ -148,9 +148,15 @@ void CUtil::GetQualifiedFilename(const CStdString &strBasePath, CStdString &strF
 			if (!( isalpha(strFilename.c_str()[0]) && strFilename.c_str()[1] == ':')) //Filename not fully qualified
 			{
 				if (strFilename.c_str()[0] == '\\') 
-					strFilename = strBasePath + strFilename.Replace('/','\\');
+        {
+          strFilename.Replace('/','\\');
+					strFilename = strBasePath + strFilename;
+        }
 				else
-					strFilename = strBasePath + '\\' + strFilename.Replace('/','\\');
+        {
+          strFilename.Replace('/','\\');
+					strFilename = strBasePath + '\\' + strFilename;
+        }
 			}
 		}
 	}
@@ -159,7 +165,10 @@ void CUtil::GetQualifiedFilename(const CStdString &strBasePath, CStdString &strF
 		if(plItemUrl.GetProtocol().length()==0 ) //Filename is local
 		{
 			if (strFilename.c_str()[0] == '/') //Begins with a slash.. not good.. but we try to make the best of it..
-				strFilename = strBasePath + strFilename.Replace('\\','/');
+      {
+        strFilename.Replace('\\','/');
+				strFilename = strBasePath + strFilename;
+      }
 			else
 				strFilename = strBasePath + '/' + strFilename;
 		}
