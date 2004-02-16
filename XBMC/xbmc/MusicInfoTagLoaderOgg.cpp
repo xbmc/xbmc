67ee858a304b000e6b3cff1844f6d6094f9bc0e5
bobbin007 <bobbin007@svn>
2004-02-16 23:49:02 +0000

    added: exception handling
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@806 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/MusicInfoTagLoaderOgg.cpp
+++ XBMC/xbmc/MusicInfoTagLoaderOgg.cpp
@@ -1,6 +1,7 @@
 #include "musicinfotagloaderogg.h"
 #include "stdstring.h"
 #include "sectionloader.h"
+#include "utils/log.h"
 
 using namespace MUSIC_INFO;
 using namespace XFILE;
@@ -15,51 +16,60 @@ CMusicInfoTagLoaderOgg::~CMusicInfoTagLoaderOgg()
 
 bool CMusicInfoTagLoaderOgg::Load(const CStdString& strFileName, CMusicInfoTag& tag)
 {
-	// retrieve the OGG Tag info from strFileName
-	// and put it in tag
-	bool bResult= false;
-	tag.SetURL(strFileName);
-	CFile file;
-	if ( file.Open( strFileName.c_str() ) )  {
-		COggTag myTag;
-		myTag.ReadTag( &file );
+	try
+	{
+		// retrieve the OGG Tag info from strFileName
+		// and put it in tag
+		bool bResult= false;
+		tag.SetURL(strFileName);
+		CFile file;
+		if ( file.Open( strFileName.c_str() ) )  {
+			COggTag myTag;
+			myTag.ReadTag( &file );
 
-		SYSTEMTIME dateTime;
-		CStdString strYear=myTag.GetYear();
-		CStdString strTitle=myTag.GetTitle();
-		CStdString strArtist=myTag.GetArtist();
-		CStdString strAlbum=myTag.GetAlbum();
-		CStdString strGenre=myTag.GetGenre();
-		int nTrackNum=myTag.GetTrackNum();
+			SYSTEMTIME dateTime;
+			CStdString strYear=myTag.GetYear();
+			CStdString strTitle=myTag.GetTitle();
+			CStdString strArtist=myTag.GetArtist();
+			CStdString strAlbum=myTag.GetAlbum();
+			CStdString strGenre=myTag.GetGenre();
+			int nTrackNum=myTag.GetTrackNum();
 
-		tag.SetTrackNumber(nTrackNum);
+			tag.SetTrackNumber(nTrackNum);
 
-		if (!strGenre.IsEmpty())
-		{
-			tag.SetGenre(strGenre);
-		}
-		if (!strTitle.IsEmpty())
-		{
-			bResult = true;
-			tag.SetTitle(strTitle);
-			tag.SetLoaded(true);
-		}
-		if (!strArtist.IsEmpty())
-		{
-			tag.SetArtist(strArtist);
-		}
-		if (!strAlbum.IsEmpty())
-		{
-			tag.SetAlbum(strAlbum);
-		}
-		if (!strYear.IsEmpty())
-		{
-			dateTime.wYear=atoi(strYear);
-			tag.SetReleaseDate(dateTime);
-		}
+			if (!strGenre.IsEmpty())
+			{
+				tag.SetGenre(strGenre);
+			}
+			if (!strTitle.IsEmpty())
+			{
+				bResult = true;
+				tag.SetTitle(strTitle);
+				tag.SetLoaded(true);
+			}
+			if (!strArtist.IsEmpty())
+			{
+				tag.SetArtist(strArtist);
+			}
+			if (!strAlbum.IsEmpty())
+			{
+				tag.SetAlbum(strAlbum);
+			}
+			if (!strYear.IsEmpty())
+			{
+				dateTime.wYear=atoi(strYear);
+				tag.SetReleaseDate(dateTime);
+			}
 
 
-		file.Close();
+			file.Close();
+		}
+		return bResult;
 	}
-	return bResult;
+	catch(...)
+	{
+		CLog::Log("Tag loader ogg: exception in file %s", strFileName.c_str());
+	}
+
+	return false;
 }
