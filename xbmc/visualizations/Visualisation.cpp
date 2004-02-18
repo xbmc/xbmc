8003a59eccee0c58481a6f3cfac11f8dd2bc1006
yamp <yamp@svn>
2004-02-18 14:05:16 +0000

    added: pass songname & visualisation name to visualisation plugins
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@837 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/visualizations/Visualisation.cpp
+++ xbmc/visualizations/Visualisation.cpp
@@ -8,9 +8,10 @@
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-CVisualisation::CVisualisation(struct Visualisation* pVisz,DllLoader* pLoader)
+CVisualisation::CVisualisation(struct Visualisation* pVisz,DllLoader* pLoader, const CStdString& strVisualisationName)
 :m_pLoader(pLoader)
 ,m_pVisz(pVisz)
+,m_strVisualisationName(strVisualisationName)
 {	
 }
 
@@ -20,36 +21,49 @@ CVisualisation::~CVisualisation()
 
 void CVisualisation::Create()
 {
+  // allow vis. to create internal things needed
+  // pass it the screen width,height
+  // and the name of the visualisation.
   int iWidth = g_graphicsContext.GetWidth();
   int iHeight= g_graphicsContext.GetHeight();
   char szTmp[129];
-  sprintf(szTmp,"create:%ix%i\n", iWidth,iHeight);
+  sprintf(szTmp,"create:%ix%i %s\n", iWidth,iHeight,m_strVisualisationName.c_str());
   OutputDebugString(szTmp);
-  m_pVisz->Create (g_graphicsContext.Get3DDevice(),iWidth,iHeight );
+  m_pVisz->Create (g_graphicsContext.Get3DDevice(),iWidth,iHeight, m_strVisualisationName.c_str());
 }
 
-void CVisualisation::Start(int iChannels, int iSamplesPerSec, int iBitsPerSample)
+void CVisualisation::Start(int iChannels, int iSamplesPerSec, int iBitsPerSample, const CStdString strSongName)
 {
-	m_pVisz->Start(iChannels, iSamplesPerSec, iBitsPerSample);
+  // notify visz. that new song has been started
+  // pass it the nr of audio channels, sample rate, bits/sample and offcourse the songname
+  m_pVisz->Start(iChannels, iSamplesPerSec, iBitsPerSample,strSongName.c_str());
 }
 
 void CVisualisation::AudioData(const short* pAudioData, int iAudioDataLength, float *pFreqData, int iFreqDataLength)
 {
+  // pass audio data to visz.
+  // audio data: is short audiodata [channel][iAudioDataLength] containing the raw audio data
+  // iAudioDataLength = length of audiodata array
+  // pFreqData = fft-ed audio data
+  // iFreqDataLength = length of pFreqData 
 	m_pVisz->AudioData(const_cast<short*>(pAudioData), iAudioDataLength, pFreqData, iFreqDataLength);
 }
 
 void CVisualisation::Render()
 {
+  // ask visz. to render itself
 	m_pVisz->Render();
 }
 
 void CVisualisation::Stop()
 {
+  // ask visz. to cleanup
 	m_pVisz->Stop();
 }
 
 
 void CVisualisation::GetInfo(VIS_INFO *info)
 {
+  // get info from vis
 	m_pVisz->GetInfo(info);
 }
