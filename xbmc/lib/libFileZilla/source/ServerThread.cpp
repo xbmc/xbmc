96dfc3e669ed5db9e3731358b47f0a8c3c6b645b
yamp <yamp@svn>
2004-02-25 06:58:27 +0000

    no message
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@918 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libFileZilla/source/ServerThread.cpp
+++ xbmc/lib/libFileZilla/source/ServerThread.cpp
@@ -93,7 +93,8 @@ BOOL CServerThread::InitInstance()
 		m_pExternalIpCheck = new CExternalIpCheck(this);
 	m_threadsync.Unlock();
 
-	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+// this causes ftp 2 hangup
+//	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
 
 	return TRUE;
 }
