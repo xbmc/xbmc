5a686a5e1cecc43e6e13d7e9a92b4dc61e9b83bd
butcheruk <butcheruk@svn>
2004-02-28 23:09:07 +0000

    Better texture compression (uses PC-based tool).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@960 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/gui3d.h
+++ guilib/gui3d.h
@@ -7,11 +7,15 @@
 #define GUILIB_GUI3D_H
 #pragma once
 
-//#define ALLOW_TEXTURE_COMPRESSION 
+#define ALLOW_TEXTURE_COMPRESSION 
 
 #ifdef _XBOX
   #include <xtl.h>  
+#ifdef ALLOW_TEXTURE_COMPRESSION
+	#define GUI_D3D_FMT D3DFMT_A8R8G8B8
+#else
   #define GUI_D3D_FMT D3DFMT_LIN_A8R8G8B8
+#endif
 #else
   #include <windows.h>
   #include <d3dx8.h>
