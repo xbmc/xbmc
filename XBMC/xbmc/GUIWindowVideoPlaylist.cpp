a158bca30ede8f286fb715e1c347c8d5cbc9e956
butcheruk <butcheruk@svn>
2004-02-29 16:39:03 +0000

    fixed: thumbnails images slightly off-centre
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@971 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIWindowVideoPlaylist.cpp
+++ XBMC/xbmc/GUIWindowVideoPlaylist.cpp
@@ -401,7 +401,7 @@ void CGUIWindowVideoPlaylist::ShowThumbPanel()
   if ( ViewByLargeIcon() )
   {
     CGUIThumbnailPanel* pControl=(CGUIThumbnailPanel*)GetControl(CONTROL_THUMBS);
-    pControl->SetThumbDimensions(10,16,100,100);
+    pControl->SetThumbDimensions(14,14,100,100);
     pControl->SetTextureDimensions(128,128);
     pControl->SetItemHeight(150);
     pControl->SetItemWidth(150);
@@ -409,7 +409,7 @@ void CGUIWindowVideoPlaylist::ShowThumbPanel()
   else
   {
     CGUIThumbnailPanel* pControl=(CGUIThumbnailPanel*)GetControl(CONTROL_THUMBS);
-    pControl->SetThumbDimensions(4,10,64,64);
+    pControl->SetThumbDimensions(8,8,64,64);
     pControl->SetTextureDimensions(80,80);
     pControl->SetItemHeight(128);
     pControl->SetItemWidth(128);
