ecf43267fdda26db1d720fbc1180cb9fa47a6879
butcheruk <butcheruk@svn>
2004-02-29 16:39:03 +0000

    fixed: thumbnails images slightly off-centre
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@971 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowMusicBase.cpp
+++ xbmc/GUIWindowMusicBase.cpp
@@ -1060,7 +1060,7 @@ void CGUIWindowMusicBase::ShowThumbPanel()
     CGUIThumbnailPanel* pControl=(CGUIThumbnailPanel*)GetControl(CONTROL_THUMBS);
 		if (pControl)
 		{
-			pControl->SetThumbDimensions(10,16,100,100);
+			pControl->SetThumbDimensions(14,14,100,100);
 			pControl->SetTextureDimensions(128,128);
 			pControl->SetItemHeight(150);
 			pControl->SetItemWidth(150);
@@ -1071,7 +1071,7 @@ void CGUIWindowMusicBase::ShowThumbPanel()
     CGUIThumbnailPanel* pControl=(CGUIThumbnailPanel*)GetControl(CONTROL_THUMBS);
 		if (pControl)
 		{
-			pControl->SetThumbDimensions(4,10,64,64);
+			pControl->SetThumbDimensions(8,8,64,64);
 			pControl->SetTextureDimensions(80,80);
 			pControl->SetItemHeight(128);
 			pControl->SetItemWidth(128);
