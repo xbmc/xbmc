a5e38005e5535d157c73057fb73a34213140d1f7
mrmario64 <mrmario64@svn>
2004-03-07 22:08:35 +0000

    don't copy any graphics for skins since we use textures.xpr
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1078 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- Build.bat
+++ Build.bat
@@ -41,9 +41,9 @@ copy release\default.xbe BUILD
 copy *.xml BUILD
 copy *.txt BUILD
 xcopy mplayer BUILD\mplayer /E /Q /I /Y
-xcopy skin BUILD\skin /E /Q /I /Y
+xcopy skin\MediaCenter\fonts BUILD\skin\MediaCenter\fonts /E /Q /I /Y
+xcopy skin\*.xml BUILD\skin /E /Q /I /Y
 %TEX% -input BUILD\skin\MediaCenter\media -output BUILD\skin\MediaCenter\media
-del /s BUILD\skin\MediaCenter\*.png
 xcopy language BUILD\language /E /Q /I /Y
 xcopy xbmc\keyboard\media BUILD\media /E /Q /I /Y
 xcopy visualisations BUILD\visualisations /E /Q /I /Y
