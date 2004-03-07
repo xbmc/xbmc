073533c4d646fc691ccc016be547c5e542eb061b
bobbin007 <bobbin007@svn>
2004-03-07 21:11:18 +0000

    added: 1.5 sec. timeout to select button control if not moving
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1069 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIWindowVideoYear.cpp
+++ XBMC/xbmc/GUIWindowVideoYear.cpp
@@ -278,9 +278,14 @@ bool CGUIWindowVideoYear::OnMessage(CGUIMessage& message)
 					nNewWindow=WINDOW_VIDEO_TITLE;
 					break;
 				}
-				g_stSettings.m_iVideoStartWindow=nNewWindow;
-				g_settings.Save();
-				m_gWindowManager.ActivateWindow(nNewWindow);
+
+				if (nNewWindow!=GetID())
+				{
+					g_stSettings.m_iVideoStartWindow=nNewWindow;
+					g_settings.Save();
+					m_gWindowManager.ActivateWindow(nNewWindow);
+				}
+
         return true;
      }
     }
