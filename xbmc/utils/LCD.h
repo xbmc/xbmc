e8d10862053a79c64c307fd01bcc94990bedb6ce
yamp <yamp@svn>
2004-03-13 10:37:59 +0000

    - added support for Xenium LCD
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1191 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/LCD.h
+++ xbmc/utils/LCD.h
@@ -5,40 +5,16 @@
 using namespace std;
 #define MAX_ROWS 20
 
-class CLCD : public CThread
+class ILCD : public CThread
 {
 public:
-  CLCD();
-  virtual ~CLCD(void);
-  static void Initialize();
-  static void Stop();
-  static void SetLine(int iLine, const CStdString& strLine);
-  static void SetBackLight(int iLight);
+  ILCD();
+  virtual ~ILCD(void);
+  virtual void Initialize()=0;
+  virtual void Stop()=0;
+  virtual void SetLine(int iLine, const CStdString& strLine)=0;
+  virtual void SetBackLight(int iLight)=0;
 protected:
-	virtual void		Process();
-  void    DisplayInit();
-  void    DisplaySetBacklight(unsigned char level) ;
-  void    DisplayProgressBar(unsigned char percent, unsigned char charcnt);
-  void    DisplayClearChars(unsigned char startpos , unsigned char line, unsigned char lenght) ;
-  void    DisplayWriteString(char *pointer) ;
-  void    DisplayWriteFixtext(const char *textstring);
-  void    DisplaySetPos(unsigned char pos, unsigned char line) ;
-  void    DisplayBuildCustomChars() ;
-  void    DisplayOut(unsigned char data, unsigned char command) ;
-  void    wait_us(unsigned int value) ;
-  unsigned int m_iColumns;				// display columns for each line
-  unsigned int m_iRows;				// total number of rows
-  unsigned int m_iRow1adr ;
-  unsigned int m_iRow2adr ;
-  unsigned int m_iRow3adr ;
-  unsigned int m_iRow4adr ;
-  unsigned int m_iActualpos;				// actual cursor possition
-  int          m_iBackLight;
-  bool         m_bUpdate[MAX_ROWS];
-  CStdString   m_strLine[MAX_ROWS];
-  int          m_iPos[MAX_ROWS];
-  DWORD        m_dwSleep[MAX_ROWS];
-  CEvent       m_event;
-
+	virtual void		Process()=0;
 };
-extern CLCD g_lcd;
+extern ILCD* g_lcd;
