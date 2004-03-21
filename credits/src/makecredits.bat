c482c024d6950ff692d3212fd50e123e600c0a63
butcheruk <butcheruk@svn>
2004-03-21 02:39:51 +0000

    Text reflection for credits
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1235 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- credits/src/makecredits.bat
+++ credits/src/makecredits.bat
@@ -2,19 +2,26 @@
 
 echo Assembling Shaders...
 xsasm /P /nologo credits.vtx
-if %errorlevel% neq 0 goto exit
+if %errorlevel% neq 0 goto error
 xsasm /P /nologo credits.pxl
-if %errorlevel% neq 0 goto exit
+if %errorlevel% neq 0 goto error
 
 echo Converting Mesh...
 conv3ds -Mmf xbmc.3ds
 meshconv xbmc.x credits.rdf
-if %errorlevel% neq 0 goto exit
+if %errorlevel% neq 0 goto error
 
 echo Bundling...
 bundler credits.rdf
-if %errorlevel% neq 0 goto exit
+if %errorlevel% neq 0 goto error
+XprPack ..\credits.xpr
+if %errorlevel% neq 0 goto error
 
 echo Success!
+goto exit
+
+:error
+
+echo ERROR!
 
 :exit
