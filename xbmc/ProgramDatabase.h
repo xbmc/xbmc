c81529c870b74e43a5679ccba60d6a1993de4dd2
tslayer <tslayer@svn>
2004-03-23 03:50:52 +0000

    Added My Programs database. Speed increase in displaying My Programs.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1255 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/ProgramDatabase.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "lib/sqlLite/sqlitedataset.h"
+#include "StdString.h"
+#include "FileItem.h"
+#include <vector>
+#include <memory>
+using namespace std;
+
+#define COMPARE_PERCENTAGE     0.90f // 90%
+#define COMPARE_PERCENTAGE_MIN 0.50f // 50%
+
+
+class CProgramDatabase
+{
+public:
+  CProgramDatabase(void);
+  virtual ~CProgramDatabase(void);
+  bool    Open() ;
+  void	  Close() ;
+  long    AddProgram(const CStdString& strFilenameAndPath, const CStdString& strDescription, const CStdString& strBookmark);
+  long    GetPath(const CStdString& strPath);
+  void	  GetProgramsByBookmark(CStdString& strBookmark, VECFILEITEMS& programs, bool bOnlyDefaultXBE);
+protected:
+  auto_ptr<SqliteDatabase>  m_pDB;
+	auto_ptr<Dataset>				  m_pDS;
+
+
+  long    AddPath(const CStdString& strPath);
+  long    AddFile(long lProgramid, long lPathId, const CStdString& strFileName, const CStdString& strDescription);
+
+  long	  AddBookMark(const CStdString& strBookmark);
+  long    GetProgram(long lPathId);
+
+  void    Split(const CStdString& strFileNameAndPath, CStdString& strPath, CStdString& strFileName);
+  void    RemoveInvalidChars(CStdString& strTxt);
+  bool	  CreateTables();
+  void    DeleteProgram(const CStdString& strPath);
+};
