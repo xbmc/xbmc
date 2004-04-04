5066209edd4c76d7146da43429cf800ae87046b6
runtime <runtime@svn>
2004-04-04 18:31:21 +0000

    runtime: Added MyBuddies (online gaming feature) and Invite dialog (online gaming scheduled invites).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1319 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.h
+++ xbmc/Application.h
@@ -29,6 +29,7 @@
 #include "GUIWindowVideoYear.h"
 #include "GUIWindowVideoTitle.h"
 #include "GUIWindowSettings.h"
+#include "GUIDialogInvite.h"
 #include "GUIDialogYesNo.h"
 #include "GUIDialogOK.h"
 #include "GUIDialogProgress.h"
@@ -66,6 +67,7 @@
 #include "GUIWindowMusicArtists.h"
 #include "GUIWindowMusicGenres.h" 
 #include "GUIWindowMusicTop100.h" 
+#include "GUIWindowBuddies.h"		//BUDDIES
 #include "GUIWindowWeather.h"		//WEATHER
 #include "GUIWindowSettingsWeather.h"	//WEATHER SETTINGS
 #include "GUIWindowSettingsCache.h"
@@ -93,89 +95,91 @@ using namespace MUSIC_INFO;
 class CApplication : public CXBApplicationEx, public IPlayerCallback, public IMsgTargetCallback
 {
 public:
-	CApplication(void);
-	virtual ~CApplication(void);
-	virtual HRESULT 		Initialize();
-	virtual void			FrameMove();
-	virtual void			Render();
-	virtual HRESULT 		Create();
+  CApplication(void);
+  virtual ~CApplication(void);
+  virtual HRESULT 			Initialize();
+  virtual void					FrameMove();
+  virtual void					Render();
+	virtual HRESULT 			Create();
 
-	void					Stop();
-	void					LoadSkin(const CStdString& strSkin);
-	void					DelayLoadSkin();
-	const CStdString&		CurrentFile();
-	virtual bool			OnMessage(CGUIMessage& message);
-	virtual	void			OnPlayBackEnded();
-	virtual	void			OnPlayBackStarted();
-	bool					PlayFile(const CStdString& strFile,bool bRestart=false);
-	void					StopPlaying();
-	void					Restart(bool bSamePosition=true);
-	void					EnableOverlay();
-	void					DisableOverlay();
-	bool					IsPlaying() const ;
-	bool					IsPlayingAudio() const ;
-	bool					IsPlayingVideo() const ;
-	void					OnKey(CKey& key);
-	void					RenderFullScreen();
-	bool					NeedRenderFullScreen();
-	void					SpinHD();
-	void				    CheckScreenSaver();		// CB: SCREENSAVER PATCH
-	void				    CheckShutdown();
-	void					SetCurrentSong(const CMusicInfoTag& tag);
-	void					SetCurrentMovie(const CIMDBMovie& tag);
-	void					ResetAllControls();
-	virtual void			Process();
-	void					ResetScreenSaver();
-	int						GetPlaySpeed() const;  
-	void					SetPlaySpeed(int iSpeed);
+	void									Stop();
+	void									LoadSkin(const CStdString& strSkin);
+	void									DelayLoadSkin();
+  const CStdString&     CurrentFile();
+	virtual bool					OnMessage(CGUIMessage& message);
+	virtual	void					OnPlayBackEnded();
+	virtual	void					OnPlayBackStarted();
+	bool									PlayFile(const CStdString& strFile,bool bRestart=false);
+  void                  StopPlaying();
+  void                  Restart(bool bSamePosition=true);
+	void									EnableOverlay();
+	void									DisableOverlay();
+	bool									IsPlaying() const ;
+	bool									IsPlayingAudio() const ;
+	bool									IsPlayingVideo() const ;
+	void									OnKey(CKey& key);
+	void									RenderFullScreen();
+  bool                  NeedRenderFullScreen();
+	void									SpinHD();
+	void				          CheckScreenSaver();		// CB: SCREENSAVER PATCH
+	void				          CheckShutdown();
+  void                  SetCurrentSong(const CMusicInfoTag& tag);
+  void                  SetCurrentMovie(const CIMDBMovie& tag);
+	void									ResetAllControls();
+  virtual void          Process();
+  void                  ResetScreenSaver();
+  int                   GetPlaySpeed() const;  
+  void                  SetPlaySpeed(int iSpeed);  
 
-	CGUIWindowHome						m_guiHome;
-	CGUIWindowPrograms					m_guiPrograms;
-	CGUIWindowSettingsPrograms			m_guiSettingsPrograms;
-	CGUIWindowPictures					m_guiPictures;
-	CGUIDialogYesNo						m_guiDialogYesNo;
-	CGUIDialogProgress					m_guiDialogProgress;
-	CGUIDialogOK						m_guiDialogOK;
-	CGUIWindowMyFiles					m_guiMyFiles;
-	CGUIWindowVideo						m_guiMyVideo;
-	CGUIWindowSettings					m_guiSettings;
-	CGUIWindowSystemInfo				m_guiSystemInfo;
-	CGUIWindowSettingsGeneral			m_guiSettingsGeneral;
-	CGUIWindowMusicInfo					m_guiMusicInfo;
-	CGUIWindowVideoInfo					m_guiVideoInfo;
-	CGUIWindowScriptsInfo				m_guiScriptsInfo;
+	CGUIWindowHome									m_guiHome;
+  CGUIWindowPrograms							m_guiPrograms;
+  CGUIWindowSettingsPrograms      m_guiSettingsPrograms;
+	CGUIWindowPictures							m_guiPictures;
+	CGUIDialogInvite								m_guiDialogInvite;
+	CGUIDialogYesNo									m_guiDialogYesNo;
+	CGUIDialogProgress							m_guiDialogProgress;
+	CGUIDialogOK										m_guiDialogOK;
+	CGUIWindowMyFiles								m_guiMyFiles;
+	CGUIWindowVideo									m_guiMyVideo;
+	CGUIWindowSettings							m_guiSettings;
+	CGUIWindowSystemInfo						m_guiSystemInfo;
+	CGUIWindowSettingsGeneral				m_guiSettingsGeneral;
+	CGUIWindowMusicInfo							m_guiMusicInfo;
+	CGUIWindowVideoInfo							m_guiVideoInfo;
+	CGUIWindowScriptsInfo						m_guiScriptsInfo;
 	//CGUIWindowSettingsScreen			m_guiSettingsScreen;		// now m_guiSettingsMyVideo
-	CGUIWindowSettingsUICalibration		m_guiSettingsUICalibration;
-	CGUIWindowSettingsScreenCalibration	m_guiSettingsScreenCalibration;
+	CGUIWindowSettingsUICalibration	m_guiSettingsUICalibration;
+	CGUIWindowSettingsScreenCalibration m_guiSettingsScreenCalibration;
 	CGUIWindowSettingsSlideShow			m_guiSettingsSlideShow;
-	CGUIWindowSettingsScreensaver		m_guiSettingsScreensaver;
+	CGUIWindowSettingsScreensaver			m_guiSettingsScreensaver;
 	//CGUIWindowSettingsOSD				m_guiSettingsOSD;
-	CGUIWindowSettingsAutoRun			m_guiSettingsAutoRun;
-	CGUIWindowScripts					m_guiScripts;
-	CGUIWindowSettingsFilter			m_guiSettingsFilter;
-	CGUIDialogSelect					m_guiDialogSelect;
-	CGUIDialogFileStacking				m_guiDialogFileStacking;
-	CGUIWindowMusicOverlay				m_guiMusicOverlay;
-	CGUIWindowFullScreen				m_guiWindowFullScreen;
-	CGUIWindowVideoOverlay				m_guiWindowVideoOverlay;
-	CGUIWindowVisualisation				m_guiWindowVisualisation;
+  CGUIWindowSettingsAutoRun       m_guiSettingsAutoRun;
+	CGUIWindowScripts								m_guiScripts;
+	CGUIWindowSettingsFilter				m_guiSettingsFilter;
+	CGUIDialogSelect								m_guiDialogSelect;
+	CGUIDialogFileStacking					m_guiDialogFileStacking;
+	CGUIWindowMusicOverlay					m_guiMusicOverlay;
+	CGUIWindowFullScreen						m_guiWindowFullScreen;
+	CGUIWindowVideoOverlay					m_guiWindowVideoOverlay;
+	CGUIWindowVisualisation					m_guiWindowVisualisation;
 	//CGUIWindowSettingsMusic				m_guiSettingsMusic;	// now m_guiSettingsMyMusic
-	CGUIWindowSlideShow					m_guiWindowSlideshow;
-	CGUIWindowMusicPlayList				m_guiMyMusicPlayList;
-	CGUIWindowVideoPlaylist				m_guiMyVideoPlayList;
-	CGUIWindowMusicSongs				m_guiMyMusicSongs;
-	CGUIWindowMusicAlbum				m_guiMyMusicAlbum;
-	CGUIWindowMusicArtists				m_guiMyMusicArtists;
-	CGUIWindowMusicGenres				m_guiMyMusicGenres;
-	CGUIWindowMusicTop100				m_guiMyMusicTop100;
-	CGUIWindowScreensaver				m_guiWindowScreensaver;
-	CGUIWindowSettingsLCD				m_guiSettingsLCD;
-	CGUIWindowSettingsSubtitles			m_guiSettingsSubtitles;
-	CGUIWindowVideoGenre				m_guiVideoGenre;
-	CGUIWindowVideoActors				m_guiVideoActors;
-	CGUIWindowVideoYear					m_guiVideoYear;
-	CGUIWindowVideoTitle				m_guiVideoTitle;
+	CGUIWindowSlideShow							m_guiWindowSlideshow;
+  CGUIWindowMusicPlayList					m_guiMyMusicPlayList;
+  CGUIWindowVideoPlaylist         m_guiMyVideoPlayList;
+	CGUIWindowMusicSongs						m_guiMyMusicSongs;
+  CGUIWindowMusicAlbum						m_guiMyMusicAlbum;
+	CGUIWindowMusicArtists					m_guiMyMusicArtists;
+	CGUIWindowMusicGenres						m_guiMyMusicGenres;
+	CGUIWindowMusicTop100						m_guiMyMusicTop100;
+	CGUIWindowScreensaver					m_guiWindowScreensaver;
+  CGUIWindowSettingsLCD           m_guiSettingsLCD;
+  CGUIWindowSettingsSubtitles     m_guiSettingsSubtitles;
+  CGUIWindowVideoGenre            m_guiVideoGenre;
+  CGUIWindowVideoActors           m_guiVideoActors;
+	CGUIWindowVideoYear             m_guiVideoYear;
+	CGUIWindowVideoTitle            m_guiVideoTitle;
 	CGUIWindowWeather					m_guiMyWeather;	//WEATHER
+	CGUIWindowBuddies						    m_guiMyBuddies;	//BUDDIES
 	CGUIWindowSettingsWeather			m_guiSettingsWeather; //WEATHER SETTINGS
 	CGUIWindowOSD						m_guiWindowOSD;
 	CGUIWindowSettingsCache				m_guiSettingsCache;
@@ -185,15 +189,15 @@ public:
 	CGUIWindowSettingsMyVideo			m_guiSettingsMyVideo;
 	CGUIWindowSettingsMyMusic			m_guiSettingsMyMusic;
 
-	CXBVirtualKeyboard   				m_keyboard;
-	CSNTPClient							m_sntpClient;
-	CDetectDVDMedia						m_DetectDVDType;
-	CAutorun							m_Autorun;
-	CDelayController					m_ctrDpad;
-	CDelayController					m_ctrIR;
-	CWebServer*							m_pWebServer;
-	CXBFileZilla*						m_pFileZilla;
-	IPlayer*							m_pPlayer;
+  CXBVirtualKeyboard   						m_keyboard;
+	CSNTPClient											m_sntpClient;
+	CDetectDVDMedia									m_DetectDVDType;
+	CAutorun												m_Autorun;
+	CDelayController								m_ctrDpad;
+	CDelayController								m_ctrIR;
+	CWebServer*											m_pWebServer;
+	CXBFileZilla*										m_pFileZilla;
+	IPlayer*												m_pPlayer;
 
 	bool		m_bSpinDown;
 	DWORD		m_dwSpinDownTime;
@@ -204,16 +208,16 @@ public:
 	DWORD		m_dwSkinTime;
 
 protected:
-	void                    UpdateLCD();
-	bool                    m_bXboxMediacenterLoaded,m_bSettingsLoaded,m_bCalibrationLoaded;;
-	CIMDBMovie              m_tagCurrentMovie;
-	CMusicInfoTag           m_tagCurrentSong;
-	int                     m_iPlaySpeed;
-	bool					m_bOverlayEnabled;
-	CStdString				m_strCurrentPlayer;
-	bool                    m_bAllSettingsLoaded;
-	CStdString              m_strCurrentFile;
-	D3DGAMMARAMP			m_OldRamp;			// CB: SCREENSAVER PATCH
+  void                    UpdateLCD();
+  bool                    m_bXboxMediacenterLoaded,m_bSettingsLoaded,m_bCalibrationLoaded;;
+  CIMDBMovie              m_tagCurrentMovie;
+  CMusicInfoTag           m_tagCurrentSong;
+  int                     m_iPlaySpeed;
+	bool										m_bOverlayEnabled;
+	CStdString							m_strCurrentPlayer;
+  bool                    m_bAllSettingsLoaded;
+  CStdString              m_strCurrentFile;
+  D3DGAMMARAMP m_OldRamp;			// CB: SCREENSAVER PATCH
 };
 
 extern CApplication g_application;
