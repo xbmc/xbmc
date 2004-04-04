b7ee4abf53bdd20a463de1ae8d2041e20f6c3366
runtime <runtime@svn>
2004-04-04 18:48:56 +0000

    runtime: Added MyBuddies (online gaming feature) and Invite dialog (online gaming scheduled invites).  ArenaItem and BuddyItem derive from GUIListExItem and implement specialized rendering of items that appear in the GUIListExControl member.  See GUIWindowsBuddies for a better understanding.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1321 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/GUIWindowBuddies.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "guiImage.h"
+#include "guiwindow.h"
+#include "guiList.h"
+#include "stdstring.h"
+#include "utils\UdpClient.h"
+#include "tinyxml/tinyxml.h"
+#include "guiDialogProgress.h"
+
+#include "utils/KaiClient.h"
+#include "BuddyItem.h"
+#include "ArenaItem.h"
+
+using namespace std;
+#include <map>
+
+class CGUIWindowBuddies : 	public CGUIWindow, public IBuddyObserver
+{
+public:
+	CGUIWindowBuddies(void);
+	virtual ~CGUIWindowBuddies(void);
+
+	virtual void			OnContactOffline(CStdString& aFriend);
+	virtual void			OnContactOnline(CStdString& aFriend);
+	virtual void			OnContactPing(CStdString& aFriend, CStdString& aVector, CStdString& aPing);
+	virtual void			OnContactRemove(CStdString& aFriend);
+	virtual void			OnEnterArena(CStdString& aVector);
+	virtual void			OnNewArena(CStdString& aVector);
+	virtual void			OnOpponentEnter(CStdString& aOpponent);
+	virtual void			OnOpponentPing(CStdString& aOpponent, CStdString& aPing);
+	virtual void			OnOpponentLeave(CStdString& aOpponent);
+	virtual void			OnSupportedTitle(DWORD aTitleId, CStdString& aVector);
+
+	virtual void			OnInitWindow();
+	virtual void			OnAction(const CAction &action);
+	virtual void			Render();
+
+protected:
+	enum State {Uninitialized,Buddies,Games,Arenas};
+
+	void OnClickModeButton	(CGUIMessage& aMessage);
+	void OnClickAddButton	(CGUIMessage& aMessage);
+	void OnClickRemoveButton(CGUIMessage& aMessage);
+	void OnClickInviteButton(CGUIMessage& aMessage);
+	void OnClickJoinButton	(CGUIMessage& aMessage);
+	void OnClickPlayButton	(CGUIMessage& aMessage);
+	void OnClickListItem	(CGUIMessage& aMessage);
+
+	void	QueryInstalledGames();
+	void	UpdatePanel();
+	void	UpdateFriends();
+	void	UpdateArena();
+	void	ChangeState();
+	void	ChangeState(State aNewState);
+
+	void	Enter(CArenaItem& aArena);
+	void	Play(CStdString& aVector);
+
+	static bool	SortFriends(CGUIItem* pStart, CGUIItem* pEnd);
+	static bool	SortArenaItems(CGUIItem* pStart, CGUIItem* pEnd);
+
+	CBuddyItem* GetBuddySelection();
+	CArenaItem* GetArenaSelection();
+
+	CGUISortedList			m_friends;
+	CGUISortedList			m_arena;
+	CGUISortedList			m_games;
+	State window_state;
+};
