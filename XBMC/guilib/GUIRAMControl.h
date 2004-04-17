a4ba71e6ab9e84f7c4bbaec96b5e058c07bcb5bb
runtime <runtime@svn>
2004-04-17 19:17:39 +0000

    runtime: Added thumbnail width, height, X & Y spacing, default image and text selected color and spacing to GUIRAMControl.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1413 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/GUIRAMControl.h
+++ XBMC/guilib/GUIRAMControl.h
@@ -46,7 +46,7 @@ public:
 	CGUIRAMControl(DWORD dwParentID, DWORD dwControlId,
 		DWORD dwPosX, DWORD dwPosY,	DWORD dwWidth, DWORD dwHeight, 
 		const CStdString& strFontName, const CStdString& strFont2Name,
-		D3DCOLOR dwTitleColor, D3DCOLOR dwNormalColor,
+		D3DCOLOR dwTitleColor, D3DCOLOR dwNormalColor, D3DCOLOR dwSelectedColor,
 		DWORD dwTextOffsetX, DWORD dwTextOffsetY);
 
 	virtual ~CGUIRAMControl(void);
@@ -63,10 +63,32 @@ public:
 
 	DWORD				GetTitleTextColor() const { return m_dwTitleColor;};
 	DWORD				GetNormalTextColor() const { return m_dwTextColor;};
+	DWORD				GetSelectedTextColor() const { return m_dwTextSelectColor;};
 	DWORD	GetTextOffsetX() const { return m_dwTextOffsetX;};
 	DWORD	GetTextOffsetY() const { return m_dwTextOffsetY;};
 	const CStdString&	GetFontName() const { return m_pFont->GetFontName(); };
 	const CStdString&	GetFont2Name() const { return m_pFont2->GetFontName(); };
+
+	void  SetTextSpacing(DWORD dwTextSpaceY) { m_dwTextSpaceY = dwTextSpaceY; };
+	DWORD  GetTextSpacing() const { return m_dwTextSpaceY; };
+
+	void  SetThumbAttributes(DWORD dwThumbWidth, DWORD dwThumbHeight, DWORD dwThumbSpaceX, DWORD dwThumbSpaceY, CStdString& strDefaultThumb)
+	{
+		m_dwThumbnailWidth = dwThumbWidth;
+		m_dwThumbnailHeight = dwThumbHeight;
+		m_dwThumbnailSpaceX = dwThumbSpaceX;
+		m_dwThumbnailSpaceY = dwThumbSpaceY;
+		m_strDefaultThumb = strDefaultThumb;
+	};
+
+	void  GetThumbAttributes(DWORD& dwThumbWidth, DWORD& dwThumbHeight, DWORD& dwThumbSpaceX, DWORD& dwThumbSpaceY, CStdString& strDefaultThumb) const
+	{
+		dwThumbWidth	= m_dwThumbnailWidth;
+		dwThumbHeight	= m_dwThumbnailHeight;
+		dwThumbSpaceX	= m_dwThumbnailSpaceX;
+		dwThumbSpaceY	= m_dwThumbnailSpaceY;
+		strDefaultThumb = m_strDefaultThumb;
+	};
 
 protected:
 
@@ -88,9 +110,11 @@ protected:
 	DWORD			m_dwThumbnailSpaceX;
 	DWORD			m_dwThumbnailSpaceY;
 	DWORD			m_dwTextSpaceY;
+	CStdString      m_strDefaultThumb;
 
 	D3DCOLOR		m_dwTitleColor;
 	D3DCOLOR		m_dwTextColor;
+	D3DCOLOR		m_dwTextSelectColor;
 	DWORD			m_dwTextOffsetX;
 	DWORD			m_dwTextOffsetY;
 	CGUIFont*		m_pFont;
