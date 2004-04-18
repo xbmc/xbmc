03a9e97d56367a2db870572a74d68fba6afd0c16
jwnmulder <jwnmulder@svn>
2004-04-18 18:56:08 +0000

    fixed, when no texture is supplied when creating python control, it will try to find the texture name in references.xml instead of using a hardcoded default.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1431 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/xbmcmodule/pyutil.h
+++ xbmc/lib/libPython/xbmcmodule/pyutil.h
@@ -16,6 +16,7 @@ namespace PYXBMC
 	int		PyGetUnicodeString(wstring& buf, PyObject* pObject, int pos = -1);
 	void	PyGUILock();
 	void	PyGUIUnlock();
+	const char*	GetDefaultImage(char* controlType, char* textureType, char* cDefault);
 }
 
 #ifdef __cplusplus
