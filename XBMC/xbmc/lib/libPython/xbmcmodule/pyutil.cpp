3db128774496944b2ae21182fbdb1e5baf73babb
jwnmulder <jwnmulder@svn>
2004-04-18 18:56:08 +0000

    fixed, when no texture is supplied when creating python control, it will try to find the texture name in references.xml instead of using a hardcoded default.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1431 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libPython/xbmcmodule/pyutil.cpp
+++ XBMC/xbmc/lib/libPython/xbmcmodule/pyutil.cpp
@@ -3,8 +3,12 @@
 #include <wchar.h>
 #include "stdstring.h"
 #include "graphiccontext.h"
+#include "..\..\..\..\guilib\tinyXML\tinyxml.h"
+#include "skininfo.h"
 
 static int iPyGUILockRef = 0;
+static TiXmlDocument pySkinReferences;
+static int iPyLoadedSkinReferences = 0;
 
 #pragma code_seg("PY_TEXT")
 #pragma data_seg("PY_DATA")
@@ -44,5 +48,55 @@ namespace PYXBMC
 			iPyGUILockRef--;
 			if (iPyGUILockRef == 0) g_graphicsContext.Unlock();	
 		}
+	}
+
+	/*
+	 * Looks in references.xml for image name
+	 * If none exist return default image name
+	 * iPyLoadedSkinReferences:
+	 * - 0 xml not loaded
+	 * - 1 tried to load xml, but it failed
+	 * - 2 xml loaded
+	 */
+	const char* GetDefaultImage(char* cControlType, char* cTextureType, char* cDefault)
+	{
+		if (iPyLoadedSkinReferences == 0)
+		{
+			// xml not loaded. We only try to load references.xml once
+			RESOLUTION res;
+			string strPath = g_SkinInfo.GetSkinPath("references.xml", &res);
+			if (pySkinReferences.LoadFile(strPath.c_str()))	iPyLoadedSkinReferences = true;
+			else return cDefault; // return default value
+				
+		}
+		else if (iPyLoadedSkinReferences == 1) return cDefault;
+
+		TiXmlElement *pControls = pySkinReferences.RootElement();
+
+		TiXmlNode *pNode = NULL;
+		TiXmlElement *pElement = NULL;
+
+		//find control element
+		while(pNode = pControls->IterateChildren("control", pNode))
+		{
+			pElement = pNode->FirstChildElement("type");
+			if (pElement) if (!strcmp(pElement->FirstChild()->Value(), cControlType)) break;
+		}
+		if (!pElement) return cDefault;
+
+		// get texture element
+		TiXmlElement *pTexture = pNode->FirstChildElement(cTextureType);
+		if (pTexture)
+		{
+			// found our textureType
+			pNode = pTexture->FirstChild();
+			if (pNode && pNode->Value()[0] != '-') return pNode->Value();
+			else
+			{
+				//set default
+				return cDefault;
+			}
+		}
+		return cDefault;
 	}
 }
