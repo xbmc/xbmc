999e3993e043a9bdfeceef3f438baa842097230f
po1ng <po1ng@svn>
2004-04-25 19:02:40 +0000

    updated linux build script
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1487 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/make-build.sh
+++ XBMC/make-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-#linux shell script to create a ready-to-upload xbmc build
+#linux shell script to create a ready-to-install xbmc build
 #from a cvs source tree containing compiled binaries
-#requires rar, xbepatch, unix2dos in path
 
 source="/xbmc/xbmc-src/XBMC"
-base="/xbmc/release"
+base="/xbmc/build"
+mkdir $base >/dev/null 2>&1
 dest="$base/xbmc-`date +%Y-%m-%d`"
 mkdir $dest
 
@@ -14,37 +14,36 @@ cp Release/default.xbe $dest
 cp *xml $dest
 cp *txt $base
 cp -r skin $dest
-
-#cp /xbmc/home.xml $dest/skin/MediaCenter
-# for replacing the default skin's home.xml with a customised version
-
-#cp -r /xbmc/HiFi $dest/skin
-#cp -r /xbmc/FlatStyle $dest/skin
-# optional skins
-
 cp -r language $dest
+cp -r weather $dest
+mkdir $dest/credits
+cp credits/* $dest/credits >/dev/null 2>&1
 mkdir $dest/media
 cp -r xbmc/keyboard/Media/* $dest/media
 cp -r visualisations $dest
 cp -r mplayer $dest
-#mkdir $dest/mplayer/codecs
-#cp /xbmc/win_dlls/*dll $dest/mplayer/codecs
-# for adding codec DLLs (see readme.txt in that directory in cvs)
 
+# win32 DLLs for wmv8/9, realmedia support (see XBMC/mplayer/codecs/readme.txt)
+cp /xbmc/win_dlls/*dll $dest/mplayer/codecs
+
+# images and other files needed for the webserver built into xbmc
 mkdir $dest/web
 cd $dest/web
-rar x $source/web/xbmc.rar >/dev/null
+rar x -inul /xbmc/xbmc-src/XBMC/web/xbmc.rar
 
+# this is to make the txt and xml files display ok in crappy windows editors like
+# notepad, xbmc itself reads unix or dos format without problems
 cd $base
-unix2dos *txt
-# XBMC can read unix format textfiles fine, this is just so the
-# files display ok in crappy windows editors like notepad.exe
-
+unix2dos *txt *nfo >/dev/null 2>&1
 cd $dest
 unix2dos *xml
-rm -rf `find . -name "CVS*" -type d`
-find skin -iname \*.png -exec rm -f "{}" \;
-# xbmc only needs the XPR format skin files
 
+find . -iname CVS\* -type d -exec rm -rf "{}" >/dev/null 2>&1 \;
+find . -iname thumbs.db -or -name \*.h -type f -exec rm -f "{}" >/dev/null 2>&1 \;
+
+# xbmc only loads textures.xpr, .pngs are not needed (except for weather)
+find skin -iname \*.png -type f -exec rm -f "{}" \;
+
+# to make the xbe work with very old modchips
 xbepatch default.xbe retail.xbe
 mv retail.xbe default.xbe
