175c7c2d8a6d763fe01f09fd51e5501c2bdf1878
jwnmulder <jwnmulder@svn>
2004-05-02 21:23:15 +0000

    *** empty log message ***
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/mplayer@1563 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- libmpdvdkit2/device.c
+++ libmpdvdkit2/device.c
@@ -254,10 +254,17 @@ static int libc_open ( dvdcss_t dvdcss, char const *psz_device )
 }
 
 #if defined( WIN32 )
+#ifdef _XBOX
+extern HANDLE xboxopendvdrom();
+#endif //_XBOX
 static int win2k_open ( dvdcss_t dvdcss, char const *psz_device )
 {
-    char psz_dvd[7];
+    char psz_dvd[70];
+#ifdef _XBOX
+    strcpy(psz_dvd,"\\Device\\Cdrom0");
+#else
     _snprintf( psz_dvd, 7, "\\\\.\\%c:", psz_device[0] );
+#endif //_XBOX
 
     /* To work around an M$ bug in IOCTL_DVD_READ_STRUCTURE, we need read
      * _and_ write access to the device (so we can make SCSI Pass Through
@@ -267,23 +274,36 @@ static int win2k_open ( dvdcss_t dvdcss, char const *psz_device )
      * won't send back the right result).
      * (See Microsoft Q241374: Read and Write Access Required for SCSI
      * Pass Through Requests) */
-    (HANDLE) dvdcss->i_fd =
+#ifdef _XBOX
+    if (!dvdcss->b_file)
+    {
+      printf("open dvdrom\n");
+     (HANDLE) dvdcss->i_fd = xboxopendvdrom();
+    }
+    else
+    {
+      printf("open file\n");
+#endif //_XBOX
+      (HANDLE) dvdcss->i_fd =
                 CreateFile( psz_dvd, GENERIC_READ | GENERIC_WRITE,
-                            FILE_SHARE_READ | FILE_SHARE_WRITE,
-                            NULL, OPEN_EXISTING,
-                            FILE_FLAG_RANDOM_ACCESS, NULL );
+                              FILE_SHARE_READ | FILE_SHARE_WRITE,
+                              NULL, OPEN_EXISTING,
+                              FILE_FLAG_RANDOM_ACCESS, NULL );
 
-    if( (HANDLE) dvdcss->i_fd == INVALID_HANDLE_VALUE )
+    	if( (HANDLE) dvdcss->i_fd == INVALID_HANDLE_VALUE )
         (HANDLE) dvdcss->i_fd =
                     CreateFile( psz_dvd, GENERIC_READ, FILE_SHARE_READ,
                                 NULL, OPEN_EXISTING,
                                 FILE_FLAG_RANDOM_ACCESS, NULL );
 
-    if( (HANDLE) dvdcss->i_fd == INVALID_HANDLE_VALUE )
-    {
+    	if( (HANDLE) dvdcss->i_fd == INVALID_HANDLE_VALUE )
+    	{
         _dvdcss_error( dvdcss, "failed opening device" );
         return -1;
+    	}
+#ifdef _XBOX
     }
+#endif //_XBOX
 
     dvdcss->i_pos = 0;
 
