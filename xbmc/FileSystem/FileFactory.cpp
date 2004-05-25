8977454ec68ced87464dd2ca7a016b3a180daa4c
jwnmulder <jwnmulder@svn>
2004-05-25 13:52:46 +0000

    fixed: xbmc was starting the shoutcast streamripper for every stream with 'http://', removed so movie streams start a bit faster.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1681 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FileFactory.cpp
+++ xbmc/FileSystem/FileFactory.cpp
@@ -26,37 +26,15 @@ IFile* CFileFactory::CreateLoader(const CStdString& strFileName)
 {
 	CURL url(strFileName);
 	CStdString strProtocol=url.GetProtocol();
-	if (strProtocol=="iso9660")
-	{
-		return (IFile*)new CFileISO();
-	}
-	
-	if (strProtocol=="xns")
-	{
-		return (IFile*)new CFileRelax();
-	}
 
-	if (strProtocol=="smb")
-	{
-		return (IFile*)new CFileSMB();
-	}
-
-	if (strProtocol=="xbms")
-	{
-		return (IFile*)new CFileXBMSP();
-	}
-	if (strProtocol=="shout" || strProtocol=="http")
-	{
-		return (IFile*)new CFileShoutcast();
-	}
-	if (strProtocol=="rtv")
-	{
-		return (IFile*)new CFileRTV();
-	}
-	if (strProtocol=="soundtrack")
-	{
-		return (IFile*)new CFileSndtrk();
-	}
+	if (strProtocol=="http")				return NULL;
+	if (strProtocol=="iso9660")			return (IFile*)new CFileISO();
+	if (strProtocol=="xns")					return (IFile*)new CFileRelax();
+	if (strProtocol=="smb")					return (IFile*)new CFileSMB();
+	if (strProtocol=="xbms")				return (IFile*)new CFileXBMSP();
+	if (strProtocol=="shout")				return (IFile*)new CFileShoutcast();
+	if (strProtocol=="rtv")					return (IFile*)new CFileRTV();
+	if (strProtocol=="soundtrack")	return (IFile*)new CFileSndtrk();
 
   return (IFile*)new CFileHD();
 }
