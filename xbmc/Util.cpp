2e2fad02510e3e4dd15021cf0a9f1a341ef09b30
jwnmulder <jwnmulder@svn>
2004-06-01 14:51:44 +0000

    fixed, LCD doesn't show ���.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1694 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -1311,7 +1311,7 @@ void CUtil::SetThumb(CFileItem* pItem)
 {
   CStdString strThumb;
   // set the thumbnail for an file item
-  
+
   // if it already has a thumbnail, then return
   if ( pItem->HasThumbnail() ) return;
 
@@ -2600,6 +2600,39 @@ void CUtil::Stat64ToStat(struct _stat *result, struct __stat64 *stat)
 	result->st_atime = (time_t)stat->st_atime;
 	result->st_mtime = (time_t)stat->st_mtime;
 	result->st_ctime = (time_t)stat->st_ctime;
+}
+
+void CUtil::StringToLCDCharSet(CStdString& strText)
+{
+	unsigned char cTest;
+	char cLCD;
+
+	for (int i=0; i < (int)strText.size(); ++i)
+	{
+		// display is not supporting all asci characters.
+		cTest = strText.at(i);
+		if (cTest >= 127)
+		{
+			if (cTest == 228) cLCD = char(225);      // fix for ä
+			else if (cTest == 246) cLCD = char(239); // fix for ö
+			else if (cTest == 252) cLCD = char(245); // fix for ü
+
+			else if (cTest >= 192 && cTest <= 197) cLCD = char(65);  // display as A
+			else if (cTest >= 200 && cTest <= 203) cLCD = char(69);  // display as E
+			else if (cTest >= 204 && cTest <= 207) cLCD = char(73);  // display as I
+			else if (cTest >= 210 && cTest <= 214) cLCD = char(79);  // display as O
+			else if (cTest >= 217 && cTest <= 220) cLCD = char(85);  // display as U
+
+			else if (cTest >= 224 && cTest <= 229) cLCD = char(97);  // display as a
+			else if (cTest >= 232 && cTest <= 235) cLCD = char(101); // display as e
+			else if (cTest >= 236 && cTest <= 239) cLCD = char(105); // display as i
+			else if (cTest >= 242 && cTest <= 246) cLCD = char(111); // display as o
+			else if (cTest >= 249 && cTest <= 252) cLCD = char(117); // display as u
+			else cLCD = ' ';
+
+			strText.SetAt(i, cLCD);
+		}
+	}
 }
 
 // around 50% faster than memcpy
