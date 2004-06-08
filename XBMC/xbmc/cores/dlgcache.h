b17f6cc3773cd27bd2d0da0550a4600214211252
elupus <elupus@svn>
2004-06-08 21:34:34 +0000

    added: [ 905913 ] abilty to cancel startup of video/audiostreams.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1731 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dlgcache.h
+++ XBMC/xbmc/cores/dlgcache.h
@@ -9,6 +9,7 @@ class CDlgCache
 	  virtual ~CDlgCache();
 	  void Update();
 	  void SetMessage(const CStdString& strMessage);
+	  bool IsCanceled() const { return m_pDlg->IsCanceled(); }
   protected:
     CGUIDialogProgress* m_pDlg;
     CStdString m_strLinePrev;
