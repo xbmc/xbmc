21d72d1913f92e5b403e12b5555faab91f595cd1
jwnmulder <jwnmulder@svn>
2004-07-02 20:46:56 +0000

    - fixed: crash in debug build when an unresolved dll symbol was called + lot's off other fixes and additions to the dll loader. - added: support for vp4 / vp5 and vp6 (You need vp4vfw.dll and vp5vfw.dll from 'http://mplayerhq.hu' and vp6vfw.dll from 'http://www.on2.com/vp6.php3')
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@1759 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/DllLoader/dll.cpp
+++ XBMC/xbmc/cores/DllLoader/dll.cpp
@@ -12,16 +12,11 @@
 
 #include "../../utils/log.h"
 
+// define if you want to output all function names which the dll call's
+//#define OUPUT_CALLED_FUNCTIONS
+
 using namespace std;
 Exp2Dll *Head = 0;
-extern "C" int dummy_Unresolved(void);
-extern "C" int dummy_Kernel32Unresolved(void);
-extern "C" int dummy_User32Unresolved(void);
-extern "C" int dummy_OLE32Unresolved(void);
-extern "C" int dummy_MSVCRTUnresolved(void);
-extern "C" int dummy_MSVCR71Unresolved(void);
-extern "C" int dummy_DX8Unresolved(void);
-static int ResolveName(char *Name, char* Function, void **Fixup);
 
 //hack to Free pe image, global variable.
 DllLoader * wmaDMOdll;
@@ -29,13 +24,14 @@ DllLoader * wmvDMOdll;
 DllLoader * wmsDMOdll;
 
 // Allocation tracking for vis modules that leak
-
 struct DllTrackInfo {
 	DllLoader* pDll;
 	unsigned MinAddr;
 	unsigned MaxAddr;
 	std::map<unsigned, unsigned> AllocList;
 };
+
+int ResolveName(char*, char*, void **);
 
 std::list<DllTrackInfo> TrackedDlls;
 typedef std::list<DllTrackInfo>::iterator TrackedDllsIter;
@@ -111,25 +107,157 @@ extern "C" void __cdecl track_free(void* p)
 	free(p);
 }
 
-
-// unresolved function tracking
-#ifdef _DEBUG
-std::map<unsigned, std::string> UnresolvedList;
-
-void LookupUnresolved(unsigned ret_addr, char* name, int len)
+std::list<unsigned long*> AllocatedFunctionList;
+int iDllDummyOutputCall = 0;
+extern "C" void dll_dummy_output(char* dllname, char* funcname)
 {
-	unsigned call_ptr = *(unsigned*)(ret_addr-4) + (ret_addr-5);
-	unsigned reloc = *(unsigned*)(call_ptr+2);
-	std::map<unsigned, std::string>::iterator p = UnresolvedList.find(reloc);
-	if (p != UnresolvedList.end())
-	{
-		strncpy(name, p->second.c_str(), len-1);
-		name[len-1] = 0;
-	}
-	else
-		*name = 0;
+	CLog::Log("%s: Unresolved function called (%s), Count number %d", dllname, funcname, ++iDllDummyOutputCall);
 }
-#endif
+
+// this piece of asm code only calls dll_dummy_output(s, s) and will return NULL
+unsigned char dummy_func[] = {
+		0x55,                           // push        ebp
+		0x8b, 0xec,                     // mov         ebp,esp
+		0xa1, 0, 0, 0, 0,               // mov         eax,dword ptr [0 0 0 0]
+		0x50,                           // push        eax
+		0xa1, 0, 0, 0, 0,               // mov         eax,dword ptr [0 0 0 0]
+		0x50,                           // push        eax
+		0xff, 0x15, 0, 0, 0, 0,         // call        dword ptr[dll_dummy_output]
+		0x83, 0xc4,	0x08,               // add         esp,8
+		0x33, 0xc0,                     // xor         eax,eax       // return NULL
+		0x5d,                           // pop         ebp
+		0xc3                            // ret
+};
+
+/* Create a new callable function
+ * This allocates a few bytes with the next content
+ * 
+ * 1 function in assembly code (dummy_func)
+ * 2 datapointer               (pointer to dll string)
+ * 3 datapointer               (pointer to function string)
+ * 4 datapointer               (pointer to function string)
+ * 5 string                    (string of chars representing dll name)
+ * 6 string                    (string of chars representing function name)
+ */
+void MakeDummyFunction(unsigned long* Addr, const char* strDllName, const char* strFunctionName)
+{
+	unsigned int iFunctionSize = sizeof(dummy_func);
+	unsigned int iDllNameSize = strlen(strDllName) + 1;
+	unsigned int iFunctionNameSize = strlen(strFunctionName) + 1;
+
+	// allocate memory for function + strings + 3 x 4 bytes for three datapointers
+	char* pData = new char[iFunctionSize + 12 + iDllNameSize + iFunctionNameSize];
+
+	char* offDataPointer1 = pData + iFunctionSize;
+	char* offDataPointer2 = pData + iFunctionSize + 4;
+	char* offDataPointer3 = pData + iFunctionSize + 8;
+	char* offStringDll = pData + iFunctionSize + 12;
+	char* offStringFunc = pData + iFunctionSize + 12 + iDllNameSize;
+
+	// 1 copy assembly code
+	memcpy(pData, dummy_func, iFunctionSize);
+
+	// insert pointers to datapointers into assembly code (fills 0x00000000 in dummy_func)
+	*(int*)(pData + 4) = (int)offDataPointer1;
+	*(int*)(pData + 10) = (int)offDataPointer2;
+	*(int*)(pData + 17) = (int)offDataPointer3;
+
+	// 2 fill datapointer with pointer to 5 (string)
+	*(int*)offDataPointer1 = (int)offStringFunc;
+	// 3 fill datapointer with pointer to 6 (string)
+	*(int*)offDataPointer2 = (int)offStringDll;
+	// 4 fill datapointer with pointer to dll_dummy_output
+	*(int*)offDataPointer3 = (int)dll_dummy_output;
+
+	// copy arguments to 5 (string) and 6 (string)
+	memcpy(offStringDll, strDllName, iDllNameSize);
+	memcpy(offStringFunc, strFunctionName, iFunctionNameSize);
+
+	// bind new function to Addr
+	*Addr = (unsigned long)pData;
+
+	// add it to AllocatedFunctionList so we can free the just created functions if we want
+	AllocatedFunctionList.push_back((unsigned long*)pData);
+}
+
+#ifdef OUPUT_CALLED_FUNCTIONS
+static char calledtmp[2048];
+
+extern "C" void dll_called_output(char* dllname, char* funcname)
+{
+	sprintf(calledtmp, "%s: called (%s)\n", dllname, funcname);
+	OutputDebugString(calledtmp);
+}
+
+// this piece of asm code calls dll_called_output(s, s) and will call function after it
+unsigned char dll_func[] = {
+
+		//0x8f, 0x05,	0, 0, 0, 0,	// pop ptr
+		//0xff, 0x35, 0, 0, 0, 0, // push ptr
+		//0x8b, 0xec,                     // mov         ebp,esp
+		//0x89, 0x25, 0, 0, 0, 0,         // mov         dword ptr [0 0 0 0], esp
+		0xe9, 0, 0, 0, 0,         // call        dword ptr[function]
+		//0x8b, 0x25,	0, 0, 0, 0,         // mov         esp, dword ptr [0 0 0 0]
+		//0x83, 0xc4,	0x00,               // add         esp, 16
+
+
+		//0xff, 0x35, 0, 0, 0, 0, // push ptr
+		//0xcb//0xc3,                           // ret
+};
+
+void MakeFunction(unsigned long* Addr, const char* strDllName, const char* strFunctionName, void* function)
+{
+	unsigned int iFunctionSize = sizeof(dll_func);
+	unsigned int iDllNameSize = strlen(strDllName) + 1;
+	unsigned int iFunctionNameSize = strlen(strFunctionName) + 1;
+
+	// allocate memory for function + strings + 4 x 4 bytes for three datapointers
+	char* pData = new char[iFunctionSize + 20 + iDllNameSize + iFunctionNameSize];
+
+	char* offDataPointer0 = pData + iFunctionSize;
+	char* offDataPointer1 = pData + iFunctionSize + 4;
+	char* offDataPointer2 = pData + iFunctionSize + 8;
+	char* offDataPointer3 = pData + iFunctionSize + 12;
+	char* offDataPointer4 = pData + iFunctionSize + 16;
+
+	char* offStringDll = pData + iFunctionSize + 20;
+	char* offStringFunc = pData + iFunctionSize + 20 + iDllNameSize;
+
+	// 1 copy assembly code
+	memcpy(pData, dll_func, iFunctionSize);
+
+	// insert pointers to datapointers into assembly code (fills 0x00000000)
+	int p;
+	if ((int)function < ((int)pData + 5)) p = -(((int)pData + 5) - (int)function);
+	else p = -((int)function - ((int)pData + 5));
+	*(int*)(pData + 1) = p;//(int)offDataPointer0; // esp pointer
+	//*(int*)(pData + 8) = (int)offDataPointer1;
+	//*(int*)(pData + 14) = (int)offDataPointer0;
+	//*(int*)(pData + 20) = (int)offDataPointer0;
+	//*(int*)(pData + 32) = (int)offDataPointer4;
+	//*(int*)(pData + 38) = (int)offDataPointer0; // esp pointer
+
+	// fill datapointers
+	//*(int*)offDataPointer0 = (int)function;
+	//*(int*)offDataPointer1 = (int)pData + 12;
+	//*(int*)offDataPointer0 = (int)function;
+	/**(int*)offDataPointer1 = (int)offStringFunc;
+	*(int*)offDataPointer2 = (int)offStringDll;
+	*(int*)offDataPointer3 = (int)dll_called_output;
+	*(int*)offDataPointer4 = (int)function;
+
+	// copy arguments to 5 (string) and 6 (string)
+	memcpy(offStringDll, strDllName, iDllNameSize);
+	memcpy(offStringFunc, strFunctionName, iFunctionNameSize);*/
+
+	// bind new function to Addr
+	*Addr = (unsigned long)pData;
+
+	// add it to AllocatedFunctionList so we can free the just created functions if we want
+	AllocatedFunctionList.push_back((unsigned long*)pData);
+}
+
+#endif // OUPUT_CALLED_FUNCTIONS
 
 #ifdef DUMPING_DATA
 
@@ -294,9 +422,16 @@ int DllLoader::ResolveImports(void)
 						char szBuf[128];
 						sprintf(szBuf,"unable to resolve ordinal %s %d\n",Name,*Table&0x7ffffff);
 						OutputDebugString(szBuf);
-						*Addr = (unsigned long) dummy_Unresolved;
+						sprintf(szBuf, "%d", *Table&0x7ffffff);
+						MakeDummyFunction(Addr, Name, szBuf);
 					} else { 
+#ifdef OUPUT_CALLED_FUNCTIONS
+						char szBuf[128];
+						sprintf(szBuf, "%d", *Table&0x7ffffff);
+            MakeFunction(Addr, Name, szBuf, Fixup);
+#else
 						*Addr = (unsigned long)Fixup;  //woohoo!!
+#endif
 					}
 				}     
 				else
@@ -309,23 +444,7 @@ int DllLoader::ResolveImports(void)
 					if( !ResolveName(Name, ImpName, &Fixup) )
 					{
 						CLog::DebugLog("Unable to resolve %s %s",Name,ImpName);
-#ifdef _DEBUG
-						UnresolvedList[(unsigned)Addr] = ImpName;
-#endif
-						if (strstr(Name,"KERNEL32")  || strstr(Name,"kernel32") )
-							*Addr = (unsigned long) dummy_Kernel32Unresolved;
-						else if (strstr(Name,"USER32")  || strstr(Name,"user32") )
-							*Addr = (unsigned long) dummy_User32Unresolved;
-						else if (strstr(Name,"ole32")  || strstr(Name,"ole32") )
-							*Addr = (unsigned long) dummy_OLE32Unresolved;
-						else if (strstr(Name,"MSVCRT")  || strstr(Name,"msvcrt") )
-							*Addr = (unsigned long) dummy_MSVCRTUnresolved;
-						else if (strstr(Name,"MSVCR71")  || strstr(Name,"msvcr71") )
-							*Addr = (unsigned long) dummy_MSVCR71Unresolved;
-						else if (strstr(Name,"xbox_dx8")  || strstr(Name,"XBOX_DX8") )
-							*Addr = (unsigned long) dummy_DX8Unresolved;
-						else
-							*Addr = (unsigned long) dummy_Unresolved;
+						MakeDummyFunction(Addr, Name, ImpName);
 						bResult=0;
 					} else {
 						if (pList)
@@ -348,8 +467,11 @@ int DllLoader::ResolveImports(void)
 								Fixup = track_free;
 							}
 						}
-
+#ifdef OUPUT_CALLED_FUNCTIONS
+            MakeFunction(Addr, Name, ImpName, Fixup);
+#else
 						*Addr = (unsigned long)Fixup;
+#endif
 					}
 				}
 				Table++;
@@ -410,8 +532,6 @@ int DllLoader::LoadExports()
 	}
 	return 0;
 }
-
-
 
 int DllLoader::ResolveExport(char *_Name, void **Addr)
 {
@@ -552,9 +672,10 @@ DllLoader::~DllLoader()
 	ImportDirTable = 0;
 	delete [] Dll;
 
-#ifdef _DEBUG
-	UnresolvedList.clear();
-#endif // _DEBUG
+	// free all functions which where created at the time we loaded the dll
+	std::list<unsigned long*>::iterator unIt = AllocatedFunctionList.begin();
+	while (unIt != AllocatedFunctionList.end()) { delete(*unIt); unIt++;	}
+	AllocatedFunctionList.clear();
 }
 
 int DllLoader::Parse()
@@ -655,8 +776,11 @@ extern "C" HMODULE __stdcall dllLoadLibraryA(LPCSTR libname)
 		strcmp(libname, "KERNEL32.DLL") == 0 || strcmp(libname, "KERNEL32") == 0 )
 		return (HMODULE)MODULE_HANDLE_kernel32;
 
-	char* plibname = new char[strlen(libname)+120]; 
-	sprintf(plibname, "%s\\%s", DEFAULT_DLLPATH ,(char *)libname);
+	char plibname[MAX_PATH+1];
+	if (strlen(libname) > 1 && libname[1] == ':')
+		sprintf(plibname, "%s", (char *)libname);
+	else
+		sprintf(plibname, "%s\\%s", DEFAULT_DLLPATH ,(char *)libname);
 	DllLoader * dllhandle = new DllLoader(plibname);
 	delete[] plibname;
 
@@ -683,10 +807,9 @@ extern "C" HMODULE __stdcall dllLoadLibraryA(LPCSTR libname)
 
 	void * address = NULL;
 	dllhandle->ResolveExport("DllMain", &address);
-	if (address)
-		dllhandle->EntryAddress = (unsigned long)address;
+	if (address) dllhandle->EntryAddress = (unsigned long)address;
 	EntryFunc * initdll = (EntryFunc *)dllhandle->EntryAddress;
-	(*initdll)( (HINSTANCE) dllhandle->hModule, 1 ,0);	//call "DllMian" with DLL_PROCESS_ATTACH
+	(*initdll)((HINSTANCE) dllhandle->hModule, 1 ,0);	//call "DllMian" with DLL_PROCESS_ATTACH
 
 	//#define DLL_PROCESS_ATTACH   1    
 	//#define DLL_THREAD_ATTACH    2    
@@ -728,104 +851,39 @@ extern "C" FARPROC __stdcall dllGetProcAddress( HMODULE hModule, LPCSTR function
 	dllhandle->ResolveExport((char *)function, &address);
 	return (FARPROC) address;
 }
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
-}
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_Kernel32Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("KERNEL32: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
+
+extern "C" DWORD WINAPI dllGetModuleFileNameA(HMODULE hModule, LPSTR lpFilename, DWORD nSize)
+{
+	if (NULL == hModule)
+	{
+		// for now, only return full path
+		strncpy(lpFilename, "q:\\mplayer\\codecs", nSize);
+		return 17; //strlen lpFilename
+	}
+	OutputDebugString("kernel32.dll incomplete function GetModuleFileNameA called\n");	//warning
+	return NULL;
 }
 
+extern "C" HMODULE WINAPI dllGetModuleHandleA(LPCSTR lpModuleName)
+{
+	/*
+	If the file name extension is omitted, the default library extension .dll is appended.
+	The file name string can include a trailing point character (.) to indicate that the module name has no extension.
+	The string does not have to specify a path. When specifying a path, be sure to use backslashes (\), not forward slashes (/).
+	The name is compared (case independently)
+	If this parameter is NULL, GetModuleHandle returns a handle to the file used to create the calling process (.exe file).
+	*/
+	char* strModuleName = new char[strlen(lpModuleName) + 5];
+	strcpy(strModuleName, lpModuleName);
 
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_User32Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("USER32: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
-}
+	if(strrchr(strModuleName, '.') == 0) strcat(strModuleName, ".dll");
 
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_OLE32Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("OLE32: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
-}
+	if (stricmp(strModuleName, "kernel32.dll") == 0) return (HMODULE)MODULE_HANDLE_kernel32;
 
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_MSVCRTUnresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("MSVCRT: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
-}
-
-
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_MSVCR71Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("MSVCRT71: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
-}
-
-
-//dummy functions used to catch unresolved function calls
-extern "C" int dummy_DX8Unresolved(void) {
-#ifdef _DEBUG
-	static int Count = 0;
-	DWORD rtn_addr;
-	__asm { mov eax, [ebp+4] }
-	__asm { mov rtn_addr, eax }
-	char name[32];
-	LookupUnresolved(rtn_addr, name, 32);
-	CLog::DebugLog("DX8: Unresolved function called from %#08x (%s), Count number %d", rtn_addr, name, Count++);
-#endif // _DEBUG
-	return 1;
+	// need to find other handles too, return NULL for now
+	OutputDebugString("kernel32.dll incomplete function GetModuleHandleA called\n");	//warning
+	delete []strModuleName;
+	return NULL;
 }
 
 static int ResolveName(char *Name, char* Function, void **Fixup)
@@ -845,3 +903,4 @@ static int ResolveName(char *Name, char* Function, void **Fixup)
 	*Fixup = NULL;
 	return 0;
 }
+
