ac4cbf6610f60805f25672570ec33d1ac973c7c3
jwnmulder <jwnmulder@svn>
2004-07-07 13:29:06 +0000

    fixed compile error
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1794 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/mplayer.cpp
+++ xbmc/cores/mplayer.cpp
@@ -649,7 +649,7 @@ CMPlayer::~CMPlayer()
 		delete entry;
 	}	//fix winnt and xbox critical data mismatch issue.
 	free_registry();	//free memory take by registry structures
-	g_smb.Purge(); // close any open smb sessions
+	smb.Purge(); // close any open smb sessions
 }
 bool CMPlayer::load()
 {
