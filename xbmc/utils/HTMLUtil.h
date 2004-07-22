af3747c300eb0d7dbb917884bd1428f60f9695c9
bobbin007 <bobbin007@svn>
2004-07-22 21:54:08 +0000

    fixed: album info from allmusic.com
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1820 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/HTMLUtil.h
+++ xbmc/utils/HTMLUtil.h
@@ -9,6 +9,7 @@ namespace HTML
 		CHTMLUtil(void);
 		virtual ~CHTMLUtil(void);
 		int FindTag(const CStdString& strHTML,const CStdString& strTag,CStdString& strtagFound, int iPos=0) const;
+		int FindClosingTag(const CStdString& strHTML,const CStdString& strTag, CStdString& strtagFound, int iPos) const;
 		void getValueOfTag(const CStdString& strTagAndValue, CStdString& strValue);
 		void getAttributeOfTag(const CStdString& strTagAndValue,const CStdString& strTag, CStdString& strValue);
 		void RemoveTags(CStdString& strHTML);
