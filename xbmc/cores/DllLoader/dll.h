6125b3913b76dd75c2be1dd22032a434b71fe4b5
savvy <savvy@svn>
2004-08-11 00:44:27 +0000

    support for QuickTime DLLs
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1874 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/dll.h
+++ xbmc/cores/DllLoader/dll.h
@@ -7,35 +7,40 @@
 #include "coffldr.h"
 #include "exp2dll.h"
 
+//#define DUMPING_DATA
+
 class DllLoader : public CoffLoader {
 protected:
 // Just pointers; dont' delete...
-    ImportDirTable_t        *ImportDirTable;
-    ExportDirTable_t        *ExportDirTable;
-    char    *Dll;
+	ImportDirTable_t	*ImportDirTable;
+	ExportDirTable_t	*ExportDirTable;
+	char	*Dll;
+	int		refcount;
 	typedef struct _ExpList {
 		Exp2Dll* exp;
 		_ExpList* next;
 	} ExpList;
-	ExpList					*Exports;
+	ExpList				*Exports;
 
 #ifdef DUMPING_DATA
-    void    PrintImportLookupTable(unsigned long ImportLookupTable_RVA);
-    void    PrintImportTable(ImportDirTable_t *ImportDirTable);
+	void    PrintImportLookupTable(unsigned long ImportLookupTable_RVA);
+	void    PrintImportTable(ImportDirTable_t *ImportDirTable);
 #endif
-    void    PrintExportTable(ExportDirTable_t *ExportDirTable);
-    int     ResolveOrdinal(char*, unsigned long, void**);
-    int     ResolveName(char*, char*, void **);
-    int     LoadExports();
+	void    PrintExportTable(ExportDirTable_t *ExportDirTable);
+	int     ResolveOrdinal(char*, unsigned long, void**);
+	int     ResolveName(char*, char*, void **);
+	int     LoadExports();
 	
 public:
     DllLoader(const char *dll, bool track = false);
     ~DllLoader();
 
-    int     Parse(void);
-    int     ResolveImports(void);
-    int     ResolveExport(char*, void**);
-
+	int     Parse(void);
+	int     ResolveImports(void);
+	int     ResolveExport(char*, void**);
+	char*	GetDLLName(void);
+	int		IncrRef();
+	int		DecrRef();
 };
 
 typedef BOOL WINAPI EntryFunc(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved);
