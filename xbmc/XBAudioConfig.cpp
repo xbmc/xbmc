2cbd4761407031f2f0c1003fea8479e64b9b1f0d
jmarshallnz <jmarshallnz@svn>
2004-08-20 17:51:07 +0000

    - 20-08-2004 changed: mplayers SeekTime() now is in ms for better seek accuracy for .cue files etc.   - 20-08-2004 changed: Updated the mplayer interface to handle the new DTS decoding capabilities.   - 20-08-2004 changed: Removed AC3/DTS pass through options.  Will always passthrough unless it can't (non-48kHz),                         or the user chooses to output stereo files to all speakers.   - 20-08-2004 changed: Audio options now set the MS Dash settings for AC3/DTS.   - 20-08-2004 changed: Music Database Reorg now Clean Music Database - just removes moved/deleted songs.   - 20-08-2004 changed: Database updates for My music to better streamline queries + size.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1957 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/XBAudioConfig.cpp
@@ -0,0 +1,70 @@
+#include "XBAudioConfig.h"
+
+XBAudioConfig g_audioConfig;
+
+XBAudioConfig::XBAudioConfig()
+{
+	m_dwAudioFlags = XGetAudioFlags();
+}
+
+XBAudioConfig::~XBAudioConfig()
+{
+}
+
+bool XBAudioConfig::HasDigitalOutput()
+{
+	DWORD dwAVPack = XGetAVPack();
+	if (dwAVPack == XC_AV_PACK_SCART ||
+		dwAVPack == XC_AV_PACK_HDTV ||
+		dwAVPack == XC_AV_PACK_SVIDEO)
+		return true;
+	return false;
+}
+
+void XBAudioConfig::SetAC3Enabled(bool bEnable)
+{
+	if (bEnable)
+		m_dwAudioFlags |= XC_AUDIO_FLAGS_ENABLE_AC3;
+	else
+		m_dwAudioFlags &= ~XC_AUDIO_FLAGS_ENABLE_AC3;
+}
+
+bool XBAudioConfig::GetAC3Enabled()
+{
+	if (!HasDigitalOutput()) return false;
+	return (XC_AUDIO_FLAGS_ENCODED(XGetAudioFlags()) & XC_AUDIO_FLAGS_ENABLE_AC3)!=0;
+}
+
+void XBAudioConfig::SetDTSEnabled(bool bEnable)
+{
+	if (bEnable)
+		m_dwAudioFlags |= XC_AUDIO_FLAGS_ENABLE_DTS;
+	else
+		m_dwAudioFlags &= ~XC_AUDIO_FLAGS_ENABLE_DTS;
+}
+
+bool XBAudioConfig::GetDTSEnabled()
+{
+	if (!HasDigitalOutput()) return false;
+	return (XC_AUDIO_FLAGS_ENCODED(XGetAudioFlags()) & XC_AUDIO_FLAGS_ENABLE_DTS)!=0;
+}
+
+bool XBAudioConfig::NeedsSave()
+{
+	if (!HasDigitalOutput()) return false;
+	return m_dwAudioFlags != XGetAudioFlags();
+}
+
+// USE VERY CAREFULLY!!
+void XBAudioConfig::Save()
+{
+	if (!NeedsSave()) return;
+	// update the EEPROM settings
+	DWORD type = REG_BINARY;
+	ExSaveNonVolatileSetting(XC_AUDIO_FLAGS,&type,(PULONG)&m_dwAudioFlags,4);
+	// check that we updated correctly
+	if (m_dwAudioFlags != XGetAudioFlags())
+	{
+		int test = 1;
+	}
+}
