85106befe775fd24484516c5c331fd5ccd687559
yuvalt <yuvalt@svn>
2004-08-21 11:43:57 +0000

    - 21-08-2004 added:    Hebrew xpr fonts   - 21-08-2004 changed:  Flip BiDi Charset has moved from XboxMediaCenter.xml to the GUI   - 21-08-2004 added:  Support for multiple font sets in a skin, switched from the GUI   - 21-08-2004 added:  Hebrew language file by okets
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1969 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIFontManager.h
+++ guilib/GUIFontManager.h
@@ -12,6 +12,8 @@
 #include "graphiccontext.h"
 #include "guifont.h"
 
+#include "tinyxml/tinyxml.h"
+
 #include <vector>
 #include "stdstring.h"
 using namespace std;
@@ -27,10 +29,12 @@ public:
   virtual ~GUIFontManager(void);
 	CGUIFont*				Load(const CStdString& strFontName,const CStdString& strFilename);
 	void						Unload(const CStdString& strFontName);
-  void						LoadFonts();
+  void						LoadFonts(const CStdString& strFontSet);
   CGUIFont*				GetFont(const CStdString& strFontName);
 	void						Clear();
 protected:
+	void LoadFonts(const TiXmlNode* fontNode);
+
   vector<CGUIFont*> m_vecFonts;
 };
 
