164a87f703e3567534f8cfbd870a03b0e7b9fd60
charlydoes <charlydoes@svn>
2004-08-22 17:20:11 +0000

    fixed: thumb aspectratio in videoinfo screen
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@1982 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GuiWindowVideoInfo.h
+++ xbmc/GuiWindowVideoInfo.h
@@ -21,10 +21,7 @@ protected:
 	void										Refresh();
 	void										Update();
 	void										SetLabel(int iControl, const CStdString& strLabel);
-	IDirect3DTexture8* 			m_pTexture;
 	CIMDBMovie*							m_pMovie;
-	int											m_iTextureWidth;
-	int											m_iTextureHeight;
 	bool										m_bViewReview;
   bool                    m_bRefresh;
 };
