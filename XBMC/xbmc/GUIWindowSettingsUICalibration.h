b500cfd0de952350c38e2acca41b6a5b082fe021
bobbin007 <bobbin007@svn>
2004-09-14 19:16:31 +0000

    changed: all settings windows remember the last selected control until going back to the prev menu
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@2098 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIWindowSettingsUICalibration.h
+++ XBMC/xbmc/GUIWindowSettingsUICalibration.h
@@ -10,6 +10,8 @@ public:
 	virtual bool	OnMessage(CGUIMessage& message);
 	virtual void	OnAction(const CAction &action);
 	virtual void	Render();
+protected:
+	int							m_iLastControl;
 //private:
 //	int	m_iOffsetX;
 //	int m_iOffsetY;
