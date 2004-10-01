0a82c6f9aafdcb8c285c4d6330b3fcc4b63a55a7
yuvalt <yuvalt@svn>
2004-10-01 22:38:24 +0000

    fixed: Remote control not functioning properly when XBMC is loaded
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2165 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/XBInputEx.cpp
+++ xbmc/XBInputEx.cpp
@@ -10,11 +10,11 @@
 // Globals for the Remote
 //-----------------------------------------------------------------------------
 
-static DWORD dwLastTime=0;
-
 #define INTERVAL 100 // msec
 
 DWORD g_prevPacketNumber[10]={0,0,0,0,0,0,0,0,0,0};
+DWORD g_eventsSinceFirstEvent[10]={0,0,0,0,0,0,0,0,0,0};
+
 // Global instance of input states
 XINPUT_STATEEX g_InputStatesEx[4];
 
@@ -37,25 +37,24 @@ HRESULT XBInput_CreateIR_Remotes( )
     // Open the devices
     for( DWORD i=0; i < XGetPortCount(); i++ )
     {
-
         ZeroMemory( &g_InputStatesEx[i], sizeof(XINPUT_STATEEX) );
         ZeroMemory( &g_IR_Remote[i], sizeof(XBIR_REMOTE) );
         if( dwDeviceMask & (1<<i) ) 
         {
-
             XINPUT_POLLING_PARAMETERS pollValues;
             pollValues.fAutoPoll       = TRUE;
             pollValues.fInterruptOut   = TRUE;
-            pollValues.bInputInterval  = 255;  
-            pollValues.bOutputInterval = 0;
+            pollValues.bInputInterval  = 8;  
+            pollValues.bOutputInterval = 8;
             pollValues.ReservedMBZ1    = 0;
             pollValues.ReservedMBZ2    = 0;
-
 
             // Get a handle to the device
             g_IR_Remote[i].hDevice = XInputOpen( XDEVICE_TYPE_IR_REMOTE, i, 
                                                 XDEVICE_NO_SLOT, &pollValues );
 
+			g_prevPacketNumber[i] = -1;
+			g_eventsSinceFirstEvent[i] = 0;
         }
     }
 
@@ -64,10 +63,6 @@ HRESULT XBInput_CreateIR_Remotes( )
     
     return S_OK;
 }
-
-
-
-
 
 
 //-----------------------------------------------------------------------------
@@ -87,8 +82,8 @@ VOID XBInput_GetInput( XBIR_REMOTE* pIR_Remote)
     XINPUT_POLLING_PARAMETERS pollValues;
     pollValues.fAutoPoll       = TRUE;
     pollValues.fInterruptOut   = TRUE;
-    pollValues.bInputInterval  = 255;  
-    pollValues.bOutputInterval = 0;
+    pollValues.bInputInterval  = 8;  
+    pollValues.bOutputInterval = 8;
     pollValues.ReservedMBZ1    = 0;
     pollValues.ReservedMBZ2    = 0;
 
@@ -99,75 +94,95 @@ VOID XBInput_GetInput( XBIR_REMOTE* pIR_Remote)
 	// Looks like the Remote doesn't send a signal when it's removed...
     DWORD dwInsertions, dwRemovals;
     if ( XGetDeviceChanges( XDEVICE_TYPE_IR_REMOTE, &dwInsertions, &dwRemovals ))
-		{
-			// Loop through all gamepads
-			for( DWORD i=0; i < XGetPortCount(); i++ )
-			{
-					// Handle removed devices.
-					g_IR_Remote[i].bRemoved = ( dwRemovals & (1<<i) ) ? TRUE : FALSE;
-					if( g_IR_Remote[i].bRemoved )
-					{
-							// if the controller was removed after XGetDeviceChanges but before
-							// XInputOpen, the device handle will be NULL
-							if( g_IR_Remote[i].hDevice )
-									XInputClose( g_IR_Remote[i].hDevice );
-							g_IR_Remote[i].hDevice = NULL;
-
-					}
-
-					// Handle inserted devices
-					g_IR_Remote[i].bInserted = ( dwInsertions & (1<<i) ) ? TRUE : FALSE;
-
-					if( g_IR_Remote[i].bInserted ) 
-					{
-							// TCR 1-14 Device Types
-							g_IR_Remote[i].hDevice = XInputOpen( XDEVICE_TYPE_IR_REMOTE, i, XDEVICE_NO_SLOT, &pollValues);
-					}
-			}
-		}
+	{
 		// Loop through all gamepads
 		for( DWORD i=0; i < XGetPortCount(); i++ )
 		{
-        // If we have a valid device, poll it's state and track button changes
-        if( g_IR_Remote[i].hDevice )
-        {
-            // Read the input state
-						ZeroMemory( &g_InputStatesEx[i], sizeof(XINPUT_STATEEX) );	
-            if (ERROR_SUCCESS==XInputGetState( g_IR_Remote[i].hDevice, (XINPUT_STATE*) &g_InputStatesEx[i] ))
-						{
+			// Handle removed devices.
+			g_IR_Remote[i].bRemoved = ( dwRemovals & (1<<i) ) ? TRUE : FALSE;
+			if( g_IR_Remote[i].bRemoved )
+			{
+				// if the controller was removed after XGetDeviceChanges but before
+				// XInputOpen, the device handle will be NULL
+				if( g_IR_Remote[i].hDevice )
+					XInputClose( g_IR_Remote[i].hDevice );
+				g_IR_Remote[i].hDevice = NULL;
+			}
 
-							if (g_prevPacketNumber[i] != g_InputStatesEx[i].dwPacketNumber)
-							{
-								g_prevPacketNumber[i]=g_InputStatesEx[i].dwPacketNumber;
-								
-								DWORD dwTimeLast= timeGetTime() - dwLastTime;
-								if (dwTimeLast < 80)
-								{/*
-									char szTmp[129];
-									sprintf(szTmp,"pkt:%i cnt:%i region:%i wbuttons:%i :%i\n",
-													g_prevPacketNumber[i],
-													g_InputStatesEx[i].IR_Remote.counter,
-													g_InputStatesEx[i].IR_Remote.region,
-													g_InputStatesEx[i].IR_Remote.wButtons,
-													dwTimeLast);
-									OutputDebugString(szTmp);*/
-									// Copy remote to local structure
-									memcpy( &pIR_Remote[i], &g_InputStatesEx[i].IR_Remote, sizeof(XINPUT_IR_REMOTE) );
-									pIR_Remote[i].hDevice=(HANDLE)1;
-								}
-								
-							}
-						}
-						else
-						{
-							// Needs to reset it... don't know a better way to do it
-							XInputClose( g_IR_Remote[i].hDevice);
-							g_IR_Remote[i].hDevice = XInputOpen( XDEVICE_TYPE_IR_REMOTE, i, 
-							                                  XDEVICE_NO_SLOT, &pollValues);
-						}
-        }
-    }	
-		dwLastTime=timeGetTime();
+			// Handle inserted devices
+			g_IR_Remote[i].bInserted = ( dwInsertions & (1<<i) ) ? TRUE : FALSE;
+
+			if( g_IR_Remote[i].bInserted ) 
+			{
+				// TCR 1-14 Device Types
+				g_IR_Remote[i].hDevice = XInputOpen( XDEVICE_TYPE_IR_REMOTE, i, XDEVICE_NO_SLOT, &pollValues);
+			}
+		}
+	}
+
+	// Loop through all gamepads
+	for( DWORD i=0; i < XGetPortCount(); i++ )
+	{
+		// If we have a valid device, poll it's state and track button changes
+		if( g_IR_Remote[i].hDevice )
+		{
+			// Read the input state
+			ZeroMemory( &g_InputStatesEx[i], sizeof(XINPUT_STATEEX) );	
+			if (ERROR_SUCCESS==XInputGetState( g_IR_Remote[i].hDevice, (XINPUT_STATE*) &g_InputStatesEx[i] ))
+			{
+				if (g_prevPacketNumber[i] != g_InputStatesEx[i].dwPacketNumber)
+				{
+					g_prevPacketNumber[i] = g_InputStatesEx[i].dwPacketNumber;
+
+					// Count the number of events since firstEvent was set (when repeat or button release)
+					if (g_InputStatesEx[i].IR_Remote.firstEvent > 0)
+					{
+						g_eventsSinceFirstEvent[i] = 0;
+					}
+					else
+					{
+						g_eventsSinceFirstEvent[i]++;
+					}
+/*
+					char szTmp[129];
+					sprintf(szTmp, "pkt:%i cnt:%i region:%i wbuttons:%i firstEvent:%i sinceFirst:%i...",
+									g_prevPacketNumber[i],
+									g_InputStatesEx[i].IR_Remote.counter,
+									g_InputStatesEx[i].IR_Remote.region,
+									g_InputStatesEx[i].IR_Remote.wButtons,
+									g_InputStatesEx[i].IR_Remote.firstEvent,
+									g_eventsSinceFirstEvent[i]);
+					OutputDebugString(szTmp);
+*/
+					// If this is the first event or if at least 2 non first events have passed (assume repeat)
+					if (g_eventsSinceFirstEvent[i] == 0 || g_eventsSinceFirstEvent[i] > 2)
+					{
+						// Copy remote to local structure
+						memcpy( &pIR_Remote[i], &g_InputStatesEx[i].IR_Remote, sizeof(XINPUT_IR_REMOTE) );
+						pIR_Remote[i].hDevice=(HANDLE)1;
+/*
+						OutputDebugString("accepted\n");
+
+					}
+					else
+					{
+						OutputDebugString("ignored\n");
+*/
+					}
+
+				}
+			}
+			else
+			{
+				// Needs to reset it... don't know a better way to do it
+				XInputClose( g_IR_Remote[i].hDevice);
+				g_IR_Remote[i].hDevice = XInputOpen( XDEVICE_TYPE_IR_REMOTE, i, 
+												XDEVICE_NO_SLOT, &pollValues);
+				g_prevPacketNumber[i] = -1;
+				g_eventsSinceFirstEvent[i] = 0;
+			}
+		}
+	}	
 }
 
 
