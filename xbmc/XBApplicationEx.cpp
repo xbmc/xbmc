87305887541ba80563992c8f3a837784fee6f20a
jmarshallnz <jmarshallnz@svn>
2004-10-05 07:46:40 +0000

    - 05-10-2004 fixed: [ 1039240 ] "Out of Memory Error" switching between pictures/slideshow   - 05-10-2004 fixed: PAL60 startup for emergency recovery console
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2179 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/XBApplicationEx.cpp
+++ xbmc/XBApplicationEx.cpp
@@ -99,7 +99,7 @@ HRESULT CXBApplicationEx::Create()
 		if (!g_graphicsContext.IsValidResolution(g_stSettings.m_GUIResolution))
 		{
 			// Oh uh - doesn't look good for starting in their wanted screenmode
-			CLog::Log(LOGERROR, "The screen rsolution requested is not valid, resetting to a valid mode");
+			CLog::Log(LOGERROR, "The screen resolution requested is not valid, resetting to a valid mode");
 			g_stSettings.m_GUIResolution = g_videoConfig.GetSafeMode();
 			g_graphicsContext.SetGUIResolution(g_stSettings.m_GUIResolution);
 		}
