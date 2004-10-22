d593f8a4532880233f82d3f6d47baa859b9bd884
bobbin007 <bobbin007@svn>
2004-10-22 12:53:46 +0000

    changed: libcdio updated to 0.70 (Thnx to Mog)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2260 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcdio/version.h
+++ xbmc/lib/libcdio/version.h
@@ -1,1 +1,10 @@
-#define CDIO_VERSION "0.62"
+/* $Id$ */
+/** \file version.h 
+ *  \brief  A file simply containing the library version number.
+ */
+
+/*! CDIO_VERSION can as a string in programs to show what version is used. */
+#define CDIO_VERSION "0.70"
+
+/*! LIBCDIO_VERSION_NUM  can be used for testing in the C preprocessor */
+#define LIBCDIO_VERSION_NUM 70
