d5799d5c0cfd1243d9d628000d808c14771ef55c
bobbin007 <bobbin007@svn>
2004-10-23 11:58:53 +0000

    reverted libCDIO from 0.70 to 0.62 again
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2276 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcdio/version.h
+++ xbmc/lib/libcdio/version.h
@@ -1,10 +1,1 @@
-/* $Id$ */
-/** \file version.h 
- *  \brief  A file simply containing the library version number.
- */
-
-/*! CDIO_VERSION can as a string in programs to show what version is used. */
-#define CDIO_VERSION "0.70"
-
-/*! LIBCDIO_VERSION_NUM  can be used for testing in the C preprocessor */
-#define LIBCDIO_VERSION_NUM 70
+#define CDIO_VERSION "0.62"
