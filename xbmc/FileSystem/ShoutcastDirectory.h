97a1c0f877f97ec5a626344b1d2015861072aef8
bobbin007 <bobbin007@svn>
2004-11-02 21:02:10 +0000

    added: my music songs, Shoutcast Top 500 streams share
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2331 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/ShoutcastDirectory.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "directory.h"
+
+using namespace DIRECTORY;
+namespace DIRECTORY
+{
+  class CShoutcastDirectory :
+    public IDirectory
+  {
+  public:
+    CShoutcastDirectory(void);
+    virtual ~CShoutcastDirectory(void);
+    virtual bool  GetDirectory(const CStdString& strPath,VECFILEITEMS &items);
+            bool  DownloadPlaylists(VECFILEITEMS &items);
+            void  CacheItems(VECFILEITEMS &items);
+            void  LoadCachedItems(VECFILEITEMS &items);
+            bool  IsCacheValid();
+  };
+}
