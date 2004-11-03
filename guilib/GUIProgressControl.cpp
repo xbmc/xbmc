421b01f3259133d99615d683ee8163bbe6a09dc9
bobbin007 <bobbin007@svn>
2004-11-03 00:35:46 +0000

    changed: progress control now can have an overlay texture
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2339 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIProgressControl.cpp
+++ guilib/GUIProgressControl.cpp
@@ -3,12 +3,13 @@
 #include "guifontmanager.h"
 
 
-CGUIProgressControl::CGUIProgressControl(DWORD dwParentID, DWORD dwControlId, int iPosX, int iPosY, DWORD dwWidth, DWORD dwHeight, CStdString& strBackGroundTexture,CStdString& strLeftTexture,CStdString& strMidTexture,CStdString& strRightTexture)
+CGUIProgressControl::CGUIProgressControl(DWORD dwParentID, DWORD dwControlId, int iPosX, int iPosY, DWORD dwWidth, DWORD dwHeight, CStdString& strBackGroundTexture,CStdString& strLeftTexture,CStdString& strMidTexture,CStdString& strRightTexture, CStdString& strOverlayTexture)
 :CGUIControl(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight)
 ,m_guiBackground(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight,strBackGroundTexture)
 ,m_guiLeft(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight,strLeftTexture)
 ,m_guiMid(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight,strMidTexture)
 ,m_guiRight(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight,strRightTexture)
+,m_guiOverlay(dwParentID, dwControlId, iPosX, iPosY,dwWidth, dwHeight,strOverlayTexture)
 {
 	m_iPercent=0;
 	ControlType = GUICONTROL_PROGRESS;
@@ -27,32 +28,53 @@ void CGUIProgressControl::Render()
 	int iHeight=m_guiBackground.GetTextureHeight();
 	m_guiBackground.Render();
 	m_guiBackground.SetHeight(iHeight);
-
+  
 	float fWidth = (float)m_iPercent;
 	fWidth/=100.0f;
 	fWidth *= (float) (m_guiBackground.GetTextureWidth() - m_guiLeft.GetTextureWidth() - m_guiRight.GetTextureWidth());
 
 	int iXPos=m_guiBackground.GetXPosition();
 	int iYPos=m_guiBackground.GetYPosition();
-	m_guiLeft.SetPosition(iXPos,iYPos);
-	m_guiLeft.SetHeight(m_guiLeft.GetTextureHeight());
+  int iOffset=abs((m_guiLeft.GetTextureHeight()-m_guiBackground.GetTextureHeight())/2);
+  if (iOffset>0)  //  Center texture to the background if necessary
+    m_guiLeft.SetPosition(iXPos,iYPos+iOffset);
+  else
+    m_guiLeft.SetPosition(iXPos,iYPos);
+  m_guiLeft.SetHeight(m_guiLeft.GetTextureHeight());
 	m_guiLeft.SetWidth(m_guiLeft.GetTextureWidth());
 	m_guiLeft.Render();
 
 	iXPos += m_guiLeft.GetTextureWidth();
 	if (m_iPercent && (int)fWidth > 1)
 	{
-		m_guiMid.SetPosition(iXPos,iYPos);
+    int iOffset=abs((m_guiMid.GetTextureHeight()-m_guiBackground.GetTextureHeight())/2);
+    if (iOffset>0)  //  Center texture to the background if necessary
+      m_guiMid.SetPosition(iXPos,iYPos+iOffset);
+    else
+      m_guiMid.SetPosition(iXPos,iYPos);
 		m_guiMid.SetHeight(m_guiMid.GetTextureHeight());
 		m_guiMid.SetWidth((int)fWidth);
 		m_guiMid.Render();
 		iXPos += (int)fWidth;
 	}
-	m_guiRight.SetPosition(iXPos,iYPos);
+
+  iOffset=abs((m_guiRight.GetTextureHeight()-m_guiBackground.GetTextureHeight())/2);
+  if (iOffset>0)  //  Center texture to the background if necessary
+    m_guiRight.SetPosition(iXPos,iYPos+iOffset);
+  else
+    m_guiRight.SetPosition(iXPos,iYPos);
 	m_guiRight.SetHeight(m_guiRight.GetTextureHeight());
 	m_guiRight.SetWidth(m_guiLeft.GetTextureWidth());
 	m_guiRight.Render();
 	
+  iOffset=abs((m_guiOverlay.GetTextureHeight()-m_guiBackground.GetTextureHeight())/2);
+  if (iOffset>0)  //  Center texture to the background if necessary
+    m_guiOverlay.SetPosition(m_guiBackground.GetXPosition(),m_guiBackground.GetYPosition()+iOffset);
+  else
+    m_guiOverlay.SetPosition(m_guiBackground.GetXPosition(),m_guiBackground.GetYPosition());
+  m_guiOverlay.SetHeight(m_guiOverlay.GetTextureHeight());
+	m_guiOverlay.SetWidth(m_guiOverlay.GetTextureWidth());
+	m_guiOverlay.Render();
 }
 
 
@@ -83,6 +105,7 @@ void CGUIProgressControl::FreeResources()
   m_guiMid.FreeResources();
   m_guiRight.FreeResources();
   m_guiLeft.FreeResources();
+  m_guiOverlay.FreeResources();
 }
 
 void CGUIProgressControl::PreAllocResources()
@@ -92,6 +115,7 @@ void CGUIProgressControl::PreAllocResources()
 	m_guiMid.PreAllocResources();
 	m_guiRight.PreAllocResources();
 	m_guiLeft.PreAllocResources();
+  m_guiOverlay.PreAllocResources();
 }
 
 void CGUIProgressControl::AllocResources()
@@ -101,9 +125,11 @@ void CGUIProgressControl::AllocResources()
   m_guiMid.AllocResources();
   m_guiRight.AllocResources();
   m_guiLeft.AllocResources();
+  m_guiOverlay.AllocResources();
 	
 	m_guiBackground.SetHeight(25);
 	m_guiRight.SetHeight(20);
 	m_guiLeft.SetHeight(20);
 	m_guiMid.SetHeight(20);
+	m_guiOverlay.SetHeight(20);
 }
