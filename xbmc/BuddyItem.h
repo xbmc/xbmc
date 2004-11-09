0748f00112349a831dd9f9ef61a6fdccbbf505f2
runtime <runtime@svn>
2004-11-09 22:57:24 +0000

    XBMC KAI "floozy" update.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2376 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/BuddyItem.h
+++ xbmc/BuddyItem.h
@@ -1,22 +1,43 @@
 #pragma once
-#include "guilistexitem.h"
-#include "stdstring.h"
-#include <vector>
-using namespace std;
+#include "StdString.h"
+#include "KaiItem.h"
 
-class CBuddyItem : public CGUIListExItem
+class CBuddyItem : public CKaiItem
 {
 public:
 	CBuddyItem(CStdString& strLabel);
 	virtual ~CBuddyItem(void);
 	virtual void OnPaint(CGUIItem::RenderContext* pContext);
-	void		 SetPingIcon(INT aWidth, INT aHeight, const CStdString& aTexture);
+	static  void SetIcons(INT aWidth, INT aHeight, const CStdString& aHeadsetTexture,
+							  const CStdString& aChatTexture, const CStdString& aPingTexture, 
+							  const CStdString& aInviteTexture, const CStdString& aBusyTexture);
 	CStdString	 GetArena();
+
 public:
 	CStdString	 m_strVector;
+	CStdString	 m_strGeoLocation;
 	DWORD		 m_dwPing;
 	bool		 m_bIsOnline;
+	bool		 m_bSpeex;
+	bool		 m_bBusy;
+	bool		 m_bHeadset;
+	bool		 m_bIsTalking;
+	bool		 m_bIsContact;
+	bool		 m_bProfileRequested;
+
+	DWORD		 m_dwSpeexCounter;
+	DWORD		 m_dwRingCounter;
+	bool		 m_bInvite;	
+	bool		 m_bRingIndicator;
+
+	int			 m_nStatus;
+
 protected:
-	CGUIImage*	 m_pPingIcon;
+
+	static CGUIImage*	 m_pPingIcon;
+	static CGUIImage*	 m_pTalkingIcon;
+	static CGUIImage*	 m_pHeadsetIcon;
+	static CGUIImage*	 m_pInviteIcon;
+	static CGUIImage*	 m_pBusyIcon;
 };
 
---
+++ xbmc/KaiItem.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "GUIListExItem.h"
+#include "StdString.h"
+#include "Utils/DownloadQueueManager.h"
+
+class CKaiItem : public CGUIListExItem, public IDownloadQueueObserver
+{
+public:
+	CKaiItem(CStdString& strLabel);
+	virtual ~CKaiItem(void);
+
+	void SetAvatar(CStdString& aUrl);
+
+	bool IsAvatarCached();
+	void UseCachedAvatar();
+
+	virtual void OnFileComplete(TICKET aTicket, CStdString& aFilePath, INT aByteRxCount, Result aResult);
+
+protected:
+	
+	void GetAvatarFilePath(CStdString& aFilePath);
+
+public:
+
+	CGUIImage*	 m_pAvatar;
+};
