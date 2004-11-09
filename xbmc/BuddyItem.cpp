bc73e68ae369bc69f87269232bfb495ab7fa8434
runtime <runtime@svn>
2004-11-09 22:57:24 +0000

    XBMC KAI "floozy" update.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2376 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/BuddyItem.cpp
+++ xbmc/BuddyItem.cpp
@@ -1,30 +1,47 @@
 
-#include "stdafx.h"
-#include "buddyitem.h"
-#include "settings.h"
-#include "utils/KaiClient.h"
+#include "StdAfx.h"
+#include "BuddyItem.h"
+#include "Settings.h"
+#include "Utils/KaiClient.h"
 
 #define PING_OFFSETX		8
 #define PING_MAX_RATING		4
 #define PING_SPACING		2
 #define PING_MAX_LATENCY	280
 
-CBuddyItem::CBuddyItem(CStdString& strLabel) : CGUIListExItem(strLabel)
+CGUIImage* CBuddyItem::m_pTalkingIcon	= NULL;
+CGUIImage* CBuddyItem::m_pHeadsetIcon	= NULL;
+CGUIImage* CBuddyItem::m_pPingIcon		= NULL;
+CGUIImage* CBuddyItem::m_pInviteIcon	= NULL;
+CGUIImage* CBuddyItem::m_pBusyIcon		= NULL;
+
+CBuddyItem::CBuddyItem(CStdString& strLabel) : CKaiItem(strLabel)
 {
 	SetCookie( CKaiClient::Item::Player );
-	m_bIsOnline=false;
-	m_strVector="/";
-	m_dwPing = 0;
-	m_pPingIcon=NULL;
+
+	m_strVector			= "/";
+	m_strGeoLocation	= "";
+
+	m_bIsContact		= FALSE;
+	m_bIsOnline			= FALSE;
+	m_bBusy				= FALSE;
+	m_bHeadset			= FALSE;	// does this buddy have a headset connected?
+	m_bSpeex			= FALSE;	// have you enabled speex for this buddy?
+	m_bIsTalking		= FALSE;	// is this buddy chatting?
+	m_bProfileRequested = FALSE;
+
+	m_dwPing			= 0;
+
+	m_dwSpeexCounter	= 0;
+	m_dwRingCounter		= 0;
+	m_bRingIndicator	= TRUE;
+	m_bInvite			= FALSE;
+
+	m_nStatus			= 0;
 }
 
 CBuddyItem::~CBuddyItem(void)
 {
-	if (m_pPingIcon)
-	{
-		m_pPingIcon->FreeResources();
-		delete m_pPingIcon;
-	}
 }
 
 CStdString CBuddyItem::GetArena()
@@ -33,43 +50,124 @@ CStdString CBuddyItem::GetArena()
 	return m_strVector.Mid(arenaDelimiter);
 }
 
-void CBuddyItem::SetPingIcon(INT aWidth, INT aHeight, const CStdString& aTexture)
+void CBuddyItem::SetIcons(INT aWidth, INT aHeight, const CStdString& aHeadsetTexture,
+							  const CStdString& aChatTexture, const CStdString& aPingTexture,
+							  const CStdString& aInviteTexture, const CStdString& aBusyTexture)
 {
-	if (m_pPingIcon)
+	if (aChatTexture.length()>0)
 	{
-		m_pPingIcon->FreeResources();
-		delete m_pPingIcon;
+		m_pTalkingIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aChatTexture,0x0);
+		m_pTalkingIcon->AllocResources();
 	}
 
-	m_pPingIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aTexture,0x0);
-	m_pPingIcon->AllocResources();
+	if (aHeadsetTexture.length()>0)
+	{
+		m_pHeadsetIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aHeadsetTexture,0x0);
+		m_pHeadsetIcon->AllocResources();
+	}
+
+	if (aPingTexture.length())
+	{
+		m_pPingIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aPingTexture,0x0);
+		m_pPingIcon->AllocResources();
+	}
+
+	if (aInviteTexture.length())
+	{
+		m_pInviteIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aInviteTexture,0x0);
+		m_pInviteIcon->AllocResources();
+	}
+
+	if (aBusyTexture.length())
+	{
+		m_pBusyIcon = new CGUIImage(0,0,0,0,aWidth,aHeight,aBusyTexture,0x0);
+		m_pBusyIcon->AllocResources();
+	}
 }
 
 void CBuddyItem::OnPaint(CGUIItem::RenderContext* pContext)
 {
-	CGUIListExItem::OnPaint(pContext);
-
-	// safely get a pointer to the derived (subclassed) context
-	//CGUIListExItem::RenderContext* pDC = dynamic_cast<CGUIListExItem::RenderContext*>(pContext);
+	CKaiItem::OnPaint(pContext);
 
 	CGUIListExItem::RenderContext* pDC = (CGUIListExItem::RenderContext*)pContext;
-	if ( (pDC) && (m_pPingIcon) && (m_dwPing>0) && (m_dwPing<=PING_MAX_LATENCY) )
+
+	if (pDC && m_pPingIcon && m_pHeadsetIcon && m_pTalkingIcon && m_pInviteIcon)
 	{
-		int iPosY = pDC->m_iPositionY;
-		iPosY += (pDC->m_pButton->GetHeight() - m_pPingIcon->GetHeight())/2;
+		int iEndButtonPosX = pDC->m_iPositionX + pDC->m_pButton->GetWidth();
+		int iPingPosY = pDC->m_iPositionY;
+		int iPingPosX = iEndButtonPosX;
 
-		int iPosX = pDC->m_iPositionX + pDC->m_pButton->GetWidth();
-		iPosX -= ( (m_pPingIcon->GetWidth()+PING_SPACING) * PING_MAX_RATING ) + PING_OFFSETX;
+		iPingPosY += (pDC->m_pButton->GetHeight() - m_pPingIcon->GetHeight())/2;
+		iPingPosX -= ( (m_pPingIcon->GetWidth()+PING_SPACING) * PING_MAX_RATING ) + PING_OFFSETX;
 
-		DWORD dwStep = PING_MAX_LATENCY / PING_MAX_RATING;
-		DWORD dwRating = PING_MAX_RATING - (m_dwPing/dwStep);
+		int iHeadsetIconPosX = iPingPosX - (m_pHeadsetIcon->GetWidth() + 4);
+		int iInviteIconPosX	 = iHeadsetIconPosX - (m_pInviteIcon->GetWidth() + 2);
 
-		for (DWORD dwGraduation=0; dwGraduation<dwRating; dwGraduation++)
+		// if buddy has been talking for the last second
+		if (m_dwSpeexCounter>0)
 		{
-			m_pPingIcon->SetPosition(iPosX,iPosY);
-			m_pPingIcon->Render();
+			m_dwSpeexCounter--;
+			m_pTalkingIcon->SetPosition(iHeadsetIconPosX,iPingPosY);
+			m_pTalkingIcon->Render();
+		}
+		// if we have enabled voice chat for this buddy
+		else if (m_bSpeex)
+		{
+			m_pHeadsetIcon->SetAlpha(255);
+			m_pHeadsetIcon->SetPosition(iHeadsetIconPosX,iPingPosY);
+			m_pHeadsetIcon->Render();
+		}
+		// if buddy is trying to establish voice chat
+		else if (m_dwRingCounter>0)
+		{
+			m_dwRingCounter--;
+			// flash headset icon
+			if (m_dwFrameCounter%60>=30)
+			{
+				m_pHeadsetIcon->SetAlpha(255);
+				m_pHeadsetIcon->SetPosition(iHeadsetIconPosX,iPingPosY);
+				m_pHeadsetIcon->Render();
+			}
+		}
+		// if buddy has a headset
+		else if (m_bHeadset)
+		{
+			m_pHeadsetIcon->SetAlpha(95);
+			m_pHeadsetIcon->SetPosition(iHeadsetIconPosX,iPingPosY);
+			m_pHeadsetIcon->Render();
+		}
+		// if buddy has sent an invite
+		if (m_bInvite)
+		{
+			if (m_dwFrameCounter%60>=30)
+			{
+				// flash invitation icon
+				m_pInviteIcon->SetPosition(iInviteIconPosX,iPingPosY);
+				m_pInviteIcon->Render();
+			}
+		}
+		// if buddy's client is detached from engine and syslink activity
+		else if (m_bBusy && m_nStatus==1)
+		{
+			m_pBusyIcon->SetPosition(iInviteIconPosX,iPingPosY);
+			m_pBusyIcon->Render();
+		}
 
-			iPosX += m_pPingIcon->GetWidth() + PING_SPACING;
+		if (m_pPingIcon && m_dwPing>0 && m_dwPing<=PING_MAX_LATENCY )
+		{
+			DWORD dwPingAlpha = 95;
+			DWORD dwStep = PING_MAX_LATENCY / PING_MAX_RATING;
+			DWORD dwRating = PING_MAX_RATING - (m_dwPing/dwStep);
+			int iPosX = iPingPosX;
+			for (DWORD dwGraduation=0; dwGraduation<dwRating; dwGraduation++)
+			{
+				dwPingAlpha+=40;
+				m_pPingIcon->SetPosition(iPosX,iPingPosY);
+				m_pPingIcon->SetAlpha(dwPingAlpha);
+				m_pPingIcon->Render();
+
+				iPosX += m_pPingIcon->GetWidth() + PING_SPACING;
+			}
 		}
 	}
 }
--- xbmc/GUIWindowBuddies.h
+++ xbmc/GUIWindowBuddies.h
@@ -2,12 +2,13 @@
 #include "guiImage.h"
 #include "guiwindow.h"
 #include "guiList.h"
-#include "stdstring.h"
-#include "utils\UdpClient.h"
-#include "tinyxml/tinyxml.h"
-#include "guiDialogProgress.h"
+#include "TinyXML/TinyXML.h"
+#include "StdString.h"
 
+#include "utils/UdpClient.h"
+#include "guiDialogProgress.h"
 #include "utils/KaiClient.h"
+
 #include "BuddyItem.h"
 #include "ArenaItem.h"
 
@@ -22,16 +23,25 @@ public:
 	CGUIWindowBuddies(void);
 	virtual ~CGUIWindowBuddies(void);
 
+	virtual void			OnInitialise(CKaiClient* pClient);
 	virtual void			OnContactOffline(CStdString& aFriend);
 	virtual void			OnContactOnline(CStdString& aFriend);
-	virtual void			OnContactPing(CStdString& aFriend, CStdString& aVector, CStdString& aPing);
+	virtual void			OnContactPing(CStdString& aFriend, CStdString& aVector, DWORD aPing, int aStatus, CStdString& aBearerCapability);
 	virtual void			OnContactRemove(CStdString& aFriend);
-	virtual void			OnEnterArena(CStdString& aVector);
+	virtual void			OnContactSpeexStatus(CStdString& aFriend, bool bSpeexEnabled);
+	virtual void			OnContactSpeexRing(CStdString& aFriend);
+	virtual void			OnContactSpeex(CStdString& aFriend);
+	virtual void			OnContactInvite(CStdString& aFriend, CStdString& aVector, CStdString& aTime, CStdString& aMessage);
+	virtual void			OnEnterArena(CStdString& aVector, BOOL bCanHost);
 	virtual void			OnEnterArenaFailed(CStdString& aVector, CStdString& aReason);
 	virtual void			OnNewArena( CStdString& aVector, CStdString& aDescription,
-										int nPlayers, int nPlayerLimit, int bPrivate );
+										int nPlayers, int nPlayerLimit, int nPassword, bool bPersonal );
+	virtual void			OnUpdateArena( CStdString& aVector, int nPlayers );
+	virtual void			OnUpdateOpponent(CStdString& aOpponent, CStdString& aAge,
+										CStdString& aBandwidth,	CStdString& aLocation, CStdString& aBio);
+	virtual void			OnUpdateOpponent(CStdString& aOpponent, CStdString& aAvatarURL);
 	virtual void			OnOpponentEnter(CStdString& aOpponent);
-	virtual void			OnOpponentPing(CStdString& aOpponent, CStdString& aPing);
+	virtual void			OnOpponentPing(CStdString& aOpponent, DWORD aPing, int aStatus, CStdString& aBearerCapability);
 	virtual void			OnOpponentLeave(CStdString& aOpponent);
 	virtual void			OnSupportedTitle(DWORD aTitleId, CStdString& aVector);
 
@@ -45,15 +55,21 @@ protected:
 	void OnClickModeButton	(CGUIMessage& aMessage);
 	void OnClickAddButton	(CGUIMessage& aMessage);
 	void OnClickRemoveButton(CGUIMessage& aMessage);
+	void OnClickSpeexButton (CGUIMessage& aMessage);
 	void OnClickInviteButton(CGUIMessage& aMessage);
 	void OnClickJoinButton	(CGUIMessage& aMessage);
 	void OnClickPlayButton	(CGUIMessage& aMessage);
+	void OnClickHostButton	(CGUIMessage& aMessage);
 	void OnClickListItem	(CGUIMessage& aMessage);
+	void OnSelectListItem	(CGUIMessage& aMessage);
 
 	void	QueryInstalledGames();
 	void	UpdatePanel();
 	void	UpdateFriends();
 	void	UpdateArena();
+
+	void	UpdateGamesPlayerCount();
+
 	void	ChangeState();
 	void	ChangeState(State aNewState);
 
@@ -65,9 +81,27 @@ protected:
 
 	CBuddyItem* GetBuddySelection();
 	CArenaItem* GetArenaSelection();
+	CGUIImage*	GetCurrentAvatar();
+
+	struct Invitation
+	{
+		CStdString vector;
+		CStdString time;
+		CStdString message;
+	};
 
 	CGUISortedList			m_friends;
 	CGUISortedList			m_arena;
 	CGUISortedList			m_games;
 	State window_state;
+
+	CGUIImage*	m_pOpponentImage;
+	CGUIImage*  m_pCurrentAvatar;
+
+	CKaiClient* m_pKaiClient;
+
+	DWORD		m_dwGamesUpdateTimer;
+
+	typedef map<CStdString,Invitation> INVITETABLE;
+	INVITETABLE m_invitations;
 };
