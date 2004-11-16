e7fe663e610c88b39adabb4eb4b2b7ad14780ca4
runtime <runtime@svn>
2004-11-16 01:43:18 +0000

    Fixed: Update Kai player count within Games view.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@2437 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIWindowBuddies.cpp
+++ XBMC/xbmc/GUIWindowBuddies.cpp
@@ -1342,9 +1342,9 @@ void CGUIWindowBuddies::OnUpdateArena(	CStdString& aVector, int nPlayers )
 	INT arenaDelimiter = aVector.ReverseFind('/')+1;
 	CStdString arenaLabel = aVector.Mid(arenaDelimiter);
 
-	CStdString strDebug;
-	strDebug.Format("KAI: updated %s player count: %d",aVector,nPlayers);
-	CLog::Log(LOGINFO,strDebug.c_str());
+	//CStdString strDebug;
+	//strDebug.Format("KAI: updated %s player count: %d",aVector,nPlayers);
+	//CLog::Log(LOGINFO,strDebug.c_str());
 
 	m_arena.Lock();
 	CArenaItem* pArena = (CArenaItem*) m_arena.Find(arenaLabel);
