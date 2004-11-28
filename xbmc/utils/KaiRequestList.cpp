d9c8076ab25eb091e6559c66a17bc74de455a4ca
runtime <runtime@svn>
2004-11-28 18:54:25 +0000

    Fixed: serialize Kai game titleid resolution and player count requests.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2506 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/utils/KaiRequestList.cpp
@@ -0,0 +1,39 @@
+
+#include "stdafx.h"
+#include "KaiRequestList.h"
+
+CKaiRequestList::CKaiRequestList(void)
+{
+	InitializeCriticalSection(&m_critical);
+}
+
+CKaiRequestList::~CKaiRequestList(void)
+{
+	DeleteCriticalSection(&m_critical);
+}
+
+void CKaiRequestList::QueueRequest(CStdString& aRequest)
+{
+	EnterCriticalSection(&m_critical);
+	
+	m_requests.push(aRequest);
+
+	LeaveCriticalSection(&m_critical);
+}
+
+
+bool CKaiRequestList::GetNext(CStdString& aRequest)
+{
+	EnterCriticalSection(&m_critical);
+	
+	bool bPending = m_requests.size()>0;
+	if (bPending)
+	{
+		aRequest = m_requests.front();
+		m_requests.pop();
+	}
+
+	LeaveCriticalSection(&m_critical);
+
+	return bPending;
+}
---
+++ xbmc/utils/KaiRequestList.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "StdString.h"
+#include <queue>
+
+class CKaiRequestList
+{
+public:
+	CKaiRequestList(void);
+	virtual ~CKaiRequestList(void);
+	
+	void QueueRequest(CStdString& aRequest);
+	bool GetNext(CStdString& aRequest);
+
+protected:
+
+	typedef std::queue<CStdString> REQUESTQUEUE;
+	REQUESTQUEUE m_requests;
+
+	CRITICAL_SECTION m_critical;
+};
