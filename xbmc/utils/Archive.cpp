932898796e0b0908f5a24466bfe8d05984102217
bobbin007 <bobbin007@svn>
2004-12-31 17:33:16 +0000

    added: a simple object serialization class
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2643 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/utils/Archive.cpp
@@ -0,0 +1,199 @@
+#include "Archive.h"
+
+#define BUFFER_MAX 4096
+
+CArchive::CArchive(CFile* pFile, int mode)
+{
+	m_pFile=pFile;
+	m_iMode=mode;
+
+	m_pBuffer=new BYTE[BUFFER_MAX];
+	m_BufferPos=0;
+}
+
+CArchive::~CArchive()
+{
+	FlushBuffer();
+	if (m_pBuffer!=NULL)
+		delete[] m_pBuffer;
+	
+	m_BufferPos=0;
+}
+
+void CArchive::Close()
+{
+	FlushBuffer();
+}
+
+bool CArchive::IsLoading()
+{
+	return (m_iMode==load);
+}
+
+bool CArchive::IsStoring()
+{
+	return (m_iMode==store);
+}
+
+CArchive& CArchive::operator<<(float f)
+{
+	int size=sizeof(float);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &f, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(int i)
+{
+	int size=sizeof(int);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &i, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(__int64 i64)
+{
+	int size=sizeof(__int64);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &i64, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(long l)
+{
+	int size=sizeof(long);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &l, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(bool b)
+{
+	int size=sizeof(bool);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &b, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(const CStdString& str)
+{
+	*this << str.GetLength();
+
+	int size=str.GetLength();
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], str.c_str(), size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(const SYSTEMTIME& time)
+{
+	int size=sizeof(SYSTEMTIME);
+	if (m_BufferPos+size>=BUFFER_MAX)
+		FlushBuffer();
+
+	memcpy(&m_pBuffer[m_BufferPos], &time, size);
+	m_BufferPos+=size;
+
+	return *this;
+}
+
+CArchive& CArchive::operator<<(ISerializable& obj)
+{
+	obj.Serialize(*this);
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(float& f)
+{
+	m_pFile->Read((void*)&f, sizeof(float));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(int& i)
+{
+	m_pFile->Read((void*)&i, sizeof(int));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(__int64& i64)
+{
+	m_pFile->Read((void*)&i64, sizeof(__int64));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(long& l)
+{
+	m_pFile->Read((void*)&l, sizeof(long));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(bool& b)
+{
+	m_pFile->Read((void*)&b, sizeof(bool));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(CStdString& str)
+{
+	int iLength=0;
+	*this >> iLength;
+	
+	m_pFile->Read((void*)str.GetBufferSetLength(iLength), iLength);
+	str.ReleaseBuffer();
+
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(SYSTEMTIME& time)
+{
+	m_pFile->Read((void*)&time, sizeof(SYSTEMTIME));
+
+	return *this;
+}
+
+CArchive& CArchive::operator>>(ISerializable& obj)
+{
+	obj.Serialize(*this);
+
+	return *this;
+}
+
+void CArchive::FlushBuffer()
+{
+	if (m_BufferPos>0)
+	{
+		m_pFile->Write(m_pBuffer, m_BufferPos);
+		m_BufferPos=0;
+	}
+}
---
+++ xbmc/utils/Archive.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "../filesystem/file.h"
+#include "StdString.h"
+class CArchive;
+
+class ISerializable
+{
+	friend CArchive;
+	virtual void Serialize(CArchive& ar)=0;
+};
+
+class CArchive
+{
+public:
+	CArchive(CFile* pFile, int mode);
+	~CArchive();
+	//	storing
+	CArchive& operator<<(float f);
+	CArchive& operator<<(int i);
+	CArchive& operator<<(__int64 i64);
+	CArchive& operator<<(long l);
+	CArchive& operator<<(bool b);
+	CArchive& operator<<(const CStdString& str);
+	CArchive& operator<<(const SYSTEMTIME& time);
+	CArchive& operator<<(ISerializable& obj);
+
+	//	loading
+	CArchive& operator>>(float& f);
+	CArchive& operator>>(int& i);
+	CArchive& operator>>(__int64& i64);
+	CArchive& operator>>(long& l);
+	CArchive& operator>>(bool& b);
+	CArchive& operator>>(CStdString& str);
+	CArchive& operator>>(SYSTEMTIME& time);
+	CArchive& operator>>(ISerializable& obj);
+
+	bool IsLoading();
+	bool IsStoring();
+
+	void Close();
+
+	enum Mode {load=0, store};
+
+protected:
+	void	FlushBuffer();
+	CFile* m_pFile;
+	int m_iMode;
+	LPBYTE m_pBuffer;
+	int m_BufferPos;
+};
+
