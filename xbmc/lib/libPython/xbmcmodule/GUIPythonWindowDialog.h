501c17d3ca7030a10865afcff4a4b24e9ad7272b
bobbin007 <bobbin007@svn>
2005-01-04 15:52:59 +0000

    fixed: [ 1092854 ] Music slide/shutdown menu freeze changed: modal dialogs are completely rendered by the window manager now
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2653 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/xbmcmodule/GUIPythonWindowDialog.h
+++ xbmc/lib/libPython/xbmcmodule/GUIPythonWindowDialog.h
@@ -10,14 +10,11 @@ public:
   virtual ~CGUIPythonWindowDialog(void);
 	virtual bool    OnMessage(CGUIMessage& message);
 	void            Activate(DWORD dwParentId);
-  virtual void    Render();
 	virtual void		Close();
 	virtual bool		IsRunning() const { return m_bRunning; }
 
 protected:
 	DWORD						m_dwParentWindowID;
 	CGUIWindow* 		m_pParentWindow;
-	DWORD						m_dwPrevRouteWindow;
-	CGUIWindow* 		m_pPrevRouteWindow;
 	bool						m_bRunning;
 };
