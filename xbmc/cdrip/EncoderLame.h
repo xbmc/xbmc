5fea68eb6f0d1abd947136b42e1430f06bd94ef5
jwnmulder <jwnmulder@svn>
2005-01-07 18:53:49 +0000

    cdrip encoders moved into dll's, this should result in a smaller executable and a bit faster cd ripping (don't forget to copy the new system folder!)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2673 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cdrip/EncoderLame.h
+++ xbmc/cdrip/EncoderLame.h
@@ -2,34 +2,27 @@
 #define _ENCODERLAME_H
 
 #include "Encoder.h"
-#include "..\lib\liblame\lame.h"
+#include "lame/lame.h"
 
-// merge sections from liblame and liblamed
-#pragma comment(linker, "/merge:LAME_TEX=LIBLAME")
-#pragma comment(linker, "/merge:LAME_DAT=LIBLAME")
-#pragma comment(linker, "/merge:LAME_BSS=LIBLAME")
-// we have a problem here, SECTION loading doesn't work ok when using stringpooling,
-// this is disabled for all projects in xbmc now. But it seemes the same happens for
-// static doubles. These crash xbmc to. Only solution for now is to prevent this section
-// from merging into LIBLAME
-//#pragma comment(linker, "/merge:LAME_RDA=LIBLAME")
-#pragma comment(linker, "/section:LIBLAME,RWE")
+class DllLoader;
 
 class CEncoderLame : public CEncoder
 {
 public:
 	CEncoderLame();
-	bool               Init(const char* strFile, int iInChannels, int iInRate, int iInBits);
-	int                Encode(int nNumBytesRead, BYTE* pbtStream);
-	bool               Close();
-	void               AddTag(int key,const char* value);
+	bool                Init(const char* strFile, int iInChannels, int iInRate, int iInBits);
+	int                 Encode(int nNumBytesRead, BYTE* pbtStream);
+	bool                Close();
+	void                AddTag(int key,const char* value);
 
-private:
-	lame_global_flags* m_pGlobalFlags;
+protected:
+	lame_global_flags*  m_pGlobalFlags;
 
-	unsigned char      m_buffer[48160]; // mp3buf_size in bytes = 1.25*(chunk size / 4) + 7200
-	char               m_inPath[MAX_PATH + 1];
-	char               m_outPath[MAX_PATH + 1];
+	unsigned char       m_buffer[48160]; // mp3buf_size in bytes = 1.25*(chunk size / 4) + 7200
+	char                m_inPath[MAX_PATH + 1];
+	char                m_outPath[MAX_PATH + 1];
+	
+	DllLoader*          m_pDLLLame;
 };
 
 #endif // _ENCODERLAME_H
