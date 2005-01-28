fe07a1ba7f48f644304c288385262afb68aa001a
elupus <elupus@svn>
2005-01-28 19:44:35 +0000

    changed: using xbox highresolution timers for timing. Possibly better sync.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/mplayer@2748 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- osdep/timer-win2.c
+++ osdep/timer-win2.c
@@ -3,7 +3,52 @@
 #include <windows.h>
 #include <mmsystem.h>
 #include "timer.h"
+#ifdef _XBOX
+const char *timer_name = "XBOX High Resolution Timers for";
+LARGE_INTEGER lPerfFreq;
+//extern bool QueryPerformanceCounter(LARGE_INTEGER *lpPerformanceCount);
 
+
+void InitTimer()
+{
+  QueryPerformanceFrequency(&lPerfFreq);
+}
+
+unsigned int GetTimer()
+{
+  LARGE_INTEGER lCount;
+  QueryPerformanceCounter(&lCount);
+  return ((unsigned int)(lCount.QuadPart * (__int64)1000000 / lPerfFreq.QuadPart));
+}
+
+unsigned int GetTimerMS()
+{
+  LARGE_INTEGER lCount;
+  QueryPerformanceCounter(&lCount);
+  return ((unsigned int)(lCount.QuadPart * (__int64)1000 / lPerfFreq.QuadPart));
+}
+
+static __int64 RelativeTime = 0;
+
+float GetRelativeTime(){
+  LARGE_INTEGER t;
+  __int64 r;
+  QueryPerformanceCounter(&t);
+  r = t.QuadPart - RelativeTime;
+  RelativeTime = t.QuadPart;
+  return (float) r / (float) lPerfFreq.QuadPart;
+}
+
+int usec_sleep(int usec_delay){
+  // Sleep(0) won't sleep for one clocktick as the unix usleep 
+  // instead it will only make the thread ready
+  // it may take some time until it actually starts to run again
+  if(usec_delay<1000)usec_delay=1000;  
+  Sleep( usec_delay/1000);
+  return 0;
+}
+
+#else
 const char *timer_name = "Windows native";
 
 // Returns current time in microseconds
@@ -38,3 +83,4 @@ float GetRelativeTime(){
 void InitTimer(){
   GetRelativeTime();
 }
+#endif
