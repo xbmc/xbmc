63ba58610b1e31c645c3b1c3e4890d5aafab5023
elupus <elupus@svn>
2005-02-01 22:04:12 +0000

    added: mplayer support for new system\players directory structure, enable Alternate mplayer core in settings to use it. Requires new compiled mplayer.dll, old dll's still needs to be in old mplayer dir.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/mplayer@2779 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- get_path.c
+++ get_path.c
@@ -13,7 +13,7 @@
 char *get_path(char *filename)
 {
 	char *buff=malloc(1024);
-	sprintf(buff, "Q:\\mplayer\\%s", filename);
+	sprintf(buff, "Q:\\system\\players\\mplayer\\%s", filename);
 	mp_msg(MSGT_GLOBAL,MSGL_V,"get_path('%s') -> '%s'\n",filename,buff);
 	return buff;
 }
