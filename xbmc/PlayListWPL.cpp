b7f7c2cf6826ed5a5d5c5a49236db8a75b13e134
bobbin007 <bobbin007@svn>
2005-02-04 18:41:27 +0000

    added: windows media player playlists
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@2801 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/PlayListWPL.cpp
@@ -0,0 +1,101 @@
+
+#include "stdafx.h"
+#include "playlistwpl.h"
+#include "util.h"
+#include "tinyxml/tinyxml.h"
+
+/* ------------------------ example wpl playlist file ---------------------------------
+		<?wpl version="1.0"?>
+		<smil>
+		    <head>
+		        <meta name="Generator" content="Microsoft Windows Media Player -- 10.0.0.3646"/>
+		        <author/>
+		        <title>Playlist</title>
+		    </head>
+		    <body>
+		        <seq>
+		            <media src="E:\MP3\Track1.mp3"/>
+		            <media src="E:\MP3\Track2.mp3"/>
+		            <media src="E:\MP3\track3.mp3"/>
+		        </seq>
+		    </body>
+		</smil>
+------------------------ end of example wpl playlist file ---------------------------------*/
+CPlayListWPL::CPlayListWPL(void)
+{
+}
+
+CPlayListWPL::~CPlayListWPL(void)
+{
+}
+
+
+bool CPlayListWPL::Load(const CStdString& strFileName)
+{
+	CStdString strBasePath;
+	CUtil::GetParentPath(strFileName,strBasePath);
+
+	Clear();
+	
+	TiXmlDocument xmlDoc;
+  if (!xmlDoc.LoadFile( strFileName.c_str() ) ) return false;
+
+	TiXmlElement* pRootElement =xmlDoc.RootElement();
+	if (!pRootElement ) return false;
+
+	TiXmlElement* pHeadElement=pRootElement->FirstChildElement("head");
+	if (pHeadElement )
+	{
+		TiXmlElement* pTitelElement=pHeadElement->FirstChildElement("title");
+		if (pTitelElement )
+			m_strPlayListName=pTitelElement->Value();
+	}
+
+	TiXmlElement* pBodyElement=pRootElement->FirstChildElement("body");
+	if (!pBodyElement ) return false;
+
+	TiXmlElement* pSeqElement=pBodyElement->FirstChildElement("seq");
+	if (!pSeqElement ) return false;
+	
+	TiXmlElement* pMediaElement=pSeqElement->FirstChildElement("media");
+	
+	if (!pMediaElement) return false;
+	while (pMediaElement)
+	{
+		CStdString strFileName=pMediaElement->Attribute("src");
+		if (strFileName.size())
+		{
+			CUtil::GetQualifiedFilename(strBasePath,strFileName);
+			CStdString strDescription=CUtil::GetFileName(strFileName);
+			CPlayListItem newItem(strDescription,strFileName);
+			Add(newItem);
+		}
+		pMediaElement=pMediaElement->NextSiblingElement();
+	}
+	return true;
+}
+
+void CPlayListWPL::Save(const CStdString& strFileName) const
+{
+	if (!m_vecItems.size()) return;
+	FILE *fd = fopen(strFileName.c_str(),"w+");
+	if (!fd) return;
+	fprintf(fd,"<?wpl version=%c1.0%c>\n",34,34);
+	fprintf(fd,"<smil>\n");
+	fprintf(fd,"    <head>\n");
+	fprintf(fd,"        <meta name=%cGenerator%c content=%cMicrosoft Windows Media Player -- 10.0.0.3646%c/>\n", 34, 34, 34, 34);
+	fprintf(fd,"        <author/>\n");
+	fprintf(fd,"        <title>%s</title>\n", m_strPlayListName.c_str());
+	fprintf(fd,"    </head>\n");
+	fprintf(fd,"    <body>\n");
+	fprintf(fd,"        <seq>\n");
+	for (int i=0; i < (int)m_vecItems.size(); ++i)
+	{
+		const CPlayListItem& item = m_vecItems[i];
+		fprintf(fd,"            <media src=%c%s%c/>", 34, item.GetFileName().c_str(), 34);
+	}
+	fprintf(fd,"        </seq>\n");
+	fprintf(fd,"    </body>\n");
+	fprintf(fd,"</smil>\n");
+	fclose(fd);
+}
