3f344f3a96c7f0894ef850ba898e053255758b1f
charlydoes <charlydoes@svn>
2005-03-13 15:15:22 +0000

    added: Cache dialog shows progressbar when caching. changed: Radiobutton renders not selected when disabled. changed: Removed clear of z-buffer in videorenders.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3048 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dlgcache.h
+++ xbmc/cores/dlgcache.h
@@ -8,6 +8,8 @@ class CDlgCache
 	  void Update();
 	  void SetMessage(const CStdString& strMessage);
 	  bool IsCanceled() const { return m_pDlg->IsCanceled(); }
+    void ShowProgressBar(bool bOnOff) { m_pDlg->ShowProgressBar(bOnOff); }
+    void SetPercentage(int iPercentage) { m_pDlg->SetPercentage(iPercentage); }
   protected:
     CGUIDialogProgress* m_pDlg;
     CStdString m_strLinePrev;
