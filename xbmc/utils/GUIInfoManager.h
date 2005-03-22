ad8432cd6a1149c7eea5b03803d5f73bd3e00b95
jmarshallnz <jmarshallnz@svn>
2005-03-22 21:43:47 +0000

    - 22-03-2005 added: VideoPlayer.Cover infoimage to the infomanager.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3110 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.h
+++ xbmc/utils/GUIInfoManager.h
@@ -28,18 +28,18 @@ public:
   wstring GetTime(bool bSeconds = false);
   wstring GetDate(bool bNumbersOnly = false);
 
-  void SetCurrentItem(const CFileItem &item);
-  void ResetCurrentItem() { m_currentSong.Clear(); m_currentMovie.Reset(); };
+  void SetCurrentItem(CFileItem &item);
+  void ResetCurrentItem();
   // Current song stuff
   /// \brief Retrieves tag info (if necessary) and fills in our current song path.
-  void SetCurrentSong(const CFileItem &item);
+  void SetCurrentSong(CFileItem &item);
   void SetCurrentSongTag(const CMusicInfoTag &tag) { m_currentSong.m_musicInfoTag = tag; m_currentSong.m_lStartOffset = 0;};
   const CMusicInfoTag &GetCurrentSongTag() const { return m_currentSong.m_musicInfoTag; };
   long GetCurrentSongStart() { return m_currentSong.m_lStartOffset; };
   long GetCurrentSongEnd() { return m_currentSong.m_lEndOffset; };
 
   // Current movie stuff
-  void SetCurrentMovie(const CFileItem &item);
+  void SetCurrentMovie(CFileItem &item);
   const CIMDBMovie &GetCurrentMovie() const { return m_currentMovie; };
 
   CStdString GetMusicLabel(const CStdString &strItem);
@@ -57,6 +57,7 @@ protected:
   // Current playing stuff
   CFileItem m_currentSong;
   CIMDBMovie m_currentMovie;
+  CStdString m_currentMovieThumb;
 
   // fan stuff
   DWORD m_lastSysHeatInfoTime;
