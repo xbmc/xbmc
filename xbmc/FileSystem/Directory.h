165c3796cbdfe600935282e3718aaf0899799d4f
bobbin007 <bobbin007@svn>
2005-04-01 13:39:10 +0000

    fixed: vcd's/svcd's autostart only the first movie on a cd
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3141 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/Directory.h
+++ xbmc/FileSystem/Directory.h
@@ -13,7 +13,7 @@ public:
   CDirectory(void);
   virtual ~CDirectory(void);
 
-  static bool GetDirectory(const CStdString& strPath, CFileItemList &items);
+  static bool GetDirectory(const CStdString& strPath, CFileItemList &items, CStdString strMask="");
   static bool Create(const char* strPath);
   static bool Exists(const char* strPath);
   static bool Remove(const char* strPath);
