74bdefa9514da4cc207f753d24779cc387aa0d72
jmarshallnz <jmarshallnz@svn>
2005-04-10 16:24:11 +0000

    - 10-04-2005 fixed: Crash in home on PM3 during video playback with UI off-center due to SetViewWindow() not correctly bounds checking.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3206 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoActors.cpp
+++ xbmc/GUIWindowVideoActors.cpp
@@ -18,8 +18,8 @@
 #define CONTROL_PLAY_DVD           6
 #define CONTROL_STACK              7
 #define CONTROL_IMDB        9
-#define CONTROL_LIST       10
-#define CONTROL_THUMBS      11
+#define CONTROL_LIST       50
+#define CONTROL_THUMBS      51
 #define CONTROL_LABELFILES        12
 #define LABEL_ACTOR              100
 
