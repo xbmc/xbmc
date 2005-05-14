e7b8d61c5a0e54d0fa9b2e65b5f9d9ce00e1d600
bobbin007 <bobbin007@svn>
2005-05-14 15:48:01 +0000

    added: WAV codec to paplayer
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@3457 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/MusicInfoTagLoaderWav.cpp
@@ -0,0 +1,35 @@
+
+#include "stdafx.h"
+#include "MusicInfoTagLoaderWav.h"
+#include "cores/paplayer/WAVCodec.h"
+
+using namespace MUSIC_INFO;
+
+CMusicInfoTagLoaderWAV::CMusicInfoTagLoaderWAV(void)
+{}
+
+CMusicInfoTagLoaderWAV::~CMusicInfoTagLoaderWAV()
+{}
+
+bool CMusicInfoTagLoaderWAV::Load(const CStdString& strFileName, CMusicInfoTag& tag)
+{
+  try
+  {
+    // WAV has no tag information other than the duration.
+
+    // Load our codec class
+    WAVCodec codec;
+    if (codec.Init(strFileName))
+    {
+      tag.SetDuration((int)(codec.m_TotalTime/1000));
+      codec.DeInit();
+      return true;
+    }
+  }
+  catch (...)
+  {
+    CLog::Log(LOGERROR, "Tag loader wav: exception in file %s", strFileName.c_str());
+  }
+
+  return false;
+}
