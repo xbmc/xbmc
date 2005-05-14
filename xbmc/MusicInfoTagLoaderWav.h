17b0ff7adb822db7c62cb5375a34852cccf02c85
bobbin007 <bobbin007@svn>
2005-05-14 15:48:01 +0000

    added: WAV codec to paplayer
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3457 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/MusicInfoTagLoaderWav.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "IMusicInfoTagLoader.h"
+
+using namespace MUSIC_INFO;
+
+namespace MUSIC_INFO
+{
+
+class CMusicInfoTagLoaderWAV: public IMusicInfoTagLoader
+{
+public:
+  CMusicInfoTagLoaderWAV(void);
+  virtual ~CMusicInfoTagLoaderWAV();
+
+  virtual bool Load(const CStdString& strFileName, CMusicInfoTag& tag);
+};
+};
