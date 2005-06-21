5dc6192ef1ef9f057440547679b0cb41f6650625
bobbin007 <bobbin007@svn>
2005-06-21 18:39:05 +0000

    changed: added more chars (����������������) to the allowed for fatx limits
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3686 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -1685,7 +1685,7 @@ void CUtil::RemoveIllegalChars( CStdString& strText)
 {
   char szRemoveIllegal [1024];
   strcpy(szRemoveIllegal , strText.c_str());
-  static char legalChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()-@[]^_`{}~.והצר ";
+  static char legalChars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&'()-@[]^_`{}~.ßֵוִהײצÜüריטחאשךֲסב ";
   char *cursor;
   for (cursor = szRemoveIllegal; *(cursor += strspn(cursor, legalChars)); /**/ )
     *cursor = '_';
