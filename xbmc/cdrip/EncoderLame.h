00ff474adcdaf5ce9534a88ad2d066fc2baafd84
jmarshallnz <jmarshallnz@svn>
2005-06-27 10:23:55 +0000

    - 27-06-2005 fixed: upper case MP3/AAC extensions weren't read correctly.  - 27-06-2005 fixed: Very long file names reading from smb could crash.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3710 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cdrip/EncoderLame.h
+++ xbmc/cdrip/EncoderLame.h
@@ -19,8 +19,8 @@ protected:
   lame_global_flags* m_pGlobalFlags;
 
   unsigned char m_buffer[48160]; // mp3buf_size in bytes = 1.25*(chunk size / 4) + 7200
-  char m_inPath[MAX_PATH + 1];
-  char m_outPath[MAX_PATH + 1];
+  char m_inPath[XBMC_MAX_PATH + 1];
+  char m_outPath[XBMC_MAX_PATH + 1];
 
   DllLoader* m_pDLLLame;
 };
