35a92f488e4fd9d748a42cf965498606bbf4ff10
jmarshallnz <jmarshallnz@svn>
2005-06-29 13:11:51 +0000

    - 28-06-2005 changed: keymap.xml format is now much simpler to understand.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3724 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ButtonTranslator.h
+++ xbmc/ButtonTranslator.h
@@ -18,9 +18,14 @@ public:
 private:
   typedef multimap<WORD, CButtonAction> buttonMap; // our button map to fill in
   map<WORD, buttonMap> translatorMap;       // mapping of windows to button maps
-  void MapAction(WORD wAction, const CStdString &strAction, TiXmlNode *pNode, buttonMap &map);
   WORD GetActionCode(WORD wWindow, const CKey &key, CStdString &strAction);
-  bool TranslateActionString(const char *szAction, WORD &wAction); 
+  bool TranslateActionString(const char *szAction, WORD &wAction);
+  WORD TranslateGamepadString(const char *szButton);
+  WORD TranslateRemoteString(const char *szButton);
+  WORD TranslateUniversalRemoteString(const char *szButton);
+  WORD TranslateKeyboardString(const char *szButton);
+  void MapWindowActions(TiXmlNode *pWindow, WORD wWindowID);
+  void MapAction(WORD wButtonCode, const char *szAction, buttonMap &map);
 };
 
 extern CButtonTranslator g_buttonTranslator;
