1ca280de4d15ec1db31cd5496f8943c204749fd7
pike2k <pike2k@svn>
2005-07-01 22:41:35 +0000

    changed: 2 small updates to Build.bat: made config paths long filename & removed 2 unnecessary copy actions.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@3734 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/Build.bat
+++ XBMC/Build.bat
@@ -5,23 +5,22 @@ rem - Create a working XBMC build with a single click
 rem ---------------------------------------------
 rem Config
 rem Set your path for VSNET main executable
-rem Set the path for xbepatch
-rem Set the path for WinRARs rar.exe (freeware)
+rem Set the path for xbepatch (this is only necessary if you have a very old bios)
+rem Set the path for WinRAR's rar.exe (freeware)
 rem and finally set the options for the final rar.
 rem ---------------------------------------------
 rem Remove 'rem' from %NET% to compile and/or clean the solution prior packing it.
 rem Remove 'rem' from 'xcopy web/python' to copy these to the BUILD directory.
-rem For any item that is REM'ed you can enable them by removing REM.
 rem ---------------------------------------------
 TITLE XBMC Build Prepare Script
 ECHO Wait while preparing the build.
 ECHO ------------------------------
 rem	CONFIG START
-	set NET=C:\Progra~1\Micros~1.NET\Common7\IDE\devenv.com
+	set NET="C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.com"
 	set OPTS=xbmc.sln /build release
 	set CLEAN=xbmc.sln /clean release
 	set XBE=xbepatch.exe
-	set RAR=C:\Progra~1\Winrar\rar.exe
+	set RAR="C:\Program Files\Winrar\rar.exe"
 	set RAROPS1=a -r -idp -inul -m5 XBMC.rar BUILD
 	set SKINS=..\Skins
 rem	CONFIG END
@@ -42,8 +41,6 @@ copy *.txt BUILD
 xcopy "skin\Project Mayhem III\fonts" "BUILD\skin\Project Mayhem III\fonts" /E /Q /I /Y
 xcopy "skin\Project Mayhem III\*.xml" "BUILD\skin\Project Mayhem III\" /E /Q /I /Y
 xcopy "skin\Project Mayhem III\media\Textures.xpr" "BUILD\skin\Project Mayhem III\media" /Q /I /Y
-xcopy "skin\Project Mayhem III\media\background.png" "BUILD\skin\Project Mayhem III\media" /Q /I /Y
-xcopy "skin\Project Mayhem III\media\background3.png" "BUILD\skin\Project Mayhem III\media" /Q /I /Y
 xcopy "skin\Project Mayhem III\sounds\*.*" "BUILD\skin\Project Mayhem III\sounds" /Q /I /Y 
 xcopy credits BUILD\credits /Q /I /Y
 xcopy language BUILD\language /E /Q /I /Y
