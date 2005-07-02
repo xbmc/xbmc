7117303cd122a434244610678bdd341ca58c7b40
xbmcapi <xbmcapi@svn>
2005-07-02 18:45:09 +0000

    Changed: Some of the API command names to make the names more logical - the old ones still work though
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3746 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libGoAhead/XBMChttp.h
+++ xbmc/lib/libGoAhead/XBMChttp.h
@@ -3,9 +3,9 @@
 /******************************** Description *********************************/
 
 /* 
- *	Header file that provides an API over HTTP between the web server and XBMC
+ *  Header file that provides an API over HTTP between the web server and XBMC
  *
- *						heavily based on XBMCweb.h
+ *            heavily based on XBMCweb.h
  */
 
 /********************************* Includes ***********************************/
@@ -16,64 +16,64 @@
 class CXbmcHttp
 {
 public:
-	CXbmcHttp();
-	~CXbmcHttp();
+  CXbmcHttp();
+  ~CXbmcHttp();
 
-	void		xbmcForm(webs_t wp, char_t *path, char_t *query);
-	int			xbmcProcessCommand( int eid, webs_t wp, char_t *command, char_t *parameter);
+  void xbmcForm(webs_t wp, char_t *path, char_t *query);
+  int  xbmcProcessCommand( int eid, webs_t wp, char_t *command, char_t *parameter);
 
-
-	int			xbmcAddToPlayList(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcPlayerPlayFile(webs_t wp, int numParas, CStdString paras[]); 
-	int			xbmcClearPlayList(webs_t wp, int numParas, CStdString paras[]); 
-	int			xbmcGetCurrentlyPlaying(  webs_t wp); 
-	int			xbmcGetDirectory(webs_t wp, int numParas, CStdString paras[]); 
-	int			xbmcGetTagFromFilename(webs_t wp, int numParas, CStdString paras[]); 
-	int			xbmcGetCurrentPlayList(webs_t wp);
-	int			xbmcSetCurrentPlayList(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetPlayListContents(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcRemoveFromPlayList(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcSetPlayListSong(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetPlayListSong(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcPlayListNext(webs_t wp);
-	int			xbmcPlayListPrev(webs_t wp);
-	int			xbmcSetVolume(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetVolume(webs_t wp);
-	int			xbmcGetPercentage(webs_t wp);
-	int			xbmcSeekPercentage(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcAction(webs_t wp, int numParas, CStdString paras[], int theAction);
-	int			xbmcExit(webs_t wp, int theAction);
-	int			xbmcGetThumb(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetThumbFilename(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcLookupAlbum(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcChooseAlbum(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcDownloadInternetFile(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcSetKey(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetMovieDetails(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcDeleteFile(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcCopyFile(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcSetFile(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcFileExists(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcShowPicture(webs_t wp, int numParas, CStdString paras[]);
-	int			xbmcGetGUIStatus(webs_t wp);
-  int     xbmcExecBuiltIn(webs_t wp, int numParas, CStdString paras[]);
-  int     xbmcConfig(webs_t wp, int numParas, CStdString paras[]);
-  int			xbmcHelp(webs_t wp);
-  int     xbmcGetSystemInfo(webs_t wp, int numParas, CStdString paras[]);
-  int     xbmcGetSystemInfoByName(webs_t wp, int numParas, CStdString paras[]);
-  int     xbmcAddToSlideshow(webs_t wp, int numParas, CStdString paras[]);
-  int			xbmcClearSlideshow(webs_t wp);
-  int			xbmcPlaySlideshow(webs_t wp, int numParas, CStdString paras[]);
-  int     xbmcSlideshowSelect(webs_t wp, int numParas, CStdString paras[]);
-  int     xbmcGetSlideshowContents( webs_t wp);
-  int     xbmcGetCurrentSlide( webs_t wp);
-  int			xbmcGUISetting(webs_t wp, int numParas, CStdString paras[]);
-  int			xbmcTakeScreenshot(webs_t wp, int numParas, CStdString paras[]);
-	CKey		GetKey();
-	void		ResetKey();
+  int  xbmcAddToPlayList(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcPlayerPlayFile(webs_t wp, int numParas, CStdString paras[]); 
+  int  xbmcClearPlayList(webs_t wp, int numParas, CStdString paras[]); 
+  int  xbmcGetCurrentlyPlaying(  webs_t wp); 
+  int  xbmcGetDirectory(webs_t wp, int numParas, CStdString paras[]); 
+  int  xbmcGetTagFromFilename(webs_t wp, int numParas, CStdString paras[]); 
+  int  xbmcGetCurrentPlayList(webs_t wp);
+  int  xbmcSetCurrentPlayList(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetPlayListContents(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcRemoveFromPlayList(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcSetPlayListSong(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetPlayListSong(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcPlayListNext(webs_t wp);
+  int  xbmcPlayListPrev(webs_t wp);
+  int  xbmcSetVolume(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetVolume(webs_t wp);
+  int  xbmcGetPercentage(webs_t wp);
+  int  xbmcSeekPercentage(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcAction(webs_t wp, int numParas, CStdString paras[], int theAction);
+  int  xbmcExit(webs_t wp, int theAction);
+  int  xbmcGetThumb(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetThumbFilename(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcLookupAlbum(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcChooseAlbum(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcDownloadInternetFile(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcSetKey(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetMovieDetails(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcDeleteFile(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcCopyFile(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcSetFile(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcFileExists(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcShowPicture(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetGUIStatus(webs_t wp);
+  int  xbmcExecBuiltIn(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcConfig(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcHelp(webs_t wp);
+  int  xbmcGetSystemInfo(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetSystemInfoByName(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcAddToSlideshow(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcClearSlideshow(webs_t wp);
+  int  xbmcPlaySlideshow(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcSlideshowSelect(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetSlideshowContents( webs_t wp);
+  int  xbmcGetCurrentSlide( webs_t wp);
+  int  xbmcGUISetting(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcTakeScreenshot(webs_t wp, int numParas, CStdString paras[]);
+  int  xbmcGetGUIDescription( webs_t wp);
+  CKey GetKey();
+  void ResetKey();
 
 private:
-	CKey key;
+  CKey key;
 
 };
 
@@ -84,5 +84,3 @@ private:
 #define WEB_PARAMETER T("parameter")
 
 extern CXbmcHttp* pXbmcHttp; //make it global so Application.cpp can access it for key/button messages
-
-
