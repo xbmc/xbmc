3307fcfd15ec90c2ff71d1b20eea803fd17805e9
jwnmulder <jwnmulder@svn>
2005-07-14 15:06:52 +0000

    fixed: dvdplayer, playing a vob file resulted in a crash with the new ffmpeg added: dvdplayer, extra audio stream information in OSD fixed: dvdplayer, no audio streams are reported when playing an avi file
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@3879 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/DVDDemuxers/DVDDemux.cpp
+++ XBMC/xbmc/cores/dvdplayer/DVDDemuxers/DVDDemux.cpp
@@ -1,5 +1,82 @@
 
 #include "../../../stdafx.h"
 #include "DVDDemux.h"
+#include "..\ffmpeg\ffmpeg.h"
 
 
+void CDemuxStreamAudio::GetStreamType(std::string& strInfo)
+{
+  char sInfo[32];
+  
+  if (codec == CODEC_ID_AC3) strcpy(sInfo, "AC3 ");
+  else if (codec == CODEC_ID_DTS) strcpy(sInfo, "DTS ");
+    
+  if (iChannels == 1) strcat(sInfo, "mono");
+  else if (iChannels == 2) strcat(sInfo, "stereo");
+  else if (iChannels == 6) strcat(sInfo, "5.1");
+  else if (iChannels != 0)
+  {
+    char temp[32];
+    sprintf(temp, " %d %s", iChannels, "channels");
+    strcat(sInfo, temp);
+  }
+  strInfo = sInfo;
+}
+
+int CDVDDemux::GetNrOfAudioStreams()
+{
+  int iCounter = 0;
+
+  for (int i = 0; i < GetNrOfStreams(); i++)
+  {
+    CDemuxStream* pStream = GetStream(i);
+    if (pStream->type == STREAM_AUDIO) iCounter++;
+  }
+  
+  return iCounter;
+}
+
+int CDVDDemux::GetNrOfVideoStreams()
+{
+  int iCounter = 0;
+
+  for (int i = 0; i < GetNrOfStreams(); i++)
+  {
+    CDemuxStream* pStream = GetStream(i);
+    if (pStream->type == STREAM_VIDEO) iCounter++;
+  }
+  
+  return iCounter;
+}
+
+CDemuxStreamAudio* CDVDDemux::GetStreamFromAudioId(int iAudioIndex)
+{
+  int counter = -1;
+  for (int i = 0; i < GetNrOfStreams(); i++)
+  {
+    CDemuxStream* pStream = GetStream(i);
+
+    if (pStream->type == STREAM_AUDIO) counter++;
+    if (iAudioIndex == counter)
+    {
+      return (CDemuxStreamAudio*)pStream;
+    }
+  }
+  return NULL;
+}
+
+CDemuxStreamVideo* CDVDDemux::GetStreamFromVideoId(int iVideoIndex)
+{
+  int counter = -1;
+  for (int i = 0; i < GetNrOfStreams(); i++)
+  {
+    CDemuxStream* pStream = GetStream(i);
+
+    if (pStream->type == STREAM_VIDEO) counter++;
+    if (iVideoIndex == counter)
+    {
+      return (CDemuxStreamVideo*)pStream;
+    }
+  }
+  return NULL;
+}
