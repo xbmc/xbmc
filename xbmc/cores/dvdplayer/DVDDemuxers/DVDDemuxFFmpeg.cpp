7949ea2b03c972e0bd8c22a2c2fd4ccb3cb46512
jwnmulder <jwnmulder@svn>
2005-07-17 20:41:45 +0000

    fixed: dvdplayer crashed if a dvd had 20 streams at the same time
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3908 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -412,7 +412,7 @@ CDemuxStream* CDVDDemuxFFmpeg::GetStream(int iStreamId)
 int CDVDDemuxFFmpeg::GetNrOfStreams()
 {
   int i = 0;
-  while (m_streams[i]) i++;
+  while (m_streams[i] && i < MAX_STREAMS) i++;
   return i;
 }
 
