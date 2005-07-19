c218b75fa8d4836068bca87f559d547178bf60cf
jmarshallnz <jmarshallnz@svn>
2005-07-19 20:48:33 +0000

    - 19-07-2005 fixed: Settings weren't saved on app exit.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@3933 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.h
+++ xbmc/Util.h
@@ -68,7 +68,7 @@ public:
   static CStdString GetTitleFromPath(const CStdString& strFileNameAndPath);
   static bool IsHD(const CStdString& strFileName);
   static bool IsBuiltIn(const CStdString& execString);
-  static void ExecBuiltIn(const CStdString& execString);
+  static bool ExecBuiltIn(const CStdString& execString);
   static bool GetParentPath(const CStdString& strPath, CStdString& strParent);
   static void GetQualifiedFilename(const CStdString &strBasePath, CStdString &strFilename);
   static bool PatchCountryVideo(F_COUNTRY Country, F_VIDEO Video);
