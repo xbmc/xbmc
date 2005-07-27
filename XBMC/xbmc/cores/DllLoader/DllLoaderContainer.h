ff1a8ac7669402494074a84c6088f8e5583d5465
jwnmulder <jwnmulder@svn>
2005-07-27 21:18:43 +0000

    fixed: pyhon did not work at all after the dll update
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@4026 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/DllLoader/DllLoaderContainer.h
+++ XBMC/xbmc/cores/DllLoader/DllLoaderContainer.h
@@ -27,56 +27,59 @@ void export_comctl32();
 void export_msvcrt();
 void export_msvcr71();
 void export_pncrt();
+void export_python23();
 
 class DllLoaderContainer
 {
 public:
   DllLoaderContainer() :
       kernel32("kernel32.dll", false, true),
+      msvcr71("msvcr71.dll", false, true),
+      msvcrt("msvcrt.dll", false, true),
+      wsock32("wsock32.dll", false, true),
+      ws2_32("ws2_32.dll", false, true),
       user32("user32.dll", false, true),
       ddraw("ddraw.dll", false, true),
       wininet("wininet.dll", false, true),
       advapi32("advapi32.dll", false, true),
-      ws2_32("ws2_32.dll", false, true),
       ole32("ole32.dll", false, true),
       xbp("xbp.dll", false, true),
       winmm("winmm.dll", false, true),
       msdmo("msdmo.dll", false, true),
       xbmc_vobsub("xbmc_vobsub.dll", false, true),
-      wsock32("wsock32.dll", false, true),
       xbox_dx8("xbox_dx8.dll", false, true),
       xbox___dx8("xbox-dx8.dll", false, true),
       version("version.dll", false, true),
       comdlg32("comdlg32.dll", false, true),
       gdi32("gdi32.dll", false, true),
       comctl32("comctl32.dll", false, true),
-      msvcrt("msvcrt.dll", false, true),
-      msvcr71("msvcr71.dll", false, true),
-      pncrt("pncrt.dll", false, true)
+      pncrt("pncrt.dll", false, true),
+      python23("python23.dll", false, true)
   {
     m_iNrOfDlls = 0;
     
     RegisterDll(&kernel32); export_kernel32();
+    RegisterDll(&msvcr71); export_msvcr71();
+    RegisterDll(&msvcrt); export_msvcrt();
+    RegisterDll(&wsock32); export_wsock32();
+    RegisterDll(&ws2_32); export_ws2_32();
     RegisterDll(&user32); export_user32();
     RegisterDll(&ddraw); export_ddraw();
     RegisterDll(&wininet); // nothing is exported in this dll, is this one really needed?
     RegisterDll(&advapi32); export_reg();
-    RegisterDll(&ws2_32); export_ws2_32();
     RegisterDll(&ole32); export_ole32();
     RegisterDll(&xbp); export_xbp();
     RegisterDll(&winmm); export_winmm();
     RegisterDll(&msdmo); export_msdmo();
     RegisterDll(&xbmc_vobsub); export_xbmc_vobsub();
-    RegisterDll(&wsock32); export_wsock32();
     RegisterDll(&xbox_dx8); export_xbox_dx8();
     RegisterDll(&xbox___dx8); export_xbox___dx8();
     RegisterDll(&version); export_version();
     RegisterDll(&comdlg32); export_comdlg32();
     RegisterDll(&gdi32); export_gdi32();
     RegisterDll(&comctl32); export_comctl32();
-    RegisterDll(&msvcrt); export_msvcrt();
-    RegisterDll(&msvcr71); export_msvcr71();
     RegisterDll(&pncrt); export_pncrt();
+    RegisterDll(&python23); export_python23();
   }
 
   DllLoader kernel32;
@@ -100,6 +103,7 @@ public:
   DllLoader msvcrt;
   DllLoader msvcr71;
   DllLoader pncrt;
+  DllLoader python23;
   
   void Clear();
   bool ContainsModule(HMODULE hModule);
