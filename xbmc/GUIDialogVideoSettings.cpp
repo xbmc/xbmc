f62cb0f84c11cbe20ff7b61e4484b32b98bd359e
jmarshallnz <jmarshallnz@svn>
2005-08-02 19:25:35 +0000

    - 02-08-2005 added: More stuff for the new video OSD system.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4069 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIDialogVideoSettings.cpp
+++ xbmc/GUIDialogVideoSettings.cpp
@@ -14,6 +14,7 @@
 #define CONTROL_DEFAULT_RADIOBUTTON 8
 #define CONTROL_DEFAULT_SPIN        9
 #define CONTROL_DEFAULT_SLIDER     10
+#define CONTROL_DEFAULT_SEPARATOR  11
 #define CONTROL_START              50
 #define CONTROL_PAGE               60
 
@@ -26,6 +27,8 @@ CGUIDialogVideoSettings::CGUIDialogVideoSettings(void)
   m_pOriginalSpin = NULL;
   m_pOriginalRadioButton = NULL;
   m_pOriginalSettingsButton = NULL;
+  m_pOriginalSlider = NULL;
+  m_pOriginalSeparator = NULL;
   m_iCurrentPage = 0;
   m_iNumPages = 0;
   m_iNumPerPage = 0;
@@ -89,12 +92,14 @@ void CGUIDialogVideoSettings::SetupPage()
   m_pOriginalRadioButton = (CGUIRadioButtonControl *)GetControl(CONTROL_DEFAULT_RADIOBUTTON);
   m_pOriginalSettingsButton = (CGUIButtonControl *)GetControl(CONTROL_DEFAULT_BUTTON);
   m_pOriginalSlider = (CGUISettingsSliderControl *)GetControl(CONTROL_DEFAULT_SLIDER);
-  if (!m_pOriginalSpin || !m_pOriginalRadioButton || !m_pOriginalSettingsButton || !m_pOriginalSlider)
+  m_pOriginalSeparator = (CGUIImage *)GetControl(CONTROL_DEFAULT_SEPARATOR);
+  if (!m_pOriginalSpin || !m_pOriginalRadioButton || !m_pOriginalSettingsButton || !m_pOriginalSlider || !m_pOriginalSeparator)
     return;
   m_pOriginalSpin->SetVisible(false);
   m_pOriginalRadioButton->SetVisible(false);
   m_pOriginalSettingsButton->SetVisible(false);
   m_pOriginalSlider->SetVisible(false);
+  m_pOriginalSeparator->SetVisible(false);
 
   // update our settings label
   SET_CONTROL_LABEL(CONTROL_SETTINGS_LABEL, g_localizeStrings.Get(13395 + m_iScreen - WINDOW_DIALOG_VIDEO_OSD_SETTINGS));
@@ -121,10 +126,14 @@ void CGUIDialogVideoSettings::SetupPage()
   int iWidth = pControlArea->GetWidth();
   int iPosY = pControlArea->GetYPosition();
   int iGapY = pControlGap->GetHeight();
-  int numSettings = m_settings.size();
-  m_iNumPerPage = (int)pControlArea->GetHeight() / iGapY;
-  if (m_iNumPerPage < 1) m_iNumPerPage = 1;
-  m_iNumPages = (numSettings + m_iNumPerPage - 1)/ m_iNumPerPage; // round up
+  int numSettings = 0;
+  for (unsigned int i=0; i < m_settings.size(); i++)
+    if (m_settings[i].type != VideoSetting::SEPARATOR)
+      numSettings++;
+
+  int numPerPage = (int)pControlArea->GetHeight() / iGapY;
+  if (numPerPage < 1) numPerPage = 1;
+  m_iNumPages = (numSettings + numPerPage - 1)/ numPerPage; // round up
   if (m_iCurrentPage >= m_iNumPages - 1)
     m_iCurrentPage = m_iNumPages - 1;
   if (m_iCurrentPage <= 0)
@@ -133,30 +142,51 @@ void CGUIDialogVideoSettings::SetupPage()
   if (pPage)
   {
     pPage->SetRange(1, m_iNumPages);
-    pPage->SetReverse(true);
+    pPage->SetReverse(false);
     pPage->SetShowRange(true);
     pPage->SetValue(m_iCurrentPage + 1);
     pPage->SetVisible(m_iNumPages > 1);
   }
 
-  // run through and create our controls
-  int numOnPage = 0;
-  for (int i = 0; i < m_iNumPerPage && i + m_iCurrentPage * m_iNumPerPage < numSettings; i++)
+  // find out page offset
+  m_iPageOffset = 0;
+  for (int i = 0; i < m_iCurrentPage; i++)
   {
-    VideoSetting &setting = m_settings.at(i + m_iCurrentPage * m_iNumPerPage);
-    AddSetting(setting, iPosX, iPosY, iWidth, CONTROL_START + i);
-	  iPosY += iGapY;
-    numOnPage++;
+    int j = 0;
+    while (j < numPerPage)
+    {
+      if (m_settings[m_iPageOffset].type != VideoSetting::SEPARATOR)
+        j++;
+      m_iPageOffset++;
+    }
+  }
+  // create our controls
+  int numSettingsOnPage = 0;
+  int numControlsOnPage = 0;
+  for (unsigned int i = m_iPageOffset; i < m_settings.size(); i++)
+  {
+    VideoSetting &setting = m_settings.at(i);
+    AddSetting(setting, iPosX, iPosY, iWidth, CONTROL_START + i - m_iPageOffset);
+    if (setting.type == VideoSetting::SEPARATOR)
+      iPosY += m_pOriginalSeparator->GetHeight();
+    else
+    {
+      iPosY += iGapY;
+      numSettingsOnPage++;
+    }
+    numControlsOnPage++;
+    if (numSettingsOnPage == numPerPage)
+      break;
   }
   // fix first and last navigation
-  CGUIControl *pControl = (CGUIControl *)GetControl(CONTROL_START + numOnPage - 1);
+  CGUIControl *pControl = (CGUIControl *)GetControl(CONTROL_START + numControlsOnPage - 1);
   if (pControl) pControl->SetNavigation(pControl->GetControlIdUp(), CONTROL_PAGE,
                                           pControl->GetControlIdLeft(), pControl->GetControlIdRight());
   pControl = (CGUIControl *)GetControl(CONTROL_START);
   if (pControl) pControl->SetNavigation(CONTROL_PAGE, pControl->GetControlIdDown(),
                                           pControl->GetControlIdLeft(), pControl->GetControlIdRight());
   pControl = (CGUIControl *)GetControl(CONTROL_PAGE);
-  if (pControl) pControl->SetNavigation(CONTROL_START + numOnPage - 1, CONTROL_START,
+  if (pControl) pControl->SetNavigation(CONTROL_START + numControlsOnPage - 1, CONTROL_START,
                                           pControl->GetControlIdLeft(), pControl->GetControlIdRight());
 }
 
@@ -173,7 +203,7 @@ void CGUIDialogVideoSettings::UpdateSetting(unsigned int num)
     }
   }
   VideoSetting &setting = m_settings.at(settingNum);
-  unsigned int controlID = settingNum + CONTROL_START + m_iCurrentPage * m_iNumPerPage;
+  unsigned int controlID = settingNum + CONTROL_START + m_iPageOffset;
   if (setting.type == VideoSetting::SPIN)
   {
     CGUISpinControlEx *pControl = (CGUISpinControlEx *)GetControl(controlID);
@@ -198,7 +228,7 @@ void CGUIDialogVideoSettings::UpdateSetting(unsigned int num)
 
 void CGUIDialogVideoSettings::OnClick(int iID)
 {
-  unsigned int settingNum = iID - CONTROL_START + m_iCurrentPage * m_iNumPerPage;
+  unsigned int settingNum = iID - CONTROL_START + m_iPageOffset;
   if (settingNum >= m_settings.size()) return;
   VideoSetting &setting = m_settings.at(settingNum);
   if (setting.type == VideoSetting::SPIN)
@@ -247,6 +277,13 @@ void CGUIDialogVideoSettings::AddSetting(VideoSetting &setting, int iPosX, int i
     pControl = new CGUIButtonControl(*m_pOriginalSettingsButton);
     if (!pControl) return ;
     ((CGUIButtonControl *)pControl)->SetText(setting.name);
+    pControl->SetPosition(iPosX, iPosY);
+    pControl->SetWidth(iWidth);
+  }
+  else if (setting.type == VideoSetting::SEPARATOR)
+  {
+    pControl = new CGUIImage(*m_pOriginalSeparator);
+    if (!pControl) return ;
     pControl->SetPosition(iPosX, iPosY);
     pControl->SetWidth(iWidth);
   }
@@ -377,6 +414,15 @@ void CGUIDialogVideoSettings::AddSlider(unsigned int id, int label, int *current
   m_settings.push_back(setting);
 }
 
+void CGUIDialogVideoSettings::AddSeparator(unsigned int id)
+{
+  VideoSetting setting;
+  setting.id = id;
+  setting.type = VideoSetting::SEPARATOR;
+  setting.data = NULL;
+  m_settings.push_back(setting);
+}
+
 void CGUIDialogVideoSettings::CreateSettings()
 {
   // clear out any old settings
@@ -416,13 +462,14 @@ void CGUIDialogVideoSettings::CreateSettings()
     AddSlider(1, 13376, &m_volume, VOLUME_MINIMUM * 0.01f, (VOLUME_MAXIMUM - VOLUME_MINIMUM) * 0.0001f, VOLUME_MAXIMUM * 0.01f, "%2.1f dB");
     AddSlider(2, 297, &g_stSettings.m_currentVideoSettings.m_AudioDelay, -g_stSettings.m_fAudioDelayRange, 0.1f, g_stSettings.m_fAudioDelayRange, "%2.1fs");
     AddAudioStreams(3);
-    AddBool(4, 13397, &g_stSettings.m_currentVideoSettings.m_SubtitleOn);
-    AddSlider(5, 303, &g_stSettings.m_currentVideoSettings.m_SubtitleDelay, -g_stSettings.m_fSubsDelayRange, 0.1f, g_stSettings.m_fSubsDelayRange, "%2.1fs");
-    AddSubtitleStreams(6);
+    AddSeparator(4);
+    AddBool(5, 13397, &g_stSettings.m_currentVideoSettings.m_SubtitleOn);
+    AddSlider(6, 303, &g_stSettings.m_currentVideoSettings.m_SubtitleDelay, -g_stSettings.m_fSubsDelayRange, 0.1f, g_stSettings.m_fSubsDelayRange, "%2.1fs");
+    AddSubtitleStreams(7);
   }
 }
 
-void CGUIDialogVideoSettings::AddAudioStreams(int id)
+void CGUIDialogVideoSettings::AddAudioStreams(unsigned int id)
 {
   VideoSetting setting;
   setting.id = id;
@@ -472,14 +519,15 @@ void CGUIDialogVideoSettings::AddAudioStreams(int id)
   m_settings.push_back(setting);
 }
 
-void CGUIDialogVideoSettings::AddSubtitleStreams(int id)
+void CGUIDialogVideoSettings::AddSubtitleStreams(unsigned int id)
 {
   VideoSetting setting;
   setting.id = id;
   setting.name = g_localizeStrings.Get(462);
   setting.type = VideoSetting::SPIN;
   setting.min = 0;
-  setting.data = &g_stSettings.m_currentVideoSettings.m_SubtitleStream;
+  setting.data = &m_subtitleStream;
+  m_subtitleStream = g_stSettings.m_currentVideoSettings.m_SubtitleStream;
 
   // get the number of audio strams for the current movie
   setting.max = (float)g_application.m_pPlayer->GetSubtitleCount() - 1;
@@ -496,6 +544,7 @@ void CGUIDialogVideoSettings::AddSubtitleStreams(int id)
 
   if (setting.max < 0)
   { // no subtitle streams - just add a "None" entry
+    m_subtitleStream = 0;
     setting.max = 0;
     setting.entry.push_back(g_localizeStrings.Get(231).c_str());
   }
@@ -580,27 +629,28 @@ void CGUIDialogVideoSettings::OnSettingChanged(unsigned int num)
           g_application.Restart(true);  // restart to make new audio track active
       }
     }
-    else if (setting.id == 4)
-      g_application.m_pPlayer->SetSubtitleVisible(g_stSettings.m_currentVideoSettings.m_SubtitleOn);
     else if (setting.id == 5)
-      g_application.m_pPlayer->SetSubTitleDelay(g_stSettings.m_currentVideoSettings.m_SubtitleDelay);
-    else if (setting.id == 6)
     {
-      if (setting.max == 0)
-        return;   // nothing to do
-      g_application.m_pPlayer->SetSubtitle(g_stSettings.m_currentVideoSettings.m_SubtitleStream);
+      g_application.m_pPlayer->SetSubtitleVisible(g_stSettings.m_currentVideoSettings.m_SubtitleOn);
+    }
+    else if (setting.id == 6)
+      g_application.m_pPlayer->SetSubTitleDelay(g_stSettings.m_currentVideoSettings.m_SubtitleDelay);
+    else if (setting.id == 7 && setting.max > 0)
+    {
+      g_stSettings.m_currentVideoSettings.m_SubtitleStream = m_subtitleStream;
+      g_application.m_pPlayer->SetSubtitle(m_subtitleStream);
     }
   }
 }
 
 bool CGUIDialogVideoSettings::HasID(DWORD dwID)
 {
-  return (dwID >= WINDOW_DIALOG_VIDEO_OSD_SETTINGS && dwID <= WINDOW_DIALOG_VIDEO_OSD_SETTINGS + 1);
+  return (dwID >= WINDOW_DIALOG_VIDEO_OSD_SETTINGS && dwID <= WINDOW_DIALOG_AUDIO_OSD_SETTINGS);
 }
 
 void CGUIDialogVideoSettings::Render()
 {
-  if (m_iScreen == WINDOW_DIALOG_VIDEO_OSD_SETTINGS + 1)
+  if (m_iScreen == WINDOW_DIALOG_AUDIO_OSD_SETTINGS)
   {
     m_volume = g_stSettings.m_nVolumeLevel * 0.01f;
     UpdateSetting(1);
