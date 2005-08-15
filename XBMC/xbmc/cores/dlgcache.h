281ded1d5cf9ed33b790838e18c6540850cf6ae6
elupus <elupus@svn>
2005-08-15 00:44:28 +0000

    added: progress dialog automatically pops up after 1.5 seconds of init when using mplayer on all medias, no only online.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@4162 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dlgcache.h
+++ XBMC/xbmc/cores/dlgcache.h
@@ -3,18 +3,25 @@
 class CDlgCache : public CThread
 {
 public:
-  CDlgCache();
+  CDlgCache(DWORD dwDelay = 0);
   virtual ~CDlgCache();
   void Update();
   void SetMessage(const CStdString& strMessage);
-  bool IsCanceled() const { return m_pDlg->IsCanceled(); }
-  void ShowProgressBar(bool bOnOff) { m_pDlg->ShowProgressBar(bOnOff); }
-  void SetPercentage(int iPercentage) { m_pDlg->SetPercentage(iPercentage); }
+  bool IsCanceled() const;
+  void ShowProgressBar(bool bOnOff);
+  void SetPercentage(int iPercentage);
+
+  void Close();
 
   virtual void Process();
 
 protected:
+
+  void OpenDialog();
+
+  DWORD m_dwTimeStamp;
+  DWORD m_dwDelay;
   CGUIDialogProgress* m_pDlg;
-  CStdString m_strLinePrev;
+  CStdString m_strLinePrev;  
   bool bSentCancel;
 };
