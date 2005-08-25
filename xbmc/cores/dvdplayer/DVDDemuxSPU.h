2174914a009a4542bdfa3cc817c4582c3c361092
jwnmulder <jwnmulder@svn>
2005-08-25 18:07:16 +0000

    code cleanup
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4247 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDDemuxSPU.h
+++ xbmc/cores/dvdplayer/DVDDemuxSPU.h
@@ -1,6 +1,7 @@
 
 #pragma once
 #include "DVDCodecs\DVDVideoCodec.h"
+#include "DVDOverlay.h"
 struct AVPicture;
 
 typedef struct SPUData
@@ -13,32 +14,18 @@ typedef struct SPUData
 }
 SPUData;
 
-typedef struct SPUInfo
+class CSPUInfo : public CDVDOverlayPicture
 {
+public:
+  CSPUInfo() : CDVDOverlayPicture()
+  {
+    pData = result;
+    bHasColor = false;
+  }
+  
   BYTE result[65536 + 20];
-  BYTE* pData; //[65536 + 20]; // buffer for parsed rle data
-  int pTFData; // pointer to top field picture data (needs rle parsing)
-  int pBFData; // pointer to bottom field picture data (needs rle parsing)
-  int x;
-  int y;
-  int width;
-  int height;
-  int iSPUSize; // size of picture data
-  __int64 iPTSStartTime;
-  __int64 iPTSStopTime;
-  int iStream; //mpeg stream id
-  bool bForced;
-  // the four contrasts  [0] = background
-  int alpha[4];
-
-  // the four yuv colors, containing [][0] = Y, [][1] = Cr, [][2] = Cb
-  // [0][] = background, [1][] = pattern, [2][] = emphasis1, [3][] = emphasis2
-  int color[4][3];
-
   bool bHasColor;
-  struct SPUInfo* next; // pointer to next SPU
-}
-SPUInfo;
+};
 
 // upto 32 streams can exist
 #define DVD_MAX_SPUSTREAMS 32
@@ -49,10 +36,10 @@ public:
   CDVDDemuxSPU();
   virtual ~CDVDDemuxSPU();
 
-  SPUInfo* AddData(BYTE* data, int iSize, int iStream, __int64 pts); // returns a packet from ParsePacket if possible
+  CSPUInfo* AddData(BYTE* data, int iSize, __int64 pts); // returns a packet from ParsePacket if possible
   //void FreePacket(SPUInfo* pSPUInfo);
 
-  SPUInfo* ParseRLE(SPUInfo* pSPU);
+  CSPUInfo* ParseRLE(CSPUInfo* pSPU, BYTE* pUnparsedData);
   // void RenderI420(DVDVideoPicture* pPicture, SPUInfo* pSPU, bool b_crop);
 
   // m_clut set by libdvdnav once in a time
@@ -62,7 +49,7 @@ public:
   bool m_bHasClut;
 
 protected:
-  SPUInfo* ParsePacket(SPUData* pSPUData); // return value should be manualy freed with FreePacket
+  CSPUInfo* ParsePacket(SPUData* pSPUData); // return value should be manualy freed with FreePacket
 
-  SPUData m_spuSreams[DVD_MAX_SPUSTREAMS];
+  SPUData m_spuData;
 };
