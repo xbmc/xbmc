a60e181da32cc958192dab6b22c7bd3707a71c45
pike2k <pike2k@svn>
2005-08-28 23:55:04 +0000

    Patch [1105861] (thx to slacker) quite a few changes to Build.bat
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4269 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- Build.bat
+++ Build.bat
@@ -4,31 +4,41 @@ rem ----PURPOSE----
 rem - Create a working XBMC build with a single click
 rem ---------------------------------------------
 rem Config
-rem Set your path for VSNET main executable
-rem Set the path for xbepatch (this is only necessary if you have a very old bios)
-rem Set the path for WinRAR's rar.exe (freeware)
+rem If you get an error that Visual studio was not found, SET your path for VSNET main executable.
+rem ONLY needed if you have a very old bios, SET the path for xbepatch. Not needed otherwise.
+rem If Winrar isn't installed under standard programs, SET the path for WinRAR's (freeware) rar.exe
 rem and finally set the options for the final rar.
 rem ---------------------------------------------
-rem Remove 'rem' from %NET% to compile and/or clean the solution prior packing it.
-rem Remove 'rem' from 'xcopy web/python' to copy these to the BUILD directory.
+rem Remove 'rem' from 'web / python' below to copy these to the BUILD directory.
 rem ---------------------------------------------
 TITLE XBMC Build Prepare Script
 ECHO Wait while preparing the build.
 ECHO ------------------------------
 rem	CONFIG START
-	set NET="%ProgramFiles%\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.com"
+	IF "%VS71COMNTOOLS%"=="" (
+	  set NET="%ProgramFiles%\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.com"
+	) ELSE (
+	  set NET="%VS71COMNTOOLS%\..\IDE\devenv.com"
+	)
+	IF NOT EXIST %NET% (
+	  set DIETEXT=Visual Studio .NET 2003 was not found.
+	  goto DIE
+	) 
 	set OPTS=xbmc.sln /build release
 	set CLEAN=xbmc.sln /clean release
 	set XBE=xbepatch.exe
 	set RAR="%ProgramFiles%\Winrar\rar.exe"
-	set RAROPS1=a -r -idp -inul -m5 XBMC.rar BUILD
-	set SKINS=..\Skins
+	set RAROPS=a -r -idp -inul -m5 XBMC.rar BUILD
 rem	CONFIG END
 rem ---------------------------------------------
 ECHO Compiling Solution...
 %NET% %CLEAN%
 del release\xbmc.map
 %NET% %OPTS%
+IF NOT EXIST release\default.xbe (
+	set DIETEXT=Default.xbe failed to build!  See .\Release\BuildLog.htm for details.
+	goto DIE
+) 
 ECHO Done!
 ECHO ------------------------------
 ECHO Copying files...
@@ -36,12 +46,12 @@ ECHO Copying files...
 rmdir BUILD /S /Q
 md BUILD
 copy release\default.xbe BUILD
-copy *.xml BUILD
-copy *.txt BUILD
+copy *.xml BUILD\
+copy *.txt BUILD\
 xcopy "skin\Project Mayhem III\fonts" "BUILD\skin\Project Mayhem III\fonts" /E /Q /I /Y
 xcopy "skin\Project Mayhem III\*.xml" "BUILD\skin\Project Mayhem III\" /E /Q /I /Y
-xcopy "skin\Project Mayhem III\media\Textures.xpr" "BUILD\skin\Project Mayhem III\media" /Q /I /Y
-xcopy "skin\Project Mayhem III\sounds\*.*" "BUILD\skin\Project Mayhem III\sounds" /Q /I /Y 
+xcopy "skin\Project Mayhem III\media\Textures.xpr" "BUILD\skin\Project Mayhem III\media\" /Q /I /Y
+xcopy "skin\Project Mayhem III\sounds\*.*" "BUILD\skin\Project Mayhem III\sounds\" /Q /I /Y 
 xcopy credits BUILD\credits /Q /I /Y
 xcopy language BUILD\language /E /Q /I /Y
 xcopy screensavers BUILD\screensavers /E /Q /I /Y
@@ -49,19 +59,43 @@ xcopy visualisations BUILD\visualisations /E /Q /I /Y
 xcopy system BUILD\system /E /Q /I /Y
 rem %rar% x web\Project_Mayhem_webserver*.rar build\web\
 rem xcopy python BUILD\python /E /Q /I /Y
-rem xcopy %SKINS% Build\Skin /E /Q /I /Y
 xcopy media BUILD\media /E /Q /I /Y
 xcopy sounds BUILD\sounds /E /Q /I /Y
 del BUILD\media\dsstdfx.bin
-del BUILD\system\players\mplayer\codecs\.cvsignore
+del BUILD\system\players\mplayer\codecs\.cvsignore 
 
 ECHO ------------------------------
 ECHO Removing CVS directories from build
 FOR /R BUILD %%d IN (CVS) DO @RD /S /Q "%%d" 2>NUL
 
 ECHO ------------------------------
-ECHO Rarring...
-%RAR% %RAROPS1%
+IF NOT EXIST %RAR% (
+	ECHO WinRAR not installed!  Skipping .rar compression...
+) ELSE (
+	ECHO Compressing build to XBMC.rar file...
+	%RAR% %RAROPS%
+)
 
-ECHO ------------------------------
-ECHO finished!
+	ECHO ------------------------------
+ECHO Build Succeeded! 
+GOTO VIEWLOG
+
+:DIE
+	ECHO !-!-!-!-!-!-!-!-!-!-!-!-!-!-!-
+set DIETEXT=ERROR: %DIETEXT%
+echo %DIETEXT%
+
+:VIEWLOG
+REM use a DOS trick to get a y/n user prompt without choice.exe
+REM Credit goes to http://www.robvanderwoude.com/amb_userinput_yn.html
+type nul>"%temp%\~Yes~No~.tmp"
+ECHO View the build log in your HTML browser? [y/n]
+del /p "%temp%\~Yes~No~.tmp">nul
+if exist "%temp%\~Yes~No~.tmp" goto END
+del "%temp%\~Yes~No~.tmp" > NUL: 2>&1
+start %~dps0\Release\BuildLog.htm
+goto END
+
+:END
+ECHO Press any key to exit...
+pause > NUL:
