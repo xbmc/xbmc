3b47b8dff2fae9e5e1535589e4511c92c9852d25
jwnmulder <jwnmulder@svn>
2005-08-29 13:09:45 +0000

    fixed: dvdplayer, libdvdcss data corruption when opening a dvd more then once and a possible crash (heap corruption) in the demuxer
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4272 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxUtils.h
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxUtils.h
@@ -11,6 +11,6 @@ class CDVDDemuxUtils
 {
 public:
   static void FreeDemuxPacket(CDVDDemux::DemuxPacket* pPacket);
-  static CDVDDemux::DemuxPacket* AllocateDemuxPacket();
+  static CDVDDemux::DemuxPacket* AllocateDemuxPacket(int iDataSize = 0);
 };
 
