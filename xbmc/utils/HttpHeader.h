d8eb73fcdde6ac6fc74c89514dadfa5a457f7cd1
jwnmulder <jwnmulder@svn>
2005-09-03 10:28:58 +0000

    committed missing files
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4296 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/utils/HttpHeader.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include "StdString.h"
+
+#define HTTPHEADER_CONTENT_TYPE "Content-Type"
+
+typedef std::map<CStdString, CStdString> HeaderParams;
+typedef std::map<CStdString, CStdString>::iterator HeaderParamsIter;
+
+class CHttpHeader
+{
+public:
+  CHttpHeader();
+  ~CHttpHeader();
+
+  void Parse(CStdString strData);
+  CStdString GetValue(CStdString strParam);
+  
+  CStdString GetContentType() { return GetValue(HTTPHEADER_CONTENT_TYPE); }
+  
+  void Clear();
+  
+protected:
+  HeaderParams m_params;
+};
