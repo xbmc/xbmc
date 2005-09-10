1fc866252b7fe6dc6596f53a4e1070835475d381
elupus <elupus@svn>
2005-09-10 02:35:57 +0000

    added: ability to select player for music and video from context menu
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4328 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/PlayerCoreFactory.h
+++ xbmc/cores/PlayerCoreFactory.h
@@ -1,11 +1,37 @@
 #pragma once
+#include "..\stdafx.h"
 #include "iplayer.h"
+
+const enum EPLAYERCORES
+{
+  EPC_NONE,
+  EPC_DVDPLAYER,
+  EPC_MPLAYER,
+  EPC_PAPLAYER,
+  EPC_SIDPLAYER,
+  EPC_MODPLAYER,
+};
+
+typedef std::vector<EPLAYERCORES> VECPLAYERCORES;
 
 class CPlayerCoreFactory
 {
 public:
   CPlayerCoreFactory();
   virtual ~CPlayerCoreFactory();
+
   IPlayer* CreatePlayer(const CStdString& strCore, IPlayerCallback& callback) const;
 
+  static EPLAYERCORES GetPlayerCore(const CStdString& strCore);
+  static CStdString GetPlayerName(const EPLAYERCORES eCore);
+
+  static IPlayer* CreatePlayer(const EPLAYERCORES eCore, IPlayerCallback& callback);
+  static void GetPlayers( const CFileItem& item, VECPLAYERCORES &vecCores); //Players supporting the specified file
+  static void GetPlayers( VECPLAYERCORES &vecCores );                       //All players
+
+  static EPLAYERCORES GetDefaultPlayer( const CFileItem& item );
+
+  static EPLAYERCORES SelectPlayerDialog(VECPLAYERCORES &vecCores, int iPosX, int iPosY);
+  static EPLAYERCORES SelectPlayerDialog(int iPosX, int iPosY);
+  
 };
