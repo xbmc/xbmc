780a7fd45b19e20d05c050d3fb7565fed4924a33
kraqh3d <kraqh3d@svn>
2005-10-11 22:53:55 +0000

    initial commit
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@4477 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/FileSystem/VirtualPathDirectory.cpp
@@ -0,0 +1,167 @@
+
+#include "../stdafx.h"
+#include "virtualpathdirectory.h"
+#include "directory.h"
+#include "../settings.h"
+#include "../util.h"
+
+using namespace DIRECTORY;
+
+// virtualpath://type/bookmarkname
+
+CVirtualPathDirectory::CVirtualPathDirectory()
+{}
+
+CVirtualPathDirectory::~CVirtualPathDirectory()
+{}
+
+bool CVirtualPathDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items)
+{
+  CLog::Log(LOGDEBUG,"CVirtualPathDirectory::GetDirectory(%s)", strPath.c_str());
+
+  /*
+  CStdString strType, strBookmark;
+  if (!GetTypeAndBookmark(strPath, strType, strBookmark))
+    return false;
+
+  VECSHARES vecShares;
+  if (!GetShares(strType, vecShares))
+    return false;
+
+  bool bIsBookmarkName = false;
+  int iIndex = CUtil::GetMatchingShare(strBookmark, vecShares, bIsBookmarkName);
+  if (!bIsBookmarkName)
+    return false;
+  if (iIndex < 0)
+    return false;
+
+  CShare share = vecShares[iIndex];
+  */
+
+  CShare share;
+  if (!GetMatchingShare(strPath, share))
+    return false;
+
+  int iFailures = 0;
+  for (int i = 0; i < (int)share.vecPaths.size(); ++i)
+  {
+    CFileItemList tempItems;
+    CLog::Log(LOGDEBUG,"Getting Directory (%s)", share.vecPaths[i].c_str());
+    if (CDirectory::GetDirectory(share.vecPaths[i], tempItems, m_strFileMask))
+      items.Append(tempItems);
+    else
+    {
+      CLog::Log(LOGERROR,"Error Getting Directory (%s)", share.vecPaths[i].c_str());
+      iFailures++;
+    }
+  }
+
+  if (iFailures == share.vecPaths.size())
+    return false;
+
+  return true;
+}
+
+bool CVirtualPathDirectory::Exists(const CStdString& strPath)
+{
+  /*
+  CStdString strType, strBookmark;
+  if (!GetTypeAndBookmark(strPath, strType, strBookmark))
+    return false;
+
+  VECSHARES vecShares;
+  if (!GetShares(strType, vecShares))
+    return false;
+
+  bool bIsBookmarkName = false;
+  int iIndex = CUtil::GetMatchingShare(strBookmark, vecShares, bIsBookmarkName);
+  if (!bIsBookmarkName)
+    return false;
+  if (iIndex < 0)
+    return false;
+
+  CShare share = vecShares[iIndex];
+  */
+
+  CShare share;
+  if (!GetMatchingShare(strPath, share))
+    return false;
+
+  int iFailures = 0;
+  for (int i = 0; i < (int)share.vecPaths.size(); ++i)
+  {
+    CLog::Log(LOGDEBUG,"Testing Existance (%s)", share.vecPaths[i].c_str());
+    if (!CDirectory::Exists(share.vecPaths[i]))
+    {
+      CLog::Log(LOGERROR,"Error Testing Existance (%s)", share.vecPaths[i].c_str());
+      iFailures++;
+    }
+  }
+
+  if (iFailures == share.vecPaths.size())
+    return false;
+
+  return true;
+}
+
+bool CVirtualPathDirectory::GetTypeAndBookmark(const CStdString& strPath, CStdString& strType, CStdString& strBookmark)
+{
+  // format: virtualpath://type/bookmarkname
+  CStdString strTemp = strPath;
+  CStdString strTest = "virtualpath://";
+  if (strTemp.Left(strTest.length()).Equals(strTest))
+  {
+    strTemp = strTemp.Mid(strTest.length());
+    int iPos = strTemp.Find('/');
+    if (iPos < 1)
+      return false;
+    strType = strTemp.Mid(0, iPos);
+    strBookmark = strTemp.Mid(iPos + 1);
+    CLog::Log(LOGDEBUG,"CVirtualPathDirectory::GetTypeAndBookmark(%s) = [%s],[%s]", strPath.c_str(), strType.c_str(), strBookmark.c_str());
+    return true;
+  }
+  return false;
+}
+
+bool CVirtualPathDirectory::GetShares(const CStdString& strType, VECSHARES& vecShares)
+{
+  VECSHARES *pShares = NULL;
+
+  // not valid for myprograms or files
+  if (strType == "myprograms")
+    return false;
+  else if (strType == "files")
+    return false;
+  else if (strType == "music")
+    pShares = &g_settings.m_vecMyMusicShares;
+  else if (strType == "video")
+    pShares = &g_settings.m_vecMyVideoShares;
+  else if (strType == "pictures")
+    pShares = &g_settings.m_vecMyPictureShares;
+
+  if (!pShares) return false;
+  
+  vecShares = *pShares;
+  return true;
+}
+
+bool CVirtualPathDirectory::GetMatchingShare(const CStdString &strPath, CShare& share)
+{
+  CStdString strType, strBookmark;
+  if (!GetTypeAndBookmark(strPath, strType, strBookmark))
+    return false;
+
+  VECSHARES vecShares;
+  if (!GetShares(strType, vecShares))
+    return false;
+
+  bool bIsBookmarkName = false;
+  int iIndex = CUtil::GetMatchingShare(strBookmark, vecShares, bIsBookmarkName);
+  if (!bIsBookmarkName)
+    return false;
+  if (iIndex < 0)
+    return false;
+
+  share = vecShares[iIndex];
+  return true;
+}
---
+++ XBMC/xbmc/FileSystem/VirtualPathDirectory.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "IDirectory.h"
+
+using namespace DIRECTORY;
+namespace DIRECTORY
+{
+class CVirtualPathDirectory :
+      public IDirectory
+{
+public:
+  CVirtualPathDirectory(void);
+  virtual ~CVirtualPathDirectory(void);
+  virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
+  virtual bool Exists(const CStdString& strPath);
+
+protected:
+  bool GetTypeAndBookmark(const CStdString& strPath, CStdString& strType, CStdString& strBookmark);
+  bool GetShares(const CStdString& strType, VECSHARES& vecShares);
+  bool GetMatchingShare(const CStdString &strPath, CShare& share);
+};
+}
