b586b73201a4e1b25c19259c20d6e0a96fe07741
mrc_xbmc <mrc_xbmc@svn>
2005-10-16 16:50:41 +0000

    Added support for Direct3d DrawIndexedPrimitve from plugins.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@4499 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/DllLoader/exports/emu_dx8.h
+++ XBMC/xbmc/cores/DllLoader/exports/emu_dx8.h
@@ -8,5 +8,6 @@ extern "C" void d3dSetRenderState(DWORD dwY, DWORD dwZ);
 extern "C" void d3dGetRenderState(DWORD dwY, DWORD* dwZ);
 extern "C" void d3dSetTransform(DWORD dwY, D3DMATRIX* dwZ);
 extern "C" bool d3dCreateTexture(unsigned int width, unsigned int height, LPDIRECT3DTEXTURE8 *pTexture);
+extern "C" void d3dDrawIndexedPrimitive(D3DPRIMITIVETYPE primType, unsigned int minIndex, unsigned int numVertices, unsigned int startIndex, unsigned int primCount);
 
 #endif // _EMU_DX8_H
