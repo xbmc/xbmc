47481119ee40db5b62a7ba8085c62182479f803b
jwnmulder <jwnmulder@svn>
2005-11-20 22:44:59 +0000

    *** empty log message ***
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4593 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/dvdplayer/DVDPerformanceCounter.h
@@ -0,0 +1,56 @@
+
+#pragma once
+#include <xbdm.h>
+#include "DVDPlayer.h"
+
+#define FILETIME_TO_ULARGE_INTEGER(ularge, filetime) { ularge.HighPart = filetime.dwHighDateTime; ularge.LowPart = filetime.dwLowDateTime; }
+
+class CDVDPacketQueue;
+
+typedef struct stProcessPerformance
+{
+  ULARGE_INTEGER  timer_thread;
+  ULARGE_INTEGER  timer_system;
+  HANDLE          hThread;
+  DWORD           iPercent; // in percent
+} ProcessPerformance;
+
+class CDVDPerformanceCounter
+{
+public:
+  CDVDPerformanceCounter();
+  ~CDVDPerformanceCounter();
+  
+  bool Initialize();
+  void DeInitialize();
+  
+  void Lock()                                       { EnterCriticalSection(&m_critSection); }
+  void Unlock()                                     { LeaveCriticalSection(&m_critSection); }
+  
+  void EnableAudioQueue(CDVDPacketQueue* pQueue)    { Lock(); m_pAudioQueue = pQueue; Unlock(); }
+  void DisableAudioQueue()                          { Lock(); m_pAudioQueue = NULL; Unlock(); }
+
+  void EnableVideoQueue(CDVDPacketQueue* pQueue)    { Lock(); m_pVideoQueue = pQueue; Unlock(); }
+  void DisableVideoQueue()                          { Lock(); m_pVideoQueue = NULL; Unlock(); }
+  
+  void EnableVideoDecodePerformance(HANDLE hThread) { Lock(); m_videoDecodePerformance.hThread = hThread; Unlock(); }
+  void DisableVideoDecodePerformance()              { Lock(); m_videoDecodePerformance.hThread = NULL; Unlock(); }
+
+  void EnableAudioDecodePerformance(HANDLE hThread) { Lock(); m_audioDecodePerformance.hThread = hThread; Unlock(); }
+  void DisableAudioDecodePerformance()              { Lock(); m_audioDecodePerformance.hThread = NULL; Unlock(); }
+
+  void EnableMainPerformance(HANDLE hThread)        { Lock(); m_mainPerformance.hThread = hThread; Unlock(); }
+  void DisableMainPerformance()                     { Lock(); m_mainPerformance.hThread = NULL; Unlock(); }
+
+  CDVDPacketQueue*          m_pAudioQueue;
+  CDVDPacketQueue*          m_pVideoQueue;
+  
+  ProcessPerformance        m_videoDecodePerformance;
+  ProcessPerformance        m_audioDecodePerformance;
+  ProcessPerformance        m_mainPerformance;
+  
+private:
+  CRITICAL_SECTION m_critSection;
+};
+
+extern CDVDPerformanceCounter g_dvdPerformanceCounter;
