4b73541e0ce029c1727558308bdcc1647d706d90
jmarshallnz <jmarshallnz@svn>
2005-11-23 23:19:59 +0000

    - 23-11-2005 added: Skin.HasTheme(themename) visibility condition.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4606 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -202,7 +202,7 @@ void CUtil::RemoveExtension(CStdString& strFileName)
     strFileMask = g_stSettings.m_szMyMusicExtensions;
     strFileMask += g_stSettings.m_szMyPicturesExtensions;
     strFileMask += g_stSettings.m_szMyVideoExtensions;
-    strFileMask += ".py|.xml|.milk";
+    strFileMask += ".py|.xml|.milk|.xpr";
 
     // Only remove if its a valid media extension
     if (strFileMask.Find(strExtension.c_str()) >= 0)
