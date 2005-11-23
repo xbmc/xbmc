8721595df1d7a223e52f1181b99df5f638e7669b
jmarshallnz <jmarshallnz@svn>
2005-11-23 02:12:41 +0000

    - 23-11-2005 fixed:   Removed invalid .jpg files from CVS.  - 23-11-2005 changed: Default album cover in skin is now "defaultAlbumCover.png"  - 23-11-2005 changed: Added flag to directory classes to specify whether they should prompt the user                        for authentication. Background scanners (picture thumb loader, RAM control) turn                        this flag off.  - 23-11-2005 changed: Redid the skin theme code.  See the online manual for information on skin themes.  - 23-11-2005 fixed: Buttonscroller didn't obey visibility conditions.  - 23-11-2005 added: <randomize> and <keepaspectratio> tags to the multiimage control.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4600 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/IDirectory.h
+++ xbmc/FileSystem/IDirectory.h
@@ -47,7 +47,9 @@ public:
 
   bool IsAllowed(const CStdString& strFile);
   void SetMask(const CStdString& strMask);
+  void SetAllowPrompting(bool allowPrompting);
 protected:
   CStdString m_strFileMask;  ///< Holds the file mask specified by SetMask()
+  bool m_allowPrompting;    ///< If true, the directory handlers may prompt the user
 };
 }
