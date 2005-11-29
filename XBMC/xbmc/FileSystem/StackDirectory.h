fcf6ea7a73f733886fae3f4415f0cfdba25b8fad
kraqh3d <kraqh3d@svn>
2005-11-29 12:01:25 +0000

    Added GetFirstStackedFile() so that we can restore nfo file matching on the first item in the stack like in the past
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@4627 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/StackDirectory.h
+++ XBMC/xbmc/FileSystem/StackDirectory.h
@@ -13,6 +13,7 @@ namespace DIRECTORY
     ~CStackDirectory();
     virtual bool GetDirectory(const CStdString& strPath, CFileItemList& items);
     CStdString GetStackedTitlePath(const CStdString &strPath);
+    CStdString GetFirstStackedFile(const CStdString &strPath);
     CStdString ConstructStackPath(const CFileItemList& items, const vector<int> &stack);
   };
 }
