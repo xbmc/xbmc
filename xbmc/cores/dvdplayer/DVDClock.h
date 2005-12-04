c1a7431895a4bd7b6ab93bb46426e63a35b35431
elupus <elupus@svn>
2005-12-04 22:36:00 +0000

    added: dvdplayer initial ff/rw support in dvdplayer. only available in debug builds. changed: cleanup of dvdplayer audio/video rendering with added support for dropping frames in decoder.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4658 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDClock.h
+++ xbmc/cores/dvdplayer/DVDClock.h
@@ -1,8 +1,13 @@
-
 #pragma once
 
+#include "../../utils/sharedsection.h"
 #define DVD_TIME_BASE 1000000
 #define DVD_NOPTS_VALUE (0x8000000000000000 ## i64)
+
+#define DVD_SEC_TO_TIME(x) ((x) * DVD_TIME_BASE)
+#define DVD_MSEC_TO_TIME(x) ((x) * (DVD_TIME_BASE / 1000))
+
+
 
 enum ClockDiscontinuityType
 {
@@ -17,6 +22,9 @@ public:
   ~CDVDClock();
 
   __int64 GetClock();
+  __int64 GetAbsoluteClock();
+  __int64 GetFrequency() { return (__int64)m_systemFrequency.QuadPart ; }
+
   void Discontinuity(ClockDiscontinuityType type, __int64 currentPts = 0LL);
   
   bool HadDiscontinuity(__int64 delay);
@@ -24,9 +32,9 @@ public:
   
   void Pause();
   void Resume();
-  
+  void SetSpeed(int iSpeed);
 protected:
-
+  CSharedSection m_critSection;
   LARGE_INTEGER m_systemUsed;
   LARGE_INTEGER m_systemFrequency;
   LARGE_INTEGER m_startClock;
