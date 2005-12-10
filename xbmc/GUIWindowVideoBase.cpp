85b02748cbc050547e1456ceb987215a1ce4fe36
kraqh3d <kraqh3d@svn>
2005-12-10 00:54:54 +0000

    - 10-12-2005 fixed: IMDB "Movie Lookup" on folders
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4678 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoBase.cpp
+++ xbmc/GUIWindowVideoBase.cpp
@@ -431,12 +431,19 @@ void CGUIWindowVideoBase::OnInfo(int iItem)
 
 void CGUIWindowVideoBase::ShowIMDB(const CStdString& strMovie, const CStdString& strFile, const CStdString& strFolder, bool bFolder)
 {
+
+  CLog::Log(LOGDEBUG,"CGUIWindowVideoBase::ShowIMDB");
+  CLog::Log(LOGDEBUG,"  strMovie  = [%s]", strMovie.c_str());
+  CLog::Log(LOGDEBUG,"  strFile   = [%s]", strFile.c_str());
+  CLog::Log(LOGDEBUG,"  strFolder = [%s]", strFolder.c_str());
+  CLog::Log(LOGDEBUG,"  bFolder   = [%s]", ((int)bFolder ? "true" : "false"));
+
   CGUIDialogProgress* pDlgProgress = (CGUIDialogProgress*)m_gWindowManager.GetWindow(WINDOW_DIALOG_PROGRESS);
   CGUIDialogSelect* pDlgSelect = (CGUIDialogSelect*)m_gWindowManager.GetWindow(WINDOW_DIALOG_SELECT);
   CGUIWindowVideoInfo* pDlgInfo = (CGUIWindowVideoInfo*)m_gWindowManager.GetWindow(WINDOW_VIDEO_INFO);
 
   CIMDB IMDB;
-  bool bUpdate(false);
+  bool bUpdate = false;
   bool bFound = false;
 
   if (!pDlgProgress) return ;
@@ -445,7 +452,7 @@ void CGUIWindowVideoBase::ShowIMDB(const CStdString& strMovie, const CStdString&
   CUtil::ClearCache();
   CStdString strMovieName = strMovie;
 
-  if (m_database.HasMovieInfo(strFile) )
+  if (m_database.HasMovieInfo(strFile))
   {
     CIMDBMovie movieDetails;
     m_database.GetMovieInfo(strFile, movieDetails);
