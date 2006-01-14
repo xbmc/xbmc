e8c7c413120e3d50488e62da256d53e5d405cd2e
kraqh3d <kraqh3d@svn>
2006-01-14 17:21:53 +0000

    - 14-01-2006 fixed: current movie info from <info> tags for files that are part of a stacked movie
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4840 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoDatabase.h
+++ xbmc/VideoDatabase.h
@@ -53,6 +53,7 @@ public:
   void GetMovies(VECMOVIES& movies);
   void GetFilePath(long lMovieId, CStdString &filePath);
   void GetMovieInfo(const CStdString& strFilenameAndPath, CIMDBMovie& details, long lMovieId = -1);
+  long GetMovieInfo(const CStdString& strFilenameAndPath);
   void SetMovieInfo(const CStdString& strFilenameAndPath, CIMDBMovie& details);
   void GetMoviesByPath(CStdString& strPath1, VECMOVIES& movies);
   void GetBookMarksForMovie(const CStdString& strFilenameAndPath, VECBOOKMARKS& bookmarks);
