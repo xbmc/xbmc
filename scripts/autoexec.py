d0430ae66b86e0b553a3a28d96c5496924d5968e
jwnmulder <jwnmulder@svn>
2006-01-21 16:53:32 +0000

    disabled autoexecute of medusa script since it is not usefull at all
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@4885 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- scripts/autoexec.py
+++ scripts/autoexec.py
@@ -2,7 +2,7 @@
 # 
 # note: - do not execute more than one script at a time which asks for user input!
 
-import xbmc
+#import xbmc
 
-xbmc.executescript('q:\\scripts\\medusa\\start_medusa.py')
+#xbmc.executescript('q:\\scripts\\medusa\\start_medusa.py')
 
