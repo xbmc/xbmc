57a3aadf2ed110241d491eb52d038762da41ec9e
jmarshallnz <jmarshallnz@svn>
2006-02-24 09:50:47 +0000

    - 24-02-2006 added: New Musepack (MPC) decoder and sources.  - 24-02-2006 fixed: Ring buffer could seek backwards to the incorrect location, causing incorrect data to be read - caused  seeking irregularities in paplayer.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5092 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/DllMpcCodec.h
+++ xbmc/cores/paplayer/DllMpcCodec.h
@@ -1,36 +1,31 @@
 #pragma once
 #include "../../DynamicDll.h"
 
-#include "mpc/MPCReader.h"
-#include "mpc/in_mpc.h"
+#include "mpc/config_win32.h"
+#include "mpc/reader.h"
+#include "mpc/streaminfo.h"
+
+struct mpc_decoder;
 
 // stuff from dll we need
 #define FRAMELEN 1152
 
-//  forward
-class CFileReader;
-
-struct MpcPlayFileStream {
-	MpcPlayStream vtbl;
-	CFileReader *file;
-};
-
 class DllMPCCodecInterface
 {
 public:
-    virtual bool Open(MpcPlayState **state, MpcPlayStream *stream, StreamInfo::BasicData *data, double *timeinseconds)=0;
-    virtual void Close(MpcPlayState *state)=0;
-    virtual int Read(MpcPlayState *state, float *buffer, int size)=0;
-    virtual int Seek(MpcPlayState *state, double timeinseconds)=0;
+    virtual bool Open(mpc_decoder **decoder, mpc_reader *reader, mpc_streaminfo *info, double *timeinseconds)=0;
+    virtual void Close(mpc_decoder *decoder)=0;
+    virtual int Read(mpc_decoder *decoder, float *buffer, int size)=0;
+    virtual int Seek(mpc_decoder *decoder, double timeinseconds)=0;
 };
 
 class DllMPCCodec : public DllDynamic, DllMPCCodecInterface
 {
-  DECLARE_DLL_WRAPPER(DllMPCCodec, Q:\\system\\players\\PAPlayer\\MPCcodec.dll)
-  DEFINE_METHOD4(bool, Open, (MpcPlayState **p1, MpcPlayStream *p2, StreamInfo::BasicData *p3, double *p4))
-  DEFINE_METHOD1(void, Close, (MpcPlayState *p1))
-  DEFINE_METHOD3(int, Read, (MpcPlayState *p1, float *p2, int p3))
-  DEFINE_METHOD2(int, Seek, (MpcPlayState *p1, double p2))
+  DECLARE_DLL_WRAPPER(DllMPCCodec, Q:\\system\\players\\PAPlayer\\libmpcdec.dll)
+  DEFINE_METHOD4(bool, Open, (mpc_decoder **p1, mpc_reader *p2, mpc_streaminfo *p3, double *p4))
+  DEFINE_METHOD1(void, Close, (mpc_decoder *p1))
+  DEFINE_METHOD3(int, Read, (mpc_decoder *p1, float *p2, int p3))
+  DEFINE_METHOD2(int, Seek, (mpc_decoder *p1, double p2))
   BEGIN_METHOD_RESOLVE()
     RESOLVE_METHOD(Open)
     RESOLVE_METHOD(Close)
