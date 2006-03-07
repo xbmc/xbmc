e611854b957cb4834ca0f76b3669688456cba4ee
kraqh3d <kraqh3d@svn>
2006-03-07 00:32:08 +0000

    - 07-03-2006 changed: Music Party Mode now uses a database table for its history, means another upgrade to version 1.6  - 07-03-2006 changed: when Music Party Mode is enabled, all the extraneous playlist buttons in the Now Playing window are disabled
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5210 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/PartyModeManager.h
+++ xbmc/PartyModeManager.h
@@ -16,6 +16,7 @@ public:
   void OnSongChange();
   void AddUserSongs(CPlayList& playlistTemp, bool bPlay = false);
   bool IsEnabled() { return m_bEnabled; };
+  int GetUniqueRandomSongsLeft();
 
 private:
   void Process();
@@ -24,10 +25,11 @@ private:
   bool ReapSongs();
   bool MovePlaying();
   void SendUpdateMessage();
+  void OnError(int iError, CStdString& strLogMessage);
 
   bool m_bEnabled;
-  int iLastUserSong;
-  vector<long> m_vecHistory;
+  int m_iLastUserSong;
+  CStdString m_strCurrentFilter;
 };
 
 extern CPartyModeManager g_partyModeManager;
