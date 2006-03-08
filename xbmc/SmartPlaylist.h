932e8249dc9d29f00ae36f8e9b9c49008760930f
jmarshallnz <jmarshallnz@svn>
2006-03-08 02:39:40 +0000

    - 08-03-2006 added: Filtering of Party mode via a PartyMode.xml in system/ folder.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5227 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/SmartPlaylist.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "StdString.h"
+#include "tinyxml/tinyxml.h"
+#include <vector>
+
+class CSmartPlaylistRule
+{
+public:
+  CSmartPlaylistRule();
+
+  enum SEARCH_FIELD { SONG_GENRE = 1,
+                            SONG_ALBUM,
+                            SONG_ARTIST,
+                            SONG_TITLE,
+                            SONG_YEAR };
+
+  enum SEARCH_OPERATOR { OPERATOR_CONTAINS = 1,
+                         OPERATOR_DOES_NOT_CONTAIN,
+                         OPERATOR_EQUALS,
+                         OPERATOR_DOES_NOT_EQUAL,
+                         OPERATOR_STARTS_WITH,
+                         OPERATOR_ENDS_WITH,
+                         OPERATOR_GREATER_THAN,
+                         OPERATOR_LESS_THAN };
+
+  CStdString GetWhereClause();
+  void TranslateStrings(const char *field, const char *oper, const char *parameter);
+
+  TiXmlElement GetAsElement();
+
+  SEARCH_FIELD       m_field;
+  SEARCH_OPERATOR    m_operator;
+  CStdString         m_parameter;
+private:
+  SEARCH_FIELD       TranslateField(const char *field);
+  SEARCH_OPERATOR    TranslateOperator(const char *oper);
+  CStdString         TranslateField(SEARCH_FIELD field);
+  CStdString         TranslateOperator(SEARCH_OPERATOR oper);
+};
+
+class CSmartPlaylist
+{
+public:
+  CSmartPlaylist();
+
+  bool Load(const CStdString &path);
+  bool Save(const CStdString &path);
+
+  void SetName(const CStdString &name);
+  void AddRule(const CSmartPlaylistRule &rule);
+  CStdString GetWhereClause();
+
+private:
+  vector<CSmartPlaylistRule> m_playlistRules;
+  CStdString m_playlistName;
+  bool m_matchAllRules;
+};
