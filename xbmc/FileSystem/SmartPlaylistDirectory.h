f221f1812958ea7913260d36a03a7bdaa07dcde9
jmarshallnz <jmarshallnz@svn>
2006-03-08 20:38:48 +0000

    - 08-03-2006 added: Smart Playlists in My Music.  Only loadable from a file (no GUI setup of smart playlists)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5234 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/SmartPlaylistDirectory.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ifiledirectory.h"
+
+using namespace DIRECTORY;
+
+namespace DIRECTORY 
+{
+  class CSmartPlaylistDirectory : public IFileDirectory
+  {
+  public:
+    CSmartPlaylistDirectory();
+    ~CSmartPlaylistDirectory();
+    virtual bool GetDirectory(const CStdString& strPath, CFileItemList& items);
+    virtual bool ContainsFiles(const CStdString& strPath);
+  };
+}
