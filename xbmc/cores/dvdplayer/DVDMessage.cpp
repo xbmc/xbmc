077e5696066583f13e75dc1b43897003dd13e38d
elupus <elupus@svn>
2006-03-11 02:18:25 +0000

    changed: decoupled video playback from master clock in dvdplayer to allow for nicer transition when timestamps in dvd wrap back changed: the way video stream is opened during dvdplayerback to match how audio and subtitle streams are opened
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5256 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDMessage.cpp
+++ xbmc/cores/dvdplayer/DVDMessage.cpp
@@ -21,22 +21,32 @@ CDVDMsgGeneralStreamChange::~CDVDMsgGeneralStreamChange()
 }
 
 /**
- * CDVDMsgGeneralStreamChange --- GENERAL_STREAMCHANGE
+ * CDVDMsgGeneralSynchronize --- GENERAL_SYNCRONIZR
  */
-CDVDMsgGeneralSynchronize::CDVDMsgGeneralSynchronize(DWORD timeout) : CDVDMsg(GENERAL_SYNCHRONIZE)
+CDVDMsgGeneralSynchronize::CDVDMsgGeneralSynchronize(DWORD timeout, DWORD sources) : CDVDMsg(GENERAL_SYNCHRONIZE)
 {
+  if( sources )
+    m_sources = sources;
+  else
+    m_sources = SYNCSOURCE_ALL;
+
   m_objects = 0;
-  m_timeout = GetTickCount() + timeout;
+  m_timeout = timeout;
 }
 
-void CDVDMsgGeneralSynchronize::Wait(volatile bool *abort)
+void CDVDMsgGeneralSynchronize::Wait(volatile bool *abort, DWORD source)
 {
+  /* if we are not requested to wait on this object just return, reference count will be decremented */
+  if (source && !(m_sources & source)) return;
+
   InterlockedIncrement(&m_objects);
 
-  if( abort )
-    while( !(*abort) && m_timeout > GetTickCount() && m_objects < GetNrOfReferences()) Sleep(1);
+  DWORD timeout = GetTickCount() + m_timeout;    
+
+  if (abort)
+    while( m_objects < GetNrOfReferences() && timeout > GetTickCount() && !(*abort)) Sleep(1);
   else
-    while( m_timeout > GetTickCount() && m_objects < GetNrOfReferences() ) Sleep(1);
+    while( m_objects < GetNrOfReferences() && timeout > GetTickCount() ) Sleep(1);
 }
 
 /**
