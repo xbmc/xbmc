0b67c3b9c77c5c43021668a75d9f80ca99e9bbb7
jmarshallnz <jmarshallnz@svn>
2006-03-22 02:03:29 +0000

    - 22-03-2006 fixed: Smart playlists didn't support ordering + limiting.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5338 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/SmartPlaylist.cpp
+++ xbmc/SmartPlaylist.cpp
@@ -116,31 +116,35 @@ CStdString CSmartPlaylistRule::GetWhereClause()
   parameter.Format(operatorString.c_str(), m_parameter.c_str());
   // now the query parameter
   CStdString query;
-  if (m_field == SONG_TITLE)
-    query = "strTitle" + parameter;
-  else if (m_field == SONG_GENRE)
+  if (m_field == SONG_GENRE)
     query = "(strGenre" + parameter + ") or (idsong IN (select idsong from genre,exgenresong where exgenresong.idgenre = genre.idgenre and genre.strGenre" + parameter + "))";
-  else if (m_field == SONG_ALBUM)
-    query = "strAlbum" + parameter;
-  else if (m_field == SONG_YEAR)
-    query = "iYear" + parameter;
   else if (m_field == SONG_ARTIST)
     query = "(strArtist" + parameter + ") or (idsong IN (select idsong from artist,exartistsong where exartistsong.idartist = artist.idartist and artist.strArtist" + parameter + "))";
-  else if (m_field == SONG_TIME)
-    query = "iDuration" + parameter;
-  else if (m_field == SONG_FILENAME)
-    query = "strFilename" + parameter;
-  else if (m_field == SONG_PLAYCOUNT)
-    query = "iTimesPlayed" + parameter;
-  else if (m_field == SONG_TRACKNUMBER)
-    query = "iTrack" + parameter;
-
+  else if (m_field != FIELD_NONE)
+    query = GetDatabaseField(m_field) + parameter;
   return query;
+}
+
+CStdString CSmartPlaylistRule::GetDatabaseField(SEARCH_FIELD field)
+{
+  if (field == SONG_TITLE) return "strTitle";
+  else if (field == SONG_GENRE) return "strGenre";
+  else if (field == SONG_ALBUM) return "strAlbum";
+  else if (field == SONG_YEAR) return "iYear";
+  else if (field == SONG_ARTIST) return "strArtist";
+  else if (field == SONG_TIME) return "iDuration";
+  else if (field == SONG_PLAYCOUNT) return "iTimesPlayed";
+  else if (field == SONG_FILENAME) return "strFilename";
+  else if (field == SONG_TRACKNUMBER) return "iTrack";
+  return "";
 }
 
 CSmartPlaylist::CSmartPlaylist()
 {
   m_matchAllRules = true;
+  m_limit = 0;
+  m_orderField = CSmartPlaylistRule::FIELD_NONE;
+  m_orderAscending = true;
 }
 
 bool CSmartPlaylist::Load(const CStdString &path)
@@ -182,6 +186,21 @@ bool CSmartPlaylist::Load(const CStdString &path)
     }
     rule = rule->NextSiblingElement("rule");
   }
+  // now any limits
+  // format is <limit>25</limit>
+  TiXmlHandle limit = ((TiXmlHandle)root->FirstChild("limit")).FirstChild();
+  if (limit.Node())
+    m_limit = atoi(limit.Node()->Value());
+  // and order
+  // format is <order direction="ascending">field</order>
+  TiXmlElement *order = root->FirstChildElement("order");
+  if (order && order->FirstChild())
+  {
+    const char *direction = order->Attribute("direction");
+    if (direction)
+      m_orderAscending = strcmpi(direction, "ascending") == 0;
+    m_orderField = CSmartPlaylistRule::TranslateField(order->FirstChild()->Value());
+  }
   return true;
 }
 
@@ -206,6 +225,25 @@ bool CSmartPlaylist::Save(const CStdString &path)
   for (vector<CSmartPlaylistRule>::iterator it = m_playlistRules.begin(); it != m_playlistRules.end(); ++it)
   {
     pRoot->InsertEndChild((*it).GetAsElement());
+  }
+  // add <limit> tag
+  if (m_limit)
+  {
+    CStdString limitFormat;
+    limitFormat.Format("%i", m_limit);
+    TiXmlText limit(limitFormat);
+    TiXmlElement nodeLimit("limit");
+    nodeLimit.InsertEndChild(limit);
+    pRoot->InsertEndChild(nodeLimit);
+  }
+  // add <order> tag
+  if (m_orderField != CSmartPlaylistRule::FIELD_NONE)
+  {
+    TiXmlText order(CSmartPlaylistRule::TranslateField(m_orderField).c_str());
+    TiXmlElement nodeOrder("order");
+    nodeOrder.SetAttribute("direction", m_orderAscending ? "ascending" : "descending");
+    nodeOrder.InsertEndChild(order);
+    pRoot->InsertEndChild(nodeOrder);
   }
   return doc.SaveFile(path);
 }
@@ -234,4 +272,18 @@ CStdString CSmartPlaylist::GetWhereClause()
     rule += ")";
   }
   return rule;
+}
+
+CStdString CSmartPlaylist::GetOrderClause()
+{
+  CStdString order;
+  if (m_orderField != CSmartPlaylistRule::FIELD_NONE)
+    order.Format("ORDER BY %s%s", CSmartPlaylistRule::GetDatabaseField(m_orderField), m_orderAscending ? "" : " DESC");
+  if (m_limit)
+  {
+    CStdString limit;
+    limit.Format(" LIMIT %i", m_limit);
+    order += limit;
+  }
+  return order;
 }
