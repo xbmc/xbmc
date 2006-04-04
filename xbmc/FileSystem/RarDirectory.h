e61c95d8bc5b2b195883d184cfc88d8a565c549c
spiff_ <spiff_@svn>
2006-04-04 23:16:03 +0000

    fixed: single filed rar archives should be up to speed again. fixed: [ 1461737 ] Navigation in CBR/CBZ. fixed: [ 1460946 ] Unable to create CD rip dir on SMB share.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5434 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/RarDirectory.h
+++ xbmc/FileSystem/RarDirectory.h
@@ -14,5 +14,6 @@ namespace DIRECTORY
     ~CRarDirectory();
     virtual bool GetDirectory(const CStdString& strPath, CFileItemList& items);
     virtual bool ContainsFiles(const CStdString& strPath);
+    virtual bool Exists(const char* strPath);
   };
 }
