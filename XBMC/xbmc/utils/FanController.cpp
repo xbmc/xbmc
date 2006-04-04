ba16db37f086d78d0d0bed7363aef8ea5547eba6
dday78 <dday78@svn>
2006-04-04 22:16:39 +0000

    XBMC should now read and display the right GPU and CPU temps for 1.6 xbox�s
    Thanks goes to pablot for the fix
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@5431 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/utils/FanController.cpp
+++ XBMC/xbmc/utils/FanController.cpp
@@ -42,6 +42,8 @@ CFanController::CFanController()
   calculatedFanSpeed = systemFanSpeed;
   unsigned long iDummy;
   bIs16Box = (HalReadSMBusValue(XCALIBUR_ADDRESS, 0, 0, (LPBYTE)&iDummy) == 0);
+  cpuTempCount = 0;
+  cpuTemp = 0;
 }
 
 
@@ -190,31 +192,66 @@ float CFanController::GetCPUTemp()
 
 void CFanController::GetCPUTempInternal()
 {
-  //HalReadSMBusValue(PIC_ADDRESS, CPU_TEMP, 0, (LPBYTE)&cpuTemp);
-
   unsigned short cpu, cpudec;
+  float temp1;
 
-  _outp(0xc004, (0x4c << 1) | 0x01);
-  _outp(0xc008, 0x01);
-  _outpw(0xc000, _inpw(0xc000));
-  _outp(0xc002, (0) ? 0x0b : 0x0a);
-  while ((_inp(0xc000) & 8));
-  cpu = _inpw(0xc006);
+  
+  if (!bIs16Box)
+  { //if it is an old xbox, then do as we have always done
+    _outp(0xc004, (0x4c << 1) | 0x01);
+    _outp(0xc008, 0x01);
+    _outpw(0xc000, _inpw(0xc000));
+    _outp(0xc002, (0) ? 0x0b : 0x0a);
+    while ((_inp(0xc000) & 8));
+    cpu = _inpw(0xc006);
 
-  _outp(0xc004, (0x4c << 1) | 0x01);
-  _outp(0xc008, 0x10);
-  _outpw(0xc000, _inpw(0xc000));
-  _outp(0xc002, (0) ? 0x0b : 0x0a);
-  while ((_inp(0xc000) & 8));
-  cpudec = _inpw(0xc006);
+    _outp(0xc004, (0x4c << 1) | 0x01);
+    _outp(0xc008, 0x10);
+    _outpw(0xc000, _inpw(0xc000));
+    _outp(0xc002, (0) ? 0x0b : 0x0a);
+    while ((_inp(0xc000) & 8));
+    cpudec = _inpw(0xc006);
 
-  cpuTemp = (float)cpu + (float)cpudec / 256.0f;
-  // The XBOX v1.6 shows the temp to high! Let's recalc it! it will only do ~minus 10 degress
-  if (bIs16Box)
-  {
-    cpuTemp *= 0.8f;
+    cpuTemp = (float)cpu + (float)cpudec / 256.0f;
   }
+  else
+  { // if its a 1.6 then we get the CPU temperature from the xcalibur
+    _outp(0xc004, (0x70 << 1) | 0x01);  // address
+    _outp(0xc008, 0xC1);                // command
+    _outpw(0xc000, _inpw(0xc000));      // clear errors
+    _outp(0xc002, 0x0d);                // start block transfer
+    while ((_inp(0xc000) & 8));         // wait for response
+   
+    if (!(_inp(0xc000) & 0x23)) // if there was a error then just skip this read..
+    {
+      _inp(0xc004);                       // read out the data reg (no. bytes in block, will be 4)
+      cpudec = _inp(0xc009);              // first byte
+      cpu    = _inp(0xc009);              // second byte
+      _inp(0xc009);                       // read out the two last bytes, dont' think its neccesary
+      _inp(0xc009);                       // but done to be on the safe side
 
+      /* the temperature recieved from the xcalibur is very jumpy, so we try and smooth it
+          out by taking the average over 10 samples */
+      temp1 = (float)cpu + (float)cpudec / 256;
+      temp1 /= 10;
+      cpuFrac += temp1;
+
+      if (cpuTempCount == 9) // if we have taken 10 samples then commit the new temperature
+      {
+        cpuTemp = cpuFrac;
+        cpuTempCount = 0;
+        cpuFrac = 0;
+      }
+      else
+      {
+        cpuTempCount++;     // increse sample count
+      }
+    }
+
+    /* the first time we read the temp sensor we set the temperature right away */
+    if (cpuTemp == 0)
+      cpuTemp = (float)cpu + (float)cpudec / 256;
+  }
 }
 
 
