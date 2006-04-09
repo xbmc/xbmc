d02b08915901b757e9ae3ee77f32fd0ec7b13692
elupus <elupus@svn>
2006-04-09 19:03:40 +0000

    changed: Added free memory logging to logfile and removed current date (time is still there)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5469 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/log.cpp
+++ xbmc/utils/log.cpp
@@ -8,19 +8,15 @@
 #include "../Settings.h"
 #include "../util.h"
 
-static CLog g_logger;
+FILE* CLog::fd = NULL;
+
+static CCriticalSection critSec;
 
 CLog::CLog()
 {}
 
 CLog::~CLog()
 {}
-
-// put here so debug and normal logger can share it
-static char tmp[16384];
-
-FILE* CLog::fd = NULL;
-CCriticalSection critSec;
 
 void CLog::Close()
 {
@@ -38,37 +34,24 @@ void CLog::Log(int loglevel, const char *format, ... )
   if (loglevel >= g_stSettings.m_iLogLevel)
   {
     CSingleLock waitLock(critSec);
-    CStdString str = levelNames[loglevel];
-    str += " ";
-    va_list va;
-
     if (!fd)
     {
-      char LogFile[1024];
+      CStdString LogFile;
       if (g_settings.QuickXMLLoad("logpath"))
       {
-        CStdString strLogPath = g_stSettings.m_szlogpath;
-        if (!strLogPath.IsEmpty())
+        CStdString strLogPath = g_stSettings.m_szlogpath;        
+        if (!strLogPath.IsEmpty() && CUtil::IsHD(strLogPath))
         {
-          //ensure there's a '\' on the end
-          if (!CUtil::HasSlashAtEnd(strLogPath))
+          if( !CUtil::HasSlashAtEnd(strLogPath) )
             strLogPath += "\\";
             
-          // proper parameter checking, make sure its an HD path
-          if (CUtil::IsHD(strLogPath))
-          {
-            sprintf(LogFile, "%sxbmc.log", strLogPath.c_str());
-            if (strLogPath.c_str() != "C:\\" || strLogPath.c_str()!= "E:\\" || strLogPath.c_str()!= "F:\\" || strLogPath.c_str()!= "G:\\" || strLogPath.c_str()!= "X:\\" || strLogPath.c_str()!= "Y:\\" || strLogPath.c_str()!= "Z:\\")
-            { 
-              // we need to create a folder, if the log file comes into a folder!
-              CreateDirectory(strLogPath.c_str(), NULL);
-            }
-          }
-          else sprintf(LogFile, "Q:\\xbmc.log");
+          if( CreateDirectory(strLogPath.c_str(), NULL) )
+            LogFile.Format("%sxbmc.log", strLogPath.c_str());
         }
-        else sprintf(LogFile, "Q:\\xbmc.log");
       }
-      else sprintf(LogFile, "Q:\\xbmc.log");
+
+      if( LogFile.length() == 0 )
+        LogFile = "Q:\\xbmc.log";
 
       fd = _fsopen(LogFile, "a+", _SH_DENYWR);
     }
@@ -76,31 +59,42 @@ void CLog::Log(int loglevel, const char *format, ... )
     if (!fd)
       return ;
 
+
     SYSTEMTIME time;
     GetLocalTime(&time);
-    CStdString strTime;
-    strTime.Format("%02.2d-%02.2d-%04.4d %02.2d:%02.2d:%02.2d ", time.wDay, time.wMonth, time.wYear, time.wHour, time.wMinute, time.wSecond);
 
+    MEMORYSTATUS stat;
+    GlobalMemoryStatus(&stat);
+
+    CStdString strPrefix, strData;
+    
+    strPrefix.Format("%02.2d:%02.2d:%02.2d M:%9u %7s: ", time.wHour, time.wMinute, time.wSecond, stat.dwAvailPhys, levelNames[loglevel]);
+
+    strData.reserve(16384);
+    va_list va;
     va_start(va, format);
-    _vsnprintf(tmp, 16384, format, va);
+    strData.FormatV(format,va);    
     va_end(va);
+    
 
-    while (1)
+    int length = 0;
+    while ( length != strData.length() )
     {
-      int ilen = strlen(tmp);
-      if (ilen <= 0) break;
-      if ( tmp[ilen - 1] == '\n' || tmp[ilen - 1] == '\r' || tmp[ilen - 1] == ' ') tmp[ilen - 1] = 0;
-      else break;
+      length = strData.length();
+      strData.TrimRight(" ");
+      strData.TrimRight('\n');      
+      strData.TrimRight("\r");
     }
 
-    strcat(tmp, "\n");
-    tmp[16384 - 1] = 0;
+    OutputDebugString(strData.c_str());
+    OutputDebugString("\n");
 
-    OutputDebugString(tmp);
-
-    fwrite(strTime.c_str(), strTime.size(), 1, fd);
-    fwrite(str.c_str(), str.size(), 1, fd);
-    fwrite(tmp, strlen(tmp), 1, fd);
+    /* fixup newline alignment, number of spaces should equal prefix length */
+    strData.Replace("\n", "\n                             ");
+    strData += "\n";
+       
+    fwrite(strPrefix.c_str(), strPrefix.size(), 1, fd);
+    fwrite(strData.c_str(), strData.size(), 1, fd);
     fflush(fd);
   }
 #ifdef _DEBUG
@@ -108,13 +102,18 @@ void CLog::Log(int loglevel, const char *format, ... )
   {
     // In debug mode dump everything to devstudio regardless of level
     CSingleLock waitLock(critSec);
-    va_list va;
+    CStdString strData;
+    strData.reserve(16384);
 
+    va_list va;
     va_start(va, format);
-    _vsnprintf(tmp, 16384, format, va);
+    strData.FormatV(format, va);    
     va_end(va);
-    strcat(tmp, "\n");
-    OutputDebugString(tmp);
+    
+    OutputDebugString(strData.c_str());
+    if( strData.Right(1) != "\n" )
+      OutputDebugString("\n");
+    
   }
 #endif
 }
@@ -122,22 +121,29 @@ void CLog::Log(int loglevel, const char *format, ... )
 void CLog::DebugLog(const char *format, ... )
 {
   CSingleLock waitLock(critSec);
-  va_list va;
 
+  CStdString strData;
+  strData.reserve(16384);
+
+  va_list va;
   va_start(va, format);
-  _vsnprintf(tmp, 16384, format, va);
+  strData.FormatV(format, va);    
   va_end(va);
-  strcat(tmp, "\n");
-  OutputDebugString(tmp);
+  
+  OutputDebugString(strData.c_str());
+  if( strData.Right(1) != "\n" )
+    OutputDebugString("\n");
 }
 
 void CLog::DebugLogMemory()
 {
   CSingleLock waitLock(critSec);
   MEMORYSTATUS stat;
+  CStdString strData;
+
   GlobalMemoryStatus(&stat);
-  sprintf(tmp, "%i bytes free\n", stat.dwAvailPhys);
-  OutputDebugString(tmp);
+  strData.Format("%i bytes free\n", stat.dwAvailPhys);
+  OutputDebugString(strData.c_str());
 }
 
 void CLog::MemDump(BYTE *pData, int length)
