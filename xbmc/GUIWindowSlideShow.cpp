632d2131e23e7d05d3a19ac2e5d34d4badf09134
spiff_ <spiff_@svn>
2006-05-06 16:27:23 +0000

    changed: when viewing a .cbr/.cbz, move to top left corner on image change.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5777 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowSlideShow.cpp
+++ xbmc/GUIWindowSlideShow.cpp
@@ -662,6 +662,14 @@ void CGUIWindowSlideShow::OnLoadPic(int iPic, int iSlideNumber, D3DTexture *pTex
         m_Image[iPic].SetTexture(iSlideNumber, pTexture, iWidth, iHeight, iRotate, CSlideShowPic::EFFECT_NO_TIMEOUT);
       m_Image[iPic].SetOriginalSize(iOriginalWidth, iOriginalHeight, bFullSize);
       m_Image[iPic].Zoom(m_iZoomFactor, true);
+
+      if (CUtil::IsInRAR(m_vecSlides[m_iCurrentSlide]) || CUtil::IsInZIP(m_vecSlides[m_iCurrentSlide])) // move to top for cbr/cbz
+      {
+        CURL url(m_vecSlides[m_iCurrentSlide]);
+        CStdString strHostName = url.GetHostName();
+        if (CUtil::GetExtension(strHostName).Equals(".cbr", false) || CUtil::GetExtension(strHostName).Equals(".cbz", false))
+          m_Image[iPic].Move((float)m_Image[iPic].GetOriginalWidth(),(float)m_Image[iPic].GetOriginalHeight());
+      }
     }
   }
   else
