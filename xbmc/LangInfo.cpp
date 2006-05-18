d25761111ea4773700d13842f64a666095ab906c
bobbin007 <bobbin007@svn>
2006-05-18 21:51:16 +0000

    added: localization of date and time format via langinfo.xml
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@5841 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/LangInfo.cpp
+++ xbmc/LangInfo.cpp
@@ -66,6 +66,41 @@ bool CLangInfo::Load(const CStdString& strFileName)
       m_strDVDSubtitleLanguage=pSubtitle->FirstChild()->Value();
   }
 
+  const TiXmlNode *pRegions = pRootElement->FirstChild("regions");
+  if (pRegions && !pRegions->NoChildren())
+  {
+    const TiXmlElement *pRegion=pRegions->FirstChildElement("region");
+    while (pRegion)
+    {
+      REGION region;
+      region.strName=pRegion->Attribute("name");
+      if (region.strName.IsEmpty())
+        region.strName="N/A";
+
+      const TiXmlNode *pDateLong=pRegion->FirstChild("datelong");
+      if (pDateLong && !pDateLong->NoChildren())
+        region.strDateFormatLong=pDateLong->FirstChild()->Value();
+
+      const TiXmlNode *pDateShort=pRegion->FirstChild("dateshort");
+      if (pDateShort && !pDateShort->NoChildren())
+        region.strDateFormatShort=pDateShort->FirstChild()->Value();
+
+      const TiXmlElement *pTime=pRegion->FirstChildElement("time");
+      if (pTime && !pTime->NoChildren())
+      {
+        region.strTimeFormat=pTime->FirstChild()->Value();
+        region.strMeridiemSymbols[MERIDIEM_SYMBOL_AM]=pTime->Attribute("symbolAM");
+        region.strMeridiemSymbols[MERIDIEM_SYMBOL_PM]=pTime->Attribute("symbolPM");
+      }
+
+      m_regions.insert(PAIR_REGIONS(region.strName, region));
+
+      pRegion=pRegion->NextSiblingElement("region");
+    }
+
+    const CStdString& strName=g_guiSettings.GetString("XBDateTime.Region");
+    SetCurrentRegion(strName);
+  }
   return true;
 }
 
@@ -77,6 +112,8 @@ void CLangInfo::Clear()
   m_strDVDAudioLanguage.Empty();
   m_strDVDSubtitleLanguage.Empty();
   m_forceUnicodeFont=false;
+  m_regions.clear();
+  m_currentRegion=NULL;
 }
 
 CStdString CLangInfo::GetGuiCharSet() const
@@ -122,4 +159,54 @@ CStdString CLangInfo::GetDVDAudioLanguage() const
 CStdString CLangInfo::GetDVDSubtitleLanguage() const
 {
   return (!m_strDVDSubtitleLanguage.IsEmpty() ? m_strDVDSubtitleLanguage : "en");
+}
+
+// Returns the format string for the date of the current language
+CStdString CLangInfo::GetDateFormat(bool bLongDate/*=false*/) const
+{
+  if (bLongDate)
+    return (m_currentRegion ? m_currentRegion->strDateFormatLong : "DDDD, D. MMMM YYYY");
+  else
+    return (m_currentRegion ? m_currentRegion->strDateFormatShort : "DD/MM/YYYY");
+}
+
+// Returns the format string for the time of the current language
+CStdString CLangInfo::GetTimeFormat() const
+{
+  return (m_currentRegion ? m_currentRegion->strTimeFormat : "HH:mm:ss");
+}
+
+// Returns the AM/PM symbol of the current language
+CStdString CLangInfo::GetMeridiemSymbol(MERIDIEM_SYMBOL symbol) const
+{
+  return (m_currentRegion ? m_currentRegion->strMeridiemSymbols[symbol] : "");
+}
+
+// Fills the array with the region names available for this language
+void CLangInfo::GetRegionNames(CStdStringArray& array)
+{
+  for (ITMAPREGIONS it=m_regions.begin(); it!=m_regions.end(); ++it)
+  {
+    CStdString strName=it->first;
+    if (strName=="N/A")
+      strName=g_localizeStrings.Get(416);
+    array.push_back(strName);
+  }
+}
+
+// Set the current region by its name, names from GetRegionNames() are valid.
+// If the region is not found the first available region is set.
+void CLangInfo::SetCurrentRegion(const CStdString& strName)
+{
+  ITMAPREGIONS it=m_regions.find(strName);
+  if (it!=m_regions.end())
+    m_currentRegion=&it->second;
+  else
+    m_currentRegion=&m_regions.begin()->second;
+}
+
+// Returns the current region set for this language
+CStdString CLangInfo::GetCurrentRegion()
+{
+  return (m_currentRegion ? m_currentRegion->strName : "");
 }
