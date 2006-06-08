313c60f7e0bb603ce88e7ce2a7628c939585ed2a
jmarshallnz <jmarshallnz@svn>
2006-06-08 09:20:25 +0000

    - 08-06-2006 fixed: Delete on playlists/smart playlists could delete all their files as well.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6022 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/SmartPlaylistDirectory.h
+++ xbmc/FileSystem/SmartPlaylistDirectory.h
@@ -13,7 +13,9 @@ namespace DIRECTORY
     ~CSmartPlaylistDirectory();
     virtual bool GetDirectory(const CStdString& strPath, CFileItemList& items);
     virtual bool ContainsFiles(const CStdString& strPath);
+    virtual bool Remove(const char *strPath) { return CFile::Delete(strPath); };
 
     static CStdString GetPlaylistByName(const CStdString& name);
+
   };
 }
