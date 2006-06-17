ae9a3e4cfe6335cf145bd32d6f1b8da1ead0b32a
nuka1195 <nuka1195@svn>
2006-06-17 17:21:51 +0000

    Re-enabled settings item on context menus.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6104 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowMusicBase.cpp
+++ xbmc/GUIWindowMusicBase.cpp
@@ -1201,9 +1201,9 @@ void CGUIWindowMusicBase::OnPopupMenu(int iItem)
   }
 
   // GeminiServer Todo: Set a MasterLock Option to Enable or disable Settings incontext menu!
-  int btn_Settings = -2;
-  if (g_passwordManager.bMasterUser || !g_guiSettings.GetBool("masterlock.locksettings") || g_guiSettings.GetInt("masterlock.lockmode") == LOCK_MODE_EVERYONE)
-    btn_Settings = pMenu->AddButton(5);    // Settings...
+  //int btn_Settings = -2;
+  //if (g_passwordManager.bMasterUser || !g_guiSettings.GetBool("masterlock.locksettings") || g_guiSettings.GetInt("masterlock.lockmode") == LOCK_MODE_EVERYONE)
+  int btn_Settings = pMenu->AddButton(5);    // Settings...
 
   // position it correctly
   pMenu->SetPosition(iPosX - pMenu->GetWidth() / 2, iPosY - pMenu->GetHeight() / 2);
