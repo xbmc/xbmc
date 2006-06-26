1da2eeea12049457862b0614949642ed35e85e91
bobbin007 <bobbin007@svn>
2006-06-26 12:55:40 +0000

    fixed: if cddb is disabled, the content of a cd is listed, then cddb is enabled, the info from cddb is not displayed
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6212 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicInfoLoader.cpp
+++ xbmc/MusicInfoLoader.cpp
@@ -74,7 +74,7 @@ bool CMusicInfoLoader::LoadItem(CFileItem* pItem)
     pItem->SetThumbnailImage(song->strThumb);
     m_songsFromDb++;
   }
-  else if ((mapItem=m_mapFileItems[pItem->m_strPath])!=NULL && mapItem->m_dateTime==pItem->m_dateTime)
+  else if ((mapItem=m_mapFileItems[pItem->m_strPath])!=NULL && pItem->m_dateTime.IsValid() && mapItem->m_dateTime==pItem->m_dateTime)
   { // Query map if we previously cached the file on HD
     pItem->m_musicInfoTag = mapItem->m_musicInfoTag;
     pItem->SetThumbnailImage(mapItem->GetThumbnailImage());
