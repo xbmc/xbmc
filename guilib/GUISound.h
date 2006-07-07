efcc36b0f35b042bf639541e76e49814ebb765b6
bobbin007 <bobbin007@svn>
2006-07-07 16:08:28 +0000

    changed: cleanup of guiaudiomanager
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6322 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ guilib/GUISound.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class CGUISound
+{
+public:
+  CGUISound();
+  virtual ~CGUISound();
+
+  bool        Load(const CStdString& strFile);
+  void        Play();
+  void        Stop();
+  bool        IsPlaying();
+  void        SetVolume(int level);
+
+private:
+  bool        LoadWav(const CStdString& strFile, WAVEFORMATEX* wfx, LPBYTE* ppWavData, int* pDataSize);
+  bool        CreateBuffer(LPWAVEFORMATEX wfx, int iLength);
+  bool        FillBuffer(LPBYTE pbData, int iLength);
+  void        FreeBuffer();
+
+private:
+  LPDIRECTSOUNDBUFFER m_soundBuffer;
+};
