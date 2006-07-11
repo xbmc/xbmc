fa361443389530939e4082af10193a769c6ae523
c0diq <c0diq@svn>
2006-07-11 06:37:25 +0000

    Fixed warnings by moving specific includes in the cpp instead of .h. Still someone should not redefine the letter T as it is a letter commonly used to describe a template (libGoAhead does).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6351 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/UPnPDirectory.h
+++ xbmc/FileSystem/UPnPDirectory.h
@@ -1,15 +1,32 @@
+/*
+* UPnP Support for XBox Media Center
+* Copyright (c) 2006 c0diq (Sylvain Rebaud)
+* Portions Copyright (c) by the authors of libPlatinum
+*
+* http://www.plutinosoft.com/blog/category/platinum/
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #pragma once
+
 #include "idirectory.h"
 
-#include "../lib/libUPnP/Platinum.h"
-#include "../lib/libUPnP/PltMediaServer.h"
-#include "../lib/libUPnP/PltMediaBrowser.h"
-#include "../lib/libUPnP/PltSyncMediaBrowser.h"
-
-//#include "Platinum.h"
-//#include "PltMediaServer.h"
-//#include "PltMediaBrowser.h"
-//#include "PltSyncMediaBrowser.h"
+/* forward declarations */
+class PLT_UPnP;
+class PLT_SyncMediaBrowser;
 
 using namespace DIRECTORY;
 
@@ -32,7 +49,6 @@ private:
     friend class CUPnPDirectory;
 
     PLT_UPnP*              m_UPnP;
-    PLT_CtrlPointReference m_CtrlPoint;
     PLT_SyncMediaBrowser*  m_MediaBrowser;
 
     static CUPnP* upnp;
