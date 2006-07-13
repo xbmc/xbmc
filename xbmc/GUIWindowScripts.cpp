e6d811fabcd4681c11348e72544f8a19e0bdfd55
darkdonno <darkdonno@svn>
2006-07-13 07:27:15 +0000

    changed: My Scripts now flatterns folders if default default.py is found
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6362 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowScripts.cpp
+++ xbmc/GUIWindowScripts.cpp
@@ -87,10 +87,12 @@ bool CGUIWindowScripts::Update(const CStdString &strDirectory)
   return true;
 }
 
+/* Moved to SetProgramThumbs() to CGUIWindowScripts::OnFinalizeFileItems()
 void CGUIWindowScripts::OnPrepareFileItems(CFileItemList &items)
 {
   items.SetProgramThumbs();
 }
+*/
 
 bool CGUIWindowScripts::OnPlayMedia(int iItem)
 {
@@ -141,3 +143,22 @@ void CGUIWindowScripts::Render()
   CGUIWindow::Render();
 }
 
+void CGUIWindowScripts::OnFinalizeFileItems(CFileItemList &items)
+{
+  // flatten any folders
+  for (int i = 0; i < items.Size(); i++)
+  {
+    CFileItem *item = items[i];
+    if (item->m_bIsFolder && !item->IsParentFolder())
+    { // folder item - let's check for a default.py file, and flatten if we have one
+      CStdString defaultPY;
+      CUtil::AddFileToFolder(item->m_strPath, "default.py", defaultPY);
+      if (CFile::Exists(defaultPY))
+      { // yes, format the item up
+        item->m_strPath = defaultPY;
+        item->m_bIsFolder = false;
+      }
+    }
+  }
+  items.SetProgramThumbs();
+}
