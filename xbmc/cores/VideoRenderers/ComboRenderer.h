a4158ce2e8d83bbc0eeb9c9c0338456a1362fd11
elupus <elupus@svn>
2006-07-31 18:04:32 +0000

    fixed: overlay rendering had tearing, renderer updated overlay surface before previous overlay had been copied (second attempt, restored double buffering)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6444 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/VideoRenderers/ComboRenderer.h
+++ xbmc/cores/VideoRenderers/ComboRenderer.h
@@ -14,9 +14,9 @@ public:
   virtual void Update(bool bPauseDrawing);
   virtual void CheckScreenSaver();
   virtual void SetupScreenshot();
+  virtual void FlipPage(int source);
 
-  // Functions called from mplayer
-  // virtual void     WaitForFlip();
+  // Functions called from mplayer  
   virtual unsigned int Configure(unsigned int width, unsigned int height, unsigned int d_width, unsigned int d_height, float fps);
   virtual unsigned int PreInit();
   virtual void UnInit();
@@ -25,21 +25,23 @@ protected:
   virtual void Render(DWORD flags);
   virtual void ManageDisplay();
   virtual void ManageTextures();
-  bool CreateYUY2Texture();
-  void DeleteYUY2Texture();
-  void ClearYUY2Texture();
+  bool CreateYUY2Texture(int index);
+  void DeleteYUY2Texture(int index);
+  void ClearYUY2Texture(int index);
   void YV12toYUY2();
   LONG YUV2RGB(BYTE y, BYTE u, BYTE v);
 
   DWORD m_hPixelShader;
 
   // RGB/YUY2 texture target(s)
-  LPDIRECT3DTEXTURE8 m_RGBTexture;
-  D3DTexture m_YUY2Texture;
+  LPDIRECT3DSURFACE8  m_RGBSurface[2];
+  LPDIRECT3DTEXTURE8  m_YUY2Texture[2];
 
   static const DWORD FVF_YUYVVERTEX = D3DFVF_XYZRHW | D3DFVF_TEX4;
   static const DWORD FVF_RGBVERTEX = D3DFVF_XYZRHW | D3DFVF_TEX1;
 
+  int m_iYUY2RenderBuffer;
+  int m_iYUY2Buffers;
   int m_iScreenWidth;
   int m_iScreenHeight;
   // screensaver stuff
