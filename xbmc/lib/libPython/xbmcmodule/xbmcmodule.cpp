180eafc656a8da9f8ffb7ddbff4f77eebb09304a
nuka1195 <nuka1195@svn>
2006-08-12 16:19:29 +0000

    - 12-08-2006 added: xbmc.getGlobalIdleTime() method to python. Updated some documentation.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6513 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/xbmcmodule/xbmcmodule.cpp
+++ xbmc/lib/libPython/xbmcmodule/xbmcmodule.cpp
@@ -10,6 +10,7 @@
 #include "..\..\libgoahead\xbmchttp.h"
 #include "..\..\..\utils\GUIInfoManager.h"
 #include "..\..\..\..\guilib\GUIAudioManager.h"
+#include "..\..\..\Application.h"
 
 // include for constants
 #include "pyutil.h"
@@ -153,7 +154,7 @@ namespace PYXBMC
 		"\n"
     "function       : string - builtin function to execute.\n"
 		"\n"
-    "List of functions - http://manual.xboxmediacenter.de/wakka.php?wakka=BuiltInFunctions \n"
+    "List of functions - http://www.xbmc.xbox-scene.com/wiki/index.php?title=List_of_Built_In_Functions \n"
 		"\n"
 		"example:\n"
     "  - xbmc.executebuiltin('XBMC.RunXBE(c:\\\\avalaunch.xbe)')\n");
@@ -177,7 +178,7 @@ namespace PYXBMC
 		"\n"
     "httpcommand    : string - http command to execute.\n"
 		"\n"
-		"List of commands - http://manual.xboxmediacenter.de/wakka.php?wakka=WebServerCommands \n"
+		"List of commands - http://www.xbmc.xbox-scene.com/wiki/index.php?title=WebServerHTTP-API#The_Commands \n"
 		"\n"
     "example:\n"
     "  - response = xbmc.executehttpapi('TakeScreenShot(q:\\\\test.jpg,0,false,200,-1,90)')\n");
@@ -373,7 +374,7 @@ namespace PYXBMC
 		"\n"
     "infotag        : string - infoTag for value you want returned.\n"
 		"\n"
-    "List of InfoTags - http://manual.xboxmediacenter.de/wakka.php?wakka=InfoLabels \n"
+    "List of InfoTags - http://www.xbmc.xbox-scene.com/wiki/index.php?title=InfoLabels \n"
 		"\n"
 		"example:\n"
 		"  - label = xbmc.getInfoLabel('Weather.Conditions')\n");
@@ -394,7 +395,7 @@ namespace PYXBMC
 		"\n"
     "infotag        : string - infotag for value you want returned.\n"
 		"\n"
-    "List of InfoTags - http://manual.xboxmediacenter.de/wakka.php?wakka=InfoLabels \n"
+    "List of InfoTags - http://www.xbmc.xbox-scene.com/wiki/index.php?title=InfoLabels \n"
 		"\n"
 		"example:\n"
 		"  - filename = xbmc.getInfoImage('Weather.Conditions')\n");
@@ -453,18 +454,19 @@ namespace PYXBMC
     return Py_None;
   }
   
-	// getVisibility() method
+	// getCondVisibility() method
   PyDoc_STRVAR(getCondVisibility__doc__,
-		"getCondVisibility(condition) -- Returns True/False as a string.\n"
+    "getCondVisibility(condition) -- Returns True (1) or False (0) as a bool.\n"
 		"\n"
     "condition      : string - condition to check.\n"
     "\n"
     "List of Conditions - http://www.xbmc.xbox-scene.com/wiki/index.php?title=List_of_Boolean_Conditions \n"
 		"\n"
-    "*Note, You may and ('+') or or ('|') conditions. You group conditions with ('[]').\n"
+    "*Note, You can combine two (or more) of the above settings by using \"+\" as an AND operator,\n"
+    "\"|\" as an OR operator, \"!\" as a NOT operator, and \"[\" and \"]\" to bracket expressions.\n"
     "\n"
 		"example:\n"
-		"  - visble = xbmc.getCondVisibility('System.KaiConnected')\n");
+		"  - visble = xbmc.getCondVisibility('[System.KaiEnabled + !Skin.String(KAI)]')\n");
 
 	PyObject* XBMC_GetCondVisibility(PyObject *self, PyObject *args)
 	{
@@ -474,6 +476,18 @@ namespace PYXBMC
     int ret = g_infoManager.TranslateString(cLine);
     return Py_BuildValue("b", g_infoManager.GetBool(ret));
   }
+
+	// getGlobalIdleTime() method
+  PyDoc_STRVAR(getGlobalIdleTime__doc__,
+    "getGlobalIdleTime() -- Returns the elapsed idle time in seconds as an integer.\n"
+		"\n"
+		"example:\n"
+		"  - t = xbmc.getGlobalIdleTime()");
+
+	PyObject* XBMC_GetGlobalIdleTime(PyObject *self)
+	{
+    return Py_BuildValue("i", g_application.GlobalIdleTime());
+  }//g_application.m_idleTimer
 
   // define c functions to be used in python here
 	PyMethodDef xbmcMethods[] = {
@@ -499,6 +513,7 @@ namespace PYXBMC
 		{"getInfoLabel", (PyCFunction)XBMC_GetInfoLabel, METH_VARARGS, getInfoLabel__doc__},
 		{"getInfoImage", (PyCFunction)XBMC_GetInfoImage, METH_VARARGS, getInfoImage__doc__},
 		{"getCondVisibility", (PyCFunction)XBMC_GetCondVisibility, METH_VARARGS, getCondVisibility__doc__},
+		{"getGlobalIdleTime", (PyCFunction)XBMC_GetGlobalIdleTime, METH_VARARGS, getGlobalIdleTime__doc__},
 
 		{"playSFX", (PyCFunction)XBMC_PlaySFX, METH_VARARGS, playSFX__doc__},
 		{"enableNavSounds", (PyCFunction)XBMC_EnableNavSounds, METH_VARARGS, enableNavSounds__doc__},
