ec6206b974aa82bafefb5ec6eac4f3cd040cee15
spiff_ <spiff_@svn>
2006-08-27 12:14:33 +0000

    changed: you can now have okay/cancel buttons in the lock dialog. skinners are free to update if they feel like it. id 28=okay, id 29=cancel.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6622 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIDialogLockSettings.h
+++ xbmc/GUIDialogLockSettings.h
@@ -9,13 +9,15 @@ public:
   virtual ~CGUIDialogLockSettings(void);
   virtual bool OnMessage(CGUIMessage &message);
   static bool ShowAndGetLock(int& iLockMode, CStdString& strPassword, int iHeader=20091);
-  static bool ShowAndGetLock(int& iLockMode, CStdString& strPassword, bool& bLockMusic, bool& bLockVideo, bool& bLockPictures, bool& bLockPrograms, bool& bLockFiles, bool& bLockSettings, int iButtonLabel=20091,bool bDetails=true);
+  static bool ShowAndGetLock(int& iLockMode, CStdString& strPassword, bool& bLockMusic, bool& bLockVideo, bool& bLockPictures, bool& bLockPrograms, bool& bLockFiles, bool& bLockSettings, int iButtonLabel=20091,bool bConditional=false, bool bDetails=true);
   static bool ShowAndGetUserAndPassword(CStdString& strUser, CStdString& strPassword, const CStdString& strURL);
 protected:
+  virtual void OnCancel();
   virtual void OnInitWindow();
   virtual void SetupPage();
   virtual void CreateSettings();
   void OnSettingChanged(unsigned int setting);
+  void EnableDetails(bool bEnable);
 
   int m_iLock;
   CStdString m_strLock;
@@ -23,6 +25,7 @@ protected:
   CStdString m_strURL;
   bool m_bChanged;
   bool m_bDetails;
+  bool m_bConditionalDetails;
   bool m_bGetUser;
   int m_iButtonLabel;
   bool m_bLockMusic; 
