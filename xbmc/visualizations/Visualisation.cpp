3f264beb1dc494dd359cbc4d06337cb65c40b7af
mrc_xbmc <mrc_xbmc@svn>
2006-08-31 18:41:23 +0000

    fixed: Pixel ratio passed into visualisations was wrong.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6679 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/visualizations/Visualisation.cpp
+++ xbmc/visualizations/Visualisation.cpp
@@ -30,7 +30,9 @@ void CVisualisation::Create(int posx, int posy, int width, int height)
   sprintf(szTmp, "create:%ix%i at %ix%i %s\n", width, height, posx, posy, m_strVisualisationName.c_str());
   OutputDebugString(szTmp);
   
-  m_pVisz->Create (g_graphicsContext.Get3DDevice(), posx, posy, width, height, m_strVisualisationName.c_str(), g_graphicsContext.GetPixelRatio(g_graphicsContext.GetVideoResolution()));
+  float pixelRatio = g_settings.m_ResInfo[g_graphicsContext.GetVideoResolution()].fPixelRatio;
+  m_pVisz->Create (g_graphicsContext.Get3DDevice(), posx, posy, width, height, m_strVisualisationName.c_str(), pixelRatio);
+
 }
 
 void CVisualisation::Start(int iChannels, int iSamplesPerSec, int iBitsPerSample, const CStdString strSongName)
