75651f6afae3e00a2ca439b2ee9201be3bdbfb99
jmarshallnz <jmarshallnz@svn>
2006-09-03 06:54:52 +0000

    - 03-09-2006 fixed: SMB workgroup/winsserver prompted for a restart even if the setting was cancelled.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6710 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/SettingsControls.h
+++ xbmc/SettingsControls.h
@@ -9,7 +9,7 @@ class CBaseSettingControl
 public:
   CBaseSettingControl(DWORD dwID, CSetting *pSetting);
   ~CBaseSettingControl() {};
-  virtual void OnClick() {};
+  virtual bool OnClick() { return false; };
   virtual void Update() {};
   DWORD GetID() { return m_dwID; };
   CSetting* GetSetting() { return m_pSetting; };
@@ -24,7 +24,7 @@ class CRadioButtonSettingControl : public CBaseSettingControl
 public:
   CRadioButtonSettingControl(CGUIRadioButtonControl* pRadioButton, DWORD dwID, CSetting *pSetting);
   ~CRadioButtonSettingControl();
-  virtual void OnClick();
+  virtual bool OnClick();
   virtual void Update();
   void Select(bool bSelect);
 private:
@@ -36,7 +36,7 @@ class CSpinExSettingControl : public CBaseSettingControl
 public:
   CSpinExSettingControl(CGUISpinControlEx* pSpin, DWORD dwID, CSetting *pSetting);
   ~CSpinExSettingControl();
-  virtual void OnClick();
+  virtual bool OnClick();
   virtual void Update();
 private:
   CGUISpinControlEx *m_pSpin;
@@ -47,7 +47,7 @@ class CButtonSettingControl : public CBaseSettingControl
 public:
   CButtonSettingControl(CGUIButtonControl* pButton, DWORD dwID, CSetting *pSetting);
   ~CButtonSettingControl();
-  virtual void OnClick();
+  virtual bool OnClick();
   virtual void Update();
 private:
   bool IsValidIPAddress(const CStdString &strIP);
@@ -59,7 +59,7 @@ class CSeparatorSettingControl : public CBaseSettingControl
 public:
   CSeparatorSettingControl(CGUIImage* pImage, DWORD dwID, CSetting *pSetting);
   ~CSeparatorSettingControl();
-  virtual void OnClick() {};
+  virtual bool OnClick() { return false; };
   virtual void Update() {};
 private:
   CGUIImage *m_pImage;
