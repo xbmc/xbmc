3145a6f28c47a72f8a2da7ea5baf22e9e91c0269
jmarshallnz <jmarshallnz@svn>
2006-09-11 13:44:40 +0000

    - 11-09-2006 fixed: Consistency: Always remove the rename/delete buttons from the context menu when they're not appropriate in media windows.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@6792 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoBase.cpp
+++ xbmc/GUIWindowVideoBase.cpp
@@ -787,14 +787,10 @@ void CGUIWindowVideoBase::OnPopupMenu(int iItem)
   {
     if ((m_vecItems.m_strPath.Equals(CUtil::VideoPlaylistsLocation())) || (GetID() == WINDOW_VIDEO_FILES && g_guiSettings.GetBool("filelists.allowfiledeletion")))
     {
-      btn_Delete = pMenu->AddButton(117);
-      btn_Rename = pMenu->AddButton(118);
-
-      // disable these functions if not supported by the protocol
-      if (m_vecItems[iItem]->IsReadOnly())
-      {
-        pMenu->EnableButton(btn_Delete, false);
-        pMenu->EnableButton(btn_Rename, false);
+      if (!m_vecItems[iItem]->IsReadOnly())
+      { // enable only if writeable
+        btn_Delete = pMenu->AddButton(117);
+        btn_Rename = pMenu->AddButton(118);
       }
     }
     if (GetID() == WINDOW_VIDEO_TITLE && (g_settings.m_vecProfiles[g_settings.m_iLastLoadedProfileIndex].canWriteDatabases() || g_passwordManager.bMasterUser))
