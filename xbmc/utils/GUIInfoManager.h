fc514ab01fcab8e483103dbec8a1e2bd64ffcd3e
pike2k <pike2k@svn>
2006-11-12 21:56:49 +0000

    *** empty log message ***
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7058 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.h
+++ xbmc/utils/GUIInfoManager.h
@@ -219,7 +219,7 @@
 #define CONTROL_HAS_FOCUS           30000
 #define BUTTON_SCROLLER_HAS_ICON    30001
 
-#define VERSION_STRING "2.0.0"
+#define VERSION_STRING "2.0.1"
 
 // the multiple information vector
 #define MULTI_INFO_START              40000
