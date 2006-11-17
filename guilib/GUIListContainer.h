0ffc534554ee1e5def850592285ada9d752a36ff
jmarshallnz <jmarshallnz@svn>
2006-11-17 00:57:24 +0000

    added: Smooth scrolling to new list container, and ability to have a horizontal list.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7130 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIListContainer.h
+++ guilib/GUIListContainer.h
@@ -16,13 +16,15 @@ class CGUIListItem;
 class CGUIListContainer : public CGUIControl
 {
 public:
-  CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height);
+  CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height, ORIENTATION orientation);
   virtual ~CGUIListContainer(void);
 
   virtual void Render();
   virtual bool OnAction(const CAction &action);
   virtual void OnDown();
   virtual void OnUp();
+  virtual void OnLeft();
+  virtual void OnRight();
   virtual bool OnMouseOver();
   virtual bool OnMouseClick(DWORD dwButton);
   virtual bool OnMouseDoubleClick(DWORD dwButton);
@@ -38,7 +40,7 @@ public:
 
   void SetPageControl(DWORD id);
 
-  void SetItemHeight(float itemHeight, float focusedHeight);
+  void SetItemSize(float itemWidth, float itemHeight, float focusedWidth, float focusedHeight);
   void SetItemLayout(const vector<CGUIControl*> &itemLayout, const vector<CGUIControl*> &focusedLayout);
 
   virtual CStdString GetDescription() const;
@@ -52,16 +54,18 @@ protected:
   bool SelectItemFromPoint(float posX, float posY);
   void RenderItem(float posX, float posY, CGUIListItem *item, bool focused);
   void Scroll(int amount);
+  bool MoveDown(DWORD nextControl);
+  bool MoveUp(DWORD nextControl);
   void ValidateOffset();
   void UpdateLayout();
+  inline float Size() const;
 
   int m_offset;
   int m_cursor;
 
-  float m_scrollOffset;
-
-  float m_itemHeight;
-  float m_focusedHeight;
+  float m_itemSize;
+  float m_focusedSize;
+  ORIENTATION m_orientation;
   int m_itemsPerPage;
 
   string m_strSuffix;
@@ -75,5 +79,10 @@ protected:
   typedef vector<CGUIControl*>::iterator iControls;
 
   DWORD m_renderTime;
+
+  void ScrollToOffset();
+  DWORD m_scrollTime;
+  float m_scrollSpeed;
+  float m_scrollOffset;
 };
 
