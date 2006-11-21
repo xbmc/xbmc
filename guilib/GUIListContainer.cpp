99f838d853ea369935de73eb06a30f5fe5d711e4
jmarshallnz <jmarshallnz@svn>
2006-11-21 10:04:43 +0000

    cleanup: Refactored list code, and added <scrolltime> setting to speed up/slow down scrolling (defaults to 200ms)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7178 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIListContainer.cpp
+++ guilib/GUIListContainer.cpp
@@ -2,21 +2,9 @@
 #include "GUIListContainer.h"
 #include "GUIListItem.h"
 
-#define SCROLL_TIME 200
-
-CGUIListContainer::CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height, ORIENTATION orientation)
-    : CGUIControl(dwParentID, dwControlId, posX, posY, width, height)
+CGUIListContainer::CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height, ORIENTATION orientation, int scrollTime)
+    : CGUIBaseContainer(dwParentID, dwControlId, posX, posY, width, height, orientation, scrollTime)
 {
-  m_cursor = 0;
-  m_offset = 0;
-  m_scrollOffset = 0;
-  m_scrollSpeed = 0;
-  m_scrollTime = 0;
-  m_itemsPerPage = 10;
-  m_pageControl = 0;
-  m_renderTime = 0;
-  m_orientation = orientation;
-  m_analogScrollCount = 0;
   ControlType = GUICONTAINER_LIST;
 //#ifdef PRE_SKIN_VERSION_2_1_COMPATIBILITY
   m_spinControl = NULL;
@@ -36,33 +24,18 @@ void CGUIListContainer::Render()
   if (m_bInvalidated)
     UpdateLayout();
 
-  m_scrollOffset += m_scrollSpeed * (m_renderTime - m_scrollTime);
+  m_scrollOffset += m_scrollSpeed * (m_renderTime - m_scrollLastTime);
   if ((m_scrollSpeed < 0 && m_scrollOffset < m_offset * m_layout.Size(m_orientation)) ||
       (m_scrollSpeed > 0 && m_scrollOffset > m_offset * m_layout.Size(m_orientation)))
   {
     m_scrollOffset = m_offset * m_layout.Size(m_orientation);
     m_scrollSpeed = 0;
   }
-  m_scrollTime = m_renderTime;
+  m_scrollLastTime = m_renderTime;
 
   int offset = (int)(m_scrollOffset / m_layout.Size(m_orientation));
   // Free memory not used on screen at the moment, do this first so there's more memory for the new items.
-  if (offset < 30000)
-  {
-    for (int i = 0; i < offset && i < (int)m_items.size(); ++i)
-    {
-      CGUIListItem *item = m_items[i];
-      if (item)
-        item->FreeMemory();
-    }
-  }
-  // + 1 for the next item in the list
-  for (int i = offset + m_itemsPerPage + 1; i < (int)m_items.size(); ++i)
-  {
-    CGUIListItem *item = m_items[i];
-    if (item)
-      item->FreeMemory();
-  }
+  FreeMemory(CorrectOffset(offset), CorrectOffset(offset + m_itemsPerPage));
 
   g_graphicsContext.SetViewPort(m_posX, m_posY, m_width, m_height);
   float posX = m_posX;
@@ -73,7 +46,7 @@ void CGUIListContainer::Render()
     posX += (offset * m_layout.Size(m_orientation) - m_scrollOffset);;
 
   int current = offset;
-  while (posX < m_posX + m_width && posY < m_posY + m_height)
+  while (posX < m_posX + m_width && posY < m_posY + m_height && m_items.size())
   {
     if (current >= (int)m_items.size())
       break;
@@ -90,7 +63,6 @@ void CGUIListContainer::Render()
 
     current++;
   }
-  g_graphicsContext.RemoveGroupTransform();
   g_graphicsContext.RestoreViewPort();
 
   if (m_pageControl)
@@ -98,38 +70,7 @@ void CGUIListContainer::Render()
     CGUIMessage msg(GUI_MSG_ITEM_SELECT, GetID(), m_pageControl, offset);
     SendWindowMessage(msg);
   }
-  CGUIControl::Render();
-}
-
-void CGUIListContainer::RenderItem(float posX, float posY, CGUIListItem *item, bool focused)
-{
-  // set the origin
-  g_graphicsContext.SetControlTransform(TransformMatrix::CreateTranslation(posX, posY));
-
-  static CGUIListItem *lastItem = NULL;
-  if (focused)
-  {
-    if (!item->GetFocusedLayout())
-    {
-      CGUIListItemLayout *layout = new CGUIListItemLayout(m_focusedLayout);
-      item->SetFocusedLayout(layout);
-    }
-    if (item != lastItem)
-      item->GetFocusedLayout()->ResetScrolling();
-    if (item->GetFocusedLayout())
-      item->GetFocusedLayout()->Render(item);
-    lastItem = item;
-  }
-  else
-  {
-    if (!item->GetLayout())
-    {
-      CGUIListItemLayout *layout = new CGUIListItemLayout(m_layout);
-      item->SetLayout(layout);
-    }
-    if (item->GetLayout())
-      item->GetLayout()->Render(item);
-  }
+  CGUIBaseContainer::Render();
 }
 
 bool CGUIListContainer::OnAction(const CAction &action)
@@ -204,124 +145,20 @@ bool CGUIListContainer::OnAction(const CAction &action)
       return handled;
     }
     break;
-
-  case ACTION_MOVE_LEFT:
-  case ACTION_MOVE_RIGHT:
-  case ACTION_MOVE_DOWN:
-  case ACTION_MOVE_UP:
-    { // use base class implementation
-      return CGUIControl::OnAction(action);
-    }
-    break;
-
-  default:
-    if (action.wID)
-    { // Don't know what to do, so send to our parent window.
-      CGUIMessage msg(GUI_MSG_CLICKED, GetID(), GetParentID(), action.wID);
-      return g_graphicsContext.SendMessage(msg);
-    }
   }
-  return false;
+  return CGUIBaseContainer::OnAction(action);
 }
 
 bool CGUIListContainer::OnMessage(CGUIMessage& message)
 {
   if (message.GetControlId() == GetID() )
   {
-    if (message.GetMessage() == GUI_MSG_LABEL_ADD)
-    {
-      CGUIListItem* item = (CGUIListItem*)message.GetLPVOID();
-      m_items.push_back(item);
-      if (m_pageControl)
-      {
-        CGUIMessage msg(GUI_MSG_LABEL_RESET, GetID(), m_pageControl, m_itemsPerPage, m_items.size());
-        SendWindowMessage(msg);
-      }
-      return true;
-    }
-    else if (message.GetMessage() == GUI_MSG_LABEL_RESET)
+    if (message.GetMessage() == GUI_MSG_LABEL_RESET)
     {
       m_cursor = 0;
-      m_items.clear();
-      if (m_pageControl)
-      {
-        CGUIMessage msg(GUI_MSG_LABEL_RESET, GetID(), m_pageControl, m_itemsPerPage, m_items.size());
-        SendWindowMessage(msg);
-      }
-      return true;
-    }
-    else if (message.GetMessage() == GUI_MSG_ITEM_SELECTED)
-    {
-      message.SetParam1(m_offset + m_cursor);
-      return true;
-    }
-    else if (message.GetMessage() == GUI_MSG_ITEM_SELECT)
-    {
-      // Check that m_offset is valid
-      ValidateOffset();
-      // only select an item if it's in a valid range
-      if (message.GetParam1() >= 0 && message.GetParam1() < (int)m_items.size())
-      {
-        // Select the item requested
-        int item = message.GetParam1();
-        if (item >= m_offset && item < m_offset + m_itemsPerPage)
-        { // the item is on the current page, so don't change it.
-          m_cursor = item - m_offset;
-        }
-        else if (item < m_offset)
-        { // item is on a previous page - make it the first item on the page
-          m_cursor = 0;
-          m_offset = item;
-          ScrollToOffset();
-        }
-        else // (item >= m_offset+m_itemsPerPage)
-        { // item is on a later page - make it the last item on the page
-          m_cursor = m_itemsPerPage - 1;
-          m_offset = item - m_cursor;
-          ScrollToOffset();
-        }
-      }
-      return true;
-    }
-    else if (message.GetMessage() == GUI_MSG_PAGE_CHANGE)
-    {
-      if (message.GetSenderId() == m_pageControl)
-      { // update our page
-        m_offset = message.GetParam1();
-        ScrollToOffset();
-        return true;
-      }
     }
   }
-  return CGUIControl::OnMessage(message);
-}
-
-void CGUIListContainer::OnUp()
-{
-  if (m_orientation == VERTICAL && MoveUp(m_dwControlUp))
-    return;
-  CGUIControl::OnUp();
-}
-
-void CGUIListContainer::OnDown()
-{
-  if (m_orientation == VERTICAL && MoveDown(m_dwControlDown))
-    return;
-  CGUIControl::OnDown();
-}
-
-void CGUIListContainer::OnLeft()
-{
-  if (m_orientation == HORIZONTAL && MoveUp(m_dwControlLeft))
-    return;
-  CGUIControl::OnLeft();
-}
-
-void CGUIListContainer::OnRight()
-{
-  if (m_orientation == HORIZONTAL && MoveDown(m_dwControlRight))
-    return;
-  CGUIControl::OnRight();
+  return CGUIBaseContainer::OnMessage(message);
 }
 
 bool CGUIListContainer::MoveUp(DWORD control)
@@ -330,17 +167,16 @@ bool CGUIListContainer::MoveUp(DWORD control)
     m_cursor--;
   else if (m_cursor == 0 && m_offset)
   {
-    m_offset--;
-    ScrollToOffset();
+    ScrollToOffset(m_offset - 1);
   }
   else if ( control == 0 || control == GetID() )
   {
     if (m_items.size() > 0)
     { // move 2 last item in list
-      m_offset = m_items.size() - m_itemsPerPage;
-      if (m_offset < 0) m_offset = 0;
-      m_cursor = m_items.size() - m_offset - 1;
-      ScrollToOffset();
+      int offset = m_items.size() - m_itemsPerPage;
+      if (offset < 0) offset = 0;
+      m_cursor = m_items.size() - offset - 1;
+      ScrollToOffset(offset);
     }
   }
   else
@@ -356,15 +192,13 @@ bool CGUIListContainer::MoveDown(DWORD control)
       m_cursor++;
     else
     {
-      m_offset++;
-      ScrollToOffset();
+      ScrollToOffset(m_offset + 1);
     }
   }
   else if( control == 0 || control == GetID() )
   { // move first item in list
-    m_offset = 0;
     m_cursor = 0;
-    ScrollToOffset();
+    ScrollToOffset(0);
   }
   else
     return false;
@@ -381,96 +215,7 @@ void CGUIListContainer::Scroll(int amount)
     m_offset = m_items.size() - m_itemsPerPage;
   }
   if (m_offset < 0) m_offset = 0;
-  ScrollToOffset();
-}
-
-int CGUIListContainer::GetSelectedItem() const
-{
-  return m_cursor + m_offset;
-}
-
-bool CGUIListContainer::SelectItemFromPoint(float posX, float posY)
-{
-  int row = 0;
-  float pos = (m_orientation == VERTICAL) ? posY : posX;
-  while (row < m_itemsPerPage)
-  {
-    float size = (row == m_offset && m_bHasFocus) ? m_focusedLayout.Size(m_orientation) : m_layout.Size(m_orientation);
-    if (pos < Size() && row + m_offset < (int)m_items.size())
-    { // found
-      m_cursor = row;
-      return true;
-    }
-    row++;
-    pos -= size;
-  }
-  return false;
-}
-
-bool CGUIListContainer::OnMouseOver()
-{
-  // select the item under the pointer
-  if (SelectItemFromPoint(g_Mouse.posX - m_posX, g_Mouse.posY - m_posY))
-    return CGUIControl::OnMouseOver();
-  return false;
-}
-
-bool CGUIListContainer::OnMouseClick(DWORD dwButton)
-{
-  if (SelectItemFromPoint(g_Mouse.posX - m_posX, g_Mouse.posY - m_posY))
-  { // send click message to window
-    SEND_CLICK_MESSAGE(GetID(), GetParentID(), ACTION_MOUSE_CLICK + dwButton);
-    return true;
-  }
-  return false;
-}
-
-bool CGUIListContainer::OnMouseDoubleClick(DWORD dwButton)
-{
-  if (SelectItemFromPoint(g_Mouse.posX - m_posX, g_Mouse.posY - m_posY))
-  { // send double click message to window
-    SEND_CLICK_MESSAGE(GetID(), GetParentID(), ACTION_MOUSE_DOUBLE_CLICK + dwButton);
-    return true;
-  }
-  return false;
-}
-
-bool CGUIListContainer::OnMouseWheel()
-{
-  Scroll(-g_Mouse.cWheel);
-  return true;
-}
-
-CStdString CGUIListContainer::GetDescription() const
-{
-  CStdString strLabel;
-  int item = m_offset + m_cursor;
-  if (item >= 0 && item < (int)m_items.size())
-  {
-    CGUIListItem *pItem = m_items[item];
-    if (pItem->m_bIsFolder)
-      strLabel.Format("[%s]", pItem->GetLabel().c_str());
-    else
-      strLabel = pItem->GetLabel();
-  }
-  return strLabel;
-}
-
-void CGUIListContainer::SetFocus(bool bOnOff)
-{
-  if (bOnOff != HasFocus())
-    Update();
-  CGUIControl::SetFocus(bOnOff);
-}
-
-void CGUIListContainer::SaveStates(vector<CControlState> &states)
-{
-  states.push_back(CControlState(GetID(), m_offset + m_cursor));
-}
-
-void CGUIListContainer::SetPageControl(DWORD id)
-{
-  m_pageControl = id;
+  ScrollToOffset(m_offset);
 }
 
 void CGUIListContainer::ValidateOffset()
@@ -487,83 +232,17 @@ void CGUIListContainer::ValidateOffset()
   }
 }
 
-void CGUIListContainer::UpdateEffectState(DWORD currentTime)
-{
-  CGUIControl::UpdateEffectState(currentTime);
-  m_renderTime = currentTime;
-}
-
-void CGUIListContainer::Animate(DWORD currentTime)
-{
-  TransformMatrix transform;
-  for (unsigned int i = 0; i < m_animations.size(); i++)
-  {
-    CAnimation &anim = m_animations[i];
-    anim.Animate(currentTime, HasRendered());
-    // Update the control states (such as visibility)
-    UpdateStates(anim.type, anim.currentProcess, anim.currentState);
-    // and render the animation effect
-    anim.RenderAnimation(transform);
-  }
-  g_graphicsContext.AddGroupTransform(transform);
-}
-
-void CGUIListContainer::UpdateLayout()
-{
-  // calculate the number of items to display
-  if (HasFocus())
-    m_itemsPerPage = (int)((Size() - m_focusedLayout.Size(m_orientation)) / m_layout.Size(m_orientation)) + 1;
-  else
-    m_itemsPerPage = (int)(Size() / m_layout.Size(m_orientation));
-  CGUIMessage msg(GUI_MSG_LABEL_RESET, GetID(), m_pageControl, m_itemsPerPage, m_items.size());
-  SendWindowMessage(msg);
-}
-
-inline float CGUIListContainer::Size() const
-{
-  return (m_orientation == HORIZONTAL) ? m_width : m_height;
-}
-
-void CGUIListContainer::ScrollToOffset()
-{
-  m_scrollSpeed = (m_offset * m_layout.Size(m_orientation) - m_scrollOffset) / SCROLL_TIME;
-}
-
-void CGUIListContainer::LoadLayout(TiXmlElement *layout)
-{
-  TiXmlElement *itemElement = layout->FirstChildElement("itemlayout");
-  if (itemElement)
-  { // we have a new item layout
-    m_layout.LoadLayout(itemElement, false);
-  }
-  itemElement = layout->FirstChildElement("focusedlayout");
-  if (itemElement)
-  { // we have a new item layout
-    m_focusedLayout.LoadLayout(itemElement, true);
-  }
-}
-
 //#ifdef PRE_SKIN_VERSION_2_1_COMPATIBILITY
 CGUIListContainer::CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height,
                                  const CLabelInfo& labelInfo, const CLabelInfo& labelInfo2,
                                  const CImage& textureButton, const CImage& textureButtonFocus,
                                  float textureHeight, float itemWidth, float itemHeight, float spaceBetweenItems, CGUIControl *pSpin)
-: CGUIControl(dwParentID, dwControlId, posX, posY, width, height) 
+: CGUIBaseContainer(dwParentID, dwControlId, posX, posY, width, height, VERTICAL, 200) 
 {
   m_layout.CreateListControlLayouts(width, textureHeight + spaceBetweenItems, false, labelInfo, labelInfo2, textureButton, textureHeight, itemWidth, itemHeight);
   m_focusedLayout.CreateListControlLayouts(width, textureHeight + spaceBetweenItems, true, labelInfo, labelInfo2, textureButtonFocus, textureHeight, itemWidth, itemHeight);
   m_height = floor(m_height / (textureHeight + spaceBetweenItems)) * (textureHeight + spaceBetweenItems);
   m_spinControl = pSpin;
-  m_cursor = 0;
-  m_offset = 0;
-  m_scrollOffset = 0;
-  m_scrollSpeed = 0;
-  m_scrollTime = 0;
-  m_analogScrollCount = 0;
-  m_itemsPerPage = 10;
-  m_pageControl = 0;
-  m_renderTime = 0;
-  m_orientation = VERTICAL;
   ControlType = GUICONTAINER_LIST;
 }
 //#endif
--- guilib/GUIListContainer.h
+++ guilib/GUIListContainer.h
@@ -5,16 +5,16 @@
 
 #pragma once
 
-#include "GUIControl.h"
-#include "GUIListItemLayout.h"
+#include "GUIBaseContainer.h"
+
 /*!
  \ingroup controls
  \brief 
  */
-class CGUIListContainer : public CGUIControl
+class CGUIListContainer : public CGUIBaseContainer
 {
 public:
-  CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height, ORIENTATION orientation);
+  CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height, ORIENTATION orientation, int scrollTime);
 //#ifdef PRE_SKIN_VERSION_2_1_COMPATIBILITY
   CGUIListContainer(DWORD dwParentID, DWORD dwControlId, float posX, float posY, float width, float height,
                          const CLabelInfo& labelInfo, const CLabelInfo& labelInfo2,
@@ -26,59 +26,12 @@ public:
 
   virtual void Render();
   virtual bool OnAction(const CAction &action);
-  virtual void OnDown();
-  virtual void OnUp();
-  virtual void OnLeft();
-  virtual void OnRight();
-  virtual bool OnMouseOver();
-  virtual bool OnMouseClick(DWORD dwButton);
-  virtual bool OnMouseDoubleClick(DWORD dwButton);
-  virtual bool OnMouseWheel();
   virtual bool OnMessage(CGUIMessage& message);
-  virtual void SetFocus(bool bOnOff);
-
-  void SetPageControl(DWORD id);
-
-  virtual CStdString GetDescription() const;
-  virtual void SaveStates(vector<CControlState> &states);
-  int GetSelectedItem() const;
-
-  virtual void Animate(DWORD currentTime);
-  virtual void UpdateEffectState(DWORD currentTime);
-  void LoadLayout(TiXmlElement *layout);
 
 protected:
-  bool SelectItemFromPoint(float posX, float posY);
-  void RenderItem(float posX, float posY, CGUIListItem *item, bool focused);
-  void Scroll(int amount);
-  bool MoveDown(DWORD nextControl);
-  bool MoveUp(DWORD nextControl);
-  void ValidateOffset();
-  void UpdateLayout();
-  inline float Size() const;
-
-  int m_offset;
-  int m_cursor;
-
-  ORIENTATION m_orientation;
-  int m_itemsPerPage;
-
-  string m_strSuffix;
-  vector<CGUIListItem*> m_items;
-  typedef vector<CGUIListItem*> ::iterator iItems;
-
-  DWORD m_pageControl;
-
-  DWORD m_renderTime;
-
-  CGUIListItemLayout m_layout;
-  CGUIListItemLayout m_focusedLayout;
-
-  float m_analogScrollCount;
-
-  void ScrollToOffset();
-  DWORD m_scrollTime;
-  float m_scrollSpeed;
-  float m_scrollOffset;
+  virtual void Scroll(int amount);
+  virtual bool MoveDown(DWORD nextControl);
+  virtual bool MoveUp(DWORD nextControl);
+  virtual void ValidateOffset();
 };
 
