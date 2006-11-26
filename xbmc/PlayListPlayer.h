471413dc64afe850e3f950bf5f6c052021230fdf
kraqh3d <kraqh3d@svn>
2006-11-26 16:15:32 +0000

    changed: playlist behaviour.  see this thread for details http://www.xboxmediacenter.com/forum/showthread.php?t=23386
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7246 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/PlayListPlayer.h
+++ xbmc/PlayListPlayer.h
@@ -5,9 +5,7 @@
 
 #define PLAYLIST_NONE    -1
 #define PLAYLIST_MUSIC   0
-#define PLAYLIST_MUSIC_TEMP 1
-#define PLAYLIST_VIDEO   2
-#define PLAYLIST_VIDEO_TEMP 3
+#define PLAYLIST_VIDEO   1
 
 using namespace PLAYLIST;
 
@@ -34,12 +32,12 @@ public:
   int GetNextSong();
   void SetCurrentSong(int iSong);
   bool HasChanged();
-  void SetCurrentPlaylist( int iPlayList );
+  void SetCurrentPlaylist(int iPlaylist);
   int GetCurrentPlaylist();
-  CPlayList& GetPlaylist( int nPlayList);
+  CPlayList& GetPlaylist(int iPlaylist);
   int RemoveDVDItems();
   void Reset();
-  void ClearPlaylist(int iPlayList);
+  void ClearPlaylist(int iPlaylist);
   void SetShuffle(int iPlaylist, bool bYesNo);
   bool IsShuffled(int iPlaylist);
   bool HasPlayedFirstFile();
@@ -47,24 +45,25 @@ public:
   void SetRepeat(int iPlaylist, REPEAT_STATE state);
   REPEAT_STATE GetRepeat(int iPlaylist);
 
+  // add items via the playlist player
+  void Add(int iPlaylist, CPlayList::CPlayListItem& item);
+  void Add(int iPlaylist, CPlayList& playlist);
+  void Add(int iPlaylist, CFileItem *pItem);
+  void Add(int iPlaylist, CFileItemList& items);
+
 protected:
   bool Repeated(int iPlaylist);
   bool RepeatedOne(int iPlaylist);
-
-  int NextShuffleItem();
-  int PreviousShuffleItem();
+  void ReShuffle(int iPlaylist, int iPosition);
   bool m_bChanged;
   bool m_bPlayedFirstFile;
+  int m_iFailedSongs;
   int m_iCurrentSong;
   int m_iCurrentPlayList;
   CPlayList m_PlaylistMusic;
-  CPlayList m_PlaylistMusicTemp;
   CPlayList m_PlaylistVideo;
-  CPlayList m_PlaylistVideoTemp;
   CPlayList m_PlaylistEmpty;
-  REPEAT_STATE m_repeatState[4];
-  bool m_shuffleState[4];
-  int m_iFailedSongs;
+  REPEAT_STATE m_repeatState[2];
 };
 
 };
@@ -74,4 +73,3 @@ protected:
  \brief Global instance of playlist player
  */
 extern CPlayListPlayer g_playlistPlayer;
-
