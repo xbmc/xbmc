4b00b1c0d26f589ab0b3a92c95254df07b36f635
kraqh3d <kraqh3d@svn>
2006-11-26 16:10:44 +0000

    changed: playlist behaviour.  see this thread for details http://www.xboxmediacenter.com/forum/showthread.php?t=23386
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@7238 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libPython/xbmcmodule/pyplaylist.cpp
+++ XBMC/xbmc/lib/libPython/xbmcmodule/pyplaylist.cpp
@@ -77,9 +77,7 @@ namespace PYXBMC
 
 		// we do not create our own playlist, just using the ones from playlistplayer
 		if (iNr != PLAYLIST_MUSIC &&
-				iNr != PLAYLIST_MUSIC_TEMP &&
-				iNr != PLAYLIST_VIDEO &&
-				iNr != PLAYLIST_VIDEO_TEMP)
+				iNr != PLAYLIST_VIDEO)
 		{
 			PyErr_SetString((PyObject*)self, "PlayList does not exist");
 			return NULL;
@@ -333,9 +331,7 @@ namespace PYXBMC
 		"int playlist can be one of the next values:\n"
 		"\n"
 		"  0 : xbmc.PLAYLIST_MUSIC\n"
-		"  1 : xbmc.PLAYLIST_MUSIC_TEMP\n"
-		"  2 : xbmc.PLAYLIST_VIDEO\n"
-		"  3 : xbmc.PLAYLIST_VIDEO_TEMP\n"
+		"  1 : xbmc.PLAYLIST_VIDEO\n"
 		"\n"
 		"Use PlayList[int position] or __getitem__(int position) to get a PlayListItem.");
 
