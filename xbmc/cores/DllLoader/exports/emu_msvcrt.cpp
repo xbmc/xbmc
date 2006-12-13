8888be656e647602f3babc225500ab81462b615b
spiff_ <spiff_@svn>
2006-12-13 22:16:51 +0000

    updated: cube codec to v0.22. see http://www.hcs64.com/in_cube.html
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7359 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
+++ xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
@@ -540,6 +540,11 @@ extern "C"
     while ((vecDirsOpen[iDirSlot].Directory) && (iDirSlot<MAX_OPEN_DIRS)) iDirSlot++;
     if (iDirSlot > MAX_OPEN_DIRS)
       return 0xFFFF; // no free slots
+    if (url.GetProtocol().Equals("filereader"))
+    {
+      CURL url2(url.GetFileName());
+      url = url2;
+    }
     CStdString fName = url.GetFileName();
     url.SetFileName("");
     url.GetURL(strURL);
