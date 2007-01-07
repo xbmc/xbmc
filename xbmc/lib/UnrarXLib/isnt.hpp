273d67a42d56915382c7f4f1f3a7aa55230de7fe
c0diq <c0diq@svn>
2007-01-07 08:16:24 +0000

    Removed file/folder
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/upnpserver@7517 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/UnrarXLib/isnt.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _RAR_ISNT_
-#define _RAR_ISNT_
-
-int WinNT();
-
-#endif
--- xbmc/memutil.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void fast_memcpy(void* d, const void* s, unsigned n);
-void fast_memset(void* d, int c, unsigned n);
-void usleep(int t);
