ac55f60bdd850c48eb36d378aba691162b071dd4
spiff_ <spiff_@svn>
2007-01-17 00:31:50 +0000

    fixed: forgot to add director node files
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@7667 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNodeDirector.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "DirectoryNode.h"
+
+namespace DIRECTORY
+{
+  namespace VIDEODATABASEDIRECTORY
+  {
+    class CDirectoryNodeDirector : public CDirectoryNode
+    {
+    public:
+      CDirectoryNodeDirector(const CStdString& strName, CDirectoryNode* pParent);
+    protected:
+      virtual NODE_TYPE GetChildType();
+      virtual bool GetContent(CFileItemList& items);
+    };
+  };
+};
