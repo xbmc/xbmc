0ec8e912c82baca0cb136b5edfe6474aa89ac06c
jmarshallnz <jmarshallnz@svn>
2007-01-24 08:03:25 +0000

    added: Import from and Export to xml stubs for the videodatabase. added: Timing of the videodb retrieval.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7718 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/XMLUtils.h
+++ guilib/XMLUtils.h
@@ -14,4 +14,9 @@ public:
   static bool GetBoolean(const TiXmlNode* pRootNode, const char* strTag, bool& bBoolValue);
   static bool GetString(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue);
   static bool GetEncoding(const TiXmlDocument* pDoc, CStdString& strEncoding);
+
+  static void SetString(TiXmlNode* pRootNode, const char *strTag, const CStdString& strValue);
+  static void SetInt(TiXmlNode* pRootNode, const char *strTag, int value);
+  static void SetFloat(TiXmlNode* pRootNode, const char *strTag, float value);
+  static void SetBoolean(TiXmlNode* pRootNode, const char *strTag, bool value);
 };
