2f49bb1b610318aa1942cd15bdb743ee13b2401f
elupus <elupus@svn>
2007-02-18 19:59:25 +0000

    added: memory tracking of virtualalloc/free_registry added: emulated wide version of RegQueryValueEx fixed: dll fixup's outside of sections would make changes to other parts of the dll fixed: dllloader would crash if the dll headers didn't contain pointers to export table added: attempt to load exports after dllmain have been called. (PECompact2 compression of export table has this effect).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@7859 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/dll_tracker_memory.h
+++ xbmc/cores/DllLoader/dll_tracker_memory.h
@@ -14,6 +14,11 @@ extern "C" void* __cdecl track_realloc(void* p, size_t s);
 extern "C" void __cdecl track_free(void* p);
 extern "C" char* __cdecl track_strdup(const char* str);
 
+WINBASEAPI LPVOID WINAPI track_VirtualAllocEx(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+WINBASEAPI BOOL WINAPI track_VirtualFreeEx(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD dwFreeType);
+
+WINBASEAPI LPVOID WINAPI track_VirtualAlloc( LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
+WINBASEAPI BOOL WINAPI track_VirtualFree(LPVOID lpAddress, SIZE_T dwSize, DWORD dwFreeType);
 
 #ifdef _XBOX
 WINBASEAPI
