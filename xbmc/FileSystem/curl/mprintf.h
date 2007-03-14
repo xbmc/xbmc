a0735abfda1b430e2689b488b7c1aafd6a7955a0
elupus <elupus@svn>
2007-03-14 21:13:05 +0000

    update: libcurl.dll to 7.16.1 version compiled with yaSSL. now supports https:// requests.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8165 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/curl/mprintf.h
+++ xbmc/FileSystem/curl/mprintf.h
@@ -1,3 +1,5 @@
+#ifndef __CURL_MPRINTF_H
+#define __CURL_MPRINTF_H
 /***************************************************************************
  *                                  _   _ ____  _
  *  Project                     ___| | | |  _ \| |
@@ -5,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) 1998 - 2004, Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2006, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -21,13 +23,14 @@
  * $Id$
  ***************************************************************************/
 
-#ifndef H_MPRINTF
-#define H_MPRINTF
-
 #include <stdarg.h>
 #include <stdio.h> /* needed for FILE */
 
 #include "curl.h"
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
 
 CURL_EXTERN int curl_mprintf(const char *format, ...);
 CURL_EXTERN int curl_mfprintf(FILE *fd, const char *format, ...);
@@ -43,14 +46,25 @@ CURL_EXTERN char *curl_mvaprintf(const char *format, va_list args);
 #ifdef _MPRINTF_REPLACE
 # define printf curl_mprintf
 # define fprintf curl_mfprintf
+#ifdef CURLDEBUG
+/* When built with CURLDEBUG we define away the sprintf() functions since we
+   don't want internal code to be using them */
+# define sprintf sprintf_was_used
+# define vsprintf vsprintf_was_used
+#else
 # define sprintf curl_msprintf
+# define vsprintf curl_mvsprintf
+#endif
 # define snprintf curl_msnprintf
 # define vprintf curl_mvprintf
 # define vfprintf curl_mvfprintf
-# define vsprintf curl_mvsprintf
 # define vsnprintf curl_mvsnprintf
 # define aprintf curl_maprintf
 # define vaprintf curl_mvaprintf
 #endif
 
-#endif /* H_MPRINTF */
+#ifdef  __cplusplus
+}
+#endif
+
+#endif /* __CURL_MPRINTF_H */
