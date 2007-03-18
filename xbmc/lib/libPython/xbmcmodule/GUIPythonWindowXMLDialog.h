499a69f1333b78c330f7c05396ebaa434bb36740
darkdonno <darkdonno@svn>
2007-03-18 11:56:57 +0000

    added: WindowXML to Python
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8229 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/lib/libPython/xbmcmodule/GUIPythonWindowXMLDialog.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "GUIPythonWindowXML.h"
+
+int Py_XBMC_Event_OnClick(void* arg);
+int Py_XBMC_Event_OnFocus(void* arg);
+int Py_XBMC_Event_OnInit(void* arg);
+
+class CGUIPythonWindowXMLDialog : public CGUIPythonWindowXML
+{
+public:
+  CGUIPythonWindowXMLDialog(DWORD dwId, CStdString strXML, CStdString strFallBackPath);
+  virtual ~CGUIPythonWindowXMLDialog(void);
+	virtual bool    OnMessage(CGUIMessage& message);
+	void            Activate(DWORD dwParentId);
+	virtual void		Close();
+	virtual bool		IsRunning() const { return m_bRunning; }
+  virtual bool    IsDialog() const { return true;};
+  virtual bool    IsModalDialog() const { return true; };
+protected:
+  virtual void    UpdateButtons() {};
+  virtual void    OnWindowLoaded() {};
+	bool						m_bRunning;
+};
+
