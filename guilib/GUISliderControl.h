377d2e04e4ba6d984490edccad54b5a7bf240dce
jmarshallnz <jmarshallnz@svn>
2007-03-21 04:57:38 +0000

    fixed: Various <colordiffuse> tags didn't apply throughout the entire controls.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8265 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUISliderControl.h
+++ guilib/GUISliderControl.h
@@ -50,6 +50,7 @@ public:
   virtual bool OnMouseWheel();
   virtual CStdString GetDescription() const;
   void SetFormatString(const char *format) { if (format) m_formatString = format; };
+  virtual void SetColorDiffuse(D3DCOLOR color);
 protected:
   virtual void Update() ;
   virtual void Move(int iNumSteps);
