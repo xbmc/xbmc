19b34c528438465387227ce4f8f3cdef71bfc574
spiff_ <spiff_@svn>
2007-03-21 20:07:47 +0000

    changed: removed director node from video library tvshow views (no scraper provided directors).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8275 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNodeTvShowsOverview.cpp
+++ xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNodeTvShowsOverview.cpp
@@ -19,8 +19,6 @@ NODE_TYPE CDirectoryNodeTvShowsOverview::GetChildType()
     return NODE_TYPE_YEAR;
   else if (GetName()=="4")
     return NODE_TYPE_ACTOR;
-  else if (GetName()=="5")
-    return NODE_TYPE_DIRECTOR;
 
   return NODE_TYPE_NONE;
 }
@@ -32,7 +30,6 @@ bool CDirectoryNodeTvShowsOverview::GetContent(CFileItemList& items)
   vecRoot.push_back(g_localizeStrings.Get(369));  // Title
   vecRoot.push_back(g_localizeStrings.Get(345));  // Year
   vecRoot.push_back(g_localizeStrings.Get(344));  // Actors
-  vecRoot.push_back(g_localizeStrings.Get(20348));  // Directors
 
   for (int i = 0; i < (int)vecRoot.size(); ++i)
   {
