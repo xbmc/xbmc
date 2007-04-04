48688fe7b3fc3fed8e6eb1d13b8f20dd7ad4be38
elupus <elupus@svn>
2007-04-04 03:20:34 +0000

    changed: removed socket emulation indirection, as it wasn't used for anything. this removes maximum socket limitation that was previusly 50 sockets and simplifies code.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8413 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/dll_tracker_socket.h
+++ xbmc/cores/DllLoader/dll_tracker_socket.h
@@ -8,9 +8,9 @@ extern "C" void tracker_socket_free_all(DllTrackInfo* pInfo);
 
 extern "C"
 {
-  int __stdcall track_socket(int af, int type, int protocol);
-  int __stdcall track_closesocket(int socket);
-  int __stdcall track_accept(int s, struct sockaddr FAR * addr, OUT int FAR * addrlen);
+  SOCKET __stdcall track_socket(int af, int type, int protocol);
+  int __stdcall track_closesocket(SOCKET socket);
+  SOCKET __stdcall track_accept(SOCKET s, struct sockaddr FAR * addr, OUT int FAR * addrlen);
 }
 
 #endif // _DLL_TRACKER_SOCKET
