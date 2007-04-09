ed09a4ba7ffceb95fd84db652c27b08112f3665a
jmarshallnz <jmarshallnz@svn>
2007-04-09 22:43:05 +0000

    fixed: Scan wasn't an option on music shares.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8450 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowMusicSongs.h
+++ xbmc/GUIWindowMusicSongs.h
@@ -10,6 +10,7 @@ public:
 
   virtual bool OnMessage(CGUIMessage& message);
 
+  void DoScan(const CStdString &strPath);
 protected:
   virtual void OnItemLoaded(CFileItem* pItem) {};
   virtual bool GetDirectory(const CStdString &strDirectory, CFileItemList &items);
@@ -19,6 +20,7 @@ protected:
   virtual void OnPopupMenu(int iItem, bool bContextDriven = true);
   virtual void DoSearch(const CStdString& strSearch, CFileItemList& items);
   virtual void OnScan(int iItem);
+
 
   // new method
   virtual void PlayItem(int iItem);
