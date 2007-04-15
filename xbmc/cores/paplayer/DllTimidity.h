4eaf9607b2e5f735bbc20ea4e3b0ebd82ed2fab6
spiff_ <spiff_@svn>
2007-04-15 16:01:59 +0000

    added: .mid/.kar codec to paplayer.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8516 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/paplayer/DllTimidity.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "../../DynamicDll.h"
+
+class DllTimidityInterface
+{
+public:
+    virtual int Init()=0;
+    virtual int LoadMID(const char* szFileName)=0;
+    virtual void FreeMID(int mid)=0;
+    virtual int FillBuffer(int mid, char* buffer, int length)=0;
+    virtual unsigned long Seek(int mid, unsigned long iTimePos)=0;
+    virtual const char* GetTitle(int mid)=0;
+    virtual const char* GetArtist(int mid)=0;
+    virtual unsigned long GetLength(int mid)=0;
+};
+
+class DllTimidity : public DllDynamic, DllTimidityInterface
+{
+  DECLARE_DLL_WRAPPER(DllTimidity, q:\\system\\players\\paplayer\\timidity.dll)
+  DEFINE_METHOD0(int, Init)
+  DEFINE_METHOD1(int, LoadMID, (const char* p1))
+  DEFINE_METHOD1(void, FreeMID, (int p1))
+  DEFINE_METHOD3(int, FillBuffer, (int p1, char* p2, int p3))
+  DEFINE_METHOD2(unsigned long, Seek, (int p1, unsigned long p2))
+  DEFINE_METHOD1(const char*, GetTitle, (int p1))
+  DEFINE_METHOD1(const char*, GetArtist, (int p1))
+  DEFINE_METHOD1(unsigned long, GetLength, (int p1))
+  BEGIN_METHOD_RESOLVE()
+    RESOLVE_METHOD_RENAME(DLL_Init, Init)
+    RESOLVE_METHOD_RENAME(DLL_LoadMID, LoadMID)
+    RESOLVE_METHOD_RENAME(DLL_FreeMID, FreeMID)
+    RESOLVE_METHOD_RENAME(DLL_FillBuffer, FillBuffer)
+    RESOLVE_METHOD_RENAME(DLL_Seek, Seek)
+    RESOLVE_METHOD_RENAME(DLL_GetTitle, GetTitle)
+    RESOLVE_METHOD_RENAME(DLL_GetArtist, GetArtist)
+    RESOLVE_METHOD_RENAME(DLL_GetLength, GetLength)
+  END_METHOD_RESOLVE()
+};
