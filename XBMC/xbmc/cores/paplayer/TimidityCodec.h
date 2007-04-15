6d2b0fe52a605948b44d42a456ab903b69e57b4a
spiff_ <spiff_@svn>
2007-04-15 16:01:59 +0000

    added: .mid/.kar codec to paplayer.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@8516 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/cores/paplayer/TimidityCodec.h
@@ -0,0 +1,43 @@
+#ifndef Timidity_CODEC_H_
+#define Timidity_CODEC_H_
+
+#include "ICodec.h"
+#include "DllTimidity.h"
+
+class TimidityCodec : public ICodec
+{
+public:
+  TimidityCodec();
+  virtual ~TimidityCodec();
+
+  virtual bool Init(const CStdString &strFile, unsigned int filecache);
+  virtual void DeInit();
+  virtual __int64 Seek(__int64 iSeekTime);
+  virtual int ReadPCM(BYTE *pBuffer, int size, int *actualsize);
+  virtual bool CanInit();
+  static bool IsSupportedFormat(const CStdString& strExt);
+
+private:
+  DllLoader* m_loader;
+  typedef void  (__cdecl *InitMethod) (void);
+  typedef int (__cdecl *LoadMethod) ( const char* p1);
+  typedef int (__cdecl *FillMethod) ( int p1, char* p2, int p3);
+  typedef void  (__cdecl *FreeMethod) ( int p1);
+  typedef unsigned long (__cdecl *LengthMethod) ( int p1);
+  typedef unsigned long (__cdecl *SeekMethod) ( int p1, unsigned long p2);
+  struct   
+  {
+    InitMethod Init;
+    LoadMethod LoadMID;
+    FillMethod FillBuffer;
+    FreeMethod FreeMID;
+    LengthMethod GetLength;
+    SeekMethod Seek;
+  } m_dll;
+
+  int m_mid;
+  int m_iTrack;
+  __int64 m_iDataPos;
+};
+
+#endif
