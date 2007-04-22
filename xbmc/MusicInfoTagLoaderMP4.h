f7818f1510a35965d80e32cb1a0488849d384822
jmarshallnz <jmarshallnz@svn>
2007-04-22 00:49:10 +0000

    fixed: MP4 tag reader cached thumb for every track, even if it already was cached.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8591 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicInfoTagLoaderMP4.h
+++ xbmc/MusicInfoTagLoaderMP4.h
@@ -14,5 +14,17 @@ public:
   virtual ~CMusicInfoTagLoaderMP4();
 
   virtual bool Load(const CStdString& strFileName, CMusicInfoTag& tag);
+
+private:
+  unsigned int ReadUnsignedInt( const char* pData );
+  void ParseTag( unsigned int metaKey, const char* pMetaData, int metaSize, CMusicInfoTag& tag);
+  int GetILSTOffset( const char* pBuffer, int bufferSize );
+  int ParseAtom( __int64 startOffset, __int64 stopOffset, CMusicInfoTag& tag );
+
+  unsigned int m_thumbSize;
+  BYTE *m_thumbData;
+  bool m_isCompilation;
+
+  XFILE::CFile m_file;
 };
 };
