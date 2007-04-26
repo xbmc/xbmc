b35bf3968e9f1ff2e0704c262719f95b56abe5bd
spiff_ <spiff_@svn>
2007-04-26 19:59:12 +0000

    fixed: all items showed in the season node when there were no seasons.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8674 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNode.cpp
+++ xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNode.cpp
@@ -220,7 +220,7 @@ void CDirectoryNode::AddQueuingFolder(CFileItemList& items)
     return;
 
   // no need for "all" item when only one item
-  if (items.Size() == 1 || items.Size() == 2 && items[0]->IsParentFolder())
+  if (items.Size() == 0 || items.Size() == 1 || items.Size() == 2 && items[0]->IsParentFolder())
     return;
 
   switch (GetChildType())
