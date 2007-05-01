27cbb615206216770ef7e34807aa31b43d2bb9ce
jmarshallnz <jmarshallnz@svn>
2007-05-01 08:28:00 +0000

    added: FLAC embedded PICTURE metadata.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@8698 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FlacTag.h
+++ xbmc/FlacTag.h
@@ -20,5 +20,6 @@ protected:
   void ProcessVorbisComment(const char *pBuffer);
   int ReadFlacHeader(void);    // returns the position after the STREAM_INFO metadata
   int FindFlacHeader(void);    // returns the offset in the file of the fLaC data
+  unsigned int ReadUnsigned();  // reads a 32 bit unsigned int
 };
 };
