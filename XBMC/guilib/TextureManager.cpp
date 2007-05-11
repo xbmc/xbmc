c2e1647159202692864dec983c882c739c7b16cc
yuvalt <yuvalt@svn>
2007-05-11 18:58:27 +0000

    Use BGRA colors
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@8884 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/TextureManager.cpp
+++ XBMC/guilib/TextureManager.cpp
@@ -1148,7 +1148,7 @@ CGLTexture::CGLTexture(SDL_Surface* surface)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
  
   glTexImage2D(GL_TEXTURE_2D, 0, 4, textureWidth, textureHeight, 0,
-               GL_RGBA, GL_UNSIGNED_BYTE, pixels);  
+               GL_BGRA, GL_UNSIGNED_BYTE, pixels);  
 }
 
 CGLTexture::~CGLTexture()
