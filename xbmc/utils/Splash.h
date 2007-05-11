5ae8389777931957724eb4d203fc3bc07f723974
yuvalt <yuvalt@svn>
2007-05-11 11:02:23 +0000

    Splash screen is no longer in another thread, as it breaks OpenGL
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@8878 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/Splash.h
+++ xbmc/utils/Splash.h
@@ -5,17 +5,27 @@
 class CSplash : public CThread
 {
 public:
-
   CSplash(const CStdString& imageName);
   virtual ~CSplash();
 
   bool IsRunning();
   bool Start();
   void Stop();
+
+  // In case you don't want to use another thread
+  void Show();
+  void Hide();
+  
 private:
+  virtual void Process();
   virtual void OnStartup();
   virtual void OnExit();
-  virtual void Process();
 
+  float fade;
   CStdString m_ImageName;
+#ifndef HAS_SDL
+  D3DGAMMARAMP newRamp;
+  D3DGAMMARAMP oldRamp;  
+ 
+#endif  
 };
