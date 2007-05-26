b8228d859d7449e2bce713b381edfd5897cf624a
yuvalt <yuvalt@svn>
2007-05-26 13:26:13 +0000

    Add support for FLAC
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@9104 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/DllLibFlac.h
+++ xbmc/cores/paplayer/DllLibFlac.h
@@ -48,7 +48,11 @@ public:
 
 class DllLibFlac : public DllDynamic, DllLibFlacInterface
 {
+#ifndef _LINUX
   DECLARE_DLL_WRAPPER(DllLibFlac, Q:\\system\\players\\PAPlayer\\libFlac.dll)
+#else
+  DECLARE_DLL_WRAPPER(DllLibFlac, Q:\\system\\players\\paplayer\\libFLAC-i486-linux.so)
+#endif
   DEFINE_METHOD0(FLAC__StreamDecoder*, FLAC__stream_decoder_new)
   DEFINE_METHOD1(void, FLAC__stream_decoder_delete, (FLAC__StreamDecoder *p1))
   DEFINE_METHOD10(FLAC__StreamDecoderInitStatus, FLAC__stream_decoder_init_stream,
