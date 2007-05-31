51c4ebe33bb5b09578155783cf534f29940c0493
charlydoes <charlydoes@svn>
2007-05-31 21:06:40 +0000

    changed: longer delay before showing busydialog and don't show the busydialog if any modal dialog is on screen.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9164 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ApplicationRenderer.cpp
+++ xbmc/ApplicationRenderer.cpp
@@ -24,7 +24,7 @@
 #include "Utils/GuiInfoManager.h"
 #include "../guilib/guiImage.h"
 
-#define MAX_MISSEDFRAMES 40
+#define BUSY_DELAY 2000 //milliseconds
 
 CApplicationRenderer g_ApplicationRenderer;
 
@@ -70,6 +70,31 @@ void CApplicationRenderer::Process()
       continue;
     }
 
+    if (!m_pWindow || iWidth == 0 || iHeight == 0)
+    {
+      m_pWindow = (CGUIDialogBusy*)m_gWindowManager.GetWindow(WINDOW_DIALOG_BUSY);
+      if (m_pWindow)
+      {
+        m_pWindow->Initialize();//need to load the window to determine size.
+        if (m_pWindow->GetID() == WINDOW_INVALID)
+        {
+          //busywindow couldn't be loaded so stop this thread.
+          m_pWindow = NULL;
+          m_bStop = true;
+          break;
+        }
+
+        SAFE_RELEASE(m_lpSurface);
+        FRECT rect = m_pWindow->GetScaledBounds();
+        m_pWindow->ClearAll(); //unload
+
+        iLeft = (int)floor(rect.left);
+        iTop =  (int)floor(rect.top);
+        iWidth = (int)ceil(rect.right - rect.left);
+        iHeight = (int)ceil(rect.bottom - rect.top);
+      }
+    }
+
     float t0 = (1000.0f/(float)g_graphicsContext.GetFPS());
     float t1 = m_time + t0; //time when we expect a new render
     float t2 = (float)timeGetTime();
@@ -77,7 +102,7 @@ void CApplicationRenderer::Process()
     {
       try
       {
-        if (m_busycount > MAX_MISSEDFRAMES)
+        if (timeGetTime() >= (m_time + BUSY_DELAY))
         {
           CSingleLock lockg (g_graphicsContext);
           if (m_prevbusycount != m_busycount)
@@ -87,34 +112,12 @@ void CApplicationRenderer::Process()
           }
           m_busycount--;
           //no busy indicator if a progress dialog is showing
-          if (m_gWindowManager.GetTopMostModalDialogID() == WINDOW_DIALOG_PROGRESS)
+          if (m_gWindowManager.HasModalDialog() || (m_gWindowManager.GetTopMostModalDialogID() == WINDOW_DIALOG_PROGRESS))
           {
             //TODO: render progress dialog here instead of in dialog::Progress
+            m_time = timeGetTime();
             Sleep(1);
             continue;
-          }
-          if (!m_pWindow || iWidth == 0 || iHeight == 0)
-          {
-            m_pWindow = (CGUIDialogBusy*)m_gWindowManager.GetWindow(WINDOW_DIALOG_BUSY);
-            if (m_pWindow)
-            {
-              m_pWindow->Initialize();//need to load the window to determine size.
-              if (m_pWindow->GetID() == WINDOW_INVALID)
-              {
-                //busywindow couldn't be loaded so stop this thread.
-                m_pWindow = NULL;
-                m_bStop = true;
-                break;
-              }
-
-              SAFE_RELEASE(m_lpSurface);
-              FRECT rect = m_pWindow->GetScaledBounds();
-
-              iLeft = (int)floor(rect.left);
-              iTop =  (int)floor(rect.top);
-              iWidth = (int)ceil(rect.right - rect.left);
-              iHeight = (int)ceil(rect.bottom - rect.top);
-            }
           }
           if (!m_pWindow || iWidth == 0 || iHeight == 0)
           {
@@ -227,7 +230,6 @@ void CApplicationRenderer::Process()
         g_graphicsContext.Unlock();
         CLog::Log(LOGERROR, __FUNCTION__" - Exception caught when  busy rendering");
       }
-      m_time = timeGetTime();
     }
     Sleep(1);
   }
@@ -245,8 +247,8 @@ void CApplicationRenderer::UpdateBusyCount()
     m_prevbusycount = m_busycount;
     if (m_pWindow && m_busyShown)
     {
-      m_pWindow->Close();
       m_busyShown = false;
+      m_pWindow->Close();
     }
   }
 }
@@ -292,7 +294,7 @@ void CApplicationRenderer::Stop()
 
 bool CApplicationRenderer::IsBusy() const
 {
-  return (m_explicitbusy > 0) || m_prevbusycount > MAX_MISSEDFRAMES;
+  return ((m_explicitbusy > 0) || m_busyShown);
 }
 
 void CApplicationRenderer::SetBusy(bool bBusy)
