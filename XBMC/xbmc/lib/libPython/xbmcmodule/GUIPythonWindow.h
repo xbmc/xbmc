7f66da3c2659ab6c1429c227c07a886627ebfb4a
codelogic <codelogic@svn>
2007-06-04 01:21:13 +0000

    Python programs can now receive all XBMC events.
    added:
    	in GUIPythonWindow.h:
        	  Py_AddPendingActionCall()
    	  Py_MakePendingActionCalls()
    
    	Custom implementation of Py_AddPendingCall() and Py_MakePendingCalls()
    	since they don't seem to fire off any events under linux.
    
    	
    
    
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@9209 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libPython/xbmcmodule/GUIPythonWindow.h
+++ XBMC/xbmc/lib/libPython/xbmcmodule/GUIPythonWindow.h
@@ -13,8 +13,15 @@ public:
   PyObject* pCallbackWindow;
   PyObject* pObject;
   int controlId; // for XML window
+#ifdef _LINUX
+  int type; // 0=Action, 1=Control;
+#endif
 };
 
+#ifdef _LINUX
+void Py_AddPendingActionCall(PyXBMCAction* inf);
+void Py_MakePendingActionCalls();
+#endif
 int Py_XBMC_Event_OnAction(void* arg);
 int Py_XBMC_Event_OnControl(void* arg);
 
