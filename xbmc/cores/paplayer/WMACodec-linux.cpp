d89c44df501c788f444d3b51f5304f8eaaee0eb7
yuvalt <yuvalt@svn>
2007-06-06 20:56:36 +0000

    add: WMA codec for linuxport
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@9238 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/paplayer/WMACodec-linux.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include "WMACodec.h"
+#include "../../Util.h"
+
+WMACodec::WMACodec()
+{
+  m_CodecName = "WMA";
+  m_iDataPos = -1; 
+  m_hnd = NULL;
+}
+
+WMACodec::~WMACodec()
+{
+  DeInit();
+}
+
+bool WMACodec::Init(const CStdString &strFile, unsigned int filecache)
+{
+  if (!m_dll.Load())
+    return false; // error logged previously
+  
+  m_dll.Init();
+
+  m_hnd = m_dll.LoadFile(strFile.c_str(), &m_TotalTime, &m_SampleRate, &m_BitsPerSample, &m_Channels);
+  if (m_hnd == 0)
+    return false;
+    
+  return true;
+}
+
+void WMACodec::DeInit()
+{
+  if (m_hnd != NULL)
+  {
+    m_dll.UnloadFile(m_hnd);
+    m_hnd = NULL;
+  }
+}
+
+__int64 WMACodec::Seek(__int64 iSeekTime)
+{
+  __int64 result = (__int64)m_dll.Seek(m_hnd, (unsigned long)iSeekTime);
+  m_iDataPos = result/1000*m_SampleRate*m_BitsPerSample*m_Channels/8;
+  
+  return result;
+}
+
+int WMACodec::ReadPCM(BYTE *pBuffer, int size, int *actualsize)
+{
+  if (m_iDataPos == -1)
+  {
+    m_iDataPos = 0;
+  }
+
+  if (m_iDataPos >= m_TotalTime/1000*m_SampleRate*m_BitsPerSample*m_Channels/8)
+  {
+    return READ_EOF;
+  }
+
+  if ((*actualsize=m_dll.FillBuffer(m_hnd, (char*)pBuffer,size))> 0)
+  {
+    m_iDataPos += *actualsize;
+    return READ_SUCCESS;
+  }
+
+  return READ_ERROR;
+}
+
+bool WMACodec::CanInit()
+{
+  return m_dll.CanLoad();
+}
+
+
