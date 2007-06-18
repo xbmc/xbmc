8b288b2de1debc075964445296730b806d60f6c8
jmarshallnz <jmarshallnz@svn>
2007-06-18 23:52:22 +0000

    fixed: [ 1735777 ] Cue file parsing error (possibly due to charset issues?)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9342 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/CueDocument.cpp
+++ xbmc/CueDocument.cpp
@@ -92,7 +92,10 @@ bool CCueDocument::Parse(const CStdString &strFile)
         CStdString titleNoQuote = strLine.Mid(5);
         titleNoQuote.TrimLeft();
         if (!titleNoQuote.IsEmpty())
+        {
+          g_charsetConverter.stringCharsetToUtf8(titleNoQuote);
           m_Track[m_iTotalTracks].strTitle = titleNoQuote;
+        }
       }
     }
     else if (strLine.Left(9) == "PERFORMER")
@@ -239,6 +242,7 @@ bool CCueDocument::ExtractQuoteInfo(const CStdString &line, CStdString &quote)
   int right = line.Find('\"', left + 1);
   if (right < 0) return false;
   quote = line.Mid(left + 1, right - left - 1);
+  g_charsetConverter.stringCharsetToUtf8(quote);
   return true;
 }
 
