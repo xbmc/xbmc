325e45f6d11b5939f66f8d36249a7114ff538fc9
d4rkm4ster <d4rkm4ster@svn>
2007-06-27 22:27:06 +0000

    added:        HAS_FILESYSTEM_DAAP and HAS_FILESYSTEM_RTV to enable libdaap and rtv support. (both currently disabled)        NSF and SID directory support (compiled in, but currently untested).
    modified:
    	Assertion in StdCodeCvt in StdString.h to print out assertion failed messages
    	instead of dropping to debugger (linux only).
    
    fixed:
    	Crash on certain CD/DVD label reads.
    	
    
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@9455 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FactoryFileDirectory.cpp
+++ xbmc/FileSystem/FactoryFileDirectory.cpp
@@ -4,10 +4,8 @@
 #include "FactoryFileDirectory.h"
 #ifdef HAS_FILESYSTEM
 #include "OGGFileDirectory.h"
-#ifndef _LINUX
 #include "NSFFileDirectory.h"
 #include "SIDFileDirectory.h"
-#endif
 #endif
 #include "RarDirectory.h"
 #include "ZipDirectory.h"
@@ -35,7 +33,6 @@ IFileDirectory* CFactoryFileDirectory::Create(const CStdString& strPath, CFileIt
   strExtension.MakeLower();
 
 #ifdef HAS_FILESYSTEM
-#ifdef _LINUX
   if (strExtension.Equals(".ogg") && CFile::Exists(strPath))
   {
     IFileDirectory* pDir=new COGGFileDirectory;
@@ -48,7 +45,6 @@ IFileDirectory* CFactoryFileDirectory::Create(const CStdString& strPath, CFileIt
     delete pDir;
     return NULL;
   }
-#else
   if (strExtension.Equals(".nsf") && CFile::Exists(strPath))
   {
     IFileDirectory* pDir=new CNSFFileDirectory;
@@ -69,7 +65,6 @@ IFileDirectory* CFactoryFileDirectory::Create(const CStdString& strPath, CFileIt
     delete pDir;
     return NULL;
   }
-#endif
 #endif
   if (strExtension.Equals(".zip"))
   {
