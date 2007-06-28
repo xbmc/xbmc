94223c62fda4bc44bb8306436aa5dc7092941f88
elupus <elupus@svn>
2007-06-28 18:46:52 +0000

    added: allow dvdplayer to use ffmpegs built-in stream handlers for rtp/rtsp/sdp/udp streams. it's abit unreliable at the moment on xbox atleast as it might lockup xbmc if there is nothing to stream. (this requires recompiled ffmpeg dll's, with network and protocols enabled).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9468 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamFFmpeg.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "DVDInputStream.h"
+#include "../DVDDemuxers/DllAvFormat.h"
+
+class CDVDInputStreamFFmpeg : public CDVDInputStream
+{
+public:
+  CDVDInputStreamFFmpeg();
+  virtual ~CDVDInputStreamFFmpeg();
+  virtual bool Open(const char* strFile, const std::string &content);
+  virtual void Close();
+  virtual int Read(BYTE* buf, int buf_size);
+  virtual __int64 Seek(__int64 offset, int whence);
+  virtual bool IsEOF();
+  virtual __int64 GetLength();
+  
+protected:
+};
