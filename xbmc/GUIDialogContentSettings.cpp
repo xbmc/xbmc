bd3174f14533b5132d66426f5bcd7528b1af7554
spiff_ <spiff_@svn>
2007-07-09 14:44:26 +0000

    fixed: issues with video scanner folder exclude unassignment.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9527 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIDialogContentSettings.cpp
+++ xbmc/GUIDialogContentSettings.cpp
@@ -186,7 +186,7 @@ void CGUIDialogContentSettings::SetupPage()
   }
   SET_CONTROL_VISIBLE(CONTROL_CONTENT_TYPE);
   // now add them scrapers to the list control
-  if (m_info.strContent.IsEmpty())
+  if (m_info.strContent.IsEmpty() || m_info.strContent.Equals("None"))
   {
     CGUIMessage msgReset(GUI_MSG_LABEL_RESET, GetID(), CONTROL_SCRAPER_LIST);
     OnMessage(msgReset); 
@@ -209,8 +209,6 @@ void CGUIDialogContentSettings::CreateSettings()
   if (m_info.strContent.IsEmpty() || m_info.strContent.Equals("None"))
   {
     AddBool(1,20380,&m_bRunScan);
-    if (m_info.strContent.Equals("None"))
-      m_bRunScan = true;
   }
 
   if (m_info.strContent.Equals("movies"))
@@ -332,6 +330,9 @@ bool CGUIDialogContentSettings::Show(SScraperInfo& scraper, bool& bRunScan, bool
 
   dialog->m_info = scraper;
   dialog->m_bRunScan = bRunScan;
+  if (scraper.strContent.Equals("None"))
+    dialog->m_bRunScan = true;
+
   dialog->m_bScanRecursive = bScanRecursive;
   dialog->m_bUseDirNames = bUseDirNames;
   dialog->m_bNeedSave = false;
@@ -341,6 +342,10 @@ bool CGUIDialogContentSettings::Show(SScraperInfo& scraper, bool& bRunScan, bool
     scraper = dialog->m_info;
     bScanRecursive = dialog->m_bScanRecursive;
     bUseDirNames = dialog->m_bUseDirNames;
+    if (scraper.strContent.Equals("None") && !dialog->m_bRunScan)
+    {
+      scraper.strContent = "";
+    }
     if (scraper.strContent.IsEmpty() && dialog->m_bRunScan)
     {
       scraper.strContent = "None";
