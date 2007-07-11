6dffa9d759b2e4982743f083a2f4d46980f890fe
spiff_ <spiff_@svn>
2007-07-11 14:28:57 +0000

    added: support for specials in video library. use season number 0.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9541 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoDatabase.h
+++ xbmc/VideoDatabase.h
@@ -138,6 +138,8 @@ typedef enum // this enum MUST match the offset struct further down!! and make s
   VIDEODB_ID_EPISODE_SEASON = 12,
   VIDEODB_ID_EPISODE_EPISODE = 13,
   VIDEODB_ID_EPISODE_ORIGINALTITLE = 14,
+  VIDEODB_ID_EPISODE_DISPLAYSEASON = 15,
+  VIDEODB_ID_EPISODE_DISPLAYEPISODE = 16,
   VIDEODB_ID_EPISODE_MAX
 } VIDEODB_EPISODE_IDS;
 
@@ -158,6 +160,8 @@ const struct SDbTableOffsets DbEpisodeOffsets[] =
   { VIDEODB_TYPE_INT, offsetof(CVideoInfoTag,m_iSeason) },
   { VIDEODB_TYPE_INT, offsetof(CVideoInfoTag,m_iEpisode) },
   { VIDEODB_TYPE_STRING, offsetof(CVideoInfoTag,m_strOriginalTitle)},
+  { VIDEODB_TYPE_INT, offsetof(CVideoInfoTag,m_iDisplaySeason) },
+  { VIDEODB_TYPE_INT, offsetof(CVideoInfoTag,m_iDisplayEpisode) },
 };
 
 class CBookmark
