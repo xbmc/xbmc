727490b3bbc6af934da6402cf2763712b01965f5
elupus <elupus@svn>
2007-07-12 18:38:28 +0000

    added: support for foo.[12x01* for tvshows
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9553 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Settings.cpp
+++ xbmc/Settings.cpp
@@ -247,7 +247,7 @@ CSettings::CSettings(void)
   // foo_[s01]_[e01]
   g_advancedSettings.m_tvshowStackRegExps.push_back("\\[[Ss]([0-9]+)\\]_\\[[Ee]([0-9]+)[^\\\\/]*"); 
   // foo.1x09*
-  g_advancedSettings.m_tvshowStackRegExps.push_back("[\\._ -]([0-9]+)x([0-9]+)[^\\\\/]*");
+  g_advancedSettings.m_tvshowStackRegExps.push_back("[\\._ -\\[]([0-9]+)x([0-9]+)[^\\\\/]*");
   // foo.s01.e01, foo.s01_e01
   g_advancedSettings.m_tvshowStackRegExps.push_back("[\\._ -][Ss]([0-9]+)[\\.-]?[Ee]([0-9]+)[^\\\\/]*"); 
   // foo.103*
