af0498e626b7b2f0e2227de8b7a18c56fcc1d5fd
elupus <elupus@svn>
2007-07-15 00:12:56 +0000

    cosmetic: weird indent
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9569 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecLiba52.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecLiba52.cpp
@@ -92,27 +92,27 @@ void CDVDAudioCodecLiba52::SetupChannels()
   {
     switch (m_iSourceFlags&~A52_LFE) {
       case A52_MONO   : m_iOutputMapping = 0x12ffff; break;
-	  case A52_CHANNEL:
-	  case A52_STEREO :
-	  case A52_DOLBY  : m_iOutputMapping = 0x1fff32; break;
+      case A52_CHANNEL:
+      case A52_STEREO :
+      case A52_DOLBY  : m_iOutputMapping = 0x1fff32; break;
       case A52_2F1R   : m_iOutputMapping = 0x1f5542; break;
-	  case A52_2F2R   : m_iOutputMapping = 0x1f5432; break;
-	  case A52_3F     : m_iOutputMapping = 0x13ff42; break;
+      case A52_2F2R   : m_iOutputMapping = 0x1f5432; break;
+      case A52_3F     : m_iOutputMapping = 0x13ff42; break;
       case A52_3F2R   : m_iOutputMapping = 0x136542; break;
-	}
+    }
   }
   else
   {
     switch (m_iSourceFlags) {
-	  case A52_MONO   : m_iOutputMapping =     0x1; break;
-	  case A52_CHANNEL:
-	  case A52_STEREO :
-	  case A52_DOLBY  : m_iOutputMapping =    0x21; break;
+      case A52_MONO   : m_iOutputMapping =     0x1; break;
+      case A52_CHANNEL:
+      case A52_STEREO :
+      case A52_DOLBY  : m_iOutputMapping =    0x21; break;
       case A52_2F1R   : m_iOutputMapping =  0x2231; break;
-	  case A52_2F2R   : m_iOutputMapping =  0x4321; break;
-	  case A52_3F     : m_iOutputMapping = 0x2ff31; break;
+      case A52_2F2R   : m_iOutputMapping =  0x4321; break;
+      case A52_3F     : m_iOutputMapping = 0x2ff31; break;
       case A52_3F2R   : m_iOutputMapping = 0x25431; break;
-	}
+    }
   }
 
   int channels = 0;
