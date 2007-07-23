41ddd4e0335846359b0803899fd2904c8f1a6864
jmarshallnz <jmarshallnz@svn>
2007-07-23 09:29:33 +0000

    fixed: Mouse cursor didn't respect control animations when determining hover and click focus.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9671 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIControlGroup.cpp
+++ guilib/GUIControlGroup.cpp
@@ -283,10 +283,12 @@ bool CGUIControlGroup::HitTest(const CPoint &point) const
 bool CGUIControlGroup::CanFocusFromPoint(const CPoint &point, CGUIControl **control, CPoint &controlPoint) const
 {
   if (!CGUIControl::CanFocus()) return false;
+  CPoint controlCoords(point);
+  m_transform.InverseTransformPosition(controlCoords.x, controlCoords.y);
   for (crControls it = m_children.rbegin(); it != m_children.rend(); ++it)
   {
     CGUIControl *child = *it;
-    if (child->CanFocusFromPoint(point - CPoint(m_posX, m_posY), control, controlPoint))
+    if (child->CanFocusFromPoint(controlCoords - CPoint(m_posX, m_posY), control, controlPoint))
       return true;
   }
   *control = NULL;
@@ -295,10 +297,12 @@ bool CGUIControlGroup::CanFocusFromPoint(const CPoint &point, CGUIControl **cont
 
 void CGUIControlGroup::UnfocusFromPoint(const CPoint &point)
 {
+  CPoint controlCoords(point);
+  m_transform.InverseTransformPosition(controlCoords.x, controlCoords.y);
   for (iControls it = m_children.begin(); it != m_children.end(); ++it)
   {
     CGUIControl *child = *it;
-    child->UnfocusFromPoint(point - CPoint(m_posX, m_posY));
+    child->UnfocusFromPoint(controlCoords - CPoint(m_posX, m_posY));
   }
   CGUIControl::UnfocusFromPoint(point);
 }
