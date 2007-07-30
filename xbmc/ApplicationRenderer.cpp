2898f6068178d9644f95e27074517ed1b30dcb4f
charlydoes <charlydoes@svn>
2007-07-30 19:25:22 +0000

    fixed: incorrect busydialog display while resolution changing (video <- gui>).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9737 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ApplicationRenderer.cpp
+++ xbmc/ApplicationRenderer.cpp
@@ -45,6 +45,7 @@ void CApplicationRenderer::OnStartup()
   m_prevbusycount = 0;
   m_lpSurface = NULL;
   m_pWindow = NULL;
+  m_Resolution = g_graphicsContext.GetVideoResolution();
 }
 
 void CApplicationRenderer::OnExit()
@@ -72,7 +73,7 @@ void CApplicationRenderer::Process()
       continue;
     }
 
-    if (!m_pWindow || iWidth == 0 || iHeight == 0)
+    if (!m_pWindow || iWidth == 0 || iHeight == 0 || m_Resolution != g_graphicsContext.GetVideoResolution())
     {
       m_pWindow = (CGUIDialogBusy*)m_gWindowManager.GetWindow(WINDOW_DIALOG_BUSY);
       if (m_pWindow)
@@ -94,6 +95,7 @@ void CApplicationRenderer::Process()
         iTop =  (int)floor(rect.top);
         iWidth = (int)ceil(rect.right - rect.left);
         iHeight = (int)ceil(rect.bottom - rect.top);
+        m_Resolution = g_graphicsContext.GetVideoResolution();
       }
     }
 
@@ -117,12 +119,17 @@ void CApplicationRenderer::Process()
             Sleep(1000);
             continue;
           }
+          if (m_Resolution != g_graphicsContext.GetVideoResolution())
+          {
+            continue;
+          }
           if (m_busycount > 0) m_busycount--;
           //no busy indicator if a progress dialog is showing
           if (m_gWindowManager.HasModalDialog() || (m_gWindowManager.GetTopMostModalDialogID() == WINDOW_DIALOG_PROGRESS))
           {
             //TODO: render progress dialog here instead of in dialog::Progress
             m_time = timeGetTime();
+            lockg.Leave();
             Sleep(1);
             continue;
           }
@@ -149,22 +156,25 @@ void CApplicationRenderer::Process()
             else
 #endif
             {
+              lockg.Leave();
               Sleep(1000);
               continue;
             }
             if (!SUCCEEDED(g_graphicsContext.Get3DDevice()->CreateImageSurface(iWidth, iHeight, desc.Format, &m_lpSurface)))
             {
               SAFE_RELEASE(lpSurfaceFront);
+              lockg.Leave();
               Sleep(1000);
               continue;
             }
             //copy part underneeth busy dialog
             const RECT rc = { iLeft, iTop, iLeft + iWidth, iTop + iHeight  };
-            const POINT ptDest = { 0, 0 };
-            if (!SUCCEEDED(g_graphicsContext.Get3DDevice()->CopyRects(lpSurfaceFront, &rc, 1, m_lpSurface, &ptDest)))
+            const RECT rcDest = { 0, 0, iWidth, iHeight  };
+            if (!CopySurface(lpSurfaceFront, &rc, m_lpSurface, &rcDest))
             {
                 SAFE_RELEASE(lpSurfaceFront);
                 SAFE_RELEASE(m_lpSurface);
+                lockg.Leave();
                 Sleep(1000);
                 continue;
             }
@@ -176,14 +186,16 @@ void CApplicationRenderer::Process()
               {
                   SAFE_RELEASE(lpSurfaceFront);
                   SAFE_RELEASE(m_lpSurface);
+                  lockg.Leave();
                   Sleep(1000);
                   continue;
               }
-              if (!SUCCEEDED(g_graphicsContext.Get3DDevice()->CopyRects(lpSurfaceFront, NULL, 0, lpSurfaceBack, NULL)))
+              if (!CopySurface(lpSurfaceFront, NULL, lpSurfaceBack, NULL))
               {
                   SAFE_RELEASE(lpSurfaceFront);
                   SAFE_RELEASE(lpSurfaceBack);
                   SAFE_RELEASE(m_lpSurface);
+                  lockg.Leave();
                   Sleep(1000);
                   continue;
               }
@@ -193,19 +205,21 @@ void CApplicationRenderer::Process()
           }
           if (!SUCCEEDED(g_graphicsContext.Get3DDevice()->GetBackBuffer( 0, D3DBACKBUFFER_TYPE_MONO, &lpSurfaceBack)))
           {
+              lockg.Leave();
               Sleep(1000);
               continue;
           }
           g_graphicsContext.Get3DDevice()->BeginScene();
           //copy dialog background to backbuffer
           const RECT rc = { 0, 0, iWidth, iHeight };
-          const POINT ptDest = { iLeft, iTop };
+          const RECT rcDest = { iLeft, iTop, iLeft + iWidth, iTop + iHeight };
           const D3DRECT rc2 = { iLeft, iTop, iLeft + iWidth, iTop + iHeight };
           g_graphicsContext.Get3DDevice()->Clear(1, &rc2, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER | D3DCLEAR_STENCIL, 0x00010001, 1.0f, 0L);
-          if (!SUCCEEDED(g_graphicsContext.Get3DDevice()->CopyRects(m_lpSurface, &rc, 1, lpSurfaceBack, &ptDest)))
+          if (!CopySurface(m_lpSurface, &rc, lpSurfaceBack, &rcDest))
           {
               SAFE_RELEASE(lpSurfaceBack);
               g_graphicsContext.Get3DDevice()->EndScene();
+              lockg.Leave();
               Sleep(1000);
               continue;
           }
@@ -226,14 +240,35 @@ void CApplicationRenderer::Process()
       }
       catch (...)
       {
+        CLog::Log(LOGERROR, __FUNCTION__" - Exception caught when  busy rendering");
         SAFE_RELEASE(lpSurfaceFront);
         SAFE_RELEASE(lpSurfaceBack);
         SAFE_RELEASE(m_lpSurface);
-        g_graphicsContext.Unlock();
-        CLog::Log(LOGERROR, __FUNCTION__" - Exception caught when  busy rendering");
       }
     }
     Sleep(1);
+  }
+}
+
+bool CApplicationRenderer::CopySurface(D3DSurface* pSurfaceSource, const RECT* rcSource, D3DSurface* pSurfaceDest, const RECT* rcDest)
+{
+  if (m_Resolution == HDTV_1080i)
+  {
+    //CopRects doesn't work at all in 1080i, D3DXLoadSurfaceFromSurface does but is ridiculously slow...
+    return SUCCEEDED(D3DXLoadSurfaceFromSurface(pSurfaceDest, NULL, rcDest, pSurfaceSource, NULL, rcSource, D3DX_FILTER_NONE, 0));
+  }
+  else
+  {
+    if (rcDest)
+    {
+      const POINT ptDest = { rcDest->left, rcDest->top };
+      return SUCCEEDED(g_graphicsContext.Get3DDevice()->CopyRects(pSurfaceSource, rcSource, 1, pSurfaceDest, &ptDest));
+    }
+    else
+    {
+      const POINT ptDest = { 0, 0 };
+      return SUCCEEDED(g_graphicsContext.Get3DDevice()->CopyRects(pSurfaceSource, rcSource, 1, pSurfaceDest, &ptDest));
+    }
   }
 }
 
