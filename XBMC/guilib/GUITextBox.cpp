36e3c6b8e3f2987d27bf5c5cfdcd13a63fd11c0c
jmarshallnz <jmarshallnz@svn>
2007-07-31 03:18:09 +0000

    added: Support for $INFO[] in <texture> tags for images.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@9745 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/GUITextBox.cpp
+++ XBMC/guilib/GUITextBox.cpp
@@ -39,7 +39,7 @@ void CGUITextBox::Render()
 	if (m_singleInfo)
 		renderLabel = g_infoManager.GetLabel(m_singleInfo);
 	else
-    renderLabel = g_infoManager.GetMultiLabel(m_multiInfo);
+    renderLabel = g_infoManager.GetMultiInfo(m_multiInfo, m_dwParentID);
 
   // need to check the last one
   if (m_renderLabel != renderLabel)
