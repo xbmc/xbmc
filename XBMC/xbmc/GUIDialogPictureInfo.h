1ff6efb70346f3e383404e53e72cb326f5ee0622
jmarshallnz <jmarshallnz@svn>
2007-08-03 10:31:45 +0000

    changed: Make picture info dynamic when in slideshow.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@9794 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIDialogPictureInfo.h
+++ XBMC/xbmc/GUIDialogPictureInfo.h
@@ -9,9 +9,13 @@ public:
   CGUIDialogPictureInfo(void);
   virtual ~CGUIDialogPictureInfo(void);
   void SetPicture(const CStdString &picture);
+  virtual void Render();
+
 protected:
   virtual void OnInitWindow();
   virtual void OnDeinitWindow(int nextWindowID);
+  void UpdatePictureInfo();
 
   CFileItemList m_pictureInfo;
+  CStdString    m_currentPicture;
 };
