099a7ba6445aa06dd3cb04a66cebca9d62f9411c
charlydoes <charlydoes@svn>
2007-08-10 19:26:45 +0000

    changed: rewrite of last.fm radio to the new playlist based radio protocol.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9882 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/paplayer.h
+++ xbmc/cores/paplayer/paplayer.h
@@ -29,6 +29,7 @@ public:
 
   virtual bool OpenFile(const CFileItem& file, const CPlayerOptions &options);
   virtual bool QueueNextFile(const CFileItem &file);
+  virtual void OnNothingToQueueNotify();
   virtual bool CloseFile()       { return CloseFileInternal(true); }
   virtual bool CloseFileInternal(bool bAudioDevice = true);
   virtual bool IsPlaying() const { return m_bIsPlaying; }
@@ -84,6 +85,7 @@ protected:
 
   bool m_bPaused;
   bool m_bIsPlaying;
+  bool m_bQueueFailed;
   bool m_bStopPlaying;
   bool m_cachingNextFile;
   int  m_crossFading;
