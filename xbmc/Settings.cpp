25bcf8af97d2abc41e8364ac9fd952c1941e48c5
spiff_ <spiff_@svn>
2007-08-12 23:00:53 +0000

    changed: renamed useevilb rootovershoot since apparently it wasnt clear enough what the setting did ;)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9919 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Settings.cpp
+++ xbmc/Settings.cpp
@@ -1206,7 +1206,7 @@ void CSettings::LoadAdvancedSettings()
   GetInteger(pRootElement, "songinfoduration", g_advancedSettings.m_songInfoDuration, 2, 1, 15);
   GetInteger(pRootElement, "busydialogdelay", g_advancedSettings.m_busyDialogDelay, 2000, 0, 5000);
 
-  XMLUtils::GetBoolean(pRootElement,"useevilb",g_advancedSettings.m_bUseEvilB);
+  XMLUtils::GetBoolean(pRootElement,"rootovershoot",g_advancedSettings.m_bUseEvilB);
 
   //Tuxbox
   pElement = pRootElement->FirstChildElement("tuxbox");
