4d7d5c4b2a3afd2ed5ff2db6cfd4bf0a7c0b1c86
elupus <elupus@svn>
2007-08-13 23:18:22 +0000

    fixed: MAX_STREAMS where wrong in XBMC_PC build for dvdplayer
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9933 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/ffmpeg/avformat.h
+++ xbmc/cores/dvdplayer/ffmpeg/avformat.h
@@ -329,12 +329,9 @@ typedef struct AVStream {
 #define AVFMTCTX_NOHEADER      0x0001 /**< signal that no header is present
                                          (streams are added dynamically) */
 
-#ifdef _XBOX
 /* dvd's can have maximally 41 streams */
+//#ifdef _XBOX needed for all builds
 #define MAX_STREAMS 42
-#else
-#define MAX_STREAMS 20
-#endif
 
 /* format I/O context */
 typedef struct AVFormatContext {
