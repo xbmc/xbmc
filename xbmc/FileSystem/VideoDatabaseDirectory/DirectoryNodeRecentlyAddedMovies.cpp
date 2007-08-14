b4841e04a868024c7e4476edac5d4d54e9ac1437
spiff_ <spiff_@svn>
2007-08-14 16:34:48 +0000

    added: recently added movies/episode nodes to the video library
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9939 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/VideoDatabaseDirectory/DirectoryNodeRecentlyAddedMovies.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "DirectoryNodeRecentlyAddedMovies.h"
+#include "QueryParams.h"
+#include "../../VideoDatabase.h"
+
+using namespace DIRECTORY::VIDEODATABASEDIRECTORY;
+
+CDirectoryNodeRecentlyAddedMovies::CDirectoryNodeRecentlyAddedMovies(const CStdString& strName, CDirectoryNode* pParent)
+  : CDirectoryNode(NODE_TYPE_RECENTLY_ADDED_MOVIES, strName, pParent)
+{
+
+}
+
+NODE_TYPE CDirectoryNodeRecentlyAddedMovies::GetChildType()
+{
+  return NODE_TYPE_TITLE_MOVIES;
+}
+
+bool CDirectoryNodeRecentlyAddedMovies::GetContent(CFileItemList& items)
+{
+  CVideoDatabase videodatabase;
+  if (!videodatabase.Open())
+    return false;
+
+  CStdString strBaseDir=BuildPath();
+  bool bSuccess=videodatabase.GetRecentlyAddedMoviesNav(strBaseDir, items);
+
+  videodatabase.Close();
+
+  return bSuccess;
+}
