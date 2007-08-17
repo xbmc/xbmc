7bf5246e35589aa1ccdb27715dce7e37e624b4a8
jmarshallnz <jmarshallnz@svn>
2007-08-17 23:27:05 +0000

    fixed: Wrap around listitem(offset).* wasn't working correctly in lists.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@9979 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIBaseContainer.cpp
+++ guilib/GUIBaseContainer.cpp
@@ -190,7 +190,9 @@ CGUIListItem *CGUIBaseContainer::GetListItem(int offset) const
 {
   if (!m_items.size())
     return NULL;
-  return m_items[(GetSelectedItem() + offset) % m_items.size()];
+  int item = (GetSelectedItem() + offset) % ((int)m_items.size());
+  if (item < 0) item += m_items.size();
+  return m_items[item];
 }
 
 bool CGUIBaseContainer::SelectItemFromPoint(const CPoint &point)
