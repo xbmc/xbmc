bfbbe5a6e434aa253e4ec6c3774eae8908599ec4
jmarshallnz <jmarshallnz@svn>
2007-08-20 04:48:03 +0000

    added: "Plugin directories" via python scripts. Allows scripters to return directory listings to incorporate their scripts within the media windows.  Plugins go in plugins/<media type>.  Add a source to the plugins the usual way (via Add Source).  Thanks to Nuka1195 and Umbehagen.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@10008 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/FileSystem/PluginDirectory.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "filesystem/idirectory.h"
+#include "fileitem.h"
+#include "settings.h"
+#include "filesystem/directory.h"
+#include <string>
+#include "stdstring.h"
+#include <vector>
+using namespace std;
+
+namespace DIRECTORY
+{
+
+class CPluginDirectory : public IDirectory
+{
+public:
+  CPluginDirectory(void);
+  ~CPluginDirectory(void);
+  virtual bool GetDirectory(const CStdString& strPath, CFileItemList& items);
+  static bool HasPlugins(const CStdString &type);
+  bool GetPluginsDirectory(const CStdString &type, CFileItemList &items);
+
+  // callbacks from python
+  static bool AddItem(int handle, const CFileItem *item);
+  static void EndOfDirectory(int handle, bool success);
+  static void AddSortMethod(int handle, SORT_METHOD sortMethod);
+
+private:
+  bool WaitOnScriptResult(const CStdString &scriptPath, const CStdString &scriptName);
+
+  static vector<CPluginDirectory*> globalHandles;
+  static int getNewHandle(CPluginDirectory *cp);
+  static void removeHandle(int handle);
+
+  CFileItemList m_listItems;
+  HANDLE        m_directoryFetched;
+
+  bool          m_cancelled;    // set to true when we are cancelled
+  bool          m_success;      // set by script in EndOfDirectory
+};
+};
