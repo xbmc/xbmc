ec932e87deb7fe3affada99f6a5046b39ce9e069
d4rkm4ster <d4rkm4ster@svn>
2007-08-21 07:24:48 +0000

    added: Framework classes for GLSL shaders. added: Initial multipass rendering support. added: FBO (framebuffer objects) support for rendering to textures for multipass rendering. (GL 1.5+)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10019 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ guilib/FrameBufferObject.cpp
@@ -0,0 +1,124 @@
+/*
+* XBoxMediaCenter
+* Frame Buffer Object
+* Copyright (c) 2007 d4rk
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "stdafx.h"
+#include <GL/glew.h>
+#include "../xbmc/Settings.h"
+#include "FrameBufferObject.h"
+
+#ifdef HAS_SDL_OPENGL
+
+//////////////////////////////////////////////////////////////////////
+// CFrameBufferObject
+//////////////////////////////////////////////////////////////////////
+
+CFrameBufferObject::CFrameBufferObject()
+{
+  m_fbo = 0;
+  m_valid = false;
+  m_supported = false;
+  m_bound = false;
+  m_texid = 0;
+}
+
+bool CFrameBufferObject::IsSupported()
+{
+  if (glewIsSupported("GL_EXT_framebuffer_object"))
+    m_supported = true;
+  else
+    m_supported = false;
+  return m_supported;
+}
+
+bool CFrameBufferObject::Initialize()
+{
+  if (!IsSupported())
+    return false;
+  
+  Cleanup();
+
+  glGenFramebuffersEXT(1, &m_fbo);
+  VerifyGLState();
+
+  if (!m_fbo)
+    return false;
+
+  m_valid = true;
+  return true;
+}
+
+void CFrameBufferObject::Cleanup()
+{
+  if (!IsValid())
+    return;
+
+  if (m_fbo)
+    glDeleteFramebuffersEXT(1, &m_fbo);
+
+  if (m_texid)
+    glDeleteTextures(1, &m_texid);
+
+  m_texid = 0;
+  m_fbo = 0;
+  m_valid = false;
+  m_bound = false;
+}
+
+bool CFrameBufferObject::CreateAndBindToTexture(GLenum target, int width, int height, GLenum format)
+{
+  if (!IsValid())
+    return false;
+
+  if (m_texid)
+    glDeleteTextures(1, &m_texid);
+  
+  glGenTextures(1, &m_texid);
+  glBindTexture(target, m_texid);
+  glTexImage2D(target, 0, format,  width, height, 0, format, GL_UNSIGNED_BYTE, NULL);
+  glTexParameterf(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameterf(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  VerifyGLState();
+  return BindToTexture(target, m_texid);
+}
+
+bool CFrameBufferObject::BindToTexture(GLenum target, GLuint texid)
+{
+  if (!IsValid())
+    return false;
+
+  m_bound = false;
+  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, m_fbo);
+  glBindTexture(target, texid);
+  glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, target, texid, 0);
+  VerifyGLState();
+  GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
+  if (status != GL_FRAMEBUFFER_COMPLETE_EXT)
+  {
+    VerifyGLState();
+    return false;
+  }
+  m_bound = true;
+  return true;
+}
+
+#endif
