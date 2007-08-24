7f80bdde48fd5e7ca64c0557902cfb035170a5a2
elupus <elupus@svn>
2007-08-24 22:25:41 +0000

    changed: don't break out of libmpeg2 decoder if the new sequence of pictures has same dimensions as last, just continue decoding.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@10072 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- mplayer/libmpcodecs/vd_libmpeg2.c
+++ mplayer/libmpcodecs/vd_libmpeg2.c
@@ -191,7 +191,7 @@ static mp_image_t* decode(sh_video_t *sh,void* data,int len,int flags){
 		&& context->picture_height == info->sequence->picture_height
 		&& context->aspect == sh->aspect
 		&& context->fmt == IMGFMT_YV12)
-			break;
+			continue;
 #endif
 
 		if(!mpcodecs_config_vo(sh,
@@ -212,7 +212,7 @@ static mp_image_t* decode(sh_video_t *sh,void* data,int len,int flags){
 		&& context->picture_height == info->sequence->picture_height
 		&& context->aspect == sh->aspect
 		&& context->fmt == IMGFMT_422P)
-			break;
+			continue;
 #endif
 
 		if(!mpcodecs_config_vo(sh,
