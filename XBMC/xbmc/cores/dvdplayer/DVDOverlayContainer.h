a3a1f724f10980f7f36ab6ae8e109e1949f4ca17
elupus <elupus@svn>
2007-08-25 16:00:49 +0000

    changed: reorganized subtitle/overlay handling in dvdplayer fixed: dvdplayer would display overlays with the wrong color for a few seconds if spuclut changed on the cell change.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@10082 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/DVDOverlayContainer.h
+++ XBMC/xbmc/cores/dvdplayer/DVDOverlayContainer.h
@@ -3,6 +3,9 @@
 
 #include "DVDCodecs/Overlay/DVDOverlay.h"
 
+class CDVDInputStreamNavigator;
+class CDVDDemuxSPU;
+
 class CDVDOverlayContainer
 {
 public:
@@ -13,20 +16,21 @@ public:
   void Unlock() { LeaveCriticalSection(&m_critSection); }
 
   void Add(CDVDOverlay* pPicture); // add a overlay to the fifo
-  
+
   VecOverlays* GetOverlays(); // get the first overlay in this fifo
   bool ContainsOverlayType(DVDOverlayType type);
-  
+
   void Remove(); // remove the first overlay in this fifo
-  
+
   void Clear(); // clear the fifo and delete all overlays
   void CleanUp(__int64 pts); // validates all overlays against current pts
   int GetSize();
 
+  void UpdateOverlayInfo(CDVDInputStreamNavigator* pStream, CDVDDemuxSPU *pSpu, int iAction);
 private:
   CDVDOverlay* Remove(CDVDOverlay* pOverlay); // removes a specific overlay
-  
+
   VecOverlays m_overlays;
-  
+
   CRITICAL_SECTION m_critSection;
 };
