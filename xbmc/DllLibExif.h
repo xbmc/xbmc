448b357a5ecb9be36fe29c4f6b055937d5b4232f
vulkanr <vulkanr@svn>
2007-08-30 15:18:41 +0000

    fixed: linux path to shared object file
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10119 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/DllLibExif.h
+++ xbmc/DllLibExif.h
@@ -10,7 +10,11 @@ public:
 
 class DllLibExif : public DllDynamic, DllLibExifInterface
 {
+#ifndef _LINUX
   DECLARE_DLL_WRAPPER(DllLibExif, Q:\\system\\libexif.dll)
+#else
+  DECLARE_DLL_WRAPPER(DllLibExif, Q:\\system\\libexif-i486-linux.so)
+#endif
   DEFINE_METHOD3(bool, process_jpeg, (const char *p1, ExifInfo_t *p2, IPTCInfo_t *p3))
   BEGIN_METHOD_RESOLVE()
     RESOLVE_METHOD(process_jpeg)
