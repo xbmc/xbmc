6782f8ab0ebfd8d214df6dfe4bd44d25dc28fb40
elupus <elupus@svn>
2007-08-31 01:38:22 +0000

    changed: simplify include of rendermanager, handle difference in RenderManager.h instead
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10129 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIDialogVideoSettings.cpp
+++ xbmc/GUIDialogVideoSettings.cpp
@@ -25,11 +25,7 @@
 #include "util.h"
 #include "application.h"
 #ifdef HAS_VIDEO_PLAYBACK
-#ifdef HAS_XBOX_HARDWARE
 #include "cores/VideoRenderers/RenderManager.h"
-#else
-#include "cores/VideoRenderers/WinRenderManager.h"
-#endif
 #endif
 #include "VideoDatabase.h"
 
