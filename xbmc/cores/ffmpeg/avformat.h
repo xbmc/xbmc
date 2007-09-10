0102785c0aacf0edf1ae758649adbe3ab834a751
vulkanr <vulkanr@svn>
2007-09-10 10:23:39 +0000

    upgrade ffmpeg libraries to latest svn (rev. 10469).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10180 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/ffmpeg/avformat.h
+++ xbmc/cores/ffmpeg/avformat.h
@@ -348,8 +348,6 @@ typedef struct AVStream {
 #define AVFMTCTX_NOHEADER      0x0001 /**< signal that no header is present
                                          (streams are added dynamically) */
 
-// XBMC
-/* dvd's can have maximally 41 streams */
 #define MAX_STREAMS 42
 
 /* format I/O context */
