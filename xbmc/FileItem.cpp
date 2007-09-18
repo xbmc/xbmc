31f4bb3e7725008264210b3be9bd6b56e1ab40bb
charlydoes <charlydoes@svn>
2007-09-18 17:16:46 +0000

    fixed: lastfm radio didn't play because xbmc tried to get the contenttype of the track before playing it which invalidated the track on the lastfm servers.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10274 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -991,8 +991,7 @@ const CStdString& CFileItem::GetContentType() const
     else if( m_strPath.Left(8).Equals("shout://")
           || m_strPath.Left(7).Equals("http://")
           || m_strPath.Left(8).Equals("https://")
-          || m_strPath.Left(7).Equals("upnp://")
-          || m_strPath.Left(9).Equals("lastfm://"))
+          || m_strPath.Left(7).Equals("upnp://"))
     {
       CFileCurl::GetContent(GetAsUrl(), m_ref);
 
