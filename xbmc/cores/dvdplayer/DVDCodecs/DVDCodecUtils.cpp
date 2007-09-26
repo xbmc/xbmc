2f2226eac2493d9b4be310e8b74fee8f15641b0d
d4rkm4ster <d4rkm4ster@svn>
2007-09-26 07:04:47 +0000

    update: memcpy optimizations for CDVDCodecUtils::CopyPictureToOverlay()
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10354 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/DVDCodecUtils.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDCodecUtils.cpp
@@ -65,29 +65,50 @@ bool CDVDCodecUtils::CopyPictureToOverlay(YV12Image* pImage, DVDVideoPicture *pS
   BYTE *d = pImage->plane[0];
   int w = pSrc->iWidth;
   int h = pSrc->iHeight;
-  for (int y = 0; y < h; y++)
+  if ((w==pSrc->iLineSize[0]) && (pSrc->iLineSize[0]==pImage->stride[0]))
   {
-    memcpy(d, s, w);
-    s += pSrc->iLineSize[0];
-    d += pImage->stride[0];
+    memcpy(d, s, w*h);
+  }
+  else
+  {
+    for (int y = 0; y < h; y++)
+    {
+      memcpy(d, s, w);
+      s += pSrc->iLineSize[0];
+      d += pImage->stride[0];
+    }
   }
   s = pSrc->data[1];
   d = pImage->plane[1];
   w = pSrc->iWidth >> 1;
   h = pSrc->iHeight >> 1;
-  for (int y = 0; y < h; y++)
+  if ((w==pSrc->iLineSize[1]) && (pSrc->iLineSize[1]==pImage->stride[1]))
   {
-    memcpy(d, s, w);
-    s += pSrc->iLineSize[1];
-    d += pImage->stride[1];
+    memcpy(d, s, w*h);
+  }
+  else
+  {
+    for (int y = 0; y < h; y++)
+    {
+      memcpy(d, s, w);
+      s += pSrc->iLineSize[1];
+      d += pImage->stride[1];
+    }
   }
   s = pSrc->data[2];
   d = pImage->plane[2];
-  for (int y = 0; y < h; y++)
+  if ((w==pSrc->iLineSize[2]) && (pSrc->iLineSize[2]==pImage->stride[2]))
   {
-    memcpy(d, s, w);
-    s += pSrc->iLineSize[2];
-    d += pImage->stride[2];
+    memcpy(d, s, w*h);
+  }
+  else
+  {
+    for (int y = 0; y < h; y++)
+    {
+      memcpy(d, s, w);
+      s += pSrc->iLineSize[2];
+      d += pImage->stride[2];
+    }
   }
   return true;
 }
