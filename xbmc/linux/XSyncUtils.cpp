1b536feec7b9206a59d21bdfa07b95266588ce34
d4rkm4ster <d4rkm4ster@svn>
2007-10-03 18:00:35 +0000

    fixed: another bug in SDL_SemWaitTimeout2().
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10421 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/linux/XSyncUtils.cpp
+++ xbmc/linux/XSyncUtils.cpp
@@ -290,13 +290,15 @@ int SDL_SemWaitTimeout2(SDL_sem *sem, Uint32 dwMilliseconds)
   req.tv_nsec += (dwMilliseconds % 1000) * 1000000;
   req.tv_sec += req.tv_nsec / 1000000000L;
   req.tv_nsec = req.tv_nsec % 1000000000L;
-  while (((nRet = sem_timedwait((sem_t*)(&sem), &req))==-1) && (errno==EINTR))
+  while (((nRet = sem_timedwait((sem_t*)(sem), &req))==-1) && (errno==EINTR))
+  {
     continue;
+  }
   if (nRet != 0)
   {
     return SDL_MUTEX_TIMEDOUT;
   }
-  return nRet;
+  return 0;
 }
 
 #endif
