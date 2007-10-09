4e8fb22a3e218cbdbf82d781f6ff9f6fe9f03f39
vulkanr <vulkanr@svn>
2007-10-09 15:01:46 +0000

    fixed: broken link and some more janitoring.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10481 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/IPlayer.h
+++ xbmc/cores/IPlayer.h
@@ -4,7 +4,7 @@
 class IPlayerCallback
 {
 public:
-  virtual ~IPlayerCallback();
+  virtual ~IPlayerCallback() {}
   virtual void OnPlayBackEnded() = 0;
   virtual void OnPlayBackStarted() = 0;
   virtual void OnPlayBackStopped() = 0;
