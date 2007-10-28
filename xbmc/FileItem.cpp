652ad8ea99f0b008cc69f28c0bd02b8221e12791
elupus <elupus@svn>
2007-10-28 19:28:10 +0000

    fixed: sorting of shoutcast channels based on listeners cosmetic: removed now unused variable
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10635 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -91,7 +91,6 @@ CFileItem::CFileItem(const CVideoInfoTag& movie)
     m_strPath = movie.m_strFileNameAndPath;
     m_bIsFolder = false;
   }
-  m_fRating = movie.m_fRating;
   *GetVideoInfoTag() = movie;
   FillInDefaultIcon();
   SetVideoThumb();
@@ -280,7 +279,6 @@ const CFileItem& CFileItem::operator=(const CFileItem& item)
 
   m_lStartOffset = item.m_lStartOffset;
   m_lEndOffset = item.m_lEndOffset;
-  m_fRating = item.m_fRating;
   m_strDVDLabel = item.m_strDVDLabel;
   m_strTitle = item.m_strTitle;
   m_iprogramCount = item.m_iprogramCount;
@@ -303,7 +301,6 @@ void CFileItem::Reset()
   FreeIcons();
   m_overlayIcon = ICON_OVERLAY_NONE;
   m_bSelected = false;
-  m_fRating = 0.0f;
   m_strDVDLabel.Empty();
   m_strTitle.Empty();
   m_strPath.Empty();
@@ -354,7 +351,6 @@ void CFileItem::Serialize(CArchive& ar)
     ar << m_iDriveType;
     ar << m_dateTime;
     ar << m_dwSize;
-    ar << m_fRating;
     ar << m_strDVDLabel;
     ar << m_strTitle;
     ar << m_iprogramCount;
@@ -407,7 +403,6 @@ void CFileItem::Serialize(CArchive& ar)
     ar >> m_iDriveType;
     ar >> m_dateTime;
     ar >> m_dwSize;
-    ar >> m_fRating;
     ar >> m_strDVDLabel;
     ar >> m_strTitle;
     ar >> m_iprogramCount;
