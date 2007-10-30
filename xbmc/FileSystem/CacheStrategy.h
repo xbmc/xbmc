868d9bdb7a7d761413645696d3e924af68c1b4f7
elupus <elupus@svn>
2007-10-30 21:43:48 +0000

    merged: HD caching system from linux branch (currently not used anywhere)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10650 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/CacheStrategy.h
@@ -0,0 +1,85 @@
+//
+// C++ Interface: CacheStrategy
+//
+// Description: 
+//
+//
+// Author: Team XBMC <>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef XFILECACHESTRATEGY_H
+#define XFILECACHESTRATEGY_H
+
+#ifdef _LINUX
+#include "PlatformDefs.h"
+#include "XHandle.h"
+#include "XFileUtils.h"
+#endif
+#include "../utils/CriticalSection.h"
+
+namespace XFILE {
+
+#define CACHE_RC_OK  0
+#define CACHE_RC_EOF 0
+#define CACHE_RC_ERROR -1
+#define CACHE_RC_WOULD_BLOCK -2
+#define CACHE_RC_TIMEOUT -3
+
+/**
+*/
+class CCacheStrategy{
+public:
+    CCacheStrategy();
+    virtual ~CCacheStrategy();
+
+	virtual int Open() = 0;
+	virtual int Close() = 0;
+
+	virtual int WriteToCache(const char *pBuffer, size_t iSize) = 0;
+	virtual int ReadFromCache(char *pBuffer, size_t iMaxSize) = 0;
+	virtual __int64 WaitForData(unsigned int iMinAvail, unsigned int iMillis) = 0;
+
+    virtual __int64 Seek(__int64 iFilePosition, int iWhence) = 0;
+	virtual void Reset(__int64 iSourcePosition) = 0;
+
+	virtual void EndOfInput(); // mark the end of the input stream so that Read will know when to return EOF
+	virtual bool IsEndOfInput();
+    virtual void ClearEndOfInput();
+
+protected:
+	bool	m_bEndOfInput;
+};
+
+/**
+*/
+class CSimpleFileCache : public CCacheStrategy {
+public:
+    CSimpleFileCache();
+    virtual ~CSimpleFileCache();
+
+	virtual int Open() ;
+	virtual int Close() ;
+
+	virtual int WriteToCache(const char *pBuffer, size_t iSize) ;
+	virtual int ReadFromCache(char *pBuffer, size_t iMaxSize) ;
+	virtual __int64 WaitForData(unsigned int iMinAvail, unsigned int iMillis) ;
+
+    virtual __int64 Seek(__int64 iFilePosition, int iWhence);
+	virtual void Reset(__int64 iSourcePosition);
+	
+	__int64	GetAvailableRead();
+
+protected:
+	HANDLE	m_hCacheFileRead;
+	HANDLE	m_hCacheFileWrite;
+	HANDLE	m_hDataAvailEvent;
+    __int64 m_nStartPosition;
+	CStdString m_fileName;
+	CCriticalSection m_sync;
+};
+
+}
+
+#endif
---
+++ xbmc/FileSystem/FileCache.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "IFile.h"
+#include "CacheStrategy.h"
+#include "../utils/CriticalSection.h"
+
+namespace XFILE
+{  
+
+  class CFileCache : public IFile, public CThread
+  {
+  public:
+    CFileCache();
+    CFileCache(CCacheStrategy *pCache, bool bDeleteCache=true);
+    virtual ~CFileCache();
+    
+    void SetCacheStrategy(CCacheStrategy *pCache, bool bDeleteCache=true);
+
+    // CThread methods
+    virtual void Process();
+    
+    // IFIle methods
+    virtual bool          Open(const CURL& url, bool bBinary = true);
+    virtual bool          Attach(IFile *pFile);
+    virtual void          Close();
+    virtual bool          Exists(const CURL& url);
+    virtual int           Stat(const CURL& url, struct __stat64* buffer);
+    
+    virtual unsigned int  Read(void* lpBuf, __int64 uiBufSize);
+    
+    virtual __int64       Seek(__int64 iFilePosition, int iWhence);
+    virtual __int64       GetPosition();
+    virtual __int64       GetLength();
+    
+    IFile *GetFileImp();
+
+    bool IsCaching()    const    ;
+    int GetCacheLevel() const    ;
+
+  private:
+    CCacheStrategy *m_pCache;
+    bool      m_bDeleteCache;
+    bool      m_bCaching;
+    int       m_nCacheLevel;
+    CFile      m_source;
+    CStdString    m_sourcePath;
+    CEvent      m_seekEvent;
+    CEvent      m_seekEnded;
+    CEvent      m_threadStarted;
+    int        m_nBytesToBuffer;
+    time_t      m_tmLastBuffering;
+    __int64      m_nSeekResult;
+    __int64      m_seekPos;
+    __int64      m_readPos;
+    CCriticalSection m_sync;
+  };
+
+}
