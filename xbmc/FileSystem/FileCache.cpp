939b68c430fa219e4024c534ef53adc741a12302
elupus <elupus@svn>
2007-11-02 00:59:41 +0000

    fixed: potential lockup in CFileCache on seek changed: allow CFileCache to use truncated reads and use the chunksize of source protocol for it's reads changed: don't allow CFileCache to request seeks on source file if it doesn't report itself as seekable changed: don't reset caching if seek failed on underlying file
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10664 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FileCache.cpp
+++ xbmc/FileSystem/FileCache.cpp
@@ -90,16 +90,26 @@ bool CFileCache::Open(const CURL& url, bool bBinary)
 
   url.GetURL(m_sourcePath);
 
+  // open cache strategy
   if (m_pCache->Open() != CACHE_RC_OK) {
     CLog::Log(LOGERROR,"CFileCache::Open - failed to open cache");
     Close();
     return false;
   }
 
+  // opening the source file.
+  if(!m_source.Open(m_sourcePath, true, READ_NO_CACHE | READ_TRUNCATED)) {
+    CLog::Log(LOGERROR,"%s - failed to open source <%s>", __FUNCTION__, m_sourcePath.c_str());
+    Close();
+    return false;
+  }
+
+  // check if source can seek
+  m_bSeekPossible = m_source.Seek(0, SEEK_POSSIBLE) > 0 ? true : false;
+
   m_readPos = 0;
   m_seekEvent.Reset();
   m_seekEnded.Reset();
-  m_threadStarted.Reset();
 
   CThread::Create(false);
 
@@ -125,94 +135,93 @@ bool CFileCache::Attach(IFile *pFile) {
   return true;
 }
 
-void CFileCache::Process() {  
-  char   buf[READ_CACHE_CHUNK_SIZE];
-  int    iRead=0;
-
+void CFileCache::Process() 
+{
   if (!m_pCache) {
     CLog::Log(LOGERROR,"CFileCache::Process - sanity failed. no cache strategy");
-    m_threadStarted.Set();
     return;
   }
 
-  // opening the source file.
-  // this better be done in this thread since its the same thread that will do the reading.
-  // e.g. libcurl do not like sharing handles across thread boundaries.
-  // empty source path string means a open handle was attached so no open required (ugly).
-  if(!m_sourcePath.IsEmpty() && !m_source.Open(m_sourcePath, true, READ_NO_CACHE)) {
-    CLog::Log(LOGERROR,"CFileCache::Process - failed to open source <%s>", m_sourcePath.c_str());
-    m_threadStarted.Set();
-    return ;
-  }
+  // setup read chunks size
+  int chunksize = m_source.GetChunkSize();
+  if(chunksize == 0)
+    chunksize = READ_CACHE_CHUNK_SIZE;
 
-  m_threadStarted.Set();
-
-  BOOL bError=FALSE;
-  while(!m_bStop && !bError)
+  // create our read buffer
+  auto_aptr<char> buffer(new char[chunksize]);
+  if (buffer.get() == NULL)
   {
-    iRead = m_source.Read(buf, READ_CACHE_CHUNK_SIZE);
+    CLog::Log(LOGERROR, "%s - failed to allocate read buffer", __FUNCTION__);
+    return;
+  }
+  
+  while(!m_bStop)
+  {
+    // check for seek events
+    if (m_seekEvent.WaitMSec(0)) 
+    {
+      m_seekEvent.Reset();
+      CLog::Log(LOGDEBUG,"%s, request seek on source to %lld", __FUNCTION__, m_seekPos);  
+      if ((m_nSeekResult = m_source.Seek(m_seekPos, SEEK_SET)) != m_seekPos)
+        CLog::Log(LOGERROR,"%s, error %d seeking. seek returned %lld", __FUNCTION__, (int)GetLastError(), m_nSeekResult);
+      else
+        m_pCache->Reset(m_seekPos);
+
+      m_seekEnded.Set();
+    }
+
+    int iRead = m_source.Read(buffer.get(), chunksize);
     if(iRead == 0)
     {
       CLog::Log(LOGINFO, "CFileCache::Process - Hit eof.");
-          m_pCache->EndOfInput();
+      m_pCache->EndOfInput();
       
       // since there is no more to read - wait either for seek or close 
       // WaitForSingleObject is CThread::WaitForSingleObject that will also listen to the
       // end thread event.
-      int nRet = WaitForSingleObject(m_seekEvent.GetHandle(), INFINITE);
+      int nRet = CThread::WaitForSingleObject(m_seekEvent.GetHandle(), INFINITE);
       if (nRet == WAIT_OBJECT_0) 
       {
-            m_pCache->ClearEndOfInput();
+        m_pCache->ClearEndOfInput();
         m_seekEvent.Set(); // hack so that later we realize seek is needed
       }
       else 
         break;
     }
     else if (iRead < 0)
-      bError = TRUE;
+      m_bStop = true;
 
     int iTotalWrite=0;
-    while (!bError && !m_bStop && (iTotalWrite < iRead || iRead == 0)) {
+    while (!m_bStop && (iTotalWrite < iRead))
+    {
       int iWrite = 0;
-      if (iRead > 0) 
-      {
-        iWrite = m_pCache->WriteToCache(buf+iTotalWrite, iRead - iTotalWrite);
+      iWrite = m_pCache->WriteToCache(buffer.get()+iTotalWrite, iRead - iTotalWrite);
 
-        // write should always work. all handling of buffering and errors should be
-        // done inside the cache strategy. only if unrecoverable error happened, WriteToCache would return error and we break.
-        if (iWrite < 0) {
-          CLog::Log(LOGERROR,"CFileCache::Process - error writing to cache");
-          bError = TRUE;
-          break;
-        }
-        else if (iWrite == 0)
-          Sleep(5);
-  
-        iTotalWrite += iWrite;
-      }
-
-      if (m_seekEvent.WaitMSec(0)) 
+      // write should always work. all handling of buffering and errors should be
+      // done inside the cache strategy. only if unrecoverable error happened, WriteToCache would return error and we break.
+      if (iWrite < 0) 
       {
-        CLog::Log(LOGDEBUG,"%s, request seek on source to %lld", __FUNCTION__, m_seekPos);  
-        if ((m_nSeekResult = m_source.Seek(m_seekPos, SEEK_SET)) != m_seekPos)
-        {
-          CLog::Log(LOGERROR,"%s, error %d seeking. seek returned %lld", __FUNCTION__, (int)GetLastError(), m_nSeekResult);
-          m_nSeekResult = -1;
-        }
-        m_pCache->Reset(m_source.GetPosition());
-        m_seekEnded.Set();
+        CLog::Log(LOGERROR,"CFileCache::Process - error writing to cache");
+        m_bStop = true;
         break;
       }
+      else if (iWrite == 0)
+        Sleep(5);
 
-      if (iRead == 0)
-        break;
+      iTotalWrite += iWrite;
     }
+
   }
 
+}
+
+void CFileCache::OnExit()
+{
   m_bStop = true;
 
   // make sure cache is set to mark end of file (read may be waiting).
-  m_pCache->EndOfInput();
+  if(m_pCache)
+    m_pCache->EndOfInput();
 
   // just in case someone's waiting...
   m_seekEnded.Set();
@@ -239,60 +248,69 @@ unsigned int CFileCache::Read(void* lpBuf, __int64 uiBufSize)
     CLog::Log(LOGERROR,"CFileCache::Read - sanity failed. no cache strategy!");
     return 0;
   }
+  
+  // attempt to read
+  int iRc = m_pCache->ReadFromCache((char *)lpBuf, (size_t)uiBufSize);
+  if (iRc > 0)
+  {
+    m_readPos += iRc;
+    return iRc;
+  }
 
-  if (m_bStop)
+  if (iRc == CACHE_RC_EOF || iRc == 0)
     return 0;
 
-  // we need to see that we can satisfy the callers request.
-  if (!m_pCache->IsEndOfInput())
+  if (iRc == CACHE_RC_WOULD_BLOCK)
   {
-    // check if we have enough data
-    __int64 nAvail = m_pCache->WaitForData(uiBufSize, 0);
-    if (nAvail < uiBufSize)
+    // buffering 
+    m_bCaching = true;
+    m_nCacheLevel = 0;
+
+    int nMinutesFromLastBuffering = (time(NULL) - m_tmLastBuffering) / 60;
+    m_nBytesToBuffer -= (nMinutesFromLastBuffering * INCREMENT_BUFFER_BYTES);
+    if (m_nBytesToBuffer < INITIAL_BUFFER_BYTES)
+      m_nBytesToBuffer = INITIAL_BUFFER_BYTES;
+
+    __int64 nToBuffer = m_nBytesToBuffer;
+
+    CLog::Log(LOGDEBUG,"%s, not enough data available. buffering. waiting for %d bytes", __FUNCTION__, nToBuffer);
+
+    __int64 nAvail = 0;
+    while (!m_pCache->IsEndOfInput() && nAvail < nToBuffer)
     {
-      // buffering 
-      m_bCaching = true;
-      m_nCacheLevel = 0;
-      int nMinutesFromLastBuffering = (time(NULL) - m_tmLastBuffering) / 60;
-      m_nBytesToBuffer -= (nMinutesFromLastBuffering * INCREMENT_BUFFER_BYTES);
-      if (m_nBytesToBuffer < INITIAL_BUFFER_BYTES)
-        m_nBytesToBuffer = INITIAL_BUFFER_BYTES;
-
-      __int64 nToBuffer = m_nBytesToBuffer;
-      if (nToBuffer < uiBufSize)
-        nToBuffer = uiBufSize;
-
-      CLog::Log(LOGDEBUG,"%s, not enough data available. buffering. waiting for %d bytes", __FUNCTION__, nToBuffer);
-      while (!m_bStop && !m_pCache->IsEndOfInput() && nAvail < nToBuffer)
-      {
-        nAvail = m_pCache->WaitForData(nToBuffer, 500);
-        m_nCacheLevel = (int)(((double)nAvail / (double)nToBuffer) * 100.0);
-      }
-
-      m_tmLastBuffering = time(NULL);
-      m_nBytesToBuffer += INCREMENT_BUFFER_BYTES;
-      if (m_nBytesToBuffer > MAX_BUFFER_BYTES)
-        m_nBytesToBuffer = MAX_BUFFER_BYTES;
-
-      m_bCaching = false;
-      m_nCacheLevel = 0;
-
+      nAvail = m_pCache->WaitForData(nToBuffer, 100);
+      if( nAvail == CACHE_RC_ERROR )
+        break;
+      m_nCacheLevel = (int)(((double)nAvail / (double)nToBuffer) * 100.0);
     }
-  }
 
-  if (!m_bStop)
-  {
-    unsigned int uiBytes = 0;
-    int iRc = m_pCache->ReadFromCache((char *)lpBuf, (size_t)uiBufSize);
+    m_tmLastBuffering = time(NULL);
+    m_nBytesToBuffer += INCREMENT_BUFFER_BYTES;
+    if (m_nBytesToBuffer > MAX_BUFFER_BYTES)
+      m_nBytesToBuffer = MAX_BUFFER_BYTES;
+
+    m_bCaching = false;
+    m_nCacheLevel = 0;
+    
+    iRc = m_pCache->ReadFromCache((char *)lpBuf, (size_t)uiBufSize);
     if (iRc > 0)
     {
-      uiBytes = iRc;
-      m_readPos += uiBytes;
+      m_readPos += iRc;
+      return iRc;
     }
-    return uiBytes;
+
+    if (iRc == CACHE_RC_EOF || iRc == 0)
+      return 0;
+
+    if (iRc == CACHE_RC_WOULD_BLOCK)
+    {
+      CLog::Log(LOGERROR, "%s - cache strategy returned CACHE_RC_WOULD_BLOCK, after having said it had data");
+      return -1;
+    }
   }
 
-  return CACHE_RC_ERROR;
+  // unknown error code
+  return iRc;
 }
 
 __int64 CFileCache::Seek(__int64 iFilePosition, int iWhence)
@@ -311,32 +329,29 @@ __int64 CFileCache::Seek(__int64 iFilePosition, int iWhence)
   else if (iWhence == SEEK_CUR)
     iTarget = iCurPos + iTarget;
   else if (iWhence == SEEK_POSSIBLE)
-    return true;
+    return m_bSeekPossible;
   else if (iWhence != SEEK_SET)
     return -1;
 
   if (iTarget == m_readPos)
     return m_readPos;
-
-  m_seekPos = iTarget;
-  if ((m_nSeekResult = m_pCache->Seek(m_seekPos, SEEK_SET)) == -1)
+  
+  if ((m_nSeekResult = m_pCache->Seek(iTarget, SEEK_SET)) != iTarget)
   {
+    if(!m_bSeekPossible)
+      return m_nSeekResult;
+
+    m_seekPos = iTarget;
     m_seekEvent.Set();
     if (!m_seekEnded.WaitMSec(INFINITE)) 
     {
-      m_seekEvent.Reset();
       CLog::Log(LOGWARNING,"CFileCache::Seek - seek to %lld failed.", m_seekPos);
       return -1;
     }
-  
-    m_seekEvent.Reset();
-    if (m_nSeekResult >= 0) 
-    {
-      m_readPos = iTarget;  
-    }
   }
-  else
-    m_readPos = iTarget;
+
+  if (m_nSeekResult >= 0) 
+    m_readPos = m_nSeekResult;  
 
   return m_nSeekResult;
 }
