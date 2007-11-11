47be6c6d69a904d6fd27bf4f752615992b632a7d
charlydoes <charlydoes@svn>
2007-11-11 14:13:17 +0000

    added: last.fm track love now also sets local rating to 5 if the song has been scanned to the database and hasn't been rated yet.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10713 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicDatabase.cpp
+++ xbmc/MusicDatabase.cpp
@@ -720,6 +720,14 @@ bool CMusicDatabase::GetSongByFileName(const CStdString& strFileName, CSong& son
   try
   {
     song.Clear();
+    CURL url(strFileName);
+    
+    if (url.GetProtocol()=="musicdb")
+    {
+      CStdString strFile = CUtil::GetFileName(strFileName);
+      CUtil::RemoveExtension(strFile);
+      return GetSongById(atol(strFile.c_str()), song);
+    }
 
     CStdString strPath;
     CUtil::GetDirectory(strFileName, strPath);
@@ -3508,6 +3516,7 @@ bool CMusicDatabase::SetSongRating(const CStdString &filePath, char rating)
 {
   try
   {
+    if (filePath.IsEmpty()) return false;
     if (NULL == m_pDB.get()) return false;
     if (NULL == m_pDS.get()) return false;
 
