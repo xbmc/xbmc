49406d837fa5481d57faf91a89beb8a8c63187e2
elupus <elupus@svn>
2007-11-13 21:05:13 +0000

    changed: allow videowindow to be alphablended, allows fadein/fadeout of the preview window
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10734 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2137,16 +2137,6 @@ void CApplication::RenderNoPresent()
 
 void CApplication::DoRender()
 {
-  // enable/disable video overlay window
-  if (IsPlayingVideo() && m_gWindowManager.GetActiveWindow() != WINDOW_FULLSCREEN_VIDEO && !m_bScreenSave)
-  {
-    g_graphicsContext.EnablePreviewWindow(true);
-  }
-  else
-  {
-    g_graphicsContext.EnablePreviewWindow(false);
-  }
-
   if(!m_pd3dDevice)
     return;
 
