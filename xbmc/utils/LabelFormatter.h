d0c4725e40ad3dc4eba0cf459a355e383086d72a
jmarshallnz <jmarshallnz@svn>
2007-11-15 22:55:56 +0000

    added: filename filtering for tagless music files via advancedsettings.xml. See the online manual for setup details.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@10757 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/LabelFormatter.h
+++ xbmc/utils/LabelFormatter.h
@@ -10,13 +10,15 @@ class CLabelFormatter
 public:
   CLabelFormatter(const CStdString &mask, const CStdString &mask2);
 
-  void FormatLabel(CFileItem *item);
-  void FormatLabel2(CFileItem *item);
-  void FormatLabels(CFileItem *item) // convenient shorthand
+  void FormatLabel(CFileItem *item) const;
+  void FormatLabel2(CFileItem *item) const;
+  void FormatLabels(CFileItem *item) const // convenient shorthand
   {
     FormatLabel(item);
     FormatLabel2(item);
   }
+
+  bool FillMusicTag(const CStdString &fileName, CMusicInfoTag *tag) const;
 
 private:
   class CMaskString
@@ -36,8 +38,9 @@ private:
   void SplitMask(unsigned int label, const CStdString &mask);
 
   // functions for retrieving content based on our mask vectors
-  CStdString GetContent(unsigned int label, const CFileItem *item);
-  CStdString GetMaskContent(const CMaskString &mask, const CFileItem *item);
+  CStdString GetContent(unsigned int label, const CFileItem *item) const;
+  CStdString GetMaskContent(const CMaskString &mask, const CFileItem *item) const;
+  void FillMusicMaskContent(const char mask, const CStdString &value, CMusicInfoTag *tag) const;
 
   vector<CStdString>   m_staticContent[2];
   vector<CMaskString>  m_dynamicContent[2];
