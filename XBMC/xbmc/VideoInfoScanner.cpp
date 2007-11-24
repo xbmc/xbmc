3fce4646887bee7a3208446f8815a8a87088aa0b
vulkanr <vulkanr@svn>
2007-11-24 22:10:06 +0000

    applied anonymous patch for separating video library scan to new category and allowing it to run on startup.
    patch description (c&p from sf):
    This patch does the following...
    
    -Creates a new settings group called 'Library' in the video settings dialog.
    -Moves the library related settings from the 'General' group to the new 'Library' group in the video settings dialog.
    -Adds 2 new video library boolean settings...'Update library on startup' (videolibrary.updateonstartup) and 'Always update library in background'
    (videolibrary.backgroundupdate).
    -On startup (via CApplication::Initialize), automatically updates the video library if the 'videolibrary.updateonstartup' setting is true.
    -Prevents the CGUIDialogVideoScan window from showing if the 'videolibrary.backgroundupdate' setting is set to true.
    
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@10834 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/VideoInfoScanner.cpp
+++ XBMC/xbmc/VideoInfoScanner.cpp
@@ -584,7 +584,7 @@ namespace VIDEO
                   }
                 }
                 else
-                  if (g_guiSettings.GetBool("myvideos.seasonthumbs"))
+                  if (g_guiSettings.GetBool("videolibrary.seasonthumbs"))
                     FetchSeasonThumbs(lResult);
               }
             }
@@ -826,7 +826,7 @@ namespace VIDEO
       ApplyIMDBThumbToFolder(strDirectory,strThumb);
     }
 
-    if (g_guiSettings.GetBool("myvideos.actorthumbs"))
+    if (g_guiSettings.GetBool("videolibrary.actorthumbs"))
       FetchActorThumbs(movieDetails.m_cast);
     return lResult;
   }
@@ -883,7 +883,7 @@ namespace VIDEO
         AddMovieAndGetThumb(&item,"tvshows",episodeDetails,lShowId);
       }
     }
-    if (g_guiSettings.GetBool("myvideos.seasonthumbs"))
+    if (g_guiSettings.GetBool("videolibrary.seasonthumbs"))
       FetchSeasonThumbs(lShowId);
     m_database.Close();
   }
