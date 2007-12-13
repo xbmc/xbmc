ed5aa79ad1e250dd9a6360514e536cf8ebf739ce
vulkanr <vulkanr@svn>
2007-12-13 15:08:11 +0000

    video auto-thumb loader. now in dvdplayer and called by VideoThumbLoader.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@10987 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ThumbLoader.cpp
+++ xbmc/ThumbLoader.cpp
@@ -38,13 +38,11 @@
 #include "cores/ffmpeg/DllAvCodec.h"
 #include "cores/ffmpeg/DllSwScale.h"
 
-#define THUMB_WIDTH g_advancedSettings.m_thumbSize
-#define THUMB_HEIGHT g_advancedSettings.m_thumbSize
-#define THUMB_TIME_IN_VIDEO 180
+#include "cores/dvdplayer/DVDPlayer.h"
 
 using namespace XFILE;
 
-CVideoThumbLoader::CVideoThumbLoader() : CBackgroundInfoLoader(1)
+CVideoThumbLoader::CVideoThumbLoader() 
 {  
 }
 
@@ -55,6 +53,8 @@ CVideoThumbLoader::~CVideoThumbLoader()
 
 void CVideoThumbLoader::OnLoaderStart() 
 {
+  // load ffmpeg libs in start - just a small optimization so that when the frame extractor will run ffmpeg will
+  // already be in memory. not a requirement.
   if (!m_dllAvUtil.Load() || !m_dllAvCodec.Load() || !m_dllAvFormat.Load() || !m_dllSwScale.Load())  
   {
     CLog::Log(LOGERROR,"%s - failed to load ffmpeg lib", __FUNCTION__);
@@ -76,119 +76,7 @@ void CVideoThumbLoader::OnLoaderFinish()
 bool CVideoThumbLoader::ExtractThumb(const CStdString &strPath, const CStdString &strTarget)
 {
   CLog::Log(LOGDEBUG,"%s - trying to extract thumb from video file %s", __FUNCTION__, strPath.c_str());
-
-  AVFormatContext *pFormatContext=NULL;
-
-  if (m_dllAvFormat.av_open_input_file(&pFormatContext, strPath, 0, 0, NULL) < 0)
-  {
-    CLog::Log(LOGERROR,"%s- failed to open input file %s", __FUNCTION__, strPath.c_str());
-    return false;
-  }
-
-  if (m_dllAvFormat.av_find_stream_info(pFormatContext) < 0 )
-  {
-    CLog::Log(LOGERROR,"%s- failed to find stream info for %s", __FUNCTION__, strPath.c_str());
-    m_dllAvFormat.av_close_input_file(pFormatContext);
-    return false;
-  }
-
-  int nVideoStream = -1;
-  for (unsigned int i = 0; i < pFormatContext->nb_streams; i++)
-  {
-    if (pFormatContext->streams[i]->codec->codec_type == CODEC_TYPE_VIDEO)
-    {
-      nVideoStream = i;
-      break;
-    }
-  }
-
-  if (nVideoStream < 0)
-  {
-    CLog::Log(LOGERROR,"%s- failed to find video stream for %s", __FUNCTION__, strPath.c_str());
-    m_dllAvFormat.av_close_input_file(pFormatContext);
-    return false;
-  }
-
-  AVStream *pStream = pFormatContext->streams[nVideoStream];
- 
-  // open the codec
-  AVCodec *pCodec = m_dllAvCodec.avcodec_find_decoder(pStream->codec->codec_id);
-  pStream->codec->workaround_bugs |= FF_BUG_AUTODETECT;
-  pStream->codec->flags |= CODEC_FLAG_LOW_DELAY;
-  pStream->codec->flags2 |= CODEC_FLAG2_CHUNKS | CODEC_FLAG2_FAST;
-
-  if(pCodec==NULL || m_dllAvCodec.avcodec_open(pStream->codec, pCodec)<0) 
-  {
-    CLog::Log(LOGERROR,"%s- failed to open codec for %s", __FUNCTION__, strPath.c_str());
-    m_dllAvFormat.av_close_input_file(pFormatContext);
-    return false;
-  }
-  
-  int nOffset = THUMB_TIME_IN_VIDEO; 
-  int nDuration = pFormatContext->duration / AV_TIME_BASE;
-  if (nOffset > nDuration / 2) // in case it is a short video - take a frame from the middle.
-    nOffset = nDuration / 2;
-
-  if (m_dllAvFormat.av_seek_frame(pFormatContext, -1, nOffset * AV_TIME_BASE, AVSEEK_FLAG_BACKWARD) < 0)
-  {
-    CLog::Log(LOGERROR,"%s- failed to seek to %d in %s", __FUNCTION__, nOffset, strPath.c_str());
-    m_dllAvFormat.av_close_input_file(pFormatContext);
-    return false;
-  }
-
-  AVFrame *pic = m_dllAvCodec.avcodec_alloc_frame();
-  int nHasPic = 0;
-  do
-  {
-    AVPacket pkt;
-    if (m_dllAvFormat.av_read_frame(pFormatContext, &pkt) < 0)
-    {
-      CLog::Log(LOGERROR,"%s- failed to read frame from %s", __FUNCTION__, strPath.c_str());
-      break;
-    }
-
-    if (pkt.stream_index != nVideoStream)
-      continue;
-
-    if (m_dllAvCodec.avcodec_decode_video(pStream->codec, pic, &nHasPic, pkt.data, pkt.size) < 0)
-    {
-      CLog::Log(LOGERROR,"%s- failed to decode video %s", __FUNCTION__, strPath.c_str());
-      av_free_packet(&pkt);
-      break;
-    }
-    av_free_packet(&pkt);
-  } while (!nHasPic);
-
-  if (!nHasPic)
-  {
-    CLog::Log(LOGERROR,"%s- failed to extract frame from %s", __FUNCTION__, strPath.c_str());
-    m_dllAvUtil.av_free(pic);
-    m_dllAvFormat.av_close_input_file(pFormatContext);
-    return false;
-  }
-
-  BYTE *pOutBuf = (BYTE*)new int[THUMB_WIDTH * THUMB_HEIGHT * 4];
-  struct SwsContext *context = m_dllSwScale.sws_getContext(pStream->codec->width, pStream->codec->height, 
-     pStream->codec->pix_fmt, THUMB_WIDTH, THUMB_HEIGHT, PIX_FMT_RGB32, SWS_BILINEAR, NULL, NULL, NULL);
-  uint8_t *src[] = { pic->data[0], pic->data[1], pic->data[2] };
-  int     srcStride[] = { pic->linesize[0], pic->linesize[1], pic->linesize[2] };
-  uint8_t *dst[] = { pOutBuf, 0, 0 };
-  int     dstStride[] = { THUMB_WIDTH*4, 0, 0 };
-
-  if (context)
-  {
-    m_dllSwScale.sws_scale(context, src, srcStride, 0, pStream->codec->height, dst, dstStride);  
-    m_dllSwScale.sws_freeContext(context);
-
-    CPicture out;
-    out.CreateThumbnailFromSurface(pOutBuf, THUMB_WIDTH, THUMB_HEIGHT, THUMB_WIDTH * 4, strTarget);
-  }
-
-  delete [] pOutBuf;
-  m_dllAvUtil.av_free(pic);
-  m_dllAvCodec.avcodec_close(pStream->codec);
-  m_dllAvFormat.av_close_input_file(pFormatContext);
-  return context != NULL;
+  return CDVDPlayer::ExtractThumb(strPath, strTarget);
 }
 
 bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
