a245d4c1345783bacdc900e5dd54b72d972aa9a1
vulkanr <vulkanr@svn>
2007-12-18 00:35:28 +0000

    fixed: ogg codec to handle network streams correctly. (e.g. some stations on shoutcast).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11042 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/OGGcodec.h
+++ xbmc/cores/paplayer/OGGcodec.h
@@ -18,6 +18,7 @@ public:
 private:
   static size_t ReadCallback(void *ptr, size_t size, size_t nmemb, void *datasource);
   static int SeekCallback(void *datasource, ogg_int64_t offset, int whence);
+  static int NoSeekCallback(void *datasource, ogg_int64_t offset, int whence);
   static int CloseCallback(void *datasource);
   static long TellCallback(void *datasource);
 
