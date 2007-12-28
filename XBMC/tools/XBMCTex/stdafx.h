3e59d381d6eaf67908dc0036c67d1921586e959f
darkdonno <darkdonno@svn>
2007-12-28 09:17:17 +0000

    changed: XBMCTex tool so its more GNUC friendly.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@11110 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/tools/XBMCTex/stdafx.h
+++ XBMC/tools/XBMCTex/stdafx.h
@@ -25,11 +25,44 @@
 #include <XGraphics.h>
 #endif
 
+#include "xbox.h"
+
+// Round a number to the nearest power of 2 rounding up
+// runs pretty quickly - the only expensive op is the bsr
+// alternive would be to dec the source, round down and double the result
+// which is slightly faster but rounds 1 to 2 
+#if defined(_MSC_VER)  
+DWORD __forceinline __stdcall PadPow2(DWORD x)
+{
+	__asm {
+		mov edx,x    // put the value in edx
+			xor ecx,ecx  // clear ecx - if x is 0 bsr doesn't alter it
+			bsr ecx,edx  // find MSB position
+			mov eax,1    // shift 1 by result effectively
+			shl eax,cl   // doing a round down to power of 2
+			cmp eax,edx  // check if x was already a power of two
+			adc ecx,0    // if it wasn't then CF is set so add to ecx
+			mov eax,1    // shift 1 by result again, this does a round
+			shl eax,cl   // up as a result of adding CF to ecx
+	}
+	// return result in eax
+}
+#else
+DWORD inline PadPow2(DWORD x) 
+{
+  --x;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  return ++x;
+}
+#endif
+
 // Debug macros
+#if defined(_DEBUG) && defined(_MSC_VER) 
 #include <crtdbg.h>
-
-#ifdef _DEBUG
-
 #define TRACE0(f)                 _RPT0(_CRT_WARN, f)
 #define TRACE1(f, a)              _RPT1(_CRT_WARN, f, a)
 #define TRACE2(f, a, b)           _RPT2(_CRT_WARN, f, a, b)
@@ -48,4 +81,4 @@
 
 #endif
 
-#include "xbox.h"
+
