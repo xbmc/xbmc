6e9f1b01005fcc45c4c83031ee73b59386184ddc
jmarshallnz <jmarshallnz@svn>
2007-12-29 08:44:50 +0000

    fixed: Multiline alignment within list and panel labels was off.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@11118 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIListLabel.cpp
+++ guilib/GUIListLabel.cpp
@@ -30,10 +30,26 @@ void CGUIListLabel::SetSelected(bool selected)
 void CGUIListLabel::Render()
 {
   DWORD color = m_selected ? m_label.selectedColor : m_label.textColor;
-  if (m_scrolling && m_renderRect.x2 < m_textWidth + m_renderRect.x1 - 0.5f)
+  bool needsToScroll(m_renderRect.Width() + 0.5f < m_textWidth); // 0.5f to deal with floating point rounding issues
+  if (m_scrolling && needsToScroll)
     m_textLayout.RenderScrolling(m_renderRect.x1, m_renderRect.y1, m_label.angle, color, m_label.shadowColor, 0, m_renderRect.Width(), m_scrollInfo);
   else
-    m_textLayout.Render(m_renderRect.x1, m_renderRect.y1, m_label.angle, color, m_label.shadowColor, 0, m_renderRect.Width());
+  {
+    float posX = m_renderRect.x1;
+    DWORD align = 0;
+    if (!needsToScroll)
+    { // hack for right and centered multiline text, as GUITextLayout::Render() treats posX as the right hand
+      // or center edge of the text (see GUIFontTTF::DrawTextInternal), and this has already been taken care of
+      // in SetLabel(), but we wish to still pass the horizontal alignment info through (so that multiline text
+      // is aligned correctly), so we must undo the SetLabel() changes for horizontal alignment.
+      if (m_label.align & XBFONT_RIGHT)
+        posX += m_renderRect.Width();
+      else if (m_label.align & XBFONT_CENTER_X)
+        posX += m_renderRect.Width() * 0.5f;
+      align = m_label.align & ~XBFONT_CENTER_Y;  // ignore vertical alignment
+    }
+    m_textLayout.Render(posX, m_renderRect.y1, m_label.angle, color, m_label.shadowColor, align, m_renderRect.Width());
+  }
   CGUIControl::Render();
 }
 
