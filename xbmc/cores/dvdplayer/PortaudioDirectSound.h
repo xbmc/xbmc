452880d9b117c28ac7b9df4f3256e4cd23006828
leafmuncher <leafmuncher@svn>
2008-01-01 05:28:08 +0000

    This is currently little more than a stub, but it shouldn't be hard to finish, using inspiration from the Portaudio paplayer.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11156 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/dvdplayer/PortaudioDirectSound.h
@@ -0,0 +1,92 @@
+/*
+* XBoxMediaPlayer
+* Copyright (c) 2002 d7o3g4q and RUNTiME
+* Portions Copyright (c) by the authors of ffmpeg and xvid
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+// AsyncAudioRenderer.h: interface for the CAsyncDirectSound class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#ifndef __ALSA_DIRECT_SOUND_H__
+#define __ALSA_DIRECT_SOUND_H__
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "../mplayer/IDirectSoundRenderer.h"
+#include "../mplayer/IAudioCallback.h"
+#include "../ssrc.h"
+#include "../../utils/PCMAmplifier.h"
+
+extern void RegisterAudioCallback(IAudioCallback* pCallback);
+extern void UnRegisterAudioCallback();
+
+class PortAudioDirectSound : public IDirectSoundRenderer
+{
+public:
+  virtual void UnRegisterAudioCallback();
+  virtual void RegisterAudioCallback(IAudioCallback* pCallback);
+  virtual DWORD GetChunkLen();
+  virtual FLOAT GetDelay();
+  PortAudioDirectSound(IAudioCallback* pCallback, int iChannels, unsigned int uiSamplesPerSec, unsigned int uiBitsPerSample, bool bResample, char* strAudioCodec = "", bool bIsMusic=false, bool bPassthrough = false);
+  virtual ~PortAudioDirectSound();
+
+  virtual DWORD AddPackets(unsigned char* data, DWORD len);
+  virtual DWORD GetSpace();
+  virtual HRESULT Deinitialize();
+  virtual HRESULT Pause();
+  virtual HRESULT Stop();
+  virtual HRESULT Resume();
+
+  virtual LONG GetMinimumVolume() const;
+  virtual LONG GetMaximumVolume() const;
+  virtual LONG GetCurrentVolume() const;
+  virtual void Mute(bool bMute);
+  virtual HRESULT SetCurrentVolume(LONG nVolume);
+  virtual int SetPlaySpeed(int iSpeed);
+  virtual void WaitCompletion();
+  virtual void DoWork();
+  virtual void SwitchChannels(int iAudioStream, bool bAudioOnAllSpeakers);
+  virtual void Flush();
+
+private:
+  //snd_pcm_t 		*m_pPlayHandle;
+  //snd_pcm_uframes_t 	m_maxFrames;
+
+  IAudioCallback* m_pCallback;
+
+  CPCMAmplifier 	m_amp;
+  LONG m_nCurrentVolume;
+  DWORD m_dwPacketSize;
+  DWORD m_dwNumPackets;
+  bool m_bPause;
+  bool m_bIsAllocated;
+  bool m_bCanPause;
+
+  unsigned int m_uiSamplesPerSec;
+  unsigned int m_uiBitsPerSample;
+  unsigned int m_uiChannels;
+
+  //snd_pcm_uframes_t m_BufferSize;
+
+  bool m_bPassthrough;
+};
+
+#endif 
+
