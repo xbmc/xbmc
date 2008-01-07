1019fc1d17218d90eebef781912dfcbb2c1c6155
spiff_ <spiff_@svn>
2008-01-07 21:35:05 +0000

    fixed: always load scraper setting xml so we get defaults saved to db
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/scrapersettings@11261 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIDialogContentSettings.cpp
+++ xbmc/GUIDialogContentSettings.cpp
@@ -427,6 +427,11 @@ bool CGUIDialogContentSettings::Show(SScraperInfo& scraper, VIDEO::SScanSettings
 
       bRunScan = false;
     }
+
+    if (!scraper.strContent.IsEmpty() && ! scraper.strContent.Equals("None") && !scraper.settings.GetPluginRoot())
+    { // load default scraper settings
+      scraper.settings.LoadSettingsXML("q:\\system\\scrapers\\video\\"+scraper.strPath);
+    }
     
     return true;
   }
