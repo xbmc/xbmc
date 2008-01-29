21bbfafa07701b1302bb5ea39bb1839b20f249e1
leafmuncher <leafmuncher@svn>
2008-01-29 06:20:12 +0000

    Changes to support statically linking on OS X.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11463 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/ffmpeg/DllAvCodec.h
+++ xbmc/cores/ffmpeg/DllAvCodec.h
@@ -17,7 +17,11 @@ extern "C" {
 #pragma warning(disable:4244)
 #endif
 
+#ifdef __APPLE__
+#include "libffmpeg-OSX/avcodec.h"
+#else
 #include "avcodec.h"
+#endif
 }
 
 class DllAvCodecInterface
@@ -51,6 +55,50 @@ public:
   virtual int avcodec_thread_init(AVCodecContext *s, int thread_count)=0;
 };
 
+#ifdef __APPLE__
+
+extern "C" { AVOption* av_set_string(void *obj, const char *name, const char *val); }  
+
+// Use direct layer, we link statically.
+class DllAvCodec : public DllDynamic, DllAvCodecInterface
+{
+public:
+  virtual ~DllAvCodec() {}
+  virtual void avcodec_register_all() { ::avcodec_register_all(); }
+  virtual void avcodec_flush_buffers(AVCodecContext *avctx) { ::avcodec_flush_buffers(avctx); }
+  virtual int avcodec_open(AVCodecContext *avctx, AVCodec *codec) { return ::avcodec_open(avctx, codec); }
+  virtual AVCodec *avcodec_find_decoder(enum CodecID id) { return ::avcodec_find_decoder(id); }
+  virtual int avcodec_close(AVCodecContext *avctx) { return ::avcodec_close(avctx); }
+  
+  virtual AVFrame *avcodec_alloc_frame() { return ::avcodec_alloc_frame(); }
+  virtual int avpicture_fill(AVPicture *picture, uint8_t *ptr, int pix_fmt, int width, int height) { return ::avpicture_fill(picture, ptr, pix_fmt, width, height); }
+  virtual int avcodec_decode_video(AVCodecContext *avctx, AVFrame *picture, int *got_picture_ptr, uint8_t *buf, int buf_size) { return ::avcodec_decode_video(avctx, picture, got_picture_ptr, buf, buf_size); }
+  virtual int avcodec_decode_audio2(AVCodecContext *avctx, int16_t *samples, int *frame_size_ptr, uint8_t *buf, int buf_size) { return ::avcodec_decode_audio2(avctx, samples, frame_size_ptr, buf, buf_size); }
+  virtual int avcodec_decode_subtitle(AVCodecContext *avctx, AVSubtitle *sub, int *got_sub_ptr, const uint8_t *buf, int buf_size) { return ::avcodec_decode_subtitle(avctx, sub, got_sub_ptr, buf, buf_size); } 
+  virtual int avpicture_get_size(int pix_fmt, int width, int height) { return ::avpicture_get_size(pix_fmt, width, height); }
+  virtual AVCodecContext *avcodec_alloc_context() { return ::avcodec_alloc_context(); }
+  virtual void avcodec_string(char *buf, int buf_size, AVCodecContext *enc, int encode) { ::avcodec_string(buf, buf_size, enc, encode); }
+  virtual void avcodec_get_context_defaults(AVCodecContext *s) { ::avcodec_get_context_defaults(s); }
+  
+  virtual AVCodecParserContext *av_parser_init(int codec_id) { return ::av_parser_init(codec_id); }
+  virtual int av_parser_parse(AVCodecParserContext *s,AVCodecContext *avctx, uint8_t **poutbuf, int *poutbuf_size, 
+                    const uint8_t *buf, int buf_size,
+                    int64_t pts, int64_t dts) { return ::av_parser_parse(s, avctx, poutbuf, poutbuf_size, buf, buf_size, pts, dts); }
+  virtual void av_parser_close(AVCodecParserContext *s) { ::av_parser_close(s); }
+  
+  virtual void avpicture_free(AVPicture *picture) { ::avpicture_free(picture); }
+  virtual int avpicture_alloc(AVPicture *picture, int pix_fmt, int width, int height) { return ::avpicture_alloc(picture, pix_fmt, width, height); }
+  virtual AVOption *av_set_string(void *obj, const char *name, const char *val) { return ::av_set_string(obj, name, val); }
+  virtual int avcodec_default_get_buffer(AVCodecContext *s, AVFrame *pic) { return ::avcodec_default_get_buffer(s, pic); }
+  virtual void avcodec_default_release_buffer(AVCodecContext *s, AVFrame *pic) { ::avcodec_default_release_buffer(s, pic); }
+  virtual int avcodec_thread_init(AVCodecContext *s, int thread_count) { return ::avcodec_thread_init(s, thread_count); }
+  
+  // DLL faking.
+  virtual bool ResolveExports() { return true; }
+  virtual bool Load() { return true; }
+  virtual void Unload() {}
+};
+#else
 class DllAvCodec : public DllDynamic, DllAvCodecInterface
 {
 #ifndef _LINUX
@@ -129,6 +177,8 @@ public:
       return avcodec_open_dont_call(avctx,codec);
     }
 };
+#endif
+
 
 // calback used for logging
 void ff_avutil_log(void* ptr, int level, const char* format, va_list va);
@@ -147,6 +197,32 @@ public:
   virtual void av_freep(void *ptr)=0;
   virtual int64_t av_rescale_rnd(int64_t a, int64_t b, int64_t c, enum AVRounding)=0;
 };
+
+#ifdef __APPLE__
+
+// Use direct layer, since we link statically.
+class DllAvUtil : public DllDynamic, DllAvUtilInterface
+{
+public:
+  
+  virtual ~DllAvUtil() {}
+ #if LIBAVUTIL_VERSION_INT < (50<<16)
+   virtual void av_log_set_callback(void (*foo)(void*, int, const char*, va_list)) { ::av_log_set_callback(foo); }
+ #endif
+   virtual void *av_malloc(unsigned int size) { return ::av_malloc(size); }
+   virtual void *av_mallocz(unsigned int size) { return ::av_mallocz(size); }
+   virtual void *av_realloc(void *ptr, unsigned int size) { return ::av_realloc(ptr, size); } 
+   virtual void av_free(void *ptr) { ::av_free(ptr); }
+   virtual void av_freep(void *ptr) { ::av_freep(ptr); }
+   virtual int64_t av_rescale_rnd(int64_t a, int64_t b, int64_t c, enum AVRounding d) { return ::av_rescale_rnd(a, b, c, d); }
+   
+   // DLL faking.
+   virtual bool ResolveExports() { return true; }
+   virtual bool Load() { return true; }
+   virtual void Unload() {}
+};
+
+#else
 
 class DllAvUtilBase : public DllDynamic, DllAvUtilInterface
 {
@@ -207,3 +283,4 @@ public:
   }
 };
 
+#endif
