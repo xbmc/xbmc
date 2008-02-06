5740338c33bec5c8819b14e952a5bf2d60c5958c
elupus <elupus@svn>
2008-02-06 00:46:08 +0000

    added: separate file handler in dvdplayer for myth streams to allow better control added: totalt time of current program when playing mythtv live streams
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@11524 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamMyth.h
@@ -0,0 +1,29 @@
+#ifdef HAS_GMYTH
+#pragma once
+
+#include "DVDInputStream.h"
+
+namespace XFILE {
+class CGMythFile;
+}
+
+class CDVDInputStreamMyth : public CDVDInputStream
+{
+public:
+  CDVDInputStreamMyth();
+  virtual ~CDVDInputStreamMyth();
+  virtual bool    Open(const char* strFile, const std::string &content);
+  virtual void    Close();
+  virtual int     Read(BYTE* buf, int buf_size);
+  virtual __int64 Seek(__int64 offset, int whence);
+  virtual bool    IsEOF();
+  virtual __int64 GetLength();
+
+  int             GetTotalTime();
+  int             GetTime();
+  std::string     GetTitle();
+protected:
+  XFILE::CGMythFile* m_pFile;
+  bool m_eof;
+};
+#endif
