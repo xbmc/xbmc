5530c200661644bfa512e4ceee1501f0c6ec4d00
elupus <elupus@svn>
2008-02-06 23:41:34 +0000

    changed: split file and directory code for gmyth client
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11540 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/GMythFile.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifdef HAS_GMYTH
+
+#include "IFile.h"
+
+typedef struct _GMythBackendInfo GMythBackendInfo;
+typedef struct _GMythLiveTV GMythLiveTV;
+typedef struct _GMythFileTransfer GMythFileTransfer;
+typedef struct _GByteArray GByteArray;
+
+namespace XFILE
+{
+
+class CGMythFile : public IFile  
+{
+public:
+  CGMythFile();
+  virtual ~CGMythFile();
+  virtual bool          Open(const CURL& url, bool binary = true);
+  virtual bool          Exists(const CURL& url);
+  virtual __int64       Seek(__int64 pos, int whence=SEEK_SET);
+  virtual __int64       GetPosition();
+  virtual __int64       GetLength();
+  virtual int           Stat(const CURL& url, struct __stat64* buffer) { return -1; }
+  virtual void          Close();
+  virtual unsigned int  Read(void* buffer, __int64 size);
+  virtual CStdString    GetContent() { return ""; }
+  virtual bool          SkipNext();
+
+  bool                  NextChannel();
+  bool                  PrevChannel();
+
+  bool                  GetVideoInfoTag(CVideoInfoTag& tag);
+  int                   GetTotalTime();
+  int                   GetStartTime();
+protected:
+
+  bool SetupTransfer();
+
+  GMythBackendInfo  *m_info;
+  GMythLiveTV       *m_livetv;
+  GMythFileTransfer *m_file;
+  GByteArray        *m_array;
+  unsigned int       m_used;
+  char*              m_filename;
+  char*              m_channel;
+};
+
+}
+
+#endif
