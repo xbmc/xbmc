001c98262acf3b1d9007d998b2ca095b7f0f4e38
leafmuncher <leafmuncher@svn>
2008-02-09 01:43:11 +0000

    Support different YUV colorspaces, passed down from dvdplayer. ARB still needs support for this, as well as the stretching to full range.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11592 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/VideoRenderers/LinuxRendererGL.h
+++ xbmc/cores/VideoRenderers/LinuxRendererGL.h
@@ -11,6 +11,7 @@
 #include "VideoShaders/YUV2RGBShader.h"
 #include "VideoShaders/VideoFilterShader.h"
 #include "../../settings/VideoSettings.h"
+#include "RenderFlags.h"
 
 using namespace Surface;
 using namespace Shaders;
@@ -45,30 +46,6 @@ typedef struct YV12Image
 #define IMAGE_FLAG_RESERVED  0x08 /* image is reserved, must be asked for specifically used to preserve images */
 #define IMAGE_FLAG_READY     0x16 /* image is ready to be uploaded to texture memory */
 #define IMAGE_FLAG_INUSE (IMAGE_FLAG_WRITING | IMAGE_FLAG_READING | IMAGE_FLAG_RESERVED)
-
-
-#define RENDER_FLAG_EVEN        0x01
-#define RENDER_FLAG_ODD         0x02
-#define RENDER_FLAG_BOTH (RENDER_FLAG_EVEN | RENDER_FLAG_ODD)
-#define RENDER_FLAG_FIELDMASK   0x03
-#define RENDER_FLAG_LAST        0x04
-
-#define RENDER_FLAG_NOOSD       0x04 /* don't draw any osd */
-#define RENDER_FLAG_NOOSDALPHA  0x08 /* don't allow alpha when osd is drawn */
-
-/* these two flags will be used if we need to render same image twice (bob deinterlacing) */
-#define RENDER_FLAG_NOLOCK      0x10   /* don't attempt to lock texture before rendering */
-#define RENDER_FLAG_NOUNLOCK    0x20   /* don't unlock texture after rendering */
-
-/* this defines what color translation coefficients */
-#define CONF_FLAGS_YUVCOEF_MASK(a) ((a) & 0x07)
-#define CONF_FLAGS_YUVCOEF_BT709 0x01
-#define CONF_FLAGS_YUVCOEF_BT601 0x02
-#define CONF_FLAGS_YUVCOEF_240M  0x03
-#define CONF_FLAGS_YUVCOEF_EBU   0x04
-
-#define CONF_FLAGS_YUV_FULLRANGE 0x08
-#define CONF_FLAGS_FULLSCREEN    0x10
 
 struct DRAWRECT
 {
@@ -205,7 +182,7 @@ protected:
 
   bool m_bConfigured;
   bool m_bValidated;
-  bool m_bFullYUVRange;
+  unsigned m_iFlags;
   GLenum m_textureTarget;
   unsigned short m_renderMethod;
   RenderQuality m_renderQuality;
