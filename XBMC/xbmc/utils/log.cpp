913663067285796c514ca807de110bb4717a349f
leafmuncher <leafmuncher@svn>
2008-02-13 12:06:16 +0000

    Put the logs in a standard place on OS X.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@11654 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/utils/log.cpp
+++ XBMC/xbmc/utils/log.cpp
@@ -46,8 +46,15 @@ void CLog::Log(int loglevel, const char *format, ... )
       // g_stSettings.m_logFolder is initialized in the CSettings constructor to Q:
       // and if we are running from DVD, it's changed to T: in CApplication::Create()
       CStdString strLogFile, strLogFileOld;
+      
+#ifdef __APPLE__
+      // Put the logs in a standard place.
+      strLogFile = "/var/tmp/xbmc.log";
+      strLogFileOld = "/var/tmp/xbmc.old.log";
+#else
       strLogFile.Format("%sxbmc.log", _P(g_stSettings.m_logFolder).c_str());
       strLogFileOld.Format("%sxbmc.old.log", _P(g_stSettings.m_logFolder).c_str());
+#endif
 
 #ifndef _LINUX
       ::DeleteFile(strLogFileOld.c_str());
