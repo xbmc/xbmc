ea368c3b261c5c2652eea0e1e33c6a3aabfc7576
elupus <elupus@svn>
2008-02-17 15:03:33 +0000

    changed: matchup all inttypes definitions.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@11699 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcdio/inttypes.h
+++ xbmc/lib/libcdio/inttypes.h
@@ -7,13 +7,13 @@
  * Should be installed into /usr/include
  * as inttypes.h*/
 typedef char *  caddr_t;
-typedef char int8_t;
+typedef signed char int8_t;
 typedef unsigned char u_int8_t;
-typedef short int16_t;
+typedef signed short int16_t;
 typedef unsigned short u_int16_t;
-typedef int int32_t;
+typedef signed int int32_t;
 typedef unsigned int u_int32_t;
-typedef __int64 int64_t;
+typedef signed __int64 int64_t;
 typedef unsigned __int64 u_int64_t;
 typedef int32_t register_t;
 typedef u_int8_t uint8_t;
