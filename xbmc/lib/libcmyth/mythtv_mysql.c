758972adeacb77203aec4b1a8293ba7fc92d1a36
elupus <elupus@svn>
2008-02-20 22:02:32 +0000

    updated: merged in libcmyth git repo into our version. (adds support for svn version of mythtv-backend)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11756 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcmyth/mythtv_mysql.c
+++ xbmc/lib/libcmyth/mythtv_mysql.c
@@ -730,10 +730,9 @@ cmyth_mysql_get_commbreak_list(cmyth_database_t db, int chanid, char * start_ts_
 	MYSQL_ROW row;
 	const char *query_str = "SELECT m.type AS type, m.mark AS mark, s.offset AS offset FROM recordedmarkup m INNER JOIN recordedseek AS s ON (m.chanid = s.chanid AND m.starttime = s.starttime AND (FLOOR(m.mark / 15) + 1) = s.mark) WHERE m.chanid = ? AND m.starttime = ? AND m.type IN (?, ?) ORDER BY mark;";
 	int rows = 0;
-	int i;
 	cmyth_mysql_query_t * query;
-	cmyth_commbreak_t commbreak = NULL;
 	query = cmyth_mysql_query_create(db,query_str);
+	cmyth_commbreak_t commbreak = NULL;
 
 	if (cmyth_mysql_query_param_int(query, chanid) < 0
 		|| cmyth_mysql_query_param_str(query, start_ts_dt) < 0
@@ -762,7 +761,7 @@ cmyth_mysql_get_commbreak_list(cmyth_database_t db, int chanid, char * start_ts_
 	}
 	memset(breaklist->commbreak_list, 0, breaklist->commbreak_count * sizeof(cmyth_commbreak_t));
 
-	i = 0;
+	int i = 0;
 	while ((row = mysql_fetch_row(res))) {
 		if ((i % 2) == 0) {
 			if (safe_atoi(row[0]) != CMYTH_COMMBREAK_START) {
