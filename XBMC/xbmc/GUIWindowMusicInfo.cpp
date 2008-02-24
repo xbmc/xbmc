1353fe09d98f869c806e14e2069acaa12fd7c47d
vulkanr <vulkanr@svn>
2008-02-24 14:40:28 +0000

    added: tune in to lastfm similar artists from within the music info dialog (if lastfm is enabled)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@11782 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIWindowMusicInfo.cpp
+++ XBMC/xbmc/GUIWindowMusicInfo.cpp
@@ -27,6 +27,7 @@
 #include "Picture.h"
 #include "GUIDialogFileBrowser.h"
 #include "GUIPassword.h"
+#include "LastFmManager.h"
 
 using namespace XFILE;
 
@@ -44,6 +45,7 @@ using namespace XFILE;
 #define CONTROL_BTN_TRACKS 5
 #define CONTROL_BTN_REFRESH 6
 #define CONTROL_BTN_GET_THUMB 10
+#define CONTROL_BTN_LASTFM 11
 
 #define CONTROL_LIST 50
 
@@ -100,6 +102,15 @@ bool CGUIWindowMusicInfo::OnMessage(CGUIMessage& message)
       {
         m_bViewReview = !m_bViewReview;
         Update();
+      }
+      else if (iControl == CONTROL_BTN_LASTFM)
+      {
+        CStdString strArtist = m_album.strArtist;
+        CUtil::URLEncode(strArtist);
+        CStdString strLink;
+        strLink.Format("lastfm://artist/%s/similarartists", strArtist.c_str());
+        CURL url(strLink);
+        CLastFmManager::GetInstance()->ChangeStation(url);
       }
     }
     break;
@@ -184,7 +195,17 @@ void CGUIWindowMusicInfo::Update()
   }
 
   // disable the GetThumb button if the user isn't allowed it
-  CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_GET_THUMB, g_settings.m_vecProfiles[g_settings.m_iLastLoadedProfileIndex].canWriteDatabases() || g_passwordManager.bMasterUser)
+  CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_GET_THUMB, g_settings.m_vecProfiles[g_settings.m_iLastLoadedProfileIndex].canWriteDatabases() || g_passwordManager.bMasterUser);
+
+  if (!m_album.strArtist.IsEmpty() && CLastFmManager::GetInstance()->IsLastFmEnabled())
+  {
+    SET_CONTROL_VISIBLE(CONTROL_BTN_LASTFM);
+  }
+  else
+  {
+    SET_CONTROL_HIDDEN(CONTROL_BTN_LASTFM);
+  }
+
 }
 
 void CGUIWindowMusicInfo::SetLabel(int iControl, const CStdString& strLabel)
