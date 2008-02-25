c250e1efae62c70759c0ffa3bb3eebad1f8eb9a2
C-Quel <C-Quel@svn>
2008-02-25 12:30:03 +0000

    fixed: imdb lookups sometimes contained chars that were not utf-8 compatible (removed PCRE_UTF8 as a temporary solution)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@11790 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/RegExp.cpp
+++ xbmc/utils/RegExp.cpp
@@ -21,7 +21,8 @@ using namespace PCRE;
 CRegExp::CRegExp()
 {
   m_re          = NULL;
-  m_iOptions    = PCRE_NEWLINE_ANY | PCRE_DOTALL | PCRE_UTF8;
+//  m_iOptions    = PCRE_NEWLINE_ANY | PCRE_DOTALL | PCRE_UTF8;
+  m_iOptions    = PCRE_NEWLINE_ANY | PCRE_DOTALL;
   m_bMatched    = false;
   m_iMatchCount = 0;
 }
