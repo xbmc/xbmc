131c6ab86c114a4523e2eac0e08d2153ac8e67c8
elupus <elupus@svn>
2008-02-25 19:32:20 +0000

    merged: r11797, r11798 from linux branch into trunk (mythtv session)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@11799 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/CMythDirectory.cpp
+++ XBMC/xbmc/FileSystem/CMythDirectory.cpp
@@ -1,43 +1,32 @@
 #include "stdafx.h"
 #include "CMythDirectory.h"
+#include "CMythSession.h"
 #include "Util.h"
 #include "DllLibCMyth.h"
 
 extern "C" {
-#if defined(_XBOX) || defined(WIN32)
-#include "lib/libcmyth/cmyth.h"
-#include "lib/libcmyth/mvp_refmem.h"
-#else
 #include "../lib/libcmyth/cmyth.h"
 #include "../lib/libcmyth/mvp_refmem.h"
-#endif
 }
 
 using namespace DIRECTORY;
-
+using namespace XFILE;
+using namespace std;
 
 bool CCMythDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items)
 {
   CURL url(strPath);
   CStdString base(strPath);
   CUtil::RemoveSlashAtEnd(base);
-  DllLibCMyth dll;
-  if(!dll.Load())
+
+  CCMythSession* session = CCMythSession::AquireSession(strPath);
+  if(!session)
     return false;
 
-  dll.dbg_level(CMYTH_DBG_DETAIL);
-
-  if(url.GetHostName().IsEmpty())
-    return false;
-
-  int port = url.GetPort();
-  if(port == 0)
-    port = 6543;
-
-  cmyth_conn_t control = dll.conn_connect_ctrl((char*)url.GetHostName().c_str(), port, 16*1024, 4096);
-  if(!control)
+  DllLibCMyth* dll = session->GetLibrary();
+  if(!dll)
   {
-    CLog::Log(LOGERROR, "%s - unable to connect to server %s, port %d", __FUNCTION__, url.GetHostName().c_str(), port);
+    CCMythSession::ReleaseSession(session);
     return false;
   }
 
@@ -57,49 +46,42 @@ bool CCMythDirectory::GetDirectory(const CStdString& strPath, CFileItemList &ite
   }
   else if(url.GetFileName() == "channels/")
   {
-    CStdString user(url.GetUserName());
-    CStdString pass(url.GetPassWord());
-    if(user == "")
-      user = "mythtv";
-    if(pass == "")
-      pass = "mythtv";
-    cmyth_database_t db = dll.database_init((char*)url.GetHostName().c_str(), "mythconverg", (char*)user.c_str(), (char*)pass.c_str());
+    cmyth_database_t db = session->GetDatabase();
     if(!db)
     {
-      CLog::Log(LOGERROR, "%s - unable to connect to db with url %s", __FUNCTION__, strPath.c_str());
-      dll.ref_release(control);
-      return false;      
+      CCMythSession::ReleaseSession(session);
+      return false;
     }
-    cmyth_chanlist_t list = dll.mysql_get_chanlist(db);
+
+    cmyth_chanlist_t list = dll->mysql_get_chanlist(db);
     if(!list)
     {
       CLog::Log(LOGERROR, "%s - unable to get list of channels with url %s", __FUNCTION__, strPath.c_str());
-      dll.ref_release(db); 
-      dll.ref_release(control); 
+      CCMythSession::ReleaseSession(session);
       return false;
     }
-    int count = dll.chanlist_get_count(list);
+    int count = dll->chanlist_get_count(list);
     for(int i = 0; i < count; i++)
     {
-      cmyth_channel_t channel = dll.chanlist_get_item(list, i);
+      cmyth_channel_t channel = dll->chanlist_get_item(list, i);
       if(channel)
       {
         CStdString name, path, icon;
 
-        int num = dll.channel_channum(channel);
+        int num = dll->channel_channum(channel);
         char* str;
-        if((str = dll.channel_name(channel)))
+        if((str = dll->channel_name(channel)))
         {
           name.Format("%d - %s", num, str); 
-          dll.ref_release(str);
+          dll->ref_release(str);
         }
         else
           name.Format("%d");
 
-        if((str = dll.channel_icon(channel)))
+        if((str = dll->channel_icon(channel)))
         {
           icon = str;
-          dll.ref_release(str);
+          dll->ref_release(str);
         }
 
         path.Format("%s/%d.ts", base.c_str(), num);
@@ -116,68 +98,74 @@ bool CCMythDirectory::GetDirectory(const CStdString& strPath, CFileItemList &ite
           item->SetLabelPreformated(true);
           items.Add(item);
         }
-        dll.ref_release(channel);
+        dll->ref_release(channel);
       }
     }
-    dll.ref_release(list);
-    dll.ref_release(db);
+    dll->ref_release(list);
   }
   else if(url.GetFileName() == "recordings/")
   {
-    cmyth_proglist_t list = dll.proglist_get_all_recorded(control);
+    cmyth_conn_t control = session->GetControl();
+    if(!control)
+    {
+      CCMythSession::ReleaseSession(session);
+      return false;
+    }
+
+    cmyth_proglist_t list = dll->proglist_get_all_recorded(control);
     if(!list)
     {
       CLog::Log(LOGERROR, "%s - unable to get list of recordings", __FUNCTION__);
-      dll.ref_release(control);
+      CCMythSession::ReleaseSession(session);
       return false;
     }
-    int count = dll.proglist_get_count(list);
+    int count = dll->proglist_get_count(list);
     for(int i=0; i<count; i++)
     {
-      cmyth_proginfo_t program = dll.proglist_get_item(list, i);
+      cmyth_proginfo_t program = dll->proglist_get_item(list, i);
       if(program)
       {
         char* str;
-        if((str = dll.proginfo_recgroup(program)))
+        if((str = dll->proginfo_recgroup(program)))
         {
           if(strcmp(str, "LiveTV") == 0)
           {
-            dll.ref_release(str);
-            dll.ref_release(program);
+            dll->ref_release(str);
+            dll->ref_release(program);
             continue;
           }
         }
 
-        if((str = dll.proginfo_pathname(program)))
+        if((str = dll->proginfo_pathname(program)))
         {
           CStdString recording, name, path;
 
           recording = CUtil::GetFileName(str);
-          dll.ref_release(str);
+          dll->ref_release(str);
 
-          if((str = dll.proginfo_title(program)))
+          if((str = dll->proginfo_title(program)))
           {
             name = str;
-            dll.ref_release(str);
+            dll->ref_release(str);
           }
           else
             name = recording;
 
-          CLog::Log(LOGDEBUG, "%s - recording %s (%s) has status %d", __FUNCTION__, name.c_str(), recording.c_str(), dll.proginfo_rec_status(program));
+          CLog::Log(LOGDEBUG, "%s - recording %s (%s) has status %d", __FUNCTION__, name.c_str(), recording.c_str(), dll->proginfo_rec_status(program));
           path.Format("%s/%s", base.c_str(), recording.c_str());
 
           CFileItem *item = new CFileItem(path, false);
           item->SetLabel(name);
 
-          if(dll.proginfo_rec_status(program) == RS_RECORDING)
+          if(dll->proginfo_rec_status(program) == RS_RECORDING)
             item->SetLabel2("(Recording)");
           else
           {
-            cmyth_timestamp_t start = dll.proginfo_rec_start(program);
-            cmyth_timestamp_t end = dll.proginfo_rec_end(program);
-            double diff = difftime(dll.timestamp_to_unixtime(end), dll.timestamp_to_unixtime(start));
-            dll.ref_release(start);
-            dll.ref_release(end);
+            cmyth_timestamp_t start = dll->proginfo_rec_start(program);
+            cmyth_timestamp_t end = dll->proginfo_rec_end(program);
+            double diff = difftime(dll->timestamp_to_unixtime(end), dll->timestamp_to_unixtime(start));
+            dll->ref_release(start);
+            dll->ref_release(end);
 
             CStdString time;
             StringUtils::SecondsToTimeString((long)diff, time, TIME_FORMAT_GUESS);
@@ -187,11 +175,11 @@ bool CCMythDirectory::GetDirectory(const CStdString& strPath, CFileItemList &ite
           item->SetLabelPreformated(true);
           items.Add(item);
         }
-        dll.ref_release(program);
+        dll->ref_release(program);
       }
     }
-    dll.ref_release(list);
+    dll->ref_release(list);
   }
-
+  CCMythSession::ReleaseSession(session);
   return true;
 }
---
+++ XBMC/xbmc/FileSystem/CMythSession.h
@@ -0,0 +1,60 @@
+#pragma once
+
+typedef struct cmyth_ringbuf   *cmyth_ringbuf_t;
+typedef struct cmyth_conn      *cmyth_conn_t;
+typedef struct cmyth_recorder  *cmyth_recorder_t;
+typedef struct cmyth_proginfo  *cmyth_proginfo_t;
+typedef struct cmyth_proglist  *cmyth_proglist_t;
+typedef struct cmyth_file      *cmyth_file_t;
+typedef struct cmyth_database  *cmyth_database_t;
+
+class DllLibCMyth;
+
+namespace XFILE
+{
+
+class CCMythSession
+  : private CThread
+{
+public:
+  static CCMythSession* AquireSession(const CURL& url);
+  static void           ReleaseSession(CCMythSession*);
+  static void           CheckIdle();
+
+  class IEventListener
+  {
+  public:
+    virtual ~IEventListener() {};
+    virtual void OnEvent(int event, const std::string& data)=0;
+  };
+
+  bool             SetListener(IEventListener *listener);
+  cmyth_conn_t     GetControl();
+  cmyth_database_t GetDatabase();
+  DllLibCMyth*     GetLibrary() { return m_dll; }
+private:
+  CCMythSession(const CURL& url);
+  ~CCMythSession();
+
+  virtual void Process();
+
+  bool             CanSupport(const CURL& url);
+  void             Disconnect();
+
+  IEventListener*  m_listener;
+  cmyth_conn_t     m_control;
+  cmyth_conn_t     m_event;
+  cmyth_database_t m_database;
+  CStdString       m_hostname;
+  CStdString       m_username;
+  CStdString       m_password;
+  int              m_port;
+  DllLibCMyth*     m_dll;
+  CCriticalSection m_section;
+  DWORD            m_timestamp;
+
+  static CCriticalSection            m_section_session;
+  static std::vector<CCMythSession*> m_sessions;
+};
+
+}
