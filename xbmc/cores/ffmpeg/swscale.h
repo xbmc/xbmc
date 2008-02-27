0593426022d9c89e8ca4c84bb695d25d88206b5c
vulkanr <vulkanr@svn>
2008-02-27 13:41:27 +0000

    update ffmpeg libs to rev. 12265.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11827 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/ffmpeg/swscale.h
+++ xbmc/cores/ffmpeg/swscale.h
@@ -29,11 +29,16 @@
 
 #include "avutil.h"
 
-#define AV_STRINGIFY(s)         AV_TOSTRING(s)
-#define AV_TOSTRING(s) #s
+#define LIBSWSCALE_VERSION_MAJOR 0
+#define LIBSWSCALE_VERSION_MINOR 5
+#define LIBSWSCALE_VERSION_MICRO 0
 
-#define LIBSWSCALE_VERSION_INT  ((0<<16)+(5<<8)+0)
-#define LIBSWSCALE_VERSION      0.5.0
+#define LIBSWSCALE_VERSION_INT  AV_VERSION_INT(LIBSWSCALE_VERSION_MAJOR, \
+                                               LIBSWSCALE_VERSION_MINOR, \
+                                               LIBSWSCALE_VERSION_MICRO)
+#define LIBSWSCALE_VERSION      AV_VERSION(LIBSWSCALE_VERSION_MAJOR, \
+                                           LIBSWSCALE_VERSION_MINOR, \
+                                           LIBSWSCALE_VERSION_MICRO)
 #define LIBSWSCALE_BUILD        LIBSWSCALE_VERSION_INT
 
 #define LIBSWSCALE_IDENT        "SwS" AV_STRINGIFY(LIBSWSCALE_VERSION)
