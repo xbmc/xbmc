fcae853c530633bd9624cfd7fc48af3aaf532aa4
topfs2 <topfs2@svn>
2008-03-02 18:45:29 +0000

    Fixed: Removable media should not show up in library mode.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11861 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoNav.cpp
+++ xbmc/GUIWindowVideoNav.cpp
@@ -88,6 +88,8 @@ bool CGUIWindowVideoNav::OnMessage(CGUIMessage& message)
     break;
   case GUI_MSG_WINDOW_INIT:
     {
+/* We don't want to show Autosourced items (ie removable pendrives, memorycards) in Library mode */
+      m_rootDir.AllowNonLocalShares(false);
       // check for valid quickpath parameter
       CStdString strDestination = message.GetStringParam();
       if (!strDestination.IsEmpty())
