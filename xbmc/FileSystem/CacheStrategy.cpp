c2e8b4e439b196eefae072135b6dddc17716e865
leafmuncher <leafmuncher@svn>
2008-03-08 10:29:57 +0000

    fixed: [OS X] Compile errors.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@11946 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/CacheStrategy.cpp
+++ xbmc/FileSystem/CacheStrategy.cpp
@@ -18,6 +18,14 @@
 #include "../utils/log.h"
 #include "../utils/SingleLock.h"
 
+#ifndef PRId64
+#ifdef _MSC_VER
+#define PRId64 "I64d"
+#else
+#define PRId64 "lld"
+#endif
+#endif
+
 namespace XFILE {
 
 CCacheStrategy::CCacheStrategy() : m_bEndOfInput(false)
