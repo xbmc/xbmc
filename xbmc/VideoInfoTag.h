537fe97be515646695f1c6b7ecda82ce6db1d5da
nuka1195 <nuka1195@svn>
2008-03-14 01:20:00 +0000

    changed: moved scraper settings to a separate cpp file (backport from linux branch 12059)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12083 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoInfoTag.h
+++ xbmc/VideoInfoTag.h
@@ -3,19 +3,9 @@
 #include "utils/Archive.h"
 #include "utils/ScraperParser.h"
 #include "PluginSettings.h"
+#include "ScraperSettings.h"
 
 #include <vector>
-
-class CScraperSettings : public CBasicSettings
-{
-public:
-  CScraperSettings();
-  virtual ~CScraperSettings();
-  bool LoadUserXML(const CStdString& strXML);
-  bool LoadSettingsXML(const CStdString& strScraper, const CStdString& strFunction="GetSettings", const CScraperUrl* url=NULL);
-  bool Load(const CStdString& strSettings, const CStdString& strSaved);
-  CStdString GetSettings() const;
-};
 
 struct SScraperInfo
 {
