3fb3e4ae529e6d03db70f0fe6b6dcda2b539df18
vulkanr <vulkanr@svn>
2008-03-20 12:54:28 +0000

    dts on linuxport wasnt working well. downmix to 2 channels when not in passthrough - like on apple (removed the ifdef).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@12215 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecLibDts.cpp
+++ XBMC/xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecLibDts.cpp
@@ -271,12 +271,9 @@ int CDVDAudioCodecLibDts::Decode(BYTE* pData, int iSize)
       {
         // Now that we've synced, get the number of channels.
         m_iOutputChannels = GetNrOfChannels(m_iFlags);
-        
-#ifdef __APPLE__
         // If we're not passing through, stay on 2-channel mixdown.
         if (g_audioContext.IsPassthroughActive() == false)
           m_iOutputChannels = 2;
-#endif
       }
     }
     
