cdaefb7c59df6bd116ca5b2e877e156fea572479
elupus <elupus@svn>
2008-03-22 23:22:54 +0000

    changed: curl http client now won't loose it's state on a seek against a server that doesn't support ranged requests. before the failed seek would lead to a lost connection, thus it was vital to know if server would accept seeks beforehand.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12242 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FileCurl.h
+++ xbmc/FileSystem/FileCurl.h
@@ -22,18 +22,15 @@ namespace XFILE
 	    virtual ~CFileCurl();
 	    virtual bool Open(const CURL& url, bool bBinary = true);
 	    virtual bool Exists(const CURL& url);
-      virtual bool ReadString(char *szLine, int iLineLength);
 	    virtual __int64	Seek(__int64 iFilePosition, int iWhence=SEEK_SET);
       virtual __int64 GetPosition();
 	    virtual __int64	GetLength();
       virtual int	Stat(const CURL& url, struct __stat64* buffer);
 	    virtual void Close();
-      virtual unsigned int Read(void* lpBuf, __int64 uiBufSize);
-      virtual CStdString GetContent()                            { return m_httpheader.GetContentType(); }
-      
-      size_t WriteCallback(char *buffer, size_t size, size_t nitems);
-      size_t HeaderCallback(void *ptr, size_t size, size_t nmemb);
-      
+      virtual bool ReadString(char *szLine, int iLineLength)     { return m_state->ReadString(szLine, iLineLength); }
+      virtual unsigned int Read(void* lpBuf, __int64 uiBufSize)  { return m_state->Read(lpBuf, uiBufSize); }
+      virtual CStdString GetContent()                            { return m_state->m_httpheader.GetContentType(); }
+            
       void SetUserAgent(CStdString sUserAgent)                   { m_userAgent = sUserAgent; }
       void SetProxy(CStdString &proxy)                           { m_proxy = proxy; }
       void SetCustomRequest(CStdString &request)                 { m_customrequest = request; }
@@ -47,22 +44,57 @@ namespace XFILE
       void ClearRequestHeaders();
       void SetBufferSize(unsigned int size);
 
-      const CHttpHeader& GetHttpHeader() { return m_httpheader; }
+      const CHttpHeader& GetHttpHeader() { return m_state->m_httpheader; }
 
       /* static function that will get content type of a file */      
       static bool GetHttpHeader(const CURL &url, CHttpHeader &headers);
       static bool GetContent(const CURL &url, CStdString &content);
+
+      class CReadState
+      {
+      public:
+          CReadState();
+          ~CReadState();
+          XCURL::CURL_HANDLE*    m_easyHandle;
+          XCURL::CURLM*          m_multiHandle;
+
+          CRingHoldBuffer m_buffer;           // our ringhold buffer
+          unsigned int    m_bufferSize;
+
+          char *          m_overflowBuffer;   // in the rare case we would overflow the above buffer
+          unsigned int    m_overflowSize;     // size of the overflow buffer
+          int             m_stillRunning; /* Is background url fetch still in progress */
+
+          __int64         m_fileSize;
+          __int64	        m_filePos;
+
+          bool            m_connected;
+
+          /* returned http header */
+          CHttpHeader m_httpheader;
+
+          size_t WriteCallback(char *buffer, size_t size, size_t nitems);
+          size_t HeaderCallback(void *ptr, size_t size, size_t nmemb);
+
+          bool         Seek(__int64 pos);
+          unsigned int Read(void* lpBuf, __int64 uiBufSize);
+          bool         ReadString(char *szLine, int iLineLength);
+          bool         FillBuffer(unsigned int want);
+
+          long         Connect(unsigned int size);
+          void         Disconnect();
+      };
+
     protected:
       void ParseAndCorrectUrl(CURL &url);
-      void SetCommonOptions();
-      void SetRequestHeaders();
-
-      bool FillBuffer(unsigned int want);
+      void SetCommonOptions(CReadState* state);
+      void SetRequestHeaders(CReadState* state);
+      void SetCorrectHeaders(CReadState* state);
 
     private:
-      
-      XCURL::CURL_HANDLE*    m_easyHandle;
-      XCURL::CURLM*          m_multiHandle;
+      CReadState*     m_state;
+      unsigned int    m_bufferSize;
+
       CStdString      m_url;
       CStdString      m_userAgent;
       CStdString      m_proxy;
@@ -72,28 +104,16 @@ namespace XFILE
       CStdString      m_ftpport;
       bool            m_ftppasvip;
       int             m_timeout;
-
-      __int64         m_fileSize;
-      __int64	      m_filePos;
       bool            m_opened;
       bool            m_useOldHttpVersion;
       bool            m_seekable;
-
-      CRingHoldBuffer m_buffer;           // our ringhold buffer
-      char *          m_overflowBuffer;   // in the rare case we would overflow the above buffer
-      unsigned int    m_overflowSize;     // size of the overflow buffer
-      unsigned int    m_bufferSize;
-
-      int             m_stillRunning; /* Is background url fetch still in progress */
+      bool            m_binary;
 
       struct XCURL::curl_slist* m_curlAliasList;
       struct XCURL::curl_slist* m_curlHeaderList;
       
       typedef std::map<CStdString, CStdString> MAPHTTPHEADERS;
       MAPHTTPHEADERS m_requestheaders;
-
-      /* returned http header */
-      CHttpHeader m_httpheader;
   };
 }
 
