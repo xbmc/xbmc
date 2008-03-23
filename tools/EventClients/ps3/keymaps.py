197263ab22f6d8abb245398118e3a17027d6a353
d4rkm4ster <d4rkm4ster@svn>
2008-03-23 20:29:26 +0000

    updated: cleaned up the ps3 remote program.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12262 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ tools/EventClients/ps3/keymaps.py
@@ -0,0 +1,64 @@
+# PS3 Remote and Controller Keymaps
+
+keymap_remote = {
+    "16": 's' ,#EJECT
+    "64": 'w' ,#AUDIO
+    "65": 'z' ,#ANGLE
+    "63": 'n' ,#SUBTITLE
+    "0f": 'd' ,#CLEAR
+    "28": 't' ,#TIME
+
+    "00": '1' ,#1
+    "01": '2' ,#2
+    "02": '3' ,#3
+    "03": '4' ,#4
+    "04": '5' ,#5
+    "05": '6' ,#6
+    "06": '7' ,#7
+    "07": '8' ,#8
+    "08": '9' ,#9
+    "09": '0' ,#0
+
+    "81": None ,#RED
+    "82": None ,#GREEN
+    "80": None ,#BLUE
+    "83": None ,#YELLOW
+
+    "70": 'I'      ,#DISPLAY
+    "1a": None     ,#TOP MENU
+    "40": 'menu'   ,#POP UP/MENU
+    "0e": 'escape' ,#RETURN
+
+    "5c": 'menu'   ,#OPTIONS/TRIANGLE
+    "5d": 'escape' ,#BACK/CIRCLE
+    "5e": 'tab'    ,#X
+    "5f": 'V'      ,#VIEW/SQUARE
+
+    "54": 'up'     ,#UP
+    "55": 'right'  ,#RIGHT
+    "56": 'down'   ,#DOWN
+    "57": 'left'   ,#LEFT
+    "0b": 'return' ,#ENTER
+
+    "5a": 'plus'     ,#L1
+    "58": 'minus'    ,#L2
+    "51": None       ,#L3
+    "5b": 'pageup'   ,#R1
+    "59": 'pagedown' ,#R2
+    "52": 'c'        ,#R3
+
+    "43": 's'                  ,#PLAYSTATION
+    "50": 'opensquarebracket'  ,#SELECT
+    "53": 'closesquarebracket' ,#START
+
+    "33": 'r'      ,#<-SCAN
+    "34": 'f'      ,#  SCAN->
+    "30": 'comma'  ,#PREV
+    "31": 'period' ,#NEXT
+    "60": None     ,#<-SLOW/STEP
+    "61": None     ,#  SLOW/STEP->
+    "32": 'P'      ,#PLAY
+    "38": 'x'      ,#STOP
+    "39": 'space'  #PAUSE
+    }
+
