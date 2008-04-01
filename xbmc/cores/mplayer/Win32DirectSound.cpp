24b111c19b25390852b9e990c1c04f4e0186e7d2
wiso <wiso@svn>
2008-04-01 21:07:15 +0000

    [win32]: removed tabs from Win32DirectSound.cpp
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12426 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/mplayer/Win32DirectSound.cpp
+++ xbmc/cores/mplayer/Win32DirectSound.cpp
@@ -58,43 +58,43 @@ CWin32DirectSound::CWin32DirectSound(IAudioCallback* pCallback, int iChannels, u
   
   WAVEFORMATEXTENSIBLE wfxex = {0};
   DSBUFFERDESC dsbpridesc;
-	DSBUFFERDESC dsbdesc;
+  DSBUFFERDESC dsbdesc;
 
   //fill waveformatex
-	ZeroMemory(&wfxex, sizeof(WAVEFORMATEXTENSIBLE));
-	wfxex.Format.cbSize          = (iChannels > 2) ? sizeof(WAVEFORMATEXTENSIBLE)-sizeof(WAVEFORMATEX) : 0;
-	wfxex.Format.nChannels       = iChannels;
-	wfxex.Format.nSamplesPerSec  = uiSamplesPerSec;
-	if (bAudioPassthrough == true) 
+  ZeroMemory(&wfxex, sizeof(WAVEFORMATEXTENSIBLE));
+  wfxex.Format.cbSize          = (iChannels > 2) ? sizeof(WAVEFORMATEXTENSIBLE)-sizeof(WAVEFORMATEX) : 0;
+  wfxex.Format.nChannels       = iChannels;
+  wfxex.Format.nSamplesPerSec  = uiSamplesPerSec;
+  if (bAudioPassthrough == true) 
   {
-		wfxex.Format.wFormatTag      = WAVE_FORMAT_DOLBY_AC3_SPDIF;
-		wfxex.Format.wBitsPerSample  = 16;
-		wfxex.Format.nBlockAlign     = 4;
-	} 
+    wfxex.Format.wFormatTag      = WAVE_FORMAT_DOLBY_AC3_SPDIF;
+    wfxex.Format.wBitsPerSample  = 16;
+    wfxex.Format.nBlockAlign     = 4;
+  } 
   else 
   {
-		wfxex.Format.wFormatTag      = (iChannels > 2) ? WAVE_FORMAT_EXTENSIBLE : WAVE_FORMAT_PCM;
-		wfxex.Format.wBitsPerSample  = uiBitsPerSample;
-		wfxex.Format.nBlockAlign     = wfxex.Format.nChannels * (wfxex.Format.wBitsPerSample >> 3);
-	}
+    wfxex.Format.wFormatTag      = (iChannels > 2) ? WAVE_FORMAT_EXTENSIBLE : WAVE_FORMAT_PCM;
+    wfxex.Format.wBitsPerSample  = uiBitsPerSample;
+    wfxex.Format.nBlockAlign     = wfxex.Format.nChannels * (wfxex.Format.wBitsPerSample >> 3);
+  }
 
   // unsure if this are the right values
   m_dwPacketSize = wfxex.Format.nBlockAlign * 3096;
   m_dwNumPackets = 16;
 
   // fill in primary sound buffer descriptor
-	memset(&dsbpridesc, 0, sizeof(DSBUFFERDESC));
-	dsbpridesc.dwSize = sizeof(DSBUFFERDESC);
-	dsbpridesc.dwFlags       = DSBCAPS_PRIMARYBUFFER;
-	dsbpridesc.dwBufferBytes = 0;
-	dsbpridesc.lpwfxFormat   = NULL;
+  memset(&dsbpridesc, 0, sizeof(DSBUFFERDESC));
+  dsbpridesc.dwSize = sizeof(DSBUFFERDESC);
+  dsbpridesc.dwFlags       = DSBCAPS_PRIMARYBUFFER;
+  dsbpridesc.dwBufferBytes = 0;
+  dsbpridesc.lpwfxFormat   = NULL;
 
   // fill in the secondary sound buffer (=stream buffer) descriptor
-	memset(&dsbdesc, 0, sizeof(DSBUFFERDESC));
-	dsbdesc.dwSize = sizeof(DSBUFFERDESC);
-	dsbdesc.dwFlags = DSBCAPS_GETCURRENTPOSITION2 /** Better position accuracy */
-	                | DSBCAPS_GLOBALFOCUS         /** Allows background playing */
-	                | DSBCAPS_CTRLVOLUME;         /** volume control enabled */
+  memset(&dsbdesc, 0, sizeof(DSBUFFERDESC));
+  dsbdesc.dwSize = sizeof(DSBUFFERDESC);
+  dsbdesc.dwFlags = DSBCAPS_GETCURRENTPOSITION2 /** Better position accuracy */
+                  | DSBCAPS_GLOBALFOCUS         /** Allows background playing */
+                  | DSBCAPS_CTRLVOLUME;         /** volume control enabled */
 
   const int channel_mask[] = 
   {
@@ -104,21 +104,21 @@ CWin32DirectSound::CWin32DirectSound(IAudioCallback* pCallback, int iChannels, u
     SPEAKER_FRONT_LEFT   | SPEAKER_FRONT_CENTER | SPEAKER_FRONT_RIGHT  | SPEAKER_BACK_LEFT    | SPEAKER_BACK_RIGHT     | SPEAKER_LOW_FREQUENCY
   };
 
-	if (iChannels > 2) 
+  if (iChannels > 2) 
   {
-		wfxex.dwChannelMask = channel_mask[iChannels - 3];
-		wfxex.SubFormat = KSDATAFORMAT_SUBTYPE_PCM;
-		wfxex.Samples.wValidBitsPerSample = wfxex.Format.wBitsPerSample;
-		// Needed for 5.1 on emu101k - shit soundblaster
-		dsbdesc.dwFlags |= DSBCAPS_LOCHARDWARE;
-	}
+    wfxex.dwChannelMask = channel_mask[iChannels - 3];
+    wfxex.SubFormat = KSDATAFORMAT_SUBTYPE_PCM;
+    wfxex.Samples.wValidBitsPerSample = wfxex.Format.wBitsPerSample;
+    // Needed for 5.1 on emu101k - shit soundblaster
+    dsbdesc.dwFlags |= DSBCAPS_LOCHARDWARE;
+  }
 
-	wfxex.Format.nAvgBytesPerSec = wfxex.Format.nSamplesPerSec * wfxex.Format.nBlockAlign;
+  wfxex.Format.nAvgBytesPerSec = wfxex.Format.nSamplesPerSec * wfxex.Format.nBlockAlign;
 
   //dsbdesc.dwBufferBytes = iChannels * uiSamplesPerSec * (wfxex.Format.wBitsPerSample >> 3); // space for 1 sec
   dsbdesc.dwBufferBytes = m_dwNumPackets * m_dwPacketSize;
 
-	dsbdesc.lpwfxFormat = (WAVEFORMATEX *)&wfxex;
+  dsbdesc.lpwfxFormat = (WAVEFORMATEX *)&wfxex;
 
   // create primary buffer and set its format
   HRESULT res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbpridesc, &m_pBufferPri, NULL);
@@ -126,8 +126,8 @@ CWin32DirectSound::CWin32DirectSound(IAudioCallback* pCallback, int iChannels, u
   {
     CLog::Log(LOGERROR, __FUNCTION__" - cannot create primary buffer (%s)", dserr2str(res));
     SAFE_RELEASE(m_pBufferPri);
-		return;
-	}
+    return;
+  }
 
   res = IDirectSoundBuffer_SetFormat( m_pBufferPri, (WAVEFORMATEX *)&wfxex );
   if ( res != DS_OK ) 
@@ -136,32 +136,32 @@ CWin32DirectSound::CWin32DirectSound(IAudioCallback* pCallback, int iChannels, u
   CLog::Log(LOGERROR, __FUNCTION__" - primary sound buffer created");
 
   // now create the stream buffer
-	res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
+  res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
 
   if (res != DS_OK) 
   {
-		if (dsbdesc.dwFlags & DSBCAPS_LOCHARDWARE) 
+    if (dsbdesc.dwFlags & DSBCAPS_LOCHARDWARE) 
     {
       SAFE_RELEASE(m_pBuffer);
-			// Try without DSBCAPS_LOCHARDWARE
-			dsbdesc.dwFlags &= ~DSBCAPS_LOCHARDWARE;
-			res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
-		}
+      // Try without DSBCAPS_LOCHARDWARE
+      dsbdesc.dwFlags &= ~DSBCAPS_LOCHARDWARE;
+      res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
+    }
     if (res != DS_OK && dsbdesc.dwFlags & DSBCAPS_CTRLVOLUME) 
     {
       SAFE_RELEASE(m_pBuffer);
-			// Try without DSBCAPS_CTRLVOLUME
-			dsbdesc.dwFlags &= ~DSBCAPS_CTRLVOLUME;
-			res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
-		}
-		if (res != DS_OK) 
+      // Try without DSBCAPS_CTRLVOLUME
+      dsbdesc.dwFlags &= ~DSBCAPS_CTRLVOLUME;
+      res = IDirectSound_CreateSoundBuffer(m_pDSound, &dsbdesc, &m_pBuffer, NULL);
+    }
+    if (res != DS_OK) 
     {
       SAFE_RELEASE(m_pBuffer);
       SAFE_RELEASE(m_pBufferPri);
       CLog::Log(LOGERROR, __FUNCTION__" - cannot create secondary (stream)buffer (%s)", dserr2str(res));
-			return;
-		}
-	}
+      return;
+    }
+  }
   CLog::Log(LOGERROR, __FUNCTION__" - secondary sound (stream)buffer created");
 
   
@@ -408,26 +408,27 @@ void CWin32DirectSound::SwitchChannels(int iAudioStream, bool bAudioOnAllSpeaker
 
 char * CWin32DirectSound::dserr2str(int err)
 {
-	switch (err) {
-		case DS_OK: return "DS_OK";
-		case DS_NO_VIRTUALIZATION: return "DS_NO_VIRTUALIZATION";
-		case DSERR_ALLOCATED: return "DS_NO_VIRTUALIZATION";
-		case DSERR_CONTROLUNAVAIL: return "DSERR_CONTROLUNAVAIL";
-		case DSERR_INVALIDPARAM: return "DSERR_INVALIDPARAM";
-		case DSERR_INVALIDCALL: return "DSERR_INVALIDCALL";
-		case DSERR_GENERIC: return "DSERR_GENERIC";
-		case DSERR_PRIOLEVELNEEDED: return "DSERR_PRIOLEVELNEEDED";
-		case DSERR_OUTOFMEMORY: return "DSERR_OUTOFMEMORY";
-		case DSERR_BADFORMAT: return "DSERR_BADFORMAT";
-		case DSERR_UNSUPPORTED: return "DSERR_UNSUPPORTED";
-		case DSERR_NODRIVER: return "DSERR_NODRIVER";
-		case DSERR_ALREADYINITIALIZED: return "DSERR_ALREADYINITIALIZED";
-		case DSERR_NOAGGREGATION: return "DSERR_NOAGGREGATION";
-		case DSERR_BUFFERLOST: return "DSERR_BUFFERLOST";
-		case DSERR_OTHERAPPHASPRIO: return "DSERR_OTHERAPPHASPRIO";
-		case DSERR_UNINITIALIZED: return "DSERR_UNINITIALIZED";
-		case DSERR_NOINTERFACE: return "DSERR_NOINTERFACE";
-		case DSERR_ACCESSDENIED: return "DSERR_ACCESSDENIED";
-		default: return "unknown";
-	}
+  switch (err) 
+  {
+    case DS_OK: return "DS_OK";
+    case DS_NO_VIRTUALIZATION: return "DS_NO_VIRTUALIZATION";
+    case DSERR_ALLOCATED: return "DS_NO_VIRTUALIZATION";
+    case DSERR_CONTROLUNAVAIL: return "DSERR_CONTROLUNAVAIL";
+    case DSERR_INVALIDPARAM: return "DSERR_INVALIDPARAM";
+    case DSERR_INVALIDCALL: return "DSERR_INVALIDCALL";
+    case DSERR_GENERIC: return "DSERR_GENERIC";
+    case DSERR_PRIOLEVELNEEDED: return "DSERR_PRIOLEVELNEEDED";
+    case DSERR_OUTOFMEMORY: return "DSERR_OUTOFMEMORY";
+    case DSERR_BADFORMAT: return "DSERR_BADFORMAT";
+    case DSERR_UNSUPPORTED: return "DSERR_UNSUPPORTED";
+    case DSERR_NODRIVER: return "DSERR_NODRIVER";
+    case DSERR_ALREADYINITIALIZED: return "DSERR_ALREADYINITIALIZED";
+    case DSERR_NOAGGREGATION: return "DSERR_NOAGGREGATION";
+    case DSERR_BUFFERLOST: return "DSERR_BUFFERLOST";
+    case DSERR_OTHERAPPHASPRIO: return "DSERR_OTHERAPPHASPRIO";
+    case DSERR_UNINITIALIZED: return "DSERR_UNINITIALIZED";
+    case DSERR_NOINTERFACE: return "DSERR_NOINTERFACE";
+    case DSERR_ACCESSDENIED: return "DSERR_ACCESSDENIED";
+    default: return "unknown";
+  }
 }
