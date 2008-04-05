de9c1964388d9ed413d10b001c33cc5e95532297
leafmuncher <leafmuncher@svn>
2008-04-05 19:32:42 +0000

    added: [OS X] support for Python wrapping on OS X.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12514 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/linux/wrapper_python.c
+++ xbmc/lib/libPython/linux/wrapper_python.c
@@ -24,38 +24,49 @@ FILE* xbp_fopen(const char *filename, const char *mode);
 FILE* xbp_freopen(const char *path, const char *mode, FILE *stream);
 FILE* xbp_fopen64(const char *filename, const char *mode);
 DIR *xbp_opendir(const char *name);
+#ifdef __APPLE__
+int xbp_stat(const char * path, struct stat * buf);
+int xbp_lstat(const char * path, struct stat * buf);
+#else
 int xbp__xstat64(int ver, const char *filename, struct stat64 *stat_buf);
 int xbp__lxstat64(int ver, const char *filename, struct stat64 *stat_buf);
+#endif
 void *xbp_dlopen(const char *filename, int flag);
 int xbp_dlclose(void *handle);
 void *xbp_dlsym(void *handle, const char *symbol);
 
-FILE *__wrap_fopen64(const char *path, const char *mode)
+#ifdef __APPLE__
+#define PYTHON_WRAP(func) __py_wrap_##func
+#else
+#define PYTHON_WRAP(func) __wrap_##func
+#endif
+
+FILE *PYTHON_WRAP(fopen64)(const char *path, const char *mode)
 {
   return xbp_fopen64(path, mode);
 }
 
-DIR* __wrap_opendir(const char *name)
+DIR* PYTHON_WRAP(opendir)(const char *name)
 {
   return xbp_opendir(name);
 }
 
-int __wrap_access(const char* path, int mode)
+int PYTHON_WRAP(access)(const char* path, int mode)
 {
   return xbp_access(path, mode);
 }
 
-char* __wrap_getcwd(char *buf, int size)
+char* PYTHON_WRAP(getcwd)(char *buf, int size)
 {
   return xbp_getcwd(buf, size);
 }
 
-int __wrap_chdir(const char *dirname)
+int PYTHON_WRAP(chdir)(const char *dirname)
 {
   return xbp_chdir(dirname);
 }
 
-int __wrap_open(const char *file, int oflag, ...)
+int PYTHON_WRAP(open)(const char *file, int oflag, ...)
 {
   if (oflag & O_CREAT) 
   {
@@ -74,46 +85,57 @@ int __wrap_open(const char *file, int oflag, ...)
 	  return xbp_open(file, oflag | O_NONBLOCK, 0);
 }
 
-FILE *__wrap_fopen(const char *path, const char *mode)
+FILE *PYTHON_WRAP(fopen)(const char *path, const char *mode)
 {
   return xbp_fopen(path, mode);
 }
 
-FILE *__wrap_freopen(const char *path, const char *mode, FILE *stream)
+FILE *PYTHON_WRAP(freopen)(const char *path, const char *mode, FILE *stream)
 {
   return xbp_freopen(path, mode, stream);
 }
 
-int __wrap_unlink(const char *filename)
+int PYTHON_WRAP(unlink)(const char *filename)
 {
   return xbp_unlink(filename);
 }
 
-int __wrap_chmod(const char *filename, int pmode)
+int PYTHON_WRAP(chmod)(const char *filename, int pmode)
 {
   return xbp_chmod(filename, pmode);
 }
 
-int __wrap_rmdir(const char *dirname)
+int PYTHON_WRAP(rmdir)(const char *dirname)
 {
   return xbp_rmdir(dirname);
 }
 
-int __wrap_utime(const char *filename, struct utimbuf *times)
+int PYTHON_WRAP(utime)(const char *filename, struct utimbuf *times)
 {
   return xbp_utime(filename, times);
 }
 
-int __wrap_rename(const char *oldname, const char *newname)
+int PYTHON_WRAP(rename)(const char *oldname, const char *newname)
 {
   return xbp_rename(oldname, newname);
 }
 
-int __wrap_mkdir(const char *dirname)
+int PYTHON_WRAP(mkdir)(const char *dirname)
 {
   return xbp_mkdir(dirname);
 }
 
+#ifdef __APPLE__
+int PYTHON_WRAP(stat)(const char * path, struct stat * buf)
+{
+  return xbp_stat(path, buf);
+}
+
+int PYTHON_WRAP(lstat)(const char * path, struct stat * buf)
+{
+  return xbp_lstat(path, buf);
+}
+#else
 int __wrap___xstat64(int ver, const char *filename, struct stat64 *stat_buf)
 {
   return xbp__xstat64(ver, filename, stat_buf);
@@ -123,18 +145,19 @@ int __wrap___lxstat64(int ver, const char *filename, struct stat64 *stat_buf)
 {
   return xbp__lxstat64(ver, filename, stat_buf);
 }
+#endif
 
-void *__wrap_dlopen(const char *filename, int flag)
+void *PYTHON_WRAP(dlopen)(const char *filename, int flag)
 {
   return xbp_dlopen(filename,flag);
 }
 
-int __wrap_dlclose(void *handle)
+int PYTHON_WRAP(dlclose)(void *handle)
 {
   return xbp_dlclose(handle);
 }
 
-void *__wrap_dlsym(void *handle, const char *symbol)
+void *PYTHON_WRAP(dlsym)(void *handle, const char *symbol)
 {
   return xbp_dlsym(handle, symbol);
 }
