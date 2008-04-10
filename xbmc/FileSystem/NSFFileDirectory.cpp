3fef12d92a4fcd817313a0c37047dcff81e5d289
spiff_ <spiff_@svn>
2008-04-10 20:57:10 +0000

    changed: reduced code duplication a bit
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12624 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/NSFFileDirectory.cpp
+++ xbmc/FileSystem/NSFFileDirectory.cpp
@@ -1,77 +1,26 @@
 
 #include "stdafx.h"
-#include "Util.h"
 #include "NSFFileDirectory.h"
 #include "MusicInfoTagLoaderNSF.h"
-#include "DirectoryCache.h"
 #include "MusicInfoTag.h"
-#include "URL.h"
-#include "FileItem.h"
 
 using namespace MUSIC_INFO;
 using namespace DIRECTORY;
 
 CNSFFileDirectory::CNSFFileDirectory(void)
-{}
+{
+  m_strExt = "nfsstream";
+}
 
 CNSFFileDirectory::~CNSFFileDirectory(void)
-{}
-
-bool CNSFFileDirectory::GetDirectory(const CStdString& strPath1, CFileItemList &items)
 {
-  CFileItemList vecCacheItems;
-  g_directoryCache.ClearDirectory(strPath1);
-  
-  CStdString strPath=strPath1;
-  CURL url(strPath);
-
-  CMusicInfoTagLoaderNSF nsf;
-
-  //  The ogg file name is the new name for our virtual
-  //  ogg directory
-  CStdString strFileName=CUtil::GetFileName(strPath);
-  int iStreams = nsf.GetStreamCount(strPath1);
-  CMusicInfoTag tag;
-  nsf.Load(strPath1,tag);
-  tag.SetDuration(4*60); // 4 mins
-
-  CUtil::AddDirectorySeperator(strPath);
-
-  for (int i=0; i<iStreams; ++i)
-  {
-    CStdString strLabel;
-    strLabel.Format("%s - Track %02.2i", strFileName,i+1);
-    CFileItem* pItem=new CFileItem(strLabel);
-    pItem->m_strPath.Format("%s%s-%i.nsfstream", strPath.c_str(),strFileName.c_str(),i+1);
-    
-    if (tag.Loaded())
-      *pItem->GetMusicInfoTag() = tag;
-
-    pItem->GetMusicInfoTag()->SetTrackNumber(i+1);
-    items.Add(pItem);
-    vecCacheItems.Add(new CFileItem(*pItem));
-  }
-
-  if (m_cacheDirectory)
-    g_directoryCache.SetDirectory(strPath, vecCacheItems);
-
-  return true;
 }
 
-bool CNSFFileDirectory::Exists(const char* strPath)
-{
-  return true;
-}
-
-bool CNSFFileDirectory::ContainsFiles(const CStdString& strPath)
+int CNSFFileDirectory::GetTrackCount(const CStdString& strPath)
 {
   CMusicInfoTagLoaderNSF nsf;
-  if (nsf.GetStreamCount(strPath) > 1)
-  {
-    CLog::Log(LOGDEBUG,"contains files!");
-    return true;
-  }
+  nsf.Load(strPath,m_tag);
+  m_tag.SetDuration(4*60); // 4 mins
 
-  return false;
+  return nsf.GetStreamCount(strPath);
 }
-
