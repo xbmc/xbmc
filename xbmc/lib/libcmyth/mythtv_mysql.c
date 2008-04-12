5c88eac894a9c8161c0d8cd2d8cd6c5ce5b2a85f
elupus <elupus@svn>
2008-04-12 12:40:47 +0000

    merged: TV guide for libcmyth from linux branch
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12641 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcmyth/mythtv_mysql.c
+++ xbmc/lib/libcmyth/mythtv_mysql.c
@@ -456,7 +456,7 @@ cmyth_mysql_get_guide(cmyth_database_t db, cmyth_program_t **prog, time_t startt
 	while((row = mysql_fetch_row(res))) {
         	if (rows >= n) {
                 	n+=10;
-                       	*prog=realloc(*prog,sizeof(**prog)*(n));
+                       	*prog=ref_realloc(*prog,sizeof(**prog)*(n));
                	}
 		(*prog)[rows].chanid = safe_atoi(row[0]);
                	(*prog)[rows].recording=0;
@@ -980,6 +980,15 @@ cmyth_channel_icon(cmyth_channel_t channel)
 	return ref_hold(channel->icon);
 }
 
+int
+cmyth_channel_visible(cmyth_channel_t channel)
+{
+	if (!channel) {
+		return -EINVAL;
+	}
+	return channel->visible;
+}
+
 cmyth_channel_t
 cmyth_channel_create(void)
 {
@@ -1001,7 +1010,7 @@ cmyth_chanlist_t cmyth_mysql_get_chanlist(cmyth_database_t db)
 {
 	MYSQL_RES *res = NULL;
 	MYSQL_ROW row;
-	const char *query_str = "SELECT chanid, channum, name, icon FROM channel;";
+	const char *query_str = "SELECT chanid, channum, name, icon, visible FROM channel;";
 	int rows = 0;
 	int i;
 	cmyth_mysql_query_t * query;
@@ -1035,7 +1044,7 @@ cmyth_chanlist_t cmyth_mysql_get_chanlist(cmyth_database_t db)
 		channel->channum = safe_atoll(row[1]);
 		channel->name = ref_strdup(row[2]);
 		channel->icon = ref_strdup(row[3]);
-
+		channel->visible = safe_atoi(row[4]);
 		chanlist->chanlist_list[rows] = channel;
 		i = 0;
 		rows++;
