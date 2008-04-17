5ab751dfa532f482f466be5d7d8262616ca1503b
topfs2 <topfs2@svn>
2008-04-17 19:42:26 +0000

    Added rfcomm sockets and Advertising too Bluetooth library
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12715 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/EventClients/lib/python/bt/bt.py
+++ tools/EventClients/lib/python/bt/bt.py
@@ -20,6 +20,15 @@ def bt_create_socket():
         sock = lightblue.socket(lightblue.L2CAP)
     return sock
 
+def bt_create_rfcomm_socket():
+    if BLUEZ:
+        sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        sock.bind(("",bluetooth.PORT_ANY))
+    else:
+        sock = lightblue.socket(lightblue.RFCOMM)
+        sock.bind(("",lightblue.PORT_ANY))
+    return sock
+
 def bt_discover_devices():
     if BLUEZ:
         nearby = bluetooth.discover_devices()
@@ -40,3 +49,17 @@ def bt_lookup_addr(bdaddr):
     else:
         return bdaddr[0]
 
+def bt_advertise(name, uuid, socket):
+    if BLUEZ:
+        bluetooth.advertise_service( socket, name,
+                           service_id = uuid,
+                           service_classes = [ uuid, bluetooth.SERIAL_PORT_CLASS ],
+                           profiles = [ bluetooth.SERIAL_PORT_PROFILE ] )
+    else:
+        lightblue.advertise(name, socket, uuid)
+
+def bt_stop_advertising(socket):
+    if BLUEZ:
+        stop_advertising(socket)
+    else:
+        stopadvertise(socket)
