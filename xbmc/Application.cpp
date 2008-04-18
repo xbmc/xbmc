730bc52285b2c3490fea9ed7c55b7637b151e4d7
wiso <wiso@svn>
2008-04-18 12:20:53 +0000

    [win32]: fixed compiler errors.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12728 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4107,8 +4107,10 @@ bool CApplication::ProcessEventServer(float frameTime)
       ResetScreenSaver();
       if (ResetScreenSaverWindow())
         return true;
-      
+
+#ifdef HAS_SDL_JOYSTICK
       g_Joystick.Reset();
+#endif
       g_Mouse.SetInactive();
       
       // Figure out what window we're taking the event for.
@@ -4127,7 +4129,8 @@ bool CApplication::ProcessEventServer(float frameTime)
         
       bool fullRange = false;
       CAction action;
-        
+
+#ifdef HAS_SDL_JOYSTICK      
       // Translate using regular joystick translator.
       if (g_buttonTranslator.TranslateJoystickString(iWin, joystickName.c_str(), (int)wKeyID, false, action.wID, action.strAction, fullRange))
       {
@@ -4141,6 +4144,7 @@ bool CApplication::ProcessEventServer(float frameTime)
       {
         printf("ERROR mapping joystick action.\n");
       }
+#endif
     }
     else
     {
