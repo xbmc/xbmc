6d29cdddf5ba64eb28a9a8800f6e2d3b5862a53f
spiff_ <spiff_@svn>
2008-04-22 19:57:43 +0000

    fixed: libid3tag wasn't properly wrapped
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@12779 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- sources/libid3tag/libid3tag/make-xbmc-lib.sh
+++ sources/libid3tag/libid3tag/make-xbmc-lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-ld -o libid3tag-i486-linux.so --export-dynamic -shared  --whole-archive .libs/libid3tag.a `grep __wrap ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.c | grep -v bash | awk -F"__wrap_"  '{print $2}' | awk -F"(" '{out=out"--wrap "$1" "} END {print out}'` 
+g++ -shared -o libid3tag-i486-linux.so .libs/*.o `cat ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.def` ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.o
