7baaeb71159bc84640f3ddae2e50235564fed274
vulkanr <vulkanr@svn>
2008-04-23 18:53:28 +0000

    another file missed in last merge.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12808 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/FileSystem/MusicFileDirectory.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "IFileDirectory.h"
+#include "MusicInfoTag.h"
+
+namespace DIRECTORY
+{
+  class CMusicFileDirectory : public IFileDirectory
+  {
+    public:
+      CMusicFileDirectory(void);
+      virtual ~CMusicFileDirectory(void);
+      virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
+      virtual bool Exists(const char* strPath);
+      virtual bool ContainsFiles(const CStdString& strPath);
+    protected:
+      virtual int GetTrackCount(const CStdString& strPath) = 0;
+      CStdString m_strExt;
+      MUSIC_INFO::CMusicInfoTag m_tag;
+  };
+}
