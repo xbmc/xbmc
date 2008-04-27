617ac5ea87ddadcf6875db57b922fc37ad763b8c
spiff_ <spiff_@svn>
2008-04-27 12:23:01 +0000

    fixed: some polish to the asap stuff
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12845 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/ASAPFileDirectory.cpp
+++ xbmc/FileSystem/ASAPFileDirectory.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "ASAPFileDirectory.h"
+#include "FileSystem/File.h"
+#include "MusicInfoTagLoaderASAP.h"
 
 using namespace DIRECTORY;
 
@@ -16,5 +18,14 @@ int CASAPFileDirectory::GetTrackCount(const CStdString &strPath)
 {
   if (!m_dll.Load())
     return 0;
-  return m_dll.asapGetSongs(strPath.c_str());
+  
+  MUSIC_INFO::CMusicInfoTagLoaderASAP loader;
+  loader.Load(strPath,m_tag);
+  m_tag.SetDuration(0); // ignore duration or all songs get duration of track 1
+
+  // hack until i figure out why the dll isnt properly wrapped
+  if (!XFILE::CFile::Cache(strPath,"z:\\cachedasap.sap"))
+    return 0;
+
+  return m_dll.asapGetSongs("z:\\cachedasap.sap");
 }
