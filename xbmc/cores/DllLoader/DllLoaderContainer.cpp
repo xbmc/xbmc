143678790b5a527fb1e0dec688da1aa5fd871564
charlydoes <charlydoes@svn>
2008-05-01 19:18:23 +0000

    changed: [WIN32] load libcurl with Win32DllLoader, it refuses to stream outside of Visual Studio with default DllLoader (streams fine when started from Studio, same exe, same location).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12862 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/DllLoaderContainer.cpp
+++ xbmc/cores/DllLoader/DllLoaderContainer.cpp
@@ -246,8 +246,15 @@ LibraryLoader* DllLoaderContainer::LoadDll(const char* sName, bool bLoadSymbols)
     pLoader = new SoLoader(sName, bLoadSymbols);
   else
 #elif defined(_WIN32PC)
-  if (!strnicmp(sName + (strlen(sName) - 4), ".vis", 4))
+  //NOTE: libcurl does not work with default DllLoader outside of Visual Studio for some mysterious reason, 
+  //load with Win32DllLoader instead.
+  if (
+    (strlen(sName) >= 4 && !strnicmp(sName + (strlen(sName) - 4), ".vis", 4)) ||
+    (strlen(sName) >= 11 && !strnicmp(sName + (strlen(sName) - 11), "libcurl.dll", 11))
+    )
+  {
     pLoader = new Win32DllLoader(sName);
+  }
   else
 #endif
     pLoader = new DllLoader(sName, m_bTrack, false, bLoadSymbols);
