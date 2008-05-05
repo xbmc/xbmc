03a9ba159a053ac7d65f9bee80e14e6f7dd8d6e3
spiff_ <spiff_@svn>
2008-05-05 16:29:29 +0000

    changed: speed up the artist info queries a bit by using an enum for the fields
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@12891 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicDatabase.cpp
+++ xbmc/MusicDatabase.cpp
@@ -749,21 +749,21 @@ CAlbum CMusicDatabase::GetAlbumFromDataset(dbiplus::Dataset* pDS)
 CArtist CMusicDatabase::GetArtistFromDataset(dbiplus::Dataset* pDS, bool needThumb)
 {
   CArtist artist;
-  artist.idArtist = pDS->fv("artistinfo.idArtist").get_asLong();
+  artist.idArtist = pDS->fv(artist_idArtist).get_asLong();
   artist.strArtist = pDS->fv("artist.strArtist").get_asString();
-  artist.strGenre = pDS->fv("artistinfo.strGenres").get_asString();
-  artist.strBiography = pDS->fv("artistinfo.strBiography").get_asString();
-  artist.strStyles = pDS->fv("artistinfo.strStyles").get_asString();
-  artist.strMoods = pDS->fv("artistinfo.strMoods").get_asString();
-  artist.strBorn = pDS->fv("artistinfo.strBorn").get_asString();
-  artist.strFormed = pDS->fv("artistinfo.strFormed").get_asString();
-  artist.strDied = pDS->fv("artistinfo.strDied").get_asString();
-  artist.strDisbanded = pDS->fv("artistinfo.strDisbanded").get_asString();
-  artist.strYearsActive = pDS->fv("artistinfo.strYearsActive").get_asString();
-  artist.strInstruments = pDS->fv("artistinfo.strInstruments").get_asString();
+  artist.strGenre = pDS->fv(artist_strGenres).get_asString();
+  artist.strBiography = pDS->fv(artist_strBiography).get_asString();
+  artist.strStyles = pDS->fv(artist_strStyles).get_asString();
+  artist.strMoods = pDS->fv(artist_strMoods).get_asString();
+  artist.strBorn = pDS->fv(artist_strBorn).get_asString();
+  artist.strFormed = pDS->fv(artist_strFormed).get_asString();
+  artist.strDied = pDS->fv(artist_strDied).get_asString();
+  artist.strDisbanded = pDS->fv(artist_strDisbanded).get_asString();
+  artist.strYearsActive = pDS->fv(artist_strYearsActive).get_asString();
+  artist.strInstruments = pDS->fv(artist_strInstruments).get_asString();
 
   if (needThumb)
-    artist.thumbURL.ParseString(pDS->fv("artistinfo.strImage").get_asString());
+    artist.thumbURL.ParseString(pDS->fv(artist_strImage).get_asString());
 
   return artist;
 }
