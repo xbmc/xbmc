d642dbe4e60747f9f314377e7db74802f2346ce2
d4rkm4ster <d4rkm4ster@svn>
2008-05-06 23:33:12 +0000

    fixed: unnecessary #ifdef, accurate seeks were fixed in 12250
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@12902 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDMessage.h
+++ xbmc/cores/dvdplayer/DVDMessage.h
@@ -227,11 +227,7 @@ class CDVDMsgPlayerSeek : public CDVDMsg
 {
 public:
   CDVDMsgPlayerSeek(int time, bool backward, bool flush = true,
-#ifdef __APPLE__
-      bool accurate = false)
-#else
       bool accurate = true)
-#endif
     : CDVDMsg(PLAYER_SEEK)
     , m_time(time)
     , m_backward(backward)
