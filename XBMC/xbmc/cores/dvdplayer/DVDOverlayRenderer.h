9acf8a7d2a4a5b99dc05fc5586dd23e623ab67bc
leafmuncher <leafmuncher@svn>
2008-05-11 04:59:11 +0000

    Added: SSA support, via Rudd's awesome patch
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@12936 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/DVDOverlayRenderer.h
+++ XBMC/xbmc/cores/dvdplayer/DVDOverlayRenderer.h
@@ -3,12 +3,14 @@
 
 #include "DVDCodecs/Video/DVDVideoCodec.h"
 #include "DVDCodecs/Overlay/DVDOverlay.h"
+#include "DVDSubtitles/DVDSubtitlesLibass.h"
 
 #ifdef HAS_VIDEO_PLAYBACK
 #include "cores/VideoRenderers/RenderManager.h"
 #endif
 
 class CDVDOverlayImage;
+class CDVDOverlaySSA;
 
 typedef struct stDVDPictureRenderer
 {
@@ -23,10 +25,12 @@ DVDPictureRenderer;
 class CDVDOverlayRenderer
 {
 public:
-  static void Render(DVDPictureRenderer* pPicture, CDVDOverlay* pOverlay);
+  static void Render(DVDPictureRenderer* pPicture, CDVDOverlay* pOverlay, double pts);
   static void Render(DVDPictureRenderer* pPicture, CDVDOverlayImage* pOverlay);
+  static void Render(DVDPictureRenderer* pPicture, CDVDOverlaySSA *pOverlay, double pts);
 
-  static void Render(YV12Image* pImage, CDVDOverlay* pOverlay)
+
+  static void Render(YV12Image* pImage, CDVDOverlay* pOverlay, double pts)
   {
     DVDPictureRenderer p;
     
@@ -43,10 +47,10 @@ public:
     p.width  = pImage->width;
     p.height = pImage->height;
     
-    Render(&p, pOverlay);
+    Render(&p, pOverlay, pts);
   }
   
-  static void Render(DVDVideoPicture* pPicture, CDVDOverlay* pOverlay)
+  static void Render(DVDVideoPicture* pPicture, CDVDOverlay* pOverlay, double pts)
   {
     DVDPictureRenderer p;
     
@@ -63,7 +67,7 @@ public:
     p.width  = pPicture->iWidth;
     p.height = pPicture->iHeight;
 
-    Render(&p, pOverlay);
+    Render(&p, pOverlay, pts);
   }
 
 private:
