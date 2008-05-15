50d8e872440df364f95b7bc4c86013565d690ab7
topfs2 <topfs2@svn>
2008-05-15 17:40:19 +0000

    added: proper licences to xbmc/(i* - z*)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@13039 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/MusicInfoTagLoaderWMA.cpp
+++ XBMC/xbmc/MusicInfoTagLoaderWMA.cpp
@@ -1,9 +1,6 @@
 /*
- *      Copyright (C) 2005-2007 Team XboxMediaCenter
- *      http://www.xboxmediacenter.com
- *
- *      Based on MediaInfo by Jérôme Martinez, Zen@MediaArea.net
- *      http://sourceforge.net/projects/mediainfo/
+ *      Copyright (C) 2005-2008 Team XBMC
+ *      http://www.xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
+ *  along with XBMC; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *  http://www.gnu.org/copyleft/gpl.html
  *
