83b31f3f52367ed86a9b51557ab9ed3fd4442d44
monkeyman_67156 <monkeyman_67156@svn>
2008-05-15 17:50:45 +0000

    added: license headers to xbmc/cores/
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@13042 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/VideoRenderers/ComboRenderer.h
+++ xbmc/cores/VideoRenderers/ComboRenderer.h
@@ -1,6 +1,27 @@
 #ifndef COMBO_RENDERER
 #define COMBO_RENDERER
 
+/*
+ *      Copyright (C) 2005-2008 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 #include "XBoxRenderer.h"
 
 //const DWORD FVF_YV12VERTEX = D3DFVF_XYZRHW|D3DFVF_TEX3;
