ea04b3dae1277ad0f371135fbefae0e444f79574
topfs2 <topfs2@svn>
2008-05-15 17:40:19 +0000

    added: proper licences to xbmc/(i* - z*)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@13039 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/XboxMediaCenter.cpp
+++ xbmc/XboxMediaCenter.cpp
@@ -1,6 +1,6 @@
 /*
- *      Copyright (C) 2005-2007 Team XboxMediaCenter
- *      http://www.xboxmediacenter.com
+ *      Copyright (C) 2005-2008 Team XBMC
+ *      http://www.xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,11 +13,12 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
+ *  along with XBMC; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *  http://www.gnu.org/copyleft/gpl.html
  *
  */
+
 
 // XboxMediaCenter
 //
