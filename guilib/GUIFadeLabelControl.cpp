972355daab54751c1da4fcf9eb6023c90423753b
jmarshallnz <jmarshallnz@svn>
2008-05-16 02:14:22 +0000

    added: Support for <aligny> in fadelabel control.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@13057 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIFadeLabelControl.cpp
+++ guilib/GUIFadeLabelControl.cpp
@@ -92,9 +92,12 @@ void CGUIFadeLabelControl::Render()
     m_lastLabel = m_currentLabel;
   }
 
+  float posY = m_posY;
+  if (m_label.align & XBFONT_CENTER_Y)
+    posY += m_height * 0.5f;
   if (m_infoLabels.size() == 1 && m_shortText) 
   { // single label set and no scrolling required - just display
-    m_textLayout.Render(m_posX + m_label.offsetX, m_posY, 0, m_label.textColor, m_label.shadowColor, 0, m_width - m_label.offsetX);
+    m_textLayout.Render(m_posX + m_label.offsetX, posY, 0, m_label.textColor, m_label.shadowColor, (m_label.align & ~3), m_width - m_label.offsetX);
     CGUIControl::Render();
     return;
   }
@@ -134,10 +137,10 @@ void CGUIFadeLabelControl::Render()
       posX = m_posX + m_width * 0.5f;
     else if (m_label.align & XBFONT_RIGHT)
       posX = m_posX + m_width;
-    m_textLayout.Render(posX, m_posY, 0, m_label.textColor, m_label.shadowColor, m_label.align & 3, m_width);
+    m_textLayout.Render(posX, posY, 0, m_label.textColor, m_label.shadowColor, m_label.align, m_width);
   }
   else
-    m_textLayout.RenderScrolling(m_posX, m_posY, 0, m_label.textColor, m_label.shadowColor, 0, m_width, m_scrollInfo);
+    m_textLayout.RenderScrolling(m_posX, posY, 0, m_label.textColor, m_label.shadowColor, (m_label.align & ~3), m_width, m_scrollInfo);
 
   if (moveToNextLabel)
   { // increment the label and reset scrolling
