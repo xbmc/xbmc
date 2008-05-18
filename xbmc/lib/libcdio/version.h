9fec073f88a3ddeb6ec07001d891e00a4c096f86
topfs2 <topfs2@svn>
2008-05-18 10:00:13 +0000

    Updated the headers for libcdio 0.80 aswell. (Might cause trouble on win32, types.h mainly)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13108 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcdio/version.h
+++ xbmc/lib/libcdio/version.h
@@ -2,11 +2,11 @@
 /** \file version.h 
  *
  *  \brief A file containing the libcdio package version
- *  number (79) and OS build name. 
+ *  number (80) and OS build name. 
  */
 
 /*! CDIO_VERSION can as a string in programs to show what version is used. */
-#define CDIO_VERSION "0.79 i686-pc-linux-gnu"
+#define CDIO_VERSION "0.80 i686-pc-linux-gnu"
 
 /*! LIBCDIO_VERSION_NUM  can be used for testing in the C preprocessor */
-#define LIBCDIO_VERSION_NUM 79
+#define LIBCDIO_VERSION_NUM 80
