7adf0818045d040481b2961f63084b8e251f9325
wiso <wiso@svn>
2008-05-19 19:22:32 +0000

    [win32]: Workaround to detect media changes because libcdio doesn't work properly for win32.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13131 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/win32/WIN32Util.h
+++ xbmc/win32/WIN32Util.h
@@ -14,6 +14,8 @@ public:
   static DWORD UmountShare(const CStdString &strPath);
   static CStdString URLEncode(const CURL &url);
   static CStdString CWIN32Util::GetLocalPath(const CStdString &strPath);
+  static char CWIN32Util::FirstDriveFromMask (ULONG unitmask);
+  static int CWIN32Util::GetDriveStatus(const CStdString &strPath);
 
 private:
   
