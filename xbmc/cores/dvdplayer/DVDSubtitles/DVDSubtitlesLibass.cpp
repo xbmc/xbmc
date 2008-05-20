6144a555a272f224b40a28286a73ae2739d2200c
elupus <elupus@svn>
2008-05-20 20:04:58 +0000

    added: ass/ssa subtitle infrastructure from linuxbranch for dvdplayer. (currently no dll, so not working) fixed: a few crashes in the ass/ssa subtitle infrastructure if it failed to load dll. fixed: ass/ssa overlays must use special buffer instead of gpu texture when rendering subtitle as reading from gpu texture is slow (alpha blending)
    Ps. This isn't cleanly mergable to linuxbranch, will require hand care, as it does have some cosmetics and changes to the ass files. I will look at merging that back later.
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@13162 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitlesLibass.cpp
@@ -0,0 +1,157 @@
+#include "stdafx.h"
+#include "DVDSubtitlesLibass.h"
+#include "DVDClock.h"
+
+using namespace std;
+
+CDVDSubtitlesLibass::CDVDSubtitlesLibass()
+{
+
+  m_track = NULL;
+  m_library = NULL;
+  m_references = 1;
+
+  if(!m_dll.Load())
+  {
+    CLog::Log(LOGERROR, "CDVDSubtitlesLibass: Failed to load libass library");
+    return;
+  }
+
+  //Setting a Default Font
+  string strFont = "Arial.ttf";
+  string strPath = "";
+
+  CLog::Log(LOGINFO, "CDVDSubtitlesLibass: Creating ASS library structure");
+  m_library  = m_dll.ass_library_init();
+  if(!m_library)
+    return;
+
+  CLog::Log(LOGINFO, "CDVDSubtitlesLibass: Initializing ASS library font settings");
+  strPath = "Q:\\media\\Fonts\\";
+  m_dll.ass_set_fonts_dir(m_library,  strPath.c_str());
+  strPath += strFont;
+
+  m_dll.ass_set_extract_fonts(m_library, 0);
+  m_dll.ass_set_style_overrides(m_library, NULL);
+
+  CLog::Log(LOGINFO, "CDVDSubtitlesLibass: Initializing ASS Renderer");
+
+  m_renderer = m_dll.ass_renderer_init(m_library);
+  if(!m_renderer)
+    return;
+  m_dll.ass_set_margins(m_renderer, 0, 0, 0, 0);
+  m_dll.ass_set_use_margins(m_renderer, 0);
+  m_dll.ass_set_font_scale(m_renderer, 1);
+  m_dll.ass_set_fonts(m_renderer, strPath.c_str(), "");
+
+}
+
+
+CDVDSubtitlesLibass::~CDVDSubtitlesLibass()
+{
+  if(m_dll.IsLoaded())
+  {
+    m_dll.ass_renderer_done(m_renderer);
+    m_dll.ass_library_done(m_library);
+    m_dll.Unload();
+  }
+}
+
+/*Decode Header of SSA, needed to properly decode demux packets*/
+bool CDVDSubtitlesLibass::DecodeHeader(char* data, int size)
+{
+
+  if(!m_library || !data)
+    return false;
+
+  if(!m_track)
+  {
+    CLog::Log(LOGINFO, "CDVDSubtitlesLibass: Creating new ASS track");
+    m_track = m_dll.ass_new_track(m_library) ;
+  }
+
+  m_dll.ass_process_codec_private(m_track, data, size);
+  return true;
+}
+
+bool CDVDSubtitlesLibass::DecodeDemuxPkt(char* data, int size, double start, double duration)
+{
+  if(!m_track)
+  {
+    CLog::Log(LOGERROR, "CDVDSubtitlesLibass: No SSA header found.");
+    return false;
+  }
+
+  m_dll.ass_process_chunk(m_track, data, size, DVD_TIME_TO_MSEC(start), DVD_TIME_TO_MSEC(duration));
+  return true;
+}
+
+bool CDVDSubtitlesLibass::ReadFile(const string& strFile)
+{
+  if(!m_library)
+  {
+    CLog::Log(LOGERROR, "CDVDSubtitlesLibass: %s - No ASS library struct", __FUNCTION__);
+    return false;
+  }
+
+  //Fixing up the pathname.
+  string fileName =  strFile;
+
+  CLog::Log(LOGINFO, "SSA Parser: Creating m_track from SSA file:  %s", fileName.c_str());
+
+  m_track = m_dll.ass_read_file(m_library, (char* )fileName.c_str(), 0);
+  if(m_track == NULL)
+    return false;
+
+  return true;
+}
+
+
+long CDVDSubtitlesLibass::Acquire()
+{
+  long count = InterlockedIncrement(&m_references);
+  return count;
+}
+
+long CDVDSubtitlesLibass::Release()
+{
+  long count = InterlockedDecrement(&m_references);
+  if (count == 0)
+    delete this;
+
+  return count;
+}
+
+long CDVDSubtitlesLibass::GetNrOfReferences()
+{
+  return m_references;
+}
+
+ass_image_t* CDVDSubtitlesLibass::RenderImage(int imageWidth, int imageHeight, double pts)
+{
+  if(!m_renderer || !m_track)
+  {
+    CLog::Log(LOGERROR, "CDVDSubtitlesLibass: %s - Missing ASS structs(m_track or m_renderer)", __FUNCTION__);
+    return NULL;
+  }
+
+  m_dll.ass_set_frame_size(m_renderer, imageWidth, imageHeight);
+  return m_dll.ass_render_frame(m_renderer, m_track, DVD_TIME_TO_MSEC(pts), NULL);
+}
+
+ass_event_t* CDVDSubtitlesLibass::GetEvents()
+{
+  if(!m_track)
+  {
+    CLog::Log(LOGERROR, "CDVDSubtitlesLibass: %s -  Missing ASS structs(m_track)", __FUNCTION__);
+    return NULL;
+  }
+  return m_track->events;
+}
+
+int CDVDSubtitlesLibass::GetNrOfEvents()
+{
+  if(!m_track)
+    return 0;
+  return m_track->n_events;
+}
