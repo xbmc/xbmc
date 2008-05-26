647881989f0f339be16d84154f982a3f10d1829d
elupus <elupus@svn>
2008-05-26 16:53:31 +0000

    merged: r13234,r13235 from trunk.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@13237 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/DVDCodecs/DVDCodecUtils.h
+++ XBMC/xbmc/cores/dvdplayer/DVDCodecs/DVDCodecUtils.h
@@ -31,6 +31,6 @@ public:
   static DVDVideoPicture* AllocatePicture(int iWidth, int iHeight);
   static void FreePicture(DVDVideoPicture* pPicture);
   static bool CopyPicture(DVDVideoPicture* pDst, DVDVideoPicture* pSrc);
-  static bool CopyPictureToOverlay(YV12Image* pImage, DVDVideoPicture *pSrc);
+  static bool CopyPicture(YV12Image* pDst, DVDVideoPicture *pSrc);
 };
 
