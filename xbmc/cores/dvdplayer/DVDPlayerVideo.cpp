6fe6812d99e86ab3316ae33a2d015539da5c1e19
elupus <elupus@svn>
2008-05-26 16:53:31 +0000

    merged: r13234,r13235 from trunk.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13237 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -602,8 +602,10 @@ void CDVDPlayerVideo::ProcessOverlays(DVDVideoPicture* pSource, YV12Image* pDest
     if (!m_pTempOverlayPicture) m_pTempOverlayPicture = CDVDCodecUtils::AllocatePicture(pSource->iWidth, pSource->iHeight);
   }
 
-  if (bHasSpecialOverlay && m_pTempOverlayPicture) CDVDCodecUtils::CopyPicture(m_pTempOverlayPicture, pSource);
-  else CDVDCodecUtils::CopyPictureToOverlay(pDest, pSource);
+  if (bHasSpecialOverlay && m_pTempOverlayPicture) 
+    CDVDCodecUtils::CopyPicture(m_pTempOverlayPicture, pSource);
+  else 
+    CDVDCodecUtils::CopyPicture(pDest, pSource);
   
   m_pOverlayContainer->Lock();
 
@@ -635,9 +637,7 @@ void CDVDPlayerVideo::ProcessOverlays(DVDVideoPicture* pSource, YV12Image* pDest
   m_pOverlayContainer->Unlock();
   
   if (bHasSpecialOverlay && m_pTempOverlayPicture)
-  {
-    CDVDCodecUtils::CopyPictureToOverlay(pDest, m_pTempOverlayPicture);
-  }
+    CDVDCodecUtils::CopyPicture(pDest, m_pTempOverlayPicture);
 }
 
 int CDVDPlayerVideo::OutputPicture(DVDVideoPicture* pPicture, double pts)
