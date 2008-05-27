f7f584ac313b3bb575346725ef5f06917cc6da6d
d4rkm4ster <d4rkm4ster@svn>
2008-05-27 18:19:40 +0000

    cleanup: minor formatting difference
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@13255 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/xbox/IoSupport.cpp
+++ XBMC/xbmc/xbox/IoSupport.cpp
@@ -86,13 +86,13 @@ stDriveMapping driveMapping[] =
 #else
 stDriveMapping driveMapping[] =
   {
-     {'P', new char[MAX_PATH], 0},
-     {'Q', new char[MAX_PATH], 0},
-     {'T', new char[MAX_PATH], 0},
-     {'Z', new char[MAX_PATH], 0},
-     {'C', new char[MAX_PATH], 0},
-     {'E', new char[MAX_PATH], 0},
-     {'U', new char[MAX_PATH], 0}
+    {'P', new char[MAX_PATH], 0},
+    {'Q', new char[MAX_PATH], 0},
+    {'T', new char[MAX_PATH], 0},
+    {'Z', new char[MAX_PATH], 0},
+    {'C', new char[MAX_PATH], 0},
+    {'E', new char[MAX_PATH], 0},
+    {'U', new char[MAX_PATH], 0}
   };
 #endif
 
