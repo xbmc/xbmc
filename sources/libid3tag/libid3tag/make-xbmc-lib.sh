d89e7a05f84438a507728a43e8acd4ebd3156ff6
malloc64 <malloc64@svn>
2008-05-28 09:43:23 +0000

    libid3tag: added -fPIC flag, deleted generated files, reran autoconf, added README and Makefile
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@13272 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- sources/libid3tag/libid3tag/make-xbmc-lib.sh
+++ sources/libid3tag/libid3tag/make-xbmc-lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-g++ -shared -o libid3tag-i486-linux.so .libs/*.o `cat ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.def` ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.o
+g++ -shared -o libid3tag-i486-linux.so .libs/*.o `cat ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.def` ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.o -fPIC -O3
