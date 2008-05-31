bf78954224955b31dab7466fd93eb68232c71c4f
vulkanr <vulkanr@svn>
2008-05-31 20:43:23 +0000

    close cache dialog when caching done.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13338 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -843,7 +843,14 @@ void CDVDPlayer::Process()
 
       // if we are caching, start playing it agian
       if (m_caching && !m_bAbortRequest)
+      {
         SetCaching(false);
+        if (m_pDlgCache)
+        {
+          m_pDlgCache->Close();
+          m_pDlgCache = NULL;
+        }
+      }
 
       // while players are still playing, keep going to allow seekbacks
       if(m_dvdPlayerAudio.m_messageQueue.GetDataSize() > 0 
