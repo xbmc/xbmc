0cfa9f630497be8ffe8c384772aa3b54f112ef83
wiso <wiso@svn>
2008-06-02 18:33:47 +0000

    allow .nrg images to be opened as DVD images.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13369 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -635,7 +635,7 @@ bool CFileItem::IsDVDImage() const
 {
   CStdString strExtension;
   CUtil::GetExtension(m_strPath, strExtension);
-  if (strExtension.Equals(".img") || strExtension.Equals(".iso")) return true;
+  if (strExtension.Equals(".img") || strExtension.Equals(".iso") || strExtension.Equals(".nrg")) return true;
   return false;
 }
 
