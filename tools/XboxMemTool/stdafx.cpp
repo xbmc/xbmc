e4ac5f5250cf8b38151b84782a872e5f52d837cf
yuvalt <yuvalt@svn>
2008-06-20 00:40:05 +0000

    remove some xbox tools
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13632 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/XboxMemTool/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// XboxMemoryUsage.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
