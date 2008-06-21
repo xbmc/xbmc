cd67ef7d853109ac1876d3903c439336d02ec1fc
althekiller <althekiller@svn>
2008-06-21 03:24:26 +0000

    fixed: Nav sounds didn't work for skins installed in U:
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13652 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIAudioManager.cpp
+++ guilib/GUIAudioManager.cpp
@@ -280,7 +280,7 @@ bool CGUIAudioManager::Load()
 
   if (g_guiSettings.GetString("lookandfeel.soundskin")=="SKINDEFAULT")
   {
-    m_strMediaDir=_P("T:\\skin\\"+g_guiSettings.GetString("lookandfeel.skin")+"\\sounds");
+    m_strMediaDir=_P("U:\\skin\\"+g_guiSettings.GetString("lookandfeel.skin")+"\\sounds");
     if ( ! CDirectory::Exists( m_strMediaDir ) )
       m_strMediaDir=_P("Q:\\skin\\"+g_guiSettings.GetString("lookandfeel.skin")+"\\sounds");
   }
