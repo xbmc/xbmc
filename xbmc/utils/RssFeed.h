b58fb3918710b776bfbdb007382b05995c76c666
vulkanr <vulkanr@svn>
2008-06-22 22:22:14 +0000

    rss directory. active for rss:// links. basic implementation of mrss and itunes tags. currently retrieval is blocking. need to add progress dialog.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13691 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/utils/RssFeed.h
@@ -0,0 +1,86 @@
+/*
+ *      Copyright (C) 2005-2008 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#ifndef RSS_FEED_H_
+#define RSS_FEED_H_
+
+#include <string>
+#include <vector>
+
+#include <time.h>
+
+#include "FileItem.h"
+
+class TiXmlElement;
+class TiXmlDocument;
+class CFileItem;
+
+/**
+ * The purpose of this class is to describe an RSS feed
+ */
+class CRssFeed 
+{
+public:
+  CRssFeed();
+  virtual ~CRssFeed();
+
+  bool Init(const CStdString& strURL);
+
+  void GetItemList(CFileItemList &feedItems) 
+  {
+    for (int i = 0; i < items.Size(); i++) {
+      feedItems.Add(new CFileItem(*items[i]));
+    }
+  }
+
+  const CStdString& GetUrl() 
+  {
+    return m_strURL;
+  }
+  
+  bool ReadFeed();
+
+private:
+  
+  CStdString CleanDescription(const CStdString& strDescription);
+  time_t ParseDate(const CStdString & strDate);
+  
+  bool IsPathToMedia(const CStdString& strPath );
+  bool IsPathToThumbnail(const CStdString& strPath );
+
+  CFileItemList items;
+  
+  // Lock that protects the feed vector
+  CCriticalSection m_ItemVectorLock;
+
+  CStdString m_strURL;
+  
+  // Channel information
+  CStdString m_strTitle;
+  CStdString m_strAuthor;
+  CStdString m_strLink;
+  CStdString m_strGuid; // globally unique identifier of the item in the entire feed
+  CStdString m_strDescription;
+  CStdString m_strThumbnail;
+};
+
+
+#endif /*RSS_FEED_H_*/
