7f03f7dd8500bef13e03f8892dd5a39878017f7c
jmarshallnz <jmarshallnz@svn>
2008-06-25 23:04:24 +0000

    changed: Episode smartplaylists now format their labels as tvshowtitle - season+episode. episodetitle.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13751 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIViewStateVideo.cpp
+++ xbmc/GUIViewStateVideo.cpp
@@ -506,11 +506,11 @@ CGUIViewStateVideoEpisodes::CGUIViewStateVideoEpisodes(const CFileItemList& item
     AddSortMethod(SORT_METHOD_DATE,552,LABEL_MASKS("%E. %T","%J","E. %T","%J"));
   }
   else
-  {
-    AddSortMethod(SORT_METHOD_VIDEO_RATING, 563, LABEL_MASKS("%H. %T", "%R"));  // Filename, Duration | Foldername, empty
-    AddSortMethod(SORT_METHOD_EPISODE,20359,LABEL_MASKS("%H. %T","%R"));
-    AddSortMethod(SORT_METHOD_PRODUCTIONCODE,20368,LABEL_MASKS("%H. %T","%P", "%H. %T","%P"));
-    AddSortMethod(SORT_METHOD_DATE,552,LABEL_MASKS("%H. %T","%J","%H. %T","%J"));
+  { // format here is tvshowtitle - season/episode number. episode title
+    AddSortMethod(SORT_METHOD_VIDEO_RATING, 563, LABEL_MASKS("%Z - %H. %T", "%R"));
+    AddSortMethod(SORT_METHOD_EPISODE,20359,LABEL_MASKS("%Z - %H. %T","%R"));
+    AddSortMethod(SORT_METHOD_PRODUCTIONCODE,20368,LABEL_MASKS("%Z - %H. %T","%P"));
+    AddSortMethod(SORT_METHOD_DATE,552,LABEL_MASKS("%Z - %H. %T","%J"));
   }
 
   SetSortMethod(g_stSettings.m_viewStateVideoNavEpisodes.m_sortMethod);
