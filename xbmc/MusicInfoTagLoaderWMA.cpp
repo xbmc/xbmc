939c644583473793a77f533e063dc5356f6070ff
jmarshallnz <jmarshallnz@svn>
2008-06-26 09:48:58 +0000

    fixed: Don't try and read blank tags (resulting in junk) in WMA files.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13761 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicInfoTagLoaderWMA.cpp
+++ xbmc/MusicInfoTagLoaderWMA.cpp
@@ -144,22 +144,28 @@ bool CMusicInfoTagLoaderWMA::Load(const CStdString& strFileName, CMusicInfoTag& 
     {
       iOffset += 24;
       int nTitleSize = pData[iOffset + 0] + pData[iOffset + 1] * 0x100;
-      //int nAuthorSize = pData[iOffset + 2] + pData[iOffset + 3] * 0x100;
+      int nAuthorSize = pData[iOffset + 2] + pData[iOffset + 3] * 0x100;
       //int nCopyrightSize = pData[iOffset + 4] + pData[iOffset + 5] * 0x100;
       //int nCommentsSize = pData[iOffset + 6] + pData[iOffset + 7] * 0x100;
 
       iOffset += 10;
 
-      // TODO: UTF-8 Do we need to "fixString" these strings at all?
       CStdString utf8String;
-      utf16String = (wchar_t*)(pData.get()+iOffset);
-      g_charsetConverter.utf16LEtoUTF8(utf16String, utf8String);
-      tag.SetTitle(utf8String);
+      if (nTitleSize)
+      {
+        // TODO: UTF-8 Do we need to "fixString" these strings at all?
+        utf16String = (wchar_t*)(pData.get()+iOffset);
+        g_charsetConverter.utf16LEtoUTF8(utf16String, utf8String);
+        tag.SetTitle(utf8String);
+      }
 
-      utf8String = "";
-      utf16String = (wchar_t*)(pData.get() + iOffset + nTitleSize);
-      g_charsetConverter.utf16LEtoUTF8(utf16String, utf8String);
-      tag.SetArtist(utf8String);
+      if (nAuthorSize)
+      {
+        utf8String = "";
+        utf16String = (wchar_t*)(pData.get() + iOffset + nTitleSize);
+        g_charsetConverter.utf16LEtoUTF8(utf16String, utf8String);
+        tag.SetArtist(utf8String);
+      }
 
       //General(ZT("Copyright"))=(LPWSTR)(pData.get()+iOffset+(nTitleSize+nAuthorSize));
       //General(ZT("Comments"))=(LPWSTR)(pData.get()+iOffset+(nTitleSize+nAuthorSize+nCopyrightSize));
