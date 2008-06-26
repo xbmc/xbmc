c04e508d1d29fe8bc3463bfb87678b3c71216e04
malloc64 <malloc64@svn>
2008-06-26 14:31:23 +0000

    dvdplayer: Updating libfaad2...
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@13779 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- sources/dvdplayer/libfaad2/CoreAAC/register_release.bat
+++
@@ -1,1 +0,0 @@
-regsvr32 release\CoreAAC.ax
--- sources/dvdplayer/libfaad2/plugins/winamp3/Defines.h
+++
@@ -1,4 +0,0 @@
-#define FILES_SUPPORT "MP4-AAC"
-#define APP_NAME "MPEG4-AAC"
-#define REGISTRY_PROGRAM_NAME "SOFTWARE\\4N\\Winamp3\\" APP_NAME
-#define APP_VER "v1.1"
