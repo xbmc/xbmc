f0bd6ca98aca7afacdf78e8da7e373ac4d66f82e
spiff_ <spiff_@svn>
2008-06-27 18:47:27 +0000

    changed: remove xbox relevant stuff (gamesaves)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13805 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/Key.h
+++ guilib/Key.h
@@ -269,7 +269,6 @@
 
 #define WINDOW_LOGIN_SCREEN               10029
 #define WINDOW_SETTINGS_PROFILES          10034
-#define WINDOW_GAMESAVES                  10035
 
 #define WINDOW_DIALOG_YES_NO              10100
 #define WINDOW_DIALOG_PROGRESS            10101
