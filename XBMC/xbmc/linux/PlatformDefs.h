9b2132de153ef579d242471f12ef77a5dd059c61
d4rkm4ster <d4rkm4ster@svn>
2008-07-02 18:18:42 +0000

    added: [OSX] support to build on Mac OS X Tiger (10.4)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@13899 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/linux/PlatformDefs.h
+++ XBMC/xbmc/linux/PlatformDefs.h
@@ -127,10 +127,16 @@ typedef void* HINSTANCE;
 typedef void* HMODULE;
 
 #ifdef __APPLE__
+#include <AvailabilityMacros.h>
 typedef int64_t   off64_t;
 typedef off_t     __off_t;
 typedef off64_t   __off64_t;
 #include <sched.h>
+#if (MAC_OS_X_VERSION_MAX_ALLOWED <= 1040)
+#define MAC_TIGER
+#else
+#define MAC_LEOPARD
+#endif
 #endif
 
 typedef unsigned int  DWORD;
@@ -291,7 +297,14 @@ typedef int (*LPTHREAD_START_ROUTINE)(void *);
 #define _O_WRONLY O_WRONLY
 #define _off_t off_t
 
+#ifdef MAC_TIGER
+#define __stat64 stat
+#define stat64 stat
+#define statfs64 statfs
+#define fstat64 fstat
+#else
 #define __stat64 stat64
+#endif
 
 struct _stati64 {
   dev_t st_dev;
