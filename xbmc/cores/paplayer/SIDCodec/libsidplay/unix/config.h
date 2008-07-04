30d84ea41c7c512acc0cecd5185762f5089e3cdb
spiff_ <spiff_@svn>
2008-07-04 13:42:39 +0000

    changed: compile sid codec instead of keeping binary in svn (linux x86 only)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13930 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/paplayer/SIDCodec/libsidplay/unix/config.h
@@ -0,0 +1,106 @@
+/* unix/config.h.  Generated from config.h.in by configure.  */
+/* unix/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if the C++ compiler supports BOOL */
+#define HAVE_BOOL 
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if your C++ compiler implements exception-handling. */
+#define HAVE_EXCEPTIONS 
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if standard member ``ios::binary'' is called ``ios::bin''. */
+/* #undef HAVE_IOS_BIN */
+
+/* Define if ``ios::openmode'' is supported. */
+#define HAVE_IOS_OPENMODE 
+
+/* Define to 1 if you support file names longer than 14 characters. */
+#define HAVE_LONG_FILE_NAMES 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the <sstream> header file. */
+#define HAVE_SSTREAM 
+
+/* Define to 1 if you have the <stdbool.h> header file. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the strcasecmp function. */
+#define HAVE_STRCASECMP 
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the strncasecmp function. */
+#define HAVE_STRNCASECMP 
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libsidplay"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libsidplay 2.1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libsidplay"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.1.1"
+
+/* The size of `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The number of bytes in type long */
+/* #undef SIZEOF_LONG */
+
+/* The size of `long int', as computed by sizeof. */
+#define SIZEOF_LONG_INT 4
+
+/* The number of bytes in type short */
+/* #undef SIZEOF_SHORT */
+
+/* The size of `short int', as computed by sizeof. */
+#define SIZEOF_SHORT_INT 2
+
+/* The number of bytes in type void* */
+/* #undef SIZEOF_VOIDP */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Email addresses */
+#define S_A_WHITE_EMAIL "sidplay2@yahoo.com"
+
