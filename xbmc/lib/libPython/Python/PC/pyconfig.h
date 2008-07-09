c940d3afeced1c9bda31542f41d4bef005b5c911
elupus <elupus@svn>
2008-07-09 18:33:22 +0000

    changed [win32]: from the point of view of compiler, python should be considered statically linked since we load it using our own DLLLoader. (removes a large amount of linking warnings on build)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13996 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/Python/PC/pyconfig.h
+++ xbmc/lib/libPython/Python/PC/pyconfig.h
@@ -204,7 +204,7 @@ typedef int pid_t;
 
 /* For Windows the Python core is in a DLL by default.  Test
 Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
-#if !defined(MS_NO_COREDLL) && !defined(Py_NO_ENABLE_SHARED)
+#if !defined(MS_NO_COREDLL) && !defined(Py_NO_ENABLE_SHARED) && !defined(_XBOX) && !defined(_WIN32)
 #	define Py_ENABLE_SHARED 1 /* standard symbol for shared library */
 #	define MS_COREDLL	/* deprecated old symbol */
 #endif /* !MS_NO_COREDLL && ... */
