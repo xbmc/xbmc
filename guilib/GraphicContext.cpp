bd876da85f7665ba754f8ba58717e7a0426fbc97
d4rkm4ster <d4rkm4ster@svn>
2008-07-12 23:57:37 +0000

    fixed: projection matrix was off by 1 pixel
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14033 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GraphicContext.cpp
+++ guilib/GraphicContext.cpp
@@ -717,7 +717,7 @@ void CGraphicContext::SetVideoResolution(RESOLUTION &res, BOOL NeedZ, bool force
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
  
-    glOrtho(0.0f, m_iScreenWidth, m_iScreenHeight, 0.0f, -1.0f, 1.0f);
+    glOrtho(0.0f, m_iScreenWidth-1, m_iScreenHeight-1, 0.0f, -1.0f, 1.0f);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity(); 
@@ -1218,7 +1218,7 @@ CSurface* CGraphicContext::InitializeSurface()
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
  
-  glOrtho(0.0f, m_iScreenWidth, m_iScreenHeight, 0.0f, -1.0f, 1.0f);
+  glOrtho(0.0f, m_iScreenWidth-1, m_iScreenHeight-1, 0.0f, -1.0f, 1.0f);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity(); 
