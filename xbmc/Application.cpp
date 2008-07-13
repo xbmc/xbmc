1d77113da53a17dcc5f5691363c2f5b1bc30b6fc
jmarshallnz <jmarshallnz@svn>
2008-07-13 02:30:28 +0000

    added: Preliminary support for an in-place edit control (based on the old one).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14036 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2432,7 +2432,17 @@ bool CApplication::OnKey(CKey& key)
     // current active window isnt the fullscreen window
     // just use corresponding section from keymap.xml
     // to map key->action
-    if (key.FromKeyboard() && (iWin == WINDOW_DIALOG_KEYBOARD || iWin == WINDOW_DIALOG_NUMERIC) )
+
+    // first determine if we should use keyboard input directly
+    bool useKeyboard = key.FromKeyboard() && (iWin == WINDOW_DIALOG_KEYBOARD || iWin == WINDOW_DIALOG_NUMERIC);
+    CGUIWindow *window = m_gWindowManager.GetWindow(iWin);
+    if (window)
+    {
+      CGUIControl *control = window->GetFocusedControl();
+      if (control && control->GetControlType() == CGUIControl::GUICONTROL_EDIT)
+        useKeyboard = true;
+    }
+    if (useKeyboard)
     {
       if (key.GetFromHttpApi())
       {
@@ -2461,20 +2471,23 @@ bool CApplication::OnKey(CKey& key)
           action.unicode = 0;
         }
       }
+#ifdef HAS_SDL
+      g_Keyboard.Reset();
+#endif
+      if (OnAction(action))
+        return true;
+      // failed to handle the keyboard action, drop down through to standard action
+    }
+    if (key.GetFromHttpApi())
+    {
+      if (key.GetButtonCode() != KEY_INVALID)
+      {
+        action.wID = (WORD) key.GetButtonCode();
+        g_buttonTranslator.GetAction(iWin, key, action);
+      }
     }
     else
-    {
-      if (key.GetFromHttpApi())
-      {
-        if (key.GetButtonCode() != KEY_INVALID)
-        {
-          action.wID = (WORD) key.GetButtonCode();
-          g_buttonTranslator.GetAction(iWin, key, action);
-        }
-      }
-      else
-        g_buttonTranslator.GetAction(iWin, key, action);
-    }
+      g_buttonTranslator.GetAction(iWin, key, action);
   }
   if (!key.IsAnalogButton())
     CLog::Log(LOGDEBUG, "%s: %i pressed, action is %i", __FUNCTION__, (int) key.GetButtonCode(), action.wID);
