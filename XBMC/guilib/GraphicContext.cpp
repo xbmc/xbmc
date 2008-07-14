a7e4144b810d2cdb5abf3007af21c80dbab9b4bd
jmarshallnz <jmarshallnz@svn>
2008-07-14 04:27:42 +0000

    changed: Use the calibrated pixelratio for aspect ratio correction of skin elements.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14057 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/GraphicContext.cpp
+++ XBMC/guilib/GraphicContext.cpp
@@ -809,6 +809,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 1920;
     g_settings.m_ResInfo[res].iHeight = 1080;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 1.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "1080i 16:9");
     break;
   case HDTV_720p:
@@ -816,6 +817,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 1280;
     g_settings.m_ResInfo[res].iHeight = 720;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_PROGRESSIVE | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 1.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "720p 16:9");
     break;
   case HDTV_480p_4x3:
@@ -823,6 +825,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_PROGRESSIVE;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "480p 4:3");
     break;
   case HDTV_480p_16x9:
@@ -830,6 +833,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_PROGRESSIVE | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f*4.0f / 3.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "480p 16:9");
     break;
   case NTSC_4x3:
@@ -837,6 +841,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "NTSC 4:3");
     break;
   case NTSC_16x9:
@@ -844,6 +849,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f*4.0f / 3.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "NTSC 16:9");
     break;
   case PAL_4x3:
@@ -851,6 +857,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 576;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED;
+    g_settings.m_ResInfo[res].fPixelRatio = 128.0f / 117.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "PAL 4:3");
     break;
   case PAL_16x9:
@@ -858,6 +865,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 576;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 128.0f / 117.0f*4.0f / 3.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "PAL 16:9");
     break;
   case PAL60_4x3:
@@ -865,6 +873,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "PAL60 4:3");
     break;
   case PAL60_16x9:
@@ -872,6 +881,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     g_settings.m_ResInfo[res].iWidth = 720;
     g_settings.m_ResInfo[res].iHeight = 480;
     g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_INTERLACED | D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 4320.0f / 4739.0f*4.0f / 3.0f;
     strcpy(g_settings.m_ResInfo[res].strMode, "PAL60 16:9");
     break;
   case DESKTOP:
@@ -882,6 +892,7 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
              "%dx%d (Full Screen)", g_settings.m_ResInfo[res].iWidth, g_settings.m_ResInfo[res].iHeight);
     if ((float)g_settings.m_ResInfo[res].iWidth/(float)g_settings.m_ResInfo[res].iHeight >= fOptimalSwitchPoint)
       g_settings.m_ResInfo[res].dwFlags = D3DPRESENTFLAG_WIDESCREEN;
+    g_settings.m_ResInfo[res].fPixelRatio = 1.0f;
     break;
   case WINDOW:
     g_settings.m_ResInfo[WINDOW] = g_settings.m_ResInfo[PAL60_4x3];
@@ -891,21 +902,16 @@ void CGraphicContext::ResetScreenParameters(RESOLUTION res)
     break;
   }
   ResetOverscan(res, g_settings.m_ResInfo[res].Overscan);
-  g_settings.m_ResInfo[res].fPixelRatio = GetPixelRatio(res);
 }
 
 float CGraphicContext::GetPixelRatio(RESOLUTION iRes) const
 {
+  // TODO: Why do we set a pixel ratio of 1.0 if the
+  //       user is not running fullscreen?
   if (!m_bFullScreenRoot)
     return 1.0f;
-  if (iRes == HDTV_1080i || iRes == HDTV_720p) return 1.0f;
-  if (iRes == HDTV_480p_4x3 || iRes == NTSC_4x3 || iRes == PAL60_4x3) return 4320.0f / 4739.0f;
-  if (iRes == HDTV_480p_16x9 || iRes == NTSC_16x9 || iRes == PAL60_16x9) return 4320.0f / 4739.0f*4.0f / 3.0f;
-  if (iRes == PAL_16x9) return 128.0f / 117.0f*4.0f / 3.0f;
 
-  // TODO: use XRandR to query physical size and obtain exact pixel ratio
-  // for now, just assume square pixels (most monitors' native resolution)
-  return 1.0f;
+  return g_settings.m_ResInfo[iRes].fPixelRatio;
 }
 
 void CGraphicContext::Clear()
