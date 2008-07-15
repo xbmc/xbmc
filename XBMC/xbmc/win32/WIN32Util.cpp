9c91bca17b2b83a9ef2c60b411d6b0aaf1558f6f
wiso <wiso@svn>
2008-07-15 17:35:53 +0000

    [win32]: fixed restart and shutdown powerstate. Thanks to chadoe for the privileges token code.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14115 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/win32/WIN32Util.cpp
+++ XBMC/xbmc/win32/WIN32Util.cpp
@@ -234,23 +234,41 @@ bool CWIN32Util::PowerManagement(PowerState State)
 {
 // SetSuspendState not available in vs2003
 #if _MSC_VER > 1400
+  HANDLE hToken;
+  TOKEN_PRIVILEGES tkp;
+  // Get a token for this process.
+  if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+  {
+    return false;
+  }
+  // Get the LUID for the shutdown privilege.
+  LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
+  tkp.PrivilegeCount = 1;  // one privilege to set   
+  tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+  // Get the shutdown privilege for this process.
+  AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0);
+  if (GetLastError() != ERROR_SUCCESS)
+  {
+    return false;
+  }
+
   switch (State)
   {
   case POWERSTATE_HIBERNATE:
     CLog::Log(LOGINFO, "Asking Windows to hibernate...");
-    return SetSuspendState(true,true,false) == TRUE;
+    return SUCCEEDED(SetSuspendState(true,true,false));
     break;
   case POWERSTATE_SUSPEND:
     CLog::Log(LOGINFO, "Asking Windows to suspend...");
-    return SetSuspendState(false,true,false) == TRUE;
+    return SUCCEEDED(SetSuspendState(false,true,false));
     break;
   case POWERSTATE_SHUTDOWN:
     CLog::Log(LOGINFO, "Shutdown Windows...");
-    return InitiateSystemShutdown(NULL, NULL,0,true,false) == TRUE;
+    return SUCCEEDED(ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_UPGRADE | SHTDN_REASON_FLAG_PLANNED));
     break;
   case POWERSTATE_REBOOT:
     CLog::Log(LOGINFO, "Rebooting Windows...");
-    return InitiateSystemShutdown(NULL, NULL,0,true,true) == TRUE;
+    return SUCCEEDED(ExitWindowsEx(EWX_REBOOT | EWX_FORCE, SHTDN_REASON_MAJOR_OPERATINGSYSTEM | SHTDN_REASON_MINOR_UPGRADE | SHTDN_REASON_FLAG_PLANNED));
     break;
   default:
     CLog::Log(LOGERROR, "Unknown PowerState called.");
