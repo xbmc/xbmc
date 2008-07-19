d4c85fa4b99c40c701f942f4cdebfae308a5c888
yuvalt <yuvalt@svn>
2008-07-19 23:40:28 +0000

    fix mms support. no longer need libmms.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14266 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FileMMS.h
+++ xbmc/FileSystem/FileMMS.h
@@ -1,5 +1,3 @@
-#ifdef HAS_MMS
-
 #ifndef FILEMMS_H_
 #define FILEMMS_H_
 
@@ -25,30 +23,58 @@
  */
 
 #include "IFile.h"
-#include "libmms/mms.h"
+#include <iconv.h>
+#include <inttypes.h>
+
+#define MMS_BUF_SIZE 102400
+#define MMS_MAX_STREAMS 20
 
 namespace XFILE
 {
-class CFileMMS : public IFile
+class CFileMMS: public IFile
 {
 public:
-  CFileMMS();
-  virtual ~CFileMMS();
-  virtual __int64 GetPosition();
-  virtual __int64 GetLength();
-  virtual bool Open(const CURL& url, bool bBinary = true);
-  virtual bool Exists(const CURL& url) { return true;};
-  virtual int Stat(const CURL& url, struct __stat64* buffer) { errno = ENOENT; return -1; };
-  virtual unsigned int Read(void* lpBuf, __int64 uiBufSize);
-  virtual __int64 Seek(__int64 iFilePosition, int iWhence = SEEK_SET);
-  virtual void Close();
-  virtual CStdString GetContent();
+	CFileMMS();
+	virtual ~CFileMMS();
+	virtual __int64 GetPosition();
+	virtual __int64 GetLength();
+	virtual bool Open(const CURL& url, bool bBinary = true);
+	virtual bool Exists(const CURL& url) { return true;	};
+	virtual int Stat(const CURL& url, struct __stat64* buffer)	{ errno = ENOENT; return -1; };
+	virtual unsigned int Read(void* lpBuf, __int64 uiBufSize);
+	virtual __int64 Seek(__int64 iFilePosition, int iWhence = SEEK_SET);
+	virtual void Close();
+	virtual CStdString GetContent();
 
 protected:
-  mms_t* m_mms;
-  CStdString m_contenttype;
+
+	typedef struct
+	{
+		uint8_t buf[MMS_BUF_SIZE];
+		int num_bytes;
+	} command_t;
+
+	void put_32(command_t *cmd, uint32_t value);
+	uint32_t get_32(unsigned char *cmd, int offset);
+	void send_command(int s, int command, uint32_t switches, uint32_t extra,
+	                int length, char *data);
+	void string_utf16(char *dest, const char *src, int len);
+	void get_answer(int s);
+	int get_data(int s, char *buf, size_t count);
+	int get_header(int s, uint8_t *header);
+	int interp_header(uint8_t *header, int header_len);
+	int get_media_packet(int s, int padding);
+	int streaming_start(char* hostname, int port, char* path);
+
+	CStdString m_contenttype;
+	int seq_num;
+	int num_stream_ids;
+	int stream_ids[MMS_MAX_STREAMS];
+	uint8_t* out_buf[MMS_BUF_SIZE];
+	int out_buf_len;
+	int s;
+	int plen;
+	iconv_t url_conv;
 };
 }
-#endif // !defined(AFX_FILESHOUTCAST_H__6B6082E6_547E_44C4_8801_9890781659C0__INCLUDED_)
-
 #endif
