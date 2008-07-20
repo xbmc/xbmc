fc7c1976035b2a7813c21f091242f7991a944d1e
spiff_ <spiff_@svn>
2008-07-20 18:27:50 +0000

    changed: build shoutcast library instead of keeping binary in svn (linux x86, linux amd64)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14294 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/lib/libshout/libshout/stdafx.h
@@ -0,0 +1,11 @@
+// stdafx.h : Includedatei für Standardsystem-Includedateien,
+// oder projektspezifische Includedateien, die häufig benutzt, aber
+// in unregelmäßigen Abständen geändert werden.
+//
+
+#pragma once
+
+
+#define WIN32_LEAN_AND_MEAN		// Selten verwendete Teile der Windows-Header nicht einbinden
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert
