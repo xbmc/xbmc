4b67c7b1f2a35d3f7cb091483adf49659c87963d
spiff_ <spiff_@svn>
2008-07-20 19:00:23 +0000

    changed: build libid3tag instead of keeping binary in svn (linux x86, linux amd64, linux powerpc)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14296 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- sources/libid3tag/libid3tag/make-xbmc-lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-g++ -shared -o libid3tag-i486-linux.so .libs/*.o `cat ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.def` ../../../XBMC/xbmc/cores/DllLoader/exports/wrapper.o -fPIC -O3
