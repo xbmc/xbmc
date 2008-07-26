1ca97da0846f9b6395a216cfe532ae670da771c5
d4rkm4ster <d4rkm4ster@svn>
2008-07-26 09:45:31 +0000

    added: some OpenGL spectrum settings
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14462 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/visualizations/OpenGLSpectrum/opengl_spectrum.cpp
+++ xbmc/visualizations/OpenGLSpectrum/opengl_spectrum.cpp
@@ -44,10 +44,33 @@ GLfloat x_angle = 20.0, x_speed = 0.0;
 GLfloat z_angle = 0.0, z_speed = 0.0;
 GLfloat heights[16][16], cHeights[16][16], scale;
 GLfloat hSpeed = 0.05;
+GLenum  g_mode = GL_FILL;
+vector<VisSetting> g_vecSettings;
 
 extern "C" void Create(void* pd3dDevice, int iPosX, int iPosY, int iWidth, int iHeight, const char* szVisualisationName, float fPixelRatio)
 {
+  g_vecSettings.clear();
+  VisSetting scale(VisSetting::SPIN, "Bar Height");
+  scale.AddEntry("Default");
+  scale.AddEntry("Big");
+  scale.AddEntry("Very Big");
+  scale.AddEntry("Small");
 
+  VisSetting mode(VisSetting::SPIN, "Mode");
+  mode.AddEntry("Default");
+  mode.AddEntry("Wireframe");
+  mode.AddEntry("Points");
+
+  VisSetting speed(VisSetting::SPIN, "Speed");
+  speed.AddEntry("Default");
+  speed.AddEntry("Slow");
+  speed.AddEntry("Very Slow");
+  speed.AddEntry("Fast");
+  speed.AddEntry("Very Fast");
+
+  g_vecSettings.push_back( scale );
+  g_vecSettings.push_back( mode );  
+  g_vecSettings.push_back( speed );
 }
 
 void draw_rectangle(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2)
@@ -78,16 +101,28 @@ void draw_bar(GLfloat x_offset, GLfloat z_offset, GLfloat height, GLfloat red, G
 {
   GLfloat width = 0.1;
 
-  glColor3f(red,green,blue);
-  draw_rectangle(x_offset, height, z_offset, x_offset + width, height, z_offset + 0.1);
+  if (g_mode == GL_POINT)
+    glColor3f(0.2, 1.0, 0.2);
+
+  if (g_mode != GL_POINT)
+    {
+      glColor3f(red,green,blue);
+      draw_rectangle(x_offset, height, z_offset, x_offset + width, height, z_offset + 0.1);
+    }
   draw_rectangle(x_offset, 0, z_offset, x_offset + width, 0, z_offset + 0.1);
 	
-  glColor3f(0.5 * red, 0.5 * green, 0.5 * blue);
-  draw_rectangle(x_offset, 0.0, z_offset + 0.1, x_offset + width, height, z_offset + 0.1);
+  if (g_mode != GL_POINT)
+    {
+      glColor3f(0.5 * red, 0.5 * green, 0.5 * blue);
+      draw_rectangle(x_offset, 0.0, z_offset + 0.1, x_offset + width, height, z_offset + 0.1);
+    }
   draw_rectangle(x_offset, 0.0, z_offset, x_offset + width, height, z_offset );
 
-  glColor3f(0.25 * red, 0.25 * green, 0.25 * blue);
-  draw_rectangle(x_offset, 0.0, z_offset , x_offset, height, z_offset + 0.1);	
+  if (g_mode != GL_POINT)
+    {
+      glColor3f(0.25 * red, 0.25 * green, 0.25 * blue);
+      draw_rectangle(x_offset, 0.0, z_offset , x_offset, height, z_offset + 0.1);	
+    }
   draw_rectangle(x_offset + width, 0.0, z_offset , x_offset + width, height, z_offset + 0.1);	
 }
 
@@ -103,7 +138,7 @@ void draw_bars(void)
   glRotatef(x_angle,1.0,0.0,0.0);
   glRotatef(y_angle,0.0,1.0,0.0);
   glRotatef(z_angle,0.0,0.0,1.0);
-
+  glPolygonMode(GL_FRONT_AND_BACK, g_mode);
   glBegin(GL_TRIANGLES);
   for(y = 0; y < 16; y++)
     {
@@ -122,10 +157,13 @@ void draw_bars(void)
               cHeights[y][x] -= hSpeed;
             }
           }
-          draw_bar(x_offset, z_offset, cHeights[y][x], r_base - (x * (r_base / 15.0)), x * (1.0 / 15), b_base);
+          draw_bar(x_offset, z_offset, 
+		   cHeights[y][x], r_base - (x * (r_base / 15.0)), 
+		   x * (1.0 / 15), b_base);
         }
     }
   glEnd();
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
   glPopMatrix();
 }
 
@@ -271,6 +309,7 @@ extern "C" void GetPresets(char ***pPresets, int *currentPreset, int *numPresets
 //-----------------------------------------------------------------------------
 extern "C" void GetSettings(vector<VisSetting> **vecSettings)
 {
+  *vecSettings = &g_vecSettings;
   return;
 }
 
@@ -279,5 +318,76 @@ extern "C" void GetSettings(vector<VisSetting> **vecSettings)
 //-----------------------------------------------------------------------------
 extern "C" void UpdateSetting(int num)
 {
+  if ( (int)g_vecSettings.size() <= num || num < 0 )
+    return;
 
+  if (strcmp(g_vecSettings[num].name, "Size")==0)
+  {
+    switch (g_vecSettings[num].current)
+      {
+      case 0:
+	scale = 1.0 / log(256.0);
+	break;
+
+      case 1:
+	scale = 2.0 / log(256.0);
+	break;
+
+      case 2:
+	scale = 3.0 / log(256.0);
+	break;
+
+      case 3:
+	scale = 0.5 / log(256.0);
+	break;
+
+      case 4:
+	scale = 0.33 / log(256.0);
+	break;
+      }
+  }
+
+  if (strcmp(g_vecSettings[num].name, "Speed")==0)
+  {
+    switch (g_vecSettings[num].current)
+      {
+      case 0:
+	hSpeed = 0.05;
+	break;
+
+      case 1:
+	hSpeed = 0.025;
+	break;
+
+      case 2:
+	hSpeed = 0.0125;
+	break;
+
+      case 3:
+	hSpeed = 0.10;
+	break;
+
+      case 4:
+	hSpeed = 0.20;
+	break;
+      }
+  }
+
+  if (strcmp(g_vecSettings[num].name, "Mode")==0)
+  {
+    switch (g_vecSettings[num].current)
+      {
+      case 0:
+	g_mode = GL_FILL;
+	break;
+
+      case 1:
+	g_mode = GL_LINE;
+	break;
+
+      case 2:
+	g_mode = GL_POINT;
+	break;
+      }
+  }
 }
