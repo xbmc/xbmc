fc4907d7dff1138b000876d01addfa7a074a5dc0
jmarshallnz <jmarshallnz@svn>
2008-07-27 03:10:50 +0000

    fixed: <info> tag in multiimage controls weren't being read.  Note that <texturepath>$INFO[blah]</texturepath> is preferable (fixes Ticket #4373).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14499 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GuiControlFactory.cpp
+++ guilib/GuiControlFactory.cpp
@@ -963,7 +963,8 @@ CGUIControl* CGUIControlFactory::Create(DWORD dwParentId, const FRECT &rect, TiX
   XMLUtils::GetBoolean(pControlNode, "scroll", bScrollLabel);
   XMLUtils::GetBoolean(pControlNode,"pulseonselect", bPulse);
 
-  GetTexture(pControlNode, "imagepath", texturePath);
+  if (!GetTexture(pControlNode, "imagepath", texturePath))
+    GetInfoLabel(pControlNode, "imagepath", texturePath.file);
   XMLUtils::GetDWORD(pControlNode,"timeperimage", timePerImage);
   XMLUtils::GetDWORD(pControlNode,"fadetime", fadeTime);
   XMLUtils::GetDWORD(pControlNode,"pauseatend", timeToPauseAtEnd);
