7f00c4316209469b87fa9c71f59cdc2abb0239a3
jmarshallnz <jmarshallnz@svn>
2008-07-28 10:23:58 +0000

    fixed: Scrolling in fadelabels was incorrect.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14529 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/GUIFont.cpp
+++ XBMC/guilib/GUIFont.cpp
@@ -123,7 +123,7 @@ void CGUIFont::DrawScrollingText(float x, float y, const std::vector<DWORD> &col
   //   pixelPos is the amount in pixels to move the string by.
   //   characterPos is the amount in characters to rotate the string by.
   //
-  float offset = 0;
+  float offset = scrollInfo.pixelPos;
   if (!scrollInfo.waitTime)
   {
     // move along by the appropriate scroll amount
@@ -152,7 +152,7 @@ void CGUIFont::DrawScrollingText(float x, float y, const std::vector<DWORD> &col
       }
       offset = scrollInfo.pixelPos;
     }
-    else
+    else if (scrollInfo.pixelSpeed < 0)
     { // scrolling backwards
       // we want to move scrollAmount, grab the next character
       float charWidth = GetCharWidth(scrollInfo.GetCurrentChar(text));
