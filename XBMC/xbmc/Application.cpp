1639700b4b8aa18b77278a5721742e99dbdd25be
althekiller <althekiller@svn>
2008-07-29 21:58:49 +0000

    fixed: Keyboard wasn't reset when leaving screensaver, so the press was processed twice.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14584 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Application.cpp
+++ XBMC/xbmc/Application.cpp
@@ -2385,8 +2385,10 @@ bool CApplication::OnKey(CKey& key)
     m_idleTimer.StartZero();
 
     ResetScreenSaver();
-    if (ResetScreenSaverWindow())
+    if (ResetScreenSaverWindow()) {
+      g_Keyboard.Reset();
       return true;
+    }
   }
 
   // get the current active window
