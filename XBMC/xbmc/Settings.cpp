ed7e3a98ebad88fd4d1e77421019167fdfacf592
kraqh3d <kraqh3d@svn>
2008-07-30 01:18:34 +0000

    added: advanced setting option to disable automatic removal of items from the database during "update library" or "update on startup"
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14588 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Settings.cpp
+++ XBMC/xbmc/Settings.cpp
@@ -226,6 +226,7 @@ CSettings::CSettings(void)
   g_advancedSettings.m_bVideoLibraryAllItemsOnBottom = false;
   g_advancedSettings.m_bVideoLibraryHideRecentlyAddedItems = false;
   g_advancedSettings.m_bVideoLibraryHideEmptySeries = false;
+  g_advancedSettings.m_bVideoLibraryCleanOnUpdate = true;
 
   g_advancedSettings.m_bUseEvilB = true;
 
@@ -1132,6 +1133,7 @@ void CSettings::LoadAdvancedSettings()
     XMLUtils::GetBoolean(pElement, "allitemsonbottom", g_advancedSettings.m_bVideoLibraryAllItemsOnBottom);
     XMLUtils::GetBoolean(pElement, "hiderecentlyaddeditems", g_advancedSettings.m_bVideoLibraryHideRecentlyAddedItems);
     XMLUtils::GetBoolean(pElement, "hideemptyseries", g_advancedSettings.m_bVideoLibraryHideEmptySeries);
+    XMLUtils::GetBoolean(pElement, "cleanonupdate", g_advancedSettings.m_bVideoLibraryCleanOnUpdate);
     GetString(pElement, "itemseparator", g_advancedSettings.m_videoItemSeparator, " / ");
   }
 
