d7ab0030bd577570ee1c08f7e4b3714abe712374
elupus <elupus@svn>
2008-07-31 17:14:19 +0000

    fixed: dvdplayer wouldn't display still frames following a VTS change (could happen on initial startup).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@14609 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -2211,6 +2211,10 @@ int CDVDPlayer::OnDVDNavResult(void* pData, int iMessage)
 
         m_SelectionStreams.Clear(STREAM_NONE, STREAM_SOURCE_NAV);
         m_SelectionStreams.Update(m_pInputStream, m_pDemuxer);
+
+        // we must hold here once more, otherwise the demuxer message
+        // will be executed after demuxer have filled with data
+        return NAVRESULT_HOLD;
       }
       break;
     case DVDNAV_CELL_CHANGE:
