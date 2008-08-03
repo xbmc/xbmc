4494ca61b0506016e1a982c45d64ecd23dcb2240
jmarshallnz <jmarshallnz@svn>
2008-08-03 00:35:00 +0000

    fixed: Multiimages could start loading folders from weird locations.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14656 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIMultiImage.cpp
+++ guilib/GUIMultiImage.cpp
@@ -316,7 +316,10 @@ void CGUIMultiImage::LoadDirectory()
 
     // Load in our images from the directory specified
     // m_currentPath is relative (as are all skin paths)
-    CStdString realPath = g_TextureManager.GetTexturePath(m_currentPath);
+    CStdString realPath = g_TextureManager.GetTexturePath(m_currentPath, true);
+    if (realPath.IsEmpty())
+      return;
+
     CUtil::AddSlashAtEnd(realPath);
     CHDDirectory dir;
     CFileItemList items;
