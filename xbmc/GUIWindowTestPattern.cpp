9d447d92fd63f68d97211aceb3dc451a2ad2684c
yuvalt <yuvalt@svn>
2008-08-05 07:19:31 +0000

    add two more test patterns for brightness/contrast and judder
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14694 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowTestPattern.cpp
+++ xbmc/GUIWindowTestPattern.cpp
@@ -24,7 +24,9 @@
 #include "Application.h"
 #include "GUIWindowManager.h"
 
-#define NUM_PATTERNS 3
+#define NUM_PATTERNS 5
+#define BOUNCE_SQUARE_SIZE 100
+#define BLINK_CYCLE 100
 
 CGUIWindowTestPattern::CGUIWindowTestPattern(void)
     : CGUIWindow(WINDOW_TEST_PATTERN, "")
@@ -55,7 +57,7 @@ bool CGUIWindowTestPattern::OnAction(const CAction &action)
   case ACTION_MOVE_RIGHT:
      m_pattern = (m_pattern + 1) % NUM_PATTERNS;
      break;
-    
+
   }
   return CGUIWindow::OnAction(action); // base class to handle basic movement etc.
 }
@@ -66,6 +68,11 @@ bool CGUIWindowTestPattern::OnMessage(CGUIMessage& message)
   {
   case GUI_MSG_WINDOW_INIT:
     m_pattern = 0;
+    m_bounceDirectionX = 1;
+    m_bounceDirectionY = 1;
+    m_bounceX = 0;
+    m_bounceY = 0;
+    m_blinkFrame = 0;
     break;
 
   }
@@ -85,15 +92,23 @@ void CGUIWindowTestPattern::Render()
   switch (m_pattern)
   {
     case 0:
-      DrawVerticalLines(top, left, bottom, right);
+      DrawContrastBrightnessPattern(top, left, bottom, right);
       break;
- 
+
     case 1:
-      DrawHorizontalLines(top, left, bottom, right);
+      DrawVerticalLines(top, left, bottom, right);
       break;
 
     case 2:
+      DrawHorizontalLines(top, left, bottom, right);
+      break;
+
+    case 3:
       DrawCheckers(top, left, bottom, right);
+      break;
+
+    case 4:
+      DrawBouncingRectangle(top, left, bottom, right);
       break;
   }
 
@@ -107,8 +122,8 @@ void CGUIWindowTestPattern::DrawVerticalLines(int top, int left, int bottom, int
   glColor3f(1, 1, 1);
   for (int i = left; i <= right; i += 2)
   {
-    glVertex3i(i, top, 0);
-    glVertex3i(i, bottom, 0);
+    glVertex2d(i, top);
+    glVertex2d(i, bottom);
   }
   glEnd();
 }
@@ -117,10 +132,10 @@ void CGUIWindowTestPattern::DrawHorizontalLines(int top, int left, int bottom, i
 {
   glBegin(GL_LINES);
   glColor3f(1, 1, 1);
-  for (int i = top; i <= bottom; i += 2) 
+  for (int i = top; i <= bottom; i += 2)
   {
-    glVertex3i(left, i, 0);
-    glVertex3i(right, i, 0);
+    glVertex2d(left, i);
+    glVertex2d(right, i);
   }
   glEnd();
 }
@@ -129,16 +144,129 @@ void CGUIWindowTestPattern::DrawCheckers(int top, int left, int bottom, int righ
 {
   glBegin(GL_POINTS);
   glColor3f(1, 1, 1);
-  for (int y = top; y <= bottom; y++) 
+  for (int y = top; y <= bottom; y++)
   {
-    for (int x = left; x <= right; x += 2) 
+    for (int x = left; x <= right; x += 2)
     {
       if (y % 2 == 0)
-        glVertex3i(x, y, 0);
+        glVertex2d(x, y);
       else
-        glVertex3i(x+1, y, 0);
+        glVertex2d(x+1, y);
     }
   }
   glEnd();
 }
 
+void CGUIWindowTestPattern::DrawBouncingRectangle(int top, int left, int bottom, int right)
+{
+  m_bounceX += m_bounceDirectionX;
+  m_bounceY += m_bounceDirectionY;
+
+  if ((m_bounceDirectionX == 1 && m_bounceX + BOUNCE_SQUARE_SIZE == right) || (m_bounceDirectionX == -1 && m_bounceX == left))
+    m_bounceDirectionX = -m_bounceDirectionX;
+
+  if ((m_bounceDirectionY == 1 && m_bounceY + BOUNCE_SQUARE_SIZE == bottom) || (m_bounceDirectionY == -1 && m_bounceY == top))
+    m_bounceDirectionY = -m_bounceDirectionY;
+
+  glColor3f(1, 1, 1);
+  glRecti(m_bounceX, m_bounceY, m_bounceX + BOUNCE_SQUARE_SIZE, m_bounceY + BOUNCE_SQUARE_SIZE);
+}
+
+void CGUIWindowTestPattern::DrawContrastBrightnessPattern(int top, int left, int bottom, int right)
+{
+  int x5p = (int) (left + (0.05f * (right - left)));
+  int y5p = (int) (top + (0.05f * (bottom - top)));
+  int x12p = (int) (left + (0.125f * (right - left)));
+  int y12p = (int) (top + (0.125f * (bottom - top)));
+  int x25p = (int) (left + (0.25f * (right - left)));
+  int y25p = (int) (top + (0.25f * (bottom - top)));
+  int x37p = (int) (left + (0.375f * (right - left)));
+  int y37p = (int) (top + (0.375f * (bottom - top)));
+  int x50p = left + (right - left) / 2;
+  int y50p = top + (bottom - top) / 2;
+  int x62p = (int) (left + (0.625f * (right - left)));
+  int y62p = (int) (top + (0.625f * (bottom - top)));
+  int x75p = (int) (left + (0.75f * (right - left)));
+  int y75p = (int) (top + (0.75f * (bottom - top)));
+  int x87p = (int) (left + (0.875f * (right - left)));
+  int y87p = (int) (top + (0.875f * (bottom - top)));
+  int x95p = (int) (left + (0.95f * (right - left)));
+  int y95p = (int) (top + (0.95f * (bottom - top)));
+
+  m_blinkFrame = (m_blinkFrame + 1) % BLINK_CYCLE;
+
+  // draw main quadrants
+  glColor3f(1, 1, 1);
+  glRecti(x50p, top, right, y50p);
+  glRecti(left, y50p, x50p, bottom);
+
+  // draw border lines
+  glBegin(GL_LINES);
+    glColor3f(1, 1, 1);
+    glVertex2d(left, y5p);
+    glVertex2d(x50p, y5p);
+    glVertex2d(x5p, top);
+    glVertex2d(x5p, y50p);
+    glVertex2d(x50p, y95p);
+    glVertex2d(right, y95p);
+    glVertex2d(x95p, y50p);
+    glVertex2d(x95p, bottom);
+
+    glColor3f(0, 0, 0);
+    glVertex2d(x50p, y5p);
+    glVertex2d(right, y5p);
+    glVertex2d(x5p, y50p);
+    glVertex2d(x5p, bottom);
+    glVertex2d(left, y95p);
+    glVertex2d(x50p, y95p);
+    glVertex2d(x95p, top);
+    glVertex2d(x95p, y50p);
+  glEnd();
+
+  // draw inner rectangles
+  glColor3f(1, 1, 1);
+  glRecti(x12p, y12p, x37p, y37p);
+  glRecti(x62p, y62p, x87p, y87p);
+
+  glColor3f(0, 0, 0);
+  glRecti(x62p, y12p, x87p, y37p);
+  glRecti(x12p, y62p, x37p, y87p);
+
+  // draw inner circles
+  if (m_blinkFrame < BLINK_CYCLE / 2)
+    glColor3f(0.05f, 0.05f, 0.05f);
+  else
+    glColor3f(0, 0, 0);
+  DrawCircle(x25p, y75p, (y37p - y12p) / 3);
+  DrawCircle(x75p, y25p, (y37p - y12p) / 3);
+
+  if (m_blinkFrame < BLINK_CYCLE / 2)
+    glColor3f(0.95f, 0.95f, 0.95f);
+  else
+    glColor3f(1, 1, 1);
+  DrawCircle(x25p, y25p, (y37p - y12p) / 3);
+  DrawCircle(x75p, y75p, (y37p - y12p) / 3);
+}
+
+void CGUIWindowTestPattern::DrawCircle(int originX, int originY, int radius)
+{
+  float angle;
+  int vectorX;
+  int vectorY;
+  int vectorY1 = originY;
+  int vectorX1 = originX;
+
+  glBegin(GL_TRIANGLES);
+  for (int i = 0; i <= 360; i++)
+  {
+    angle = (float)(((double)i)/57.29577957795135);
+    vectorX = (int) (originX + (radius*(float)sin((double)angle)));
+    vectorY = (int) (originY + (radius*(float)cos((double)angle)));
+    glVertex2d(originX, originY);
+    glVertex2d(vectorX1, vectorY1);
+    glVertex2d(vectorX, vectorY);
+    vectorY1 = vectorY;
+    vectorX1 = vectorX;
+  }
+  glEnd();
+}
