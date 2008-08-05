2a5c471c3103a75bf9cd3a699b192ae74bd3ce57
jmarshallnz <jmarshallnz@svn>
2008-08-05 23:41:51 +0000

    changed: Lower frame limit of 10fps for scrolling text + less weight on the moving average.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14713 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/GUIFont.cpp
+++ XBMC/guilib/GUIFont.cpp
@@ -38,12 +38,14 @@ namespace MathUtils {
 
 float CScrollInfo::GetPixelsPerFrame()
 {
-  static const float alphaEMA = 0.01f;
+  static const float alphaEMA = 0.05f;
 
   if (0 == pixelSpeed)
     return 0; // not scrolling
   DWORD currentTime = timeGetTime();
   float delta = m_lastFrameTime ? (float)(currentTime - m_lastFrameTime) : m_averageFrameTime;
+  if (delta > 100)
+    delta = 100; // assume a minimum of 10 fps
   m_lastFrameTime = currentTime;
   // do an exponential moving average of the frame time
   m_averageFrameTime = m_averageFrameTime + (delta - m_averageFrameTime) * alphaEMA;
