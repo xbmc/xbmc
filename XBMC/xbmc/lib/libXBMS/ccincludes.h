28e068fec9598206cdde0072f827456d4f2269af
jmarshallnz <jmarshallnz@svn>
2008-08-10 03:02:24 +0000

    fixed: win32 build error.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14795 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libXBMS/ccincludes.h
+++ XBMC/xbmc/lib/libXBMS/ccincludes.h
@@ -39,13 +39,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <inttypes.h>
-#include <unistd.h>
 
 #include "ccutil.h"
 
 #ifdef _XBOX
 #include <xtl.h>
 #elif defined(_LINUX)
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/time.h>
