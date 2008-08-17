3ff004842e66a785a5d1bca88d9300934e6c9373
althekiller <althekiller@svn>
2008-08-17 20:09:13 +0000

    fixed: Don't check loglevel twice when rendering freemem info (it was checking the wrong constant values anyway).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14979 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/VideoRenderers/LinuxRenderer.cpp
+++ xbmc/cores/VideoRenderers/LinuxRenderer.cpp
@@ -1090,8 +1090,7 @@ void CLinuxRenderer::Render(DWORD flags)
       g_application.RenderFullScreen();
     }
 
-    if (g_advancedSettings.m_logLevel <= LOGNOTICE)
-       g_application.RenderMemoryStatus();
+    g_application.RenderMemoryStatus();
   }
 }
 
