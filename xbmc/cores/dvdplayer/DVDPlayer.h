cb5134b28444297acbb743b09cae39f9dd2efc39
elupus <elupus@svn>
2008-08-17 18:45:50 +0000

    changed: moved stream info extraction using dvdplayer codecs to separate file
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14977 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayer.h
+++ xbmc/cores/dvdplayer/DVDPlayer.h
@@ -210,13 +210,7 @@ public:
   virtual bool IsCaching() const { return m_caching; } 
   virtual int GetCacheLevel() const ; 
 
-  virtual int OnDVDNavResult(void* pData, int iMessage);
-
-  static bool ExtractThumb(const CStdString &strPath, const CStdString &strTarget);
-  
-  // GetFileMetaData will fill pItem's properties according to what can be extracted from the file.
-  static void GetFileMetaData(const CStdString &strPath, CFileItem *pItem); 
-    
+  virtual int OnDVDNavResult(void* pData, int iMessage);    
 protected:  
   friend class CSelectionStreams;
   void LockStreams()                                            { EnterCriticalSection(&m_critStreamSection); }
