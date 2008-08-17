636de11001cd99c7ca6c7617185c935ba9cf77ca
jmarshallnz <jmarshallnz@svn>
2008-08-17 06:38:10 +0000

    changed: Apply musicfilenamefilters whenever we load music tags for the infomanager.  Should fix Ticket #4453.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@14965 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileItem.cpp
+++ XBMC/xbmc/FileItem.cpp
@@ -2596,24 +2596,54 @@ bool CFileItem::LoadMusicTag()
     return true;
   // check db
   CMusicDatabase musicDatabase;
-  CSong song;
-  musicDatabase.Open();
-  if (musicDatabase.GetSongByFileName(m_strPath, song))
+  if (musicDatabase.Open())
   {
-    GetMusicInfoTag()->SetSong(song);
-    return true;
+    CSong song;
+    if (musicDatabase.GetSongByFileName(m_strPath, song))
+    {
+      GetMusicInfoTag()->SetSong(song);
+      return true;
+    }
+    musicDatabase.Close();
   }
   // load tag from file
+  CLog::Log(LOGDEBUG, "%s: loading tag information for file: %s", __FUNCTION__, m_strPath.c_str());
+  CMusicInfoTagLoaderFactory factory;
+  auto_ptr<IMusicInfoTagLoader> pLoader (factory.CreateLoader(m_strPath));
+  if (NULL != pLoader.get())
+  {
+    if (pLoader->Load(m_strPath, *GetMusicInfoTag()))
+      return true;
+  }
+  // no tag - try some other things
+  if (IsCDDA())
+  {
+    // we have the tracknumber...
+    int iTrack = GetMusicInfoTag()->GetTrackNumber();
+    if (iTrack >= 1)
+    {
+      CStdString strText = g_localizeStrings.Get(554); // "Track"
+      if (strText.GetAt(strText.size() - 1) != ' ')
+        strText += " ";
+      CStdString strTrack;
+      strTrack.Format(strText + "%i", iTrack);
+      GetMusicInfoTag()->SetTitle(strTrack);
+      GetMusicInfoTag()->SetLoaded(true);
+      return true;
+    }
+  }
   else
   {
-    CLog::Log(LOGDEBUG, "%s: loading tag information for file: %s", __FUNCTION__, m_strPath.c_str());
-    CMusicInfoTagLoaderFactory factory;
-    CMusicInfoTag tag;
-    auto_ptr<IMusicInfoTagLoader> pLoader (factory.CreateLoader(m_strPath));
-    if (NULL != pLoader.get())
+    CStdString fileName = CUtil::GetFileName(m_strPath);
+    CUtil::RemoveExtension(fileName);
+    for (unsigned int i = 0; i < g_advancedSettings.m_musicTagsFromFileFilters.size(); i++)
     {
-      if (pLoader->Load(m_strPath, *GetMusicInfoTag()))
+      CLabelFormatter formatter(g_advancedSettings.m_musicTagsFromFileFilters[i], "");
+      if (formatter.FillMusicTag(fileName, GetMusicInfoTag()))
+      {
+        GetMusicInfoTag()->SetLoaded(true);
         return true;
+      }
     }
   }
   return false;
