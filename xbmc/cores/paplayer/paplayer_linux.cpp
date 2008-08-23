7ee504de826c27c31219428ac6a0c92ba6490d34
tslayer <tslayer@svn>
2008-08-23 00:07:08 +0000

    Fixed: 2nd Shoutcast stream failed to play while 1st stream was still playing.  Caused other issues(such as XBMC hang).  Disabled crossfading for shoutcast.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15110 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/paplayer_linux.cpp
+++ xbmc/cores/paplayer/paplayer_linux.cpp
@@ -124,7 +124,7 @@ bool PAPlayer::OpenFile(const CFileItem& file, const CPlayerOptions &options)
 
   m_crossFading = g_guiSettings.GetInt("musicplayer.crossfade");
   //no crossfading for cdda, cd-reading goes mad and no crossfading for last.fm doesn't like two connections
-  if (file.IsCDDA() || file.IsLastFM()) m_crossFading = 0;
+  if (file.IsCDDA() || file.IsLastFM() || file.IsShoutCast() ) m_crossFading = 0;
   if (m_crossFading && IsPlaying())
   {
     //do a short crossfade on trackskip
