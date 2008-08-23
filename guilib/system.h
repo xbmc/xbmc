59e4c2b8cefcfcb91f9722f05dcd6d0066f4ba1b
charlydoes <charlydoes@svn>
2008-08-23 22:32:06 +0000

    [WIN32] fixed: HAS_SYSINFO wasn't defined.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15133 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/system.h
+++ guilib/system.h
@@ -167,6 +167,7 @@
 #define HAS_WIN32_NETWORK
 #define HAS_LIRC
 #define HAS_IRSERVERSUITE // depends on HAS_LIRC define
+#define HAS_SYSINFO
 
 #undef HAS_SDL_AUDIO   // use dsound for audio on win32
 #undef HAS_SCREENSAVER // no screensavers
