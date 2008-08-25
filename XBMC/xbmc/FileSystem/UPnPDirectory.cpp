56b25f1c55f8578d9fbf9a1e5eaf56e3fd422dfe
blittan <blittan@svn>
2008-08-25 22:35:45 +0000

    removed: Changelog Linux binary, replaced by python script. updated: Swedish translation, based on English r15195 fixed: replaced "Xbox Media Center" with "XBMC" on line 182
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@15196 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/UPnPDirectory.cpp
+++ XBMC/xbmc/FileSystem/UPnPDirectory.cpp
@@ -179,7 +179,7 @@ CUPnPDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items)
         }
 
         // same thing but special case for XBMC
-        if (root_id == "0" && ((*device)->m_ModelName.Find("Xbox Media Center", 0, true) >= 0)) {
+        if (root_id == "0" && ((*device)->m_ModelName.Find("XBMC", 0, true) >= 0)) {
 
             // look for a specific type to differentiate which folder we want
             if (audio && !video && !image) {
