850787d9147a0c367706252eadc80fda3f0a3fc5
AlTheKiller <AlTheKiller@svn>
2008-08-27 05:55:24 +0000

    added: liba52 sources from vendor branch.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@15224 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/cores/dvdplayer/Codecs/current/vc++/inttypes.h
@@ -0,0 +1,13 @@
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+#ifdef ARCH_X86
+typedef signed long long int64_t;
+#endif
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+#ifdef ARCH_X86
+typedef unsigned long long uint64_t;
+#endif
