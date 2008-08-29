500ecc1f0421cbc729176c1687e094973e61630c
AlTheKiller <AlTheKiller@svn>
2008-08-29 19:46:54 +0000

    added: libmpeg2-0.5.1 sources from vendor.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@15257 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/cores/dvdplayer/Codecs/libmpeg2/libvo/vo_internal.h
@@ -0,0 +1,38 @@
+/*
+ * vo_internal.h
+ * Copyright (C) 2000-2003 Michel Lespinasse <walken@zoy.org>
+ * Copyright (C) 1999-2000 Aaron Holtzman <aholtzma@ess.engr.uvic.ca>
+ *
+ * This file is part of mpeg2dec, a free MPEG-2 video stream decoder.
+ * See http://libmpeg2.sourceforge.net/ for updates.
+ *
+ * mpeg2dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mpeg2dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+extern vo_open_t vo_xv_open;
+extern vo_open_t vo_xv2_open;
+extern vo_open_t vo_x11_open;
+extern vo_open_t vo_dxrgb_open;
+extern vo_open_t vo_dx_open;
+extern vo_open_t vo_sdl_open;
+extern vo_open_t vo_null_open;
+extern vo_open_t vo_nullslice_open;
+extern vo_open_t vo_nullskip_open;
+extern vo_open_t vo_nullrgb16_open;
+extern vo_open_t vo_nullrgb32_open;
+extern vo_open_t vo_pgm_open;
+extern vo_open_t vo_pgmpipe_open;
+extern vo_open_t vo_md5_open;
+
