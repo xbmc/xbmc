700013868461f9acf0ffedceb0c0d1c8d7b68d03
jmarshallnz <jmarshallnz@svn>
2008-09-02 02:07:32 +0000

    fixed: Ticket #4768 - Choosing fanart for a movie adds it to the tvshows list.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15332 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoInfo.h
+++ xbmc/GUIWindowVideoInfo.h
@@ -47,6 +47,7 @@ protected:
   void Refresh();
   void Update();
   void SetLabel(int iControl, const CStdString& strLabel);
+  VIDEODB_CONTENT_TYPE GetContentType(const CFileItem *pItem) const;
 
   // link cast to movies
   void ClearCastList();
