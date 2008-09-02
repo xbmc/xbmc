fc9c44bac1eb959028803a8ac550e8cf6cb9b8f7
c0diq <c0diq@svn>
2008-09-02 01:06:48 +0000

    updated: zlib
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15328 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libUPnP/Platinum/ThirdParty/Neptune/ThirdParty/zlib-1.2.3/crc32.c
+++ xbmc/lib/libUPnP/Platinum/ThirdParty/Neptune/ThirdParty/zlib-1.2.3/crc32.c
@@ -9,7 +9,7 @@
  * factor of two increase in speed on a Power PC G4 (PPC7455) using gcc -O3.
  */
 
-/* @(#) $Id$ */
+/* @(#) $Id: crc32.c 87 2008-08-15 19:03:36Z bok $ */
 
 /*
   Note on the use of DYNAMIC_CRC_TABLE: there is no mutex or semaphore
