bcca583e9eba5ef08cc187bc5b4f3c1087bf46d8
davilla <davilla@svn>
2008-09-04 02:48:54 +0000

    [OSX] revert to static SDL libs that are built with xbmc patch, make xbmc patch name reflect what it patches, add wget/config/build script so that others know how to build OSX static SDL libs.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15384 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/lib/libSDL-OSX/buildSDL-osx.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+
+#-------------------------------------------------------------------------------
+# build libSDL/libSDLmain
+#depends_libs: libX11.6
+
+wget http://www.libsdl.org/release/SDL-1.2.13.tar.gz
+tar -xzf SDL-1.2.13.tar.gz
+cd SDL-1.2.13
+patch -p1 <../SDL-1.2.13-xbmc.patch
+./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --disable-nasm 
+make
+cp build/.libs/libSDL.a ../libSDL-osx.a
+cp build/libSDLmain.a ../libSDLmain-osx.a
+cd ..
+rm -rf SDL-1.2.13 SDL-1.2.13.tar.gz
+
+
+#-------------------------------------------------------------------------------
+# build libSDL_image
+#depends_libs: libsdl, libpng, libjpeg, libtiff, zlib
+
+wget http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.6.tar.gz
+tar -xzf SDL_image-1.2.6.tar.gz
+cd SDL_image-1.2.6
+./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --disable-sdltest 
+make
+mv .libs/libSDL_image.a ../libSDL_image-osx.a
+cd ..
+rm -rf SDL_image-1.2.6 SDL_image-1.2.6.tar.gz
+
+
+#-------------------------------------------------------------------------------
+# build libSDL_mixer
+#depends_libs: libsdl, libvorbis, libogg
+
+wget http://www.libsdl.org/projects/SDL_mixer/release/SDL_mixer-1.2.8.tar.gz
+tar -xzf SDL_mixer-1.2.8.tar.gz
+cd SDL_mixer-1.2.8
+./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --disable-music-mp3 --disable-sdltest
+make
+mv build/.libs/libSDL_mixer.a ../libSDL_mixer-osx.a
+cd ..
+rm -rf SDL_mixer-1.2.8 SDL_mixer-1.2.8.tar.gz
+
