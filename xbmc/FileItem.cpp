97cc674da2eb9f65608273f1ce09f17b043c71a9
vdrfan <vdrfan@svn>
2008-09-06 12:15:27 +0000

    fixed: do not try to cache local fanart for upnp
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15423 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -2423,7 +2423,7 @@ void CFileItem::CacheFanart() const
     return;
   // We don't have a cached image, so let's see if the user has a local image they want to use
 
-  if (IsInternetStream() || CUtil::IsFTP(m_strPath)) // no local fanart available for these
+  if (IsInternetStream() || CUtil::IsFTP(m_strPath) || CUtil::IsUPnP(m_strPath)) // no local fanart available for these
     return;
 
   CStdString localFanart;
