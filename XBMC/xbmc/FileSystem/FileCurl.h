6fd6808f3f5eb15b6f42e98e19e495d6e01b043f
elupus <elupus@svn>
2008-09-06 15:03:06 +0000

    changed: disable multi session connections agains libupnp's http server as it often stall on start the new session untill you finish the other (or perhaps just continue to read)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@15430 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/FileCurl.h
+++ XBMC/xbmc/FileSystem/FileCurl.h
@@ -128,6 +128,7 @@ namespace XFILE
       bool            m_opened;
       bool            m_useOldHttpVersion;
       bool            m_seekable;
+      bool            m_multisession;
       bool            m_binary;
 
       CRingBuffer     m_buffer;           // our ringhold buffer
