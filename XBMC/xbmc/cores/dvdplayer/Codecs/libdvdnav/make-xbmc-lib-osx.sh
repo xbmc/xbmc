02f75d4eb6ec7c269f9708fe088b220ac2267df2
davilla <davilla@svn>
2008-09-07 04:05:33 +0000

    [OSX] libdvdnav, libDVDCSS. minor tweeks to build scripts
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@15439 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/dvdplayer/Codecs/libdvdnav/make-xbmc-lib-osx.sh
+++ XBMC/xbmc/cores/dvdplayer/Codecs/libdvdnav/make-xbmc-lib-osx.sh
@@ -10,7 +10,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.4
 cd $XBMC_ROOT/xbmc/cores/dvdplayer/Codecs/libDVDCSS/
 make distclean >/dev/null 2>&1
 
-./configure CFLAGS="-fPIC -DXBMC -D_DLL -D_LINUX -fno-common -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" \
+./configure CFLAGS="-fPIC -fno-common -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" \
     --with-pic
 
 make
@@ -29,6 +29,7 @@ gcc -bundle -flat_namespace -undefined suppress -shared -fPIC \
 	-mmacosx-version-min=10.4 -o libdvdnav-osx.so src/.libs/*.o src/dvdread/.libs/*.o src/vm/.libs/*.o ../libDVDCSS/src/*.o
 
 cd $XBMC_ROOT/tools/Mach5;./mach5.rb $XBMC_ROOT/xbmc/cores/dvdplayer/Codecs/libdvdnav/libdvdnav-osx.so;mv output.so $XBMC_ROOT/system/players/dvdplayer/libdvdnav-osx.so
+chmod +x $XBMC_ROOT/system/players/dvdplayer/libdvdnav-osx.so
 
 rm $XBMC_ROOT/xbmc/cores/dvdplayer/Codecs/libdvdnav/libdvdnav-osx.so
 
