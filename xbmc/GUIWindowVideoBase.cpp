0a940e13f67c9cfa5184e351acad66de607f72f2
vdrfan <vdrfan@svn>
2008-09-11 15:46:42 +0000

    fixed: Ticket #4832 - IMDB no longer using folder names for lookups on video_ts vob files. (Note: this will also fix scraping on rarred content)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15523 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoBase.cpp
+++ xbmc/GUIWindowVideoBase.cpp
@@ -483,16 +483,12 @@ bool CGUIWindowVideoBase::ShowIMDB(CFileItem *item, const SScraperInfo& info2)
         IMDB.SetScraperInfo(info);
 
   CStdString movieName;
-  if (item->m_bIsFolder || settings.parent_name) // always search based on file paths on refresh
-  {
-    movieName = item->m_strPath;
-    if (!item->m_bIsFolder)
-      CUtil::GetParentPath(item->m_strPath,movieName);
-    CUtil::RemoveSlashAtEnd(movieName);
-    movieName = CUtil::GetFileName(movieName);
-  }
+  if (item->m_bIsFolder || settings.parent_name)
+    movieName = CUtil::GetMovieName(item);
   else
     movieName = CUtil::GetFileName(item->m_strPath);
+
+  CUtil::RemoveExtension(movieName);
 
   // 3. Run a loop so that if we Refresh we re-run this block
   bool needsRefresh(false);
