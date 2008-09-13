8a05a2308b797a02657e4eaeaa30b07657cbfc82
jmarshallnz <jmarshallnz@svn>
2008-09-13 10:06:35 +0000

    changed: Moved the "Hide artists who appear only on compilations" setting in to the GUI (missed files)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15544 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Settings.h
+++ xbmc/Settings.h
@@ -198,7 +198,6 @@ public:
 
     bool m_bMusicLibraryHideAllItems;
     bool m_bMusicLibraryAllItemsOnBottom;
-    bool m_bMusicLibraryHideCompilationArtists;
     bool m_bMusicLibraryAlbumsSortByArtistThenYear;
     CStdString m_strMusicLibraryAlbumFormat;
     CStdString m_strMusicLibraryAlbumFormatRight;
