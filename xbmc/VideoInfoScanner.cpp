dd9d31f1ecd6225e0d2a56cc18a7e2121dbf2fee
vdrfan <vdrfan@svn>
2008-10-04 15:59:52 +0000

    fixed: nfo scraping for movies was broken due to the latest changes made in revison 15731 .. sorry ;)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15775 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoInfoScanner.cpp
+++ xbmc/VideoInfoScanner.cpp
@@ -1314,6 +1314,9 @@ namespace VIDEO
             m_pObserver->OnSetTitle(CUtil::GetFileName(strPath));
           }
 
+          if(!info.strContent.Equals("tvshows"))
+            GetIMDBDetails(pItem, scrUrl, info, bGrabAny, pDlgProgress); 
+
           return URL_NFO;
         }
       }
