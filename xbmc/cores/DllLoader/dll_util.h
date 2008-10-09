2cf61467d1cc23d0b5df91f6c36a82184a38627f
wiso <wiso@svn>
2008-10-09 17:47:26 +0000

    [WIN32]: preliminary workaround to resolve missing exports with GetProcAddress.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15834 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/dll_util.h
+++ xbmc/cores/DllLoader/dll_util.h
@@ -28,6 +28,7 @@ extern "C"
 #endif
 
 uintptr_t create_dummy_function(const char* strDllName, const char* strFunctionName);
+uintptr_t get_win_function_address(const char* strDllName, const char* strFunctionName);
 
 #ifdef _cplusplus
 }
