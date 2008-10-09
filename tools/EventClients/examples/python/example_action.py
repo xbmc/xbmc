a624ab87dc389b3a4d27512fe45e9bd14b97b076
d4rkm4ster <d4rkm4ster@svn>
2008-10-09 10:21:29 +0000

    added: example event client for sending XBMC actions from the command line. usage: ./example_action.py "XBMC.ActivateWindow(ShutdownMenu)"
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15830 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ tools/EventClients/examples/python/example_action.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+# This is a simple example showing how you can send a key press event
+# to XBMC using the XBMCClient class
+
+import sys
+sys.path.append("../../lib/python")
+
+import time
+from xbmcclient import XBMCClient,ACTION_EXECBUILTIN,ACTION_BUTTON
+
+def main():
+
+    host = "localhost"
+    port = 9777
+    
+    # Create an XBMCClient object and connect
+    xbmc = XBMCClient("Example Remote", "../../icons/bluetooth.png")
+    xbmc.connect()
+
+    # send a up key press using the xbox gamepad map "XG" and button
+    # name "dpadup" ( see PacketBUTTON doc for more details)
+    try:
+        xbmc.send_action(sys.argv[2], ACTION_BUTTON)
+    except:
+        try:
+            xbmc.send_action(sys.argv[1], ACTION_EXECBUILTIN)
+        except Exception, e:
+            print str(e)
+            xbmc.send_action("XBMC.ActivateWindow(ShutdownMenu)")
+    
+
+    # ok we're done, close the connection
+    # Note that closing the connection clears any repeat key that is
+    # active. So in this example, the actual release button event above
+    # need not have been sent.
+    xbmc.close()
+
+if __name__=="__main__":
+    main()
