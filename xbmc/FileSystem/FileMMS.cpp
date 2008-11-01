17786802aa9daf8bc4860ff3222c36b6140a72f7
elupus <elupus@svn>
2008-11-01 00:01:06 +0000

    changed: reduce verbosity of mms file's
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@16086 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FileMMS.cpp
+++ xbmc/FileSystem/FileMMS.cpp
@@ -355,10 +355,13 @@ int CFileMMS::get_media_packet(int s, int padding)
     return 0;
   }
 
-  for (int i = 0; i < 8; i++)
+  if(g_advancedSettings.m_logLevel > LOG_LEVEL_NORMAL)
   {
-        CLog::Log(LOGDEBUG, "MMS: pre_header[%d] = %02x (%d)\n", i,
-                                pre_header[i], pre_header[i]);
+    for (int i = 0; i < 8; i++)
+    {
+          CLog::Log(LOGDEBUG, "MMS: pre_header[%d] = %02x (%d)\n", i,
+                                  pre_header[i], pre_header[i]);
+    }
   }
 
   if (pre_header[4] == 0x04)
