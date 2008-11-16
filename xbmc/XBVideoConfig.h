fd79113868b4a6647f541d7633dfb3a9ccd4ae52
elupus <elupus@svn>
2008-11-16 16:58:06 +0000

    changed: make sure we are able to set and restore xrandr modes when starting video and stopping.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@16201 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/XBVideoConfig.h
+++ xbmc/XBVideoConfig.h
@@ -53,7 +53,7 @@ public:
   RESOLUTION GetSafeMode() const;
   RESOLUTION GetBestMode() const;
 #ifdef HAS_SDL
-  void GetDesktopResolution(int &w, int &h, float &hz) const;
+  void GetCurrentResolution(RESOLUTION_INFO &info) const;
   int GetNumberOfResolutions() { return m_iNumResolutions; }
   void GetResolutionInfo(int num, RESOLUTION_INFO &info) { info = m_ResInfo[num]; }
 #endif
