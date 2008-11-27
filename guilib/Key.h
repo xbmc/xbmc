e510f99fd4e1ab5d72dd7410f1bcf9a4b67d687b
spiff_ <spiff_@svn>
2008-11-27 02:52:41 +0000

    added: ticket #3078 - Remote Audio Ouput Mode Selection.  action is audiotoggledigital
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@16365 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/Key.h
+++ guilib/Key.h
@@ -264,6 +264,7 @@
 #define ACTION_TOGGLE_FULLSCREEN      199 // switch 2 desktop resolution
 #define ACTION_TOGGLE_WATCHED         200 // Toggle watched status (videos)
 #define ACTION_SCAN_ITEM              201 // scan item
+#define ACTION_TOGGLE_DIGITAL_ANALOG  202 // switch digital <-> analog
 
 // Window ID defines to make the code a bit more readable
 #define WINDOW_INVALID                     9999
