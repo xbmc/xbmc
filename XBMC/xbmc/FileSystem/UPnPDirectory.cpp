49daf2689a839b43f5d617f35f46a6949a93348f
c0diq <c0diq@svn>
2008-12-11 12:14:38 +0000

    typo
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@16516 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/UPnPDirectory.cpp
+++ XBMC/xbmc/FileSystem/UPnPDirectory.cpp
@@ -178,7 +178,7 @@ CUPnPDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items)
             }
         }
 
-#ifdef DISABLE_SECIALCASE
+#ifdef DISABLE_SPECIALCASE
         // same thing but special case for XBMC
         if (root_id == "0" && (((*device)->m_ModelName.Find("XBMC", 0, true) >= 0) || ((*device)->m_ModelName.Find("Xbox Media Center", 0, true) >= 0))) {
 
