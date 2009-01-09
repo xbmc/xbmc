29c03ee82d2314dd2cd0da835c2cdbf4e94b463b
topfs2 <topfs2@svn>
2009-01-09 18:47:54 +0000

    Made paplayer_linux use a pcm buffer so it will only send safe chunks to the audiorenderer, this should fix buffer underrun on win32 audiorender and trying to get too much data from decoder which could lead to stutter
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@16981 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/paplayer.h
+++ XBMC/xbmc/cores/paplayer/paplayer.h
@@ -179,7 +179,7 @@ private:
   int               m_bitsPerSample[2];
 #elif defined(_LINUX)
   IDirectSoundRenderer* m_pAudioDecoder[2];
-  int m_channelPacketSize[2];
+  std::vector<unsigned char> m_pcmBuffer[2];
 #endif
 
   AudioPacket      m_packet[2][PACKET_COUNT];
