6c983b8b4a4d1eae6a0c4723e5d52330e80551df
jmarshallnz <jmarshallnz@svn>
2009-01-10 06:58:02 +0000

    fixed: A few warnings.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@16991 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIDialogFileBrowser.cpp
+++ XBMC/xbmc/GUIDialogFileBrowser.cpp
@@ -616,7 +616,7 @@ bool CGUIDialogFileBrowser::ShowAndGetImage(const CFileItemList &items, VECSOURC
   }
 
   if (flip)
-    *flip = browser->m_bFlip;
+    *flip = browser->m_bFlip != 0;
 
   m_gWindowManager.Remove(browser->GetID());
   delete browser;
