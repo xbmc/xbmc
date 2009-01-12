ea359dbccca60b294e6095271be74038abdc98d7
tslayer <tslayer@svn>
2009-01-12 00:11:30 +0000

    Fixed: Ticket #5612 - Added function to SetExtFileInfo on IDirectory.  When set to false, GetDirectory in SMB won't perform stat.  Better fix than r17018.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17025 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/VirtualPathDirectory.cpp
+++ xbmc/FileSystem/VirtualPathDirectory.cpp
@@ -83,7 +83,7 @@ bool CVirtualPathDirectory::GetDirectory(const CStdString& strPath, CFileItemLis
 
     CFileItemList tempItems;
     CLog::Log(LOGDEBUG,"Getting Directory (%s)", share.vecPaths[i].c_str());
-    if (CDirectory::GetDirectory(share.vecPaths[i], tempItems, m_strFileMask, m_useFileDirectories, m_allowPrompting, m_cacheDirectory))
+    if (CDirectory::GetDirectory(share.vecPaths[i], tempItems, m_strFileMask, m_useFileDirectories, m_allowPrompting, m_cacheDirectory, m_extFileInfo))
       items.Append(tempItems);
     else
     {
