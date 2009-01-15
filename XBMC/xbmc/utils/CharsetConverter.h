e0a870087561e36ec17781563700e49d8942e701
elupus <elupus@svn>
2009-01-15 22:45:10 +0000

    changed: don't always assume CStdStr<X> can be assigned from CStdStr<Y> in charset converter's convert added: CCharsetConverter::utf8To overloads for CStdStr<int32_t> and CStdStr<int16_t> targets (all utf16/ucs2 explicit operations should probably use similar functions instead of CStdStringW)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@17107 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/utils/CharsetConverter.h
+++ XBMC/xbmc/utils/CharsetConverter.h
@@ -46,6 +46,8 @@ public:
   void utf8ToStringCharset(CStdStringA& strSourceDest);
 
   void utf8To(const CStdStringA& strDestCharset, const CStdStringA& strSource, CStdStringA& strDest);
+  void utf8To(const CStdStringA& strDestCharset, const CStdStringA& strSource, CStdStr<int16_t>& strDest);
+  void utf8To(const CStdStringA& strDestCharset, const CStdStringA& strSource, CStdStr<int32_t>& strDest);
 
   void stringCharsetToUtf8(const CStdStringA& strSource, CStdStringA& strDest);
 
