363b9871aac06ff1fda2a72f5aba0ac6bfe97bf4
topfs2 <topfs2@svn>
2009-01-16 21:30:21 +0000

    [LINUX] fixed segfault on transition between songs in paplayer
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@17127 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/paplayer_linux.cpp
+++ XBMC/xbmc/cores/paplayer/paplayer_linux.cpp
@@ -739,7 +739,8 @@ void PAPlayer::ResetTime()
 __int64 PAPlayer::GetTime()
 {
   __int64  timeplus = m_BytesPerSecond ? (__int64)(((float) m_bytesSentOut / (float)m_BytesPerSecond ) * 1000.0) : 0;
-  timeplus -= (__int64)(m_pAudioDecoder[m_currentStream]->GetDelay() * 1000.0f);
+  if (m_pAudioDecoder[m_currentStream])
+    timeplus -= (__int64)(m_pAudioDecoder[m_currentStream]->GetDelay() * 1000.0f);
   return m_timeOffset + timeplus - m_currentFile->m_lStartOffset * 1000 / 75;
 }
 
