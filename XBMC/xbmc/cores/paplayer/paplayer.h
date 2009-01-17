c1916bf6b9d7216f61d825f145a8770380e7cd8c
topfs2 <topfs2@svn>
2009-01-17 20:16:55 +0000

    Made PAPlayer handle time with timeGetTime() instead of counting sent bytes, this grants much higher detail with time dealing fastforwarding or rewinding
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@17132 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/paplayer.h
+++ XBMC/xbmc/cores/paplayer/paplayer.h
@@ -57,6 +57,46 @@ struct AudioPacket
   int   stream;
 };
 
+class PAPlayerClock
+{
+public:
+  PAPlayerClock() { ResetClock(); }
+  __int64 GetTimeMS()
+  {
+    return Update();
+  }
+  void SetSpeed(int iSpeed)
+  {
+    Update();
+    m_Speed = iSpeed;
+  }
+
+  void SetClock(__int64 time)
+  {
+    m_timeAtLastUpdate = timeGetTime();
+    m_time = time;
+  }
+
+  void ResetClock()
+  {
+    m_timeAtLastUpdate = timeGetTime();
+    m_time = 0;
+    m_Speed = 1;
+  }
+
+private:
+  __int64 Update()
+  {
+    m_time += (timeGetTime() - m_timeAtLastUpdate) * m_Speed;
+    m_timeAtLastUpdate = timeGetTime();
+    return m_time;
+  }
+
+  __int64 m_time;
+  __int64 m_timeAtLastUpdate;
+  int     m_Speed;
+};
+
 class PAPlayer : public IPlayer, public CThread
 {
 public:
@@ -140,7 +180,6 @@ private:
 
   __int64 m_SeekTime;
   int     m_IsFFwdRewding;
-  __int64 m_timeOffset;
   bool    m_forceFadeToNext;
 
   int m_currentDecoder;
@@ -185,8 +224,8 @@ private:
   float             m_latency[2];
   unsigned char*    m_pcmBuffer[2];
   int               m_bufferPos[2];
-  int               m_packetsSentOut;
 #endif
+  PAPlayerClock     m_clock;
 
   AudioPacket      m_packet[2][PACKET_COUNT];
 
