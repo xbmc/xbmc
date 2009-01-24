ae2533c0b2a984e3d41f18942d7dcca6239acac2
charlydoes <charlydoes@svn>
2009-01-24 20:19:10 +0000

    fixed: partymode invalid query.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17306 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/PartyModeManager.cpp
+++ xbmc/PartyModeManager.cpp
@@ -586,7 +586,7 @@ bool CPartyModeManager::AddInitialSongs(vector<pair<int,long> > &songIDs)
       database.Open();
       database.GetSongsByWhere("", sqlWhereMusic, items);
     }
-    if (sqlWhereVideo.size() > 17)
+    if (sqlWhereVideo.size() > 19)
     {
       sqlWhereVideo[sqlWhereVideo.size() - 1] = ')'; // replace the last comma with closing bracket
       CVideoDatabase database;
