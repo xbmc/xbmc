1b857bcdcc2cba687849e4dda87e071f3808f7ee
jmarshallnz <jmarshallnz@svn>
2009-01-25 23:41:03 +0000

    changed: Prepare for g_advancedSettings.m_cachePath to become special://temp/
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17353 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -4551,12 +4551,12 @@ void CUtil::WipeDir(const CStdString& strPath) // DANGEROUS!!!!
   CUtil::GetRecursiveDirsListing(strPath,items);
   for (int i=items.Size()-1;i>-1;--i) // need to wipe them backwards
   {
-    ::RemoveDirectory((items[i]->m_strPath+"\\").c_str());
+    CDirectory::Remove((items[i]->m_strPath+"\\").c_str());
   }
 
   CStdString tmpPath = strPath;
   AddSlashAtEnd(tmpPath);
-  ::RemoveDirectory(tmpPath.c_str());
+  CDirectory::Remove(tmpPath.c_str());
 }
 
 void CUtil::ClearFileItemCache()
