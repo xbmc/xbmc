fe9668443d24d64639efb233e25b6934ec1c3c0e
wiso <wiso@svn>
2009-02-06 20:03:23 +0000

    [WIN32]: fixed projectM wouldn't find any presets after special path change
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17594 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/visualizations/XBMCProjectM/libprojectM/win32-dirent.cpp
+++ xbmc/visualizations/XBMCProjectM/libprojectM/win32-dirent.cpp
@@ -35,7 +35,7 @@ DIR *opendir(const char *name)
     {
         size_t base_length = strlen(name);
         const char *all = /* search pattern must end with suitable wildcard */
-            strchr("/\\", name[base_length - 1]) ? "*" : "/*";
+            strchr("/\\", name[base_length - 1]) ? "" : "/";
 
         if((dir = (DIR *) malloc(sizeof *dir)) != 0 &&
            (dir->name = (char *) malloc(base_length + strlen(all) + 1)) != 0)
