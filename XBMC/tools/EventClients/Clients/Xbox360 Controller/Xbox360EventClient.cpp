b1cb8b149560bc5066988da11f915292c51e0c0a
wiso <wiso@svn>
2009-02-14 19:15:25 +0000

    [WIN32]: fixed Xbox 360 Controller shoulder buttons not working with x360 event client. (thanks to darwin and bb10). replaced some tabs with blanks.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@17720 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/tools/EventClients/Clients/Xbox360 Controller/Xbox360EventClient.cpp
+++ XBMC/tools/EventClients/Clients/Xbox360 Controller/Xbox360EventClient.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include "Xbox360Controller.h"
 #include "../../lib/c++/xbmcclient.h"
-#pragma comment(lib, "wsock32.lib")		  // needed for xmbclient.h?
+#pragma comment(lib, "wsock32.lib")      // needed for xmbclient.h?
 
 // global variable :(
 // needed for exit event handler
@@ -12,112 +12,112 @@ CXBMCClient *client;
 
 BOOL exitHandler( DWORD ctrlType )
 {
-	// TODO: Send BYE Packet
-	delete client;
+  // TODO: Send BYE Packet
+  delete client;
 
-	WSACleanup();
+  WSACleanup();
 
-	return FALSE;
+  return FALSE;
 }
 
 void checkTrigger(Xbox360Controller &cont, CXBMCClient *client, int num, const char* name)
 {
-	if (cont.triggerMoved(num))
-	{
-		client->SendButton(name, "XG", 0x20, cont.getTrigger(num) * 128);
-	}
+  if (cont.triggerMoved(num))
+  {
+    client->SendButton(name, "XG", 0x20, cont.getTrigger(num) * 128);
+  }
 }
 
 void checkThumb(Xbox360Controller &cont, CXBMCClient *client, int num,
-				const char* leftname, const char* rightname)
+        const char* leftname, const char* rightname)
 {
-	if (cont.thumbMoved(num))
-	{
-		if (cont.getThumb(num) < 0)
-		{
-			client->SendButton(leftname, "XG", 0x20, -cont.getThumb(num));
-		}
-		else
-		{
-			client->SendButton(rightname, "XG", 0x20, cont.getThumb(num));
-		}
-	}
+  if (cont.thumbMoved(num))
+  {
+    if (cont.getThumb(num) < 0)
+    {
+      client->SendButton(leftname, "XG", 0x20, -cont.getThumb(num));
+    }
+    else
+    {
+      client->SendButton(rightname, "XG", 0x20, cont.getThumb(num));
+    }
+  }
 }
 
 void checkButton(Xbox360Controller &cont, CXBMCClient *client, int num, const char* name)
 {
-	if (cont.buttonPressed(num))
-	{
-		client->SendButton(name, "XG", 0x02);
-	}
-	else if (cont.buttonReleased(num))
-	{
-		client->SendButton(name, "XG", 0x04);
-	}
+  if (cont.buttonPressed(num))
+  {
+    client->SendButton(name, "XG", 0x02);
+  }
+  else if (cont.buttonReleased(num))
+  {
+    client->SendButton(name, "XG", 0x04);
+  }
 }
 
 int main(int argc, char* argv[])
 {
-	char *host = "localhost";
-	char *port = "9777";
+  char *host = "localhost";
+  char *port = "9777";
 
-	Xbox360Controller cont(0);
+  Xbox360Controller cont(0);
 
-	// Start Winsock stuff
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+  // Start Winsock stuff
+  WSADATA wsaData;
+  WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	if ( argc > 3 )
-	{
-		printf("USAGE: %s [HOST [PORT]]\n\nThe event client connects to the XBMC EventServer at HOST:PORT.\
-			    Default value for HOST is localhost, default value for port	is 9777.\n", argv[0]);
-		return -1;
-	}
+  if ( argc > 3 )
+  {
+    printf("USAGE: %s [HOST [PORT]]\n\nThe event client connects to the XBMC EventServer at HOST:PORT.\
+          Default value for HOST is localhost, default value for port  is 9777.\n", argv[0]);
+    return -1;
+  }
 
-	if ( argc > 1 )
-	{
-		host = argv[1];
-	}
+  if ( argc > 1 )
+  {
+    host = argv[1];
+  }
 
-	if ( argc > 2 )
-	{
-		port = argv[2];
-	}
+  if ( argc > 2 )
+  {
+    port = argv[2];
+  }
 
-	client = new CXBMCClient(host, atoi(port));
+  client = new CXBMCClient(host, atoi(port));
 
-	SetConsoleCtrlHandler( (PHANDLER_ROUTINE) exitHandler, TRUE);
+  SetConsoleCtrlHandler( (PHANDLER_ROUTINE) exitHandler, TRUE);
 
-	client->SendHELO("Xbox 360 Controller", 0);
+  client->SendHELO("Xbox 360 Controller", 0);
 
-	while(true)
-	{
-		if (cont.isConnected())
-		{
-			cont.updateState();
-			checkButton(cont, client, 0, "a");
-			checkButton(cont, client, 1, "b");
-			checkButton(cont, client, 2, "x");
-			checkButton(cont, client, 3, "y");
-			checkButton(cont, client, 4, "dpadup");
-			checkButton(cont, client, 5, "dpaddown");
-			checkButton(cont, client, 6, "dpadleft");
-			checkButton(cont, client, 7, "dpadright");
-			checkButton(cont, client, 8, "start");
-			checkButton(cont, client, 9, "back");
-			checkButton(cont, client, 10, "leftthumbbutton");
-			checkButton(cont, client, 11, "rightthumbbutton");
-			checkButton(cont, client, 12, "lefttrigger");
-			checkButton(cont, client, 13, "righttrigger");
-			checkTrigger(cont, client, 0, "rightanalogtrigger");
-			checkTrigger(cont, client, 1, "leftanalogtrigger");
-			checkThumb(cont, client, 0, "leftthumbstickleft", "leftthumbstickright");
-			checkThumb(cont, client, 1, "leftthumbstickdown", "leftthumbstickup");
-			checkThumb(cont, client, 2, "rightthumbstickleft", "rightthumbstickright");
-			checkThumb(cont, client, 3, "rightthumbstickdown", "rightthumbstickup");
-		}
-		Sleep(10);
-	}
+  while(true)
+  {
+    if (cont.isConnected())
+    {
+      cont.updateState();
+      checkButton(cont, client, 0, "a");
+      checkButton(cont, client, 1, "b");
+      checkButton(cont, client, 2, "x");
+      checkButton(cont, client, 3, "y");
+      checkButton(cont, client, 4, "dpadup");
+      checkButton(cont, client, 5, "dpaddown");
+      checkButton(cont, client, 6, "dpadleft");
+      checkButton(cont, client, 7, "dpadright");
+      checkButton(cont, client, 8, "start");
+      checkButton(cont, client, 9, "back");
+      checkButton(cont, client, 10, "leftthumbbutton");
+      checkButton(cont, client, 11, "rightthumbbutton");
+      checkButton(cont, client, 12, "white");
+      checkButton(cont, client, 13, "black");
+      checkTrigger(cont, client, 0, "rightanalogtrigger");
+      checkTrigger(cont, client, 1, "leftanalogtrigger");
+      checkThumb(cont, client, 0, "leftthumbstickleft", "leftthumbstickright");
+      checkThumb(cont, client, 1, "leftthumbstickdown", "leftthumbstickup");
+      checkThumb(cont, client, 2, "rightthumbstickleft", "rightthumbstickright");
+      checkThumb(cont, client, 3, "rightthumbstickdown", "rightthumbstickup");
+    }
+    Sleep(10);
+  }
 
-	return 0;
+  return 0;
 }
