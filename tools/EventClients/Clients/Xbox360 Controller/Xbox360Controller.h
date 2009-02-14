731b7787ac60285dd0394aa97f39ccba998a0cbe
wiso <wiso@svn>
2009-02-14 19:15:25 +0000

    [WIN32]: fixed Xbox 360 Controller shoulder buttons not working with x360 event client. (thanks to darwin and bb10). replaced some tabs with blanks.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17720 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/EventClients/Clients/Xbox360 Controller/Xbox360Controller.h
+++ tools/EventClients/Clients/Xbox360 Controller/Xbox360Controller.h
@@ -7,22 +7,22 @@
 class Xbox360Controller
 {
 private:
-    XINPUT_STATE state;
-    int num;
-	bool button_down[14];
-	bool button_pressed[14];
-    bool button_released[14];
+  XINPUT_STATE state;
+  int num;
+  bool button_down[14];
+  bool button_pressed[14];
+  bool button_released[14];
 
-    XINPUT_STATE getState();
-	void updateButton(int num, int button);
+  XINPUT_STATE getState();
+  void updateButton(int num, int button);
 public:
-    Xbox360Controller(int num);
-	void updateState();
-    bool isConnected();
-	bool buttonPressed(int num);
-	bool buttonReleased(int num);
-	bool thumbMoved(int num);
-	SHORT getThumb(int num);
-	bool triggerMoved(int num);
-	BYTE getTrigger(int num);
+  Xbox360Controller(int num);
+  void updateState();
+  bool isConnected();
+  bool buttonPressed(int num);
+  bool buttonReleased(int num);
+  bool thumbMoved(int num);
+  SHORT getThumb(int num);
+  bool triggerMoved(int num);
+  BYTE getTrigger(int num);
 };
