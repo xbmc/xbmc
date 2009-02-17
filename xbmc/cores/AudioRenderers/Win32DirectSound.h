f9068b41f062e19bcdb6b69300bcddb775264670
elupus <elupus@svn>
2009-02-17 00:30:37 +0000

    changed: remove no pointless DoWork in audio renderers
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17782 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/AudioRenderers/Win32DirectSound.h
+++ xbmc/cores/AudioRenderers/Win32DirectSound.h
@@ -61,7 +61,6 @@ public:
   virtual HRESULT SetCurrentVolume(LONG nVolume);
   virtual int SetPlaySpeed(int iSpeed);
   virtual void WaitCompletion();
-  virtual void DoWork();
   virtual void SwitchChannels(int iAudioStream, bool bAudioOnAllSpeakers);
 
 private:
