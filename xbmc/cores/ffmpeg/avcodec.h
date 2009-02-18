993e26dbb219cb470cd3aedb1486a95c35e1ce62
elupus <elupus@svn>
2009-02-18 15:09:04 +0000

    fixed: make ffmpeg headers we use match the library we use
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@17814 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/ffmpeg/avcodec.h
+++ xbmc/cores/ffmpeg/avcodec.h
@@ -2404,14 +2404,27 @@ enum AVSubtitleType {
 };
 
 typedef struct AVSubtitleRect {
-    uint16_t x;
-    uint16_t y;
-    uint16_t w;
-    uint16_t h;
-    uint16_t nb_colors;
-    int linesize;
-    uint32_t *rgba_palette;
-    uint8_t *bitmap;
+    int x;         ///< top left corner  of pict, undefined when pict is not set
+    int y;         ///< top left corner  of pict, undefined when pict is not set
+    int w;         ///< width            of pict, undefined when pict is not set
+    int h;         ///< height           of pict, undefined when pict is not set
+    int nb_colors; ///< number of colors in pict, undefined when pict is not set
+
+    /**
+     * data+linesize for the bitmap of this subtitle.
+     * can be set for text/ass as well once they where rendered
+     */
+    AVPicture pict;
+    enum AVSubtitleType type;
+
+    char *text;                     ///< 0 terminated plain UTF-8 text
+
+    /**
+     * 0 terminated ASS/SSA compatible event line.
+     * The pressentation of this is unaffected by the other values in this
+     * struct.
+     */
+    char *ass;
 } AVSubtitleRect;
 
 typedef struct AVSubtitle {
@@ -2419,7 +2432,7 @@ typedef struct AVSubtitle {
     uint32_t start_display_time; /* relative to packet pts, in ms */
     uint32_t end_display_time; /* relative to packet pts, in ms */
     unsigned num_rects;
-    AVSubtitleRect *rects;
+    AVSubtitleRect **rects;
 } AVSubtitle;
 
 
