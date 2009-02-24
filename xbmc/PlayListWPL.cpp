67e18352aa419ca3b23291daa5af61c97b9d3bb3
jmarshallnz <jmarshallnz@svn>
2009-02-24 07:33:09 +0000

    fixed: Ticket #5908 - CDs with special characters in the title/artist name don't rip. changed: Made MakeLegalFileName assume a filename, and introduced MakeLegalPath() that assumes a path, but legalizes the filename.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18052 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/PlayListWPL.cpp
+++ xbmc/PlayListWPL.cpp
@@ -108,7 +108,7 @@ bool CPlayListWPL::LoadData(istream& stream)
 void CPlayListWPL::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
-  CStdString strPlaylist = CUtil::MakeLegalFileName(strFileName);
+  CStdString strPlaylist = CUtil::MakeLegalPath(strFileName);
   CFile file;
   if (!file.OpenForWrite(strPlaylist, true, true))
   {
