42bd662fc0971be9283599247ac7cf5de8533f80
topfs2 <topfs2@svn>
2009-03-01 10:30:24 +0000

    Reverted r17132. Did not work properly on win32 and behaved weird with streams or failed playback
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18176 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/paplayer_linux.cpp
+++ xbmc/cores/paplayer/paplayer_linux.cpp
@@ -63,6 +63,7 @@ PAPlayer::PAPlayer(IPlayerCallback& callback) : IPlayer(callback)
   m_iSpeed = 1;
   m_SeekTime=-1;
   m_IsFFwdRewding = false;
+  m_timeOffset = 0; 
 
   m_pAudioDecoder[0] = NULL;
   m_pAudioDecoder[1] = NULL;
@@ -146,12 +147,11 @@ bool PAPlayer::OpenFile(const CFileItem& file, const CPlayerOptions &options)
   m_iSpeed = 1;
   m_bPaused = false;
   m_bStopPlaying = false;
-  ResetTime();
-  m_clock.SetSpeed(0);
+  m_bytesSentOut = 0;
 
   CLog::Log(LOGINFO, "PAPlayer: Playing %s", file.m_strPath.c_str());
 
-  m_clock.SetClock((__int64)(options.starttime * 1000));
+  m_timeOffset = (__int64)(options.starttime * 1000); 
 
   m_decoder[m_currentDecoder].GetDataFormat(&m_Channels, &m_SampleRate, &m_BitsPerSample);
 
@@ -175,7 +175,6 @@ bool PAPlayer::OpenFile(const CFileItem& file, const CPlayerOptions &options)
   m_bQueueFailed = false;
 
   m_decoder[m_currentDecoder].Start();  // start playback
-  m_clock.SetSpeed(m_iSpeed);
 
   return true;
 }
@@ -399,7 +398,6 @@ void PAPlayer::Pause()
 
   if (m_bPaused)
   {
-    m_clock.SetSpeed(0);
     if (m_pAudioDecoder[m_currentStream])
       m_pAudioDecoder[m_currentStream]->Pause();
 
@@ -410,7 +408,6 @@ void PAPlayer::Pause()
   }
   else
   {
-    m_clock.SetSpeed(m_iSpeed);
     if (m_pAudioDecoder[m_currentStream])
       m_pAudioDecoder[m_currentStream]->Resume();
 
@@ -470,7 +467,6 @@ void PAPlayer::Process()
 void PAPlayer::ToFFRW(int iSpeed)
 {
   m_iSpeed = iSpeed;
-  m_clock.SetSpeed(iSpeed);
 }
 
 void PAPlayer::UpdateCacheLevel()
@@ -556,8 +552,7 @@ bool PAPlayer::ProcessPAP()
           m_pAudioDecoder[m_currentStream]->Resume();
 
           m_callback.OnPlayBackStarted();
-          ResetTime();
-          m_clock.SetClock(m_nextFile->m_lStartOffset * 1000 / 75);
+          m_timeOffset = m_nextFile->m_lStartOffset * 1000 / 75; 
           *m_currentFile = *m_nextFile;
           m_nextFile->Reset();
           m_cachingNextFile = false;
@@ -613,8 +608,8 @@ bool PAPlayer::ProcessPAP()
             m_decoder[m_currentDecoder].Destroy();
             m_decoder[1 - m_currentDecoder].Start();
             m_callback.OnPlayBackStarted();
-            ResetTime();
-            m_clock.SetClock(m_nextFile->m_lStartOffset * 1000 / 75);
+            m_timeOffset = m_nextFile->m_lStartOffset * 1000 / 75;
+            m_bytesSentOut = 0;
             *m_currentFile = *m_nextFile;
             m_nextFile->Reset();
             m_cachingNextFile = false;
@@ -658,8 +653,8 @@ bool PAPlayer::ProcessPAP()
         // set the next track playing (.cue sheet)
         m_decoder[m_currentDecoder].SetStatus(STATUS_PLAYING);
         m_callback.OnPlayBackStarted();
-        ResetTime();
-        m_clock.SetClock(m_nextFile->m_lStartOffset * 1000 / 75);
+        m_timeOffset = m_nextFile->m_lStartOffset * 1000 / 75;
+        m_bytesSentOut = 0;
         *m_currentFile = *m_nextFile;
         m_nextFile->Reset();
         m_cachingNextFile = false;
@@ -734,12 +729,14 @@ bool PAPlayer::ProcessPAP()
 void PAPlayer::ResetTime()
 {
   m_bytesSentOut = 0;
-  m_clock.ResetClock();
 }
 
 __int64 PAPlayer::GetTime()
 {
-  return m_clock.GetTimeMS() - m_currentFile->m_lStartOffset * 1000 / 75;
+  __int64  timeplus = m_BytesPerSecond ? (__int64)(((float) m_bytesSentOut / (float) m_BytesPerSecond ) * 1000.0) : 0; 
+  if (m_pAudioDecoder[m_currentStream]) 
+    timeplus -= (__int64)(m_pAudioDecoder[m_currentStream]->GetDelay() * 1000.0f); 
+  return m_timeOffset + timeplus - m_currentFile->m_lStartOffset * 1000 / 75; 
 }
 
 __int64 PAPlayer::GetTotalTime64()
@@ -863,9 +860,8 @@ void PAPlayer::HandleSeeking()
   if (m_SeekTime != -1)
   {
     DWORD time = timeGetTime();
-    m_clock.SetClock(m_decoder[m_currentDecoder].Seek(m_SeekTime));
-    CLog::Log(LOGDEBUG, "Seek to time %f took %u ms",
-              0.001f * m_SeekTime, timeGetTime() - time);
+    m_timeOffset = m_decoder[m_currentDecoder].Seek(m_SeekTime);
+    CLog::Log(LOGDEBUG, "Seek to time %f took %i ms", 0.001f * m_SeekTime, timeGetTime() - time);
     FlushStreams();
     m_SeekTime = -1;
   }
@@ -894,7 +890,6 @@ bool PAPlayer::HandleFFwdRewd()
   { // stop ffwd/rewd
     m_IsFFwdRewding = false;
     SetVolume(g_stSettings.m_nVolumeLevel);
-    m_clock.SetClock(m_decoder[m_currentDecoder].Seek(GetTime()));
     FlushStreams();
     return true;
   }
@@ -902,24 +897,26 @@ bool PAPlayer::HandleFFwdRewd()
   int snippet = m_BytesPerSecond / 2;
   if ( m_bytesSentOut >= snippet )
   {
-    // Calculate time to seek to if we do FF/RW
+    // Calculate offset to seek if we do FF/RW
     __int64 time = GetTime();
+    if (m_IsFFwdRewding) snippet = (int)m_bytesSentOut;
+    time += (__int64)((double)snippet * (m_iSpeed - 1.0) / m_BytesPerSecond * 1000.0);
 
     // Is our offset inside the track range?
     if (time >= 0 && time <= m_decoder[m_currentDecoder].TotalTime())
     { // just set next position to read
       m_IsFFwdRewding = true;
       time += m_currentFile->m_lStartOffset * 1000 / 75;
-      m_clock.SetClock(m_decoder[m_currentDecoder].Seek(time));
+      m_timeOffset = m_decoder[m_currentDecoder].Seek(time);
       FlushStreams();
       SetVolume(g_stSettings.m_nVolumeLevel - VOLUME_FFWD_MUTE); // override xbmc mute
     }
     else if (time < 0)
     { // ...disable seeking and start the track again
-      ToFFRW(1);
       time = m_currentFile->m_lStartOffset * 1000 / 75;
-      m_clock.SetClock(m_decoder[m_currentDecoder].Seek(time));
+      m_timeOffset = m_decoder[m_currentDecoder].Seek(time);
       FlushStreams();
+      m_iSpeed = 1;
       SetVolume(g_stSettings.m_nVolumeLevel); // override xbmc mute
     } // is our next position greater then the end sector...
     else //if (time > m_codec->m_TotalTime)
