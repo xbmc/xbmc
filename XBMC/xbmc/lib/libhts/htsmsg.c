c2b73e0d968b6272e70cf4ed166c5aa960ef752d
elupus <elupus@svn>
2009-03-02 01:27:15 +0000

    fixed: warning about _GNU_SOURCE being redefined
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@18196 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libhts/htsmsg.c
+++ XBMC/xbmc/lib/libhts/htsmsg.c
@@ -16,8 +16,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define _GNU_SOURCE /* strdup */
-
 #include <assert.h>
 #include <sys/types.h>
 #include <stdio.h>
