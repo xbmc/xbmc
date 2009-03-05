283bb242abaf270311bc733cf3bdd05d9ed69040
elupus <elupus@svn>
2009-03-05 17:01:33 +0000

    changed: upgraded libhts (tvheadend message library) to latest version
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18281 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libhts/htsmsg.h
+++ xbmc/lib/libhts/htsmsg.h
@@ -28,18 +28,28 @@
 TAILQ_HEAD(htsmsg_field_queue, htsmsg_field);
 
 typedef struct htsmsg {
+  /**
+   * fields 
+   */
   struct htsmsg_field_queue hm_fields;
-  int hm_array;
-  const void *hm_data;                /* Data to be free'd when message
-					 is free'd */
+
+  /**
+   * Set if this message is a list, otherwise it is a map.
+   */
+  int hm_islist;
+
+  /**
+   * Data to be free'd when the message is destroyed
+   */
+  const void *hm_data;
 } htsmsg_t;
 
 
-#define HMF_MSG 1
+#define HMF_MAP  1
 #define HMF_S64 2
 #define HMF_STR 3
 #define HMF_BIN 4
-#define HMF_VEC 5
+#define HMF_LIST 5
 
 typedef struct htsmsg_field {
   TAILQ_ENTRY(htsmsg_field) hmf_link;
@@ -67,54 +77,184 @@ typedef struct htsmsg_field {
 #define hmf_bin     u.bin.data
 #define hmf_binsize u.bin.len
 
-#define htsmsg_get_msg_by_field(f) \
- ((f)->hmf_type == HMF_MSG ? &(f)->hmf_msg : NULL)
+#define htsmsg_get_map_by_field(f) \
+ ((f)->hmf_type == HMF_MAP ? &(f)->hmf_msg : NULL)
 
 #define HTSMSG_FOREACH(f, msg) TAILQ_FOREACH(f, &(msg)->hm_fields, hmf_link)
 
-htsmsg_t *htsmsg_create(void);
+/**
+ * Create a new map
+ */
+htsmsg_t *htsmsg_create_map(void);
 
-htsmsg_t *htsmsg_create_array(void);
+/**
+ * Create a new list
+ */
+htsmsg_t *htsmsg_create_list(void);
 
+/**
+ * Destroys a message (map or list)
+ */
 void htsmsg_destroy(htsmsg_t *msg);
 
+/**
+ * Add an integer field where source is unsigned 32 bit.
+ */
 void htsmsg_add_u32(htsmsg_t *msg, const char *name, uint32_t u32);
+
+/**
+ * Add an integer field where source is signed 32 bit.
+ */
 void htsmsg_add_s32(htsmsg_t *msg, const char *name,  int32_t s32);
+
+/**
+ * Add an integer field where source is signed 64 bit.
+ */
 void htsmsg_add_s64(htsmsg_t *msg, const char *name,  int64_t s64);
+
+/**
+ * Add a string field.
+ */
 void htsmsg_add_str(htsmsg_t *msg, const char *name, const char *str);
+
+/**
+ * Add an field where source is a list or map message.
+ */
 void htsmsg_add_msg(htsmsg_t *msg, const char *name, htsmsg_t *sub);
+
+/**
+ * Add an field where source is a list or map message.
+ *
+ * This function will not strdup() \p name but relies on the caller
+ * to keep the string allocated for as long as the message is valid.
+ */
 void htsmsg_add_msg_extname(htsmsg_t *msg, const char *name, htsmsg_t *sub);
 
+/**
+ * Add an binary field. The data is copied to a malloced storage
+ */
 void htsmsg_add_bin(htsmsg_t *msg, const char *name, const void *bin,
 		    size_t len);
+
+/**
+ * Add an binary field. The data is not copied, instead the caller
+ * is responsible for keeping the data valid for as long as the message
+ * is around.
+ */
 void htsmsg_add_binptr(htsmsg_t *msg, const char *name, const void *bin,
 		       size_t len);
 
+/**
+ * Get an integer as an unsigned 32 bit integer.
+ *
+ * @return HTSMSG_ERR_FIELD_NOT_FOUND - Field does not exist
+ *         HTSMSG_ERR_CONVERSION_IMPOSSIBLE - Field is not an integer or
+ *              out of range for the requested storage.
+ */
 int htsmsg_get_u32(htsmsg_t *msg, const char *name, uint32_t *u32p);
+
+/**
+ * Get an integer as an signed 32 bit integer.
+ *
+ * @return HTSMSG_ERR_FIELD_NOT_FOUND - Field does not exist
+ *         HTSMSG_ERR_CONVERSION_IMPOSSIBLE - Field is not an integer or
+ *              out of range for the requested storage.
+ */
 int htsmsg_get_s32(htsmsg_t *msg, const char *name,  int32_t *s32p);
+
+/**
+ * Get an integer as an signed 64 bit integer.
+ *
+ * @return HTSMSG_ERR_FIELD_NOT_FOUND - Field does not exist
+ *         HTSMSG_ERR_CONVERSION_IMPOSSIBLE - Field is not an integer or
+ *              out of range for the requested storage.
+ */
 int htsmsg_get_s64(htsmsg_t *msg, const char *name,  int64_t *s64p);
+
+/**
+ * Get pointer to a binary field. No copying of data is performed.
+ *
+ * @param binp Pointer to a void * that will be filled in with a pointer
+ *             to the data
+ * @param lenp Pointer to a size_t that will be filled with the size of
+ *             the data
+ *
+ * @return HTSMSG_ERR_FIELD_NOT_FOUND - Field does not exist
+ *         HTSMSG_ERR_CONVERSION_IMPOSSIBLE - Field is not a binary blob.
+ */
 int htsmsg_get_bin(htsmsg_t *msg, const char *name, const void **binp,
 		   size_t *lenp);
-htsmsg_t *htsmsg_get_array(htsmsg_t *msg, const char *name);
 
+/**
+ * Get a field of type 'list'. No copying is done.
+ *
+ * @return NULL if the field can not be found or not of list type.
+ *         Otherwise a htsmsg is returned.
+ */
+htsmsg_t *htsmsg_get_list(htsmsg_t *msg, const char *name);
+
+/**
+ * Get a field of type 'string'. No copying is done.
+ *
+ * @return NULL if the field can not be found or not of string type.
+ *         Otherwise a pointer to the data is returned.
+ */
 const char *htsmsg_get_str(htsmsg_t *msg, const char *name);
-htsmsg_t *htsmsg_get_msg(htsmsg_t *msg, const char *name);
 
-htsmsg_t *htsmsg_get_msg_multi(htsmsg_t *msg, ...);
+/**
+ * Get a field of type 'map'. No copying is done.
+ *
+ * @return NULL if the field can not be found or not of map type.
+ *         Otherwise a htsmsg is returned.
+ */
+htsmsg_t *htsmsg_get_map(htsmsg_t *msg, const char *name);
 
+/**
+ * Traverse a hierarchy of htsmsg's to find a specific child.
+ */
+htsmsg_t *htsmsg_get_map_multi(htsmsg_t *msg, ...);
+
+/**
+ * Given the field \p f, return a string if it is of type string, otherwise
+ * return NULL
+ */
 const char *htsmsg_field_get_string(htsmsg_field_t *f);
 
+/**
+ * Return the field \p name as an u32.
+ *
+ * @return An unsigned 32 bit integer or NULL if the field can not be found
+ *         or if conversion is not possible.
+ */
 int htsmsg_get_u32_or_default(htsmsg_t *msg, const char *name, uint32_t def);
 
+/**
+ * Remove the given field called \p name from the message \p msg.
+ */
 int htsmsg_delete_field(htsmsg_t *msg, const char *name);
 
+/**
+ * Detach will remove the given field (and only if it is a list or map)
+ * from the message and make it a 'standalone message'. This means
+ * the the contents of the sub message will stay valid if the parent is
+ * destroyed. The caller is responsible for freeing this new message.
+ */
 htsmsg_t *htsmsg_detach_submsg(htsmsg_field_t *f);
 
+/**
+ * Print a message to stdout. 
+ */
 void htsmsg_print(htsmsg_t *msg);
 
+/**
+ * Create a new field. Primarily intended for htsmsg internal functions.
+ */
 htsmsg_field_t *htsmsg_field_add(htsmsg_t *msg, const char *name,
 				 int type, int flags);
 
+/**
+ * Clone a message.
+ */
 htsmsg_t *htsmsg_copy(htsmsg_t *src);
 
 #define HTSMSG_FOREACH(f, msg) TAILQ_FOREACH(f, &(msg)->hm_fields, hmf_link)
