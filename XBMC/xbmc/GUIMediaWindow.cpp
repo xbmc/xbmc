ed9375f627f839822924d4d0b4fa451f277a6183
spiff_ <spiff_@svn>
2009-03-08 23:31:11 +0000

    added: ticket #6024 - Clear window properties at plugin root or virtual root for media windows. thanks to nuka1195
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@18341 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/GUIMediaWindow.cpp
+++ XBMC/xbmc/GUIMediaWindow.cpp
@@ -572,6 +572,10 @@ bool CGUIMediaWindow::GetDirectory(const CStdString &strDirectory, CFileItemList
     }
   }
 
+  // clear window properties at root or plugin root
+  if (items.IsVirtualDirectoryRoot() || items.IsPluginRoot())
+    ClearProperties();
+
   return true;
 }
 
