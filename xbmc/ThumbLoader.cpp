d823063e29602654182ec393cb3f58e845e5637a
spiff_ <spiff_@svn>
2009-03-09 21:27:06 +0000

    fixed: ticket #4611 - Auto-generated thumbs do not display in Library
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18367 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ThumbLoader.cpp
+++ xbmc/ThumbLoader.cpp
@@ -73,6 +73,18 @@ bool CVideoThumbLoader::ExtractThumb(const CStdString &strPath, const CStdString
 bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
 {
   if (pItem->m_bIsShareOrDrive) return true;
+  if (pItem->IsVideoDb() && pItem->HasVideoInfoTag())
+  {
+    CFileItem item(*pItem->GetVideoInfoTag());
+    bool bResult = LoadItem(&item);
+    if (bResult)
+    {
+      pItem->SetProperty("HasAutoThumb",item.GetProperty("HasAutoThumb"));
+      pItem->SetProperty("AutoThumbImage",item.GetProperty("AutoThumbImage"));
+      pItem->SetThumbnailImage(item.GetThumbnailImage());
+    }
+    return bResult;
+  }
   CStdString cachedThumb(pItem->GetCachedVideoThumb());
 
   if (!pItem->HasThumbnail())
