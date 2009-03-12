c9db6abc64440161c3d6c6f5e02a41c88d7545a0
arnova <arnova@svn>
2009-03-12 12:30:00 +0000

    changed: Curl's retry logic now increases the lowspeed-time whenever it's caused by a timeout. Also changed some cosmetic stuff.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@18449 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Settings.h
+++ XBMC/xbmc/Settings.h
@@ -256,7 +256,7 @@ public:
     int m_iTuxBoxZapWaitTime;
     bool m_bTuxBoxSendAllAPids;
 
-    int m_curlclienttimeout;
+    int m_curlconnecttimeout;
     int m_curllowspeedtime;
 
 #ifdef HAS_SDL
