e50b86c7637a46673ff816a6d697c20178b625de
davilla <davilla@svn>
2009-03-13 20:05:27 +0000

    updated prep for powerpc-osx
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18486 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libSDL-OSX/buildSDL-osx.sh
+++ xbmc/lib/libSDL-OSX/buildSDL-osx.sh
@@ -1,5 +1,14 @@
 #!/bin/bash 
 
+# Get ARCH from Makefile.include
+file=../../../Makefile.include
+if [ -f $file ]
+then
+  ARCH=$(grep "^ARCH=" $file | awk -F"=" '{print $2}')
+else
+  echo "$file not found... You must run configure first!"
+  exit 1
+fi
 
 #-------------------------------------------------------------------------------
 # build libSDL/libSDLmain
@@ -9,10 +18,10 @@ wget http://www.libsdl.org/release/SDL-1.2.13.tar.gz
 tar -xzf SDL-1.2.13.tar.gz
 cd SDL-1.2.13
 patch -p0 <../SDL_SetWidthHeight.diff
-./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --disable-nasm 
+./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --disable-nasm  --disable-altivec
 make
-cp build/.libs/libSDL.a ../libSDL-osx.a
-cp build/libSDLmain.a ../libSDLmain-osx.a
+cp build/.libs/libSDL.a ../libSDL-${ARCH}.a
+cp build/libSDLmain.a ../libSDLmain-${ARCH}.a
 cd ..
 rm -rf SDL-1.2.13 SDL-1.2.13.tar.gz
 
@@ -26,7 +35,7 @@ tar -xzf SDL_image-1.2.6.tar.gz
 cd SDL_image-1.2.6
 ./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --enable-static --disable-shared --disable-sdltest 
 make
-cp .libs/libSDL_image.a ../libSDL_image-osx.a
+cp .libs/libSDL_image.a ../libSDL_image-${ARCH}.a
 cd ..
 rm -rf SDL_image-1.2.6 SDL_image-1.2.6.tar.gz
 
@@ -40,7 +49,7 @@ tar -xzf SDL_mixer-1.2.8.tar.gz
 cd SDL_mixer-1.2.8
 ./configure MACOSX_DEPLOYMENT_TARGET=10.4 CFLAGS="-O2 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" --enable-static --disable-shared --disable-music-ogg --disable-music-mp3 --disable-music-mod --disable-music-midi --disable-sdltest
 make
-cp build/.libs/libSDL_mixer.a ../libSDL_mixer-osx.a
+cp build/.libs/libSDL_mixer.a ../libSDL_mixer-${ARCH}.a
 cd ..
 rm -rf SDL_mixer-1.2.8 SDL_mixer-1.2.8.tar.gz
 
