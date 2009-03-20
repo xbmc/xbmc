1f6c5e1116e194c6e4cdf0cc82c7427dc5f9ffac
elupus <elupus@svn>
2009-03-20 16:06:39 +0000

    fixed: forgot to remove function prototypes
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/xbmc-vdpau@18718 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/Surface.h
+++ XBMC/guilib/Surface.h
@@ -91,8 +91,6 @@ public:
   void EnableVSync(bool enable=true);
   bool ResizeSurface(int newWidth, int newHeight);
   void RefreshCurrentContext();
-  void BindPixmap();
-  void ReleasePixmap(); 
   DWORD GetNextSwap();
   void NotifyAppFocusChange(bool bGaining);
 #ifdef _WIN32
