87da9cece39eeea7b736b91b11cd81a646bac5f5
davilla <davilla@svn>
2009-03-20 03:38:26 +0000

    move to objc++
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18707 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/osx/CocoaInterface.h
@@ -0,0 +1,62 @@
+//
+//  Utils.h
+//  XBMC
+//
+//  Created by Elan Feingold on 1/5/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _OSX_UTILS_H_
+#define _OSX_UTILS_H_
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+  // Pools.
+  //
+  void* InitializeAutoReleasePool();
+  void DestroyAutoReleasePool(void* pool);
+  
+  // Graphics.
+  //
+  int Cocoa_GetScreenIndex(void);
+  void Cocoa_GetScreenResolution(int* w, int* h);
+  double Cocoa_GetScreenRefreshRate(int screen_id);
+  void Cocoa_GetScreenResolutionOfAnotherScreen(int display, int* w, int* h);
+  int  Cocoa_GetNumDisplays();
+  int  Cocoa_GetDisplayID(int screen);
+  
+  // Open GL.
+  //
+  void  Cocoa_GL_MakeCurrentContext(void* theContext);
+  void  Cocoa_GL_ReleaseContext(void* context);
+  void  Cocoa_GL_SwapBuffers(void* theContext);
+  void* Cocoa_GL_GetWindowPixelFormat();
+  void* Cocoa_GL_GetFullScreenPixelFormat(int screen);
+  void* Cocoa_GL_GetCurrentContext();
+  void* Cocoa_GL_CreateContext(void* pixFmt, void* shareCtx);
+  void* Cocoa_GL_ResizeWindow(void *theContext, int w, int h);
+  void  Cocoa_GL_SetFullScreen(int width, int height, bool fs, bool blankOtherDisplay, bool GL_FullScreen);
+  void  Cocoa_GL_EnableVSync(bool enable);
+
+  // SDL Hack
+  //
+  void* Cocoa_GL_ReplaceSDLWindowContext();
+  
+  // Power and Screen
+  //
+  int  Cocoa_IdleDisplays();
+  void Cocoa_UpdateSystemActivity();
+  void Cocoa_DisableOSXScreenSaver();
+  
+  // AppleScript
+  //
+  void Cocoa_DoAppleScript(const char* scriptSource);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
