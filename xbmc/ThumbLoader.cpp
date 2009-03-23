379052e52619a20488e6ba444a66ea9a0ca9ccc2
jmarshallnz <jmarshallnz@svn>
2009-03-23 06:31:55 +0000

    fixed: Ticket #5305 - Program Plugin - External Thumbnails do not work.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18845 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/ThumbLoader.cpp
+++ xbmc/ThumbLoader.cpp
@@ -36,6 +36,35 @@
 using namespace XFILE;
 using namespace DIRECTORY;
 
+CThumbLoader::CThumbLoader()
+{
+}
+
+CThumbLoader::~CThumbLoader()
+{
+}
+
+bool CThumbLoader::LoadRemoteThumb(CFileItem *pItem)
+{
+  // look for remote thumbs
+  CStdString thumb(pItem->GetThumbnailImage());
+  if (!g_TextureManager.CanLoad(thumb))
+  {
+    CStdString cachedThumb(pItem->GetCachedVideoThumb());
+    if (CFile::Exists(cachedThumb))
+      pItem->SetThumbnailImage(cachedThumb);
+    else
+    {
+      CPicture pic;
+      if(pic.DoCreateThumbnail(thumb, cachedThumb))
+        pItem->SetThumbnailImage(cachedThumb);
+      else
+        pItem->SetThumbnailImage("");
+    }
+  }
+  return pItem->HasThumbnail();
+}
+
 CVideoThumbLoader::CVideoThumbLoader()
 {
 }
@@ -121,23 +150,7 @@ bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
     }
   }
   else
-  {
-    // look for remote thumbs
-    CStdString thumb(pItem->GetThumbnailImage());
-    if (!g_TextureManager.CanLoad(thumb))
-    {
-      if(CFile::Exists(cachedThumb))
-          pItem->SetThumbnailImage(cachedThumb);
-      else
-      {
-        CPicture pic;
-        if(pic.DoCreateThumbnail(thumb, cachedThumb))
-          pItem->SetThumbnailImage(cachedThumb);
-        else
-          pItem->SetThumbnailImage("");
-      }
-    }
-  }
+    LoadRemoteThumb(pItem);
 
   if (!pItem->HasProperty("fanart_image"))
   {
@@ -165,6 +178,8 @@ bool CProgramThumbLoader::LoadItem(CFileItem *pItem)
   if (pItem->m_bIsShareOrDrive) return true;
   if (!pItem->HasThumbnail())
     pItem->SetUserProgramThumb();
+  else
+    LoadRemoteThumb(pItem);
   return true;
 }
 
@@ -182,24 +197,7 @@ bool CMusicThumbLoader::LoadItem(CFileItem* pItem)
   if (!pItem->HasThumbnail())
     pItem->SetUserMusicThumb();
   else
-  {
-    // look for remote thumbs
-    CStdString thumb(pItem->GetThumbnailImage());
-    if (!g_TextureManager.CanLoad(thumb))
-    {
-      CStdString cachedThumb(pItem->GetCachedVideoThumb());
-      if(CFile::Exists(cachedThumb))
-        pItem->SetThumbnailImage(cachedThumb);
-      else
-      {
-        CPicture pic;
-        if(pic.DoCreateThumbnail(thumb, cachedThumb))
-          pItem->SetThumbnailImage(cachedThumb);
-        else
-          pItem->SetThumbnailImage("");
-      }
-    }
-  }
+    LoadRemoteThumb(pItem);
   return true;
 }
 
