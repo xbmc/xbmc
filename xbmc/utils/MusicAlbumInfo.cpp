afa94e071739ad1811ad03efee125a5a467598cb
jmarshallnz <jmarshallnz@svn>
2009-03-24 05:18:11 +0000

    fixed: Ticket #5082 - TinyXML's SetCondenseWhitespace was used inconsistently across XBMC (and wasn't threadsafe).  It is now defaulted to false, and we don't touch it.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@18873 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/MusicAlbumInfo.cpp
+++ xbmc/utils/MusicAlbumInfo.cpp
@@ -163,7 +163,6 @@ bool CMusicAlbumInfo::Load(XFILE::CFileCurl& http, const SScraperInfo& info, con
     g_charsetConverter.unknownToUTF8(strXML);
 
     // ok, now parse the xml file
-  TiXmlBase::SetCondenseWhiteSpace(false);
   TiXmlDocument doc;
   doc.Parse(strXML.c_str(),0,TIXML_ENCODING_UTF8);
   if (!doc.RootElement())
@@ -185,7 +184,6 @@ bool CMusicAlbumInfo::Load(XFILE::CFileCurl& http, const SScraperInfo& info, con
     }
     xurl = xurl->NextSiblingElement("url");
   }
-  TiXmlBase::SetCondenseWhiteSpace(true);
 
   return ret;
 }
