25992b1884f8ab79cd072537a0470c4fea05d377
ceros7 <ceros7@svn>
2009-03-27 07:33:47 +0000

    modified: allow xbmc to use external libwavpack
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/external-libraries-support@18976 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/WAVPackcodec.h
+++ XBMC/xbmc/cores/paplayer/WAVPackcodec.h
@@ -49,7 +49,11 @@ private:
 
   char m_errormsg[512];
   WavpackContext* m_Handle;
+  #if (defined USE_EXTERNAL_LIBRARIES) || (defined USE_EXTERNAL_LIBWAVPACK)
+  WavpackStreamReader m_Callbacks;
+  #else
   stream_reader m_Callbacks;
+  #endif
 
   BYTE*     m_Buffer;
   int       m_BufferSize;
