9010e8f67f774db585c3b0871720b1ee274ea197
davilla <davilla@svn>
2009-03-29 07:01:29 +0000

    commit libdvdnav-4.1.3 into linuxport
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@19046 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/xbmc/cores/dvdplayer/Codecs/libdvd/libdvdnav/doc/mainpage.cpp
@@ -0,0 +1,51 @@
+/*!
+
+\mainpage The libdvdnav Reference Documentation
+
+\section intro Introduction
+
+This reference is not maintained. See the README file for pointers on
+current documentation.
+
+This reference documentation is intended to both provide a comprehensive reference to
+the <tt>libdvdnav</tt> DVD navigation library and a brief introduction to the library
+for new users.
+
+This documentation is automatically generated from the <tt>libdvdnav</tt> code so
+should remain somewhat relevant but as always be wary that <tt>libdvdnav</tt>
+is currently in heavy development and the API is not yet frozen.
+
+\subsection feedback Feedback
+
+All comments, error reports, additional information and criticism of all sorts
+should be directed to <tt>dvd-devel@lists.sf.net</tt>.
+
+\subsection disclaimer Disclaimer
+
+No liability for the contents of this documents can be accepted. Use the concepts, 
+examples and other content at your own risk. Additionally, this is an early version, 
+with many possibilities for inaccuracies and errors.
+
+As far as I know, all of the programmes mentioned here and example code quoted are 
+released under the GNU General Public License or the Lesser GNU Public License. 
+
+\subsection copyright Copyright information
+
+This document is copyrighted &copy;2002 by Richard Wareham and the <tt>libdvdnav</tt>
+project. It is distributed under the following terms:
+
+  - This document may be reproduced and distributed in whole or in part, in any medium
+    physical or electronic, as long as this copyright notice is retained on all copies.
+    Commercial redistribution is allowed and encouraged; however, the author would like 
+    to be notified of any such distributions.
+  - All translations, derivative works, or aggregate works incorporating any parts of 
+    this document must be covered under this copyright notice. That is, you may not produce
+    a derivative work from this manual and impose additional restrictions on its distribution.
+
+\section sections Section
+
+  - A tutorial is in section \ref tutorial .
+  - A complete reference may be found in the <A HREF="modules.html">modules</a> section.
+
+*/
+
