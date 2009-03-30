1ffcb11e4e174e192eb2b8e6a9077d07dbbc560a
motd2k <motd2k@svn>
2009-03-30 16:33:08 +0000

    Removed some VDPAU compiler warnings.  Made getGLError default off.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19085 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/log.cpp
+++ xbmc/utils/log.cpp
@@ -237,7 +237,8 @@ void _VerifyGLState(const char* szfile, const char* szfunction, int lineno){
                   matrix[ixx*4+3]);                                     \
       }                                                                 \
   }
-
+  if (g_advancedSettings.m_logLevel < LOG_LEVEL_DEBUG_FREEMEM)
+    return;
   GLenum err = glGetError();
   if (err==GL_NO_ERROR)
     return;
