e62acab0fd82402ccb02b2dbcf0156fbd7fddee2
tslayer <tslayer@svn>
2009-04-03 20:38:47 +0000

    Fixed: Couldn't rip CDs with special characters to SMB directory.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19208 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cdrip/CDDARipper.cpp
+++ xbmc/cdrip/CDDARipper.cpp
@@ -259,7 +259,7 @@ bool CCDDARipper::RipTrack(CFileItem* pItem)
   // if album name is set, then we use this as the directory to place the new file in.
   if (pItem->GetMusicInfoTag()->GetAlbum().size() > 0)
   {
-    strDirectory += CUtil::MakeLegalFileName(pItem->GetMusicInfoTag()->GetAlbum().c_str());
+    strDirectory += CUtil::MakeLegalFileName(pItem->GetMusicInfoTag()->GetAlbum().c_str(), ripPath.IsSmb() ? true : false);
     CUtil::AddDirectorySeperator(strDirectory);
   }
 
@@ -324,7 +324,7 @@ bool CCDDARipper::RipCD()
   CStdString strAlbumDir;
   if (vecItems[0]->GetMusicInfoTag()->GetAlbum().size() > 0)
   {
-    strAlbumDir=CUtil::MakeLegalFileName(vecItems[0]->GetMusicInfoTag()->GetAlbum().c_str());
+    strAlbumDir=CUtil::MakeLegalFileName(vecItems[0]->GetMusicInfoTag()->GetAlbum().c_str(), ripPath.IsSmb() ? true : false );
   }
 
     // No legal fatx directory name or no album in tag
