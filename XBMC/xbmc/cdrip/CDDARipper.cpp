xbmc/xbmc

commit 37bd0a5d43c94338adaa85456293d72fdd83c347

fixed: Ticket #5908 - CDs with special characters in the title/artist name don't rip. changed: Made MakeLegalFileName assume a filename, and introduced MakeLegalPath() that assumes a path, but legalizes the filename.
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@18052 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

XBMC/xbmc/cdrip/CDDARipper.cpp
@@ -412,6 +412,6 @@ CStdString CCDDARipper::GetTrackName(CFileItem *item, bool isFatX)
   track += GetExtension(g_guiSettings.GetInt("cddaripper.encoder"));
 
   // make sure the filename is legal
-  track = CUtil::MakeLegalFileName(track.c_str());
+  track = CUtil::MakeLegalFileName(track);
   return track;
 }

XBMC/xbmc/FileSystem/RarManager.cpp
@@ -213,7 +213,7 @@ bool CRarManager::CacheRarredFile(CStdString& strPathInCache, const CStdString& 
     pFile->m_iUsed = 1;
   }
   CUtil::AddFileToFolder(strDir,CUtil::GetFileName(strPathInRar),pFile->m_strCachedPath); // GetFileName
-  pFile->m_strCachedPath = CUtil::MakeLegalFileName(pFile->m_strCachedPath);
+  pFile->m_strCachedPath = CUtil::MakeLegalPath(pFile->m_strCachedPath);
   pFile->m_bAutoDel = (bOptions & EXFILE_AUTODELETE) != 0;
   pFile->m_iOffset = iOffset;
   strPathInCache = pFile->m_strCachedPath;

XBMC/xbmc/GUIWindowVideoBase.cpp
@@ -1609,7 +1609,7 @@ void CGUIWindowVideoBase::AddToDatabase(int iItem)
     CStackDirectory stack;
     strXml = stack.GetFirstStackedFile(pItem->m_strPath) + ".xml";
   }
-  CStdString strCache = CUtil::MakeLegalFileName("special://temp/" + CUtil::GetFileName(strXml));
+  CStdString strCache = CUtil::AddFileToFolder("special://temp/", CUtil::MakeLegalFileName(CUtil::GetFileName(strXml)));
   if (CFile::Exists(strXml))
   {
     bGotXml = true;

XBMC/xbmc/PlayListB4S.cpp
@@ -115,7 +115,7 @@ void CPlayListB4S::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
   CStdString strPlaylist = strFileName;
-  strPlaylist = CUtil::MakeLegalFileName(strPlaylist);
+  strPlaylist = CUtil::MakeLegalPath(strPlaylist);
   CFile file;
   if (!file.OpenForWrite(strPlaylist, true, true))
   {

XBMC/xbmc/PlayListM3U.cpp
@@ -138,7 +138,7 @@ bool CPlayListM3U::Load(const CStdString& strFileName)
 void CPlayListM3U::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
-  CStdString strPlaylist = CUtil::MakeLegalFileName(strFileName);
+  CStdString strPlaylist = CUtil::MakeLegalPath(strFileName);
   CFile file;
   if (!file.OpenForWrite(strPlaylist,false,true))
   {

XBMC/xbmc/PlayListPLS.cpp
@@ -183,7 +183,7 @@ bool CPlayListPLS::Load(const CStdString &strFile)
 void CPlayListPLS::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
-  CStdString strPlaylist = CUtil::MakeLegalFileName(strFileName);
+  CStdString strPlaylist = CUtil::MakeLegalPath(strFileName);
   CFile file;
   if (!file.OpenForWrite(strPlaylist, true, true))
   {

XBMC/xbmc/PlayListWPL.cpp
@@ -108,7 +108,7 @@ bool CPlayListWPL::LoadData(istream& stream)
 void CPlayListWPL::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
-  CStdString strPlaylist = CUtil::MakeLegalFileName(strFileName);
+  CStdString strPlaylist = CUtil::MakeLegalPath(strFileName);
   CFile file;
   if (!file.OpenForWrite(strPlaylist, true, true))
   {

XBMC/xbmc/PlayListXML.cpp
@@ -173,7 +173,7 @@ bool CPlayListXML::Load( const CStdString& strFileName )
 void CPlayListXML::Save(const CStdString& strFileName) const
 {
   if (!m_vecItems.size()) return ;
-  CStdString strPlaylist = CUtil::MakeLegalFileName(strFileName);
+  CStdString strPlaylist = CUtil::MakeLegalPath(strFileName);
   CFile file;
   if (!file.OpenForWrite(strPlaylist, true, true))
   {

commit 134f11c6cf90cb60338f9d04213dc436e334c689

Fixed: CD Ripping was broken due to change with r18052.  Use MakeLegalPath when calling the encoder.
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@19200 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

XBMC/xbmc/cdrip/CDDARipper.cpp
@@ -86,7 +86,7 @@ bool CCDDARipper::Init(const CStdString& strTrackFile, const CStdString& strFile
   }
 
   // init encoder
-  CStdString strFile2=CUtil::MakeLegalFileName(strFile);
+  CStdString strFile2=CUtil::MakeLegalPath(strFile);
   if (!m_pEncoder->Init(strFile2.c_str(), 2, 44100, 16))
   {
     m_cdReader.DeInit();