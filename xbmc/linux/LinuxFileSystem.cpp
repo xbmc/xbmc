6d753716068a1acca93b705af3d558896079f31b
maestrodd <maestrodd@svn>
2009-04-04 21:51:37 +0000

    fixed: memleak through early exit
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19235 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/linux/LinuxFileSystem.cpp
+++ xbmc/linux/LinuxFileSystem.cpp
@@ -141,14 +141,14 @@ void CLinuxFileSystem::GetDrives(int *DeviceType, int len, VECSOURCES &shares)
           char* fs    = reMount.GetReplaceString("\\2");
 #ifdef __APPLE__
           // Ignore the stuff that doesn't make sense.
-        if (strcmp(fs, "devfs") == 0 || strcmp(fs, "fdesc") == 0 || strcmp(fs, "autofs") == 0 || strcmp(fs, "dev") == 0 || strcmp(fs, "mnt") == 0)
-          continue;
-
-        // Skip this for now, until we can figure out the name of the root volume.
-        if (strcmp(mount, "/") == 0)
-          continue;
-
-        result.push_back(mount);
+        if (strcmp(fs, "devfs") != 0 || strcmp(fs, "fdesc") != 0 || strcmp(fs, "autofs") != 0 || strcmp(fs, "dev") != 0 || strcmp(fs, "mnt") != 0)
+        {
+          // Skip this for now, until we can figure out the name of the root volume.
+          if (strcmp(mount, "/") != 0)
+          {
+            result.push_back(mount);
+          }
+        }
 #else
           // Ignore root
           if (strcmp(mount, "/") == 0)
