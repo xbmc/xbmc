b7bd9d0ab78129877e065b02d23f9ab6c495d527
elupus <elupus@svn>
2009-04-05 13:09:49 +0000

    changed: reading if g_VDPAU points to something needs no lock
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19256 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -851,7 +851,9 @@ void CDVDPlayer::Process()
     UpdateApplication(1000);
 
 #ifdef HAVE_LIBVDPAU
-    { CSharedLock lock(g_renderManager.GetSection());
+    if(g_VDPAU)
+    {
+      CSharedLock lock(g_renderManager.GetSection());
       if (g_VDPAU && g_VDPAU->VDPAURecovered)
       {
         CLog::Log(LOGDEBUG, "CDVDPlayer::Process - caught preemption");
