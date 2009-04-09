912b0b1eec8fea9fe698ab7b384696559bd47f99
elupus <elupus@svn>
2009-04-09 20:35:17 +0000

    changed: third attempt at fixing #6272.
    This time we ignore all events from players until we have actually started playback.
    Then we send of the events in application instead, but only if the playlist player is empty.
    
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19357 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4299,7 +4299,6 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
 #endif
 
   // tell system we are starting a file
-  while(m_vPlaybackStarting.size()) m_vPlaybackStarting.pop();
   m_bPlaybackStarting = true;
 
   // We should restart the player, unless the previous and next tracks are using
@@ -4350,19 +4349,22 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
     if (!g_guiSettings.GetBool("lookandfeel.soundsduringplayback"))
       g_audioManager.Enable(false);
   }
-
-  if(!bResult || !IsPlaying())
-  {
-    // since we didn't manage to get playback started, send any queued up messages
-    while(m_vPlaybackStarting.size())
-    {
-      m_gWindowManager.SendMessage(m_vPlaybackStarting.front());
-      m_vPlaybackStarting.pop();
-    }
-  }
-
-  while(m_vPlaybackStarting.size()) m_vPlaybackStarting.pop();
   m_bPlaybackStarting = false;
+  if(bResult)
+  {
+    // we must have started, otherwise player might send this later
+    if(IsPlaying())
+      OnPlayBackStarted();
+  }
+  else
+  {
+    // we send this if it isn't playlistplayer that is doing this
+    int next = g_playlistPlayer.GetNextSong();
+    int size = g_playlistPlayer.GetPlaylist(g_playlistPlayer.GetCurrentPlaylist()).size();
+    if(next < 0 
+    || next >= size)
+      OnPlayBackStopped();
+  }
 
   return bResult;
 }
@@ -4385,9 +4387,7 @@ void CApplication::OnPlayBackEnded()
 
   CGUIMessage msg(GUI_MSG_PLAYBACK_ENDED, 0, 0);
 
-  if(m_bPlaybackStarting)
-    m_vPlaybackStarting.push(msg);
-  else
+  if(!m_bPlaybackStarting)
     m_gWindowManager.SendThreadMessage(msg);
 }
 
@@ -4447,9 +4447,7 @@ void CApplication::OnPlayBackStopped()
   CLog::Log(LOGDEBUG, "Playback was stopped\n");
 
   CGUIMessage msg( GUI_MSG_PLAYBACK_STOPPED, 0, 0 );
-  if(m_bPlaybackStarting)
-    m_vPlaybackStarting.push(msg);
-  else
+  if(!m_bPlaybackStarting)
     m_gWindowManager.SendThreadMessage(msg);
 }
 
