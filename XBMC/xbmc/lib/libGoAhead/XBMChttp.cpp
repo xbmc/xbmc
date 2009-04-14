4f82ee4125e68dcc950c7bd676362f2df8c26c49
davilla <davilla@svn>
2009-04-14 21:37:55 +0000

    fixed: [osx] compiler warnings, g_infoManager.GetImage passes a DWORD
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@19476 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libGoAhead/XBMChttp.cpp
+++ XBMC/xbmc/lib/libGoAhead/XBMChttp.cpp
@@ -1413,7 +1413,7 @@ int CXbmcHttp::xbmcGetCurrentlyPlaying(int numParas, CStdString paras[])
 	      return SetResponse(openTag+"Changed:False");
         //if still here, continue collecting info
 	  }
-	  thumb=g_infoManager.GetImage(VIDEOPLAYER_COVER, -1);
+	  thumb=g_infoManager.GetImage(VIDEOPLAYER_COVER, (DWORD)-1);
 		
 		//CPicture pic;
         //pic.CacheSkinImage("defaultAlbumCover.png", cachedThumb);
@@ -1465,7 +1465,7 @@ int CXbmcHttp::xbmcGetCurrentlyPlaying(int numParas, CStdString paras[])
         output+=closeTag+openTag+"Bitrate"+tag+":"+bitRate;  
       if (!sampleRate.IsEmpty())
         output+=closeTag+openTag+"Samplerate"+tag+":"+sampleRate;  
-	  thumb=g_infoManager.GetImage(MUSICPLAYER_COVER, -1);
+	  thumb=g_infoManager.GetImage(MUSICPLAYER_COVER, (DWORD)-1);
       copyThumb(thumb,thumbFn);
 	  output+=closeTag+openTag+"Thumb"+tag+":"+thumb;
     }
