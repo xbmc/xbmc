7e755bcc205c8f5fa3267a3e6fb27241d285c449
maestrodd <maestrodd@svn>
2009-04-14 21:19:22 +0000

    [OSX] added support for IRKeyboardEmu (AppleRemote emulating keyboard) in OSXRemote
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@19471 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/tools/EventClients/Clients/OSXRemote/RemoteControlWrapper/IRKeyboardEmu.h
@@ -0,0 +1,16 @@
+//
+//  IRKeyboardEmu.h
+//  XBMCHelper
+//
+//  Created by Stephan Diederich on 14.04.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "AppleRemote.h"
+
+@interface IRKeyboardEmu : AppleRemote {
+
+}
+
+@end
