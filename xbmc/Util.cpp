bb05417055a07b5e600e70593ef4b09c28a5c31c
jmarshallnz <jmarshallnz@svn>
2009-04-14 03:11:28 +0000

    cleanup: cleanup the cleanup.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19459 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -2369,7 +2369,7 @@ const BUILT_IN commands[] = {
   { "LastFM.Love",                false,  "Add the current playing last.fm radio track to the last.fm loved tracks" },
   { "LastFM.Ban",                 false,  "Ban the current playing last.fm radio track" },
   { "Container.Refresh",          false,  "Refresh current listing" },
-  { "Container.Update",           false,  "Update current listing" },
+  { "Container.Update",           false,  "Update current listing. Send Container.Update(path,replace) to reset the path history" },
   { "Container.NextViewMode",     false,  "Move to the next view type (and refresh the listing)" },
   { "Container.PreviousViewMode", false,  "Move to the previous view type (and refresh the listing)" },
   { "Container.SetViewMode",      true,   "Move to the view with the given id" },
@@ -3345,20 +3345,14 @@ int CUtil::ExecBuiltIn(const CStdString& execString)
   }  
   else if (execute.Equals("container.update"))
   {
-    bool resetHistory = false;
-    CStdString path = strParameterCaseIntact;
-    if (!strParameterCaseIntact.IsEmpty() && execute.Equals("container.update"))
+    CGUIMessage message(GUI_MSG_NOTIFY_ALL, m_gWindowManager.GetActiveWindow(), 0, GUI_MSG_UPDATE, 0);
+    message.SetStringParam(strParameterCaseIntact);
+    int pos = parameter.find_last_of(",");
+    if (pos >= 0 && parameter.Mid(pos+1).Equals("replace"))
     {
-      int pos = parameter.find_last_of(",");
-      if (pos >= 0)
-      {
-        resetHistory = parameter.Mid(pos+1).Equals("replace");
-        if (resetHistory)
-          path = strParameterCaseIntact.Left(pos);
-      }
+      message.SetStringParam(strParameterCaseIntact.Left(pos));
+      message.SetParam2(1); // reset the history
     }
-    CGUIMessage message(GUI_MSG_NOTIFY_ALL, m_gWindowManager.GetActiveWindow(), 0, GUI_MSG_UPDATE, resetHistory ? 1 : 0);
-    message.SetStringParam(path);
     g_graphicsContext.SendMessage(message);
   }
   else if (execute.Equals("container.nextviewmode"))
