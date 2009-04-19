88eca96a44b029dceddd070c9cc74d1d8eb9d427
elupus <elupus@svn>
2009-04-19 14:45:49 +0000

    fixed: explicit vsync on intel jaunty drivers didn't work, due to glXWaitGL() not actually waiting for gpu to be idle.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19539 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/Surface.cpp
+++ guilib/Surface.cpp
@@ -909,7 +909,7 @@ void CSurface::Flip()
 #ifdef HAS_GLX
     if (m_iVSyncMode == 3)
     {
-      glXWaitGL();
+      glFinish();
       unsigned int before, after;
       if(_glXGetVideoSyncSGI(&before) != 0)
         CLog::Log(LOGERROR, "%s - glXGetVideoSyncSGI - Failed to get current retrace count", __FUNCTION__);
@@ -927,7 +927,7 @@ void CSurface::Flip()
     }
     else if (m_iVSyncMode == 4)
     {
-      glXWaitGL();
+      glFinish();
       unsigned int vCount;
       if(_glXGetVideoSyncSGI(&vCount) == 0)
         _glXWaitVideoSyncSGI(2, (vCount+1)%2, &vCount);
