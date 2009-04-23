0b5faed3c2b58febb74a035b7b20ba0669c36bfd
maestrodd <maestrodd@svn>
2009-04-23 18:18:35 +0000

    fixed: make sure lock is held when modifying shared items; fixed assertion: services don't need to exist anymore (e.g. fast dis-/enable cycle)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19641 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/linux/ZeroconfAvahi.cpp
+++ xbmc/linux/ZeroconfAvahi.cpp
@@ -181,6 +181,7 @@ bool CZeroconfAvahi::doRemoveService(const std::string& fcr_ident)
 
 void CZeroconfAvahi::doStop()
 {
+  ScopedEventLoopBlock l_block(mp_poll);
   for(tServiceMap::const_iterator it = m_services.begin(); it != m_services.end(); ++it)
   {
     if (it->second->mp_group)
@@ -268,12 +269,13 @@ void CZeroconfAvahi::groupCallback(AvahiEntryGroup *fp_group, AvahiEntryGroupSta
       if (it->second->mp_group == fp_group)
         break;
     }
-    assert( it != p_instance->m_services.end() );
-    char* alt_name = avahi_alternative_service_name( it->second->m_name.c_str() );
-    it->second->m_name = alt_name;
-    avahi_free(alt_name);
-    CLog::Log(LOGNOTICE, "CZeroconfAvahi::groupCallback: Service name collision. Renamed to: %s", it->second->m_name.c_str());
-    p_instance->addService(it->second);
+    if( it != p_instance->m_services.end() ) {
+      char* alt_name = avahi_alternative_service_name( it->second->m_name.c_str() );
+      it->second->m_name = alt_name;
+      avahi_free(alt_name);
+      CLog::Log(LOGNOTICE, "CZeroconfAvahi::groupCallback: Service name collision. Renamed to: %s", it->second->m_name.c_str());
+      p_instance->addService(it->second);
+    }
     break;
   }
 
