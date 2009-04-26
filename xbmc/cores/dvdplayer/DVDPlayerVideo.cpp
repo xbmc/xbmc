4adf9d71a2e17a51e9adc942bcb00023bcee2251
vdrfan <vdrfan@svn>
2009-04-26 16:37:47 +0000

    cosmetics: fixed compiler warnings (just a few)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19722 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -867,8 +867,8 @@ int CDVDPlayerVideo::OutputPicture(DVDVideoPicture* pPicture, double pts)
   m_FlipTimeStamp += iFrameDuration;
 
   // ask decoder to drop frames next round, as we are very late
-  if( limited == false  && iClockSleep < -DVD_MSEC_TO_TIME(100)
-  ||  limited == true   && iClockSleep < -iFrameDuration*0.5 )
+  if( (limited == false  && iClockSleep < -DVD_MSEC_TO_TIME(100))
+  ||  (limited == true   && iClockSleep < -iFrameDuration*0.5 ) )
     result |= EOS_VERYLATE;
 
   if( m_speed < 0 )
