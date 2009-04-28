4d336d1ed20ecd5e23886ae5df882e81bd2272ef
topfs2 <topfs2@svn>
2009-04-28 21:55:29 +0000

    Added a small python EventClient to control XBMC in a similair way to dbus-send
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19786 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ tools/EventClients/Clients/XBMC Send/xbmc-send.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+from socket import *
+try:
+    from xbmc.xbmcclient import *
+except:
+    sys.path.append('../../lib/python')
+    from xbmcclient import *
+
+def usage():
+    print "xbmc-send [OPTION] --action=ACTION"
+    print 'Example'
+    print '\txbmc-send --ip=192.168.0.1 --p 9777 --action="XBMC.Quit"'
+    print "Options"
+    print "\t--help, -h.\t\t\tWill bring up this message"
+    print "\t-i IP, --ip=IP.\t\t\tChoose what IP to connect to (default=localhost)"
+    print "\t-p PORT, --port=PORT.\t\tChoose what PORT to connect to (default=9777)"
+    pass
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hipa:v", ["help", "ip=", "port=", "action="])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    ip = "localhost"
+    port = 9777
+    actions = []
+    verbose = False
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-i", "--ip"):
+            ip = a
+        elif o in ("-a", "--action"):
+            actions.append(a)
+        else:
+            assert False, "unhandled option"
+
+    print ip
+    print actions
+    
+    addr = (ip, port)
+    sock = socket(AF_INET,SOCK_DGRAM)
+    
+    for action in actions:
+        packet = PacketACTION(actionmessage=action, actiontype=ACTION_BUTTON)
+        packet.send(sock, addr)
+
+if __name__=="__main__":
+    main()
