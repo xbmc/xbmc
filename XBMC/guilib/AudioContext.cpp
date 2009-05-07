6abd260dce682efff6633e4522428031cfc2b634
wiso <wiso@svn>
2009-05-07 21:15:28 +0000

    [WIN32] fixed: XBMC would crash on cross fading by reverting r19933.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20045 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/guilib/AudioContext.cpp
+++ XBMC/guilib/AudioContext.cpp
@@ -76,15 +76,10 @@ void CAudioContext::SetActiveDevice(int iDevice)
   /* if device is the same, no need to bother */
 #ifdef _WIN32PC
   if(m_iDevice == iDevice && g_guiSettings.GetString("audiooutput.audiodevice").Equals(m_strDevice))
-  {
-    DSCAPS devCaps = {0};
-    if (SUCCEEDED(m_pDirectSoundDevice->GetCaps(&devCaps))) // Make sure the DirectSound interface is still valid.
-      return;
-  }
 #else
   if(m_iDevice == iDevice)
-    return;
 #endif
+    return;
 
   if (iDevice==DEFAULT_DEVICE)
   {
