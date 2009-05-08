e414521d13d12a643b3f30119cd7be767151acdb
phi2039 <phi2039@svn>
2009-05-08 05:38:01 +0000

    fixed: [win32] Re-committed r19933 to fix HDMI device disconnect issues, and fixed crash while cross-fading.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@20057 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/AudioContext.cpp
+++ guilib/AudioContext.cpp
@@ -76,6 +76,13 @@ void CAudioContext::SetActiveDevice(int iDevice)
   /* if device is the same, no need to bother */
 #ifdef _WIN32PC
   if(m_iDevice == iDevice && g_guiSettings.GetString("audiooutput.audiodevice").Equals(m_strDevice))
+  {
+    DSCAPS devCaps = {0};
+    devCaps.dwSize = sizeof(devCaps);
+    if (SUCCEEDED(m_pDirectSoundDevice->GetCaps(&devCaps))) // Make sure the DirectSound interface is still valid.
+      return;
+  }
+
 #else
   if(m_iDevice == iDevice)
 #endif
