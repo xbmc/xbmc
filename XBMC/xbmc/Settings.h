4ccbcef481874e5d806424159c5772c03eaea670
jmarshallnz <jmarshallnz@svn>
2009-05-14 04:52:53 +0000

    changed: Move CSettings::GetInteger/GetFloat with range specified to XMLUtils as these are useful elsewhere.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20256 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Settings.h
+++ XBMC/xbmc/Settings.h
@@ -441,11 +441,6 @@ public:
   bool SaveSources();
 
 protected:
-  // these 3 don't have a default - used for advancedsettings.xml
-  bool GetInteger(const TiXmlElement* pRootElement, const char *strTagName, int& iValue, const int iMin, const int iMax);
-  bool GetFloat(const TiXmlElement* pRootElement, const char *strTagName, float& fValue, const float fMin, const float fMax);
-  bool GetString(const TiXmlElement* pRootElement, const char *strTagName, CStdString& strValue);
-
   void GetCustomRegexps(TiXmlElement *pRootElement, CStdStringArray& settings);
   void GetCustomRegexpReplacers(TiXmlElement *pRootElement, CStdStringArray& settings);
   void GetCustomExtensions(TiXmlElement *pRootElement, CStdString& extensions);
