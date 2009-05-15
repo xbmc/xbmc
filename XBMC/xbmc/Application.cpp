c103f7af5a56e6cc4eccd0b9cbc237516e697795
phi2039 <phi2039@svn>
2009-05-15 03:52:29 +0000

    fixed: [osx] Changed #if defined(APPLE) to #if defined(__APPLE__) for GUI sounds handling in CApplication::PlayFile
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20292 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/Application.cpp
+++ XBMC/xbmc/Application.cpp
@@ -4352,7 +4352,7 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
 
   // Workaround for bug/quirk in SDL_Mixer on OSX.
   // TODO: Remove after GUI Sounds redux
-#if defined(APPLE)
+#if defined(__APPLE__)
   if (!g_guiSettings.GetBool("lookandfeel.soundsduringplayback"))
     g_audioManager.Enable(false);
 #endif   
@@ -4385,7 +4385,7 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
     }
 #endif
 
-#if !defined(APPLE)
+#if !defined(__APPLE__)
     if (!g_guiSettings.GetBool("lookandfeel.soundsduringplayback"))
       g_audioManager.Enable(false);
 #endif    
