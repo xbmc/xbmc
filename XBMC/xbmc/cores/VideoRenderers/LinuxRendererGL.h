17b1bc378aa2d59788eb3020639e89c36b50e160
elupus <elupus@svn>
2009-05-20 20:35:12 +0000

    changed: combined renderer method OnClose and UnInit back into one, and call that from gui thread
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20479 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/VideoRenderers/LinuxRendererGL.h
+++ XBMC/xbmc/cores/VideoRenderers/LinuxRendererGL.h
@@ -135,7 +135,6 @@ public:
   virtual void         FlipPage(int source);
   virtual unsigned int PreInit();
   virtual void         UnInit();
-  virtual void         OnClose(); // called from main GUI thread
   virtual void         Reset(); /* resets renderer after seek for example */
 
   void AutoCrop(bool bCrop);
