421ed8a36ce60468a1762db0e804bbfb2301f589
vdrfan <vdrfan@svn>
2009-05-22 14:55:51 +0000

    removed: unused variables
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@20546 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/MusicInfoScanner.cpp
+++ xbmc/MusicInfoScanner.cpp
@@ -750,8 +750,6 @@ int CMusicInfoScanner::GetPathHash(const CFileItemList &items, CStdString &hash)
 {
   // Create a hash based on the filenames, filesize and filedate.  Also count the number of files
   if (0 == items.Size()) return 0;
-  unsigned char md5hash[16];
-  char md5HexString[33];
   XBMC::MD5 md5state;
   int count = 0;
   for (int i = 0; i < items.Size(); ++i)
