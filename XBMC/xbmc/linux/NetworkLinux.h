c44a2f8180a8ab3aec5edc7df3efaddedd82bfe5
topfs2 <topfs2@svn>
2009-05-22 08:37:12 +0000

    Cosmetics
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/NonRootNetworkConfiguration@20534 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/linux/NetworkLinux.h
+++ XBMC/xbmc/linux/NetworkLinux.h
@@ -33,55 +33,53 @@ class CNetworkLinux;
 class CNetworkInterfaceLinux : public CNetworkInterface
 {
 public:
-   CNetworkInterfaceLinux(CNetworkLinux* network, CStdString interfaceName);
-   ~CNetworkInterfaceLinux(void);
+  CNetworkInterfaceLinux(CNetworkLinux* network, CStdString interfaceName);
+  ~CNetworkInterfaceLinux(void);
 
-   virtual CStdString& GetName(void);
+  virtual CStdString& GetName(void);
 
-   virtual bool IsEnabled(void);
-   virtual bool IsConnected(void);
-   virtual bool IsWireless(void);
+  virtual bool IsEnabled(void);
+  virtual bool IsConnected(void);
+  virtual bool IsWireless(void);
 
-   virtual CStdString GetMacAddress(void);
+  virtual CStdString GetMacAddress(void);
 
-   virtual CStdString GetCurrentIPAddress();
-   virtual CStdString GetCurrentNetmask();
-   virtual CStdString GetCurrentDefaultGateway(void);
-   virtual CStdString GetCurrentWirelessEssId(void);
+  virtual CStdString GetCurrentIPAddress();
+  virtual CStdString GetCurrentNetmask();
+  virtual CStdString GetCurrentDefaultGateway(void);
+  virtual CStdString GetCurrentWirelessEssId(void);
 
-   virtual void GetSettings(NetworkAssignment& assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
-   virtual void SetSettings(NetworkAssignment& assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
+  virtual void GetSettings(NetworkAssignment& assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
+  virtual void SetSettings(NetworkAssignment& assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
 
-   // Returns the list of access points in the area
-   virtual std::vector<NetworkAccessPoint> GetAccessPoints(void);
-
+  // Returns the list of access points in the area
+  virtual std::vector<NetworkAccessPoint> GetAccessPoints(void);
 private:
-   void WriteSettings(FILE* fw, NetworkAssignment assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
-   CStdString     m_interfaceName;
-   CNetworkLinux* m_network;
+  void WriteSettings(FILE* fw, NetworkAssignment assignment, CStdString& ipAddress, CStdString& networkMask, CStdString& defaultGateway, CStdString& essId, CStdString& key, EncMode& encryptionMode);
+  CStdString     m_interfaceName;
+  CNetworkLinux* m_network;
   void AddNetworkAccessPoint(std::vector<NetworkAccessPoint> &apv, const char *NetworkPath, DBusConnection *con);
 };
 
 class CNetworkLinux : public CNetwork
 {
 public:
-   CNetworkLinux(void);
-   virtual ~CNetworkLinux(void);
+  CNetworkLinux(void);
+  virtual ~CNetworkLinux(void);
 
-   // Return the list of interfaces
-   virtual std::vector<CNetworkInterface*>& GetInterfaceList(void);
+  // Return the list of interfaces
+  virtual std::vector<CNetworkInterface*>& GetInterfaceList(void);
 
-   // Get/set the nameserver(s)
-   virtual std::vector<CStdString> GetNameServers(void);
-   virtual void SetNameServers(std::vector<CStdString> nameServers);
+  // Get/set the nameserver(s)
+  virtual std::vector<CStdString> GetNameServers(void);
+  virtual void SetNameServers(std::vector<CStdString> nameServers);
 
-   friend class CNetworkInterfaceLinux;
-
+  friend class CNetworkInterfaceLinux;
 private:
-   int GetSocket() { return m_sock; }
-   void queryInterfaceList();
-   std::vector<CNetworkInterface*> m_interfaces;
-   int m_sock;
+  int GetSocket() { return m_sock; }
+  void queryInterfaceList();
+  std::vector<CNetworkInterface*> m_interfaces;
+  int m_sock;
 };
 
 #endif
