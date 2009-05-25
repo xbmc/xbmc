9a065df8ee9a65c6e127f9fbbba65da06043e7c1
bobo1on1 <bobo1on1@svn>
2009-05-25 00:21:55 +0000

    cosmetic
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@20629 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoReferenceClock.cpp
+++ xbmc/VideoReferenceClock.cpp
@@ -30,12 +30,12 @@
   #include <QuartzCore/CVDisplayLink.h>
   #include "CocoaInterface.h"
 #elif defined(_WIN32)
- #pragma comment (lib,"d3d9.lib")
- #if(DIRECT3D_VERSION > 0x0900)
-  #pragma comment (lib,"DxErr.lib")
- #else
-  #pragma comment (lib,"Dxerr9.lib")
- #endif
+  #pragma comment (lib,"d3d9.lib")
+  #if(DIRECT3D_VERSION > 0x0900)
+    #pragma comment (lib,"DxErr.lib")
+  #else
+    #pragma comment (lib,"Dxerr9.lib")
+  #endif
 #endif
 
 using namespace std;
