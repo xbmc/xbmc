f1e19e66b9a7d55eac76b27c9daf1534541a883c
phi2039 <phi2039@svn>
2009-05-28 22:42:39 +0000

    changed: Added new method IAudioRenderer::GetCacheTime()
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@20735 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/AudioRenderers/ALSADirectSound.h
+++ xbmc/cores/AudioRenderers/ALSADirectSound.h
@@ -48,6 +48,7 @@ public:
   virtual void RegisterAudioCallback(IAudioCallback* pCallback);
   virtual DWORD GetChunkLen();
   virtual FLOAT GetDelay();
+  virtual FLOAT GetCacheTime();
   CALSADirectSound();
   virtual bool Initialize(IAudioCallback* pCallback, int iChannels, unsigned int uiSamplesPerSec, unsigned int uiBitsPerSample, bool bResample, const char* strAudioCodec = "", bool bIsMusic=false, bool bPassthrough = false);
   virtual ~CALSADirectSound();
