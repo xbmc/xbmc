e61138d6102d1153e4053d022c005064a6f8dd61
ceros7 <ceros7@svn>
2009-05-29 23:07:03 +0000

    fix: Allow our modules to be compatible with Python2.x.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/external-libraries-support@20781 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libPython/XBPythonDll.h
+++ XBMC/xbmc/lib/libPython/XBPythonDll.h
@@ -57,6 +57,13 @@
   #define Py_InitModule4 Py_InitModule4_64
   #endif
 #endif
+/* Allow our modules to be compatible with Python2.4.
+   http://www.python.org/dev/peps/pep-0353/ */
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
 
 #ifdef __cplusplus
 extern "C"
