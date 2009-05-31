4aa59ff67a0a41e40d6ee385216b17ff8b32846b
ceros7 <ceros7@svn>
2009-05-31 05:50:29 +0000

    revert: For some reason a project file for Windows was changed. Reverting the change.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@20821 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/VS2008Express/update_svn_rev.bat
+++ project/VS2008Express/update_svn_rev.bat
@@ -1,2 +1,8 @@
+@echo off
 rem subwcrev is included in the tortoise svn client: http://tortoisesvn.net/downloads
-subwcrev ../../../../xbmc_linuxport ../../xbmc/win32/svn_rev.tmpl ../../xbmc/win32/svn_rev.h
+SET REV_FILE=..\..\xbmc\win32\svn_rev.h
+IF EXIST %REV_FILE% (
+  del %REV_FILE%
+)
+subwcrev ../.. ../../xbmc/win32/svn_rev.tmpl %REV_FILE%
+SET REV_FILE=
