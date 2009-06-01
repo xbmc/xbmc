047b5ff3b123439615c6a02e1b7aa63afc3a0bfe
charlydoes <charlydoes@svn>
2009-06-01 17:33:27 +0000

    fixed: AC3Codec incorrect dll interface (r20820), paplayer uses a patched version of liba52 to be able to decode little-endian ac3-wav files.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20854 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/DllAc3codec.h
+++ XBMC/xbmc/cores/paplayer/DllAc3codec.h
@@ -105,7 +105,7 @@ public:
   virtual ~DllAc3CodecInterface() {}
   virtual a52_state_t * a52_init (uint32_t mm_accel)=0;
   virtual sample_t * a52_samples (a52_state_t * state)=0;
-  virtual int a52_syncinfo (uint8_t * buf, int * flags, int * sample_rate, int * bit_rate)=0;
+  virtual int a52_syncinfo (a52_state_t * state, uint8_t * buf, int * flags, int * sample_rate, int * bit_rate)=0;
   virtual int a52_frame (a52_state_t * state, uint8_t * buf, int * flags, level_t * level, sample_t bias)=0;
   virtual void a52_dynrng (a52_state_t * state, level_t (* call) (level_t, void *), void * data)=0;
   virtual int a52_block (a52_state_t * state)=0;
@@ -117,7 +117,7 @@ class DllAc3Codec : public DllDynamic, DllAc3CodecInterface
   DECLARE_DLL_WRAPPER(DllAc3Codec, DLL_PATH_AC3_CODEC)
   DEFINE_METHOD1(a52_state_t *, a52_init, (uint32_t p1))
   DEFINE_METHOD1(sample_t *, a52_samples, (a52_state_t *p1))
-  DEFINE_METHOD4(int, a52_syncinfo, (uint8_t * p1, int * p2, int * p3, int * p4))
+  DEFINE_METHOD5(int, a52_syncinfo, (a52_state_t * p1, uint8_t * p2, int * p3, int * p4, int * p5))
   DEFINE_METHOD5(int, a52_frame, (a52_state_t * p1, uint8_t * p2, int * p3, level_t * p4, sample_t p5))
   DEFINE_METHOD3(void, a52_dynrng, (a52_state_t * p1, level_t (* p2) (level_t, void *), void * p3))
   DEFINE_METHOD1(int, a52_block, (a52_state_t * p1))
