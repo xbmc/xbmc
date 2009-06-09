99a241086a3c76f06be6e664f58c341eb4bc8063
wiso <wiso@svn>
2009-06-09 18:19:01 +0000

    [WIN32] added: loginfo if running XBMC as restricted user or admin (tested on Vista untested on XP)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@20977 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/win32/WIN32Util.cpp
+++ XBMC/xbmc/win32/WIN32Util.cpp
@@ -792,6 +792,30 @@ bool CWIN32Util::HasReqGLVersion()
     return false;
 }
 
+BOOL CWIN32Util::IsCurrentUserLocalAdministrator()
+{
+  BOOL b;
+  SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+  PSID AdministratorsGroup; 
+  b = AllocateAndInitializeSid(
+      &NtAuthority,
+      2,
+      SECURITY_BUILTIN_DOMAIN_RID,
+      DOMAIN_ALIAS_RID_ADMINS,
+      0, 0, 0, 0, 0, 0,
+      &AdministratorsGroup); 
+  if(b)
+  {
+    if (!CheckTokenMembership( NULL, AdministratorsGroup, &b)) 
+    {
+         b = FALSE;
+    } 
+    FreeSid(AdministratorsGroup); 
+  }
+
+  return(b);
+}
+
 extern "C"
 {
   FILE *fopen_utf8(const char *_Filename, const char *_Mode)
