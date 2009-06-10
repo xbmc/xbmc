3875e4a4019b71194c20848cdcc2402669c087dd
spiff_ <spiff_@svn>
2009-06-10 18:57:11 +0000

    fixed: no need to check a unsigned variable for negative values. thanks to capnbry for "laughing at me"
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@21006 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/PlayerCoreFactory.cpp
+++ XBMC/xbmc/cores/PlayerCoreFactory.cpp
@@ -69,7 +69,7 @@ IPlayer* CPlayerCoreFactory::CreatePlayer(const CStdString& strCore, IPlayerCall
 
 IPlayer* CPlayerCoreFactory::CreatePlayer(const PLAYERCOREID eCore, IPlayerCallback& callback)
 {
-  if (eCore-1 < 0 || eCore-1 > s_vecCoreConfigs.size()-1)
+  if (eCore-1 > s_vecCoreConfigs.size()-1)
     return NULL;
 
   return s_vecCoreConfigs[eCore-1]->CreatePlayer(callback);
