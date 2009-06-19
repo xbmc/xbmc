4f5d37813513611171960880a12f9994fef2c1aa
althekiller <althekiller@svn>
2009-06-19 21:19:37 +0000

    merged: r21157 from linuxport
    fixed: Lastfm radio jacked up the music playlist shuffle and repeast state.
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@21160 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/LastFmManager.cpp
+++ xbmc/LastFmManager.cpp
@@ -62,8 +62,6 @@ CLastFmManager::CLastFmManager()
 {
   m_hWorkerEvent = CreateEvent(NULL, false, false, NULL);
   m_RadioTrackQueue = new CPlayList;
-  m_bLastShuffleState = g_playlistPlayer.IsShuffled(PLAYLIST_MUSIC);
-  m_LastRepeatState = g_playlistPlayer.GetRepeat(PLAYLIST_MUSIC);
 }
 
 CLastFmManager::~CLastFmManager()
@@ -247,8 +245,6 @@ bool CLastFmManager::ChangeStation(const CURL& stationUrl)
   UpdateProgressDialog(261); //Waiting for start....
 
   g_playlistPlayer.ClearPlaylist(PLAYLIST_MUSIC);
-  g_playlistPlayer.SetShuffle(PLAYLIST_MUSIC, false);
-  g_playlistPlayer.SetRepeat(PLAYLIST_MUSIC, PLAYLIST::REPEAT_NONE);
   RequestRadioTracks();
   CacheTrackThumb(XBMC_LASTFM_MINTRACKS);
   AddToPlaylist(XBMC_LASTFM_MINTRACKS);
@@ -601,6 +597,12 @@ void CLastFmManager::OnStartup()
 
 void CLastFmManager::Process()
 {
+  bool bLastShuffleState = g_playlistPlayer.IsShuffled(PLAYLIST_MUSIC);
+  PLAYLIST::REPEAT_STATE LastRepeatState = g_playlistPlayer.GetRepeat(PLAYLIST_MUSIC);
+  
+  g_playlistPlayer.SetShuffle(PLAYLIST_MUSIC, false);
+  g_playlistPlayer.SetRepeat(PLAYLIST_MUSIC, PLAYLIST::REPEAT_NONE);
+  
   while (!m_bStop)
   {
     WaitForSingleObject(m_hWorkerEvent, INFINITE);
@@ -615,6 +617,10 @@ void CLastFmManager::Process()
     iNrCachedTracks = m_RadioTrackQueue->size();
     CacheTrackThumb(iNrCachedTracks);
   }
+	
+	g_playlistPlayer.SetShuffle(PLAYLIST_MUSIC, bLastShuffleState);
+  g_playlistPlayer.SetRepeat(PLAYLIST_MUSIC, LastRepeatState);
+  
   CLog::Log(LOGINFO,"LastFM thread terminated");
 }
 
@@ -623,8 +629,6 @@ void CLastFmManager::StopRadio(bool bKillSession /*= true*/)
   if (bKillSession) 
   {
 	m_RadioSession = "";
-	g_playlistPlayer.SetRepeat(PLAYLIST_MUSIC, m_LastRepeatState);
-	g_playlistPlayer.SetShuffle(PLAYLIST_MUSIC, m_bLastShuffleState);
   }
   if (m_ThreadHandle)
   {
