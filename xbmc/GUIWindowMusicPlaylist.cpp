ed03d3700996bbe2005e482fedd2954c4116452f
althekiller <althekiller@svn>
2009-06-26 01:40:50 +0000

    merged: r21220 from linuxport
    fixed: Couldn't edit partymode playlist if the xsp didn't exist and
    special://profile was special://masterprofile.
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk/XBMC@21221 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowMusicPlaylist.cpp
+++ xbmc/GUIWindowMusicPlaylist.cpp
@@ -597,7 +597,7 @@ bool CGUIWindowMusicPlayList::OnContextButton(int itemNumber, CONTEXT_BUTTON but
 
   case CONTEXT_BUTTON_EDIT_PARTYMODE:
   {
-    CStdString playlist = "P:\\PartyMode.xsp";
+    CStdString playlist = g_settings.GetUserDataItem("PartyMode.xsp");
     if (CGUIDialogSmartPlaylistEditor::EditPlaylist(playlist))
     {
       // apply new rules
