9cba89388de4cf0f586948b4967b62892475c7d4
dteirney <dteirney@svn>
2009-06-27 00:31:15 +0000

    Updated: TV shows are listed based on the program ID value so they appear in both the TV Shows directory and Movies directory if the movie length hack is used (so TV Shows that are dual episodes show up in the TV Shows directory along with the other "normal" recordings).
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@21228 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/FileSystem/CMythDirectory.cpp
+++ XBMC/xbmc/FileSystem/CMythDirectory.cpp
@@ -568,7 +568,10 @@ bool CCMythDirectory::IsTvShow(const cmyth_proginfo_t program)
    * There isn't enough information exposed by libcmyth to distinguish between an episode in a series and a
    * one off TV show. See comment in IsMovie for more information.
    *
-   * Anything that isn't a movie is considered a TV show.
+   * Return anything that isn't a movie as per the program ID. This may result in a recording being
+   * in both the Movies and TV Shows folders if the advanced setting to choose a movie based on
+   * recording length is used, but means that at least all recorded TV Shows can be found in one
+   * place.
    */
-  return !IsMovie(program);
+  return GetValue(m_dll->proginfo_programid(program)).Left(2) != "MV";
 }
