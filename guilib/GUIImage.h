8c12b6937a15a3cc9f869c9685c9dbfdd498aacc
jmarshallnz <jmarshallnz@svn>
2009-07-02 09:27:59 +0000

    changed: Drop back to the fallback image (or none) if an image doesn't exist.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@21336 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIImage.h
+++ guilib/GUIImage.h
@@ -109,6 +109,7 @@ protected:
 
   CGUITexture m_texture;
   std::vector<CFadingTexture *> m_fadingTextures;
+  CStdString m_currentTexture;
 
   unsigned int m_crossFadeTime;
   unsigned int m_currentFadeTime;
