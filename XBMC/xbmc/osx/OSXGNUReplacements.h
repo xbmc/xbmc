70fc61548b88fb1df0c569e95dc7579169b6d285
maestrodd <maestrodd@svn>
2009-07-06 09:57:51 +0000

    [OSX] added getline to osxgnureplacements
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@21439 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/osx/OSXGNUReplacements.h
+++ XBMC/xbmc/osx/OSXGNUReplacements.h
@@ -65,6 +65,7 @@ extern "C" {
 #endif /* __cplusplus */
 
 ssize_t getdelim(char **lineptr, size_t *n, int delimiter, FILE *stream);
+ssize_t getline (char **lineptr, size_t *n, FILE *stream);
 
 #ifdef __cplusplus
 }
