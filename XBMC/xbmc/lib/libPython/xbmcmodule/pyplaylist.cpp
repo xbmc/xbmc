f3332b07d5fef5c746d12820dbab461739908aad
maestrodd <maestrodd@svn>
2009-07-29 12:55:36 +0000

    fixed: don't pass non PODs to varargs
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@21897 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/lib/libPython/xbmcmodule/pyplaylist.cpp
+++ XBMC/xbmc/lib/libPython/xbmcmodule/pyplaylist.cpp
@@ -100,7 +100,7 @@ namespace PYXBMC
       return Py_BuildValue((char*)"l", self->item->GetMusicInfoTag()->GetDuration());
 
     if (self->item->HasVideoInfoTag())
-      return Py_BuildValue((char*)"l", self->item->GetVideoInfoTag()->m_strRuntime);
+      return Py_BuildValue((char*)"s", self->item->GetVideoInfoTag()->m_strRuntime.c_str());
 
     return Py_BuildValue((char*)"l", 0);
   }
