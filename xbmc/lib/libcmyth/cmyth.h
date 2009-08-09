354bf8a6953e6d422b3cd71cf9d41fd0fccfc441
dteirney <dteirney@svn>
2009-08-09 07:04:53 +0000

    Updated: change libcmyth to use the myth protocol QUERY_COMMBREAK rather that the MySQL database for cmyth_get_commbreaklist. Rename existing mysql method to match other mysql variants in libcmyth.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@22065 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libcmyth/cmyth.h
+++ xbmc/lib/libcmyth/cmyth.h
@@ -995,7 +995,8 @@ extern int cmyth_set_bookmark(cmyth_conn_t conn, cmyth_proginfo_t prog,
 	long long bookmark);
 extern cmyth_commbreaklist_t cmyth_commbreaklist_create(void);
 extern cmyth_commbreak_t cmyth_commbreak_create(void);
-extern cmyth_commbreaklist_t cmyth_get_commbreaklist(cmyth_database_t db, cmyth_conn_t conn, cmyth_proginfo_t prog);
+extern cmyth_commbreaklist_t cmyth_mysql_get_commbreaklist(cmyth_database_t db, cmyth_conn_t conn, cmyth_proginfo_t prog);
+extern cmyth_commbreaklist_t cmyth_get_commbreaklist(cmyth_conn_t conn, cmyth_proginfo_t prog);
 extern int cmyth_rcv_commbreaklist(cmyth_conn_t conn, int *err, cmyth_commbreaklist_t breaklist, int count);
 
 /*
