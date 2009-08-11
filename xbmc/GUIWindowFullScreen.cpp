d2331ca32d580d87d250d91742b5c806ddc9c7ad
topfs2 <topfs2@svn>
2009-08-11 18:32:11 +0000

    Made mousewheel seek when in fullscreenvideo, similair to how totem and other media players handle mousewheel
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@22116 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowFullScreen.cpp
+++ xbmc/GUIWindowFullScreen.cpp
@@ -551,6 +551,14 @@ bool CGUIWindowFullScreen::OnMouse(const CPoint &point)
       pOSD->DoModal();
     }
   }
+  if (g_Mouse.GetWheel())
+  { // Mouse wheel
+    int wheel = abs(g_Mouse.GetWheel());
+    CAction action;
+    action.fAmount1 = 0.5f * (float)wheel;
+    action.wID = g_Mouse.GetWheel() > 0 ? ACTION_ANALOG_SEEK_FORWARD : ACTION_ANALOG_SEEK_BACK;
+    return g_application.OnAction(action);
+  }
   return true;
 }
 
