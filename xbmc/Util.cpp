e52fadf0de4cbf501b90a2300aa36df7b9f2d8d8
spiff_ <spiff_@svn>
2009-08-16 17:32:40 +0000

    fixed: subtitle caching would fail for stacked 2CD movies where each file was in separate directories (rarred stuff)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@22200 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -1355,6 +1355,14 @@ void CUtil::CacheSubtitles(const CStdString& strMovie, CStdString& strExtensionC
   // checking if any of the common subdirs exist ..
   CLog::Log(LOGDEBUG,"%s: Checking for common subirs...", __FUNCTION__);
 
+  vector<CStdString> token;
+  Tokenize(strPath,token,"/\\");
+  if (token[token.size()-1].size() == 3 && token[token.size()-1].Mid(0,2).Equals("cd"))
+  {
+    CStdString strPath2;
+    GetParentPath(strPath,strPath2);
+    strLookInPaths.push_back(strPath2);
+  } 
   int iSize = strLookInPaths.size();
   for (int i=0;i<iSize;++i)
   {
