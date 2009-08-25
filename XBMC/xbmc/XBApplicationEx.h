c434767bcd094a09fb2487ab3cf29809c39a8438
yuvalt <yuvalt@svn>
2009-08-25 10:43:37 +0000

    forgotten commit
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/directx@22402 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/XBApplicationEx.h
+++ XBMC/xbmc/XBApplicationEx.h
@@ -22,29 +22,12 @@
 #define XBAPPLICATIONEX_H
 
 #include "IWindowManagerCallback.h"
-#include "MouseStat.h"
-#if defined (HAS_SDL) && defined (HAS_SDL_JOYSTICK)
-#include "common/SDLJoystick.h"
-#endif
-#ifdef HAS_IRSERVERSUITE
-#include "common/IRServerSuite/IRServerSuite.h"
-#elif defined(HAS_LIRC)
-#include "common/LIRC.h"
-#endif
 
 class CXBApplicationEx : public IWindowManagerCallback
 {
 public:
   CXBApplicationEx();
   ~CXBApplicationEx();
-
-  // Main objects used for creating and rendering the 3D scene
-#ifdef HAS_DX
-  LPDIRECT3D9 m_pD3D;              // The D3D enumerator object
-  LPDIRECT3DDEVICE9 m_pd3dDevice;        // The D3D rendering device
-  LPDIRECT3DSURFACE9 m_pBackBuffer;       // The back buffer
-  //LPDIRECT3DSURFACE9    m_pDepthBuffer;      // The depth buffer
-#endif
 
   // Variables for timing
   bool m_bStop;
@@ -54,15 +37,6 @@ public:
   // Overridable functions for the 3D scene created by the app
   virtual HRESULT Initialize() { return S_OK; }
   virtual HRESULT Cleanup() { return S_OK; }
-  //elis void ReadInput(XBMC_Event newEvent);
-
-  /* eli
-  bool ProcessOSShortcuts(XBMC_Event &event);
-  bool ProcessWin32Shortcuts(XBMC_Event &event);
-  bool ProcessLinuxShortcuts(XBMC_Event &event);
-  bool ProcessOSXShortcuts(XBMC_Event &event);
-  */
-
 
 public:
   // Functions to create, run, and clean up the application
