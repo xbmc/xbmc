da84a036a46fb86e310abf8cd43060305a661123
elupus <elupus@svn>
2009-08-26 22:58:29 +0000

    changed: move yuv->rgb shaders out to files and modify to just do a matrix multiplication
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@22449 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ XBMC/system/shaders/yuv2rgb_basic.glsl
@@ -0,0 +1,28 @@
+#if(XBMC_texture_rectangle)
+# extension GL_ARB_texture_rectangle : enable
+# define texture2D texture2DRect
+# define sampler2D sampler2DRect
+#endif
+
+uniform sampler2D m_sampY;
+uniform sampler2D m_sampU;
+uniform sampler2D m_sampV;
+varying vec2      m_cordY;
+varying vec2      m_cordU;
+varying vec2      m_cordV;
+
+uniform mat4      m_yuvmat;
+
+
+void main()
+{
+  vec4 yuv, rgb;
+  yuv.rgba = vec4( texture2D(m_sampY, m_cordY).r
+                 , texture2D(m_sampU, m_cordU).r
+                 , texture2D(m_sampV, m_cordV).r
+                 , 1.0 );
+
+  rgb   = m_yuvmat * yuv;
+  rgb.a = gl_Color.a;
+  gl_FragColor = rgb;
+}
