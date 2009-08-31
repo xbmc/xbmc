4c8f3efcd5f26d3882733a81cb29dd55a9b6848b
AlTheKiller <AlTheKiller@svn>
2009-08-31 20:42:18 +0000

    Removed: Old nullsoft api from PAPlayer MP3 codec Changed: Move PAPlayer MP3 codec toward loading unmodified system dll. (optimizations incoming)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@22525 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/MP3codec.h
+++ xbmc/cores/paplayer/MP3codec.h
@@ -25,6 +25,31 @@
 #include "MusicInfoTagLoaderMP3.h"
 #include "DllMadCodec.h"
 
+enum madx_sig {
+	ERROR_OCCURED,
+	MORE_INPUT,
+	FLUSH_BUFFER,
+	CALL_AGAIN,
+  SKIP_FRAME
+};
+
+struct madx_house {
+  struct mad_stream stream;
+  struct mad_frame  frame;
+  struct mad_synth  synth;
+  mad_timer_t       timer;
+  unsigned long     frame_cnt;
+  unsigned char*    output_ptr;
+};
+
+struct madx_stat {
+  size_t 		     write_size;
+  size_t		     readsize;
+  size_t		     remaining;
+  size_t 		     framepcmsize;
+  bool           flushed;
+};
+
 class MP3Codec : public CachingCodec
 {
 public:
@@ -41,14 +66,35 @@ public:
   virtual bool SkipNext();
   virtual bool HasFloatData() const { return m_BitsPerSampleInternal == 32; };
 private:
+
+  /* TODO decoder functions */
+  virtual int Decode(void *in, int in_len, void *out, int *out_len, unsigned int out_fmt[8]);
+  virtual void Flush();
+  int madx_init(madx_house* mxhouse);
+  madx_sig madx_read(unsigned char* in_buffer, unsigned char* out_buffer, madx_house *mxhouse, madx_stat* mxstat, int maxwrite, bool discard = false);
+  void madx_deinit(madx_house* mxhouse);
+  /* END decoder functions */
+
   void OnFileReaderClearEvent();
   void FlushDecoder();
   int Read(int size, bool init = false);
 
+  /* TODO decoder vars */
+  unsigned char* m_InputBufferInt;
+  unsigned char* m_InputBufferRemain;
+  int m_BytesDecoded;
+  bool m_HaveData;
+  unsigned int m_formatdata[8];
+  unsigned char  flushcnt;
+
+  madx_house mxhouse;
+  madx_stat  mxstat;
+  madx_sig   mxsig;
+  /* END decoder vars */
+
   // Decoding variables
   __int64 m_lastByteOffset;
   bool    m_eof;
-  IAudioDecoder* m_pDecoder;    // handle to the codec.
   bool    m_Decoding;
   bool    m_CallAgainWithSameBuffer;
 
