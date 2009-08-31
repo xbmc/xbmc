b82cc4d567d7b76896b9b01be2df97201602f366
AlTheKiller <AlTheKiller@svn>
2009-08-31 20:42:18 +0000

    Removed: Old nullsoft api from PAPlayer MP3 codec Changed: Move PAPlayer MP3 codec toward loading unmodified system dll. (optimizations incoming)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport@22525 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/cores/paplayer/DllMadCodec.h
+++ XBMC/xbmc/cores/paplayer/DllMadCodec.h
@@ -21,21 +21,71 @@
  *
  */
 
+#if (defined HAVE_CONFIG_H) && (!defined WIN32)
+  #include "config.h"
+#endif
+
+#undef byte
+#if (defined USE_EXTERNAL_LIBMAD)
+  #include <mad.h>
+#else
+  #include "mad.h"
+#endif
+
 #include "DynamicDll.h"
-#include "dec_if.h"
+
 
 class DllMadCodecInterface
 {
 public:
-  virtual ~DllMadCodecInterface() {}
-  virtual IAudioDecoder* CreateAudioDecoder (unsigned int type, IAudioOutput **output)=0;
+  virtual void  mad_frame_init(struct mad_frame *) = 0;
+  virtual void  mad_frame_finish(struct mad_frame *) = 0;
+  virtual void  mad_frame_mute(struct mad_frame *) = 0;
+  virtual int   mad_frame_decode(struct mad_frame *, struct mad_stream *) = 0;
+  virtual void  mad_stream_init(struct mad_stream *) = 0;
+  virtual void  mad_stream_finish(struct mad_stream *) = 0;
+  virtual void  mad_stream_buffer(struct mad_stream *, unsigned char const *, unsigned long) = 0;
+  virtual int   mad_stream_sync(struct mad_stream *) = 0;
+  virtual char const* mad_stream_errorstr(struct mad_stream const *) = 0;
+  virtual void  mad_synth_init(struct mad_synth *) = 0;
+  virtual void  mad_synth_frame(struct mad_synth *, struct mad_frame const *) = 0;
+  virtual void  mad_synth_mute(struct mad_synth *) = 0;
+  virtual void  mad_timer_add(mad_timer_t *, mad_timer_t) = 0;
+  virtual mad_timer_t Get_mad_timer_zero() = 0;
 };
 
 class DllMadCodec : public DllDynamic, DllMadCodecInterface
 {
   DECLARE_DLL_WRAPPER(DllMadCodec, DLL_PATH_MAD_CODEC)
-  DEFINE_METHOD2(IAudioDecoder*, CreateAudioDecoder, (unsigned int p1, IAudioOutput **p2))
+  DEFINE_METHOD1(void,  mad_frame_init,     (struct mad_frame *p1))
+  DEFINE_METHOD1(void,  mad_frame_finish,   (struct mad_frame *p1))
+  DEFINE_METHOD1(void,  mad_frame_mute,     (struct mad_frame *p1))
+  DEFINE_METHOD2(int,   mad_frame_decode,   (struct mad_frame *p1, struct mad_stream *p2))
+  DEFINE_METHOD1(void,  mad_stream_init,    (struct mad_stream *p1))
+  DEFINE_METHOD1(void,  mad_stream_finish,  (struct mad_stream *p1))
+  DEFINE_METHOD3(void,  mad_stream_buffer,  (struct mad_stream *p1, unsigned char const *p2, unsigned long p3))
+  DEFINE_METHOD1(int,   mad_stream_sync,    (struct mad_stream *p1))
+  DEFINE_METHOD1(char const*, mad_stream_errorstr, (struct mad_stream const *p1))
+  DEFINE_METHOD1(void,  mad_synth_init,     (struct mad_synth *p1))
+  DEFINE_METHOD2(void,  mad_synth_frame,    (struct mad_synth *p1, struct mad_frame const *p2))
+  DEFINE_METHOD1(void,  mad_synth_mute,     (struct mad_synth *p1))
+  DEFINE_METHOD2(void,  mad_timer_add,      (mad_timer_t *p1, mad_timer_t p2))
+  DEFINE_GLOBAL(mad_timer_t, mad_timer_zero)
   BEGIN_METHOD_RESOLVE()
-    RESOLVE_METHOD(CreateAudioDecoder)
+    RESOLVE_METHOD(mad_frame_init)
+    RESOLVE_METHOD(mad_frame_finish)
+    RESOLVE_METHOD(mad_frame_mute)
+    RESOLVE_METHOD(mad_frame_decode)
+    RESOLVE_METHOD(mad_stream_init)
+    RESOLVE_METHOD(mad_stream_finish)
+    RESOLVE_METHOD(mad_stream_buffer)
+    RESOLVE_METHOD(mad_stream_sync)
+    RESOLVE_METHOD(mad_stream_errorstr)
+    RESOLVE_METHOD(mad_synth_init)
+    RESOLVE_METHOD(mad_synth_frame)
+    RESOLVE_METHOD(mad_synth_mute)
+    RESOLVE_METHOD(mad_timer_add)
+    RESOLVE_METHOD(mad_timer_zero)
   END_METHOD_RESOLVE()
 };
+
