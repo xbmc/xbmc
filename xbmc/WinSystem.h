0b0988cd4f6fc054e119b99bc4dd3c4be6051634
jmarshallnz <jmarshallnz@svn>
2009-09-02 02:48:08 +0000

    added: Resolution.h to remove the WinSystem.h includes from GraphicContext.h
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@22604 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/WinSystem.h
+++ xbmc/WinSystem.h
@@ -23,78 +23,7 @@
 #define WINDOW_SYSTEM_BASE_H
 
 #include "WinEvents.h"
-#include "StdString.h"
-
-enum RESOLUTION {
-  RES_INVALID = -1,
-  RES_HDTV_1080i = 0,
-  RES_HDTV_720p = 1,
-  RES_HDTV_480p_4x3 = 2,
-  RES_HDTV_480p_16x9 = 3,
-  RES_NTSC_4x3 = 4,
-  RES_NTSC_16x9 = 5,
-  RES_PAL_4x3 = 6,
-  RES_PAL_16x9 = 7,
-  RES_PAL60_4x3 = 8,
-  RES_PAL60_16x9 = 9,
-  RES_AUTORES = 10,
-  RES_WINDOW = 11,
-  RES_DESKTOP = 12,
-  RES_CUSTOM = 13
-};
-
-enum VSYNC {
-  VSYNC_DISABLED = 0,
-  VSYNC_VIDEO = 1,
-  VSYNC_ALWAYS = 2,
-  VSYNC_DRIVER = 3
-};
-
-struct OVERSCAN
-{
-  int left;
-  int top;
-  int right;
-  int bottom;
-public:
-  OVERSCAN() {
-    left = top = right = bottom = 0;
-  }
-  OVERSCAN(const OVERSCAN& os) {
-    left = os.left; top = os.top;
-    right = os.right; bottom = os.bottom;
-  }
-};
-
-struct RESOLUTION_INFO
-{
-  OVERSCAN Overscan;
-  bool bFullScreen;
-  int iScreen;
-  int iWidth;
-  int iHeight;
-  int iSubtitles;
-  DWORD dwFlags;
-  float fPixelRatio;
-  float fRefreshRate;
-  CStdString strMode;
-  CStdString strOutput;
-  CStdString strId;
-  public:
-  RESOLUTION_INFO() {
-    bFullScreen = false;
-    iScreen = iWidth = iHeight = iSubtitles = dwFlags = 0;
-    fPixelRatio = fRefreshRate = 0.f;
-  }
-  RESOLUTION_INFO(const RESOLUTION_INFO& res) {
-    Overscan = res.Overscan; bFullScreen = res.bFullScreen;
-    iScreen = res.iScreen; iWidth = res.iWidth; iHeight = res.iHeight;
-    iSubtitles = res.iSubtitles; dwFlags = res.dwFlags;
-    fPixelRatio = res.fPixelRatio; fRefreshRate = res.fRefreshRate;
-    strMode = res.strMode; strOutput = res.strOutput; strId = res.strId;
-  }
-
-};
+#include "Resolution.h"
 
 typedef enum _WindowSystemType
 {
