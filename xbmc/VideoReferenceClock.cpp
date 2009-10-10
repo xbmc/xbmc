7c39f46fb54ec306b495b91972fdb8dba596c532
jmarshallnz <jmarshallnz@svn>
2009-10-10 02:51:37 +0000

    fixed: unnecessary, wrong, and silly change in r23570
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@23571 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoReferenceClock.cpp
+++ xbmc/VideoReferenceClock.cpp
@@ -480,7 +480,7 @@ void CVideoReferenceClock::RunD3D()
     if ((RasterStatus.InVBlank && LastLine > 0) || (RasterStatus.ScanLine < LastLine))
     {
       //calculate how many vblanks happened
-      int64_t Now = CurrentHostCounter();
+      Now = CurrentHostCounter();
       VBlankTime = (double)(Now - LastVBlankTime) / (double)m_SystemFrequency;
       NrVBlanks = MathUtils::round_int(VBlankTime * (double)m_RefreshRate);
 
