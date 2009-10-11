bb5054f3c9bf9f4bb8ea209cc69cc841a9339cec
jmarshallnz <jmarshallnz@svn>
2009-10-11 07:09:00 +0000

    fixed: Ticket #7253 - subtitles fail to come on if not already cached when using the "t" key on keyboard.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@23596 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowFullScreen.cpp
+++ xbmc/GUIWindowFullScreen.cpp
@@ -246,7 +246,13 @@ bool CGUIWindowFullScreen::OnAction(const CAction &action)
 
   case ACTION_SHOW_SUBTITLES:
     {
-      g_application.m_pPlayer->SetSubtitleVisible(!g_application.m_pPlayer->GetSubtitleVisible());
+      g_stSettings.m_currentVideoSettings.m_SubtitleOn = !g_stSettings.m_currentVideoSettings.m_SubtitleOn;
+      g_application.m_pPlayer->SetSubtitleVisible(g_stSettings.m_currentVideoSettings.m_SubtitleOn);
+      if (!g_stSettings.m_currentVideoSettings.m_SubtitleCached && g_stSettings.m_currentVideoSettings.m_SubtitleOn)
+      {
+        g_application.Restart(true); // cache subtitles
+        Close();
+      }
     }
     return true;
     break;
