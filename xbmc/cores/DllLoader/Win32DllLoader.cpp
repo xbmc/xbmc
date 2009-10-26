3752df5bd758697a4520f0642086a6f21f7f0585
jmarshallnz <jmarshallnz@svn>
2009-10-26 22:39:27 +0000

    added: implementations of _findfirst64i32() and _findnext64i32(), altered how _findfirst() and _findnext() return handles and ensured those handles were free'd in _findclose().
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24047 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/Win32DllLoader.cpp
+++ xbmc/cores/DllLoader/Win32DllLoader.cpp
@@ -41,6 +41,8 @@ Export win32_exports[] =
 {
   // kernel32
   { "FindFirstFileA",                               -1, (void*)dllFindFirstFileA,                               NULL },
+  { "FindNextFileA",                                -1, (void*)dllFindNextFileA,                                NULL },
+  { "FindClose",                                    -1, (void*)dllFindClose,                                    NULL },
   { "GetFileAttributesA",                           -1, (void*)dllGetFileAttributesA,                        NULL },
   { "LoadLibraryA",                                 -1, (void*)dllLoadLibraryA,                              (void*)track_LoadLibraryA },
   { "FreeLibrary",                                  -1, (void*)dllFreeLibrary,                               (void*)track_FreeLibrary },
@@ -79,6 +81,8 @@ Export win32_exports[] =
   { "_findclose",                 -1, (void*)dll_findclose,                 NULL },
   { "_findfirst",                 -1, (void*)dll_findfirst,                 NULL },
   { "_findnext",                  -1, (void*)dll_findnext,                  NULL },
+  { "_findfirst64i32",            -1, (void*)dll_findfirst64i32,            NULL },
+  { "_findnext64i32",             -1, (void*)dll_findnext64i32,             NULL },
   { "fclose",                     -1, (void*)dll_fclose,                    (void*)track_fclose},
   { "feof",                       -1, (void*)dll_feof,                      NULL },
   { "fgets",                      -1, (void*)dll_fgets,                     NULL },
