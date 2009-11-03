8df33ad681d2500e15f79406595e9c791260cf32
spiff_ <spiff_@svn>
2009-11-03 15:49:55 +0000

    changed: update version string
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24234 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.h
+++ xbmc/utils/GUIInfoManager.h
@@ -418,7 +418,7 @@ class CDateTime;
 #define CONTROL_HAS_FOCUS           30000
 #define BUTTON_SCROLLER_HAS_ICON    30001
 
-#define VERSION_STRING "pre-9.10"
+#define VERSION_STRING "9.11 alpha 1"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
