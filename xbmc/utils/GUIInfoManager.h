c5c06c0147441e4ea31970d8f83e9a4bd031b202
althekiller <althekiller@svn>
2009-11-05 16:15:08 +0000

    fixed: CDDB lookup.  Our version string must not include spaces, it is used in the HTTP user agent. (Thanks to vdrfan) added: Comment about the above to avoid this in the future.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24306 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.h
+++ xbmc/utils/GUIInfoManager.h
@@ -418,7 +418,9 @@ class CDateTime;
 #define CONTROL_HAS_FOCUS           30000
 #define BUTTON_SCROLLER_HAS_ICON    30001
 
-#define VERSION_STRING "9.11 alpha 1"
+// Version string MUST NOT contain spaces.  It is used
+// in the HTTP request user agent.
+#define VERSION_STRING "9.11-alpha1"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
