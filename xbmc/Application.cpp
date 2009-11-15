ddbac2d7c8bc6e61fc4a288663d4fbf321d70584
elupus <elupus@svn>
2009-11-15 23:41:08 +0000

    fixed: after playing a livetv stream xbmc would stop storing video settings
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24648 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4091,10 +4091,6 @@ void CApplication::SaveFileState()
 
 void CApplication::UpdateFileState()
 {
-  // No resume for livetv
-  if (m_progressTrackingItem->IsLiveTV())
-    return;
-
   // Did the file change?
   if (m_progressTrackingItem->m_strPath != "" && m_progressTrackingItem->m_strPath != CurrentFile())
   {
