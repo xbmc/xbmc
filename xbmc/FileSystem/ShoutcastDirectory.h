78e3b1cf3e59008b040433c7402c07407c16e672
topfs2 <topfs2@svn>
2009-11-16 20:05:31 +0000

    [Fixed] Both shoutcast and lastfm also need to assume directory exists
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24675 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/ShoutcastDirectory.h
+++ xbmc/FileSystem/ShoutcastDirectory.h
@@ -38,6 +38,7 @@ public:
   virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
   virtual bool IsAllowed(const CStdString &strFile) const { return true; };
   virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_ALWAYS; };
+  virtual bool Exists(const char* strPath) { return true; }
   bool ParseGenres(TiXmlElement *root, CFileItemList &items, CURL &url);
   bool ParseStations(TiXmlElement *root, CFileItemList &items, CURL &url);
 protected:
