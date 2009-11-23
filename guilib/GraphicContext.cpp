4c83327f64a2ef0b68845470399a7b258a23888f
alwinus <alwinus@svn>
2009-11-23 01:08:40 +0000

    merged: changes from trunk. revs: r24907:r24917
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/pvr-testing/XBMC@24919 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GraphicContext.cpp
+++ guilib/GraphicContext.cpp
@@ -707,17 +707,19 @@ bool CGraphicContext::IsFullScreenRoot () const
 bool CGraphicContext::ToggleFullScreenRoot ()
 {
   RESOLUTION newRes;
-  
+  RESOLUTION uiRes;  ///< resolution to save - not necessarily the same as the one we switch to (e.g. during video playback)
+
   if (m_bFullScreenRoot)
   {
-    newRes = RES_WINDOW;
+    newRes = uiRes = RES_WINDOW;
   }
   else
   {
     if (g_guiSettings.m_LookAndFeelResolution > RES_DESKTOP)
       newRes = g_guiSettings.m_LookAndFeelResolution;
     else
-      newRes = RES_DESKTOP;      
+      newRes = RES_DESKTOP;
+    uiRes = newRes;
 
 #if defined(HAS_VIDEO_PLAYBACK)
     if (g_graphicsContext.IsFullScreenVideo() || g_graphicsContext.IsCalibrating())
@@ -728,13 +730,12 @@ bool CGraphicContext::ToggleFullScreenRoot ()
       m_bFullScreenRoot = false;
     }
 #endif
-
   }
 
   SetVideoResolution(newRes);
-  g_guiSettings.SetResolution(newRes);
-  
-  return  m_bFullScreenRoot;
+  g_guiSettings.SetResolution(uiRes);
+
+  return m_bFullScreenRoot;
 }
 
 void CGraphicContext::SetMediaDir(const CStdString &strMediaDir)
