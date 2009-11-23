536e6e55275da76bb82a1f609c6f356e4f8e9638
tiben20 <tiben20@svn>
2009-11-23 18:59:06 +0000

    Another step into getting evr working.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/dsplayer@24938 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DSPlayer/Filters/EVRAllocatorPresenter.cpp
+++ xbmc/cores/DSPlayer/Filters/EVRAllocatorPresenter.cpp
@@ -2,8 +2,9 @@
 #include "MacrovisionKicker.h"
 #include "utils/log.h"
 #include "dshowutil/dshowutil.h"
-
+#include <mfapi.h>
 #include "vmr9.h"
+#include "IPinHook.h"
 class COuterEVR
 	: public CUnknown
 	//, public IVMRMixerBitmap9
@@ -228,11 +229,51 @@ m_refCount(1),
 m_D3D(d3d),
 m_D3DDev(d3dd)
 {
-  HRESULT hrr;
-  if (FAILED(DXVA2CreateDirect3DDeviceManager9(&m_iResetToken,&m_pDeviceManager)))
+  m_nRenderState = Shutdown;
+	HMODULE		hLib;
+  // Load EVR specifics DLLs
+	hLib = LoadLibrary ("dxva2.dll");
+	pfDXVA2CreateDirect3DDeviceManager9	= hLib ? (PTR_DXVA2CreateDirect3DDeviceManager9) GetProcAddress (hLib, "DXVA2CreateDirect3DDeviceManager9") : NULL;
+	
+	// Load EVR functions
+	hLib = LoadLibrary ("evr.dll");
+	pfMFCreateDXSurfaceBuffer			= hLib ? (PTR_MFCreateDXSurfaceBuffer)			GetProcAddress (hLib, "MFCreateDXSurfaceBuffer") : NULL;
+	pfMFCreateVideoSampleFromSurface	= hLib ? (PTR_MFCreateVideoSampleFromSurface)	GetProcAddress (hLib, "MFCreateVideoSampleFromSurface") : NULL;
+	pfMFCreateVideoMediaType			= hLib ? (PTR_MFCreateVideoMediaType)			GetProcAddress (hLib, "MFCreateVideoMediaType") : NULL;
+
+	if (!pfDXVA2CreateDirect3DDeviceManager9 || !pfMFCreateDXSurfaceBuffer || !pfMFCreateVideoSampleFromSurface || !pfMFCreateVideoMediaType)
+	{
+		if (!pfDXVA2CreateDirect3DDeviceManager9)
+			_Error += L"Could not find DXVA2CreateDirect3DDeviceManager9 (dxva2.dll)\n";
+		if (!pfMFCreateDXSurfaceBuffer)
+			_Error += L"Could not find MFCreateDXSurfaceBuffer (evr.dll)\n";
+		if (!pfMFCreateVideoSampleFromSurface)
+			_Error += L"Could not find MFCreateVideoSampleFromSurface (evr.dll)\n";
+		if (!pfMFCreateVideoMediaType)
+			_Error += L"Could not find MFCreateVideoMediaType (evr.dll)\n";
+		hr = E_FAIL;
+		return;
+	}
+
+	// Load Vista specifics DLLs
+	hLib = LoadLibrary ("AVRT.dll");
+	pfAvSetMmThreadCharacteristicsW		= hLib ? (PTR_AvSetMmThreadCharacteristicsW)	GetProcAddress (hLib, "AvSetMmThreadCharacteristicsW") : NULL;
+	pfAvSetMmThreadPriority				= hLib ? (PTR_AvSetMmThreadPriority)			GetProcAddress (hLib, "AvSetMmThreadPriority") : NULL;
+	pfAvRevertMmThreadCharacteristics	= hLib ? (PTR_AvRevertMmThreadCharacteristics)	GetProcAddress (hLib, "AvRevertMmThreadCharacteristics") : NULL;
+
+  if (SUCCEEDED(pfDXVA2CreateDirect3DDeviceManager9(&m_iResetToken,&m_pDeviceManager)))
   {
-    CLog::Log(LOGERROR,"Failed to create DXVA2CreateDirect3DDeviceManager9");
-    m_pDeviceManager->ResetDevice(d3dd, m_iResetToken);
+    CLog::Log(LOGERROR,"Sucess to create DXVA2CreateDirect3DDeviceManager9");
+    hr = m_pDeviceManager->ResetDevice(d3dd, m_iResetToken);
+  }
+  CComPtr<IDirectXVideoDecoderService>	pDecoderService;
+  HANDLE							hDevice;
+  hr = m_pDeviceManager->OpenDeviceHandle(&hDevice);
+  hr = m_pDeviceManager->GetVideoService(hDevice,__uuidof(IDirectXVideoDecoderService), (void**)&pDecoderService);
+  if (SUCCEEDED(hr))
+  {
+	HookDirectXVideoDecoderService (pDecoderService);
+    m_pDeviceManager->CloseDeviceHandle (hDevice);
   }
 }
 
@@ -242,26 +283,41 @@ CEVRAllocatorPresenter::~CEVRAllocatorPresenter()
 
 STDMETHODIMP CEVRAllocatorPresenter::CreateRenderer(IUnknown** ppRenderer)
 {
-CheckPointer(ppRenderer, E_POINTER);
-*ppRenderer = NULL;
-HRESULT	hr = E_FAIL;
-CMacrovisionKicker*		pMK  = new CMacrovisionKicker(NAME("CMacrovisionKicker"), NULL);
-		CComPtr<IUnknown>		pUnk = (IUnknown*)(INonDelegatingUnknown*)pMK;
-		COuterEVR *pOuterEVR = new COuterEVR(NAME("COuterEVR"), pUnk, hr, this);
-		m_pOuterEVR = pOuterEVR;
-		pMK->SetInner((IUnknown*)(INonDelegatingUnknown*)pOuterEVR);
-CComQIPtr<IBaseFilter> pBF = pUnk;
-  CComPtr<IMFVideoPresenter>		pVP;
-  CComPtr<IMFVideoRenderer>		pMFVR;
-  CComQIPtr<IMFGetService, &__uuidof(IMFGetService)> pMFGS = pBF;
-  hr = pMFGS->GetService (MR_VIDEO_RENDER_SERVICE, IID_IMFVideoRenderer, (void**)&pMFVR);
+  CheckPointer(ppRenderer, E_POINTER);
+  *ppRenderer = NULL;
+  HRESULT hr = E_FAIL;
+  do
+  {
+    CMacrovisionKicker* pMK  = new CMacrovisionKicker(NAME("CMacrovisionKicker"), NULL);
+    CComPtr<IUnknown> pUnk = (IUnknown*)(INonDelegatingUnknown*)pMK;
+    COuterEVR *pOuterEVR = new COuterEVR(NAME("COuterEVR"), pUnk, hr, this);
+    m_pOuterEVR = pOuterEVR;
+    pMK->SetInner((IUnknown*)(INonDelegatingUnknown*)pOuterEVR);
+    CComQIPtr<IBaseFilter> pBF = pUnk;
+    if (FAILED (hr))
+    {
+    CLog::Log(LOGERROR,"Create enchanced video renderer");
+    break;
+    }
+    CComPtr<IMFVideoPresenter>    pVP;
+    CComPtr<IMFVideoRenderer>    pMFVR;
+    CComQIPtr<IMFGetService, &__uuidof(IMFGetService)> pMFGS = pBF;
+    hr = pMFGS->GetService (MR_VIDEO_RENDER_SERVICE, IID_IMFVideoRenderer, (void**)&pMFVR);
+    if(SUCCEEDED(hr)) 
+       hr = QueryInterface (__uuidof(IMFVideoPresenter), (void**)&pVP);
+    if(SUCCEEDED(hr)) 
+      hr = pMFVR->InitializeRenderer (NULL, pVP);
+    //something related with no crash in vista
+	CComPtr<IPin>      pPin = DShowUtil::GetFirstPin(pBF);
+    CComQIPtr<IMemInputPin> pMemInputPin = pPin;    
+    m_fUseInternalTimer = HookNewSegmentAndReceive((IPinC*)(IPin*)pPin, (IMemInputPinC*)(IMemInputPin*)pMemInputPin);
+    if(FAILED(hr))
+    *ppRenderer = NULL;
+    else
+    *ppRenderer = pBF.Detach();
+  } while (0);
 
-		if(SUCCEEDED(hr)) hr = QueryInterface (__uuidof(IMFVideoPresenter), (void**)&pVP);
-		if(SUCCEEDED(hr)) hr = pMFVR->InitializeRenderer (NULL, pVP);
-if(FAILED(hr))
-			*ppRenderer = NULL;
-		else
-			*ppRenderer = pBF.Detach();
+  return hr;
 
 }
 
@@ -339,13 +395,13 @@ HRESULT hr = E_NOINTERFACE;
     AddRef();
     hr = S_OK;
   } 
-  /*else if( riid == IID_IQualProp) 
+  else if( riid == IID_IQualProp) 
   {
     *ppvObject = static_cast<IQualProp*>( this );
     AddRef();
     hr = S_OK;
   }
-  else if( riid == IID_IMFRateSupport) 
+  /*else if( riid == IID_IMFRateSupport) 
   {
     *ppvObject = static_cast<IMFRateSupport*>( this );
     AddRef();
@@ -384,9 +440,7 @@ HRESULT hr = E_NOINTERFACE;
     hr=E_NOINTERFACE;
   }
   if ( FAILED(hr) ) 
-  {
-    CLog::Log(LOGERROR,"QueryInterface failed" );
-  }
+    CLog::Log(LOGERROR,"Failed to query %s",DShowUtil::CStringFromGUID(riid).c_str());
   return hr;
 }
 
@@ -406,6 +460,67 @@ ULONG CEVRAllocatorPresenter::Release()
     return ret;
 }
 
+//IMFVideoPresenter
+HRESULT STDMETHODCALLTYPE CEVRAllocatorPresenter::ProcessMessage(MFVP_MESSAGE_TYPE eMessage, ULONG_PTR ulParam)
+{
+  HRESULT hr = S_OK;
+  switch (eMessage)
+  {
+  case MFVP_MESSAGE_BEGINSTREAMING :			// The EVR switched from stopped to paused. The presenter should allocate resources
+		//ResetStats();
+		TRACE_EVR ("EVR: MFVP_MESSAGE_BEGINSTREAMING\n");
+		break;
+
+	case MFVP_MESSAGE_CANCELSTEP :				// Cancels a frame step
+		TRACE_EVR ("EVR: MFVP_MESSAGE_CANCELSTEP\n");
+		//CompleteFrameStep (true);
+		break;
+
+	case MFVP_MESSAGE_ENDOFSTREAM :				// All input streams have ended. 
+		TRACE_EVR ("EVR: MFVP_MESSAGE_ENDOFSTREAM\n");
+		m_bPendingMediaFinished = true;
+		break;
+
+	case MFVP_MESSAGE_ENDSTREAMING :			// The EVR switched from running or paused to stopped. The presenter should free resources
+		TRACE_EVR ("EVR: MFVP_MESSAGE_ENDSTREAMING\n");
+		break;
+
+	case MFVP_MESSAGE_FLUSH :					// The presenter should discard any pending samples
+		SetEvent(m_hEvtFlush);
+		m_bEvtFlush = true;
+		TRACE_EVR ("EVR: MFVP_MESSAGE_FLUSH\n");
+		while (WaitForSingleObject(m_hEvtFlush, 1) == WAIT_OBJECT_0);
+		break;
+
+	case MFVP_MESSAGE_INVALIDATEMEDIATYPE :		// The mixer's output format has changed. The EVR will initiate format negotiation, as described previously
+		/*
+			1) The EVR sets the media type on the reference stream.
+			2) The EVR calls IMFVideoPresenter::ProcessMessage on the presenter with the MFVP_MESSAGE_INVALIDATEMEDIATYPE message.
+			3) The presenter sets the media type on the mixer's output stream.
+			4) The EVR sets the media type on the substreams.
+		*/
+		m_bPendingRenegotiate = true;
+		while (*((volatile bool *)&m_bPendingRenegotiate))
+			Sleep(1);
+		break;
+
+	case MFVP_MESSAGE_PROCESSINPUTNOTIFY :		// One input stream on the mixer has received a new sample
+//		GetImageFromMixer();
+		break;
+
+	case MFVP_MESSAGE_STEP :					// Requests a frame step.
+		TRACE_EVR ("EVR: MFVP_MESSAGE_STEP\n");
+		m_nStepCount = ulParam;
+		hr = S_OK;
+		break;
+
+	default :
+		ASSERT (FALSE);
+		break;
+	}
+  return hr;
+}
+
 HRESULT CEVRAllocatorPresenter::GetCurrentMediaType(IMFVideoMediaType** ppMediaType)
 {
   HRESULT hr = S_OK;
@@ -413,10 +528,10 @@ HRESULT CEVRAllocatorPresenter::GetCurrentMediaType(IMFVideoMediaType** ppMediaT
   if (ppMediaType == NULL)
     return E_POINTER;
 
-  /*if (m_pMediaType == NULL)
+  if (m_pMediaType == NULL)
   {
 	  CLog::Log(LOGERROR,"MediaType is NULL");
-  }*/
+  }
   hr = m_pMediaType->QueryInterface(__uuidof(IMFVideoMediaType), (void**)ppMediaType);
   if FAILED(hr)
     CLog::Log(LOGERROR,"Query interface failed in GetCurrentMediaType");
@@ -425,10 +540,167 @@ HRESULT CEVRAllocatorPresenter::GetCurrentMediaType(IMFVideoMediaType** ppMediaT
   return hr;
 }
 
-HRESULT STDMETHODCALLTYPE CEVRAllocatorPresenter::ProcessMessage( 
-  MFVP_MESSAGE_TYPE eMessage,
-  ULONG_PTR ulParam)
+// IMFTopologyServiceLookupClient        
+STDMETHODIMP CEVRAllocatorPresenter::InitServicePointers(/* [in] */ __in  IMFTopologyServiceLookup *pLookup)
 {
-	HRESULT hr = S_OK;
-	return hr;
+	HRESULT						hr;
+	DWORD						dwObjects = 1;
+
+	TRACE_EVR ("EVR: CEVRAllocatorPresenter::InitServicePointers\n");
+	hr = pLookup->LookupService (MF_SERVICE_LOOKUP_GLOBAL, 0, MR_VIDEO_MIXER_SERVICE,
+								  __uuidof (IMFTransform), (void**)&m_pMixer, &dwObjects);
+
+	hr = pLookup->LookupService (MF_SERVICE_LOOKUP_GLOBAL, 0, MR_VIDEO_RENDER_SERVICE,
+								  __uuidof (IMediaEventSink ), (void**)&m_pSink, &dwObjects);
+
+	hr = pLookup->LookupService (MF_SERVICE_LOOKUP_GLOBAL, 0, MR_VIDEO_RENDER_SERVICE,
+								  __uuidof (IMFClock ), (void**)&m_pClock, &dwObjects);
+
+
+	StartWorkerThreads();
+	return S_OK;
+}
+
+STDMETHODIMP CEVRAllocatorPresenter::ReleaseServicePointers()
+{
+	TRACE_EVR ("EVR: CEVRAllocatorPresenter::ReleaseServicePointers\n");
+	StopWorkerThreads();
+	m_pMixer	= NULL;
+	m_pSink		= NULL;
+	m_pClock	= NULL;
+	return S_OK;
+}
+
+// IMFVideoDeviceID
+STDMETHODIMP CEVRAllocatorPresenter::GetDeviceID(/* [out] */	__out  IID *pDeviceID)
+{
+	CheckPointer(pDeviceID, E_POINTER);
+	*pDeviceID = IID_IDirect3DDevice9;
+	return S_OK;
+}
+
+
+// IMFGetService
+STDMETHODIMP CEVRAllocatorPresenter::GetService (/* [in] */ __RPC__in REFGUID guidService,
+								/* [in] */ __RPC__in REFIID riid,
+								/* [iid_is][out] */ __RPC__deref_out_opt LPVOID *ppvObject)
+{
+	if (guidService == MR_VIDEO_RENDER_SERVICE)
+		return NonDelegatingQueryInterface (riid, ppvObject);
+	else if (guidService == MR_VIDEO_ACCELERATION_SERVICE)
+		return m_pDeviceManager->QueryInterface (__uuidof(IDirect3DDeviceManager9), (void**) ppvObject);
+
+	return E_NOINTERFACE;
+}
+
+
+void CEVRAllocatorPresenter::StartWorkerThreads()
+{
+	DWORD		dwThreadId;
+
+	if (m_nRenderState == Shutdown)
+	{
+		m_hEvtQuit		= CreateEvent (NULL, TRUE, FALSE, NULL);
+		m_hEvtFlush		= CreateEvent (NULL, TRUE, FALSE, NULL);
+
+		m_hThread		= ::CreateThread(NULL, 0, PresentThread, (LPVOID)this, 0, &dwThreadId);
+		SetThreadPriority(m_hThread, THREAD_PRIORITY_TIME_CRITICAL);
+		//m_hGetMixerThread = ::CreateThread(NULL, 0, GetMixerThreadStatic, (LPVOID)this, 0, &dwThreadId);
+		SetThreadPriority(m_hGetMixerThread, THREAD_PRIORITY_HIGHEST);
+
+		m_nRenderState		= Stopped;
+		TRACE_EVR ("EVR: Worker threads started...\n");
+	}
+}
+
+DWORD WINAPI CEVRAllocatorPresenter::PresentThread(LPVOID lpParam)
+{
+	CEVRAllocatorPresenter*		pThis = (CEVRAllocatorPresenter*) lpParam;
+	pThis->RenderThread();
+	return 0;
+}
+void CEVRAllocatorPresenter::RenderThread()
+{
+  HANDLE				hAvrt;
+	DWORD				dwTaskIndex	= 0;
+	HANDLE				hEvts[]		= { m_hEvtQuit, m_hEvtFlush};
+	bool				bQuit		= false;
+    TIMECAPS			tc;
+	DWORD				dwResolution;
+	MFTIME				nsSampleTime;
+	LONGLONG			llClockTime;
+	DWORD				dwUser = 0;
+	DWORD				dwObject;
+
+	
+	// Tell Vista Multimedia Class Scheduler we are a playback thretad (increase priority)
+	if (pfAvSetMmThreadCharacteristicsW)	hAvrt = pfAvSetMmThreadCharacteristicsW (L"Playback", &dwTaskIndex);
+	if (pfAvSetMmThreadPriority)			pfAvSetMmThreadPriority (hAvrt, AVRT_PRIORITY_HIGH /*AVRT_PRIORITY_CRITICAL*/);
+
+    timeGetDevCaps(&tc, sizeof(TIMECAPS));
+    dwResolution = min(max(tc.wPeriodMin, 0), tc.wPeriodMax);
+    dwUser		= timeBeginPeriod(dwResolution);
+
+	int NextSleepTime = 1;
+  while (!bQuit)
+  {
+    NextSleepTime = 1;
+		dwObject = WaitForMultipleObjects (countof(hEvts), hEvts, FALSE, max(NextSleepTime < 0 ? 1 : NextSleepTime, 0));
+		switch (dwObject)
+		{
+		case WAIT_OBJECT_0 :
+			bQuit = true;
+			break;
+		case WAIT_OBJECT_0 + 1 :
+			// Flush pending samples!
+			//FlushSamples();
+			m_bEvtFlush = false;
+			ResetEvent(m_hEvtFlush);
+			TRACE_EVR ("EVR: Flush done!\n");
+			break;
+		case WAIT_TIMEOUT :
+
+			if (m_bPendingRenegotiate)
+			{
+				//FlushSamples();
+				//RenegotiateMediaType();
+				m_bPendingRenegotiate = false;
+			}
+			if (m_bPendingResetDevice)
+			{
+				m_bPendingResetDevice = false;
+				/*CAutoLock lock(this);
+				CAutoLock lock2(&m_ImageProcessingLock);
+				CAutoLock cRenderLock(&m_RenderLock);*/
+
+				
+				//RemoveAllSamples();
+
+				//CDX9AllocatorPresenter::ResetDevice();
+                CComPtr<IMFSample>		pMFSample;
+				CComPtr<IDirect3DSurface9> m_pVideoSurface;
+				HRESULT hr = pfMFCreateVideoSampleFromSurface (m_pVideoSurface, &pMFSample);
+				/*for(int i = 0; i < m_nNbDXSurface; i++)
+				{
+					CComPtr<IMFSample>		pMFSample;
+					HRESULT hr = pfMFCreateVideoSampleFromSurface (m_pVideoSurface[i], &pMFSample);
+
+					if (SUCCEEDED (hr))
+					{
+						pMFSample->SetUINT32 (GUID_SURFACE_INDEX, i);
+						m_FreeSamples.AddTail (pMFSample);
+					}
+					ASSERT (SUCCEEDED (hr));
+				}*/
+
+			}
+			break;
+		}
+	}
+}
+
+HRESULT CEVRAllocatorPresenter::TRACE_EVR(const char *strTrace)
+{
+	CLog::Log(LOGNOTICE,"%s",strTrace);
+	return S_OK;
 }
--- xbmc/cores/DSPlayer/Filters/EVRAllocatorPresenter.h
+++ xbmc/cores/DSPlayer/Filters/EVRAllocatorPresenter.h
@@ -7,8 +7,25 @@
 #include <evr.h>
 #include <dxva2api.h>
 #include <atlcomcli.h>
+#include <streams.h>
 #include <d3d9.h>
 #include "idsrenderer.h"
+#include <avrt.h>
+// dxva.dll
+typedef HRESULT (__stdcall *PTR_DXVA2CreateDirect3DDeviceManager9)(UINT* pResetToken, IDirect3DDeviceManager9** ppDeviceManager);
+
+// mf.dll
+typedef HRESULT (__stdcall *PTR_MFCreatePresentationClock)(IMFPresentationClock** ppPresentationClock);
+
+// evr.dll
+typedef HRESULT (__stdcall *PTR_MFCreateDXSurfaceBuffer)(REFIID riid, IUnknown* punkSurface, BOOL fBottomUpWhenLinear, IMFMediaBuffer** ppBuffer);
+typedef HRESULT (__stdcall *PTR_MFCreateVideoSampleFromSurface)(IUnknown* pUnkSurface, IMFSample** ppSample);
+typedef HRESULT (__stdcall *PTR_MFCreateVideoMediaType)(const MFVIDEOFORMAT* pVideoFormat, IMFVideoMediaType** ppIVideoMediaType);
+
+// AVRT.dll
+typedef HANDLE  (__stdcall *PTR_AvSetMmThreadCharacteristicsW)(LPCWSTR TaskName, LPDWORD TaskIndex);
+typedef BOOL	(__stdcall *PTR_AvSetMmThreadPriority)(HANDLE AvrtHandle, AVRT_PRIORITY Priority);
+typedef BOOL	(__stdcall *PTR_AvRevertMmThreadCharacteristics)(HANDLE AvrtHandle);
 
 class COuterEVR;
 [uuid("7612B889-0929-4363-9BA3-580D735AA0F6")]
@@ -19,7 +36,8 @@ class CEVRAllocatorPresenter : public IDsRenderer,
                                public IMFGetService,
                                public IMFTopologyServiceLookupClient,
                                public IMFVideoDisplayControl,
-                               public IEVRTrustedVideoPlugin
+                               public IEVRTrustedVideoPlugin,
+							   public IQualProp
 {
 public:
   CEVRAllocatorPresenter(HRESULT& hr, HWND wnd, CStdString &_Error,IDirect3D9* d3d = NULL, IDirect3DDevice9* d3dd = NULL);
@@ -27,29 +45,37 @@ public:
 
   STDMETHODIMP CreateRenderer(IUnknown** ppRenderer);
 
-// IBaseFilter delegate
+  //IBaseFilter delegate
   bool GetState( DWORD dwMilliSecsTimeout, FILTER_STATE *State, HRESULT &_ReturnValue);
+
   // IUnknown
   virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid,void** ppvObject);
   virtual ULONG STDMETHODCALLTYPE AddRef();
   virtual ULONG STDMETHODCALLTYPE Release();
   STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void** ppv);
+
+  //IQualProp
+  virtual HRESULT STDMETHODCALLTYPE get_FramesDroppedInRenderer(int *pcFrames){return E_NOTIMPL;};
+  virtual HRESULT STDMETHODCALLTYPE get_FramesDrawn(int *pcFramesDrawn){return E_NOTIMPL;};
+  virtual HRESULT STDMETHODCALLTYPE get_AvgFrameRate(int *piAvgFrameRate){return E_NOTIMPL;};
+  virtual HRESULT STDMETHODCALLTYPE get_Jitter(int *iJitter){return E_NOTIMPL;};
+  virtual HRESULT STDMETHODCALLTYPE get_AvgSyncOffset(int *piAvg){return E_NOTIMPL;};
+  virtual HRESULT STDMETHODCALLTYPE get_DevSyncOffset(int *piDev){return E_NOTIMPL;};
   //IMFVideoPresenter
-	// IMFVideoPresenter
-	STDMETHODIMP	ProcessMessage(MFVP_MESSAGE_TYPE eMessage, ULONG_PTR ulParam);
-	STDMETHODIMP	GetCurrentMediaType(__deref_out  IMFVideoMediaType **ppMediaType);
+  STDMETHODIMP ProcessMessage(MFVP_MESSAGE_TYPE eMessage, ULONG_PTR ulParam);
+  STDMETHODIMP GetCurrentMediaType(__deref_out  IMFVideoMediaType **ppMediaType);
 
 	// IMFTopologyServiceLookupClient        
-	STDMETHODIMP	InitServicePointers(/* [in] */ __in  IMFTopologyServiceLookup *pLookup){return E_NOTIMPL;};
-	STDMETHODIMP	ReleaseServicePointers(){return E_NOTIMPL;};
+	STDMETHODIMP	InitServicePointers(/* [in] */ __in  IMFTopologyServiceLookup *pLookup);
+	STDMETHODIMP	ReleaseServicePointers();
 
 	// IMFVideoDeviceID
-	STDMETHODIMP	GetDeviceID(/* [out] */	__out  IID *pDeviceID){return E_NOTIMPL;};
+	STDMETHODIMP	GetDeviceID(/* [out] */	__out  IID *pDeviceID);
 
 	// IMFGetService
 	STDMETHODIMP	GetService (/* [in] */ __RPC__in REFGUID guidService,
 								/* [in] */ __RPC__in REFIID riid,
-								/* [iid_is][out] */ __RPC__deref_out_opt LPVOID *ppvObject){return E_NOTIMPL;};
+								/* [iid_is][out] */ __RPC__deref_out_opt LPVOID *ppvObject);
   // IMFClockStateSink
 	STDMETHODIMP	OnClockStart(/* [in] */ MFTIME hnsSystemTime, /* [in] */ LONGLONG llClockStartOffset){return E_NOTIMPL;};        
 	STDMETHODIMP	STDMETHODCALLTYPE OnClockStop(/* [in] */ MFTIME hnsSystemTime){return E_NOTIMPL;};
@@ -90,18 +116,65 @@ public:
 	STDMETHODIMP	UnlockDevice(HANDLE hDevice, BOOL fSaveState){return E_NOTIMPL;};
 	STDMETHODIMP	GetVideoService(HANDLE hDevice, REFIID riid, void **ppService){return E_NOTIMPL;};
 private:
+typedef enum
+{
+  Started = State_Running,
+  Stopped = State_Stopped,
+  Paused = State_Paused,
+  Shutdown = State_Running + 1
+} RENDER_STATE;
 
   long m_refCount;
+  bool m_fUseInternalTimer;
+  int                                       m_nStepCount;
+  HANDLE                                    m_hEvtQuit;			// Stop rendering thread event
+  bool                                      m_bEvtQuit;
+  HANDLE                                    m_hEvtFlush;		// Discard all buffers
+  bool                                      m_bEvtFlush;
+  HANDLE									m_hThread;
+  HANDLE									m_hGetMixerThread;
+  RENDER_STATE							m_nRenderState;
+
+  void									RenderThread();
+  static DWORD WINAPI						PresentThread(LPVOID lpParam);
+
+  void									GetMixerThread();
+  static DWORD WINAPI						GetMixerThreadStatic(LPVOID lpParam);
+  bool                                      m_bPendingRenegotiate;
+  bool                                      m_bPendingMediaFinished;
+  bool                                      m_bPendingResetDevice;
+
+
   COuterEVR *m_pOuterEVR;
+// === Media type negociation functions
+  HRESULT									RenegotiateMediaType();
+  HRESULT									IsMediaTypeSupported(IMFMediaType* pMixerType);
+  HRESULT									CreateProposedOutputType(IMFMediaType* pMixerType, IMFMediaType** pType);
+  HRESULT									SetMediaType(IMFMediaType* pType);
 
-
+// === Functions pointers on Vista / .Net3 specifics library
+  PTR_DXVA2CreateDirect3DDeviceManager9	pfDXVA2CreateDirect3DDeviceManager9;
+  PTR_MFCreateDXSurfaceBuffer				pfMFCreateDXSurfaceBuffer;
+  PTR_MFCreateVideoSampleFromSurface		pfMFCreateVideoSampleFromSurface;
+  PTR_MFCreateVideoMediaType				pfMFCreateVideoMediaType;
+//vista specific										
+  PTR_AvSetMmThreadCharacteristicsW		pfAvSetMmThreadCharacteristicsW;
+  PTR_AvSetMmThreadPriority				pfAvSetMmThreadPriority;
+  PTR_AvRevertMmThreadCharacteristics		pfAvRevertMmThreadCharacteristics;
 protected:
   CComPtr<IDirect3D9> m_D3D;
   CComPtr<IDirect3DDevice9> m_D3DDev;
   CComPtr<IDirect3DDeviceManager9> m_pDeviceManager;
   CComPtr<IMFMediaType> m_pMediaType;
-
+  CComPtr<IMFTransform> m_pMixer;
+  CComPtr<IMediaEventSink> m_pSink;
+  CComPtr<IMFClock> m_pClock;
   UINT m_iResetToken;
+
+  //msg handlings
+  void StartWorkerThreads();
+  void StopWorkerThreads(){};
+  HRESULT TRACE_EVR(const char* strTrace);
 };
 
 #endif
