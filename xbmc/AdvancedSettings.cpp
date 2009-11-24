55857043bf1153ee435a6155170d021395fa0ed1
althekiller <althekiller@svn>
2009-11-24 00:24:09 +0000

    fixed: Resolution added as part of multi-ep tv episode (more proper fix)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24955 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/AdvancedSettings.cpp
+++ xbmc/AdvancedSettings.cpp
@@ -158,7 +158,7 @@ void CAdvancedSettings::Initialize()
   // foo.103*, 103 foo
   m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[\\\\/\\._ -]([0-9]+)([0-9][0-9])([\\._ -][^\\\\/]*)$"));
 
-  m_tvshowMultiPartStackRegExp = "^[-EeXx]+([0-9]+[^(480p|480i|576p|576i|720p|720i|1080p|1080i)])";
+  m_tvshowMultiPartStackRegExp = "^[-EeXx]+([0-9]+[^ip])";
 
   m_remoteRepeat = 480;
   m_controllerDeadzone = 0.2f;
