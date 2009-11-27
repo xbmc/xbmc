411889ff07110370daabc02b97adf468185f9903
tiben20 <tiben20@svn>
2009-11-27 02:48:45 +0000

    -Forgot in last commit -Also removed some unused projects linking
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/dsplayer@25085 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DSPlayer/FGFilter.cpp
+++ xbmc/cores/DSPlayer/FGFilter.cpp
@@ -429,9 +429,7 @@ HRESULT CFGFilterVideoRenderer::Create(IBaseFilter** ppBF, CInterfaceList<IUnkno
   if (m_clsid == __uuidof(CDX9AllocatorPresenter))
     pCAP = new CDX9AllocatorPresenter(hr,m_hWnd,__err,g_Windowing.Get3DObject(),g_Windowing.Get3DDevice());
   if (m_clsid == __uuidof(CEVRAllocatorPresenter))
-  {
     pCAP = new CEVRAllocatorPresenter(hr,m_hWnd,__err,g_Windowing.Get3DObject(),g_Windowing.Get3DDevice());
-  }
 
   if(pCAP == NULL)
     return E_FAIL;
