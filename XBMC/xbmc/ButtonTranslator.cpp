3b556eb538ce5127091d7d9433bc94c380c30765
alcoheca <alcoheca@svn>
2009-11-27 03:11:04 +0000

    fixed: windows build (except for one issue) added: addonbrowser identifier to CButtonTranslator
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/pvr-testing@25086 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- XBMC/xbmc/ButtonTranslator.cpp
+++ XBMC/xbmc/ButtonTranslator.cpp
@@ -871,6 +871,7 @@ int CButtonTranslator::TranslateWindowString(const char *szWindow)
   else if (strWindow.Equals("pictures")) windowID = WINDOW_PICTURES;
   else if (strWindow.Equals("files") || strWindow.Equals("filemanager")) windowID = WINDOW_FILES;
   else if (strWindow.Equals("settings")) windowID = WINDOW_SETTINGS_MENU;
+  else if (strWindow.Equals("addonbrowser")) windowID = WINDOW_DIALOG_ADDON_BROWSER;
   else if (strWindow.Equals("music")) windowID = WINDOW_MUSIC;
   else if (strWindow.Equals("musicfiles")) windowID = WINDOW_MUSIC_FILES;
   else if (strWindow.Equals("musiclibrary")) windowID = WINDOW_MUSIC_NAV;
