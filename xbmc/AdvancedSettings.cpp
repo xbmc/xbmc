dcbf67bfc7a9c05b7a7d165604a5fcceeebb0d31
althekiller <althekiller@svn>
2009-12-03 23:22:33 +0000

    fixed: Allow stupidly large stacks assuming they use the tagged case (eg CD1 - CD23948209384209384)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/9.11_Camelot@25266 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/AdvancedSettings.cpp
+++ xbmc/AdvancedSettings.cpp
@@ -139,7 +139,7 @@ void CAdvancedSettings::Initialize()
   m_moviesExcludeFromScanRegExps.push_back("[-._ ]sample");
   m_tvshowExcludeFromScanRegExps.push_back("[-._ ]sample[-._ ]");
 
-  m_videoStackRegExps.push_back("(.*?)([ _.-]*(?:cd|dvd|p(?:(?:ar)t)|dis[ck])[ _.-]*[1-4a-d]+)(.*?)(\\.[^.]+)$");
+  m_videoStackRegExps.push_back("(.*?)([ _.-]*(?:cd|dvd|p(?:(?:ar)t)|dis[ck])[ _.-]*[0-9a-d]+)(.*?)(\\.[^.]+)$");
   m_videoStackRegExps.push_back("(.*?)([ ._-]*[a-d])(.*?)(\\.[^.]+)$");
   // This one is a bit too greedy to enable by default.  It will stack sequels
   // in a flat dir structure, but is perfectly safe in a dir-per-vid one.
