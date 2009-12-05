a21e3555efbb88102f2223e7b079ccc8268b0e74
arnova <arnova@svn>
2009-12-05 00:23:29 +0000

    [XBOX] revert: r22032 & r25272 for now to fix ticket #7964
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/xbox@25288 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlaySpu.h
+++ xbmc/cores/dvdplayer/DVDCodecs/Overlay/DVDOverlaySpu.h
@@ -46,6 +46,7 @@ public:
     memset(result, 0, sizeof(result));
     memset(alpha, 0, sizeof(alpha));
     memset(color, 0, sizeof(color));
+    memset(stats, 0, sizeof(stats));
     memset(highlight_alpha, 0, sizeof(highlight_alpha));
     memset(highlight_color, 0, sizeof(highlight_color));
   }
@@ -57,6 +58,8 @@ public:
   int width;
   int height;
 
+  int stats[4]; // nr of pixels for each color found in the overlay.
+  
   // the four contrasts, [0] = background
   int alpha[4];
   bool bHasAlpha;
@@ -76,4 +79,13 @@ public:
   // should be used on the highlighted areas
   int highlight_color[4][3];
   int highlight_alpha[4];
+  
+  bool CanDisplayWithAlphas(int a[4]) 
+  { 
+    return( 
+      a[0] * stats[0] > 0 || 
+      a[1] * stats[1] > 0 || 
+      a[2] * stats[2] > 0 || 
+      a[3] * stats[3] > 0); 
+  } 
 };
