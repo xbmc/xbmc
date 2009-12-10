862534cc1bcac1e8ee0ccc8c72aa2511cd46ae80
MaestroDD <MaestroDD@svn>
2009-12-10 13:42:04 +0000

    fixed: marking an item as watched jumps to beginning of list if "Hide Watched" is enabled
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@25520 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoBase.cpp
+++ xbmc/GUIWindowVideoBase.cpp
@@ -1233,11 +1233,15 @@ bool CGUIWindowVideoBase::OnContextButton(int itemNumber, CONTEXT_BUTTON button)
     OnRenameItem(itemNumber);
     return true;
   case CONTEXT_BUTTON_MARK_WATCHED:
-    MarkWatched(item,true);
-    CUtil::DeleteVideoDatabaseDirectoryCache();
-    Update(m_vecItems->m_strPath);
-    return true;
+    {
+      int itemNumber = m_viewControl.GetSelectedItem();
+      MarkWatched(item,true);
+      m_viewControl.SetSelectedItem(itemNumber - 1);
 
+      CUtil::DeleteVideoDatabaseDirectoryCache();
+      Update(m_vecItems->m_strPath);
+      return true;
+    }
   case CONTEXT_BUTTON_MARK_UNWATCHED:
     MarkWatched(item,false);
     CUtil::DeleteVideoDatabaseDirectoryCache();
