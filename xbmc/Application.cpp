8d4dda8c4e327a9b3340a17eb0bfe13f1945063c
wiso <wiso@svn>
2009-12-14 18:20:12 +0000

    [WIN32] build libshout within the XBMC main project.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@25685 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -286,7 +286,6 @@ using namespace DBUSSERVER;
   #endif
   #pragma comment (lib,"../../xbmc/lib/libGoAhead/goahead_win32d.lib") // SECTIONNAME=LIBHTTP
   #pragma comment (lib,"../../xbmc/lib/sqLite/libSQLite3_win32d.lib")
-  #pragma comment (lib,"../../xbmc/lib/libshout/libshout_win32d.lib" )
   #pragma comment (lib,"../../xbmc/lib/libcdio/libcdio_win32d.lib" )
   #pragma comment (lib,"../../xbmc/lib/libiconv/libiconvd.lib")
   #pragma comment (lib,"../../xbmc/lib/libfribidi/libfribidid.lib")
@@ -300,7 +299,6 @@ using namespace DBUSSERVER;
   #endif
   #pragma comment (lib,"../../xbmc/lib/libGoAhead/goahead_win32.lib")
   #pragma comment (lib,"../../xbmc/lib/sqLite/libSQLite3_win32.lib")
-  #pragma comment (lib,"../../xbmc/lib/libshout/libshout_win32.lib" )
   #pragma comment (lib,"../../xbmc/lib/libcdio/libcdio_win32.lib" )
   #pragma comment (lib,"../../xbmc/lib/libiconv/libiconv.lib")
   #pragma comment (lib,"../../xbmc/lib/libfribidi/libfribidi.lib")
