7f99423542690e58712853c3cdaa640f0fce936c
jmarshallnz <jmarshallnz@svn>
2009-12-22 21:45:57 +0000

    fixed: Music/Video overlays would disappear during the Dim screensaver.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@25984 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIWindowManager.cpp
+++ guilib/GUIWindowManager.cpp
@@ -803,6 +803,9 @@ void CGUIWindowManager::UnloadNotOnDemandWindows()
 
 bool CGUIWindowManager::IsOverlayAllowed() const
 {
+  if (GetActiveWindow() == WINDOW_FULLSCREEN_VIDEO ||
+      GetActiveWindow() == WINDOW_SCREENSAVER)
+    return false;
   return m_bShowOverlay;
 }
 
