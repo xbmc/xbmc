66ed59c3b6a4c7e3792262e336fc0807b9e3bd0e
davilla <davilla@svn>
2009-12-29 16:28:33 +0000

    Ticket #8318, fixed possible memleak. Thanks ettl.martin
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@26167 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -1924,7 +1924,7 @@ void CUtil::TakeScreenshot(const CStdString &filename, bool sync)
   for (int y = 0; y < height; y++)
     memcpy(outpixels + y * stride, pixels + (height - y - 1) * stride, stride);
 
-  delete pixels; 
+  delete [] pixels; 
 
 #else
   //nothing to take a screenshot from
@@ -1953,7 +1953,7 @@ void CUtil::TakeScreenshot(const CStdString &filename, bool sync)
     if (!CPicture::CreateThumbnailFromSurface(outpixels, width, height, stride, filename))
       CLog::Log(LOGERROR, "Unable to write screenshot %s", filename.c_str());
 
-    delete outpixels;
+    delete [] outpixels;
   }
   else
   {
