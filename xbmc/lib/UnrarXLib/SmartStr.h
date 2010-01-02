3f6c28631e579a5a770003faa4e36b30249b8984
malloc64 <malloc64@svn>
2010-01-02 04:35:09 +0000

    Cosmetic: Tabs to spaces in SmartStr.h
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@26274 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/UnrarXLib/SmartStr.h
+++ xbmc/lib/UnrarXLib/SmartStr.h
@@ -5,43 +5,43 @@
 
 class CSmartStr
 {
-	char * m_szPtr;
+  char * m_szPtr;
 public:
-	CSmartStr( int iCount ) : m_szPtr(NULL)
-	{
-		if ( iCount )
-		{
-			m_szPtr = new char [iCount];
-		}
-	}
-	~CSmartStr()
-	{
-		if ( m_szPtr )
-		{
-			delete [] m_szPtr;
-		}
-	}
-	operator char *() { return m_szPtr; }
+  CSmartStr( int iCount ) : m_szPtr(NULL)
+  {
+    if ( iCount )
+    {
+      m_szPtr = new char [iCount];
+    }
+  }
+  ~CSmartStr()
+  {
+    if ( m_szPtr )
+    {
+      delete [] m_szPtr;
+    }
+  }
+  operator char *() { return m_szPtr; }
 };
 
 class CSmartStrW
 {
-	wchar * m_szPtr;
+  wchar * m_szPtr;
 public:
-	CSmartStrW( int iCount ) : m_szPtr(NULL)
-	{
-		if ( iCount )
-		{
-			m_szPtr = new wchar [iCount];
-		}
-	}
-	~CSmartStrW()
-	{
-		if ( m_szPtr )
-		{
-			delete [] m_szPtr;
-		}
-	}
-	operator wchar *() { return m_szPtr; }
+  CSmartStrW( int iCount ) : m_szPtr(NULL)
+  {
+    if ( iCount )
+    {
+      m_szPtr = new wchar [iCount];
+    }
+  }
+  ~CSmartStrW()
+  {
+    if ( m_szPtr )
+    {
+      delete [] m_szPtr;
+    }
+  }
+  operator wchar *() { return m_szPtr; }
 };
 
