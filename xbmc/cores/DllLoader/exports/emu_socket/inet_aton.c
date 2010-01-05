01bba03e7f5becc6c15c3f4fb124b1af52edd44f
ceros7 <ceros7@svn>
2010-01-05 00:58:29 +0000

    Update inet_*.c files with implementations found in FreeBSD. Code used was found in the git repo for FreeBSD at gitorious.org. Code needs testing by Windows dev.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/gpl-compat@26386 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/exports/emu_socket/inet_aton.c
+++ xbmc/cores/DllLoader/exports/emu_socket/inet_aton.c
@@ -1,32 +1,20 @@
-/*
- * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden).
+/*-
+ * Copyright (c) 2001 Charles Mott <cm@linktel.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- *
- * 4. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -36,19 +24,16 @@
  * SUCH DAMAGE.
  */
 
-/* $Id$ */
-
-/* Minimal implementation of inet_aton.
- * Cannot distinguish between failure and a local broadcast address. */
-
 #ifdef _XBOX
 #include <xtl.h>
 #include <winsockx.h>
 #else
 #include <windows.h>
 #endif
-#include <stddef.h>
-#include "emu_socket.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <netinet/in.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -61,11 +46,103 @@ extern "C" {
 int
 inet_aton(const char *cp, struct in_addr *addr)
 {
-  addr->s_addr = inet_addr(cp);
-  return (addr->s_addr == INADDR_NONE) ? 0 : 1;
+    u_long parts[4];
+    in_addr_t val;
+    const char *c;
+    char *endptr;
+    int gotend, n;
+
+    c = (const char *)cp;
+    n = 0;
+
+    /*
+     * Run through the string, grabbing numbers until
+     * the end of the string, or some error
+     */
+    gotend = 0;
+    while (!gotend) {
+        unsigned long l;
+
+        l = strtoul(c, &endptr, 0);
+
+        if (l == ULONG_MAX || (l == 0 && endptr == c))
+            return (0);
+
+        val = (in_addr_t)l;
+
+        /*
+         * If the whole string is invalid, endptr will equal
+         * c.. this way we can make sure someone hasn't
+         * gone '.12' or something which would get past
+         * the next check.
+         */
+        if (endptr == c)
+            return (0);
+        parts[n] = val;
+        c = endptr;
+
+        /* Check the next character past the previous number's end */
+        switch (*c) {
+        case '.' :
+
+            /* Make sure we only do 3 dots .. */
+            if (n == 3) /* Whoops. Quit. */
+                return (0);
+            n++;
+            c++;
+            break;
+
+        case '\0':
+            gotend = 1;
+            break;
+
+        default:
+            if (isspace((unsigned char)*c)) {
+                gotend = 1;
+                break;
+            } else {
+
+                /* Invalid character, then fail. */
+                return (0);
+            }
+        }
+
+    }
+
+    /* Concoct the address according to the number of parts specified. */
+    switch (n) {
+    case 0:             /* a -- 32 bits */
+
+        /*
+         * Nothing is necessary here.  Overflow checking was
+         * already done in strtoul().
+         */
+        break;
+    case 1:             /* a.b -- 8.24 bits */
+        if (val > 0xffffff || parts[0] > 0xff)
+            return (0);
+        val |= parts[0] << 24;
+        break;
+
+    case 2:             /* a.b.c -- 8.8.16 bits */
+        if (val > 0xffff || parts[0] > 0xff || parts[1] > 0xff)
+            return (0);
+        val |= (parts[0] << 24) | (parts[1] << 16);
+        break;
+
+    case 3:             /* a.b.c.d -- 8.8.8.8 bits */
+        if (val > 0xff || parts[0] > 0xff || parts[1] > 0xff ||
+            parts[2] > 0xff)
+            return (0);
+        val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
+        break;
+    }
+
+    if (addr != NULL)
+        addr->s_addr = htonl(val);
+    return (1);
 }
 
 #ifdef __cplusplus
 }
 #endif
-
--- xbmc/cores/DllLoader/exports/emu_socket/inet_pton.c
+++ xbmc/cores/DllLoader/exports/emu_socket/inet_pton.c
@@ -1,42 +1,19 @@
 /*
- * Copyright (c) 1999 Kungliga Tekniska Högskolan
- * (Royal Institute of Technology, Stockholm, Sweden).
- * All rights reserved.
+ * Copyright (c) 2004 by Internet Systems Consortium, Inc. ("ISC")
+ * Copyright (c) 1996,1999 by Internet Software Consortium.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *      This product includes software developed by the Kungliga Tekniska
- *      Högskolan and its contributors.
- *
- * 4. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
-/* $Id$ */
 
 #ifdef _XBOX
 #include <xtl.h>
@@ -44,26 +21,215 @@
 #else
 #include <windows.h>
 #endif
-#include <stddef.h>
-#include "emu_socket.h"
-#include <errno.h>
+#include <rpc/types.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-extern int inet_aton(const char *cp, struct in_addr *addr);
 
+#ifdef __STDC__
+#define __weak_reference(sym,alias) \
+    __asm__(".weak " #alias);   \
+    __asm__(".equ "  #alias ", " #sym)
+#else
+#define __weak_reference(sym,alias) \
+    __asm__(".weak alias");     \
+    __asm__(".equ alias, sym")
+#endif  /* __STDC__ */
+
+/*%
+ * WARNING: Don't even consider trying to compile this on a system where
+ * sizeof(int) < 4.  sizeof(int) > 4 is fine; all the world's not a VAX.
+ */
+
+static int  inet_pton4(const char *src, u_char *dst);
+static int  inet_pton6(const char *src, u_char *dst);
+
+/* int
+ * inet_pton(af, src, dst)
+ *  convert from presentation format (which usually means ASCII printable)
+ *  to network format (which is usually some kind of binary format).
+ * return:
+ *  1 if the address was valid for the specified address family
+ *  0 if the address wasn't valid (`dst' is untouched in this case)
+ *  -1 if some other error occurred (`dst' is untouched in this case, too)
+ * author:
+ *  Paul Vixie, 1996.
+ */
 int
-inet_pton(int af, const char *src, void *dst)
+__rpc_inet_pton(int af, const char * __restrict src, void * __restrict dst)
 {
-  if (af != AF_INET)
-  {
-    errno = EAFNOSUPPORT;
-    return -1;
-  }
-  return inet_aton (src, (struct in_addr *)dst);
+    switch (af) {
+    case AF_INET:
+        return (inet_pton4(src, (u_char*)dst));
+    case AF_INET6:
+        return (inet_pton6(src, (u_char*)dst));
+    default:
+        return (-1);
+    }
+    /* NOTREACHED */
 }
+
+/* int
+ * inet_pton4(src, dst)
+ *  like inet_aton() but without all the hexadecimal and shorthand.
+ * return:
+ *  1 if `src' is a valid dotted quad, else 0.
+ * notice:
+ *  does not touch `dst' unless it's returning 1.
+ * author:
+ *  Paul Vixie, 1996.
+ */
+static int
+inet_pton4(const char *src, u_char *dst)
+{
+    static const char digits[] = "0123456789";
+    int saw_digit, octets, ch;
+#define NS_INADDRSZ 4
+    u_char tmp[NS_INADDRSZ], *tp;
+
+    saw_digit = 0;
+    octets = 0;
+    *(tp = tmp) = 0;
+    while ((ch = *src++) != '\0') {
+        const char *pch;
+
+        if ((pch = strchr(digits, ch)) != NULL) {
+            u_int c_new = *tp * 10 + (pch - digits);
+
+            if (saw_digit && *tp == 0)
+                return (0);
+            if (c_new > 255)
+                return (0);
+            *tp = c_new;
+            if (!saw_digit) {
+                if (++octets > 4)
+                    return (0);
+                saw_digit = 1;
+            }
+        } else if (ch == '.' && saw_digit) {
+            if (octets == 4)
+                return (0);
+            *++tp = 0;
+            saw_digit = 0;
+        } else
+            return (0);
+    }
+    if (octets < 4)
+        return (0);
+    memcpy(dst, tmp, NS_INADDRSZ);
+    return (1);
+}
+
+/* int
+ * inet_pton6(src, dst)
+ *  convert presentation level address to network order binary form.
+ * return:
+ *  1 if `src' is a valid [RFC1884 2.2] address, else 0.
+ * notice:
+ *  (1) does not touch `dst' unless it's returning 1.
+ *  (2) :: in a full address is silently ignored.
+ * credit:
+ *  inspired by Mark Andrews.
+ * author:
+ *  Paul Vixie, 1996.
+ */
+static int
+inet_pton6(const char *src, u_char *dst)
+{
+    static const char xdigits_l[] = "0123456789abcdef",
+              xdigits_u[] = "0123456789ABCDEF";
+#define NS_IN6ADDRSZ    16
+#define NS_INT16SZ  2
+    u_char tmp[NS_IN6ADDRSZ], *tp, *endp, *colonp;
+    const char *xdigits, *curtok;
+    int ch, seen_xdigits;
+    u_int val;
+
+    memset((tp = tmp), '\0', NS_IN6ADDRSZ);
+    endp = tp + NS_IN6ADDRSZ;
+    colonp = NULL;
+    /* Leading :: requires some special handling. */
+    if (*src == ':')
+        if (*++src != ':')
+            return (0);
+    curtok = src;
+    seen_xdigits = 0;
+    val = 0;
+    while ((ch = *src++) != '\0') {
+        const char *pch;
+
+        if ((pch = strchr((xdigits = xdigits_l), ch)) == NULL)
+            pch = strchr((xdigits = xdigits_u), ch);
+        if (pch != NULL) {
+            val <<= 4;
+            val |= (pch - xdigits);
+            if (++seen_xdigits > 4)
+                return (0);
+            continue;
+        }
+        if (ch == ':') {
+            curtok = src;
+            if (!seen_xdigits) {
+                if (colonp)
+                    return (0);
+                colonp = tp;
+                continue;
+            } else if (*src == '\0') {
+                return (0);
+            }
+            if (tp + NS_INT16SZ > endp)
+                return (0);
+            *tp++ = (u_char) (val >> 8) & 0xff;
+            *tp++ = (u_char) val & 0xff;
+            seen_xdigits = 0;
+            val = 0;
+            continue;
+        }
+        if (ch == '.' && ((tp + NS_INADDRSZ) <= endp) &&
+            inet_pton4(curtok, tp) > 0) {
+            tp += NS_INADDRSZ;
+            seen_xdigits = 0;
+            break;  /*%< '\\0' was seen by inet_pton4(). */
+        }
+        return (0);
+    }
+    if (seen_xdigits) {
+        if (tp + NS_INT16SZ > endp)
+            return (0);
+        *tp++ = (u_char) (val >> 8) & 0xff;
+        *tp++ = (u_char) val & 0xff;
+    }
+    if (colonp != NULL) {
+        /*
+         * Since some memmove()'s erroneously fail to handle
+         * overlapping regions, we'll do the shift by hand.
+         */
+        const int n = tp - colonp;
+        int i;
+
+        if (tp == endp)
+            return (0);
+        for (i = 1; i <= n; i++) {
+            endp[- i] = colonp[n - i];
+            colonp[n - i] = 0;
+        }
+        tp = endp;
+    }
+    if (tp != endp)
+        return (0);
+    memcpy(dst, tmp, NS_IN6ADDRSZ);
+    return (1);
+}
+
+/*
+ * Weak aliases for applications that use certain private entry points,
+ * and fail to include <arpa/inet.h>.
+ */
+#undef inet_pton
+__weak_reference(__inet_pton, inet_pton);
+
 #ifdef __cplusplus
 }
 #endif
-
