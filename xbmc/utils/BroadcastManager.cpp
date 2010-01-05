feaa35bc6413de149eccd70b32efebbf16bbcb90
topfs2 <topfs2@svn>
2010-01-05 16:47:05 +0000

    Added preliminary broadcast manager
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/jsonrpc@26400 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/utils/BroadcastManager.cpp
@@ -0,0 +1,54 @@
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "BroadcastManager.h"
+#include <stdio.h>
+#include "log.h"
+
+using namespace std;
+
+vector<IBroadcastListener *> CBroadcastManager::m_listeners;
+
+void CBroadcastManager::AddListener(IBroadcastListener *listener)
+{
+  m_listeners.push_back(listener);
+}
+
+void CBroadcastManager::RemoveListener(IBroadcastListener *listener)
+{
+  for (unsigned int i = 0; i < m_listeners.size(); i++)
+  {
+    if (m_listeners[i] == listener)
+    {
+      m_listeners.erase(m_listeners.begin() + i);
+      printf("found and removed listener\n");
+      return;
+    }
+  }
+}
+
+void CBroadcastManager::Broadcast(EBroadcastFlag flag, string message)
+{
+  CLog::Log(LOGDEBUG, "BroadcastManager - Broadcast: %s", message.c_str());
+  printf("BroadcastManager - Broadcast: %s\n", message.c_str());
+  for (unsigned int i = 0; i < m_listeners.size(); i++)
+    m_listeners[i]->Broadcast(flag, message);
+}
