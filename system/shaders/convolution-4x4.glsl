803dd03d5d37deb7ba4f7ad86a7ef13fee14cc52
bobo1on1 <bobo1on1@svn>
2010-01-12 15:57:22 +0000

    cosmetic: dump unneeded variables
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@26722 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- system/shaders/convolution-4x4.glsl
+++ system/shaders/convolution-4x4.glsl
@@ -15,17 +15,12 @@ vec4 pixel(float xpos, float ypos)
 
 vec4 line (float ypos, float xf, vec4 linetaps)
 {
-  float xpos;
   vec4  pixels;
 
-  xpos    = -1.0 - xf;
-  pixels  = pixel(xpos, ypos) * linetaps.r;
-  xpos    =  0.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps.g;
-  xpos    =  1.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps.b;
-  xpos    =  2.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps.a;
+  pixels  = pixel(-1.0 - xf, ypos) * linetaps.r;
+  pixels += pixel(      -xf, ypos) * linetaps.g;
+  pixels += pixel( 1.0 - xf, ypos) * linetaps.b;
+  pixels += pixel( 2.0 - xf, ypos) * linetaps.a;
 
   return pixels;
 }
@@ -34,19 +29,14 @@ void main()
 {
   float xf = fract(gl_TexCoord[0].x / stepx);
   float yf = fract(gl_TexCoord[0].y / stepy);
-  float ypos;
 
   vec4 linetaps   = weight(1.0 - xf);
   vec4 columntaps = weight(1.0 - yf);
 
-  ypos = -1.0 - yf;
-  gl_FragColor  = line(ypos, xf, linetaps) * columntaps.r;
-  ypos =  0.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps) * columntaps.g;
-  ypos =  1.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps) * columntaps.b;
-  ypos =  2.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps) * columntaps.a;
+  gl_FragColor  = line(-1.0 - yf, xf, linetaps) * columntaps.r;
+  gl_FragColor += line(      -yf, xf, linetaps) * columntaps.g;
+  gl_FragColor += line( 1.0 - yf, xf, linetaps) * columntaps.b;
+  gl_FragColor += line( 2.0 - yf, xf, linetaps) * columntaps.a;
 
   gl_FragColor.a = gl_Color.a;
 }
--- system/shaders/convolution-6x6.glsl
+++ system/shaders/convolution-6x6.glsl
@@ -15,21 +15,14 @@ vec4 pixel(float xpos, float ypos)
 
 vec4 line (float ypos, float xf, vec4 linetaps1, vec4 linetaps2)
 {
-  float xpos;
   vec4  pixels;
 
-  xpos    = -2.0 - xf;
-  pixels  = pixel(xpos, ypos) * linetaps1.r;
-  xpos    = -1.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps2.r;
-  xpos    =  0.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps1.g;
-  xpos    =  1.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps2.g;
-  xpos    =  2.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps1.b;
-  xpos    =  3.0 - xf;
-  pixels += pixel(xpos, ypos) * linetaps2.b;
+  pixels  = pixel(-2.0 - xf, ypos) * linetaps1.r;
+  pixels += pixel(-1.0 - xf, ypos) * linetaps2.r;
+  pixels += pixel(      -xf, ypos) * linetaps1.g;
+  pixels += pixel( 1.0 - xf, ypos) * linetaps2.g;
+  pixels += pixel( 2.0 - xf, ypos) * linetaps1.b;
+  pixels += pixel( 3.0 - xf, ypos) * linetaps2.b;
 
   return pixels;
 }
@@ -38,25 +31,18 @@ void main()
 {
   float xf = fract(gl_TexCoord[0].x / stepx);
   float yf = fract(gl_TexCoord[0].y / stepy);
-  float ypos;
 
   vec4 linetaps1   = weight((1.0 - xf) / 2.0);
   vec4 linetaps2   = weight((1.0 - xf) / 2.0 + 0.5);
   vec4 columntaps1 = weight((1.0 - yf) / 2.0);
   vec4 columntaps2 = weight((1.0 - yf) / 2.0 + 0.5);
 
-  ypos = -2.0 - yf;
-  gl_FragColor  = line(ypos, xf, linetaps1, linetaps2) * columntaps1.r;
-  ypos = -1.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps1, linetaps2) * columntaps2.r;
-  ypos =  0.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps1, linetaps2) * columntaps1.g;
-  ypos =  1.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps1, linetaps2) * columntaps2.g;
-  ypos =  2.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps1, linetaps2) * columntaps1.b;
-  ypos =  3.0 - yf;
-  gl_FragColor += line(ypos, xf, linetaps1, linetaps2) * columntaps2.b;
+  gl_FragColor  = line(-2.0 - yf, xf, linetaps1, linetaps2) * columntaps1.r;
+  gl_FragColor += line(-1.0 - yf, xf, linetaps1, linetaps2) * columntaps2.r;
+  gl_FragColor += line(      -yf, xf, linetaps1, linetaps2) * columntaps1.g;
+  gl_FragColor += line( 1.0 - yf, xf, linetaps1, linetaps2) * columntaps2.g;
+  gl_FragColor += line( 2.0 - yf, xf, linetaps1, linetaps2) * columntaps1.b;
+  gl_FragColor += line( 3.0 - yf, xf, linetaps1, linetaps2) * columntaps2.b;
 
   gl_FragColor.a = gl_Color.a;
 }
