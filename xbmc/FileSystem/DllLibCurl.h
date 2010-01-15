c314f7099ba38cf9bd57b042ba9eea7d70a377f0
davilla <davilla@svn>
2010-01-15 05:22:36 +0000

    [osx] add trunk/lib/libcurl-OSX, fixup configure. makefiles and xcode project to ref headers from here
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@26847 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/DllLibCurl.h
+++ xbmc/FileSystem/DllLibCurl.h
@@ -27,11 +27,7 @@
 namespace XCURL
 {
   #define CURL CURL_HANDLE
-  #if defined(_LINUX) && !defined(__APPLE__) || defined(_WIN32)
   #include <curl/curl.h>
-  #else
-  #include "curl/curl.h"
-  #endif
   #undef CURL
 
   class DllLibCurlInterface
