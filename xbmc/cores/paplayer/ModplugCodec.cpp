8b4fb8dbf66d12e2025756a85e97e8829fe12e79
spiff_ <spiff_@svn>
2010-01-23 22:29:35 +0000

    fixed: total time and seeks were broken in modplugcodec
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27116 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/paplayer/ModplugCodec.cpp
+++ xbmc/cores/paplayer/ModplugCodec.cpp
@@ -73,7 +73,7 @@ bool ModplugCodec::Init(const CStdString &strFile, unsigned int filecache)
   m_Channels = 2;
   m_SampleRate = 44100;
   m_BitsPerSample = 16;
-  m_TotalTime = (__int64)(m_dll.ModPlug_GetLength(m_module))/1000;
+  m_TotalTime = (__int64)(m_dll.ModPlug_GetLength(m_module));
 
   return true;
 }
@@ -87,7 +87,7 @@ void ModplugCodec::DeInit()
 
 __int64 ModplugCodec::Seek(__int64 iSeekTime)
 {
-  m_dll.ModPlug_Seek(m_module, (int)(iSeekTime/1000));
+  m_dll.ModPlug_Seek(m_module, (int)iSeekTime);
   return iSeekTime;
 }
 
