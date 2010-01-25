0e149aa77b2645522537c02357102f4024f41338
lcapriotti <lcapriotti@svn>
2010-01-25 13:17:46 +0000

    support for Intel only
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27165 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/XBMCLive/SDK/buildWithOptions.sh
+++ tools/XBMCLive/SDK/buildWithOptions.sh
@@ -1,30 +1,12 @@
 #!/bin/sh
 
-#      Copyright (C) 2005-2008 Team XBMC
-#      http://www.xbmc.org
-#
-#  This Program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
-#
-#  This Program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with XBMC; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-#  http://www.gnu.org/copyleft/gpl.html
-
 # Using apt-cacher(-ng) to speed up apt-get downloads
 
 SDK_BUILDHOOKS=""
 
 # getopt-parse.bash
 
-TEMP=$(getopt -o snp:ulkg --long xbmc-svn,nvidia-only,proxy:,usb-image,live-only,keep-workarea,grub2 -- "$@")
+TEMP=$(getopt -o snp:ulkgi --long xbmc-svn,nvidia-only,proxy:,usb-image,live-only,keep-workarea,grub2,intel-only -- "$@")
 eval set -- "$TEMP"
 
 while true
@@ -58,6 +40,11 @@ do
 	-g|--grub2)
 		echo "Enable option: Use grub2"
 		export SDK_BUILDHOOKS="$SDK_BUILDHOOKS ./grub2Hook.sh"
+		shift
+		;;
+	-i|--intel-only)
+		echo "Enable option: Intel support only"
+		export SDK_BUILDHOOKS="$SDK_BUILDHOOKS ./intelOnlyHook.sh"
 		shift
 		;;
 	-p|--proxy)
