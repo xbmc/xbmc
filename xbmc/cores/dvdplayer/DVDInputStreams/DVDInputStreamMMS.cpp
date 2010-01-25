933998afc3360e56002a0fba0592da43f5570820
davilla <davilla@svn>
2010-01-25 20:52:57 +0000

    fixed, upstream libmms still uses reserved keyword 'this' as a function parameter name. Some distros have it fixed but still exists in upstream
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27171 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamMMS.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamMMS.cpp
@@ -30,9 +30,14 @@
 
 #include "Application.h"
 
+// work around for braindead usage of "this" keyword as parameter name in libmms headers
+// some distros have already patched this but upstream @ https://launchpad.net/libmms
+// does still has the "this" usage.
+#define this instance
 #include <libmms/mmsio.h> // FIXME: remove this header once the ubuntu headers is fixed (variable named this)
 #include <libmms/mms.h>
 #include <libmms/mmsh.h>
+#undef this
 
 using namespace XFILE;
 
