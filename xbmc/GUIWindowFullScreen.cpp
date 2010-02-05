7b096b4b1c52ff9459b87052463b522d617fc0e4
elupus <elupus@svn>
2010-02-05 22:10:42 +0000

    changed: make sure we preload the fullscreen info window aswell
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27494 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowFullScreen.cpp
+++ xbmc/GUIWindowFullScreen.cpp
@@ -165,6 +165,7 @@ void CGUIWindowFullScreen::AllocResources(bool forceLoad)
   PreloadDialog(WINDOW_OSD);
   PreloadDialog(WINDOW_DIALOG_VIDEO_OSD_SETTINGS);
   PreloadDialog(WINDOW_DIALOG_AUDIO_OSD_SETTINGS);
+  PreloadDialog(WINDOW_DIALOG_FULLSCREEN_INFO);
   // No need to preload these here, as they're preloaded by our app
 //  PreloadDialog(WINDOW_DIALOG_SEEK_BAR);
 //  PreloadDialog(WINDOW_DIALOG_VOLUME_BAR);
@@ -178,6 +179,7 @@ void CGUIWindowFullScreen::FreeResources(bool forceUnload)
   UnloadDialog(WINDOW_OSD);
   UnloadDialog(WINDOW_DIALOG_VIDEO_OSD_SETTINGS);
   UnloadDialog(WINDOW_DIALOG_AUDIO_OSD_SETTINGS);
+  UnloadDialog(WINDOW_DIALOG_FULLSCREEN_INFO);
   // No need to unload these here, as they're preloaded by our app
 //  UnloadDialog(WINDOW_DIALOG_SEEK_BAR);
 //  UnloadDialog(WINDOW_DIALOG_VOLUME_BAR);
