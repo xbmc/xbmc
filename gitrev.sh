340b36d700dc61034e2d97ce3342b3fbd382f9bb
theuni <theuni@svn>
2010-02-06 03:17:55 +0000

    fixed: configure would hang forever when using a pure git repo added: let pure git repo get the correct svn revision
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27498 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ gitrev.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+N=0
+R=""
+MAX=1
+while [[ $N -le $MAX && "x$R" == "x" ]]
+do
+  R=$(git log -1 --pretty=format:%b HEAD~$N | sed -e 's/.*@\([0-9]\+\) .*/\1/')
+  N=$[$N+1]
+done
+if [[ "x$R" != "x" ]]; then
+  if [[ $N > 1 ]]; then
+    R=$R\+$((N-1))
+  fi
+  echo $R
+fi
