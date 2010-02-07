041604ba36dd384b320551c3d357fee02266590f
elupus <elupus@svn>
2010-02-07 14:57:04 +0000

    changed: only skip busy dialog if progress dialog is already up
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27545 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/Directory.cpp
+++ xbmc/FileSystem/Directory.cpp
@@ -153,7 +153,7 @@ bool CDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items, C
           while(!get.Wait(10))
           {
             CSingleLock lock(g_graphicsContext);
-            if(g_windowManager.HasModalDialog())
+            if(g_windowManager.IsWindowVisible(WINDOW_DIALOG_PROGRESS))
             {
               if(dialog)
               {
