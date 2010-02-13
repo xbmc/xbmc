fe64bfb87d007e98516bc6ca7a4e52b49fdcb3ff
spiff_ <spiff_@svn>
2010-02-13 16:40:14 +0000

    added: ticket #6816, ticket #8764 - player.folderpath and player.filenameandpath info labels. useful for displaying extra crap for the currently playing item
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27711 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.cpp
+++ xbmc/utils/GUIInfoManager.cpp
@@ -225,6 +225,8 @@ int CGUIInfoManager::TranslateSingleString(const CStdString &strCondition)
     else if (strTest.Equals("player.chaptername")) ret = PLAYER_CHAPTERNAME;
     else if (strTest.Equals("player.starrating")) ret = PLAYER_STAR_RATING;
     else if (strTest.Equals("player.passthrough")) ret = PLAYER_PASSTHROUGH;
+    else if (strTest.Equals("player.folderpath")) ret = PLAYER_PATH;
+    else if (strTest.Equals("player.filenameandpath")) ret = PLAYER_FILEPATH;
   }
   else if (strCategory.Equals("weather"))
   {
@@ -1048,6 +1050,26 @@ CStdString CGUIInfoManager::GetLabel(int info, int contextWindow)
     if(g_application.IsPlaying() && g_application.m_pPlayer)
       strLabel = GetDuration(TIME_FORMAT_HH_MM);
     break;
+  case PLAYER_PATH:
+  case PLAYER_FILEPATH:
+     if (m_currentFile)
+     {
+       if (m_currentFile->HasMusicInfoTag())
+         strLabel = m_currentFile->GetMusicInfoTag()->GetURL();
+       else if (m_currentFile->HasVideoInfoTag())
+         strLabel = m_currentFile->GetVideoInfoTag()->m_strFileNameAndPath;
+       if (strLabel.IsEmpty())
+         strLabel = m_currentFile->m_strPath;
+     }
+     if (info == PLAYER_PATH)
+     {
+       // do this twice since we want the path outside the archive if this
+       // is to be of use.
+       if (CUtil::IsInArchive(strLabel))
+         strLabel = CUtil::GetParentPath(strLabel);
+       strLabel = CUtil::GetParentPath(strLabel);
+     }
+     break;
   case MUSICPLAYER_TITLE:
   case MUSICPLAYER_ALBUM:
   case MUSICPLAYER_ARTIST:
