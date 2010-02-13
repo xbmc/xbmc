2e38ab174ec5fb1d281dabf1ed07937428fb3066
spiff_ <spiff_@svn>
2010-02-13 17:34:55 +0000

    fixed: ticket #8536 - joystick deadzone and calibration. thanks to bobdevis
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27721 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/common/SDLJoystick.h
+++ guilib/common/SDLJoystick.h
@@ -16,6 +16,7 @@
 #include <SDL/SDL_events.h>
 
 #define MAX_AXES 64
+#define MAX_AXISAMOUNT 32768
 
 
 // Class to manage all connected joysticks
@@ -27,26 +28,17 @@ public:
 
   void Initialize();
   void Reset(bool axis=false);
-  void CalibrateAxis(SDL_Joystick *joy);
   void ResetAxis(int axisId) { m_Amount[axisId] = 0; }
   void Update();
   void Update(SDL_Event& event);
-  float GetAmount(int axis)
-  {
-    if (m_Amount[axis]>0)
-      return (float)(m_Amount[axis]-m_SafeRange)/(32768.0f-(float)m_SafeRange);
-    return (float)(m_Amount[axis]+m_SafeRange)/(32768.0f-(float)m_SafeRange);
-  }
-  float GetAmount()
-  {
-    return GetAmount(m_AxisId);
-  }
   bool GetButton (int& id, bool consider_repeat=true);
   bool GetAxis (int &id) { if (!IsAxisActive()) return false; id=m_AxisId; return true; }
   bool GetHat (int &id, int &position, bool consider_repeat=true);
   std::string GetJoystick() { return (m_JoyId>-1)?m_JoystickNames[m_JoyId]:""; }
   int GetAxisWithMaxAmount();
-  void SetSafeRange(int val) { m_SafeRange=(val>32767)?32767:val; }
+  float GetAmount(int axis);
+  float GetAmount() { return GetAmount(m_AxisId); }
+  float SetDeadzone(float val);
 
 private:
   void SetAxisActive(bool active=true) { m_ActiveFlags = active?(m_ActiveFlags|JACTIVE_AXIS):(m_ActiveFlags&(~JACTIVE_AXIS)); }
@@ -57,14 +49,13 @@ private:
   bool IsHatActive() { return (m_ActiveFlags & JACTIVE_HAT) == JACTIVE_HAT; }
 
   int m_Amount[MAX_AXES];
-  int m_DefaultAmount[MAX_AXES];
   int m_AxisId;
   int m_ButtonId;
   uint8_t m_HatState;
- 	int m_HatId; 
+  int m_HatId; 
   int m_JoyId;
   int m_NumAxes;
-  int m_SafeRange; // dead zone
+  int m_DeadzoneRange;
   uint32_t m_pressTicksButton;
   uint32_t m_pressTicksHat;
   uint8_t m_ActiveFlags;
