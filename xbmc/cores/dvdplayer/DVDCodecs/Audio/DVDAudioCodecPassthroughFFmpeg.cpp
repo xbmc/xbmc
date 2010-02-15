605d3b2e07c10f3567e108509a32daed645ecc72
gnif2 <gnif2@svn>
2010-02-15 08:23:12 +0000

    Fixed missing ->extradata, hit dw in vim by accident :(
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@27831 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecPassthroughFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecPassthroughFFmpeg.cpp
@@ -261,7 +261,7 @@ void CDVDAudioCodecPassthroughFFmpeg::DisposeMuxer(Muxer &muxer)
     if (muxer.m_WroteHeader)
       m_dllAvFormat.av_write_trailer(muxer.m_pFormat);
     muxer.m_WroteHeader = false;
-    delete[] muxer.m_pStream->codec;
+    delete[] muxer.m_pStream->codec->extradata;
     m_dllAvUtil.av_freep(&muxer.m_pFormat->pb);
     m_dllAvUtil.av_freep(&muxer.m_pFormat);
     m_dllAvUtil.av_freep(&muxer.m_pStream);
