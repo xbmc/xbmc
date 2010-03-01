773e307a49fdae1e87022140b94d0b7a304217c9
charlydoes <charlydoes@svn>
2010-03-01 19:07:14 +0000

    [WIN32] changed: install libmicrohttpd dlls in system\webserver instead of app root.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28262 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/Win32BuildSetup/dependencies/getdeps.bat
+++ project/Win32BuildSetup/dependencies/getdeps.bat
@@ -3,6 +3,7 @@ rem copies additional dependencies from lib directories
 
 set CUR_PATH=%cd%
 cd "%~dp0"
-xcopy /Y ..\..\..\lib\libmicrohttpd_win32\bin\*.dll
+md ..\..\..\system\webserver > nul
+xcopy /Y ..\..\..\lib\libmicrohttpd_win32\bin\*.dll ..\..\..\system\webserver
 cd "%CUR_PATH%"
 set CUR_PATH=
