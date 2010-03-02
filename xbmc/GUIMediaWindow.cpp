0087f7fe97167bfa28da23cb83a749d4709127df
arnova <arnova@svn>
2010-03-02 09:33:17 +0000

    fixed: Don't do GetDirectory() for paths named ? in GUIMediaWindow. Fixes error messages when using Python scripts
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28302 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIMediaWindow.cpp
+++ xbmc/GUIMediaWindow.cpp
@@ -644,6 +644,9 @@ bool CGUIMediaWindow::GetDirectory(const CStdString &strDirectory, CFileItemList
 // This function calls OnPrepareFileItems() and OnFinalizeFileItems()
 bool CGUIMediaWindow::Update(const CStdString &strDirectory)
 {
+  if (strDirectory == "?")
+    return false;
+
   // get selected item
   int iItem = m_viewControl.GetSelectedItem();
   CStdString strSelectedItem = "";
