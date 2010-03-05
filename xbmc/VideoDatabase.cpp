7a69a4eee78194f24edf4c896e3854538354df54
alcoheca <alcoheca@svn>
2010-03-05 18:49:25 +0000

    fixed: deadlock in GetScraperForPath
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28398 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoDatabase.cpp
+++ xbmc/VideoDatabase.cpp
@@ -5577,7 +5577,9 @@ bool CVideoDatabase::GetScraperForPath(const CStdString& strPath, ScraperPtr& sc
       // path is not excluded
       // try and ascertain scraper for this path
       CONTENT_TYPE content = TranslateContent(strcontent);
-      assert(content != CONTENT_NONE);
+
+      //FIXME paths stored should not have empty strContent
+      //assert(content != CONTENT_NONE);
       CStdString scraperID = m_pDS->fv("path.strScraper").get_asString();
 
       AddonPtr addon;
@@ -5629,8 +5631,8 @@ bool CVideoDatabase::GetScraperForPath(const CStdString& strPath, ScraperPtr& sc
             settings.exclude = true;
             break;
           }
-          strPath1 = strParent;
         }
+        strPath1 = strParent;
       }
     }
     m_pDS->close();
