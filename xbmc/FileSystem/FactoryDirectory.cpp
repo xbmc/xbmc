f09eb0413056fd52a08f61882c3771dec153ade9
elupus <elupus@svn>
2010-03-06 15:50:46 +0000

    changed: just cause hostname is a UUID, doesn't mean it's a plugin directrory. fixed: upnp didn't work after addon merges
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28427 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/FactoryDirectory.cpp
+++ xbmc/FileSystem/FactoryDirectory.cpp
@@ -126,7 +126,8 @@ IDirectory* CFactoryDirectory::Create(const CStdString& strPath)
 #ifdef HAS_FILESYSTEM
   if (strProtocol == "iso9660") return new CISO9660Directory();
 #endif
-  if (StringUtils::ValidateUUID(url.GetHostName())) return new CPluginDirectory(ADDON::TranslateContent(strProtocol));
+  if (StringUtils::ValidateUUID(url.GetHostName()) 
+  &&  ADDON::TranslateContent(strProtocol) != CONTENT_NONE) return new CPluginDirectory(ADDON::TranslateContent(strProtocol));
   if (strProtocol == "zip") return new CZipDirectory();
 #ifdef HAS_FILESYSTEM_RAR
   if (strProtocol == "rar") return new CRarDirectory();
