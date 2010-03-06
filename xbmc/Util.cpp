2b9fbb42715889949aab732de8dbf6f99f799a1e
elupus <elupus@svn>
2010-03-06 02:22:19 +0000

    changed: don't assume double slashes in urls are invalid, that MUST be up to the underlying protocol to decide
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28415 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -2250,33 +2250,11 @@ CStdString CUtil::ValidatePath(const CStdString &path)
   // check the path for incorrect slashes
 #ifdef _WIN32
   if (CUtil::IsDOSPath(path))
-  {
     result.Replace('/', '\\');
-    // Fixup for double back slashes (but ignore the \\ of unc-paths)
-    for (int x=1; x<result.GetLength()-1; x++)
-    {
-      if (result[x] == '\\' && result[x+1] == '\\')
-        result.Delete(x);
-    }
-  }
   else if (path.Find("://") >= 0 || path.Find(":\\\\") >= 0)
-  {
     result.Replace('\\', '/');
-    // Fixup for double forward slashes(/) but don't touch the :// of URLs
-    for (int x=1; x<result.GetLength()-1; x++)
-    {
-      if (result[x] == '/' && result[x+1] == '/' && result[x-1] != ':')
-        result.Delete(x);
-    }
-  }
 #else
   result.Replace('\\', '/');
-  // Fixup for double forward slashes(/) but don't touch the :// of URLs
-  for (int x=1; x<result.GetLength()-1; x++)
-  {
-    if (result[x] == '/' && result[x+1] == '/' && result[x-1] != ':')
-      result.Delete(x);
-  }
 #endif
   return result;
 }
