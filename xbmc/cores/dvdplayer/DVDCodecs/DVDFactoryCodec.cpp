a3b72986762edf2c6f3d09e2d6a007a40224835d
bobo1on1 <bobo1on1@svn>
2010-03-11 17:17:48 +0000

    added: log line about compiled in hw support
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28541 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
@@ -126,6 +126,20 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec( CDVDStreamInfo &hint )
   CDVDVideoCodec* pCodec = NULL;
   CDVDCodecOptions options;
 
+  CStdString hwSupport;
+#ifdef HAVE_LIBCRYSTALHD
+  hwSupport += "Crystal HD:yes ";
+#else
+  hwSupport += "Crystal HD:no ";
+#endif
+#if defined(HAVE_LIBVDPAU) && defined(_LINUX)
+  hwSupport += "VDPAU:yes";
+#elif defined(_LINUX)
+  hwSupport += "VDPAU:no";
+#endif
+
+  CLog::Log(LOGDEBUG, "CDVDFactoryCodec: compiled in hardware support: %s", hwSupport.c_str());
+
 #if defined(HAVE_LIBCRYSTALHD)
   if (!hint.software && CCrystalHD::GetInstance()->DevicePresent())
   {
