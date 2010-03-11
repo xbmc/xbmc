779e7e3bee8760186c9ebaff45fe361d4fa94d61
elupus <elupus@svn>
2010-03-11 21:07:49 +0000

    changed: always set a default label for htsp directories (fixes #8916, thx USBGuy)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28545 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/HTSPDirectory.cpp
+++ xbmc/FileSystem/HTSPDirectory.cpp
@@ -383,6 +383,7 @@ bool CHTSPDirectory::GetChannels( const CURL &base
     item->m_strPath = url.Get();
     CHTSPSession::ParseItem(it->second, tag, event, *item);
     item->m_bIsFolder = false;
+    item->SetLabel(item->m_strTitle);
     item->m_strTitle.Format("%d", it->second.num);
 
     items.Add(item);
