92714a58a89126c0443f027db85a42ea1a788377
jmarshallnz <jmarshallnz@svn>
2010-03-16 07:02:18 +0000

    cleanup: Removed unneeded if block and temporaries
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28602 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoInfoScanner.cpp
+++ xbmc/VideoInfoScanner.cpp
@@ -601,14 +601,8 @@ namespace VIDEO
       {
         if (m_pObserver && !url.strTitle.IsEmpty())
           m_pObserver->OnSetTitle(url.strTitle);
-        long lResult=1;
-        // force thumb and fanart
 
-        if (pDlgProgress)
-          lResult=GetIMDBDetails(pItem.get(), url, info2, false, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-        else
-          lResult=GetIMDBDetails(pItem.get(), url, info2, false, NULL, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-
+        long lResult=GetIMDBDetails(pItem.get(), url, info2, false, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
         if (!bRefresh)
         {
           // fetch episode guide
@@ -713,14 +707,8 @@ namespace VIDEO
           {
             if (m_pObserver && !url.strTitle.IsEmpty())
               m_pObserver->OnSetTitle(url.strTitle);
-            long lResult=1;
-            // force thumb and fanart
 
-            if (pDlgProgress)
-              lResult=GetIMDBDetails(pItem.get(), url, info2, bDirNames, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-            else
-              lResult=GetIMDBDetails(pItem.get(), url, info2, bDirNames, NULL, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-
+            GetIMDBDetails(pItem.get(), url, info2, bDirNames, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
             return 1;
           }
         }
@@ -810,14 +798,8 @@ namespace VIDEO
           {
             if (m_pObserver && !url.strTitle.IsEmpty())
               m_pObserver->OnSetTitle(url.strTitle);
-            long lResult=1;
-            // force thumb and fanart
 
-            if (pDlgProgress)
-              lResult=GetIMDBDetails(pItem.get(), url, info2, false, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-            else
-              lResult=GetIMDBDetails(pItem.get(), url, info2, false, NULL, result == CNfoFile::COMBINED_NFO, ignoreNfo);
-
+            GetIMDBDetails(pItem.get(), url, info2, false, pDlgProgress, result == CNfoFile::COMBINED_NFO, ignoreNfo);
             return 1;
           }
         }
