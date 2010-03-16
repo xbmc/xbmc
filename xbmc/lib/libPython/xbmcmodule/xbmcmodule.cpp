b1f78773bf49bb8c347f60e9240078ad83234385
vdrfan <vdrfan@svn>
2010-03-16 16:17:14 +0000

    added: python function to return a cleaned movie title and year string if available
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28608 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/xbmcmodule/xbmcmodule.cpp
+++ xbmc/lib/libPython/xbmcmodule/xbmcmodule.cpp
@@ -734,6 +734,32 @@ namespace PYXBMC
     return Py_BuildValue((char*)"s", strPath.c_str());
   }
 
+  // getcleanmovietitle function
+  PyDoc_STRVAR(getCleanMovieTitle__doc__,
+    "getCleanMovieTitle(path) -- Returns a clean movie title and year string if available.\n"
+    "\n"
+    "path           : string or unicode - String to clean\n"
+    "\n"
+    "example:\n"
+    "  - title, year = xbmc.getCleanMovieTitle('/path/to/moviefolder/')\n");
+
+  PyObject* XBMC_GetCleanMovieTitle(PyObject *self, PyObject *args)
+  {
+    PyObject *pObjectText;
+    if (!PyArg_ParseTuple(args, (char*)"O", &pObjectText)) return NULL;
+
+    CStdString strPath;
+    if (!PyXBMCGetUnicodeString(strPath, pObjectText, 1)) return NULL;
+
+    CFileItem item(strPath, true);
+    CStdString strName = item.GetMovieName(true);
+
+    CStdString strTitle, strTitleAndYear, strYear;
+    CUtil::CleanString(strName, strTitle, strTitleAndYear, strYear, true);
+
+    return Py_BuildValue((char*)"s,s", strTitle.c_str(), strYear.c_str());
+  }
+
   // validatePath function
   PyDoc_STRVAR(validatePath__doc__,
     "validatePath(path) -- Returns the validated path.\n"
@@ -924,6 +950,8 @@ namespace PYXBMC
 
     {(char*)"getRegion", (PyCFunction)XBMC_GetRegion, METH_VARARGS|METH_KEYWORDS, getRegion__doc__},
     {(char*)"getSupportedMedia", (PyCFunction)XBMC_GetSupportedMedia, METH_VARARGS|METH_KEYWORDS, getSupportedMedia__doc__},
+
+    {(char*)"getCleanMovieTitle", (PyCFunction)XBMC_GetCleanMovieTitle, METH_VARARGS|METH_KEYWORDS, getCleanMovieTitle__doc__},
 
     {(char*)"skinHasImage", (PyCFunction)XBMC_SkinHasImage, METH_VARARGS|METH_KEYWORDS, skinHasImage__doc__},
     {NULL, NULL, 0, NULL}
