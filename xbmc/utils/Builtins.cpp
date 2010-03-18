7f4b5c2436fcaaad43ed7647f372fb0a15bcd2cc
jmarshallnz <jmarshallnz@svn>
2010-03-18 07:32:39 +0000

    changed: use ReloadSkin rather than DelayLoadSkin for the skin theme builtin
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28646 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/Builtins.cpp
+++ xbmc/utils/Builtins.cpp
@@ -881,7 +881,7 @@ int CBuiltins::Execute(const CStdString& execString)
     // also set the default color theme
     g_guiSettings.SetString("lookandfeel.skincolors", CUtil::ReplaceExtension(strSkinTheme, ".xml"));
 
-    g_application.DelayLoadSkin();
+    g_application.ReloadSkin();
   }
   else if (execute.Equals("skin.setstring") || execute.Equals("skin.setimage") || execute.Equals("skin.setfile") ||
            execute.Equals("skin.setpath") || execute.Equals("skin.setnumeric") || execute.Equals("skin.setlargeimage"))
