13fa6272d4447a67b37578844aad602d7c343650
vdrfan <vdrfan@svn>
2010-03-21 17:06:17 +0000

    changed: unified CVideoDatabase::SetDetailsFor* in order to use return value of AddMovieAndGetThumb
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28733 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoDatabase.cpp
+++ xbmc/VideoDatabase.cpp
@@ -1905,7 +1905,7 @@ int CVideoDatabase::SetDetailsForEpisode(const CStdString& strFilenameAndPath, c
   return -1;
 }
 
-void CVideoDatabase::SetDetailsForMusicVideo(const CStdString& strFilenameAndPath, const CVideoInfoTag& details)
+int CVideoDatabase::SetDetailsForMusicVideo(const CStdString& strFilenameAndPath, const CVideoInfoTag& details)
 {
   try
   {
@@ -1923,7 +1923,7 @@ void CVideoDatabase::SetDetailsForMusicVideo(const CStdString& strFilenameAndPat
     if (idMVideo < 0)
     {
       CommitTransaction();
-      return;
+      return -1;
     }
 
     vector<int> vecDirectors;
@@ -1970,11 +1970,13 @@ void CVideoDatabase::SetDetailsForMusicVideo(const CStdString& strFilenameAndPat
     sql += FormatSQL(" where idMVideo=%i", idMVideo);
     m_pDS->exec(sql.c_str());
     CommitTransaction();
+    return idMVideo;
   }
   catch (...)
   {
     CLog::Log(LOGERROR, "%s (%s) failed", __FUNCTION__, strFilenameAndPath.c_str());
   }
+  return -1;
 }
 
 void CVideoDatabase::SetStreamDetailsForFile(const CStreamDetails& details, const CStdString &strFileNameAndPath)
