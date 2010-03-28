c33b43e94b05b02468630aef899cbfa8f49a52d4
elupus <elupus@svn>
2010-03-28 15:46:36 +0000

    changed: for now prefer ConfigBitStreamRaw=1 for dxva, since there seem to be a bug in ffmpeg regarding mode 2 and multiple slices
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28914 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
@@ -531,10 +531,11 @@ bool CDecoder::OpenDecoder(AVCodecContext *avctx)
   for(unsigned i = 0; i< cfg_count; i++)
   {
     CLog::Log(LOGDEBUG, "DXVA - bitstream type %d", cfg_list[i].ConfigBitstreamRaw);
-    if(config.ConfigBitstreamRaw == 0 && cfg_list[i].ConfigBitstreamRaw)
+    if(config.ConfigBitstreamRaw == 0 && cfg_list[i].ConfigBitstreamRaw == 1)
       config = cfg_list[i];
-    if(config.ConfigBitstreamRaw == 1 && cfg_list[i].ConfigBitstreamRaw == 2)
-      config = cfg_list[i];
+    //ConfigBitstreamRaw = 2 seems to be broken in current ffmpeg, so prefer mode 1 for now
+    //if(config.ConfigBitstreamRaw == 1 && cfg_list[i].ConfigBitstreamRaw == 2)
+    //  config = cfg_list[i];
   }
   CoTaskMemFree(cfg_list);
 
