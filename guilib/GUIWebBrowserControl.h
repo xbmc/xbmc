3d997978faacbb275586d7ed6b218dc1951c96f2
ceros7 <ceros7@svn>
2010-04-01 22:45:45 +0000

    Remove unused code from GUIWebBrowserControl source files, essentially starting over with these files.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/llqtwebkit-browser@28962 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIWebBrowserControl.h
+++ guilib/GUIWebBrowserControl.h
@@ -32,12 +32,6 @@
 #include "GUIControl.h"
 #include "GUIEmbeddedBrowserWindowObserver.h"
 
-// #include "berkelium/Berkelium.hpp"
-// #include "berkelium/Window.hpp"
-// #include "berkelium/WindowDelegate.hpp"
-
-// class GlDelegate;
-
 class CGUIWebBrowserControl : public CGUIControl
 {
 public:
@@ -46,33 +40,14 @@ public:
   virtual CGUIWebBrowserControl *Clone() const { return new CGUIWebBrowserControl(*this); };
 
   virtual void Render();
-  //virtual bool OnMessage(CGUIMessage& message);
-  //virtual bool OnAction(const CAction &action);
   virtual void AllocResources();
   virtual void FreeResources();
   virtual EVENT_RESULT OnMouseEvent(const CPoint &point, const CMouseEvent &event);
-
   void Back();
   void Forward();
-
-#if 0
-  bool m_bKeepSession;
-  bool m_isZoomed;
-
-protected:
-  bool OnMouseOver(const CPoint &point);
-  bool OnMouseWheel(char wheel, const CPoint &point);
-  bool OnMouseClick(DWORD dwButton, const CPoint &point);
-
-  int m_realPosX, m_realPosY, m_realHeight, m_realWidth;
-  bool m_bDirty;    /* to prevent two operations in the same loop which cause bad things during scrolling */
-#endif
-
 private:
   unsigned int m_texture;
   CGUIEmbeddedBrowserWindowObserver *m_observer;
-//   Berkelium::Window *m_window;
-//   GlDelegate *m_delegate;
 };
 
 #endif
