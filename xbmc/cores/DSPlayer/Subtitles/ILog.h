98b2d18676e4766cf40918798391acbe068530a0
blinkseb <blinkseb@svn>
2010-04-13 09:54:09 +0000

    [DSPlayer] forget in r29208
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/dsplayer@29230 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/DSPlayer/Subtitles/ILog.h
@@ -0,0 +1,42 @@
+#pragma once
+
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#define LOGDEBUG   0
+#define LOGINFO    1
+#define LOGNOTICE  2
+#define LOGWARNING 3
+#define LOGERROR   4
+#define LOGSEVERE  5
+#define LOGFATAL   6
+#define LOGNONE    7
+
+/**
+ * ILog: Allow the subtitles dll to log message within XBMC
+ */
+class ILog
+{
+public:
+  virtual void Log(int loglevel, const char *format, ...) = 0;
+};
+
+extern ILog* g_log;
