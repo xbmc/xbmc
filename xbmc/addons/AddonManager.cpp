d10c108e7af5bda0b635af3915e141c683e326dc
jmarshallnz <jmarshallnz@svn>
2010-04-26 23:19:38 +0000

    fixed: missing addon summary.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29524 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/addons/AddonManager.cpp
+++ xbmc/addons/AddonManager.cpp
@@ -555,8 +555,7 @@ bool CAddonMgr::AddonFromInfoXML(const TiXmlElement *rootElement,
   }
 
   /* Retrieve summary */
-  CStdString summary;
-  if (!GetTranslatedString(rootElement,"summary",summary))
+  if (!GetTranslatedString(rootElement,"summary",addonProps.summary))
   {
     CLog::Log(LOGERROR, "ADDON: %s missing <summary> element, ignoring", rootElement->GetDocument()->Value());
     return false;
