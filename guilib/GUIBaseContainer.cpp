xbmc/xbmc

commit 022762f72b3da67ba6b00ee876d6161acdbaa5f6

changed: moved CSkinInfo to namespace ADDON
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29510 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/addons/Skin.h
@@ -28,6 +28,9 @@
 
 class TiXmlNode;
 
+namespace ADDON
+{
+
 class CSkinInfo
 {
 public:
@@ -131,4 +134,5 @@ protected:
   bool m_debugging;
 };
 
-extern CSkinInfo g_SkinInfo;
+} /*namespace ADDON*/
+extern ADDON::CSkinInfo g_SkinInfo;

xbmc/lib/libPython/xbmcmodule/winxml.cpp
@@ -44,6 +44,7 @@
 #include "FileSystem/File.h"
 
 using namespace std;
+using ADDON::CSkinInfo;
 
 #define ACTIVE_WINDOW g_windowManager.GetActiveWindow()
 

xbmc/lib/libPython/xbmcmodule/winxmldialog.cpp
@@ -57,6 +57,7 @@ extern "C" {
 #endif
 
 using namespace std;
+using ADDON::CSkinInfo;
 
 namespace PYXBMC
 {

xbmc/Application.cpp
@@ -269,6 +269,7 @@
 #endif
 
 using namespace std;
+using namespace ADDON;
 using namespace XFILE;
 #ifdef HAS_DVD_DRIVE
 using namespace MEDIA_DETECT;

xbmc/GUIWindowSettingsCategory.cpp
@@ -1460,7 +1460,7 @@ void CGUIWindowSettingsCategory::OnSettingChanged(CBaseSettingControl *pSettingC
     CGUISpinControlEx *pControl = (CGUISpinControlEx *)GetControl(pSettingControl->GetID());
     CStdString strSkin = pControl->GetCurrentLabel();
     CStdString strSkinPath = g_settings.GetSkinFolder(strSkin);
-    if (CSkinInfo::Check(strSkinPath))
+    if (ADDON::CSkinInfo::Check(strSkinPath))
     {
       m_strErrorMessage.Empty();
       pControl->SetItemInvalid(false);

xbmc/addons/IAddon.h
@@ -62,6 +62,8 @@ namespace ADDON
   typedef boost::shared_ptr<IAddon> AddonPtr;
   class CVisualisation;
   typedef boost::shared_ptr<CVisualisation> VizPtr;
+  class CSkinInfo;
+  typedef boost::shared_ptr<CSkinInfo> SkinPtr;
 
   class CAddonMgr;
   class AddonVersion;

xbmc/addons/Skin.cpp
@@ -35,6 +35,9 @@ using namespace XFILE;
 
 #define SKIN_MIN_VERSION 2.1f
 
+namespace ADDON
+{
+
 CSkinInfo g_SkinInfo; // global
 
 CSkinInfo::CSkinInfo()
@@ -350,3 +353,5 @@ int CSkinInfo::GetFirstWindow() const
     startWindow = WINDOW_STARTUP_ANIM;
   return startWindow;
 }
+
+} /*namespace ADDON*/

commit 04c59f7aca6eeb7128b1fd67f3a9dba74919ad42

fixed: fallout after r29510 (except build system stuff)
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29514 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

guilib/GUIBaseContainer.cpp
@@ -34,6 +34,7 @@
 #include "MathUtils.h"
 
 using namespace std;
+using namespace ADDON;
 
 #define HOLD_TIME_START 100
 #define HOLD_TIME_END   3000

guilib/GUIButtonScroller.cpp
@@ -32,6 +32,7 @@
 #include "Key.h"
 
 using namespace std;
+using namespace ADDON;
 
 #define SCROLL_SPEED 6.0f
 #define ANALOG_SCROLL_START 0.8f

guilib/GUIColorManager.cpp
@@ -54,7 +54,7 @@ void CGUIColorManager::Load(const CStdString &colorFile)
 
   // first load the default color map if it exists
   CStdString path, basePath;
-  CUtil::AddFileToFolder(g_SkinInfo.GetBaseDir(), "colors", basePath);
+  CUtil::AddFileToFolder(ADDON::g_SkinInfo.GetBaseDir(), "colors", basePath);
   CUtil::AddFileToFolder(basePath, "defaults.xml", path);
 
   if (xmlDoc.LoadFile(PTH_IC(path)))

guilib/GUIControlFactory.cpp
@@ -66,6 +66,7 @@
 #include "StringUtils.h"
 
 using namespace std;
+using namespace ADDON;
 
 typedef struct
 {

guilib/GUIFontManager.cpp
@@ -402,7 +402,7 @@ void GUIFontManager::LoadFonts(const TiXmlNode* fontNode)
 bool GUIFontManager::OpenFontFile(TiXmlDocument& xmlDoc)
 {
   // Get the file to load fonts from:
-  CStdString strPath = g_SkinInfo.GetSkinPath("Font.xml", &m_skinResolution);
+  CStdString strPath = ADDON::g_SkinInfo.GetSkinPath("Font.xml", &m_skinResolution);
   CLog::Log(LOGINFO, "Loading fonts from %s", strPath.c_str());
 
   // first try our preferred file

guilib/GUIIncludes.cpp
@@ -26,6 +26,7 @@
 #include "tinyXML/tinyxml.h"
 
 using namespace std;
+using namespace ADDON;
 
 CGUIIncludes::CGUIIncludes()
 {

guilib/GUIListItemLayout.cpp
@@ -29,6 +29,7 @@
 #include "tinyXML/tinyxml.h"
 
 using namespace std;
+using namespace ADDON;
 
 CGUIListItemLayout::CGUIListItemLayout()
 : m_group(0, 0, 0, 0, 0, 0)

guilib/GUIWindow.cpp
@@ -45,6 +45,7 @@
 #endif
 
 using namespace std;
+using namespace ADDON;
 
 CGUIWindow::CGUIWindow(int id, const CStdString &xmlFile)
 {

guilib/GUIWindowManager.cpp
@@ -363,7 +363,7 @@ void CGUIWindowManager::ActivateWindow_Internal(int iWindowID, const vector<CStd
   }
   if (iWindowID == WINDOW_START)
   { // virtual start window
-    iWindowID = g_SkinInfo.GetStartWindow();
+    iWindowID = ADDON::g_SkinInfo.GetStartWindow();
   }
 
   // debug

guilib/VisibleEffect.cpp
@@ -28,6 +28,7 @@
 #include "tinyXML/tinyxml.h"
 
 using namespace std;
+using namespace ADDON;
 
 CAnimEffect::CAnimEffect(const TiXmlElement *node, EFFECT_TYPE effect)
 {

xbmc/GUIDialogAudioSubtitleSettings.cpp
@@ -69,7 +69,7 @@ CGUIDialogAudioSubtitleSettings::~CGUIDialogAudioSubtitleSettings(void)
 
 void CGUIDialogAudioSubtitleSettings::CreateSettings()
 {
-  m_usePopupSliders = g_SkinInfo.HasSkinFile("DialogSlider.xml");
+  m_usePopupSliders = ADDON::g_SkinInfo.HasSkinFile("DialogSlider.xml");
 
   // clear out any old settings
   m_settings.clear();

xbmc/GUIDialogVideoSettings.cpp
@@ -67,7 +67,7 @@ CGUIDialogVideoSettings::~CGUIDialogVideoSettings(void)
 
 void CGUIDialogVideoSettings::CreateSettings()
 {
-  m_usePopupSliders = g_SkinInfo.HasSkinFile("DialogSlider.xml");
+  m_usePopupSliders = ADDON::g_SkinInfo.HasSkinFile("DialogSlider.xml");
   // clear out any old settings
   m_settings.clear();
   // create our settings

xbmc/GUIWindowLoginScreen.cpp
@@ -298,7 +298,7 @@ void CGUIWindowLoginScreen::LoadProfile(unsigned int profile)
 #ifdef HAS_PYTHON
   g_pythonParser.m_bLogin = true;
 #endif
-  g_windowManager.ChangeActiveWindow(g_SkinInfo.GetFirstWindow());
+  g_windowManager.ChangeActiveWindow(ADDON::g_SkinInfo.GetFirstWindow());
 
   g_application.UpdateLibraries();
 }

xbmc/addons/Skin.h
@@ -134,5 +134,6 @@ protected:
   bool m_debugging;
 };
 
+extern CSkinInfo g_SkinInfo;
+
 } /*namespace ADDON*/
-extern ADDON::CSkinInfo g_SkinInfo;

xbmc/lib/libPython/xbmcmodule/pyutil.cpp
@@ -105,7 +105,7 @@ namespace PYXBMC
     control.SetAttribute("type", cControlType);
     TiXmlElement filler("description");
     control.InsertEndChild(filler);
-    g_SkinInfo.ResolveIncludes(&control, cControlType);
+    ADDON::g_SkinInfo.ResolveIncludes(&control, cControlType);
 
     // ok, now check for our texture type
     TiXmlElement *pTexture = control.FirstChildElement(cTextureType);

xbmc/lib/libPython/xbmcmodule/winxml.cpp
@@ -44,7 +44,7 @@
 #include "FileSystem/File.h"
 
 using namespace std;
-using ADDON::CSkinInfo;
+using namespace ADDON;
 
 #define ACTIVE_WINDOW g_windowManager.GetActiveWindow()
 

xbmc/lib/libPython/xbmcmodule/winxmldialog.cpp
@@ -57,7 +57,7 @@ extern "C" {
 #endif
 
 using namespace std;
-using ADDON::CSkinInfo;
+using namespace ADDON;
 
 namespace PYXBMC
 {

xbmc/utils/GUIInfoManager.cpp
@@ -3805,7 +3805,7 @@ CStdString CGUIInfoManager::GetItemLabel(const CFileItem *item, int info) const
       if(strThumb.IsEmpty() && !item->GetIconImage().IsEmpty())
       {
         strThumb = item->GetIconImage();
-        if (g_SkinInfo.GetVersion() <= 2.10)
+        if (ADDON::g_SkinInfo.GetVersion() <= 2.10)
           strThumb.Insert(strThumb.Find("."), "Big");
       }
       return strThumb;