d464d91cc58cc43e5e911110ff25fe28cb0cc52c
alcoheca <alcoheca@svn>
2010-05-07 08:05:46 +0000

    fixed: missing from last commit
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29877 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/addons/Addon.cpp
+++ xbmc/addons/Addon.cpp
@@ -239,7 +239,8 @@ CAddon::CAddon(cp_plugin_info_t *props)
   : m_props(props)
   , m_parent(AddonPtr())
 {
-  BuildLibName(props);
+  if (m_props.type != ADDON_VIZ && m_props.type != ADDON_SCREENSAVER)
+    BuildLibName(props);
   BuildProfilePath();
   CUtil::AddFileToFolder(Profile(), "settings.xml", m_userSettingsPath);
   m_enabled = true;
