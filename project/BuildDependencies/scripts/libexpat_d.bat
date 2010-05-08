fce0d151c6aed0c9e1120b5f96eb206b78428098
wiso <wiso@svn>
2010-05-08 19:55:35 +0000

    [WIN32] initial commit of downloader scripts to full fill the build dependencies (call DownloadBuildDeps.bat and add trunk/project/BuildDependencies/include and trunk/project/BuildDependencies/lib to the vs directories)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29931 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ project/BuildDependencies/scripts/libexpat_d.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+
+rmdir tmp /S /Q
+md tmp
+cd tmp
+
+echo Downloading libexpat ...
+echo 
+%WGET% "http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/expat_2.0.1-1_win32.zip"
+%WGET% "http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/expat-dev_2.0.1-1_win32.zip"
+
+%ZIP% x expat_2.0.1-1_win32.zip
+%ZIP% x expat-dev_2.0.1-1_win32.zip
+
+copy include\* %CUR_PATH%\include\ /Y
+copy lib\expat.lib %CUR_PATH%\lib\libexpat.lib /Y
+copy bin\libexpat-1.dll %XBMC_PATH%\system\libexpat.dll
+
+cd ..
