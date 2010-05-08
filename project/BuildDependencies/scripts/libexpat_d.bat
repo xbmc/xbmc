b22193da3e916356d3180818bd11482d76eadd66
wiso <wiso@svn>
2010-05-08 20:55:36 +0000

    [WIN32] download requirements for libass (fontconfig)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29934 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/BuildDependencies/scripts/libexpat_d.bat
+++ project/BuildDependencies/scripts/libexpat_d.bat
@@ -5,7 +5,7 @@ md tmp
 cd tmp
 
 echo Downloading libexpat ...
-echo 
+echo ------------------------
 %WGET% "http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/expat_2.0.1-1_win32.zip"
 %WGET% "http://ftp.gnome.org/pub/gnome/binaries/win32/dependencies/expat-dev_2.0.1-1_win32.zip"
 
@@ -15,5 +15,7 @@ echo
 copy include\* %CUR_PATH%\include\ /Y
 copy lib\expat.lib %CUR_PATH%\lib\libexpat.lib /Y
 copy bin\libexpat-1.dll %XBMC_PATH%\system\libexpat.dll
+rem libexpat-1.dll for libfontconfig-1.dll which is needed for libass.dll
+copy bin\libexpat-1.dll %XBMC_PATH%\system\players\dvdplayer\
 
 cd ..
