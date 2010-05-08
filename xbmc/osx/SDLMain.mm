96a56f5a745a0e2646bdea12803aa7118874e863
davilla <davilla@svn>
2010-05-08 22:01:23 +0000

    cosmetics, re-indent to 2 spaces
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29939 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/osx/SDLMain.mm
+++ xbmc/osx/SDLMain.mm
@@ -55,98 +55,98 @@ static BOOL   gCalledAppMainline = FALSE;
 
 static NSString *getApplicationName(void)
 {
-    NSDictionary *dict;
-    NSString *appName = 0;
+  NSDictionary *dict;
+  NSString *appName = 0;
 
-    // Determine the application name
-    dict = (NSDictionary *)CFBundleGetInfoDictionary(CFBundleGetMainBundle());
-    if (dict)
-        appName = [dict objectForKey: @"CFBundleName"];
-    
-    if (![appName length])
-        appName = [[NSProcessInfo processInfo] processName];
+  // Determine the application name
+  dict = (NSDictionary *)CFBundleGetInfoDictionary(CFBundleGetMainBundle());
+  if (dict)
+    appName = [dict objectForKey: @"CFBundleName"];
 
-    return appName;
+  if (![appName length])
+    appName = [[NSProcessInfo processInfo] processName];
+
+  return appName;
 }
 static void setupApplicationMenu(void)
 {
-    // warning: this code is very odd
-    NSMenu *appleMenu;
-    NSMenuItem *menuItem;
-    NSString *title;
-    NSString *appName;
-    
-    appName = getApplicationName();
-    appleMenu = [[NSMenu alloc] initWithTitle:@""];
-    
-    // Add menu items
-    title = [@"About " stringByAppendingString:appName];
-    [appleMenu addItemWithTitle:title action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
+  // warning: this code is very odd
+  NSMenu *appleMenu;
+  NSMenuItem *menuItem;
+  NSString *title;
+  NSString *appName;
 
-    [appleMenu addItem:[NSMenuItem separatorItem]];
+  appName = getApplicationName();
+  appleMenu = [[NSMenu alloc] initWithTitle:@""];
 
-    title = [@"Hide " stringByAppendingString:appName];
-    [appleMenu addItemWithTitle:title action:@selector(hide:) keyEquivalent:@"h"];
+  // Add menu items
+  title = [@"About " stringByAppendingString:appName];
+  [appleMenu addItemWithTitle:title action:@selector(orderFrontStandardAboutPanel:) keyEquivalent:@""];
 
-    menuItem = (NSMenuItem *)[appleMenu addItemWithTitle:@"Hide Others" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
-    [menuItem setKeyEquivalentModifierMask:(NSAlternateKeyMask|NSCommandKeyMask)];
+  [appleMenu addItem:[NSMenuItem separatorItem]];
 
-    [appleMenu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
+  title = [@"Hide " stringByAppendingString:appName];
+  [appleMenu addItemWithTitle:title action:@selector(hide:) keyEquivalent:@"h"];
 
-    [appleMenu addItem:[NSMenuItem separatorItem]];
+  menuItem = (NSMenuItem *)[appleMenu addItemWithTitle:@"Hide Others" action:@selector(hideOtherApplications:) keyEquivalent:@"h"];
+  [menuItem setKeyEquivalentModifierMask:(NSAlternateKeyMask|NSCommandKeyMask)];
 
-    title = [@"Quit " stringByAppendingString:appName];
-    [appleMenu addItemWithTitle:title action:@selector(terminate:) keyEquivalent:@"q"];
+  [appleMenu addItemWithTitle:@"Show All" action:@selector(unhideAllApplications:) keyEquivalent:@""];
 
-    
-    // Put menu into the menubar
-    menuItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [menuItem setSubmenu:appleMenu];
-    [[NSApp mainMenu] addItem:menuItem];
+  [appleMenu addItem:[NSMenuItem separatorItem]];
 
-    // Tell the application object that this is now the application menu
-    [NSApp setAppleMenu:appleMenu];
+  title = [@"Quit " stringByAppendingString:appName];
+  [appleMenu addItemWithTitle:title action:@selector(terminate:) keyEquivalent:@"q"];
 
-    // Finally give up our references to the objects
-    [appleMenu release];
-    [menuItem release];
+
+  // Put menu into the menubar
+  menuItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
+  [menuItem setSubmenu:appleMenu];
+  [[NSApp mainMenu] addItem:menuItem];
+
+  // Tell the application object that this is now the application menu
+  [NSApp setAppleMenu:appleMenu];
+
+  // Finally give up our references to the objects
+  [appleMenu release];
+  [menuItem release];
 }
 
 // Create a window menu
 static void setupWindowMenu(void)
 {
-    NSMenu      *windowMenu;
-    NSMenuItem  *windowMenuItem;
-    NSMenuItem  *menuItem;
+  NSMenu      *windowMenu;
+  NSMenuItem  *windowMenuItem;
+  NSMenuItem  *menuItem;
 
-    windowMenu = [[NSMenu alloc] initWithTitle:@"Window"];
-    
-    // "Full/Windowed Toggle" item
-    menuItem = [[NSMenuItem alloc] initWithTitle:@"Full/Windowed Toggle" action:@selector(fullScreenToggle:) keyEquivalent:@"f"];
-    [windowMenu addItem:menuItem];
-    [menuItem release];
+  windowMenu = [[NSMenu alloc] initWithTitle:@"Window"];
 
-    // "Full/Windowed Toggle" item
-    menuItem = [[NSMenuItem alloc] initWithTitle:@"Float on Top" action:@selector(floatOnTopToggle:) keyEquivalent:@"t"];
-    [windowMenu addItem:menuItem];
-    [menuItem release];
+  // "Full/Windowed Toggle" item
+  menuItem = [[NSMenuItem alloc] initWithTitle:@"Full/Windowed Toggle" action:@selector(fullScreenToggle:) keyEquivalent:@"f"];
+  [windowMenu addItem:menuItem];
+  [menuItem release];
 
-    // "Minimize" item
-    menuItem = [[NSMenuItem alloc] initWithTitle:@"Minimize" action:@selector(performMiniaturize:) keyEquivalent:@"m"];
-    [windowMenu addItem:menuItem];
-    [menuItem release];
-    
-    // Put menu into the menubar
-    windowMenuItem = [[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""];
-    [windowMenuItem setSubmenu:windowMenu];
-    [[NSApp mainMenu] addItem:windowMenuItem];
-    
-    // Tell the application object that this is now the window menu
-    [NSApp setWindowsMenu:windowMenu];
+  // "Full/Windowed Toggle" item
+  menuItem = [[NSMenuItem alloc] initWithTitle:@"Float on Top" action:@selector(floatOnTopToggle:) keyEquivalent:@"t"];
+  [windowMenu addItem:menuItem];
+  [menuItem release];
 
-    // Finally give up our references to the objects
-    [windowMenu release];
-    [windowMenuItem release];
+  // "Minimize" item
+  menuItem = [[NSMenuItem alloc] initWithTitle:@"Minimize" action:@selector(performMiniaturize:) keyEquivalent:@"m"];
+  [windowMenu addItem:menuItem];
+  [menuItem release];
+
+  // Put menu into the menubar
+  windowMenuItem = [[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""];
+  [windowMenuItem setSubmenu:windowMenu];
+  [[NSApp mainMenu] addItem:windowMenuItem];
+
+  // Tell the application object that this is now the window menu
+  [NSApp setWindowsMenu:windowMenu];
+
+  // Finally give up our references to the objects
+  [windowMenu release];
+  [windowMenuItem release];
 }
 
 @interface XBMCApplication : NSApplication
@@ -163,14 +163,14 @@ static void setupWindowMenu(void)
 // Invoked from the Quit menu item
 - (void)terminate:(id)sender
 {
-    // remove any notification handlers
-    [[[NSWorkspace sharedWorkspace] notificationCenter] removeObserver:self];
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+  // remove any notification handlers
+  [[[NSWorkspace sharedWorkspace] notificationCenter] removeObserver:self];
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
 
-    // Post a SDL_QUIT event
-    SDL_Event event;
-    event.type = SDL_QUIT;
-    SDL_PushEvent(&event);
+  // Post a SDL_QUIT event
+  SDL_Event event;
+  event.type = SDL_QUIT;
+  SDL_PushEvent(&event);
 }
 
 - (void)fullScreenToggle:(id)sender
@@ -218,7 +218,6 @@ static void setupWindowMenu(void)
 		CFRelease(url);
 		CFRelease(url2);
   }
-
 }
 
 - (void) applicationWillTerminate: (NSNotification *) note
@@ -246,55 +245,56 @@ static void setupWindowMenu(void)
 // Called after the internal event loop has started running.
 - (void) applicationDidFinishLaunching: (NSNotification *) note
 {
-    // enable multithreading, we should NOT have to do this but as we are mixing NSThreads/pthreads...
-    if (![NSThread isMultiThreaded])
-      [NSThread detachNewThreadSelector:@selector(kickstartMultiThreaded:) toTarget:self withObject:nil];
+  // enable multithreading, we should NOT have to do this but as we are mixing NSThreads/pthreads...
+  if (![NSThread isMultiThreaded])
+    [NSThread detachNewThreadSelector:@selector(kickstartMultiThreaded:) toTarget:self withObject:nil];
 
-    // Set the working directory to the .app's parent directory
-    [self setupWorkingDirectory:gFinderLaunch];
+  // Set the working directory to the .app's parent directory
+  [self setupWorkingDirectory:gFinderLaunch];
 
-    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
-      selector:@selector(deviceDidMountNotification:)
-      name:NSWorkspaceDidMountNotification
-      object:nil];
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+    selector:@selector(deviceDidMountNotification:)
+    name:NSWorkspaceDidMountNotification
+    object:nil];
 
-    [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
-      selector:@selector(deviceDidUnMountNotification:)
-      name:NSWorkspaceDidUnmountNotification
-      object:nil];
-		
-    [[NSNotificationCenter defaultCenter] addObserver:self
-      selector:@selector(windowDidMoveNotification:)
-      name:NSWindowDidMoveNotification
-      object:nil];
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+    selector:@selector(deviceDidUnMountNotification:)
+    name:NSWorkspaceDidUnmountNotification
+    object:nil];
 
-    [[NSNotificationCenter defaultCenter] addObserver:self
-      selector:@selector(windowDidReSizeNotification:)
-      name:NSWindowDidResizeNotification
-      object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(windowDidMoveNotification:)
+    name:NSWindowDidMoveNotification
+    object:nil];
+
+  [[NSNotificationCenter defaultCenter] addObserver:self
+    selector:@selector(windowDidReSizeNotification:)
+    name:NSWindowDidResizeNotification
+    object:nil];
       
-    // We're going to manually manage the screensaver.
-    setenv("SDL_VIDEO_ALLOW_SCREENSAVER", "1", true);
+  // We're going to manually manage the screensaver.
+  setenv("SDL_VIDEO_ALLOW_SCREENSAVER", "1", true);
 
-    // Hand off to main application code
-    gCalledAppMainline = TRUE;
+  // Hand off to main application code
+  gCalledAppMainline = TRUE;
 
-    // stop the main loop so we return to main (below) and can
-    // call SDL_main there.
-    [NSApp stop:nil];
+  // stop the main loop so we return to main (below) and can
+  // call SDL_main there.
+  [NSApp stop:nil];
 
-    //post a NOP event, so the run loop actually stops
-    //see http://www.cocoabuilder.com/archive/cocoa/219842-nsapp-stop.html
-    NSEvent* event = [NSEvent otherEventWithType: NSApplicationDefined
-                                        location: NSMakePoint(0,0)
-                                   modifierFlags: 0
-                                       timestamp: 0.0
-                                    windowNumber: 0
-                                         context: nil
-                                         subtype: 0
-                                           data1: 0
-                                           data2: 0];
-    [NSApp postEvent: event atStart: true];
+  //post a NOP event, so the run loop actually stops
+  //see http://www.cocoabuilder.com/archive/cocoa/219842-nsapp-stop.html
+  NSEvent* event = [NSEvent otherEventWithType: NSApplicationDefined
+    location: NSMakePoint(0,0)
+    modifierFlags: 0
+    timestamp: 0.0
+    windowNumber: 0
+    context: nil
+    subtype: 0
+    data1: 0
+    data2: 0];
+  //
+  [NSApp postEvent: event atStart: true];
 }
 
 /*
@@ -314,37 +314,38 @@ static void setupWindowMenu(void)
  */
 - (BOOL)application:(NSApplication *)theApplication openFile:(NSString *)filename
 {
-    const char *temparg;
-    size_t arglen;
-    char *arg;
-    char **newargv;
+  const char *temparg;
+  size_t arglen;
+  char *arg;
+  char **newargv;
 
-    // MacOS is passing command line args.
-    if (!gFinderLaunch)
-        return FALSE;
+  // MacOS is passing command line args.
+  if (!gFinderLaunch)
+    return FALSE;
 
-    // app has started, ignore this document.
-    if (gCalledAppMainline)
-        return FALSE;
+  // app has started, ignore this document.
+  if (gCalledAppMainline)
+    return FALSE;
 
-    temparg = [filename UTF8String];
-    arglen = SDL_strlen(temparg) + 1;
-    arg = (char *) SDL_malloc(arglen);
-    if (arg == NULL)
-        return FALSE;
+  temparg = [filename UTF8String];
+  arglen = SDL_strlen(temparg) + 1;
+  arg = (char *) SDL_malloc(arglen);
+  if (arg == NULL)
+    return FALSE;
 
-    newargv = (char **) realloc(gArgv, sizeof (char *) * (gArgc + 2));
-    if (newargv == NULL)
-    {
-        SDL_free(arg);
-        return FALSE;
-    }
-    gArgv = newargv;
+  newargv = (char **) realloc(gArgv, sizeof (char *) * (gArgc + 2));
+  if (newargv == NULL)
+  {
+    SDL_free(arg);
+    return FALSE;
+  }
+  gArgv = newargv;
 
-    SDL_strlcpy(arg, temparg, arglen);
-    gArgv[gArgc++] = arg;
-    gArgv[gArgc] = NULL;
-    return TRUE;
+  SDL_strlcpy(arg, temparg, arglen);
+  gArgv[gArgc++] = arg;
+  gArgv[gArgc] = NULL;
+
+  return TRUE;
 }
 
 - (void) deviceDidMountNotification:(NSNotification *) note 
@@ -391,7 +392,6 @@ static void setupWindowMenu(void)
   }
   */
 }
-
 @end
 
 #ifdef main
@@ -400,68 +400,68 @@ static void setupWindowMenu(void)
 /* Main entry point to executable - should *not* be SDL_main! */
 int main(int argc, char *argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    XBMCDelegate *xbmc_delegate;
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  XBMCDelegate *xbmc_delegate;
 
-    // Block SIGPIPE
-    // SIGPIPE repeatably kills us, turn it off
-    {
-      sigset_t set;
-      sigemptyset(&set);
-      sigaddset(&set, SIGPIPE);
-      sigprocmask(SIG_BLOCK, &set, NULL);
-    }
+  // Block SIGPIPE
+  // SIGPIPE repeatably kills us, turn it off
+  {
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGPIPE);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+  }
 
-    /* Copy the arguments into a global variable */
-    /* This is passed if we are launched by double-clicking */
-    if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) {
-        gArgv = (char **) SDL_malloc(sizeof (char *) * 2);
-        gArgv[0] = argv[0];
-        gArgv[1] = NULL;
-        gArgc = 1;
-        gFinderLaunch = YES;
-    } else {
-        int i;
-        gArgc = argc;
-        gArgv = (char **) SDL_malloc(sizeof (char *) * (argc+1));
-        for (i = 0; i <= argc; i++)
-            gArgv[i] = argv[i];
-        gFinderLaunch = NO;
-    }
+  /* Copy the arguments into a global variable */
+  /* This is passed if we are launched by double-clicking */
+  if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) {
+    gArgv = (char **) SDL_malloc(sizeof (char *) * 2);
+    gArgv[0] = argv[0];
+    gArgv[1] = NULL;
+    gArgc = 1;
+    gFinderLaunch = YES;
+  } else {
+    gArgc = argc;
+    gArgv = (char **) SDL_malloc(sizeof (char *) * (argc+1));
+    for (int i = 0; i <= argc; i++)
+        gArgv[i] = argv[i];
+    gFinderLaunch = NO;
+  }
 
-    // Ensure the application object is initialised
-    [XBMCApplication sharedApplication];
-    
+  // Ensure the application object is initialised
+  [XBMCApplication sharedApplication];
+
 #ifdef SDL_USE_CPS
-    {
-        CPSProcessSerNum PSN;
-        /* Tell the dock about us */
-        if (!CPSGetCurrentProcess(&PSN))
-            if (!CPSEnableForegroundOperation(&PSN,0x03,0x3C,0x2C,0x1103))
-                if (!CPSSetFrontProcess(&PSN))
-                    [XBMCApplication sharedApplication];
-    }
+  {
+    CPSProcessSerNum PSN;
+    /* Tell the dock about us */
+    if (!CPSGetCurrentProcess(&PSN))
+      if (!CPSEnableForegroundOperation(&PSN,0x03,0x3C,0x2C,0x1103))
+        if (!CPSSetFrontProcess(&PSN))
+          [XBMCApplication sharedApplication];
+  }
 #endif
 
-    // Set up the menubars
-    [NSApp setMainMenu:[[NSMenu alloc] init]];
-    setupApplicationMenu();
-    setupWindowMenu();
+  // Set up the menubars
+  [NSApp setMainMenu:[[NSMenu alloc] init]];
+  setupApplicationMenu();
+  setupWindowMenu();
 
-    // Create XBMCDelegate and make it the app delegate
-    xbmc_delegate = [[XBMCDelegate alloc] init];
-    [NSApp setDelegate:xbmc_delegate];
+  // Create XBMCDelegate and make it the app delegate
+  xbmc_delegate = [[XBMCDelegate alloc] init];
+  [NSApp setDelegate:xbmc_delegate];
 
-    // Start the main event loop
-    [NSApp run];
+  // Start the main event loop
+  [NSApp run];
 
-    // call SDL_main which calls our real main in xbmc.cpp
-    // we never return from here as quiting xbmc will call exit() directly.
-    // see http://lists.libsdl.org/pipermail/sdl-libsdl.org/2008-September/066542.html
-    SDL_main(gArgc, gArgv);
-    
-    [xbmc_delegate release];
-    [pool release];
-    return 0;
+  // call SDL_main which calls our real main in xbmc.cpp
+  // we never return from here as quiting xbmc will call exit() directly.
+  // see http://lists.libsdl.org/pipermail/sdl-libsdl.org/2008-September/066542.html
+  SDL_main(gArgc, gArgv);
+
+  [xbmc_delegate release];
+  [pool release];
+
+  return 0;
 }
 
