a4416efaf6c8d48ebd2af88e30cac21d80c1b3df
gnif2 <gnif2@svn>
2010-05-10 11:22:29 +0000

    fixed: passthrough does not enable when output mode is not digital, should fix #8751
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29986 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayerAudio.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerAudio.cpp
@@ -249,9 +249,10 @@ bool CDVDPlayerAudio::OpenDecoder(CDVDStreamInfo &hints, BYTE* buffer /* = NULL*
 
   /* store our stream hints */
   m_streaminfo = hints;
+  bool passthrough = g_guiSettings.GetInt("audiooutput.mode") == AUDIO_DIGITAL;
 
   CLog::Log(LOGNOTICE, "Finding audio codec for: %i", m_streaminfo.codec);
-  m_pAudioCodec = CDVDFactoryCodec::CreateAudioCodec( m_streaminfo );
+  m_pAudioCodec = CDVDFactoryCodec::CreateAudioCodec(m_streaminfo, passthrough);
   if( !m_pAudioCodec )
   {
     CLog::Log(LOGERROR, "Unsupported audio codec");
