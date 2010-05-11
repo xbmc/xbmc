a76b8eaf732038efa064636ee7732c48d01df463
davilla <davilla@svn>
2010-05-11 03:13:53 +0000

    cosmetics
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@29998 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/VideoRenderers/LinuxRenderer.cpp
+++ xbmc/cores/VideoRenderers/LinuxRenderer.cpp
@@ -469,16 +469,16 @@ void CLinuxRenderer::ReleaseImage(int source, bool preserve)
 
   // transform from YUV to RGB
   struct SwsContext *context = m_dllSwScale->sws_getContext(m_image.width, m_image.height, PIX_FMT_YUV420P, m_backbuffer->w, m_backbuffer->h, PIX_FMT_BGRA, SWS_BILINEAR, NULL, NULL, NULL);
-  uint8_t *src[] = { m_image.plane[0], m_image.plane[1], m_image.plane[2], 0 };
-  int     srcStride[] = { m_image.stride[0], m_image.stride[1], m_image.stride[2], 0 };
-  uint8_t *dst[] = { (uint8_t*)m_backbuffer->pixels, 0, 0, 0 };
-  int     dstStride[] = { m_backbuffer->pitch, 0, 0, 0 };
+  uint8_t *src[]  = { m_image.plane[0], m_image.plane[1], m_image.plane[2], 0 };
+  int srcStride[] = { m_image.stride[0], m_image.stride[1], m_image.stride[2], 0 };
+  uint8_t *dst[]  = { (uint8_t*)m_backbuffer->pixels, 0, 0, 0 };
+  int dstStride[] = { m_backbuffer->pitch, 0, 0, 0 };
 
   m_dllSwScale->sws_scale(context, src, srcStride, 0, m_image.height, dst, dstStride);
 
-for (int n=0; n<720*90;n++) {
-   *(((uint8_t*)m_backbuffer->pixels) + (720*10) + n) = 70;
-}
+  for (int n=0; n<720*90;n++) {
+    *(((uint8_t*)m_backbuffer->pixels) + (720*10) + n) = 70;
+  }
 
   m_dllSwScale->sws_freeContext(context);
 
