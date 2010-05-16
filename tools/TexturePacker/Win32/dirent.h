cd4c1f8fa4253ddf5ac126c043f5279db484ebc9
ceros7 <ceros7@svn>
2010-05-16 01:59:09 +0000

    Move copyright information to top of files.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30208 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/TexturePacker/Win32/dirent.h
+++ tools/TexturePacker/Win32/dirent.h
@@ -7,8 +7,17 @@
 
     Author:  Kevlin Henney (kevlin@acm.org, kevlin@curbralan.com)
     History: Created March 1997. Updated June 2003.
-    Rights:  See end of file.
-    
+
+    Copyright Kevlin Henney, 1997, 2003. All rights reserved.
+
+    Permission to use, copy, modify, and distribute this software and its
+    documentation for any purpose is hereby granted without fee, provided
+    that this copyright and permissions notice appear in all copies and
+    derivatives.
+
+    This software is supplied "as is" without express or implied warranty.
+
+    But that said, if there are any problems please get in touch.
 */
 
 #ifdef __cplusplus
@@ -38,21 +47,6 @@ DIR           *opendir(const char *);
 int           closedir(DIR *);
 struct dirent *readdir(DIR *);
 void          rewinddir(DIR *);
-
-/*
-
-    Copyright Kevlin Henney, 1997, 2003. All rights reserved.
-
-    Permission to use, copy, modify, and distribute this software and its
-    documentation for any purpose is hereby granted without fee, provided
-    that this copyright and permissions notice appear in all copies and
-    derivatives.
-    
-    This software is supplied "as is" without express or implied warranty.
-
-    But that said, if there are any problems please get in touch.
-
-*/
 
 #ifdef __cplusplus
 }
