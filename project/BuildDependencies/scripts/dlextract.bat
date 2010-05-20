795d615806f1a6c2b24c6b7f496d2268a4610cbf
CrystalPT <CrystalPT@svn>
2010-05-20 19:42:33 +0000

    [WIN32] Download the build dependencies only once, dl & extract refactored in independent .bat
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30354 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ project/BuildDependencies/scripts/dlextract.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+
+echo Downloading %1
+echo --------------
+
+cd %DL_PATH%
+
+FOR /F "eol=; tokens=1,2" %%f IN (%2) DO (
+echo %%f %%g
+  IF NOT EXIST %%f (
+    %WGET% "%%g/%%f"
+  ) ELSE (
+    echo Already have %%f
+  )
+
+  copy /b %%f %TMP_PATH%
+)
+
+echo Extracting...
+echo -------------
+
+cd %TMP_PATH%
+
+FOR /F "eol=; tokens=1,2" %%f IN (%2) DO (
+  %ZIP% x %%f
+)
