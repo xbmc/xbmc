394670a2cbd2256af64e19ffa21dac665c7b7c7a
jmarshallnz <jmarshallnz@svn>
2010-05-21 01:13:08 +0000

    fixed: ActivateWindow(VideoLibrary,MovieSets) broken. Ticket #9226, thanks to ronie.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30359 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowVideoNav.cpp
+++ xbmc/GUIWindowVideoNav.cpp
@@ -163,10 +163,10 @@ bool CGUIWindowVideoNav::OnMessage(CGUIMessage& message)
           destPath = "videodb://1/6/";
         else if (strDestination.Equals("MovieCountries"))
           destPath = "videodb://1/7/";
+        else if (strDestination.Equals("MovieSets"))
+          destPath = "videodb://1/8/";
         else if (strDestination.Equals("Movies"))
           destPath = "videodb://1/";
-        else if (strDestination.Equals("MovieSets"))
-          destPath = "videodb://1/7/";
         else if (strDestination.Equals("TvShowGenres"))
           destPath = "videodb://2/1/";
         else if (strDestination.Equals("TvShowTitles"))
