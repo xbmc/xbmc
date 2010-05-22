78f309f6c21a9e67483ca6c5026ddd054add398e
elupus <elupus@svn>
2010-05-22 18:03:12 +0000

    changed: no point in logging twice for each loadlibrary call
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30441 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/DllLoader/dll.cpp
+++ xbmc/cores/DllLoader/dll.cpp
@@ -69,7 +69,6 @@ extern "C" HMODULE __stdcall dllLoadLibraryExtended(LPCSTR lib_file, LPCSTR sour
   /* extract path */
   getpath(libpath, lib_file);
 
-  CLog::Log(LOGDEBUG, "LoadLibraryA('%s')", libname);
   if (sourcedll)
   {
     /* also check for invalid paths wich begin with a \ */
