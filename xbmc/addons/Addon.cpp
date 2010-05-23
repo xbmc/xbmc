ac3c8a37e226c20408045f1c42471b8ae49e675c
spiff_ <spiff_@svn>
2010-05-23 22:57:14 +0000

    changed: cleanup
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30483 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/addons/Addon.cpp
+++ xbmc/addons/Addon.cpp
@@ -401,9 +401,7 @@ CStdString CAddon::GetString(uint32_t id)
  */
 bool CAddon::HasSettings()
 {
-  CStdString addonFileName = m_props.path;
-  CUtil::AddFileToFolder(addonFileName, "resources", addonFileName);
-  CUtil::AddFileToFolder(addonFileName, "settings.xml", addonFileName);
+  CStdString addonFileName = CUtil::AddFileToFolder(m_props.path, "resources/settings.xml");
 
   // Load the settings file to verify it's valid
   TiXmlDocument xmlDoc;
@@ -420,9 +418,7 @@ bool CAddon::HasSettings()
 
 bool CAddon::LoadSettings()
 {
-  CStdString addonFileName = m_props.path;
-  CUtil::AddFileToFolder(addonFileName, "resources", addonFileName);
-  CUtil::AddFileToFolder(addonFileName, "settings.xml", addonFileName);
+  CStdString addonFileName = CUtil::AddFileToFolder(m_props.path, "resources/settings.xml");
 
   if (!m_addonXmlDoc.LoadFile(addonFileName))
   {
