01704aa4b16d08df840f47c26af4c2a892198e61
spiff_ <spiff_@svn>
2010-05-23 22:58:23 +0000

    added: store supported content in addondatabase
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30485 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/AddonDatabase.h
+++ xbmc/AddonDatabase.h
@@ -51,7 +51,7 @@ public:
 protected:
   virtual bool CreateTables();
   virtual bool UpdateOldVersion(int version);
-  virtual int GetMinVersion() const { return 5; }
+  virtual int GetMinVersion() const { return 6; }
   const char *GetDefaultDBName() const { return "Addons"; }
 };
 
