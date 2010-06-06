4a8121a99a1f72c1b6e48ace45bb186c341fff41
jmarshallnz <jmarshallnz@svn>
2010-06-06 04:59:58 +0000

    fixed: Add cache=no to zip URLs when copying files from them to stop them being extracted to a temp dir first
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30861 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/File.cpp
+++ xbmc/FileSystem/File.cpp
@@ -124,11 +124,19 @@ char* get() { return p; }
 };
 
 // This *looks* like a copy function, therefor the name "Cache" is misleading
-bool CFile::Cache(const CStdString& strFileName, const CStdString& strDest, XFILE::IFileCallback* pCallback, void* pContext)
+bool CFile::Cache(const CStdString& strFileName1, const CStdString& strDest, XFILE::IFileCallback* pCallback, void* pContext)
 {
   CFile file;
   CAsyncFileCallback* helper = NULL;
 
+  // special case for zips - ignore caching
+  CStdString strFileName(strFileName1);
+  if (CUtil::IsInZIP(strFileName))
+  {
+    CURL url(strFileName);
+    url.SetOptions("?cache=no");
+    strFileName = url.Get();
+  }
   if (file.Open(strFileName, READ_TRUNCATED))
   {
     if (file.GetLength() <= 0)
