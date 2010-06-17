dba0b6b9c633d61654be6a755def8baefd59447a
davilla <davilla@svn>
2010-06-17 07:49:30 +0000

    fixed crash, don't RenderTTFSubtitles is g_application.m_pPlayer is nil
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31137 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowFullScreen.cpp
+++ xbmc/GUIWindowFullScreen.cpp
@@ -786,14 +786,15 @@ void CGUIWindowFullScreen::FrameMove()
 
 void CGUIWindowFullScreen::Render()
 {
-  RenderTTFSubtitles();
+  if (g_application.m_pPlayer)
+    RenderTTFSubtitles();
   CGUIWindow::Render();
 }
 
 void CGUIWindowFullScreen::RenderTTFSubtitles()
 {
   if ((g_application.GetCurrentPlayer() == EPC_MPLAYER || g_application.GetCurrentPlayer() == EPC_DVDPLAYER) &&
-      CUtil::IsUsingTTFSubtitles() && g_application.m_pPlayer->GetSubtitleVisible())
+      CUtil::IsUsingTTFSubtitles() && (g_application.m_pPlayer->GetSubtitleVisible())
   {
     CSingleLock lock (m_fontLock);
 
