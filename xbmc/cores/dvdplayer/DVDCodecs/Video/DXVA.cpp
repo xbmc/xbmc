a6f7e528970bfc11e71ca6b503ea5fed6a3f0c33
CrystalPT <CrystalPT@svn>
2010-06-26 15:14:07 +0000

    [WIN32] fixed: there are no DXVA status reports for MPEG-2
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31425 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
@@ -518,8 +518,8 @@ int CDecoder::Check(AVCodecContext* avctx)
     return VC_FLUSHED;
   }
 
-  if(avctx->codec_id != CODEC_ID_MPEG2VIDEO
-  && avctx->codec_id != CODEC_ID_H264
+  // Status reports are available only for the DXVA2_ModeH264 and DXVA2_ModeVC1 modes
+  if(avctx->codec_id != CODEC_ID_H264
   && avctx->codec_id != CODEC_ID_VC1
   && avctx->codec_id != CODEC_ID_WMV3)
     return 0;
@@ -539,11 +539,6 @@ int CDecoder::Check(AVCodecContext* avctx)
   if(FAILED( hr = m_decoder->Execute(&params)))
   {
     CLog::Log(LOGWARNING, "DXVA - failed to get decoder status - 0x%08X", hr);
-
-    // temporary ugly hack for testing! pretend there was no error.
-    if(avctx->codec_id == CODEC_ID_MPEG2VIDEO)
-      return 0;
-
     return VC_ERROR;
   }
 
