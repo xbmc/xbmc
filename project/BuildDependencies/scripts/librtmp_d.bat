eaec3d0d02778110bf23f7ab05141795c70564cc
theuni <theuni@svn>
2010-07-01 03:14:14 +0000

    [WIN32] updated: librtmp to v2.3. now fetching the official release from mplayerhq.hu
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31540 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/BuildDependencies/scripts/librtmp_d.bat
+++ project/BuildDependencies/scripts/librtmp_d.bat
@@ -7,7 +7,8 @@ CALL dlextract.bat librtmp %FILES%
 
 cd %TMP_PATH%
 
-xcopy librtmp\* "%CUR_PATH%\include\librtmp\" /E /Q /I /Y
-copy librtmp.dll "%XBMC_PATH%\system\players\dvdplayer\" /Y
+xcopy rtmpdump-2.3\librtmp\*.h "%CUR_PATH%\include\librtmp\" /E /Q /I /Y
+copy rtmpdump-2.3\librtmp\COPYING "%CUR_PATH%\include\librtmp\" /Y
+copy rtmpdump-2.3\librtmp\librtmp.dll "%XBMC_PATH%\system\players\dvdplayer\" /Y
 
 cd %LOC_PATH%
