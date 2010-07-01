xbmc/xbmc

commit 0d27aa556cc638574e8b2e37d8aa9738a8710973

changed: removed dupe code for manual lookups
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@26223 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/GUIWindowVideoBase.cpp
@@ -188,6 +188,10 @@ bool CGUIWindowVideoBase::OnMessage(CGUIMessage& message)
             return false;
 
           CFileItemPtr item = m_vecItems->Get(iItem);
+
+          if (item->m_strPath.Equals("add") || item->IsParentFolder())
+            return false;
+
           if (m_vecItems->IsPlugin() || m_vecItems->IsRSS())
             info.strContent = "plugin";
           else if(m_vecItems->IsLiveTV())
@@ -297,7 +301,12 @@ void CGUIWindowVideoBase::UpdateButtons()
 
 void CGUIWindowVideoBase::OnInfo(CFileItem* pItem, const SScraperInfo& info)
 {
-  if ( !pItem ) return ;
+  if (!pItem)
+    return;
+
+  if (pItem->IsParentFolder() || pItem->m_bIsShareOrDrive || pItem->m_strPath.Equals("add"))
+    return;
+
   // ShowIMDB can kill the item as this window can be closed while we do it,
   // so take a copy of the item now
   CFileItem item(*pItem);
@@ -308,6 +317,39 @@ void CGUIWindowVideoBase::OnInfo(CFileItem* pItem, const SScraperInfo& info)
     else
       item.m_strPath = item.GetVideoInfoTag()->m_strFileNameAndPath;
   }
+  else
+  {
+    if (item.m_bIsFolder && !info.strContent.Equals("tvshows"))
+    {
+      CFileItemList items;
+      CDirectory::GetDirectory(item.m_strPath, items);
+      items.Stack();
+
+      // check for media files
+      bool bFoundFile(false);
+      for (int i = 0; i < items.Size(); ++i)
+      {
+        CFileItemPtr item2 = items[i];
+
+        if (item2->IsVideo() && !item2->IsPlayList() &&
+	    !CUtil::ExcludeFileOrFolder(item2->m_strPath, g_advancedSettings.m_moviesExcludeFromScanRegExps))
+        {
+          item.m_strPath = item2->m_strPath;
+          item.m_bIsFolder = false;
+          bFoundFile = true;
+          break;
+        }
+      }
+
+      // no video file in this folder
+      if (!bFoundFile)
+      {
+        CGUIDialogOK::ShowAndGetInput(13346,20349,20022,20022);
+        return;
+      }
+    }
+  }
+
   bool modified = ShowIMDB(&item, info);
   if (modified && !info.strContent.Equals("plugin") && !info.strContent.Equals("livetv") &&
      (g_windowManager.GetActiveWindow() == WINDOW_VIDEO_FILES ||

xbmc/GUIWindowVideoFiles.cpp
@@ -313,21 +313,6 @@ void CGUIWindowVideoFiles::OnPrepareFileItems(CFileItemList &items)
 
 bool CGUIWindowVideoFiles::OnClick(int iItem)
 {
-  if ( iItem < 0 || iItem >= (int)m_vecItems->Size() ) return true;
-  CFileItemPtr pItem = m_vecItems->Get(iItem);
-  CStdString strExtension;
-  CUtil::GetExtension(pItem->m_strPath, strExtension);
-
-  if (strcmpi(strExtension.c_str(), ".nfo") == 0) // WTF??
-  {
-    SScraperInfo info;
-    info.strPath = "imdb.xml";
-    info.strContent = "movies";
-    info.strTitle = "IMDb";
-    OnInfo(pItem.get(),info);
-    return true;
-  }
-
   return CGUIWindowVideoBase::OnClick(iItem);
 }
 
@@ -359,114 +344,6 @@ bool CGUIWindowVideoFiles::OnPlayMedia(int iItem)
 
     return CGUIWindowVideoBase::OnPlayMedia(iItem);
   }
-}
-
-void CGUIWindowVideoFiles::OnInfo(CFileItem* pItem, const SScraperInfo& info)
-{
-  if ( !pItem ) return ;
-  bool bFolder(false);
-  if (info.strContent.Equals("tvshows"))
-  {
-    CGUIWindowVideoBase::OnInfo(pItem,info);
-    return;
-  }
-  CStdString strFolder = "";
-  CStdString strFile = pItem->m_strPath;
-  if (pItem->m_bIsFolder && pItem->IsParentFolder()) return ;
-  if (pItem->m_bIsShareOrDrive) // oh no u don't
-    return ;
-  if (pItem->m_bIsFolder)
-  {
-    // IMDB is done on a folder
-    // stack and then find first file in folder
-    bFolder = true;
-    CFileItemList vecitems;
-    GetStackedDirectory(pItem->m_strPath, vecitems);
-    bool bFoundFile(false);
-    for (int i = 0; i < (int)vecitems.Size(); ++i)
-    {
-      CFileItemPtr item = vecitems[i];
-      if (!item->m_bIsFolder)
-      {
-        if (item->IsVideo() && !item->IsNFO() && !item->IsPlayList() &&
-            item->m_strPath.Find("-trailer.") == -1)
-        {
-          bFoundFile = true;
-          strFile = item->m_strPath;
-          break;
-        }
-      }
-      else
-      { // check for a dvdfolder
-        if (item->GetLabel().CompareNoCase("VIDEO_TS") == 0)
-        { // found a dvd folder - grab the main .ifo file
-          CUtil::AddFileToFolder(item->m_strPath, "VIDEO_TS.IFO", strFile);
-          if (CFile::Exists(strFile))
-          {
-            bFoundFile = true;
-            break;
-          }
-        }
-        // check for a "CD1" folder
-        if (item->GetLabel().CompareNoCase("CD1") == 0)
-        {
-          CFileItemList items;
-          GetStackedDirectory(item->m_strPath, items);
-          for (int i = 0; i < items.Size(); i++)
-          {
-            CFileItemPtr item = items[i];
-            if (!item->m_bIsFolder && item->IsVideo() && !item->IsNFO() &&
-                !item->IsPlayList())
-            {
-              bFoundFile = true;
-              strFile = item->m_strPath;
-              break;
-            }
-          }
-          if (bFoundFile)
-            break;
-        }
-      }
-    }
-    if (!bFoundFile)
-    {
-      // no video file in this folder?
-      if (info.strContent.Equals("movies"))
-        CGUIDialogOK::ShowAndGetInput(13346,20349,20022,20022);
-
-      return ;
-    }
-  }
-
-  // setup our item with the label and thumb information
-  CFileItem item(strFile, false);
-  item.SetLabel(pItem->GetLabel());
-
-  // hack since our label sometimes contains extensions
-  if(!pItem->m_bIsFolder && g_guiSettings.GetBool("filelists.showextensions") &&
-     !pItem->IsLabelPreformated())
-  {
-    item.RemoveExtension();
-  }
-
-  item.SetCachedVideoThumb();
-  if (!item.HasThumbnail() && pItem->GetProperty("HasAutoThumb") != "1") // inherit from the original item if it exists
-    item.SetThumbnailImage(pItem->GetThumbnailImage());
-
-  if(!info.strContent.Equals("plugin")
-  && !info.strContent.Equals("livetv"))
-    AddFileToDatabase(&item);
-  else
-  {
-    if (pItem->HasVideoInfoTag())
-      *item.GetVideoInfoTag() = *pItem->GetVideoInfoTag();
-  }
-  // we need to also request any thumbs also be applied to the folder item
-  if (pItem->m_bIsFolder)
-    item.SetProperty("set_folder_thumb", pItem->m_strPath);
-  if (ShowIMDB(&item,info) && !info.strContent.Equals("plugin")
-                           && !info.strContent.Equals("livetv"))
-    Update(m_vecItems->m_strPath);
 }
 
 void CGUIWindowVideoFiles::AddFileToDatabase(const CFileItem* pItem)
@@ -646,7 +523,7 @@ void CGUIWindowVideoFiles::GetContextButtons(int itemNumber, CContextButtons &bu
           if (info.strContent.Equals("musicvideos"))
             infoString = 20393;
 
-          if (item->m_bIsFolder)
+          if (item->m_bIsFolder && !item->IsParentFolder())
           {
             if (!pScanDlg || (pScanDlg && !pScanDlg->IsScanning()))
               if (!item->IsPlayList() && !item->IsLiveTV())

xbmc/GUIWindowVideoFiles.h
@@ -37,7 +37,6 @@ public:
   virtual bool OnAction(const CAction &action);
 
   void GetStackedDirectory(const CStdString &strPath, CFileItemList &items);
-  virtual void OnInfo(CFileItem* pItem, const SScraperInfo& info);
 protected:
   virtual bool GetDirectory(const CStdString &strDirectory, CFileItemList &items);
   virtual bool OnPlayMedia(int iItem);

commit 1e9d8ba66aefea11d61130dca90e452a55726946

fixed: Regression in r26223 that caused folder thumbs to not be updated from the movie information dialog.
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31541 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/GUIWindowVideoBase.cpp
@@ -344,6 +344,10 @@ void CGUIWindowVideoBase::OnInfo(CFileItem* pItem, const ADDON::ScraperPtr& scra
     }
   }
 
+  // we need to also request any thumbs be applied to the folder item
+  if (pItem->m_bIsFolder)
+    item.SetProperty("set_folder_thumb", pItem->m_strPath);
+
   bool modified = ShowIMDB(&item, scraper);
   if (modified &&
      (g_windowManager.GetActiveWindow() == WINDOW_VIDEO_FILES ||