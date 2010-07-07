396294f6f924ac8dd0450194deeac97e967e1c4d
davilla <davilla@svn>
2010-07-07 20:22:16 +0000

    [osx] 1) fixed so script can be run in xbmc root or local dir, 2) space monster
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31653 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- lib/librtmp/darwin_package_librtmp.sh
+++ lib/librtmp/darwin_package_librtmp.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 SRC_LIB_RTMP="/opt/local/lib/librtmp.so.0"
-DST_LIB_RTMP="../../system/librtmp.so"
+if [ -e ./system ]; then
+  DST_LIB_RTMP="./system/librtmp.so"
+else
+  DST_LIB_RTMP="../../system/librtmp.so"
+fi
 
 if [ -f $SRC_LIB_RTMP ]; then
   # copy librtmp into xbmc's system directory, we
@@ -9,7 +13,7 @@ if [ -f $SRC_LIB_RTMP ]; then
   cp $SRC_LIB_RTMP $DST_LIB_RTMP
 
   # rename any dependency libs to inside xbmc's app framework
-  for a in $(otool -L "$DST_LIB_RTMP" | grep opt | awk ' { print $1 } ') ; do 
+  for a in $(otool -L "$DST_LIB_RTMP" | grep opt | awk ' { print $1 } ') ; do
     echo "Processing $a"
     install_name_tool -change "$a" @executable_path/../Frameworks/$(basename $a) "$DST_LIB_RTMP"
   done
