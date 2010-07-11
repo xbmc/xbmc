119f05103c5cfbc5e13bb36135ad00791fafb406
spiff_ <spiff_@svn>
2010-07-11 21:19:12 +0000

    added: ticket #9576 - Support SearchStringEncoding in Artist/Album searching. thanks to hojel
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31736 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/ScraperParser.cpp
+++ xbmc/utils/ScraperParser.cpp
@@ -113,6 +113,19 @@ bool CScraperParser::LoadFromXML()
         m_SearchStringEncoding = "UTF-8";
     }
 
+    pChildElement = m_pRootElement->FirstChildElement("CreateArtistSearchUrl");
+    if (pChildElement)
+    {
+      if (!(m_SearchStringEncoding = pChildElement->Attribute("SearchStringEncoding")))
+        m_SearchStringEncoding = "UTF-8";
+    }
+    pChildElement = m_pRootElement->FirstChildElement("CreateAlbumSearchUrl");
+    if (pChildElement)
+    {
+      if (!(m_SearchStringEncoding = pChildElement->Attribute("SearchStringEncoding")))
+        m_SearchStringEncoding = "UTF-8";
+    }
+
     return true;
   }
 
