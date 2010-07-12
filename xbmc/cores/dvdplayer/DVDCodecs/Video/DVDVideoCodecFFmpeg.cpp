15ce84dbf8ac1aef5a0bc8edb5c35b406811e420
theuni <theuni@svn>
2010-07-12 21:14:53 +0000

    changed: ffmpeg's internal vp8 decoder does not support edge emulation, so disable it.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31750 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
@@ -218,7 +218,7 @@ bool CDVDVideoCodecFFmpeg::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options
   m_pCodecContext->workaround_bugs = FF_BUG_AUTODETECT;
   m_pCodecContext->get_format = GetFormat;
 
-  if (pCodec->id != CODEC_ID_H264 && pCodec->capabilities & CODEC_CAP_DR1)
+  if (pCodec->id != CODEC_ID_H264 && pCodec->id != CODEC_ID_VP8 && pCodec->capabilities & CODEC_CAP_DR1)
     m_pCodecContext->flags |= CODEC_FLAG_EMU_EDGE;
 
   // if we don't do this, then some codecs seem to fail.
