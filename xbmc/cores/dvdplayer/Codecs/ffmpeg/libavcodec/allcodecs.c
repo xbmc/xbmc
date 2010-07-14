a9a28d9ed0e2d47886e201f5ade4b11dba507c4d
theuni <theuni@svn>
2010-07-14 16:54:34 +0000

    re-add libfaad wrapper to ffmpeg for now
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31799 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/Codecs/ffmpeg/libavcodec/allcodecs.c
+++ xbmc/cores/dvdplayer/Codecs/ffmpeg/libavcodec/allcodecs.c
@@ -343,6 +343,7 @@ void avcodec_register_all(void)
     /* external libraries */
     REGISTER_ENCDEC  (LIBDIRAC, libdirac);
     REGISTER_ENCODER (LIBFAAC, libfaac);
+    REGISTER_DECODER (LIBFAAD, libfaad);
     REGISTER_DECODER (LIBFAAD_LATM, libfaad_latm);
     REGISTER_ENCDEC  (LIBGSM, libgsm);
     REGISTER_ENCDEC  (LIBGSM_MS, libgsm_ms);
