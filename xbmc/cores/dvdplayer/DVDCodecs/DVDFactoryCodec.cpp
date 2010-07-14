9c945ae840f59f1a76f899ec47b091f7f4c92f2c
theuni <theuni@svn>
2010-07-14 16:54:43 +0000

    Revert "quick and dirty removal of libfaad from configure. needs a proper fix. this should be reverted for release branch."
    This reverts commit 3d40dfaea2a1bd097a31910a8049d1716ed45fe9.
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31800 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
@@ -42,6 +42,7 @@
   #include "Audio/DVDAudioCodecLibDts.h"
 #endif
 #include "Audio/DVDAudioCodecLibMad.h"
+#include "Audio/DVDAudioCodecLibFaad.h"
 #include "Audio/DVDAudioCodecPcm.h"
 #include "Audio/DVDAudioCodecLPcm.h"
 #if defined(USE_LIBA52_DECODER) || defined(USE_LIBDTS_DECODER)
@@ -268,7 +269,7 @@ CDVDAudioCodec* CDVDFactoryCodec::CreateAudioCodec( CDVDStreamInfo &hint, bool p
   case CODEC_ID_AAC:
   //case CODEC_ID_MPEG4AAC:
     {
-      pCodec = OpenCodec( new CDVDAudioCodecFFmpeg(), hint, options );
+      pCodec = OpenCodec( new CDVDAudioCodecLibFaad(), hint, options );
       if( pCodec ) return pCodec;
       break;
     }
