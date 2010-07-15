585ef9c5b239c053f6b9b1122de12adb5bd845a5
jmarshallnz <jmarshallnz@svn>
2010-07-15 07:28:43 +0000

    added: autoclose timeout to the keyboard dialog (via doModal).  Ticket #9579, thanks to Nuka1195.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31819 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libPython/xbmcmodule/keyboard.cpp
+++ xbmc/lib/libPython/xbmcmodule/keyboard.cpp
@@ -75,10 +75,12 @@ namespace PYXBMC
 
   // doModal() Method
   PyDoc_STRVAR(doModal__doc__,
-    "doModal() -- Show keyboard and wait for user action.\n"
+    "doModal([autoclose]) -- Show keyboard and wait for user action.\n"
+    "\n"
+    "autoclose      : [opt] integer - milliseconds to autoclose dialog. (default=do not autoclose)\n"
     "\n"
     "example:\n"
-    "  - kb.doModal()");
+    "  - kb.doModal(30000)");
 
   PyObject* Keyboard_DoModal(Keyboard *self, PyObject *args)
   {
@@ -88,6 +90,9 @@ namespace PYXBMC
       PyErr_SetString(PyExc_SystemError, "Unable to load virtual keyboard");
       return NULL;
     }
+    int autoClose = 0;
+
+    if (!PyArg_ParseTuple(args, (char*)"|i", &autoClose)) return NULL;
 
     pKeyboard->Initialize();
     pKeyboard->CenterWindow();
@@ -95,6 +100,8 @@ namespace PYXBMC
     CStdString strDefault(self->strDefault);
     pKeyboard->SetText(strDefault);
     pKeyboard->SetHiddenInput(self->bHidden);
+    if (autoClose > 0)
+      pKeyboard->SetAutoClose(autoClose);
 
     // do modal of dialog
     ThreadMessage tMsg = {TMSG_DIALOG_DOMODAL, WINDOW_DIALOG_KEYBOARD, g_windowManager.GetActiveWindow()};
