xbmc/xbmc

commit aca6e24392f8675d9aed59eb3b8121760071970d

changed: if we set cache state init, we must make sure players echo back their started state
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@28323 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -1950,7 +1950,9 @@ void CDVDPlayer::SetCaching(ECacheState state)
   {
     m_clock.SetSpeed(DVD_PLAYSPEED_PAUSE);
     m_dvdPlayerAudio.SetSpeed(DVD_PLAYSPEED_PAUSE);
+    m_dvdPlayerAudio.SendMessage(new CDVDMsg(CDVDMsg::PLAYER_STARTED), 1);
     m_dvdPlayerVideo.SetSpeed(DVD_PLAYSPEED_PAUSE);
+    m_dvdPlayerVideo.SendMessage(new CDVDMsg(CDVDMsg::PLAYER_STARTED), 1);
   }
 
   if(state == CACHESTATE_PLAY

xbmc/cores/dvdplayer/DVDPlayerAudio.cpp
@@ -432,6 +432,11 @@ int CDVDPlayerAudio::DecodeFrame(DVDAudioFrame &audioframe, bool bDropPacket)
 
       m_decode.Release();
     }
+    else if (pMsg->IsType(CDVDMsg::PLAYER_STARTED))
+    {
+      if(m_started)
+        m_messageParent.Put(new CDVDMsgInt(CDVDMsg::PLAYER_STARTED, DVDPLAYER_AUDIO));
+    }
     else if (pMsg->IsType(CDVDMsg::GENERAL_EOF))
     {
       CLog::Log(LOGDEBUG, "CDVDPlayerAudio - CDVDMsg::GENERAL_EOF");

xbmc/cores/dvdplayer/DVDPlayerAudio.h
@@ -105,7 +105,7 @@ public:
   // waits until all available data has been rendered
   void WaitForBuffers();
   bool AcceptsData()                                    { return !m_messageQueue.IsFull(); }
-  void SendMessage(CDVDMsg* pMsg)                       { m_messageQueue.Put(pMsg); }
+  void SendMessage(CDVDMsg* pMsg, int priority = 0)     { m_messageQueue.Put(pMsg, priority); }
 
   void SetVolume(long nVolume)                          { m_dvdAudio.SetVolume(nVolume); }
   void SetDynamicRangeCompression(long drc)             { m_dvdAudio.SetDynamicRangeCompression(drc); }

xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -426,6 +426,11 @@ void CDVDPlayerVideo::Process()
       if(m_speed == DVD_PLAYSPEED_PAUSE)
         m_iNrOfPicturesNotToSkip = 0;
     }
+    else if (pMsg->IsType(CDVDMsg::PLAYER_STARTED))
+    {
+      if(m_started)
+        m_messageParent.Put(new CDVDMsgInt(CDVDMsg::PLAYER_STARTED, DVDPLAYER_VIDEO));
+    }
 
     if (pMsg->IsType(CDVDMsg::DEMUXER_PACKET))
     {

xbmc/cores/dvdplayer/DVDPlayerVideo.h
@@ -56,7 +56,7 @@ public:
   // just waiting for packetqueue should be enough for video
   void WaitForBuffers()                             { m_messageQueue.WaitUntilEmpty(); }
   bool AcceptsData()                                { return !m_messageQueue.IsFull(); }
-  void SendMessage(CDVDMsg* pMsg)                   { m_messageQueue.Put(pMsg); }
+  void SendMessage(CDVDMsg* pMsg, int priority = 0) { m_messageQueue.Put(pMsg, priority); }
 
 #ifdef HAS_VIDEO_PLAYBACK
   void Update(bool bPauseDrawing)                   { g_renderManager.Update(bPauseDrawing); }

commit 2e45bd765fd410fc6e63ec79f05a3440b3c1860e

fixed, regression from r28323 where DVDPlayer would not wait until decoders signaled PLAYER_STARTED when seeking because of a stale PLAYER_STARTED message, thanks elupus
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31830 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -2729,6 +2729,9 @@ void CDVDPlayer::FlushBuffers(bool queued, double pts, bool accurate)
       msg->Wait(&m_bStop, 0);
       msg->Release();
 
+      // purge any pending PLAYER_STARTED messages
+      m_messenger.Flush(CDVDMsg::PLAYER_STARTED);
+
       // we should now wait for init cache
       SetCaching(CACHESTATE_INIT);
       m_CurrentAudio.started    = false;