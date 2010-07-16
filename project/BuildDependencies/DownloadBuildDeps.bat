c1244baafa5737572d44ad5ea09e4288393ea95f
wiso <wiso@svn>
2010-07-16 19:56:02 +0000

    [WIN32] stupid Windows still shows access denied if rmdir is followed by md. This way a little less but still ...
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31856 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/BuildDependencies/DownloadBuildDeps.bat
+++ project/BuildDependencies/DownloadBuildDeps.bat
@@ -6,6 +6,11 @@ SET CUR_PATH=%CD%
 SET XBMC_PATH=%CD%\..\..
 SET TMP_PATH=%CD%\scripts\tmp
 
+rem can't run rmdir and md back to back. access denied error otherwise.
+IF EXIST lib rmdir lib /S /Q
+IF EXIST include rmdir include /S /Q
+IF EXIST %TMP_PATH% rmdir %TMP_PATH% /S /Q
+
 IF $%1$ == $$ (
   SET DL_PATH="%CD%\downloads"
 ) ELSE (
@@ -17,15 +22,11 @@ SET ZIP=%CUR_PATH%\..\Win32BuildSetup\tools\7z\7za
 
 IF NOT EXIST %DL_PATH% md %DL_PATH%
 
-rem can't run rmdir and md back to back. access denied error otherwise.
-IF EXIST lib rmdir lib /S /Q
-IF EXIST include rmdir include /S /Q
-IF EXIST %TMP_PATH% rmdir %TMP_PATH% /S /Q
 md lib
 md include
 md %TMP_PATH%
 
-cd scripts
+pause
 
 FOR /F "tokens=*" %%S IN ('dir /B "*_d.bat"') DO (
   echo running %%S ...
