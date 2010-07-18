0a4f12c40055dbb45aacad9d18b6d86780131b7a
CrystalPT <CrystalPT@svn>
2010-07-18 17:24:50 +0000

    [WIN32] fixed: using D3DPOOL_DEFAULT and dynamic textures is not a good idea without D3D9Ex. Textures get evicted without notification due to memory pressure. Especially visible under XP
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31957 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/RenderSystemDX.cpp
+++ xbmc/RenderSystemDX.cpp
@@ -399,7 +399,7 @@ bool CRenderSystemDX::CreateDevice()
 
   m_maxTextureSize = min(caps.MaxTextureWidth, caps.MaxTextureHeight);
 
-  if (g_advancedSettings.m_AllowDynamicTextures && (caps.Caps2 & D3DCAPS2_DYNAMICTEXTURES))
+  if (g_advancedSettings.m_AllowDynamicTextures && m_useD3D9Ex && (caps.Caps2 & D3DCAPS2_DYNAMICTEXTURES))
   {
     m_defaultD3DUsage = D3DUSAGE_DYNAMIC;
     m_defaultD3DPool  = D3DPOOL_DEFAULT;
