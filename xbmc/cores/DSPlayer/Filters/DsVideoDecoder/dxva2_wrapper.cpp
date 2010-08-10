55a9b6d81fa41f26871bd408401331367d8a1d5a
tiben20 <tiben20@svn>
2010-08-10 02:59:57 +0000

    [DSPlayer] dxva2 wrapper for dxva1
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/dsplayer@32665 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/DSPlayer/Filters/DsVideoDecoder/dxva2_wrapper.cpp
@@ -0,0 +1,94 @@
+/*
+ *      Copyright (C) 2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "dxva1wrapper.h"
+CDXDecWrapper::CDXDecWrapper(IAMVideoAccelerator *decoder, int count, void **surface)
+: CUnknown("CDXDecWrapper",NULL)
+{ 
+  m_pAMVideoAccelerator = decoder;
+  surface_count = count;
+  surface_id = (void**)calloc(count, sizeof(*surface));
+  for (int i = 0; i < surface_count; i++)
+    surface_id[i] = surface[i];
+  
+  DWORD req_count = 0;
+  if (FAILED(m_pAMVideoAccelerator->GetInternalCompBufferInfo(&req_count, NULL)))
+    req_count = 0;
+  buffer_info_count = req_count;
+  buffer_info = (AMVACompBufferInfo *) calloc(req_count, sizeof(*buffer_info));
+ 
+  if (req_count > 0 && FAILED(m_pAMVideoAccelerator->GetInternalCompBufferInfo(&req_count, buffer_info)))
+      buffer_info_count = 0;
+
+}
+
+CDXDecWrapper::~CDXDecWrapper()
+{
+  free(buffer_info);
+  free(surface_id);
+}
+
+int CDXDecWrapper::GetSurfaceIndex(void *id)
+{
+  for (int i = 0; i < surface_count; i++) 
+  {
+    if (surface_id[i] == id)
+        return i;
+  }
+    ASSERT(0);
+    return 0;
+}
+
+STDMETHODIMP CDXDecWrapper::GetVideoDecoderService(IDirectXVideoDecoderService **ppService)
+{
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::GetCreationParameters(GUID *pDeviceGuid, DXVA2_VideoDesc *pVideoDesc, DXVA2_ConfigPictureDecode *pConfig, IDirect3DSurface9 ***pDecoderRenderTargets, UINT *pNumSurfaces)
+{
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::GetBuffer(UINT BufferType, void **ppBuffer, UINT *pBufferSize)
+{
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::ReleaseBuffer(UINT BufferType)
+{
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::BeginFrame(IDirect3DSurface9 *pRenderTarget, void *pvPVPData)
+{
+  DWORD index = GetSurfaceIndex(pRenderTarget);
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::EndFrame(HANDLE *pHandleComplete)
+{
+  return E_NOTIMPL;
+}
+
+STDMETHODIMP CDXDecWrapper::Execute(const DXVA2_DecodeExecuteParams *pExecuteParams)
+{
+  return E_NOTIMPL;
+}
---
+++ xbmc/cores/DSPlayer/Filters/DsVideoDecoder/dxva2_wrapper.h
@@ -0,0 +1,68 @@
+/*
+ *      Copyright (C) 2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#pragma once
+#include "dxva2api.h"
+#include "streams.h"
+#include "dxva.h"
+#include "videoacc.h"
+#include "amva.h"
+class CDXDecWrapper 
+  : public CUnknown
+  , public IDirectXVideoDecoder
+{
+public:
+  CDXDecWrapper(IAMVideoAccelerator *decoder, int count, void **surface);
+  virtual ~CDXDecWrapper();
+  DECLARE_IUNKNOWN
+  STDMETHODIMP GetVideoDecoderService(IDirectXVideoDecoderService **ppService);
+  STDMETHODIMP GetCreationParameters(GUID *pDeviceGuid, DXVA2_VideoDesc *pVideoDesc, DXVA2_ConfigPictureDecode *pConfig, IDirect3DSurface9 ***pDecoderRenderTargets, UINT *pNumSurfaces);
+  STDMETHODIMP GetBuffer(UINT BufferType, void **ppBuffer, UINT *pBufferSize);
+  STDMETHODIMP ReleaseBuffer(UINT BufferType);
+  STDMETHODIMP BeginFrame(IDirect3DSurface9 *pRenderTarget, void *pvPVPData);
+  STDMETHODIMP EndFrame(HANDLE *pHandleComplete);
+  STDMETHODIMP Execute(const DXVA2_DecodeExecuteParams *pExecuteParams);
+protected:
+  int GetSurfaceIndex(void *id);
+  IAMVideoAccelerator *m_pAMVideoAccelerator;
+  int  surface_count;
+  void **surface_id;
+  int  buffer_info_count;
+  AMVACompBufferInfo *buffer_info;
+
+};
+
+
+
+typedef struct {
+    IDirectXVideoDecoder     *wrapper;
+    //IDirectXVideoDecoderVtbl* vtbl;
+ 
+    IAMVideoAccelerator *decoder;
+ 
+ 
+    int  surface_count;
+    void **surface_id;
+ 
+    /* */
+    int                buffer_info_count;
+    AMVACompBufferInfo *buffer_info;
+} va_wrapper;
