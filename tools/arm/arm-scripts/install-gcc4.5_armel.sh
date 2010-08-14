31d2b9dc4c37330398e1db25aee66c26eae8a908
davilla <davilla@svn>
2010-08-14 23:45:41 +0000

    [tg2] update gcc-4.5 deb URLs
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/arm-tegra2@32785 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/arm/arm-scripts/install-gcc4.5_armel.sh
+++ tools/arm/arm-scripts/install-gcc4.5_armel.sh
@@ -5,15 +5,16 @@
 #
 
 # The URL sources
-URLS="http://ftp.debian.org/debian/pool/main/b/binutils/binutils_2.20.51.20100418-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/cpp-4.5_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/g++-4.5_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/gcc-4.5_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/gcc-4.5-base_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libgcc1_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libgomp1_4.5-20100321-1_armel.deb \
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libstdc++6_4.5-20100321-1_armel.deb \ 
-http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libstdc++6-4.5-dev_4.5-20100321-1_armel.deb \
+URLS="http://ftp.debian.org/debian/pool/main/b/binutils/binutils_2.20.51.20100813-1_armel.deb  \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/cpp-4.5_4.5.1-1_armel.deb \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/fixincludes_4.5.1-1_armel.deb \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/g++-4.5_4.5.1-1_armel.deb   \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/gcc-4.5_4.5.1-1_armel.deb \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/gcc-4.5-base_4.5.1-1_armel.deb \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libgcc1_4.5.1-1_armel.deb   \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libgomp1_4.5.1-1_armel.deb \
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libstdc++6_4.5.1-1_armel.deb  \ 
+http://ftp.debian.org/debian/pool/main/g/gcc-4.5/libstdc++6-4.5-dev_4.5.1-1_armel.deb  \
 http://ftp.debian.org/debian/pool/main/libe/libelf/libelfg0_0.8.13-1_armel.deb"
 
 # Download them using wget
@@ -21,5 +22,5 @@ mkdir -p gcc-4.5-debs
 for u in $URLS; do wget --directory-prefix=./gcc-4.5-debs $u; done
 
 # Install gcc-4.5
-dpkg -i ./gcc-4.5-debs/*.deb
+# dpkg -i ./gcc-4.5-debs/*.deb
 
