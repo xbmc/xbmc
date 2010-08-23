2eb263e44e9069faeb0e6c455b2c20a92677a2cb
gnif2 <gnif2@svn>
2010-08-23 05:40:53 +0000

    added: [AE] Comments to IAESink for each method to be implemented
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/AudioEngine@33050 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/AudioEngine/AESink.h
+++ xbmc/cores/AudioEngine/AESink.h
@@ -28,16 +28,49 @@
 class IAESink : public IRunnable
 {
 public:
+  /* return the name of this sync for logging */
   virtual const char *GetName() = 0;
 
   IAESink() {};
   virtual ~IAESink() {};
+
+  /*
+    The sink does NOT have to honour anything in the format struct or the device
+    if however it does not honour what is requested, it MUST update device/format
+    with what it does support.
+  */
   virtual bool Initialize  (AEAudioFormat &format, CStdString &device) = 0;
+
+  /*
+    Deinitialize the sink for destruction
+  */
   virtual void Deinitialize() = 0;
+
+  /*
+    Return true if the supplied format and device are compatible with the current open sink
+  */
   virtual bool IsCompatible(const AEAudioFormat format, const CStdString device) = 0;
 
-  virtual void         Stop          () = 0;
-  virtual float        GetDelay      () = 0;
-  virtual unsigned int AddPackets    (uint8_t *data, unsigned int samples) = 0;
+  /*
+    This method must block while the sink is stopping
+  */
+  virtual void Stop() = 0;
+
+  /*
+    This method must return the delay in miliseconds till new data will be sent out
+  */
+  virtual float GetDelay() = 0;
+
+  /*
+    This method must buffer packets to be sent out, the format of data and MUST
+    block when the buffer is full. Be very careful about synchronization here.
+  */
+  virtual unsigned int AddPackets(uint8_t *data, unsigned int samples) = 0;
+
+  /*
+    This is the THREAD that sends out the buffered data, and MUST unblock AddPackets
+    when there is a free buffer. Be very careful about synchronization here.
+  */
+  virtual void Run() = 0;
 };
 
