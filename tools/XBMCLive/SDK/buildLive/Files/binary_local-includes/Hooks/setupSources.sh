4499512a4b4c1ac7d816840d25139e43c8e2ce5e
lcapriotti <lcapriotti@svn>
2010-08-23 13:52:51 +0000

    post-install customizations: setup XBMC sources, advanced settings for NVIDIA cards
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33067 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ tools/XBMCLive/SDK/buildLive/Files/binary_local-includes/Hooks/setupSources.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+xbmcUser=$(getent passwd 1000 | sed -e 's/\:.*//')
+
+#
+# Create directories for XBMC sources
+#
+
+if [ ! -d "/home/$xbmcUser/Music" ]; then
+	mkdir /home/$xbmcUser/Music
+	chmod 755 /home/$xbmcUser/Music
+fi
+
+if [ ! -d "/home/$xbmcUser/Pictures" ]; then
+	mkdir /home/$xbmcUser/Pictures
+	chmod 755 /home/$xbmcUser/Pictures
+fi
+
+if [ ! -d "/home/$xbmcUser/TV Shows" ]; then
+	mkdir "/home/$xbmcUser/TV Shows"
+	chmod 755 "/home/$xbmcUser/TV Shows"
+fi
+
+if [ ! -d "/home/$xbmcUser/Videos" ]; then
+	mkdir /home/$xbmcUser/Videos
+	chmod 755 /home/$xbmcUser/Videos
+fi
+
+mkdir -p /home/$xbmcUser/.xbmc/userdata
+
+if [ ! -f /home/$xbmcUser/.xbmc/userdata/sources.xml ] ; then
+	cat > /home/$xbmcUser/.xbmc/userdata/sources.xml << 'EOF'
+<sources>
+    <video>
+        <default pathversion="1"></default>
+        <source>
+            <name>Videos</name>
+            <path pathversion="1">/home/xbmc/Videos/</path>
+        </source>
+        <source>
+            <name>TV Shows</name>
+            <path pathversion="1">/home/xbmc/TV Shows/</path>
+        </source>
+    </video>
+    <music>
+        <default pathversion="1"></default>
+        <source>
+            <name>Music</name>
+            <path pathversion="1">/home/xbmc/Music/</path>
+        </source>
+    </music>
+    <pictures>
+        <default pathversion="1"></default>
+        <source>
+            <name>Music</name>
+            <path pathversion="1">/home/xbmc/Pictures/</path>
+        </source>
+    </pictures>
+</sources>
+EOF
+
+chown -R $xbmcUser:$xbmcUser /home/$xbmcUser/.xbmc
