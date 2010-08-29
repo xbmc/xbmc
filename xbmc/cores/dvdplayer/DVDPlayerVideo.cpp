cbe6dc0207c09ef81795ce69143e22b83f46026b
elupus <elupus@svn>
2010-08-29 19:13:13 +0000

    changed: don't allow framedrops if input queue is empty changed: reset output drop counters when we disallaw framedrops
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@33321 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -458,8 +458,14 @@ void CDVDPlayerVideo::Process()
 #ifdef PROFILE
       bRequestDrop = false;
 #else
-      if (m_iNrOfPicturesNotToSkip > 0) bRequestDrop = false;
-      if (m_speed < 0)                  bRequestDrop = false;
+      if (m_messageQueue.GetDataSize() == 0
+      ||  m_iNrOfPicturesNotToSkip > 0
+      ||  m_speed < 0)
+      {
+        bRequestDrop = false;
+        m_iDroppedRequest = 0;
+        m_iLateFrames     = 0;
+      }
 #endif
 
       // if player want's us to drop this packet, do so nomatter what
