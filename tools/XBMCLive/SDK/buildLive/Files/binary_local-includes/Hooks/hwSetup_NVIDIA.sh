3c334a868c4886bc6d615409d07c1db0bc25b2f1
theuni <theuni@svn>
2010-08-30 04:56:45 +0000

    live: lots of cleanup on nvidia script
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@33331 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/XBMCLive/SDK/buildLive/Files/binary_local-includes/Hooks/hwSetup_NVIDIA.sh
+++ tools/XBMCLive/SDK/buildLive/Files/binary_local-includes/Hooks/hwSetup_NVIDIA.sh
@@ -2,7 +2,7 @@
 
 
 #check Nvidia GPU
-nvidiaGpuType=lspci -nn | grep '03:00.0' | grep 'nVidia Corporation'
+nvidiaGpuType=$(lspci -nn | grep 'VGA' | grep 'nVidia Corporation')
 if [ ! -n "$nvidiaGpuType" ] ; then
 	exit 0
 fi
@@ -11,11 +11,10 @@ xbmcUser=$(getent passwd 1000 | sed -e 's/\:.*//')
 
 mkdir -p /home/$xbmcUser/.xbmc/userdata
 
-if ! -f /home/$xbmcUser/.xbmc/userdata/advancedsettings.xml ; then
+if [ ! -f /home/$xbmcUser/.xbmc/userdata/advancedsettings.xml ] ; then
 	cat > /home/$xbmcUser/.xbmc/userdata/advancedsettings.xml << EOF
 <advancedsettings>
     <gputempcommand>echo "$(nvclock -T | sed -ne "s/=> GPU temp.*: \([0-9]\+\).*/\1/p") C"</gputempcommand>
-    <useddsfanart>true</useddsfanart>
 </advancedsettings>
 EOF
 fi
