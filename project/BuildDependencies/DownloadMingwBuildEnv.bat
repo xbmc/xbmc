9a83ca52452ed702a9cfb2e109195295fb2fef49
wiso <wiso@svn>
2010-08-30 20:57:52 +0000

    [WIN32] add the call to vcvars32.bat to msys.bat
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33348 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/BuildDependencies/DownloadMingwBuildEnv.bat
+++ project/BuildDependencies/DownloadMingwBuildEnv.bat
@@ -49,6 +49,20 @@ CALL pi_patches.bat
 
 cd %CUR_PATH%
 
-rmdir %TMP_PATH% /S /Q
+rem insert call to vcvars32.bat in msys.bat
+SET NET90VARS="%VS90COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+SET NET100VARS="%VS100COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+cd %MSYS_INSTALL_PATH%
+Move msys.bat msys.bat_dist
+IF EXIST %NET90VARS% (
+	ECHO CALL %NET90VARS%>>msys.bat
+) ELSE IF EXIST %NET100VARS% (
+	ECHO CALL %NET100VARS%>>msys.bat
+)
+TYPE msys.bat_dist>>msys.bat
+
+cd %CUR_PATH%
+
+IF EXIST %TMP_PATH% rmdir %TMP_PATH% /S /Q
 
 pause
