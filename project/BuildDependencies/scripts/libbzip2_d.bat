d96976912de40c7a61203104dca417790cef41c9
wiso <wiso@svn>
2010-08-31 18:18:00 +0000

    [WIN32] get third party bzip2 lib.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33378 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ project/BuildDependencies/scripts/libbzip2_d.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SET LOC_PATH=%CD%
+SET FILES=%LOC_PATH%\libbzip2_d.txt
+
+CALL dlextract.bat libbzip2 %FILES%
+
+cd %TMP_PATH%
+
+copy include\bzlib2.h "%CUR_PATH%\include\" /Y
+copy lib\bzip2.lib "%CUR_PATH%\lib\" /Y
+
+cd %LOC_PATH%
