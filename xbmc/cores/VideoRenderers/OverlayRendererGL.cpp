xbmc/xbmc

commit fb3ce2347a5c8abd9c6d672670f6ad790806b765

[tg2] GL_BGRA does not exist on GLES
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/arm-tegra2@33517 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/VideoRenderers/OverlayRendererGL.cpp
@@ -157,7 +157,7 @@ COverlayTextureGL::COverlayTextureGL(CDVDOverlayImage* o)
             , o->width * 4
             , &m_u, &m_v
             , GL_RGBA
-            , GL_BGRA
+            , GL_RGBA
             , rgba);
   free(rgba);
 
@@ -231,7 +231,7 @@ COverlayTextureGL::COverlayTextureGL(CDVDOverlaySpu* o)
             , o->width * 4
             , &m_u, &m_v
             , GL_RGBA
-            , GL_BGRA
+            , GL_RGBA
             , rgba + min_x + min_y * o->width);
 
   free(rgba);

commit 1c8beda7cd42544c6d09d9d5a4c3265a723980fb

[tg2] fixed broken GL from r33517, subtitles will need some tg2 love later"
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/arm-tegra2@33518 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/VideoRenderers/OverlayRendererGL.cpp
@@ -157,7 +157,11 @@ COverlayTextureGL::COverlayTextureGL(CDVDOverlayImage* o)
             , o->width * 4
             , &m_u, &m_v
             , GL_RGBA
+#ifdef HAS_GLES
             , GL_RGBA
+#else
+            , GL_BGRA
+#endif
             , rgba);
   free(rgba);
 
@@ -231,7 +235,11 @@ COverlayTextureGL::COverlayTextureGL(CDVDOverlaySpu* o)
             , o->width * 4
             , &m_u, &m_v
             , GL_RGBA
+#ifdef HAS_GLES
             , GL_RGBA
+#else
+            , GL_BGRA
+#endif
             , rgba + min_x + min_y * o->width);
 
   free(rgba);