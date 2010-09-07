5cff1b2ca1c0b970d4fd5ff339e1ef067f904d5d
elupus <elupus@svn>
2010-09-07 00:06:31 +0000

    changed: allow space between season and episode "S01 E01"
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33579 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/AdvancedSettings.cpp
+++ xbmc/AdvancedSettings.cpp
@@ -149,8 +149,8 @@ void CAdvancedSettings::Initialize()
   // in a flat dir structure, but is perfectly safe in a dir-per-vid one.
   //m_videoStackRegExps.push_back("(.*?)([ ._-]*[0-9])(.*?)(\\.[^.]+)$");
 
-  // foo.s01.e01, foo.s01_e01, S01E02 foo
-  m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[Ss]([0-9]+)[][._-]*[Ee]([0-9]+)([^\\\\/]*)$"));
+  // foo.s01.e01, foo.s01_e01, S01E02 foo, S01 - E02
+  m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[Ss]([0-9]+)[][._- ]*[Ee]([0-9]+)([^\\\\/]*)$"));
   // foo.ep01, foo.EP_01
   m_tvshowStackRegExps.push_back(TVShowRegexp(false,"[\\._ -]()[Ee][Pp]_?([0-9]+)([^\\\\/]*)$"));
   // foo.yyyy.mm.dd.* (byDate=true)
