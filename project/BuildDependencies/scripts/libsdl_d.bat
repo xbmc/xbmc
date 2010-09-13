760f8df6810c18b2e392dded15a870a21ceeaa55
jmarshallnz <jmarshallnz@svn>
2010-09-13 22:44:16 +0000

    fixed: win32 build deps for sdl_image
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33750 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- project/BuildDependencies/scripts/libsdl_d.bat
+++ project/BuildDependencies/scripts/libsdl_d.bat
@@ -12,6 +12,8 @@ copy SDL-1.2.14\lib\SDL.lib "%CUR_PATH%\lib\SDL.lib" /Y
 copy SDL-1.2.14\lib\SDL.dll "%XBMC_PATH%\project\Win32BuildSetup\dependencies\SDL.dll"
 
 copy SDL-1.2.14\lib\SDL.dll "%XBMC_PATH%\tools\TexturePacker\SDL.dll"
-copy *.dll "%XBMC_PATH%\tools\TexturePacker\"
+copy SDL_image-1.2.10\include\SDL_image.h "%CUR_PATH%\include\SDL\"
+copy SDL_image-1.2.10\lib\*.dll "%XBMC_PATH%\tools\TexturePacker\"
+copy SDL_image-1.2.10\lib\SDL_image.lib "%CUR_PATH%\lib\SDL_image.lib" /Y
 
 cd %LOC_PATH%
