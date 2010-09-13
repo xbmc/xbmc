43caa8af34f81dcce6f1aab27e0dfcd267e916c9
malard <malard@svn>
2010-09-13 23:17:50 +0000

    fixed: switching context wont leave the progress bar distorted; changed: video playlist only renders when needs to.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33762 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- addons/webinterface.default/js/NowPlayingManager.js
+++ addons/webinterface.default/js/NowPlayingManager.js
@@ -195,6 +195,7 @@ NowPlayingManager.prototype = {
 							} else {
 								$('#nextText').hide();
 								$('#nowPlayingPlaylist').hide();
+								$('#nextTrack').hide();
 							}
 							if (!this.comparePlaylistItems(activeItem, this.activePlaylistItem)) {
 								this.activePlaylistItem = activeItem;
@@ -246,14 +247,15 @@ NowPlayingManager.prototype = {
 					this.trackBaseTime = data.result.time;
 					this.playing = data.result.playing;
 					this.paused = data.result.paused;
-					if (!this.autoRefreshData) {
-						if (data.result.playing) {
-							this.autoRefreshData = true;
-						}
-						if (this.activePlayer == 'Audio') {
-							this.refreshAudioData();
-						} else if (this.activePlayer == 'Video') {
-							this.refreshVideoData();
+					if (!this.autoRefreshAudioData && !this.autoRefreshVideoData) {
+						if (data.result.playing) {				
+							if (this.activePlayer == 'Audio') {
+								this.autoRefreshAudioData = true;
+								this.refreshAudioData();
+							} else if (this.activePlayer == 'Video') {
+								this.autoRefreshVideoData = true;
+								this.refreshVideoData();
+							}
 						}
 					}
 				}
@@ -268,7 +270,7 @@ NowPlayingManager.prototype = {
 			this.refreshAudioData();
 		},
 		refreshAudioData: function() {
-			if (this.autoRefreshData && !this.audioRefreshTimer) {
+			if (this.autoRefreshAudioData && !this.audioRefreshTimer) {
 				this.audioRefreshTimer = 1;
 				setTimeout(jQuery.proxy(this.refreshAudioDataLoop, this), 1000);
 			}
@@ -296,6 +298,7 @@ NowPlayingManager.prototype = {
 						$('#audioAlbumTitle').hide();
 					}
 					$('#audioArtistTitle').html(this.activePlaylistItem.artist);
+					$('#progressBar').attr('style', '');
 				}
 				$('#audioDuration').html(durationToString(this.trackBaseTime) + ' / ' + durationToString(this.activePlaylistItem.duration));
 				var buttonWidth = $('#progressBar .progressIndicator').width();
@@ -315,7 +318,7 @@ NowPlayingManager.prototype = {
 			this.refreshVideoData();
 		},
 		refreshVideoData: function() {
-			if (this.autoRefreshData && !this.videoRefreshTimer) {
+			if (this.autoRefreshVideoData && !this.videoRefreshTimer) {
 				this.videoRefreshTimer = 1;
 				setTimeout(jQuery.proxy(this.refreshVideoDataLoop, this), 1000);
 			}
@@ -328,21 +331,24 @@ NowPlayingManager.prototype = {
 				this.showPauseButton();
 			}
 			if (this.activePlaylistItem) {
-				var imgPath = DEFAULT_VIDEO_COVER;
-				if (this.activePlaylistItem.thumbnail) {
-					imgPath = (this.activePlaylistItem.thumbnail.startsWith('special://') ? '/vfs/' : 'images/') + this.activePlaylistItem.thumbnail;
+				if (this.activePlaylistItem != this.lastPlaylistItem) {
+					this.lastPlaylistItem = this.activePlaylistItem;
+					var imgPath = DEFAULT_VIDEO_COVER;
+					if (this.activePlaylistItem.thumbnail) {
+						imgPath = (this.activePlaylistItem.thumbnail.startsWith('special://') ? '/vfs/' : 'images/') + this.activePlaylistItem.thumbnail;
+					}
+					$('#videoCoverArt').html('<img src="' + imgPath + '" alt="' + this.activePlaylistItem.title + ' cover art">');
+					var imgWidth = $('#videoCoverArt img').width();
+					$('#progressBar').width(365 - (imgWidth - 100));
+					$('#videoTrackWrap').width(365 - (imgWidth - 100));
+					$('#videoTitle').width(365 - (imgWidth - 100));
+					$('#videoShowTitle').html(this.activePlaylistItem.showtitle||'&nbsp;');
+					var extra = '';
+					if (this.activePlaylistItem.season && this.activePlaylistItem.episode) {
+						extra = this.activePlaylistItem.season + 'x' + this.activePlaylistItem.episode + ' ';
+					}
+					$('#videoTitle').html(extra + this.activePlaylistItem.title);
 				}
-				$('#videoCoverArt').html('<img src="' + imgPath + '" alt="' + this.activePlaylistItem.title + ' cover art">');
-				var imgWidth = $('#videoCoverArt img').width();
-				$('#progressBar').width(365 - (imgWidth - 100));
-				$('#trackWrap').width(365 - (imgWidth - 100));
-				$('#videoTitle').width(365 - (imgWidth - 100));
-				$('#videoShowTitle').html(this.activePlaylistItem.showtitle||'&nbsp;');
-				var extra = '';
-				if (this.activePlaylistItem.season && this.activePlaylistItem.episode) {
-					extra = this.activePlaylistItem.season + 'x' + this.activePlaylistItem.episode + ' ';
-				}
-				$('#videoTitle').html(extra + this.activePlaylistItem.title);
 				$('#videoDuration').html(durationToString(this.trackBaseTime) + ' / ' + durationToString(this.activePlaylistItem.duration));
 				var buttonWidth = $('#progressBar .progressIndicator').width();
 				var progressBarWidth = (this.trackBaseTime / this.activePlaylistItem.duration) * 100;
@@ -357,7 +363,8 @@ NowPlayingManager.prototype = {
 			}
 		},
 		stopRefreshTime: function() {
-			this.autoRefreshData = false;
+			this.autoRefreshAudioData = false;
+			this.autoRefreshVideoData = false;
 		},
 		comparePlaylistItems: function(item1, item2) {
 			if (!item1 || !item2) {
@@ -427,6 +434,7 @@ NowPlayingManager.prototype = {
 							} else {
 								$('#nextText').hide();
 								$('#nowPlayingPlaylist').hide();
+								$('#nextTrack').hide();
 							}
 							if (!this.comparePlaylistItems(activeItem, this.activePlaylistItem)) {
 								this.activePlaylistItem = activeItem;
