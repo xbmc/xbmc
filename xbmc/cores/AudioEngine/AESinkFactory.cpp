8e19e1b8a159523f3472aa4a388c3b53b71bb0b9
gnif2 <gnif2@svn>
2010-09-15 09:03:37 +0000

    added missing files
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/AudioEngine@33828 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/AudioEngine/AESinkFactory.cpp
@@ -0,0 +1,106 @@
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "AESinkFactory.h"
+#include "SystemInfo.h"
+
+#ifdef _WIN32
+#include "Sinks/AESinkWASAPI.h"
+#include "Sinks/AESinkDirectSound.h"
+#elif defined _LINUX
+#include "Sinks/AESinkALSA.h"
+#include "Sinks/AESinkOSS.h"
+#elif defined __APPLE__
+#include "Sinks/AESinkCoreAudio.h"
+#else
+#pragma message("NOTICE: No audio sink for target platform.  Audio output will not be available.")
+#endif
+
+#define TRY_SINK(SINK) \
+{ \
+  tmpFormat = desiredFormat; \
+  tmpDevice = device; \
+  sink      = new CAESink ##SINK(); \
+  if (sink->Initialize(tmpFormat, tmpDevice)) \
+  { \
+    desiredFormat = tmpFormat; \
+    device        = tmpDevice; \
+    return sink; \
+  } \
+  sink->Deinitialize(); \
+  delete sink; \
+}
+
+IAESink *CAESinkFactory::Create(CStdString &driver, CStdString &device, AEAudioFormat &desiredFormat)
+{
+  AEAudioFormat  tmpFormat;
+  CStdString     tmpDevice;
+  IAESink        *sink;
+
+#ifdef _WIN32
+  if(driver == "WASAPI")
+    TRY_SINK(WASAPI)
+  else if(driver == "DIRECTSOUND")
+    TRY_SINK(DirectSound)
+
+  if(g_sysinfo.IsVistaOrHigher())
+    TRY_SINK(WASAPI)
+  else
+    TRY_SINK(DirectSound)
+#elif defined _LINUX
+  if (driver == "ALSA")
+    TRY_SINK(ALSA)
+  else if (driver == "OSS")
+    TRY_SINK(OSS)
+
+  if(driver != "ALSA")
+      TRY_SINK(ALSA)
+  if(driver != "OSS" )
+    TRY_SINK(OSS)
+#elif defined __APPLE__
+  //TRY_SINK(CoreAudio);
+#endif
+
+  //Complete failure.
+  return NULL;
+}
+
+void CAESinkFactory::Enumerate(AEDeviceList &devices)
+{
+#ifdef _WIN32
+
+  if(g_sysinfo.IsVistaOrHigher())
+    CAESinkWASAPI::EnumerateDevices(devices, false);
+  else
+    CAESinkDirectSound::EnumerateDevices(devices, false);
+    
+#elif defined _LINUX
+
+  CAESinkALSA::EnumerateDevices(devices, false);
+  CAESinkOSS ::EnumerateDevices(devices, false);
+
+#elif defined __APPLE__
+
+  CAESinkCoreAudio::EnumerateDevices(devices, false);
+
+#endif
+}
+
