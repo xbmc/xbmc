7478e23935a9faa7f76f496d6786631f9ac8e57d
lcapriotti <lcapriotti@svn>
2010-09-16 10:26:04 +0000

    use rc.local since d-i kernel does not provide enough info to do it at install time
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@33882 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/XBMCLive/SDK/buildLive/Files/binary_local-includes/Hooks/hwSetup_NVIDIA_HDMI.sh
+++ tools/XBMCLive/SDK/buildLive/Files/binary_local-includes/Hooks/hwSetup_NVIDIA_HDMI.sh
@@ -18,15 +18,49 @@
 #  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 #  http://www.gnu.org/copyleft/gpl.html
 
-NvidiaHDMIFirstGen=$(lspci -nn | grep '0403' | grep '10de:0ac0') #MCP79 High Definition Audio 
-NvidiaHDMISecondGen=$(lspci -nn | grep '0403' | grep '10de:0be3') #MCP79 High Definition Audio 
+cat > /usr/bin/hwSetupNvidiaHDMI.sh << 'BLOB'
+#!/bin/bash -x
+
+#      Copyright (C) 2005-2008 Team XBMC
+#      http://www.xbmc.org
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with XBMC; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+
+#
+# Remove entry in rc.local - launch script automatically only once 
+#
+sed -i -e "/XBMCLIVE/d" /tmp/rc.local 
+
+#
+# Nvidia ION detection
+#
+
+NvidiaHDMIFirstGen=$(lspci -nn | grep '0403' | grep '10de:0ac0') #MCP79 High Definition Audio
+NvidiaHDMISecondGen=$(lspci -nn | grep '0403' | grep '10de:0be3') #MCP79 High Definition Audio
 
 if [ ! -n "$NvidiaHDMISecondGen" ] && [ ! -n "$NvidiaHDMIFirstGen" ] ; then
-        exit 0
+	exit 0
 fi
 
-HDMICARD=$(aplay -l | grep 'NVIDIA HDMI' | awk -F: '{ print $1 }' | awk '{ print $2 }')
-HDMIDEVICE=$(aplay -l | grep 'NVIDIA HDMI' | awk -F: '{ print $2 }' | awk '{ print $5 }')
+#
+# Set Nvidia HDMI variables
+#
+
+HDMICARD=$(aplay -l | grep 'NVIDIA HDMI' -m1 | awk -F: '{ print $1 }' | awk '{ print $2 }')
+HDMIDEVICE=$(aplay -l | grep 'NVIDIA HDMI' -m1 | awk -F: '{ print $2 }' | awk '{ print $5 }')
 
 if [ -n "$NvidiaHDMIFirstGen" ] ; then
 	# "VT1708S Digital"
@@ -45,6 +79,26 @@ if [ -n "$NvidiaHDMISecondGen" ] ; then
 fi
 
 #
+# Retrieve Digital Settings before .asoundrc creation
+#
+
+DIGITALCARD=$(aplay -l | grep "$DIGITALCONTROL" | awk -F: '{ print $1 }' | awk '{ print $2 }')
+DIGITALDEVICE=$(aplay -l | grep "$DIGITALCONTROL" | awk -F: '{ print $2 }' | awk '{ print $5 }')
+
+
+#
+# Bails out if we don't have digital outputs
+#
+if [ -z $HDMICARD ] || [ -z $HDMIDEVICE ] || [ -z $DIGITALCARD ] || [ -z $DIGITALDEVICE ]; then
+	exit 0
+fi
+
+#
+# Restart only if needed
+#
+restartALSA=""
+
+#
 # Setup kernel module parameters
 #
 
@@ -52,12 +106,13 @@ if [ -n "$NvidiaHDMISecondGen" ] ; then
 	if ! grep -i -q snd-hda-intel /etc/modprobe.d/alsa-base.conf ; then
 		if [ $HDMICARD,$HDMIDEVICE == 1,3 ]; then
 			echo 'options snd-hda-intel enable_msi=0 probe_mask=0xffff,0xfff2' >> /etc/modprobe.d/alsa-base.conf
-		elif [ $HDMICARD,$HDMIDEVICE == 0,3 ]; then
+			restartALSA="1"
+                elif [ $HDMICARD,$HDMIDEVICE == 0,3 ]; then
 			echo 'options snd-hda-intel enable_msi=0 probe_mask=0xfff2' >> /etc/modprobe.d/alsa-base.conf
-		elif [ $HDMICARD,$HDMIDEVICE == 2,3 ]; then
+			restartALSA="1"
+                elif [ $HDMICARD,$HDMIDEVICE == 2,3 ]; then
 			echo 'options snd-hda-intel enable_msi=0 probe_mask=0xffff,0xffff,0xfff2' >> /etc/modprobe.d/alsa-base.conf
-	#	else
-	#		echo "No matching value detected"
+			restartALSA="1"
 		fi
 	fi
 fi
@@ -66,10 +121,16 @@ fi
 # Setup .asoundrc
 #
 
-xbmcUser=$(getent passwd 1000 | sed -e 's/\:.*//')
+xbmcUser=xbmc
+# Read configuration variable file if it is present
+[ -r /etc/default/xbmc-live ] && . /etc/default/xbmc-live
+
+if ! getent passwd $xbmcUser >/dev/null; then
+	xbmcUser=$(getent passwd 1000 | sed -e 's/\:.*//')
+fi
 
 if [ ! -f /home/$xbmcUser/.asoundrc ] ; then
-	cat > /home/$xbmcUser/.asoundrc << 'EOF'
+        cat > /home/$xbmcUser/.asoundrc << 'EOF'
 pcm.!default {
         type plug
         slave {
@@ -135,22 +196,50 @@ pcm.receiver {
 }
 EOF
 
-	DIGITALCARD=$(aplay -l | grep $DIGITALCONTROL | awk -F: '{ print $1 }' | awk '{ print $2 }')
-	DIGITALDEVICE=$(aplay -l | grep $DIGITALCONTROL | awk -F: '{ print $2 }' | awk '{ print $5 }')
+        sed -i "s/=HDMICARD=/card $HDMICARD/g" /home/$xbmcUser/.asoundrc
+        sed -i "s/=HDMIDEVICE=/device $HDMIDEVICE/g" /home/$xbmcUser/.asoundrc
 
-	sed -i "s/=HDMICARD=/card $HDMICARD/g" /home/$xbmcUser/.asoundrc
-	sed -i "s/=HDMIDEVICE=/device $HDMIDEVICE/g" /home/$xbmcUser/.asoundrc
+        sed -i "s/=DIGITALCARD=/card $DIGITALCARD/g" /home/$xbmcUser/.asoundrc
+        sed -i "s/=DIGITALDEVICE=/device $DIGITALDEVICE/g" /home/$xbmcUser/.asoundrc
 
-	sed -i "s/=DIGITALCARD=/card $DIGITALCARD/g" /home/$xbmcUser/.asoundrc
-	sed -i "s/=DIGITALDEVICE=/device $DIGITALDEVICE/g" /home/$xbmcUser/.asoundrc
+        chown -R $xbmcUser:$xbmcUser /home/$xbmcUser/.asoundrc
 
-	chown -R $xbmcUser:$xbmcUser /home/$xbmcUser/.asoundrc
+	restartALSA="1"
 fi
 
-#
-# Unmute digital output
-#
 
-/usr/bin/amixer -q -c 0 sset 'IEC958 Default PCM',0 unmute > /dev/null
-/usr/bin/amixer -q -c 0 sset 'IEC958',0 unmute > /dev/null
-/usr/bin/amixer -q -c 0 sset 'IEC958',1 unmute > /dev/null
+if [ -n "$restartALSA" ] ; then
+	#
+	# Restart Alsa
+	#
+
+	alsa-utils stop &> /dev/null
+	alsa force-reload &> /dev/null
+	alsa-utils start &> /dev/null
+
+	#
+	# Unmute digital output
+	#
+
+	/usr/bin/amixer -q -c 0 sset 'IEC958 Default PCM',0 unmute &> /dev/null
+	/usr/bin/amixer -q -c 0 sset 'IEC958',0 unmute &> /dev/null
+	/usr/bin/amixer -q -c 0 sset 'IEC958',1 unmute &> /dev/null
+
+	#
+	# Store alsa settings
+	#
+
+	alsactl store &> /dev/null
+fi
+
+# Remove entry in rc.local
+sed -i -e "/XBMCLIVE/d" /etc/rc.local 
+BLOB
+chmod +x /usr/bin/hwSetupNvidiaHDMI.sh
+
+sed -i -e "/exit 0/d" /etc/rc.local 
+
+echo "# XBMCLIVE" >> /etc/rc.local 
+echo "/usr/bin/hwSetupNvidiaHDMI.sh # XBMCLIVE" >> /etc/rc.local 
+echo "# XBMCLIVE" >> /etc/rc.local 
+echo "exit 0" >> /etc/rc.local 
