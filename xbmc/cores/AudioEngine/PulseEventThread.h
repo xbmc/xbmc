3e14e497152528e05a8ba2b78ba02fb7cfa16105
gnif2 <gnif2@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2010-09-17 04:06:23 +0000

    moved: [AE] renamed and split out the new PulseEventThread into its own files
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/AudioEngine@33916 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/AudioEngine/PulseEventThread.h
@@ -0,0 +1,52 @@
+#pragma once
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "PulseStream.h"
+#include "utils/Event.h"
+#include "utils/Thread.h"
+#include "utils/CriticalSection.h"
+
+class CPulseStream;
+class CPulseEventThread : public IRunnable
+{
+public:
+  virtual void Run();
+
+private:
+  friend class CPulseStream;
+
+  bool                 m_run;
+  CPulseStream        *m_stream;
+  CEvent               m_event;
+  CThread              m_thread;
+  IAEStream::AECBFunc *m_cbFunc;
+  void                *m_arg;
+  CCriticalSection     m_lock;
+  CCriticalSection     m_lockEvent;
+
+  CPulseEventThread(CPulseStream *stream);
+  virtual ~CPulseEventThread();
+
+  void Trigger();
+  void SetCallback(IAEStream::AECBFunc *cbFunc, void *arg);
+};
+
