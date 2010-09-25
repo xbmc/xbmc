d9eacfca38816fed0322f5aaba0e3e47830b68e4
blinkseb <blinkseb@svn>
2010-09-25 14:44:21 +0000

    fixed: use utf-16 on character uppercase
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34180 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GUIBaseContainer.cpp
+++ guilib/GUIBaseContainer.cpp
@@ -807,8 +807,7 @@ void CGUIBaseContainer::UpdateScrollByLetter()
     CGUIListItemPtr item = m_items[i];
     // The letter offset jumping is only for ASCII characters at present, and
     // our checks are all done in uppercase
-    CStdString nextLetter = item->GetSortLabel().Left(1);
-    nextLetter.ToUpper();
+    CStdString nextLetter = g_charsetConverter.utf8Left(item->GetSortLabel().Left(1).ToUpper(), 1);
     if (currentMatch != nextLetter)
     {
       currentMatch = nextLetter;
