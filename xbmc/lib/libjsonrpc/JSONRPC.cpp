f86b5b77dc8be4bc48240e33a964cccfc307a53d
malard <malard@svn>
2010-10-02 23:33:47 +0000

    added: GetGenres jsonrpc method - Thanks Kabooga! #9469
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34375 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/lib/libjsonrpc/JSONRPC.cpp
+++ xbmc/lib/libjsonrpc/JSONRPC.cpp
@@ -155,7 +155,7 @@ Command CJSONRPC::m_commands[] = {
   { "AudioLibrary.GetArtists",                      CAudioLibrary::GetArtists,                           Response,     ReadData,        "Retrieve all artists" },
   { "AudioLibrary.GetAlbums",                       CAudioLibrary::GetAlbums,                            Response,     ReadData,        "Retrieve all albums from specified artist or genre, Fields: album_description, album_theme, album_mood, album_style, album_type, album_label, album_artist, album_genre, album_rating, album_title" },
   { "AudioLibrary.GetSongs",                        CAudioLibrary::GetSongs,                             Response,     ReadData,        "Retrieve all songs from specified album, artist or genre" },
-
+  { "AudioLibrary.GetGenres",                       CAudioLibrary::GetGenres,                            Response,     ReadData,        "Retrieve all genres" },
   { "AudioLibrary.ScanForContent",                  CAudioLibrary::ScanForContent,                       Response,     ScanLibrary,     "" },
 
 // Video library
