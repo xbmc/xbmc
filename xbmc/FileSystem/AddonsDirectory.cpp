27b106f15e02a82b98a26ea8c95b150ef7ff2e41
spiff_ <spiff_@svn>
2010-10-04 23:46:10 +0000

    fixed: get more... should always sort on bottom
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34455 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/AddonsDirectory.cpp
+++ xbmc/FileSystem/AddonsDirectory.cpp
@@ -248,6 +248,7 @@ bool CAddonsDirectory::GetScriptsAndPlugins(const CStdString &content, CFileItem
   item->SetLabelPreformated(true);
   item->SetLabel(g_localizeStrings.Get(21452));
   item->SetIconImage("DefaultAddon.png");
+  item->SetSpecialSort(SORT_ON_BOTTOM);
   items.Add(item);
 
   items.SetContent("addons");
