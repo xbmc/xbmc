08de1bac7fb42a045a7a60b0da4925ecdb0bd828
gnif2 <gnif2@svn>
2010-10-08 05:41:28 +0000

    added: AEEncoder interface (based on IDVDAudioEncoder) added: initial implementation of AEEncoderFFmpeg (based on DVDAudioEncoderFFmpeg)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/AudioEngine@34554 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ xbmc/cores/AudioEngine/AEEncoder.h
@@ -0,0 +1,89 @@
+#pragma once
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "DVDStreamInfo.h"
+#include "AEAudioFormat.h"
+
+/**
+ * IAEEncoder interface for on the fly audio compression
+ */
+class IAEEncoder
+{
+public:
+  /**
+   * Constructor
+   */
+  IAEEncoder() {};
+
+  /**
+   * Destructor
+   */
+  virtual ~IAEEncoder() {};
+
+  /**
+   * Called to setup the encoder to accept data in the specified format
+   * @param channels the number of channels in the channel layout
+   * @param channelLayout the channel layout
+   * @param sampleRate the source audio sample rate
+   * @return true on success, false on failure
+   */
+  virtual bool Initialize(unsigned int channels, enum AEChannel *channelLayout, unsigned int sampleRate) = 0;
+
+  /**
+   * Reset the encoder for new data
+   */
+  virtual void Reset() = 0;
+
+  /**
+   * Returns the bitrate of the encoder
+   * @return bit rate in bits per second
+   */
+  virtual unsigned int GetBitRate() = 0;
+
+  /**
+   * Returns the CodecID of the encoder
+   * @return the ffmpeg codec id
+   */
+  virtual CodecID GetCodecID() = 0;
+
+  /**
+   * Return the number of frames needed to encode
+   * @return number of frames (frames * channels = samples * bits per sample = bytes)
+   */
+  virtual unsigned int GetFrames() = 0;
+
+  /**
+   * Encodes the supplied samples
+   * @param data the PCM samples in float format
+   * @param frames the number of audio frames in data (bytes / bits per sample = samples / channels = frames)
+   * @return the number of samples consumed
+   */
+  virtual int Encode (float *data, unsigned int frames) = 0;
+
+  /**
+   * Get the encoded data
+   * @param data return pointer to the buffer with the current encoded block
+   * @return the size in bytes of *data
+   */
+  virtual int GetData(uint8_t **data) = 0;
+};
+
---
+++ xbmc/cores/AudioEngine/Encoders/AEEncoderFFmpeg.cpp
@@ -0,0 +1,141 @@
+/*
+ *      Copyright (C) 2005-2010 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#define AC3_ENCODE_BITRATE 640000
+
+#include "AEEncoderFFmpeg.h"
+#include <string.h>
+
+CAEEncoderFFmpeg::CAEEncoderFFmpeg():
+  m_CodecCtx(NULL),
+  m_Buffer  (NULL),
+  m_Remapped(NULL)
+{
+}
+
+CAEEncoderFFmpeg::~CAEEncoderFFmpeg()
+{
+  delete[] m_Remapped;
+
+  Reset();
+  m_dllAvUtil.av_freep(&m_CodecCtx);
+}
+
+bool CAEEncoderFFmpeg::Initialize(unsigned int channels, AEChLayout channelMap, unsigned int sampleRate)
+{
+  Reset();
+  if (!channelMap || !m_dllAvUtil.Load() || !m_dllAvCodec.Load())
+    return false;
+
+  AVCodec *codec;
+  codec = m_dllAvCodec.avcodec_find_encoder(CODEC_ID_AC3);
+  if (!codec)
+    return false;
+
+  /* always assume 6 channels, 5.1... m_remap will give us what we want */
+  m_CodecCtx = m_dllAvCodec.avcodec_alloc_context();
+  m_CodecCtx->bit_rate       = AC3_ENCODE_BITRATE;
+  m_CodecCtx->sample_rate    = sampleRate;
+  m_CodecCtx->channels       = 6;
+  m_CodecCtx->channel_layout = CH_LAYOUT_5POINT1_BACK;
+  m_CodecCtx->sample_fmt     = SAMPLE_FMT_FLT;
+
+  if (m_dllAvCodec.avcodec_open(m_CodecCtx, codec))
+  {
+    m_dllAvUtil.av_freep(&m_CodecCtx);
+    return false;
+  }
+
+  /* remap the channels according to the specified channel layout */
+  int index = 0;
+  if (m_CodecCtx->channel_layout & CH_FRONT_LEFT           ) m_ChannelMap[index++] = AE_CH_FL  ;
+  if (m_CodecCtx->channel_layout & CH_FRONT_RIGHT          ) m_ChannelMap[index++] = AE_CH_FR  ;
+  if (m_CodecCtx->channel_layout & CH_FRONT_CENTER         ) m_ChannelMap[index++] = AE_CH_FC  ;
+  if (m_CodecCtx->channel_layout & CH_LOW_FREQUENCY        ) m_ChannelMap[index++] = AE_CH_LFE ;
+  if (m_CodecCtx->channel_layout & CH_BACK_LEFT            ) m_ChannelMap[index++] = AE_CH_BL  ;
+  if (m_CodecCtx->channel_layout & CH_BACK_RIGHT           ) m_ChannelMap[index++] = AE_CH_BR  ;
+  if (m_CodecCtx->channel_layout & CH_FRONT_LEFT_OF_CENTER ) m_ChannelMap[index++] = AE_CH_FLOC;
+  if (m_CodecCtx->channel_layout & CH_FRONT_RIGHT_OF_CENTER) m_ChannelMap[index++] = AE_CH_FROC;
+  if (m_CodecCtx->channel_layout & CH_BACK_CENTER          ) m_ChannelMap[index++] = AE_CH_BC  ;
+  if (m_CodecCtx->channel_layout & CH_SIDE_LEFT            ) m_ChannelMap[index++] = AE_CH_SL  ;
+  if (m_CodecCtx->channel_layout & CH_SIDE_RIGHT           ) m_ChannelMap[index++] = AE_CH_SR  ;
+  if (m_CodecCtx->channel_layout & CH_TOP_CENTER           ) m_ChannelMap[index++] = AE_CH_TC  ;
+  if (m_CodecCtx->channel_layout & CH_TOP_FRONT_LEFT       ) m_ChannelMap[index++] = AE_CH_TFL ;
+  if (m_CodecCtx->channel_layout & CH_TOP_FRONT_CENTER     ) m_ChannelMap[index++] = AE_CH_TFC ;
+  if (m_CodecCtx->channel_layout & CH_TOP_FRONT_RIGHT      ) m_ChannelMap[index++] = AE_CH_TFR ;
+  if (m_CodecCtx->channel_layout & CH_TOP_BACK_LEFT        ) m_ChannelMap[index++] = AE_CH_TBL ;
+  if (m_CodecCtx->channel_layout & CH_TOP_BACK_CENTER      ) m_ChannelMap[index++] = AE_CH_TBC ;
+  if (m_CodecCtx->channel_layout & CH_TOP_BACK_RIGHT       ) m_ChannelMap[index++] = AE_CH_TBR ;
+
+  if (!m_Remap.Initialize(channelMap, m_ChannelMap, false, false))
+  {
+    m_dllAvUtil.av_freep(&m_CodecCtx);
+    return false; 
+  }
+
+  m_NeededFrames = m_CodecCtx->frame_size;
+  m_Buffer       = new uint8_t[FF_MIN_BUFFER_SIZE];
+  m_Remapped     = new float[m_CodecCtx->frame_size * channels];
+
+  return true;
+}
+
+void CAEEncoderFFmpeg::Reset()
+{
+  m_BufferSize = 0;
+}
+
+unsigned int CAEEncoderFFmpeg::GetBitRate()
+{
+  return AC3_ENCODE_BITRATE;
+}
+
+CodecID CAEEncoderFFmpeg::GetCodecID()
+{
+  return CODEC_ID_AC3;
+}
+
+unsigned int CAEEncoderFFmpeg::GetFrames()
+{
+  return m_NeededFrames;
+}
+
+int CAEEncoderFFmpeg::Encode(float *data, unsigned int frames)
+{
+  /* remap the data and encode it in blocks */
+  if (frames < m_NeededFrames)
+    return 0;
+
+  m_Remap.Remap(data, m_Remapped, m_NeededFrames);
+  m_BufferSize = m_dllAvCodec.avcodec_encode_audio(m_CodecCtx, m_Buffer, FF_MIN_BUFFER_SIZE, (short*)m_Remapped);
+
+  return m_NeededFrames;
+}
+
+int CAEEncoderFFmpeg::GetData(uint8_t **data)
+{
+  int size;
+  *data = m_Buffer;
+  size  = m_BufferSize;
+  m_BufferSize = 0;
+  return size;
+}
+
