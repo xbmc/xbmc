xbmc/xbmc

commit 9d53397603249227fe20a402d57b72ed7d466d7a

fixed: inconsistent labels for add-ons in the media windows.
(cherry picked from commit c8c5843cefc8f740aa19f34b7d826c8428947c87)

git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@34596 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/FileSystem/AddonsDirectory.cpp
@@ -206,8 +206,9 @@ CFileItemPtr CAddonsDirectory::FileItemFromAddon(AddonPtr &addon, const CStdStri
 
   CFileItemPtr item(new CFileItem(path, folder));
   item->SetLabel(addon->Name());
-  item->SetLabel2(addon->Summary());
+  item->SetLabel2(addon->Version().str);
   item->SetThumbnailImage(addon->Icon());
+  item->SetLabelPreformated(true);
   item->SetIconImage("DefaultAddon.png");
   item->SetProperty("fanart_image", addon->FanArt());
   CAddonDatabase::SetPropertiesFromAddon(addon, item);

commit c3858aea99a20ab49fdd4c964895be4fbafa3f80

fixed: regression from r34596 - add-on version shown as label2 for repositories
git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34641 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/FileSystem/AddonsDirectory.cpp
@@ -208,7 +208,8 @@ CFileItemPtr CAddonsDirectory::FileItemFromAddon(AddonPtr &addon, const CStdStri
 
   CFileItemPtr item(new CFileItem(path, folder));
   item->SetLabel(addon->Name());
-  item->SetLabel2(addon->Version().str);
+  if (!(basePath.Equals("addons://") && addon->Type() == ADDON_REPOSITORY))
+    item->SetLabel2(addon->Version().str);
   item->SetThumbnailImage(addon->Icon());
   item->SetLabelPreformated(true);
   item->SetIconImage("DefaultAddon.png");