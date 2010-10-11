690178fb80593ae4c3e0d8eb17c8b59a9e11eaae
theuni <theuni@svn>
2010-10-11 16:37:03 +0000

    To cptspiff's dismay, actually adding rss editor addon to svn to make Dharma packaging easier. Not needed in trunk.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@34686 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

---
+++ addons/script.rss.editor/default.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import xbmc
+import xbmcaddon
+
+# Script constants
+__scriptname__ = "RSS Editor"
+__author__ = "rwparris2"
+__url__ = "http://code.google.com/p/xbmc-addons/"
+__credits__ = "Team XBMC"
+__version__ = "1.5.6"
+__settings__ = xbmcaddon.Addon(id='script.rss.editor')
+__language__ = __settings__.getLocalizedString
+
+print "[SCRIPT] '%s: version %s' initialized!" % (__scriptname__, __version__, )
+
+if (__name__ == "__main__"):
+    import resources.lib.rssEditor as rssEditor
+    ui = rssEditor.GUI("script-RSS_Editor-rssEditor.xml", os.getcwd(), "default", setNum = 'set1')
+    del ui
+
+sys.modules.clear()
