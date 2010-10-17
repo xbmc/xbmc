d9e98ca60bda8c19105694ee26fca15d0ab3ade0
dteirney <dteirney@svn>
2010-10-17 01:22:11 +0000

    Fixed: when scanning videos using the VideoInfoTag, make sure that the season and episode are valid before using them as the match otherwise the title search in the scanner never gets triggered.
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34793 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/VideoInfoScanner.cpp
+++ xbmc/VideoInfoScanner.cpp
@@ -788,9 +788,9 @@ namespace VIDEO
     CVideoInfoTag* tag = item->GetVideoInfoTag();
     /*
      * First check the season and episode number. This takes precedence over the original air
-     * date and episode title.
+     * date and episode title. Must be a valid season and episode number combination.
      */
-    if (tag->m_iSeason > -1 && tag->m_iEpisode > -1)
+    if (tag->m_iSeason > -1 && tag->m_iEpisode > 0)
     {
       SEpisode episode;
       episode.strPath = item->m_strPath;
