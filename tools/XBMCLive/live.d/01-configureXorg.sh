1efe2e9cd3e5afa20f9dd716e96dcd716c70d0f1
lcapriotti <lcapriotti@svn>
2010-10-22 07:25:27 +0000

    allow aticonfig work to on a proper environment
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@34933 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- tools/XBMCLive/live.d/01-configureXorg.sh
+++ tools/XBMCLive/live.d/01-configureXorg.sh
@@ -67,6 +67,13 @@ if [ "$GPUTYPE" = "NVIDIA" ]; then
 fi
 
 if [ "$GPUTYPE" = "AMD" ]; then
+	# Try fglrx first
+	if [ $LSBRELEASE -gt 910 ]; then
+		# only on lucid!
+		update-alternatives --set gl_conf /usr/lib/fglrx/ld.so.conf
+		ldconfig
+	fi
+
 	# run aticonfig
 	/usr/lib/fglrx/bin/aticonfig --initial --sync-vsync=on -f
 	ATICONFIG_RETURN_CODE=$? 
@@ -74,13 +81,13 @@ if [ "$GPUTYPE" = "AMD" ]; then
 	if [ $ATICONFIG_RETURN_CODE -eq 255 ]; then
 		# aticonfig returns 255 on old unsuported ATI cards 
 		# Let the X default ati driver handle the card 
-		modprobe radeon # Required to permit KMS switching and support hardware GL  
-	else
 		if [ $LSBRELEASE -gt 910 ]; then
 			# only on lucid!
-			update-alternatives --set gl_conf /usr/lib/fglrx/ld.so.conf
+			# revert to mesa
+			update-alternatives --set gl_conf /usr/lib/mesa/ld.so.conf
 			ldconfig
 		fi
+		modprobe radeon # Required to permit KMS switching and support hardware GL  
 	fi
 fi
 
