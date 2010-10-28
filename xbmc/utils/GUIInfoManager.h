773855f33b1d3c369707d495d67c4de0f28995df
theuni <theuni@svn>
2010-10-28 20:14:05 +0000

    changed: update version strings for beta4
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@35067 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/utils/GUIInfoManager.h
+++ xbmc/utils/GUIInfoManager.h
@@ -431,7 +431,7 @@ class CDateTime;
 
 // Version string MUST NOT contain spaces.  It is used
 // in the HTTP request user agent.
-#define VERSION_STRING "10.0-beta3"
+#define VERSION_STRING "10.0-beta4"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
