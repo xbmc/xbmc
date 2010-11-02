d14c3dd00cd93702cb25bef32067a5a044b4b342
vdrfan <vdrfan@svn>
2010-11-02 16:55:34 +0000

    fixed: #10589 - clean library on http(s) source empties the library (again)
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@35130 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/FileSystem/HTTPDirectory.h
+++ xbmc/FileSystem/HTTPDirectory.h
@@ -30,6 +30,7 @@ namespace XFILE
       CHTTPDirectory(void);
       virtual ~CHTTPDirectory(void);
       virtual bool GetDirectory(const CStdString& strPath, CFileItemList &items);
+      virtual bool Exists(const char* strPath);
       virtual DIR_CACHE_TYPE GetCacheType(const CStdString& strPath) const { return DIR_CACHE_ONCE; };
     private:
   };
