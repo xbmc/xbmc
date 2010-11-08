c5afc06c2ee886ba5c812060fa28a35263a92e5f
vdrfan <vdrfan@svn>
2010-11-08 15:26:26 +0000

    fixed: #10652 - add-ons not fully sorted by name
    (cherry picked from commit 802a6132d1f75800c4d58331e537c3369168571d)
    
    git-svn-id: https://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/Dharma@35206 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIViewStateAddonBrowser.cpp
+++ xbmc/GUIViewStateAddonBrowser.cpp
@@ -35,9 +35,9 @@ using namespace ADDON;
 
 CGUIViewStateAddonBrowser::CGUIViewStateAddonBrowser(const CFileItemList& items) : CGUIViewState(items)
 {
-  AddSortMethod(SORT_METHOD_LABEL, 551, LABEL_MASKS("%L", "%I", "%L", ""));  // Filename, Size | Foldername, empty
+  AddSortMethod(SORT_METHOD_LABEL_IGNORE_FOLDERS, 551, LABEL_MASKS("%L", "%I", "%L", ""));  // Filename, Size | Foldername, empty
   AddSortMethod(SORT_METHOD_DATE, 552, LABEL_MASKS("%L", "%J", "%L", "%J"));  // Filename, Date | Foldername, Date
-  SetSortMethod(SORT_METHOD_LABEL);
+  SetSortMethod(SORT_METHOD_LABEL_IGNORE_FOLDERS);
 
   SetViewAsControl(DEFAULT_VIEW_LIST);
 
