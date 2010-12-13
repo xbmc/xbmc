545f2cea3a4428ed2c82aac7f98579d0de1fe6de
spiff <spiff@xbmc.org>
2010-12-13 17:31:54 +0100

    add copyright headers to the udf 2.5 code

--- xbmc/FileSystem/udf25.cpp
+++ xbmc/FileSystem/udf25.cpp
@@ -1,3 +1,27 @@
+/*
+ *      Copyright (C) 2010 Team Boxee
+ *      http://www.boxee.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ *
+ * Note: parts of this code comes from libdvdread.
+ * Jorgen Lundman and team boxee did the necessary modifications to support udf 2.5
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
