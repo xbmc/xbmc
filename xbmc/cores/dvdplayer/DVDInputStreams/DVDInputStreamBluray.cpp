cc04fb3a8da728e55f95ba25f0ba8e71b7122e2e
spiff <spiff@xbmc.org>
2010-12-13 19:46:29 +0100

    changed: enable chunked reads for bd's

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -207,7 +207,7 @@ static BD_FILE_H *file_open(const char* filename, const char *mode)
     file->eof   = file_eof;
 
     CFile* fp = new CFile();
-    if(fp->Open(strFilename))
+    if(fp->Open(strFilename,READ_CHUNKED))
     {
       file->internal = (void*)fp;
       return file;
