eb727638ed9f837e67ddfab4c2942039e4958a29
CrystalP <CrystalP@xbmc.org>
2011-01-14 21:32:45 -0500

    cosmetic

--- project/Win32BuildSetup/extract_git_rev.bat
+++ project/Win32BuildSetup/extract_git_rev.bat
@@ -1,33 +1,33 @@
-@echo off
+@ECHO OFF
 
-rem Batch file output: %GIT_REV% variable, containing the git revision
+REM Batch file output: %GIT_REV% variable, containing the git revision
 
-rem Use tgit.exe of TortoiseGit if available
+REM Use tgit.exe of TortoiseGit if available
 SET GITEXE="tgit.exe"
 %GITEXE% --version > NUL 2>&1
-if errorlevel 1 goto :notgit
+IF errorlevel 1 GOTO :notgit
 GOTO :extract
 
 :notgit
 
-rem Fallback on msysgit - must be in the path
+REM Fallback on msysgit - must be in the path
 SET GITEXE="git.exe"
 %GITEXE% --help > NUL 2>&1
-if errorlevel 1 goto :nogit
+IF errorlevel 1 GOTO :nogit
 GOTO :extract
 
 :nogit
 
-rem Failure - no git tool to extract information.
+REM Failure - no git tool to extract information.
 SET GIT_REV=Unknown
 GOTO :done
 
 :extract
 
-set oldCurrentDir=%CD%
-cd ..\..
+SET oldCurrentDir=%CD%
+CD ..\..
 FOR /F "tokens=1 delims= " %%A IN ('%GITEXE% rev-parse --short HEAD') DO SET GIT_REV=%%A
-cd %oldCurrentDir%
+CD %oldCurrentDir%
 
 :done
 
