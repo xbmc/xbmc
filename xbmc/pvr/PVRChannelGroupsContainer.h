38020e670974ad62bcb68518e2920c08ae860211
Lars Op den Kamp <lars@opdenkamp.eu>
2011-01-22 15:51:05 +0100

    pvr: documentation in PVRChannelGroupsContainer

--- xbmc/pvr/PVRChannelGroupsContainer.h
+++ xbmc/pvr/PVRChannelGroupsContainer.h
@@ -26,23 +26,64 @@
 class CPVRChannelGroupsContainer
 {
 private:
-  CPVRChannelGroups *m_groupsRadio;
-  CPVRChannelGroups *m_groupsTV;
+  CPVRChannelGroups *m_groupsRadio; /*!< all radio channel groups */
+  CPVRChannelGroups *m_groupsTV;    /*!< all TV channel groups */
 
 public:
+  /*!
+   * @brief Create a new container for all channel groups
+   */
   CPVRChannelGroupsContainer(void);
   ~CPVRChannelGroupsContainer(void);
 
+  /*!
+   * @brief Load all channel groups and all channels in those channel groups.
+   * @return True if all groups were loaded, false otherwise.
+   */
   bool Load(void);
+
+  /*!
+   * @brief Unload and destruct all channel groups and all channels in them.
+   */
   void Unload(void);
 
+  /*!
+   * @brief Get the TV channel groups.
+   * @return The TV channel groups.
+   */
   CPVRChannelGroups *GetTV(void) { return Get(false); }
+
+  /*!
+   * @brief Get the radio channel groups.
+   * @return The radio channel groups.
+   */
   CPVRChannelGroups *GetRadio(void) { return Get(true); }
+
+  /*!
+   * @brief Get the radio or TV channel groups.
+   * @param bRadio If true, get the radio channel groups. Get the TV channel groups otherwise.
+   * @return The requested groups.
+   */
   CPVRChannelGroups *Get(bool bRadio);
 
+  /*!
+   * @brief Get the group containing all TV channels.
+   * @return The group containing all TV channels.
+   */
   CPVRChannelGroup *GetGroupAllTV(void) { return GetGroupAll(false); }
+
+  /*!
+   * @brief Get the group containing all radio channels.
+   * @return The group containing all radio channels.
+   */
   CPVRChannelGroup *GetGroupAllRadio(void) { return GetGroupAll(true); }
+
+  /*!
+   * @brief Get the group containing all TV or radio channels.
+   * @param bRadio If true, get the group containing all radio channels. Get the group containing all TV channels otherwise.
+   * @return The requested group.
+   */
   CPVRChannelGroup *GetGroupAll(bool bRadio);
 };
 
-extern CPVRChannelGroupsContainer g_PVRChannelGroups;
+extern CPVRChannelGroupsContainer g_PVRChannelGroups; /*!< The channel groups container, containing all TV and radio channel groups. */
