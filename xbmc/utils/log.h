d79f3bd0a499b3f8cd0fc3c69d11ca18fc5a1de9
althekiller <althekiller@xbmc.org>
2011-01-28 03:50:54 -0500

    Move GL utility functions out of logging class

--- xbmc/utils/log.h
+++ xbmc/utils/log.h
@@ -62,24 +62,3 @@ public:
   static bool Init(const char* path);
 };
 
-// GL Error checking macro
-// this function is useful for tracking down GL errors, which otherwise
-// just result in undefined behavior and can be difficult to track down.
-//
-// Just call it 'VerifyGLState()' after a sequence of GL calls
-//
-// if GL_DEBUGGING and HAS_GL are defined, the function checks
-// for GL errors and prints the current state of the various matrices;
-// if not it's just an empty inline stub, and thus won't affect performance
-// and will be optimized out.
-
-void _VerifyGLState(const char* szfile, const char* szfunction, int lineno);
-#if defined(GL_DEBUGGING) && defined(HAS_GL)
-#define VerifyGLState() _VerifyGLState(__FILE__, __FUNCTION__, __LINE__)
-#else
-#define VerifyGLState()
-#endif
-
-void LogGraphicsInfo();
-
-
