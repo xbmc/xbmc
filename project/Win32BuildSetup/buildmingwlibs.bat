652e8e30fd053f95d0defc5e3393fae38aacd513
WiSo <wiso@xbmc.org>
2011-01-30 00:00:50 +0100

    [WIN32] changed: use a shell script for the mingw dlls and call it from the batch file. more flexible and working better.

--- project/Win32BuildSetup/buildmingwlibs.bat
+++ project/Win32BuildSetup/buildmingwlibs.bat
@@ -1,8 +1,4 @@
 @ECHO OFF
 rem compiles a bunch of mingw libs and not more
-rem might be a nice addition if we check for success?
-rem still not working right
-
-..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -e /bin/sh --login /xbmc/lib/ffmpeg/build_xbmc_win32.sh
-..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -e /bin/sh --login /xbmc/lib/libdvd/build-xbmc-win32.sh
+..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -title "building mingw dlls" -e /bin/sh --login /xbmc/project/Win32BuildSetup/buildmingwlibs.sh
 
---
+++ project/Win32BuildSetup/buildmingwlibs.sh
@@ -0,0 +1,12 @@
+
+echo "##### building ffmpeg dlls #####"
+#sh /xbmc/lib/ffmpeg/build_xbmc_win32.sh
+echo "##### building of ffmpeg dlls done #####"
+
+echo "##### building libdvd dlls #####"
+sh /xbmc/lib/libdvd/build-xbmc-win32.sh
+echo "##### building of libdvd dlls done #####"
+
+# wait for key press
+echo press a key to close the window
+read
