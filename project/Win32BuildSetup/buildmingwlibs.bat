c99bbf45e1f8d2f7d590231190351ef3090d263b
WiSo <wiso@xbmc.org>
2011-01-29 18:30:37 +0100

    [WIN32] use rxvt and don't force interaction

--- project/Win32BuildSetup/buildmingwlibs.bat
+++ project/Win32BuildSetup/buildmingwlibs.bat
@@ -3,6 +3,6 @@ rem compiles a bunch of mingw libs and not more
 rem might be a nice addition if we check for success?
 rem still not working right
 
-..\BuildDependencies\msys\bin\sh --login -i /xbmc/lib/ffmpeg/build_xbmc_win32.sh
-..\BuildDependencies\msys\bin\sh --login -i /xbmc/lib/libdvd/build-xbmc-win32.sh
+..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -e /bin/sh --login /xbmc/lib/ffmpeg/build_xbmc_win32.sh
+..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -e /bin/sh --login /xbmc/lib/libdvd/build-xbmc-win32.sh
 
