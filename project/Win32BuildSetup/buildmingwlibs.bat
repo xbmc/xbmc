c1345070ae927c4f0d345c30a6cc8f7dbe44f79a
WiSo <wiso@xbmc.org>
2011-01-31 18:18:14 +0100

    [WIN32] set M$ env

--- project/Win32BuildSetup/buildmingwlibs.bat
+++ project/Win32BuildSetup/buildmingwlibs.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+rem set M$ env
+call "%VS100COMNTOOLS%..\..\VC\bin\vcvars32.bat"
 rem compiles a bunch of mingw libs and not more
 ..\BuildDependencies\msys\bin\rxvt -backspacekey  -sl 2500 -sr -fn Courier-12 -tn msys -geometry 120x25 -title "building mingw dlls" -e /bin/sh --login /xbmc/project/Win32BuildSetup/buildmingwlibs.sh
 
