a75e552c0d713ab461782b58854d995406cfe806
WiSo <wiso@xbmc.org>
2011-02-01 20:42:39 +0100

    [WIN32] preparations to build mingw dlls withing the buildsetup process. still commented out.

--- project/Win32BuildSetup/BuildSetup.bat
+++ project/Win32BuildSetup/BuildSetup.bat
@@ -127,6 +127,13 @@ IF %comp%==vs2010 (
   GOTO MAKE_BUILD_EXE
 
 :MAKE_BUILD_EXE
+  rem ECHO Compiling mingw libs
+  rem call buildmingwlibs.bat
+  rem IF EXIST errormingw (
+  rem 	set DIETEXT="failed to build mingw libs"
+  rem 	goto DIE
+  rem )
+  
   ECHO Copying files...
   IF EXIST BUILD_WIN32 rmdir BUILD_WIN32 /S /Q
 
