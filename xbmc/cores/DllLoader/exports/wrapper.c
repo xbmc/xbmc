15f57b2031385d484e969ce7e5106f03f4ba1c81
Fneufneu <fneufneu@xbmc.org>
2011-02-04 00:23:58 +0100

    FreeBSD: reuse APPLE typedef in wrapper.c

--- xbmc/cores/DllLoader/exports/wrapper.c
+++ xbmc/cores/DllLoader/exports/wrapper.c
@@ -37,7 +37,7 @@
 #include <dirent.h>
 #endif
 
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__FreeBSD__)
 typedef int64_t   off64_t;
 typedef off_t     __off_t;
 typedef off64_t   __off64_t;
