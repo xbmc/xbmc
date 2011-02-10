9c1d0dfc1f258f62fe5bb6878b7f490c2b53e81b
vdrfan <vdrfan-nospam@xbmc.org>
2011-02-10 19:39:30 +0100

    changed: use recently added SORT_METHOD_PLAYCOUNT in TOP100 songs view

--- xbmc/music/GUIViewStateMusic.cpp
+++ xbmc/music/GUIViewStateMusic.cpp
@@ -327,8 +327,8 @@ CGUIViewStateMusicDatabase::CGUIViewStateMusicDatabase(const CFileItemList& item
     break;
   case NODE_TYPE_SONG_TOP100:
     {
-      AddSortMethod(SORT_METHOD_NONE, 554, LABEL_MASKS(strTrackLeft, strTrackRight));  // Userdefined, Userdefined | empty, empty
-      SetSortMethod(SORT_METHOD_NONE);
+      AddSortMethod(SORT_METHOD_NONE, 576, LABEL_MASKS("%T - %A", "%V"));
+      SetSortMethod(SORT_METHOD_PLAYCOUNT);
 
       SetViewAsControl(g_settings.m_viewStateMusicNavSongs.m_viewMode);
 
