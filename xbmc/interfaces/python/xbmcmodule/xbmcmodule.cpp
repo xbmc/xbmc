xbmc/xbmc

commit 07214fed10baf7dff89e12ccc781d3ece4106306

added: xbmcvfs.(copy,delete,rename) and xbmc.subHashAndFileSize
Thanks to jcarroll,davilla and jmarshall for a lot of the code here, Montellese for the VS2010 project fixup.

usage:
success = xbmcvfs.copy(source, destination)
xbmcvfs.delete(file)
success = xbmcvfs.rename(file,newFileName)

size, hash = xbmc.subHashAndFileSize(file)

xbmc/interfaces/python/xbmcmodule/xbmcmodule.cpp
@@ -63,6 +63,7 @@
 #include "SectionLoader.h"
 #include "settings/Settings.h"
 #include "guilib/LocalizeStrings.h"
+#include "FileUtils.h"
 
 // include for constants
 #include "pyutil.h"
@@ -957,6 +958,35 @@ namespace PYXBMC
 
     return Py_BuildValue((char*)"b", exists);
   }
+  
+  PyDoc_STRVAR(subHashAndFileSize__doc__,
+    "subHashAndFileSize(file)\n"
+    "\n"
+    "file        : file to calculate subtitle hash and size for"
+    "\n"
+    "example:\n"
+    " size,hash = xbmcvfs.subHashAndFileSize(file)\n"); 
+  PyObject* XBMC_subHashAndFileSize(PyObject *self, PyObject *args, PyObject *kwds)
+  {
+    PyObject *f_line;
+    if (!PyArg_ParseTuple(
+      args,
+      (char*)"O",
+      &f_line))
+    {
+      return NULL;
+    }
+    CStdString strSource;
+    if (!PyXBMCGetUnicodeString(strSource, f_line, 1)) return NULL;
+    
+    CStdString strSize;
+    CStdString strHash;
+    Py_BEGIN_ALLOW_THREADS
+    CFileUtils::SubtitleFileSizeAndHash(strSource, strSize, strHash);
+    Py_END_ALLOW_THREADS
+    
+    return Py_BuildValue((char*)"ss",strSize.c_str(), strHash.c_str());
+  } 
 
   // define c functions to be used in python here
   PyMethodDef xbmcMethods[] = {
@@ -1004,6 +1034,8 @@ namespace PYXBMC
     {(char*)"getCleanMovieTitle", (PyCFunction)XBMC_GetCleanMovieTitle, METH_VARARGS|METH_KEYWORDS, getCleanMovieTitle__doc__},
 
     {(char*)"skinHasImage", (PyCFunction)XBMC_SkinHasImage, METH_VARARGS|METH_KEYWORDS, skinHasImage__doc__},
+    {(char*)"subHashAndFileSize", (PyCFunction)XBMC_subHashAndFileSize, METH_VARARGS, subHashAndFileSize__doc__},
+
     {NULL, NULL, 0, NULL}
   };
 

xbmc/utils/FileUtils.h
@@ -6,4 +6,5 @@ class CFileUtils
 public:
   static bool DeleteItem(const CFileItemPtr &item, bool force=false);
   static bool RenameFile(const CStdString &strFile);
+  static bool CFileUtils::SubtitleFileSizeAndHash(const CStdString &path, CStdString &strSize, CStdString &strHash);
 };

XBMC.xcodeproj/project.pbxproj
@@ -38,6 +38,7 @@
 /* End PBXAggregateTarget section */
 
 /* Begin PBXBuildFile section */
+		183C454D130C4D55006AA317 /* xbmcvfsmodule.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 189047D11301DEAB00C11012 /* xbmcvfsmodule.cpp */; };
 		183FDF8A11AF0B0500B81E9C /* PluginSource.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 183FDF8811AF0B0500B81E9C /* PluginSource.cpp */; };
 		183FDF8B11AF0B0500B81E9C /* PluginSource.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 183FDF8811AF0B0500B81E9C /* PluginSource.cpp */; };
 		184C472F1296BC6E0006DB3E /* Service.cpp in Sources */ = {isa = PBXBuildFile; fileRef = 184C472D1296BC6E0006DB3E /* Service.cpp */; };
@@ -1913,6 +1914,7 @@
 		183FDF8911AF0B0500B81E9C /* PluginSource.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = PluginSource.h; sourceTree = "<group>"; };
 		184C472D1296BC6E0006DB3E /* Service.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = Service.cpp; sourceTree = "<group>"; };
 		184C472E1296BC6E0006DB3E /* Service.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = Service.h; sourceTree = "<group>"; };
+		189047D11301DEAB00C11012 /* xbmcvfsmodule.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = xbmcvfsmodule.cpp; sourceTree = "<group>"; };
 		18B49FF11152BFA5001AF8A6 /* Addon.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = Addon.cpp; sourceTree = "<group>"; };
 		18B49FF21152BFA5001AF8A6 /* Addon.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = Addon.h; sourceTree = "<group>"; };
 		18B49FF31152BFA5001AF8A6 /* AddonDll.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = AddonDll.h; sourceTree = "<group>"; };
@@ -5999,6 +6001,7 @@
 		E38E19DA0D25F9FB00618676 /* xbmcmodule */ = {
 			isa = PBXGroup;
 			children = (
+				189047D11301DEAB00C11012 /* xbmcvfsmodule.cpp */,
 				7CD2CCFE11B38B000009EFC1 /* PythonAddon.cpp */,
 				7CD2CCFF11B38B000009EFC1 /* PythonAddon.h */,
 				7CD2CD0011B38B000009EFC1 /* xbmcaddonmodule.cpp */,
@@ -8184,6 +8187,7 @@
 				433219D912E4C6A500CD7486 /* UDFDirectory.cpp in Sources */,
 				7C4705AE12EF584C00369E51 /* AddonInstaller.cpp in Sources */,
 				18C1D22D13033F6A00CFFE59 /* GLUtils.cpp in Sources */,
+				183C454D130C4D55006AA317 /* xbmcvfsmodule.cpp in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

project/VS2010Express/XBMC.vcxproj
@@ -491,6 +491,7 @@
     <ClCompile Include="..\..\xbmc\interfaces\python\xbmcmodule\xbmcguimodule.cpp" />
     <ClCompile Include="..\..\xbmc\interfaces\python\xbmcmodule\xbmcmodule.cpp" />
     <ClCompile Include="..\..\xbmc\interfaces\python\xbmcmodule\xbmcplugin.cpp" />
+    <ClCompile Include="..\..\xbmc\interfaces\python\xbmcmodule\xbmcvfsmodule.cpp" />
     <ClCompile Include="..\..\xbmc\interfaces\python\XBPython.cpp" />
     <ClCompile Include="..\..\xbmc\interfaces\python\XBPyThread.cpp" />
     <ClCompile Include="..\..\xbmc\LangInfo.cpp" />

project/VS2010Express/XBMC.vcxproj.filters
@@ -2465,6 +2465,9 @@
     <ClCompile Include="..\..\xbmc\win32\stat_utf8.cpp">
       <Filter>win32</Filter>
     </ClCompile>
+    <ClCompile Include="..\..\xbmc\interfaces\python\xbmcmodule\xbmcvfsmodule.cpp">
+      <Filter>interfaces\python\xbmcmodule</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="..\..\xbmc\win32\pch.h">

xbmc/interfaces/python/XBPython.cpp
@@ -120,6 +120,8 @@ extern "C" {
   void InitAddonModule(void);
   void InitAddonTypes(void);
   void DeinitAddonModule(void);
+  void InitVFSModule(void);
+  void DeinitVFSModule(void);
 }
 
 XBPython::XBPython()
@@ -298,7 +300,8 @@ void XBPython::InitializeInterpreter()
   InitPluginModule(); // init xbmcplugin modules
   InitGUIModule(); // init xbmcgui modules
   InitAddonModule(); // init xbmcaddon modules
-
+  InitVFSModule(); // init xbmcvfs modules
+  
   // redirecting default output to debug console
   if (PyRun_SimpleString(""
         "import xbmc\n"
@@ -326,6 +329,7 @@ void XBPython::DeInitializeInterpreter()
   DeinitPluginModule();
   DeinitGUIModule();
   DeinitAddonModule();
+  DeinitVFSModule();
 }
 
 /**

xbmc/interfaces/python/xbmcmodule/Makefile
@@ -33,6 +33,7 @@ SRCS=action.cpp \
      xbmcguimodule.cpp \
      xbmcmodule.cpp \
      xbmcplugin.cpp \
+     xbmcvfsmodule.cpp \
 
 LIB=xbmcmodule.a
 

xbmc/interfaces/python/xbmcmodule/xbmcvfsmodule.cpp
@@ -1,0 +1,205 @@
+/*
+ *      Copyright (C) 2005-2011 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "system.h"
+#if (defined USE_EXTERNAL_PYTHON)
+#if (defined HAVE_LIBPYTHON2_6)
+#include <python2.6/Python.h>
+#elif (defined HAVE_LIBPYTHON2_5)
+#include <python2.5/Python.h>
+#elif (defined HAVE_LIBPYTHON2_4)
+#include <python2.4/Python.h>
+#else
+#error "Could not determine version of Python to use."
+#endif
+#else
+#include "python/Include/Python.h"
+#endif
+#include "../XBPythonDll.h"
+
+#include "filesystem/File.h"
+#include "pyutil.h"
+
+using namespace std;
+using namespace XFILE;
+using namespace PYXBMC;
+
+#ifndef __GNUC__
+#pragma code_seg("PY_TEXT")
+#pragma data_seg("PY_DATA")
+#pragma bss_seg("PY_BSS")
+#pragma const_seg("PY_RDATA")
+#endif
+
+#if defined(__GNUG__) && (__GNUC__>4) || (__GNUC__==4 && __GNUC_MINOR__>=2)
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
+  namespace xbmcvfs
+  {
+    /*****************************************************************
+     * start of xbmcvfs methods
+     *****************************************************************/
+    typedef struct {
+      PyObject_HEAD
+      CFile* pFile;
+    } File;
+    
+    // copy() method
+    PyDoc_STRVAR(copy__doc__,
+      "copy(source, destination) -- copy file to destination, returns true/false.\n"
+      "\n"
+      "source          : file to copy.\n"
+      "destination     : destination file"
+      "\n"
+      "example:\n"
+      "  success = xbmcvfs.copy(source, destination)\n");
+    
+    PyObject* vfs_copy(PyObject *self, PyObject *args)
+    {
+      PyObject *f_line;
+      PyObject *d_line;
+      if (!PyArg_ParseTuple(
+        args,
+        (char*)"OO",
+        &f_line,
+        &d_line))
+      {
+        return NULL;
+      }
+      CStdString strSource;
+      CStdString strDestnation;
+      bool bResult = true;
+      
+      if (!PyXBMCGetUnicodeString(strSource, f_line, 1)) return NULL;
+      if (!PyXBMCGetUnicodeString(strDestnation, d_line, 1)) return NULL;
+      Py_BEGIN_ALLOW_THREADS
+      bResult = CFile::Cache(strSource, strDestnation);
+      Py_END_ALLOW_THREADS
+      
+      return Py_BuildValue((char*)"b", bResult);
+    }
+    PyDoc_STRVAR(delete__doc__,
+      "delete(file)\n"
+      "\n"
+      "file        : file to delete"
+      "\n"
+      "example:\n"
+      "  xbmcvfs.delete(file)\n");
+    
+    // delete a file
+    PyObject* vfs_delete(File *self, PyObject *args, PyObject *kwds)
+    {
+      PyObject *f_line;
+      if (!PyArg_ParseTuple(
+        args,
+        (char*)"O",
+        &f_line))
+      {
+        return NULL;
+      }
+      CStdString strSource;
+      if (!PyXBMCGetUnicodeString(strSource, f_line, 1)) return NULL;
+      
+      Py_BEGIN_ALLOW_THREADS
+      self->pFile->Delete(strSource);
+      Py_END_ALLOW_THREADS
+      
+      Py_INCREF(Py_None);
+      return Py_None;
+      
+    }
+    
+    PyDoc_STRVAR(rename__doc__,
+      "rename(file, newFileName)\n"
+      "\n"
+      "file        : file to reaname"
+      "newFileName : new filename, including the full path"
+      "\n"
+      "example:\n"
+      "  success = xbmcvfs.rename(file,newFileName)\n");
+    
+    // rename a file
+    PyObject* vfs_rename(File *self, PyObject *args, PyObject *kwds)
+    {
+      PyObject *f_line;
+      PyObject *d_line;
+      if (!PyArg_ParseTuple(
+        args,
+        (char*)"OO",
+        &f_line,
+        &d_line))
+      {
+        return NULL;
+      }
+      CStdString strSource;
+      CStdString strDestnation;
+      if (!PyXBMCGetUnicodeString(strSource, f_line, 1)) return NULL;
+      if (!PyXBMCGetUnicodeString(strDestnation, d_line, 1)) return NULL;
+      
+      bool bResult;
+      Py_BEGIN_ALLOW_THREADS
+      bResult = self->pFile->Rename(strSource,strDestnation);
+      Py_END_ALLOW_THREADS
+      
+      return Py_BuildValue((char*)"b", bResult);
+      
+    }  
+     
+    
+    // define c functions to be used in python here
+    PyMethodDef xbmcvfsMethods[] = {
+      {(char*)"copy", (PyCFunction)vfs_copy, METH_VARARGS, copy__doc__},
+      {(char*)"delete", (PyCFunction)vfs_delete, METH_VARARGS, delete__doc__},
+      {(char*)"rename", (PyCFunction)vfs_rename, METH_VARARGS, rename__doc__},
+      {NULL, NULL, 0, NULL}
+    };
+    
+    /*****************************************************************
+     * end of methods and python objects
+     * initxbmc(void);
+     *****************************************************************/
+    
+    
+    PyMODINIT_FUNC
+    DeinitVFSModule()
+    {
+      // no need to Py_DECREF our objects (see InitXBMCMVFSModule()) as they were created only
+      // so that they could be added to the module, which steals a reference.
+    }
+    
+    PyMODINIT_FUNC
+    InitVFSModule()
+    {
+      // init general xbmc modules
+      PyObject* pXbmcvfsModule;
+      pXbmcvfsModule = Py_InitModule((char*)"xbmcvfs", xbmcvfsMethods);
+      if (pXbmcvfsModule == NULL) return;
+    }    
+  }
+  
+#ifdef __cplusplus
+}
+#endif

commit a1471416116ee5204774ab75d2a16953dae11036

fixed: linux build.
It was broken by 07214fed10baf7dff89e12ccc781d3ece4106306

xbmc/interfaces/python/xbmcmodule/xbmcmodule.cpp
@@ -63,7 +63,7 @@
 #include "SectionLoader.h"
 #include "settings/Settings.h"
 #include "guilib/LocalizeStrings.h"
-#include "FileUtils.h"
+#include "utils/FileUtils.h"
 
 // include for constants
 #include "pyutil.h"

xbmc/utils/FileUtils.h
@@ -6,5 +6,5 @@ class CFileUtils
 public:
   static bool DeleteItem(const CFileItemPtr &item, bool force=false);
   static bool RenameFile(const CStdString &strFile);
-  static bool CFileUtils::SubtitleFileSizeAndHash(const CStdString &path, CStdString &strSize, CStdString &strHash);
+  static bool SubtitleFileSizeAndHash(const CStdString &path, CStdString &strSize, CStdString &strHash);
 };