ca68de7039e347075dee46e9175a6665a6ddaf9e
Anssi Hannula <anssi@xbmc.org>
2011-02-19 11:13:36 +0200

    added: print endianness of CoreAudio streams into debug log

--- xbmc/osx/CoreAudio.cpp
+++ xbmc/osx/CoreAudio.cpp
@@ -45,19 +45,23 @@ const char* StreamDescriptionToString(AudioStreamBasicDescription desc, CStdStri
   switch (desc.mFormatID)
   {
     case kAudioFormatLinearPCM:
-      str.Format("[%4.4s] %s%u Channel %u-bit %s (%uHz)", 
+      str.Format("[%4.4s] %s%u Channel %u-bit %s %s (%uHz)",
                  fourCC,
                  (desc.mFormatFlags & kAudioFormatFlagIsNonMixable) ? "" : "Mixable ",
                  desc.mChannelsPerFrame,
                  desc.mBitsPerChannel,
                  (desc.mFormatFlags & kAudioFormatFlagIsFloat) ? "Floating Point" : "Signed Integer",
+                 (desc.mFormatFlags & kAudioFormatFlagIsBigEndian) ? "BE" : "LE",
                  (UInt32)desc.mSampleRate);
       break;
     case kAudioFormatAC3:
       str.Format("[%4.4s] AC-3/DTS (%uHz)", fourCC, (UInt32)desc.mSampleRate);
       break;
     case kAudioFormat60958AC3:
-      str.Format("[%4.4s] AC-3/DTS for S/PDIF (%uHz)", fourCC, (UInt32)desc.mSampleRate);
+      str.Format("[%4.4s] AC-3/DTS for S/PDIF %s (%uHz)",
+                 fourCC,
+                 (desc.mFormatFlags & kAudioFormatFlagIsBigEndian) ? "BE" : "LE",
+                 (UInt32)desc.mSampleRate);
       break;
     default:
       str.Format("[%4.4s]", fourCC);
