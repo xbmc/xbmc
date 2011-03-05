1337ec62ef04304cbd9313f7d9f68c0912e5541a
Lars Op den Kamp <lars@opdenkamp.eu>
2011-03-05 17:28:21 +0100

    libhts: sync with upstream

--- lib/libhts/htsmsg_xml.h
+++ lib/libhts/htsmsg_xml.h
@@ -1,6 +1,6 @@
 /*
- *  Functions converting HTSMSGs to/from a simple binary format
- *  Copyright (C) 2007 Andreas �man
+ *  Functions converting HTSMSGs to/from XML
+ *  Copyright (C) 2008 Andreas �man
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,18 +16,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef HTSMSG_BINARY_H_
-#define HTSMSG_BINARY_H_
+#ifndef HTSMSG_XML_H_
+#define HTSMSG_XML_H_
 
 #include "htsmsg.h"
+#include "htsbuf.h"
 
-/**
- * htsmsg_binary_deserialize
- */
-htsmsg_t *htsmsg_binary_deserialize(const void *data, size_t len,
-                                    const void *buf);
+htsmsg_t *htsmsg_xml_deserialize(char *src, char *errbuf, size_t errbufsize);
 
-int htsmsg_binary_serialize(htsmsg_t *msg, void **datap, size_t *lenp,
-                            int maxlen);
+int put_utf8(char *out, int c);
 
-#endif /* HTSMSG_BINARY_H_ */
+#endif /* HTSMSG_XML_H_ */
