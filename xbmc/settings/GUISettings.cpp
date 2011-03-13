1b1de8cdf41a1def2a7994dca27bbc5890849ccf
S. Davilla <davilla@4pi.com>
2011-03-13 00:17:03 -0500

    fixed, hide mouse settings for ios/atv2... not the other way around

--- xbmc/settings/GUISettings.cpp
+++ xbmc/settings/GUISettings.cpp
@@ -487,7 +487,7 @@ void CGUISettings::Initialize()
   AddSeparator(in, "input.sep1");
 #endif
   AddBool(in, "input.remoteaskeyboard", 21449, false);
-#if !(defined(__APPLE__) && defined(__arm_))
+#if (defined(__APPLE__) && defined(__arm_))
   AddBool(NULL, "input.enablemouse", 21369, true);
 #else
   AddBool(in, "input.enablemouse", 21369, true);
