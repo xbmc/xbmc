e22f5124b8e700273798d21a9a3351e2df804a80
Lars Op den Kamp <lars@opdenkamp.eu>
2011-03-17 00:30:24 +0100

    epg: keep a table locked when updating it. fixes possible crash when accessing the epg table that's currently being updated

--- xbmc/epg/Epg.cpp
+++ xbmc/epg/Epg.cpp
@@ -369,8 +369,6 @@ bool CEpg::Update(time_t start, time_t end, int iUpdateTime, bool bStoreInDb /* 
   m_lastScanTime.GetAsTime(iLastUpdate);
   bUpdate = (iNow > iLastUpdate + iUpdateTime);
 
-  lock.Leave();
-
   if (bUpdate)
   {
     m_bInhibitSorting = true;
@@ -381,7 +379,6 @@ bool CEpg::Update(time_t start, time_t end, int iUpdateTime, bool bStoreInDb /* 
     if (bGrabSuccess && size() > 0)
     {
       Sort();
-      lock.Enter();
       FixOverlappingEvents(false);
 
       if (bStoreInDb)
@@ -392,7 +389,6 @@ bool CEpg::Update(time_t start, time_t end, int iUpdateTime, bool bStoreInDb /* 
         database->CommitInsertQueries();
       }
 
-      lock.Leave();
       m_lastScanTime = CDateTime::GetCurrentDateTime();
     }
     else
