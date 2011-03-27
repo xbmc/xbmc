ba26841fca102a6066c129a910977b440b6b2795
Joakim <elupus@ecce.se>
2011-03-27 14:11:11 +0200

    fixed: dvdplayer could end up stalled on a stream change waiting for players to start

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -2570,6 +2570,9 @@ bool CDVDPlayer::OpenAudioStream(int iStream, int source)
   m_CurrentAudio.stream = (void*)pStream;
   m_CurrentAudio.started = false;
 
+  /* we are potentially going to be waiting on this */
+  m_dvdPlayerAudio.SendMessage(new CDVDMsg(CDVDMsg::PLAYER_STARTED), 1);
+
   /* audio normally won't consume full cpu, so let it have prio */
   m_dvdPlayerAudio.SetPriority(GetThreadPriority(*this)+1);
 
@@ -2621,6 +2624,9 @@ bool CDVDPlayer::OpenVideoStream(int iStream, int source)
   m_CurrentVideo.hint = hint;
   m_CurrentVideo.stream = (void*)pStream;
   m_CurrentVideo.started = false;
+
+  /* we are potentially going to be waiting on this */
+  m_dvdPlayerVideo.SendMessage(new CDVDMsg(CDVDMsg::PLAYER_STARTED), 1);
 
 #if defined(__APPLE__)
   // Apple thread scheduler works a little different than Linux. It
