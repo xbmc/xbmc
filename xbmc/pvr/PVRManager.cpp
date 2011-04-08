xbmc/xbmc

commit c44b3a437d7dc2d9706b6ba8c383aec6e8f83470

pvr: use a mutex for changes to selected channel groups

xbmc/pvr/PVRManager.cpp
@@ -269,6 +269,7 @@ void CPVRManager::Process(void)
 bool CPVRManager::ChannelSwitch(unsigned int iChannel)
 {
   const CPVRChannel *channel = NULL;
+  CSingleLock lock(m_critSection);
 
   if (m_addons->IsPlayingRadio())
   {
@@ -704,6 +705,8 @@ void CPVRManager::LoadCurrentChannelSettings()
 
 void CPVRManager::SetPlayingGroup(CPVRChannelGroup *group)
 {
+  CSingleLock lock(m_critSection);
+
   if (group && group->IsRadio())
     m_currentRadioGroup = group;
   else if (group && !group->IsRadio())
@@ -712,6 +715,8 @@ void CPVRManager::SetPlayingGroup(CPVRChannelGroup *group)
 
 const CPVRChannelGroup *CPVRManager::GetPlayingGroup(bool bRadio /* = false */)
 {
+  CSingleLock lock(m_critSection);
+
   if (bRadio && !m_currentRadioGroup)
     m_currentRadioGroup = (CPVRChannelGroup *) GetChannelGroups()->GetGroupAllRadio();
   else if (!bRadio &&!m_currentTVGroup)
@@ -763,7 +768,7 @@ void CPVRManager::TriggerChannelGroupsUpdate(void)
 bool CPVRManager::OpenLiveStream(const CPVRChannel &tag)
 {
   bool bReturn = false;
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
 
   CLog::Log(LOGDEBUG,"PVRManager - %s - opening live stream on channel '%s'",
       __FUNCTION__, tag.ChannelName().c_str());
@@ -785,7 +790,7 @@ bool CPVRManager::OpenLiveStream(const CPVRChannel &tag)
 bool CPVRManager::OpenRecordedStream(const CPVRRecording &tag)
 {
   bool bReturn = false;
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
 
   CLog::Log(LOGDEBUG,"PVRManager - %s - opening recorded stream '%s'",
       __FUNCTION__, tag.m_strFile.c_str());
@@ -801,7 +806,7 @@ bool CPVRManager::OpenRecordedStream(const CPVRRecording &tag)
 
 void CPVRManager::CloseStream(void)
 {
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
 
   if (m_addons->IsReadingLiveStream())
   {
@@ -828,7 +833,7 @@ void CPVRManager::CloseStream(void)
 
 void CPVRManager::UpdateCurrentFile(void)
 {
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
   if (m_currentFile)
     UpdateItem(*m_currentFile);
 }
@@ -845,7 +850,7 @@ bool CPVRManager::UpdateItem(CFileItem& item)
     return false;
   }
 
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
   g_application.CurrentFileItem() = *m_currentFile;
   g_infoManager.SetCurrentItem(*m_currentFile);
 
@@ -917,7 +922,7 @@ bool CPVRManager::StartPlayback(const CPVRChannel *channel, bool bPreview /* = f
 
 bool CPVRManager::PerformChannelSwitch(const CPVRChannel &channel, bool bPreview)
 {
-  CSingleLock lock(m_critSectionStreams);
+  CSingleLock lock(m_critSection);
 
   CLog::Log(LOGDEBUG, "PVRManager - %s - switching to channel '%s'",
       __FUNCTION__, channel.ChannelName().c_str());

xbmc/pvr/PVRManager.h
@@ -428,7 +428,7 @@ private:
   //@{
   CFileItem *                     m_currentFile;
   CPVRDatabase                    m_database;                    /*!< the database for all PVR related data */
-  CCriticalSection                m_critSectionStreams;          /*!< critical section for all changes to this class */
+  CCriticalSection                m_critSection;                 /*!< critical section for all changes to this class */
   bool                            m_bFirstStart;                 /*!< true when the PVR manager was started first, false otherwise */
   bool                            m_bLoaded;
   //@}

commit 651ba9a93b4a80b6edb668b2591346e2bb785b97

pvr: cache channel numbers CPVRChannel. speeds up channel number queries and fixes deadlock when starting playback introduced in c44b3a437d7dc2d9706b6ba8c383aec6e8f83470

xbmc/pvr/PVRManager.cpp
@@ -271,24 +271,14 @@ bool CPVRManager::ChannelSwitch(unsigned int iChannel)
   const CPVRChannel *channel = NULL;
   CSingleLock lock(m_critSection);
 
-  if (m_addons->IsPlayingRadio())
+  const CPVRChannelGroup *playingGroup = GetPlayingGroup(m_addons->IsPlayingRadio());
+  if (playingGroup == NULL)
   {
-    if (m_currentRadioGroup != NULL)
-      channel = m_currentRadioGroup->GetByChannelNumber(iChannel);
-
-    if (channel == NULL)
-      channel = GetChannelGroups()->GetGroupAllRadio()->GetByChannelNumber(iChannel);
-  }
-  else
-  {
-    /* always use the TV group if we're not playing a radio channel */
-    if (m_currentTVGroup != NULL)
-      channel = m_currentTVGroup->GetByChannelNumber(iChannel);
-
-    if (channel == NULL)
-      channel = GetChannelGroups()->GetGroupAllTV()->GetByChannelNumber(iChannel);
+    CLog::Log(LOGERROR, "PVRManager - %s - cannot a selected group", __FUNCTION__);
+    return false;
   }
 
+  channel = playingGroup->GetByChannelNumber(iChannel);
   if (channel == NULL)
   {
     CLog::Log(LOGERROR, "PVRManager - %s - cannot find channel %d", __FUNCTION__, iChannel);
@@ -707,10 +697,24 @@ void CPVRManager::SetPlayingGroup(CPVRChannelGroup *group)
 {
   CSingleLock lock(m_critSection);
 
-  if (group && group->IsRadio())
+  if (group == NULL)
+    return;
+
+  bool bChanged(false);
+  if (group->IsRadio())
+  {
+    bChanged = m_currentRadioGroup == NULL || *m_currentRadioGroup != *group;
     m_currentRadioGroup = group;
-  else if (group && !group->IsRadio())
+  }
+  else
+  {
+    bChanged = m_currentTVGroup == NULL || *m_currentTVGroup != *group;
     m_currentTVGroup = group;
+  }
+
+  /* set this group as selected group and set channel numbers */
+  if (bChanged)
+    group->SetSelectedGroup();
 }
 
 const CPVRChannelGroup *CPVRManager::GetPlayingGroup(bool bRadio /* = false */)
@@ -718,9 +722,9 @@ const CPVRChannelGroup *CPVRManager::GetPlayingGroup(bool bRadio /* = false */)
   CSingleLock lock(m_critSection);
 
   if (bRadio && !m_currentRadioGroup)
-    m_currentRadioGroup = (CPVRChannelGroup *) GetChannelGroups()->GetGroupAllRadio();
+    SetPlayingGroup((CPVRChannelGroup *) GetChannelGroups()->GetGroupAllRadio());
   else if (!bRadio &&!m_currentTVGroup)
-    m_currentTVGroup = (CPVRChannelGroup *) GetChannelGroups()->GetGroupAllTV();
+    SetPlayingGroup((CPVRChannelGroup *) GetChannelGroups()->GetGroupAllTV());
 
   return bRadio ? m_currentRadioGroup : m_currentTVGroup;
 }

xbmc/pvr/channels/PVRChannel.cpp
@@ -71,6 +71,7 @@ CPVRChannel::CPVRChannel(bool bRadio /* = false */)
   m_bIsVirtual              = false;
   m_iLastWatched            = 0;
   m_bChanged                = false;
+  m_iCachedChannelNumber    = 0;
 
   m_EPG                     = NULL;
   m_bEPGEnabled             = true;
@@ -245,12 +246,7 @@ bool CPVRChannel::SetChannelID(int iChannelId, bool bSaveInDb /* = false */)
 
 int CPVRChannel::ChannelNumber(void) const
 {
-  int iReturn = -1;
-  const CPVRChannelGroup *group = CPVRManager::Get()->GetPlayingGroup(m_bIsRadio);
-  if (group)
-    iReturn = group->GetChannelNumber(*this);
-
-  return iReturn;
+  return m_iCachedChannelNumber;
 }
 
 bool CPVRChannel::SetHidden(bool bIsHidden, bool bSaveInDb /* = false */)
@@ -761,4 +757,9 @@ bool CPVRChannel::SetEPGScraper(const CStdString &strScraper, bool bSaveInDb /* 
   }
 
   return bReturn;
+}
+
+void CPVRChannel::SetCachedChannelNumber(unsigned int iChannelNumber)
+{
+  m_iCachedChannelNumber = iChannelNumber;
 }

xbmc/pvr/channels/PVRChannel.h
@@ -28,6 +28,7 @@
 
 class CPVREpgInfoTag;
 class CPVREpg;
+class CPVRChannelGroup;
 class CPVRChannelGroupInternal;
 
 /** PVR Channel class */
@@ -35,6 +36,7 @@ class CPVRChannelGroupInternal;
 class CPVRChannel : public Observable
 {
   friend class CPVREpgContainer;
+  friend class CPVRChannelGroup;
   friend class CPVRChannelGroupInternal;
   friend class CPVRDatabase;
 
@@ -42,15 +44,16 @@ private:
   /*! @name XBMC related channel data
    */
   //@{
-  int        m_iChannelId;              /*!< the identifier given to this channel by the TV database */
-  bool       m_bIsRadio;                /*!< true if this channel is a radio channel, false if not */
-  bool       m_bIsHidden;               /*!< true if this channel is hidden, false if not */
-  bool       m_bClientIsRecording;      /*!< true if a recording is currently running on this channel, false if not */
-  CStdString m_strIconPath;             /*!< the path to the icon for this channel */
-  CStdString m_strChannelName;          /*!< the name for this channel used by XBMC */
-  bool       m_bIsVirtual;              /*!< true if this channel is marked as virtual, false if not */
-  time_t     m_iLastWatched;            /*!< last time channel has been watched */
-  bool       m_bChanged;                /*!< true if anything in this entry was changed that needs to be persisted */
+  int          m_iChannelId;              /*!< the identifier given to this channel by the TV database */
+  bool         m_bIsRadio;                /*!< true if this channel is a radio channel, false if not */
+  bool         m_bIsHidden;               /*!< true if this channel is hidden, false if not */
+  bool         m_bClientIsRecording;      /*!< true if a recording is currently running on this channel, false if not */
+  CStdString   m_strIconPath;             /*!< the path to the icon for this channel */
+  CStdString   m_strChannelName;          /*!< the name for this channel used by XBMC */
+  bool         m_bIsVirtual;              /*!< true if this channel is marked as virtual, false if not */
+  time_t       m_iLastWatched;            /*!< last time channel has been watched */
+  bool         m_bChanged;                /*!< true if anything in this entry was changed that needs to be persisted */
+  unsigned int m_iCachedChannelNumber;    /*!< the cached channel number in the selected group */
   //@}
 
   /*! @name EPG related channel data
@@ -359,6 +362,9 @@ private:
    */
   void UpdateEncryptionName(void);
 
+  void SetCachedChannelNumber(unsigned int iChannelNumber);
+  bool CacheIcon(void);
+
 public:
   /*!
    * @brief Return true if this channel is encrypted.
@@ -483,6 +489,4 @@ public:
   bool SetEPGScraper(const CStdString &strScraper, bool bSaveInDb = false);
 
   //@}
-
-  bool CacheIcon(void);
 };

xbmc/pvr/channels/PVRChannelGroup.cpp
@@ -724,8 +724,29 @@ bool CPVRChannelGroup::HasChanges(void) const
 
 void CPVRChannelGroup::CacheIcons(void)
 {
+  CSingleLock lock(m_critSection);
   for (unsigned int iChannelPtr = 0; iChannelPtr < size(); iChannelPtr++)
   {
     at(iChannelPtr).channel->CacheIcon();
   }
+}
+
+void CPVRChannelGroup::ResetChannelNumbers(void)
+{
+  CSingleLock lock(m_critSection);
+  for (unsigned int iChannelPtr = 0; iChannelPtr < size(); iChannelPtr++)
+    at(iChannelPtr).channel->SetCachedChannelNumber(0);
+}
+
+void CPVRChannelGroup::SetSelectedGroup(void)
+{
+  CSingleLock lock(m_critSection);
+
+  /* reset all channel numbers */
+  ((CPVRChannelGroup *) CPVRManager::GetChannelGroups()->GetGroupAll(m_bRadio))->ResetChannelNumbers();
+
+  /* set all channel numbers on members of this group */
+  unsigned int iChannelNumber(1);
+  for (unsigned int iChannelPtr = 0; iChannelPtr < size(); iChannelPtr++)
+    at(iChannelPtr).channel->SetCachedChannelNumber(iChannelNumber++);
 }

xbmc/pvr/channels/PVRChannelGroup.h
@@ -267,6 +267,9 @@ public:
 
   //@}
 
+  virtual void SetSelectedGroup(void);
+  virtual void ResetChannelNumbers(void);
+
   /*! @name getters
    */
   //@{