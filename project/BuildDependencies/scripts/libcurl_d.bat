c81a2724206d91b31cf1faf955b149a477911312
WiSo <wiso@xbmc.org>
2011-04-09 22:50:44 +0200

    [WIN32] download zlib from our mirror. use it instead the one from libcurl

--- project/BuildDependencies/scripts/libcurl_d.bat
+++ project/BuildDependencies/scripts/libcurl_d.bat
@@ -7,6 +7,8 @@ CALL dlextract.bat libcurl %FILES%
 
 cd %TMP_PATH%
 
+del curl-7.21.1-devel-mingw32\bin\zlib1.dll
+
 xcopy curl-7.21.1-devel-mingw32\include\curl "%CUR_PATH%\include\curl" /E /Q /I /Y
 copy curl-7.21.1-devel-mingw32\bin\*.dll "%XBMC_PATH%\system\" /Y
 
