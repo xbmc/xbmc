0a1d5bf4dcdd390f727136eed234f75d650d4433
Lars Op den Kamp <lars@opdenkamp.eu>
2011-04-22 19:16:59 +0200

    tvheadend addon: clean up SubscriptionStart()

--- xbmc/pvrclients/tvheadend/HTSPDemux.h
+++ xbmc/pvrclients/tvheadend/HTSPDemux.h
@@ -46,8 +46,6 @@ protected:
   DemuxPacket *ParseMuxPacket(htsmsg_t *m);
 
 private:
-  void SetLanguageInfo(const char *strLanguage, char *strDestination);
-
   unsigned              m_subs;
   cHTSPSession          m_session;
   int                   m_channel;
