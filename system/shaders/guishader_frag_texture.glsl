852cc30304ea2ce71ff597e44868e13954e694ae
S. Davilla <davilla@4pi.com>
2011-05-02 19:44:23 -0400

    [all] changed m_colour from mediump to lowp, improves gui fps on resource limited platforms and does not effect mainline platforms

--- system/shaders/guishader_frag_texture.glsl
+++ system/shaders/guishader_frag_texture.glsl
@@ -22,7 +22,7 @@
 precision mediump   float;
 uniform   sampler2D m_samp0;
 varying   vec4      m_cord0;
-varying   vec4      m_colour;
+varying   lowp vec4      m_colour;
 
 // SM_TEXTURE shader
 void main ()
