6af53ba87b6b045b5151812a217b31c7a41e7dc4
S. Davilla <davilla@4pi.com>
2011-05-04 16:09:52 -0400

    change all usage of m_colour from mediump to lowp in shaders, yields a slight fps increase

--- system/shaders/guishader_frag_rgba.glsl
+++ system/shaders/guishader_frag_rgba.glsl
@@ -24,7 +24,7 @@ uniform sampler2D m_samp0;
 uniform sampler2D m_samp1;
 varying vec4      m_cord0;
 varying vec4      m_cord1;
-varying vec4      m_colour;
+varying lowp vec4 m_colour;
 uniform int       m_method;
 
 // SM_TEXTURE_NOBLEND
