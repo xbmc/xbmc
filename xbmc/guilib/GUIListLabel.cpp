a57b8fac3de33868afe34c64c27a655eea44860e
Tobias Arrskog <topfs2@xbmc.org>
2011-05-08 22:43:06 +0200

    Have GUIListLabel mark dirty regions

--- xbmc/guilib/GUIListLabel.cpp
+++ xbmc/guilib/GUIListLabel.cpp
@@ -75,7 +75,9 @@ bool CGUIListLabel::UpdateColors()
 
 void CGUIListLabel::Process(unsigned int currentTime)
 {
-  m_label.Process(currentTime);
+  if (m_label.Process(currentTime))
+    MarkDirtyRegion();
+
   CGUIControl::Process(currentTime);
 }
 
