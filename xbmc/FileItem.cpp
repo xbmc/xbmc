95ddabb6d1a67fc7fd521f39e8599b478f5a773a
Lars Op den Kamp <lars@opdenkamp.eu>
2011-05-12 13:23:58 +0200

    pvr: remove unneeded SetDefaultIcon()

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -159,7 +159,6 @@ CFileItem::CFileItem(const CPVREpgInfoTag& tag)
   SetLabel(tag.Title());
   m_strLabel2 = tag.Plot();
 
-  FillInDefaultIcon();
   if (!tag.Icon().IsEmpty())
   {
     SetThumbnailImage(tag.Icon());
@@ -185,7 +184,6 @@ CFileItem::CFileItem(const CEpgInfoTag& tag)
   SetLabel(tag.Title());
   m_strLabel2 = tag.Plot();
 
-  FillInDefaultIcon();
   if (!tag.Icon().IsEmpty())
   {
     SetThumbnailImage(tag.Icon());
@@ -229,7 +227,6 @@ CFileItem::CFileItem(const CPVRChannel& channel)
     }
   }
 
-  FillInDefaultIcon();
   if (!channel.IconPath().IsEmpty())
   {
     SetThumbnailImage(channel.IconPath());
@@ -254,8 +251,6 @@ CFileItem::CFileItem(const CPVRRecording& record)
   *GetPVRRecordingInfoTag() = record;
   SetLabel(record.m_strTitle);
   m_strLabel2 = record.m_strPlot;
-
-  FillInDefaultIcon();
 }
 
 CFileItem::CFileItem(const CPVRTimerInfoTag& timer)
@@ -275,8 +270,6 @@ CFileItem::CFileItem(const CPVRTimerInfoTag& timer)
   *GetPVRTimerInfoTag() = timer;
   SetLabel(timer.m_strTitle);
   m_strLabel2 = timer.m_strSummary;
-
-  FillInDefaultIcon();
 }
 
 CFileItem::CFileItem(const CArtist& artist)
