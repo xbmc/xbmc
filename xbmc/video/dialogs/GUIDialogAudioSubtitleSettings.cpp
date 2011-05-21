xbmc/xbmc

commit 0f862bbcf17751bf47e5f99f3c299901a10c82dc

pvr: save video settings right after they have been changed by user, fixes overwrite of channel settings; use video db for recordings

xbmc/video/dialogs/GUIDialogVideoSettings.cpp
@@ -33,8 +33,10 @@
 #include "dialogs/GUIDialogYesNo.h"
 #include "settings/Settings.h"
 #include "addons/Skin.h"
+#include "pvr/PVRManager.h"
 
 using namespace std;
+using namespace PVR;
 
 CGUIDialogVideoSettings::CGUIDialogVideoSettings(void)
     : CGUIDialogSettings(WINDOW_DIALOG_VIDEO_OSD_SETTINGS, "VideoOSDSettings.xml")
@@ -213,6 +215,8 @@ void CGUIDialogVideoSettings::OnSettingChanged(SettingInfo &setting)
       g_settings.Save();
     }
   }
+
+  g_PVRManager.TriggerSaveChannelSettings();
 }
 
 CStdString CGUIDialogVideoSettings::FormatInteger(float value, float minimum)

xbmc/Application.cpp
@@ -3623,10 +3623,7 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
 
     OutputDebugString("new file set audiostream:0\n");
     // Switch to default options
-    // PVR channel settings are stored later
-    // default settings would overwrite changed settings for pvr channels
-    if (!item.IsPVR())
-      g_settings.m_currentVideoSettings = g_settings.m_defaultVideoSettings;
+    g_settings.m_currentVideoSettings = g_settings.m_defaultVideoSettings;
     // see if we have saved options in the database
 
     m_iPlaySpeed = 1;
@@ -4158,12 +4155,15 @@ bool CApplication::IsPlayingFullScreenVideo() const
 
 void CApplication::SaveFileState()
 {
-  if (!g_settings.GetCurrentProfile().canWriteDatabases())
-    return;
-  CJob* job = new CSaveFileStateJob(*m_progressTrackingItem,
-      m_progressTrackingVideoResumeBookmark,
-      m_progressTrackingPlayCountUpdate);
-  CJobManager::GetInstance().AddJob(job, NULL);
+  if (!m_progressTrackingItem->IsPVRChannel())
+  {
+    if (!g_settings.GetCurrentProfile().canWriteDatabases())
+      return;
+    CJob* job = new CSaveFileStateJob(*m_progressTrackingItem,
+        m_progressTrackingVideoResumeBookmark,
+        m_progressTrackingPlayCountUpdate);
+    CJobManager::GetInstance().AddJob(job, NULL);
+  }
 }
 
 void CApplication::UpdateFileState()
@@ -5426,7 +5426,7 @@ bool CApplication::ProcessAndStartPlaylist(const CStdString& strPlayList, CPlayL
 void CApplication::SaveCurrentFileSettings()
 {
   // don't store settings for PVR in video database
-  if (m_itemCurrentFile->IsVideo() && !m_itemCurrentFile->IsPVR())
+  if (m_itemCurrentFile->IsVideo() && !m_itemCurrentFile->IsPVRChannel())
   {
     // save video settings
     if (g_settings.m_currentVideoSettings != g_settings.m_defaultVideoSettings)

xbmc/pvr/PVRManager.cpp
@@ -553,6 +553,8 @@ bool CPVRManager::StartRecordingOnPlayingChannel(bool bOnOff)
 
 void CPVRManager::SaveCurrentChannelSettings(void)
 {
+  CSingleLock lock(m_critSection);
+
   CPVRChannel channel;
   if (!m_addons->GetPlayingChannel(&channel))
     return;
@@ -714,6 +716,12 @@ bool CPVRChannelGroupsUpdateJob::DoWork(void)
   return g_PVRChannelGroups->Update(false);
 }
 
+bool CPVRChannelSettingsSaveJob::DoWork(void)
+{
+  g_PVRManager.SaveCurrentChannelSettings();
+  return true;
+}
+
 bool CPVRManager::OpenLiveStream(const CPVRChannel &tag)
 {
   bool bReturn = false;
@@ -766,9 +774,6 @@ void CPVRManager::CloseStream(void)
       time_t tNow;
       CDateTime::GetCurrentDateTime().GetAsTime(tNow);
       channel.SetLastWatched(tNow, true);
-
-      /* Store current settings inside Database */
-      SaveCurrentChannelSettings();
     }
   }
 
@@ -879,7 +884,6 @@ bool CPVRManager::PerformChannelSwitch(const CPVRChannel &channel, bool bPreview
   CLog::Log(LOGDEBUG, "PVRManager - %s - switching to channel '%s'",
       __FUNCTION__, channel.ChannelName().c_str());
 
-  SaveCurrentChannelSettings();
   if (m_currentFile)
   {
     delete m_currentFile;
@@ -1063,6 +1067,21 @@ void CPVRManager::TriggerChannelGroupsUpdate(void)
     return;
 
   m_pendingUpdates.push_back(new CPVRChannelGroupsUpdateJob());
+
+  lock.Leave();
+  SetEvent(m_triggerEvent);
+}
+
+void CPVRManager::TriggerSaveChannelSettings(void)
+{
+  CSingleLock lock(m_critSectionTriggers);
+  if (!m_bLoaded)
+    return;
+
+  if (IsJobPending("pvr-save-channelsettings"))
+    return;
+
+  m_pendingUpdates.push_back(new CPVRChannelSettingsSaveJob());
 
   lock.Leave();
   SetEvent(m_triggerEvent);

xbmc/pvr/PVRManager.h
@@ -284,6 +284,11 @@ namespace PVR
     void TriggerChannelGroupsUpdate(void);
 
     /*!
+     * @brief Let the background thread save the current video settings.
+     */
+    void TriggerSaveChannelSettings(void);
+
+    /*!
      * @brief Update the channel that is currently active.
      * @param item The new channel.
      * @return True if it was updated correctly, false otherwise.
@@ -391,6 +396,11 @@ namespace PVR
      */
     bool IsSelectedGroup(const CPVRChannelGroup &group) const;
 
+    /*!
+     * @brief Persist the current channel settings in the database.
+     */
+    void SaveCurrentChannelSettings(void);
+
   protected:
     /*!
      * @brief PVR update and control thread.
@@ -482,11 +492,6 @@ namespace PVR
     bool StartUpdateThreads(void);
 
     /*!
-     * @brief Persist the current channel settings in the database.
-     */
-    void SaveCurrentChannelSettings(void);
-
-    /*!
      * @brief Load the settings for the current channel from the database.
      */
     void LoadCurrentChannelSettings(void);
@@ -572,6 +577,16 @@ namespace PVR
     CPVRChannelGroupsUpdateJob(void) {}
     virtual ~CPVRChannelGroupsUpdateJob() {}
     virtual const char *GetType() const { return "pvr-update-channelgroups"; }
+
+    virtual bool DoWork();
+  };
+
+  class CPVRChannelSettingsSaveJob : public CJob
+  {
+  public:
+    CPVRChannelSettingsSaveJob(void) {}
+    virtual ~CPVRChannelSettingsSaveJob() {}
+    virtual const char *GetType() const { return "pvr-save-channelsettings"; }
 
     virtual bool DoWork();
   };

xbmc/utils/SaveFileStateJob.h
@@ -30,8 +30,7 @@ bool CSaveFileStateJob::DoWork()
 
   if (progressTrackingFile != "")
   {
-	// do not store PVR settings in video database
-    if (m_item.IsVideo() && !m_item.IsPVR())
+    if (m_item.IsVideo())
     {
       CLog::Log(LOGDEBUG, "%s - Saving file state for video item %s", __FUNCTION__, progressTrackingFile.c_str());
 

commit 0bb27099fd2d26c67447ce0ac18fa98b511438f9

pvr: persist settings when audio settings changed. only save pvr channel settings when a pvr channel is being played. fixes issues caused by 0f862bbcf17751bf47e5f99f3c299901a10c82dc. closes #110

xbmc/video/dialogs/GUIDialogAudioSubtitleSettings.cpp
@@ -37,9 +37,11 @@
 #include "settings/Settings.h"
 #include "settings/GUISettings.h"
 #include "guilib/LocalizeStrings.h"
+#include "pvr/PVRManager.h"
 
 using namespace std;
 using namespace XFILE;
+using namespace PVR;
 
 #ifdef HAS_VIDEO_PLAYBACK
 extern void xbox_audio_switch_channel(int iAudioStream, bool bAudioOnAllSpeakers); //lowlevel audio
@@ -341,6 +343,9 @@ void CGUIDialogAudioSubtitleSettings::OnSettingChanged(SettingInfo &setting)
       g_settings.Save();
     }
   }
+
+  if (g_PVRManager.IsPlayingRadio() || g_PVRManager.IsPlayingTV())
+    g_PVRManager.TriggerSaveChannelSettings();
 }
 
 void CGUIDialogAudioSubtitleSettings::FrameMove()

xbmc/video/dialogs/GUIDialogVideoSettings.cpp
@@ -216,7 +216,8 @@ void CGUIDialogVideoSettings::OnSettingChanged(SettingInfo &setting)
     }
   }
 
-  g_PVRManager.TriggerSaveChannelSettings();
+  if (g_PVRManager.IsPlayingRadio() || g_PVRManager.IsPlayingTV())
+    g_PVRManager.TriggerSaveChannelSettings();
 }
 
 CStdString CGUIDialogVideoSettings::FormatInteger(float value, float minimum)