f318fdf4ce93dd1d6742528d9d9acf5ac26bda92
Lars Op den Kamp <lars@opdenkamp.eu>
2011-05-21 14:40:54 +0200

    pvr: add missing channelsettings in CPVRDatabase

--- xbmc/pvr/PVRDatabase.h
+++ xbmc/pvr/PVRDatabase.h
@@ -57,7 +57,7 @@ namespace PVR
      * @brief Get the minimal database version that is required to operate correctly.
      * @return The minimal database version.
      */
-    virtual int GetMinVersion() const { return 14; };
+    virtual int GetMinVersion() const { return 15; };
 
     /*!
      * @brief Get the default sqlite database filename.
