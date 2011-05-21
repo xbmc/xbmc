9aa6c4edd610052f40ff0ea977b1c2fd678539e2
WiSo <wiso@xbmc.org>
2011-05-21 20:27:17 +0200

    [WIN32] create debug project directory to copy dlls in

--- project/BuildDependencies/scripts/1_copy_deps_d.bat
+++ project/BuildDependencies/scripts/1_copy_deps_d.bat
@@ -6,3 +6,4 @@ xcopy "%XBMC_PATH%\lib\win32\libmicrohttpd_win32\bin\*.dll" "%XBMC_PATH%\system\
 
 rem create directories
 IF NOT EXIST "%XBMC_PATH%\system\players\paplayer" md "%XBMC_PATH%\system\players\paplayer"
+IF NOT EXIST "%XBMC_PATH%\project\VS2010Express\XBMC\Debug (DirectX)" md "%XBMC_PATH%\project\VS2010Express\XBMC\Debug (DirectX)"
