xbmc/xbmc

commit 025772d6e6fbcb6a2cc26dd1af29c0c5f2f477d7

pvr: fix some possible crashes when the pvrmanager isn't started and settings are changed

xbmc/pvr/PVRManager.cpp
@@ -420,7 +420,9 @@ void CPVRManager::ResetDatabase(bool bShowProgress /* = true */)
   }
 
   /* stop the thread */
-  Stop();
+  if (g_guiSettings.GetBool("pvrmanager.enabled"))
+    Stop();
+
   if (bShowProgress)
   {
     pDlgProgress->SetPercentage(20);
@@ -471,11 +473,14 @@ void CPVRManager::ResetDatabase(bool bShowProgress /* = true */)
     m_database->Close();
   }
 
-  Cleanup();
+  CLog::Log(LOGNOTICE,"PVRManager - %s - PVR database cleared", __FUNCTION__);
 
-  CLog::Log(LOGNOTICE,"PVRManager - %s - PVR database cleared. restarting the PVRManager", __FUNCTION__);
-
-  Start();
+  if (g_guiSettings.GetBool("pvrmanager.enabled"))
+  {
+    CLog::Log(LOGNOTICE,"PVRManager - %s - restarting the PVRManager", __FUNCTION__);
+    Cleanup();
+    Start();
+  }
 
   if (bShowProgress)
   {
@@ -490,7 +495,9 @@ void CPVRManager::ResetEPG(void)
 
   StopUpdateThreads();
   m_epg->Reset();
-  StartUpdateThreads();
+
+  if (g_guiSettings.GetBool("pvrmanager.enabled"))
+    StartUpdateThreads();
 }
 
 bool CPVRManager::IsPlaying(void) const
@@ -923,37 +930,44 @@ bool CPVRManager::PerformChannelSwitch(const CPVRChannel &channel, bool bPreview
 
 int CPVRManager::GetTotalTime(void) const
 {
-  return m_guiInfo->GetDuration();
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? 0 : m_guiInfo->GetDuration();
 }
 
 int CPVRManager::GetStartTime(void) const
 {
-  return m_guiInfo->GetStartTime();
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? 0 : m_guiInfo->GetStartTime();
 }
 
 bool CPVRManager::TranslateBoolInfo(DWORD dwInfo) const
 {
-  return m_guiInfo->TranslateBoolInfo(dwInfo);
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? false : m_guiInfo->TranslateBoolInfo(dwInfo);
 }
 
 bool CPVRManager::TranslateCharInfo(DWORD dwInfo, CStdString &strValue) const
 {
-  return m_guiInfo->TranslateCharInfo(dwInfo, strValue);
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? false : m_guiInfo->TranslateCharInfo(dwInfo, strValue);
 }
 
 int CPVRManager::TranslateIntInfo(DWORD dwInfo) const
 {
-  return m_guiInfo->TranslateIntInfo(dwInfo);
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? 0 : m_guiInfo->TranslateIntInfo(dwInfo);
 }
 
 bool CPVRManager::HasTimer(void) const
 {
-  return m_guiInfo->HasTimers();
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? false : m_guiInfo->HasTimers();
 }
 
 bool CPVRManager::IsRecording(void) const
 {
-  return m_guiInfo->IsRecording();
+  CSingleLock lock(m_critSection);
+  return !m_guiInfo ? false : m_guiInfo->IsRecording();
 }
 
 void CPVRManager::LocalizationChanged(void)

xbmc/settings/GUIWindowSettingsCategory.cpp
@@ -1948,7 +1948,7 @@ void CGUIWindowSettingsCategory::OnSettingChanged(CBaseSettingControl *pSettingC
   {
     CUtil::DeleteVideoDatabaseDirectoryCache();
   }
-  else if (strSetting.Equals("pvrmenu.searchicons"))
+  else if (strSetting.Equals("pvrmenu.searchicons") && g_PVRManager.IsStarted())
   {
     g_PVRManager.SearchMissingChannelIcons();
   }
@@ -1962,12 +1962,12 @@ void CGUIWindowSettingsCategory::OnSettingChanged(CBaseSettingControl *pSettingC
     if (CGUIDialogYesNo::ShowAndGetInput(19098, 19188, 750, 0))
       g_PVRManager.ResetEPG();
   }
-  else if (strSetting.Equals("pvrmanager.channelscan"))
+  else if (strSetting.Equals("pvrmanager.channelscan") && g_PVRManager.IsStarted())
   {
     if (CGUIDialogYesNo::ShowAndGetInput(19098, 19118, 19194, 0))
       g_PVRManager.StartChannelScan();
   }
-  else if (strSetting.Equals("pvrmanager.channelmanager"))
+  else if (strSetting.Equals("pvrmanager.channelmanager") && g_PVRManager.IsStarted())
   {
     CGUIDialogPVRChannelManager *dialog = (CGUIDialogPVRChannelManager *)g_windowManager.GetWindow(WINDOW_DIALOG_PVR_CHANNEL_MANAGER);
     if (dialog)

commit 47b298a11a3e541d3692dccb279c1d69251a8206

pvr: fix deadlock in CPVRManager caused by 025772d6e6fbcb6a2cc26dd1af29c0c5f2f477d7. closes #120.

xbmc/pvr/PVRManager.cpp
@@ -931,43 +931,81 @@ bool CPVRManager::PerformChannelSwitch(const CPVRChannel &channel, bool bPreview
 int CPVRManager::GetTotalTime(void) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return 0;
+  lock.Leave();
+
   return !m_guiInfo ? 0 : m_guiInfo->GetDuration();
 }
 
 int CPVRManager::GetStartTime(void) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return 0;
+  lock.Leave();
+
   return !m_guiInfo ? 0 : m_guiInfo->GetStartTime();
 }
 
 bool CPVRManager::TranslateBoolInfo(DWORD dwInfo) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return !m_guiInfo ? false : m_guiInfo->TranslateBoolInfo(dwInfo);
 }
 
 bool CPVRManager::TranslateCharInfo(DWORD dwInfo, CStdString &strValue) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return !m_guiInfo ? false : m_guiInfo->TranslateCharInfo(dwInfo, strValue);
 }
 
 int CPVRManager::TranslateIntInfo(DWORD dwInfo) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return 0;
+  lock.Leave();
+
   return !m_guiInfo ? 0 : m_guiInfo->TranslateIntInfo(dwInfo);
 }
 
 bool CPVRManager::HasTimer(void) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return !m_guiInfo ? false : m_guiInfo->HasTimers();
 }
 
 bool CPVRManager::IsRecording(void) const
 {
   CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return !m_guiInfo ? false : m_guiInfo->IsRecording();
+}
+
+void CPVRManager::ShowPlayerInfo(int iTimeout)
+{
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return;
+  lock.Leave();
+
+  m_guiInfo->ShowPlayerInfo(iTimeout);
 }
 
 void CPVRManager::LocalizationChanged(void)
@@ -988,36 +1026,71 @@ bool CPVRManager::IsRunning(void) const
 
 bool CPVRManager::IsPlayingTV(void) const
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return m_addons->IsPlayingTV();
 }
 
 bool CPVRManager::IsPlayingRadio(void) const
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return m_addons->IsPlayingRadio();
 }
 
 bool CPVRManager::IsPlayingRecording(void) const
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return m_addons->IsPlayingRecording();
 }
 
 bool CPVRManager::IsRunningChannelScan(void) const
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return false;
+  lock.Leave();
+
   return m_addons->IsRunningChannelScan();
 }
 
 PVR_ADDON_CAPABILITIES *CPVRManager::GetCurrentClientProperties(void)
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return NULL;
+  lock.Leave();
+
   return m_addons->GetCurrentAddonCapabilities();
 }
 
 void CPVRManager::StartChannelScan(void)
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return;
+  lock.Leave();
+
   return m_addons->StartChannelScan();
 }
 
 void CPVRManager::SearchMissingChannelIcons(void)
 {
+  CSingleLock lock(m_critSection);
+  if (!m_bLoaded)
+    return;
+  lock.Leave();
+
   return m_channelGroups->SearchMissingChannelIcons();
 }
 
@@ -1132,9 +1205,4 @@ void CPVRManager::ExecutePendingJobs(void)
   }
 
   ResetEvent(m_triggerEvent);
-}
-
-void CPVRManager::ShowPlayerInfo(int iTimeout)
-{
-  m_guiInfo->ShowPlayerInfo(iTimeout);
 }