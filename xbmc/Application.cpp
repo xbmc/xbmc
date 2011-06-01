568190da691eb95057fd960499aafc06496663ec
Jonathan Marshall <jmarshall@never.you.mind>
2011-06-01 20:50:27 +1200

    switch g_windowManager.Process() to CApplication::FrameMove() so that window close anims work as expected

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2789,6 +2789,14 @@ void CApplication::FrameMove()
   ProcessGamepad(frameTime);
   ProcessEventServer(frameTime);
 
+  // Process events and animate controls before rendering
+  unsigned int currentTime = CTimeUtils::GetFrameTime();
+  g_windowManager.Process(currentTime);
+  if (g_Mouse.IsActive())
+  {
+    CDirtyRegionList dirtyregions;
+    m_guiPointer.DoProcess(currentTime, dirtyregions);
+  }
   g_windowManager.FrameMove();
 }
 
@@ -4804,17 +4812,6 @@ void CApplication::Process()
     ProcessSlow();
   }
 
-  unsigned int currentTime = CTimeUtils::GetFrameTime();
-
-  // Process events and animate controls before rendering
-  g_windowManager.Process(currentTime);
-
-  // Render the mouse pointer
-  if (g_Mouse.IsActive())
-  {
-    CDirtyRegionList dirtyregions;
-    m_guiPointer.DoProcess(currentTime, dirtyregions);
-  }
 }
 
 // We get called every 500ms
