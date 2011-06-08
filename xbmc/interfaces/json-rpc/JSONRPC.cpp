edbd033e0c912a3789e96f321aaadd6aac2ce590
Malard <martin.ellis@pulse-eight.com>
2011-06-08 11:07:21 -0400

    added: updated advancedsettings to work via commandline

--- xbmc/interfaces/json-rpc/JSONRPC.cpp
+++ xbmc/interfaces/json-rpc/JSONRPC.cpp
@@ -346,7 +346,7 @@ CStdString CJSONRPC::MethodCall(const CStdString &inputString, ITransportLayer *
     hasResponse = true;
   }
 
-  CStdString str = hasResponse ? CJSONVariantWriter::Write(outputroot, g_advancedSettings.m_jsonOutputCompact) : "";
+  CStdString str = hasResponse ? CJSONVariantWriter::Write(outputroot, g_advancedSettings.OutputCompactJSON()) : "";
   return str;
 }
 
