ad48e030a5accc8019982f262f69d7583e595aea
Martin Ellis <malard@gmail.com>
2011-06-09 10:48:40 +0100

    cosmetics: changed copyright year

--- xbmc/settings/KaraokeSettings.cpp
+++ xbmc/settings/KaraokeSettings.cpp
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2005-2008 Team XBMC
+ *      Copyright (C) 2005-2011 Team XBMC
  *      http://xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
