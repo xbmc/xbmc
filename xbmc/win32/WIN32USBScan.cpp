755c63248575433ee89c0f3986767fe3f3336c2a
Martin Ellis <malard@gmail.com>
2011-06-11 21:28:06 +0100

    changed: only enumerate USB HID devices that are installed

--- xbmc/win32/WIN32USBScan.cpp
+++ xbmc/win32/WIN32USBScan.cpp
@@ -1,12 +1,14 @@
 #include <setupapi.h>
 #include "WIN32USBScan.h"
 
+static GUID USB_HID_GUID = { 0x4D1E55B2, 0xF16F, 0x11CF, { 0x88, 0xCB, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 } };
+
 CWIN32USBScan::CWIN32USBScan()
 {
   HDEVINFO hDevInfo;
   SP_DEVINFO_DATA DeviceInfoData;
 
-  hDevInfo = SetupDiGetClassDevs(NULL, 0, 0, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+  hDevInfo = SetupDiGetClassDevs(&USB_HID_GUID, 0, 0, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
   DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
   for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &DeviceInfoData); i++)
   {
