f0a50bbc068d59c80cc10ab5fdb38d5006aa280c
WiSo <wiso@xbmc.org>
2011-06-19 01:27:25 +0200

    [WIN32] fixed: release build needs other boost named libs

--- project/BuildDependencies/scripts/boost_d.bat
+++ project/BuildDependencies/scripts/boost_d.bat
@@ -10,9 +10,9 @@ cd %TMP_PATH%
 xcopy boost_1_46_1-headers-win32\* "%CUR_PATH%\include\" /E /Q /I /Y
 
 copy boost_1_46_1-debug-libs-win32\lib\libboost_thread-vc100-mt-gd.lib "%CUR_PATH%\lib\libboost_thread-vc100-mt-sgd-1_46_1.lib"/Y
-copy boost_1_46_1-libs-win32\lib\libboost_thread-vc100-mt.lib "%CUR_PATH%\lib\libboost_thread-vc100-mt-1_46_1.lib" /Y
+copy boost_1_46_1-libs-win32\lib\libboost_thread-vc100-mt.lib "%CUR_PATH%\lib\libboost_thread-vc100-mt-s-1_46_1.lib" /Y
 
 copy boost_1_46_1-debug-libs-win32\lib\libboost_date_time-vc100-mt-gd.lib "%CUR_PATH%\lib\libboost_date_time-vc100-mt-sgd-1_46_1.lib"/Y
-copy boost_1_46_1-libs-win32\lib\libboost_date_time-vc100-mt.lib "%CUR_PATH%\lib\libboost_date_time-vc100-mt-1_46_1.lib" /Y
+copy boost_1_46_1-libs-win32\lib\libboost_date_time-vc100-mt.lib "%CUR_PATH%\lib\libboost_date_time-vc100-mt-s-1_46_1.lib" /Y
 
 cd %LOC_PATH%
