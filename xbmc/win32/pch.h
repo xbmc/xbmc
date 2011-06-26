63a5540f6189eef94f8068515b3b03e1bfc0631b
Martin van Beurden <chadoe@xbmc.org>
2011-06-26 20:06:02 +0200

    [WIN32] added threading boost files to the precompiled header file.

--- xbmc/win32/pch.h
+++ xbmc/win32/pch.h
@@ -18,6 +18,9 @@
 #include "D3D9.h"
 #include "D3DX9.h"
 #include "boost/shared_ptr.hpp"
+#include "boost/thread/condition_variable.hpp"
+#include "boost/thread/recursive_mutex.hpp"
+#include "boost/thread/shared_mutex.hpp"
 #include "SDL\SDL.h"
 // anything below here should be headers that very rarely (hopefully never)
 // change yet are included almost everywhere.
