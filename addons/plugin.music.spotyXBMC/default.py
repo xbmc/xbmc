42bc51606178d81439d7e3953d6c2a02566e95f6
David Erenger <david.erenger@gmail.com>
2011-06-29 07:58:12 -0700

    Edited addons/plugin.music.spotyXBMC/default.py via GitHub

--- addons/plugin.music.spotyXBMC/default.py
+++ addons/plugin.music.spotyXBMC/default.py
@@ -5,7 +5,7 @@ __version__ = "0.0.1"
 __plugin__ = "spotyXBMC-" + __version__
 __author__ = "David Erenger"
 __url__ = "https://github.com/akezeke/xbmc"
-__settings__ = xbmcaddon.Addon(id='plugin.music.spotify')
+__settings__ = xbmcaddon.Addon(id='plugin.music.spotyXBMC')
 __language__ = __settings__.getLocalizedString
 
 def startPlugin():	
