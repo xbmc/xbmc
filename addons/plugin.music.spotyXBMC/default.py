c56e21bcd4135b74edc2b3bfc014c419c2a9e056
David Erenger <david.erenger@gmail..com>
2011-06-29 16:49:18 +0200

    Added spotyXBMC launcher addon

---
+++ addons/plugin.music.spotyXBMC/default.py
@@ -0,0 +1,15 @@
+import xbmcplugin,xbmcgui,xbmcaddon
+
+# plugin constants
+__version__ = "0.0.1"
+__plugin__ = "spotyXBMC-" + __version__
+__author__ = "David Erenger"
+__url__ = "https://github.com/akezeke/xbmc"
+__settings__ = xbmcaddon.Addon(id='plugin.music.spotify')
+__language__ = __settings__.getLocalizedString
+
+def startPlugin():	
+        xbmc.executebuiltin('XBMC.ReplaceWindow(10005,"musicdb://spotify/menu/main/")')
+	xbmcplugin.endOfDirectory(handle=int(sys.argv[1]))
+
+startPlugin()
