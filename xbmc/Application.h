xbmc/xbmc

commit 846506d9b17d8227a7c1e50741072edccccab0e9

Removed raw SDL_[sync] calls from Application and introduced a raw condition variable to be used for more SDL replacement and removed #if's that were used to turn off synchronization functionality if SDL wasn't there.

xbmc/Application.h
@@ -25,6 +25,7 @@
 #include "XBApplicationEx.h"
 
 #include "guilib/IMsgTargetCallback.h"
+#include "threads/Condition.h"
 
 class CFileItem;
 class CFileItemList;
@@ -64,8 +65,6 @@ namespace ADDON
 #ifdef HAS_WEB_SERVER
 #include "network/WebServer.h"
 #endif
-
-#include "threads/XBMC_mutex.h"
 
 class CKaraokeLyricsManager;
 class CApplicationMessenger;
@@ -351,8 +350,8 @@ protected:
   
 #if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
   int        m_frameCount;
-  SDL_mutex* m_frameMutex;
-  SDL_cond*  m_frameCond;
+  CCriticalSection m_frameMutex;
+  XbmcThreads::ConditionVariable  m_frameCond;
 #endif
 
   void SetHardwareVolume(long hardwareVolume);

xbmc/Application.cpp
@@ -295,6 +295,8 @@ using namespace JSONRPC;
 #endif
 using namespace ANNOUNCEMENT;
 
+using namespace XbmcThreads;
+
 // uncomment this if you want to use release libs in the debug build.
 // Atm this saves you 7 mb of memory
 #define USE_RELEASE_LIBS
@@ -331,11 +333,7 @@ CApplication::CApplication(void) : m_itemCurrentFile(new CFileItem), m_progressT
 #endif
   m_currentStack = new CFileItemList;
 
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
   m_frameCount = 0;
-  m_frameMutex = SDL_CreateMutex();
-  m_frameCond = SDL_CreateCond();
-#endif
 
   m_bPresentFrame = false;
   m_bPlatformDirectories = true;
@@ -353,13 +351,6 @@ CApplication::~CApplication(void)
   delete m_pKaraokeMgr;
 #endif
 
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
-  if (m_frameMutex)
-    SDL_DestroyMutex(m_frameMutex);
-
-  if (m_frameCond)
-    SDL_DestroyCond(m_frameCond);
-#endif
   delete m_dpms;
 }
 
@@ -1923,16 +1914,16 @@ void CApplication::RenderScreenSaver()
 bool CApplication::WaitFrame(unsigned int timeout)
 {
   bool done = false;
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
+
   // Wait for all other frames to be presented
-  SDL_mutexP(m_frameMutex);
+  CSingleLock lock(m_frameMutex);
   //wait until event is set, but modify remaining time
   DWORD dwStartTime = CTimeUtils::GetTimeMS();
   DWORD dwRemainingTime = timeout;
   while(m_frameCount > 0)
   {
-    int result = SDL_CondWaitTimeout(m_frameCond, m_frameMutex, dwRemainingTime);
-    if (result == 0)
+    ConditionVariable::TimedWaitResponse result = m_frameCond.wait(lock, dwRemainingTime);
+    if (result == ConditionVariable::OK)
     {
       //fix time to wait because of spurious wakeups
       DWORD dwElapsed = CTimeUtils::GetTimeMS() - dwStartTime;
@@ -1944,31 +1935,29 @@ bool CApplication::WaitFrame(unsigned int timeout)
       else
       {
         //ran out of time
-        result = SDL_MUTEX_TIMEDOUT;
+        result = ConditionVariable::TIMEDOUT;
       }
     }
 
-    if(result == SDL_MUTEX_TIMEDOUT)
+    if(result == ConditionVariable::TIMEDOUT)
       break;
     if(result < 0)
       CLog::Log(LOGWARNING, "CApplication::WaitFrame - error from conditional wait");
   }
   done = m_frameCount == 0;
-  SDL_mutexV(m_frameMutex);
-#endif
+
   return done;
 }
 
 void CApplication::NewFrame()
 {
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
   // We just posted another frame. Keep track and notify.
-  SDL_mutexP(m_frameMutex);
-  m_frameCount++;
-  SDL_mutexV(m_frameMutex);
+  {
+    CSingleLock lock(m_frameMutex);
+    m_frameCount++;
+  }
 
-  SDL_CondBroadcast(m_frameCond);
-#endif
+  m_frameCond.notifyAll();
 }
 
 void CApplication::Render()
@@ -2004,8 +1993,7 @@ void CApplication::Render()
     m_bPresentFrame = false;
     if (!extPlayerActive && g_graphicsContext.IsFullScreenVideo() && !IsPaused())
     {
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
-      SDL_mutexP(m_frameMutex);
+      CSingleLock lock(m_frameMutex);
 
       //wait until event is set, but modify remaining time
       DWORD dwStartTime = CTimeUtils::GetTimeMS();
@@ -2013,8 +2001,8 @@ void CApplication::Render()
       // If we have frames or if we get notified of one, consume it.
       while(m_frameCount == 0)
       {
-        int result = SDL_CondWaitTimeout(m_frameCond, m_frameMutex, dwRemainingTime);
-        if (result == 0)
+        ConditionVariable::TimedWaitResponse result = m_frameCond.wait(lock, dwRemainingTime);
+        if (result == ConditionVariable::OK)
         {
           //fix time to wait because of spurious wakeups
           DWORD dwElapsed = CTimeUtils::GetTimeMS() - dwStartTime;
@@ -2026,21 +2014,17 @@ void CApplication::Render()
           else
           {
             //ran out of time
-            result = SDL_MUTEX_TIMEDOUT;
+            result = ConditionVariable::TIMEDOUT;
           }
         }
 
-        if(result == SDL_MUTEX_TIMEDOUT)
+        if(result == ConditionVariable::TIMEDOUT)
           break;
         if(result < 0)
           CLog::Log(LOGWARNING, "CApplication::Render - error from conditional wait");
       }
 
       m_bPresentFrame = m_frameCount > 0;
-      SDL_mutexV(m_frameMutex);
-#else
-      m_bPresentFrame = true;
-#endif
       decrement = m_bPresentFrame;
       hasRendered = true;
     }
@@ -2123,13 +2107,12 @@ void CApplication::Render()
   g_renderManager.UpdateResolution();
   g_renderManager.ManageCaptures();
 
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
-  SDL_mutexP(m_frameMutex);
-  if(m_frameCount > 0 && decrement)
-    m_frameCount--;
-  SDL_mutexV(m_frameMutex);
-  SDL_CondBroadcast(m_frameCond);
-#endif
+  {
+    CSingleLock lock(m_frameMutex);
+    if(m_frameCount > 0 && decrement)
+      m_frameCount--;
+  }
+  m_frameCond.notifyAll();
 }
 
 void CApplication::SetStandAlone(bool value)
@@ -5226,12 +5209,11 @@ bool CApplication::SwitchToFullScreen()
   // See if we're playing a video, and are in GUI mode
   if ( IsPlayingVideo() && g_windowManager.GetActiveWindow() != WINDOW_FULLSCREEN_VIDEO)
   {
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
     // Reset frame count so that timing is FPS will be correct.
-    SDL_mutexP(m_frameMutex);
-    m_frameCount = 0;
-    SDL_mutexV(m_frameMutex);
-#endif
+    {
+      CSingleLock lock(m_frameMutex);
+      m_frameCount = 0;
+    }
 
     // then switch to fullscreen mode
     g_windowManager.ActivateWindow(WINDOW_FULLSCREEN_VIDEO);
@@ -5403,15 +5385,10 @@ bool CApplication::IsCurrentThread() const
 
 bool CApplication::IsPresentFrame()
 {
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
-  SDL_mutexP(m_frameMutex);
+  CSingleLock lock(m_frameMutex);
   bool ret = m_bPresentFrame;
-  SDL_mutexV(m_frameMutex);
 
   return ret;
-#else
-  return false;
-#endif
 }
 
 #if defined(HAS_LINUX_NETWORK)

xbmc/threads/Condition.cpp
@@ -1,0 +1,63 @@
+/*
+ *      Copyright (C) 2005-2011 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "threads/Condition.h"
+
+namespace XbmcThreads
+{
+
+  ConditionVariable::TimedWaitResponse ConditionVariable::wait(CSingleLock& lock, int milliseconds)
+  {
+    ConditionVariable::TimedWaitResponse ret = OK;
+    try
+    {
+      ret = (impl.timed_wait(lock, boost::posix_time::milliseconds(milliseconds))) ? OK : TIMEDOUT;
+    }
+    catch (boost::thread_interrupted exception)
+    {
+      ret = INTERRUPTED;
+    }
+    catch (...)
+    {
+      ret = ERROR;
+    }
+    return ret;
+  }
+
+  ConditionVariable::TimedWaitResponse ConditionVariable::wait(CCriticalSection& mutex, int milliseconds)
+  {
+    ConditionVariable::TimedWaitResponse ret = OK;
+    try
+    {
+      ret = (impl.timed_wait(mutex, boost::posix_time::milliseconds(milliseconds))) ? OK : TIMEDOUT;
+    }
+    catch (boost::thread_interrupted exception)
+    {
+      ret = INTERRUPTED;
+    }
+    catch (...)
+    {
+      ret = ERROR;
+    }
+    return ret;
+  }
+}
+

xbmc/threads/Condition.h
@@ -1,0 +1,55 @@
+/*
+ *      Copyright (C) 2005-2011 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#pragma once
+
+/**
+ * This is a thin wrapper around boost::condition_variable (I
+ * would prefer to use it directly but ...) and will be replacing
+ * existing WaitForSingleObject, SDL_cond, etc.
+ */
+#include <boost/thread/condition_variable.hpp>
+
+#include "threads/SingleLock.h"
+
+namespace XbmcThreads
+{
+  // Did I meantion it was a 'thin' wrapper?
+  class ConditionVariable
+  {
+  private:
+    boost::condition_variable_any impl;
+
+  public:
+
+    enum TimedWaitResponse { OK = 0, TIMEDOUT, INTERRUPTED=-1, ERROR=-2 };
+
+    inline void wait(CSingleLock& lock) { impl.wait(lock); }
+    inline void wait(CCriticalSection& mutex) { impl.wait(mutex); }
+
+    TimedWaitResponse wait(CSingleLock& lock, int milliseconds);
+    TimedWaitResponse wait(CCriticalSection& mutex, int milliseconds);
+
+    inline void notifyAll() { impl.notify_all(); }
+    inline void notify() { impl.notify_one(); }
+  };
+}
+

xbmc/threads/Makefile
@@ -1,4 +1,5 @@
 SRCS=Atomics.cpp \
+     Condition.cpp \
      Event.cpp \
      LockFree.cpp \
      Semaphore.cpp \

commit bb73b9e3278f81b0e663beadf92c6e89ff2b786c

fixed: platforms previously building without sdl and with HAS_XBMC_MUTEX were broken by 846506d9.
Remove the now-useless check.

xbmc/Application.h
@@ -339,11 +339,9 @@ protected:
   bool m_bTestMode;
   bool m_bSystemScreenSaverEnable;
   
-#if defined(HAS_SDL) || defined(HAS_XBMC_MUTEX)
   int        m_frameCount;
   CCriticalSection m_frameMutex;
   XbmcThreads::ConditionVariable  m_frameCond;
-#endif
 
   void Mute();
   void UnMute();