7d687f4e3221fc4a1f8ae4c997026507d5406471
pieh <grajen3@o2.pl>
2011-07-17 10:39:40 +0200

    added property(IsSpecial) to special items (seasons, episodes)

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -115,6 +115,7 @@ CFileItem::CFileItem(const CVideoInfoTag& movie)
     m_bIsFolder = false;
   }
   *GetVideoInfoTag() = movie;
+  if (movie.m_iSeason == 0) SetProperty("isspecial", "true");
   FillInDefaultIcon();
   SetCachedVideoThumb();
 }
