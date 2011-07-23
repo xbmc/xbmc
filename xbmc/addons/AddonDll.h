xbmc/xbmc

commit a6bbd91ba963e9a137a978101c1489a4b78d3f41

pvr: fix memory leaks in PVRClient.cpp and AddonDll.h

xbmc/addons/AddonDll.h
@@ -192,26 +192,27 @@ bool CAddonDll<TheDll, TheStruct, TheProps>::Create()
   /* Allocate the helper function class to allow crosstalk over
      helper libraries */
   m_pHelpers = new CAddonCallbacks(this);
+  m_initialized = true;
 
   /* Call Create to make connections, initializing data or whatever is
      needed to become the AddOn running */
   try
   {
     ADDON_STATUS status = m_pDll->Create(m_pHelpers->GetCallbacks(), m_pInfo);
-    if (status == ADDON_STATUS_OK)
-      m_initialized = true;
-    else if ((status == ADDON_STATUS_NEED_SETTINGS) || (status == ADDON_STATUS_NEED_SAVEDSETTINGS))
+    if ((status == ADDON_STATUS_NEED_SETTINGS) || (status == ADDON_STATUS_NEED_SAVEDSETTINGS))
     {
       m_needsavedsettings = (status == ADDON_STATUS_NEED_SAVEDSETTINGS);
-      if (TransferSettings() == ADDON_STATUS_OK)
-        m_initialized = true;
-      else
+      if (TransferSettings() != ADDON_STATUS_OK)
+      {
         new CAddonStatusHandler(ID(), status, "", false);
+        return false;
+      }
     }
     else
     { // Addon failed initialization
       CLog::Log(LOGERROR, "ADDON: Dll %s - Client returned bad status (%i) from Create and is not usable", Name().c_str(), status);
       new CAddonStatusHandler(ID(), status, "", false);
+      return false;
     }
   }
   catch (std::exception &e)

xbmc/pvr/addons/PVRClient.cpp
@@ -73,6 +73,11 @@ CPVRClient::CPVRClient(const cp_extension_t *ext) :
 
 CPVRClient::~CPVRClient(void)
 {
+  if (m_pInfo)
+  {
+    delete m_pInfo;
+    m_pInfo = NULL;
+  }
 }
 
 bool CPVRClient::Create(int iClientId)
@@ -112,6 +117,8 @@ void CPVRClient::Destroy(void)
     /* Tell the client to destroy */
     CAddonDll<DllPVRClient, PVRClient, PVR_PROPERTIES>::Destroy();
     m_menuhooks.clear();
+    delete m_pInfo;
+    m_pInfo = NULL;
   }
   catch (exception &e)
   {

commit 3ad72b78c2eca1a5c7eab5a3f337297129e89c8e

pvr: fix loading pvr addons (my commit a6bbd91ba963e9a137a978101c1489a4b78d3f41 broke it)

xbmc/addons/AddonDll.h
@@ -199,6 +199,8 @@ bool CAddonDll<TheDll, TheStruct, TheProps>::Create()
   try
   {
     ADDON_STATUS status = m_pDll->Create(m_pHelpers->GetCallbacks(), m_pInfo);
+    if (status == ADDON_STATUS_OK)
+      return true;
     if ((status == ADDON_STATUS_NEED_SETTINGS) || (status == ADDON_STATUS_NEED_SAVEDSETTINGS))
     {
       m_needsavedsettings = (status == ADDON_STATUS_NEED_SAVEDSETTINGS);