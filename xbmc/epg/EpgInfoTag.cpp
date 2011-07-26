028edcb6dd775787f497fac83c2396ac543075bc
Lars Op den Kamp <lars@opdenkamp.eu>
2011-07-26 21:55:28 +0200

    epg: remove m_strPlotOutline from m_strPlot if it's the same. closes #129

--- xbmc/epg/EpgInfoTag.cpp
+++ xbmc/epg/EpgInfoTag.cpp
@@ -233,9 +233,14 @@ void CEpgInfoTag::SetPlotOutline(const CStdString &strPlotOutline)
 
 void CEpgInfoTag::SetPlot(const CStdString &strPlot)
 {
-  if (m_strPlot != strPlot)
+
+  CStdString strPlotClean = (m_strPlotOutline.length() > 0 && strPlot.Left(m_strPlotOutline.length()).Equals(m_strPlotOutline)) ?
+    strPlot.Right(strPlot.length() - m_strPlotOutline.length()) :
+    strPlot;
+
+  if (m_strPlot != strPlotClean)
   {
-    m_strPlot = strPlot;
+    m_strPlot = strPlotClean;
     m_bChanged = true;
     UpdatePath();
   }
