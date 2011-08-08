2c80fa694928dd6c4425c01145a5f43a6c9a6e10
Lars Op den Kamp <lars@opdenkamp.eu>
2011-08-08 23:40:18 +0200

    epg: don't add pvrchannels and timers to epginfotag based fileitems. fixes invalid epg pointers in the ui

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -151,11 +151,6 @@ CFileItem::CFileItem(const CEpgInfoTag& tag)
 
   Reset();
 
-  if (tag.HasPVRChannel())
-    *GetPVRChannelInfoTag() = *tag.ChannelTag();
-  if (tag.HasTimer())
-    *GetPVRTimerInfoTag() = *tag.Timer();
-
   m_strPath = tag.Path();
   m_bIsFolder = false;
   *GetEPGInfoTag() = tag;
