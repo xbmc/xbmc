xbmc/xbmc

commit 140875fcc6a70bcaa9776402c86cdf2e997e3c80

fixed: don't consider closing modal dialog as active

xbmc/guilib/GUIWindowManager.cpp
@@ -726,7 +726,7 @@ int CGUIWindowManager::GetTopMostModalDialogID() const
   for (crDialog it = m_activeDialogs.rbegin(); it != m_activeDialogs.rend(); ++it)
   {
     CGUIWindow *dialog = *it;
-    if (dialog->IsModalDialog())
+    if (dialog->IsModalDialog() && !dialog->IsAnimating(ANIM_TYPE_WINDOW_CLOSE))
     { // have a modal window
       return dialog->GetID();
     }

commit d3ef3cf94c7481b15817d9843e7a8a483583d160

fixed bug (not being able to activate osd, info dialogs) introduced in 140875f

xbmc/guilib/GUIWindowManager.cpp
@@ -720,13 +720,13 @@ bool CGUIWindowManager::HasDialogOnScreen() const
 
 /// \brief Get the ID of the top most routed window
 /// \return id ID of the window or WINDOW_INVALID if no routed window available
-int CGUIWindowManager::GetTopMostModalDialogID() const
+int CGUIWindowManager::GetTopMostModalDialogID(bool ignoreClosing /*= false*/) const
 {
   CSingleLock lock(g_graphicsContext);
   for (crDialog it = m_activeDialogs.rbegin(); it != m_activeDialogs.rend(); ++it)
   {
     CGUIWindow *dialog = *it;
-    if (dialog->IsModalDialog() && !dialog->IsAnimating(ANIM_TYPE_WINDOW_CLOSE))
+    if (dialog->IsModalDialog() && (!ignoreClosing || !dialog->IsAnimating(ANIM_TYPE_WINDOW_CLOSE)))
     { // have a modal window
       return dialog->GetID();
     }
@@ -789,7 +789,7 @@ int CGUIWindowManager::GetActiveWindow() const
 // same as GetActiveWindow() except it first grabs dialogs
 int CGUIWindowManager::GetFocusedWindow() const
 {
-  int dialog = GetTopMostModalDialogID();
+  int dialog = GetTopMostModalDialogID(true);
   if (dialog != WINDOW_INVALID)
     return dialog;
 

xbmc/guilib/GUIWindowManager.h
@@ -108,7 +108,7 @@ public:
   void RouteToWindow(CGUIWindow* dialog);
   void AddModeless(CGUIWindow* dialog);
   void RemoveDialog(int id);
-  int GetTopMostModalDialogID() const;
+  int GetTopMostModalDialogID(bool ignoreClosing = false) const;
 
   void SendThreadMessage(CGUIMessage& message);
   void SendThreadMessage(CGUIMessage& message, int window);