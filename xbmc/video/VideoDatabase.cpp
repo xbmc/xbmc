bb6d1d40fd4265801b519812031e8039b5b9b502
arnova <nospam@void.org>
2011-08-16 13:35:58 +0200

    fixed: TV-show info would no longer show in the files node since b07914fd9fe7e893e267403db7bd992091541209 (thanks JM)

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -7892,6 +7892,11 @@ bool CVideoDatabase::GetItemsForPath(const CStdString &content, const CStdString
     GetMusicVideosByWhere("", where, items);
   }
   for (int i = 0; i < items.Size(); i++)
-    items[i]->SetPath(items[i]->GetVideoInfoTag()->m_strFileNameAndPath);
+  {
+    if (content == "movies")
+      items[i]->SetPath(items[i]->GetVideoInfoTag()->m_strFileNameAndPath);
+    else
+      items[i]->SetPath(items[i]->GetVideoInfoTag()->m_basePath);
+  }
   return items.Size() > 0;
 }
