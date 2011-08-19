650e966bfe737f6189d7698c4aaf7aff3bcc849f
Robert Knight <robertknight@gmail.com>
2011-08-19 20:07:49 +0100

    Set platform defines correctly in Platform.h

--- src/Platform.h
+++ src/Platform.h
@@ -1,8 +1,16 @@
 #pragma once
 
-#define PLATFORM_LINUX
-#define PLATFORM_MAC
-#define PLATFORM_WINDOWS
+#ifdef __linux__
+ #define PLATFORM_LINUX
+#endif
+
+#ifdef WIN32
+ #define PLATFORM_WINDOWS
+#endif
+
+#ifdef __APPLE__
+ #define PLATFORM_MAC
+#endif
 
 #if defined(PLATFORM_LINUX) || defined(PLATFORM_MAC)
  #define PLATFORM_UNIX
