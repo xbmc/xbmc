1c819e8b74acc756a20f9ed82fa9b9edc204d775
davilla <davilla@4pi.com>
2011-09-10 19:30:08 -0400

    [ios] fixed, reported dithered display, change EGL layer from 565 to A8

--- xbmc/osx/atv2/XBMCEAGLView.mm
+++ xbmc/osx/atv2/XBMCEAGLView.mm
@@ -73,8 +73,7 @@
     eaglLayer.opaque = TRUE;
     eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
       [NSNumber numberWithBool:FALSE], kEAGLDrawablePropertyRetainedBacking,
-      kEAGLColorFormatRGB565, kEAGLDrawablePropertyColorFormat,
-      //kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat,
+      kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat,
       nil];
 		
     EAGLContext *aContext = [[EAGLContext alloc] 
