f62f6527c0dd533e31b87c87977111e520b6bac4
Martin Ellis <malard@gmail.com>
2011-09-14 23:06:59 +0100

    changed: if we're in the 'root' of video sources, don't show a further parent directory as this destroys the point of the File window

--- xbmc/windows/GUIMediaWindow.cpp
+++ xbmc/windows/GUIMediaWindow.cpp
@@ -652,7 +652,7 @@ bool CGUIMediaWindow::GetDirectory(const CStdString &strDirectory, CFileItemList
       m_history.RemoveParentPath();
   }
 
-  if (m_guiState.get() && !m_guiState->HideParentDirItems() && !items.GetPath().IsEmpty())
+  if (m_guiState.get() && !m_guiState->HideParentDirItems() && !items.GetPath().IsEmpty() && !items.GetPath().Equals("sources://video/"))
   {
     CFileItemPtr pItem(new CFileItem(".."));
     pItem->SetPath(strParentPath);
