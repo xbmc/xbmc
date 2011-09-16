47feca4c8a5f3be3b4dbbdcef02ffd061b4366bd
vdrfan <vdrfan-nospam-@xbmc.org>
2011-09-16 11:00:48 +0200

    changed: removed dupe path substitute code and moved it to URIUtils

--- xbmc/playlists/PlayListB4S.cpp
+++ xbmc/playlists/PlayListB4S.cpp
@@ -98,8 +98,7 @@ bool CPlayListB4S::LoadData(istream& stream)
       if (pNodeInfo)
       {
         CStdString strInfo = pNodeInfo->FirstChild()->Value();
-        if (URIUtils::IsRemote(m_strBasePath) && g_advancedSettings.m_pathSubstitutions.size() > 0)
-          strFileName = CUtil::SubstitutePath(strFileName);
+        strFileName = URIUtils::SubstitutePath(strFileName);
         CUtil::GetQualifiedFilename(m_strBasePath, strFileName);
         CFileItemPtr newItem(new CFileItem(strInfo));
         newItem->SetPath(strFileName);
