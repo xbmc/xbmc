9660d7282e5608ed586d76239623b428ad0cb132
CrystalP <CrystalP@xbmc.org>
2011-09-29 22:52:58 -0400

    Kill log spam introduced in Skin Variables PR

--- xbmc/guilib/GUIInfoTypes.cpp
+++ xbmc/guilib/GUIInfoTypes.cpp
@@ -286,6 +286,8 @@ void CGUIInfoLabel::Parse(const CStdString &label)
       }
     }
 
+    if (format != NONE)
+    {
     if (pos1 > 0)
       m_info.push_back(CInfoPortion(0, work.Left(pos1), ""));
 
@@ -321,6 +323,7 @@ void CGUIInfoLabel::Parse(const CStdString &label)
     {
       CLog::Log(LOGERROR, "Error parsing label - missing ']' in \"%s\"", label.c_str());
       return;
+    }
     }
   }
   while (format != NONE);
