7c34a566e80607531f894e45ee88bccdaae8b67a
CrystalP <CrystalP@xbmc.org>
2011-10-06 13:25:28 -0400

    Changed format string - %td not supported by our MingW environment. (cherry picked from commit 69279f61ff9ffab87f34e61e9f27380a3c81b8f0)

--- lib/ffmpeg/libavcodec/h264.c
+++ lib/ffmpeg/libavcodec/h264.c
@@ -2568,7 +2568,7 @@ static int decode_slice(struct AVCodecContext *avctx, void *arg){
                 return 0;
             }
             if( ret < 0 || h->cabac.bytestream > h->cabac.bytestream_end + 2) {
-                av_log(h->s.avctx, AV_LOG_ERROR, "error while decoding MB %d %d, bytestream (%td)\n", s->mb_x, s->mb_y, h->cabac.bytestream_end - h->cabac.bytestream);
+                av_log(h->s.avctx, AV_LOG_ERROR, "error while decoding MB %d %d, bytestream (%u)\n", s->mb_x, s->mb_y, h->cabac.bytestream_end - h->cabac.bytestream);
                 ff_er_add_slice(s, s->resync_mb_x, s->resync_mb_y, s->mb_x, s->mb_y, (AC_ERROR|DC_ERROR|MV_ERROR)&part_mask);
                 return -1;
             }
