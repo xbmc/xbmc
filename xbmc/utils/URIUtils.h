bb1edcf30287528ad6397d868f76c62a2993672f
elupus <elupus@xbmc.org>
2011-10-10 19:19:23 +0200

    Factor out code that checks if hostname contains a parent path

--- xbmc/utils/URIUtils.h
+++ xbmc/utils/URIUtils.h
@@ -108,5 +108,8 @@ public:
     AddFileToFolder(strFolder, strFile, result);
     return result;
   }
+
+  static bool ProtocolHasParentInHostname(const CStdString& prot);
+  static bool ProtocolHasEncodedHostname(const CStdString& prot);
 };
 
