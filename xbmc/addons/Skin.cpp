xbmc/xbmc

commit 371c73be51f3e5620ecd35c42115829d0ba29abb

revert 43f00d26 and move just includes.xml loading after reading localization xmls

xbmc/addons/Skin.cpp
@@ -113,7 +113,6 @@ void CSkinInfo::Start(const CStdString &strBaseDir)
         m_resolutions.push_back(res);
     }
   }
-  LoadIncludes();
 }
 
 struct closestRes

xbmc/addons/Skin.h
@@ -108,6 +108,7 @@ public:
 
 //  static bool Check(const CStdString& strSkinDir); // checks if everything is present and accounted for without loading the skin
   static double GetMinVersion();
+  void LoadIncludes();
 protected:
   /*! \brief Given a resolution, retrieve the corresponding directory name
    \param res RESOLUTION to translate
@@ -123,7 +124,6 @@ protected:
    */
   void GetDefaultResolution(const cp_extension_t *ext, const char *tag, RESOLUTION &res, const RESOLUTION &def) const;
 
-  void LoadIncludes();
   bool LoadStartupWindows(const cp_extension_t *ext);
 
   RESOLUTION_INFO m_defaultRes;

xbmc/Application.cpp
@@ -1662,6 +1662,7 @@ void CApplication::LoadSkin(const SkinPtr& skin)
 
   CLog::Log(LOGINFO, "  load skin from:%s", skin->Path().c_str());
   g_SkinInfo = skin;
+  g_SkinInfo->Start();
 
   CLog::Log(LOGINFO, "  load fonts for skin...");
   g_graphicsContext.SetMediaDir(skin->Path());
@@ -1695,7 +1696,7 @@ void CApplication::LoadSkin(const SkinPtr& skin)
 
   g_localizeStrings.LoadSkinStrings(langPath, skinEnglishPath);
 
-  g_SkinInfo->Start();
+  g_SkinInfo->LoadIncludes();
 
   int64_t start;
   start = CurrentHostCounter();

commit e823c3438c251c3ced3b2dca5e272ffc57ab1aa1

Enable addons to use their own includes.xml
Before, XBMC would complain if an addon script was missing its includes.xml. Ironically, if this file existed, XBMC would silently ignore it.

Upon finding the addon-specific includes.xml, XBMC will merge these includes with the current skin's, enabling their use within the addon's windows.

This commit has been updated to fix the breaking changes introduced in 371c73b.

Fixes #6212

xbmc/addons/Skin.cpp
@@ -180,6 +180,15 @@ void CSkinInfo::LoadIncludes()
   m_includes.LoadIncludes(includesPath);
 }
 
+void CSkinInfo::AddIncludes(const CSkinInfo &startedSkin)
+{
+  if (!startedSkin.m_includes.IsEmpty())
+  {
+    CStdString includesPath = PTH_IC(startedSkin.GetSkinPath("includes.xml"));
+    m_includes.LoadIncludes(includesPath);
+  }
+}
+
 void CSkinInfo::ResolveIncludes(TiXmlElement *node)
 {
   m_includes.ResolveIncludes(node);

xbmc/addons/Skin.h
@@ -93,6 +93,12 @@ public:
    */
   static bool TranslateResolution(const CStdString &name, RESOLUTION_INFO &res);
 
+  /*! \brief Add the includes loaded from another skin to the current includes.
+      \param startedSkin the skin (should have already been loaded via Start(), otherwise
+             nothing will happen)
+   */
+  void AddIncludes(const CSkinInfo &startedSkin);
+
   void ResolveIncludes(TiXmlElement *node);
 
   float GetEffectsSlowdown() const { return m_effectsSlowDown; };

xbmc/guilib/GUIIncludes.h
@@ -38,6 +38,7 @@ public:
   void ClearIncludes();
   bool LoadIncludes(const CStdString &includeFile);
   bool LoadIncludesFromXML(const TiXmlElement *root);
+  bool IsEmpty() const {return !m_includes.size() && !m_defaults.size() && !m_constants.size();};
 
   /*! \brief Resolve <include>name</include> tags recursively for the given XML element
    Replaces any instances of <include file="foo">bar</include> with the value of the include

xbmc/interfaces/python/xbmcmodule/winxml.cpp
@@ -83,23 +83,46 @@ namespace PYXBMC
       CStdString basePath = URIUtils::AddFileToFolder(fallbackPath, g_SkinInfo->ID());
 
       // Check for the matching folder for the skin in the fallback skins folder (if it exists)
+      bool bSkinPathFound = false; // avoid hitting fs twice
       if (XFILE::CFile::Exists(basePath))
       {
         props.path = basePath;
         CSkinInfo skinInfo(props, res);
-        skinInfo.Start();
+
+        // Load resolutions, fonts, strings and includes, IN THIS ORDER
+        skinInfo.Start(); // for resolutions
+        // additional strings can be loaded here
+        skinInfo.LoadIncludes();
+        // additional fonts can be loaded here
+
         strSkinPath = skinInfo.GetSkinPath(strXMLname, &res);
+        if (XFILE::CFile::Exists(strSkinPath))
+        {
+          // These includes are added to the skin's includes because the skin provided them
+          bSkinPathFound = true;
+          g_SkinInfo->AddIncludes(skinInfo);
+        }
       }
-      
-      if (!XFILE::CFile::Exists(strSkinPath))
+
+      if (!bSkinPathFound)
       {
         // Finally fallback to the DefaultSkin as it didn't exist in either the XBMC Skin folder or the fallback skin folder
         props.path = URIUtils::AddFileToFolder(fallbackPath, strDefault);
         CSkinInfo skinInfo(props, res);
 
-        skinInfo.Start();
+        // Load resolutions, fonts, strings and includes, IN THIS ORDER
+        skinInfo.Start(); // for resolutions
+        // additional strings can be loaded here
+        skinInfo.LoadIncludes();
+        // additional fonts can be loaded here
+
         strSkinPath = skinInfo.GetSkinPath(strXMLname, &res);
-        if (!XFILE::CFile::Exists(strSkinPath))
+        if (XFILE::CFile::Exists(strSkinPath))
+        {
+          // Add the addon's local includes to the main include namespace
+          g_SkinInfo->AddIncludes(skinInfo);
+        }
+        else
         {
           PyErr_SetString(PyExc_TypeError, "XML File for Window is missing");
           return NULL;

xbmc/interfaces/python/xbmcmodule/winxmldialog.cpp
@@ -84,23 +84,46 @@ namespace PYXBMC
       CStdString basePath = URIUtils::AddFileToFolder(fallbackPath, g_SkinInfo->ID());
 
       // Check for the matching folder for the skin in the fallback skins folder (if it exists)
+      bool bSkinPathFound = false; // avoid hitting fs twice
       if (XFILE::CFile::Exists(basePath))
       {
         props.path = basePath;
         CSkinInfo skinInfo(props, res);
-        skinInfo.Start();
+
+        // Load resolutions, fonts, strings and includes, IN THIS ORDER
+        skinInfo.Start(); // for resolutions
+        // additional strings can be loaded here
+        skinInfo.LoadIncludes();
+        // additional fonts can be loaded here
+
         strSkinPath = skinInfo.GetSkinPath(strXMLname, &res);
+        if (XFILE::CFile::Exists(strSkinPath))
+        {
+          // These includes are added to the skin's includes because the skin provided them
+          bSkinPathFound = true;
+          g_SkinInfo->AddIncludes(skinInfo);
+        }
       }
 
-      if (!XFILE::CFile::Exists(strSkinPath))
+      if (!bSkinPathFound)
       {
         // Finally fallback to the DefaultSkin as it didn't exist in either the XBMC Skin folder or the fallback skin folder
         props.path = URIUtils::AddFileToFolder(fallbackPath, strDefault);
         CSkinInfo skinInfo(props, res);
 
-        skinInfo.Start();
+        // Load resolutions, fonts, strings and includes, IN THIS ORDER
+        skinInfo.Start(); // for resolutions
+        // additional strings can be loaded here
+        skinInfo.LoadIncludes();
+        // additional fonts can be loaded here
+
         strSkinPath = skinInfo.GetSkinPath(strXMLname, &res);
-        if (!XFILE::CFile::Exists(strSkinPath))
+        if (XFILE::CFile::Exists(strSkinPath))
+        {
+          // Add the addon's local includes to the main include namespace
+          g_SkinInfo->AddIncludes(skinInfo);
+        }
+        else
         {
           PyErr_SetString(PyExc_TypeError, "XML File for Window is missing");
           return NULL;