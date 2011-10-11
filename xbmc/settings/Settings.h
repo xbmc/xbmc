7e7a13cd2d11a449c3413bbe82abaff86be8c96d
davilla <davilla@4pi.com>
2011-10-10 23:37:54 -0400

    [ios] changed/fixed, detect skin.touched. if present enable packing it and use for default skin if permitted by code ifdef. fixed also check when xcode does copy root files for packaging into xbmc.app. fixed errors in copy root files, skin.touched has background not backgrounds dir

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -23,7 +23,7 @@
 #define PRE_SKIN_VERSION_9_10_COMPATIBILITY 1
 #define PRE_SKIN_VERSION_11_COMPATIBILITY 1
 
-#if defined(TARGET_DARWIN_IOS) && !defined(TARGET_DARWIN_IOS_ATV2)
+#if defined(HAS_SKIN_TOUCHED) && defined(TARGET_DARWIN_IOS) && !defined(TARGET_DARWIN_IOS_ATV2)
 #define DEFAULT_SKIN          "skin.touched"
 #else
 #define DEFAULT_SKIN          "skin.confluence"
