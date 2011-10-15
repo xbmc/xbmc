56d8d4606cb0c40d4886fa4e9405674b53ec9779
spiff <spiff@xbmc.org>
2011-10-15 08:39:27 +0200

    fixed: __attribute__(unused) makes visual studio no happy

--- xbmc/system.h
+++ xbmc/system.h
@@ -23,6 +23,7 @@
 
 #if defined(HAVE_CONFIG_H) && !defined(TARGET_WINDOWS)
 #include "config.h"
+#define DECLARE_UNUSED(a,b) a __attribute__(unused) b;
 #endif
 
 /*****************
@@ -115,6 +116,8 @@
 #define HAS_ZEROCONF
 #define HAS_AIRPLAY
 #define HAVE_LIBCEC
+
+#define DECLARE_UNUSED(a,b) a b;
 #endif
 
 /*****************
