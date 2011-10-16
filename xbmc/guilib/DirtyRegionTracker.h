bb9e0554dd0a297d91260c119929be982998b5a3
Joakim Plate <elupus@ecce.se>
2011-10-16 13:01:53 +0200

    changed: track 3 buffers on linux too for dirty rendering
    intel has tripple buffered backbuffer on recent drivers

--- xbmc/guilib/DirtyRegionTracker.h
+++ xbmc/guilib/DirtyRegionTracker.h
@@ -25,10 +25,8 @@
 
 #if defined(TARGET_DARWIN_IOS)
 #define DEFAULT_BUFFERING 4
-#elif defined(_WIN32)
-#define DEFAULT_BUFFERING 3
 #else
-#define DEFAULT_BUFFERING 2
+#define DEFAULT_BUFFERING 3
 #endif
 
 class CDirtyRegionTracker
