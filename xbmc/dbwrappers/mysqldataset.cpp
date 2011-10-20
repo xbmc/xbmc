f28884767ebea418ee595d4373b28d49431a1ff6
firnsy <firnsy@securixlive.com>
2011-10-20 22:14:22 +1100

    changed: ensure db copy process is accurate with it's failures.

--- xbmc/dbwrappers/mysqldataset.cpp
+++ xbmc/dbwrappers/mysqldataset.cpp
@@ -139,13 +139,13 @@ int MysqlDatabase::connect(bool create_new) {
       }
       else if (create_new)
       {
-        CLog::Log(LOGDEBUG, "Creating new db becuase you asked me.");
-
         char sqlcmd[512];
+        int ret;
+
         sprintf(sqlcmd, "CREATE DATABASE `%s`", db.c_str());
-        if ( query_with_reconnect(sqlcmd) )
+        if ( (ret=query_with_reconnect(sqlcmd)) != MYSQL_OK )
         {
-          throw DbErrors("Can't create database: '%s'\nError: %s", db.c_str(), strerror(errno));
+          throw DbErrors("Can't create new database: '%s' (%d)", db.c_str(), ret);
         }
       }
 
@@ -197,10 +197,11 @@ int MysqlDatabase::drop() {
   if (!active)
     throw DbErrors("Can't drop database: no active connection...");
   char sqlcmd[512];
+  int ret;
   sprintf(sqlcmd,"DROP DATABASE `%s`", db.c_str());
-  if ( query_with_reconnect(sqlcmd) )
+  if ( (ret=query_with_reconnect(sqlcmd)) != MYSQL_OK )
   {
-    throw DbErrors("Can't drop database: '%s'\nError: %s", db.c_str(), strerror(errno));
+    throw DbErrors("Can't drop database: '%s' (%d)", db.c_str(), ret);
   }
   disconnect();
   return DB_COMMAND_OK;
@@ -211,19 +212,20 @@ int MysqlDatabase::copy(const char *backup_name) {
     throw DbErrors("Can't copy database: no active connection...");
 
   char sql[512];
+  int ret;
 
   // create the new database
   sprintf(sql, "CREATE DATABASE `%s`", backup_name);
-  if ( query_with_reconnect(sql) )
-    throw DbErrors("Can't create database for copy: '%s'\nError: %s", db.c_str(), strerror(errno));
+  if ( (ret=query_with_reconnect(sql)) != MYSQL_OK )
+    throw DbErrors("Can't create database for copy: '%s' (%d)", db.c_str(), ret);
 
   // ensure we're connected to the db we are about to copy
-  if (mysql_select_db(conn, db.c_str()))
+  if ( (ret=mysql_select_db(conn, db.c_str())) != MYSQL_OK )
     throw DbErrors("Can't connect to master database: '%s'",db.c_str());
 
   // grab a list of base tables only (no views)
   sprintf(sql, "SHOW FULL TABLES WHERE Table_type = 'BASE TABLE'");
-  if ( query_with_reconnect(sql) )
+  if ( (ret=query_with_reconnect(sql)) != MYSQL_OK )
     throw DbErrors("Can't determine base tables");
 
   // get list of all tables from old DB
@@ -234,7 +236,7 @@ int MysqlDatabase::copy(const char *backup_name) {
     if (mysql_num_rows(res) == 0)
     {
       mysql_free_result(res);
-      return -1; // EMPTY DB
+      throw DbErrors("The source DB was unexpectedly empty.");
     }
 
     MYSQL_ROW row;
