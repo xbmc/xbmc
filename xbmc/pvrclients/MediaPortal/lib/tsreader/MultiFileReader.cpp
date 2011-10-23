f3b27daaf861ccc22e629b066bb0a7835c1dc048
Marcel Groothuis <margro.xbmc@gmail.com>
2011-10-23 15:59:47 +0200

    MediaPortal pvr addon: fixes for compiler warnings

--- xbmc/pvrclients/MediaPortal/lib/tsreader/MultiFileReader.cpp
+++ xbmc/pvrclients/MediaPortal/lib/tsreader/MultiFileReader.cpp
@@ -431,7 +431,7 @@ long MultiFileReader::RefreshTSBufferFile()
 
       if (m_bDebugOutput)
       {
-        XBMC->Log(LOG_DEBUG, "MultiFileReader: Removing file %s\n", file->filename);
+        XBMC->Log(LOG_DEBUG, "MultiFileReader: Removing file %s\n", file->filename.c_str());
       }
       
       delete file;
@@ -482,7 +482,7 @@ long MultiFileReader::RefreshTSBufferFile()
       // Convert the current filename (wchar to normal char)
       char* wide2normal = new char[length + 1];
       wcstombs( wide2normal, pwCurrFile, length );
-      wide2normal[length] = NULL;
+      wide2normal[length] = '\0';
       std::string sCurrFile = wide2normal;
       delete[] wide2normal;
 
