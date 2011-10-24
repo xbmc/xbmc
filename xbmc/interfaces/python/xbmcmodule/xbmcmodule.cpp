b4fa51090610d2ba1cd8db2cebc580f041c02729
bobo1on1 <bob-nospam-@xbmc.org>
2011-10-24 20:10:25 +0200

    fixed: only compile in python rendercapture binding when python version is >= 2.6, since it uses PyByteArray_FromStringAndSize

--- xbmc/interfaces/python/xbmcmodule/xbmcmodule.cpp
+++ xbmc/interfaces/python/xbmcmodule/xbmcmodule.cpp
@@ -1000,20 +1000,27 @@ namespace PYXBMC
   InitXBMCTypes(bool bInitTypes)
   {
     initKeyboard_Type();
-    initRenderCapture_Type();
     initPlayer_Type();
     initPlayList_Type();
     initPlayListItem_Type();
     initInfoTagMusic_Type();
     initInfoTagVideo_Type();
 
+#ifdef HAS_PYRENDERCAPTURE
+    initRenderCapture_Type();
+#endif
+
     if (PyType_Ready(&Keyboard_Type) < 0 ||
-        PyType_Ready(&RenderCapture_Type) < 0 ||
         PyType_Ready(&Player_Type) < 0 ||
         PyType_Ready(&PlayList_Type) < 0 ||
         PyType_Ready(&PlayListItem_Type) < 0 ||
         PyType_Ready(&InfoTagMusic_Type) < 0 ||
         PyType_Ready(&InfoTagVideo_Type) < 0) return;
+
+#ifdef HAS_PYRENDERCAPTURE
+    if (PyType_Ready(&RenderCapture_Type) < 0)
+      return;
+#endif
   }
 
   PyMODINIT_FUNC
@@ -1030,12 +1037,15 @@ namespace PYXBMC
     PyObject* pXbmcModule;
 
     Py_INCREF(&Keyboard_Type);
-    Py_INCREF(&RenderCapture_Type);
     Py_INCREF(&Player_Type);
     Py_INCREF(&PlayList_Type);
     Py_INCREF(&PlayListItem_Type);
     Py_INCREF(&InfoTagMusic_Type);
     Py_INCREF(&InfoTagVideo_Type);
+
+#ifdef HAS_PYRENDERCAPTURE
+    Py_INCREF(&RenderCapture_Type);
+#endif
 
     pXbmcModule = Py_InitModule((char*)"xbmc", xbmcMethods);
     if (pXbmcModule == NULL) return;
@@ -1083,7 +1093,9 @@ namespace PYXBMC
     PyModule_AddIntConstant(pXbmcModule, (char*)"LOGNONE", LOGNONE);
     PyModule_AddObject(pXbmcModule, (char*)"abortRequested", PyBool_FromLong(0));
 
+#ifdef HAS_PYRENDERCAPTURE
     PyModule_AddObject(pXbmcModule, (char*)"RenderCapture", (PyObject*)&RenderCapture_Type);
+#endif
 
     // render capture user states
     PyModule_AddIntConstant(pXbmcModule, (char*)"CAPTURE_STATE_WORKING", (int)CAPTURESTATE_WORKING);
