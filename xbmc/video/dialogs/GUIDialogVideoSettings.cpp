1d24de44f748d321ee9f678a3300b24fbe88fa1e
Joel Wikberg <joel@wikberg.eu>
2011-11-21 15:47:18 +0100

    Fixed saving of DSPlayer scaling method.

--- xbmc/video/dialogs/GUIDialogVideoSettings.cpp
+++ xbmc/video/dialogs/GUIDialogVideoSettings.cpp
@@ -186,7 +186,7 @@ void CGUIDialogVideoSettings::CreateSettings()
     entries.push_back(make_pair(DS_SCALINGMETHOD_BILINEAR_2_75     , 55009));
     entries.push_back(make_pair(DS_SCALINGMETHOD_BILINEAR_2_100    , 55010));
     
-    //m_scalingMethod = g_settings.m_currentVideoSettings.GetDSPlayerScalingMethod();
+    m_scalingMethod = g_settings.m_currentVideoSettings.GetDSPlayerScalingMethod();
     AddSpin(VIDEO_SETTINGS_SCALINGMETHOD, 16300, &m_scalingMethod, entries);
 
     entries.clear();
