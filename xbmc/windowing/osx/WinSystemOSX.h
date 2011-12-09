8b9fc03e68da355f19870219ae697f3fee777e02
Memphiz <memphis@machzwo.de>
2011-12-09 20:51:42 +0100

    [osx] - fixed selection of initial display mode - don't use CGDisplayBestModeForParameters - it might prever a interlaced mode before a progressive one - figure out the right mode on our own

--- xbmc/windowing/osx/WinSystemOSX.h
+++ xbmc/windowing/osx/WinSystemOSX.h
@@ -58,7 +58,7 @@ protected:
   void* CreateWindowedContext(void* shareCtx);
   void* CreateFullScreenContext(int screen_index, void* shareCtx);
   void  GetScreenResolution(int* w, int* h, double* fps, int screenIdx);
-  void  EnableVSync(bool enable);
+  void  EnableVSync(bool enable); 
   bool  SwitchToVideoMode(int width, int height, double refreshrate, int screenIdx);
   void  FillInVideoModes();
   bool  FlushBuffer(void);
