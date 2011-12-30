xbmc/xbmc

commit 825175e4a300ea1409cc288f675c570b2cc4b46a

changed: avoid including Autorun.h all over xbmc

xbmc/Application.cpp
@@ -364,10 +364,12 @@ CApplication::CApplication(void) : m_itemCurrentFile(new CFileItem), m_progressT
   m_bEnableLegacyRes = false;
   m_bSystemScreenSaverEnable = false;
   m_pInertialScrollingHandler = new CInertialScrollingHandler();
+  m_Autorun = new CAutorun();
 }
 
 CApplication::~CApplication(void)
 {
+  delete m_Autorun;
   delete m_currentStack;
 
 #ifdef HAS_KARAOKE
@@ -4794,7 +4796,7 @@ void CApplication::ProcessSlow()
 #ifdef HAS_DVD_DRIVE
   // checks whats in the DVD drive and tries to autostart the content (xbox games, dvd, cdda, avi files...)
   if (!IsPlayingVideo())
-    m_Autorun.HandleAutorun();
+    m_Autorun->HandleAutorun();
 #endif
 
   // update upnp server/renderer states

xbmc/Application.h
@@ -38,6 +38,11 @@ namespace ADDON
   typedef boost::shared_ptr<IAddon> AddonPtr;
 }
 
+namespace MEDIA_DETECT
+{
+  class CAutorun;
+}
+
 #include "cores/IPlayer.h"
 #include "cores/playercorefactory/PlayerCoreFactory.h"
 #include "PlayListPlayer.h"
@@ -47,7 +52,6 @@ namespace ADDON
 #ifdef _WIN32
 #include "win32/WIN32Util.h"
 #endif
-#include "Autorun.h"
 #include "video/Bookmark.h"
 #include "utils/Stopwatch.h"
 #include "ApplicationMessenger.h"
@@ -211,7 +215,7 @@ public:
 #endif
 
 #ifdef HAS_DVD_DRIVE
-  MEDIA_DETECT::CAutorun m_Autorun;
+  MEDIA_DETECT::CAutorun* m_Autorun;
 #endif
 
 #if !defined(_WIN32) && defined(HAS_DVD_DRIVE)

xbmc/music/windows/GUIWindowMusicSongs.cpp
@@ -37,6 +37,7 @@
 #include "guilib/LocalizeStrings.h"
 #include "utils/log.h"
 #include "utils/URIUtils.h"
+#include "Autorun.h"
 
 #define CONTROL_BTNVIEWASICONS     2
 #define CONTROL_BTNSORTBY          3

xbmc/pictures/GUIWindowPictures.cpp
@@ -40,6 +40,7 @@
 #include "utils/TimeUtils.h"
 #include "utils/log.h"
 #include "utils/URIUtils.h"
+#include "Autorun.h"
 
 #define CONTROL_BTNVIEWASICONS      2
 #define CONTROL_BTNSORTBY           3

xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -65,6 +65,7 @@
 #include "GUIUserMessages.h"
 #include "addons/Skin.h"
 #include "storage/MediaManager.h"
+#include "Autorun.h"
 
 using namespace std;
 using namespace XFILE;

xbmc/windows/GUIWindowFileManager.cpp
@@ -58,6 +58,7 @@
 #include "utils/FileOperationJob.h"
 #include "utils/FileUtils.h"
 #include "utils/URIUtils.h"
+#include "Autorun.h"
 
 using namespace std;
 using namespace XFILE;

commit 619f0b041bc94d4d5d1d18630eeef40e2f4e4d71

fixed: compile with --disable-optical-drive broke in 825175e4a300ea1409cc288f675c570b2cc4b46a

xbmc/Application.cpp
@@ -372,14 +372,18 @@ CApplication::CApplication(void) : m_WebServer(*new CWebServer)
   m_bEnableLegacyRes = false;
   m_bSystemScreenSaverEnable = false;
   m_pInertialScrollingHandler = new CInertialScrollingHandler();
+#ifdef HAS_DVD_DRIVE
   m_Autorun = new CAutorun();
+#endif
 }
 
 CApplication::~CApplication(void)
 {
   delete &m_WebServer;
   delete &m_progressTrackingVideoResumeBookmark;
+#ifdef HAS_DVD_DRIVE
   delete m_Autorun;
+#endif
   delete m_currentStack;
 
 #ifdef HAS_KARAOKE