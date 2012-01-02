56f993c9b62559296a2453a7ee9cc885d25b8f16
CrystalP <CrystalP@xbmc.org>
2012-01-02 14:13:52 -0500

    [WIN] change git exe priority order
    Try msysgit first, as it's a direct port of the official tool.
    Newer msysgit installs a wrapper accessible in the path, try that first,
    then git.exe if found, and last tortoise git command line tool tgit.exe.

--- project/Win32BuildSetup/extract_git_rev.bat
+++ project/Win32BuildSetup/extract_git_rev.bat
@@ -4,26 +4,27 @@ REM Batch file output: %GIT_REV% variable, containing the git revision
 
 SET GIT_REV=unknown
 
-REM Use tgit.exe of TortoiseGit if available
-SET GITEXE=tgit.exe
-%GITEXE% --version > NUL 2>&1
-IF errorlevel 9009 IF NOT errorlevel 9010 GOTO :notgit
+
+REM Try wrapped msysgit - must be in the path
+SET GITEXE=git.cmd
+CALL %GITEXE% --help > NUL 2>&1
+IF errorlevel 1 GOTO nowrapmsysgit
 GOTO :extract
 
-:notgit
+:nowrapmsysgit
 
-REM Fallback on msysgit - must be in the path
+REM Fallback on regular msysgit - must be in the path
 SET GITEXE=git.exe
 %GITEXE% --help > NUL 2>&1
-IF errorlevel 9009 IF NOT errorlevel 9010 GOTO :nomsysgit
+IF errorlevel 9009 IF NOT errorlevel 9010 GOTO nomsysgit
 GOTO :extract
 
 :nomsysgit
 
-REM Fallback on wrapped msysgit - must be in the path
-SET GITEXE=git.cmd
-CALL %GITEXE% --help > NUL 2>&1
-IF errorlevel 1 GOTO :done
+REM Fallback on tgit.exe of TortoiseGit if available
+SET GITEXE=tgit.exe
+%GITEXE% --version > NUL 2>&1
+IF errorlevel 9009 IF NOT errorlevel 9010 GOTO done
 GOTO :extract
 
 
