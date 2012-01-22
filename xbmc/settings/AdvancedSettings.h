2b0d7e9a765bf3e25469751248b9c5aedc5289b5
Memphiz <memphis@machzwo.de>
2012-01-22 12:56:46 +0100

    [cleanup] - removed unused address variables for lcd displays

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -141,10 +141,6 @@ class CAdvancedSettings
 
     int m_lcdRows;
     int m_lcdColumns;
-    int m_lcdAddress1;
-    int m_lcdAddress2;
-    int m_lcdAddress3;
-    int m_lcdAddress4;
     bool m_lcdHeartbeat;
     bool m_lcdDimOnScreenSave;
     int m_lcdScrolldelay;
