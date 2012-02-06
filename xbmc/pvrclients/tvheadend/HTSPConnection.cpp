bb4a4ace41cefecc272b4fa5cbbe8140bed2fe2a
Lars Op den Kamp <lars@opdenkamp.eu>
2012-02-06 17:17:48 +0100

    tvheadend add-on: use libCEC's tcp sockets instead of lib/libTcpSocket. uses mutex protected sockets instead of unprotected ones

--- xbmc/pvrclients/tvheadend/HTSPConnection.cpp
+++ xbmc/pvrclients/tvheadend/HTSPConnection.cpp
@@ -22,6 +22,7 @@
 #include "HTSPConnection.h"
 #include "../../../lib/platform/threads/mutex.h"
 #include "../../../lib/platform/util/timeutils.h"
+#include "../../../lib/platform/sockets/tcp.h"
 #include "client.h"
 
 extern "C" {
@@ -36,7 +37,7 @@ using namespace ADDON;
 using namespace PLATFORM;
 
 CHTSPConnection::CHTSPConnection() :
-    m_fd(INVALID_SOCKET),
+    m_socket(NULL),
     m_challenge(NULL),
     m_iChallengeLength(0),
     m_iProtocol(0),
@@ -63,22 +64,20 @@ bool CHTSPConnection::Connect()
   uint64_t iNow = GetTimeMs();
   uint64_t iTarget = iNow + m_iConnectTimeout;
 
-  char errbuf[1024];
-  int  errlen = sizeof(errbuf);
-
   XBMC->Log(LOG_DEBUG, "%s - connecting to '%s', port '%d'", __FUNCTION__, m_strHostname.c_str(), m_iPortnumber);
 
-  m_fd = INVALID_SOCKET;
-  while (m_fd == INVALID_SOCKET && iNow < iTarget)
+  if (!m_socket)
+    m_socket = new CTcpConnection(m_strHostname, m_iPortnumber);
+  while (!m_socket->IsOpen() && iNow < iTarget)
   {
-    m_fd = tcp_connect(m_strHostname.c_str(), m_iPortnumber, errbuf, errlen, iTarget - iNow);
-    CCondition::Sleep(100);
+    if (!m_socket->Open(iTarget - iNow))
+      CCondition::Sleep(100);
     iNow = GetTimeMs();
   }
 
-  if(m_fd == INVALID_SOCKET)
+  if (!m_socket->IsOpen())
   {
-    XBMC->Log(LOG_ERROR, "%s - failed to connect to the backend (%s)", __FUNCTION__, errbuf);
+    XBMC->Log(LOG_ERROR, "%s - failed to connect to the backend (%s)", __FUNCTION__, m_socket->GetError().c_str());
     return false;
   }
 
@@ -115,10 +114,11 @@ void CHTSPConnection::Close()
     return;
   m_bIsConnected = false;
 
-  if(m_fd != INVALID_SOCKET)
+  if(m_socket->IsOpen())
   {
-    tcp_close(m_fd);
-    m_fd = INVALID_SOCKET;
+    m_socket->Close();
+    delete m_socket;
+    m_socket = NULL;
   }
 
   if(m_challenge)
@@ -132,17 +132,16 @@ void CHTSPConnection::Close()
 void CHTSPConnection::Abort(void)
 {
   if (!m_bIsConnected)
-	  return;
+    return;
   m_bIsConnected = false;
 
-  tcp_shutdown(m_fd);
+  m_socket->Shutdown();
 }
 
-htsmsg_t* CHTSPConnection::ReadMessage(int timeout)
+htsmsg_t* CHTSPConnection::ReadMessage(int iInitialTimeout /* = 10000 */, int iDatapacketTimeout /* = 10000 */)
 {
   void*    buf;
   uint32_t l;
-  int      x;
 
   if(m_queue.size())
   {
@@ -151,33 +150,28 @@ htsmsg_t* CHTSPConnection::ReadMessage(int timeout)
     return m;
   }
 
-  if (!IsConnected() || m_fd == INVALID_SOCKET)
+  if (!IsConnected() || !m_socket->IsOpen())
   {
     XBMC->Log(LOG_ERROR, "%s - not connected", __FUNCTION__);
     return NULL;
   }
 
-  x = tcp_read_timeout(m_fd, &l, 4, timeout);
-  if(x == ETIMEDOUT)
-    return htsmsg_create_map();
-
-  if(x)
+  if (m_socket->Read(&l, 4, iInitialTimeout) != 4)
   {
-    XBMC->Log(LOG_ERROR, "%s - Failed to read packet size (%d)", __FUNCTION__, x);
-    Close();
-    return NULL;
+    if(m_socket->GetErrorNumber() != ETIMEDOUT && m_socket->GetErrorNumber() != 0)
+      XBMC->Log(LOG_ERROR, "%s - Failed to read packet size (%s)", __FUNCTION__, m_socket->GetError().c_str());
+    return htsmsg_create_map();
   }
 
-  l   = ntohl(l);
+  l = ntohl(l);
   if(l == 0)
     return htsmsg_create_map();
 
   buf = malloc(l);
 
-  x = tcp_read(m_fd, buf, l);
-  if(x)
+  if(m_socket->Read(buf, l, iDatapacketTimeout) != (ssize_t)l)
   {
-    XBMC->Log(LOG_ERROR, "%s - Failed to read packet (%d)", __FUNCTION__, x);
+    XBMC->Log(LOG_ERROR, "%s - Failed to read packet (%s)", __FUNCTION__, m_socket->GetError().c_str());
     free(buf);
     Close();
     return NULL;
@@ -198,8 +192,9 @@ bool CHTSPConnection::SendMessage(htsmsg_t* m)
   }
   htsmsg_destroy(m);
 
-  if(tcp_send(m_fd, (char*)buf, len, 0) < 0)
+  if (m_socket->Write(buf, len) != (ssize_t)len)
   {
+    XBMC->Log(LOG_ERROR, "%s - Failed to write packet (%s)", __FUNCTION__, m_socket->GetError().c_str());
     free(buf);
     Close();
     return false;
