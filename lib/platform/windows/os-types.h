685bbc20008f38701a1a5afcce9bd8cfacf4c975
Marcel Groothuis <margro.xbmc@gmail.com>
2012-02-07 20:35:38 +0100

    platform: remove "signed" to prevent redefinition errors in combination with stdint.h

--- lib/platform/windows/os-types.h
+++ lib/platform/windows/os-types.h
@@ -55,10 +55,10 @@ typedef SOCKET tcp_socket_t;
 typedef HANDLE serial_socket_t;
 #define INVALID_SERIAL_SOCKET_VALUE INVALID_HANDLE_VALUE
 
-typedef signed __int8    int8_t;
-typedef signed __int16   int16_t;
-typedef signed __int32   int32_t;
-typedef signed __int64   int64_t;
+typedef __int8    int8_t;
+typedef __int16   int16_t;
+typedef __int32   int32_t;
+typedef __int64   int64_t;
 typedef unsigned __int8  uint8_t;
 typedef unsigned __int16 uint16_t;
 typedef unsigned __int32 uint32_t;
