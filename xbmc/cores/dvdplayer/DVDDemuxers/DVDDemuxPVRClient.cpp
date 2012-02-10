515e1a62fc24136e071dbb589b609e2191153e31
FernetMenta <fernetmenta@online.de>
2012-02-10 20:43:59 +0100

    pvr: close stream if connection to backend terminates

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxPVRClient.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxPVRClient.cpp
@@ -134,7 +134,11 @@ DemuxPacket* CDVDDemuxPVRClient::Read()
 
   DemuxPacket* pPacket = g_PVRClients->ReadDemuxStream();
   if (!pPacket)
-    return CDVDDemuxUtils::AllocateDemuxPacket(0);
+  {
+    if (m_pInput)
+      m_pInput->Close();
+    return NULL;
+  }
 
   if (pPacket->iStreamId == DMX_SPECIALID_STREAMINFO)
   {
