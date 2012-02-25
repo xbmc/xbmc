998687de644fa25dcc27cf2e6e1a5522e09224f6
theuni <theuni-nospam-@xbmc.org>
2012-02-25 16:49:46 -0500

    release: version strings bump for rc1

--- tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
+++ tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
@@ -46,7 +46,7 @@ fi
 PACKAGE=org.xbmc.xbmc-ios
 
 VERSION=11.0                                                                                                                                                                                                    
-REVISION=0~b3
+REVISION=0~rc1
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
