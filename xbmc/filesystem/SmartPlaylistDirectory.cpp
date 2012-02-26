858c84ad8aee0c019127d80fbdbd2bc65b73ffe0
Jonathan Marshall <jmarshall@never.you.mind>
2012-02-26 20:11:47 +1300

    fixed: CSmartPlaylistDirectory::GetPlaylistByName could return non-xsp files

--- xbmc/filesystem/SmartPlaylistDirectory.cpp
+++ xbmc/filesystem/SmartPlaylistDirectory.cpp
@@ -137,17 +137,19 @@ namespace XFILE
     CFileItemList list;
     bool filesExist = false;
     if (playlistType == "songs" || playlistType == "albums")
-      filesExist = CDirectory::GetDirectory("special://musicplaylists/", list, "*.xsp");
+      filesExist = CDirectory::GetDirectory("special://musicplaylists/", list, ".xsp", false);
     else // all others are video
-      filesExist = CDirectory::GetDirectory("special://videoplaylists/", list, "*.xsp");
+      filesExist = CDirectory::GetDirectory("special://videoplaylists/", list, ".xsp", false);
     if (filesExist)
     {
       for (int i = 0; i < list.Size(); i++)
       {
         CFileItemPtr item = list[i];
-        if (item->GetLabel().CompareNoCase(name) == 0)
-        { // found :)
-          return item->GetPath();
+        CSmartPlaylist playlist;
+        if (playlist.OpenAndReadName(item->GetPath()))
+        {
+          if (playlist.GetName().CompareNoCase(name) == 0)
+            return item->GetPath();
         }
       }
     }
