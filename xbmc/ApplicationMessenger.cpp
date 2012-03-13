48749dea8a1626f87399d7633d9a6e525a396107
Jonathan Marshall <jmarshall@never.you.mind>
2012-03-14 11:28:04 +1300

    ability to send guimessages via the application messenger

--- xbmc/ApplicationMessenger.cpp
+++ xbmc/ApplicationMessenger.cpp
@@ -707,6 +707,17 @@ case TMSG_POWERDOWN:
       }
       break;
 
+    case TMSG_GUI_MESSAGE:
+      {
+        if (pMsg->lpVoid)
+        {
+          CGUIMessage *message = (CGUIMessage *)pMsg->lpVoid;
+          g_windowManager.SendMessage(*message, pMsg->dwParam1);
+          delete message;
+        }
+      }
+      break;
+
     case TMSG_GUI_INFOLABEL:
       {
         if (pMsg->lpVoid)
@@ -1136,6 +1147,14 @@ void CApplicationMessenger::SendAction(const CAction &action, int windowID, bool
   ThreadMessage tMsg = {TMSG_GUI_ACTION};
   tMsg.dwParam1 = windowID;
   tMsg.lpVoid = new CAction(action);
+  SendMessage(tMsg, waitResult);
+}
+
+void CApplicationMessenger::SendGUIMessage(const CGUIMessage &message, int windowID, bool waitResult)
+{
+  ThreadMessage tMsg = {TMSG_GUI_MESSAGE};
+  tMsg.dwParam1 = windowID == WINDOW_INVALID ? 0 : windowID;
+  tMsg.lpVoid = new CGUIMessage(message);
   SendMessage(tMsg, waitResult);
 }
 
