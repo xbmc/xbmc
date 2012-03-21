04d4066d2f6ea573aa365a030816329b0006253b
theuni <theuni-nospam-@xbmc.org>
2012-03-21 00:33:54 -0400

    release: bump version strings for final release

--- xbmc/GUIInfoManager.h
+++ xbmc/GUIInfoManager.h
@@ -425,7 +425,7 @@ namespace INFO
 
 #define VERSION_MAJOR 11
 #define VERSION_MINOR 0
-#define VERSION_TAG "-RC2"
+#define VERSION_TAG ""
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
