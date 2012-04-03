3343e3a12ccc3a40dd7fa6696b6fb207663068af
Jonathan Marshall <jmarshall@never.you.mind>
2012-04-03 20:42:14 +1200

    SetClipRegion/RestoreClipRegion must be matched as per the doxy

--- xbmc/guilib/GUIPanelContainer.cpp
+++ xbmc/guilib/GUIPanelContainer.cpp
@@ -108,7 +108,8 @@ void CGUIPanelContainer::Render()
   // Free memory not used on screen at the moment, do this first so there's more memory for the new items.
   FreeMemory(CorrectOffset(offset - cacheBefore, 0), CorrectOffset(offset + cacheAfter + m_itemsPerPage + 1, 0));
 
-  g_graphicsContext.SetClipRegion(m_posX, m_posY, m_width, m_height);
+  if (g_graphicsContext.SetClipRegion(m_posX, m_posY, m_width, m_height))
+  {
   CPoint origin = CPoint(m_posX, m_posY) + m_renderOffset;
   float pos = (m_orientation == VERTICAL) ? origin.y : origin.x;
   float end = (m_orientation == VERTICAL) ? m_posY + m_height : m_posX + m_width;
@@ -163,7 +164,7 @@ void CGUIPanelContainer::Render()
   }
 
   g_graphicsContext.RestoreClipRegion();
-
+  }
   CGUIControl::Render();
 }
 
