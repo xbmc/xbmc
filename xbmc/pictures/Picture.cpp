xbmc/xbmc

commit c3d33baecff239c0adc1e121bb1d0b3597688c3f

add CPicture::CreateTiledThumb for faster creation of picture folder thumbs

xbmc/pictures/Picture.cpp
@@ -29,6 +29,7 @@
 #include "utils/log.h"
 #include "utils/URIUtils.h"
 #include "DllSwScale.h"
+#include "guilib/Texture.h"
 
 using namespace XFILE;
 
@@ -163,6 +164,60 @@ bool CThumbnailWriter::DoWork()
   delete [] m_buffer;
 
   return success;
+}
+
+bool CPicture::CreateTiledThumb(const std::vector<std::string> &files, const std::string &thumb)
+{
+  if (!files.size())
+    return false;
+
+  unsigned int num_across = (unsigned int)ceil(sqrt(files.size()));
+  unsigned int num_down = (files.size() + num_across - 1) / num_across;
+
+  unsigned int tile_width = g_advancedSettings.m_thumbSize / num_across;
+  unsigned int tile_height = g_advancedSettings.m_thumbSize / num_down;
+  unsigned int tile_gap = std::max(1,g_advancedSettings.m_thumbSize / 512);
+
+  // create a buffer for the resulting thumb
+  uint32_t *buffer = (uint32_t *)calloc(g_advancedSettings.m_thumbSize * g_advancedSettings.m_thumbSize, 4);
+  for (unsigned int i = 0; i < files.size(); ++i)
+  {
+    int x = i % num_across;
+    int y = i / num_across;
+    // load in the image
+    CTexture texture;
+    unsigned int width = tile_width - 2*tile_gap, height = tile_height - 2*tile_gap;
+    if (texture.LoadFromFile(files[i], width, height, g_guiSettings.GetBool("pictures.useexifrotation")) && texture.GetWidth() && texture.GetHeight())
+    {
+      GetScale(texture.GetWidth(), texture.GetHeight(), width, height);
+
+      // scale appropriately
+      uint32_t *scaled = new uint32_t[width * height];
+      if (ScaleImage(texture.GetPixels(), texture.GetWidth(), texture.GetHeight(), texture.GetPitch(),
+                     (uint8_t *)scaled, width, height, width * 4))
+      {
+        if (!texture.GetOrientation() || OrientateImage(scaled, width, height, texture.GetOrientation()))
+        {
+          // drop into the texture
+          unsigned int posX = x*tile_width + (tile_width - width)/2;
+          unsigned int posY = y*tile_height + (tile_height - height)/2;
+          uint32_t *dest = buffer + posX + posY*g_advancedSettings.m_thumbSize;
+          for (unsigned int y = 0; y < height; ++y)
+          {
+            memcpy(dest, scaled, width*4);
+            dest += g_advancedSettings.m_thumbSize;
+            scaled += width;
+          }
+        }
+      }
+      delete[] scaled;
+    }
+  }
+  // now save to a file
+  bool ret = CreateThumbnailFromSurface((uint8_t *)buffer, g_advancedSettings.m_thumbSize, g_advancedSettings.m_thumbSize,
+                                        g_advancedSettings.m_thumbSize * 4, thumb);
+  free(buffer);
+  return ret;
 }
 
 void CPicture::GetScale(unsigned int width, unsigned int height, unsigned int &out_width, unsigned int &out_height)

xbmc/pictures/Picture.h
@@ -34,6 +34,12 @@ public:
   static bool CacheThumb(const CStdString& sourceUrl, const CStdString& destFile);
   static bool CacheFanart(const CStdString& SourceUrl, const CStdString& destFile);
 
+  /*! \brief Create a tiled thumb of the given files
+   \param files the files to create the thumb from
+   \param thumb the filename of the thumb
+   */
+  static bool CreateTiledThumb(const std::vector<std::string> &files, const std::string &thumb);
+
 private:
   static bool CacheImage(const CStdString& sourceUrl, const CStdString& destFile, int width, int height);
   static void GetScale(unsigned int width, unsigned int height, unsigned int &out_width, unsigned int &out_height);

xbmc/pictures/PictureThumbLoader.cpp
@@ -223,16 +223,18 @@ void CPictureThumbLoader::ProcessFoldersAndArchives(CFileItem *pItem)
       else
       {
         // ok, now we've got the files to get the thumbs from, lets create it...
-        // we basically load the 4 thumbs, resample to 62x62 pixels, and add them
-        CStdString strFiles[4];
+        // we basically load the 4 images and combine them
+        vector<string> files;
         for (int thumb = 0; thumb < 4; thumb++)
-          strFiles[thumb] = CTextureCache::Get().CheckAndCacheImage(CTextureCache::GetWrappedThumbURL(items[thumb]->GetPath()), false);
+          files.push_back(items[thumb]->GetPath());
         CStdString thumb = CTextureCache::GetWrappedImageURL(pItem->GetPath(), "picturefolder");
         CStdString relativeCacheFile = CTextureCache::GetCacheFile(thumb);
-        CPicture::CreateFolderThumb(strFiles, CTextureCache::GetCachedPath(relativeCacheFile));
-        CTextureCache::Get().AddCachedTexture(thumb, relativeCacheFile, "");
-        db.SetTextureForPath(pItem->GetPath(), "thumb", thumb);
-        pItem->SetThumbnailImage(CTextureCache::GetCachedPath(relativeCacheFile));
+        if (CPicture::CreateTiledThumb(files, CTextureCache::GetCachedPath(relativeCacheFile)))
+        {
+          CTextureCache::Get().AddCachedTexture(thumb, relativeCacheFile, "");
+          db.SetTextureForPath(pItem->GetPath(), "thumb", thumb);
+          pItem->SetThumbnailImage(CTextureCache::GetCachedPath(relativeCacheFile));
+        }
       }
     }
     // refill in the icon to get it to update

commit bad7c177611a0fc36a29b268bf64838241e7be30

thumbs: fix crash introduced by c3d33bae.
We would crash when creating tiled thumbs because the pointer for delete[]
would be at the end of the buffer.

xbmc/pictures/Picture.cpp
@@ -240,11 +240,12 @@ bool CPicture::CreateTiledThumb(const std::vector<std::string> &files, const std
           unsigned int posX = x*tile_width + (tile_width - width)/2;
           unsigned int posY = y*tile_height + (tile_height - height)/2;
           uint32_t *dest = buffer + posX + posY*g_advancedSettings.m_thumbSize;
+          uint32_t *src = scaled;
           for (unsigned int y = 0; y < height; ++y)
           {
-            memcpy(dest, scaled, width*4);
+            memcpy(dest, src, width*4);
             dest += g_advancedSettings.m_thumbSize;
-            scaled += width;
+            src += width;
           }
         }
       }