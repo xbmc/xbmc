8d007da6cae1e998f762377c85ab749abc6f1e96
Lars Op den Kamp <lars@opdenkamp.eu>
2012-04-11 23:18:03 +0200

    pvr: fix CDVDInputStream::NextStream() override after the merge

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
@@ -248,9 +248,9 @@ bool CDVDInputStreamPVRManager::UpdateItem(CFileItem& item)
   return false;
 }
 
-bool CDVDInputStreamPVRManager::NextStream()
+CDVDInputStream::ENextStream CDVDInputStreamPVRManager::NextStream()
 {
-  if(!m_pFile) return false;
+  if(!m_pFile) return NEXTSTREAM_NONE;
 
   if (m_pOtherStream)
     return m_pOtherStream->NextStream();
@@ -259,10 +259,10 @@ bool CDVDInputStreamPVRManager::NextStream()
     if(m_pFile->SkipNext())
     {
       m_eof = false;
-      return true;
+      return NEXTSTREAM_OPEN;
     }
   }
-  return false;
+  return NEXTSTREAM_NONE;
 }
 
 bool CDVDInputStreamPVRManager::CanRecord()
