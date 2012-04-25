4f2f0c2d0fc352095d858fcaa414b500f0866746
Geoffrey McRae <gnif@xbmc.org>
2012-04-25 16:52:10 +1200

    [AE] remove SDL_mixer and HAS_SDL_AUDIO

--- xbmc/system.h
+++ xbmc/system.h
@@ -128,7 +128,6 @@
   #if defined(TARGET_DARWIN_OSX)
     #define HAS_GL
     #define HAS_SDL
-    #define HAS_SDL_AUDIO
     #define HAS_SDL_OPENGL
     #define HAS_SDL_WIN_EVENTS
   #endif
@@ -158,7 +157,6 @@
 #ifndef HAS_SDL_OPENGL
 #define HAS_SDL_OPENGL
 #endif
-#define HAS_SDL_AUDIO
 #define HAS_SDL_WIN_EVENTS
 #endif
 #define HAS_LINUX_NETWORK
