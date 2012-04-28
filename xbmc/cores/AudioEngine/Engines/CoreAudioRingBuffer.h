1f82f0d6473b94f7501255ef9475f1919f1bccaa
Jonathan Marshall <jmarshall@never.you.mind>
2012-04-28 12:48:53 +1200

    core: update copyright years (coreaudio)

--- xbmc/cores/AudioEngine/Engines/CoreAudioRingBuffer.h
+++ xbmc/cores/AudioEngine/Engines/CoreAudioRingBuffer.h
@@ -1,5 +1,5 @@
 /*
- *      Copyright (C) 2010 Team XBMC
+ *      Copyright (C) 2010-2012 Team XBMC
  *      http://www.xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
