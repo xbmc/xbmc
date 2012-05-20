6117327e8b0e03c3fa1fe1bf339c2aa1202fe82e
xbmc <fernetmenta@online.de>
2012-05-20 14:11:26 +0200

    X11: add SDL joystick until we have a better solution

--- xbmc/windowing/WinEventsX11.cpp
+++ xbmc/windowing/WinEventsX11.cpp
@@ -34,6 +34,10 @@
 #include "guilib/GUIWindowManager.h"
 #include "input/MouseStat.h"
 
+#ifdef HAS_SDL_JOYSTICK
+#include "input/SDLJoystick.h"
+#endif
+
 CWinEventsX11* CWinEventsX11::WinEvents = 0;
 
 static uint32_t SymMappingsX11[][2] =
@@ -545,6 +549,28 @@ bool CWinEventsX11::MessagePump()
   }// while
 
   ret |= ProcessKeyRepeat();
+
+#ifdef HAS_SDL_JOYSTICK
+  SDL_Event event;
+  while (SDL_PollEvent(&event))
+  {
+    switch(event.type)
+    {
+      case SDL_JOYBUTTONUP:
+      case SDL_JOYBUTTONDOWN:
+      case SDL_JOYAXISMOTION:
+      case SDL_JOYBALLMOTION:
+      case SDL_JOYHATMOTION:
+        g_Joystick.Update(event);
+        ret = true;
+        break;
+
+      default:
+        break;
+    }
+    memset(&event, 0, sizeof(SDL_Event));
+  }
+#endif
 
   return ret;
 }
