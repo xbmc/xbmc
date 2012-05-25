86c48b31b98571912acc193c046e834d17475823
amet <amet.nospam@gmail.com>
2012-05-25 19:10:08 +0400

    remove SubtitleFileSizeAndHash, not needed as xbmcvfs has read/seek function now

--- xbmc/utils/FileUtils.h
+++ xbmc/utils/FileUtils.h
@@ -7,5 +7,4 @@ public:
   static bool DeleteItem(const CFileItemPtr &item, bool force=false);
   static bool DeleteItem(const CStdString &strPath, bool force=false);
   static bool RenameFile(const CStdString &strFile);
-  static bool SubtitleFileSizeAndHash(const CStdString &path, CStdString &strSize, CStdString &strHash);
 };
