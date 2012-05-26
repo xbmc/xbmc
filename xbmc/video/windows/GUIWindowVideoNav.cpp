xbmc/xbmc

commit c7cf16cdb3391d6492548aa05751f400449a9952

no need for deleting the videodb cache when running the video thumb loader as info from it is not stored in there anyway

xbmc/video/windows/GUIWindowVideoNav.cpp
@@ -267,15 +267,6 @@ CStdString CGUIWindowVideoNav::GetQuickpathName(const CStdString& strPath) const
   }
 }
 
-void CGUIWindowVideoNav::OnItemLoaded(CFileItem* pItem)
-{
-  /* even though the background loader is running multiple threads and we could,
-     be acting on someone else's flag, we don't care who invalidates the cache
-     only that it is done.  We also don't care if it is done multiple times due
-     to a race between multiple threads here at the same time */
-  CUtil::DeleteVideoDatabaseDirectoryCache();
-}
-
 bool CGUIWindowVideoNav::GetDirectory(const CStdString &strDirectory, CFileItemList &items)
 {
   CFileItem directory(strDirectory, true);

xbmc/video/windows/GUIWindowVideoNav.h
@@ -48,7 +48,7 @@ protected:
    */
   void LoadVideoInfo(CFileItemList &items);
 
-  virtual void OnItemLoaded(CFileItem* pItem);
+  virtual void OnItemLoaded(CFileItem* pItem) {};
   void OnLinkMovieToTvShow(int itemnumber, bool bRemove);
   // override base class methods
   virtual bool GetDirectory(const CStdString &strDirectory, CFileItemList &items);

commit 88690caea1df91d8e424fdb5cd532371e543db12

make sure to delete the videodb cache when switching watched mode (broken since c7cf16cdb3391d6492548aa05751f400449a9952)

xbmc/video/windows/GUIWindowVideoNav.cpp
@@ -172,6 +172,9 @@ bool CGUIWindowVideoNav::OnMessage(CGUIMessage& message)
         g_settings.CycleWatchMode(m_vecItems->GetContent());
         g_settings.Save();
 
+        // We need to clear any cached listing because a new filter is being applied
+        CUtil::DeleteVideoDatabaseDirectoryCache();
+
         // TODO: Can we perhaps filter this directly?  Probably not for some of the more complicated views,
         //       but for those perhaps we can just display them all, and only filter when we get a list
         //       of actual videos?
@@ -195,6 +198,10 @@ bool CGUIWindowVideoNav::OnMessage(CGUIMessage& message)
         else
           g_settings.SetWatchMode(m_vecItems->GetContent(), VIDEO_SHOW_ALL);
         g_settings.Save();
+
+        // We need to clear any cached listing because a new filter is being applied
+        CUtil::DeleteVideoDatabaseDirectoryCache();
+
         // TODO: Can we perhaps filter this directly?  Probably not for some of the more complicated views,
         //       but for those perhaps we can just display them all, and only filter when we get a list
         //       of actual videos?