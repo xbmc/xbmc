8c1f246cf73fabdaad68f8496332855099bc7efc
arnova <nospam@void.org>
2012-05-29 08:32:08 +0200

    changed: Proper fix for 50b1b726140de2ba12a7cf12b0a60242a001a6f6 (thanks jm)

--- xbmc/ThumbLoader.cpp
+++ xbmc/ThumbLoader.cpp
@@ -204,11 +204,14 @@ bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
   // video db items normally have info in the database
   if (pItem->HasVideoInfoTag() && pItem->GetArt().empty())
   {
-    if (FillLibraryArt(pItem) &&
-        pItem->GetVideoInfoTag()->m_type != "movie" &&
-        pItem->GetVideoInfoTag()->m_type != "episode" &&
-        pItem->GetVideoInfoTag()->m_type != "tvshow" &&
-        pItem->GetVideoInfoTag()->m_type != "musicvideo")
+    FillLibraryArt(pItem);
+
+    if (pItem->GetVideoInfoTag()->m_type == "set"      ||
+        pItem->GetVideoInfoTag()->m_type == "actor"    ||
+        pItem->GetVideoInfoTag()->m_type == "artist"   ||
+        pItem->GetVideoInfoTag()->m_type == "director" ||
+        pItem->GetVideoInfoTag()->m_type == "writer"   ||
+        pItem->GetVideoInfoTag()->m_type == "season"   )
     {
       m_database->Close();
       return true; // nothing else to be done
