bd7bc1cfa09056b77de185dc56d12ad4d5de6df9
Jonathan Marshall <jmarshall@never.you.mind>
2012-06-02 15:31:07 +1200

    [cuesheet] fix cue sheet info tags being overridden by the AnnouncementManager

--- xbmc/interfaces/AnnouncementManager.cpp
+++ xbmc/interfaces/AnnouncementManager.cpp
@@ -159,7 +159,7 @@ void CAnnouncementManager::Announce(AnnouncementFlag flag, const char *sender, c
       if (musicdatabase.Open())
       {
         CSong song;
-        if (musicdatabase.GetSongByFileName(item->GetPath(), song))
+        if (musicdatabase.GetSongByFileName(item->GetPath(), song, item->m_lStartOffset))
         {
           item->GetMusicInfoTag()->SetSong(song);
           item->SetMusicThumb();
