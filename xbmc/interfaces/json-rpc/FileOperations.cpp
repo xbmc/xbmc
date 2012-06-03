e03955b30c80bcf83121b51444f2b8bf5c702388
montellese <montellese@xbmc.org>
2012-06-03 20:09:16 +0200

    jsonrpc: fix bad logic in CFileOperations::FillFileItem preventing valid files from being picked up (fixes #13080)

--- xbmc/interfaces/json-rpc/FileOperations.cpp
+++ xbmc/interfaces/json-rpc/FileOperations.cpp
@@ -234,17 +234,22 @@ bool CFileOperations::FillFileItem(const CFileItemPtr &originalItem, CFileItem &
     else if (media.Equals("music"))
       status = CAudioLibrary::FillFileItem(strFilename, item);
 
-    if (!status && originalItem->GetLabel().empty())
+    if (!status)
     {
       bool isDir = CDirectory::Exists(strFilename);
-      CStdString label = CUtil::GetTitleFromPath(strFilename, isDir);
-      if (!label.empty())
+      if (originalItem->GetLabel().empty())
       {
+        CStdString label = CUtil::GetTitleFromPath(strFilename, isDir);
+        if (label.empty())
+          return false;
+
         item = CFileItem(strFilename, isDir);
         item.SetLabel(label);
-
-        status = true;
       }
+      else
+        item = *originalItem.get();
+
+      status = true;
     }
   }
 
