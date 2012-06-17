537bec4a5e297cd5d2ebe83d50ae5ee4c0ab5b07
Andres Mejia <amejia004@gmail.com>
2012-06-16 22:35:40 -0400

    Enable the ao_set_metadata* functions if available from libshairport.

--- xbmc/network/AirTunesServer.cpp
+++ xbmc/network/AirTunesServer.cpp
@@ -389,6 +389,7 @@ int CAirTunesServer::AudioOutputFunctions::ao_close(ao_device *device)
   return 0;
 }
 
+#ifdef HAVE_AUDIOOUTPUT_AO_SET_METADATA_AUDIOOUTPUT_AO_SET_METADATA_COVERART
 void CAirTunesServer::AudioOutputFunctions::ao_set_metadata(const char *buffer, unsigned int size)
 {
   CAirTunesServer::SetMetadataFromBuffer(buffer, size);
@@ -398,6 +399,7 @@ void CAirTunesServer::AudioOutputFunctions::ao_set_metadata_coverart(const char 
 {
   CAirTunesServer::SetCoverArtFromBuffer(buffer, size);
 }
+#endif
 
 /* -- Device Setup/Playback/Teardown -- */
 int CAirTunesServer::AudioOutputFunctions::ao_append_option(ao_option **options, const char *key, const char *value)
@@ -667,8 +669,10 @@ bool CAirTunesServer::Initialize(const CStdString &password)
     ao.ao_append_option = AudioOutputFunctions::ao_append_option;
     ao.ao_free_options = AudioOutputFunctions::ao_free_options;
     ao.ao_get_option = AudioOutputFunctions::ao_get_option;
-    ao.ao_set_metadata = AudioOutputFunctions::ao_set_metadata;    
-    ao.ao_set_metadata_coverart = AudioOutputFunctions::ao_set_metadata_coverart;        
+#ifdef HAVE_AUDIOOUTPUT_AO_SET_METADATA_AUDIOOUTPUT_AO_SET_METADATA_COVERART
+    ao.ao_set_metadata = AudioOutputFunctions::ao_set_metadata;
+    ao.ao_set_metadata_coverart = AudioOutputFunctions::ao_set_metadata_coverart;
+#endif
     struct printfPtr funcPtr;
     funcPtr.extprintf = shairport_log;
 
