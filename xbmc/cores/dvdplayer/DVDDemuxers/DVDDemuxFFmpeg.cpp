e0e36e093a8569e0e1fc9ccf02591a4c01ae8a6f
Andres Mejia <amejia004@gmail.com>
2012-06-17 13:29:08 -0400

    Fix segfault from double free.

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -523,6 +523,7 @@ void CDVDDemuxFFmpeg::Dispose()
       if (m_ioContext->buffer)
         m_dllAvUtil.av_free(m_ioContext->buffer);
       m_dllAvUtil.av_free(m_ioContext);
+      m_ioContext = NULL;
     }
     else
       m_dllAvFormat.av_close_input_file(m_pFormatContext);
@@ -532,9 +533,9 @@ void CDVDDemuxFFmpeg::Dispose()
   {
     m_dllAvUtil.av_free(m_ioContext->buffer);
     m_dllAvUtil.av_free(m_ioContext);
+    m_ioContext = NULL;
   }
 
-  m_ioContext = NULL;
   m_pFormatContext = NULL;
   m_speed = DVD_PLAYSPEED_NORMAL;
 
