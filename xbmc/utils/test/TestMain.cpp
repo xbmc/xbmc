a391dfd83f91a4cf7e896d5c5f3cdf2a1478d265
Andres Mejia <amejia004@gmail.com>
2012-06-19 13:29:02 -0400

    [GSOC] Use google test for xbmc/utils/test test cases.

--- xbmc/utils/test/TestMain.cpp
+++ xbmc/utils/test/TestMain.cpp
@@ -19,6 +19,11 @@
  *
  */
 
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "ThreadingTest"
-#include <boost/test/unit_test.hpp>
+#include "gtest/gtest.h"
+
+using ::testing::InitGoogleTest;
+
+int main(int argc, char **argv) {
+  InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
