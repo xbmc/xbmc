0a51b459d5f953cf0ec853086d2a633e58973a4e
FernetMenta <fernetmenta@online.de>
2012-07-05 15:24:22 +0200

    X11: Add xbmc icon
    This does not take icon transparancy into account, so
    could look weird.

--- xbmc/windowing/X11/WinSystemX11.h
+++ xbmc/windowing/X11/WinSystemX11.h
@@ -86,6 +86,7 @@ protected:
   Display*     m_dpy;
 
   Cursor       m_invisibleCursor;
+  Pixmap       m_icon;
   bool         m_bWasFullScreenBeforeMinimize;
   bool         m_minimized;
   int          m_RREventBase;
