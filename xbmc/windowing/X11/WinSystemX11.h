c3465fb3ae45d82f60392cee24f1d65e089d0e41
FernetMenta <fernetmenta@online.de>
2012-07-05 12:00:26 +0200

    X11: deactivate screen saver on startup

--- xbmc/windowing/X11/WinSystemX11.h
+++ xbmc/windowing/X11/WinSystemX11.h
@@ -53,6 +53,7 @@ public:
   virtual void ShowOSMouse(bool show);
   virtual void ResetOSScreensaver();
   virtual bool EnableFrameLimiter();
+  virtual void EnableSystemScreenSaver(bool bEnable);
 
   virtual void NotifyAppActiveChange(bool bActivated);
   virtual void NotifyAppFocusChange(bool bGaining);
