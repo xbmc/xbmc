2e89e8f52ac06776a6d7481c109375390d442b83
Oldnemesis <oldnemesis@xbmc>
2012-07-06 23:44:34 -0700

    Fixed (hopefully) the Win32 build

--- lib/timidity/timidity/optcode.h
+++ lib/timidity/timidity/optcode.h
@@ -21,6 +21,8 @@
 #ifndef OPTCODE_H_INCLUDED
 #define OPTCODE_H_INCLUDED 1
 
+#include "sysdep.h"
+
 /* Generic version of imuldiv. */
 #define imuldiv8(a, b) \
     (int32)(((int64)(a) * (int64)(b)) >> 8)
