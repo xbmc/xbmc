3fab5fbe110225bd397d0bee64bc60e83d774d8a
Oldnemesis <oldnemesis@xbmc>
2012-07-08 22:56:34 -0700

    Reimplementation of Karaoke video background player with a generic player class

--- xbmc/music/karaoke/karaokewindowbackground.cpp
+++ xbmc/music/karaoke/karaokewindowbackground.cpp
@@ -28,11 +28,11 @@
 #include "GUIUserMessages.h"
 #include "guilib/GUIVisualisationControl.h"
 #include "guilib/GUIImage.h"
-#include "cores/dvdplayer/DVDPlayer.h"
 #include "threads/SingleLock.h"
 #include "utils/log.h"
 
 #include "karaokewindowbackground.h"
+#include "karaokevideobackground.h"
 
 
 #define CONTROL_ID_VIS           1
@@ -50,19 +50,13 @@ CKaraokeWindowBackground::CKaraokeWindowBackground()
 
   m_videoPlayer = 0;
   m_parentWindow = 0;
-  m_videoLastTime = 0;
-  m_playingDefaultVideo = false;
-  m_videoEnded = false;
 }
 
 
 CKaraokeWindowBackground::~CKaraokeWindowBackground()
 {
   if ( m_videoPlayer )
-  {
-     m_videoPlayer->CloseFile();
-     delete m_videoPlayer;
-  }
+    delete m_videoPlayer;
 }
 
 
@@ -95,7 +89,6 @@ void CKaraokeWindowBackground::Init(CGUIWindow * wnd)
   {
     CLog::Log( LOGDEBUG, "Karaoke default background is video %s", g_advancedSettings.m_karaokeDefaultBackgroundFilePath.c_str() );
     m_defaultMode = BACKGROUND_VIDEO;
-    m_path = g_advancedSettings.m_karaokeDefaultBackgroundFilePath;
   }
 }
 
@@ -141,31 +134,12 @@ bool CKaraokeWindowBackground::OnMessage(CGUIMessage & message)
 
 void CKaraokeWindowBackground::Render()
 {
-  // We cannot use CSingleLock on m_CritSectionVideoEnded, because OnPlayBackEnded() might be
-  // called from a different thread, and since we cannot proceed on m_CritSectionShared, we'll deadlock
-  bool ended;
-  {
-    CSingleLock lock( m_CritSectionVideoEnded );
-    ended = m_videoEnded;
-  }
-
   CSingleLock lock (m_CritSectionShared);
-  // Do we need to restart video?
-  if ( ended )
-  {
-    NextVideo();
-    return; // VERY important! Player thread is locked now, and if we continue, we'll lock in g_renderManager.Present()
-  }
 
   // Proceed with video rendering
-  if ( m_currentMode == BACKGROUND_VIDEO )
+  if ( m_currentMode == BACKGROUND_VIDEO && m_videoPlayer )
   {
-#ifdef HAS_VIDEO_PLAYBACK
-    if ( g_application.IsPresentFrame() )
-      g_renderManager.Present();
-    else
-      g_renderManager.RenderUpdate(true, 0, 255);
-#endif
+    m_videoPlayer->Render();
   }
 
   // For other visualisations just disable the screen saver
@@ -206,44 +180,19 @@ void CKaraokeWindowBackground::StartImage( const CStdString& path )
 }
 
 
-void CKaraokeWindowBackground::StartVideo( const CStdString& path, int64_t offset)
+void CKaraokeWindowBackground::StartVideo( const CStdString& path )
 {
-  CFileItem item( path, false);
-  m_videoEnded = false;
-
-  // Video options
-  CPlayerOptions options;
-  options.video_only = true;
-
-  if ( offset > 0 )
-    options.starttime = (double) (offset / 1000.0);
-
-  if ( !item.IsVideo() )
-  {
-    CLog::Log(LOGERROR, "KaraokeVideoBackground: file %s is not a video file, ignoring", path.c_str() );
-    return;
-  }
-
-  if ( item.IsDVD() )
-  {
-    CLog::Log(LOGERROR, "KaraokeVideoBackground: DVD video playback is not supported");
-    return;
-  }
-
   if ( !m_videoPlayer )
-    m_videoPlayer = new CDVDPlayer(*this);
+	m_videoPlayer = new KaraokeVideoBackground();
 
-  if ( !m_videoPlayer )
-    return;
-
-  if ( !m_videoPlayer->OpenFile( item, options ) )
+  if ( !m_videoPlayer->Start( path ) )
   {
-    CLog::Log(LOGERROR, "KaraokeVideoBackground: error opening video file %s", item.GetPath().c_str());
+    delete m_videoPlayer;
+    m_videoPlayer = 0;
+    m_currentMode = BACKGROUND_NONE;
     return;
   }
-
-  CLog::Log(LOGDEBUG, "KaraokeVideoBackground: video file %s opened successfully", item.GetPath().c_str());
-
+  
   m_ImgControl->SetVisible( false );
   m_VisControl->SetVisible( false );
   m_currentMode = BACKGROUND_VIDEO;
@@ -267,10 +216,7 @@ void CKaraokeWindowBackground::StartDefault()
       break;
 
     case BACKGROUND_VIDEO:
-      StartVideo( m_path, m_videoLastTime );
-
-      if ( m_currentMode == BACKGROUND_VIDEO )
-        m_playingDefaultVideo = true;
+      StartVideo();
       break;
 
     default:
@@ -285,16 +231,8 @@ void CKaraokeWindowBackground::Stop()
   CSingleLock lock (m_CritSectionShared);
   m_currentMode = BACKGROUND_NONE;
 
-  // Disable and hide all control
   if ( m_videoPlayer )
-  {
-     if ( m_playingDefaultVideo )
-       m_videoLastTime = m_videoPlayer->GetTime();
-
-     m_videoPlayer->CloseFile();
-     delete m_videoPlayer;
-     m_videoPlayer = 0;
-  }
+    m_videoPlayer->Stop();
 
   CLog::Log( LOGDEBUG, "Karaoke background stopped" );
 }
@@ -302,29 +240,15 @@ void CKaraokeWindowBackground::Stop()
 
 void CKaraokeWindowBackground::OnPlayBackEnded()
 {
-  CSingleLock lock ( m_CritSectionVideoEnded );
-  m_videoEnded = true;
-/*  CSingleLock lock (m_CritSectionShared);
-
-  CLog::Log( LOGDEBUG, "KaraokeVideoBackground: stopping" );
-  Stop();
-  CLog::Log( LOGDEBUG, "KaraokeVideoBackground: stopped" );
-  m_videoLastTime = 0;
-  StartVideo( m_path, m_videoLastTime );
-  CLog::Log( LOGDEBUG, "KaraokeVideoBackground: restarting video from the beginning" );
-*/
 }
-
 
 void CKaraokeWindowBackground::OnPlayBackStarted()
 {
 }
 
-
 void CKaraokeWindowBackground::OnPlayBackStopped()
 {
 }
-
 
 void CKaraokeWindowBackground::OnQueueNextItem()
 {
@@ -332,27 +256,4 @@ void CKaraokeWindowBackground::OnQueueNextItem()
 
 void CKaraokeWindowBackground::Pause(bool now_paused)
 {
-  if ( m_currentMode == BACKGROUND_VIDEO && m_videoPlayer )
-  {
-    if ( (now_paused && !m_videoPlayer->IsPaused())
-    || ( !now_paused && m_videoPlayer->IsPaused() ) )
-      m_videoPlayer->Pause();
-  }
-}
-
-void CKaraokeWindowBackground::NextVideo()
-{
-  // This function should not be called directly from the callback! Deadlock!!!
-  m_videoPlayer->CloseFile();
-
-  // Only one video selected, restarting
-  m_videoLastTime = 0;
-
-  {
-    CSingleLock lock(m_CritSectionVideoEnded );
-    m_videoEnded = false;
-  }
-
-  StartVideo( m_path, m_videoLastTime );
-  CLog::Log( LOGDEBUG, "KaraokeVideoBackground: restarting video from the beginning" );
 }
