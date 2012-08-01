dea557c2430296227c08deb59779f23c84369225
montellese <montellese@xbmc.org>
2012-08-01 10:16:29 +0200

    release: bump versions for August merge window

--- xbmc/GUIInfoManager.h
+++ xbmc/GUIInfoManager.h
@@ -425,7 +425,7 @@ namespace INFO
 
 #define VERSION_MAJOR 12
 #define VERSION_MINOR 0
-#define VERSION_TAG "-ALPHA4"
+#define VERSION_TAG "-ALPHA5"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
