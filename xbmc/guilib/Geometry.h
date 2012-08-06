2a7e9e90f39391a17f4c4af6a3398ead1612ffe2
davilla <davilla@4pi.com>
2012-08-06 02:16:37 -0400

    add subtraction to CRect for determining bounds overlap coverage, thx to Memphiz for figuring this out

--- xbmc/guilib/Geometry.h
+++ xbmc/guilib/Geometry.h
@@ -27,6 +27,8 @@
 #else
 #define XBMC_FORCE_INLINE
 #endif
+
+#include <vector>
 #include <algorithm>
 
 class CPoint
@@ -153,6 +155,192 @@ public:
   {
     return Width() * Height();
   };
+
+  std::vector<CRect> SubtractRect(CRect splitterRect)
+  {
+    #define ADD_RECT_ABOVE_INTERSECTION(add) \
+    { \
+      add = CRect(x1, y1, x2, intersection.y1);\
+      newRectaglesList.push_back(add);\
+    }
+    #define ADD_RECT_BELOW_INTERSECTION(add) \
+    {\
+      add = CRect(x1, intersection.y2, x2, y2);\
+      newRectaglesList.push_back(add);\
+    }
+    #define ADD_RECT_LEFT_OF_INTERSECTION(add) \
+    {\
+      add = CRect(x1, intersection.y1, intersection.x1, intersection.y2);\
+      newRectaglesList.push_back(add);\
+    }
+    #define ADD_RECT_RIGHT_OF_INTERSECTION(add) \
+    {\
+      add = CRect(intersection.x2, intersection.y1, x2, intersection.y2);\
+      newRectaglesList.push_back(add);\
+    }
+
+    std::vector<CRect> newRectaglesList;
+    CRect intersection = splitterRect.Intersect(*this);
+
+    if (!intersection.IsEmpty())
+    {
+      CRect add;
+      // intersection rect upper edge
+      if (intersection.y1 == y1)          // y starts with baserect
+      {
+        // intersection rect lower edge
+        if (intersection.y2 == y2)        // y goes down to baserect
+        {
+          if (intersection.x1 == x1)      // x starts with baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { // result 100% covered
+              // add nothing
+            }
+            else // x ends in baserect
+            { // results in 1 rect
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+          else                            // x starts in baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { // results in 1 rect
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { //results in 2 rects
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+        }
+        else // y ends in baserect
+        {
+          if (intersection.x1 == x1)      // x starts with baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { //results in 1 rect, below intersection
+              ADD_RECT_BELOW_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { // results in 2 rects
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+          else // x starts in baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { //results in 2 rects
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { //results in 3 rects
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+        }
+      }
+      else // y starts in baserect
+      {
+        // intersection rect lower edge
+        if (intersection.y2 == y2)        // y goes down to baserect
+        {
+          if (intersection.x1 == x1)      // xstarts with baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { //results in 1 rect above intersection
+              ADD_RECT_ABOVE_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { // results in 2 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+          else                            // x starts in baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { // results in 2 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { // results in 3 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+        }
+        else                              // y ends in baserect
+        {
+          if (intersection.x1 == x1)      // x starts with baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { // results in 2 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_BELOW_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { // results in 3 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+          else                            // x starts in baserect
+          {
+            if (intersection.x2 == x2)    // x ends with baserect
+            { // results in 3 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+            }
+            else                          // x ends in baserect
+            { // results in 4 rects
+              ADD_RECT_ABOVE_INTERSECTION(add);
+              ADD_RECT_BELOW_INTERSECTION(add);
+              ADD_RECT_LEFT_OF_INTERSECTION(add);
+              ADD_RECT_RIGHT_OF_INTERSECTION(add);
+            }
+          }
+        }
+      }
+    }
+    else
+    {
+      newRectaglesList.push_back(*this);
+    }
+
+    return newRectaglesList;
+  }
+
+  std::vector<CRect> SubtractRects(std::vector<CRect> intersectionList)
+  {
+    std::vector<CRect> fragmentsList;
+    fragmentsList.push_back(*this);
+
+    for (std::vector<CRect>::iterator splitter = intersectionList.begin(); splitter != intersectionList.end(); ++splitter)
+    {
+      std::vector<CRect> toAddList;
+
+      for (std::vector<CRect>::iterator fragment = fragmentsList.begin(); fragment != fragmentsList.end(); ++fragment)
+      {
+        std::vector<CRect> newFragmentsList = fragment->SubtractRect(*splitter);
+        toAddList.insert(toAddList.end(), newFragmentsList.begin(), newFragmentsList.end());
+      }
+
+      fragmentsList.clear();
+      fragmentsList.insert(fragmentsList.end(), toAddList.begin(), toAddList.end());
+    }
+
+    return fragmentsList;
+  }
 
   bool operator !=(const CRect &rect) const
   {
