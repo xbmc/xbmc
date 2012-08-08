xbmc/android

[droid] Add guards and include fixes

xbmc/cores/DllLoader/exports/util/EmuFileWrapper.h
@@ -28,7 +28,7 @@
 #include "system.h"
 #include "threads/CriticalSection.h"
 
-#if defined(_LINUX) && !defined(__APPLE__) && !defined(__FreeBSD__)
+#if defined(_LINUX) && !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__ANDROID__)
 #define _file _fileno
 #endif
 

xbmc/cores/DllLoader/exports/wrapper.c
@@ -26,7 +26,6 @@
 //
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/statvfs.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -37,7 +36,11 @@
 #include <dirent.h>
 #endif
 
-#if defined(__APPLE__) || defined(__FreeBSD__)
+#if !defined(__ANDROID__)
+#include <sys/statvfs.h>
+#endif
+
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__ANDROID__)
 typedef int64_t   off64_t;
 typedef off_t     __off_t;
 typedef off64_t   __off64_t;

xbmc/guilib/XBTFReader.cpp
@@ -189,7 +189,7 @@ bool CXBTFReader::Load(const CXBTFFrame& frame, unsigned char* buffer)
   {
     return false;
   }
-#if defined(__APPLE__) || defined(__FreeBSD__)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__ANDROID__)
     if (fseeko(m_file, (off_t)frame.GetOffset(), SEEK_SET) == -1)
 #else
     if (fseeko64(m_file, (off_t)frame.GetOffset(), SEEK_SET) == -1)

xbmc/xbmc

xbmc/cores/DllLoader/exports/util/EmuFileWrapper.h
@@ -28,7 +28,7 @@
 #include "system.h"
 #include "threads/CriticalSection.h"
 
-#if defined(_LINUX) && !defined(TARGET_DARWIN) && !defined(__FreeBSD__)
+#if defined(_LINUX) && !defined(TARGET_DARWIN) && !defined(__FreeBSD__) && !defined(__ANDROID__)
 #define _file _fileno
 #endif
 

xbmc/cores/DllLoader/exports/wrapper.c
@@ -26,7 +26,9 @@
 //
 #include <sys/types.h>
 #include <sys/stat.h>
+#if !defined(__ANDROID__)
 #include <sys/statvfs.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -37,7 +39,7 @@
 #include <dirent.h>
 #endif
 
-#if defined(TARGET_DARWIN) || defined(TARGET_FREEBSD)
+#if defined(TARGET_DARWIN) || defined(__FreeBSD__) || defined(__ANDROID__)
 typedef off_t     __off_t;
 typedef int64_t   off64_t;
 typedef off64_t   __off64_t;

xbmc/guilib/XBTFReader.cpp
@@ -189,7 +189,7 @@ bool CXBTFReader::Load(const CXBTFFrame& frame, unsigned char* buffer)
   {
     return false;
   }
-#if defined(TARGET_DARWIN) || defined(__FreeBSD__)
+#if defined(TARGET_DARWIN) || defined(__FreeBSD__) || defined(__ANDROID__)
     if (fseeko(m_file, (off_t)frame.GetOffset(), SEEK_SET) == -1)
 #else
     if (fseeko64(m_file, (off_t)frame.GetOffset(), SEEK_SET) == -1)