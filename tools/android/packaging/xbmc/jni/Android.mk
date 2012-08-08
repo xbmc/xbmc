xbmc/android

[droid] add packaging

tools/android/packaging/xbmc/jni/Android.mk
@@ -1,0 +1,3 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := xbmcapp

xbmc/xbmc

tools/android/packaging/xbmc/jni/Android.mk
@@ -1,0 +1,3 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := xbmc