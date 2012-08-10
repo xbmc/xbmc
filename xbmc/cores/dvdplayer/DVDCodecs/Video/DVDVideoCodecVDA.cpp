7d42c13e5fc6515f6122811eb0946bb7fc6f3d2c
davilla <davilla@4pi.com>
2012-08-10 08:39:42 -0700

    [osx] fixed frame display order stutter, VDA under ATI GPUs need to run synchronous

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVDA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVDA.cpp
@@ -954,6 +954,10 @@ int CDVDVideoCodecVDA::Decode(BYTE* pData, int iSize, double dts, double pts)
     }
   }
 
+  // force synchronous decode to fix issues with ATI GPUs,
+  // we still have to sort returned frames by pts to handle out-of-order demuxer packets. 
+  m_dll->VDADecoderFlush((VDADecoder)m_vda_decoder, kVDADecoderFlush_EmitFrames);
+
   if (m_queue_depth < m_max_ref_frames)
     return VC_BUFFER;
 
