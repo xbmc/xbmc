e2f5d9941ffb279b1e1d108fc7b24415277fe973
gimli <ebsi4711@gmail.com>
2012-08-10 15:36:18 +0200

    [rbp] added raspberrypi platform support files

---
+++ xbmc/linux/RBP.h
@@ -0,0 +1,60 @@
+#pragma once
+/*
+ *      Copyright (C) 2005-2011 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#ifndef USE_VCHIQ_ARM
+#define USE_VCHIQ_ARM
+#endif
+#ifndef __VIDEOCORE4__
+#define __VIDEOCORE4__
+#endif
+#ifndef HAVE_VMCS_CONFIG
+#define HAVE_VMCS_CONFIG
+#endif
+
+#if defined(HAVE_CONFIG_H) && !defined(TARGET_WINDOWS)
+#include "config.h"
+#define DECLARE_UNUSED(a,b) a __attribute__((unused)) b;
+#endif
+
+#if defined(TARGET_RASPBERRY_PI)
+#include "DllBCM.h"
+#include "cores/omxplayer/OMXCore.h"
+
+class CRBP
+{
+public:
+  CRBP();
+  ~CRBP();
+
+  bool Initialize();
+  void LogFirmwareVerison();
+  void Deinitialize();
+
+private:
+  DllBcmHost *m_DllBcmHost;
+  bool       m_initialized;
+  bool       m_omx_initialized;
+  COMXCore   *m_OMX;
+};
+
+extern CRBP g_RBP;
+#endif
