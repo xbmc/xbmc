a6ca7d4fadcdb479440a83299bb2bc5a97eafe91
Dominic Clifton <me@dominicclifton.name>
2012-08-27 21:04:26 +0100

    [ambipi] Adding calculation code for grid points and sample rectangles

--- xbmc/peripherals/devices/PeripheralAmbiPi.cpp
+++ xbmc/peripherals/devices/PeripheralAmbiPi.cpp
@@ -19,18 +19,40 @@
  *
  */
 
+//#include "system.h"
 #include "PeripheralAmbiPi.h"
 #include "utils/log.h"
 
+//#include "threads/SingleLock.h"
+
+#include "cores/VideoRenderers/RenderManager.h"
+#include "cores/VideoRenderers/WinRenderer.h"
+
 #define AMBIPI_DEFAULT_PORT 20434
+
+extern CXBMCRenderManager g_renderManager;
 
 using namespace PERIPHERALS;
 
 CPeripheralAmbiPi::CPeripheralAmbiPi(const PeripheralType type, const PeripheralBusType busType, const CStdString &strLocation, const CStdString &strDeviceName, int iVendorId, int iProductId) :
   CPeripheral(type, busType, strLocation, strDeviceName, iVendorId, iProductId),
-  m_bStarted(false)
-{
+  CThread("AmbiPi"),
+  m_bStarted(false),
+  m_bIsRunning(false),
+  m_pGrid(NULL),
+  m_previousImageWidth(0),
+  m_previousImageHeight(0)
+{  
   m_features.push_back(FEATURE_AMBIPI);
+}
+
+CPeripheralAmbiPi::~CPeripheralAmbiPi(void)
+{
+  CLog::Log(LOGDEBUG, "%s - Removing AmbiPi on %s", __FUNCTION__, m_strLocation.c_str());
+
+  StopThread(true);
+
+  delete m_pGrid;
 }
 
 bool CPeripheralAmbiPi::InitialiseFeature(const PeripheralFeature feature)
@@ -40,20 +62,16 @@ bool CPeripheralAmbiPi::InitialiseFeature(const PeripheralFeature feature)
   }
 
   CLog::Log(LOGDEBUG, "%s - Initialising AmbiPi on %s", __FUNCTION__, m_strLocation.c_str());
-
-  m_bStarted = true;
-
+  
   LoadAddressFromConfiguration();
+  UpdateGridFromConfiguration();
 
-  // TODO connect ambi (TCP)
   ConnectToDevice();
 
-  return CPeripheral::InitialiseFeature(feature);
-}
+  m_bStarted = true;
+  Create();
 
-void CPeripheralAmbiPi::ConnectToDevice()
-{
-  CLog::Log(LOGINFO, "%s - Connecting to AmbiPi on %s:%d", __FUNCTION__, m_address.c_str(), m_port);  
+  return CPeripheral::InitialiseFeature(feature);
 }
 
 void CPeripheralAmbiPi::LoadAddressFromConfiguration()
@@ -73,7 +91,236 @@ void CPeripheralAmbiPi::LoadAddressFromConfiguration()
   m_address = GetSettingString("address");
 }
 
-CPeripheralAmbiPi::~CPeripheralAmbiPi(void)
+void CPeripheralAmbiPi::UpdateGridFromConfiguration()
 {
-  CLog::Log(LOGDEBUG, "%s - Removing AmbiPi on %s", __FUNCTION__, m_strLocation.c_str());
+
+  unsigned int width = 16;
+  unsigned int height = 11;
+
+  if (m_pGrid)
+    delete m_pGrid;
+
+  m_pGrid = new CAmbiPiGrid(width, height);
+
 }
+
+bool CPeripheralAmbiPi::ConfigureRenderCallback()
+{
+  if (!g_renderManager.IsStarted() || !g_renderManager.IsConfigured()) {
+    return false;
+  }
+
+  g_renderManager.RegisterRenderUpdateCallBack((const void*)this, RenderUpdateCallBack);
+  return true;
+}
+
+void CPeripheralAmbiPi::RenderUpdateCallBack(const void *ctx, const CRect &SrcRect, const CRect &DestRect)
+{
+  CPeripheralAmbiPi *peripheralAmbiPi = (CPeripheralAmbiPi*)ctx;
+  //CLog::Log(LOGINFO, "%s - AmbiPi callback hit", __FUNCTION__);  
+
+  peripheralAmbiPi->ProcessImage();
+}
+
+void CPeripheralAmbiPi::ProcessImage()
+{
+  UpdateImage();  
+  GenerateDataStreamFromImage();
+}
+
+void CPeripheralAmbiPi::UpdateImage()
+{
+  CWinRenderer *pRenderer = g_renderManager.m_pRenderer;
+  
+  int index = pRenderer->GetImage(&m_image);
+}
+
+void CPeripheralAmbiPi::GenerateDataStreamFromImage()
+{
+  //CLog::Log(LOGDEBUG, "%s - Processing image", __FUNCTION__);  
+
+  UpdateSampleRectangles(m_image.width, m_image.height);
+}
+
+void CPeripheralAmbiPi::UpdateSampleRectangles(unsigned int imageWidth, unsigned int imageHeight)
+{
+  if (imageWidth == m_previousImageWidth || imageHeight == m_previousImageHeight)
+    return;
+
+  m_previousImageWidth = imageWidth;
+  m_previousImageHeight = imageHeight;
+
+  m_pGrid->UpdateSampleRectangles(imageWidth, imageHeight);
+}
+
+void CPeripheralAmbiPi::ConnectToDevice()
+{
+  CLog::Log(LOGINFO, "%s - Connecting to AmbiPi on %s:%d", __FUNCTION__, m_address.c_str(), m_port);  
+
+  // TODO connect ambi (TCP)
+}
+
+#define RETRY_DELAY_WHEN_UNCONFIGURED 1
+#define RETRY_DELAY_WHEN_CONFIGURED 5
+
+void CPeripheralAmbiPi::Process(void)
+{
+
+  {
+    CSingleLock lock(m_critSection);
+    m_bIsRunning = true;
+  }
+
+  int retryDelay = RETRY_DELAY_WHEN_UNCONFIGURED;
+
+  while (!m_bStop)
+  {
+    if (!m_bStop) {
+      bool configured = ConfigureRenderCallback();
+      
+      retryDelay = configured ? RETRY_DELAY_WHEN_CONFIGURED : RETRY_DELAY_WHEN_UNCONFIGURED;
+    }
+
+    if (!m_bStop)
+      Sleep(retryDelay);
+  }
+
+  {
+    CSingleLock lock(m_critSection);
+    m_bStarted = false;
+    m_bIsRunning = false;
+  }
+
+}
+
+
+CAmbiPiGrid::~CAmbiPiGrid()
+{
+  free(m_gridPoints);
+}
+
+CAmbiPiGrid::CAmbiPiGrid(unsigned int width, unsigned int height) :
+  m_width(width),
+  m_height(height)
+{  
+  
+  m_numGridPoints = (m_width * 2) + ((m_height - 2) * 2);
+  unsigned int allocSize = m_numGridPoints * sizeof(SGridPoint);
+  m_gridPoints = (SGridPoint*)malloc(allocSize);
+  ZeroMemory(m_gridPoints, allocSize);
+
+  UpdateGridPoints(width, height);
+  DumpCoordinates();
+}
+
+void CAmbiPiGrid::UpdateGridPoints(unsigned int width, unsigned int height)
+{
+  unsigned int start_position_x = width / 2;
+
+  unsigned int zeroBasedY;
+  unsigned int leftHalfZeroBasedX;
+  unsigned int rightHalfZeroBasedX;
+
+  unsigned int x;
+  unsigned int y;
+
+  unsigned int leftGridPointIndex = 0;
+  unsigned int rightGridPointIndex = m_numGridPoints / 2;
+
+  for (y = 1; y <= height; y++)
+  {
+    zeroBasedY = y - 1;
+    if (y == 1) 
+    {
+      // ProcessFromCenterToEdges
+
+      for (x = start_position_x; x > 0; x--)
+      {
+        leftHalfZeroBasedX = x - 1;
+        rightHalfZeroBasedX = width - x;
+        UpdateGridPoint(leftGridPointIndex++, leftHalfZeroBasedX, zeroBasedY);
+        UpdateGridPoint(rightGridPointIndex++, rightHalfZeroBasedX, zeroBasedY);
+      }
+    } 
+    else if (y == height)
+    {
+      // ProcessFromEdgesToCenter
+
+      for (x = 1; x <= start_position_x; x++)
+      {
+        leftHalfZeroBasedX = x - 1;
+        rightHalfZeroBasedX = width - x;
+        UpdateGridPoint(leftGridPointIndex++, leftHalfZeroBasedX, zeroBasedY);
+        UpdateGridPoint(rightGridPointIndex++, rightHalfZeroBasedX, zeroBasedY);
+      }
+    }
+    else
+    {
+      // ProcessEdgesOnly
+
+      leftHalfZeroBasedX = 0;
+      rightHalfZeroBasedX = width - 1;
+      UpdateGridPoint(leftGridPointIndex++, leftHalfZeroBasedX, y - 1);
+      UpdateGridPoint(rightGridPointIndex++, rightHalfZeroBasedX, y - 1);
+    }
+  }
+  CLog::Log(LOGDEBUG, "%s - current indexes, left: %d, right: %d", __FUNCTION__, leftGridPointIndex, rightGridPointIndex);  
+}
+
+void CAmbiPiGrid::UpdateGridPoint(unsigned int gridPointIndex, unsigned int x, unsigned int y)
+{
+  SGridPoint* pGridPoint = m_gridPoints + gridPointIndex;
+  pGridPoint->m_x = x;
+  pGridPoint->m_y = y;
+}
+
+void CAmbiPiGrid::DumpCoordinates(void)
+{
+  unsigned int gridPointIndex = 0;
+  SGridPoint* pGridPoint;
+
+  while (gridPointIndex < m_numGridPoints) {
+
+    pGridPoint = m_gridPoints + gridPointIndex;
+    CLog::Log(LOGDEBUG, "%s - Coordinates, index: %d, x: %d, y: %d", __FUNCTION__, gridPointIndex, pGridPoint->m_x, pGridPoint->m_y);  
+
+    gridPointIndex++;
+  }  
+}
+
+void CAmbiPiGrid::UpdateSampleRectangles(unsigned int imageWidth, unsigned int imageHeight)
+{
+
+  unsigned int gridPointIndex = 0;
+  SGridPoint* pGridPoint;
+
+  unsigned int sampleWidth = imageWidth / m_width;
+  unsigned int sampleHeight = imageHeight / m_height;
+
+  CLog::Log(LOGDEBUG, "%s - Updating grid sample rectangles for image, width: %d, height: %d, sampleWidth: %d, sampleHeight: %d", __FUNCTION__, imageWidth, imageHeight, sampleWidth, sampleHeight);  
+
+  while (gridPointIndex < m_numGridPoints) {
+
+    pGridPoint = m_gridPoints + gridPointIndex;
+
+    pGridPoint->sampleRect.SetRect(
+      pGridPoint->m_x * sampleWidth,
+      pGridPoint->m_y * sampleHeight,
+      std::min(imageWidth, (pGridPoint->m_x * sampleWidth) + sampleWidth),
+      std::min(imageHeight, (pGridPoint->m_y * sampleHeight) + sampleHeight)
+    );
+
+    CLog::Log(LOGDEBUG, "%s - updated rectangle, x: %d, y: %d, rect: (left: %f, top: %f, right: %f, bottom: %f)",
+      __FUNCTION__, 
+      pGridPoint->m_x,
+      pGridPoint->m_y,
+      pGridPoint->sampleRect.x1,
+      pGridPoint->sampleRect.y1,
+      pGridPoint->sampleRect.x2,
+      pGridPoint->sampleRect.y2
+    );  
+
+    gridPointIndex++;
+  }
+}
+
--- xbmc/peripherals/devices/PeripheralAmbiPi.h
+++ xbmc/peripherals/devices/PeripheralAmbiPi.h
@@ -20,24 +20,79 @@
  *
  */
 
+#include "system.h"
+#include "threads/Thread.h"
+#include "threads/CriticalSection.h"
+
 #include "Peripheral.h"
+#include "guilib/Geometry.h"
+
+#include "cores/VideoRenderers/BaseRenderer.h"
 
 namespace PERIPHERALS
 {
-  class CPeripheralAmbiPi : public CPeripheral
+  struct SGridPoint
+  {
+    unsigned int     m_x;
+    unsigned int     m_y;
+
+    CRect sampleRect;
+  };
+
+  class CAmbiPiGrid
+  {
+  public:
+    CAmbiPiGrid(unsigned int width, unsigned int height);
+    ~CAmbiPiGrid(void);
+    void UpdateSampleRectangles(unsigned int imageWidth, unsigned int imageHeight);
+
+  protected:
+    unsigned int m_width;
+    unsigned int m_height;
+    unsigned int m_numGridPoints;
+    SGridPoint* m_gridPoints;
+
+    void UpdateGridPoints(unsigned int width, unsigned int height);    
+
+  private:
+    void UpdateGridPoint(unsigned int leftGridPointIndex, unsigned int x, unsigned int y);
+    void DumpCoordinates(void);
+  };
+
+
+  class CPeripheralAmbiPi : public CPeripheral, private CThread
   {
   public:
     CPeripheralAmbiPi(const PeripheralType type, const PeripheralBusType busType, const CStdString &strLocation, const CStdString &strDeviceName, int iVendorId, int iProductId);
-    virtual CPeripheralAmbiPi::~CPeripheralAmbiPi(void);
+    virtual ~CPeripheralAmbiPi(void);
 
   protected:
 	  bool InitialiseFeature(const PeripheralFeature feature);
 
-    void CPeripheralAmbiPi::ConnectToDevice();
-    void CPeripheralAmbiPi::LoadAddressFromConfiguration();
+    void ConnectToDevice();
+    void LoadAddressFromConfiguration();
+    bool ConfigureRenderCallback();
+    void Process(void);
+
+    void ProcessImage();
+    void UpdateImage();
+    void GenerateDataStreamFromImage();
+    void UpdateGridFromConfiguration();
 
     bool                              m_bStarted;
+    bool                              m_bIsRunning;
     int                               m_port;
     CStdString                        m_address;
+
+  private:
+    CCriticalSection                  m_critSection;
+    YV12Image                         m_image;    
+    CAmbiPiGrid*                      m_pGrid;
+    unsigned int                      m_previousImageWidth;
+    unsigned int                      m_previousImageHeight;
+
+    static void RenderUpdateCallBack(const void *ctx, const CRect &SrcRect, const CRect &DestRect);
+
+    void UpdateSampleRectangles(unsigned int imageWidth, unsigned int imageHeight);
   };
 }
