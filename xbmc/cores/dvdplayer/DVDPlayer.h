bf185f8a22a1b54aa8ad3fc2f34abd1cde820238
ace20022 <ace20022@ymail.com>
2012-08-31 11:31:26 +0200

    Open demuxers for external audio files. Update the SelectionStream. Adapt all relevant places where m_pDemuxer is used; in particular ReadPacket(...) and add ReadExternalAudioPacket(...)

--- xbmc/cores/dvdplayer/DVDPlayer.h
+++ xbmc/cores/dvdplayer/DVDPlayer.h
@@ -157,6 +157,7 @@ public:
 
   void             Update  (SelectionStream& s);
   void             Update  (CDVDInputStream* input, CDVDDemux* demuxer);
+  void             UpdateExtAudio  (CDVDInputStream* input, CDVDDemux* ext_demuxer);
 };
 
 
@@ -329,7 +330,8 @@ protected:
   void UpdateTimestamps(CCurrentStream& current, DemuxPacket* pPacket);
   void SendPlayerMessage(CDVDMsg* pMsg, unsigned int target);
 
-  bool ReadPacket(DemuxPacket*& packet, CDemuxStream*& stream);
+  bool ReadPacket(DemuxPacket*& packet, CDemuxStream*& stream, bool onlyVideo);
+  bool ReadExternalAudioPacket(DemuxPacket*& packet, CDemuxStream*& stream);
   bool IsValidStream(CCurrentStream& stream);
   bool IsBetterStream(CCurrentStream& current, CDemuxStream* stream);
   bool CheckDelayedChannelEntry(void);
@@ -338,6 +340,7 @@ protected:
   bool OpenExternalAudioInputStreams();
   bool OpenDemuxStream();
   void OpenDefaultStreams(bool reset = true);
+  bool OpenExternalAudioDemuxStreams();
 
   void UpdateApplication(double timeout);
   void UpdatePlayState(double timeout);
@@ -385,6 +388,10 @@ protected:
 
   CStdString m_lastSub;
   std::vector<CDVDInputStream*> m_extInputStreams;
+  std::vector<CDVDDemux*> m_extDemuxer;
+  bool m_extAudio;
+  int m_extAudioOffset;
+  int m_extAudioId;
 
   struct SDVDInfo
   {
