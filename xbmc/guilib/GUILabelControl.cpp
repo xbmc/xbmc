ad9327a63b4a16c52c62098693e229a3d9f00446
Kyle Hill <kyle.hill@tacomafia.net>
2012-09-01 10:31:38 -0500

    Remove unused member m_autoWidth with GUILabelControl

--- xbmc/guilib/GUILabelControl.cpp
+++ xbmc/guilib/GUILabelControl.cpp
@@ -34,7 +34,6 @@ CGUILabelControl::CGUILabelControl(int parentID, int controlID, float posX, floa
   m_dwCounter = 0;
   ControlType = GUICONTROL_LABEL;
   m_startHighlight = m_endHighlight = 0;
-  m_autoWidth = false;
   m_minWidth = 0;
   if ((labelInfo.align & XBFONT_RIGHT) && m_width)
     m_posX -= m_width;
