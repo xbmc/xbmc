cd657c4356a3254f1559c75187e98af1a2fe31bd
pieh <misiek.piechowiak@gmail.com>
2012-09-02 20:04:26 +0200

    use CUtil::GetExternalStreamNameAndLangFromFilename to determine external subtitles's language and name

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -3594,8 +3594,8 @@ int CDVDPlayer::AddSubtitleFile(const std::string& filename, const std::string& 
   s.type     = STREAM_SUBTITLE;
   s.id       = 0;
   s.filename = filename;
-  s.name     = URIUtils::GetFileName(filename);
   s.flags    = flags;
+  CUtil::GetExternalStreamNameAndLangFromFilename(m_filename, filename, s.name, s.language);
   m_SelectionStreams.Update(s);
   return m_SelectionStreams.IndexOf(STREAM_SUBTITLE, s.source, s.id);
 }
