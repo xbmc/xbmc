f249e652379a063164ee1ccf421c30bd1d37d07f
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-09-05 15:29:40 +0200

    gles: fix YV12 rendering (FIXUP e6e434f).

--- system/shaders/yuv2rgb_basic_gles.glsl
+++ system/shaders/yuv2rgb_basic_gles.glsl
@@ -31,19 +31,7 @@ uniform mat4      m_yuvmat;
 
 void main()
 {
-#if defined(XBMC_YV12)
-
-  vec4 yuv, rgb;
-  yuv.rgba = vec4( texture2D(m_sampY, m_cordY).r
-                 , texture2D(m_sampU, m_cordU).g
-                 , texture2D(m_sampV, m_cordV).a
-                 , 1.0 );
-
-  rgb   = m_yuvmat * yuv;
-  rgb.a = m_alpha;
-  gl_FragColor = rgb;
-
-#elif defined(XBMC_Y_UV)
+#if defined(XBMC_Y_UV)
 
   vec4 yuv, rgb;
   yuv.rgba = vec4( texture2D(m_sampY, m_cordY).r
@@ -55,7 +43,7 @@ void main()
   rgb.a = m_alpha;
   gl_FragColor = rgb;
 
-#elif defined(XBMC_Y_U_V)
+#elif defined(XBMC_Y_U_V) || defined(XBMC_YV12)
 
   vec4 yuv, rgb;
   yuv.rgba = vec4( texture2D(m_sampY, m_cordY).r
