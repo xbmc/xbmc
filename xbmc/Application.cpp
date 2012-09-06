773e3cecbdc43311356250cfdc05a2a8b5f4a3ae
montellese <montellese@xbmc.org>
2012-09-06 11:45:35 +0200

    Revert "Fix memory leak in Application.cpp"
    This reverts commit dc3db93c6f7bdde96f61248830609a2421d046e8.
    The two additional deletes added in that commit were already there a few lines
    further up which caused a crash because of deleting an already deleted memory
    address.

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -478,8 +478,6 @@ CApplication::~CApplication(void)
 #endif
 
   delete m_dpms;
-  delete m_videoInfoScanner;
-  delete m_musicInfoScanner;
   delete m_seekHandler;
   delete m_pInertialScrollingHandler;
 }
