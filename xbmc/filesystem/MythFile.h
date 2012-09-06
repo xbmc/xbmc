b06dbeb0bcbfca137b3f07d12fb2619a2deb2575
Kyle Hill <kyle.hill@tacomafia.net>
2012-09-06 13:52:50 -0500

    Remove unused member from MythFile.h

--- xbmc/filesystem/MythFile.h
+++ xbmc/filesystem/MythFile.h
@@ -101,7 +101,6 @@ protected:
   cmyth_recorder_t  m_recorder;
   cmyth_proginfo_t  m_program;
   cmyth_file_t      m_file;
-  cmyth_timestamp_t m_starttime;
   CStdString        m_filename;
   CVideoInfoTag     m_infotag;
 
