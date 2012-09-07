3fc04bc4a273b9acf05fa8e6f6e4fc001fd1f8c8
Jonathan Marshall <jmarshall@never.you.mind>
2012-09-07 17:28:19 +1200

    cleanup: remove old advanced settings for recently added nodes - no longer needed now that it's customisable

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -6106,7 +6106,7 @@ bool CVideoDatabase::GetRecentlyAddedMoviesNav(const CStdString& strBaseDir, CFi
 {
   Filter filter;
   filter.order = "dateAdded desc, idMovie desc";
-  filter.limit = PrepareSQL("%u", limit ? limit : g_advancedSettings.m_iVideoLibraryRecentlyAddedItems);
+  filter.limit = PrepareSQL("%u", limit ? limit : 25);
   return GetMoviesByWhere(strBaseDir, filter, items);
 }
 
@@ -6114,7 +6114,7 @@ bool CVideoDatabase::GetRecentlyAddedEpisodesNav(const CStdString& strBaseDir, C
 {
   Filter filter;
   filter.order = "dateAdded desc, idEpisode desc";
-  filter.limit = PrepareSQL("%u", limit ? limit : g_advancedSettings.m_iVideoLibraryRecentlyAddedItems);
+  filter.limit = PrepareSQL("%u", limit ? limit : 25);
   return GetEpisodesByWhere(strBaseDir, filter, items, false);
 }
 
@@ -6122,7 +6122,7 @@ bool CVideoDatabase::GetRecentlyAddedMusicVideosNav(const CStdString& strBaseDir
 {
   Filter filter;
   filter.order = "dateAdded desc, idMVideo desc";
-  filter.limit = PrepareSQL("%u", limit ? limit : g_advancedSettings.m_iVideoLibraryRecentlyAddedItems);
+  filter.limit = PrepareSQL("%u", limit ? limit : 25);
   return GetMusicVideosByWhere(strBaseDir, filter, items);
 }
 
