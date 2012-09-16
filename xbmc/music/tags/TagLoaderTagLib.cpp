d23e851c963869c946f66e77de5721d2fcf5f24c
Kyle Hill <kyle.hill@tacomafia.net>
2012-09-16 12:18:02 -0500

    Fix use-after-free error in TagLoaderTagLib.cpp

--- xbmc/music/tags/TagLoaderTagLib.cpp
+++ xbmc/music/tags/TagLoaderTagLib.cpp
@@ -152,6 +152,7 @@ bool CTagLoaderTagLib::Load(const string& strFileName, CMusicInfoTag& tag, Embed
     if (!file || !file->isValid())
     {
       delete file;
+      oggFlacFile = NULL;
       file = oggVorbisFile = new Ogg::Vorbis::File(stream);
     }
   }
