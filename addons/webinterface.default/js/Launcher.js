736c3c01b3700061f76a03aaa8a9cf725245741a
asylumfunk <asylumfunk@asylumfunk.com>
2012-09-18 20:57:28 -0700

    cleanup: removed unnecessary abstraction - This system may have made sense for scriptaculous, -- but it's overkill here. - We want to import a list of javascript files. -- So we create an array and iterate across. --- No need to lookup DOM elements, ---- or parsing attribute hrefs w/ regexps - removed outdated comments -- Script is no longer based on scriptaculous.

--- addons/webinterface.default/js/Launcher.js
+++ addons/webinterface.default/js/Launcher.js
@@ -18,51 +18,26 @@
  *
  */
 
-/* This launcher is based on the scriptaculous.js launch script */
-
-// Copyright (c) 2005-2008 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-// For details, see the script.aculo.us web site: http://script.aculo.us/
-
 (function()
 {
+  var scripts =
+  [
+    "js/jquery-1.5.2.min.js",
+    "js/jquery.lazyload.js",
+    "js/iscroll-min.js",
+    "js/Core.js",
+    "js/MediaLibrary.js",
+    "js/NowPlayingManager.js"
+  ];
+  
   var DEBUG_MODE = false; /* Set to true to disable cached javascript */
   var VERSION = '1.0.0';
-  var JS_REGEX = /Launcher\.js(\?.*)?$/;
   var load_script = function(libraryName) {
     document.write('<script type="text/javascript" src="' + libraryName + '?' + (DEBUG_MODE ? Math.random() : VERSION) + '"><\/script>');
   };
 
-  var i, j, s, includes, load, path, scripts;
-  scripts = window.document.getElementsByTagName('script');
+  var i;
   for (i = 0; i < scripts.length; i += 1) {
-    s = scripts[i];
-    if (s.src.match(JS_REGEX)) {
-      path = s.src.replace(JS_REGEX, '');
-      load = s.src.match(/\?.*load=([a-zA-Z0-9.,\-]*)/);
-      includes = (load && load[1] || '').split(',');
-      for (j = 0; j < includes.length; j += 1) {
-        load_script(path + includes[j] + '.js');
-      }
-    }
+    load_script(scripts[i]);
   }
 })();
