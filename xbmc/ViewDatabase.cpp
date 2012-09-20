6c271d1f2bebfaee65a09c0306612f5774c92e42
Tobias Hieta <tobias@plexapp.com>
2012-09-20 23:01:30 +0200

    Plex removes in ViewDatabase

--- xbmc/ViewDatabase.cpp
+++ xbmc/ViewDatabase.cpp
@@ -79,6 +79,7 @@ bool CViewDatabase::UpdateOldVersion(int version)
 
 bool CViewDatabase::GetViewState(const CStdString &path, int window, CViewState &state, const CStdString &skin)
 {
+#ifndef __PLEX__
   try
   {
     if (NULL == m_pDB.get()) return false;
@@ -109,11 +110,13 @@ bool CViewDatabase::GetViewState(const CStdString &path, int window, CViewState 
   {
     CLog::Log(LOGERROR, "%s, failed on path '%s'", __FUNCTION__, path.c_str());
   }
+#endif
   return false;
 }
 
 bool CViewDatabase::SetViewState(const CStdString &path, int window, const CViewState &state, const CStdString &skin)
 {
+#ifndef __PLEX__
   try
   {
     if (NULL == m_pDB.get()) return false;
@@ -144,6 +147,8 @@ bool CViewDatabase::SetViewState(const CStdString &path, int window, const CView
     CLog::Log(LOGERROR, "%s failed on path '%s'", __FUNCTION__, path.c_str());
   }
   return true;
+#endif
+  return false;
 }
 
 bool CViewDatabase::ClearViewStates(int windowID)
