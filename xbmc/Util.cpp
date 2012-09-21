cb0a5ac8641c19c16e15f5c69bc95e34eaecef38
Tobias Hieta <tobias@plexapp.com>
2012-09-21 16:32:57 +0200

    I renamed to much, this broke the special protocol

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -519,21 +519,12 @@ void CUtil::GetHomePath(CStdString& strPath, const CStdString& strTarget)
       for (int n=strlen(given_path)-1; given_path[n] != '/'; n--)
         given_path[n] = '\0';
 
-#ifndef __PLEX__
       #if defined(__arm__)
         strcat(given_path, "/XBMCData/XBMCHome/");
       #else
         // Assume local path inside application bundle.
         strcat(given_path, "../Resources/XBMC/");
       #endif
-#else
-      #if defined(__arm__)
-        strcat(given_path, "/PlexData/PlexHome/");
-      #else
-      // Assume local path inside application bundle.
-        strcat(given_path, "../Resources/Plex/");
-      #endif
-#endif
 
       // Convert to real path.
       char real_path[2*MAXPATHLEN];
