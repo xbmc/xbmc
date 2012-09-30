465c6927594b239e8ae22687d5962669c849e72c
Lars Op den Kamp <lars@opdenkamp.eu>
2012-10-01 00:37:32 +0200

    [pvr] bump interface version number to 1.3.0

--- xbmc/addons/include/xbmc_pvr_types.h
+++ xbmc/addons/include/xbmc_pvr_types.h
@@ -72,10 +72,10 @@ struct DemuxPacket;
 #define PVR_STREAM_MAX_STREAMS 20
 
 /* current PVR API version */
-#define XBMC_PVR_API_VERSION "1.2.0"
+#define XBMC_PVR_API_VERSION "1.3.0"
 
 /* min. PVR API version */
-#define XBMC_PVR_MIN_API_VERSION "1.2.0"
+#define XBMC_PVR_MIN_API_VERSION "1.3.0"
 
 #ifdef __cplusplus
 extern "C" {
