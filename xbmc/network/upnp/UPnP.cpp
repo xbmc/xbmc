9777cc07e3b37370c8b8055725dedd13dfa0df5f
Alasdair Campbell <alcoheca@gmail.com>
2012-10-13 20:30:59 +0100

    [UPnP] crash with no configured network interfaces fixes #13402

--- xbmc/network/upnp/UPnP.cpp
+++ xbmc/network/upnp/UPnP.cpp
@@ -208,9 +208,11 @@ CUPnP::CUPnP() :
         m_IP = g_application.getNetwork().GetFirstConnectedInterface()->GetCurrentIPAddress().c_str();
     }
     NPT_List<NPT_IpAddress> list;
-    if (NPT_SUCCEEDED(PLT_UPnPMessageHelper::GetIPAddresses(list))) {
+    if (NPT_SUCCEEDED(PLT_UPnPMessageHelper::GetIPAddresses(list)) && list.GetItemCount()) {
         m_IP = (*(list.GetFirstItem())).ToString();
     }
+    else if(m_IP.IsEmpty())
+        m_IP = "localhost";
 
     // start upnp monitoring
     m_UPnP->Start();
