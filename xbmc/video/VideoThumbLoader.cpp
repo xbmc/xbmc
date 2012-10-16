xbmc/xbmc

commit 92858a007b1a208ce19e38f1d872fd17d8666601

use a list of allowed types rather than disallowed types of library items to filter out items that don't require further processing.

xbmc/video/VideoThumbLoader.cpp
@@ -202,12 +202,10 @@ bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
   {
     FillLibraryArt(*pItem);
 
-    if (pItem->GetVideoInfoTag()->m_type == "set"      ||
-        pItem->GetVideoInfoTag()->m_type == "actor"    ||
-        pItem->GetVideoInfoTag()->m_type == "artist"   ||
-        pItem->GetVideoInfoTag()->m_type == "director" ||
-        pItem->GetVideoInfoTag()->m_type == "writer"   ||
-        pItem->GetVideoInfoTag()->m_type == "season"   )
+    if (pItem->GetVideoInfoTag()->m_type != "movie"      &&
+        pItem->GetVideoInfoTag()->m_type != "tvshow"     &&
+        pItem->GetVideoInfoTag()->m_type != "episode"    &&
+        pItem->GetVideoInfoTag()->m_type != "musicvideo")
     {
       m_database->Close();
       return true; // nothing else to be done

commit 5fa2b863691b5205ca2b0fb63f9b94f3667ba7aa

fixed: Video art would no longer show for non-video db items (regression in 92858a007b1a208ce19e38f1d872fd17d8666601)

xbmc/video/VideoThumbLoader.cpp
@@ -202,10 +202,11 @@ bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
   {
     FillLibraryArt(*pItem);
 
-    if (pItem->GetVideoInfoTag()->m_type != "movie"      &&
-        pItem->GetVideoInfoTag()->m_type != "tvshow"     &&
-        pItem->GetVideoInfoTag()->m_type != "episode"    &&
-        pItem->GetVideoInfoTag()->m_type != "musicvideo")
+    if (!pItem->GetVideoInfoTag()->m_type.empty()         &&
+         pItem->GetVideoInfoTag()->m_type != "movie"      &&
+         pItem->GetVideoInfoTag()->m_type != "tvshow"     &&
+         pItem->GetVideoInfoTag()->m_type != "episode"    &&
+         pItem->GetVideoInfoTag()->m_type != "musicvideo")
     {
       m_database->Close();
       return true; // nothing else to be done
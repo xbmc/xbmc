5fa2b863691b5205ca2b0fb63f9b94f3667ba7aa
arnova <arnova@void.org>
2012-10-16 21:05:59 +0200

    fixed: Video art would no longer show for non-video db items (regression in 92858a007b1a208ce19e38f1d872fd17d8666601)

--- xbmc/video/VideoThumbLoader.cpp
+++ xbmc/video/VideoThumbLoader.cpp
@@ -202,10 +202,11 @@ bool CVideoThumbLoader::LoadItem(CFileItem* pItem)
   {
     FillLibraryArt(*pItem);
 
-    if (pItem->GetVideoInfoTag()->m_type != "movie"      &&
-        pItem->GetVideoInfoTag()->m_type != "tvshow"     &&
-        pItem->GetVideoInfoTag()->m_type != "episode"    &&
-        pItem->GetVideoInfoTag()->m_type != "musicvideo")
+    if (!pItem->GetVideoInfoTag()->m_type.empty()         &&
+         pItem->GetVideoInfoTag()->m_type != "movie"      &&
+         pItem->GetVideoInfoTag()->m_type != "tvshow"     &&
+         pItem->GetVideoInfoTag()->m_type != "episode"    &&
+         pItem->GetVideoInfoTag()->m_type != "musicvideo")
     {
       m_database->Close();
       return true; // nothing else to be done
