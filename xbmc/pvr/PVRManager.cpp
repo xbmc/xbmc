a8ac7bdbfc65cbb4fe6973ca6fc553227c072379
Lars Op den Kamp <lars@opdenkamp.eu>
2012-10-19 03:01:33 +0200

    [pvr/epg] fixed crash when resetting the EPG db

--- xbmc/pvr/PVRManager.cpp
+++ xbmc/pvr/PVRManager.cpp
@@ -556,17 +556,9 @@ void CPVRManager::ResetEPG(void)
 {
   CLog::Log(LOGNOTICE,"PVRManager - %s - clearing the EPG database", __FUNCTION__);
 
-  StopUpdateThreads();
-  g_EpgContainer.Stop();
+  Stop();
   g_EpgContainer.Reset();
-
-  if (g_guiSettings.GetBool("pvrmanager.enabled"))
-  {
-    static_cast<CPVRChannelGroupInternal *>(m_channelGroups->GetGroupAllTV().get())->CreateChannelEpgs(true);
-    static_cast<CPVRChannelGroupInternal *>(m_channelGroups->GetGroupAllRadio().get())->CreateChannelEpgs(true);
-    g_EpgContainer.Start();
-    StartUpdateThreads();
-  }
+  Start();
 }
 
 bool CPVRManager::IsPlaying(void) const
