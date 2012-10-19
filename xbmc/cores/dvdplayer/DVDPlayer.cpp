c6b3724a3f3690a392f0974e9ff043684632b4b0
Joakim Plate <elupus@ecce.se>
2012-10-19 21:50:10 +0200

    dvdplayer: fixed missing teletext if it shows up after open

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -916,9 +916,6 @@ bool CDVDPlayer::IsBetterStream(CCurrentStream& current, CDemuxStream* stream)
     if(current.type == STREAM_SUBTITLE)
       return false;
 
-    if(current.type == STREAM_TELETEXT)
-      return false;
-
     if(current.id < 0)
       return true;
   }
