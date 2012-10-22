xbmc/xbmc

commit 32343cb34c534a76e3fac404e203d366d80925de

Fix use of iterator after erase() in DeleteTimersOnChannel
Calling erase() invalidates timerIt.

xbmc/pvr/timers/PVRTimers.cpp
@@ -438,7 +438,7 @@ bool CPVRTimers::DeleteTimersOnChannel(const CPVRChannel &channel, bool bDeleteR
 
   for (map<CDateTime, vector<CPVRTimerInfoTagPtr>* >::reverse_iterator it = m_tags.rbegin(); it != m_tags.rend(); it++)
   {
-    for (vector<CPVRTimerInfoTagPtr>::iterator timerIt = it->second->begin(); timerIt != it->second->end(); timerIt++)
+    for (vector<CPVRTimerInfoTagPtr>::iterator timerIt = it->second->begin(); timerIt != it->second->end(); )
     {
       CPVRTimerInfoTagPtr timer = (*timerIt);
 
@@ -453,8 +453,10 @@ bool CPVRTimers::DeleteTimersOnChannel(const CPVRChannel &channel, bool bDeleteR
       if (timer->ChannelNumber() == channel.ChannelNumber() && timer->m_bIsRadio == channel.IsRadio())
       {
         bReturn = timer->DeleteFromClient(true) || bReturn;
-        it->second->erase(timerIt);
+        timerIt = it->second->erase(timerIt);
       }
+      else
+        timerIt++;
     }
   }
 

commit 5f75ec6acbb5be038b2f429a017aed50c0eb4ddd

Fix infinite loop introduced by 32343cb
This changeset makes sure that timerIt is always incremented by exactly
once through all branches of this loop.  It also rewrites the for loop
as a while loop to make this odd logic more explicit.

xbmc/pvr/timers/PVRTimers.cpp
@@ -438,17 +438,24 @@ bool CPVRTimers::DeleteTimersOnChannel(const CPVRChannel &channel, bool bDeleteR
 
   for (map<CDateTime, vector<CPVRTimerInfoTagPtr>* >::reverse_iterator it = m_tags.rbegin(); it != m_tags.rend(); it++)
   {
-    for (vector<CPVRTimerInfoTagPtr>::iterator timerIt = it->second->begin(); timerIt != it->second->end(); )
+    vector<CPVRTimerInfoTagPtr>::iterator timerIt = it->second->begin();
+    while (timerIt != it->second->end())
     {
       CPVRTimerInfoTagPtr timer = (*timerIt);
 
       if (bCurrentlyActiveOnly &&
           (CDateTime::GetCurrentDateTime() < timer->StartAsLocalTime() ||
            CDateTime::GetCurrentDateTime() > timer->EndAsLocalTime()))
+      {
+        ++timerIt;
         continue;
+      }
 
       if (!bDeleteRepeating && timer->m_bIsRepeating)
+      {
+        ++timerIt;
         continue;
+      }
 
       if (timer->ChannelNumber() == channel.ChannelNumber() && timer->m_bIsRadio == channel.IsRadio())
       {
@@ -456,7 +463,7 @@ bool CPVRTimers::DeleteTimersOnChannel(const CPVRChannel &channel, bool bDeleteR
         timerIt = it->second->erase(timerIt);
       }
       else
-        timerIt++;
+        ++timerIt;
     }
   }
 