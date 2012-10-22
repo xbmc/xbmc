b507f10d44843cac48e81640c4b241c860dc479e
Voyager1 <Voyager@xbmc.org>
2012-10-22 18:39:08 +0200

    re enable manually add to library

--- xbmc/video/windows/GUIWindowVideoNav.cpp
+++ xbmc/video/windows/GUIWindowVideoNav.cpp
@@ -1042,6 +1042,13 @@ void CGUIWindowVideoNav::GetContextButtons(int itemNumber, CContextButtons &butt
           buttons.Add(CONTEXT_BUTTON_DELETE, 117);
           buttons.Add(CONTEXT_BUTTON_RENAME, 118);
         }
+
+        if (!item->IsLiveTV()) 
+        { 
+          if (!m_database.HasMovieInfo(item->GetPath()) &&  !m_database.HasEpisodeInfo(item->GetPath())) 
+             buttons.Add(CONTEXT_BUTTON_ADD_TO_LIBRARY, 527); // Add to Database 
+        } 
+
         // add "Set/Change content" to folders
         if (item->m_bIsFolder && !item->IsPlayList() && !item->IsSmartPlayList() && !item->IsLiveTV() && !item->IsPlugin() && !item->IsAddonsPath() && !URIUtils::IsUPnP(item->GetPath()))
         {
@@ -1370,6 +1377,12 @@ bool CGUIWindowVideoNav::OnContextButton(int itemNumber, CONTEXT_BUTTON button)
       CUtil::DeleteVideoDatabaseDirectoryCache();
       Refresh();
       return true;
+    }
+
+  case CONTEXT_BUTTON_ADD_TO_LIBRARY: 
+    {
+      AddToDatabase(itemNumber); 
+      return true; 
     }
 
   default:
