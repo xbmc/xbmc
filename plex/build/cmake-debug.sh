7e3125d00ef9201f4f705499737d362d9609a8a9
Tobias Hieta <tobias@plexapp.com>
2012-10-28 21:34:05 +0100

    Use ccache and Ninja by default

--- plex/build/cmake-debug.sh
+++ plex/build/cmake-debug.sh
@@ -3,4 +3,5 @@
 # Everything is setup to compile with CLang
 export CC=clang
 export CXX=clang
-cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=output ../..
+export PATH=/usr/local/Cellar/ccache/3.1.8/libexec/:$PATH
+cmake -GNinja -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_INSTALL_PREFIX=output ../..
