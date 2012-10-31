xbmc/xbmc

commit ee17b9c6df428bb469c12065965977fa61a80eff

fixed: init logging parameters
quells uninitialized variables when running the test suite

xbmc/settings/AdvancedSettings.cpp
@@ -315,6 +315,8 @@ void CAdvancedSettings::Initialize()
 
   m_databaseMusic.Reset();
   m_databaseVideo.Reset();
+
+  m_logLevelHint = m_logLevel = LOG_LEVEL_NONE;
 }
 
 bool CAdvancedSettings::Load()

commit 6615f66a25741a592775c544c66baf040c5c2d9a

fixed: initialize log level to normal or we end up with no logging
regression after ee17b9c6df428bb469c12065965977fa61a80eff

xbmc/settings/AdvancedSettings.cpp
@@ -316,7 +316,7 @@ void CAdvancedSettings::Initialize()
   m_databaseMusic.Reset();
   m_databaseVideo.Reset();
 
-  m_logLevelHint = m_logLevel = LOG_LEVEL_NONE;
+  m_logLevelHint = m_logLevel = LOG_LEVEL_NORMAL;
 }
 
 bool CAdvancedSettings::Load()