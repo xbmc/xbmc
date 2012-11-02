xbmc/xbmc

commit 8885f2f2207c0d3c6cd14c22754a1e52872ad56f

dvdplayer: reduce likelyhood of jumpy pts causing duplicate subtitles

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.cpp
@@ -80,7 +80,7 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 {
   CDVDOverlay* pOverlay = NULL;
 
-  if (iPts < m_fLastPts)
+  if (iPts + DVD_SEC_TO_TIME(1) < m_fLastPts)
     Reset();
 
   if (m_pCurrent)

commit db72953bda6f927a95bb18d89003fc1ba518aaa6

fixed: subtitles would sometime go missing on start
Caused by: 8885f2f2207c0d3c6cd14c22754a1e52872ad56f

xbmc/cores/dvdplayer/DVDPlayerSubtitle.cpp
@@ -195,8 +195,11 @@ void CDVDPlayerSubtitle::Process(double pts)
     if(pts == DVD_NOPTS_VALUE)
       return;
 
-    if (pts < m_lastPts)
+    if (pts + DVD_SEC_TO_TIME(1) < m_lastPts)
+    {
       m_pOverlayContainer->Clear();
+      m_pSubtitleFileParser->Reset();
+    }
 
     if(m_pOverlayContainer->GetSize() >= 5)
       return;

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.cpp
@@ -29,7 +29,6 @@ CDVDSubtitleLineCollection::CDVDSubtitleLineCollection()
   m_pTail = NULL;
 
   m_iSize = 0;
-  m_fLastPts = DVD_NOPTS_VALUE;
 }
 
 CDVDSubtitleLineCollection::~CDVDSubtitleLineCollection()
@@ -80,9 +79,6 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 {
   CDVDOverlay* pOverlay = NULL;
 
-  if (iPts + DVD_SEC_TO_TIME(1) < m_fLastPts)
-    Reset();
-
   if (m_pCurrent)
   {
     while (m_pCurrent && m_pCurrent->pOverlay->iPTSStopTime < iPts)
@@ -96,7 +92,6 @@ CDVDOverlay* CDVDSubtitleLineCollection::Get(double iPts)
 
       // advance to the next overlay
       m_pCurrent = m_pCurrent->pNext;
-      m_fLastPts = iPts;
     }
   }
   return pOverlay;
@@ -124,5 +119,4 @@ void CDVDSubtitleLineCollection::Clear()
   m_pHead    = NULL;
   m_pCurrent = NULL;
   m_iSize    = 0;
-  m_fLastPts = DVD_NOPTS_VALUE;
 }

xbmc/cores/dvdplayer/DVDSubtitles/DVDSubtitleLineCollection.h
@@ -55,7 +55,6 @@ private:
   ListElement* m_pTail;
 
   int m_iSize;
-  double m_fLastPts;
   //CRITICAL_SECTION m_critSection;
 };
 