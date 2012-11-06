80b22f2917ea9fb611bebc8f7d81b8dacad422d7
Tobias Hieta <tobias@hieta.se>
2012-11-06 10:57:58 +0000

    Make install target work on Linux

--- plex/scripts/copy-resources.sh
+++ plex/scripts/copy-resources.sh
@@ -9,51 +9,45 @@ TARGET_BUILD_DIR=$3
 echo "From $SRCROOT to $TARGET_BUILD_DIR/$TARGET_NAME"
 
 # rsync command with exclusions for items we don't want in the app package
-SYNC="rsync -aq --exclude .DS_Store* --exclude *.dll --exclude *.DLL --exclude *linux.* --exclude *arm-osx.* --exclude *.zlib --exclude *.a"
+SYNC="rsync -aq --exclude .DS_Store* --exclude *.dll --exclude *.DLL --exclude *arm-osx.* --exclude *.zlib --exclude *.a"
 
 # rsync command for excluding pngs and jpgs as well. Note that if the skin itself is not compiled
 # using XBMCTex then excluding the pngs and jpgs will most likely make the skin unusable 
-SYNCSKIN="rsync -aq --exclude CVS* --exclude .svn* --exclude .cvsignore* --exclude .cvspass* --exclude .DS_Store* --exclude *.dll  --exclude *.DLL --exclude *linux.* --exclude *.png --exclude *.jpg --exclude *.bat --exclude Media* --exclude .git*"
+SYNCSKIN="rsync -aq --exclude CVS* --exclude .svn* --exclude .cvsignore* --exclude .cvspass* --exclude .DS_Store* --exclude *.dll  --exclude *.DLL --exclude *.png --exclude *.jpg --exclude *.bat --exclude Media* --exclude .git*"
 
 # rsync command for including everything but the skins
 ADDONSYNC="rsync -aq --exclude .DS_Store* --exclude skin.confluence --exclude skin.touched --exclude skin.mediastream"
 
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/addons"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/language"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/media"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/sounds"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/system"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/userdata"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/media"
-mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/extras/user"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/addons"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/language"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/media"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/sounds"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/system"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/userdata"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/media"
+mkdir -p "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/extras/user"
 
-${SYNC} "$SRCROOT/LICENSE.GPL" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/"
-${SYNC} "$SRCROOT/xbmc/osx/Credits.html" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/"
-#${SYNC} "$SRCROOT/tools/darwin/runtime"	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/tools/darwin"
-${ADDONSYNC} "$SRCROOT/addons"		"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${ADDONSYNC} "$SRCROOT/plex/addons" "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${SYNC} "$SRCROOT/language"		"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${SYNC} "$SRCROOT/media" 		"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-#${SYNCSKIN} "$SRCROOT/addons/skin.confluence" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/addons"
-#${SYNC} "$SRCROOT/addons/skin.confluence/backgrounds" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/addons/skin.confluence"
-#${SYNC} "$SRCROOT/addons/skin.confluence/icon.png" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/addons/skin.confluence"
-${SYNC} "$SRCROOT/sounds" 		"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${SYNC} "$SRCROOT/system" 		"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${SYNC} "$SRCROOT/userdata" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
-${SYNCSKIN} "$SRCROOT/addons/skin.mediastream" 	"$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/addons"
+${SYNC} "$SRCROOT/LICENSE.GPL" 	"$TARGET_BUILD_DIR/$TARGET_NAME/"
+${SYNC} "$SRCROOT/xbmc/osx/Credits.html" 	"$TARGET_BUILD_DIR/$TARGET_NAME/"
+${ADDONSYNC} "$SRCROOT/addons"		"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${ADDONSYNC} "$SRCROOT/plex/addons" "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNC} "$SRCROOT/language"		"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNC} "$SRCROOT/media" 		"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNC} "$SRCROOT/sounds" 		"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNC} "$SRCROOT/system" 		"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNC} "$SRCROOT/userdata" 	"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
+${SYNCSKIN} "$SRCROOT/addons/skin.mediastream" 	"$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/addons"
 
 # copy extra packages if applicable
 if [ -d "$SRCROOT/extras/system" ]; then
-	${SYNC} "$SRCROOT/extras/system/" "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC"
+	${SYNC} "$SRCROOT/extras/system/" "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC"
 fi
 
 # copy extra user packages if applicable
 if [ -d "$SRCROOT/extras/user" ]; then
-	${SYNC} "$SRCROOT/extras/user/" "$TARGET_BUILD_DIR/$TARGET_NAME/Contents/Resources/XBMC/extras/user"
+	${SYNC} "$SRCROOT/extras/user/" "$TARGET_BUILD_DIR/$TARGET_NAME/XBMC/extras/user"
 fi
-
-
 
 # magic that gets the icon to update
 touch "$TARGET_BUILD_DIR/$TARGET_NAME"
