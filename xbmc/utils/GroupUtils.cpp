5735ae25160586c96c269372d61b7e0523c89b3a
ace20022 <ace20022@ymail.com>
2012-11-06 07:52:51 +0100

    Add the item/video path to the "browse for art" dialog. This includes TV Shows (first path), seasons, episodes, movies, sets (multipath).

--- xbmc/utils/GroupUtils.cpp
+++ xbmc/utils/GroupUtils.cpp
@@ -26,6 +26,8 @@
 #include "utils/StringUtils.h"
 #include "utils/Variant.h"
 #include "video/VideoInfoTag.h"
+#include "utils/URIUtils.h"
+#include "filesystem/MultiPathDirectory.h"
 
 using namespace std;
 
@@ -77,6 +79,7 @@ bool GroupUtils::Group(GroupBy groupBy, const CFileItemList &items, CFileItemLis
 
       int ratings = 0;
       int iWatched = 0; // have all the movies been played at least once?
+      std::set<CStdString> pathSet;
       for (std::set<CFileItemPtr>::const_iterator movie = set->second.begin(); movie != set->second.end(); movie++)
       {
         CVideoInfoTag* movieInfo = (*movie)->GetVideoInfoTag();
@@ -103,7 +106,15 @@ bool GroupUtils::Group(GroupBy groupBy, const CFileItemList &items, CFileItemLis
         setInfo->m_playCount += movieInfo->m_playCount;
         if (movieInfo->m_playCount > 0)
           iWatched++;
+
+        //accumulate the path for a multipath construction
+        CFileItem video(movieInfo->m_basePath, false);
+        if (video.IsVideo())
+          pathSet.insert(URIUtils::GetParentPath(movieInfo->m_basePath));
+        else
+          pathSet.insert(movieInfo->m_basePath);
       }
+      setInfo->m_basePath = XFILE::CMultiPathDirectory::ConstructMultiPath(pathSet);
 
       if (ratings > 1)
         pItem->GetVideoInfoTag()->m_fRating /= ratings;
