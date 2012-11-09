aa5f5496afa358a5086670749a54084fcb1c7ef1
Jonathan Marshall <jmarshall@never.you.mind>
2012-11-10 12:22:48 +1300

    fix inconsistency when calling TV Show information on a season where the art for the item was taken from the season rather than the show

--- xbmc/video/windows/GUIWindowVideoBase.cpp
+++ xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -286,6 +286,10 @@ void CGUIWindowVideoBase::OnInfo(CFileItem* pItem, const ADDON::ScraperPtr& scra
   CFileItem item(*pItem);
   if (item.IsVideoDb() && item.HasVideoInfoTag())
   {
+    if (item.GetVideoInfoTag()->m_type == "season")
+    { // clear out the art - we're really grabbing the info on the show here
+      item.SetArt(map<string, string>());
+    }
     item.SetPath(item.GetVideoInfoTag()->GetPath());
   }
   else
