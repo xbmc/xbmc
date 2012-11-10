501c503695dbf3b1468d1cb27382b236641f79a2
Joakim Plate <elupus@ecce.se>
2012-11-10 23:04:27 +0100

    vaapi: relicence ffmpeg helper code to LGPL 2.1+
    Permission by:
    Joakim Plate <elupus@xbmc.org>
    Gwenole Beauchesne <gwenole.beauchesne@intel.com>
    Edgar Hucek <gimli@xbmc.org>
    Yurly Romanenko <yromanen@gmail.com>

--- xbmc/cores/dvdplayer/DVDCodecs/Video/VAAPI.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/VAAPI.cpp
@@ -2,19 +2,19 @@
  *      Copyright (C) 2005-2012 Team XBMC
  *      http://www.xbmc.org
  *
- *  This Program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
  *
- *  This Program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, see
- *  <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 #include "system.h"
