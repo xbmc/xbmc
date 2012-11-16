4bdaa0f589eb80b524c58d1bec7ebed85dcf93f4
montellese <montellese@xbmc.org>
2012-11-16 17:26:33 +0100

    settings: fix default values of "locale.language" and "locale.country"

--- xbmc/settings/GUISettings.cpp
+++ xbmc/settings/GUISettings.cpp
@@ -884,8 +884,8 @@ void CGUISettings::Initialize()
   AddString(laf, "lookandfeel.rssedit", 21450, "", BUTTON_CONTROL_STANDARD);
 
   CSettingsCategory* loc = AddCategory(SETTINGS_APPEARANCE, "locale", 14090);
-  AddString(loc, "locale.language",248,"english", SPIN_CONTROL_TEXT);
-  AddString(loc, "locale.country", 20026, "USA", SPIN_CONTROL_TEXT);
+  AddString(loc, "locale.language",248,"English", SPIN_CONTROL_TEXT);
+  AddString(loc, "locale.country", 20026, "USA (12h)", SPIN_CONTROL_TEXT);
   AddString(loc, "locale.charset", 14091, "DEFAULT", SPIN_CONTROL_TEXT); // charset is set by the language file
 
   bool use_timezone = false;
