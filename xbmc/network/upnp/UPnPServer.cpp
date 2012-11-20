0e558293f9dddc810702e4f6213c1e1257aa7593
Alasdair Campbell <alcoheca@gmail.com>
2012-11-20 10:24:11 +0000

    upnp: fix missing music info on BrowseMetadata calls fixes #13554

--- xbmc/network/upnp/UPnPServer.cpp
+++ xbmc/network/upnp/UPnPServer.cpp
@@ -4,6 +4,7 @@
 #include "GUIViewState.h"
 #include "Platinum.h"
 #include "video/VideoThumbLoader.h"
+#include "music/Artist.h"
 #include "music/MusicThumbLoader.h"
 #include "interfaces/AnnouncementManager.h"
 #include "filesystem/Directory.h"
@@ -257,8 +258,30 @@ CUPnPServer::Build(CFileItemPtr                  item,
                 item->SetLabel("Music Library");
                 item->SetLabelPreformated(true);
             } else {
-                if (!item->HasMusicInfoTag())
-                    item->LoadMusicTag();
+                if (!item->HasMusicInfoTag()) {
+                    MUSICDATABASEDIRECTORY::CQueryParams params;
+                    MUSICDATABASEDIRECTORY::CDirectoryNode::GetDatabaseInfo((const char*)path, params);
+
+                    CMusicDatabase db;
+                    if (!db.Open() ) return NULL;
+
+                    if (params.GetSongId() >= 0 ) {
+                        CSong song;
+                        if (db.GetSongById(params.GetSongId(), song))
+                            item->GetMusicInfoTag()->SetSong(song);
+                    }
+                    else if (params.GetAlbumId() >= 0 ) {
+                        CAlbum album;
+                        if (db.GetAlbumInfo(params.GetAlbumId(), album, NULL))
+                            item->GetMusicInfoTag()->SetAlbum(album);
+                    }
+                    else if (params.GetArtistId() >= 0 ) {
+                        CArtist artist;
+                        if (db.GetArtistInfo(params.GetArtistId(), artist, false))
+                            item->GetMusicInfoTag()->SetArtist(artist);
+                    }
+                }
+
 
                 if (item->GetLabel().IsEmpty()) {
                     /* if no label try to grab it from node type */
