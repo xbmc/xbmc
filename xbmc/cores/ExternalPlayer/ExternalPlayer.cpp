bf602fe2d24b2e6a14f74eb3158e471aefd13b03
Joakim Plate <elupus@ecce.se>
2012-12-05 22:03:05 +0100

    ExternalPlayer: unwind udf:// paths too (used for blurays)

--- xbmc/cores/ExternalPlayer/ExternalPlayer.cpp
+++ xbmc/cores/ExternalPlayer/ExternalPlayer.cpp
@@ -143,7 +143,7 @@ void CExternalPlayer::Process()
     // Unwind archive names
     CURL url(m_launchFilename);
     CStdString protocol = url.GetProtocol();
-    if (protocol == "zip" || protocol == "rar"/* || protocol == "iso9660" ??*/)
+    if (protocol == "zip" || protocol == "rar"/* || protocol == "iso9660" ??*/ || protocol == "udf")
     {
       mainFile = url.GetHostName();
       archiveContent = url.GetFileName();
