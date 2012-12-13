xbmc/xbmc

commit 25eea81654add5a5d3394ae46b1ecd0edb1313b4

udf: use hostname to open iso image instead of .iso in path

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -38,22 +38,6 @@
 using namespace std;
 using namespace XFILE;
 
-static bool is_udf_iso_path(const char* filename)
-{
-  bool bResult = false;
-
-  const char* ptr = strcasestr(filename, ".iso");
-  if(ptr)
-  {
-    ptr += strlen(".iso");
-    if(*ptr == '/' && strlen(++ptr) > 0)
-    {
-      bResult = true;
-    }
-  }
-  return bResult;
-}
-
 void DllLibbluray::file_close(BD_FILE_H *file)
 {
   if (file)
@@ -98,8 +82,9 @@ BD_FILE_H * DllLibbluray::file_open(const char* filename, const char *mode)
     BD_FILE_H *file = new BD_FILE_H;
 
     CStdString strFilename(filename);
-
-    if(is_udf_iso_path(filename))
+    CStdString strExtension(URIUtils::GetExtension(filename));
+    if(strExtension == ".iso"
+    || strExtension == ".img")
     {
       CURL::Encode(strFilename);
       strFilename.Format("udf://%s", strFilename);
@@ -173,12 +158,6 @@ BD_DIR_H *DllLibbluray::dir_open(const char* dirname)
     SDirState *st = new SDirState();
 
     CStdString strDirname(dirname);
-    if(is_udf_iso_path(dirname))
-    {
-      CURL::Encode(strDirname);
-      strDirname.Format("udf://%s", strDirname);
-      CLog::Log(LOGDEBUG, "CDVDInputStreamBluray - Opening udf dir %s...", strDirname.c_str());
-    }
 
     if(!CDirectory::GetDirectory(strDirname, st->list))
     {

xbmc/filesystem/UDFDirectory.cpp
@@ -38,15 +38,26 @@ CUDFDirectory::~CUDFDirectory(void)
 bool CUDFDirectory::GetDirectory(const CStdString& strPath,
                                  CFileItemList &items)
 {
-  CStdString strRoot = strPath;
+  CStdString strRoot, strSub;
+  CURL url;
+  if(strPath.Left(6) == "udf://")
+  {
+    url.Parse(strPath);
+    CURL url(strPath);
+  }
+  else
+  {
+    url.SetProtocol("udf");
+    url.SetHostName(strPath);
+  }
+  strRoot  = url.Get();
+  strSub   = url.GetFileName();
+
   URIUtils::AddSlashAtEnd(strRoot);
-
-  CURL url(strPath);
-
-  CStdString strDirectory = url.GetHostName();
+  URIUtils::AddSlashAtEnd(strSub);
 
   udf25 udfIsoReader;
-  udf_dir_t *dirp = udfIsoReader.OpenDir(strDirectory);
+  udf_dir_t *dirp = udfIsoReader.OpenDir(url.GetHostName(), strSub);
 
   if (dirp == NULL)
     return false;

xbmc/filesystem/UDFFile.cpp
@@ -48,9 +48,7 @@ CUDFFile::~CUDFFile()
 //*********************************************************************************************
 bool CUDFFile::Open(const CURL& url)
 {
-  CStdString strFName = url.GetHostName();
-
-  m_hFile = m_udfIsoReaderLocal.OpenFile((char*)strFName.c_str());
+  m_hFile = m_udfIsoReaderLocal.OpenFile(url.GetHostName(), url.GetFileName());
   if (m_hFile == INVALID_HANDLE_VALUE)
   {
     m_bOpened = false;
@@ -104,29 +102,18 @@ int64_t CUDFFile::GetPosition()
 
 bool CUDFFile::Exists(const CURL& url)
 {
-  string strFName = "\\";
-  strFName += url.GetFileName();
-  for (int i = 0; i < (int)strFName.size(); ++i )
-  {
-    if (strFName[i] == '/') strFName[i] = '\\';
-  }
-  m_hFile = m_udfIsoReaderLocal.OpenFile((char*)strFName.c_str());
+  m_hFile = m_udfIsoReaderLocal.OpenFile(url.GetHostName(), url.GetFileName());
   if (m_hFile == INVALID_HANDLE_VALUE)
     return false;
 
   m_udfIsoReaderLocal.CloseFile(m_hFile);
+  m_hFile = INVALID_HANDLE_VALUE;
   return true;
 }
 
 int CUDFFile::Stat(const CURL& url, struct __stat64* buffer)
 {
-  string strFName = "\\";
-  strFName += url.GetFileName();
-  for (int i = 0; i < (int)strFName.size(); ++i )
-  {
-    if (strFName[i] == '/') strFName[i] = '\\';
-  }
-  m_hFile = m_udfIsoReaderLocal.OpenFile((char*)strFName.c_str());
+  m_hFile = m_udfIsoReaderLocal.OpenFile(url.GetHostName(), url.GetFileName());
   if (m_hFile != INVALID_HANDLE_VALUE)
   {
     buffer->st_size = m_udfIsoReaderLocal.GetFileSize(m_hFile);

xbmc/filesystem/udf25.cpp
@@ -464,28 +464,6 @@ uint64_t DVDFileSeekForce(BD_FILE bdfile, uint64_t offset, int64_t force_size)
   return offset;
 }
 
-int UDFSplitIsoFile(const char *fullFilename, char* iso, char* file)
-{
-  const char* filename = strcasestr(fullFilename, ".iso");
-  if(!filename)
-    return -1;
-
-  filename += strlen(".iso");
-  if(*filename != '/')
-    return -1;
-
-  size_t size = strlen(filename);
-  memcpy(file, filename, size);
-  file[size] = 0;
-
-  size = filename - fullFilename;
-  memcpy(iso, fullFilename,  filename - fullFilename);
-  iso[size] = 0;
-
-  return 0;
-}
-
-
 int udf25::UDFScanDirX( udf_dir_t *dirp )
 {
   char filename[ MAX_UDF_FILE_NAME_LEN ];
@@ -1190,15 +1168,11 @@ UDF_FILE udf25::UDFFindFile( const char* filename, uint64_t *filesize )
   return result;
 }
 
-HANDLE udf25::OpenFile( const char* fullFilename )
+HANDLE udf25::OpenFile( const char* isoname, const char* filename )
 {
   uint64_t filesize;
   UDF_FILE file = NULL;
   BD_FILE bdfile = NULL;
-  char isoname[256], filename[256];
-
-  if(UDFSplitIsoFile(fullFilename, isoname, filename) !=0 )
-    return INVALID_HANDLE_VALUE;
 
   m_fp = file_open(isoname);
   if(m_fp)
@@ -1338,12 +1312,12 @@ int64_t udf25::GetFilePosition(HANDLE hFile)
   return bdfile->seek_pos;
 }
 
-udf_dir_t *udf25::OpenDir( const char *subdir )
+udf_dir_t *udf25::OpenDir(  const char *isofile, const char *subdir )
 {
   udf_dir_t *result;
   BD_FILE bd_file;
 
-  bd_file = (BD_FILE)OpenFile(subdir);
+  bd_file = (BD_FILE)OpenFile(isofile, subdir);
 
   if (bd_file == (BD_FILE)INVALID_HANDLE_VALUE)
   {

xbmc/filesystem/udf25.h
@@ -183,11 +183,11 @@ public:
   int64_t GetFileSize(HANDLE hFile);
   int64_t GetFilePosition(HANDLE hFile);
   int64_t Seek(HANDLE hFile, int64_t lOffset, int whence);
-  HANDLE OpenFile( const char* filename );
+  HANDLE OpenFile(  const char *isofile, const char* filename );
   long ReadFile(HANDLE fd, unsigned char *pBuffer, long lSize);
   void CloseFile(HANDLE hFile);
 
-  udf_dir_t *OpenDir( const char *subdir );
+  udf_dir_t *OpenDir( const char *isofile, const char *subdir );
   udf_dirent_t *ReadDir( udf_dir_t *dirp );
   int CloseDir( udf_dir_t *dirp );
 

commit 487c0e0e96c141ffb7d5780a8479f54da03f846c

dvdplayer: fixed bluray playback from directly from passed iso/img file
This was broken since 25eea81654add5a5d3394ae46b1ecd0edb1313b4

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -82,20 +82,6 @@ BD_FILE_H * DllLibbluray::file_open(const char* filename, const char *mode)
 {
     BD_FILE_H *file = new BD_FILE_H;
 
-    CStdString strFilename(filename);
-    CStdString strExtension(URIUtils::GetExtension(filename));
-    if(strExtension == ".iso"
-    || strExtension == ".img")
-    {
-      CURL::Encode(strFilename);
-      strFilename.Format("udf://%s", strFilename);
-      CLog::Log(LOGDEBUG, "CDVDInputStreamBluray - Opening file udf iso file %s... (%p)", strFilename.c_str(), file);
-    }
-    else
-    {
-      CLog::Log(LOGDEBUG, "CDVDInputStreamBluray - Opening file %s... (%p)", strFilename.c_str(), file);
-    }
-
     file->close = file_close;
     file->seek  = file_seek;
     file->read  = file_read;
@@ -104,7 +90,7 @@ BD_FILE_H * DllLibbluray::file_open(const char* filename, const char *mode)
     file->eof   = file_eof;
 
     CFile* fp = new CFile();
-    if(fp->Open(strFilename))
+    if(fp->Open(filename))
     {
       file->internal = (void*)fp;
       return file;
@@ -285,6 +271,17 @@ bool CDVDInputStreamBluray::Open(const char* strFile, const std::string& content
     }
     root     = strPath;
     filename = URIUtils::GetFileName(strFile);
+  }
+
+
+  /* translate to udf file system if needed */
+  CStdString ext(URIUtils::GetExtension(root));
+  if(ext == ".iso"
+  || ext == ".img")
+  {
+    CURL url("udf://");
+    url.SetHostName(root);
+    root = url.Get();
   }
 
   if (!m_dll)