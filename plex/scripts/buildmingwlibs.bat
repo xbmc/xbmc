73437e4389eddffb266ae6dd72bfd7501b929941
Tobias Hieta <tobias@plexapp.com>
2012-12-13 10:24:18 +0100

    Use our own buildminglibs scripts instead

--- plex/scripts/buildmingwlibs.bat
+++ plex/scripts/buildmingwlibs.bat
@@ -8,7 +8,7 @@ IF EXIST ..\..\project\BuildDependencies\msys\bin\sh.exe (
   rem compiles a bunch of mingw libs and not more
   echo bla>..\..\project\Win32BuildSetup\noprompt
   echo bla>..\..\project\Win32BuildSetup\makeclean
-  ..\..\project\BuildDependencies\msys\bin\sh --login /xbmc/project/Win32BuildSetup/buildmingwlibs.sh
+  ..\..\project\BuildDependencies\msys\bin\sh --login /xbmc/plex/scripts/buildmingwlibs.sh
 ) ELSE (
   ECHO bla>errormingw
   ECHO mingw environment not found
