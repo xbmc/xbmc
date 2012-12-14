369d971bb4108369e5310f0cc9d39ff6c2151091
Tobias Hieta <tobias@plexapp.com>
2012-12-14 16:13:28 +0100

    Fixes to Windows installer.

---
+++ plex/scripts/WindowsSign.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+rem
+rem Usage - Sign.cmd <target file>
+rem
+
+setlocal
+
+set TargetFile=%~1
+set SigningCert=%~dp0PlexOfficialSPC.pfx
+if not exist "%SigningCert%" (
+  set SigningCert=%~dp0PlexTestSPC.pfx
+)
+
+set PasswordArg=
+if "%SigningCertPassword%" NEQ "" (
+  set PasswordArg=/p %SigningCertPassword%
+)
+
+signtool.exe sign /f "%SigningCert%" %PasswordArg% /t http://timestamp.verisign.com/scripts/timstamp.dll "%TargetFile%"
+if "%errorlevel%" == 1 (
+  echo FATAL ERROR - could sign %TargetFile%
+)
+
+signtool.exe verify /pa "%TargetFile%"
+if "%errorlevel%" == 1 (
+  echo FATAL ERROR - could not verify signature for %TargetFile%
+)
+
+endlocal
