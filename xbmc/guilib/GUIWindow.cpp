ccfb770d0b71239264bb5aa37fb2e76aafd36005
pieh <misiek.piechowiak@gmail.com>
2012-12-15 10:10:47 +0100

    decouple rendering and window closing, add AfterRender method (that will be called each frame even if we actually didn't render anything) and move window closing logic there fixes #13650

--- xbmc/guilib/GUIWindow.cpp
+++ xbmc/guilib/GUIWindow.cpp
@@ -357,15 +357,15 @@ void CGUIWindow::DoRender()
   if (CGUIControlProfiler::IsRunning()) CGUIControlProfiler::Instance().EndFrame();
 }
 
-void CGUIWindow::Render()
+void CGUIWindow::AfterRender()
 {
-  CGUIControlGroup::Render();
   // Check to see if we should close at this point
   // We check after the controls have finished rendering, as we may have to close due to
   // the controls rendering after the window has finished it's animation
   // we call the base class instead of this class so that we can find the change
   if (m_closing && !CGUIControlGroup::IsAnimating(ANIM_TYPE_WINDOW_CLOSE))
     Close(true);
+
 }
 
 void CGUIWindow::Close_Internal(bool forceClose /*= false*/, int nextWindowID /*= 0*/, bool enableSound /*= true*/)
