849653ab85443905a0a73ce420d43eef095a80cc
Lars Op den Kamp <lars@opdenkamp.eu>
2012-12-17 19:50:43 +0100

    Revert "[epg] no longer use a temporary epg table while updating". Updating after the initial load was broken after this. I'll check why it broke after Frodo and just revert this one for now.
    This reverts commit c964d40629116192faff80b4dd11e5f5946407f6.

--- xbmc/epg/Epg.cpp
+++ xbmc/epg/Epg.cpp
@@ -782,7 +782,22 @@ bool CEpg::IsRemovableTag(const CEpgInfoTag &tag) const
 
 bool CEpg::LoadFromClients(time_t start, time_t end)
 {
-  return UpdateFromScraper(start, end);
+  bool bReturn(false);
+  CPVRChannelPtr channel = Channel();
+  if (channel)
+  {
+    CEpg tmpEpg(channel);
+    if (tmpEpg.UpdateFromScraper(start, end))
+      bReturn = UpdateEntries(tmpEpg, !g_guiSettings.GetBool("epg.ignoredbforclient"));
+  }
+  else
+  {
+    CEpg tmpEpg(m_iEpgID, m_strName, m_strScraperName);
+    if (tmpEpg.UpdateFromScraper(start, end))
+      bReturn = UpdateEntries(tmpEpg, !g_guiSettings.GetBool("epg.ignoredbforclient"));
+  }
+
+  return bReturn;
 }
 
 CEpgInfoTagPtr CEpg::GetNextEvent(const CEpgInfoTag& tag) const
