d0f79ae181c6667047592be56ae912900053088c
wsoltys <wiso@no.way>
2012-12-18 20:29:38 +0100

    added cximage to the project

---
+++ xbmc/guilib/cximage.h
@@ -0,0 +1,39 @@
+#pragma once
+/*
+ *      Copyright (C) 2012 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#include "iimage.h"
+#include "pictures/DllImageLib.h"
+
+class CXImage : public IImage
+{
+public:
+  CXImage();
+  ~CXImage();
+
+  virtual bool LoadImageFromMemory(unsigned char* buffer, unsigned int bufSize, unsigned int width, unsigned int height, const std::string& mimeType);
+  virtual bool Decode(const unsigned char *pixels, unsigned int pitch, unsigned int format);
+  virtual bool CreateThumbnailFromSurface(unsigned char* buffer, unsigned int width, unsigned int height, unsigned int format, unsigned int pitch, const CStdString& destFile);
+
+private:
+  DllImageLib m_dll;
+  ImageInfo m_image;
+};
