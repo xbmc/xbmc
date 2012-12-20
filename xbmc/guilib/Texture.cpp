577ea51c83b3285e20f3bbc244a0f40de119dadb
gimli <ebsi4711@gmail.com>
2012-12-20 06:59:12 +0100

    [guilib] d36f87ed13b11794ea25b990f644c74c9558296f makes the raspberrypi NPOT workaround obsolete

--- xbmc/guilib/Texture.cpp
+++ xbmc/guilib/Texture.cpp
@@ -70,13 +70,11 @@ void CBaseTexture::Allocate(unsigned int width, unsigned int height, unsigned in
     while (GetPitch() < g_Windowing.GetMinDXTPitch())
       m_textureWidth += GetBlockSize();
 
-#if !defined(TARGET_RASPBERRY_PI)
   if (!g_Windowing.SupportsNPOT((m_format & XB_FMT_DXT_MASK) != 0))
   {
     m_textureWidth = PadPow2(m_textureWidth);
     m_textureHeight = PadPow2(m_textureHeight);
   }
-#endif
   if (m_format & XB_FMT_DXT_MASK)
   { // DXT textures must be a multiple of 4 in width and height
     m_textureWidth = ((m_textureWidth + 3) / 4) * 4;
