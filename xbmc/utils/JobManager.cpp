9bf0f788541dc2a59b64dfe331b1ddbfa5910c3e
Tobias Hieta <tobias@plexapp.com>
2012-12-22 14:23:33 +0100

    Increase the numbers of workers, help first start experience.

--- xbmc/utils/JobManager.cpp
+++ xbmc/utils/JobManager.cpp
@@ -419,6 +419,10 @@ void CJobManager::RemoveWorker(const CJobWorker *worker)
 
 unsigned int CJobManager::GetMaxWorkers(CJob::PRIORITY priority) const
 {
+#ifndef __PLEX__
   static const unsigned int max_workers = 5;
+#else
+  static const unsigned int max_workers = 10;
+#endif
   return max_workers - (CJob::PRIORITY_HIGH - priority);
 }
