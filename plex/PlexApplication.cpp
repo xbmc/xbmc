d34e63936e0b59d24c5775b807bbe16011e371e1
Tobias Hieta <tobias@plexapp.com>
2012-12-22 17:41:12 +0100

    Use better AppCast url.

--- plex/PlexApplication.cpp
+++ plex/PlexApplication.cpp
@@ -30,7 +30,7 @@ PlexApplicationPtr PlexApplication::Create()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-PlexApplication::PlexApplication() : m_autoUpdater("http://nightlies.plexapp.com/laika/latest/appcast.xml")
+PlexApplication::PlexApplication() : m_autoUpdater("http://plexapp.com/appcast/plexht/appcast.xml")
 {
   // We don't want the background music player whacked on exit (destructor issues), so we'll keep a reference.
   m_serviceListener = PlexServiceListener::Create();
