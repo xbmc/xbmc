xbmc/xbmc

commit 07e51ad46ed1eb6b7cffd925a2c65bc1d69090c4

[rbp/omxplayer] fixed edl marks. for omx we have to do a full flush. fixes #13672 #13787

xbmc/cores/omxplayer/OMXPlayerAudio.cpp
@@ -147,7 +147,6 @@ void OMXPlayerAudio::OpenStream(CDVDStreamInfo &hints, COMXAudioCodecOMX *codec)
   m_errortime       = CurrentHostCounter();
   m_silence         = false;
   m_started         = false;
-  m_flush           = false;
   m_nChannels       = 0;
   m_synctype        = SYNC_DISCON;
   m_stalled         = m_messageQueue.GetPacketCount(CDVDMsg::DEMUXER_PACKET) == 0;
@@ -369,12 +368,6 @@ bool OMXPlayerAudio::Decode(DemuxPacket *pkt, bool bDropPacket)
 
       while(!m_bStop)
       {
-        if(m_flush)
-        {
-          m_flush = false;
-          break;
-        }
-
         if(m_omxAudio.GetSpace() < (unsigned int)pkt->iSize)
         {
           Sleep(10);
@@ -417,12 +410,6 @@ bool OMXPlayerAudio::Decode(DemuxPacket *pkt, bool bDropPacket)
 
     while(!m_bStop)
     {
-      if(m_flush)
-      {
-        m_flush = false;
-        break;
-      }
-
       if(m_omxAudio.GetSpace() < (unsigned int)pkt->iSize)
       {
         Sleep(10);
@@ -623,7 +610,6 @@ void OMXPlayerAudio::Process()
 
 void OMXPlayerAudio::Flush()
 {
-  m_flush = true;
   m_messageQueue.Flush();
   m_messageQueue.Put( new CDVDMsg(CDVDMsg::GENERAL_FLUSH), 1);
 }

xbmc/cores/omxplayer/OMXPlayerAudio.h
@@ -81,7 +81,6 @@ protected:
 
   struct timespec           m_starttime, m_endtime;
   bool                      m_buffer_empty;
-  bool                      m_flush;
   //SYNC_DISCON, SYNC_SKIPDUP, SYNC_RESAMPLE
   int                       m_synctype;
   int                       m_nChannels;

xbmc/cores/omxplayer/OMXPlayerVideo.cpp
@@ -76,7 +76,6 @@ OMXPlayerVideo::OMXPlayerVideo(OMXClock *av_clock,
   m_open                  = false;
   m_stream_id             = -1;
   m_fFrameRate            = 25.0f;
-  m_flush                 = false;
   m_hdmi_clock_sync       = false;
   m_speed                 = DVD_PLAYSPEED_NORMAL;
   m_stalled               = false;
@@ -121,7 +120,6 @@ bool OMXPlayerVideo::OpenStream(CDVDStreamInfo &hints)
 
   m_hints       = hints;
   m_Deinterlace = ( g_settings.m_currentVideoSettings.m_DeinterlaceMode == VS_DEINTERLACEMODE_OFF ) ? false : true;
-  m_flush       = false;
   m_hdmi_clock_sync = (g_guiSettings.GetInt("videoplayer.adjustrefreshrate") != ADJUST_REFRESHRATE_OFF);
   m_started     = false;
   m_stalled     = m_messageQueue.GetPacketCount(CDVDMsg::DEMUXER_PACKET) == 0;
@@ -170,8 +168,6 @@ bool OMXPlayerVideo::OpenStream(CDVDStreamInfo &hints, COMXVideo *codec)
 
 bool OMXPlayerVideo::CloseStream(bool bWaitForBuffers)
 {
-  m_flush   = true;
-
   // wait until buffers are empty
   if (bWaitForBuffers && m_speed > 0) m_messageQueue.WaitUntilEmpty();
 
@@ -615,12 +611,6 @@ void OMXPlayerVideo::Process()
 
       while (!m_bStop)
       {
-        if(m_flush)
-        {
-          m_flush = false;
-          break;
-        }
-
         if((int)m_omxVideo.GetFreeSpace() < pPacket->iSize)
         {
           Sleep(10);
@@ -685,7 +675,6 @@ void OMXPlayerVideo::Process()
 
 void OMXPlayerVideo::Flush()
 {
-  m_flush = true;
   m_messageQueue.Flush();
   m_messageQueue.Put(new CDVDMsg(CDVDMsg::GENERAL_FLUSH), 1);
 }

xbmc/cores/omxplayer/OMXPlayerVideo.h
@@ -53,7 +53,6 @@ protected:
   COMXVideo                 m_omxVideo;
   float                     m_fFrameRate;
   bool                      m_Deinterlace;
-  bool                      m_flush;
   bool                      m_hdmi_clock_sync;
   double                    m_iVideoDelay;
   int                       m_speed;

xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -1880,7 +1880,7 @@ void COMXPlayer::CheckAutoSceneSkip()
     /*
      * Seeking is NOT flushed so any content up to the demux point is retained when playing forwards.
      */
-    m_messenger.Put(new CDVDMsgPlayerSeek((int)seek, true, false, true, false, true));
+    m_messenger.Put(new CDVDMsgPlayerSeek((int)seek, true, true, true, false, true));
     /*
      * Seek doesn't always work reliably. Last physical seek time is recorded to prevent looping
      * if there was an error with seeking and it landed somewhere unexpected, perhaps back in the
@@ -1898,7 +1898,7 @@ void COMXPlayer::CheckAutoSceneSkip()
     /*
      * Seeking is NOT flushed so any content up to the demux point is retained when playing forwards.
      */
-    m_messenger.Put(new CDVDMsgPlayerSeek(cut.end + 1, true, false, true, false, true));
+    m_messenger.Put(new CDVDMsgPlayerSeek(cut.end + 1, true, true, true, false, true));
     /*
      * Each commercial break is only skipped once so poorly detected commercial breaks can be
      * manually re-entered. Start and end are recorded to prevent looping and to allow seeking back

commit 4388d8e435f2a38bf0049cb16e27a6110a5da812

[rbp/omxplayer] fixed seek. 07e51ad46ed1eb6b7cffd925a2c65bc1d69090c4 broke it. we still need the flush protection in the video/audio player. extra protect the access to m_flush

xbmc/cores/omxplayer/OMXPlayerAudio.cpp
@@ -147,6 +147,7 @@ void OMXPlayerAudio::OpenStream(CDVDStreamInfo &hints, COMXAudioCodecOMX *codec)
   m_errortime       = CurrentHostCounter();
   m_silence         = false;
   m_started         = false;
+  m_flush           = false;
   m_nChannels       = 0;
   m_synctype        = SYNC_DISCON;
   m_stalled         = m_messageQueue.GetPacketCount(CDVDMsg::DEMUXER_PACKET) == 0;
@@ -368,6 +369,14 @@ bool OMXPlayerAudio::Decode(DemuxPacket *pkt, bool bDropPacket)
 
       while(!m_bStop)
       {
+        if(m_flush)
+        {
+          CSingleLock lock(m_flushLock);
+          m_flush = false;
+          lock.Leave();
+          break;
+        }
+
         if(m_omxAudio.GetSpace() < (unsigned int)pkt->iSize)
         {
           Sleep(10);
@@ -410,6 +419,12 @@ bool OMXPlayerAudio::Decode(DemuxPacket *pkt, bool bDropPacket)
 
     while(!m_bStop)
     {
+      if(m_flush)
+      {
+        m_flush = false;
+        break;
+      }
+
       if(m_omxAudio.GetSpace() < (unsigned int)pkt->iSize)
       {
         Sleep(10);
@@ -610,6 +625,8 @@ void OMXPlayerAudio::Process()
 
 void OMXPlayerAudio::Flush()
 {
+  CSingleLock lock(m_flushLock);
+  m_flush = true;
   m_messageQueue.Flush();
   m_messageQueue.Put( new CDVDMsg(CDVDMsg::GENERAL_FLUSH), 1);
 }

xbmc/cores/omxplayer/OMXPlayerAudio.h
@@ -42,6 +42,7 @@ using namespace std;
 class OMXPlayerAudio : public CThread
 {
 protected:
+  CCriticalSection      m_flushLock;
   CDVDMessageQueue      m_messageQueue;
   CDVDMessageQueue      &m_messageParent;
 
@@ -81,6 +82,7 @@ protected:
 
   struct timespec           m_starttime, m_endtime;
   bool                      m_buffer_empty;
+  bool                      m_flush;
   //SYNC_DISCON, SYNC_SKIPDUP, SYNC_RESAMPLE
   int                       m_synctype;
   int                       m_nChannels;

xbmc/cores/omxplayer/OMXPlayerVideo.cpp
@@ -76,6 +76,7 @@ OMXPlayerVideo::OMXPlayerVideo(OMXClock *av_clock,
   m_open                  = false;
   m_stream_id             = -1;
   m_fFrameRate            = 25.0f;
+  m_flush                 = false;
   m_hdmi_clock_sync       = false;
   m_speed                 = DVD_PLAYSPEED_NORMAL;
   m_stalled               = false;
@@ -120,6 +121,7 @@ bool OMXPlayerVideo::OpenStream(CDVDStreamInfo &hints)
 
   m_hints       = hints;
   m_Deinterlace = ( g_settings.m_currentVideoSettings.m_DeinterlaceMode == VS_DEINTERLACEMODE_OFF ) ? false : true;
+  m_flush       = false;
   m_hdmi_clock_sync = (g_guiSettings.GetInt("videoplayer.adjustrefreshrate") != ADJUST_REFRESHRATE_OFF);
   m_started     = false;
   m_stalled     = m_messageQueue.GetPacketCount(CDVDMsg::DEMUXER_PACKET) == 0;
@@ -168,6 +170,8 @@ bool OMXPlayerVideo::OpenStream(CDVDStreamInfo &hints, COMXVideo *codec)
 
 bool OMXPlayerVideo::CloseStream(bool bWaitForBuffers)
 {
+  m_flush   = true;
+
   // wait until buffers are empty
   if (bWaitForBuffers && m_speed > 0) m_messageQueue.WaitUntilEmpty();
 
@@ -611,6 +615,14 @@ void OMXPlayerVideo::Process()
 
       while (!m_bStop)
       {
+        if(m_flush)
+        {
+          CSingleLock lock(m_flushLock);
+          m_flush = false;
+          lock.Leave();
+          break;
+        }
+
         if((int)m_omxVideo.GetFreeSpace() < pPacket->iSize)
         {
           Sleep(10);
@@ -675,6 +687,8 @@ void OMXPlayerVideo::Process()
 
 void OMXPlayerVideo::Flush()
 {
+  CSingleLock lock(m_flushLock);
+  m_flush = true;
   m_messageQueue.Flush();
   m_messageQueue.Put(new CDVDMsg(CDVDMsg::GENERAL_FLUSH), 1);
 }

xbmc/cores/omxplayer/OMXPlayerVideo.h
@@ -44,6 +44,7 @@ using namespace std;
 class OMXPlayerVideo : public CThread
 {
 protected:
+  CCriticalSection          m_flushLock;
   CDVDMessageQueue          m_messageQueue;
   int                       m_stream_id;
   bool                      m_open;
@@ -53,6 +54,7 @@ protected:
   COMXVideo                 m_omxVideo;
   float                     m_fFrameRate;
   bool                      m_Deinterlace;
+  bool                      m_flush;
   bool                      m_hdmi_clock_sync;
   double                    m_iVideoDelay;
   int                       m_speed;