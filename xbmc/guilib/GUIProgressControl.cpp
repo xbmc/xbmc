bf9a671b02dc0c66dbec8529d7a4bb626a84f2b3
Jonathan Marshall <jmarshall@never.you.mind>
2013-01-04 13:48:35 +1300

    progress control with reveal set to true didn't update correctly with dirty regions set to 1. fixes #13887

--- xbmc/guilib/GUIProgressControl.cpp
+++ xbmc/guilib/GUIProgressControl.cpp
@@ -242,7 +242,12 @@ bool CGUIProgressControl::UpdateLayout(void)
     {
       bChanged |= m_guiMid.SetWidth(m_width);
       float x = posX, y = posY + offset, w = width, h = fScaleY * m_guiMid.GetTextureHeight();
-      m_guiMidClipRect = CRect(x, y, x + w, y + h);
+      CRect rect(x, y, x + w, y + h);
+      if (rect != m_guiMidClipRect)
+      {
+        m_guiMidClipRect = rect;
+        bChanged = true;
+      }
     }
     else
     {
@@ -276,7 +281,12 @@ bool CGUIProgressControl::UpdateLayout(void)
     {
       bChanged |= m_guiMid.SetWidth(fScaleX * fFullWidth);
       float x = posX, y = posY + offset, w =  fScaleX * fWidth, h = fScaleY * m_guiMid.GetTextureHeight();
-      m_guiMidClipRect = CRect(x, y, x + w, y + h);
+      CRect rect(x, y, x + w, y + h);
+      if (rect != m_guiMidClipRect)
+      {
+        m_guiMidClipRect = rect;
+        bChanged = true;
+      }
     }
     else
     {
