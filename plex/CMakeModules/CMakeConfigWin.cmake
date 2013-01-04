7ff0c4fd1898c58e4ffa052f4d8ec696d6acf821
Tobias Hieta <tobias@plexapp.com>
2013-01-04 10:38:02 +0100

    Fix windows build

--- plex/CMakeModules/CMakeConfigWin.cmake
+++ plex/CMakeModules/CMakeConfigWin.cmake
@@ -34,6 +34,12 @@ set(non_link_libs
   SDL_image
 )
 
+if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+  set(external_libs ${external_libs} tinyxmlSTLd libboost_thread-vc100-mt-sgd-1_46_1 libboost_system-vc100-mt-sgd-1_46_1)
+else()
+  set(external_libs ${external_libs} tinyxmlSTL libboost_thread-vc100-mt-s-1_46_1 libboost_system-vc100-mt-s-1_46_1)
+endif()
+
 foreach(lib ${external_libs})
   plex_find_library(${lib} 0 1 ${dependdir}/lib 1)
 endforeach()
@@ -46,11 +52,10 @@ foreach(lib ${system_libs})
   plex_find_library(${lib} 0 0 "" 1)
 endforeach()
 
-if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-  set(external_libs ${external_libs} tinyxmlSTLd libboost_thread-vc100-mt-sgd-1_46_1 libboost_system-vc100-mt-sgd-1_46_1)
-else()
-  set(external_libs ${external_libs} tinyxmlSTL libboost_thread-vc100-mt-s-1_46_1 libboost_system-vc100-mt-s-1_46_1)
-endif()
 
 set(ARCH "x86-win")
 set(EXECUTABLE_NAME "Plex Home Theater")
+set(FFMPEG_INCLUDE_DIRS ${dependdir}/include)
+set(LIBPATH .)
+set(BINPATH .)
+set(RESOURCEPATH .)
