690b6d8ff7826ad00b362a92deeb7591a223259a
davilla <davilla@4pi.com>
2013-01-06 09:37:30 -0500

    [aml] fixed typo

--- xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
@@ -1428,7 +1428,7 @@ bool CAMLCodec::OpenDecoder(CDVDStreamInfo &hints)
     am_private->video_rate = 0.5 + (float)UNIT_FREQ * 1001 / 25000;
   }
 
-  // check for SD h265 content incorrectly reported as 60 fsp
+  // check for SD h264 content incorrectly reported as 60 fsp
   // mp4/avi containers :(
   if (hints.codec == CODEC_ID_H264 && hints.width == 720 && am_private->video_rate == 1602)
   {
