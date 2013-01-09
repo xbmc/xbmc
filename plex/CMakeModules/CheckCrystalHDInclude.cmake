3d95867d968144b699438e477c4f2b5eeeee526f
Tobias Hieta <tobias@plexapp.com>
2013-01-09 12:13:30 +0100

    Fix crystalhd crash on linux

--- plex/CMakeModules/CheckCrystalHDInclude.cmake
+++ plex/CMakeModules/CheckCrystalHDInclude.cmake
@@ -1,16 +1,18 @@
 
-plex_find_header(libcrystalhd/libcrystalhd_if ${dependdir}/include)
+if(APPLE)
+  plex_find_header(libcrystalhd/libcrystalhd_if ${dependdir}/include)
 
-if(DEFINED HAVE_LIBCRYSTALHD_LIBCRYSTALHD_IF_H)
-  CHECK_C_SOURCE_COMPILES("
-    #include <libcrystalhd/bc_dts_types.h>
-    #include <libcrystalhd/bc_dts_defs.h>
-    PBC_INFO_CRYSTAL bCrystalInfo;
-    int main() {}
-  " CHECK_CRYSTALHD_VERSION)
-  if(CHECK_CRYSTALHD_VERSION)
-    set(HAVE_LIBCRYSTALHD 2)
-  else()
-    set(HAVE_LIBCRYSTALHD 1)
+  if(DEFINED HAVE_LIBCRYSTALHD_LIBCRYSTALHD_IF_H)
+    CHECK_C_SOURCE_COMPILES("
+      #include <libcrystalhd/bc_dts_types.h>
+      #include <libcrystalhd/bc_dts_defs.h>
+      PBC_INFO_CRYSTAL bCrystalInfo;
+      int main() {}
+    " CHECK_CRYSTALHD_VERSION)
+    if(CHECK_CRYSTALHD_VERSION)
+      set(HAVE_LIBCRYSTALHD 2)
+    else()
+      set(HAVE_LIBCRYSTALHD 1)
+    endif()
   endif()
 endif()
