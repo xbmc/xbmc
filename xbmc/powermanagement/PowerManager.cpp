195b7651084e69d5f20c399fd478d42863e0c323
Memphiz <memphis@machzwo.de>
2013-01-19 01:05:17 +0100

    [osx] - on resume don't toggle windowed/fullscreen - this leads to moving XBMC to screen1 on dual screen setups and isn't needed on osx at all

--- xbmc/powermanagement/PowerManager.cpp
+++ xbmc/powermanagement/PowerManager.cpp
@@ -220,10 +220,10 @@ void CPowerManager::OnWake()
 #if defined(HAS_SDL) || defined(TARGET_WINDOWS)
   if (g_Windowing.IsFullScreen())
   {
-#ifdef _WIN32
+#if defined(_WIN32)
     ShowWindow(g_hWnd,SW_RESTORE);
     SetForegroundWindow(g_hWnd);
-#else
+#elsif !defined(TARGET_DARWIN_OSX)
     // Hack to reclaim focus, thus rehiding system mouse pointer.
     // Surely there's a better way?
     g_graphicsContext.ToggleFullScreenRoot();
