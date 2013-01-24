aea93c367c6e898e1a0a5518dfd64e4d8dcc2372
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-01-24 23:27:26 +0100

    Change default musicvideo scraper from the broken last.fm to theaudiodb.com. In case last.fm was still in use replace it with theaudiodb.com

--- xbmc/settings/GUISettings.cpp
+++ xbmc/settings/GUISettings.cpp
@@ -815,7 +815,7 @@ void CGUISettings::Initialize()
 
   AddDefaultAddon(NULL, "scrapers.moviesdefault", 21413, "metadata.themoviedb.org", ADDON_SCRAPER_MOVIES);
   AddDefaultAddon(NULL, "scrapers.tvshowsdefault", 21414, "metadata.tvdb.com", ADDON_SCRAPER_TVSHOWS);
-  AddDefaultAddon(NULL, "scrapers.musicvideosdefault", 21415, "metadata.musicvideos.last.fm", ADDON_SCRAPER_MUSICVIDEOS);
+  AddDefaultAddon(NULL, "scrapers.musicvideosdefault", 21415, "metadata.musicvideos.theaudiodb.com", ADDON_SCRAPER_MUSICVIDEOS);
 
   // service settings
   AddGroup(SETTINGS_SERVICE, 14036);
@@ -1458,6 +1458,13 @@ void CGUISettings::LoadXML(TiXmlElement *pRootElement, bool hideSettings /* = fa
   if (GetString("screensaver.mode") == "screensaver.xbmc.builtin.slideshow")
   {
     SetString("screensaver.mode", "screensaver.xbmc.builtin.dim");
+    updated = true;
+  }
+
+  // replace broken last.fm musicvideo scraper with theaudiodb.com if it's still in use
+  if (GetString("scrapers.musicvideosdefault") == "metadata.musicvideos.last.fm")
+  {
+    SetString("scrapers.musicvideosdefault", "metadata.musicvideos.theaudiodb.com");
     updated = true;
   }
 
