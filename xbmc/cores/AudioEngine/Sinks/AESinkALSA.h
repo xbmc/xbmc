370ac3d7dc0abf34b54e4cdc0502d775d8f7e607
fritsch <peter.fruehberger@gmail.com>
2013-02-05 21:17:10 +0100

    AE: Enable SoftSuspend on Linux (Stop h0gg1ng the sound device)

--- xbmc/cores/AudioEngine/Sinks/AESinkALSA.h
+++ xbmc/cores/AudioEngine/Sinks/AESinkALSA.h
@@ -49,6 +49,8 @@ public:
   virtual double       GetCacheTotal   ();
   virtual unsigned int AddPackets      (uint8_t *data, unsigned int frames, bool hasAudio);
   virtual void         Drain           ();
+  virtual bool         SoftSuspend();
+  virtual bool         SoftResume();
 
   static void EnumerateDevicesEx(AEDeviceInfoList &list, bool force = false);
 private:
