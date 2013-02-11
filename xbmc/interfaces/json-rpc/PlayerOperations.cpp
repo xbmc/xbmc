5da61581a199abeba53475e40f5e45582cc1924e
ace20022 <ace20022@ymail.com>
2013-02-11 17:24:38 +0100

    Fix the result of Player.GetProperties with param "currentaudiostream" for video without an audio stream (index = -1).

--- xbmc/interfaces/json-rpc/PlayerOperations.cpp
+++ xbmc/interfaces/json-rpc/PlayerOperations.cpp
@@ -1362,10 +1362,11 @@ JSONRPC_STATUS CPlayerOperations::GetPropertyValue(PlayerType player, const CStd
             value.Empty();
             g_application.m_pPlayer->GetAudioStreamLanguage(index, value);
             result["language"] = value;
+
+            result["codec"] = g_application.m_pPlayer->GetAudioCodecName();
+            result["bitrate"] = g_application.m_pPlayer->GetAudioBitrate();
+            result["channels"] = g_application.m_pPlayer->GetChannels();
           }
-          result["codec"] = g_application.m_pPlayer->GetAudioCodecName();
-          result["bitrate"] = g_application.m_pPlayer->GetAudioBitrate();
-          result["channels"] = g_application.m_pPlayer->GetChannels();
         }
         else
           result = CVariant(CVariant::VariantTypeNull);
