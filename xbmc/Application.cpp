e2fb5124f2fb0194efd2104a5ec546cab5b06cc1
Joakim Plate <elupus@ecce.se>
2013-02-20 23:04:17 +0100

    airplay: switch to non apple tv name
    This makes discovery of xbmc more reliable in mac osx.
    
    Thanx to matli on forum for find.
    
    Signed-off-by: Joakim Plate <elupus@ecce.se>
    Signed-off-by: Memphiz <memphis@machzwo.de>

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -1597,7 +1597,7 @@ bool CApplication::StartAirplayServer()
         txt.push_back(std::make_pair("deviceid", "FF:FF:FF:FF:FF:F2"));
       }
       txt.push_back(std::make_pair("features", "0x77"));
-      txt.push_back(std::make_pair("model", "AppleTV2,1"));
+      txt.push_back(std::make_pair("model", "Xbmc,1"));
       txt.push_back(std::make_pair("srcvers", AIRPLAY_SERVER_VERSION_STR));
       CZeroconf::GetInstance()->PublishService("servers.airplay", "_airplay._tcp", g_infoManager.GetLabel(SYSTEM_FRIENDLY_NAME), listenPort, txt);
       ret = true;
