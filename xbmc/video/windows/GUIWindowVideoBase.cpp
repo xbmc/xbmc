fafeeb81f650bf3cc750d2e39c9697c80c9cc115
montellese <montellese@xbmc.org>
2013-02-25 18:09:59 +0100

    refresh the correct video viewed in the video info dialog (fixes #13676)

--- xbmc/video/windows/GUIWindowVideoBase.cpp
+++ xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -456,6 +456,14 @@ bool CGUIWindowVideoBase::ShowIMDB(CFileItem *item, const ScraperPtr &info2)
     needsRefresh = pDlgInfo->NeedRefresh();
     if (!needsRefresh)
       return pDlgInfo->HasUpdatedThumb();
+    // check if the item in the video info dialog has changed and if so, get the new item
+    else if (pDlgInfo->GetCurrentListItem() != NULL)
+    {
+      item = pDlgInfo->GetCurrentListItem().get();
+
+      if (item->IsVideoDb() && item->HasVideoInfoTag())
+        item->SetPath(item->GetVideoInfoTag()->GetPath());
+    }
   }
 
   // quietly return if Internet lookups are disabled
