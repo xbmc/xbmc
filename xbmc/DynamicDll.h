bdcac078fc67e12b75233c015d00913b67b61d14
Garrett Brown <garbearucla@gmail.com>
2013-02-26 02:35:56 -0800

    Marked DllDynamic::IsLoaded() as const

--- xbmc/DynamicDll.h
+++ xbmc/DynamicDll.h
@@ -502,7 +502,7 @@ public:
   virtual ~DllDynamic();
   virtual bool Load();
   virtual void Unload();
-  virtual bool IsLoaded() { return m_dll!=NULL; }
+  virtual bool IsLoaded() const { return m_dll!=NULL; }
   bool CanLoad();
   bool EnableDelayedUnload(bool bOnOff);
   bool SetFile(const CStdString& strDllName);
