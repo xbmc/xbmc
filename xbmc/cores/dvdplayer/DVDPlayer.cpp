7bd16554e0e6e71f77aeb50c420d97d5be4f0cdf
xbmc <fernetmenta@online.de>
2013-02-27 09:02:29 +0100

    Revert "dvdplayer: update player state with reference to dts and absolute time"
    This reverts commit f05ffc6d8544f37d8721c6bc863387a3ae85ae9a.

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -3231,11 +3231,6 @@ void CDVDPlayer::FlushBuffers(bool queued, double pts, bool accurate)
     if(pts != DVD_NOPTS_VALUE)
       m_clock.Discontinuity(pts);
     UpdatePlayState(0);
-
-    // update state, buffers are flushed and it may take some time until
-    // we get an update from players
-    CSingleLock lock(m_StateSection);
-    m_State = m_StateInput;
   }
 }
 
@@ -3859,10 +3854,8 @@ int CDVDPlayer::AddSubtitleFile(const std::string& filename, const std::string& 
 
 void CDVDPlayer::UpdatePlayState(double timeout)
 {
-  if((m_StateInput.timestamp != 0)
-     && (m_StateInput.timestamp + DVD_MSEC_TO_TIME(timeout) > CDVDClock::GetAbsoluteClock())
-     && (m_StateInput.dts != DVD_NOPTS_VALUE)
-     && (abs(m_StateInput.dts - m_StateInput.dts_state) < DVD_MSEC_TO_TIME(timeout)))
+  if(m_StateInput.timestamp != 0
+  && m_StateInput.timestamp + DVD_MSEC_TO_TIME(timeout) > CDVDClock::GetAbsoluteClock())
     return;
 
   SPlayerState state(m_StateInput);
@@ -3988,7 +3981,6 @@ void CDVDPlayer::UpdatePlayState(double timeout)
     state.cache_bytes = 0;
 
   state.timestamp = CDVDClock::GetAbsoluteClock();
-  state.dts_state = state.dts;
 
   CSingleLock lock(m_StateSection);
   m_StateInput = state;
