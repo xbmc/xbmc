b6d1ee2364a7b52e225251e35e0d02baf797a4ec
ulion <ulion2002@gmail.com>
2013-03-01 15:18:31 +0800

    Fix empty music title announce.

--- xbmc/interfaces/AnnouncementManager.cpp
+++ xbmc/interfaces/AnnouncementManager.cpp
@@ -190,7 +190,10 @@ void CAnnouncementManager::Announce(AnnouncementFlag flag, const char *sender, c
       // TODO: Can be removed once this is properly handled when starting playback of a file
       item->SetProperty(LOOKUP_PROPERTY, false);
 
-      object["item"]["title"] = item->GetMusicInfoTag()->GetTitle();
+      CStdString title = item->GetMusicInfoTag()->GetTitle();
+      if (title.IsEmpty())
+        title = item->GetLabel();
+      object["item"]["title"] = title;
 
       if (item->GetMusicInfoTag()->GetTrackNumber() > 0)
         object["item"]["track"] = item->GetMusicInfoTag()->GetTrackNumber();
