85934ef935aa6199c8c9d1e6a91901675bc9cd20
Chris "Koying" Browet <cbro@semperpax.com>
2013-03-04 16:38:45 +0100

    FIX: Crash when unloading fonts

--- xbmc/guilib/GUIFontManager.cpp
+++ xbmc/guilib/GUIFontManager.cpp
@@ -239,13 +239,13 @@ void GUIFontManager::ReloadTTFFonts(void)
 
 void GUIFontManager::UnloadTTFFonts()
 {
+  for (vector<CGUIFont*>::iterator i = m_vecFonts.begin(); i != m_vecFonts.end(); i++)
+    (*i)->SetFont(NULL);
+
   for (vector<CGUIFontTTFBase*>::iterator i = m_vecFontFiles.begin(); i != m_vecFontFiles.end(); i++)
     delete (*i);
 
   m_vecFontFiles.clear();
-
-  for (vector<CGUIFont*>::iterator i = m_vecFonts.begin(); i != m_vecFonts.end(); i++)
-    (*i)->SetFont(NULL);
 }
 
 void GUIFontManager::Unload(const CStdString& strFontName)
