6d43f9e0dd411312d3b87540a5f8c4c84d5d79b6
montellese <montellese@xbmc.org>
2013-03-05 12:01:41 +0100

    rss: cosmetics and cleanup

--- xbmc/utils/RssReader.h
+++ xbmc/utils/RssReader.h
@@ -1,14 +1,4 @@
-// RssReader.h: interface for the CRssReader class.
-//
-//////////////////////////////////////////////////////////////////////
-
-#if !defined(AFX_RSSREADER_H__157FED93_0CDE_4295_A9AF_75BEF4E81761__INCLUDED_)
-#define AFX_RSSREADER_H__157FED93_0CDE_4295_A9AF_75BEF4E81761__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
-
 /*
  *      Copyright (C) 2005-2013 Team XBMC
  *      http://www.xbmc.org
@@ -29,21 +19,14 @@
  *
  */
 
-#include "StdString.h"
-#include "threads/CriticalSection.h"
-#include "threads/Thread.h"
-
-#include <vector>
 #include <list>
-
-#include "utils/XBMCTinyXML.h"
+#include <vector>
 
 #include "system.h"
-
-
-#define RSS_COLOR_BODY  0
-#define RSS_COLOR_HEADLINE 1
-#define RSS_COLOR_CHANNEL 2
+#include "threads/CriticalSection.h"
+#include "threads/Thread.h"
+#include "utils/StdString.h"
+#include "utils/XBMCTinyXML.h"
 
 typedef uint32_t character_t;
 typedef std::vector<character_t> vecText;
@@ -104,7 +87,7 @@ public:
   void Start();
   void Stop();
   void Reset();
-  bool IsActive() { return m_bActive; }
+  bool IsActive() const { return m_bActive; }
 
   bool GetReader(int controlID, int windowID, IRssObserver* observer, CRssReader *&reader);
 
@@ -121,5 +104,3 @@ private:
 };
 
 extern CRssManager g_rssManager;
-
-#endif // !defined(AFX_RSSREADER_H__157FED93_0CDE_4295_A9AF_75BEF4E81761__INCLUDED_)
