xbmc/xbmc

commit 2d45fad482ca0384e96605761712a0a97f85a937

XMLUtils: add std::string version of GetString()

xbmc/utils/XMLUtils.cpp
@@ -126,18 +126,26 @@ bool XMLUtils::GetBoolean(const TiXmlNode* pRootNode, const char* strTag, bool& 
 
 bool XMLUtils::GetString(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue)
 {
+  std::string value;
+  bool ret = GetString(pRootNode, strTag, value);
+  strStringValue = value;
+  return ret;
+}
+
+bool XMLUtils::GetString(const TiXmlNode* pRootNode, const char* strTag, std::string& strStringValue)
+{
   const TiXmlElement* pElement = pRootNode->FirstChildElement(strTag );
   if (!pElement) return false;
   const char* encoded = pElement->Attribute("urlencoded");
   const TiXmlNode* pNode = pElement->FirstChild();
   if (pNode != NULL)
   {
-    strStringValue = pNode->Value();
+    strStringValue = pNode->ValueStr();
     if (encoded && strcasecmp(encoded,"yes") == 0)
-      CURL::Decode(strStringValue);
+      strStringValue = CURL::Decode(strStringValue);
     return true;
   }
-  strStringValue.Empty();
+  strStringValue.clear();
   return false;
 }
 

xbmc/utils/XMLUtils.h
@@ -39,6 +39,7 @@ public:
   static bool GetInt(const TiXmlNode* pRootNode, const char* strTag, int& iIntValue);
   static bool GetBoolean(const TiXmlNode* pRootNode, const char* strTag, bool& bBoolValue);
   static bool GetString(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue);
+  static bool GetString(const TiXmlNode* pRootNode, const char* strTag, std::string& strStringValue);
   /*! \brief Get multiple tags, concatenating the values together.
    Transforms
      <tag>value1</tag>

commit 91fbd97b4c8e1d1509eb7529a43d42cc93c259e9

fixed: XMLUtils::GetString(.. CStdString& ..) was broken by 2d45fad482ca0384e96605761712a0a97f85a937. This caused eg. all advancedsettings defaults for strings to be cleared (eg. default players etc.)

xbmc/utils/XMLUtils.cpp
@@ -127,9 +127,12 @@ bool XMLUtils::GetBoolean(const TiXmlNode* pRootNode, const char* strTag, bool& 
 bool XMLUtils::GetString(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue)
 {
   std::string value;
-  bool ret = GetString(pRootNode, strTag, value);
-  strStringValue = value;
-  return ret;
+  if (GetString(pRootNode, strTag, value))
+  {
+    strStringValue = value;
+    return true;
+  }
+  return false;
 }
 
 bool XMLUtils::GetString(const TiXmlNode* pRootNode, const char* strTag, std::string& strStringValue)