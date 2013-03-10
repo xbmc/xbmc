a7aa0f00e9e8f11f17f288feee589967a3a65262
S. Davilla <davilla@4pi.com>
2013-03-10 15:54:13 -0400

    Revert "[osx] - fix volume mediakeys now control xbmc volume (fixes volume buttons on nyxboard and hw volume buttons on macs/osx)"
    This reverts commit 6bb569c17c58c5ebc4532cc11404efe4bc70732a.

--- xbmc/osx/HotKeyController.m
+++ xbmc/osx/HotKeyController.m
@@ -178,9 +178,7 @@ static CGEventRef tapEventCallback2(CGEventTapProxy proxy, CGEventType type, CGE
   int keyState = (keyFlags & 0xFF00) >> 8;
   BOOL keyIsRepeat = (keyFlags & 0x1) > 0;
   
-  // allow repeated keypresses for volume buttons
-  // all other repeated keypresses are handled by the os (is this really good?)
-  if (keyIsRepeat && keyCode != NX_KEYTYPE_SOUND_UP && keyCode != NX_KEYTYPE_SOUND_DOWN) 
+  if (keyIsRepeat) 
     return event;
   
   NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
@@ -295,7 +293,7 @@ static CGEventRef tapEventCallback(CGEventTapProxy proxy, CGEventType type, CGEv
     m_eventPort = NULL;
     m_runLoopSource = NULL;
     m_controlSysPower = NO;
-    m_controlSysVolume = YES; // volume keys control xbmc volume
+    m_controlSysVolume = NO;
   }
   return self;
 }
