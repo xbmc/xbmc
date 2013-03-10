9bcc05a68f0a1cde4ff06b4fd74c0a07ec2919ec
Chris "koying" Browet <cbro@semperpax.com>
2013-03-10 14:46:40 +0100

    CHG: [droid] wraps native activity in a java class

---
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
@@ -0,0 +1,19 @@
+package org.xbmc.xbmc;
+
+import android.app.NativeActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main extends NativeActivity 
+{
+  public Main() 
+  {
+    super();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) 
+  {
+    super.onCreate(savedInstanceState);
+  }
+}
