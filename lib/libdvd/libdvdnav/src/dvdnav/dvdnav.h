2e171e6221b7a2e21003e0b5b87d5444883bc400
ace20022 <ace20022@ymail.com>
2013-03-20 14:33:55 +0100

    [Fix] Fixes dvdnav_audio_stream_format(...). Don't change the audio format numbers, e.g. DTS is 6 not 5, 5 is unknown.

--- lib/libdvd/libdvdnav/src/dvdnav/dvdnav.h
+++ lib/libdvd/libdvdnav/src/dvdnav/dvdnav.h
@@ -63,12 +63,6 @@ typedef int32_t dvdnav_status_t;
 #define DVDNAV_STATUS_ERR 0
 #define DVDNAV_STATUS_OK  1
 
-#define DVDNAV_FORMAT_AC3 0
-#define DVDNAV_FORMAT_MPEGAUDIO 3
-#define DVDNAV_FORMAT_LPCM 4
-#define DVDNAV_FORMAT_DTS 5
-#define DVDNAV_FORMAT_SDDS 6
-
 /*********************************************************************
  * initialisation & housekeeping functions                           *
  *********************************************************************/
