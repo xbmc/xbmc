d75740b319f94c290b90d815ab7e57334cb45d6f
WiSo <wiso@xbmc.org>
2013-03-31 15:31:11 +0200

    [WIN32] fixed typo in bzip2 dependencies.

--- project/BuildDependencies/scripts/libbzip2_d.bat
+++ project/BuildDependencies/scripts/libbzip2_d.bat
@@ -7,7 +7,7 @@ CALL dlextract.bat libbzip2 %FILES%
 
 cd %TMP_PATH%
 
-copy include\bzlib2.h "%CUR_PATH%\include\" /Y
+copy include\* "%CUR_PATH%\include\" /Y
 copy lib\bzip2.lib "%CUR_PATH%\lib\" /Y
 
 cd %LOC_PATH%
