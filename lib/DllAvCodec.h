e79c8dbe78ad970f74c7af8f19ab8037cf43ae07
David Härdeman <david@hardeman.nu>
2013-04-01 22:25:52 +0200

    ffmpeg compatibility fix for matroska embedded subtitles
    Commit 2626cc4580bfd560c6983338d77b2c11c16af94f in the ffmpeg repo
    changed the codec id of matroska embedded S_TEXT/UTF-8 subtitles
    from CODEC_ID_TEXT to AV_CODEC_ID_SUBRIP. That commit is part of
    ffmpeg 1.0 and later.
    
    Add the new codec id to provide compatibility for the updated
    ffmpeg library and external ffmpeg libraries.

--- lib/DllAvCodec.h
+++ lib/DllAvCodec.h
@@ -63,6 +63,12 @@ extern "C" {
 #endif
 }
 
+#if LIBAVCODEC_VERSION_MICRO >= 100
+  #define LIBAVCODEC_FROM_FFMPEG
+#else
+  #define LIBAVCODEC_FROM_LIBAV
+#endif
+
 #include "threads/SingleLock.h"
 
 class DllAvCodecInterface
