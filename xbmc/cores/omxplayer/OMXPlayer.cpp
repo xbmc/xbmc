6a6e84035760ff380933f9a33becc4ca617cb73b
Stephan Raue <stephan@openelec.tv>
2013-04-06 19:52:08 +0200

    [OMXPlayer] [PATCH] dvdplayer: another fix for seeking/fw/rw in PVR Based on https://github.com/xbmc/xbmc/commit/676b9e7347be62a518851c30d7e203b71849fc47 in master and added in https://github.com/xbmc/xbmc/commit/28de0da9afeb6bcbc2c8a3cad31c79225b07ec7d

--- xbmc/cores/omxplayer/OMXPlayer.cpp
+++ xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -2085,7 +2085,7 @@ void COMXPlayer::HandleMessages()
 
         // if input streams doesn't support seektime we must convert back to clock
         if(dynamic_cast<CDVDInputStream::ISeekTime*>(m_pInputStream) == NULL)
-          time -= m_State.time_offset;
+          time -= DVD_TIME_TO_MSEC(m_State.time_offset);
 
         CLog::Log(LOGDEBUG, "demuxer seek to: %d", time);
         if (m_pDemuxer && m_pDemuxer->SeekTime(time, msg.GetBackward(), &start))
