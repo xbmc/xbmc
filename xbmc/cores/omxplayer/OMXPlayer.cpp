fa268701fbc986e07577a26cd70107cbeb30eded
Stephan Raue <stephan@openelec.tv>
2013-04-06 18:54:23 +0200

    fix deadlock when trying to go to disc menu Based on https://github.com/xbmc/xbmc/commit/befdcfad8d2b14c9d935c3662a56853d617f884d in master and added in https://github.com/xbmc/xbmc/commit/28de0da9afeb6bcbc2c8a3cad31c79225b07ec7d

--- xbmc/cores/omxplayer/OMXPlayer.cpp
+++ xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -3524,7 +3524,7 @@ bool COMXPlayer::OnAction(const CAction &action)
         pMenus->OnMenu();
         // send a message to everyone that we've gone to the menu
         CGUIMessage msg(GUI_MSG_VIDEO_MENU_STARTED, 0, 0);
-        g_windowManager.SendMessage(msg);
+        g_windowManager.SendThreadMessage(msg);
         return true;
       }
       break;
