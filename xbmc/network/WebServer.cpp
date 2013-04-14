114700a1d4357c71d6538018aa9e1f875f962441
montellese <montellese@xbmc.org>
2013-04-14 21:09:33 +0200

    webserver: fix 64-bit print format for Content-Length

--- xbmc/network/WebServer.cpp
+++ xbmc/network/WebServer.cpp
@@ -416,7 +416,7 @@ int CWebServer::CreateFileDownloadResponse(struct MHD_Connection *connection, co
       getData = false;
 
       CStdString contentLength;
-      contentLength.Format("%I64d", file->GetLength());
+      contentLength.Format("%" PRId64, fileLength);
 
       response = MHD_create_response_from_data(0, NULL, MHD_NO, MHD_NO);
       if (response == NULL)
