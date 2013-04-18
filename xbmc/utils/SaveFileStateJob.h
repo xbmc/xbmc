xbmc/xbmc

commit ba7a58ccc16cd1a22124ab8f286aa0374ba928b8

upnp: client support for updating files on server

xbmc/utils/SaveFileStateJob.h
@@ -7,6 +7,7 @@
 #include "pvr/PVRManager.h"
 #include "pvr/recordings/PVRRecordings.h"
 #include "settings/MediaSettings.h"
+#include "network/upnp/UPnP.h"
 
 class CSaveFileStateJob : public CJob
 {
@@ -32,12 +33,18 @@ bool CSaveFileStateJob::DoWork()
   CStdString progressTrackingFile = m_item.GetPath();
   if (m_item.HasVideoInfoTag() && m_item.GetVideoInfoTag()->m_strFileNameAndPath.Find("removable://") == 0)
     progressTrackingFile = m_item.GetVideoInfoTag()->m_strFileNameAndPath; // this variable contains removable:// suffixed by disc label+uniqueid or is empty if label not uniquely identified
-  else if (m_item.HasProperty("original_listitem_url") && 
-      URIUtils::IsPlugin(m_item.GetProperty("original_listitem_url").asString()))
+  else if (m_item.HasProperty("original_listitem_url"))
     progressTrackingFile = m_item.GetProperty("original_listitem_url").asString();
 
   if (progressTrackingFile != "")
   {
+#ifdef HAS_UPNP
+    // checks if UPnP server of this file is available and supports updating
+    if (URIUtils::IsUPnP(progressTrackingFile)
+          && UPNP::CUPnP::SaveFileState(m_item, m_bookmark, m_updatePlayCount)) {
+        return true;
+    }
+#endif
     if (m_item.IsVideo())
     {
       CLog::Log(LOGDEBUG, "%s - Saving file state for video item %s", __FUNCTION__, progressTrackingFile.c_str());
@@ -86,6 +93,16 @@ bool CSaveFileStateJob::DoWork()
               PVR::CPVRRecording *recording = m_item.GetPVRRecordingInfoTag();
               recording->SetLastPlayedPosition(m_bookmark.timeInSeconds <= 0.0f ? 0 : (int)m_bookmark.timeInSeconds);
               recording->m_resumePoint = m_bookmark;
+            }
+
+            // UPnP announce resume point changes to clients
+            // however not if playcount is modified as that already announces
+            if (m_item.IsVideoDb() && !m_updatePlayCount)
+            {
+              CVariant data;
+              data["id"] = m_item.GetVideoInfoTag()->m_iDbId;
+              data["type"] = m_item.GetVideoInfoTag()->m_type;
+              ANNOUNCEMENT::CAnnouncementManager::Announce(ANNOUNCEMENT::VideoLibrary, "xbmc", "OnUpdate", data);
             }
 
             updateListing = true;

xbmc/network/upnp/UPnP.cpp
@@ -40,6 +40,7 @@
 #include "guilib/GUIWindowManager.h"
 #include "GUIInfoManager.h"
 #include "utils/TimeUtils.h"
+#include "video/VideoInfoTag.h"
 #include "guilib/Key.h"
 #include "Util.h"
 
@@ -188,6 +189,81 @@ public:
         CGUIMessage message(GUI_MSG_NOTIFY_ALL, 0, 0, GUI_MSG_UPDATE_PATH);
         message.SetStringParam(path.GetChars());
         g_windowManager.SendThreadMessage(message);
+    }
+
+    bool MarkWatched(const CFileItem& item, const bool watched)
+    {
+        if (watched) {
+            CFileItem temp(item);
+            temp.SetProperty("original_listitem_url", item.GetPath());
+            return SaveFileState(temp, CBookmark(), watched);
+        }
+        else {
+            CLog::Log(LOGDEBUG, "UPNP: Marking video item %s as watched", item.GetPath().c_str());
+            return InvokeUpdateObject(item.GetPath().c_str(), "<upnp:playCount>1</upnp:playCount>", "<upnp:playCount>0</upnp:playCount>");
+        }
+    }
+
+    bool SaveFileState(const CFileItem& item, const CBookmark& bookmark, const bool updatePlayCount)
+    {
+        string path = item.GetProperty("original_listitem_url").asString();
+        if (!item.HasVideoInfoTag() || path.empty())  {
+          return false;
+        }
+
+        NPT_String curr_value;
+        NPT_String new_value;
+
+        if (item.GetVideoInfoTag()->m_resumePoint.timeInSeconds != bookmark.timeInSeconds) {
+            CLog::Log(LOGDEBUG, "UPNP: Updating resume point for item %s", path.c_str());
+            long time = bookmark.timeInSeconds;
+            if (time < 0) time = 0;
+            curr_value.Append(NPT_String::Format("<upnp:lastPlaybackPosition>%ld</upnp:lastPlaybackPosition>",
+                                                 (long)item.GetVideoInfoTag()->m_resumePoint.timeInSeconds));
+            new_value.Append(NPT_String::Format("<upnp:lastPlaybackPosition>%ld</upnp:lastPlaybackPosition>", time));
+        }
+        if (updatePlayCount) {
+            CLog::Log(LOGDEBUG, "UPNP: Marking video item %s as watched", path.c_str());
+            if (!curr_value.IsEmpty()) curr_value.Append(",");
+            if (!new_value.IsEmpty()) new_value.Append(",");
+            curr_value.Append("<upnp:playCount>0</upnp:playCount>");
+            new_value.Append("<upnp:playCount>1</upnp:playCount>");
+        }
+
+        return InvokeUpdateObject(path.c_str(), (const char*)curr_value, (const char*)new_value);
+    }
+
+    bool InvokeUpdateObject(const char* id, const char* curr_value, const char* new_value)
+    {
+        CURL url(id);
+        PLT_DeviceDataReference device;
+        PLT_Service* cds;
+        PLT_ActionReference action;
+
+        CLog::Log(LOGDEBUG, "UPNP: attempting to invoke UpdateObject for %s", id);
+
+        // check this server supports UpdateObject action
+        NPT_CHECK_LABEL(FindServer(url.GetHostName().c_str(), device),failed);
+        NPT_CHECK_LABEL(device->FindServiceById("urn:upnp-org:serviceId:ContentDirectory", cds),failed);
+
+        NPT_CHECK_SEVERE(m_CtrlPoint->CreateAction(
+            device,
+            "urn:schemas-upnp-org:service:ContentDirectory:1",
+            "UpdateObject",
+            action));
+
+        NPT_CHECK_LABEL(action->SetArgumentValue("ObjectID", url.GetFileName().c_str()), failed);
+        NPT_CHECK_LABEL(action->SetArgumentValue("CurrentTagValue", curr_value), failed);
+        NPT_CHECK_LABEL(action->SetArgumentValue("NewTagValue", new_value), failed);
+
+        NPT_CHECK_LABEL(m_CtrlPoint->InvokeAction(action, NULL),failed);
+
+        CLog::Log(LOGDEBUG, "UPNP: invoked UpdateObject successfully");
+        return true;
+
+    failed:
+        CLog::Log(LOGINFO, "UPNP: invoking UpdateObject failed");
+        return false;
     }
 };
 
@@ -343,6 +419,34 @@ CUPnPServer* CUPnP::GetServer()
   if(upnp)
     return (CUPnPServer*)upnp->m_ServerHolder->m_Device.AsPointer();
   return NULL;
+}
+
+/*----------------------------------------------------------------------
+|   CUPnP::MarkWatched
++---------------------------------------------------------------------*/
+bool
+CUPnP::MarkWatched(const CFileItem& item, const bool watched)
+{
+    if (upnp && upnp->m_MediaBrowser) {
+        // dynamic_cast is safe here, avoids polluting CUPnP.h header file
+        CMediaBrowser* browser = dynamic_cast<CMediaBrowser*>(upnp->m_MediaBrowser);
+        return browser->MarkWatched(item, watched);
+    }
+    return false;
+}
+
+/*----------------------------------------------------------------------
+|   CUPnP::SaveFileState
++---------------------------------------------------------------------*/
+bool
+CUPnP::SaveFileState(const CFileItem& item, const CBookmark& bookmark, const bool updatePlayCount)
+{
+    if (upnp && upnp->m_MediaBrowser) {
+        // dynamic_cast is safe here, avoids polluting CUPnP.h header file
+        CMediaBrowser* browser = dynamic_cast<CMediaBrowser*>(upnp->m_MediaBrowser);
+        return browser->SaveFileState(item, bookmark, updatePlayCount);
+    }
+    return false;
 }
 
 /*----------------------------------------------------------------------

xbmc/network/upnp/UPnP.h
@@ -30,6 +30,8 @@ class PLT_SyncMediaBrowser;
 class PLT_MediaController;
 class PLT_MediaObject;
 class PLT_MediaItemResource;
+class CFileItem;
+class CBookmark;
 
 namespace UPNP
 {
@@ -66,6 +68,12 @@ public:
     static void   ReleaseInstance(bool bWait);
     static bool   IsInstantiated() { return upnp != NULL; }
 
+    static bool MarkWatched(const CFileItem& item,
+                            const bool watched);
+
+    static bool SaveFileState(const CFileItem& item,
+                              const CBookmark& bookmark,
+                              const bool updatePlayCount);
 private:
     // methods
     CUPnPRenderer* CreateRenderer(int port = 0);

xbmc/video/windows/GUIWindowVideoBase.cpp
@@ -64,6 +64,7 @@
 #include "utils/log.h"
 #include "utils/FileUtils.h"
 #include "interfaces/AnnouncementManager.h"
+#include "network/upnp/UPnP.h"
 #include "pvr/PVRManager.h"
 #include "pvr/recordings/PVRRecordings.h"
 #include "utils/URIUtils.h"
@@ -1688,11 +1689,16 @@ void CGUIWindowVideoBase::MarkWatched(const CFileItemPtr &item, bool bMark)
            (!bMark && !(pItem->GetVideoInfoTag()->m_playCount))))
         continue;
 
-      // Clear resume bookmark
-      if (bMark)
-        database.ClearBookMarksOfFile(pItem->GetPath(), CBookmark::RESUME);
+#ifdef HAS_UPNP
+      if (!URIUtils::IsUPnP(item->GetPath()) || !UPNP::CUPnP::MarkWatched(*pItem, bMark))
+#endif
+      {
+        // Clear resume bookmark
+        if (bMark)
+          database.ClearBookMarksOfFile(pItem->GetPath(), CBookmark::RESUME);
 
-      database.SetPlayCount(*pItem, bMark ? 1 : 0);
+        database.SetPlayCount(*pItem, bMark ? 1 : 0);
+      }
     }
     
     database.Close(); 

commit e71c8d0ed2fe3195e9f354db962ef10bc962229c

[fix] SaveFileState was broken by ba7a58c

xbmc/utils/SaveFileStateJob.h
@@ -34,7 +34,12 @@ bool CSaveFileStateJob::DoWork()
   if (m_item.HasVideoInfoTag() && m_item.GetVideoInfoTag()->m_strFileNameAndPath.Find("removable://") == 0)
     progressTrackingFile = m_item.GetVideoInfoTag()->m_strFileNameAndPath; // this variable contains removable:// suffixed by disc label+uniqueid or is empty if label not uniquely identified
   else if (m_item.HasProperty("original_listitem_url"))
-    progressTrackingFile = m_item.GetProperty("original_listitem_url").asString();
+  {
+    // only use original_listitem_url for Python & UPnP sources
+    CStdString original = m_item.GetProperty("original_listitem_url").asString();
+    if (URIUtils::IsPlugin(original) || URIUtils::IsUPnP(original))
+      progressTrackingFile = original;
+  }
 
   if (progressTrackingFile != "")
   {