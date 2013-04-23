e23197335aa38fe213a03974bd3dbd7b0b4e79a5
Karlson2k <k2k@narod.ru>
2013-04-23 17:29:55 +0400

    Improve compatibility with C++11 (Visual Studio 2012, Intel C++ compiler 13.0)

--- xbmc/epg/Epg.cpp
+++ xbmc/epg/Epg.cpp
@@ -96,7 +96,10 @@ CEpg &CEpg::operator =(const CEpg &right)
   m_pvrChannel        = right.m_pvrChannel;
 
   for (map<CDateTime, CEpgInfoTagPtr>::const_iterator it = right.m_tags.begin(); it != right.m_tags.end(); it++)
-    m_tags.insert(make_pair(it->first, new CEpgInfoTag(*it->second)));
+  {
+    CEpgInfoTagPtr EITPtr (new CEpgInfoTag(*it->second));
+    m_tags.insert(make_pair(it->first, EITPtr));
+  }
 
   return *this;
 }
@@ -354,7 +357,7 @@ bool CEpg::UpdateEntry(const CEpgInfoTag &tag, bool bUpdateDatabase /* = false *
   infoTag->m_pvrChannel   = m_pvrChannel;
 
   if (bUpdateDatabase)
-    m_changedTags.insert(make_pair<int, CEpgInfoTagPtr>(infoTag->UniqueBroadcastID(), infoTag));
+    m_changedTags.insert(make_pair(infoTag->UniqueBroadcastID(), infoTag));
 
   return true;
 }
@@ -617,7 +620,7 @@ bool CEpg::FixOverlappingEvents(bool bUpdateDb /* = false */)
     {
       // delete the current tag. it's completely overlapped
       if (bUpdateDb)
-        m_deletedTags.insert(make_pair<int, CEpgInfoTagPtr>(currentTag->UniqueBroadcastID(), currentTag));
+        m_deletedTags.insert(make_pair(currentTag->UniqueBroadcastID(), currentTag));
 
       if (m_nowActiveStart == it->first)
         m_nowActiveStart.SetValid(false);
@@ -629,7 +632,7 @@ bool CEpg::FixOverlappingEvents(bool bUpdateDb /* = false */)
     {
       currentTag->SetStartFromUTC(previousTag->EndAsUTC());
       if (bUpdateDb)
-        m_changedTags.insert(make_pair<int, CEpgInfoTagPtr>(currentTag->UniqueBroadcastID(), currentTag));
+        m_changedTags.insert(make_pair(currentTag->UniqueBroadcastID(), currentTag));
 
       previousTag = it->second;
     }
@@ -649,8 +652,8 @@ bool CEpg::FixOverlappingEvents(bool bUpdateDb /* = false */)
 
       if (bUpdateDb)
       {
-        m_changedTags.insert(make_pair<int, CEpgInfoTagPtr>(currentTag->UniqueBroadcastID(), currentTag));
-        m_changedTags.insert(make_pair<int, CEpgInfoTagPtr>(previousTag->UniqueBroadcastID(), previousTag));
+        m_changedTags.insert(make_pair(currentTag->UniqueBroadcastID(), currentTag));
+        m_changedTags.insert(make_pair(previousTag->UniqueBroadcastID(), previousTag));
       }
 
       previousTag = it->second;
