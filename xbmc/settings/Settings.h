8eab507a79f01a3cbccb10a379a89890c14aefd0
montellese <montellese@xbmc.org>
2013-04-27 12:21:52 +0200

    settings: integrate new settings system into CSettings

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -20,53 +20,78 @@
  */
 
 #include <set>
+#include <string>
 
-#include "settings/ISettingsHandler.h"
-#include "settings/ISubSettings.h"
+#include "settings/ISettingCallback.h"
+#include "settings/ISettingCreator.h"
+#include "settings/Setting.h"
 #include "threads/CriticalSection.h"
-#include "utils/StdString.h"
 
-class CGUISettings;
+class CSettingSection;
+class CSettingsManager;
 class TiXmlElement;
 class TiXmlNode;
 
-class CSettings : private ISettingsHandler, ISubSettings
+class CSettings : public ISettingCreator
 {
 public:
-  CSettings(void);
-  virtual ~CSettings(void);
+  CSettings();
+  virtual ~CSettings();
 
-  void RegisterSettingsHandler(ISettingsHandler *settingsHandler);
-  void UnregisterSettingsHandler(ISettingsHandler *settingsHandler);
-  void RegisterSubSettings(ISubSettings *subSettings);
-  void UnregisterSubSettings(ISubSettings *subSettings);
+  static CSettings& Get();
 
+  // implementation of ISettingCreator
+  virtual CSetting* CreateSetting(const std::string &settingType, const std::string &settingId, CSettingsManager *settingsManager = NULL) const;
+
+  bool Initialize();
   bool Load();
-  void Save() const;
-  bool SaveSettings(const CStdString& strSettingsFile, CGUISettings *localSettings = NULL) const;
-  bool Reset();
-  void Clear();
+  bool Load(const std::string &file);
+  bool Load(const TiXmlElement *root, bool hide = false);
+  void SetLoaded();
+  bool Save();
+  bool Save(const std::string &file);
+  void Unload();
+  void Uninitialize();
 
-protected:
-  bool LoadSettings(const CStdString& strSettingsFile);
+  void RegisterCallback(ISettingCallback *callback, const std::set<std::string> &settingList);
+  void UnregisterCallback(ISettingCallback *callback);
+
+  CSetting* GetSetting(const std::string &id) const;
+  CSettingSection* GetSection(const std::string &section) const;
+  SettingDependencyMap GetDependencies(const std::string &id) const;
+  SettingDependencyMap GetDependencies(const CSetting *setting) const;
+  void* GetSettingOptionsFiller(const CSetting *setting);
+    
+  bool GetBool(const std::string &id) const;
+  int GetInt(const std::string &id) const;
+  double GetNumber(const std::string &id) const;
+  std::string GetString(const std::string &id) const;
+
+  bool SetBool(const std::string &id, bool value);
+  bool ToggleBool(const std::string &id);
+  bool SetInt(const std::string &id, int value);
+  bool SetNumber(const std::string &id, double value);
+  bool SetString(const std::string &id, const std::string &value);
+
+  bool LoadSetting(const TiXmlNode *node, const std::string &settingId);
 
 private:
-  // implementation of ISettingsHandler
-  virtual bool OnSettingsLoading();
-  virtual void OnSettingsLoaded();
-  virtual bool OnSettingsSaving() const;
-  virtual void OnSettingsSaved() const;
-  virtual void OnSettingsCleared();
+  CSettings(const CSettings&);
+  CSettings const& operator=(CSettings const&);
 
-  // implementation of ISubSettings
-  virtual bool Load(const TiXmlNode *settings);
-  virtual bool Save(TiXmlNode *settings) const;
-
+  bool Initialize(const std::string &file);
+  bool InitializeDefinitions();
+  void InitializeSettingTypes();
+  void InitializeVisibility();
+  void InitializeDefaults();
+  void InitializeOptionFillers();
+  void InitializeConditions();
+  void InitializeISettingsHandlers();
+  void InitializeISubSettings();
+  void InitializeISettingCallbacks();
+  bool Reset();
+  
+  bool m_initialized;
+  CSettingsManager *m_settingsManager;
   CCriticalSection m_critical;
-  typedef std::set<ISettingsHandler*> SettingsHandlers;
-  SettingsHandlers m_settingsHandlers;
-  typedef std::set<ISubSettings*> SubSettings;
-  SubSettings m_subSettings;
 };
-
-extern class CSettings g_settings;
