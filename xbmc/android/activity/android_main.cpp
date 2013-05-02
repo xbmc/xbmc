e602582e77f69735c317eb772733264716c5f5c7
Cory Fields <theuni-nospam-@xbmc.org>
2013-05-02 18:17:42 -0400

    jni: simplify android_main
    We no longer have to send this stuff to our jni handlers.
    
    Also scope the objects we create so that they are explicitly destroyed before
    we exit.

--- xbmc/android/activity/android_main.cpp
+++ xbmc/android/activity/android_main.cpp
@@ -19,25 +19,15 @@
  */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <dlfcn.h>
-
 #include <android_native_app_glue.h>
-#include <jni.h>
-#include "JNIThreading.h"
-
 #include "EventLoop.h"
 #include "XBMCApp.h"
-#include "JNIManager.h"
-#include <android/log.h>
 
 extern void android_main(struct android_app* state)
 {
+{
   // make sure that the linker doesn't strip out our glue
   app_dummy();
-  int val = xbmc_jni_on_load(state->activity->vm, state->activity->env);
-  __android_log_print(ANDROID_LOG_VERBOSE, "XBMC","xbmc_jni_on_load returned %i\n",val);
-  CAndroidJNIManager::GetInstance().SetActivityInstance(state->activity->clazz);
   CEventLoop eventLoop(state);
   CXBMCApp xbmcApp(state->activity);
   if (xbmcApp.isValid())
@@ -54,6 +44,6 @@ extern void android_main(struct android_app* state)
   // those loaded libs in the state they were in when we quit XBMC last time
   // which will lead to crashes because of global/static classes that haven't
   // been properly uninitialized
-  xbmc_jni_on_unload();
+}
   exit(0);
 }
