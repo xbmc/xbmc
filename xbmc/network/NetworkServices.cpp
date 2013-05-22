44d8e8a2cc185b5a36bdde39f114c9886ebaacca
ulion <ulion2002@gmail.com>
2013-05-22 17:27:33 +0800

    [Fix] Airplay can work without airtunes server.

--- xbmc/network/NetworkServices.cpp
+++ xbmc/network/NetworkServices.cpp
@@ -215,15 +215,13 @@ bool CNetworkServices::OnSettingChanging(const CSetting *setting)
         return false;
       }
 
-      if (!StartAirTunesServer())
-      {
-        CGUIDialogOK::ShowAndGetInput(g_localizeStrings.Get(1274), "", g_localizeStrings.Get(33100), "");
-        return false;
-      }
+      StartAirTunesServer();
     }
     else
     {
-      if (!StopAirPlayServer(true) || !StopAirTunesServer(true))
+      bool ret = StopAirPlayServer(true);
+      StopAirTunesServer(true);
+      if (!ret)
         return false;
     }
   }
