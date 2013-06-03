79a7103fb5aa1663df865913c5e8b7c6bd256b87
Rawk <mattias@klingawallin.se>
2013-06-03 20:37:19 +0200

    Remove broken usage of CSettingsCategory & CSettings

--- xbmc/utils/test/TestSystemInfo.cpp
+++ xbmc/utils/test/TestSystemInfo.cpp
@@ -29,7 +29,7 @@ class TestSystemInfo : public testing::Test
 protected:
   TestSystemInfo()
   {
-    CSettingsCategory* net = CSettings::Get().AddCategory(4, "network", 798);
+/*    CSettingsCategory* net = CSettings::Get().AddCategory(4, "network", 798);
     CSettings::Get().AddBool(net, "network.usehttpproxy", 708, false);
     CSettings::Get().AddString(net, "network.httpproxyserver", 706, "",
                             EDIT_CONTROL_INPUT);
@@ -40,11 +40,11 @@ protected:
     CSettings::Get().AddString(net, "network.httpproxypassword", 733, "",
                             EDIT_CONTROL_HIDDEN_INPUT,true,733);
     CSettings::Get().AddInt(net, "network.bandwidth", 14041, 0, 0, 512, 100*1024,
-                         SPIN_CONTROL_INT_PLUS, 14048, 351);
+                         SPIN_CONTROL_INT_PLUS, 14048, 351);*/
   }
   ~TestSystemInfo()
   {
-    CSettings::Get().Clear();
+//    CSettings::Get().Clear();
   }
 };
 
