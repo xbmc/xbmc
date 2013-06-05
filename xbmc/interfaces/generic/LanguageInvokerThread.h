adba3b0a7cc37d81243e9762744f734f94c251a8
montellese <montellese@xbmc.org>
2013-06-06 00:09:27 +0200

    interfaces: add ILanguageInvoker and ScriptInvocationManager

---
+++ xbmc/interfaces/generic/LanguageInvokerThread.h
@@ -0,0 +1,49 @@
+#pragma once
+/*
+ *      Copyright (C) 2013 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "ILanguageInvoker.h"
+#include "threads/Thread.h"
+
+class CScriptInvocationManager;
+
+class CLanguageInvokerThread : public ILanguageInvoker, protected CThread
+{
+public:
+  CLanguageInvokerThread(ILanguageInvoker *invoker, CScriptInvocationManager *invocationManager);
+  ~CLanguageInvokerThread();
+
+  virtual InvokerState GetState();
+
+protected:
+  virtual bool execute(const std::string &script, const std::vector<std::string> &arguments);
+  virtual bool stop(bool wait);
+
+  virtual void OnStartup();
+  virtual void Process();
+  virtual void OnExit();
+  virtual void OnException();
+
+private:
+  ILanguageInvoker *m_invoker;
+  CScriptInvocationManager *m_invocationManager;
+  std::string m_script;
+  std::vector<std::string> m_args;
+};
---
+++ xbmc/interfaces/generic/ScriptInvocationManager.h
@@ -0,0 +1,80 @@
+#pragma once
+/*
+ *      Copyright (C) 2013 Team XBMC
+ *      http://www.xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <map>
+#include <set>
+#include <boost/shared_ptr.hpp>
+
+#include "addons/IAddon.h"
+#include "threads/CriticalSection.h"
+
+class ILanguageInvocationHandler;
+class ILanguageInvoker;
+class CLanguageInvokerThread;
+typedef boost::shared_ptr<CLanguageInvokerThread> CLanguageInvokerThreadPtr;
+
+class CScriptInvocationManager
+{
+public:
+  static CScriptInvocationManager& Get();
+
+  void Process();
+  void Uninitialize();
+
+  void RegisterLanguageInvocationHandler(ILanguageInvocationHandler *invocationHandler, const std::string &extension);
+  void RegisterLanguageInvocationHandler(ILanguageInvocationHandler *invocationHandler, const std::set<std::string> &extensions);
+  void UnregisterLanguageInvocationHandler(ILanguageInvocationHandler *invocationHandler);
+  bool HasLanguageInvoker(const std::string &script) const;
+  ILanguageInvoker* GetLanguageInvoker(const std::string &script) const;
+
+  int Execute(const std::string &script, const ADDON::AddonPtr &addon = ADDON::AddonPtr(), const std::vector<std::string> &arguments = std::vector<std::string>());
+  bool Stop(int scriptId, bool wait = false);
+  bool Stop(const std::string &scriptPath, bool wait = false);
+
+  bool IsRunning(int scriptId) const;
+
+protected:
+  friend class CLanguageInvokerThread;
+
+  void OnScriptEnded(int scriptId);
+
+private:
+  CScriptInvocationManager();
+  CScriptInvocationManager(const CScriptInvocationManager&);
+  CScriptInvocationManager const& operator=(CScriptInvocationManager const&);
+  virtual ~CScriptInvocationManager();
+
+  typedef struct {
+    CLanguageInvokerThreadPtr thread;
+    std::string script;
+    bool done;
+  } LanguageInvokerThread;
+  typedef std::map<int, LanguageInvokerThread> LanguageInvokerThreadMap;
+  typedef std::map<std::string, ILanguageInvocationHandler*> LanguageInvocationHandlerMap;
+
+  LanguageInvokerThread getInvokerThread(int scriptId) const;
+
+  LanguageInvocationHandlerMap m_invocationHandlers;
+  LanguageInvokerThreadMap m_scripts;
+  std::map<std::string, int> m_scriptPaths;
+  int m_nextId;
+  CCriticalSection m_critSection;
+};
