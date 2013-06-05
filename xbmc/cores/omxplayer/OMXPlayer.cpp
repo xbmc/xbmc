352862d1c2d612b930bbd2ea1b487b96878ad333
Franz Koch <franz.koch@elements-net.de>
2013-06-05 22:57:01 +0200

    [3D] move stereomode detection from filename to stereoscopicsmanager and make the flags configurable via advanced settings.

--- xbmc/cores/omxplayer/OMXPlayer.cpp
+++ xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -81,6 +81,7 @@
 #include "utils/URIUtils.h"
 #include "GUIInfoManager.h"
 #include "guilib/GUIWindowManager.h"
+#include "guilib/StereoscopicsManager.h"
 #include "Application.h"
 #include "ApplicationMessenger.h"
 #include "DVDPerformanceCounter.h"
@@ -3181,6 +3182,9 @@ bool COMXPlayer::OpenVideoStream(int iStream, int source, bool reset)
   if(pMenus && pMenus->IsInMenu())
     hint.stills = true;
 
+  if (hint.stereo_mode.empty())
+    hint.stereo_mode = CStereoscopicsManager::Get().DetectStereoModeByString(m_filename);
+
   if(m_CurrentVideo.id    < 0
   || m_CurrentVideo.hint != hint)
   {
@@ -3204,13 +3208,6 @@ bool COMXPlayer::OpenVideoStream(int iStream, int source, bool reset)
   }
   else if (reset)
     m_omxPlayerVideo.SendMessage(new CDVDMsg(CDVDMsg::GENERAL_RESET));
-
-  unsigned flags = 0;
-  if(m_filename.find("3DSBS") != string::npos || m_filename.find("HSBS") != string::npos)
-    flags = CONF_FLAGS_STEREO_MODE_SBS;
-  else if(m_filename.find("3DTAB") != string::npos || m_filename.find("HTAB") != string::npos)
-    flags = CONF_FLAGS_STEREO_MODE_TAB;
-  m_omxPlayerVideo.SetFlags(flags);
 
   /* store information about stream */
   m_CurrentVideo.id = iStream;
