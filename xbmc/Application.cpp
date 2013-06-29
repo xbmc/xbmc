xbmc/xbmc

commit 8768ec8a7260b7dd46c28e601a398d493808edeb

Move player action handling out of GUIDialogVideoOSD and GUIWindowFullScreen into Application

xbmc/Application.cpp
@@ -2390,14 +2390,6 @@ bool CApplication::OnAction(const CAction &action)
       return OnAction(CAction(ACTION_PLAYER_PLAY));
   }
 
-  // Now check with the playlist player if action can be handled.
-  // In case of the action PREV_ITEM, we only allow the playlist player to take it if we're less than 3 seconds into playback.
-  if (!(action.GetID() == ACTION_PREV_ITEM && m_pPlayer && m_pPlayer->CanSeek() && GetTime() > 3) )
-  {
-    if (g_playlistPlayer.OnAction(action))
-      return true;
-  }
-
   //if the action would start or stop inertial scrolling
   //by gesture - bypass the normal OnAction handler of current window
   if( !m_pInertialScrollingHandler->CheckForInertialScrolling(&action) )
@@ -2481,6 +2473,18 @@ bool CApplication::OnAction(const CAction &action)
     }
     return true;
   }
+
+  // Now check with the playlist player if action can be handled.
+  // In case of the action PREV_ITEM, we only allow the playlist player to take it if we're less than 3 seconds into playback.
+  if (!(action.GetID() == ACTION_PREV_ITEM && m_pPlayer && m_pPlayer->CanSeek() && GetTime() > 3) )
+  {
+    if (g_playlistPlayer.OnAction(action))
+      return true;
+  }
+
+  // Now check with the player if action can be handled.
+  if (m_pPlayer != NULL && m_pPlayer->OnAction(action))
+    return true;
 
   // stop : stops playing current audio song
   if (action.GetID() == ACTION_STOP)

xbmc/video/dialogs/GUIDialogVideoOSD.cpp
@@ -62,12 +62,6 @@ void CGUIDialogVideoOSD::FrameMove()
 
 bool CGUIDialogVideoOSD::OnAction(const CAction &action)
 {
-  if (action.GetID() == ACTION_NEXT_ITEM || action.GetID() == ACTION_PREV_ITEM || action.GetID() == ACTION_CHANNEL_UP || action.GetID() == ACTION_CHANNEL_DOWN)
-  {
-    // these could indicate next chapter if video supports it
-    if (g_application.m_pPlayer != NULL && g_application.m_pPlayer->OnAction(action))
-      return true;
-  }
   if (action.GetID() == ACTION_SHOW_OSD)
   {
     Close();

xbmc/video/windows/GUIWindowFullScreen.cpp
@@ -125,9 +125,6 @@ CGUIWindowFullScreen::~CGUIWindowFullScreen(void)
 
 bool CGUIWindowFullScreen::OnAction(const CAction &action)
 {
-  if (g_application.m_pPlayer != NULL && g_application.m_pPlayer->OnAction(action))
-    return true;
-
   if (m_timeCodePosition > 0 && action.GetButtonCode())
   { // check whether we have a mapping in our virtual videotimeseek "window" and have a select action
     CKey key(action.GetButtonCode());

commit ba636950960c0242345293fb2635e28f2e4f3412

[fix] check active window before delegating player action handling - fixes problem introduced in 8768ec8a7260b7dd46c28e601a398d493808edeb

xbmc/Application.cpp
@@ -2483,8 +2483,12 @@ bool CApplication::OnAction(const CAction &action)
   }
 
   // Now check with the player if action can be handled.
-  if (m_pPlayer != NULL && m_pPlayer->OnAction(action))
-    return true;
+  if (g_windowManager.GetActiveWindow() == WINDOW_FULLSCREEN_VIDEO ||
+      g_windowManager.GetActiveWindow() == WINDOW_DIALOG_VIDEO_OSD && (action.GetID() == ACTION_NEXT_ITEM || action.GetID() == ACTION_PREV_ITEM || action.GetID() == ACTION_CHANNEL_UP || action.GetID() == ACTION_CHANNEL_DOWN))
+  {
+    if (m_pPlayer != NULL && m_pPlayer->OnAction(action))
+      return true;
+  }
 
   // stop : stops playing current audio song
   if (action.GetID() == ACTION_STOP)