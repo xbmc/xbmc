xbmc/xbmc

commit 5ca72dbcd4b41513892e5fb602f00eb784d54a52

Update AMLPlayer.cpp
Proper fixed as suggested by davilla and  Arne

xbmc/cores/amlplayer/AMLPlayer.cpp
@@ -1800,7 +1800,7 @@ int CAMLPlayer::UpdatePlayerInfo(int pid, player_info_t *info)
   // we get called when status changes or after update time expires.
   // static callback from libamplayer, since it does not pass an opaque,
   // we have to retreve our player class reference the hard way.
-  CAMLPlayer *amlplayer = dynamic_cast<CAMLPlayer*>(g_application.m_pPlayer);
+  CAMLPlayer *amlplayer = boost::dynamic_cast<CAMLPlayer*>(g_application.m_pPlayer);
   if (amlplayer)
   {
     CSingleLock lock(amlplayer->m_aml_state_csection);

commit d274a2d761382806217e71075547764a1b1c9349

fixed broken build from 5ca72dbcd4b41513892e5fb602f00eb784d54a52

xbmc/cores/amlplayer/AMLPlayer.cpp
@@ -1800,7 +1800,7 @@ int CAMLPlayer::UpdatePlayerInfo(int pid, player_info_t *info)
   // we get called when status changes or after update time expires.
   // static callback from libamplayer, since it does not pass an opaque,
   // we have to retreve our player class reference the hard way.
-  CAMLPlayer *amlplayer = boost::dynamic_cast<CAMLPlayer*>(g_application.m_pPlayer);
+  boost::shared_ptr<CAMLPlayer> amlplayer = boost::dynamic_pointer_cast<CAMLPlayer>(g_application.m_pPlayer);
   if (amlplayer)
   {
     CSingleLock lock(amlplayer->m_aml_state_csection);