a58360d6d97ebd721be6c43514b382e29f6f5e69
M. Kaijser <mcm.kaijser@gmail.com>
2013-08-01 15:07:23 +0200

    [cosemetic] update copyright headers

--- tools/depends/target/android-sources-ics/libgui.cpp
+++ tools/depends/target/android-sources-ics/libgui.cpp
@@ -1,6 +1,6 @@
 /*
- *      Copyright (C) 2010-2012 Team XBMC
- *      http://www.xbmc.org
+ *      Copyright (C) 2010-2013 Team XBMC
+ *      http://xbmc.org
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
