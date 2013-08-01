8b212b86755b4e63ca0529a2360c8de36cebb693
Fice <fice@arcor.de>
2013-08-01 22:19:07 +0200

    [FIX] CEC: end of non-void reached

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -1703,6 +1703,7 @@ bool CPeripheralCecAdapter::ToggleDeviceState(CecStateChange mode /*= STATE_SWIT
     ActivateSource();
     return true;
   }
+  return m_cecAdapter->IsLibCECActiveSource();
 }
 
 #endif
