2c8b6466b9e1b56090a05a7461c2f6060b24d255
John Rennie <john.rennie@ratsauce.co.uk>
2013-08-02 10:38:54 +0100

    Win32: change GUIDialogKeyboardGeneric to use g_Windowing.GetClipboard

--- xbmc/dialogs/GUIDialogKeyboardGeneric.cpp
+++ xbmc/dialogs/GUIDialogKeyboardGeneric.cpp
@@ -758,20 +758,7 @@ void CGUIDialogKeyboardGeneric::OnPasteClipboard(void)
   if (szStr)
     pasted_text = szStr;
 #elif defined TARGET_WINDOWS
-  if (OpenClipboard(NULL))
-  {
-    HGLOBAL hglb = GetClipboardData(CF_UNICODETEXT);
-    if (hglb != NULL)
-    {
-      LPWSTR lpwstr = (LPWSTR) GlobalLock(hglb);
-      if (lpwstr != NULL)
-      {
-        pasted_text = lpwstr;
-        GlobalUnlock(hglb);
-      }
-    }
-    CloseClipboard();
-  }
+  pasted_text = g_Windowing.GetClipboard();
 #endif
 
   // Insert the pasted text at the current cursor position.
