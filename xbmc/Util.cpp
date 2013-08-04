f38268ebe619b608912fbb742b6c84bef901425d
Karlson2k <k2k@narod.ru>
2013-08-05 03:58:52 +0400

    CUtil::GetHomePath fixes: for win32 expand wide string instead of UTF8 For all platforms: get executable path only when needed

--- xbmc/Util.cpp
+++ xbmc/Util.cpp
@@ -389,23 +389,29 @@ void CUtil::RunShortcut(const char* szShortcutPath)
 
 void CUtil::GetHomePath(CStdString& strPath, const CStdString& strTarget)
 {
-  CStdString strHomePath;
-  strHomePath = ResolveExecutablePath();
   strPath = CEnvironment::getenv(strTarget);
 
+#ifdef TARGET_WINDOWS
   if (!strPath.IsEmpty())
   {
-#ifdef TARGET_WINDOWS
-    char tmp[1024];
     //expand potential relative path to full path
-    if(GetFullPathName(strPath, 1024, tmp, 0) != 0)
+    CStdStringW strPathW;
+    g_charsetConverter.utf8ToW(strPath, strPathW, false);
+    const int bufSize = GetFullPathNameW(strPathW, 0, NULL, NULL);
+    if (bufSize > 0)
     {
-      strPath = tmp;
+      wchar_t * buf = new wchar_t[bufSize];
+      if (GetFullPathNameW(strPathW, bufSize, buf, NULL) == bufSize-1)
+        g_charsetConverter.wToUTF8(buf, strPath);
+
+      delete [] buf;
     }
-#endif
   }
-  else
+#endif
+
+  if (strPath.IsEmpty())
   {
+    CStdString strHomePath = ResolveExecutablePath();
 #if defined(TARGET_DARWIN)
     int      result = -1;
     char     given_path[2*MAXPATHLEN];
