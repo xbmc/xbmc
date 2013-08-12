ada43b2dd1255eec9cae8f3c061fbf138346d33a
unknown <fernetmenta@online.de>
2013-08-12 09:40:38 +0200

    ActiveAE: engage transcode for stereo if upmix is selected

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -1172,7 +1172,7 @@ void CActiveAE::ApplySettingsToFormat(AEAudioFormat &format, AudioSettings &sett
            settings.ac3passthrough &&
            (!settings.multichannellpcm || (m_settings.mode != AUDIO_HDMI)) && 
            !m_streams.empty() &&
-           format.m_channelLayout.Count() > 2)
+           (format.m_channelLayout.Count() > 2 || settings.stereoupmix))
   {
     format.m_dataFormat = AE_FMT_AC3;
     format.m_sampleRate = 48000;
