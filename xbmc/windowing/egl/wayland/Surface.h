60eb0fefcd628cee9701e3f97f5d365440c2973b
Sam Spilsbury <smspillaz@gmail.com>
2013-08-17 14:23:09 +0800

    Add wayland object listeners.
    We will need direct access to some wayland objects in testing,
    some of which are only available to the client itself. Add some
    hooks to the client classes to advertise via a singleton via
    any other interested code that some wayland object became available.
    
    Test that we received the xbmc_wayland object after connection and
    sync.

--- xbmc/windowing/egl/wayland/Surface.h
+++ xbmc/windowing/egl/wayland/Surface.h
@@ -19,7 +19,9 @@
  *  <http://www.gnu.org/licenses/>.
  *
  */
+#include <boost/function.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/scoped_ptr.hpp>
 
 struct wl_surface;
 struct wl_callback;
@@ -49,6 +51,28 @@ private:
 
   IDllWaylandClient &m_clientLibrary;
   struct wl_surface *m_surface;
+};
+
+/* This is effectively just a seam for testing purposes so that
+ * we can listen for extra objects that the core implementation might
+ * not necessarily be interested in. It isn't possible to get any
+ * notification from within weston that a surface was created so
+ * we need to rely on the client side in order to do that */
+class WaylandSurfaceListener
+{
+public:
+
+  typedef boost::function<void(Surface &)> Handler;
+  
+  void SetHandler(const Handler &);
+  void SurfaceCreated(Surface &);
+
+  static WaylandSurfaceListener & GetInstance();
+private:
+
+  Handler m_handler;
+  
+  static boost::scoped_ptr<WaylandSurfaceListener> m_instance;
 };
 }
 }
