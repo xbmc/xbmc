f049d5f7ddfd598aded309b1812a793c80241727
Memphiz <memphis@machzwo.de>
2013-09-08 20:22:29 +0200

    [airplay] - fix possible deadlock when stopping picture airplay - thx @ulion

--- xbmc/network/AirPlayServer.cpp
+++ xbmc/network/AirPlayServer.cpp
@@ -39,7 +39,7 @@
 #include "utils/md5.h"
 #include "utils/Variant.h"
 #include "settings/Settings.h"
-#include "guilib/GUIWindowManager.h"
+#include "guilib/Key.h"
 #include "URL.h"
 #include "cores/IPlayer.h"
 #include "interfaces/AnnouncementManager.h"
@@ -924,7 +924,7 @@ int CAirPlayServer::CTCPClient::ProcessRequest( CStdString& responseHeader,
       }
       else //if we are not playing and get the stop request - we just wanna stop picture streaming
       {
-        g_windowManager.PreviousWindow();
+        CApplicationMessenger::Get().SendAction(ACTION_PREVIOUS_MENU);
       }
     }
   }
