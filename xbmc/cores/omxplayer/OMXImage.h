1376f157168a6a1538bc706b277636acab6706c3
popcornmix <popcornmix@gmail.com>
2013-09-12 23:23:39 +0100

    [rbp/omxplayer] Restructure COMXImage
    Tidy up of COMXImage. Separate into file reading, encode and decode classes with separate locks.
    This allows some overlap of functionality which improves the speed.

--- xbmc/cores/omxplayer/OMXImage.h
+++ xbmc/cores/omxplayer/OMXImage.h
@@ -37,72 +37,81 @@
 using namespace XFILE;
 using namespace std;
 
+class COMXImageFile;
+
 class COMXImage
 {
 public:
-  COMXImage();
-  virtual ~COMXImage();
+  static COMXImageFile *LoadJpeg(const CStdString& texturePath);
+  static void CloseJpeg(COMXImageFile *file);
 
-  // Required overrides
-  void Close(void);
-  bool ClampLimits(unsigned int &width, unsigned int &height);
+  static bool DecodeJpeg(COMXImageFile *file, unsigned int maxWidth, unsigned int maxHeight, unsigned int stride, void *pixels);
+  static bool CreateThumbnailFromSurface(unsigned char* buffer, unsigned int width, unsigned int height,
+      unsigned int format, unsigned int pitch, const CStdString& destFile);
+  static bool ClampLimits(unsigned int &width, unsigned int &height, unsigned int m_width, unsigned int m_height, bool transposed = false);
+};
+
+class COMXImageFile
+{
+public:
+  COMXImageFile();
+  virtual ~COMXImageFile();
   bool ReadFile(const CStdString& inputFile);
-  bool IsProgressive() { return m_progressive; };
-  bool IsAlpha() { return m_alpha; };
   int  GetOrientation() { return m_orientation; };
-  unsigned int GetOriginalWidth()  { return m_omx_image.nFrameWidth; };
-  unsigned int GetOriginalHeight() { return m_omx_image.nFrameHeight; };
   unsigned int GetWidth()  { return m_width; };
   unsigned int GetHeight() { return m_height; };
-  OMX_IMAGE_CODINGTYPE GetCodingType();
-  const uint8_t *GetImageBuffer() { return (const uint8_t *)m_image_buffer; };
   unsigned long GetImageSize() { return m_image_size; };
-  OMX_IMAGE_CODINGTYPE GetCompressionFormat() { return m_omx_image.eCompressionFormat; };
-  bool Decode(unsigned int width, unsigned int height);
-  bool Encode(unsigned char *buffer, int size, unsigned int width, unsigned int height, unsigned int pitch);
-  unsigned int GetDecodedWidth() { return (unsigned int)m_decoded_format.format.image.nFrameWidth; };
-  unsigned int GetDecodedHeight() { return (unsigned int)m_decoded_format.format.image.nFrameHeight; };
-  unsigned int GetDecodedStride() { return (unsigned int)m_decoded_format.format.image.nStride; };
-  unsigned char *GetDecodedData();
-  unsigned int GetDecodedSize();
-  unsigned int GetEncodedWidth() { return (unsigned int)m_encoded_format.format.image.nFrameWidth; };
-  unsigned int GetEncodedHeight() { return (unsigned int)m_encoded_format.format.image.nFrameHeight; };
-  unsigned int GetEncodedStride() { return (unsigned int)m_encoded_format.format.image.nStride; };
-  unsigned char *GetEncodedData();
-  unsigned int GetEncodedSize();
-  bool SwapBlueRed(unsigned char *pixels, unsigned int height, unsigned int pitch, 
-      unsigned int elements = 4, unsigned int offset=0);
-  bool CreateThumbnail(const CStdString& sourceFile, const CStdString& destFile, 
-      int minx, int miny, bool rotateExif);
-  bool CreateThumbnailFromMemory(unsigned char* buffer, unsigned int bufSize, 
-      const CStdString& destFile, unsigned int minx, unsigned int miny);
-  bool CreateThumbnailFromSurface(unsigned char* buffer, unsigned int width, unsigned int height, 
-      unsigned int format, unsigned int pitch, const CStdString& destFile);
+  const uint8_t *GetImageBuffer() { return (const uint8_t *)m_image_buffer; };
+  const char *GetFilename() { return m_filename; };
 protected:
-  bool HandlePortSettingChange(unsigned int resize_width, unsigned int resize_height);
+  OMX_IMAGE_CODINGTYPE GetCodingType(unsigned int &width, unsigned int &height);
   uint8_t           *m_image_buffer;
-  bool              m_is_open;
   unsigned long     m_image_size;
   unsigned int      m_width;
   unsigned int      m_height;
-  bool              m_progressive;
-  bool              m_alpha;
   int               m_orientation;
-  XFILE::CFile      m_pFile;
-  OMX_IMAGE_PORTDEFINITIONTYPE  m_omx_image;
+  const char *      m_filename;
+};
 
+class COMXImageDec
+{
+public:
+  COMXImageDec();
+  virtual ~COMXImageDec();
+
+  // Required overrides
+  void Close();
+  bool Decode(const uint8_t *data, unsigned size, unsigned int width, unsigned int height, unsigned stride, void *pixels);
+  unsigned int GetDecodedWidth() { return (unsigned int)m_decoded_format.format.image.nFrameWidth; };
+  unsigned int GetDecodedHeight() { return (unsigned int)m_decoded_format.format.image.nFrameHeight; };
+  unsigned int GetDecodedStride() { return (unsigned int)m_decoded_format.format.image.nStride; };
+protected:
+  bool HandlePortSettingChange(unsigned int resize_width, unsigned int resize_height);
   // Components
   COMXCoreComponent             m_omx_decoder;
-  COMXCoreComponent             m_omx_encoder;
   COMXCoreComponent             m_omx_resize;
   COMXCoreTunel                 m_omx_tunnel_decode;
   OMX_BUFFERHEADERTYPE          *m_decoded_buffer;
-  OMX_BUFFERHEADERTYPE          *m_encoded_buffer;
   OMX_PARAM_PORTDEFINITIONTYPE  m_decoded_format;
-  OMX_PARAM_PORTDEFINITIONTYPE  m_encoded_format;
+  CCriticalSection              m_OMXSection;
+};
 
-  bool                          m_decoder_open;
-  bool                          m_encoder_open;
+class COMXImageEnc
+{
+public:
+  COMXImageEnc();
+  virtual ~COMXImageEnc();
+
+  // Required overrides
+  bool CreateThumbnailFromSurface(unsigned char* buffer, unsigned int width, unsigned int height,
+      unsigned int format, unsigned int pitch, const CStdString& destFile);
+protected:
+  bool Encode(unsigned char *buffer, int size, unsigned int width, unsigned int height, unsigned int pitch);
+  // Components
+  COMXCoreComponent             m_omx_encoder;
+  OMX_BUFFERHEADERTYPE          *m_encoded_buffer;
+  OMX_PARAM_PORTDEFINITIONTYPE  m_encoded_format;
+  CCriticalSection              m_OMXSection;
 };
 
 #endif
