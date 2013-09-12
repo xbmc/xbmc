24353db3001bd5c0865b2d1622ae8ecc1aa04acb
Tobias Hieta <tobias@plexapp.com>
2013-09-12 09:00:27 -1000

    Another stupid crashbug.

--- plex/FileSystem/PlexAttributeParser.cpp
+++ plex/FileSystem/PlexAttributeParser.cpp
@@ -88,7 +88,7 @@ void CPlexAttributeParserMediaUrl::Process(const CURL &url, const CStdString &ke
   /* reset the options to not bust cache stuff */
   mediaUrl.SetOptions("");
 
-  if (mediaUrl.GetHostName() == "myplex")
+  if (mediaUrl.GetHostName() == "myplex" && g_plexApplication.serverManager)
   {
     CPlexServerPtr bestServer = g_plexApplication.serverManager->GetBestServer();
     if (bestServer)
