1b8c370c3e9f723c244e3f87ea623d25d65d0d6b
davilla <davilla@4pi.com>
2013-10-08 14:01:51 -0400

    fixed, runtime determined settings need OnSettingUpdate callback handlers

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -911,6 +911,8 @@ void CSettings::InitializeISettingCallbacks()
   settingSet.insert("screensaver.settings");
   settingSet.insert("videoscreen.guicalibration");
   settingSet.insert("videoscreen.testpattern");
+  settingSet.insert("videoplayer.useamcodec");
+  settingSet.insert("videoplayer.usemediacodec");
   m_settingsManager->RegisterCallback(&g_application, settingSet);
 
   settingSet.clear();
