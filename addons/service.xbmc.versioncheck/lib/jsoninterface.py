e4f0689f793e39d1af5b98f79d6c25f411806fba
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-10-16 15:21:01 +0200

    [addons] sync addons to repo

---
+++ addons/service.xbmc.versioncheck/lib/jsoninterface.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2013 Team-XBMC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import sys
+import xbmc
+import xbmcaddon
+import xbmcvfs
+import lib.common
+from lib.common import log
+
+__addonpath__    = lib.common.__addonpath__
+
+import json as jsoninterface
+
+def get_installedversion():
+    # retrieve current installed version
+    json_query = xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Application.GetProperties", "params": {"properties": ["version", "name"]}, "id": 1 }')
+    json_query = unicode(json_query, 'utf-8', errors='ignore')
+    json_query = jsoninterface.loads(json_query)
+    version_installed = []
+    if json_query.has_key('result') and json_query['result'].has_key('version'):
+        version_installed  = json_query['result']['version']
+        log("Version installed %s" %version_installed)
+    return version_installed
+    
+def get_versionfilelist():
+    # retrieve versionlists from supplied version file
+    version_file = os.path.join(__addonpath__, 'resources/versions.txt')
+    # Eden didn't have xbmcvfs.File()
+    if xbmcaddon.Addon('xbmc.addon').getAddonInfo('version') < "11.9.3":
+        file = open(version_file, 'r')
+    else:
+        file = xbmcvfs.File(version_file)
+    data = file.read()
+    file.close()
+    version_query = unicode(data, 'utf-8', errors='ignore')
+    version_query = jsoninterface.loads(version_query)
+    return version_query
---
+++ addons/service.xbmc.versioncheck/lib/shellhandlerapt.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+#
+#     Copyright (C) 2013 Team-XBMC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+import xbmc
+from common import *
+
+try:
+    from subprocess import check_output
+    from subprocess import call
+except:
+    log('subprocess import error')
+
+
+class ShellHandlerApt:
+
+    _pwd = ""
+
+    def __init__(self, usesudo=False):
+        self.sudo = usesudo
+        installed, candidate = self._check_versions("xbmc", False)
+        if not installed:
+            # there is no package installed via repo, so we exit here
+            log("No installed package found, exiting")
+            import sys
+            sys.exit(0)
+
+    def _check_versions(self, package, update=True):
+        _cmd = "apt-cache policy " + package
+
+        if update and not self._update_cache():
+            return False, False
+
+        try:
+            result = check_output([_cmd], shell=True).split("\n")
+        except Exception as error:
+            log("ShellHandlerApt: exception while executing shell command %s: %s" %(_cmd, error))
+            return False, False
+
+        if result[0].replace(":", "") == package:
+            installed = result[1].split()[1]
+            candidate = result[2].split()[1]
+            if installed == "(none)":
+                installed = False
+            if candidate == "(none)":
+                candiate = False
+            return installed, candidate
+        else:
+            log("ShellHandlerApt: error during version check")
+            return False, False
+
+    def _update_cache(self):
+        _cmd = 'apt-get update'
+        try:
+            if self.sudo:
+                x = check_output('echo \'%s\' | sudo -S %s' %(self._getpassword(), _cmd), shell=True)
+            else:
+                x = check_output(_cmd.split())
+        except Exception as error:
+            log("Exception while executing shell command %s: %s" %(_cmd, error))
+            return False
+
+        return True
+
+    def check_upgrade_available(self, package):
+        '''returns True if newer package is available in the repositories'''
+        installed, candidate = self._check_versions(package)
+        if installed and candidate:
+            if installed != candidate:
+                log("Version installed  %s" %installed)
+                log("Version available  %s" %candidate)
+                return True
+            else:
+                log("Already on newest version")
+        elif not installed:
+                log("No installed package found")
+                return False
+        else:
+            return False
+
+    def upgrade_package(self, package):
+        _cmd = "apt-get install -y " + package
+        try:
+            if self.sudo:
+                x = check_output('echo \'%s\' | sudo -S %s' %(self._getpassword(), _cmd), shell=True)
+            else:
+                x = check_output(_cmd.split())
+            log("Upgrade successful")
+        except Exception as error:
+            log("Exception while executing shell command %s: %s" %(_cmd, error))
+            return False
+
+        return True
+
+    def upgrade_system(self):
+        _cmd = "apt-get upgrade -y"
+        try:
+            log("Upgrading system")
+            if self.sudo:
+                x = check_output('echo \'%s\' | sudo -S %s' %(self._getpassword(), _cmd), shell=True)
+            else:
+                x = check_output(_cmd.split())
+        except Exception as error:
+            log("Exception while executing shell command %s: %s" %(_cmd, error))
+            return False
+
+        return True
+
+    def _getpassword(self):
+        if len(self._pwd) == 0:
+            self._pwd = get_password_from_user()
+        return self._pwd
