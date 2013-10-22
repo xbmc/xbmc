b79897c90d38e9b3dcf5b742b96c6db65805c8bd
Jonathan Marshall <jmarshall@xbmc.org>
2013-10-22 13:53:02 +1300

    fix compile warnings

--- xbmc/utils/RegExp.cpp
+++ xbmc/utils/RegExp.cpp
@@ -465,7 +465,7 @@ bool CRegExp::LogCheckUtf8Support(void)
   {
     CLog::Log(LOGNOTICE, "Consider installing PCRE lib version 8.10 or later with enabled Unicode properties and UTF-8 support. Your PCRE lib version: %s", PCRE::pcre_version());
 #if PCRE_UCP == 0
-    CLog::Log(LOGNOTICE, "You will need to rebuild XBMC after PCRE lib update.", PCRE::pcre_version());
+    CLog::Log(LOGNOTICE, "You will need to rebuild XBMC after PCRE lib update.");
 #endif
   }
 
