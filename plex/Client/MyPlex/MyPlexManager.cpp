2656c4e57bdd840a0c91959cc82303b5e9943dad
mmccurdy07 <mmccurdy@gmail.com>
2013-11-01 18:43:15 -0700

    Typo in "signed in" toast.  Fix #743.

--- plex/Client/MyPlex/MyPlexManager.cpp
+++ plex/Client/MyPlex/MyPlexManager.cpp
@@ -84,7 +84,7 @@ void CMyPlexManager::BroadcastState()
     case STATE_LOGGEDIN:
     {
       g_guiSettings.SetString("myplex.status", g_localizeStrings.Get(44011) + " (" + CStdString(m_currentUserInfo.username) + ")");
-      CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, "Logged into to myPlex", m_currentUserInfo.username, 5000, false);
+      CGUIDialogKaiToast::QueueNotification(CGUIDialogKaiToast::Info, g_localizeStrings.Get(44105), m_currentUserInfo.username, 5000, false);
       break;
     }
     case STATE_NOT_LOGGEDIN:
