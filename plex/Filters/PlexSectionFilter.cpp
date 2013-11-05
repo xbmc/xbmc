b8458e0642f5c0ce36aa2d26250efcffb6873709
Tobias Hieta <tobias@plexapp.com>
2013-11-05 16:51:20 +0100

    Localize Queue & Recommended

--- plex/Filters/PlexSectionFilter.cpp
+++ plex/Filters/PlexSectionFilter.cpp
@@ -8,6 +8,8 @@
 #include "PlexApplication.h"
 #include "Client/PlexServerDataLoader.h"
 
+#include "LocalizeStrings.h"
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 CPlexSectionFilter::CPlexSectionFilter(const CURL &sectionUrl) : m_sectionUrl(sectionUrl)
 {
@@ -184,8 +186,8 @@ CPlexMyPlexPlaylistFilter::CPlexMyPlexPlaylistFilter(const CURL &sectionUrl) : C
   m_currentPrimaryFilter = "queue";
   m_currentSortOrder = "";
 
-  m_primaryFilters["queue"] = "Queue";
-  m_primaryFilters["recommendations"] = "Recommended";
+  m_primaryFilters["queue"] = g_localizeStrings.Get(44021);
+  m_primaryFilters["recommendations"] = g_localizeStrings.Get(44022);
 
   CPlexSecondaryFilterPtr unwatchedFilter = CPlexSecondaryFilterPtr(new CPlexSecondaryFilter("Unwatched", "unwatched", "Unwatched", CPlexSecondaryFilter::FILTER_TYPE_BOOLEAN));
   m_secondaryFilters["unwatched"] = unwatchedFilter;
