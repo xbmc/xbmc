378971b493e1a086f5f95414aad24834e3aac36e
Lars Op den Kamp <lars@opdenkamp.eu>
2013-11-09 02:33:46 +0100

    [pvr] bump api to 1.9.0

--- xbmc/addons/include/xbmc_pvr_types.h
+++ xbmc/addons/include/xbmc_pvr_types.h
@@ -75,10 +75,10 @@ struct DemuxPacket;
 #define PVR_STREAM_MAX_STREAMS 20
 
 /* current PVR API version */
-#define XBMC_PVR_API_VERSION "1.8.1"
+#define XBMC_PVR_API_VERSION "1.9.0"
 
 /* min. PVR API version */
-#define XBMC_PVR_MIN_API_VERSION "1.8.0"
+#define XBMC_PVR_MIN_API_VERSION "1.9.0"
 
 #ifdef __cplusplus
 extern "C" {
