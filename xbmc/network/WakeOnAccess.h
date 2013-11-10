67973c3eb22a444940f7de23cbc2457b611c3206
Trent Nelson <trent.a.b.nelson@gmail.com>
2013-11-10 15:09:04 -0500

    Respect results of WOA.

--- xbmc/network/WakeOnAccess.h
+++ xbmc/network/WakeOnAccess.h
@@ -28,8 +28,8 @@ class CWakeOnAccess : private IJobCallback, public ISettingsHandler
 public:
   static CWakeOnAccess &Get();
 
-  void WakeUpHost (const CURL& fileUrl);
-  void WakeUpHost (const CStdString& hostName, const std::string& customMessage);
+  bool WakeUpHost (const CURL& fileUrl);
+  bool WakeUpHost (const CStdString& hostName, const std::string& customMessage);
 
   void QueueMACDiscoveryForAllRemotes();
 
@@ -77,5 +77,5 @@ private:
 
   bool m_enabled;
 
-  void WakeUpHost(const WakeUpEntry& server);
+  bool WakeUpHost(const WakeUpEntry& server);
 };
