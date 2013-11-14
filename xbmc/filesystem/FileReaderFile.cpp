0586b4632abe70b7283ae53fb4ce5348bc5a8e57
Jonathan Marshall <jmarshall@xbmc.org>
2013-11-15 11:27:56 +1300

    [cosmetic] adds comments regarding assumptions of string format

--- xbmc/filesystem/FileReaderFile.cpp
+++ xbmc/filesystem/FileReaderFile.cpp
@@ -43,6 +43,7 @@ CFileReaderFile::~CFileReaderFile()
 //*********************************************************************************************
 bool CFileReaderFile::Open(const CURL& url)
 {
+  // URL is of the form filereader://<foo>
   CStdString strURL = url.Get();
   strURL = strURL.substr(13);
   return m_reader.Open(strURL,READ_CACHED);
