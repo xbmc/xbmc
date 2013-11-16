bc8b4d4253006e12258cac80e049ad7eb5aa47fc
Rainer Hochecker <fernetmenta@online.de>
2013-11-16 07:49:11 +0100

    [pvr] bump add-ons (fix version of addon.gui)

--- project/Win32BuildSetup/buildpvraddons.bat
+++ project/Win32BuildSetup/buildpvraddons.bat
@@ -9,7 +9,7 @@ SET DEPS_DIR=..\BuildDependencies
 SET TMP_DIR=%DEPS_DIR%\tmp
 
 SET LIBNAME=xbmc-pvr-addons
-SET VERSION=54a6b9dfbf7b0f51902f9d4c69d83baa8e47d97a
+SET VERSION=d4cb4d5c0e7fc1a4b9a3b911c3053df178c36944
 SET SOURCE=%LIBNAME%
 SET GIT_URL=git://github.com/opdenkamp/%LIBNAME%.git
 SET SOURCE_DIR=%TMP_DIR%\%SOURCE%
