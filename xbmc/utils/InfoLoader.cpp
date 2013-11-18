fa91832f69ed6aa479e2441c1a01edb36f00806d
Karlson2k <k2k@narod.ru>
2013-11-18 13:21:06 +0400

    fix incorrect time interval comparison in 8e2f2de2015684246603224d4ad8fc9035178307

--- xbmc/utils/InfoLoader.cpp
+++ xbmc/utils/InfoLoader.cpp
@@ -48,7 +48,7 @@ CStdString CInfoLoader::GetInfo(int info)
     m_busy = true;
     CJobManager::GetInstance().AddJob(GetJob(), this);
   }
-  if (m_busy && m_refreshTime + 1000 < CTimeUtils::GetFrameTime())
+  if (m_busy && CTimeUtils::GetFrameTime() - m_refreshTime > 1000)
   {
     return BusyInfo(info);
   }
