f672f4be379ed371540532095ce010c7ada0d40d
Tobias Hieta <tobias@plexapp.com>
2013-11-20 10:54:55 +0100

    Make windows builds much easier

---
+++ plex/scripts/fetch-depends-windows.bat
@@ -0,0 +1,11 @@
+@echo off
+if "%WORKSPACE%"=="" (
+	rem if we don't have a workspace let's just assume we are being executed from plex\scripts
+	set WORKSPACE="%~dp0..\.."
+)
+
+set buildno=4
+rem set sha1=
+
+%WORKSPACE%\project\BuildDependencies\bin\wget.exe -O %WORKSPACE%\plex\Dependencies\windows-i386-xbmc-deps.tar.gz http://nightlies.plexapp.com/plex-dependencies/plex-home-theater-deps-windows/%buildno%/windows-i386-xbmc-deps.tar.gz
+%WORKSPACE%\plex\scripts\tar.exe -C %WORKSPACE% -xzf %WORKSPACE%\plex\Dependencies\windows-i386-xbmc-deps.tar.gz
