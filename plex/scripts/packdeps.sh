b76f5ce3b3820c5d01a7d2ada4da084aa37dda9b
Tobias Hieta <tobias@plexapp.com>
2013-11-20 16:54:36 +0100

    We need to include dump_syms in our dependencies as well.

--- plex/scripts/packdeps.sh
+++ plex/scripts/packdeps.sh
@@ -1,3 +1,3 @@
 cd /xbmc
 files=$(find system -iname *.dll)
-tar -czf /xbmc/upload/windows-i386-xbmc-deps.tar.gz $files project/BuildDependencies/lib project/BuildDependencies/include tools/TexturePacker/*.dll project/Win32BuildSetup/dependencies
+tar -czf /xbmc/upload/windows-i386-xbmc-deps.tar.gz $files project/BuildDependencies/lib project/BuildDependencies/include tools/TexturePacker/*.dll project/Win32BuildSetup/dependencies project/BuildDependencies/bin/dump_syms.exe
