0512476ca6c414f9e81037c19d07af73c7cd2a0c
Tobias Hieta <tobias@plexapp.com>
2013-11-21 12:50:16 +0100

    Add some picture infoporn.

--- plex/FileSystem/PlexDirectoryTypeParserPicture.cpp
+++ plex/FileSystem/PlexDirectoryTypeParserPicture.cpp
@@ -8,16 +8,34 @@
 
 #include "PlexDirectoryTypeParserPicture.h"
 #include "PlexAttributeParser.h"
+#include "Pictures/PictureInfoTag.h"
 
 void CPlexDirectoryTypeParserPicture::Process(CFileItem &item, CFileItem &mediaContainer, TiXmlElement *itemElement)
 {
   ParseMediaNodes(item, itemElement);
   if (item.m_mediaItems.size() > 0 && item.m_mediaItems[0]->m_mediaParts.size() > 0)
   {
+    CFileItemPtr media = item.m_mediaItems[0];
     /* Pass the path URL via the transcoder, because XBMC raw file reader sucks */
     CPlexAttributeParserMediaUrl mUrl;
-    mUrl.Process(item.GetPath(), "picture", item.m_mediaItems[0]->m_mediaParts[0]->GetProperty("unprocessed_key").asString(), &item);
+    mUrl.Process(item.GetPath(), "picture", media->m_mediaParts[0]->GetProperty("unprocessed_key").asString(), &item);
     item.SetPath(item.GetArt("picture"));
-//    CLog::Log(LOGDEBUG, "CPlexDirectoryTypeParserPicture: setting key = %s", item.GetPath().c_str());
+
+    CPictureInfoTag* tag = item.GetPictureInfoTag();
+
+    CStdString res;
+    res.Format("%s,%s", media->GetProperty("width").asString(), media->GetProperty("height").asString());
+    tag->SetInfo(SLIDE_RESOLUTION, res);
+
+    if (media->HasProperty("aperture"))
+      tag->SetInfo(SLIDE_EXIF_APERTURE, media->GetProperty("aperture").asString());
+    if (media->HasProperty("exposure"))
+      tag->SetInfo(SLIDE_EXIF_EXPOSURE, media->GetProperty("exposure").asString());
+    if (media->HasProperty("iso"))
+      tag->SetInfo(SLIDE_EXIF_ISO_EQUIV, media->GetProperty("iso").asString());
+    if (media->HasProperty("make"))
+      tag->SetInfo(SLIDE_EXIF_CAMERA_MAKE, media->GetProperty("make").asString());
+    if (media->HasProperty("model"))
+      tag->SetInfo(SLIDE_EXIF_CAMERA_MODEL, media->GetProperty("model").asString());
   }
 }
