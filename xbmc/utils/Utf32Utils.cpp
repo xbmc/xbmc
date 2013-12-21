1e806702d6d7d8f8c3ce23e7104e78228a981e30
Karlson2k <k2k@narod.ru>
2013-12-21 19:36:29 +0400

    Utf32Utils: add "NaturalCompare()" for comparing string using natural numbers order

--- xbmc/utils/Utf32Utils.cpp
+++ xbmc/utils/Utf32Utils.cpp
@@ -18,7 +18,11 @@
  *
  */
 
+#include <cassert>
+#include <locale>
+#include <algorithm>
 #include "Utf32Utils.h"
+#include "utils/CharsetConverter.h"
 
 const CUtf32Utils::digitsMap CUtf32Utils::m_digitsMap(digitsMapFiller());
 
@@ -37,5 +41,112 @@ bool CUtf32Utils::GetDigitValue(char32_t chr, double& resultValue)
     return true;
   }
   return false;
+}
+
+int CUtf32Utils::NaturalCompare(const std::u32string& left, const std::u32string right)
+{
+  const char32_t* const leftC = left.c_str();
+  const char32_t* const rightC = right.c_str();
+  const size_t leftLen = left.length();
+  const size_t rightLen = right.length();
+  const size_t shortestLen = std::min(leftLen, rightLen);
+  
+  const std::collate<wchar_t>& cl = std::use_facet< std::collate<wchar_t> >(std::locale());
+
+  for (size_t pos = 0; pos < shortestLen; pos++)
+  {
+    if (leftC[pos] != rightC[pos])
+    {
+      double valL, valR;
+      if (GetDigitValue(leftC[pos], valL) && GetDigitValue(rightC[pos], valR))
+      {
+        // FIXME: improve comparison of non-Arabic (and non-Arabic-Indic) numbers
+        size_t posL = pos, posR = pos;
+        bool leftIsDigit = true;  // initial value
+        bool rightIsDigit = true; // initial value
+
+        // check for non-zero digits before current position "pos"
+        size_t lookBackPos = pos;
+        while (lookBackPos-- > 0)
+        {
+          double digitVal;
+          // "left" and "right" are same before "pos", no need to check them both
+          if (!GetDigitValue(leftC[lookBackPos], digitVal))
+          { // no digits or only zero digits before "pos"
+            // skip all leading zero digits in "left"
+            while (leftIsDigit && valL == 0) 
+              leftIsDigit = GetDigitValue(leftC[++posL], valL);
+            // skip all leading zero digits in "right"
+            while (rightIsDigit && valR == 0)
+              rightIsDigit = GetDigitValue(rightC[++posR], valR);
+            break;
+          }
+          if (digitVal != 0)
+            break; // non-zero digit is found before "pos", no need to skip leading zeros
+        }
+
+        const size_t numberOfLeadZerosL = posL - pos; // "pos" is pointing to first different (by code) digit
+        const size_t numberOfLeadZerosR = posR - pos; // "pos" is pointing to first different (by code) digit
+
+        // check if "left" and "right" have any digits after leading zeros
+        if (leftIsDigit && rightIsDigit)
+        {
+          double numCompare = 0; // compare fist non-zero different digits
+          do
+          {
+            if (numCompare == 0)
+              numCompare = valL - valR;
+            leftIsDigit = GetDigitValue(leftC[++posL], valL);
+            rightIsDigit = GetDigitValue(rightC[++posR], valR);
+          } while (leftIsDigit && rightIsDigit);
+
+          if (leftIsDigit)
+            return 1;   // "left" has more digits (excluding leading zeros), assuming that "left" is greater than "right"
+          else if (rightIsDigit)
+            return -1;  // "right" has more digits (excluding leading zeros), assuming that "right" is greater than "left"
+          
+          // "left" and "right" have equal number of digits (excluding leading zeros),
+          if (numCompare != 0)
+            return numCompare > 0 ? 1 : -1; // assuming that result is determined by first non-zero different digits
+
+          // excluding leading zeros, "left" and "right" have equal number of digits, and all digits have same values
+        }
+        else if (leftIsDigit)
+          return valL > 0 ? 1 : -1; // "right" has only zeros and "left" contains non-zero
+        else if (rightIsDigit)
+          return valR > 0 ? -1 : 1; // "left" has only zeros and "right" contains non-zero
+
+        if (numberOfLeadZerosL > numberOfLeadZerosR)
+          return -1; // "left" has more leading zeros, assume "left" is lower
+        else if (numberOfLeadZerosL < numberOfLeadZerosR)
+          return -1; // "right" has more leading zeros, assume "right" is lower
+
+        // "left" and "right" have equal number of digits, and all digits have same values
+        assert(posL == posR);
+      }
+      const std::wstring leftWChr(g_charsetConverter.charUtf32toWstr(leftC[pos]));   // wstring representation of "left[pos]"
+      const std::wstring rightWChr(g_charsetConverter.charUtf32toWstr(rightC[pos])); // wstring representation of "right[pos]"
+      const wchar_t* const leftWChrC = leftWChr.c_str();
+      const wchar_t* const rightWChrC = rightWChr.c_str();
+      const int compareRsl = cl.compare(leftWChrC, leftWChrC + leftWChr.length(), rightWChrC, rightWChrC + rightWChr.length());
+      if (compareRsl != 0)
+        return compareRsl;
+      else
+      { // bad conversion to wstring?
+        // fallback: compare characters by code
+        if (leftC[pos] > rightC[pos])
+          return 1;
+        else if (leftC[pos] < rightC[pos])
+          return -1;
+      }
+    }
+  }
+
+  if (leftLen > rightLen)
+    return 1;
+  else if (leftLen < rightLen)
+    return -1;
+
+  return 0; // strings are equal
 }
 
--- xbmc/utils/Utf32Utils.h
+++ xbmc/utils/Utf32Utils.h
@@ -31,6 +31,16 @@ public:
   static bool IsDigit(char32_t chr);
   static bool GetDigitValue(char32_t chr, double& resultValue);
 
+  /**
+   * Compare two strings with respecting natural order for numbers
+   * "a0" < "a2" < "a03" < "a10" < "a0100" < "b4"
+   * @param left string to compare
+   * @param right string to compare
+   * @return positive number if left is greater then right, negative number if left is smaller then right
+   *         and zero if strings are equal
+   */
+  static int NaturalCompare(const std::u32string& left, const std::u32string right);
+
 private:
   typedef std::map<const char32_t, const double> digitsMap;
   typedef digitsMap::value_type digitsMapElement;
