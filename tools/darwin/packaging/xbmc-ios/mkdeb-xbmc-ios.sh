60e963944869257eb02c25debda5cb5a81605e43
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-01-01 13:15:23 +0100

    [release] version bump to 13.0 beta1

--- tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
+++ tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
@@ -46,7 +46,7 @@ fi
 PACKAGE=org.xbmc.xbmc-ios
 
 VERSION=13.0
-REVISION=0~alpha12
+REVISION=0~beta1
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
