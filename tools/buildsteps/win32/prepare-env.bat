acd012d9d41c611f84e31cd1d62dff2cb0baff33
WiSo <wiso@xbmc.org>
2014-01-10 23:16:04 +0100

    [WIN32] remove build_setup directory before the first git command to avoid problems with possible .git files.

--- tools/buildsteps/win32/prepare-env.bat
+++ tools/buildsteps/win32/prepare-env.bat
@@ -2,17 +2,18 @@
 
 ECHO Workspace is %WORKSPACE%
 
-rem git clean the untracked files but not the directories
-rem to keep the downloaded dependencies
-rem we assume git in path as this is a requirement
-
 cd %WORKSPACE%
+rem clean the BUILD_WIN32 at first to avoid problems with possible git files in there
+IF EXIST %WORKSPACE%\project\Win32BuildSetup\BUILD_WIN32 rmdir %WORKSPACE%\project\Win32BuildSetup\BUILD_WIN32 /S /Q
+
+rem we assume git in path as this is a requirement
+rem git clean the untracked files and directories
+rem but keep the downloaded dependencies
 ECHO running git clean -xfd -e "project/BuildDependencies/downloads" -e "project/BuildDependencies/downloads2"
 git clean -xfd -e "project/BuildDependencies/downloads" -e "project/BuildDependencies/downloads2"
 
 rem cleaning additional directories
 ECHO delete build directories
-IF EXIST %WORKSPACE%\project\Win32BuildSetup\BUILD_WIN32 rmdir %WORKSPACE%\project\Win32BuildSetup\BUILD_WIN32 /S /Q
 IF EXIST %WORKSPACE%\project\Win32BuildSetup\dependencies rmdir %WORKSPACE%\project\Win32BuildSetup\dependencies /S /Q
 
 IF EXIST %WORKSPACE%\project\BuildDependencies\include rmdir %WORKSPACE%\project\BuildDependencies\include /S /Q
