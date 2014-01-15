xbmc/xbmc

commit 5dc310eb2dd6bee1207cd29b1ce4a7eb5cf57fff

[cstdstring] demise left/right and replace with other options

xbmc/URL.cpp
@@ -118,7 +118,7 @@ void CURL::Parse(const CStdString& strURL1)
         return ;
       }
       iPos += extLen + 1;
-      CStdString archiveName = strURL.Left(iPos);
+      std::string archiveName = strURL.substr(0, iPos);
       struct __stat64 s;
       if (XFILE::CFile::Stat(archiveName, &s) == 0)
       {
@@ -131,12 +131,12 @@ void CURL::Parse(const CStdString& strURL1)
           Encode(archiveName);
           if (is_apk)
           {
-            CURL c((CStdString)"apk" + "://" + archiveName + '/' + strURL.Right(strURL.size() - iPos - 1));
+            CURL c("apk://" + archiveName + "/" + strURL.substr(iPos + 1));
             *this = c;
           }
           else
           {
-            CURL c((CStdString)"zip" + "://" + archiveName + '/' + strURL.Right(strURL.size() - iPos - 1));
+            CURL c("zip://" + archiveName + "/" + strURL.substr(iPos + 1));
             *this = c;
           }
           return;
@@ -146,7 +146,7 @@ void CURL::Parse(const CStdString& strURL1)
   }
   else
   {
-    SetProtocol(strURL.Left(iPos));
+    SetProtocol(strURL.substr(0, iPos));
     iPos += 3;
   }
 
@@ -236,7 +236,7 @@ void CURL::Parse(const CStdString& strURL1)
 
         if (iSemiColon >= 0)
         {
-          m_strDomain = strUserNamePassword.Left(iSemiColon);
+          m_strDomain = strUserNamePassword.substr(0, iSemiColon);
           strUserNamePassword.Delete(0, iSemiColon + 1);
         }
       }
@@ -245,9 +245,8 @@ void CURL::Parse(const CStdString& strURL1)
       int iColon = strUserNamePassword.Find(":");
       if (iColon >= 0)
       {
-        m_strUserName = strUserNamePassword.Left(iColon);
-        iColon++;
-        m_strPassword = strUserNamePassword.Right(strUserNamePassword.size() - iColon);
+        m_strUserName = strUserNamePassword.substr(0, iColon);
+        m_strPassword = strUserNamePassword.substr(iColon + 1);
       }
       // username
       else
@@ -270,10 +269,8 @@ void CURL::Parse(const CStdString& strURL1)
     int iColon = strHostNameAndPort.Find(":");
     if (iColon >= 0)
     {
-      m_strHostName = strHostNameAndPort.Left(iColon);
-      iColon++;
-      CStdString strPort = strHostNameAndPort.Right(strHostNameAndPort.size() - iColon);
-      m_iPort = atoi(strPort.c_str());
+      m_strHostName = strHostNameAndPort.substr(0, iColon);
+      m_iPort = atoi(strHostNameAndPort.substr(iColon + 1).c_str());
     }
     else
     {
@@ -287,10 +284,8 @@ void CURL::Parse(const CStdString& strURL1)
     int iColon = strHostNameAndPort.Find(":");
     if (iColon >= 0)
     {
-      m_strHostName = strHostNameAndPort.Left(iColon);
-      iColon++;
-      CStdString strPort = strHostNameAndPort.Right(strHostNameAndPort.size() - iColon);
-      m_iPort = atoi(strPort.c_str());
+      m_strHostName = strHostNameAndPort.substr(0, iColon);
+      m_iPort = atoi(strHostNameAndPort.substr(iColon + 1).c_str());
     }
     else
     {
@@ -305,7 +300,7 @@ void CURL::Parse(const CStdString& strURL1)
       if(iSlash < 0)
         m_strShareName = m_strFileName;
       else
-        m_strShareName = m_strFileName.Left(iSlash);
+        m_strShareName = m_strFileName.substr(0, iSlash);
     }
   }
 
@@ -315,7 +310,7 @@ void CURL::Parse(const CStdString& strURL1)
     || m_strProtocol.CompareNoCase("videodb") == 0
     || m_strProtocol.CompareNoCase("sources") == 0
     || m_strProtocol.CompareNoCase("pvr") == 0
-    || m_strProtocol.Left(3).CompareNoCase("mem") == 0)
+    || StringUtils::StartsWith(m_strProtocol, "mem"))
   {
     if (m_strHostName != "" && m_strFileName != "")
     {

xbmc/addons/GUIDialogAddonSettings.cpp
@@ -441,8 +441,8 @@ bool CGUIDialogAddonSettings::ShowVirtualKeyboard(int iControl)
           if (!value.empty())
           {
             // assumes HH:MM
-            timedate.wHour = atoi(value.Left(2));
-            timedate.wMinute = atoi(value.Right(2));
+            timedate.wHour = atoi(value.substr(0, 2).c_str());
+            timedate.wMinute = atoi(value.substr(3, 2).c_str());
           }
           if (CGUIDialogNumeric::ShowAndGetTime(timedate, label))
           {

xbmc/filesystem/AFPFile.cpp
@@ -721,8 +721,8 @@ bool CAFPFile::OpenForWrite(const CURL& url, bool bOverWrite)
 bool CAFPFile::IsValidFile(const CStdString& strFileName)
 {
   if (strFileName.Find('/') == -1   || // doesn't have sharename
-      strFileName.Right(2)  == "/." || // not current folder
-      strFileName.Right(3)  == "/..")  // not parent folder
+      StringUtils::EndsWith(strFileName, "/.") || // not current folder
+      StringUtils::EndsWith(strFileName, "/.."))  // not parent folder
   {
     return false;
   }

xbmc/filesystem/CurlFile.cpp
@@ -694,7 +694,7 @@ void CCurlFile::ParseAndCorrectUrl(CURL &url2)
     }
 
     /* make sure we keep slashes */
-    if(url2.GetFileName().Right(1) == "/")
+    if(StringUtils::EndsWith(url2.GetFileName(), "/"))
       filename += "/";
 
     url2.SetFileName(filename);

xbmc/filesystem/SmbFile.cpp
@@ -750,8 +750,8 @@ bool CSmbFile::OpenForWrite(const CURL& url, bool bOverWrite)
 bool CSmbFile::IsValidFile(const CStdString& strFileName)
 {
   if (strFileName.Find('/') == -1 || /* doesn't have sharename */
-      strFileName.Right(2) == "/." || /* not current folder */
-      strFileName.Right(3) == "/..")  /* not parent folder */
+      StringUtils::EndsWith(strFileName, "/.") || /* not current folder */
+      StringUtils::EndsWith(strFileName, "/.."))  /* not parent folder */
       return false;
   return true;
 }

xbmc/filesystem/StackDirectory.cpp
@@ -161,7 +161,7 @@ namespace XFILE
     CStdString file, folder;
     int pos = strPath.Find(" , ");
     if (pos > 0)
-      URIUtils::Split(strPath.Left(pos), folder, file);
+      URIUtils::Split((CStdString)strPath.substr(0, pos), folder, file);
     else
       URIUtils::Split(strPath, folder, file); // single filed stacks - should really not happen
 

xbmc/music/windows/GUIWindowMusicNav.cpp
@@ -257,7 +257,7 @@ bool CGUIWindowMusicNav::OnClick(int iItem)
   if (iItem < 0 || iItem >= m_vecItems->Size()) return false;
 
   CFileItemPtr item = m_vecItems->Get(iItem);
-  if (item->GetPath().Left(14) == "musicsearch://")
+  if (StringUtils::StartsWith(item->GetPath(), "musicsearch://"))
   {
     if (m_searchWithEdit)
       OnSearchUpdate();

xbmc/network/AirPlayServer.cpp
@@ -704,7 +704,7 @@ int CAirPlayServer::CTCPClient::ProcessRequest( CStdString& responseHeader,
   int startQs = uri.Find('?');
   if (startQs != -1)
   {
-    uri = uri.Left(startQs);
+    uri.erase(startQs);
   }
 
   // This is the socket which will be used for reverse HTTP

commit a02dbe5027e79dcb36cdf363026542aaed40aaab

CURL: remove Encode() overload; Encode() return new value instead of modifying parameter, include fix of using wrong Encode() overload introduced in  5dc310eb2dd6bee1207cd29b1ce4a7eb5cf57fff

xbmc/URL.cpp
@@ -40,7 +40,7 @@ using namespace ADDON;
 CStdString URLEncodeInline(const CStdString& strData)
 {
   CStdString buffer = strData;
-  CURL::Encode(buffer);
+  buffer = CURL::Encode(buffer);
   return buffer;
 }
 
@@ -128,7 +128,7 @@ void CURL::Parse(const CStdString& strURL1)
         if (!(s.st_mode & S_IFDIR))
 #endif
         {
-          Encode(archiveName);
+          archiveName = Encode(archiveName);
           if (is_apk)
           {
             CURL c("apk://" + archiveName + "/" + strURL.substr(iPos + 1));
@@ -743,9 +743,9 @@ void CURL::Decode(CStdString& strURLData)
   strURLData = strResult;
 }
 
-void CURL::Encode(CStdString& strURLData)
+std::string CURL::Encode(const std::string& strURLData)
 {
-  CStdString strResult;
+  std::string strResult;
 
   /* wonder what a good value is here is, depends on how often it occurs */
   strResult.reserve( strURLData.length() * 2 );
@@ -764,20 +764,14 @@ void CURL::Encode(CStdString& strURLData)
       strResult += strTmp;
     }
   }
-  strURLData = strResult;
+
+  return strResult;
 }
 
 std::string CURL::Decode(const std::string& strURLData)
 {
   CStdString url = strURLData;
   Decode(url);
-  return url;
-}
-
-std::string CURL::Encode(const std::string& strURLData)
-{
-  CStdString url = strURLData;
-  Encode(url);
   return url;
 }
 

xbmc/URL.h
@@ -72,7 +72,6 @@ public:
   static bool IsFileOnly(const CStdString &url); ///< return true if there are no directories in the url.
   static bool IsFullPath(const CStdString &url); ///< return true if the url includes the full path
   static void Decode(CStdString& strURLData);
-  static void Encode(CStdString& strURLData);
   static std::string Decode(const std::string& strURLData);
   static std::string Encode(const std::string& strURLData);
   static CStdString TranslateProtocol(const CStdString& prot);

xbmc/addons/GUIDialogAddonSettings.cpp
@@ -265,7 +265,7 @@ bool CGUIDialogAddonSettings::ShowVirtualKeyboard(int iControl)
             else
               ((CGUIButtonControl*) control)->SetLabel2(value);
             if (bEncoded)
-              CURL::Encode(value);
+              value = CURL::Encode(value);
           }
         }
         else if (strcmp(type, "number") == 0 && CGUIDialogNumeric::ShowAndGetNumber(value, label))

xbmc/addons/Scraper.cpp
@@ -576,7 +576,7 @@ std::vector<CScraperUrl> CScraper::FindMovie(XFILE::CCurlFile &fcurl, const CStd
 
   vector<CStdString> vcsIn(1);
   g_charsetConverter.utf8To(SearchStringEncoding(), sTitle, vcsIn[0]);
-  CURL::Encode(vcsIn[0]);
+  vcsIn[0] = CURL::Encode(vcsIn[0]);
   if (!sYear.empty())
     vcsIn.push_back(sYear);
 
@@ -702,8 +702,8 @@ std::vector<CMusicAlbumInfo> CScraper::FindAlbum(CCurlFile &fcurl, const CStdStr
   std::vector<CStdString> extras(2);
   g_charsetConverter.utf8To(SearchStringEncoding(), sAlbum, extras[0]);
   g_charsetConverter.utf8To(SearchStringEncoding(), sArtist, extras[1]);
-  CURL::Encode(extras[0]);
-  CURL::Encode(extras[1]);
+  extras[0] = CURL::Encode(extras[0]);
+  extras[1] = CURL::Encode(extras[1]);
   CScraperUrl scurl;
   vector<CStdString> vcsOut = RunNoThrow("CreateAlbumSearchUrl", scurl, fcurl, &extras);
   if (vcsOut.size() > 1)
@@ -798,7 +798,7 @@ std::vector<CMusicArtistInfo> CScraper::FindArtist(CCurlFile &fcurl,
   // returns an XML <url> element parseable by CScraperUrl
   std::vector<CStdString> extras(1);
   g_charsetConverter.utf8To(SearchStringEncoding(), sArtist, extras[0]);
-  CURL::Encode(extras[0]);
+  extras[0] = CURL::Encode(extras[0]);
   CScraperUrl scurl;
   vector<CStdString> vcsOut = RunNoThrow("CreateArtistSearchUrl", scurl, fcurl, &extras);
 
@@ -1009,7 +1009,7 @@ bool CScraper::GetArtistDetails(CCurlFile &fcurl, const CScraperUrl &scurl,
   // pass in the original search string for chaining to search other sites
   vector<CStdString> vcIn;
   vcIn.push_back(sSearch);
-  CURL::Encode(vcIn[0]);
+  vcIn[0] = CURL::Encode(vcIn[0]);
 
   vector<CStdString> vcsOut = RunNoThrow("GetArtistDetails", scurl, fcurl, &vcIn);
 

xbmc/filesystem/AFPFile.cpp
@@ -41,7 +41,7 @@ using namespace XFILE;
 CStdString URLEncode(const CStdString value)
 {
   CStdString encoded(value);
-  CURL::Encode(encoded);
+  encoded = CURL::Encode(encoded);
   return encoded;
 }
 

xbmc/filesystem/CurlFile.cpp
@@ -690,7 +690,7 @@ void CCurlFile::ParseAndCorrectUrl(CURL &url2)
         filename += "/";
 
       partial = *it;
-      CURL::Encode(partial);
+      partial = CURL::Encode(partial);
       filename += partial;
     }
 

xbmc/filesystem/MultiPathDirectory.cpp
@@ -225,7 +225,7 @@ void CMultiPathDirectory::AddToMultiPath(CStdString& strMultiPath, const CStdStr
   CStdString strPath1 = strPath;
   URIUtils::AddSlashAtEnd(strMultiPath);
   //CLog::Log(LOGDEBUG, "-- adding path: %s", strPath.c_str());
-  CURL::Encode(strPath1);
+  strPath1 = CURL::Encode(strPath1);
   strMultiPath += strPath1;
   strMultiPath += "/";
 }

xbmc/filesystem/SAPDirectory.cpp
@@ -365,7 +365,7 @@ bool CSAPSessions::ParseAnnounce(char* data, int len)
 
   // add a new session to our buffer
   CStdString user = origin.username;
-  CURL::Encode(user);
+  user = CURL::Encode(user);
   CStdString path = StringUtils::Format("sap://%s/%s/0x%x.sdp", header.origin.c_str(), desc.origin.c_str(), header.msgid);
   CSession session;
   session.path           = path;

xbmc/filesystem/SMBDirectory.cpp
@@ -417,7 +417,7 @@ void CSMBDirectory::UnMountShare(const CStdString &strType, const CStdString &st
 CStdString CSMBDirectory::GetMountPoint(const CStdString &strType, const CStdString &strName)
 {
   CStdString strPath = strType + strName;
-  CURL::Encode(strPath);
+  strPath = CURL::Encode(strPath);
 
 #if defined(TARGET_DARWIN)
   CStdString str = getenv("HOME");

xbmc/filesystem/SmbFile.cpp
@@ -245,7 +245,7 @@ CStdString CSMB::URLEncode(const CURL &url)
 CStdString CSMB::URLEncode(const CStdString &value)
 {
   CStdString encoded(value);
-  CURL::Encode(encoded);
+  encoded = CURL::Encode(encoded);
   return encoded;
 }
 

xbmc/filesystem/StackDirectory.cpp
@@ -128,7 +128,7 @@ namespace XFILE
                   strStackTitle = Title1 + Ignore1 + Extension1;
                   // Check if source path uses URL encoding
                   if (URIUtils::ProtocolHasEncodedFilename(CURL(strCommonDir).GetProtocol()))
-                    CURL::Encode(strStackTitle);
+                    strStackTitle = CURL::Encode(strStackTitle);
 
                   itRegExp = RegExps.end();
                   break;

xbmc/filesystem/UPnPDirectory.cpp
@@ -333,7 +333,7 @@ CUPnPDirectory::GetDirectory(const CStdString& strPath, CFileItemList &items)
             else
                 id = (const char*) (*entry)->m_ReferenceID;
 
-            CURL::Encode(id);
+            id = CURL::Encode(id);
             URIUtils::AddSlashAtEnd(id);
             pItem->SetPath(CStdString((const char*) "upnp://" + uuid + "/" + id.c_str()));
 

xbmc/filesystem/ZeroconfDirectory.cpp
@@ -187,7 +187,7 @@ bool CZeroconfDirectory::GetDirectory(const CStdString& strPath, CFileItemList &
         CURL url;
         url.SetProtocol("zeroconf");
         CStdString service_path = CZeroconfBrowser::ZeroconfService::toPath(*it);
-        CURL::Encode(service_path);
+        service_path = CURL::Encode(service_path);
         url.SetFileName(service_path);
         item->SetPath(url.Get());
 

xbmc/music/windows/GUIWindowMusicNav.cpp
@@ -767,7 +767,7 @@ void CGUIWindowMusicNav::DisplayEmptyDatabaseMessage(bool bDisplay)
 void CGUIWindowMusicNav::OnSearchUpdate()
 {
   CStdString search(GetProperty("search").asString());
-  CURL::Encode(search);
+  search = CURL::Encode(search);
   if (!search.empty())
   {
     CStdString path = "musicsearch://" + search + "/";

xbmc/network/AirPlayServer.cpp
@@ -863,7 +863,7 @@ int CAirPlayServer::CTCPClient::ProcessRequest( CStdString& responseHeader,
     if (status != AIRPLAY_STATUS_NEED_AUTH)
     {
       CStdString userAgent="AppleCoreMedia/1.0.0.8F455 (AppleTV; U; CPU OS 4_3 like Mac OS X; de_de)";
-      CURL::Encode(userAgent);
+      userAgent = CURL::Encode(userAgent);
       location += "|User-Agent=" + userAgent;
 
       CFileItem fileToPlay(location, false);

xbmc/network/WebServer.cpp
@@ -853,7 +853,7 @@ bool CWebServer::PrepareDownload(const char *path, CVariant &details, std::strin
       url = "image/";
     else
       url = "vfs/";
-    CURL::Encode(strPath);
+    strPath = CURL::Encode(strPath);
     url += strPath;
     details["path"] = url;
     return true;

xbmc/network/upnp/UPnP.cpp
@@ -184,7 +184,7 @@ public:
         NPT_String path = "upnp://"+device->GetUUID()+"/";
         if (!NPT_StringsEqual(item_id, "0")) {
             CStdString id = item_id;
-            CURL::Encode(id);
+            id = CURL::Encode(id);
             URIUtils::AddSlashAtEnd(id);
             path += id.c_str();
         }

xbmc/network/upnp/UPnPServer.cpp
@@ -225,7 +225,7 @@ NPT_String CUPnPServer::BuildSafeResourceUri(const NPT_HttpUrl &rooturi,
     else
       filename = URIUtils::GetFileName(file_path);
 
-    CURL::Encode(filename);
+    filename = CURL::Encode(filename);
     md5state.append(file_path);
     md5state.getDigest(md5);
     md5 += "/" + filename;

xbmc/utils/ScraperParser.cpp
@@ -529,7 +529,7 @@ void CScraperParser::Clean(CStdString& strDirty)
     if ((i2 = strDirty.find("!!!ENCODE!!!",i+12)) != std::string::npos)
     {
       strBuffer = strDirty.substr(i+12,i2-i-12);
-      CURL::Encode(strBuffer);
+      strBuffer = CURL::Encode(strBuffer);
       strDirty.replace(i, i2-i+12, strBuffer);
       i += strBuffer.size();
     }

xbmc/utils/ScraperUrl.cpp
@@ -347,7 +347,7 @@ CStdString CScraperUrl::GetThumbURL(const CScraperUrl::SUrlEntry &entry)
   if (entry.m_spoof.empty())
     return entry.m_url;
   CStdString spoof = entry.m_spoof;
-  CURL::Encode(spoof);
+  spoof = CURL::Encode(spoof);
   return entry.m_url + "|Referer=" + spoof;
 }
 

xbmc/utils/URIUtils.cpp
@@ -1120,13 +1120,13 @@ void URIUtils::CreateArchivePath(CStdString& strUrlPath,
   if( !strPwd.empty() )
   {
     strBuffer = strPwd;
-    CURL::Encode(strBuffer);
+    strBuffer = CURL::Encode(strBuffer);
     strUrlPath += strBuffer;
     strUrlPath += "@";
   }
 
   strBuffer = strArchivePath;
-  CURL::Encode(strBuffer);
+  strBuffer = CURL::Encode(strBuffer);
 
   strUrlPath += strBuffer;
 
@@ -1139,7 +1139,7 @@ void URIUtils::CreateArchivePath(CStdString& strUrlPath,
 
 #if 0 // options are not used
   strBuffer = strCachePath;
-  CURL::Encode(strBuffer);
+  strBuffer = CURL::Encode(strBuffer);
 
   strUrlPath += "?cache=";
   strUrlPath += strBuffer;