d9d3a4124aeb782941c7a72ab031fb497498251f
Jonathan Marshall <jmarshall@xbmc.org>
2014-01-26 19:55:28 +1300

    [addonversion] adds compare() function and use this for < and == operators

--- xbmc/addons/AddonVersion.h
+++ xbmc/addons/AddonVersion.h
@@ -51,6 +51,7 @@ namespace ADDON
     AddonVersion& operator=(const AddonVersion& other);
     bool operator<(const AddonVersion& other) const;
     bool operator==(const AddonVersion& other) const;
+    int compare(const AddonVersion& other) const;
     CStdString Print() const;
     const char *c_str() const { return m_originalVersion.c_str(); };
     bool empty() const;
