9597e744c38ecc7f7731348329bcf876f62eea95
Rainer Hochecker <fernetmenta@online.de>
2014-03-15 12:56:48 +0100

    ffmpeg demuxer: fix incorrectly set bitsPerSample for audio streams

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -1078,7 +1078,7 @@ CDemuxStream* CDVDDemuxFFmpeg::AddStream(int iId)
         st->iSampleRate = pStream->codec->sample_rate;
         st->iBlockAlign = pStream->codec->block_align;
         st->iBitRate = pStream->codec->bit_rate;
-        st->iBitsPerSample = pStream->codec->bits_per_coded_sample;
+        st->iBitsPerSample = pStream->codec->bits_per_raw_sample;
 	
         if(m_dllAvUtil.av_dict_get(pStream->metadata, "title", NULL, 0))
           st->m_description = m_dllAvUtil.av_dict_get(pStream->metadata, "title", NULL, 0)->value;
