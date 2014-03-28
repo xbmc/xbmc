ce52900c9da452647d62fce71448ec7e4d86e3eb
Trent Nelson <trent.a.b.nelson@gmail.com>
2014-03-28 11:25:17 -0600

    version bump to 13.0 beta4

--- tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
+++ tools/darwin/packaging/xbmc-ios/mkdeb-xbmc-ios.sh
@@ -46,7 +46,7 @@ fi
 PACKAGE=org.xbmc.xbmc-ios
 
 VERSION=13.0
-REVISION=0~beta3
+REVISION=0~beta4
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
--- xbmc/GUIInfoManager.h
+++ xbmc/GUIInfoManager.h
@@ -498,7 +498,7 @@ namespace INFO
 
 #define VERSION_MAJOR               13
 #define VERSION_MINOR               0
-#define VERSION_TAG                 "-BETA3"
+#define VERSION_TAG                 "-BETA4"
 
 #define LISTITEM_START              35000
 #define LISTITEM_THUMB              (LISTITEM_START)
