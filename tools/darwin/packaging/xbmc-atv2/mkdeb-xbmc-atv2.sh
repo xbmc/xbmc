3a77615b0d5e8ecb8323d5494a160c302e46fd0d
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-04-09 10:04:06 +0200

    [release] version bump to 13.0 rc1

--- tools/darwin/packaging/xbmc-atv2/mkdeb-xbmc-atv2.sh
+++ tools/darwin/packaging/xbmc-atv2/mkdeb-xbmc-atv2.sh
@@ -45,7 +45,7 @@ fi
 PACKAGE=org.xbmc.xbmc-atv2
 
 VERSION=13.0
-REVISION=0~beta4
+REVISION=0~rc1
 ARCHIVE=${PACKAGE}_${VERSION}-${REVISION}_iphoneos-arm.deb
 
 echo Creating $PACKAGE package version $VERSION revision $REVISION
