fa6ca17b3bf71941c3bed8429d7c7db1af57c4f7
montellese <montellese@xbmc.org>
2014-05-07 19:20:09 +0200

    [win32] extend JsonSchemaBuilder.bat to auto-generate xbmc.json's addon.xml

--- tools/windows/JsonSchemaBuilder.bat
+++ tools/windows/JsonSchemaBuilder.bat
@@ -3,10 +3,18 @@
 SET cur_dir=%CD%
 
 SET base_dir=%cur_dir%\..\..
-SET bin_dir=%cur_dir%\..\BuildDependencies\bin\json-rpc
+SET builddeps_dir=%cur_dir%\..\BuildDependencies
+SET bin_dir=%builddeps_dir%\bin
+SET msys_bin_dir=%builddeps_dir%\msys\bin
 SET jsonrpc_path=%base_dir%\xbmc\interfaces\json-rpc
 SET jsonrpc_schema_path=%jsonrpc_path%\schema
-SET output=ServiceDescription.h
+SET jsonrpc_output=ServiceDescription.h
 
-"%bin_dir%\JsonSchemaBuilder.exe" "%jsonrpc_schema_path%\version.txt" "%jsonrpc_schema_path%\license.txt" "%jsonrpc_schema_path%\methods.json" "%jsonrpc_schema_path%\types.json" "%jsonrpc_schema_path%\notifications.json"
-MOVE /Y %output% "%jsonrpc_path%\%output%"
+SET xbmc_json_path=%base_Dir%\addons\xbmc.json
+SET xbmc_json_output=addon.xml
+
+SET /p version=<"%jsonrpc_schema_path%\version.txt"
+"%msys_bin_dir%\sed.exe" s/@jsonrpc_version@/%version%/g "%xbmc_json_path%\%xbmc_json_output%.in" > "%xbmc_json_path%\%xbmc_json_output%"
+
+"%bin_dir%\json-rpc\JsonSchemaBuilder.exe" "%jsonrpc_schema_path%\version.txt" "%jsonrpc_schema_path%\license.txt" "%jsonrpc_schema_path%\methods.json" "%jsonrpc_schema_path%\types.json" "%jsonrpc_schema_path%\notifications.json"
+MOVE /Y %jsonrpc_output% "%jsonrpc_path%\%jsonrpc_output%"
