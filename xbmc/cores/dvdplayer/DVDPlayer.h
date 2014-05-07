4736843cf94b9351d5ca3a92fe5812a029a61336
Joakim Plate <elupus@ecce.se>
2014-05-07 21:20:59 +0200

    dvdplayer: drop unused last sub variable

--- xbmc/cores/dvdplayer/DVDPlayer.h
+++ xbmc/cores/dvdplayer/DVDPlayer.h
@@ -373,8 +373,6 @@ protected:
   CDVDDemux* m_pDemuxer;            // demuxer for current playing file
   CDVDDemux* m_pSubtitleDemuxer;
 
-  CStdString m_lastSub;
-
   struct SDVDInfo
   {
     void Clear()
