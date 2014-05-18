7098ba3b02792e7b0780780981b34aff7716c22e
Jonathan Marshall <jmarshall@xbmc.org>
2014-05-18 15:12:43 +1200

    [fonts] re-write the fontmanager loading from XML to cleanup a bunch of silly old code

--- xbmc/guilib/GUIFontManager.h
+++ xbmc/guilib/GUIFontManager.h
@@ -63,7 +63,7 @@ public:
   virtual bool OnMessage(CGUIMessage &message);
 
   void Unload(const CStdString& strFontName);
-  void LoadFonts(const CStdString& strFontSet);
+  void LoadFonts(const std::string &fontSet);
   CGUIFont* LoadTTF(const CStdString& strFontName, const CStdString& strFilename, color_t textColor, color_t shadowColor, const int iSize, const int iStyle, bool border = false, float lineSpacing = 1.0f, float aspect = 1.0f, const RESOLUTION_INFO *res = NULL, bool preserveAspect = false);
   CGUIFont* GetFont(const CStdString& strFontName, bool fallback = true);
 
@@ -83,7 +83,7 @@ protected:
   static void RescaleFontSizeAndAspect(float *size, float *aspect, const RESOLUTION_INFO &sourceRes, bool preserveAspect);
   void LoadFonts(const TiXmlNode* fontNode);
   CGUIFontTTFBase* GetFontFile(const CStdString& strFontFile);
-  bool OpenFontFile(CXBMCTinyXML& xmlDoc);
+  static void GetStyle(const TiXmlNode *fontNode, int &iStyle);
 
   std::vector<CGUIFont*> m_vecFonts;
   std::vector<CGUIFontTTFBase*> m_vecFontFiles;
