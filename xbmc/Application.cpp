ac89cf593e87af65b12ea196ce9378f4bec9b4c8
Tobias Hieta <tobias@plexapp.com>
2014-05-20 11:32:19 +0200

    Switch to Now Playing when playing the first music track
    Fixes #1999

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -4551,6 +4551,11 @@ bool CApplication::PlayFile(const CFileItem& item_, bool bRestart)
     {
       if (g_windowManager.GetActiveWindow() == WINDOW_FULLSCREEN_VIDEO)
         g_windowManager.ActivateWindow(WINDOW_VISUALISATION);
+
+      /* PLEX */
+      if (!g_playlistPlayer.HasPlayedFirstFile())
+        ActivateVisualizer();
+      /* END PLEX */
     }
 
 #ifdef HAS_VIDEO_PLAYBACK
