915435d465c9d306c6f170ef6743662f4d00e98d
popcornmix <popcornmix@gmail.com>
2014-06-16 19:06:00 +0100

    [resampler] Use ffmpeg for quiet noise generation
    Opening the GPU resampler is relatively expensive compared to the small amount of work done here.
    This function takes ~110ms when using GPU resampler and about 14ms with ffmpeg resample on Pi 2.
    
    Provide a way of flagging that GPU acceleration is not required

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -2684,7 +2684,7 @@ bool CActiveAE::ResampleSound(CActiveAESound *sound)
   dst_config.bits_per_sample = CAEUtil::DataFormatToUsedBits(m_internalFormat.m_dataFormat);
   dst_config.dither_bits = CAEUtil::DataFormatToDitherBits(m_internalFormat.m_dataFormat);
 
-  IAEResample *resampler = CAEResampleFactory::Create();
+  IAEResample *resampler = CAEResampleFactory::Create(AERESAMPLEFACTORY_QUICK_RESAMPLE);
   resampler->Init(dst_config.channel_layout,
                   dst_config.channels,
                   dst_config.sample_rate,
