99dd84271f41504394390f6a35f0d7eef192d307
netzmacht <mail@netzmacht.de>
2014-06-21 18:02:23 +0200

    Update LinuxInputDevices.cpp

--- xbmc/input/linux/LinuxInputDevices.cpp
+++ xbmc/input/linux/LinuxInputDevices.cpp
@@ -274,6 +274,26 @@ typedef enum
 
 static char remoteStatus = 0xFF; // paired, battery OK
 
+/* [AI]: calibration values */
+int calib_x_d;
+float calib_x_fact;
+int calib_y_d;
+float calib_y_fact;
+int swap_axes = 0;
+/* 
+ * Some touch controllers use the same input events as mouses and they need special handling
+ *     touch_mouse=1 the touchscreen will respond to touch screen and mouse events
+ *                   This will cause mouse clicks to not work
+ *     touch_mouse=0 the the touchscreen will only respond to touch screen events 
+ *                   This will cause normal mouse and touch screens which are not sending mouse events to work
+ * If you cannot click with a single tap with your touch screen you need to set touch_mouse=1
+ */
+uint32_t touch_mouse = 0;
+uint32_t click_confines = 7; 
+FILE *c_fp;
+/* [AI]: calibration values */
+
+
 CLinuxInputDevice::CLinuxInputDevice(const std::string fileName, int index)
 {
   m_fd = -1;
@@ -296,6 +316,50 @@ CLinuxInputDevice::CLinuxInputDevice(const std::string fileName, int index)
   m_deviceMaxKeyCode = 0;
   m_deviceMaxAxis = 0;
   m_bUnplugged = false;
+
+  /* [AI]: get calibration values from file */
+  /* set neutral values in case the read fails */
+  calib_x_d = g_graphicsContext.GetWidth();
+  calib_y_d = g_graphicsContext.GetHeight();
+  calib_x_fact = -1.0f;
+  calib_y_fact = -1.0f;
+
+  /* open the file */
+  char *pCalibFile = (char*)malloc(50);
+  sprintf(pCalibFile, "%s/touchscreen_axes_calib", getenv("HOME"));
+  c_fp = fopen(pCalibFile, "r");
+  free(pCalibFile);
+  if (c_fp == NULL)
+  {
+      printf("Error reading %s file\n", pCalibFile);
+  }
+  else
+  {
+    fscanf(c_fp, "calib_x_d=%d;calib_x_fact=%f;calib_y_d=%d;calib_y_fact=%f;swap_axes=%d;click_confines=%u;touch_mouse=%u\n",
+        &calib_x_d,
+        &calib_x_fact,
+        &calib_y_d,
+        &calib_y_fact,
+        &swap_axes,
+        &click_confines,
+        &touch_mouse
+    );
+    printf("File touchscreen_axes_calib successfully read:\n");
+
+    /* close the file */
+    fclose(c_fp);
+  }
+
+  printf("\n  calib_x_d=%d;\n  calib_x_fact=%f;\n  calib_y_d=%d;\n  calib_y_fact=%f;\n  swap_axes=%d;\n  &click_confines=%u\n  touch_mouse=%u\n",
+      calib_x_d,
+      calib_x_fact,
+      calib_y_d,
+      calib_y_fact,
+      swap_axes,
+      click_confines,
+      touch_mouse
+  );
+  /* [AI]: read calibration values from file */
 
   Open();
 }
@@ -454,6 +518,15 @@ XBMCMod CLinuxInputDevice::UpdateModifiers(XBMC_Event& devt)
 bool CLinuxInputDevice::KeyEvent(const struct input_event& levt, XBMC_Event& devt)
 {
   int code = levt.code;
+  
+  /* [AI]: only for touchscreens, when PRESS action occurs */
+  if((levt.code == BTN_TOUCH || levt.code == BTN_TOOL_FINGER || levt.code == BTN_MOUSE)
+      && levt.value == 1 && (levt.type == EV_ABS || touch_mouse))
+  {
+    //CLog::Log(LOGWARNING, "[AI]: CLinuxInputDevice::KeyEvent: Hack coordinates \n");
+    //m_mouseX = g_graphicsContext.GetWidth() + 2;
+    //m_mouseY = g_graphicsContext.GetHeight() + 2;
+  }
 
   /* map touchscreen and smartpad events to button mouse */
   if (code == BTN_TOUCH || code == BTN_TOOL_FINGER)
@@ -602,11 +675,29 @@ bool CLinuxInputDevice::AbsEvent(const struct input_event& levt, XBMC_Event& dev
   switch (levt.code)
   {
   case ABS_X:
-    m_mouseX = levt.value;
+    //m_mouseX = levt.value;
+    if(levt.value != g_Mouse.GetRawX())
+    {
+      g_Mouse.SetRawX(levt.value);
+      //printf("x: %d\n\r", g_Mouse.GetRawX());
+    }
+    if(swap_axes)
+      m_mouseY = (int)(((float)levt.value) * (calib_y_fact)) + calib_y_d;
+    else
+      m_mouseX = (int)(((float)levt.value) * (calib_x_fact)) + calib_x_d;
     break;
 
   case ABS_Y:
-    m_mouseY = levt.value;
+    //m_mouseY = levt.value;
+    if(levt.value != g_Mouse.GetRawY())
+    {
+      g_Mouse.SetRawY(levt.value);
+      //printf("y: %d\n\r", g_Mouse.GetRawY());
+    }
+    if(swap_axes)
+      m_mouseX = (int)(((float)levt.value) * (calib_x_fact)) + calib_x_d;
+    else
+      m_mouseY = (int)(((float)levt.value) * (calib_y_fact)) + calib_y_d;
     break;
   
   case ABS_MISC:
