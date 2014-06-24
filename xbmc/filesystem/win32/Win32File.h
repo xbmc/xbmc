ff02a20444604a849f8f6542ba1fd428b492cc04
Karlson2k <k2k@narod.ru>
2014-06-24 23:05:56 +0400

    [win32][vfs] Implement new CWin32File class for local files on win32 platforms * rewritten from scratch * added a lot of validity checks and results checks * correctly handle all over/under sizes of variables * automatically hide files in filesystem with name staring from dot * fixed bug - Write didn't update i/o pointer * always return real current file size instead of cached value * reimplemented Stat() better than common runtima stat() (more accurate time values, set real number of hard links, better set _S_IEXEC attribute etc.)

---
+++ xbmc/filesystem/win32/Win32File.h
@@ -0,0 +1,62 @@
+#pragma once
+/*
+*      Copyright (C) 2014 Team XBMC
+*      http://xbmc.org
+*
+*  This Program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2, or (at your option)
+*  any later version.
+*
+*  This Program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with XBMC; see the file COPYING.  If not, see
+*  <http://www.gnu.org/licenses/>.
+*
+*/
+
+#include "filesystem/IFile.h"
+#include <string>
+
+typedef void* HANDLE; // forward declaration
+
+namespace XFILE
+{
+  class CWin32File : public IFile
+  {
+  public:
+    CWin32File();
+    virtual ~CWin32File();
+
+    virtual bool Open(const CURL& url);
+    virtual bool OpenForWrite(const CURL& url, bool bOverWrite = false);
+    virtual void Close();
+
+    virtual unsigned int Read(void* lpBuf, int64_t uiBufSize);
+    virtual int Write(const void* lpBuf, int64_t uiBufSize);
+    virtual int64_t Seek(int64_t iFilePosition, int iWhence = SEEK_SET);
+    virtual int Truncate(int64_t toSize);
+    virtual int64_t GetPosition();
+    virtual int64_t GetLength();
+    virtual void Flush();
+
+    virtual bool Delete(const CURL& url);
+    virtual bool Rename(const CURL& urlCurrentName, const CURL& urlNewName);
+    virtual bool SetHidden(const CURL& url, bool hidden);
+    virtual bool Exists(const CURL& url);
+    virtual int Stat(const CURL& url, struct __stat64* statData);
+    virtual int Stat(struct __stat64* statData);
+
+  protected:
+    HANDLE  m_hFile;
+    int64_t m_filePos;
+    bool    m_allowWrite;
+    // file path and name in win32 long form "\\?\D:\path\to\file.ext"
+    std::wstring m_filepathnameW;
+  };
+
+}
