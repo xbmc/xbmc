0580934ff115aa7228a92dc6fd8770c34b2e8608
Anssi Hannula <anssi@xbmc.org>
2014-07-23 21:47:34 +0300

    [android] Add 5.1 PCM playback support
    Tested on ADT-1 Android TV device.
    
    This commits add code for supporting arbitrary channel layouts with the
    AudioTrack API. However, at least on ADT-1 any non-5.1 layout gets
    downmixed to 2.0, so add a compile-time define for limiting playback to
    2.0 and 5.1 (so that e.g. 4.0/5.0 content is played back as 5.1), at
    least for now (i.e. until a device with support for non-2.0 non-5.1
    appears).

--- xbmc/android/jni/AudioFormat.cpp
+++ xbmc/android/jni/AudioFormat.cpp
@@ -25,7 +25,21 @@
 using namespace jni;
 
 int CJNIAudioFormat::ENCODING_PCM_16BIT = 0x00000002;
-int CJNIAudioFormat::CHANNEL_OUT_STEREO = 0x0000000c;
+
+int CJNIAudioFormat::CHANNEL_OUT_STEREO  = 0x0000000c;
+int CJNIAudioFormat::CHANNEL_OUT_5POINT1 = 0x000000fc;
+
+int CJNIAudioFormat::CHANNEL_OUT_FRONT_LEFT            = 0x00000004;
+int CJNIAudioFormat::CHANNEL_OUT_FRONT_LEFT_OF_CENTER  = 0x00000100;
+int CJNIAudioFormat::CHANNEL_OUT_FRONT_CENTER          = 0x00000010;
+int CJNIAudioFormat::CHANNEL_OUT_FRONT_RIGHT_OF_CENTER = 0x00000200;
+int CJNIAudioFormat::CHANNEL_OUT_FRONT_RIGHT           = 0x00000008;
+int CJNIAudioFormat::CHANNEL_OUT_LOW_FREQUENCY         = 0x00000020;
+int CJNIAudioFormat::CHANNEL_OUT_BACK_LEFT             = 0x00000040;
+int CJNIAudioFormat::CHANNEL_OUT_BACK_CENTER           = 0x00000400;
+int CJNIAudioFormat::CHANNEL_OUT_BACK_RIGHT            = 0x00000080;
+
+int CJNIAudioFormat::CHANNEL_INVALID                   = 0x00000000;
 
 void CJNIAudioFormat::PopulateStaticFields()
 {
@@ -36,6 +50,17 @@ void CJNIAudioFormat::PopulateStaticFields()
     CJNIAudioFormat::ENCODING_PCM_16BIT = get_static_field<int>(c, "ENCODING_PCM_16BIT");
     if (sdk >= 5)
       CJNIAudioFormat::CHANNEL_OUT_STEREO = get_static_field<int>(c, "CHANNEL_OUT_STEREO");
+      CJNIAudioFormat::CHANNEL_OUT_5POINT1 = get_static_field<int>(c, "CHANNEL_OUT_5POINT1");
+      CJNIAudioFormat::CHANNEL_OUT_FRONT_LEFT = get_static_field<int>(c, "CHANNEL_OUT_FRONT_LEFT");
+      CJNIAudioFormat::CHANNEL_OUT_FRONT_LEFT_OF_CENTER = get_static_field<int>(c, "CHANNEL_OUT_FRONT_LEFT_OF_CENTER");
+      CJNIAudioFormat::CHANNEL_OUT_FRONT_CENTER = get_static_field<int>(c, "CHANNEL_OUT_FRONT_CENTER");
+      CJNIAudioFormat::CHANNEL_OUT_FRONT_RIGHT_OF_CENTER = get_static_field<int>(c, "CHANNEL_OUT_FRONT_RIGHT_OF_CENTER");
+      CJNIAudioFormat::CHANNEL_OUT_FRONT_RIGHT = get_static_field<int>(c, "CHANNEL_OUT_FRONT_RIGHT");
+      CJNIAudioFormat::CHANNEL_OUT_LOW_FREQUENCY = get_static_field<int>(c, "CHANNEL_OUT_LOW_FREQUENCY");
+      CJNIAudioFormat::CHANNEL_OUT_BACK_LEFT = get_static_field<int>(c, "CHANNEL_OUT_BACK_LEFT");
+      CJNIAudioFormat::CHANNEL_OUT_BACK_CENTER = get_static_field<int>(c, "CHANNEL_OUT_BACK_CENTER");
+      CJNIAudioFormat::CHANNEL_OUT_BACK_RIGHT = get_static_field<int>(c, "CHANNEL_OUT_BACK_RIGHT");
+      CJNIAudioFormat::CHANNEL_INVALID = get_static_field<int>(c, "CHANNEL_INVALID");
   }
 }
 
