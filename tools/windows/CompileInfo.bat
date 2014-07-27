2fe6547bb9a5a0a521de4d33a355c5a9cb5b3b34
montellese <montellese@xbmc.org>
2014-07-27 11:42:31 +0200

    [win32] use all uppercase characters for batch keywords in CompileInfo.bat

--- tools/windows/CompileInfo.bat
+++ tools/windows/CompileInfo.bat
@@ -8,10 +8,10 @@ SET bin_dir=%builddeps_dir%\bin
 SET msys_bin_dir=%builddeps_dir%\msys\bin
 
 REM read the version values from version.txt
-for /f %%i in ('%msys_bin_dir%\awk.exe "/VERSION_MAJOR/ {print $2}" %base_dir%\version.txt') do set major=%%i
-for /f %%i in ('%msys_bin_dir%\awk.exe "/VERSION_MINOR/ {print $2}" %base_dir%\version.txt') do set minor=%%i
-for /f %%i in ('%msys_bin_dir%\awk.exe "/VERSION_TAG/ {print $2}" %base_dir%\version.txt') do set tag=%%i
-for /f %%i in ('%msys_bin_dir%\awk.exe "/ADDON_API/ {print $2}" %base_dir%\version.txt') do set addon_api=%%i
+FOR /f %%i IN ('%msys_bin_dir%\awk.exe "/VERSION_MAJOR/ {print $2}" %base_dir%\version.txt') DO SET major=%%i
+FOR /f %%i IN ('%msys_bin_dir%\awk.exe "/VERSION_MINOR/ {print $2}" %base_dir%\version.txt') DO SET minor=%%i
+FOR /f %%i IN ('%msys_bin_dir%\awk.exe "/VERSION_TAG/ {print $2}" %base_dir%\version.txt') DO SET tag=%%i
+FOR /f %%i IN ('%msys_bin_dir%\awk.exe "/ADDON_API/ {print $2}" %base_dir%\version.txt') DO SET addon_api=%%i
 
 REM create the files with the proper version information
 "%msys_bin_dir%\sed.exe" -e s/@APP_VERSION_MAJOR@/%major%/g -e s/@APP_VERSION_MINOR@/%minor%/g -e s/@APP_VERSION_TAG@/%tag%/g "%base_dir%\xbmc\CompileInfo.cpp.in" > "%base_dir%\xbmc\CompileInfo.cpp"
