fe82454ca2f7dc5a18032619e7413594ce3a22e8
Stanislav Vlasic <svlasic@gmail.com>
2014-08-04 19:08:26 +0200

    [AML M8b] Check for Meson8B inside Meson8 block

--- xbmc/utils/AMLUtils.cpp
+++ xbmc/utils/AMLUtils.cpp
@@ -171,10 +171,13 @@ enum AML_DEVICE_TYPE aml_get_device_type()
       aml_device_type = AML_DEVICE_TYPE_M3;
     else if (cpu_hardware.find("Meson6") != std::string::npos)
       aml_device_type = AML_DEVICE_TYPE_M6;
-    else if ((cpu_hardware.find("Meson8") != std::string::npos) && (cpu_hardware.find("Meson8B") == std::string::npos))
-      aml_device_type = AML_DEVICE_TYPE_M8;
-    else if (cpu_hardware.find("Meson8B") != std::string::npos)
-      aml_device_type = AML_DEVICE_TYPE_M8B;
+    else if (cpu_hardware.find("Meson8") != std::string::npos)
+    {
+      if (cpu_hardware.find("Meson8B") == std::string::npos)
+        aml_device_type = AML_DEVICE_TYPE_M8;
+      else
+        aml_device_type = AML_DEVICE_TYPE_M8B;
+    }
     else
       aml_device_type = AML_DEVICE_TYPE_UNKNOWN;
   }
