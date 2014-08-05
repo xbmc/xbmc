541c75091735c37e35181432d5d2a7a8fcf62759
Tobias Hieta <tobias@plexapp.com>
2014-08-05 15:39:15 +0200

    Add pre-compiled headers to xcode.

--- plex/pch_plex.h
+++ plex/pch_plex.h
@@ -1,10 +1,14 @@
+
+#ifdef _WIN32
 #pragma once
+#endif
+
+#ifndef __PCH_PLEX_H__
+#define __PCH_PLEX_H__
+
+#ifdef __cplusplus
+#ifdef _WIN32
 #define _CRT_RAND_S
-#include <vector>
-#include <map>
-#include <string>
-#include <iostream>
-#include <fstream>
 #if !(defined(_WINSOCKAPI_) || defined(_WINSOCK_H))
 #include <winsock2.h>
 #endif
@@ -22,7 +26,18 @@
 #else
 #include <d3d9types.h>
 #endif
+#endif
+
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
 #include "boost/shared_ptr.hpp"
 // anything below here should be headers that very rarely (hopefully never)
 // change yet are included almost everywhere.
 #include "utils/StdString.h"
+#include "PlexTypes.h"
+#endif
+
+#endif
