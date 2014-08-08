0ebe138fcfa10d477e3612cddf52404feac56c50
John Smith <johns90812@gmail.com>
2014-08-08 13:22:45 +0200

    Teletext font width dynamic change fix. Ticket #15332.

--- xbmc/video/Teletext.cpp
+++ xbmc/video/Teletext.cpp
@@ -1452,6 +1452,8 @@ void CTeletextDecoder::DoRenderPage(int startrow, int national_subset_bak)
       break;
     }
   }
+  m_RenderInfo.FontWidth_Normal = m_RenderInfo.Width / (m_RenderInfo.nofirst ? 39 : 40);
+  SetFontWidth(m_RenderInfo.FontWidth_Normal);
 
   if (m_RenderInfo.TranspMode || m_RenderInfo.Boxed)
   {
