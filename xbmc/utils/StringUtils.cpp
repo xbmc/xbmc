064a8a2e1b84e4cf22c207452a4027f98a220b8a
Karlson2k <k2k@narod.ru>
2014-08-19 14:20:55 +0400

    StringUtils::Split: optimize

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -661,7 +661,7 @@ std::string StringUtils::Join(const vector<string> &strings, const std::string& 
 
 vector<string> StringUtils::Split(const std::string& input, const std::string& delimiter, unsigned int iMaxStrings /* = 0 */)
 {
-  vector<string> results;
+  std::vector<std::string> results;
   if (input.empty())
     return results;
   if (delimiter.empty())
@@ -670,58 +670,21 @@ vector<string> StringUtils::Split(const std::string& input, const std::string& d
     return results;
   }
 
-  size_t iPos = std::string::npos;
-  size_t newPos = std::string::npos;
-  size_t sizeS2 = delimiter.size();
-  size_t isize = input.size();
-
-  vector<unsigned int> positions;
-
-  newPos = input.find(delimiter, 0);
-
-  if (newPos == std::string::npos)
+  const size_t delimLen = delimiter.length();
+  size_t nextDelim;
+  size_t textPos = 0;
+  do
   {
-    results.push_back(input);
-    return results;
-  }
-
-  while (newPos != std::string::npos)
-  {
-    positions.push_back(newPos);
-    iPos = newPos;
-    newPos = input.find(delimiter, iPos + sizeS2);
-  }
-
-  // numFound is the number of delimiters which is one less
-  // than the number of substrings
-  unsigned int numFound = positions.size();
-  if (iMaxStrings > 0 && numFound >= iMaxStrings)
-    numFound = iMaxStrings - 1;
-
-  for ( unsigned int i = 0; i <= numFound; i++ )
-  {
-    string s;
-    if ( i == 0 )
+    if (--iMaxStrings == 0)
     {
-      if ( i == numFound )
-        s = input;
-      else
-        s = input.substr(i, positions[i]);
+      results.push_back(input.substr(textPos));
+      break;
     }
-    else
-    {
-      size_t offset = positions[i - 1] + sizeS2;
-      if ( offset < isize )
-      {
-        if ( i == numFound )
-          s = input.substr(offset);
-        else if ( i > 0 )
-          s = input.substr( positions[i - 1] + sizeS2,
-                         positions[i] - positions[i - 1] - sizeS2 );
-      }
-    }
-    results.push_back(s);
-  }
+    nextDelim = input.find(delimiter, textPos);
+    results.push_back(input.substr(textPos, nextDelim - textPos));
+    textPos = nextDelim + delimLen;
+  } while (nextDelim != std::string::npos);
+
   return results;
 }
 
