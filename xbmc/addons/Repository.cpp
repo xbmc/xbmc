xbmc/xbmc

commit 2b35f385c5d62d19b6857b1baf56f0d509f51545

[addons] adds CanInstall() to CAddon to allow for non-install of add-ons by type rather than special-casing for PVR in CRepository

xbmc/addons/Repository.cpp
@@ -26,7 +26,6 @@
 #include "dialogs/GUIDialogKaiToast.h"
 #include "filesystem/File.h"
 #include "filesystem/PluginDirectory.h"
-#include "pvr/PVRManager.h"
 #include "settings/Settings.h"
 #include "utils/log.h"
 #include "utils/StringUtils.h"
@@ -282,10 +281,7 @@ bool CRepositoryUpdateJob::DoWork()
         if (URIUtils::IsInternetStream(newAddon->Path()))
           referer = StringUtils::Format("Referer=%s-%s.zip",addon->ID().c_str(),addon->Version().asString().c_str());
 
-        if (newAddon->Type() == ADDON_PVRDLL &&
-            !PVR::CPVRManager::Get().InstallAddonAllowed(newAddon->ID()))
-          PVR::CPVRManager::Get().MarkAsOutdated(addon->ID(), referer);
-        else
+        if (!newAddon->CanInstall(referer))
           CAddonInstaller::Get().Install(addon->ID(), true, referer);
       }
       else

xbmc/addons/Addon.h
@@ -191,6 +191,8 @@ public:
   /*! \brief retrieve the running instance of an add-on if it persists while running.
    */
   virtual AddonPtr GetRunningInstance() const { return AddonPtr(); }
+
+  virtual bool CanInstall(const std::string& referer) { return true; }
 protected:
   friend class CAddonCallbacksAddon;
 

xbmc/addons/IAddon.h
@@ -118,6 +118,7 @@ namespace ADDON
     virtual void OnDisabled() =0;
     virtual void OnEnabled() =0;
     virtual AddonPtr GetRunningInstance() const=0;
+    virtual bool CanInstall(const std::string& referer) =0;
 
   protected:
     virtual bool LoadSettings(bool bForce = false) =0;

xbmc/pvr/addons/PVRClient.cpp
@@ -81,6 +81,16 @@ AddonPtr CPVRClient::GetRunningInstance() const
   return CAddon::GetRunningInstance();
 }
 
+bool CPVRClient::CanInstall(const std::string &referer)
+{
+  if (!PVR::CPVRManager::Get().InstallAddonAllowed(ID()))
+  {
+    PVR::CPVRManager::Get().MarkAsOutdated(ID(), referer);
+    return false;
+  }
+  return CAddon::CanInstall(referer);
+}
+
 void CPVRClient::ResetProperties(int iClientId /* = PVR_INVALID_CLIENT_ID */)
 {
   /* initialise members */

xbmc/pvr/addons/PVRClient.h
@@ -61,6 +61,7 @@ namespace PVR
     virtual void OnDisabled();
     virtual void OnEnabled();
     virtual ADDON::AddonPtr GetRunningInstance() const;
+    virtual bool CanInstall(const std::string &referer);
 
     /** @name PVR add-on methods */
     //@{

commit eb04b26980e5d822ae1ba8ea2efdaf82548a1306

[addons][Fix] Make the auto update of addons work again. This fixes a regression introduced by 2b35f385c5d62d19b6857b1baf56f0d509f51545.

xbmc/addons/Repository.cpp
@@ -297,7 +297,7 @@ bool CRepositoryUpdateJob::DoWork()
         if (URIUtils::IsInternetStream(newAddon->Path()))
           referer = StringUtils::Format("Referer=%s-%s.zip",addon->ID().c_str(),addon->Version().asString().c_str());
 
-        if (!newAddon->CanInstall(referer))
+        if (newAddon->CanInstall(referer))
           CAddonInstaller::Get().Install(addon->ID(), true, referer);
       }
       else