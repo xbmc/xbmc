b63b219ae6cfbc6b7e97292f16bdc4d395691f30
cascade256 <128hi256@gmail.com>
2014-09-06 01:55:08 -0700

    Fixed merge conflict

--- xbmc/cores/dvdplayer/DVDPlayer.h
+++ xbmc/cores/dvdplayer/DVDPlayer.h
@@ -187,8 +187,6 @@ public:
   virtual float GetPercentage();
   virtual float GetCachePercentage();
 
-  virtual void RegisterAudioCallback(IAudioCallback* pCallback) { m_dvdPlayerAudio.RegisterAudioCallback(pCallback); }
-  virtual void UnRegisterAudioCallback()                        { m_dvdPlayerAudio.UnRegisterAudioCallback(); }
   virtual void SetVolume(float nVolume)                         { m_dvdPlayerAudio.SetVolume(nVolume); }
   virtual void SetEDL(const std::string& path);
   virtual void SetMute(bool bOnOff)                             { m_dvdPlayerAudio.SetMute(bOnOff); }
