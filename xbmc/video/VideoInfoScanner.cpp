27b6754c664993f07b42c77431e528996944cfb8
Thomas Amland <thomas.amland@gmail.com>
2014-09-06 16:04:20 +0200

    [videoinfoscanner] extract tv part from DoScan

--- xbmc/video/VideoInfoScanner.cpp
+++ xbmc/video/VideoInfoScanner.cpp
@@ -314,6 +314,86 @@ namespace VIDEO
     return !m_bStop;
   }
 
+  bool CVideoInfoScanner::ScanTVFolder(const CStdString& strDirectory, const SScanSettings& settings, bool foundDirectly)
+  {
+    const vector<string> &regexps =  g_advancedSettings.m_tvshowExcludeFromScanRegExps;
+    if (CUtil::ExcludeFileOrFolder(strDirectory, regexps))
+      return true;
+
+    CFileItemList items;
+    bool bSkip = false;
+    CStdString hash, dbHash;
+
+    if (foundDirectly && !settings.parent_name_root)
+    {
+      CDirectory::GetDirectory(strDirectory, items, g_advancedSettings.m_videoExtensions);
+      items.SetPath(strDirectory);
+      GetPathHash(items, hash);
+      bSkip = true;
+      if (!m_database.GetPathHash(strDirectory, dbHash) || dbHash != hash)
+      {
+        m_database.SetPathHash(strDirectory, hash);
+        bSkip = false;
+      }
+      else
+        items.Clear();
+    }
+    else
+    {
+      CFileItemPtr item(new CFileItem(URIUtils::GetFileName(strDirectory)));
+      item->SetPath(strDirectory);
+      item->m_bIsFolder = true;
+      items.Add(item);
+      items.SetPath(URIUtils::GetParentPath(item->GetPath()));
+    }
+
+    if (!bSkip)
+    {
+      if (RetrieveVideoInfo(items, settings.parent_name_root, CONTENT_TVSHOWS))
+      {
+        if (!m_bStop && (CONTENT_TVSHOWS == CONTENT_MOVIES || CONTENT_TVSHOWS == CONTENT_MUSICVIDEOS))
+        {
+          m_database.SetPathHash(strDirectory, hash);
+          if (m_bClean)
+            m_pathsToClean.insert(m_database.GetPathId(strDirectory));
+          CLog::Log(LOGDEBUG, "VideoInfoScanner: Finished adding information from dir %s", CURL::GetRedacted(strDirectory).c_str());
+        }
+      }
+      else
+      {
+        if (m_bClean)
+          m_pathsToClean.insert(m_database.GetPathId(strDirectory));
+        CLog::Log(LOGDEBUG, "VideoInfoScanner: No (new) information was found in dir %s", CURL::GetRedacted(strDirectory).c_str());
+      }
+    }
+    else if (hash != dbHash && (CONTENT_TVSHOWS == CONTENT_MOVIES || CONTENT_TVSHOWS == CONTENT_MUSICVIDEOS))
+    { // update the hash either way - we may have changed the hash to a fast version
+      m_database.SetPathHash(strDirectory, hash);
+    }
+
+    if (m_handle)
+      OnDirectoryScanned(strDirectory);
+
+    for (int i = 0; i < items.Size(); ++i)
+    {
+      CFileItemPtr pItem = items[i];
+
+      if (m_bStop)
+        break;
+
+      // if we have a directory item (non-playlist) we then recurse into that folder
+      // do not recurse for tv shows - we have already looked recursively for episodes
+      if (pItem->m_bIsFolder && !pItem->IsParentFolder() && !pItem->IsPlayList() && settings.recurse > 0 && CONTENT_TVSHOWS != CONTENT_TVSHOWS)
+      {
+        if (!DoScan(pItem->GetPath()))
+        {
+          m_bStop = true;
+        }
+      }
+    }
+    return !m_bStop;
+  }
+
   bool CVideoInfoScanner::DoScan(const CStdString& strDirectory)
   {
     if (m_handle)
@@ -355,35 +435,9 @@ namespace VIDEO
     }
     else if (content == CONTENT_TVSHOWS)
     {
-      const vector<string> &regexps =  g_advancedSettings.m_tvshowExcludeFromScanRegExps;
-      if (CUtil::ExcludeFileOrFolder(strDirectory, regexps))
-        return true;
-
       if (m_handle)
         m_handle->SetTitle(StringUtils::Format(g_localizeStrings.Get(20319).c_str(), info->Name().c_str()));
-
-      if (foundDirectly && !settings.parent_name_root)
-      {
-        CDirectory::GetDirectory(strDirectory, items, g_advancedSettings.m_videoExtensions);
-        items.SetPath(strDirectory);
-        GetPathHash(items, hash);
-        bSkip = true;
-        if (!m_database.GetPathHash(strDirectory, dbHash) || dbHash != hash)
-        {
-          m_database.SetPathHash(strDirectory, hash);
-          bSkip = false;
-        }
-        else
-          items.Clear();
-      }
-      else
-      {
-        CFileItemPtr item(new CFileItem(URIUtils::GetFileName(strDirectory)));
-        item->SetPath(strDirectory);
-        item->m_bIsFolder = true;
-        items.Add(item);
-        items.SetPath(URIUtils::GetParentPath(item->GetPath()));
-      }
+      return ScanTVFolder(strDirectory, settings, foundDirectly);
     }
 
     if (!bSkip)
