63bd1711899f55c96c3f5062b17ef1470f197d0a
Rainer Hochecker <fernetmenta@online.de>
2014-09-09 20:31:03 +0200

    dxva: fix c/p error after b92d51f9fc3ad40da13aa0c153a13e278937ecee

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DXVA.cpp
@@ -349,6 +349,8 @@ bool CDXVAContext::EnsureContext(CDXVAContext **ctx, CDecoder *decoder)
   {
     m_context->m_refCount++;
     *ctx = m_context;
+    if (!m_context->IsValidDecoder(decoder))
+      m_context->m_decoders.push_back(decoder);
     return true;
   }
   m_context = new CDXVAContext();
