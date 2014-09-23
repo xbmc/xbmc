84f0c92b18041603d02bbdd926b356f1e48dbf26
Memphiz <memphis@machzwo.de>
2014-09-23 22:30:50 +0200

    [airplay] - implement different announcements when using the ios8 compatibility mode

--- xbmc/network/AirTunesServer.cpp
+++ xbmc/network/AirTunesServer.cpp
@@ -389,9 +389,9 @@ bool CAirTunesServer::StartServer(int port, bool nonlocal, bool usePassword, con
     txt.push_back(std::make_pair("pw",  usePassword?"true":"false"));
     txt.push_back(std::make_pair("vn",  "3"));
     txt.push_back(std::make_pair("da",  "true"));
-    txt.push_back(std::make_pair("vs",  "130.14"));
     txt.push_back(std::make_pair("md",  "0,1,2"));
     txt.push_back(std::make_pair("am",  "Xbmc,1"));
+    txt.push_back(std::make_pair("vs",  "130.14"));
 
     CZeroconf::GetInstance()->PublishService("servers.airtunes", "_raop._tcp", appName, port, txt);
   }
