7bd3dbd9f3e71a5005b002e4f480905ffd900d1b
Tobias Hieta <tobias@plexapp.com>
2014-10-02 13:13:08 +0200

    Add more data to the user selector.

--- plex/GUI/GUIDialogPlexUserSelect.cpp
+++ plex/GUI/GUIDialogPlexUserSelect.cpp
@@ -18,15 +18,39 @@ bool CGUIDialogPlexUserSelect::OnMessage(CGUIMessage &message)
     CFileItemPtr user = CFileItemPtr(new CFileItem);
     user->SetLabel("Maru");
     user->SetArt("thumb", "http://mousebreath.com/wp-content/uploads/2011/08/maru__02.jpg");
+    user->SetProperty("isAdminUser", true);
 
     list.Add(user);
 
     user = CFileItemPtr(new CFileItem);
     user->SetLabel("Malört");
     user->SetArt("thumb", "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTL6jvyGu9b1xJAnhgb9bqEWGFbM79j8neTjV-wik5PnRDD3W0wHQ");
+    user->SetProperty("isPinProtected", true);
 
     list.Add(user);
 
+    user = CFileItemPtr(new CFileItem);
+    user->SetLabel("Gin");
+    user->SetArt("thumb", "http://www.gmkfreelogos.com/logos/B/img/Bombay_Gin.gif");
+
+    list.Add(user);
+
+    user = CFileItemPtr(new CFileItem);
+    user->SetLabel("Vodka");
+    user->SetArt("thumb", "http://daleelviradelivery.files.wordpress.com/2012/01/a_absolut_vodka1.jpg");
+
+    list.Add(user);
+    user = CFileItemPtr(new CFileItem);
+    user->SetLabel("Whiskey");
+    user->SetArt("thumb", "http://mackmyra.com/temp/images/USliter.jpg");
+    user->SetProperty("isPinProtected", true);
+
+    list.Add(user);
+    user = CFileItemPtr(new CFileItem);
+    user->SetLabel("Snaps!");
+    user->SetArt("thumb", "http://www.viinikauppa.com/catalog/images/500014915.jpg");
+
+    list.Add(user);
     Add(list);
   }
 
