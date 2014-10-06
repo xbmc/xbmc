07abeb034fc2c4a7b6a8c15753eae550cd1a9586
Karlson2k <k2k@narod.ru>
2014-10-06 23:42:09 +0400

    WIN32Util::PowerManagement: real checks for privileges adjustment, do not adjust more then once

--- xbmc/win32/WIN32Util.cpp
+++ xbmc/win32/WIN32Util.cpp
@@ -192,24 +192,28 @@ char CWIN32Util::FirstDriveFromMask (ULONG unitmask)
 
 bool CWIN32Util::PowerManagement(PowerState State)
 {
-  HANDLE hToken;
-  TOKEN_PRIVILEGES tkp;
-  // Get a token for this process.
-  if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+  static bool gotShutdownPrivileges = false;
+  if (!gotShutdownPrivileges)
   {
-    return false;
-  }
-  // Get the LUID for the shutdown privilege.
-  LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
-  tkp.PrivilegeCount = 1;  // one privilege to set
-  tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-  // Get the shutdown privilege for this process.
-  AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0);
-  CloseHandle(hToken);
+    HANDLE hToken;
+    // Get a token for this process.
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+    {
+      // Get the LUID for the shutdown privilege.
+      TOKEN_PRIVILEGES tkp = {};
+      if (LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid))
+      {
+        tkp.PrivilegeCount = 1;  // one privilege to set
+        tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+        // Get the shutdown privilege for this process.
+        if (AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0))
+          gotShutdownPrivileges = true;
+      }
+      CloseHandle(hToken);
+    }
 
-  if (GetLastError() != ERROR_SUCCESS)
-  {
-    return false;
+    if (!gotShutdownPrivileges)
+      return false;
   }
 
   // process OnSleep() events. This is called in main thread.
