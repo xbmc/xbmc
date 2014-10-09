2845082e7210d59d88a5f914239250bd3cc00136
Tobias Hieta <tobias@plexapp.com>
2014-10-09 11:51:19 +0200

    Switch myPlex url to staging2 for debugging parental control.

--- plex/Client/PlexConnection.h
+++ plex/Client/PlexConnection.h
@@ -105,7 +105,7 @@ class CMyPlexConnection : public CPlexConnection
 {
   public:
     CMyPlexConnection()
-    : CPlexConnection(CPlexConnection::CONNECTION_MYPLEX, "plex.tv", 443, "https") {}
+    : CPlexConnection(CPlexConnection::CONNECTION_MYPLEX, "staging2.plex.tv", 443, "https") {}
 
     virtual CStdString GetAccessToken() const
     {
