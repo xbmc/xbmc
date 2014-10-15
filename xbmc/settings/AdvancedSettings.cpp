7048fb68ae8753000116d3f1939914fe035eaa48
clashlab <nicolabriet@gmail.com>
2014-10-15 12:40:47 +0200

    Use multiple regexp on m_videoCleanDateTimeRegExp

--- xbmc/settings/AdvancedSettings.cpp
+++ xbmc/settings/AdvancedSettings.cpp
@@ -188,7 +188,9 @@ void CAdvancedSettings::Initialize()
   m_fullScreenOnMovieStart = true;
   m_cachePath = "special://temp/";
 
-  m_videoCleanDateTimeRegExp = "(.*[^ _\\,\\.\\(\\)\\[\\]\\-])[ _\\.\\(\\)\\[\\]\\-]+(19[0-9][0-9]|20[0-9][0-9])([ _\\,\\.\\(\\)\\[\\]\\-]|[^0-9]$)?|(?P<year>19[0-9][0-9]|20[0-9][0-9])[ _\\.\\(\\)\\[\\]\\-]+(?P<title>.+[^ _\\,\\.\\(\\)\\[\\]\\-])([ _\\,\\.\\(\\)\\[\\]\\-][^0-9]|$)?";
+  m_videoCleanDateTimeRegExps.clear();
+  m_videoCleanDateTimeRegExps.push_back("(?P<title>.+[^ _\\,\\.\\(\\)\\[\\]\\-])[ _\\.\\(\\)\\[\\]\\-]+(?P<year>19[0-9][0-9]|20[0-1][0-9])([ _\\,\\.\\(\\)\\[\\]\\-][^0-9]|$)");
+  m_videoCleanDateTimeRegExps.push_back("(?P<year>19[0-9][0-9]|20[0-1][0-9])[ _\\.\\(\\)\\[\\]\\-]+(?P<title>.+[^ _\\,\\.\\(\\)\\[\\]\\-])([ _\\,\\.\\(\\)\\[\\]\\-][^0-9]|$)");
 
   m_videoCleanStringRegExps.clear();
   m_videoCleanStringRegExps.push_back("[ _\\,\\.\\(\\)\\[\\]\\-](ac3|dts|custom|dc|remastered|divx|divx5|dsr|dsrip|dutch|dvd|dvd5|dvd9|dvdrip|dvdscr|dvdscreener|screener|dvdivx|cam|fragment|fs|hdtv|hdrip|hdtvrip|internal|limited|multisubs|ntsc|ogg|ogm|pal|pdtv|proper|repack|rerip|retail|r3|r5|bd5|se|svcd|swedish|german|read.nfo|nfofix|unrated|extended|ws|telesync|ts|telecine|tc|brrip|bdrip|480p|480i|576p|576i|720p|720i|1080p|1080i|3d|hrhd|hrhdtv|hddvd|bluray|x264|h264|xvid|xvidvd|xxx|www.www|cd[1-9]|\\[.*\\])([ _\\,\\.\\(\\)\\[\\]\\-]|$)");
@@ -427,6 +429,7 @@ bool CAdvancedSettings::Load()
 void CAdvancedSettings::ParseSettingsFile(const std::string &file)
 {
   CXBMCTinyXML advancedXML;
+  CStdString cleanDateTimeRegExp;
   if (!CFile::Exists(file))
   {
     CLog::Log(LOGNOTICE, "No settings file to load (%s)", file.c_str());
@@ -540,7 +543,23 @@ void CAdvancedSettings::ParseSettingsFile(const std::string &file)
     if (pVideoExcludes)
       GetCustomRegexps(pVideoExcludes, m_videoCleanStringRegExps);
 
-    XMLUtils::GetString(pElement,"cleandatetime", m_videoCleanDateTimeRegExp);
+    pVideoExcludes = pElement->FirstChildElement("cleandatetime");
+    if (pVideoExcludes)
+    {
+      if (XMLUtils::HasChild(pVideoExcludes, "regexp"))
+        GetCustomRegexps(pVideoExcludes, m_videoCleanDateTimeRegExps);
+      else
+      {
+        // Support old behavior, only one regexp
+        XMLUtils::GetString(pElement,"cleandatetime", cleanDateTimeRegExp);
+        if (!cleanDateTimeRegExp.empty())
+        {
+          m_videoCleanDateTimeRegExps.clear();
+          m_videoCleanDateTimeRegExps.push_back(cleanDateTimeRegExp.c_str());
+        }
+      }
+    }
+
     XMLUtils::GetString(pElement,"ppffmpegdeinterlacing",m_videoPPFFmpegDeint);
     XMLUtils::GetString(pElement,"ppffmpegpostprocessing",m_videoPPFFmpegPostProc);
     XMLUtils::GetInt(pElement,"vdpauscaling",m_videoVDPAUScaling);
@@ -1136,6 +1155,7 @@ void CAdvancedSettings::ParseSettingsFile(const std::string &file)
 void CAdvancedSettings::Clear()
 {
   m_videoCleanStringRegExps.clear();
+  m_videoCleanDateTimeRegExps.clear();
   m_moviesExcludeFromScanRegExps.clear();
   m_tvshowExcludeFromScanRegExps.clear();
   m_videoExcludeFromListingRegExps.clear();
