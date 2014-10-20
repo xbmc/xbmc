fb418732835d77eb27e1f3031fddc6d0d0b7976f
Stephan Raue <stephan@openelec.tv>
2014-10-20 16:19:11 +0200

    [rebrand/cmake] Rename cmake scripts, change include/xbmc and lib/xbmc locations to include/kodi and lib/kodi so audio addons can be build and installed in the right location. Tested on linux/OpenELEC

--- project/cmake/scripts/common/kodi-prepare-env.cmake
+++ project/cmake/scripts/common/kodi-prepare-env.cmake
@@ -13,27 +13,27 @@ if(NOT DEFINED APP_VERSION_MAJOR OR NOT DEFINED APP_VERSION_MINOR)
   message(FATAL_ERROR "Could not determine app version! make sure that ${XBMCROOT}/version.txt exists")
 endif()
 
-### copy all the addon binding header files to include/xbmc
-# make sure include/xbmc exists and is empty
-set(XBMC_LIB_DIR ${DEPENDS_PATH}/lib/xbmc)
+### copy all the addon binding header files to include/kodi
+# make sure include/kodi exists and is empty
+set(XBMC_LIB_DIR ${DEPENDS_PATH}/lib/kodi)
 if(NOT EXISTS "${XBMC_LIB_DIR}/")
   file(MAKE_DIRECTORY ${XBMC_LIB_DIR})
 endif()
 
-set(XBMC_INCLUDE_DIR ${DEPENDS_PATH}/include/xbmc)
+set(XBMC_INCLUDE_DIR ${DEPENDS_PATH}/include/kodi)
 if(NOT EXISTS "${XBMC_INCLUDE_DIR}/")
   file(MAKE_DIRECTORY ${XBMC_INCLUDE_DIR})
 endif()
 
-# xbmc-config.cmake.in (further down) expects a "prefix" variable
+# kodi-config.cmake.in (further down) expects a "prefix" variable
 get_filename_component(prefix "${DEPENDS_PATH}" ABSOLUTE)
 
-# generate the proper xbmc-config.cmake file
-configure_file(${XBMCROOT}/project/cmake/xbmc-config.cmake.in ${XBMC_LIB_DIR}/xbmc-config.cmake @ONLY)
-# copy cmake helpers to lib/xbmc
-file(COPY ${XBMCROOT}/project/cmake/scripts/common/xbmc-addon-helpers.cmake ${XBMCROOT}/project/cmake/scripts/common/addoptions.cmake DESTINATION ${XBMC_LIB_DIR})
+# generate the proper kodi-config.cmake file
+configure_file(${XBMCROOT}/project/cmake/kodi-config.cmake.in ${XBMC_LIB_DIR}/kodi-config.cmake @ONLY)
+# copy cmake helpers to lib/kodi
+file(COPY ${XBMCROOT}/project/cmake/scripts/common/kodi-addon-helpers.cmake ${XBMCROOT}/project/cmake/scripts/common/addoptions.cmake DESTINATION ${XBMC_LIB_DIR})
 
-### copy all the addon binding header files to include/xbmc
+### copy all the addon binding header files to include/kodi
 # parse addon-bindings.mk to get the list of header files to copy
 file(STRINGS ${XBMCROOT}/xbmc/addons/addon-bindings.mk bindings)
 string(REPLACE "\n" ";" bindings "${bindings}")
@@ -41,6 +41,6 @@ foreach(binding ${bindings})
   string(REPLACE " =" ";" binding "${binding}")
   string(REPLACE "+=" ";" binding "${binding}")
   list(GET binding 1 header)
-  # copy the header file to include/xbmc
+  # copy the header file to include/kodi
   file(COPY ${XBMCROOT}/${header} DESTINATION ${XBMC_INCLUDE_DIR})
 endforeach()
