a0c76f05581f5d49f56cd4ba7e34c62e1cf788b6
montellese <montellese@xbmc.org>
2014-11-01 00:10:32 +0100

    cmake: rename XBMCROOT to APP_ROOT with backwards compatibility

--- project/cmake/scripts/common/prepare-env.cmake
+++ project/cmake/scripts/common/prepare-env.cmake
@@ -1,6 +1,6 @@
 # parse version.txt to get the version info
-if(EXISTS "${XBMCROOT}/version.txt")
-  file(STRINGS "${XBMCROOT}/version.txt" versions)
+if(EXISTS "${APP_ROOT}/version.txt")
+  file(STRINGS "${APP_ROOT}/version.txt" versions)
   foreach (version ${versions})
     if(version MATCHES "^VERSION_.*")
       string(REGEX MATCH "^[^ ]+" version_name ${version})
@@ -16,7 +16,7 @@ endif()
 
 # bail if we can't parse versions
 if(NOT DEFINED APP_VERSION_MAJOR OR NOT DEFINED APP_VERSION_MINOR)
-  message(FATAL_ERROR "Could not determine app version! make sure that ${XBMCROOT}/version.txt exists")
+  message(FATAL_ERROR "Could not determine app version! make sure that ${APP_ROOT}/version.txt exists")
 endif()
 
 ### copy all the addon binding header files to include/kodi
@@ -45,23 +45,23 @@ endif()
 get_filename_component(prefix "${DEPENDS_PATH}" ABSOLUTE)
 
 # generate the proper kodi-config.cmake file
-configure_file(${XBMCROOT}/project/cmake/kodi-config.cmake.in ${KODI_LIB_DIR}/kodi-config.cmake @ONLY)
+configure_file(${APP_ROOT}/project/cmake/kodi-config.cmake.in ${KODI_LIB_DIR}/kodi-config.cmake @ONLY)
 # copy cmake helpers to lib/kodi
-file(COPY ${XBMCROOT}/project/cmake/scripts/common/addon-helpers.cmake ${XBMCROOT}/project/cmake/scripts/common/addoptions.cmake DESTINATION ${KODI_LIB_DIR})
+file(COPY ${APP_ROOT}/project/cmake/scripts/common/addon-helpers.cmake ${APP_ROOT}/project/cmake/scripts/common/addoptions.cmake DESTINATION ${KODI_LIB_DIR})
 
 # generate xbmc-config.cmake for backwards compatibility to xbmc
-configure_file(${XBMCROOT}/project/cmake/xbmc-config.cmake.in ${XBMC_LIB_DIR}/xbmc-config.cmake @ONLY)
+configure_file(${APP_ROOT}/project/cmake/xbmc-config.cmake.in ${XBMC_LIB_DIR}/xbmc-config.cmake @ONLY)
 
 ### copy all the addon binding header files to include/kodi
 # parse addon-bindings.mk to get the list of header files to copy
-file(STRINGS ${XBMCROOT}/xbmc/addons/addon-bindings.mk bindings)
+file(STRINGS ${APP_ROOT}/xbmc/addons/addon-bindings.mk bindings)
 string(REPLACE "\n" ";" bindings "${bindings}")
 foreach(binding ${bindings})
   string(REPLACE " =" ";" binding "${binding}")
   string(REPLACE "+=" ";" binding "${binding}")
   list(GET binding 1 header)
   # copy the header file to include/kodi
-  file(COPY ${XBMCROOT}/${header} DESTINATION ${KODI_INCLUDE_DIR})
+  file(COPY ${APP_ROOT}/${header} DESTINATION ${KODI_INCLUDE_DIR})
 
   # auto-generate header files for backwards comaptibility to xbmc with deprecation warning
   get_filename_component(headerfile ${header} NAME)
