80747ba72faea09a74b64d1d81004c3e899c13e3
Ben Woods <woodsb02@gmail.com>
2014-11-07 22:49:33 +0800

    Add TARGET_FREEBSD
    Conflicts:
    	lib/cpluff/CMakeLists.txt

--- plex/CMakeModules/PlatformConfigPOSIX.cmake
+++ plex/CMakeModules/PlatformConfigPOSIX.cmake
@@ -38,7 +38,12 @@ else(TARGET_RPI)
 endif(TARGET_RPI)
 
 ############ Check for pthread_setname -> HAVE_PTHREAD_SETNAME_NP
-set(HAVE_PTHREAD_SETNAME_NP 1)
+if(TARGET_FREEBSD)
+  set(HAVE_PTHREAD_SET_NAME_NP 1)
+else()
+  set(HAVE_PTHREAD_SETNAME_NP 1)
+endif()
+
 if(NOT TARGET_RPI)
   set(HAS_SDL_JOYSTICK 1)
 endif()
