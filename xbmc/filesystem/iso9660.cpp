a72861ac87530d2b1ad94828200c4c5716429fa7
Joakim Plate <elupus@ecce.se>
2014-11-21 23:15:51 +0100

    fixed: CID 719030 Copy into fixed size buffer
    The string operation will write past the end of the fixed-size destination buffer if the source buffer is too large.
    In iso9660::​FindNextFile(CXHandle *, _WIN32_FIND_DATA *): A source buffer of statically unknown size is copied into a fixed-size destination buffer (CWE-120)

--- xbmc/filesystem/iso9660.cpp
+++ xbmc/filesystem/iso9660.cpp
@@ -628,7 +628,11 @@ HANDLE iso9660::FindFirstFile( char *szLocalFolder, WIN32_FIND_DATA *wfdFile )
 
     if ( m_searchpointer )
     {
-      strcpy(wfdFile->cFileName, m_searchpointer->name );
+      if (sizeof(m_searchpointer->name) > sizeof(wfdFile->cFileName) -1)
+        CLog::Log(LOGWARNING, "iso9660::FindFirstFile length of search pattern too large");
+
+      strncpy(wfdFile->cFileName, m_searchpointer->name, sizeof(wfdFile->cFileName) - 1 );
+      wfdFile->cFileName[sizeof(wfdFile->cFileName) - 1] = 0;
 
       if ( m_searchpointer->type == 2 )
         wfdFile->dwFileAttributes |= FILE_ATTRIBUTE_DIRECTORY;
