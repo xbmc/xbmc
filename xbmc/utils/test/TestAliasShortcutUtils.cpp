60d418e75a083716c7333101d31488aecdffd09e
Memphiz <memphis@machzwo.de>
2014-12-10 11:59:35 +0100

    [tests/osx] - add unit tests for aliasshortcut resolving

--- xbmc/utils/test/TestAliasShortcutUtils.cpp
+++ xbmc/utils/test/TestAliasShortcutUtils.cpp
@@ -19,26 +19,85 @@
  */
 
 #include "utils/AliasShortcutUtils.h"
+#include "filesystem/File.h"
+#include "test/TestUtils.h"
 
+#if defined(TARGET_DARWIN_OSX)
+#include "osx/DarwinUtils.h"
+#endif
 #include "gtest/gtest.h"
 
 TEST(TestAliasShortcutUtils, IsAliasShortcut)
 {
-  std::string a;
+  XFILE::CFile *tmpFile = XBMC_CREATETEMPFILE("noaliastest");
+  std::string noalias = XBMC_TEMPFILEPATH(tmpFile);
+
 #if defined(TARGET_DARWIN_OSX)
-  /* TODO: Write test case for OSX */
-#else
-  EXPECT_FALSE(IsAliasShortcut(a));
+  XFILE::CFile *aliasDestFile = XBMC_CREATETEMPFILE("aliastest");
+  std::string alias = XBMC_TEMPFILEPATH(aliasDestFile);
+
+  //we only need the path here so delete the alias file
+  //which will be recreated as shortcut later:
+  XBMC_DELETETEMPFILE(aliasDestFile);
+
+  // create alias from a pointing to /Volumes
+  CDarwinUtils::CreateAliasShortcut(alias, "/Volumes");
+  EXPECT_TRUE(IsAliasShortcut(alias));
+  XFILE::CFile::Delete(alias);
+
+  // volumes is not a shortcut but a dir
+  EXPECT_FALSE(IsAliasShortcut("/Volumes"));
 #endif
+
+  // a regular file is not a shortcut
+  EXPECT_FALSE(IsAliasShortcut(noalias));
+  XBMC_DELETETEMPFILE(tmpFile);
+
+  // empty string is not an alias
+  std::string emptyString;
+  EXPECT_FALSE(IsAliasShortcut(emptyString));
+
+  // non-existent file is no alias
+  std::string nonExistingFile="/IDontExistsNormally/somefile.txt";
+  EXPECT_FALSE(IsAliasShortcut(nonExistingFile));
 }
 
 TEST(TestAliasShortcutUtils, TranslateAliasShortcut)
 {
-  std::string a;
-  TranslateAliasShortcut(a);
+  XFILE::CFile *tmpFile = XBMC_CREATETEMPFILE("noaliastest");
+  std::string noalias = XBMC_TEMPFILEPATH(tmpFile);
+  std::string noaliastemp = noalias;
+
 #if defined(TARGET_DARWIN_OSX)
-  /* TODO: Write test case for OSX */
-#else
-  EXPECT_STREQ("", a.c_str());
+  XFILE::CFile *aliasDestFile = XBMC_CREATETEMPFILE("aliastest");
+  std::string alias = XBMC_TEMPFILEPATH(aliasDestFile);
+
+  //we only need the path here so delete the alias file
+  //which will be recreated as shortcut later:
+  XBMC_DELETETEMPFILE(aliasDestFile);
+
+  // create alias from a pointing to /Volumes
+  CDarwinUtils::CreateAliasShortcut(alias, "/Volumes");
+
+  // resolve the shortcut
+  TranslateAliasShortcut(alias);
+  EXPECT_STREQ("/Volumes", alias.c_str());
+  XFILE::CFile::Delete(alias);
 #endif
+
+  // translating a non-shortcut url should result in no change...
+  TranslateAliasShortcut(noaliastemp);
+  EXPECT_STREQ(noaliastemp.c_str(), noalias.c_str());
+  XBMC_DELETETEMPFILE(tmpFile);
+
+  //translate empty should stay empty
+  std::string emptyString;
+  TranslateAliasShortcut(emptyString);
+  EXPECT_STREQ("", emptyString.c_str());
+
+  // translate non-existent file should result in no change...
+  std::string nonExistingFile="/IDontExistsNormally/somefile.txt";
+  std::string resolvedNonExistingFile=nonExistingFile;
+  TranslateAliasShortcut(resolvedNonExistingFile);
+  EXPECT_STREQ(resolvedNonExistingFile.c_str(), nonExistingFile.c_str());
 }
