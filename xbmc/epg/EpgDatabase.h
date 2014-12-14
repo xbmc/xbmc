3799bcd74ebf7aece8c8d1130d93a60bf2e3538d
Ryan Gribble <ryan.gribble@gmail.com>
2014-12-14 22:23:43 +1000

    [pvr] EPG_INFO field "strIconPath" was not being stored/retrieved in the EPG database

--- xbmc/epg/EpgDatabase.h
+++ xbmc/epg/EpgDatabase.h
@@ -54,7 +54,7 @@ namespace EPG
      * @brief Get the minimal database version that is required to operate correctly.
      * @return The minimal database version.
      */
-    virtual int GetSchemaVersion(void) const { return 8; };
+    virtual int GetSchemaVersion(void) const { return 9; };
 
     /*!
      * @brief Get the default sqlite database filename.
