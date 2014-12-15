396a03ef130c0a4198ac08094c78ee24bca48c7a
Martijn Kaijser <mcm.kaijser@gmail.com>
2014-12-15 19:48:08 +0100

    [addons] sync with repo

--- addons/service.xbmc.versioncheck/lib/versions.py
+++ addons/service.xbmc.versioncheck/lib/versions.py
@@ -25,90 +25,97 @@ def compare_version(version_installed, versionlist):
     versionlist_beta = versionlist['releases']['beta']
     versionlist_alpha = versionlist['releases']['alpha']
     versionlist_prealpha = versionlist['releases']['prealpha'] 
-    log("Version installed %s" %version_installed)
+    log('Version installed %s' %version_installed)
     ### Check to upgrade to newest available stable version
     # check on smaller major version. Smaller version than available always notify
     oldversion = False
-    msg = ''
-
-    # check if current major version is smaller than available major stable
+    version_available = ''
+    # check if installed major version is smaller than available major stable
     # here we don't care if running non stable
     if version_installed['major'] < int(versionlist_stable[0]['major']):
-        msg = 32003
-        oldversion = "stable"
-        log("Version available  %s" %versionlist_stable[0])
-        log("You are running an older version")
+        version_available = versionlist_stable[0]
+        oldversion = 'stable'
+        log('Version available  %s' %versionlist_stable[0])
+        log('You are running an older version')
 
 
-    # check if current major version is equal than available major stable
+    # check if installed major version is equal than available major stable
     # however also check on minor version and still don't care about non stable
     elif version_installed['major'] == int(versionlist_stable[0]['major']):
         if version_installed['minor'] < int(versionlist_stable[0]['minor']):
-            msg = 32003
-            oldversion = "stable"
-            log("Version available  %s" %versionlist_stable[0])
-            log("You are running an older minor version")
+            version_available = versionlist_stable[0]
+            oldversion = 'stable'
+            log('Version available  %s' %versionlist_stable[0])
+            log('You are running an older minor version')
         # check for <= minor !stable
-        elif version_installed['tag'] != "stable" and version_installed['minor'] <= int(versionlist_stable[0]['minor']):
-            msg = 32003
+        elif version_installed['tag'] != 'stable' and version_installed['minor'] <= int(versionlist_stable[0]['minor']):
+            version_available = versionlist_stable[0]
             oldversion = True
-            log("Version available  %s" %versionlist_stable[0])
-            log("You are running an older non stable minor version")
+            log('Version available  %s' %versionlist_stable[0])
+            log('You are running an older non stable minor version')
         else:
-            log("Version available  %s" %versionlist_stable[0])
-            log("There is no newer stable available")
-
-    ## Check to upgrade to newest available RC version if not installed stable
-    ## Check also oldversion hasn't been set true by previous check because if so this need to be skipped
-    elif version_installed['tag'] != "stable":
-        # check if you are using a RC lower than current RC
-        # then check if you are using a alpha/beta lower than current RC
+            log('Version available  %s' %versionlist_stable[0])
+            log('There is no newer stable available')
+    
+    # Already skipped a possible newer stable build. Let's continue with non stable builds.
+    # Check also 'oldversion' hasn't been set to 'stable' or true by previous checks because if so,
+    # those part need to be skipped
+    
+    #check for RC builds
+    if not oldversion and version_installed['tag'] in ['releasecandidate']:
+        # check if you are using a RC build lower than current available RC
+        # then check if you are using a beta/alpha lower than current available RC
+        # 14.0rc3 is newer than:  14.0rc1, 14.0b9, 14.0a15
+        if version_installed.get('tagversion','') < versionlist_rc[0]['tagversion']:
+            version_available = versionlist_rc[0]
+            oldversion = True
+            log('Version available  %s' %versionlist_rc[0])
+            log('You are running an older RC version')
+    # now check if installed !=rc
+    elif not oldversion and version_installed['tag'] in ['beta','alpha','prealpha']:
         if version_installed['major'] <= int(versionlist_rc[0]['major']):
-            if version_installed['tag'] in ["releasecandidate"]:
-                if version_installed['revision'] < versionlist_rc[0]['revision']:
-                    msg = 32004
-                    oldversion = True
-                    log("Version available  %s" %versionlist_rc[0])
-                    log("You are running an older RC version")
-                elif version_installed['revision'] < versionlist_rc[0]['revision']:
-                    msg = 32004
-                    oldversion = True
-                    log("Version available  %s" %versionlist_rc[0])
-                    log("You are running an older non RC version")
-                else:
-                    log("Version available  %s" %versionlist_rc[0])
-                    log("You are running newest RC version")
-
-        # exclude if you are running an RC
-        # check if you are using a beta lower than current beta
-        # then check if you are using a alpha/beta lower than current RC
-        if not oldversion and version_installed['tag'] not in ["releasecandidate"] and version_installed['major'] <= int(versionlist_beta[0]['major']):
-            if version_installed['tag'] in ["beta"]:
-                if version_installed['revision'] < versionlist_beta[0]['revision']:
-                    msg = 32005
-                    oldversion = True
-                    log("Version available  %s" %versionlist_beta[0])
-                    log("You are running an older beta version")
-                elif version_installed['revision'] < versionlist_beta[0]['revision']:
-                    msg = 32005
-                    oldversion = True
-                    log("Version available  %s" %versionlist_beta[0])
-                    log("You are running an older non beta version")
-                else:
-                    log("Version available  %s" %versionlist_beta[0])
-                    log("You are running newest beta version")
-
-        # exclude if you are running an RC/beta
-        # check if you are using a beta lower than current beta
-        # then check if you are using a alpha/beta lower than current RC
-        if not oldversion and version_installed['tag'] in ["prealpha"] and version_installed['major'] <= int(versionlist_prealpha[0]['major']):
-            if version_installed['revision'] < versionlist_prealpha[0]['revision']:
-                msg = 32006
+            if version_installed['minor'] <= int(versionlist_beta[0]['minor']):
+                version_available = versionlist_rc[0]
                 oldversion = True
-                log("Version available  %s" %versionlist_prealpha[0])
-                log("You are running an older alpha version")
-            else:
-                log("Version available  %s" %versionlist_prealpha[0])
-                log("You are running newest alpha version")
+                log('Version available  %s' %versionlist_rc[0])
+                log('You are running an older non RC version')
 
-    return oldversion, msg
+    #check for beta builds
+    if not oldversion and version_installed['tag'] == 'beta':
+        # check if you are using a RC build lower than current available RC
+        # then check if you are using a beta/alpha lower than current available RC
+        # 14.0b3 is newer than:  14.0b1, 14.0a15
+        if version_installed.get('tagversion','') < versionlist_beta[0]['tagversion']:
+            version_available = versionlist_beta[0]
+            oldversion = True
+            log('Version available  %s' %versionlist_beta[0])
+            log('You are running an older beta version')
+    # now check if installed !=beta
+    elif not oldversion and version_installed['tag'] in ['alpha','prealpha']:
+        if version_installed['major'] <= int(versionlist_beta[0]['major']):
+            if version_installed['minor'] <= int(versionlist_beta[0]['minor']):
+                version_available = versionlist_beta[0]
+                oldversion = True
+                log('Version available  %s' %versionlist_beta[0])
+                log('You are running an older non beta version')
+
+    #check for alpha builds and older
+    if not oldversion and version_installed['tag'] == 'alpha':
+        # check if you are using a RC build lower than current available RC
+        # then check if you are using a beta/alpha lower than current available RC
+        # 14.0a3 is newer than: 14.0a1 or pre-alpha
+        if version_installed.get('tagversion','') < versionlist_alpha[0]['tagversion']:
+            version_available = versionlist_alpha[0]
+            oldversion = True
+            log('Version available  %s' %versionlist_alpha[0])
+            log('You are running an older alpha version')
+    # now check if installed !=alpha
+    elif not oldversion and version_installed['tag'] in ['prealpha']:
+        if version_installed['major'] <= int(versionlist_alpha[0]['major']):
+            if version_installed['minor'] <= int(versionlist_alpha[0]['minor']):
+                version_available = versionlist_alpha[0]
+                oldversion = True
+                log('Version available  %s' %versionlist_alpha[0])
+                log('You are running an older non alpah version')     
+    version_stable = versionlist_stable[0]
+    return oldversion, version_installed, version_available, version_stable
--- addons/service.xbmc.versioncheck/service.py
+++ addons/service.xbmc.versioncheck/service.py
@@ -22,6 +22,7 @@ import xbmc
 import lib.common
 from lib.common import log, dialog_yesno
 from lib.common import upgrade_message as _upgrademessage
+from lib.common import upgrade_message2 as _upgrademessage2
 
 __addon__        = lib.common.__addon__
 __addonversion__ = lib.common.__addonversion__
@@ -34,20 +35,14 @@ class Main:
     def __init__(self):
         linux = False
         packages = []
-        if not xbmc.getCondVisibility('System.HasAddon(os.openelec.tv)'):
-            if not sys.argv[0]:
-                xbmc.executebuiltin('XBMC.AlarmClock(CheckAtBoot,XBMC.RunScript(service.xbmc.versioncheck, started),00:00:30,silent)')
-                xbmc.executebuiltin('XBMC.AlarmClock(CheckWhileRunning,XBMC.RunScript(service.xbmc.versioncheck, started),24:00:00,silent,loop)')
-            elif sys.argv[0] and sys.argv[1] == 'started':
-                if xbmc.getCondVisibility('System.Platform.Linux') and __addon__.getSetting("upgrade_apt") == 'true':
-                    packages = ['xbmc']
-                    _versionchecklinux(packages)
-                else:
-                    oldversion, msg = _versioncheck()
-                    if oldversion:
-                        _upgrademessage(msg, oldversion, False)
-            else:
-                pass
+        xbmc.sleep(5000)
+        if xbmc.getCondVisibility('System.Platform.Linux') and __addon__.getSetting("upgrade_apt") == 'true':
+            packages = ['xbmc']
+            _versionchecklinux(packages)
+        else:
+            oldversion, version_installed, version_available, version_stable = _versioncheck()
+            if oldversion:
+                _upgrademessage2( version_installed, version_available, version_stable, oldversion, False)
                 
 def _versioncheck():
     # initial vars
@@ -58,8 +53,8 @@ def _versioncheck():
     # retrieve version installed
     version_installed = get_installedversion()
     # copmpare installed and available
-    oldversion, msg = compare_version(version_installed, versionlist)
-    return oldversion, msg
+    oldversion, version_installed, version_available, version_stable = compare_version(version_installed, versionlist)
+    return oldversion, version_installed, version_available, version_stable
 
 
 def _versionchecklinux(packages):
