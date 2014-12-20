5306fb4e2c33f237e673fadaa480001d2093478a
montellese <montellese@xbmc.org>
2014-12-20 09:38:56 +0100

    events: add specific IEvent implementations

---
+++ xbmc/events/AddonEvent.h
@@ -0,0 +1,40 @@
+#pragma once
+/*
+ *      Copyright (C) 2015 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "events/UniqueEvent.h"
+#include "addons/IAddon.h"
+
+class CAddonEvent : public CUniqueEvent
+{
+public:
+  CAddonEvent(ADDON::AddonPtr addon, const CVariant& description);
+  CAddonEvent(ADDON::AddonPtr addon, const CVariant& description, const CVariant& details);
+  CAddonEvent(ADDON::AddonPtr addon, const CVariant& description, const CVariant& details, const CVariant& executionLabel);
+  CAddonEvent(ADDON::AddonPtr addon, EventLevel level, const CVariant& description);
+  CAddonEvent(ADDON::AddonPtr addon, EventLevel level, const CVariant& description, const CVariant& details);
+  CAddonEvent(ADDON::AddonPtr addon, EventLevel level, const CVariant& description, const CVariant& details, const CVariant& executionLabel);
+  virtual ~CAddonEvent() { }
+
+  virtual const char* GetType() const { return "AddonEvent"; }
+
+protected:
+  ADDON::AddonPtr m_addon;
+};
---
+++ xbmc/events/NotificationEvent.h
@@ -0,0 +1,45 @@
+#pragma once
+/*
+ *      Copyright (C) 2015 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "events/UniqueEvent.h"
+
+class CNotificationEvent : public CUniqueEvent
+{
+public:
+  CNotificationEvent(const CVariant& label, const CVariant& description, EventLevel level = EventLevelInformation)
+    : CUniqueEvent(label, description, level)
+  { }
+  CNotificationEvent(const CVariant& label, const CVariant& description, const std::string& icon, EventLevel level = EventLevelInformation)
+    : CUniqueEvent(label, description, icon, level)
+  { }
+  CNotificationEvent(const CVariant& label, const CVariant& description, const std::string& icon, const CVariant& details, EventLevel level = EventLevelInformation)
+    : CUniqueEvent(label, description, icon, details, level)
+  { }
+  CNotificationEvent(const CVariant& label, const CVariant& description, const std::string& icon, const CVariant& details, const CVariant& executionLabel, EventLevel level = EventLevelInformation)
+    : CUniqueEvent(label, description, icon, details, executionLabel, level)
+  { }
+  virtual ~CNotificationEvent() { }
+
+  virtual const char* GetType() const { return "NotificationEvent"; }
+
+  virtual bool CanExecute() const { return false; }
+  virtual bool Execute() const { return true; }
+};
