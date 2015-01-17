ec171eae6aad8bbf51fdf52c97446db6418c96a1
Rafał Chwiała <rchwiala@gmail.com>
2015-01-17 15:55:52 +0100

    platinum Changes to external subtitles over UPnP works
    Added custom data to resources field - needed to add some attributes to res.
    Added new struct PLK_SecResource - needed by Somasung devices. It's not specialized struct (just general one), because I can't find any "sec" specification.

--- lib/libUPnP/Platinum/Source/Devices/MediaServer/PltMediaItem.h
+++ lib/libUPnP/Platinum/Source/Devices/MediaServer/PltMediaItem.h
@@ -172,6 +172,12 @@ typedef struct {
   NPT_String unique_identifier;
 } PLT_XbmcInfo;
 
+typedef struct {
+  NPT_String name;
+  NPT_Map<NPT_String, NPT_String> attributes;
+  NPT_String value;
+} PLT_SecResource;
+
 /*----------------------------------------------------------------------
 |   PLT_MediaItemResource
 +---------------------------------------------------------------------*/
@@ -192,6 +198,9 @@ public:
     NPT_UInt32       m_NbAudioChannels;
     NPT_String       m_Resolution;
     NPT_UInt32       m_ColorDepth;
+    /* to add custom data to resource, that are not standard one, or are only
+    proper for some type of devices (UPnP)*/
+    NPT_Map<NPT_String, NPT_String> m_CustomData;
 };
 
 /*----------------------------------------------------------------------
@@ -249,6 +258,9 @@ public:
 
     /* resources related */
     NPT_Array<PLT_MediaItemResource> m_Resources;
+
+    /* sec resources related */
+    NPT_Array<PLT_SecResource> m_SecResources;
 
     /* XBMC specific */
     PLT_XbmcInfo m_XbmcInfo;
