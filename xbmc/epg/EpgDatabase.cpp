6e0d0033554f4e25a790086390cbfe719a36fecb
Angelinas <angelinastan@gmail.com>
2015-01-24 22:59:51 +0100

    [pvr]Add some new tag in EPG fix white space
    fix white space

--- xbmc/epg/EpgDatabase.cpp
+++ xbmc/epg/EpgDatabase.cpp
@@ -225,7 +225,7 @@ int CEpgDatabase::Get(CEpg &epg)
         newTag->m_iBroadcastId       = m_pDS->fv("idBroadcast").get_asInt();
         newTag->m_strTitle           = m_pDS->fv("sTitle").get_asString().c_str();
         newTag->m_strPlotOutline     = m_pDS->fv("sPlotOutline").get_asString().c_str();
-		newTag->m_strShowID          = m_pDS->fv("sShowID").get_asString().c_str();
+	newTag->m_strShowID          = m_pDS->fv("sShowID").get_asString().c_str();
         newTag->m_strActor           = m_pDS->fv("sActor").get_asString().c_str();
         newTag->m_strDirector        = m_pDS->fv("sDirector").get_asString().c_str();
         newTag->m_strWriter          = m_pDS->fv("sWriter").get_asString().c_str();
