6e4afa4389494f14756349e862e7e6869d22abeb
ace20022 <ace20022@xbmc.org>
2015-02-03 15:19:06 +0100

    [dialogs] Move CGUIWindowVideoBase::ShowPlaySelection to its own class.

--- xbmc/Autorun.cpp
+++ xbmc/Autorun.cpp
@@ -42,7 +42,7 @@
 #include "utils/StringUtils.h"
 #include "utils/URIUtils.h"
 #include "utils/log.h"
-#include "video/windows/GUIWindowVideoBase.h"
+#include "dialogs/GUIDialogSimpleMenu.h"
 #ifdef HAS_CDDA_RIPPER
 #include "cdrip/CDDARipper.h"
 #endif
@@ -207,7 +207,7 @@ bool CAutorun::RunDisc(IDirectory* pDir, const std::string& strDrive, int& nAdde
 
           g_playlistPlayer.ClearPlaylist(PLAYLIST_VIDEO);
           g_playlistPlayer.SetShuffle (PLAYLIST_VIDEO, false);
-          if (!CGUIWindowVideoBase::ShowPlaySelection(item))
+          if (!CGUIDialogSimpleMenu::ShowPlaySelection(item))
             return false;
           g_playlistPlayer.Add(PLAYLIST_VIDEO, item);
           g_playlistPlayer.SetCurrentPlaylist(PLAYLIST_VIDEO);
