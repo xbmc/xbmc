72617fe44c1ef5f020e3093bcfbb8e121b281657
Arne Morten Kvarving <cptspiff@gmail.com>
2015-02-17 14:26:38 +0100

    added: add audio codec extensions to the music extension list

--- xbmc/settings/AdvancedSettings.cpp
+++ xbmc/settings/AdvancedSettings.cpp
@@ -39,6 +39,7 @@
 #include "filesystem/SpecialProtocol.h"
 #include "addons/IAddon.h"
 #include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
 #include "addons/GUIDialogAddonSettings.h"
 #include "CompileInfo.h"
 #if defined(TARGET_DARWIN_IOS)
@@ -1411,5 +1412,16 @@ void CAdvancedSettings::setExtraLogLevel(const std::vector<CVariant> &components
 
 std::string CAdvancedSettings::GetMusicExtensions() const
 {
-  return m_musicExtensions;
+  std::string result(m_musicExtensions);
+
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CAudioDecoder> dec(std::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    result += '|';
+    result += dec->GetExtensions();
+  }
+
+  return result;
 }
