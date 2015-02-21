e87adc525e1207ebebb60d33067a64401a6edac2
smallint <tahoma@gmx.de>
2015-02-21 07:48:45 +0000

    [imx] Ifdef'd iMX6 specific include

--- xbmc/windowing/egl/EGLNativeTypeIMX.cpp
+++ xbmc/windowing/egl/EGLNativeTypeIMX.cpp
@@ -27,7 +27,9 @@
 #include <math.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
+#ifdef HAS_IMXVPU
 #include <linux/mxcfb.h>
+#endif
 #include "utils/log.h"
 #include "utils/RegExp.h"
 #include "utils/StringUtils.h"
