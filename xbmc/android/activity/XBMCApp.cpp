koying/SPMC

ADD: [jni] add Activity and refactor to use our specific descendant

xbmc/android/activity/XBMCApp.cpp
@@ -95,7 +95,7 @@ std::vector<androidPackage> CXBMCApp::m_applications;
 
 
 CXBMCApp::CXBMCApp(ANativeActivity* nativeActivity)
-  : CJNIContext(nativeActivity)
+  : CJNIXbmcMainActivity(nativeActivity)
   , CJNIBroadcastReceiver("com.semperpax.spmc/XBMCBroadcastReceiver")
 {
   m_activity = nativeActivity;

xbmc/android/activity/XBMCApp.h
@@ -30,7 +30,7 @@
 #include "IInputHandler.h"
 
 #include "xbmc.h"
-#include "android/jni/Context.h"
+#include "android/jni/Activity.h"
 #include "android/jni/BroadcastReceiver.h"
 #include "threads/Event.h"
 
@@ -52,8 +52,7 @@ struct androidPackage
   std::string packageLabel;
 };
 
-
-class CXBMCApp : public IActivityHandler, public CJNIContext, public CJNIBroadcastReceiver
+class CXBMCApp : public IActivityHandler, public CJNIXbmcMainActivity, public CJNIBroadcastReceiver
 {
 public:
   CXBMCApp(ANativeActivity *nativeActivity);

xbmc/android/activity/android_main.cpp
@@ -88,7 +88,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
     JNINativeMethod mOnNewIntent = {
       "_onNewIntent",
       "(Landroid/content/Intent;)V",
-      (void*)&CJNIContext::_onNewIntent
+      (void*)&CJNIXbmcMainActivity::_onNewIntent
     };
     env->RegisterNatives(cMain, &mOnNewIntent, 1);
   }

xbmc/android/jni/Activity.cpp
@@ -1,0 +1,69 @@
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "Activity.h"
+#include "Intent.h"
+
+#include "jutils/jutils-details.hpp"
+
+#include <android/native_activity.h>
+
+using namespace jni;
+
+CJNIActivity::CJNIActivity(const ANativeActivity *nativeActivity) : CJNIContext(nativeActivity)
+{
+}
+
+CJNIActivity::~CJNIActivity()
+{
+}
+
+bool CJNIActivity::moveTaskToBack(bool nonRoot)
+{
+  return call_method<jboolean>(jhobject(m_context),
+    "moveTaskToBack", "(Z)Z",
+    nonRoot);
+}
+
+/////////////////////////////////////////////////
+/// \brief CJNIXbmcMain::CJNIXbmcMain
+/// \param nativeActivity
+///
+
+CJNIXbmcMainActivity* CJNIXbmcMainActivity::m_appInstance(NULL);
+
+CJNIXbmcMainActivity::CJNIXbmcMainActivity(const ANativeActivity *nativeActivity)
+  : CJNIActivity(nativeActivity)
+{
+  m_appInstance = this;
+}
+
+CJNIXbmcMainActivity::~CJNIXbmcMainActivity()
+{
+  m_appInstance = NULL;
+}
+
+void CJNIXbmcMainActivity::_onNewIntent(JNIEnv *env, jobject context, jobject intent)
+{
+  (void)env;
+  (void)context;
+  if(m_appInstance)
+    m_appInstance->onNewIntent(CJNIIntent(jhobject(intent)));
+}

xbmc/android/jni/Activity.h
@@ -1,0 +1,57 @@
+#pragma once
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "JNIBase.h"
+#include "Context.h"
+
+struct ANativeActivity;
+
+class CJNIActivity : public CJNIContext
+{
+public:
+  CJNIActivity(const ANativeActivity *nativeActivity);
+  ~CJNIActivity();
+
+  static bool moveTaskToBack(bool nonRoot);
+
+private:
+  CJNIActivity();
+};
+
+///////////////////////////////////
+
+class CJNIXbmcMainActivity : public CJNIActivity
+{
+public:
+  CJNIXbmcMainActivity(const ANativeActivity *nativeActivity);
+  ~CJNIXbmcMainActivity();
+
+  static CJNIXbmcMainActivity* GetAppInstance() { return m_appInstance; }
+
+  static void _onNewIntent(JNIEnv *env, jobject context, jobject intent);
+
+private:
+  static CJNIXbmcMainActivity *m_appInstance;
+
+protected:
+  virtual void onNewIntent(CJNIIntent intent)=0;
+};
+

xbmc/android/jni/BroadcastReceiver.cpp
@@ -21,6 +21,7 @@
 #include "BroadcastReceiver.h"
 #include "Intent.h"
 #include "Context.h"
+#include "Activity.h"
 #include "ClassLoader.h"
 #include "jutils/jutils-details.hpp"
 
@@ -29,7 +30,7 @@ using namespace jni;
 CJNIBroadcastReceiver *CJNIBroadcastReceiver::m_receiverInstance(NULL);
 CJNIBroadcastReceiver::CJNIBroadcastReceiver(const std::string &className) : CJNIBase(className)
 {
-  CJNIContext *appInstance = CJNIContext::GetAppInstance();
+  CJNIXbmcMainActivity *appInstance = CJNIXbmcMainActivity::GetAppInstance();
   if (!appInstance || className.empty())
     return;
 

xbmc/android/jni/SurfaceTexture.cpp
@@ -20,6 +20,7 @@
 
 #include "JNIBase.h"
 #include "Context.h"
+#include "Activity.h"
 #include "ClassLoader.h"
 #include "SurfaceTexture.h"
 
@@ -36,7 +37,7 @@ CJNISurfaceTextureOnFrameAvailableListener* CJNISurfaceTextureOnFrameAvailableLi
 CJNISurfaceTextureOnFrameAvailableListener::CJNISurfaceTextureOnFrameAvailableListener()
 : CJNIBase("com/semperpax/spmc/XBMCOnFrameAvailableListener")
 {
-  CJNIContext *appInstance = CJNIContext::GetAppInstance();
+  CJNIXbmcMainActivity *appInstance = CJNIXbmcMainActivity::GetAppInstance();
   if (!appInstance)
     return;
 

xbmc/xbmc

xbmc/android/activity/XBMCApp.cpp
@@ -94,7 +94,7 @@ std::vector<androidPackage> CXBMCApp::m_applications;
 
 
 CXBMCApp::CXBMCApp(ANativeActivity* nativeActivity)
-  : CJNIContext(nativeActivity)
+  : CJNIApplicationMainActivity(nativeActivity)
   , CJNIBroadcastReceiver("org/xbmc/kodi/XBMCBroadcastReceiver")
   , m_wakeLock(NULL)
 {

xbmc/android/activity/XBMCApp.h
@@ -30,7 +30,7 @@
 #include "IInputHandler.h"
 
 #include "xbmc.h"
-#include "android/jni/Context.h"
+#include "android/jni/Activity.h"
 #include "android/jni/BroadcastReceiver.h"
 #include "threads/Event.h"
 
@@ -52,8 +52,7 @@ struct androidPackage
   std::string packageLabel;
 };
 
-
-class CXBMCApp : public IActivityHandler, public CJNIContext, public CJNIBroadcastReceiver
+class CXBMCApp : public IActivityHandler, public CJNIApplicationMainActivity, public CJNIBroadcastReceiver
 {
 public:
   CXBMCApp(ANativeActivity *nativeActivity);

xbmc/android/activity/android_main.cpp
@@ -96,7 +96,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
     JNINativeMethod mOnNewIntent = {
       "_onNewIntent",
       "(Landroid/content/Intent;)V",
-      (void*)&CJNIContext::_onNewIntent
+      (void*)&CJNIApplicationMainActivity::_onNewIntent
     };
     env->RegisterNatives(cMain, &mOnNewIntent, 1);
   }

xbmc/android/jni/Activity.cpp
@@ -1,0 +1,69 @@
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "Activity.h"
+#include "Intent.h"
+
+#include "jutils/jutils-details.hpp"
+
+#include <android/native_activity.h>
+
+using namespace jni;
+
+CJNIActivity::CJNIActivity(const ANativeActivity *nativeActivity) : CJNIContext(nativeActivity)
+{
+}
+
+CJNIActivity::~CJNIActivity()
+{
+}
+
+bool CJNIActivity::moveTaskToBack(bool nonRoot)
+{
+  return call_method<jboolean>(jhobject(m_context),
+    "moveTaskToBack", "(Z)Z",
+    nonRoot);
+}
+
+/////////////////////////////////////////////////
+/// \brief CJNIApplicationMainActivity::CJNIApplicationMainActivity
+/// \param nativeActivity
+///
+
+CJNIApplicationMainActivity* CJNIApplicationMainActivity::m_appInstance(NULL);
+
+CJNIApplicationMainActivity::CJNIApplicationMainActivity(const ANativeActivity *nativeActivity)
+  : CJNIActivity(nativeActivity)
+{
+  m_appInstance = this;
+}
+
+CJNIApplicationMainActivity::~CJNIApplicationMainActivity()
+{
+  m_appInstance = NULL;
+}
+
+void CJNIApplicationMainActivity::_onNewIntent(JNIEnv *env, jobject context, jobject intent)
+{
+  (void)env;
+  (void)context;
+  if(m_appInstance)
+    m_appInstance->onNewIntent(CJNIIntent(jhobject(intent)));
+}

xbmc/android/jni/Activity.h
@@ -1,0 +1,57 @@
+#pragma once
+/*
+ *      Copyright (C) 2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "JNIBase.h"
+#include "Context.h"
+
+struct ANativeActivity;
+
+class CJNIActivity : public CJNIContext
+{
+public:
+  CJNIActivity(const ANativeActivity *nativeActivity);
+  ~CJNIActivity();
+
+  static bool moveTaskToBack(bool nonRoot);
+
+private:
+  CJNIActivity();
+};
+
+///////////////////////////////////
+
+class CJNIApplicationMainActivity : public CJNIActivity
+{
+public:
+  CJNIApplicationMainActivity(const ANativeActivity *nativeActivity);
+  ~CJNIApplicationMainActivity();
+
+  static CJNIApplicationMainActivity* GetAppInstance() { return m_appInstance; }
+
+  static void _onNewIntent(JNIEnv *env, jobject context, jobject intent);
+
+private:
+  static CJNIApplicationMainActivity *m_appInstance;
+
+protected:
+  virtual void onNewIntent(CJNIIntent intent)=0;
+};
+

xbmc/android/jni/BroadcastReceiver.cpp
@@ -21,6 +21,7 @@
 #include "BroadcastReceiver.h"
 #include "Intent.h"
 #include "Context.h"
+#include "Activity.h"
 #include "ClassLoader.h"
 #include "jutils/jutils-details.hpp"
 
@@ -29,7 +30,7 @@ using namespace jni;
 CJNIBroadcastReceiver *CJNIBroadcastReceiver::m_receiverInstance(NULL);
 CJNIBroadcastReceiver::CJNIBroadcastReceiver(const std::string &className) : CJNIBase(className)
 {
-  CJNIContext *appInstance = CJNIContext::GetAppInstance();
+  CJNIApplicationMainActivity *appInstance = CJNIApplicationMainActivity::GetAppInstance();
   if (!appInstance || className.empty())
     return;
 

xbmc/android/jni/SurfaceTexture.cpp
@@ -20,6 +20,7 @@
 
 #include "JNIBase.h"
 #include "Context.h"
+#include "Activity.h"
 #include "ClassLoader.h"
 #include "SurfaceTexture.h"
 
@@ -36,7 +37,7 @@ CJNISurfaceTextureOnFrameAvailableListener* CJNISurfaceTextureOnFrameAvailableLi
 CJNISurfaceTextureOnFrameAvailableListener::CJNISurfaceTextureOnFrameAvailableListener()
 : CJNIBase("org/xbmc/kodi/XBMCOnFrameAvailableListener")
 {
-  CJNIContext *appInstance = CJNIContext::GetAppInstance();
+  CJNIApplicationMainActivity *appInstance = CJNIApplicationMainActivity::GetAppInstance();
   if (!appInstance)
     return;
 