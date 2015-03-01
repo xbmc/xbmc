notspiff/kodi-cmake

use shared pointer of ILanguageInvoker

xbmc/interfaces/generic/ILanguageInvoker.h
@@ -21,11 +21,12 @@
 
 #include <string>
 #include <vector>
+#include <boost/shared_ptr.hpp>
 
-#include "ILanguageInvocationHandler.h"
 #include "addons/IAddon.h"
 
 class CLanguageInvokerThread;
+class ILanguageInvocationHandler;
 
 typedef enum {
   InvokerStateUninitialized,
@@ -39,28 +40,19 @@ typedef enum {
 class ILanguageInvoker
 {
 public:
-  ILanguageInvoker(ILanguageInvocationHandler *invocationHandler)
-    : m_id(-1), m_state(InvokerStateUninitialized),
-      m_invocationHandler(invocationHandler)
-  { }
-  virtual ~ILanguageInvoker() { }
+  ILanguageInvoker(ILanguageInvocationHandler *invocationHandler);
+  virtual ~ILanguageInvoker();
 
-  virtual bool Execute(const std::string &script, const std::vector<std::string> &arguments = std::vector<std::string>())
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptStarted(this);
-
-    return execute(script, arguments);
-  }
-  virtual bool Stop(bool abort = false) { return stop(abort); }
+  virtual bool Execute(const std::string &script, const std::vector<std::string> &arguments = std::vector<std::string>());
+  virtual bool Stop(bool abort = false);
+  virtual bool IsStopping() const;
 
   void SetId(int id) { m_id = id; }
   int GetId() const { return m_id; }
   void SetAddon(const ADDON::AddonPtr &addon) { m_addon = addon; }
   InvokerState GetState() const { return m_state; }
-  bool IsActive() const { return GetState() > InvokerStateUninitialized && GetState() < InvokerStateDone; }
-  bool IsRunning() const { return GetState() == InvokerStateRunning; }
-  virtual bool IsStopping() const { return GetState() == InvokerStateStopping; }
+  bool IsActive() const;
+  bool IsRunning() const;
 
 protected:
   friend class CLanguageInvokerThread;
@@ -68,24 +60,10 @@ protected:
   virtual bool execute(const std::string &script, const std::vector<std::string> &arguments) = 0;
   virtual bool stop(bool abort) = 0;
 
-  virtual void onExecutionFailed()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptEnded(this);
-  }
-  virtual void onExecutionDone()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptEnded(this);
-  }
+  virtual void onExecutionFailed();
+  virtual void onExecutionDone();
 
-  void setState(InvokerState state)
-  {
-    if (state <= m_state)
-      return;
-
-    m_state = state;
-  }
+  void setState(InvokerState state);
 
   ADDON::AddonPtr m_addon;
 
@@ -94,3 +72,5 @@ private:
   InvokerState m_state;
   ILanguageInvocationHandler *m_invocationHandler;
 };
+
+typedef boost::shared_ptr<ILanguageInvoker> LanguageInvokerPtr;

xbmc/xbmc

xbmc/interfaces/generic/ILanguageInvoker.h
@@ -19,13 +19,14 @@
  *
  */
 
+#include <memory>
 #include <string>
 #include <vector>
 
-#include "ILanguageInvocationHandler.h"
 #include "addons/IAddon.h"
 
 class CLanguageInvokerThread;
+class ILanguageInvocationHandler;
 
 typedef enum {
   InvokerStateUninitialized,
@@ -39,29 +40,20 @@ typedef enum {
 class ILanguageInvoker
 {
 public:
-  ILanguageInvoker(ILanguageInvocationHandler *invocationHandler)
-    : m_id(-1), m_state(InvokerStateUninitialized),
-      m_invocationHandler(invocationHandler)
-  { }
-  virtual ~ILanguageInvoker() { }
+  ILanguageInvoker(ILanguageInvocationHandler *invocationHandler);
+  virtual ~ILanguageInvoker();
 
-  virtual bool Execute(const std::string &script, const std::vector<std::string> &arguments = std::vector<std::string>())
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptStarted(this);
-
-    return execute(script, arguments);
-  }
-  virtual bool Stop(bool abort = false) { return stop(abort); }
+  virtual bool Execute(const std::string &script, const std::vector<std::string> &arguments = std::vector<std::string>());
+  virtual bool Stop(bool abort = false);
+  virtual bool IsStopping() const;
 
   void SetId(int id) { m_id = id; }
   int GetId() const { return m_id; }
   const ADDON::AddonPtr& GetAddon() const { return m_addon; }
   void SetAddon(const ADDON::AddonPtr &addon) { m_addon = addon; }
   InvokerState GetState() const { return m_state; }
-  bool IsActive() const { return GetState() > InvokerStateUninitialized && GetState() < InvokerStateDone; }
-  bool IsRunning() const { return GetState() == InvokerStateRunning; }
-  virtual bool IsStopping() const { return GetState() == InvokerStateStopping; }
+  bool IsActive() const;
+  bool IsRunning() const;
 
 protected:
   friend class CLanguageInvokerThread;
@@ -69,51 +61,14 @@ protected:
   virtual bool execute(const std::string &script, const std::vector<std::string> &arguments) = 0;
   virtual bool stop(bool abort) = 0;
 
-  virtual void pulseGlobalEvent()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->PulseGlobalEvent();
-  }
+  virtual void pulseGlobalEvent();
+  virtual bool onExecutionInitialized();
+  virtual void onAbortRequested();
+  virtual void onExecutionFailed();
+  virtual void onExecutionDone();
+  virtual void onExecutionFinalized();
 
-  virtual bool onExecutionInitialized()
-  {
-    if (m_invocationHandler == NULL)
-      return false;
-
-    return m_invocationHandler->OnScriptInitialized(this);
-  }
-
-  virtual void onAbortRequested()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptAbortRequested(this);
-  }
-
-  virtual void onExecutionFailed()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptEnded(this);
-  }
-
-  virtual void onExecutionDone()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptEnded(this);
-  }
-
-  virtual void onExecutionFinalized()
-  {
-    if (m_invocationHandler)
-      m_invocationHandler->OnScriptFinalized(this);
-  }
-
-  void setState(InvokerState state)
-  {
-    if (state <= m_state)
-      return;
-
-    m_state = state;
-  }
+  void setState(InvokerState state);
 
   ADDON::AddonPtr m_addon;
 
@@ -122,3 +77,5 @@ private:
   InvokerState m_state;
   ILanguageInvocationHandler *m_invocationHandler;
 };
+
+typedef std::shared_ptr<ILanguageInvoker> LanguageInvokerPtr;