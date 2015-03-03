topfs2/xbmc-cmake

add support for audio decoder addons

xbmc/addons/Addon.cpp
@@ -87,6 +87,7 @@ static const TypeMapping types[] =
    {"xbmc.addon.audio",                  ADDON_AUDIO,                1038, "DefaultAddonMusic.png" },
    {"xbmc.addon.image",                  ADDON_IMAGE,                1039, "DefaultAddonPicture.png" },
    {"xbmc.addon.executable",             ADDON_EXECUTABLE,           1043, "DefaultAddonProgram.png" },
+   {"xbmc.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" }};
 
 const CStdString TranslateType(const ADDON::TYPE &type, bool pretty/*=false*/)
@@ -390,6 +391,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_PLUGIN:
       case ADDON_SERVICE:
       case ADDON_REPOSITORY:
+      case ADDON_AUDIODECODER:
         {
           CStdString temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
           m_strLibName = temp;

xbmc/addons/AudioDecoder.h
@@ -1,0 +1,76 @@
+/*
+ *      Copyright (C) 2013 Arne Morten Kvarving
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include "AddonDll.h"
+#include "include/xbmc_audiodec_types.h"
+#include "cores/paplayer/ICodec.h"
+#include "music/tags/ImusicInfoTagLoader.h"
+#include "filesystem/MusicFileDirectory.h"
+
+namespace MUSIC_INFO
+{
+  class CMusicInfoTag;
+  class EmbeddedArt;
+}
+
+typedef DllAddon<AudioDecoder, AUDIODEC_PROPS> DllAudioDecoder;
+namespace ADDON
+{
+  typedef CAddonDll<DllAudioDecoder,
+                    AudioDecoder, AUDIODEC_PROPS> AudioDecoderDll;
+
+  class CAudioDecoder : public AudioDecoderDll,
+                        public ICodec,
+                        public MUSIC_INFO::IMusicInfoTagLoader,
+                        public XFILE::CMusicFileDirectory
+  {
+  public:
+    CAudioDecoder(const AddonProps &props) : AudioDecoderDll(props) {};
+    CAudioDecoder(const cp_extension_t *ext);
+    virtual ~CAudioDecoder() {}
+    virtual AddonPtr Clone() const;
+
+    // Things that MUST be supplied by the child classes
+    bool Init(const CStdString& strFile, unsigned int filecache);
+    int ReadPCM(uint8_t* buffer, int size, int* actualsize);
+    int64_t Seek(int64_t time);
+    bool CanInit() { return true; }
+    void DeInit();
+    void Destroy();
+    bool Load(const CStdString& strFileName,
+              MUSIC_INFO::CMusicInfoTag& tag,
+              MUSIC_INFO::EmbeddedArt *art = NULL);
+    int GetTrackCount(const CStdString& strPath);
+    virtual CAEChannelInfo GetChannelInfo();
+
+    const std::string& GetExtensions() const { return m_extension; }
+    const std::string& GetMimetypes() const { return m_mimetype; }
+    bool HasTags() const { return m_tags; }
+    bool HasTracks() const { return m_tracks; }
+  protected:
+    std::string m_extension;
+    std::string m_mimetype;
+    void* m_context;
+    bool m_tags;
+    bool m_tracks;
+    const AEChannel* m_channel;
+  };
+
+} /*namespace ADDON*/

xbmc/cores/paplayer/CodecFactory.cpp
@@ -37,9 +37,25 @@
 #include "DVDPlayerCodec.h"
 #include "PCMCodec.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 
 ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
 {
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->GetExtensions().find("."+strFileType) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
   if (strFileType.Equals("mp3") || strFileType.Equals("mp2"))
     return new DVDPlayerCodec();
   else if (strFileType.Equals("pcm") || strFileType.Equals("l16"))
@@ -114,6 +130,22 @@ ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
 ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdString& strContent, unsigned int filecache)
 {
   CURL urlFile(strFile);
+
+  if (!strContent.empty())
+  {
+    VECADDONS codecs;
+    CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+    for (size_t i=0;i<codecs.size();++i)
+    {
+      boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+      if (dec->GetMimetypes().find(strContent) != std::string::npos)
+      {
+        CAudioDecoder* result = new CAudioDecoder(*dec);
+        static_cast<AudioDecoderDll&>(*result).Create();
+        return result;
+      }
+    }
+  }
   if( strContent.Equals("audio/mpeg")
   ||  strContent.Equals("audio/mpeg3")
   ||  strContent.Equals("audio/mp3") )

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -49,7 +49,10 @@
 #include "settings/AdvancedSettings.h"
 #include "FileItem.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
 
+using namespace ADDON;
 using namespace XFILE;
 using namespace PLAYLIST;
 using namespace std;
@@ -81,6 +84,23 @@ IFileDirectory* CFileDirectoryFactory::Create(const CStdString& strPath, CFileIt
 
     delete pDir;
     return NULL;
+  }
+
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (!strExtension.empty() && dec->HasTracks() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      if (result->ContainsFiles(strPath))
+        return result;
+      delete result;
+      return NULL;
+    }
   }
   if (strExtension.Equals(".nsf") && CFile::Exists(strPath))
   {

xbmc/music/tags/MusicInfoTagLoaderFactory.cpp
@@ -40,6 +40,10 @@
 #include "cores/paplayer/ASAPCodec.h"
 #endif
 
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 using namespace MUSIC_INFO;
 
 CMusicInfoTagLoaderFactory::CMusicInfoTagLoaderFactory()
@@ -65,6 +69,20 @@ IMusicInfoTagLoader* CMusicInfoTagLoaderFactory::CreateLoader(const CStdString& 
   if (strExtension.empty())
     return NULL;
 
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    boost::shared_ptr<CAudioDecoder> dec(boost::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->HasTags() && dec->GetExtensions().find("."+strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
+
+
   if (strExtension == "aac" ||
       strExtension == "ape" || strExtension == "mac" ||
       strExtension == "mp3" || 
@@ -75,11 +93,8 @@ IMusicInfoTagLoader* CMusicInfoTagLoaderFactory::CreateLoader(const CStdString& 
       strExtension == "ogg" || strExtension == "oga" || strExtension == "oggstream" ||
       strExtension == "aif" || strExtension == "aiff" ||
       strExtension == "wav" ||
-#ifdef HAS_MOD_PLAYER
-      ModPlayer::IsSupportedFormat(strExtension) ||
       strExtension == "mod" || strExtension == "nsf" || strExtension == "nsfstream" ||
       strExtension == "s3m" || strExtension == "it" || strExtension == "xm" ||
-#endif
       strExtension == "wv")
   {
     CTagLoaderTagLib *pTagLoader = new CTagLoaderTagLib();

xbmc/xbmc

xbmc/addons/Addon.cpp
@@ -90,6 +90,7 @@ static const TypeMapping types[] =
    {"xbmc.addon.image",                  ADDON_IMAGE,                1039, "DefaultAddonPicture.png" },
    {"xbmc.addon.executable",             ADDON_EXECUTABLE,           1043, "DefaultAddonProgram.png" },
    {"xbmc.audioencoder",                 ADDON_AUDIOENCODER,         200,  "DefaultAddonAudioEncoder.png" },
+   {"kodi.audiodecoder",                 ADDON_AUDIODECODER,         201,  "DefaultAddonAudioDecoder.png" },
    {"xbmc.service",                      ADDON_SERVICE,             24018, "DefaultAddonService.png" }};
 
 const std::string TranslateType(const ADDON::TYPE &type, bool pretty/*=false*/)
@@ -397,6 +398,7 @@ void CAddon::BuildLibName(const cp_extension_t *extension)
       case ADDON_REPOSITORY:
       case ADDON_AUDIOENCODER:
       case ADDON_CONTEXT_ITEM:
+      case ADDON_AUDIODECODER:
         {
           std::string temp = CAddonMgr::Get().GetExtValue(extension->configuration, "@library");
           m_strLibName = temp;

xbmc/addons/AudioDecoder.h
@@ -1,0 +1,76 @@
+/*
+ *      Copyright (C) 2013 Arne Morten Kvarving
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+#include "AddonDll.h"
+#include "include/kodi_audiodec_types.h"
+#include "cores/paplayer/ICodec.h"
+#include "music/tags/ImusicInfoTagLoader.h"
+#include "filesystem/MusicFileDirectory.h"
+
+namespace MUSIC_INFO
+{
+  class CMusicInfoTag;
+  class EmbeddedArt;
+}
+
+typedef DllAddon<AudioDecoder, AUDIODEC_PROPS> DllAudioDecoder;
+namespace ADDON
+{
+  typedef CAddonDll<DllAudioDecoder,
+                    AudioDecoder, AUDIODEC_PROPS> AudioDecoderDll;
+
+  class CAudioDecoder : public AudioDecoderDll,
+                        public ICodec,
+                        public MUSIC_INFO::IMusicInfoTagLoader,
+                        public XFILE::CMusicFileDirectory
+  {
+  public:
+    CAudioDecoder(const AddonProps &props) : AudioDecoderDll(props) {};
+    CAudioDecoder(const cp_extension_t *ext);
+    virtual ~CAudioDecoder() {}
+    virtual AddonPtr Clone() const;
+
+    // Things that MUST be supplied by the child classes
+    bool Init(const std::string& strFile, unsigned int filecache);
+    int ReadPCM(uint8_t* buffer, int size, int* actualsize);
+    int64_t Seek(int64_t time);
+    bool CanInit() { return true; }
+    void DeInit();
+    void Destroy();
+    bool Load(const std::string& strFileName,
+              MUSIC_INFO::CMusicInfoTag& tag,
+              MUSIC_INFO::EmbeddedArt *art = NULL);
+    int GetTrackCount(const std::string& strPath);
+    virtual CAEChannelInfo GetChannelInfo();
+
+    const std::string& GetExtensions() const { return m_extension; }
+    const std::string& GetMimetypes() const { return m_mimetype; }
+    bool HasTags() const { return m_tags; }
+    bool HasTracks() const { return m_tracks; }
+  protected:
+    std::string m_extension;
+    std::string m_mimetype;
+    void* m_context;
+    bool m_tags;
+    bool m_tracks;
+    const AEChannel* m_channel;
+  };
+
+} /*namespace ADDON*/

xbmc/cores/paplayer/CodecFactory.cpp
@@ -36,11 +36,27 @@
 #include "URL.h"
 #include "DVDPlayerCodec.h"
 #include "utils/StringUtils.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 
 ICodec* CodecFactory::CreateCodec(const std::string& strFileType)
 {
   std::string fileType = strFileType;
   StringUtils::ToLower(fileType);
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CAudioDecoder> dec(std::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->GetExtensions().find("."+fileType) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
   if (fileType == "mp3" || fileType == "mp2")
     return new DVDPlayerCodec();
   else if (fileType == "pcm" || fileType == "l16")
@@ -121,9 +137,24 @@ ICodec* CodecFactory::CreateCodecDemux(const std::string& strFile, const std::st
   CURL urlFile(strFile);
   std::string content = strContent;
   StringUtils::ToLower(content);
-  if( content == "audio/mpeg"
-  ||  content == "audio/mpeg3"
-  ||  content == "audio/mp3" )
+  if (!content.empty())
+  {
+    VECADDONS codecs;
+    CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+    for (size_t i=0;i<codecs.size();++i)
+    {
+      std::shared_ptr<CAudioDecoder> dec(std::static_pointer_cast<CAudioDecoder>(codecs[i]));
+      if (dec->GetMimetypes().find(content) != std::string::npos)
+      {
+        CAudioDecoder* result = new CAudioDecoder(*dec);
+        static_cast<AudioDecoderDll&>(*result).Create();
+        return result;
+      }
+    }
+  }
+  if (content == "audio/mpeg"
+  || content == "audio/mpeg3"
+  || content == "audio/mp3")
   {
     DVDPlayerCodec *dvdcodec = new DVDPlayerCodec();
     dvdcodec->SetContentType(content);

xbmc/filesystem/FileDirectoryFactory.cpp
@@ -50,7 +50,10 @@
 #include "FileItem.h"
 #include "utils/StringUtils.h"
 #include "URL.h"
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
 
+using namespace ADDON;
 using namespace XFILE;
 using namespace PLAYLIST;
 using namespace std;
@@ -66,6 +69,25 @@ IFileDirectory* CFileDirectoryFactory::Create(const CURL& url, CFileItem* pItem,
 {
   if (url.IsProtocol("stack")) // disqualify stack as we need to work with each of the parts instead
     return NULL;
+
+  std::string strExtension=URIUtils::GetExtension(url);
+  StringUtils::ToLower(strExtension);
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CAudioDecoder> dec(std::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (!strExtension.empty() && dec->HasTracks() &&
+        dec->GetExtensions().find(strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      if (result->ContainsFiles(url))
+        return result;
+      delete result;
+      return NULL;
+    }
+  }
 
 #ifdef HAS_FILESYSTEM
   if ((url.IsFileType("ogg") || url.IsFileType("oga")) && CFile::Exists(url))

xbmc/music/tags/MusicInfoTagLoaderFactory.cpp
@@ -40,6 +40,10 @@
 #include "cores/paplayer/ASAPCodec.h"
 #endif
 
+#include "addons/AddonManager.h"
+#include "addons/AudioDecoder.h"
+
+using namespace ADDON;
 using namespace MUSIC_INFO;
 
 CMusicInfoTagLoaderFactory::CMusicInfoTagLoaderFactory()
@@ -65,6 +69,20 @@ IMusicInfoTagLoader* CMusicInfoTagLoaderFactory::CreateLoader(const std::string&
   if (strExtension.empty())
     return NULL;
 
+  VECADDONS codecs;
+  CAddonMgr::Get().GetAddons(ADDON_AUDIODECODER, codecs);
+  for (size_t i=0;i<codecs.size();++i)
+  {
+    std::shared_ptr<CAudioDecoder> dec(std::static_pointer_cast<CAudioDecoder>(codecs[i]));
+    if (dec->HasTags() && dec->GetExtensions().find("."+strExtension) != std::string::npos)
+    {
+      CAudioDecoder* result = new CAudioDecoder(*dec);
+      static_cast<AudioDecoderDll&>(*result).Create();
+      return result;
+    }
+  }
+
+
   if (strExtension == "aac" ||
       strExtension == "ape" || strExtension == "mac" ||
       strExtension == "mp3" || 
@@ -75,11 +93,8 @@ IMusicInfoTagLoader* CMusicInfoTagLoaderFactory::CreateLoader(const std::string&
       strExtension == "ogg" || strExtension == "oga" || strExtension == "oggstream" ||
       strExtension == "aif" || strExtension == "aiff" ||
       strExtension == "wav" ||
-#ifdef HAS_MOD_PLAYER
-      ModPlayer::IsSupportedFormat(strExtension) ||
       strExtension == "mod" || strExtension == "nsf" || strExtension == "nsfstream" ||
       strExtension == "s3m" || strExtension == "it" || strExtension == "xm" ||
-#endif
       strExtension == "wv")
   {
     CTagLoaderTagLib *pTagLoader = new CTagLoaderTagLib();