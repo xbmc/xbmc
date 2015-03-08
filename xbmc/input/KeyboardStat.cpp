1e743bc21cae57d2ebe34bb4899832d7a74f24ef
montellese <montellese@xbmc.org>
2015-03-08 11:08:30 +0100

    input: remove unused CKeyboardLayoutConfiguration

--- xbmc/input/KeyboardStat.cpp
+++ xbmc/input/KeyboardStat.cpp
@@ -24,13 +24,13 @@
 //#define DEBUG_KEYBOARD_GETCHAR
 
 #include "KeyboardStat.h"
-#include "KeyboardLayoutConfiguration.h"
 #include "windowing/XBMC_events.h"
 #include "utils/TimeUtils.h"
 #include "input/XBMC_keytable.h"
 #include "input/XBMC_vkeys.h"
 #include "peripherals/Peripherals.h"
 #include "peripherals/devices/PeripheralHID.h"
+#include "utils/log.h"
 
 using namespace std;
 using namespace PERIPHERALS;
