fc80bb681e65013023db0e2d6f37395cc0bfd674
Rainer Hochecker <fernetmenta@online.de>
2015-03-10 20:40:33 +0100

    renderer: grab corect lock when calling IsGui/VideoLayer

--- xbmc/cores/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoRenderers/RenderManager.cpp
@@ -816,7 +816,7 @@ void CXBMCRenderManager::Render(bool clear, DWORD flags, DWORD alpha, bool gui)
 
 bool CXBMCRenderManager::IsGuiLayer()
 {
-  { CSingleLock lock(m_presentlock);
+  { CSharedLock lock(m_sharedSection);
 
     if (!m_pRenderer)
       return false;
@@ -829,7 +829,7 @@ bool CXBMCRenderManager::IsGuiLayer()
 
 bool CXBMCRenderManager::IsVideoLayer()
 {
-  { CSingleLock lock(m_presentlock);
+  { CSharedLock lock(m_sharedSection);
 
     if (!m_pRenderer)
       return false;
