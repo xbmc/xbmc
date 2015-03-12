0e4e1d7ab3e4fd1d45f63c4ec01c14a34db79f46
montellese <montellese@xbmc.org>
2015-03-13 00:45:47 +0100

    lib: get rid of copy of libmicrohttpd

--- lib/libmicrohttpd/src/daemon/https/minitasn1/gstr.h
+++
@@ -1,5 +0,0 @@
-void MHD__asn1_str_cpy (char *dest, size_t dest_tot_size, const char *src);
-void MHD__asn1_str_cat (char *dest, size_t dest_tot_size, const char *src);
-
-#define Estrcpy(x,y) MHD__asn1_str_cpy(x,MAX_ERROR_DESCRIPTION_SIZE,y)
-#define Estrcat(x,y) MHD__asn1_str_cat(x,MAX_ERROR_DESCRIPTION_SIZE,y)
