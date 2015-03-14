42d0a79764ca36b7d709129cc0c0ce8f5e90d402
popcornmix <popcornmix@gmail.com>
2015-03-14 12:38:08 +0000

    [rbp] Switch to using transform flags for 3d modes

--- xbmc/cores/omxplayer/OMXVideo.h
+++ xbmc/cores/omxplayer/OMXVideo.h
@@ -60,7 +60,7 @@ public:
   void Reset(void);
   void SetDropState(bool bDrop);
   std::string GetDecoderName() { return m_video_codec_name; };
-  void SetVideoRect(const CRect& SrcRect, const CRect& DestRect, RENDER_STEREO_MODE video_mode, RENDER_STEREO_MODE display_mode);
+  void SetVideoRect(const CRect& SrcRect, const CRect& DestRect, RENDER_STEREO_MODE video_mode, RENDER_STEREO_MODE display_mode, bool stereo_invert);
   int GetInputBufferSize();
   bool GetPlayerInfo(double &match, double &phase, double &pll);
   void SubmitEOS();
