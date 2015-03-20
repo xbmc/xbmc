c03ecbeff265d422cc503806023cc1b28050e7bb
Matthias Kortstiege <mkortstiege@kodi.tv>
2015-03-20 12:20:53 +0100

    [guilib] hint text not displayed in keyboard dialog

--- xbmc/guilib/GUIEditControl.cpp
+++ xbmc/guilib/GUIEditControl.cpp
@@ -486,13 +486,14 @@ void CGUIEditControl::ProcessText(unsigned int currentTime)
     changed |= m_label2.SetMaxRect(m_clipRect.x1 + m_textOffset, m_posY, m_clipRect.Width() - m_textOffset, m_height);
 
     std::wstring text = GetDisplayedText();
-    // add the cursor and highlighting if we're focused
-    if ((HasFocus() || m_cursorShowAlways) && m_inputType != INPUT_TYPE_READONLY)
-      changed |= SetStyledText(text);
+    std::string hint = m_hintInfo.GetLabel(GetParentID());
+
+    if (!HasFocus() && text.empty() && !hint.empty())
+      changed |= m_label2.SetText(hint);
     else
     {
-      if (text.empty())
-        changed |= m_label2.SetText(m_hintInfo.GetLabel(GetParentID()));
+      if (m_inputType != INPUT_TYPE_READONLY)
+        changed |= SetStyledText(text);
       else
         changed |= m_label2.SetTextW(text);
     }
@@ -582,10 +583,13 @@ bool CGUIEditControl::SetStyledText(const std::wstring &text)
   }
 
   // show the cursor
-  unsigned int ch = L'|';
-  if ((++m_cursorBlink % 64) > 32)
-    ch |= (3 << 16);
-  styled.insert(styled.begin() + m_cursorPos, ch);
+  if (HasFocus() || GetParentID() == WINDOW_DIALOG_KEYBOARD)
+  {
+    unsigned int ch = L'|';
+    if ((++m_cursorBlink % 64) > 32)
+      ch |= (3 << 16);
+    styled.insert(styled.begin() + m_cursorPos, ch);
+  }
 
   return m_label2.SetStyledText(styled, colors);
 }
