fritsch/xbmc-cmake

added: support audio books in music info scanner

xbmc/filesystem/MusicDatabaseDirectory/DirectoryNodeOverview.cpp
@@ -87,7 +87,7 @@ bool CDirectoryNodeOverview::GetContent(CFileItemList& items) const
       if (musicDatabase.GetSongsCount() == 0)
         continue;
     }
-    if ((i == 3 && !showSingles) // singles
+    if (i == 3 && !showSingles) // singles
       continue;
     else if (i == 9 && musicDatabase.GetCompilationAlbumsCount() == 0) // compilations
       continue;

xbmc/xbmc

xbmc/filesystem/MusicDatabaseDirectory/DirectoryNodeOverview.cpp
@@ -79,7 +79,7 @@ bool CDirectoryNodeOverview::GetContent(CFileItemList& items) const
 
   for (unsigned int i = 0; i < sizeof(OverviewChildren) / sizeof(Node); ++i)
   {
-    if ((i == 3 && !showSingles) // singles
+    if (i == 3 && !hasSingles) // singles
       continue;
     else if (i == 9 && !hasCompilations) // compilations
       continue;