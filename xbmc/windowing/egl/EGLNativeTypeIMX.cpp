bdc1d8b05ff7b5c9aa62f5e11e3405ad27afc3a0
Sam Nazarko <email@samnazarko.co.uk>
2015-04-11 18:22:51 +0100

    i.MX: allow other modes to be set as resolution.
    * This has had no ill effect so far
    
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/windowing/egl/EGLNativeTypeIMX.cpp
+++ xbmc/windowing/egl/EGLNativeTypeIMX.cpp
@@ -280,8 +280,6 @@ bool CEGLNativeTypeIMX::ProbeResolutions(std::vector<RESOLUTION_INFO> &resolutio
   RESOLUTION_INFO res;
   for (size_t i = 0; i < probe_str.size(); i++)
   {
-    if(!StringUtils::StartsWith(probe_str[i], "S:"))
-      continue;
     if(ModeToResolution(probe_str[i], &res))
       resolutions.push_back(res);
   }
