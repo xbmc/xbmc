815e388a73b8e5ba121228d359eeb0bba1fee7f8
Chris "Koying" Browet <cbro@semperpax.com>
2015-04-19 19:24:46 +0200

    FIX: passthrough volume warning no longer showed

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2427,11 +2427,9 @@ bool CApplication::OnAction(const CAction &action)
       else
         volume = action.GetAmount() * step;
       if (volume != m_volumeLevel)
-      {
         SetVolume(volume, false);
-        // show visual feedback of volume change...
-        ShowVolumeBar(&action);
-      }
+      // show visual feedback of volume...
+      ShowVolumeBar(&action);
     }
     return true;
   }
