b4904e9cb0c8967d9a08c44119523dea3dbbfd14
Anton Fedchin <afedchin@ruswizards.com>
2015-04-22 19:08:44 +0300

    [installer] Got rid of DX9 from installation package.

--- project/Win32BuildSetup/getdeploydependencies.bat
+++ project/Win32BuildSetup/getdeploydependencies.bat
@@ -24,12 +24,5 @@ if not exist dependencies\vcredist\2013\vcredist_x86.exe (
   ..\BuildDependencies\bin\wget -nv -O dependencies\vcredist\2013\vcredist_x86.exe %KODI_MIRROR%/build-deps/win32/vcredist/2013/vcredist_x86.exe
 )
 
-if not exist dependencies\dxsetup mkdir dependencies\dxsetup
-for %%f in (DSETUP.dll dsetup32.dll dxdllreg_x86.cab DXSETUP.exe dxupdate.cab Jun2010_D3DCompiler_43_x86.cab Jun2010_d3dx9_43_x86.cab) do (
-  if not exist dependencies\dxsetup\%%f (
-    ..\BuildDependencies\bin\wget -nv -O dependencies\dxsetup\%%f %KODI_MIRROR%/build-deps/win32/dxsetup/%%f
-  )
-)
-
 :: Restore the previous current directory
 POPD
