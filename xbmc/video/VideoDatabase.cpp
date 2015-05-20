xbmc/xbmc

commit 9f432dc62b8a0aae1972e8a1190c5bd870c2e6e1

[videodatabase] include tvshows base dir in GetPathsForTvShow

xbmc/video/VideoDatabase.cpp
@@ -636,6 +636,13 @@ bool CVideoDatabase::GetPathsForTvShow(int idShow, set<int>& paths)
   {
     if (NULL == m_pDB.get()) return false;
     if (NULL == m_pDS.get()) return false;
+
+    // add base path
+    strSQL = PrepareSQL("SELECT strPath FROM tvshow_view WHERE idShow=%i", idShow);
+    if (m_pDS->query(strSQL.c_str()))
+      paths.insert(GetPathId(m_pDS->fv(0).get_asString()));
+
+    // add all other known paths
     strSQL = PrepareSQL("SELECT DISTINCT idPath FROM files JOIN episode ON episode.idFile=files.idFile WHERE episode.idShow=%i",idShow);
     m_pDS->query(strSQL.c_str());
     while (!m_pDS->eof())

commit a2a4dc8c3638c0ab8a31baf7ecabfed1e6a82498

tvshow_view is still tvshowview in Helix
Issue introduced in 9f432dc62b8a0aae1972e8a1190c5bd870c2e6e1

xbmc/video/VideoDatabase.cpp
@@ -638,7 +638,7 @@ bool CVideoDatabase::GetPathsForTvShow(int idShow, set<int>& paths)
     if (NULL == m_pDS.get()) return false;
 
     // add base path
-    strSQL = PrepareSQL("SELECT strPath FROM tvshow_view WHERE idShow=%i", idShow);
+    strSQL = PrepareSQL("SELECT strPath FROM tvshowview WHERE idShow=%i", idShow);
     if (m_pDS->query(strSQL.c_str()))
       paths.insert(GetPathId(m_pDS->fv(0).get_asString()));
 