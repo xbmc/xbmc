925382bc09ede2f2a0eba3dbe3f54bde38081e3c
Balint Reczey <balint@balintreczey.hu>
2015-05-25 12:46:33 +0200

    cximage: Fix dcraw input sanitization (CVE-2015-3885)
    From http://www.ocert.org/advisories/ocert-2015-006.html :
    The dcraw tool, as well as several other projects re-using its code,
    suffers from an integer overflow condition which lead to a buffer
    overflow. The vulnerability concerns the 'len' variable, parsed
    without validation from opened images, used in the ljpeg_start()
    function.
    A maliciously crafted raw image file can be used to trigger the
    vulnerability, causing a Denial of Service condition.

--- lib/cximage-6.0/raw/libdcr.c
+++ lib/cximage-6.0/raw/libdcr.c
@@ -783,7 +783,8 @@ enough to decode Canon, Kodak and Adobe DNG images.
 
 int DCR_CLASS dcr_ljpeg_start (DCRAW* p, struct dcr_jhead *jh, int info_only)
 {
-	int c, tag, len;
+	int c, tag;
+	ushort len;
 	uchar data[0x10000], *dp;
 
 	dcr_init_decoder(p);
