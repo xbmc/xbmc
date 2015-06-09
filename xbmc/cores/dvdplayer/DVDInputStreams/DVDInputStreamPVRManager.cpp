33d8630d386414d6f9264294afe4caca6848b481
Tobias Markus <tobbi.bugs@googlemail.com>
2015-06-09 15:51:22 +0200

    Remove const keyword from CDVDInputStreamPVRManager::GetSelectedChannel() Fixes CID 1272226

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
@@ -294,7 +294,7 @@ bool CDVDInputStreamPVRManager::SelectChannel(const CPVRChannelPtr &channel)
   return false;
 }
 
-CPVRChannelPtr CDVDInputStreamPVRManager::GetSelectedChannel() const
+CPVRChannelPtr CDVDInputStreamPVRManager::GetSelectedChannel()
 {
   return g_PVRManager.GetCurrentChannel();
 }
