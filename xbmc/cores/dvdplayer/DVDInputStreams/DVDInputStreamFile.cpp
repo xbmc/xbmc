a51ba3873a77a5a77053df501ad00f2c5c85a369
arnova <arnova@void.org>
2015-07-12 16:50:51 +0200

    changed: For memory file cache there's no point creating a bigger cache than filesize

--- xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamFile.cpp
+++ xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamFile.cpp
@@ -53,6 +53,10 @@ bool CDVDInputStreamFile::Open(const char* strFile, const std::string& content, 
     return false;
 
   unsigned int flags = READ_TRUNCATED | READ_BITRATE | READ_CHUNKED;
+  
+  // If this file is audio and/or video (= not a subtitle) flag to caller
+  if (!CFileItem(strFile).IsSubtitle())
+    flags |= READ_AUDIO_VIDEO;
 
   /*
    * There are 4 buffer modes available (configurable in as.xml)
