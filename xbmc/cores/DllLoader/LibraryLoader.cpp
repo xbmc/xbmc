xbmc/xbmc

commit cac60cd991f0d23eacf2024d94d172fdec060a5b

[dllloader] have LibraryLoader hold std::string's

xbmc/cores/DllLoader/LibraryLoader.cpp
@@ -23,52 +23,37 @@
 #include <stdlib.h>
 #include "utils/log.h"
 
-LibraryLoader::LibraryLoader(const char* libraryFile)
+LibraryLoader::LibraryLoader(const std::string& libraryFile)
 {
-  m_sFileName = strdup(libraryFile);
+  m_fileName = libraryFile;
 
-  char* sPath = strrchr(m_sFileName, '\\');
-  if (!sPath) sPath = strrchr(m_sFileName, '/');
-  if (sPath)
-  {
-    sPath++;
-    m_sPath=(char*)malloc(sPath - m_sFileName+1);
-    strncpy(m_sPath, m_sFileName, sPath - m_sFileName);
-    m_sPath[sPath - m_sFileName] = 0;
-  }
-  else
-    m_sPath=NULL;
+  size_t pos = m_fileName.find_last_of("\\/");
+  if (pos != std::string::npos)
+    m_path = m_fileName.substr(0, pos);
 
   m_iRefCount = 1;
 }
 
 LibraryLoader::~LibraryLoader()
 {
-  free(m_sFileName);
-  free(m_sPath);
 }
 
-char* LibraryLoader::GetName()
+const char *LibraryLoader::GetName() const
 {
-  if (m_sFileName)
-  {
-    char* sName = strrchr(m_sFileName, '/');
-    if (sName) return sName + 1;
-    else return m_sFileName;
-  }
-  return (char*)"";
+  size_t pos = m_fileName.find_last_of('/');
+  if (pos != std::string::npos)
+    return &m_fileName.at(pos);
+  return "";
 }
 
-char* LibraryLoader::GetFileName()
+const char *LibraryLoader::GetFileName() const
 {
-  if (m_sFileName) return m_sFileName;
-  return (char*)"";
+  return m_fileName.c_str();
 }
 
-char* LibraryLoader::GetPath()
+const char *LibraryLoader::GetPath() const
 {
-  if (m_sPath) return m_sPath;
-  return (char*)"";
+  return m_path.c_str();
 }
 
 int LibraryLoader::IncrRef()

xbmc/cores/DllLoader/DllLoader.cpp
@@ -290,9 +290,9 @@ int DllLoader::ResolveImports(void)
             Imp->Name_RVA != 0 ||
             Imp->ImportAddressTable_RVA != 0)
     {
-      char *Name = (char*)RVA2Data(Imp->Name_RVA);
+      const char *Name = (const char*)RVA2Data(Imp->Name_RVA);
 
-      char* FileName=ResolveReferencedDll(Name);
+      const char* FileName=ResolveReferencedDll(Name);
       //  If possible use the dll name WITH path to resolve exports. We could have loaded
       //  a dll with the same name as another dll but from a different directory
       if (FileName) Name=FileName;
@@ -350,7 +350,7 @@ int DllLoader::ResolveImports(void)
   return bResult;
 }
 
-char* DllLoader::ResolveReferencedDll(char* dll)
+const char* DllLoader::ResolveReferencedDll(const char* dll)
 {
   DllLoader* pDll = (DllLoader*) DllLoaderContainer::LoadModule(dll, GetPath(), m_bLoadSymbols);
 
@@ -411,7 +411,7 @@ int DllLoader::ResolveExport(const char *sName, void **pAddr, bool logging)
     return 1;
   }
 
-  char* sDllName = strrchr(GetFileName(), '\\');
+  const char* sDllName = strrchr(GetFileName(), '\\');
   if (sDllName) sDllName += 1;
   else sDllName = GetFileName();
 
@@ -434,7 +434,7 @@ int DllLoader::ResolveOrdinal(unsigned long ordinal, void **pAddr)
     return 1;
   }
 
-  char* sDllName = strrchr(GetFileName(), '\\');
+  const char* sDllName = strrchr(GetFileName(), '\\');
   if (sDllName) sDllName += 1;
   else sDllName = GetFileName();
 
@@ -496,7 +496,7 @@ Export* DllLoader::GetExportByFunctionName(const char* sFunctionName)
   return NULL;
 }
 
-int DllLoader::ResolveOrdinal(char *sName, unsigned long ordinal, void **fixup)
+int DllLoader::ResolveOrdinal(const char *sName, unsigned long ordinal, void **fixup)
 {
   DllLoader* pDll = (DllLoader*) DllLoaderContainer::GetModule(sName);
 
@@ -517,7 +517,7 @@ int DllLoader::ResolveOrdinal(char *sName, unsigned long ordinal, void **fixup)
   return 0;
 }
 
-int DllLoader::ResolveName(char *sName, char* sFunction, void **fixup)
+int DllLoader::ResolveName(const char *sName, char* sFunction, void **fixup)
 {
   DllLoader* pDll = (DllLoader*) DllLoaderContainer::GetModule(sName);
 

xbmc/cores/DllLoader/DllLoader.h
@@ -101,9 +101,9 @@ protected:
   void PrintImportTable(ImportDirTable_t *ImportDirTable);
   void PrintExportTable(ExportDirTable_t *ExportDirTable);
 
-  int ResolveOrdinal(char*, unsigned long, void**);
-  int ResolveName(char*, char*, void **);
-  char* ResolveReferencedDll(char* dll);
+  int ResolveOrdinal(const char*, unsigned long, void**);
+  int ResolveName(const char*, char*, void **);
+  const char* ResolveReferencedDll(const char* dll);
   int LoadExports();
   void LoadSymbols();
   static void UnloadSymbols();

xbmc/cores/DllLoader/DllLoaderContainer.cpp
@@ -333,7 +333,7 @@ void DllLoaderContainer::UnloadPythonDlls()
   // unload all dlls that python could have loaded
   for (int i = 0; i < m_iNrOfDlls && m_dlls[i] != NULL; i++)
   {
-    char* name = m_dlls[i]->GetName();
+    const char* name = m_dlls[i]->GetName();
     if (strstr(name, ".pyd") != NULL)
     {
       LibraryLoader* pDll = m_dlls[i];

xbmc/cores/DllLoader/LibraryLoader.h
@@ -21,6 +21,8 @@
 #ifndef LIBRARY_LOADER
 #define LIBRARY_LOADER
 
+#include <string>
+
 #include "system.h"
 #ifdef TARGET_POSIX
 #include "PlatformDefs.h"
@@ -29,7 +31,7 @@
 class LibraryLoader
 {
 public:
-  LibraryLoader(const char* libraryFile);
+  LibraryLoader(const std::string& libraryFile);
   virtual ~LibraryLoader();
 
   virtual bool Load() = 0;
@@ -41,9 +43,9 @@ public:
   virtual HMODULE GetHModule() = 0;
   virtual bool HasSymbols() = 0;
 
-  char* GetName(); // eg "mplayer.dll"
-  char* GetFileName(); // "special://xbmcbin/system/mplayer/players/mplayer.dll"
-  char* GetPath(); // "special://xbmcbin/system/mplayer/players/"
+  const char *GetName() const; // eg "mplayer.dll"
+  const char *GetFileName() const; // "special://xbmcbin/system/mplayer/players/mplayer.dll"
+  const char *GetPath() const; // "special://xbmcbin/system/mplayer/players/"
 
   int IncrRef();
   int DecrRef();
@@ -52,8 +54,8 @@ public:
 private:
   LibraryLoader(const LibraryLoader&);
   LibraryLoader& operator=(const LibraryLoader&);
-  char* m_sFileName;
-  char* m_sPath;
+  std::string m_fileName;
+  std::string m_path;
   int   m_iRefCount;
 };
 

xbmc/cores/DllLoader/SoLoader.cpp
@@ -27,7 +27,7 @@
 #include "android/loader/AndroidDyload.h"
 #endif
 
-SoLoader::SoLoader(const char *so, bool bGlobal) : LibraryLoader(so)
+SoLoader::SoLoader(const std::string &so, bool bGlobal) : LibraryLoader(so)
 {
   m_soHandle = NULL;
   m_bGlobal = bGlobal;

xbmc/cores/DllLoader/SoLoader.h
@@ -31,7 +31,7 @@
 class SoLoader : public LibraryLoader
 {
 public:
-  SoLoader(const char *so, bool bGlobal = false);
+  SoLoader(const std::string &so, bool bGlobal = false);
   ~SoLoader();
 
   virtual bool Load();

xbmc/cores/DllLoader/dll.cpp
@@ -272,7 +272,7 @@ extern "C" DWORD WINAPI dllGetModuleFileNameA(HMODULE hModule, LPSTR lpFilename,
     return 0;
   }
 
-  char* sName = dll->GetFileName();
+  const char* sName = dll->GetFileName();
   if (sName)
   {
     strncpy(lpFilename, sName, nSize);

commit 6ba0ec7d6d0479e588d2d2b2629122effdb17828

[dllloader] Fix logging of dlls without absolute path
There was a bug introduced in cac60cd9 where a filename without a slash is not printed at all.
The previous code would just return the filename.

It causes log to show 'DEBUG: Unloading:' rather than 'DEBUG: Unloading: libnfs.so.4'.

xbmc/cores/DllLoader/LibraryLoader.cpp
@@ -42,7 +42,7 @@ const char *LibraryLoader::GetName() const
   size_t pos = m_fileName.find_last_of('/');
   if (pos != std::string::npos)
     return &m_fileName.at(pos);
-  return "";
+  return m_fileName.c_str();
 }
 
 const char *LibraryLoader::GetFileName() const