ead1547d3b364e4e314521153ac35c566ac23926
metaron <andrew@thomasinfletcher.com>
2015-07-24 15:15:12 +0100

    [PVR] Add Recording scraper support to API
    season/episode in pvr:// filename so ttvdb scraper finds episodes
    year in filename so imdb scraper identifyies correct re-make
    subtitle (strEpisodeName) for human readable episode name

--- xbmc/addons/include/xbmc_pvr_types.h
+++ xbmc/addons/include/xbmc_pvr_types.h
@@ -407,6 +407,11 @@ extern "C" {
   typedef struct PVR_RECORDING {
     char   strRecordingId[PVR_ADDON_NAME_STRING_LENGTH];  /*!< @brief (required) unique id of the recording on the client. */
     char   strTitle[PVR_ADDON_NAME_STRING_LENGTH];        /*!< @brief (required) the title of this recording */
+    char   strEpisodeName[PVR_ADDON_NAME_STRING_LENGTH];  /*!< @brief (optional) episode name (also known as subtitle) */
+    int    iSeriesNumber;                                 /*!< @brief (optional) series number (usually called season). Set to "0" for specials/pilot. For 'invalid' case see iEpisodeNumber */
+    int    iEpisodeNumber;                                /*!< @brief (optional) episode number within the "iSeriesNumber" season. To indicate that season/episode is 'invalid' set both to 0 */
+    int    iYear;                                         /*!< @brief (optional) year of first release (use to identify a specific movie re-make). Set to '0' for invalid. */
+
     char   strStreamURL[PVR_ADDON_URL_STRING_LENGTH];     /*!< @brief (required) stream URL to access this recording */
     char   strDirectory[PVR_ADDON_URL_STRING_LENGTH];     /*!< @brief (optional) directory of this recording on the client */
     char   strPlotOutline[PVR_ADDON_DESC_STRING_LENGTH];  /*!< @brief (optional) plot outline */
