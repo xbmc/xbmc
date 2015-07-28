cb24ed3e7e1da3aaadd7b96a7c55d27292581307
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2015-07-28 21:09:54 +0200

    [EPG] Jump to 'now' on init of EPG timeline view

--- xbmc/pvr/windows/GUIWindowPVRGuide.cpp
+++ xbmc/pvr/windows/GUIWindowPVRGuide.cpp
@@ -54,6 +54,11 @@ void CGUIWindowPVRGuide::OnInitWindow()
   if (m_guiState.get())
     m_viewControl.SetCurrentView(m_guiState->GetViewAsControl(), false);
 
+  CGUIEPGGridContainer *epgGridContainer =
+    dynamic_cast<CGUIEPGGridContainer*>(GetControl(m_viewControl.GetCurrentControl()));
+  if (epgGridContainer)
+    epgGridContainer->GoToNow();
+
   CGUIWindowPVRBase::OnInitWindow();
 }
 
