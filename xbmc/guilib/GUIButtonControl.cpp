1b8453654ca37c1e90405817dde9ef3fbd7fa37c
Matthias Kortstiege <mkortstiege@kodi.tv>
2015-07-29 18:35:43 +0200

    [guilib] fix hitrect for auto-width buttons

--- xbmc/guilib/GUIButtonControl.cpp
+++ xbmc/guilib/GUIButtonControl.cpp
@@ -142,7 +142,8 @@ void CGUIButtonControl::ProcessText(unsigned int currentTime)
   CRect labelRenderRect = m_label.GetRenderRect();
   CRect label2RenderRect = m_label2.GetRenderRect();
 
-  bool changed = m_label.SetMaxRect(m_posX, m_posY, GetWidth(), m_height);
+  float renderWidth = GetWidth();
+  bool changed = m_label.SetMaxRect(m_posX, m_posY, renderWidth, m_height);
   changed |= m_label.SetText(m_info.GetLabel(m_parentID));
   changed |= m_label.SetScrolling(HasFocus());
 
@@ -150,11 +151,18 @@ void CGUIButtonControl::ProcessText(unsigned int currentTime)
   if (m_minWidth && (m_label.GetRenderRect() != labelRenderRect))
     SetInvalid();
 
+  // auto-width - adjust hitrect
+  if (m_minWidth && m_width != renderWidth)
+  {
+    CRect rect(m_posX, m_posY, renderWidth, m_height);
+    SetHitRect(rect);
+  }
+
   // render the second label if it exists
   if (!m_info2.GetLabel(m_parentID).empty())
   {
     changed |= m_label2.SetAlign(XBFONT_RIGHT | (m_label.GetLabelInfo().align & XBFONT_CENTER_Y) | XBFONT_TRUNCATED);
-    changed |= m_label2.SetMaxRect(m_posX, m_posY, GetWidth(), m_height);
+    changed |= m_label2.SetMaxRect(m_posX, m_posY, renderWidth, m_height);
     changed |= m_label2.SetText(m_info2.GetLabel(m_parentID));
     changed |= m_label2.SetScrolling(HasFocus());
 
