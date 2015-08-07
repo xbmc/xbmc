78592c3be8d317f0fff810fb4dc46573625c04f1
JoethePartylion <kevin.ellis22@live.co.uk>
2015-08-07 15:10:17 +0100

    Fix empty videoplayer.plot infolabel when playing recorded TV.
    Update GUIInfoManager.cpp
    
    Update GUIInfoLabels.h

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -4017,6 +4017,15 @@ std::string CGUIInfoManager::GetVideoLabel(int item)
       }
     }
   }
+  else if (m_currentFile->HasPVRRecordingInfoTag())
+  {
+    switch (item)
+    {
+      /* Now playing infos */
+    case VIDEOPLAYER_PLOT:
+      return m_currentFile->GetPVRRecordingInfoTag()->m_strPlot;
+    }
+  }
   else if (m_currentFile->HasVideoInfoTag())
   {
     switch (item)
