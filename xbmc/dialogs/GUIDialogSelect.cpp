xbmc/xbmc

commit 71e2e31904d01f10be4569f71cf388c304fc3ef1

Fix the following coverity issues that the variables are uninitialized in the constructor or any functions that it calls fix 719141, 719143, 719144, 719146, 719147, 719148, 719149, 727842, 122884 fix 718193, 718210, 1261374, 1261377

xbmc/dialogs/GUIDialogSelect.cpp
@@ -35,6 +35,8 @@ CGUIDialogSelect::CGUIDialogSelect(void)
     : CGUIDialogBoxBase(WINDOW_DIALOG_SELECT, "DialogSelect.xml")
 {
   m_bButtonEnabled = false;
+  m_bButtonPressed = false;
+  m_bConfirmed = false;
   m_buttonString = -1;
   m_useDetails = false;
   m_vecList = new CFileItemList;
@@ -87,8 +89,6 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 
   case GUI_MSG_WINDOW_INIT:
     {
-      m_bButtonPressed = false;
-      m_bConfirmed = false;
       CGUIDialog::OnMessage(message);
       return true;
     }

xbmc/cores/dvdplayer/DVDAudio.cpp
@@ -40,6 +40,8 @@ CDVDAudio::CDVDAudio(volatile bool &bStop)
   m_iBitrate = 0;
   m_SecondsPerByte = 0.0;
   m_bPaused = true;
+  m_playingPts = DVD_NOPTS_VALUE; //silence coverity uninitialized warning, is set elsewhere
+  m_timeOfPts = 0.0; //silence coverity uninitialized warning, is set elsewhere
 }
 
 CDVDAudio::~CDVDAudio()

xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
@@ -186,6 +186,7 @@ CDVDVideoCodecFFmpeg::CDVDVideoCodecFFmpeg() : CDVDVideoCodec()
   m_decoderPts = DVD_NOPTS_VALUE;
   m_codecControlFlags = 0;
   m_requestSkipDeint = false;
+  m_skippedDeint = 0; //silence coverity uninitialized warning, is set elsewhere
 }
 
 CDVDVideoCodecFFmpeg::~CDVDVideoCodecFFmpeg()

xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.h
@@ -121,7 +121,7 @@ protected:
   double m_dts;
   bool   m_started;
   std::vector<PixelFormat> m_formats;
-  double m_decoderPts, m_decoderInterval;
+  double m_decoderPts;
   int    m_skippedDeint;
   bool   m_requestSkipDeint;
   int    m_codecControlFlags;

xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxCC.cpp
@@ -80,6 +80,7 @@ public:
   {
     m_data = (uint8_t*)malloc(size);
     m_size = size;
+    m_pts = 0.0; //silence coverity uninitialized warning, is set elsewhere
   }
   virtual ~CCaptionBlock()
   {
@@ -98,7 +99,7 @@ bool reorder_sort (CCaptionBlock *lhs, CCaptionBlock *rhs)
 CDVDDemuxCC::CDVDDemuxCC(AVCodecID codec)
 {
   m_hasData = false;
-  m_curPts = 0;
+  m_curPts = 0.0;
   m_ccDecoder = NULL;
   m_codec = codec;
 }

xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -139,6 +139,7 @@ CDVDPlayerVideo::CDVDPlayerVideo( CDVDClock* pClock
   m_iVideoDelay = 0;
   m_iSubtitleDelay = 0;
   m_FlipTimeStamp = 0.0;
+  m_FlipTimePts = 0.0f; //silence coverity uninitialized warning, is set elsewhere
   m_iLateFrames = 0;
   m_iDroppedRequest = 0;
   m_fForcedAspectRatio = 0;

xbmc/dialogs/GUIDialogBusy.cpp
@@ -88,6 +88,7 @@ CGUIDialogBusy::CGUIDialogBusy(void)
 {
   m_loadType = LOAD_ON_GUI_INIT;
   m_bModal = true;
+  m_bCanceled = false;
   m_progress = 0;
 }
 

xbmc/dialogs/GUIDialogCache.h
@@ -54,5 +54,4 @@ protected:
   std::string m_strLinePrev;
   std::string m_strLinePrev2;
   bool bSentCancel;
-  bool m_bOpenTried;
 };

commit ed235c4469260f73ce32fe7577b79f2570697033

[gui] fix confirm state not reset in CGUIDialogSelect
regression from 71e2e3190

xbmc/dialogs/GUIDialogSelect.cpp
@@ -88,6 +88,8 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 
   case GUI_MSG_WINDOW_INIT:
     {
+      m_bButtonPressed = false;
+      m_bConfirmed = false;
       CGUIDialog::OnMessage(message);
       return true;
     }