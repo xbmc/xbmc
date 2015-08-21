xbmc/xbmc

commit e002a1b88d136b24a0d4b60c8ceb335364718dea

[gui] change CGUIDialogSelect::GetSelectedItems to return indices

xbmc/dialogs/GUIDialogSelect.cpp
@@ -39,7 +39,6 @@ CGUIDialogSelect::CGUIDialogSelect(void)
   m_buttonString = -1;
   m_useDetails = false;
   m_vecList = new CFileItemList;
-  m_selectedItems = new CFileItemList;
   m_multiSelection = false;
   m_iSelected = -1;
   m_loadType = KEEP_IN_MEMORY;
@@ -48,7 +47,6 @@ CGUIDialogSelect::CGUIDialogSelect(void)
 CGUIDialogSelect::~CGUIDialogSelect(void)
 {
   delete m_vecList;
-  delete m_selectedItems;
 }
 
 bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
@@ -65,14 +63,14 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
       m_multiSelection = false;
 
       // construct selected items list
-      m_selectedItems->Clear();
+      m_selectedItems.clear();
       m_iSelected = -1;
       for (int i = 0 ; i < m_vecList->Size() ; i++)
       {
         CFileItemPtr item = m_vecList->Get(i);
         if (item->IsSelected())
         {
-          m_selectedItems->Add(item);
+          m_selectedItems.push_back(i);
           if (m_iSelected == -1)
             m_iSelected = i;
         }
@@ -156,7 +154,7 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 bool CGUIDialogSelect::OnBack(int actionID)
 {
   m_iSelected = -1;
-  m_selectedItems->Clear();
+  m_selectedItems.clear();
   m_bConfirmed = false;
   return CGUIDialog::OnBack(actionID);
 }
@@ -170,7 +168,7 @@ void CGUIDialogSelect::Reset()
   m_multiSelection = false;
   m_iSelected = -1;
   m_vecList->Clear();
-  m_selectedItems->Clear();
+  m_selectedItems.clear();
 }
 
 int CGUIDialogSelect::Add(const std::string& strLabel)
@@ -211,7 +209,9 @@ int CGUIDialogSelect::GetSelectedLabel() const
 
 const CFileItemPtr CGUIDialogSelect::GetSelectedItem() const
 {
-  return m_selectedItems->Size() > 0 ? m_selectedItems->Get(0) : CFileItemPtr(new CFileItem);
+  if (m_selectedItems.size() > 0 && m_vecList->Size() > 0)
+    return m_vecList->Get(m_selectedItems.front());
+  return CFileItemPtr(new CFileItem);
 }
 
 const std::string& CGUIDialogSelect::GetSelectedLabelText() const
@@ -219,9 +219,9 @@ const std::string& CGUIDialogSelect::GetSelectedLabelText() const
   return GetSelectedItem()->GetLabel();
 }
 
-const CFileItemList& CGUIDialogSelect::GetSelectedItems() const
+const std::vector<int>& CGUIDialogSelect::GetSelectedItems() const
 {
-  return *m_selectedItems;
+  return m_selectedItems;
 }
 
 void CGUIDialogSelect::EnableButton(bool enable, int string)
@@ -256,7 +256,7 @@ void CGUIDialogSelect::SetSelected(int iSelected)
   if (!m_multiSelection || m_iSelected < 0 || m_iSelected > iSelected)
     m_iSelected = iSelected;
   m_vecList->Get(iSelected)->Select(true);
-  m_selectedItems->Add(m_vecList->Get(iSelected));
+  m_selectedItems.push_back(iSelected);
 }
 
 void CGUIDialogSelect::SetSelected(const std::string &strSelectedLabel)
@@ -321,7 +321,7 @@ void CGUIDialogSelect::OnWindowLoaded()
 void CGUIDialogSelect::OnInitWindow()
 {
   m_viewControl.SetItems(*m_vecList);
-  m_selectedItems->Clear();
+  m_selectedItems.clear();
   if (m_iSelected == -1)
   {
     for(int i = 0 ; i < m_vecList->Size(); i++)

xbmc/dialogs/GUIDialogSelect.h
@@ -43,7 +43,7 @@ public:
   int GetSelectedLabel() const;
   const std::string& GetSelectedLabelText() const;
   const CFileItemPtr GetSelectedItem() const;
-  const CFileItemList& GetSelectedItems() const;
+  const std::vector<int>& GetSelectedItems() const;
   void EnableButton(bool enable, int string);
   bool IsButtonPressed();
   void Sort(bool bSortOrder = true);
@@ -67,7 +67,7 @@ protected:
   bool m_useDetails;
   bool m_multiSelection;
 
-  CFileItemList* m_selectedItems;
+  std::vector<int> m_selectedItems;
   CFileItemList* m_vecList;
   CGUIViewControl m_viewControl;
 };

xbmc/addons/GUIWindowAddonBrowser.cpp
@@ -612,10 +612,9 @@ int CGUIWindowAddonBrowser::SelectAddonID(const vector<ADDON::TYPE> &types, vect
     return 0;
 
   addonIDs.clear();
-  const CFileItemList& list = dialog->GetSelectedItems();
-  for (int i = 0; i < list.Size(); i++)
+  for (int i : dialog->GetSelectedItems())
   {
-    const CFileItemPtr& item = list.Get(i);
+    const CFileItemPtr& item = items.Get(i);
 
     // check if one of the selected addons needs to be installed
     if (showInstallable)

xbmc/dialogs/GUIDialogSmartPlaylistRule.cpp
@@ -356,10 +356,9 @@ void CGUIDialogSmartPlaylistRule::OnBrowse()
   pDialog->Open();
   if (pDialog->IsConfirmed())
   {
-    const CFileItemList &items = pDialog->GetSelectedItems();
     m_rule.m_parameter.clear();
-    for (int index = 0; index < items.Size(); index++)
-      m_rule.m_parameter.push_back(items[index]->GetLabel());
+    for (int i : pDialog->GetSelectedItems())
+      m_rule.m_parameter.push_back(items.Get(i)->GetLabel());
 
     UpdateButtons();
   }

xbmc/settings/windows/GUIControlSettings.cpp
@@ -378,11 +378,10 @@ bool CGUIControlListSetting::OnClick()
   if (!dialog->IsConfirmed())
     return false;
 
-  const CFileItemList &items = dialog->GetSelectedItems();
   std::vector<CVariant> values;
-  for (int index = 0; index < items.Size(); index++)
+  for (int i : dialog->GetSelectedItems())
   {
-    const CFileItemPtr item = items[index];
+    const CFileItemPtr item = options.Get(i);
     if (item == NULL || !item->HasProperty("value"))
       return false;
 

xbmc/video/dialogs/GUIDialogVideoInfo.cpp
@@ -1359,7 +1359,8 @@ bool CGUIDialogVideoInfo::GetMoviesForSet(const CFileItem *setItem, CFileItemLis
 
   if (dialog->IsConfirmed())
   {
-    selectedMovies.Copy(dialog->GetSelectedItems());
+    for (int i : dialog->GetSelectedItems())
+      selectedMovies.Add(listItems.Get(i));
     return (selectedMovies.Size() > 0);
   }
   else
@@ -1524,7 +1525,8 @@ bool CGUIDialogVideoInfo::GetItemsForTag(const std::string &strHeading, const st
   dialog->EnableButton(true, 186);
   dialog->Open();
 
-  items.Copy(dialog->GetSelectedItems());
+  for (int i : dialog->GetSelectedItems())
+    items.Add(listItems.Get(i));
   return items.Size() > 0;
 }
 

commit fe58cca78f02c62567ae5124b6952f75f22fd67b

[gui] fix CGUIDialogSelect::GetSelectedItem not returning the selected item
regression after e002a1b

xbmc/dialogs/GUIDialogSelect.cpp
@@ -40,7 +40,7 @@ CGUIDialogSelect::CGUIDialogSelect(void)
   m_useDetails = false;
   m_vecList = new CFileItemList;
   m_multiSelection = false;
-  m_iSelected = -1;
+  m_selectedItem = nullptr;
   m_loadType = KEEP_IN_MEMORY;
 }
 
@@ -64,15 +64,15 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 
       // construct selected items list
       m_selectedItems.clear();
-      m_iSelected = -1;
+      m_selectedItem = nullptr;
       for (int i = 0 ; i < m_vecList->Size() ; i++)
       {
         CFileItemPtr item = m_vecList->Get(i);
         if (item->IsSelected())
         {
           m_selectedItems.push_back(i);
-          if (m_iSelected == -1)
-            m_iSelected = i;
+          if (!m_selectedItem)
+            m_selectedItem = item;
         }
       }
 
@@ -121,7 +121,7 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
       }
       if (CONTROL_BUTTON == iControl)
       {
-        m_iSelected = -1;
+        m_selectedItem = nullptr;
         m_bButtonPressed = true;
         if (m_multiSelection)
           m_bConfirmed = true;
@@ -153,7 +153,7 @@ bool CGUIDialogSelect::OnMessage(CGUIMessage& message)
 
 bool CGUIDialogSelect::OnBack(int actionID)
 {
-  m_iSelected = -1;
+  m_selectedItem = nullptr;
   m_selectedItems.clear();
   m_bConfirmed = false;
   return CGUIDialog::OnBack(actionID);
@@ -166,7 +166,7 @@ void CGUIDialogSelect::Reset()
   m_bButtonPressed = false;
   m_useDetails = false;
   m_multiSelection = false;
-  m_iSelected = -1;
+  m_selectedItem = nullptr;
   m_vecList->Clear();
   m_selectedItems.clear();
 }
@@ -204,13 +204,13 @@ void CGUIDialogSelect::SetItems(CFileItemList* pList)
 
 int CGUIDialogSelect::GetSelectedLabel() const
 {
-  return m_iSelected;
+  return m_selectedItems.size() > 0 ? m_selectedItems[0] : -1;
 }
 
 const CFileItemPtr CGUIDialogSelect::GetSelectedItem() const
 {
-  if (m_selectedItems.size() > 0 && m_vecList->Size() > 0)
-    return m_vecList->Get(m_selectedItems.front());
+  if (m_selectedItem)
+    return m_selectedItem;
   return CFileItemPtr(new CFileItem);
 }
 
@@ -253,8 +253,9 @@ void CGUIDialogSelect::SetSelected(int iSelected)
   // or if it doesn't have a valid value yet
   // or if the current value is bigger than the new one
   // so that we always focus the item nearest to the beginning of the list
-  if (!m_multiSelection || m_iSelected < 0 || m_iSelected > iSelected)
-    m_iSelected = iSelected;
+  if (!m_multiSelection || !m_selectedItem ||
+      (!m_selectedItems.empty() && m_selectedItems.back() > iSelected))
+    m_selectedItem = m_vecList->Get(iSelected);
   m_vecList->Get(iSelected)->Select(true);
   m_selectedItems.push_back(iSelected);
 }
@@ -322,13 +323,13 @@ void CGUIDialogSelect::OnInitWindow()
 {
   m_viewControl.SetItems(*m_vecList);
   m_selectedItems.clear();
-  if (m_iSelected == -1)
+  if (!m_selectedItem)
   {
     for(int i = 0 ; i < m_vecList->Size(); i++)
     {
       if (m_vecList->Get(i)->IsSelected())
       {
-        m_iSelected = i;
+        m_selectedItem = m_vecList->Get(i);
         break;
       }
     }
@@ -344,8 +345,8 @@ void CGUIDialogSelect::OnInitWindow()
   SetupButton();
   CGUIDialogBoxBase::OnInitWindow();
 
-  // if m_iSelected < 0 focus first item
-  m_viewControl.SetSelectedItem(std::max(m_iSelected, 0));
+  // if nothing is selected, focus first item
+  m_viewControl.SetSelectedItem(std::max(GetSelectedLabel(), 0));
 }
 
 void CGUIDialogSelect::OnWindowUnload()

xbmc/dialogs/GUIDialogSelect.h
@@ -63,7 +63,7 @@ protected:
   bool m_bButtonEnabled;
   int m_buttonString;
   bool m_bButtonPressed;
-  int m_iSelected;
+  CFileItemPtr m_selectedItem;
   bool m_useDetails;
   bool m_multiSelection;
 