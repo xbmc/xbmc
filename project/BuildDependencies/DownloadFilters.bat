f8cf1ea726b3e16c0d6ad08fd89a97f15f933ff1
aracnoz <aracnoz@yahoo.it>
2015-08-24 14:30:51 +0200

    added BuildDependencies/DownloadFilters.bat to download LavFilters and XySubFilter

---
+++ project/BuildDependencies/DownloadFilters.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET CUR_PATH=%CD%
+SET APP_PATH=%CD%\..\..
+SET TMP_PATH=%CD%\scripts\tmp
+
+rem can't run rmdir and md back to back. access denied error otherwise.
+IF EXIST lib rmdir lib /S /Q
+IF EXIST include rmdir include /S /Q
+IF EXIST %TMP_PATH% rmdir %TMP_PATH% /S /Q
+
+IF $%1$ == $$ (
+  SET DL_PATH="%CD%\downloads"
+) ELSE (
+  SET DL_PATH="%1"
+)
+
+SET WGET=%CUR_PATH%\bin\wget
+SET ZIP=%CUR_PATH%\..\Win32BuildSetup\tools\7z\7za
+
+IF NOT EXIST %DL_PATH% md %DL_PATH%
+
+md lib
+md include
+md %TMP_PATH%
+
+cd scripts
+
+SET FORMED_OK_FLAG=%TMP_PATH%\got-all-formed-packages
+REM Trick to preserve console title
+start /b /wait cmd.exe /c get_filters.cmd
+IF NOT EXIST %FORMED_OK_FLAG% (
+  ECHO ERROR: Not all formed packages are ready!
+  EXIT /B 101
+)
+
+cd %CUR_PATH%
+
+rmdir %TMP_PATH% /S /Q
+EXIT /B 0
---
+++ project/BuildDependencies/scripts/get_filters.cmd
@@ -0,0 +1,105 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET LAV_HTTP="https://github.com/Nevcairiel/LAVFilters/releases/download/0.65/"
+SET LAV_FILE="LAVFilters-0.65-x86.zip"
+SET LAV_PATH="%TMP_PATH%\LAVFilters-0.65-x86\system\players\dsplayer\LAVFilters"
+
+SET SUB_HTTP="https://github.com/Cyberbeing/xy-VSFilter/releases/download/3.1.0.705/"
+SET SUB_FILE="XySubFilter_3.1.0.705_x86_BETA2.zip"
+SET SUB_PATH="%TMP_PATH%\XySubFilter_3.1.0.705_x86_BETA2\system\players\dsplayer\XySubFilter"
+
+SET CUR_HTTP=%LAV_HTTP%
+SET CUR_PATH=%LAV_PATH%
+
+REM Clear succeed flag
+IF EXIST %FORMED_OK_FLAG% (
+  del /F /Q "%FORMED_OK_FLAG%" || EXIT /B 4
+)
+
+CALL :setStageName Starting downloads of formed packages...
+SET SCRIPT_PATH=%CD%
+CD %DL_PATH% || EXIT /B 10
+
+SET CUR_HTTP=%LAV_HTTP%
+SET CUR_PATH=%LAV_PATH%
+CALL :processFile %LAV_FILE%
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+SET CUR_HTTP=%SUB_HTTP%
+SET CUR_PATH=%SUB_PATH%
+CALL :processFile %SUB_FILE%
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+
+CALL :setStageName All formed packages ready.
+ECHO %DATE% %TIME% > "%FORMED_OK_FLAG%"
+EXIT /B 0
+REM End of main body
+
+:processFile
+CALL :setStageName Getting %1...
+SET RetryDownload=YES
+:startDownloadingFile
+IF EXIST %1 (
+  ECHO Using downloaded %1
+) ELSE (
+  CALL :setSubStageName Downloading %1...
+  %WGET% --no-check-certificate --output-document="%DL_PATH%/%1" "%CUR_HTTP%/%1" || EXIT /B 7
+  TITLE Getting  %i
+)
+
+IF NOT EXIST %CUR_PATH% md %CUR_PATH%
+
+CALL :setSubStageName Extracting %1...
+copy /b "%1" "%TMP_PATH%" || EXIT /B 5
+PUSHD "%TMP_PATH%" || EXIT /B 10
+%ZIP% x %1 -o%CUR_PATH% || (
+  IF %RetryDownload%==YES (
+    POPD || EXIT /B 5
+    ECHO WARNNING! Can't extract files from archive %1!
+    ECHO WARNNING! Deleting %1 and will retry downloading.
+    del /f "%1"
+    SET RetryDownload=NO
+    GOTO startDownloadingFile
+  )
+  exit /B 6
+)
+
+dir /A:-D "%~n1\*.*" >NUL 2>NUL && (
+CALL :setSubStageName Pre-Cleaning %1...
+REM Remove any non-dir files in extracted ".\packagename\"
+FOR /F %%f IN ('dir /B /A:-D "%~n1\*.*"') DO (del "%~n1\%%f" /F /Q || EXIT /B 4)
+)
+
+CALL :setSubStageName Copying %1 to build tree...
+REM Copy only content of extracted ".\packagename\"
+XCOPY "%~n1\*" "%APP_PATH%\" /E /I /Y /F /R /H /K || EXIT /B 5
+
+dir /A:-D * >NUL 2>NUL && (
+CALL :setSubStageName Post-Cleaning %1.zip...
+REM Delete package archive and possible garbage
+FOR /F %%f IN ('dir /B /A:-D') DO (del %%f /F /Q || EXIT /B 4)
+)
+
+ECHO.
+ECHO Done %1.
+POPD || EXIT /B 10
+
+EXIT /B 0
+REM end of :processFile
+
+:setStageName
+ECHO.
+ECHO ==================================================
+ECHO %*
+ECHO.
+TITLE %*
+EXIT /B 0
+
+:setSubStageName
+ECHO.
+ECHO --------------------------------------------------
+ECHO %*
+ECHO.
+EXIT /B 0
