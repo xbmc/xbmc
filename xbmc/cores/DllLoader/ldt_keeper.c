523817d861a8abd317d505d3135df33f6c583b3c
Edmund Grimley Evans <edmund.grimley.evans@gmail.com>
2015-08-26 08:00:58 +0200

    arm64: Fix build breakages due to architecture specific guards
    Origin: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=796532

--- xbmc/cores/DllLoader/ldt_keeper.c
+++ xbmc/cores/DllLoader/ldt_keeper.c
@@ -19,7 +19,7 @@
  */
 
 //#ifndef __powerpc__
-#if !defined(__powerpc__) && !defined(__ppc__) && !defined(__arm__) && !defined(__mips__)
+#if !defined(__powerpc__) && !defined(__ppc__) && !defined(__arm__) && !defined(__mips__) && !defined(__aarch64__)
 
 #include "ldt_keeper.h"
 
