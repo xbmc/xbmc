bd63370406db249f837a9bef531dee2d061e254b
Matthias Kortstiege <mkortstiege@kodi.tv>
2015-08-28 10:44:51 +0200

    [dbwrappers] use std:: instead of using namespace std

--- xbmc/dbwrappers/qry_dat.cpp
+++ xbmc/dbwrappers/qry_dat.cpp
@@ -41,8 +41,6 @@
 #pragma warning (disable:4715)
 #endif
 
-using namespace std;
-
 namespace dbiplus {
 
 //Constructors 
@@ -169,8 +167,8 @@ field_value::~field_value(){
 
   
 //Conversations functions
-string field_value::get_asString() const {
-    string tmp;
+std::string field_value::get_asString() const {
+    std::string tmp;
     switch (field_type) {
     case ft_String: {
       tmp = str_value;
@@ -655,7 +653,7 @@ void field_value::set_asString(const char *s) {
   str_value = s;
   field_type = ft_String;}
 
-void field_value::set_asString(const string & s) {
+void field_value::set_asString(const std::string & s) {
   str_value = s;
   field_type = ft_String;}
   
@@ -702,8 +700,8 @@ fType field_value::get_field_type() {
   return field_type;}
 
   
-string field_value::gft() {
-    string tmp;
+std::string field_value::gft() {
+    std::string tmp;
     switch (field_type) {
     case ft_String: {
       tmp.assign("string");
