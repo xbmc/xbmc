turulli/test

[add] Set as default for <TvShowName> episodes

xbmc/Application.cpp
@@ -3624,13 +3624,18 @@ void CApplication::LoadVideoSettings(const CFileItem& item)
     CFileItem item = CurrentFileItem();
     CStreamDetails streamDetails = item.GetVideoInfoTag()->m_streamDetails;
     int res = streamDetails.VideoDimsToResolution(streamDetails.GetVideoWidth(), streamDetails.GetVideoHeight());
-   
+    std::string tvShowName = item.GetVideoInfoTag()->m_strShowTitle;
+
     CLog::Log(LOGDEBUG, "Loading madvr settings for %s with resolution id: %i", item.GetPath().c_str(), res);
 
-    // Load stored settings if they exist, otherwise use default
+    // Load stored files settings
     if (!dsdbs.GetVideoSettings(item.GetPath().c_str(), CMediaSettings::Get().GetCurrentMadvrSettings()))
-      if (!dsdbs.GetDefResMadvrSettings(res, CMediaSettings::Get().GetCurrentMadvrSettings())) 
-        CMediaSettings::Get().GetCurrentMadvrSettings() = CMediaSettings::Get().GetDefaultMadvrSettings();
+      // if not present Load stored TvShowName settings
+      if (!dsdbs.GetDefResMadvrSettings(-1, tvShowName, CMediaSettings::Get().GetCurrentMadvrSettings()))
+        // if not present Load stored Resolution settings
+        if (!dsdbs.GetDefResMadvrSettings(res, "", CMediaSettings::Get().GetCurrentMadvrSettings())) 
+          // if not present Load default setting
+          CMediaSettings::Get().GetCurrentMadvrSettings() = CMediaSettings::Get().GetDefaultMadvrSettings();
 
     CMediaSettings::Get().GetAtStartMadvrSettings() = CMediaSettings::Get().GetCurrentMadvrSettings();
 

xbmc/cores/DSPlayer/DSPlayer.cpp
@@ -1061,8 +1061,10 @@ void CDSPlayer::SetMadvrResolution()
 
   CStreamDetails streamDetails;
   int res = streamDetails.VideoDimsToResolution(GetPictureWidth(), GetPictureHeight());
+  std::string str = g_application.CurrentFileItem().GetVideoInfoTag()->m_strShowTitle;
 
   CMediaSettings::Get().GetCurrentMadvrSettings().m_Resolution = res;
+  CMediaSettings::Get().GetCurrentMadvrSettings().m_TvShowName = str;
 }
 
 // Time is in millisecond

xbmc/video/dialogs/GUIDialogVideoSettings.cpp
@@ -396,10 +396,17 @@ void CGUIDialogVideoSettings::SaveChoice()
   if (!pDlg)
     return;
 
+  CFileItem &item = g_application.CurrentFileItem();
+  std::string tvShowName = CMediaSettings::Get().GetCurrentMadvrSettings().m_TvShowName;
+  int currentRes = CMediaSettings::Get().GetCurrentMadvrSettings().m_Resolution;
+
   pDlg->Add(g_localizeStrings.Get(70601).c_str());
   pDlg->Add(g_localizeStrings.Get(70602).c_str());
   pDlg->Add(g_localizeStrings.Get(70603).c_str());
   pDlg->Add(g_localizeStrings.Get(70604).c_str());
+  if (item.HasVideoInfoTag() && (item.GetVideoContentType() == VIDEODB_CONTENT_EPISODES || item.GetVideoContentType() == VIDEODB_CONTENT_TVSHOWS))
+    
+    pDlg->Add(StringUtils::Format(g_localizeStrings.Get(70605).c_str(), tvShowName.c_str()));
   pDlg->Add(g_localizeStrings.Get(12376).c_str());
 
   pDlg->SetHeading(70600);
@@ -420,16 +427,26 @@ void CGUIDialogVideoSettings::SaveChoice()
       label = 70603;
     if (selected == MADVR_RES_2160)
       label = 70604;
-    
-    if (CGUIDialogYesNo::ShowAndGetInput(label, 750, 0, 12377))
+    if (selected == MADVR_RES_EPISODES)
+      label = 70605;
+
+    if (CGUIDialogYesNo::ShowAndGetInput(StringUtils::Format(g_localizeStrings.Get(label).c_str(), tvShowName.c_str()), 750, 0, 12377))
     { // reset the settings
 
       CDSPlayerDatabase dspdb;
       if (!dspdb.Open())
         return;
 
-      dspdb.EraseVideoSettings(selected);
-      dspdb.CreateVideoSettings(selected, CMediaSettings::Get().GetCurrentMadvrSettings());
+      if (selected == MADVR_RES_EPISODES)
+      {
+        dspdb.EraseVideoSettings(-1, currentRes, tvShowName);
+        dspdb.CreateVideoSettings(-1, currentRes, tvShowName, CMediaSettings::Get().GetCurrentMadvrSettings());
+      }
+      else
+      {
+        dspdb.EraseVideoSettings(selected, selected, "");
+        dspdb.CreateVideoSettings(selected, selected, "", CMediaSettings::Get().GetCurrentMadvrSettings());
+      }
       dspdb.Close();
     }
   }

xbmc/xbmc

Conflicts:
	xbmc/Application.cpp
	xbmc/cores/DSPlayer/DSPlayer.cpp
	xbmc/video/dialogs/GUIDialogVideoSettings.cpp

xbmc/Application.cpp
@@ -3924,13 +3924,18 @@ void CApplication::LoadVideoSettings(const CFileItem& item)
     CFileItem item = CurrentFileItem();
     CStreamDetails streamDetails = item.GetVideoInfoTag()->m_streamDetails;
     int res = streamDetails.VideoDimsToResolution(streamDetails.GetVideoWidth(), streamDetails.GetVideoHeight());
-   
+    std::string tvShowName = item.GetVideoInfoTag()->m_strShowTitle;
+
     CLog::Log(LOGDEBUG, "Loading madvr settings for %s with resolution id: %i", item.GetPath().c_str(), res);
 
-    // Load stored settings if they exist, otherwise use default
+    // Load stored files settings
     if (!dsdbs.GetVideoSettings(item.GetPath().c_str(), CMediaSettings::GetInstance().GetCurrentMadvrSettings()))
-      if (!dsdbs.GetDefResMadvrSettings(res, CMediaSettings::GetInstance().GetCurrentMadvrSettings())) 
-        CMediaSettings::GetInstance().GetCurrentMadvrSettings() = CMediaSettings::GetInstance().GetDefaultMadvrSettings();
+      // if not present Load stored TvShowName settings
+      if (!dsdbs.GetDefResMadvrSettings(-1, tvShowName, CMediaSettings::GetInstance().GetCurrentMadvrSettings()))
+        // if not present Load stored Resolution settings
+        if (!dsdbs.GetDefResMadvrSettings(res, "", CMediaSettings::GetInstance().GetCurrentMadvrSettings())) 
+          // if not present Load default setting
+          CMediaSettings::GetInstance().GetCurrentMadvrSettings() = CMediaSettings::GetInstance().GetDefaultMadvrSettings();
 
     CMediaSettings::GetInstance().GetAtStartMadvrSettings() = CMediaSettings::GetInstance().GetCurrentMadvrSettings();
 

xbmc/cores/DSPlayer/DSPlayer.cpp
@@ -1062,8 +1062,10 @@ void CDSPlayer::SetMadvrResolution()
 
   CStreamDetails streamDetails;
   int res = streamDetails.VideoDimsToResolution(GetPictureWidth(), GetPictureHeight());
+  std::string str = g_application.CurrentFileItem().GetVideoInfoTag()->m_strShowTitle;
 
   CMediaSettings::GetInstance().GetCurrentMadvrSettings().m_Resolution = res;
+  CMediaSettings::GetInstance().GetCurrentMadvrSettings().m_TvShowName = str;
 }
 
 // Time is in millisecond

xbmc/video/dialogs/GUIDialogVideoSettings.cpp
@@ -396,10 +396,17 @@ void CGUIDialogVideoSettings::SaveChoice()
   if (!pDlg)
     return;
 
+  CFileItem &item = g_application.CurrentFileItem();
+  std::string tvShowName = CMediaSettings::GetInstance().GetCurrentMadvrSettings().m_TvShowName;
+  int currentRes = CMediaSettings::GetInstance().GetCurrentMadvrSettings().m_Resolution;
+
   pDlg->Add(g_localizeStrings.Get(70601).c_str());
   pDlg->Add(g_localizeStrings.Get(70602).c_str());
   pDlg->Add(g_localizeStrings.Get(70603).c_str());
   pDlg->Add(g_localizeStrings.Get(70604).c_str());
+  if (item.HasVideoInfoTag() && (item.GetVideoContentType() == VIDEODB_CONTENT_EPISODES || item.GetVideoContentType() == VIDEODB_CONTENT_TVSHOWS))
+    
+    pDlg->Add(StringUtils::Format(g_localizeStrings.Get(70605).c_str(), tvShowName.c_str()));
   pDlg->Add(g_localizeStrings.Get(12376).c_str());
 
   pDlg->SetHeading(70600);
@@ -420,16 +427,26 @@ void CGUIDialogVideoSettings::SaveChoice()
       label = 70603;
     if (selected == MADVR_RES_2160)
       label = 70604;
-    
-    if (CGUIDialogYesNo::ShowAndGetInput(label, 750, 0, 12377))
+    if (selected == MADVR_RES_EPISODES)
+      label = 70605;
+
+    if (CGUIDialogYesNo::ShowAndGetInput(StringUtils::Format(g_localizeStrings.Get(label).c_str(), tvShowName.c_str()), 750, 0, 12377))
     { // reset the settings
 
       CDSPlayerDatabase dspdb;
       if (!dspdb.Open())
         return;
 
-      dspdb.EraseVideoSettings(selected);
-      dspdb.CreateVideoSettings(selected, CMediaSettings::GetInstance().GetCurrentMadvrSettings());
+      if (selected == MADVR_RES_EPISODES)
+      {
+        dspdb.EraseVideoSettings(-1, currentRes, tvShowName);
+        dspdb.CreateVideoSettings(-1, currentRes, tvShowName, CMediaSettings::GetInstance().GetCurrentMadvrSettings());
+      }
+      else
+      {
+        dspdb.EraseVideoSettings(selected, selected, "");
+        dspdb.CreateVideoSettings(selected, selected, "", CMediaSettings::GetInstance().GetCurrentMadvrSettings());
+      }
       dspdb.Close();
     }
   }