2bea4c322dcc682f9ca3c2480acfc8c13b731fed
montellese <montellese@kodi.tv>
2015-09-10 19:25:02 +0200

    GroupUtils: remove unused GroupAndSort()

--- xbmc/utils/GroupUtils.h
+++ xbmc/utils/GroupUtils.h
@@ -38,5 +38,4 @@ class GroupUtils
 {
 public:
   static bool Group(GroupBy groupBy, const std::string &baseDir, const CFileItemList &items, CFileItemList &groupedItems, GroupAttribute groupAttributes = GroupAttributeNone);
-  static bool GroupAndSort(GroupBy groupBy, const std::string &baseDir, const CFileItemList &items, const SortDescription &sortDescription, CFileItemList &groupedItems, GroupAttribute groupAttributes = GroupAttributeNone);
 };
