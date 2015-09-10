6f313636d6dce70bb1f97d47322a9183269badde
Arne Morten Kvarving <cptspiff@gmail.com>
2015-09-10 15:21:56 +0200

    fixed: macro existence should not rely on config.h usage

--- xbmc/system.h
+++ xbmc/system.h
@@ -20,8 +20,11 @@
  *
  */
 
-#if defined(HAVE_CONFIG_H) && !defined(TARGET_WINDOWS)
+#if defined(HAVE_CONFIG_H)
 #include "config.h"
+#endif
+
+#if !defined(TARGET_WINDOWS)
 #define DECLARE_UNUSED(a,b) a __attribute__((unused)) b;
 #endif
 
