cc329c38183510da639eef5b796bc38853af3272
Pär Björklund <per.bjorklund@gmail.com>
2015-09-10 18:59:21 +0200

    Remove auto_aptr as std::unique_ptr handles this just fine by specifying [] in the template type

--- xbmc/utils/AutoPtrHandle.h
+++ xbmc/utils/AutoPtrHandle.h
@@ -65,46 +65,4 @@ protected:
   SOCKET m_hSocket;
 };
 
-/*
- * This template class is very similar to the standard "unique_ptr", but it is
- * used for *array* pointers rather than *object* pointers, i.e. the pointer
- * passed to it must have been allocated with "new[]", and "auto_aptr" will
- * delete it with "delete[]".
- *
- * Class released under GPL and was taken from:
- * http://userpage.fu-berlin.de/~mbayer/tools/html2text.html
- */
-template <class T>
-class auto_aptr
-{
-
-public:
-
-  // Constructor/copy/destroy
-
-  explicit auto_aptr(T *x = 0) : p(x) {}
-  auto_aptr(const auto_aptr<T> &x) : p(x.p) { ((auto_aptr<T> *) &x)->p = 0; }
-  auto_aptr<T>& operator=(const auto_aptr<T> &x)
-  { delete[] p; p = x.p; ((auto_aptr<T> *) &x)->p = 0; return *this; }
-  // Extension: "operator=(T *)" is identical to "auto_aptr::reset(T *)".
-  void operator=(T *x) { delete[] p; p = x; }
-  ~auto_aptr() { delete[] p; }
-
-  // Members
-
-  T &operator[](size_t idx) const { if (!p) abort(); return p[idx]; }
-T *get() const { return (T *) p; }
-  T *release() { T *tmp = p; p = 0; return tmp; }
-  void reset(T *x = 0) { delete[] p; p = x; }
-
-  // These would make a nice extension, but are not provided by many other
-  // implementations.
-  //operator const void *() const { return p; }
-  //int operator!() const { return p == 0; }
-
-private:
-  T *p;
-};
-
-
 }
