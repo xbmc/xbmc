1dc69a4b14c51056c91e4732a394271e0db77208
Garrett Brown <themagnificentmrb@gmail.com>
2015-09-15 09:49:16 -0700

    [python] Fix listing of protocol root
    This allows xbmcvfs.listdir() to list protocols that are valid with no
    hostname.

--- xbmc/interfaces/legacy/ModuleXbmcvfs.cpp
+++ xbmc/interfaces/legacy/ModuleXbmcvfs.cpp
@@ -24,6 +24,7 @@
 #include "filesystem/Directory.h"
 #include "utils/FileUtils.h"
 #include "utils/URIUtils.h"
+#include "URL.h"
 #include "Util.h"
 
 namespace XBMCAddon
@@ -100,6 +101,11 @@ namespace XBMCAddon
         {
           URIUtils::RemoveSlashAtEnd(itemPath);
           std::string strFileName = URIUtils::GetFileName(itemPath);
+          if (strFileName.empty())
+          {
+            CURL url(itemPath);
+            strFileName = url.GetHostName();
+          }
           ret.first().push_back(strFileName);
         }
         else // file
