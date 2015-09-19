xbmc/xbmc

commit 6e0fefdbfd60e5ff0f12e1711df011d8428629ca

[linux] - fix wrong local FileTime due to not honoring daylight savings time

xbmc/linux/XTimeUtils.cpp
@@ -83,7 +83,12 @@ BOOL FileTimeToLocalFileTime(const FILETIME* lpFileTime, LPFILETIME lpLocalFileT
   l.u.LowPart = lpFileTime->dwLowDateTime;
   l.u.HighPart = lpFileTime->dwHighDateTime;
 
-  l.QuadPart -= (uint64_t) timezone * 10000000;
+  time_t ft;
+  struct tm tm_ft;
+  FileTimeToTimeT(lpFileTime, &ft);
+  localtime_r(&ft, &tm_ft);
+
+  l.QuadPart += tm_ft.tm_gmtoff * 10000000;
 
   lpLocalFileTime->dwLowDateTime = l.u.LowPart;
   lpLocalFileTime->dwHighDateTime = l.u.HighPart;

commit 068cd636ff4d662f0203bc3bdd53b982ccf8639d

FIX: proper casting of file time TZ adjustment
Regression from
https://github.com/xbmc/xbmc/commit/6e0fefdbfd60e5ff0f12e1711df011d8428629ca

xbmc/linux/XTimeUtils.cpp
@@ -88,7 +88,7 @@ BOOL FileTimeToLocalFileTime(const FILETIME* lpFileTime, LPFILETIME lpLocalFileT
   FileTimeToTimeT(lpFileTime, &ft);
   localtime_r(&ft, &tm_ft);
 
-  l.QuadPart += tm_ft.tm_gmtoff * 10000000;
+  l.QuadPart += (ULONGLONG)tm_ft.tm_gmtoff * 10000000;
 
   lpLocalFileTime->dwLowDateTime = l.u.LowPart;
   lpLocalFileTime->dwHighDateTime = l.u.HighPart;