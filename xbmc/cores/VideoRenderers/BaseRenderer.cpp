aeb7c480efc2f0d8d6b17f36f1dd59661ef92824
fritsch <Peter.Fruehberger@gmail.com>
2015-09-19 09:37:41 +0200

    AdjustRefreshrate: Don't switch to bogus AVR introduced modes

--- xbmc/cores/VideoRenderers/BaseRenderer.cpp
+++ xbmc/cores/VideoRenderers/BaseRenderer.cpp
@@ -254,7 +254,8 @@ RESOLUTION CBaseRenderer::FindClosestResolution(float fps, float multiplier, RES
       // concerning dimension and refreshrate weight
       // skip lower resolutions
       if (((int) m_sourceWidth < orig.iScreenWidth) // orig res large enough
-      || (info.iScreenWidth < orig.iScreenWidth) // new res is smaller
+      || (info.iScreenWidth < orig.iScreenWidth) // new width would be smaller
+      || (info.iScreenHeight < orig.iScreenHeight) // new height would be smaller
       || (info.dwFlags & D3DPRESENTFLAG_MODEMASK) != (curr.dwFlags & D3DPRESENTFLAG_MODEMASK)) // don't switch to interlaced modes
       {
         continue;
