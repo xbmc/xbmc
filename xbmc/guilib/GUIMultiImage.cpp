xbmc/xbmc

commit cb2dd229ce5b6f479f9a6ebd51998831554d2ae2

[GuiLib] Member initialization issues, i.e.:          - Member variable 'Foo::bar' is not initialized in the constructor.          - Member variable 'Foo::bar' is in the wrong place in the initializer list.          - Variable 'foo' is assigned in constructor body.            Consider performing initialization in initialization list.

xbmc/guilib/GUIMultiImage.cpp
@@ -49,9 +49,8 @@ CGUIMultiImage::CGUIMultiImage(int parentID, int controlID, float posX, float po
 }
 
 CGUIMultiImage::CGUIMultiImage(const CGUIMultiImage &from)
-: CGUIControl(from), m_image(from.m_image)
+  : CGUIControl(from), m_texturePath(), m_imageTimer(), m_files(), m_image(from.m_image)
 {
-  m_texturePath = from.m_texturePath;
   m_timePerImage = from.m_timePerImage;
   m_timeToPauseAtEnd = from.m_timeToPauseAtEnd;
   m_randomized = from.m_randomized;

xbmc/guilib/GUIBorderedImage.cpp
@@ -29,9 +29,8 @@ CGUIBorderedImage::CGUIBorderedImage(int parentID, int controlID, float posX, fl
 }
 
 CGUIBorderedImage::CGUIBorderedImage(const CGUIBorderedImage &right)
-: CGUIImage(right), m_borderImage(right.m_borderImage)
+: CGUIImage(right), m_borderImage(right.m_borderImage), m_borderSize(right.m_borderSize)
 {
-  m_borderSize = right.m_borderSize;
   ControlType = GUICONTROL_BORDEREDIMAGE;
 }
 

xbmc/guilib/GUICheckMarkControl.cpp
@@ -29,8 +29,8 @@ CGUICheckMarkControl::CGUICheckMarkControl(int parentID, int controlID, float po
     , m_imgCheckMark(posX, posY, checkWidth, checkHeight, textureCheckMark)
     , m_imgCheckMarkNoFocus(posX, posY, checkWidth, checkHeight, textureCheckMarkNF)
     , m_label(posX, posY, width, height, labelInfo)
+    , m_strLabel("")
 {
-  m_strLabel = "";
   m_bSelected = false;
   m_label.GetLabelInfo().align |= XBFONT_CENTER_Y;
   ControlType = GUICONTROL_CHECKMARK;

xbmc/guilib/GUIControl.cpp
@@ -30,7 +30,8 @@
 
 using namespace std;
 
-CGUIControl::CGUIControl()
+CGUIControl::CGUIControl() :
+  m_diffuseColor(0xffffffff)
 {
   m_hasProcessed = false;
   m_bHasFocus = false;
@@ -42,7 +43,6 @@ CGUIControl::CGUIControl()
   m_visibleCondition = 0;
   m_enableCondition = 0;
   m_enabled = true;
-  m_diffuseColor = 0xffffffff;
   m_posX = 0;
   m_posY = 0;
   m_width = 0;
@@ -58,7 +58,8 @@ CGUIControl::CGUIControl()
 }
 
 CGUIControl::CGUIControl(int parentID, int controlID, float posX, float posY, float width, float height)
-: m_hitRect(posX, posY, posX + width, posY + height)
+: m_hitRect(posX, posY, posX + width, posY + height),
+  m_diffuseColor(0xffffffff)
 {
   m_posX = posX;
   m_posY = posY;
@@ -69,7 +70,6 @@ CGUIControl::CGUIControl(int parentID, int controlID, float posX, float posY, fl
   m_parentID = parentID;
   m_visible = VISIBLE;
   m_visibleFromSkinCondition = true;
-  m_diffuseColor = 0xffffffff;
   m_forceHidden = false;
   m_visibleCondition = 0;
   m_enableCondition = 0;

xbmc/guilib/GUIFadeLabelControl.cpp
@@ -23,14 +23,13 @@
 using namespace std;
 
 CGUIFadeLabelControl::CGUIFadeLabelControl(int parentID, int controlID, float posX, float posY, float width, float height, const CLabelInfo& labelInfo, bool scrollOut, unsigned int timeToDelayAtEnd, bool resetOnLabelChange)
-    : CGUIControl(parentID, controlID, posX, posY, width, height), m_scrollInfo(50, labelInfo.offsetX, labelInfo.scrollSpeed)
+    : CGUIControl(parentID, controlID, posX, posY, width, height), m_label(labelInfo), m_scrollInfo(50, labelInfo.offsetX, labelInfo.scrollSpeed)
     , m_textLayout(labelInfo.font, false)
+    , m_fadeAnim(CAnimation::CreateFader(100, 0, timeToDelayAtEnd, 200))
 {
-  m_label = labelInfo;
   m_currentLabel = 0;
   ControlType = GUICONTROL_FADELABEL;
   m_scrollOut = scrollOut;
-  m_fadeAnim = CAnimation::CreateFader(100, 0, timeToDelayAtEnd, 200);
   m_fadeAnim.ApplyAnimation();
   m_lastLabel = -1;
   m_scrollSpeed = labelInfo.scrollSpeed;  // save it for later
@@ -39,14 +38,13 @@ CGUIFadeLabelControl::CGUIFadeLabelControl(int parentID, int controlID, float po
 }
 
 CGUIFadeLabelControl::CGUIFadeLabelControl(const CGUIFadeLabelControl &from)
-: CGUIControl(from), m_infoLabels(from.m_infoLabels), m_scrollInfo(from.m_scrollInfo), m_textLayout(from.m_textLayout)
+: CGUIControl(from), m_infoLabels(from.m_infoLabels), m_label(from.m_label), m_scrollInfo(from.m_scrollInfo), m_textLayout(from.m_textLayout), 
+  m_fadeAnim(from.m_fadeAnim)
 {
-  m_label = from.m_label;
   m_scrollOut = from.m_scrollOut;
   m_scrollSpeed = from.m_scrollSpeed;
   m_resetOnLabelChange = from.m_resetOnLabelChange;
 
-  m_fadeAnim = from.m_fadeAnim;
   m_fadeAnim.ApplyAnimation();
   m_currentLabel = 0;
   m_lastLabel = -1;

xbmc/guilib/GUIImage.cpp
@@ -37,9 +37,14 @@ CGUIImage::CGUIImage(int parentID, int controlID, float posX, float posY, float 
 }
 
 CGUIImage::CGUIImage(const CGUIImage &left)
-    : CGUIControl(left), m_texture(left.m_texture)
+  : CGUIControl(left), 
+  m_image(left.m_image),
+  m_info(left.m_info),
+  m_texture(left.m_texture),
+  m_fadingTextures(),
+  m_currentTexture(),
+  m_currentFallback()
 {
-  m_info = left.m_info;
   m_crossFadeTime = left.m_crossFadeTime;
   // defaults
   m_currentFadeTime = 0;

xbmc/guilib/GUIListLabel.cpp
@@ -24,8 +24,8 @@
 CGUIListLabel::CGUIListLabel(int parentID, int controlID, float posX, float posY, float width, float height, const CLabelInfo& labelInfo, const CGUIInfoLabel &info, bool alwaysScroll)
     : CGUIControl(parentID, controlID, posX, posY, width, height)
     , m_label(posX, posY, width, height, labelInfo, alwaysScroll ? CGUILabel::OVER_FLOW_SCROLL : CGUILabel::OVER_FLOW_TRUNCATE)
+    , m_info(info)
 {
-  m_info = info;
   m_alwaysScroll = alwaysScroll;
   // TODO: Remove this "correction"
   if (labelInfo.align & XBFONT_RIGHT)

xbmc/guilib/GUIMultiSelectText.cpp
@@ -39,10 +39,10 @@ CGUIMultiSelectTextControl::CSelectableString::CSelectableString(CGUIFont *font,
 
 CGUIMultiSelectTextControl::CGUIMultiSelectTextControl(int parentID, int controlID, float posX, float posY, float width, float height, const CTextureInfo& textureFocus, const CTextureInfo& textureNoFocus, const CLabelInfo& labelInfo, const CGUIInfoLabel &content)
     : CGUIControl(parentID, controlID, posX, posY, width, height)
+    , m_label(labelInfo)
+    , m_info(content)
     , m_button(parentID, controlID, posX, posY, width, height, textureFocus, textureNoFocus, labelInfo)
 {
-  m_info = content;
-  m_label = labelInfo;
   m_selectedItem = 0;
   m_offset = 0;
   m_totalWidth = 0;

commit 7b6f18f61bf97485c0aa24798ae4a437168fd446

[fix][gui] Fix multi images in grouplists/layouts. This fixes a regression introduced with cb2dd229ce5b6f479f9a6ebd51998831554d2ae2 .

xbmc/guilib/GUIMultiImage.cpp
@@ -49,7 +49,7 @@ CGUIMultiImage::CGUIMultiImage(int parentID, int controlID, float posX, float po
 }
 
 CGUIMultiImage::CGUIMultiImage(const CGUIMultiImage &from)
-  : CGUIControl(from), m_texturePath(), m_imageTimer(), m_files(), m_image(from.m_image)
+  : CGUIControl(from), m_texturePath(from.m_texturePath), m_imageTimer(), m_files(), m_image(from.m_image)
 {
   m_timePerImage = from.m_timePerImage;
   m_timeToPauseAtEnd = from.m_timeToPauseAtEnd;