edcc3794d192a82496e2d2e5ff1cef8c7b1e36a2
Rainer Hochecker <fernetmenta@online.de>
2015-10-11 13:37:48 +0200

    [pvr] reset changed flag for channels after storing them to db

--- xbmc/pvr/channels/PVRChannel.h
+++ xbmc/pvr/channels/PVRChannel.h
@@ -250,6 +250,11 @@ namespace PVR
     bool IsEmpty() const;
 
     bool IsChanged() const;
+
+    /*!
+     * @brief reset changed flag after persist
+     */
+    void Persisted();
     //@}
 
     /*! @name Client related channel methods
