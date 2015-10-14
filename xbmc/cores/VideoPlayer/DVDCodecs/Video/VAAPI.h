e927a934ed7f954b38dd27cdc5de3fe925373c4c
Rainer Hochecker <fernetmenta@online.de>
2015-10-14 20:15:46 +0200

    vaapi: cope with comtinuous get_buffer errors

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/VAAPI.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/VAAPI.h
@@ -457,7 +457,7 @@ protected:
   CVideoSurfaces m_videoSurfaces;
   vaapi_context m_hwContext;
   AVCodecContext* m_avctx;
-  bool m_getBufferError;
+  int m_getBufferError;
 
   COutput m_vaapiOutput;
   CVaapiBufferStats m_bufferStats;
