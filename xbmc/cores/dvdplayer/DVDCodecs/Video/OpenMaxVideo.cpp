xbmc/xbmc

commit 9e9396b073ba973b2847896a11244a9f37ecbaff

Clean up ApplicationMessenger
ApplicationMessenger had become a place to stick all kinds of convenience
methods and depending on just about everything. This PR cleans this up
and returns it back to a messaging class.

Removed the bool parameters and used naming from Win32 API with
SendMsg for blocking and PostMsg for non-blocking messaging.

xbmc/cores/dvdplayer/DVDCodecs/Video/OpenMaxVideo.cpp
@@ -33,13 +33,15 @@
 #include "utils/log.h"
 #include "utils/TimeUtils.h"
 #include "guilib/GUIWindowManager.h"
-#include "ApplicationMessenger.h"
+#include "messaging/ApplicationMessenger.h"
 #include "Application.h"
 
 #include <OMX_Core.h>
 #include <OMX_Component.h>
 #include <OMX_Index.h>
 #include <OMX_Image.h>
+
+using namespace KODI::MESSAGING;
 
 #if 1
 //#define OMX_DEBUG_EMPTYBUFFERDONE
@@ -749,11 +751,10 @@ OMX_ERRORTYPE COpenMaxVideo::AllocOMXOutputBuffers(void)
     callbackData.callback = &CallbackAllocOMXEGLTextures;
     callbackData.userptr = (void *)this;
 
-    ThreadMessage tMsg;
     tMsg.dwMessage = TMSG_CALLBACK;
     tMsg.lpVoid = (void*)&callbackData;
 
-    CApplicationMessenger::Get().SendMessage(tMsg, true);
+    CApplicationMessenger::Get().SendMsg(TMSG_CALLBACK, -1, -1 static_cast<void*>(&callbackData));
 
     omx_err = OMX_ErrorNone;
   }
@@ -1225,12 +1226,8 @@ void OpenMaxVideoBuffer::ReleaseTexture()
     deleteInfo->callback.callback = &OpenMaxDeleteTextures;
     deleteInfo->callback.userptr = (void *)deleteInfo;
 
-    ThreadMessage tMsg;
-    tMsg.dwMessage = TMSG_CALLBACK;
-    tMsg.lpVoid = (void*)&deleteInfo->callback;
-
     // HACK, this should be synchronous, but it's not possible since Stop blocks the GUI thread.
-    CApplicationMessenger::Get().SendMessage(tMsg, false);
+    CApplicationMessenger::Get().PostMsg(TMSG_CALLBACK, -1, -1 static_cast<void*>(deleteInfo));
   }
 
 }

commit 3f015da9fb6c09eddce6ca1c902db7e99d136d46

Fix regressions introduced by refactoring in commit 9e9396b073ba973b2847896a11244a9f37ecbaff

xbmc/cores/dvdplayer/DVDCodecs/Video/OpenMaxVideo.cpp
@@ -751,10 +751,7 @@ OMX_ERRORTYPE COpenMaxVideo::AllocOMXOutputBuffers(void)
     callbackData.callback = &CallbackAllocOMXEGLTextures;
     callbackData.userptr = (void *)this;
 
-    tMsg.dwMessage = TMSG_CALLBACK;
-    tMsg.lpVoid = (void*)&callbackData;
-
-    CApplicationMessenger::GetInstance().SendMsg(TMSG_CALLBACK, -1, -1 static_cast<void*>(&callbackData));
+    CApplicationMessenger::GetInstance().SendMsg(TMSG_CALLBACK, -1, -1, static_cast<void*>(&callbackData));
 
     omx_err = OMX_ErrorNone;
   }
@@ -1227,7 +1224,7 @@ void OpenMaxVideoBuffer::ReleaseTexture()
     deleteInfo->callback.userptr = (void *)deleteInfo;
 
     // HACK, this should be synchronous, but it's not possible since Stop blocks the GUI thread.
-    CApplicationMessenger::GetInstance().PostMsg(TMSG_CALLBACK, -1, -1 static_cast<void*>(deleteInfo));
+    CApplicationMessenger::GetInstance().PostMsg(TMSG_CALLBACK, -1, -1, static_cast<void*>(deleteInfo));
   }
 
 }