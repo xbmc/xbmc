dc89d422aacb0118f69d25aac485d4f1cedee3de
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2015-11-05 22:58:16 +0100

    [PVR] Remove redundant method CGUIWindowPVRTimers::ShowTimerSettings.

--- xbmc/pvr/windows/GUIWindowPVRTimers.cpp
+++ xbmc/pvr/windows/GUIWindowPVRTimers.cpp
@@ -408,28 +408,3 @@ bool CGUIWindowPVRTimers::ShowNewTimerDialog(void)
 
   return bReturn;
 }
-
-bool CGUIWindowPVRTimers::ShowTimerSettings(CFileItem *item)
-{
-  /* Check item is TV timer information tag */
-  if (!item->IsPVRTimer())
-  {
-    CLog::Log(LOGERROR, "CGUIWindowPVRTimers: Can't open timer settings dialog, no timer info tag!");
-    return false;
-  }
-
-  /* Load timer settings dialog */
-  CGUIDialogPVRTimerSettings* pDlgInfo = (CGUIDialogPVRTimerSettings*)g_windowManager.GetWindow(WINDOW_DIALOG_PVR_TIMER_SETTING);
-
-  if (!pDlgInfo)
-    return false;
-
-  /* inform dialog about the file item */
-  pDlgInfo->SetTimer(item);
-
-  /* Open dialog window */
-  pDlgInfo->Open();
-
-  /* Get modify flag from window and return it to caller */
-  return pDlgInfo->IsConfirmed();
-}
