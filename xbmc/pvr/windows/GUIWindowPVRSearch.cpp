9deb6333008035c1bc271b66f0313fdc8f6583e7
metaron <andrew@thomasinfletcher.com>
2015-11-23 23:16:52 +0000

    [PVR] Allow timer creation for EPG in recent past

--- xbmc/pvr/windows/GUIWindowPVRSearch.cpp
+++ xbmc/pvr/windows/GUIWindowPVRSearch.cpp
@@ -67,9 +67,10 @@ void CGUIWindowPVRSearch::GetContextButtons(int itemNumber, CContextButtons &but
           buttons.Add(CONTEXT_BUTTON_DELETE_TIMER, 19060);  /* Delete timer */
       }
     }
-    else if (epg->EndAsLocalTime() > CDateTime::GetCurrentDateTime())
+    else
     {
-      buttons.Add(CONTEXT_BUTTON_START_RECORD, 264);      /* Record */
+      if (epg->EndAsLocalTime() > CDateTime::GetCurrentDateTime())
+        buttons.Add(CONTEXT_BUTTON_START_RECORD, 264);      /* Record */
       buttons.Add(CONTEXT_BUTTON_ADD_TIMER, 19061);       /* Add timer */
     }
 
