6a18c8fad6b0c6c842be2869e01500ec492c3831
Chris "Koying" Browet <cbro@semperpax.com>
2015-12-20 16:39:12 +0100

    ADD: [droid] API (>= 21) based passthrough and FLOAT

--- xbmc/platform/android/jni/AudioFormat.h
+++ xbmc/platform/android/jni/AudioFormat.h
@@ -28,6 +28,12 @@ class CJNIAudioFormat
     static void PopulateStaticFields();
 
     static int ENCODING_PCM_16BIT;
+    static int ENCODING_PCM_FLOAT;
+    static int ENCODING_AC3;
+    static int ENCODING_E_AC3;
+    static int ENCODING_DTS;
+    static int ENCODING_DTS_HD;
+    static int ENCODING_DOLBY_TRUEHD;
 
     static int CHANNEL_OUT_STEREO;
     static int CHANNEL_OUT_5POINT1;
