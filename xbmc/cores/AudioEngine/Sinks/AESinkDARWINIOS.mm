c4cde2fb2f11f5502ad263c60c785884677c5f86
Memphiz <memphis@machzwo.de>
2015-12-21 10:44:27 +0100

    [ios] - renamed AESinkDARWINIOS.cpp to .mm

--- xbmc/cores/AudioEngine/Sinks/AESinkDARWINIOS.mm
+++ xbmc/cores/AudioEngine/Sinks/AESinkDARWINIOS.mm
@@ -19,6 +19,8 @@
  */
 
 #include "cores/AudioEngine/Sinks/AESinkDARWINIOS.h"
+
+#define BOOL XBMC_BOOL
 #include "cores/AudioEngine/Utils/AEUtil.h"
 #include "cores/AudioEngine/Utils/AERingBuffer.h"
 #include "cores/AudioEngine/Sinks/osx/CoreAudioHelpers.h"
@@ -27,6 +29,7 @@
 #include "utils/StringUtils.h"
 #include "threads/Condition.h"
 #include "windowing/WindowingFactory.h"
+#undef BOOL
 
 #include <sstream>
 #include <AudioToolbox/AudioToolbox.h>
