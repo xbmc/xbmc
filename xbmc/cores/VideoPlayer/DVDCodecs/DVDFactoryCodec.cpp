9162742905e18bc81e9c605e93dd51d364c9a926
xbmc <fernetmenta@online.de>
2015-12-29 09:33:25 +0100

    VideoPlayer: cleanup codec factory

--- xbmc/cores/VideoPlayer/DVDCodecs/DVDFactoryCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/DVDFactoryCodec.cpp
@@ -82,7 +82,7 @@ CDVDVideoCodec* CDVDFactoryCodec::OpenCodec(CDVDVideoCodec* pCodec, CDVDStreamIn
   {
     CLog::Log(LOGERROR, "FactoryCodec - Video: Failed with exception");
   }
-  return NULL;
+  return nullptr;
 }
 
 CDVDAudioCodec* CDVDFactoryCodec::OpenCodec(CDVDAudioCodec* pCodec, CDVDStreamInfo &hints, CDVDCodecOptions &options )
@@ -104,7 +104,7 @@ CDVDAudioCodec* CDVDFactoryCodec::OpenCodec(CDVDAudioCodec* pCodec, CDVDStreamIn
   {
     CLog::Log(LOGERROR, "FactoryCodec - Audio: Failed with exception");
   }
-  return NULL;
+  return nullptr;
 }
 
 CDVDOverlayCodec* CDVDFactoryCodec::OpenCodec(CDVDOverlayCodec* pCodec, CDVDStreamInfo &hints, CDVDCodecOptions &options )
@@ -126,192 +126,45 @@ CDVDOverlayCodec* CDVDFactoryCodec::OpenCodec(CDVDOverlayCodec* pCodec, CDVDStre
   {
     CLog::Log(LOGERROR, "FactoryCodec - Audio: Failed with exception");
   }
-  return NULL;
+  return nullptr;
 }
 
 
 CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, const CRenderInfo &info, IVPClockCallback* clock)
 {
-  CDVDVideoCodec* pCodec = NULL;
+  CDVDVideoCodec* pCodec = nullptr;
   CDVDCodecOptions options;
 
-  if(info.formats.empty())
+  if (info.formats.empty())
     options.m_formats.push_back(RENDER_FMT_YUV420P);
   else
     options.m_formats = info.formats;
 
   options.m_opaque_pointer = info.opaque_pointer;
 
-  //when support for a hardware decoder is not compiled in
-  //only print it if it's actually available on the platform
-  std::string hwSupport;
-#if defined(TARGET_DARWIN_OSX)
-  hwSupport += "VDADecoder:yes ";
-#endif
-#if defined(HAVE_VIDEOTOOLBOXDECODER) && defined(TARGET_DARWIN)
-  hwSupport += "VideoToolBoxDecoder:yes ";
-#elif defined(TARGET_DARWIN)
-  hwSupport += "VideoToolBoxDecoder:no ";
-#endif
-#if defined(HAS_LIBAMCODEC)
-  hwSupport += "AMCodec:yes ";
-#else
-  hwSupport += "AMCodec:no ";
-#endif
-#if defined(TARGET_ANDROID)
-  hwSupport += "MediaCodec:yes ";
-#else
-  hwSupport += "MediaCodec:no ";
-#endif
-#if defined(HAVE_LIBOPENMAX)
-  hwSupport += "OpenMax:yes ";
-#elif defined(TARGET_POSIX)
-  hwSupport += "OpenMax:no ";
-#endif
-#if defined(HAVE_LIBVDPAU) && defined(TARGET_POSIX)
-  hwSupport += "VDPAU:yes ";
-#elif defined(TARGET_POSIX) && !defined(TARGET_DARWIN)
-  hwSupport += "VDPAU:no ";
-#endif
-#if defined(TARGET_WINDOWS) && defined(HAS_DX)
-  hwSupport += "DXVA:yes ";
-#elif defined(TARGET_WINDOWS)
-  hwSupport += "DXVA:no ";
-#endif
-#if defined(HAVE_LIBVA) && defined(TARGET_POSIX)
-  hwSupport += "VAAPI:yes ";
-#elif defined(TARGET_POSIX) && !defined(TARGET_DARWIN)
-  hwSupport += "VAAPI:no ";
-#endif
-#if defined(HAS_IMXVPU)
-  hwSupport += "iMXVPU:yes ";
-#else
-  hwSupport += "iMXVPU:no ";
-#endif
-#if defined(HAS_MMAL)
-  hwSupport += "MMAL:yes ";
-#else
-  hwSupport += "MMAL:no ";
-#endif
-  CLog::Log(LOGDEBUG, "CDVDFactoryCodec: compiled in hardware support: %s", hwSupport.c_str());
-
-#if defined(HAS_LIBAMCODEC)
-  // amcodec can handle dvd playback.
-  if (!hint.software && CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEAMCODEC))
-  {
-    switch(hint.codec)
-    {
-      case AV_CODEC_ID_MPEG4:
-      case AV_CODEC_ID_MSMPEG4V2:
-      case AV_CODEC_ID_MSMPEG4V3:
-        // Avoid h/w decoder for SD; Those files might use features
-        // not supported and can easily be soft-decoded
-        if (hint.width <= 800)
-          break;
-      default:
-        if ( (pCodec = OpenCodec(new CDVDVideoCodecAmlogic(clock), hint, options)) ) return pCodec;
-    }
-  }
-#endif
-
-#if defined(HAS_IMXVPU)
   if (!hint.software)
   {
-    if ( (pCodec = OpenCodec(new CDVDVideoCodecIMX(), hint, options)) ) return pCodec;
+#if defined(HAS_IMXVPU)
+    pCodec = OpenCodec(new CDVDVideoCodecIMX(), hint, options);
+#elif defined(HAS_LIBAMCODEC)
+    pCodec = OpenCodec(new CDVDVideoCodecAmlogic(clock), hint, options);
+#elif defined(HAVE_VIDEOTOOLBOXDECODER)
+    pCodec = OpenCodec(new CDVDVideoCodecVideoToolBox(), hint, options);
+#elif defined(TARGET_ANDROID)
+    pCodec = OpenCodec(new CDVDVideoCodecAndroidMediaCodec(), hint, options);
+#elif defined(HAVE_LIBOPENMAX)
+    pCodec = OpenCodec(new CDVDVideoCodecOpenMax(), hint, options);
+#elif defined(HAS_MMAL)
+    pCodec = OpenCodec(new CMMALVideo(), hint, options);
+#endif
+    if (pCodec)
+      return pCodec;
   }
-#endif
-
-#if defined(TARGET_DARWIN_OSX)
-  if (!hint.software && CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEVDA) && !g_advancedSettings.m_useFfmpegVda)
-  {
-    if (hint.codec == AV_CODEC_ID_H264 && !hint.ptsinvalid)
-    {
-      if ( (pCodec = OpenCodec(new CDVDVideoCodecVDA(), hint, options)) ) return pCodec;
-    }
-  }
-#endif
-
-#if defined(HAVE_VIDEOTOOLBOXDECODER)
-  if (!hint.software && CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEVIDEOTOOLBOX))
-  {
-    if (g_sysinfo.HasVideoToolBoxDecoder())
-    {
-      switch(hint.codec)
-      {
-        case AV_CODEC_ID_H264:
-          if (hint.codec == AV_CODEC_ID_H264 && hint.ptsinvalid)
-            break;
-          if ( (pCodec = OpenCodec(new CDVDVideoCodecVideoToolBox(), hint, options)) ) return pCodec;
-          break;
-        default:
-          break;
-      }
-    }
-  }
-#endif
-
-#if defined(TARGET_ANDROID)
-  if (!hint.software && CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEMEDIACODECSURFACE))
-  {
-    switch(hint.codec)
-    {
-      case AV_CODEC_ID_MPEG4:
-      case AV_CODEC_ID_MSMPEG4V2:
-      case AV_CODEC_ID_MSMPEG4V3:
-        // Avoid h/w decoder for SD; Those files might use features
-        // not supported and can easily be soft-decoded
-        if (hint.width <= 800)
-          break;
-      default:
-        CLog::Log(LOGINFO, "MediaCodec (Surface) Video Decoder...");
-        if ( (pCodec = OpenCodec(new CDVDVideoCodecAndroidMediaCodec(true), hint, options)) ) return pCodec;
-    }
-  }
-  if (!hint.software && CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEMEDIACODEC))
-  {
-    switch(hint.codec)
-    {
-      case AV_CODEC_ID_MPEG4:
-      case AV_CODEC_ID_MSMPEG4V2:
-      case AV_CODEC_ID_MSMPEG4V3:
-        // Avoid h/w decoder for SD; Those files might use features
-        // not supported and can easily be soft-decoded
-        if (hint.width <= 800)
-          break;
-      default:
-        CLog::Log(LOGINFO, "MediaCodec Video Decoder...");
-        if ( (pCodec = OpenCodec(new CDVDVideoCodecAndroidMediaCodec(false), hint, options)) ) return pCodec;
-    }
-  }
-#endif
-
-#if defined(HAVE_LIBOPENMAX)
-    if (CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEOMX) && !hint.software )
-    {
-      if (hint.codec == AV_CODEC_ID_H264 || hint.codec == AV_CODEC_ID_MPEG2VIDEO || hint.codec == AV_CODEC_ID_VC1)
-      {
-        if ( (pCodec = OpenCodec(new CDVDVideoCodecOpenMax(), hint, options)) ) return pCodec;
-      }
-    }
-#endif
-
-#if defined(HAS_MMAL)
-    if (CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_USEMMAL) && !hint.software )
-    {
-      if (hint.codec == AV_CODEC_ID_H264 || hint.codec == AV_CODEC_ID_H263 || hint.codec == AV_CODEC_ID_MPEG4 ||
-          hint.codec == AV_CODEC_ID_MPEG1VIDEO || hint.codec == AV_CODEC_ID_MPEG2VIDEO ||
-          hint.codec == AV_CODEC_ID_VP6 || hint.codec == AV_CODEC_ID_VP6F || hint.codec == AV_CODEC_ID_VP6A || hint.codec == AV_CODEC_ID_VP8 ||
-          hint.codec == AV_CODEC_ID_THEORA || hint.codec == AV_CODEC_ID_MJPEG || hint.codec == AV_CODEC_ID_MJPEGB || hint.codec == AV_CODEC_ID_VC1 || hint.codec == AV_CODEC_ID_WMV3)
-      {
-        if ( (pCodec = OpenCodec(new CMMALVideo(), hint, options)) ) return pCodec;
-      }
-    }
-#endif
 
   // try to decide if we want to try halfres decoding
 #if !defined(TARGET_POSIX) && !defined(TARGET_WINDOWS)
   float pixelrate = (float)hint.width*hint.height*hint.fpsrate/hint.fpsscale;
-  if( pixelrate > 1400.0f*720.0f*30.0f )
+  if (pixelrate > 1400.0f*720.0f*30.0f)
   {
     CLog::Log(LOGINFO, "CDVDFactoryCodec - High video resolution detected %dx%d, trying half resolution decoding ", hint.width, hint.height);
     options.m_keys.push_back(CDVDCodecOption("lowres","1"));
@@ -320,9 +173,11 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, const C
 
   std::string value = StringUtils::Format("%d", info.max_buffer_size);
   options.m_keys.push_back(CDVDCodecOption("surfaces", value));
-  if( (pCodec = OpenCodec(new CDVDVideoCodecFFmpeg(), hint, options)) ) return pCodec;
+  pCodec = OpenCodec(new CDVDVideoCodecFFmpeg(), hint, options);
+  if (pCodec)
+    return pCodec;
 
-  return NULL;
+  return nullptr;;
 }
 
 CDVDAudioCodec* CDVDFactoryCodec::CreateAudioCodec(CDVDStreamInfo &hint, bool allowpassthrough, bool allowdtshddecode)
@@ -345,7 +200,7 @@ CDVDAudioCodec* CDVDFactoryCodec::CreateAudioCodec(CDVDStreamInfo &hint, bool al
   if (pCodec)
     return pCodec;
 
-  return NULL;
+  return nullptr;
 }
 
 CDVDOverlayCodec* CDVDFactoryCodec::CreateOverlayCodec( CDVDStreamInfo &hint )
@@ -358,28 +213,33 @@ CDVDOverlayCodec* CDVDFactoryCodec::CreateOverlayCodec( CDVDStreamInfo &hint )
     case AV_CODEC_ID_TEXT:
     case AV_CODEC_ID_SUBRIP:
       pCodec = OpenCodec(new CDVDOverlayCodecText(), hint, options);
-      if( pCodec ) return pCodec;
+      if (pCodec)
+        return pCodec;
       break;
 
     case AV_CODEC_ID_SSA:
     case AV_CODEC_ID_ASS:
       pCodec = OpenCodec(new CDVDOverlayCodecSSA(), hint, options);
-      if( pCodec ) return pCodec;
+      if (pCodec)
+        return pCodec;
 
       pCodec = OpenCodec(new CDVDOverlayCodecText(), hint, options);
-      if( pCodec ) return pCodec;
+      if (pCodec)
+        return pCodec;
       break;
 
     case AV_CODEC_ID_MOV_TEXT:
       pCodec = OpenCodec(new CDVDOverlayCodecTX3G(), hint, options);
-      if( pCodec ) return pCodec;
+      if (pCodec)
+        return pCodec;
       break;
 
     default:
       pCodec = OpenCodec(new CDVDOverlayCodecFFmpeg(), hint, options);
-      if( pCodec ) return pCodec;
+      if (pCodec)
+        return pCodec;
       break;
   }
 
-  return NULL;
+  return nullptr;
 }
