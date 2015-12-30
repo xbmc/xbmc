6b423ce6e816247195c7ca863a31fb523a7e9556
arnova <arnova@void.org>
2015-12-30 20:01:47 +0100

    fixed: Our dir cache should always ignore url (protocol) options (fixes #16469)

--- xbmc/URL.h
+++ xbmc/URL.h
@@ -67,6 +67,7 @@ public:
   char GetDirectorySeparator() const;
 
   std::string Get() const;
+  std::string GetWithoutOptions() const;
   std::string GetWithoutUserDetails(bool redact = false) const;
   std::string GetWithoutFilename() const;
   std::string GetRedacted() const;
