df06d19032dcd66b3aaab67c2caf0cb7273fadfc
stevegal <stephenjgalbraith@gmail.com>
2016-01-06 23:29:52 +0000

    mnmore debug attempts on add timer

--- xbmc/interfaces/json-rpc/PVROperations.cpp
+++ xbmc/interfaces/json-rpc/PVROperations.cpp
@@ -339,44 +339,63 @@ JSONRPC_STATUS CPVROperations::GetTimerDetails(const std::string &method, ITrans
 
 JSONRPC_STATUS CPVROperations::AddTimer(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
 {
+	  if (!g_PVRManager.IsStarted())
+	    return FailedToExecute;
+
+	  EpgSearchFilter filter;
+	  filter.Reset();
+	  filter.m_iUniqueBroadcastId = parameterObject["broadcastid"].asUnsignedInteger();
+
+	  CFileItemList broadcasts;
+	  int resultSize = g_EpgContainer.GetEPGSearch(broadcasts, filter);
+
+	  if (resultSize <= 0)
+	    return InvalidParams;
+	  else if (resultSize > 1)
+	    return InternalError;
+
+	  CFileItemPtr broadcast = broadcasts.Get(0);
+	  HandleFileItem("broadcastid", false, "broadcastdetails", broadcast, parameterObject, parameterObject["properties"], result, false);
+
+	  return OK;
   //if (!g_PVRManager.IsStarted())
   //  return FailedToExecute;
 
-  EpgSearchFilter filter;
-  filter.Reset();
-  filter.m_iUniqueBroadcastId = parameterObject["broadcastid"].asUnsignedInteger();
-
-  CFileItemList broadcasts;
-  int resultSize = g_EpgContainer.GetEPGSearch(broadcasts, filter);
-
-  if (resultSize <= 0)
-    return InvalidRequest;
-  else if (resultSize > 1)
-    return InternalError;
-
-  CFileItemPtr broadcast = broadcasts.Get(0);
-
-  CEpgInfoTagPtr epgTag = broadcast->GetEPGInfoTag();
-  if (!epgTag)
-      return InternalError;
-    
-  bool repeating =(bool)parameterObject["repeating"].asBoolean(false);
-
-  if (epgTag->Timer() == NULL)
-  {
-      CPVRTimerInfoTagPtr newTimer = CPVRTimerInfoTag::CreateFromEpg(epgTag,repeating);
-      if (newTimer)
-      {
-          CPVRTimers* timers = g_PVRTimers;
-          bool added = timers->AddTimer(newTimer);
-          if (added) {
-             HandleFileItem("timerid", false, "timerdetails", CFileItemPtr(new CFileItem(newTimer)), parameterObject, parameterObject["properties"], result, false);
-             return OK;
-          }
-      }
-      return FailedToExecute;
-  }
-  return FailedToExecute;
+//  EpgSearchFilter filter;
+//  filter.Reset();
+//  filter.m_iUniqueBroadcastId = parameterObject["broadcastid"].asUnsignedInteger();
+//
+//  CFileItemList broadcasts;
+//  int resultSize = g_EpgContainer.GetEPGSearch(broadcasts, filter);
+//
+//  if (resultSize <= 0)
+//    return InvalidRequest;
+//  else if (resultSize > 1)
+//    return InternalError;
+//
+//  CFileItemPtr broadcast = broadcasts.Get(0);
+//
+//  CEpgInfoTagPtr epgTag = broadcast->GetEPGInfoTag();
+//  if (!epgTag)
+//      return InternalError;
+//
+//  bool repeating =(bool)parameterObject["repeating"].asBoolean(false);
+//
+//  if (epgTag->Timer() == NULL)
+//  {
+//      CPVRTimerInfoTagPtr newTimer = CPVRTimerInfoTag::CreateFromEpg(epgTag,repeating);
+//      if (newTimer)
+//      {
+//          CPVRTimers* timers = g_PVRTimers;
+//          bool added = timers->AddTimer(newTimer);
+//          if (added) {
+//             HandleFileItem("timerid", false, "timerdetails", CFileItemPtr(new CFileItem(newTimer)), parameterObject, parameterObject["properties"], result, false);
+//             return OK;
+//          }
+//      }
+//      return ParseError;
+//  }
+//  return MethodNotFound;
 }
 
 JSONRPC_STATUS CPVROperations::DeleteTimer(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
