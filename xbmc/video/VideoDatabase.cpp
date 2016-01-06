xbmc/xbmc

commit 5755573981ca706a82a7ecbbdf5de112a570377a

Respect parent path decision when cleaning paths.
When cleaning the video library and a source containing TV shows was unavailable,
choosing to keep it only kept the source root, while subdirectories were
removed.
The result was that TV shows "disappeared".

This is fixed by checking if there's a decision for the parent path when
cleaning the path table.

xbmc/video/VideoDatabase.cpp
@@ -8090,7 +8090,7 @@ void CVideoDatabase::CleanDatabase(CGUIDialogProgressBarHandle* handle, const se
     }
 
     CLog::Log(LOGDEBUG, "%s: Cleaning paths that don't exist and have content set...", __FUNCTION__);
-    sql = "SELECT path.idPath, path.strPath FROM path "
+    sql = "SELECT path.idPath, path.strPath, path.idParentPath FROM path "
             "WHERE NOT ((strContent IS NULL OR strContent = '') "
                    "AND (strSettings IS NULL OR strSettings = '') "
                    "AND (strHash IS NULL OR strHash = '') "
@@ -8100,8 +8100,12 @@ void CVideoDatabase::CleanDatabase(CGUIDialogProgressBarHandle* handle, const se
     while (!m_pDS->eof())
     {
       std::map<int, bool>::const_iterator pathsDeleteDecision = pathsDeleteDecisions.find(m_pDS->fv(0).get_asInt());
-      if ((pathsDeleteDecision != pathsDeleteDecisions.end() && pathsDeleteDecision->second) ||
-          (pathsDeleteDecision == pathsDeleteDecisions.end() && !CDirectory::Exists(m_pDS->fv(1).get_asString(), false)))
+      // Check if we have a decision for the parent path
+      std::map<int, bool>::const_iterator pathsDeleteDecisionByParent = pathsDeleteDecisions.find(m_pDS->fv(2).get_asInt());
+      if (((pathsDeleteDecision != pathsDeleteDecisions.end() && pathsDeleteDecision->second) ||
+           (pathsDeleteDecision == pathsDeleteDecisions.end() && !CDirectory::Exists(m_pDS->fv(1).get_asString(), false))) &&
+          ((pathsDeleteDecisionByParent != pathsDeleteDecisions.end() && pathsDeleteDecisionByParent->second) ||
+           (pathsDeleteDecisionByParent == pathsDeleteDecisions.end())))
         strIds += m_pDS->fv(0).get_asString() + ",";
 
       m_pDS->next();

commit 5f4ef0cfbcad770b170d862360f34077a63514c2

[videodb] Fix TV shows cleaning after 5755573981ca706a82a7ecbbdf5de112a570377a.
A regression in 5755573981ca706a82a7ecbbdf5de112a570377a meant removing a
deleted TV show needed multiple library cleanings (i.e. TV shows only got
removed when filesToTestForDelete was empty).  This is much more noticeable
after 5cae3b5bb79a0c7b1945297e54343349f78cf497.

After 5755573981ca706a82a7ecbbdf5de112a570377a:
  * if we had files to be checked for deletion: CleanMediaType would return a
    decision for the TV show path (delete) and a decision for the parent path
    (keep), so the TV show wasn't removed from the database;
  * if no files needed to be checked for deletion: CleanMediaType would return
    earlier and pathsDeleteDecisions was empty, so we'd have checked if the
    path existed and there was a decision for the parent path (delete) or no
    decision at all.

This commit checks if there's a decision to delete the parent path *or* it
exists (we only end up there if there's a decision to delete the path or it
doesn't exist), so that you can delete a TV show with episodes (or other random
files).

See:
  * http://forum.kodi.tv/showthread.php?tid=238881

xbmc/video/VideoDatabase.cpp
@@ -8122,11 +8122,13 @@ void CVideoDatabase::CleanDatabase(CGUIDialogProgressBarHandle* handle, const st
     }
 
     CLog::Log(LOGDEBUG, "%s: Cleaning paths that don't exist and have content set...", __FUNCTION__);
-    sql = "SELECT path.idPath, path.strPath, path.idParentPath FROM path "
-            "WHERE NOT ((strContent IS NULL OR strContent = '') "
-                   "AND (strSettings IS NULL OR strSettings = '') "
-                   "AND (strHash IS NULL OR strHash = '') "
-                   "AND (exclude IS NULL OR exclude != 1))";
+    sql = "SELECT path.idPath, path.strPath, path.idParentPath, path2.strPath AS strParentPath "
+            "FROM path "
+            "LEFT JOIN path AS path2 ON path2.idPath = path.idParentPath "
+            "WHERE NOT ((path.strContent IS NULL OR path.strContent = '') "
+                   "AND (path.strSettings IS NULL OR path.strSettings = '') "
+                   "AND (path.strHash IS NULL OR path.strHash = '') "
+                   "AND (path.exclude IS NULL OR path.exclude != 1))";
     m_pDS->query(sql);
     std::string strIds;
     while (!m_pDS->eof())
@@ -8134,10 +8136,14 @@ void CVideoDatabase::CleanDatabase(CGUIDialogProgressBarHandle* handle, const st
       std::map<int, bool>::const_iterator pathsDeleteDecision = pathsDeleteDecisions.find(m_pDS->fv(0).get_asInt());
       // Check if we have a decision for the parent path
       std::map<int, bool>::const_iterator pathsDeleteDecisionByParent = pathsDeleteDecisions.find(m_pDS->fv(2).get_asInt());
-      if (((pathsDeleteDecision != pathsDeleteDecisions.end() && pathsDeleteDecision->second) ||
-           (pathsDeleteDecision == pathsDeleteDecisions.end() && !CDirectory::Exists(m_pDS->fv(1).get_asString(), false))) &&
-          ((pathsDeleteDecisionByParent != pathsDeleteDecisions.end() && pathsDeleteDecisionByParent->second) ||
-           (pathsDeleteDecisionByParent == pathsDeleteDecisions.end())))
+      bool hasDecision = pathsDeleteDecision != pathsDeleteDecisions.end();
+      bool hasDecisionForParent = pathsDeleteDecisionByParent != pathsDeleteDecisions.end();
+      std::string parentPath = m_pDS->fv(3).get_asString();
+
+      if (((hasDecision && pathsDeleteDecision->second) ||
+           (!hasDecision && !CDirectory::Exists(m_pDS->fv(1).get_asString(), false))) &&
+          ((hasDecisionForParent && (pathsDeleteDecisionByParent->second || (!parentPath.empty() && CDirectory::Exists(parentPath, false)))) ||
+           !hasDecisionForParent))
         strIds += m_pDS->fv(0).get_asString() + ",";
 
       m_pDS->next();