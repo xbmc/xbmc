PhenomG4/kodi-cmake

changed: make CBuiltins a singleton

xbmc/input/ButtonTranslator.cpp
@@ -1324,7 +1324,7 @@ bool CButtonTranslator::TranslateActionString(const char *szAction, int &action)
   action = ACTION_NONE;
   std::string strAction = szAction;
   StringUtils::ToLower(strAction);
-  if (CBuiltins::HasCommand(strAction)) 
+  if (CBuiltins::Get().HasCommand(strAction))
     action = ACTION_BUILT_IN_FUNCTION;
 
   for (unsigned int index=0;index < ARRAY_SIZE(actions);++index)

xbmc/interfaces/json-rpc/GUIOperations.cpp
@@ -65,7 +65,7 @@ JSONRPC_STATUS CGUIOperations::ActivateWindow(const std::string &method, ITransp
       cmd += "," + param->asString();
   }
   cmd += ")";
-  CBuiltins::Execute(cmd);
+  CBuiltins::Get().Execute(cmd);
 
   return ACK;
 }

xbmc/interfaces/json-rpc/PlayerOperations.cpp
@@ -288,7 +288,7 @@ JSONRPC_STATUS CPlayerOperations::PlayPause(const std::string &method, ITranspor
         return FailedToExecute;
       
       if (parameterObject["play"].isString())
-        CBuiltins::Execute("playercontrol(play)");
+        CBuiltins::Get().Execute("playercontrol(play)");
       else
       {
         if (parameterObject["play"].asBoolean())
@@ -364,9 +364,9 @@ JSONRPC_STATUS CPlayerOperations::SetSpeed(const std::string &method, ITransport
       else if (parameterObject["speed"].isString())
       {
         if (parameterObject["speed"].asString().compare("increment") == 0)
-          CBuiltins::Execute("playercontrol(forward)");
+          CBuiltins::Get().Execute("playercontrol(forward)");
         else
-          CBuiltins::Execute("playercontrol(rewind)");
+          CBuiltins::Get().Execute("playercontrol(rewind)");
       }
       else
         return InvalidParams;
@@ -401,13 +401,13 @@ JSONRPC_STATUS CPlayerOperations::Seek(const std::string &method, ITransportLaye
       {
         std::string step = value.isString() ? value.asString() : value["step"].asString();
         if (step == "smallforward")
-          CBuiltins::Execute("playercontrol(smallskipforward)");
+          CBuiltins::Get().Execute("playercontrol(smallskipforward)");
         else if (step == "smallbackward")
-          CBuiltins::Execute("playercontrol(smallskipbackward)");
+          CBuiltins::Get().Execute("playercontrol(smallskipbackward)");
         else if (step == "bigforward")
-          CBuiltins::Execute("playercontrol(bigskipforward)");
+          CBuiltins::Get().Execute("playercontrol(bigskipforward)");
         else if (step == "bigbackward")
-          CBuiltins::Execute("playercontrol(bigskipbackward)");
+          CBuiltins::Get().Execute("playercontrol(bigskipbackward)");
         else
           return InvalidParams;
       }

xbmc/interfaces/json-rpc/SystemOperations.cpp
@@ -47,7 +47,7 @@ JSONRPC_STATUS CSystemOperations::GetProperties(const std::string &method, ITran
 
 JSONRPC_STATUS CSystemOperations::EjectOpticalDrive(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
 {
-  return CBuiltins::Execute("EjectTray") == 0 ? ACK : FailedToExecute;
+  return CBuiltins::Get().Execute("EjectTray") == 0 ? ACK : FailedToExecute;
 }
 
 JSONRPC_STATUS CSystemOperations::Shutdown(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)

xbmc/settings/MediaSettings.cpp
@@ -315,7 +315,7 @@ void CMediaSettings::OnSettingAction(const CSetting *setting)
       g_application.StartMusicCleanup(true);
   }
   else if (settingId == "musiclibrary.export")
-    CBuiltins::Execute("exportlibrary(music)");
+    CBuiltins::Get().Execute("exportlibrary(music)");
   else if (settingId == "musiclibrary.import")
   {
     std::string path;
@@ -335,7 +335,7 @@ void CMediaSettings::OnSettingAction(const CSetting *setting)
       g_application.StartVideoCleanup(true);
   }
   else if (settingId == "videolibrary.export")
-    CBuiltins::Execute("exportlibrary(video)");
+    CBuiltins::Get().Execute("exportlibrary(video)");
   else if (settingId == "videolibrary.import")
   {
     std::string path;

xbmc/settings/dialogs/GUIDialogContentSettings.cpp
@@ -109,7 +109,7 @@ bool CGUIDialogContentSettings::OnMessage(CGUIMessage &message)
           std::string content = m_vecItems->Get(iSelected)->GetPath().substr(14);
           OnCancel();
           Close();
-          CBuiltins::Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.metadata.scraper." + content + ",return)");
+          CBuiltins::Get().Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.metadata.scraper." + content + ",return)");
           return true;
         }
 

xbmc/windows/GUIMediaWindow.cpp
@@ -1012,7 +1012,7 @@ bool CGUIMediaWindow::OnClick(int iItem)
     }
     else if (StringUtils::StartsWithNoCase(pItem->GetPath(), "addons://more/"))
     {
-      CBuiltins::Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.addon." + pItem->GetPath().substr(14) + ",return)");
+      CBuiltins::Get().Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.addon." + pItem->GetPath().substr(14) + ",return)");
       return true;
     }
 

xbmc/xbmc

xbmc/input/ButtonTranslator.cpp
@@ -1365,7 +1365,7 @@ bool CButtonTranslator::TranslateActionString(const char *szAction, int &action)
   action = ACTION_NONE;
   std::string strAction = szAction;
   StringUtils::ToLower(strAction);
-  if (CBuiltins::HasCommand(strAction)) 
+  if (CBuiltins::GetInstance().HasCommand(strAction))
     action = ACTION_BUILT_IN_FUNCTION;
 
   for (unsigned int index=0;index < ARRAY_SIZE(actions);++index)

xbmc/interfaces/json-rpc/GUIOperations.cpp
@@ -65,7 +65,7 @@ JSONRPC_STATUS CGUIOperations::ActivateWindow(const std::string &method, ITransp
       cmd += "," + param->asString();
   }
   cmd += ")";
-  CBuiltins::Execute(cmd);
+  CBuiltins::GetInstance().Execute(cmd);
 
   return ACK;
 }

xbmc/interfaces/json-rpc/PlayerOperations.cpp
@@ -290,7 +290,7 @@ JSONRPC_STATUS CPlayerOperations::PlayPause(const std::string &method, ITranspor
         return FailedToExecute;
       
       if (parameterObject["play"].isString())
-        CBuiltins::Execute("playercontrol(play)");
+        CBuiltins::GetInstance().Execute("playercontrol(play)");
       else
       {
         if (parameterObject["play"].asBoolean())
@@ -366,9 +366,9 @@ JSONRPC_STATUS CPlayerOperations::SetSpeed(const std::string &method, ITransport
       else if (parameterObject["speed"].isString())
       {
         if (parameterObject["speed"].asString().compare("increment") == 0)
-          CBuiltins::Execute("playercontrol(forward)");
+          CBuiltins::GetInstance().Execute("playercontrol(forward)");
         else
-          CBuiltins::Execute("playercontrol(rewind)");
+          CBuiltins::GetInstance().Execute("playercontrol(rewind)");
       }
       else
         return InvalidParams;
@@ -403,13 +403,13 @@ JSONRPC_STATUS CPlayerOperations::Seek(const std::string &method, ITransportLaye
       {
         std::string step = value.isString() ? value.asString() : value["step"].asString();
         if (step == "smallforward")
-          CBuiltins::Execute("playercontrol(smallskipforward)");
+          CBuiltins::GetInstance().Execute("playercontrol(smallskipforward)");
         else if (step == "smallbackward")
-          CBuiltins::Execute("playercontrol(smallskipbackward)");
+          CBuiltins::GetInstance().Execute("playercontrol(smallskipbackward)");
         else if (step == "bigforward")
-          CBuiltins::Execute("playercontrol(bigskipforward)");
+          CBuiltins::GetInstance().Execute("playercontrol(bigskipforward)");
         else if (step == "bigbackward")
-          CBuiltins::Execute("playercontrol(bigskipbackward)");
+          CBuiltins::GetInstance().Execute("playercontrol(bigskipbackward)");
         else
           return InvalidParams;
       }

xbmc/interfaces/json-rpc/SystemOperations.cpp
@@ -48,7 +48,7 @@ JSONRPC_STATUS CSystemOperations::GetProperties(const std::string &method, ITran
 
 JSONRPC_STATUS CSystemOperations::EjectOpticalDrive(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
 {
-  return CBuiltins::Execute("EjectTray") == 0 ? ACK : FailedToExecute;
+  return CBuiltins::GetInstance().Execute("EjectTray") == 0 ? ACK : FailedToExecute;
 }
 
 JSONRPC_STATUS CSystemOperations::Shutdown(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)

xbmc/settings/MediaSettings.cpp
@@ -368,7 +368,7 @@ void CMediaSettings::OnSettingAction(const CSetting *setting)
       g_application.StartMusicCleanup(true);
   }
   else if (settingId == CSettings::SETTING_MUSICLIBRARY_EXPORT)
-    CBuiltins::Execute("exportlibrary(music)");
+    CBuiltins::GetInstance().Execute("exportlibrary(music)");
   else if (settingId == CSettings::SETTING_MUSICLIBRARY_IMPORT)
   {
     std::string path;
@@ -391,7 +391,7 @@ void CMediaSettings::OnSettingAction(const CSetting *setting)
       g_application.StartVideoCleanup(true);
   }
   else if (settingId == CSettings::SETTING_VIDEOLIBRARY_EXPORT)
-    CBuiltins::Execute("exportlibrary(video)");
+    CBuiltins::GetInstance().Execute("exportlibrary(video)");
   else if (settingId == CSettings::SETTING_VIDEOLIBRARY_IMPORT)
   {
     std::string path;

xbmc/settings/dialogs/GUIDialogContentSettings.cpp
@@ -114,7 +114,7 @@ bool CGUIDialogContentSettings::OnMessage(CGUIMessage &message)
           std::string content = m_vecItems->Get(iSelected)->GetPath().substr(14);
           OnCancel();
           Close();
-          CBuiltins::Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.metadata.scraper." + content + ",return)");
+          CBuiltins::GetInstance().Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.metadata.scraper." + content + ",return)");
           return true;
         }
 

xbmc/windows/GUIMediaWindow.cpp
@@ -1013,7 +1013,7 @@ bool CGUIMediaWindow::OnClick(int iItem)
     }
     else if (StringUtils::StartsWithNoCase(pItem->GetPath(), "addons://more/"))
     {
-      CBuiltins::Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.addon." + pItem->GetPath().substr(14) + ",return)");
+      CBuiltins::GetInstance().Execute("ActivateWindow(AddonBrowser,addons://all/xbmc.addon." + pItem->GetPath().substr(14) + ",return)");
       return true;
     }
 