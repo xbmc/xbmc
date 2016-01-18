4491e2b693d3ee9d87520121dd99a5cadbab5304
Rainer Hochecker <fernetmenta@online.de>
2016-01-18 18:33:01 +0100

    VideoPlayer: hasta la vista marco

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
@@ -515,14 +515,9 @@ int CDVDVideoCodecFFmpeg::Decode(uint8_t* pData, int iSize, double dts, double p
   av_init_packet(&avpkt);
   avpkt.data = pData;
   avpkt.size = iSize;
-#define SET_PKT_TS(ts) \
-  if(ts != DVD_NOPTS_VALUE)\
-    avpkt.ts = (ts / DVD_TIME_BASE) * AV_TIME_BASE;\
-  else\
-    avpkt.ts = AV_NOPTS_VALUE
-  SET_PKT_TS(pts);
-  SET_PKT_TS(dts);
-#undef SET_PKT_TS
+  avpkt.dts = (dts == DVD_NOPTS_VALUE) ? AV_NOPTS_VALUE : dts / DVD_TIME_BASE * AV_TIME_BASE;
+  avpkt.pts = (pts == DVD_NOPTS_VALUE) ? AV_NOPTS_VALUE : pts / DVD_TIME_BASE * AV_TIME_BASE;
+
   /* We lie, but this flag is only used by pngdec.c.
    * Setting it correctly would allow CorePNG decoding. */
   avpkt.flags = AV_PKT_FLAG_KEY;
