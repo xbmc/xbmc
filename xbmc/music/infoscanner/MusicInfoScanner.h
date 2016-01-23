d25de7bb4e20dbd7e15903056eb0aabc7e9f3e7b
Deniz Türkoglu <turkoglu.deniz@gmail.com>
2016-01-23 17:41:12 +0100

    Refactor InfoScanners
    We were not sharing any code between the scanners although they
    have some logic that can be easily shared. This patch is the
    initial refactoring to remedy the situation, however there is
    space left for more refactoring.

--- xbmc/music/infoscanner/MusicInfoScanner.h
+++ xbmc/music/infoscanner/MusicInfoScanner.h
@@ -18,10 +18,11 @@
  *  <http://www.gnu.org/licenses/>.
  *
  */
-#include "threads/Thread.h"
-#include "music/MusicDatabase.h"
+#include "InfoScanner.h"
 #include "MusicAlbumInfo.h"
 #include "MusicInfoScraper.h"
+#include "music/MusicDatabase.h"
+#include "threads/Thread.h"
 
 class CAlbum;
 class CArtist;
@@ -41,7 +42,7 @@ enum INFO_RET
   INFO_ADDED 
 };
 
-class CMusicInfoScanner : CThread, public IRunnable
+class CMusicInfoScanner : CThread, public IRunnable, public CInfoScanner
 {
 public:
   /*! \brief Flags for controlling the scanning process
@@ -188,7 +189,7 @@ protected:
   int GetPathHash(const CFileItemList &items, std::string &hash);
   void GetAlbumArtwork(long id, const CAlbum &artist);
 
-  bool DoScan(const std::string& strDirectory);
+  bool DoScan(const std::string& strDirectory) override;
 
   virtual void Run();
   int CountFiles(const CFileItemList& items, bool recursive);
