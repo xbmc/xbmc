0f0f0f428b9592e995e7b767620c162aa99f9544
Chris "Koying" Browet <cbro@semperpax.com>
2016-01-30 14:38:43 +0100

    fixup PT: properly check for PT type disabling

--- xbmc/cores/AudioEngine/AEFactory.cpp
+++ xbmc/cores/AudioEngine/AEFactory.cpp
@@ -179,15 +179,15 @@ bool CAEFactory::SupportsRaw(AEDataFormat format, int samplerate)
     return false;
 
   // check if the format is enabled in settings
-  if ((format & ~PT_FORMAT_RAW_CLASS == AE_FMT_AC3) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_AC3PASSTHROUGH))
+  if ((((AEDataFormat)((int)format & ~PT_FORMAT_RAW_CLASS)) == AE_FMT_AC3) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_AC3PASSTHROUGH))
     return false;
-  if ((format & ~PT_FORMAT_RAW_CLASS == AE_FMT_DTS) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_DTSPASSTHROUGH))
+  if ((((AEDataFormat)((int)format & ~PT_FORMAT_RAW_CLASS)) == AE_FMT_DTS) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_DTSPASSTHROUGH))
     return false;
-  if ((format & ~PT_FORMAT_RAW_CLASS == AE_FMT_EAC3) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_EAC3PASSTHROUGH))
+  if ((((AEDataFormat)((int)format & ~PT_FORMAT_RAW_CLASS)) == AE_FMT_EAC3) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_EAC3PASSTHROUGH))
     return false;
-  if ((format & ~PT_FORMAT_RAW_CLASS == AE_FMT_TRUEHD) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_TRUEHDPASSTHROUGH))
+  if ((((AEDataFormat)((int)format & ~PT_FORMAT_RAW_CLASS)) == AE_FMT_TRUEHD) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_TRUEHDPASSTHROUGH))
     return false;
-  if ((format & ~PT_FORMAT_RAW_CLASS == AE_FMT_DTSHD) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_DTSHDPASSTHROUGH))
+  if ((((AEDataFormat)((int)format & ~PT_FORMAT_RAW_CLASS)) == AE_FMT_DTSHD) && !CSettings::GetInstance().GetBool(CSettings::SETTING_AUDIOOUTPUT_DTSHDPASSTHROUGH))
     return false;
 
   if(AE)
