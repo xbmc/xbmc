191b14d5f489ac4078f6be9b66d720eadc579832
Rainer Hochecker <fernetmenta@online.de>
2016-02-01 08:02:42 +0100

    AE: fix inconsistencies in calculation of buffered time

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -65,6 +65,11 @@ void CEngineStats::AddSamples(int samples, std::list<CActiveAEStream*> &streams)
 {
   CSingleLock lock(m_lock);
   m_bufferedSamples += samples;
+
+  for (auto stream : streams)
+  {
+    UpdateStream(stream);
+  }
 }
 
 void CEngineStats::GetDelay(AEDelayStatus& status)
