0952392954fdd119e14ac22e250c6e8d55e9a5bf
Glenn-1990 <g_christiaensen@msn.com>
2016-02-07 17:34:58 +0100

    [PVR] timeshift info should be updated first otherwise duration gets calculated with outdated timeshift info, this fixes a issue with jumping timeshift markers

--- xbmc/pvr/PVRGUIInfo.cpp
+++ xbmc/pvr/PVRGUIInfo.cpp
@@ -218,11 +218,11 @@ void CPVRGUIInfo::Process(void)
     Sleep(0);
 
     if (!m_bStop)
-      UpdatePlayingTag();
+      UpdateTimeshift();
     Sleep(0);
 
     if (!m_bStop)
-      UpdateTimeshift();
+      UpdatePlayingTag();
     Sleep(0);
 
     if (!m_bStop)
