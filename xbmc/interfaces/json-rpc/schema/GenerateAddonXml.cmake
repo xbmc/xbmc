eacc6440409275f0f2a01051a28559dd89f16763
Christian Fetzer <fetzer.ch@gmail.com>
2016-02-07 18:26:03 +0100

    [cmake] Add CMakeLists.txt to xbmc subfolders

---
+++ xbmc/interfaces/json-rpc/schema/GenerateAddonXml.cmake
@@ -0,0 +1,5 @@
+file(STRINGS ${CORE_SOURCE_DIR}/xbmc/interfaces/json-rpc/schema/version.txt jsonrpc_version)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml)
+configure_file(${CORE_SOURCE_DIR}/addons/xbmc.json/addon.xml.in
+               ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml @ONLY)
