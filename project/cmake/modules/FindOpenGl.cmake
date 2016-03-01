c2c2192cd416f5f472ee06ca71c3bc74fd821b07
Christian Fetzer <fetzer.ch@gmail.com>
2016-03-01 22:11:49 +0100

    [cmake] Modernize FindOpenGl.cmake

--- project/cmake/modules/FindOpenGl.cmake
+++ project/cmake/modules/FindOpenGl.cmake
@@ -1,24 +1,43 @@
-# - Try to find OpenGL
-# Once done this will define
+#.rst:
+# FindOpenGl
+# ----------
+# Finds the FindOpenGl library
 #
-# OPENGL_FOUND - system has OpenGL
-# OPENGL_INCLUDE_DIRS - the OpenGL include directory
-# OPENGL_LIBRARIES - The OpenGL libraries
+# This will will define the following variables::
+#
+# OPENGL_FOUND - system has OpenGl
+# OPENGL_INCLUDE_DIRS - the OpenGl include directory
+# OPENGL_LIBRARIES - the OpenGl libraries
+# OPENGL_DEFINITIONS - the OpenGl definitions
 
-if(PKG_CONFIG_FOUND AND NOT APPLE)
-  pkg_check_modules (OPENGL gl glu)
-  list(APPEND OPENGL_INCLUDE_DIRS "/usr/include")
+if(PKG_CONFIG_FOUND)
+  pkg_check_modules(PC_OPENGL gl glu)
+endif()
+
+if(NOT CORE_SYSTEM_NAME STREQUAL darwin)
+  find_path(OPENGL_INCLUDE_DIR GL/gl.h
+                               PATHS ${PC_OPENGL_gl_INCLUDEDIR})
+  find_library(OPENGL_gl_LIBRARY NAMES GL
+                                 PATHS ${PC_OPENGL_gl_LIBDIR})
+  find_library(OPENGL_glu_LIBRARY NAMES GLU
+                                  PATHS ${PC_OPENGL_glu_LIBDIR})
 else()
-  set(CMAKE_MODULE_PATH "")
-  find_package(OpenGL)
-  if(OPENGL_FOUND)
-    set(OPENGL_INCLUDE_DIRS ${OPENGL_INCLUDE_DIR})
-  endif()
+  find_library(OPENGL_gl_LIBRARY NAMES OpenGL
+                                 PATHS ${CMAKE_OSX_SYSROOT}/System/Library
+                                 PATH_SUFFIXES Frameworks
+                                 NO_DEFAULT_PATH)
+  set(OPENGL_INCLUDE_DIR ${OPENGL_gl_LIBRARY}/Headers)
+  set(OPENGL_glu_LIBRARY ${OPENGL_gl_LIBRARY})
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OpenGl DEFAULT_MSG OPENGL_INCLUDE_DIRS OPENGL_LIBRARIES)
+find_package_handle_standard_args(OpenGl
+                                  REQUIRED_VARS OPENGL_gl_LIBRARY OPENGL_glu_LIBRARY OPENGL_INCLUDE_DIR)
 
-list(APPEND OPENGL_DEFINITIONS -DHAVE_LIBGL=1)
+if(OPENGL_FOUND)
+  set(OPENGL_INCLUDE_DIRS ${OPENGL_INCLUDE_DIR})
+  set(OPENGL_LIBRARIES ${OPENGL_gl_LIBRARY} ${OPENGL_glu_LIBRARY})
+  set(OPENGL_DEFINITIONS -DHAVE_LIBGL=1)
+endif()
 
-mark_as_advanced(OPENGL_INCLUDE_DIRS OPENGL_LIBRARIES OPENGL_DEFINITIONS)
+mark_as_advanced(OPENGL_INCLUDE_DIR OPENGL_gl_LIBRARY OPENGL_glu_LIBRARY)
