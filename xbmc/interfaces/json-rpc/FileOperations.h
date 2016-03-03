9b84d8fd89008461293630eb8ba5f645226df743
Tolriq <tolriq@tolriq.com>
2016-03-03 12:19:04 +0100

    Add Files.SetFileDetails to JSON API
    Allows remote to set playcount / resume point on files that are not scrapped / scanned.
    Needed to allow smooth user experience when watching their content from anywhere other than source Kodi.

--- xbmc/interfaces/json-rpc/FileOperations.h
+++ xbmc/interfaces/json-rpc/FileOperations.h
@@ -32,6 +32,7 @@ namespace JSONRPC
     static JSONRPC_STATUS GetRootDirectory(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     static JSONRPC_STATUS GetDirectory(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     static JSONRPC_STATUS GetFileDetails(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+    static JSONRPC_STATUS SetFileDetails(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     
     static JSONRPC_STATUS PrepareDownload(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     static JSONRPC_STATUS Download(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
