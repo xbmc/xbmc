ce2ad954e7f3149d0a43df92f018ac5179885802
Rainer Hochecker <fernetmenta@online.de>
2016-03-07 19:34:06 +0100

    [pvr] bump api to 5.1.0

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/xbmc_pvr_types.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/xbmc_pvr_types.h
@@ -77,10 +77,10 @@ struct DemuxPacket;
 #define PVR_STREAM_MAX_STREAMS 20
 
 /* current PVR API version */
-#define XBMC_PVR_API_VERSION "5.0.0"
+#define XBMC_PVR_API_VERSION "5.1.0"
 
 /* min. PVR API version */
-#define XBMC_PVR_MIN_API_VERSION "5.0.0"
+#define XBMC_PVR_MIN_API_VERSION "5.1.0"
 
 #ifdef __cplusplus
 extern "C" {
