855f3288300324f8af6decf30b3be195137a8530
Anton Fedchin <anightik@gmail.com>
2016-03-15 18:25:30 +0300

    [guilib] Resolution::FindClosestResolution: don't switch between screens.

--- xbmc/guilib/Resolution.cpp
+++ xbmc/guilib/Resolution.cpp
@@ -227,7 +227,8 @@ RESOLUTION CResolutionUtils::FindClosestResolution(float fps, int width, bool is
       if ((width < orig.iScreenWidth) || // orig res large enough
          (info.iScreenWidth < orig.iScreenWidth) || // new res is smaller
          (info.iScreenHeight < orig.iScreenHeight) || // new height would be smaller
-         (info.dwFlags & D3DPRESENTFLAG_MODEMASK) != (curr.dwFlags & D3DPRESENTFLAG_MODEMASK)) // don't switch to interlaced modes
+         (info.dwFlags & D3DPRESENTFLAG_MODEMASK) != (curr.dwFlags & D3DPRESENTFLAG_MODEMASK) || // don't switch to interlaced modes
+         (info.iScreen != curr.iScreen)) // skip not current displays
       {
         continue;
       }
