xbmc/xbmc

commit 4a2fea095b2384a640828967cbdc415addce1903

RenderSystem - cleanup

xbmc/windowing/X11/WinSystemX11GLESContext.cpp
@@ -19,9 +19,11 @@ CWinSystemX11GLESContext::~CWinSystemX11GLESContext()
   delete m_pGLContext;
 }
 
-bool CWinSystemX11GLESContext::PresentRenderImpl(const CDirtyRegionList& dirty)
+void CWinSystemX11GLESContext::PresentRenderImpl(bool rendered)
 {
-  m_pGLContext->SwapBuffers(dirty, m_iVSyncMode);
+  if (rendered)
+    m_pGLContext->SwapBuffers(m_iVSyncMode);
+
   if (m_delayDispReset && m_dispResetTimer.IsTimePast())
   {
     m_delayDispReset = false;
@@ -30,7 +32,6 @@ bool CWinSystemX11GLESContext::PresentRenderImpl(const CDirtyRegionList& dirty)
     for (std::vector<IDispResource *>::iterator i = m_resources.begin(); i != m_resources.end(); ++i)
       (*i)->OnResetDisplay();
   }
-  return true;
 }
 
 void CWinSystemX11GLESContext::SetVSyncImpl(bool enable)

xbmc/Application.cpp
@@ -1987,8 +1987,7 @@ void CApplication::Render()
       Sleep(singleFrameTime - frameTime);
   }
 
-  if (flip)
-    g_graphicsContext.Flip(dirtyRegions);
+  g_graphicsContext.Flip(flip);
 
   if (!extPlayerActive && g_graphicsContext.IsFullScreenVideo() && !m_pPlayer->IsPausedPlayback())
   {

xbmc/guilib/GraphicContext.cpp
@@ -979,9 +979,9 @@ void CGraphicContext::SetMediaDir(const std::string &strMediaDir)
   m_strMediaDir = strMediaDir;
 }
 
-void CGraphicContext::Flip(const CDirtyRegionList& dirty)
+void CGraphicContext::Flip(bool rendered)
 {
-  g_Windowing.PresentRender(dirty);
+  g_Windowing.PresentRender(rendered);
 
   if(m_stereoMode != m_nextStereoMode)
   {

xbmc/guilib/GraphicContext.h
@@ -130,7 +130,7 @@ public:
   void SetRenderingResolution(const RESOLUTION_INFO &res, bool needsScaling);  ///< Sets scaling up for rendering
   void SetScalingResolution(const RESOLUTION_INFO &res, bool needsScaling);    ///< Sets scaling up for skin loading etc.
   float GetScalingPixelRatio() const;
-  void Flip(const CDirtyRegionList& dirty);
+  void Flip(bool rendered);
   void InvertFinalCoords(float &x, float &y) const;
   inline float ScaleFinalXCoord(float x, float y) const XBMC_FORCE_INLINE { return m_finalTransform.matrix.TransformXCoord(x, y, 0); }
   inline float ScaleFinalYCoord(float x, float y) const XBMC_FORCE_INLINE { return m_finalTransform.matrix.TransformYCoord(x, y, 0); }

xbmc/rendering/RenderSystem.h
@@ -102,7 +102,7 @@ public:
 
   virtual bool BeginRender() = 0;
   virtual bool EndRender() = 0;
-  virtual bool PresentRender(const CDirtyRegionList& dirty) = 0;
+  virtual void PresentRender(bool rendered) = 0;
   virtual bool ClearBuffers(color_t color) = 0;
   virtual bool IsExtSupported(const char* extension) = 0;
 

xbmc/rendering/dx/RenderSystemDX.cpp
@@ -1108,19 +1108,22 @@ bool CRenderSystemDX::CreateStates()
   return true;
 }
 
-bool CRenderSystemDX::PresentRenderImpl(const CDirtyRegionList &dirty)
+void CRenderSystemDX::PresentRenderImpl(bool rendered)
 {
   HRESULT hr;
 
+  if (!rendered)
+    return;
+  
   if (!m_bRenderCreated || m_resizeInProgress)
-    return false;
+    return;
 
   if (m_nDeviceStatus != S_OK)
   {
     // if DXGI_STATUS_OCCLUDED occurred we just clear command queue and return
     if (m_nDeviceStatus == DXGI_STATUS_OCCLUDED)
       FinishCommandList(false);
-    return false;
+    return;
   }
 
   if ( m_stereoMode == RENDER_STEREO_MODE_INTERLACED
@@ -1154,7 +1157,7 @@ bool CRenderSystemDX::PresentRenderImpl(const CDirtyRegionList &dirty)
   if (DXGI_ERROR_DEVICE_REMOVED == hr)
   {
     CLog::Log(LOGDEBUG, "%s - device removed", __FUNCTION__);
-    return false;
+    return;
   }
 
   if (DXGI_ERROR_INVALID_CALL == hr)
@@ -1167,14 +1170,12 @@ bool CRenderSystemDX::PresentRenderImpl(const CDirtyRegionList &dirty)
   if (FAILED(hr))
   {
     CLog::Log(LOGDEBUG, "%s - Present failed. %s", __FUNCTION__, GetErrorDescription(hr).c_str());
-    return false;
+    return;
   }
 
   // after present swapchain unbinds RT view from immediate context, need to restore it because it can be used by something else
   if (m_pContext == m_pImdContext)
     m_pContext->OMSetRenderTargets(1, &m_pRenderTargetView, m_depthStencilView);
-
-  return true;
 }
 
 bool CRenderSystemDX::BeginRender()

xbmc/rendering/dx/RenderSystemDX.h
@@ -96,7 +96,7 @@ protected:
   void DeleteDevice();
   void OnDeviceLost();
   void OnDeviceReset();
-  bool PresentRenderImpl(const CDirtyRegionList &dirty);
+  void PresentRenderImpl(bool rendered);
 
   void SetFocusWnd(HWND wnd) { m_hFocusWnd = wnd; }
   void SetDeviceWnd(HWND wnd) { m_hDeviceWnd = wnd; }

xbmc/rendering/gl/RenderSystemGL.cpp
@@ -106,9 +106,6 @@ bool CRenderSystemGL::InitRenderSystem()
 {
   m_bVSync = false;
   m_iVSyncMode = 0;
-  m_iSwapStamp = 0;
-  m_iSwapTime = 0;
-  m_iSwapRate = 0;
   m_bVsyncInit = false;
   m_maxTextureSize = 2048;
   m_renderCaps = 0;
@@ -293,49 +290,13 @@ bool CRenderSystemGL::IsExtSupported(const char* extension)
   return m_RenderExtensions.find(name) != std::string::npos;
 }
 
-bool CRenderSystemGL::PresentRender(const CDirtyRegionList& dirty)
+void CRenderSystemGL::PresentRender(bool rendered)
 {
   if (!m_bRenderCreated)
-    return false;
+    return;
 
-  if (m_iVSyncMode != 0 && m_iSwapRate != 0)
-  {
-    int64_t curr, diff, freq;
-    curr = CurrentHostCounter();
-    freq = CurrentHostFrequency();
-
-    if(m_iSwapStamp == 0)
-      m_iSwapStamp = curr;
-
-    /* calculate our next swap timestamp */
-    diff = curr - m_iSwapStamp;
-    diff = m_iSwapRate - diff % m_iSwapRate;
-    m_iSwapStamp = curr + diff;
-
-    /* sleep as close as we can before, assume 1ms precision of sleep *
-     * this should always awake so that we are guaranteed the given   *
-     * m_iSwapTime to do our swap                                     */
-    diff = (diff - m_iSwapTime) * 1000 / freq;
-    if (diff > 0)
-      Sleep((DWORD)diff);
-  }
-
-  bool result = PresentRenderImpl(dirty);
+  PresentRenderImpl(rendered);
   m_latencyCounter++;
-
-  if (m_iVSyncMode && m_iSwapRate != 0)
-  {
-    int64_t curr, diff;
-    curr = CurrentHostCounter();
-
-    diff = curr - m_iSwapStamp;
-    m_iSwapStamp = curr;
-
-    if (MathUtils::abs(diff - m_iSwapRate) < MathUtils::abs(diff))
-      CLog::Log(LOGDEBUG, "%s - missed requested swap",__FUNCTION__);
-  }
-
-  return result;
 }
 
 void CRenderSystemGL::SetVSync(bool enable)
@@ -353,7 +314,6 @@ void CRenderSystemGL::SetVSync(bool enable)
 
   m_iVSyncMode   = 0;
   m_iVSyncErrors = 0;
-  m_iSwapRate    = 0;
   m_bVSync       = enable;
   m_bVsyncInit   = true;
 
@@ -361,28 +321,6 @@ void CRenderSystemGL::SetVSync(bool enable)
 
   if (!enable)
     return;
-
-  if (g_advancedSettings.m_ForcedSwapTime != 0.0)
-  {
-    /* some hardware busy wait on swap/glfinish, so we must manually sleep to avoid 100% cpu */
-    double rate = g_graphicsContext.GetFPS();
-    if (rate <= 0.0 || rate > 1000.0)
-    {
-      CLog::Log(LOGWARNING, "Unable to determine a valid horizontal refresh rate, vsync workaround disabled %.2g", rate);
-      m_iSwapRate = 0;
-    }
-    else
-    {
-      int64_t freq;
-      freq = CurrentHostFrequency();
-      m_iSwapRate   = (int64_t)((double)freq / rate);
-      m_iSwapTime   = (int64_t)(0.001 * g_advancedSettings.m_ForcedSwapTime * freq);
-      m_iSwapStamp  = 0;
-      CLog::Log(LOGINFO, "GL: Using artificial vsync sleep with rate %f", rate);
-      if(!m_iVSyncMode)
-        m_iVSyncMode = 1;
-    }
-  }
 
   if (!m_iVSyncMode)
     CLog::Log(LOGERROR, "GL: Vertical Blank Syncing unsupported");

commit d0364aaa64ee1f97f1424e13330b6c249c6eb487

windowing/X11/GLESContext: fix broken Flip() call
Breakage from commit 4a2fea09

xbmc/windowing/X11/WinSystemX11GLESContext.cpp
@@ -53,11 +53,10 @@ bool CWinSystemX11GLESContext::SetWindow(int width, int height, bool fullscreen,
   CWinSystemX11::SetWindow(width, height, fullscreen, output, &newwin);
   if (newwin)
   {
-    CDirtyRegionList dr;
     RefreshGLContext(m_currentOutput.compare(output) != 0);
     XSync(m_dpy, FALSE);
     g_graphicsContext.Clear(0);
-    g_graphicsContext.Flip(dr);
+    g_graphicsContext.Flip(true);
     ResetVSync();
 
     m_windowDirty = false;