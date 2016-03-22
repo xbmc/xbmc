904748816d561667cee171086726e227e8d51b2a
wsnipex <wsnipex@a1.net>
2016-03-22 19:48:11 +0100

    [python] use our provided CA cert if building with unified deps

--- xbmc/interfaces/python/XBPython.cpp
+++ xbmc/interfaces/python/XBPython.cpp
@@ -594,8 +594,9 @@ bool XBPython::OnScriptInitialized(ILanguageInvoker *invoker)
     buf = "OS=win32";
     CEnvironment::putenv(buf);
 
-#elif defined(TARGET_ANDROID)
-   setenv("SSL_CERT_FILE", CSpecialProtocol::TranslatePath("special://xbmc/system/certs/cacert.pem").c_str(), 1);
+#endif
+#if defined(DEPENDS_BUILD)
+   setenv("SSL_CERT_FILE", CSpecialProtocol::TranslatePath("special://xbmc/system/certs/cacert.pem").c_str(), 0);
 #endif
 
     if (PyEval_ThreadsInitialized())
