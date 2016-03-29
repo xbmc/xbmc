xbmc/xbmc

commit e797f4514a88e20a67d2afe0b2870fa316092775

[addons] fix control condition: always allow addons to be enabled

xbmc/addons/GUIDialogAddonInfo.cpp
@@ -190,8 +190,16 @@ void CGUIDialogAddonInfo::UpdateControls()
   CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_INSTALL, canInstall || canUninstall);
   SET_CONTROL_LABEL(CONTROL_BTN_INSTALL, isInstalled ? 24037 : 24038);
 
-  CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_ENABLE, canDisable);
-  SET_CONTROL_LABEL(CONTROL_BTN_ENABLE, m_addonEnabled ? 24021 : 24022);
+  if (m_addonEnabled)
+  {
+    SET_CONTROL_LABEL(CONTROL_BTN_ENABLE, 24021);
+    CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_ENABLE, canDisable);
+  }
+  else
+  {
+    SET_CONTROL_LABEL(CONTROL_BTN_ENABLE, 24022);
+    CONTROL_ENABLE(CONTROL_BTN_ENABLE);
+  }
 
   CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_UPDATE, isInstalled);
 

commit 619eb34ce1ea444ce093fe58e7110e057a9af693

[addons] dont enable 'enable' control for addons that aren't installed
regression from e797f4514a88e20a67d2afe0b2870fa316092775

xbmc/addons/GUIDialogAddonInfo.cpp
@@ -198,7 +198,7 @@ void CGUIDialogAddonInfo::UpdateControls()
   else
   {
     SET_CONTROL_LABEL(CONTROL_BTN_ENABLE, 24022);
-    CONTROL_ENABLE(CONTROL_BTN_ENABLE);
+    CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_ENABLE, isInstalled);
   }
 
   CONTROL_ENABLE_ON_CONDITION(CONTROL_BTN_UPDATE, isInstalled);