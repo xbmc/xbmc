42eef924faf028fdd15ea1f9a71c1968b16367f1
fritsch <Peter.Fruehberger@gmail.com>
2016-03-30 09:53:50 +0200

    FFmpeg 3.0 Transition: Alter changed mips compile params

--- tools/depends/target/ffmpeg/autobuild.sh
+++ tools/depends/target/ffmpeg/autobuild.sh
@@ -160,8 +160,7 @@ CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
 	--enable-nonfree \
 	--enable-pthreads \
 	--enable-zlib \
-	--disable-mips32r2 \
-	--disable-mipsdspr1 \
+	--disable-mipsdsp \
 	--disable-mipsdspr2 \
         ${FLAGS}
 
