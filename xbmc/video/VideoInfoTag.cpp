xbmc/xbmc

commit e6e2d0ace5cfe6191b8f7727ced1747854254ce4

video library: add functionality to set custom names for tvshow seasons

xbmc/video/VideoInfoTag.cpp
@@ -83,6 +83,7 @@ void CVideoInfoTag::Reset()
   m_duration = 0;
   m_lastPlayed.Reset();
   m_showLink.clear();
+  m_namedSeasons.clear();
   m_streamDetails.Reset();
   m_playCount = 0;
   m_fEpBookmark = 0;
@@ -241,6 +242,14 @@ bool CVideoInfoTag::Save(TiXmlNode *node, const std::string &tag, bool savePathI
   }
   XMLUtils::SetStringArray(movie, "artist", m_artist);
   XMLUtils::SetStringArray(movie, "showlink", m_showLink);
+
+  for (const auto& namedSeason : m_namedSeasons)
+  {
+    TiXmlElement season("namedseason");
+    season.SetAttribute("number", namedSeason.first);
+    season.SetValue(namedSeason.second);
+    movie->InsertEndChild(season);
+  }
  
   TiXmlElement resume("resume");
   XMLUtils::SetFloat(&resume, "position", (float)m_resumePoint.timeInSeconds);
@@ -330,6 +339,12 @@ void CVideoInfoTag::Archive(CArchive& ar)
     ar << m_iTrack;
     ar << dynamic_cast<IArchivable&>(m_streamDetails);
     ar << m_showLink;
+    ar << static_cast<int>(m_namedSeasons.size());
+    for (const auto& namedSeason : m_namedSeasons)
+    {
+      ar << namedSeason.first;
+      ar << namedSeason.second;
+    }
     ar << m_fEpBookmark;
     ar << m_basePath;
     ar << m_parentPathID;
@@ -409,6 +424,18 @@ void CVideoInfoTag::Archive(CArchive& ar)
     ar >> m_iTrack;
     ar >> dynamic_cast<IArchivable&>(m_streamDetails);
     ar >> m_showLink;
+
+    int namedSeasonSize;
+    ar >> namedSeasonSize;
+    for (int i = 0; i < namedSeasonSize; ++i)
+    {
+      int seasonNumber;
+      ar >> seasonNumber;
+      std::string seasonName;
+      ar >> seasonName;
+      m_namedSeasons.insert(std::make_pair(seasonNumber, seasonName));
+    }
+
     ar >> m_fEpBookmark;
     ar >> m_basePath;
     ar >> m_parentPathID;
@@ -528,7 +555,15 @@ void CVideoInfoTag::ToSortable(SortItem& sortable, Field field) const
       sortable[FieldPath] = path;
     break;
   }
-  case FieldSortTitle:                sortable[FieldSortTitle] = m_strSortTitle; break;
+  case FieldSortTitle:
+  {
+    // seasons with a custom name/title need special handling as they should be sorted by season number
+    if (m_type == MediaTypeSeason && !m_strSortTitle.empty())
+      sortable[FieldSortTitle] = StringUtils::Format(g_localizeStrings.Get(20358).c_str(), m_iSeason);
+    else
+      sortable[FieldSortTitle] = m_strSortTitle;
+    break;
+  }
   case FieldTvShowStatus:             sortable[FieldTvShowStatus] = m_strStatus; break;
   case FieldProductionCode:           sortable[FieldProductionCode] = m_strProductionCode; break;
   case FieldAirDate:                  sortable[FieldAirDate] = m_firstAired.IsValid() ? m_firstAired.GetAsDBDate() : (m_premiered.IsValid() ? m_premiered.GetAsDBDate() : StringUtils::Empty); break;
@@ -726,6 +761,18 @@ void CVideoInfoTag::ParseNative(const TiXmlElement* movie, bool prioritise)
   std::vector<std::string> showLink(m_showLink);
   if (XMLUtils::GetStringArray(movie, "showlink", showLink, prioritise, g_advancedSettings.m_videoItemSeparator))
     SetShowLink(showLink);
+
+  const TiXmlElement* namedSeason = movie->FirstChildElement("namedseason");
+  while (namedSeason != nullptr)
+  {
+    int seasonNumber;
+    std::string seasonName = namedSeason->ValueStr();
+    if (!seasonName.empty() &&
+        namedSeason->Attribute("number", &seasonNumber) != nullptr)
+      m_namedSeasons.insert(std::make_pair(seasonNumber, seasonName));
+
+    namedSeason = thumb->NextSiblingElement("namedseason");
+  }
 
   // cast
   const TiXmlElement* node = movie->FirstChildElement("actor");
@@ -1091,6 +1138,11 @@ void CVideoInfoTag::SetShowLink(std::vector<std::string> showLink)
 void CVideoInfoTag::SetUniqueId(std::string uniqueId)
 {
   m_strUniqueId = Trim(std::move(uniqueId));
+}
+
+void CVideoInfoTag::SetNamedSeasons(std::map<int, std::string> namedSeasons)
+{
+  m_namedSeasons = std::move(namedSeasons);
 }
 
 std::string CVideoInfoTag::Trim(std::string &&value)

xbmc/video/VideoDatabase.cpp
@@ -191,7 +191,7 @@ void CVideoDatabase::CreateTables()
   m_pDS->exec("CREATE TABLE sets ( idSet integer primary key, strSet text, strOverview text)");
 
   CLog::Log(LOGINFO, "create seasons table");
-  m_pDS->exec("CREATE TABLE seasons ( idSeason integer primary key, idShow integer, season integer)");
+  m_pDS->exec("CREATE TABLE seasons ( idSeason integer primary key, idShow integer, season integer, name text)");
 
   CLog::Log(LOGINFO, "create art table");
   m_pDS->exec("CREATE TABLE art(art_id INTEGER PRIMARY KEY, media_id INTEGER, media_type TEXT, type TEXT, url TEXT)");
@@ -2344,6 +2344,21 @@ bool CVideoDatabase::UpdateDetailsForTvShow(int idTvShow, const CVideoInfoTag &d
   // add "all seasons" - the rest are added in SetDetailsForEpisode
   AddSeason(idTvShow, -1);
 
+  // add any named seasons
+  for (const auto& namedSeason : details.m_namedSeasons)
+  {
+    // make sure the named season exists
+    int seasonId = AddSeason(idTvShow, namedSeason.first);
+
+    // get any existing details for the named season
+    CVideoInfoTag season;
+    if (!GetSeasonInfo(seasonId, season) || season.m_strSortTitle == namedSeason.second)
+      continue;
+
+    season.SetSortTitle(namedSeason.second);
+    SetDetailsForSeason(season, std::map<std::string, std::string>(), idTvShow, seasonId);
+  }
+
   SetArtForItem(idTvShow, MediaTypeTvShow, artwork);
   for (std::map<int, std::map<std::string, std::string>>::const_iterator i = seasonArt.begin(); i != seasonArt.end(); ++i)
   {
@@ -2390,8 +2405,11 @@ int CVideoDatabase::SetDetailsForSeason(const CVideoInfoTag& details, const std:
     SetArtForItem(idSeason, MediaTypeSeason, artwork);
 
     // and insert the new row
-    std::string sql = PrepareSQL("UPDATE seasons SET season=%i WHERE idSeason=%i", details.m_iSeason, idSeason);
-    m_pDS->exec(sql);
+    std::string sql = PrepareSQL("UPDATE seasons SET season=%i", details.m_iSeason);
+    if (!details.m_strSortTitle.empty())
+      sql += PrepareSQL(", name='%s'", details.m_strSortTitle.c_str());
+    sql += PrepareSQL(" WHERE idSeason=%i", idSeason);
+    m_pDS->exec(sql.c_str());
     CommitTransaction();
 
     return idSeason;
@@ -2500,7 +2518,7 @@ int CVideoDatabase::AddSeason(int showID, int season)
   int seasonId = GetSeasonId(showID, season);
   if (seasonId < 0)
   {
-    if (ExecuteQuery(PrepareSQL("INSERT INTO seasons (idShow,season) VALUES(%i,%i)", showID, season)))
+    if (ExecuteQuery(PrepareSQL("INSERT INTO seasons (idShow,season,name) VALUES(%i,%i,'')", showID, season)))
       seasonId = (int)m_pDS->lastinsertid();
   }
   return seasonId;
@@ -4634,11 +4652,14 @@ void CVideoDatabase::UpdateTables(int iVersion)
 
   if (iVersion < 97)
     m_pDS->exec("ALTER TABLE sets ADD strOverview TEXT");
+
+  if (iVersion < 98)
+    m_pDS->exec("ALTER TABLE seasons ADD name text");
 }
 
 int CVideoDatabase::GetSchemaVersion() const
 {
-  return 97;
+  return 98;
 }
 
 bool CVideoDatabase::LookupByFolders(const std::string &path, bool shows)
@@ -5828,6 +5849,7 @@ bool CVideoDatabase::GetSeasonsByWhere(const std::string& strBaseDir, const Filt
       int id = m_pDS->fv(VIDEODB_ID_SEASON_ID).get_asInt();
       int showId = m_pDS->fv(VIDEODB_ID_SEASON_TVSHOW_ID).get_asInt();
       int iSeason = m_pDS->fv(VIDEODB_ID_SEASON_NUMBER).get_asInt();
+      std::string name = m_pDS->fv(VIDEODB_ID_SEASON_NAME).get_asString();
       std::string path = m_pDS->fv(VIDEODB_ID_SEASON_TVSHOW_PATH).get_asString();
 
       if (mapSeasons.find(std::make_pair(showId, iSeason)) == mapSeasons.end() &&
@@ -5836,11 +5858,14 @@ bool CVideoDatabase::GetSeasonsByWhere(const std::string& strBaseDir, const Filt
       {
         mapSeasons.insert(std::make_pair(showId, iSeason));
 
-        std::string strLabel;
-        if (iSeason == 0)
-          strLabel = g_localizeStrings.Get(20381);
-        else
-          strLabel = StringUtils::Format(g_localizeStrings.Get(20358).c_str(), iSeason);
+        std::string strLabel = name;
+        if (strLabel.empty())
+        {
+          if (iSeason == 0)
+            strLabel = g_localizeStrings.Get(20381);
+          else
+            strLabel = StringUtils::Format(g_localizeStrings.Get(20358).c_str(), iSeason);
+        }
         CFileItemPtr pItem(new CFileItem(strLabel));
 
         CVideoDbUrl itemUrl = videoUrl;
@@ -5853,6 +5878,8 @@ bool CVideoDatabase::GetSeasonsByWhere(const std::string& strBaseDir, const Filt
 
         pItem->m_bIsFolder = true;
         pItem->GetVideoInfoTag()->m_strTitle = strLabel;
+        if (!name.empty())
+          pItem->GetVideoInfoTag()->m_strSortTitle = name;
         pItem->GetVideoInfoTag()->m_iSeason = iSeason;
         pItem->GetVideoInfoTag()->m_iDbId = id;
         pItem->GetVideoInfoTag()->m_iIdSeason = id;

xbmc/video/VideoDatabase.h
@@ -246,16 +246,17 @@ typedef enum // this enum MUST match the offset struct further down!! and make s
   VIDEODB_ID_SEASON_ID = 0,
   VIDEODB_ID_SEASON_TVSHOW_ID = 1,
   VIDEODB_ID_SEASON_NUMBER = 2,
-  VIDEODB_ID_SEASON_TVSHOW_PATH = 3,
-  VIDEODB_ID_SEASON_TVSHOW_TITLE = 4,
-  VIDEODB_ID_SEASON_TVSHOW_PLOT = 5,
-  VIDEODB_ID_SEASON_TVSHOW_PREMIERED = 6,
-  VIDEODB_ID_SEASON_TVSHOW_GENRE = 7,
-  VIDEODB_ID_SEASON_TVSHOW_STUDIO = 8,
-  VIDEODB_ID_SEASON_TVSHOW_MPAA = 9,
-  VIDEODB_ID_SEASON_EPISODES_TOTAL = 10,
-  VIDEODB_ID_SEASON_EPISODES_WATCHED = 11,
-  VIDEODB_ID_SEASON_PREMIERED = 12,
+  VIDEODB_ID_SEASON_NAME = 3,
+  VIDEODB_ID_SEASON_TVSHOW_PATH = 4,
+  VIDEODB_ID_SEASON_TVSHOW_TITLE = 5,
+  VIDEODB_ID_SEASON_TVSHOW_PLOT = 6,
+  VIDEODB_ID_SEASON_TVSHOW_PREMIERED = 7,
+  VIDEODB_ID_SEASON_TVSHOW_GENRE = 8,
+  VIDEODB_ID_SEASON_TVSHOW_STUDIO = 9,
+  VIDEODB_ID_SEASON_TVSHOW_MPAA = 10,
+  VIDEODB_ID_SEASON_EPISODES_TOTAL = 11,
+  VIDEODB_ID_SEASON_EPISODES_WATCHED = 12,
+  VIDEODB_ID_SEASON_PREMIERED = 13,
   VIDEODB_ID_SEASON_MAX
 } VIDEODB_SEASON_IDS;
 

xbmc/video/VideoInfoTag.h
@@ -125,7 +125,7 @@ public:
   void SetAlbum(std::string album);
   void SetShowLink(std::vector<std::string> showLink);
   void SetUniqueId(std::string uniqueId);
-
+  void SetNamedSeasons(std::map<int, std::string> namedSeasons);
 
   std::string m_basePath; // the base path of the video, for folder-based lookups
   int m_parentPathID;      // the parent path id where the base path of the video lies
@@ -164,6 +164,7 @@ public:
   std::string m_strAlbum;
   CDateTime m_lastPlayed;
   std::vector<std::string> m_showLink;
+  std::map<int, std::string> m_namedSeasons;
   int m_playCount;
   int m_iTop250;
   int m_iYear;

xbmc/video/dialogs/GUIDialogVideoInfo.cpp
@@ -900,7 +900,7 @@ int CGUIDialogVideoInfo::ManageVideoItem(const CFileItemPtr &item)
 
   CContextButtons buttons;
   if (type == MediaTypeMovie || type == MediaTypeVideoCollection ||
-      type == MediaTypeTvShow || type == MediaTypeEpisode ||
+      type == MediaTypeTvShow || type == MediaTypeSeason || type == MediaTypeEpisode ||
       type == MediaTypeMusicVideo)
     buttons.Add(CONTEXT_BUTTON_EDIT, 16105);
 
@@ -959,7 +959,8 @@ int CGUIDialogVideoInfo::ManageVideoItem(const CFileItemPtr &item)
     }
   }
 
-  buttons.Add(CONTEXT_BUTTON_DELETE, 646);
+  if (type != MediaTypeSeason)
+    buttons.Add(CONTEXT_BUTTON_DELETE, 646);
 
   CContextMenuManager::GetInstance().AddVisibleItems(item, buttons, CContextMenuManager::MANAGE);
 
@@ -1056,24 +1057,58 @@ bool CGUIDialogVideoInfo::UpdateVideoItemTitle(const CFileItemPtr &pItem)
     return false;
 
   int iDbId = pItem->GetVideoInfoTag()->m_iDbId;
+  MediaType mediaType = pItem->GetVideoInfoTag()->m_type;
+
   CVideoInfoTag detail;
-  VIDEODB_CONTENT_TYPE iType = (VIDEODB_CONTENT_TYPE)pItem->GetVideoContentType();
-  if (iType == VIDEODB_CONTENT_MOVIES)
+  std::string title;
+  if (mediaType == MediaTypeMovie)
+  {
     database.GetMovieInfo("", detail, iDbId);
-  else if (iType == VIDEODB_CONTENT_MOVIE_SETS)
+    title = detail.m_strTitle;
+  }
+  else if (mediaType == MediaTypeVideoCollection)
+  {
     database.GetSetInfo(iDbId, detail);
-  else if (iType == VIDEODB_CONTENT_EPISODES)
+    title = detail.m_strTitle;
+  }
+  else if (mediaType == MediaTypeEpisode)
+  {
     database.GetEpisodeInfo(pItem->GetPath(), detail, iDbId);
-  else if (iType == VIDEODB_CONTENT_TVSHOWS)
+    title = detail.m_strTitle;
+  }
+  else if (mediaType == MediaTypeSeason)
+  {
+    database.GetSeasonInfo(iDbId, detail);
+    title = detail.m_strSortTitle;
+  }
+  else if (mediaType == MediaTypeTvShow)
+  {
     database.GetTvShowInfo(pItem->GetVideoInfoTag()->m_strFileNameAndPath, detail, iDbId);
-  else if (iType == VIDEODB_CONTENT_MUSICVIDEOS)
+    title = detail.m_strTitle;
+  }
+  else if (mediaType == MediaTypeMusicVideo)
+  {
     database.GetMusicVideoInfo(pItem->GetVideoInfoTag()->m_strFileNameAndPath, detail, iDbId);
+    title = detail.m_strTitle;
+  }
 
   // get the new title
-  if (!CGUIKeyboardFactory::ShowAndGetInput(detail.m_strTitle, CVariant{g_localizeStrings.Get(16105)}, false))
+  if (!CGUIKeyboardFactory::ShowAndGetInput(title, CVariant{ g_localizeStrings.Get(16105) }, false))
     return false;
 
-  database.UpdateMovieTitle(iDbId, detail.m_strTitle, iType);
+  if (mediaType == MediaTypeSeason)
+  {
+    detail.m_strSortTitle = title;
+    std::map<std::string, std::string> artwork;
+    database.SetDetailsForSeason(detail, artwork, detail.m_iIdShow, detail.m_iDbId);
+  }
+  else
+  {
+    detail.m_strTitle = title;
+    VIDEODB_CONTENT_TYPE iType = (VIDEODB_CONTENT_TYPE)pItem->GetVideoContentType();
+    database.UpdateMovieTitle(iDbId, detail.m_strTitle, iType);
+  }
+
   return true;
 }
 

xbmc/video/windows/GUIWindowVideoNav.cpp
@@ -936,6 +936,7 @@ void CGUIWindowVideoNav::GetContextButtons(int itemNumber, CContextButtons &butt
         if (!g_application.IsVideoScanning() && item->IsVideoDb() && item->HasVideoInfoTag() &&
            (item->GetVideoInfoTag()->m_type == MediaTypeMovie ||          // movies
             item->GetVideoInfoTag()->m_type == MediaTypeTvShow ||         // tvshows
+           (item->GetVideoInfoTag()->m_type == MediaTypeSeason && item->GetVideoInfoTag()->m_iSeason > 0) ||  // seasons without "all seasons" and "specials"
             item->GetVideoInfoTag()->m_type == MediaTypeEpisode ||        // episodes
             item->GetVideoInfoTag()->m_type == MediaTypeMusicVideo ||     // musicvideos
             item->GetVideoInfoTag()->m_type == "tag" ||                   // tags

commit 0bd69217580cf72bfaed1d8cfa1053a5f6a19eb5

videoinfotag: fix typo introduced in e6e2d0ace5cfe6191b8f7727ced1747854254ce4
Fixes the crash reported at
http://forum.kodi.tv/showthread.php?tid=125749&pid=2302058#pid2302058

xbmc/video/VideoInfoTag.cpp
@@ -771,7 +771,7 @@ void CVideoInfoTag::ParseNative(const TiXmlElement* movie, bool prioritise)
         namedSeason->Attribute("number", &seasonNumber) != nullptr)
       m_namedSeasons.insert(std::make_pair(seasonNumber, seasonName));
 
-    namedSeason = thumb->NextSiblingElement("namedseason");
+    namedSeason = namedSeason->NextSiblingElement("namedseason");
   }
 
   // cast