b190f664c76eca1071062a1d50bbd5dfdc0f426b
Max Kellermann <max@duempel.org>
2016-04-19 20:31:51 +0200

    pvr/PVRChannelGroup: include cleanup

--- xbmc/pvr/channels/PVRChannelGroup.cpp
+++ xbmc/pvr/channels/PVRChannelGroup.cpp
@@ -23,11 +23,13 @@
  * - use Observable here, so we can use event driven operations later
  */
 
+#include "PVRChannelGroup.h"
+#include "PVRChannelGroupsContainer.h"
+
 #include "Util.h"
 #include "dialogs/GUIDialogExtendedProgressBar.h"
 #include "epg/EpgContainer.h"
 #include "guilib/GUIWindowManager.h"
-#include "music/tags/MusicInfoTag.h"
 #include "settings/AdvancedSettings.h"
 #include "settings/lib/Setting.h"
 #include "settings/Settings.h"
@@ -38,9 +40,6 @@
 #include "pvr/PVRDatabase.h"
 #include "pvr/PVRManager.h"
 #include "pvr/addons/PVRClients.h"
-
-#include "PVRChannelGroup.h"
-#include "PVRChannelGroupsContainer.h"
 
 using namespace PVR;
 using namespace EPG;
