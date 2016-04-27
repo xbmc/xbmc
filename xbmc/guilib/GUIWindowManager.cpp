f76ff5b83fe78940de6c11052afc0655670861c7
phil65 <philipptemminghoff@gmx.de>
2016-04-27 23:38:06 +0200

    [gui] - change lock for CGUIWindowManager::Render() to CSingleLock

--- xbmc/guilib/GUIWindowManager.cpp
+++ xbmc/guilib/GUIWindowManager.cpp
@@ -1063,7 +1063,7 @@ void CGUIWindowManager::RenderEx() const
 bool CGUIWindowManager::Render()
 {
   assert(g_application.IsCurrentThread());
-  CSingleExit lock(g_graphicsContext);
+  CSingleLock lock(g_graphicsContext);
 
   CDirtyRegionList dirtyRegions = m_tracker.GetDirtyRegions();
 
