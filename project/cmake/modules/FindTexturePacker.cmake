b811a6f12bb6a91c0e722109ab6444d7ce12ecd1
Christian Fetzer <fetzer.ch@gmail.com>
2016-04-28 22:03:14 +0200

    [cmake/osx] Use TexturePacker from buildtools-native

--- project/cmake/modules/FindTexturePacker.cmake
+++ project/cmake/modules/FindTexturePacker.cmake
@@ -16,6 +16,13 @@ if(NOT TARGET TexturePacker::TexturePacker)
     add_executable(TexturePacker::TexturePacker IMPORTED GLOBAL)
     set_target_properties(TexturePacker::TexturePacker PROPERTIES
                                                        IMPORTED_LOCATION "${CORE_SOURCE_DIR}/tools/TexturePacker/TexturePacker.exe")
+  elseif(APPLE)
+    find_program(TEXTUREPACKER_EXECUTABLE NAMES TexturePacker)
+    if(TEXTUREPACKER_EXECUTABLE)
+      add_executable(TexturePacker::TexturePacker IMPORTED GLOBAL)
+      set_target_properties(TexturePacker::TexturePacker PROPERTIES
+                                                         IMPORTED_LOCATION "${TEXTUREPACKER_EXECUTABLE}")
+    endif()
   else()
     add_subdirectory(${CORE_SOURCE_DIR}/tools/depends/native/TexturePacker build/texturepacker)
     add_executable(TexturePacker::TexturePacker ALIAS TexturePacker)
