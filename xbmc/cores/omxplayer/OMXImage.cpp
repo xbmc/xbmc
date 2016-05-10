c8a3e68542beb399af9b6c84296b542348949458
popcornmix <popcornmix@gmail.com>
2016-05-10 18:26:08 +0100

    omximage: Fix resolution in logging message

--- xbmc/cores/omxplayer/OMXImage.cpp
+++ xbmc/cores/omxplayer/OMXImage.cpp
@@ -909,7 +909,7 @@ bool COMXImageFile::ReadFile(const std::string& inputFile, int orientation)
   OMX_IMAGE_CODINGTYPE eCompressionFormat = GetCodingType(m_width, m_height, orientation);
   if(eCompressionFormat != OMX_IMAGE_CodingJPEG || m_width < 1 || m_height < 1)
   {
-    CLog::Log(LOGDEBUG, "%s::%s %s GetCodingType=0x%x (%dx%x)\n", CLASSNAME, __func__, m_filename, eCompressionFormat, m_width, m_height);
+    CLog::Log(LOGDEBUG, "%s::%s %s GetCodingType=0x%x (%dx%d)\n", CLASSNAME, __func__, m_filename, eCompressionFormat, m_width, m_height);
     return false;
   }
 
