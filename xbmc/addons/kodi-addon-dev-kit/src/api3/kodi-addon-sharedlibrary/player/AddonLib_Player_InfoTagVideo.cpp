74c33d86b7ce00a9d45e81b88be704a1709721a1
Alwin Esch <alwin.esch@web.de>
2016-05-24 14:34:10 +0200

    [addon] add API level 3 add-on static library for future development

---
+++ xbmc/addons/kodi-addon-dev-kit/src/api3/kodi-addon-sharedlibrary/player/AddonLib_Player_InfoTagVideo.cpp
@@ -0,0 +1,205 @@
+/*
+ *      Copyright (C) 2016 Team KODI
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KODI; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "InterProcess.h"
+#include KITINCLUDE(ADDON_API_LEVEL, player/InfoTagVideo.hpp)
+#include KITINCLUDE(ADDON_API_LEVEL, player/Player.hpp)
+
+API_NAMESPACE
+
+namespace KodiAPI
+{
+
+namespace Player
+{
+
+  CInfoTagVideo::CInfoTagVideo(CPlayer* player)
+   : m_duration(0)
+  {
+    if (!player || !player->m_ControlHandle)
+    {
+      return;
+    }
+
+    AddonInfoTagVideo infoTag;
+    if (g_interProcess.m_Callbacks->AddonInfoTagVideo.GetFromPlayer(g_interProcess.m_Handle, player, &infoTag))
+    {
+      TransferInfoTag(infoTag);
+      g_interProcess.m_Callbacks->AddonInfoTagVideo.Release(g_interProcess.m_Handle, &infoTag);
+    }
+  }
+
+  CInfoTagVideo::~CInfoTagVideo()
+  {
+  }
+
+  const std::string& CInfoTagVideo::GetDirector() const
+  {
+    return m_director;
+  }
+
+  const std::string& CInfoTagVideo::GetWritingCredits() const
+  {
+    return m_writingCredits;
+  }
+
+  const std::string& CInfoTagVideo::GetGenre() const
+  {
+    return m_genre;
+  }
+
+  const std::string& CInfoTagVideo::GetCountry() const
+  {
+    return m_country;
+  }
+
+  const std::string& CInfoTagVideo::GetTagLine() const
+  {
+    return m_tagLine;
+  }
+
+  const std::string& CInfoTagVideo::GetPlotOutline() const
+  {
+    return m_plotOutline;
+  }
+
+  const std::string& CInfoTagVideo::GetPlot() const
+  {
+    return m_plot;
+  }
+
+  const std::string& CInfoTagVideo::GetTrailer() const
+  {
+    return m_trailer;
+  }
+
+  const std::string& CInfoTagVideo::GetPictureURL() const
+  {
+    return m_pictureURL;
+  }
+
+  const std::string& CInfoTagVideo::GetTitle() const
+  {
+    return m_title;
+  }
+
+  const std::string& CInfoTagVideo::GetVotes() const
+  {
+    return m_votes;
+  }
+
+  const std::string& CInfoTagVideo::GetCast() const
+  {
+    return m_cast;
+  }
+
+  const std::string& CInfoTagVideo::GetFile() const
+  {
+    return m_file;
+  }
+
+  const std::string& CInfoTagVideo::GetPath() const
+  {
+    return m_path;
+  }
+
+  const std::string& CInfoTagVideo::GetIMDBNumber() const
+  {
+    return m_IMDBNumber;
+  }
+
+  const std::string& CInfoTagVideo::GetMPAARating() const
+  {
+    return m_MPAARating;
+  }
+
+  int CInfoTagVideo::GetYear() const
+  {
+    return m_year;
+  }
+
+  double CInfoTagVideo::GetRating() const
+  {
+    return m_rating;
+  }
+
+  int CInfoTagVideo::GetPlayCount() const
+  {
+    return m_playCount;
+  }
+
+  const std::string& CInfoTagVideo::GetLastPlayed() const
+  {
+    return m_lastPlayed;
+  }
+
+  const std::string& CInfoTagVideo::GetOriginalTitle() const
+  {
+    return m_originalTitle;
+  }
+
+  const std::string& CInfoTagVideo::GetPremiered() const
+  {
+    return m_premiered;
+  }
+
+  const std::string& CInfoTagVideo::GetFirstAired() const
+  {
+    return m_firstAired;
+  }
+
+  unsigned int CInfoTagVideo::GetDuration() const
+  {
+    return m_duration;
+  }
+
+  void CInfoTagVideo::TransferInfoTag(AddonInfoTagVideo& infoTag)
+  {
+    m_director = infoTag.m_director;
+    m_writingCredits = infoTag.m_writingCredits;
+    m_genre = infoTag.m_genre;
+    m_country = infoTag.m_country;
+    m_tagLine = infoTag.m_tagLine;
+    m_plotOutline = infoTag.m_plotOutline;
+    m_plot = infoTag.m_plot;
+    m_trailer = infoTag.m_trailer;
+    m_pictureURL = infoTag.m_pictureURL;
+    m_title = infoTag.m_title;
+    m_votes = infoTag.m_votes;
+    m_cast = infoTag.m_cast;
+    m_file = infoTag.m_file;
+    m_path = infoTag.m_path;
+    m_IMDBNumber = infoTag.m_IMDBNumber;
+    m_MPAARating = infoTag.m_MPAARating;
+    m_year = infoTag.m_year;
+    m_rating = infoTag.m_rating;
+    m_playCount = infoTag.m_playCount;
+    m_lastPlayed = infoTag.m_lastPlayed;
+    m_originalTitle = infoTag.m_originalTitle;
+    m_premiered = infoTag.m_premiered;
+    m_firstAired = infoTag.m_firstAired;
+    m_duration = infoTag.m_duration;
+  }
+
+
+} /* namespace Player */
+} /* namespace KodiAPI */
+
+END_NAMESPACE()
---
+++ xbmc/addons/kodi-addon-dev-kit/src/api3/kodi-addon-sharedlibrary/pvr/AddonLib_PVR_Stream.cpp
@@ -0,0 +1,210 @@
+/*
+ *      Copyright (C) 2016 Team KODI
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with KODI; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#define API_LEVELS_ACTIVE 1
+#include <time.h>
+#include "InterProcess.h"
+#include KITINCLUDE(ADDON_API_LEVEL, AddonLib.hpp)
+#include KITINCLUDE(ADDON_API_LEVEL, pvr/StreamUtils.hpp)
+
+#include <string>
+#include <stdarg.h>
+
+API_NAMESPACE
+
+namespace KodiAPI
+{
+namespace PVR
+{
+
+  CPVRStream::CPVRStream()
+  {
+    Clear();
+  }
+
+  CPVRStream::CPVRStream(const CPVRStream &other)
+  {
+    memcpy(this, &other, sizeof(PVR_STREAM_PROPERTIES::PVR_STREAM));
+  }
+
+  CPVRStream& CPVRStream::operator=(const CPVRStream &other)
+  {
+    memcpy(this, &other, sizeof(PVR_STREAM_PROPERTIES::PVR_STREAM));
+    return *this;
+  }
+
+  bool CPVRStream::operator==(const CPVRStream &other) const
+  {
+    return iPID == other.iPID && iCodecId == other.iCodecId;
+  }
+
+  bool CPVRStream::operator<(const CPVRStream &other) const
+  {
+    if (iCodecType == API_NAMESPACE_NAME::KodiAPI::KODI_CODEC_TYPE_VIDEO)
+      return true;
+    else if (other.iCodecType != API_NAMESPACE_NAME::KodiAPI::KODI_CODEC_TYPE_VIDEO)
+      return iPID < other.iPID;
+    else
+      return false;
+  }
+
+  bool CPVRStream::IsCleared() const
+  {
+    return iCodecId   == KODI_INVALID_CODEC_ID &&
+           iCodecType == API_NAMESPACE_NAME::KodiAPI::KODI_CODEC_TYPE_UNKNOWN;
+  }
+
+  void CPVRStream::Clear()
+  {
+    memset(this, 0, sizeof(PVR_STREAM_PROPERTIES::PVR_STREAM));
+    iCodecId = KODI_INVALID_CODEC_ID;
+    iCodecType = API_NAMESPACE_NAME::KodiAPI::KODI_CODEC_TYPE_UNKNOWN;
+  }
+
+  /*\___________________________________________________________________________
+  \*/
+
+  CStreamProperties::CStreamProperties(void)
+  {
+    // make sure the vector won't have to resize itself later
+    m_streamVector = new stream_vector();
+    m_streamVector->reserve(PVR_STREAM_MAX_STREAMS);
+  }
+
+  CStreamProperties::~CStreamProperties(void)
+  {
+    delete m_streamVector;
+  }
+
+  void CStreamProperties::Clear(void)
+  {
+    m_streamVector->clear();
+    m_streamIndex.clear();
+  }
+
+  int CStreamProperties::GetStreamId(unsigned int iPID) const
+  {
+    std::map<unsigned int, int>::const_iterator it = m_streamIndex.find(iPID);
+    if (it != m_streamIndex.end())
+      return it->second;
+
+    return -1;
+  }
+
+  CPVRStream* CStreamProperties::GetStreamById(unsigned int iPID) const
+  {
+    int position = GetStreamId(iPID);
+    return position != -1 ? &m_streamVector->at(position) : NULL;
+  }
+
+  void CStreamProperties::GetStreamData(unsigned int iPID, CPVRStream* stream)
+  {
+    CPVRStream *foundStream = GetStreamById(iPID);
+    if (foundStream)
+      *stream = *foundStream;
+    else
+    {
+      stream->iSubtitleInfo = -1;
+      stream->iPID = iPID;
+    }
+  }
+
+  bool CStreamProperties::GetProperties(PVR_STREAM_PROPERTIES* props)
+  {
+    unsigned int i = 0;
+    for (stream_vector::const_iterator it = m_streamVector->begin();
+         it != m_streamVector->end(); ++it, ++i)
+    {
+      memcpy(&props->stream[i], &(*it), sizeof(PVR_STREAM_PROPERTIES::PVR_STREAM));
+    }
+
+    props->iStreamCount = m_streamVector->size();
+    return (props->iStreamCount > 0);
+  }
+
+  void CStreamProperties::UpdateStreams(stream_vector &newStreams)
+  {
+    // sort the new streams
+    std::sort(newStreams.begin(), newStreams.end());
+
+    // ensure we never have more than PVR_STREAMS_MAX_STREAMS streams
+    if (newStreams.size() > PVR_STREAM_MAX_STREAMS)
+    {
+      while (newStreams.size() > PVR_STREAM_MAX_STREAMS)
+        newStreams.pop_back();
+
+      KodiAPI::Log(ADDON_LOG_ERROR, "%s - max amount of streams reached", __FUNCTION__);
+    }
+
+    stream_vector::iterator newStreamPosition;
+    for (stream_vector::iterator it = m_streamVector->begin(); it != m_streamVector->end(); ++it)
+    {
+      newStreamPosition = std::find(newStreams.begin(), newStreams.end(), *it);
+
+      // if the current stream no longer exists we clear it, otherwise we
+      // copy it and remove it from newStreams
+      if (newStreamPosition == newStreams.end())
+        it->Clear();
+      else
+      {
+        *it = *newStreamPosition;
+        newStreams.erase(newStreamPosition);
+      }
+    }
+
+    // replace cleared streams with new streams
+    for (stream_vector::iterator it = m_streamVector->begin();
+         it != m_streamVector->end() && !newStreams.empty(); ++it)
+    {
+      if (it->IsCleared())
+      {
+        *it = newStreams.front();
+        newStreams.erase(newStreams.begin());
+      }
+    }
+
+    // append any remaining new streams
+    m_streamVector->insert(m_streamVector->end(), newStreams.begin(), newStreams.end());
+
+    // remove trailing cleared streams
+    while (m_streamVector->back().IsCleared())
+      m_streamVector->pop_back();
+
+    // update the index
+    UpdateIndex();
+  }
+
+  /**
+   * Updates the stream index
+   */
+  void CStreamProperties::UpdateIndex()
+  {
+    m_streamIndex.clear();
+
+    int i = 0;
+    for (stream_vector::const_iterator it = m_streamVector->begin(); it != m_streamVector->end(); ++it, ++i)
+      m_streamIndex[it->iPID] = i;
+  }
+
+
+} /* namespace PVR */
+} /* namespace KodiAPI */
+
+END_NAMESPACE()
