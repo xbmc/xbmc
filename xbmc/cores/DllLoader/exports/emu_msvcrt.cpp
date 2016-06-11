4f23feb4998efa041cafaf2196fc8fc96476e57d
Stefan Saraev <stefan@saraev.ca>
2016-06-11 10:54:25 +0300

    [cleanup] completely remove filereader protocol

--- xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
+++ xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
@@ -877,11 +877,6 @@ extern "C"
     while ((iDirSlot < MAX_OPEN_DIRS) && (vecDirsOpen[iDirSlot].curr_index != -1)) iDirSlot++;
     if (iDirSlot >= MAX_OPEN_DIRS)
       return -1; // no free slots
-    if (url.IsProtocol("filereader"))
-    {
-      CURL url2(url.GetFileName());
-      url = url2;
-    }
     strURL = url.Get();
     bVecDirsInited = true;
     vecDirsOpen[iDirSlot].items.Clear();
@@ -1000,12 +995,6 @@ extern "C"
     {
       CLog::Log(LOGDEBUG, "Dll: Max open dirs reached");
       return NULL; // no free slots
-    }
-
-    if (url.IsProtocol("filereader"))
-    {
-      CURL url2(url.GetFileName());
-      url = url2;
     }
 
     bVecDirsInited = true;
