0b96f616740e5ea916938911a058a453824a191f
Tobias Kortkamp <git@tobik.me>
2016-06-13 10:18:21 +0200

    Add sndio sink

--- xbmc/cores/AudioEngine/AESinkFactory.cpp
+++ xbmc/cores/AudioEngine/AESinkFactory.cpp
@@ -42,6 +42,10 @@
   #if defined(HAS_PULSEAUDIO)
     #include "Sinks/AESinkPULSE.h"
   #endif
+  #if defined(HAS_SNDIO)
+    #include "Sinks/AESinkSNDIO.h"
+  #endif
+  #include "Sinks/AESinkOSS.h"
 #else
   #pragma message("NOTICE: No audio sink for target platform.  Audio output will not be available.")
 #endif
@@ -83,6 +87,10 @@ void CAESinkFactory::ParseDevice(std::string &device, std::string &driver)
   #if defined(HAS_PULSEAUDIO)
         driver == "PULSE"       ||
   #endif
+  #if defined(HAS_SNDIO)
+        driver == "SNDIO"       ||
+  #endif
+        driver == "OSS"         ||
 #endif
         driver == "PROFILER"    ||
         driver == "NULL")
@@ -120,7 +128,7 @@ IAESink *CAESinkFactory::TrySink(std::string &driver, std::string &device, AEAud
     sink = new CAESinkDARWINIOS();
 #elif defined(TARGET_DARWIN_OSX)
     sink = new CAESinkDARWINOSX();
-#elif defined(TARGET_LINUX) || defined(TARGET_FREEBSD)
+#elif defined(TARGET_LINUX) || defined(TARGET_FREEBSD) || defined(TARGET_OPENBSD)
  #if defined(HAS_PULSEAUDIO)
     if (driver == "PULSE")
       sink = new CAESinkPULSE();
@@ -128,6 +136,10 @@ IAESink *CAESinkFactory::TrySink(std::string &driver, std::string &device, AEAud
  #if defined(HAS_ALSA)
     if (driver == "ALSA")
       sink = new CAESinkALSA();
+ #endif
+ #if defined(HAS_SNDIO)
+    if (driver == "SNDIO")
+      sink = new CAESinkSNDIO();
  #endif
  #if defined(TARGET_FREEBSD)
     if (driver == "OSS")
@@ -246,6 +258,10 @@ void CAESinkFactory::EnumerateEx(AESinkInfoList &list, bool force)
     if (envSink == "ALSA")
       CAESinkALSA::EnumerateDevicesEx(info.m_deviceInfoList, force);
     #endif
+    #if defined(HAS_SNDIO)
+    if (envSink == "SNDIO")
+        CAESinkSNDIO::EnumerateDevicesEx(info.m_deviceInfoList, force);
+    #endif
     #if defined(TARGET_FREEBSD)
     if (envSink == "OSS")
       CAESinkOSS::EnumerateDevicesEx(info.m_deviceInfoList, force);
@@ -282,6 +298,18 @@ void CAESinkFactory::EnumerateEx(AESinkInfoList &list, bool force)
     return;
   }
   #endif
+
+  #if defined(HAS_SNDIO)
+  info.m_deviceInfoList.clear();
+  info.m_sinkName = "SNDIO";
+  CAESinkSNDIO::EnumerateDevicesEx(info.m_deviceInfoList, force);
+  if(!info.m_deviceInfoList.empty())
+  {
+    list.push_back(info);
+    return;
+  }
+  #endif
+
   #if defined(TARGET_FREEBSD)
   info.m_deviceInfoList.clear();
   info.m_sinkName = "OSS";
---
+++ xbmc/cores/AudioEngine/Sinks/AESinkSNDIO.h
@@ -0,0 +1,57 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
+#pragma once
+/*
+ *      Copyright (C) 2010-2013 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "cores/AudioEngine/Interfaces/AESink.h"
+#include "cores/AudioEngine/Utils/AEDeviceInfo.h"
+#include <stdint.h>
+#include <sndio.h>
+
+#include "threads/CriticalSection.h"
+
+class CAESinkSNDIO : public IAESink
+{
+public:
+  virtual const char *GetName() { return "sndio"; }
+
+  CAESinkSNDIO();
+  virtual ~CAESinkSNDIO();
+
+  virtual bool Initialize(AEAudioFormat &format, std::string &device);
+  virtual void Deinitialize();
+
+  virtual void Stop();
+  virtual void GetDelay(AEDelayStatus& status);
+  virtual double GetCacheTotal() { return 0.0; }
+  virtual unsigned int AddPackets(uint8_t **data, unsigned int frames, unsigned int offset);
+  virtual void Drain();
+  static void EnumerateDevicesEx(AEDeviceInfoList &list, bool force = false);
+private:
+  void AudioFormatToPar(AEAudioFormat& format);
+  bool ParToAudioFormat(AEAudioFormat& format);
+  static void OnmoveCb(void *arg, int delta);
+
+  struct sio_hdl *m_hdl;
+  struct sio_par m_par;
+  ssize_t m_played;
+  ssize_t m_written;
+};
+
