3bd3e4908cc35c1daccbd20da6486fbff7666ab0
fritsch <Peter.Fruehberger@gmail.com>
2016-06-28 12:26:59 +0200

    ActiveAE: Handle failrures separately (squash)

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.cpp
@@ -3036,17 +3036,7 @@ IAESound *CActiveAE::MakeSound(const std::string& file)
       else
         sent = -1;
 
-      if (sent != -1)
-      {
-        receive_frame_error = avcodec_receive_frame(dec_ctx, decoded_frame);
-
-        if (receive_frame_error >= 0)
-          got_frame = 1;
-        else if (receive_frame_error != AVERROR(EAGAIN))
-          received = -1;
-      }
-
-      if (sent == -1 || received == -1)
+      if (sent == -1)
       {
         av_frame_free(&decoded_frame);
         avcodec_free_context(&dec_ctx);
@@ -3058,6 +3048,30 @@ IAESound *CActiveAE::MakeSound(const std::string& file)
         }
         delete sound;
         return NULL;
+      }
+
+      if (sent != -1)
+      {
+        receive_frame_error = avcodec_receive_frame(dec_ctx, decoded_frame);
+
+        if (receive_frame_error >= 0)
+          got_frame = 1;
+        else if (receive_frame_error != AVERROR(EAGAIN))
+          received = -1;
+
+        if (received == -1)
+        {
+          av_frame_free(&decoded_frame);
+          avcodec_free_context(&dec_ctx);
+          avformat_close_input(&fmt_ctx);
+          if (io_ctx)
+          {
+            av_freep(&io_ctx->buffer);
+            av_freep(&io_ctx);
+          }
+          delete sound;
+          return NULL;
+        }
       }
       if (got_frame)
       {
