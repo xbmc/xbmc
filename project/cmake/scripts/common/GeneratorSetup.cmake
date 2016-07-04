3f855dcf78858a2d531c272309028303e4bc11e7
Christian Fetzer <fetzer.ch@gmail.com>
2016-07-04 22:29:38 +0200

    [cmake] Print CMake version and add some warnings
    CMake 3.5.1 suffers from a crash at configure time that happens on some
    Systems: http://cmake.org/Bug/view.php?id=16044
    This is fixed in 3.5.2.
    
    Darwin requires CMake at least 3.4 or the usage of the patched version
    in depends.

--- project/cmake/scripts/common/GeneratorSetup.cmake
+++ project/cmake/scripts/common/GeneratorSetup.cmake
@@ -23,6 +23,21 @@ else()
   message(STATUS "Generator: Single-configuration: ${CMAKE_BUILD_TYPE} (${CMAKE_GENERATOR})")
 endif()
 
+# Print CMake version
+message(STATUS "CMake Version: ${CMAKE_VERSION}")
+
+# Deal with CMake special cases
+if(CMAKE_VERSION VERSION_EQUAL 3.5.1)
+  message(WARNING "CMake 3.5.1 introduced a crash during configuration. "
+                  "Please consider upgrading to 3.5.2 (cmake.org/Bug/view.php?id=16044)")
+endif()
+
+# Darwin needs CMake 3.4
+if(APPLE AND CMAKE_VERSION VERSION_LESS 3.4)
+  message(WARNING "Build on Darwin requires CMake 3.4 or later (tdb library support) "
+                  "or the usage of the patched version in depends.")
+endif()
+
 # Ninja needs CMake 3.2 due to ExternalProject BUILD_BYPRODUCTS usage
 if(CMAKE_GENERATOR STREQUAL Ninja AND CMAKE_VERSION VERSION_LESS 3.2)
   message(FATAL_ERROR "Generator: Ninja requires CMake 3.2 or later")
