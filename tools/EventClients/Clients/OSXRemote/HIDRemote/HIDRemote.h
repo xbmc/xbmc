fbadb29dc42d1cb4d723eececbe9e74731681402
Memphiz <memphis@machzwo.de>
2016-07-09 21:51:18 +0200

    [XBMCHelper] - updated HIDRemote class to version 1.4

--- tools/EventClients/Clients/OSXRemote/HIDRemote/HIDRemote.h
+++ tools/EventClients/Clients/OSXRemote/HIDRemote/HIDRemote.h
@@ -1,16 +1,16 @@
 //
 //  HIDRemote.h
-//  HIDRemote V1.1.1
+//  HIDRemote V1.4 (18th February 2015)
 //
 //  Created by Felix Schwarz on 06.04.07.
-//  Copyright 2007-2009 IOSPIRIT GmbH. All rights reserved.
+//  Copyright 2007-2015 IOSPIRIT GmbH. All rights reserved.
 //
 //  The latest version of this class is available at
 //     http://www.iospirit.com/developers/hidremote/
 //
 //  ** LICENSE *************************************************************************
 //
-//  Copyright (c) 2007-2009 IOSPIRIT GmbH (http://www.iospirit.com/)
+//  Copyright (c) 2007-2014 IOSPIRIT GmbH (http://www.iospirit.com/)
 //  All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without modification,
@@ -41,7 +41,6 @@
 //  ************************************************************************************
 
 
-
 //  ************************************************************************************
 //  ********************************** DOCUMENTATION ***********************************
 //  ************************************************************************************
@@ -51,10 +50,22 @@
 //
 //  ************************************************************************************
 
-
 #import <Cocoa/Cocoa.h>
 
+// For legacy SDKs
+#ifndef MAC_OS_X_VERSION_10_9
+#define MAC_OS_X_VERSION_10_9 1090
+#endif /* MAC_OS_X_VERSION_10_9 */
+
+#ifndef MAC_OS_X_VERSION_10_10
+#define MAC_OS_X_VERSION_10_10 101000
+#endif /* MAC_OS_X_VERSION_10_10 */
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_10
+// Carbon is only required on OS X versions prior to 10.10 (for getting the OS version via Gestalt() -
+// replaced by [[NSProcessInfo processInfo] operatingSystemVersion] in 10.10)
 #include <Carbon/Carbon.h>
+#endif
 
 #include <unistd.h>
 #include <mach/mach.h>
@@ -241,11 +252,27 @@ typedef enum
 	BOOL _secureEventInputWorkAround;
 	UInt64 _lastSecureEventInputPIDSum;
 	uid_t _lastFrontUserSession;
+	BOOL _lastScreenIsLocked;
 	
 	// Exclusive lock lending
 	BOOL _exclusiveLockLending;
+	BOOL _sendExclusiveResourceReuseNotification;
 	NSNumber *_waitForReturnByPID;
 	NSNumber *_returnToPID;
+	BOOL _isRestarting;
+	
+	// Status notifications
+	BOOL _sendStatusNotifications;
+	NSString *_pidString;
+	
+	// Status
+	BOOL _applicationIsTerminating;
+	BOOL _isStopping;
+	
+	// Thread safety
+	#ifdef HIDREMOTE_THREADSAFETY_HARDENED_NOTIFICATION_HANDLING /* #define HIDREMOTE_THREADSAFETY_HARDENED_NOTIFICATION_HANDLING if you're running your HIDRemote instance on a background thread (requires OS X 10.5 or later) */
+	NSThread *_runOnThread;
+	#endif
 }
 
 #pragma mark -- PUBLIC: Shared HID Remote --
@@ -254,6 +281,7 @@ typedef enum
 #pragma mark -- PUBLIC: System Information --
 + (BOOL)isCandelairInstalled;
 + (BOOL)isCandelairInstallationRequiredForRemoteMode:(HIDRemoteMode)remoteMode;
++ (SInt32)OSXVersion;
 - (HIDRemoteAluminumRemoteSupportLevel)aluminiumRemoteSystemSupportLevel;
 
 #pragma mark -- PUBLIC: Interface / API --
@@ -261,6 +289,7 @@ typedef enum
 - (void)stopRemoteControl;
 
 - (BOOL)isStarted;
+- (HIDRemoteMode)startedInMode;
 
 - (unsigned)activeRemoteControlCount;
 
@@ -285,6 +314,9 @@ typedef enum
 - (void)setExclusiveLockLendingEnabled:(BOOL)newExclusiveLockLendingEnabled;
 - (BOOL)exclusiveLockLendingEnabled;
 
+- (BOOL)isApplicationTerminating;
+- (BOOL)isStopping;
+
 #pragma mark -- PRIVATE: HID Event handling --
 - (void)_handleButtonCode:(HIDRemoteButtonCode)buttonCode isPressed:(BOOL)isPressed hidAttribsDict:(NSMutableDictionary *)hidAttribsDict;
 - (void)_sendButtonCode:(HIDRemoteButtonCode)buttonCode isPressed:(BOOL)isPressed hidAttribsDict:(NSMutableDictionary *)hidAttribsDict;
@@ -299,6 +331,8 @@ typedef enum
 #pragma mark -- PRIVATE: Distributed notifiations handling --
 - (void)_postStatusWithAction:(NSString *)action;
 - (void)_handleNotifications:(NSNotification *)notification;
+- (void)_setSendStatusNotifications:(BOOL)doSend;
+- (BOOL)_sendStatusNotifications;
 
 #pragma mark -- PRIVATE: Application becomes active / inactive handling for kHIDRemoteModeExclusiveAuto --
 - (void)_appStatusChanged:(NSNotification *)notification;
@@ -335,6 +369,8 @@ extern NSString *kHIDRemoteAluminumRemoteSupportOnDemand;
 extern NSString *kHIDRemoteDNHIDRemotePing;
 extern NSString *kHIDRemoteDNHIDRemoteRetry;
 extern NSString *kHIDRemoteDNHIDRemoteStatus;
+
+extern NSString *kHIDRemoteDNHIDRemoteRetryGlobalObject;
 
 #pragma mark -- Distributed notifications userInfo keys and values --
 extern NSString *kHIDRemoteDNStatusHIDRemoteVersionKey;
