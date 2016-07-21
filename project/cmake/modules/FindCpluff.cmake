xbmc/xbmc

commit 810895469a621c2eb25dbb2ef4aba75ecf067c8b

[cmake] do not hardcode make command
use CMAKE_MAKE_PROGRAM variable to fix FreeBSD build

project/cmake/modules/FindCpluff.cmake
@@ -22,7 +22,7 @@ if(NOT WIN32)
                                         --host=${ARCH}
                                         CFLAGS=${defines}
                                         LDFLAGS=${ldflags}
-                      BUILD_COMMAND make V=1)
+                      BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} V=1)
   ExternalProject_Add_Step(libcpluff autoreconf
                                      DEPENDEES download update patch
                                      DEPENDERS configure

tools/depends/target/ffmpeg/CMakeLists.txt
@@ -82,7 +82,7 @@ externalproject_add(ffmpeg
                       --enable-encoder=png
                       --enable-encoder=mjpeg
                       ${ffmpeg_conf}
-                    BUILD_COMMAND make ${PARALLEL_FLAGS})
+                    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} ${PARALLEL_FLAGS})
 
 install(CODE "Message(Done)")
 

commit f3c66856dd71c9145e3c1955fe106af974bd7bf4

[cmake] Let ExternalProject_Add figure out the right 'make' command
ExternalProject.cmake sets a default BUILD_COMMAND to 'make'. Not
overriding it has the advantage that it figures out the best option
itself (including passing down parallel build parameters).

This fixes the Ninja build on Linux due to a problem introduced in
810895469a621c2eb25dbb2ef4aba75ecf067c8b where ninja would be used to
build ffmpeg and cpluff (which doesn't work).

project/cmake/modules/FindCpluff.cmake
@@ -21,8 +21,7 @@ if(NOT WIN32)
                                         --prefix=<INSTALL_DIR>
                                         --host=${ARCH}
                                         CFLAGS=${defines}
-                                        LDFLAGS=${ldflags}
-                      BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} V=1)
+                                        LDFLAGS=${ldflags})
   ExternalProject_Add_Step(libcpluff autoreconf
                                      DEPENDEES download update patch
                                      DEPENDERS configure

tools/depends/target/ffmpeg/CMakeLists.txt
@@ -48,12 +48,6 @@ if(GNUTLS_FOUND)
   list(APPEND ffmpeg_conf --enable-gnutls)
 endif()
 
-include(ProcessorCount)
-ProcessorCount(N)
-if(NOT N EQUAL 0)
-  set(PARALLEL_FLAGS -j${N})
-endif()
-
 include(ExternalProject)
 externalproject_add(ffmpeg
                     SOURCE_DIR ${CMAKE_SOURCE_DIR}
@@ -81,8 +75,7 @@ externalproject_add(ffmpeg
                       --enable-protocol=http
                       --enable-encoder=png
                       --enable-encoder=mjpeg
-                      ${ffmpeg_conf}
-                    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} ${PARALLEL_FLAGS})
+                      ${ffmpeg_conf})
 
 install(CODE "Message(Done)")
 