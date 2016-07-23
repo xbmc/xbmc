2e6aa8ad2205f0cfa056e6dfb0eb8a08d33a80e3
Christian Fetzer <fetzer.ch@gmail.com>
2016-07-23 20:22:46 +0200

    [cmake/linux] Move WiiRemote target to ExtraTargets.cmake

--- project/cmake/scripts/linux/ExtraTargets.cmake
+++ project/cmake/scripts/linux/ExtraTargets.cmake
@@ -3,3 +3,8 @@ if(ENABLE_X11 AND XRANDR_FOUND)
   add_executable(${APP_NAME_LC}-xrandr ${CORE_SOURCE_DIR}/xbmc-xrandr.c)
   target_link_libraries(kodi-xrandr ${SYSTEM_LDFLAGS} ${X_LIBRARIES} m ${XRANDR_LIBRARIES})
 endif()
+
+# WiiRemote
+if(ENABLE_EVENTCLIENTS AND CORE_SYSTEM_NAME STREQUAL linux)
+  add_subdirectory(${CORE_SOURCE_DIR}/tools/EventClients/Clients/WiiRemote build/WiiRemote)
+endif()
