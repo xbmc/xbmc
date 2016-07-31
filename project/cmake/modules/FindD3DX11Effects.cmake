212e51b5620a819c6bb837dab0de1e4fa64f14d4
Christian Fetzer <fetzer.ch@gmail.com>
2016-07-31 20:06:46 +0200

    [cmake/win32] Build Effects11 with CMake

--- project/cmake/modules/FindD3DX11Effects.cmake
+++ project/cmake/modules/FindD3DX11Effects.cmake
@@ -1,30 +1,8 @@
-# - Builds D3DX11Effects as external project
+# - Finds D3DX11 dependencies
 # Once done this will define
 #
-# D3DX11EFFECTS_FOUND - system has D3DX11Effects
-# D3DX11EFFECTS_INCLUDE_DIRS - the D3DX11Effects include directories
 # D3DCOMPILER_DLL - Path to the Direct3D Compiler
-
-include(ExternalProject)
-set(D3DX11EFFECTS_LIBRARY_RELEASE ${CORE_SOURCE_DIR}/lib/win32/Effects11/libs/Effects11/Release/Effects11.lib)
-set(D3DX11EFFECTS_LIBRARY_DEBUG ${CORE_SOURCE_DIR}/lib/win32/Effects11/libs/Effects11/Debug/Effects11.lib)
-ExternalProject_Add(d3dx11effects
-            SOURCE_DIR ${CORE_SOURCE_DIR}/lib/win32/Effects11
-            PREFIX ${CORE_BUILD_DIR}/Effects11
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND msbuild ${CORE_SOURCE_DIR}/lib/win32/Effects11/Effects11_2013.sln
-                                  /t:Effects11 /p:Configuration=${CORE_BUILD_CONFIG}
-            INSTALL_COMMAND ""
-            BUILD_BYPRODUCTS ${D3DX11EFFECTS_LIBRARY_RELEASE} ${D3DX11EFFECTS_LIBRARY_DEBUG})
-set_target_properties(d3dx11effects PROPERTIES FOLDER "External Projects")
-
-set(D3DX11EFFECTS_FOUND 1)
-set(D3DX11EFFECTS_INCLUDE_DIRS ${CORE_SOURCE_DIR}/lib/win32/Effects11/inc)
-
-include(SelectLibraryConfigurations)
-select_library_configurations(D3DX11EFFECTS)
-
-mark_as_advanced(D3DX11EFFECTS_FOUND)
+# FXC - Path to the DirectX Effects Compiler (FXC)
 
 find_file(D3DCOMPILER_DLL
           NAMES d3dcompiler_47.dll d3dcompiler_46.dll
