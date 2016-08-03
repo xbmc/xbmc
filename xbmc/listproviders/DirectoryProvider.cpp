xbmc/xbmc

commit 2702a31f6dc1e9b6d9aaab56bc52f0825675d951

[addons] fix event handling

xbmc/listproviders/DirectoryProvider.cpp
@@ -260,6 +260,19 @@ void CDirectoryProvider::Fetch(std::vector<CGUIListItemPtr> &items) const
   }
 }
 
+void CDirectoryProvider::OnEvent(const ADDON::AddonEvent& event)
+{
+  CSingleLock lock(m_section);
+  if (URIUtils::IsProtocol(m_currentUrl, "addons"))
+  {
+    if (typeid(event) == typeid(ADDON::AddonEvents::Enabled) ||
+        typeid(event) == typeid(ADDON::AddonEvents::Disabled) ||
+        typeid(event) == typeid(ADDON::AddonEvents::InstalledChanged) ||
+        typeid(event) == typeid(ADDON::AddonEvents::MetadataChanged))
+    m_updateState = PENDING;
+  }
+}
+
 void CDirectoryProvider::Reset(bool immediately /* = false */)
 {
   // cancel any pending jobs
@@ -278,7 +291,7 @@ void CDirectoryProvider::Reset(bool immediately /* = false */)
     m_currentSort.sortOrder = SortOrderAscending;
     m_currentLimit = 0;
     m_updateState = OK;
-    RegisterListProvider(false);
+    RegisterListProvider();
   }
 }
 
@@ -365,17 +378,20 @@ void CDirectoryProvider::FireJob()
   m_jobID = CJobManager::GetInstance().AddJob(new CDirectoryJob(m_currentUrl, m_currentSort, m_currentLimit, m_parentID), this);
 }
 
-void CDirectoryProvider::RegisterListProvider(bool hasLibraryContent)
+void CDirectoryProvider::RegisterListProvider()
 {
-  if (hasLibraryContent && !m_isAnnounced)
+  CSingleLock lock(m_section);
+  if (!m_isAnnounced)
   {
     m_isAnnounced = true;
     CAnnouncementManager::GetInstance().AddAnnouncer(this);
+    ADDON::CAddonMgr::GetInstance().Events().Subscribe(this, &CDirectoryProvider::OnEvent);
   }
-  else if (!hasLibraryContent && m_isAnnounced)
+  else if (m_isAnnounced)
   {
     m_isAnnounced = false;
     CAnnouncementManager::GetInstance().RemoveAnnouncer(this);
+    ADDON::CAddonMgr::GetInstance().Events().Unsubscribe(this);
   }
 }
 
@@ -387,9 +403,7 @@ bool CDirectoryProvider::UpdateURL()
 
   m_currentUrl = value;
 
-  // Register this provider only if we have library content
-  RegisterListProvider(URIUtils::IsLibraryContent(m_currentUrl));
-
+  RegisterListProvider();
   return true;
 }
 

xbmc/listproviders/DirectoryProvider.h
@@ -22,7 +22,7 @@
 
 #include <string>
 #include <vector>
-
+#include "addons/AddonEvents.h"
 #include "IListProvider.h"
 #include "guilib/GUIStaticItem.h"
 #include "utils/Job.h"
@@ -85,8 +85,9 @@ private:
   CCriticalSection m_section;
 
   void FireJob();
-  void RegisterListProvider(bool hasLibraryContent);
+  void RegisterListProvider();
   bool UpdateURL();
   bool UpdateLimit();
   bool UpdateSort();
+  void OnEvent(const ADDON::AddonEvent& event);
 };

Kodi.xcodeproj/project.pbxproj
@@ -2572,6 +2572,9 @@
 		2A4AAD251CEBC2060057AD44 /* DVDDemuxVobsub.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DVDDemuxVobsub.h; sourceTree = "<group>"; };
 		2A4AAD261CEBC2130057AD44 /* DVDFactoryDemuxer.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DVDFactoryDemuxer.h; sourceTree = "<group>"; };
 		2A4AAD281CEBC64A0057AD44 /* GUIInfoLabels.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = GUIInfoLabels.h; sourceTree = "<group>"; };
+		2A5531F81D4A0F9C00BDCE99 /* AddonEvents.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = AddonEvents.h; sourceTree = "<group>"; };
+		2A5531F91D4A0FEB00BDCE99 /* EventStream.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = EventStream.h; sourceTree = "<group>"; };
+		2A5531FA1D4A0FEB00BDCE99 /* EventStreamDetail.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = EventStreamDetail.h; sourceTree = "<group>"; };
 		2A7B2BDB1BD6F16600044BCD /* PVRSettings.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = PVRSettings.cpp; sourceTree = "<group>"; };
 		2A7B2BDE1BD6F18B00044BCD /* PVRSettings.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = PVRSettings.h; sourceTree = "<group>"; };
 		2A7E24C71CEBBC3B003096EB /* libKODI_peripheral.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = libKODI_peripheral.h; path = "kodi-addon-dev-kit/include/kodi/libKODI_peripheral.h"; sourceTree = "<group>"; };
@@ -5112,6 +5115,7 @@
 				18B7C3821294203F009E7A26 /* AddonDatabase.cpp */,
 				18B7C3831294203F009E7A26 /* AddonDatabase.h */,
 				18B49FF31152BFA5001AF8A6 /* AddonDll.h */,
+				2A5531F81D4A0F9C00BDCE99 /* AddonEvents.h */,
 				7C4705AC12EF584C00369E51 /* AddonInstaller.cpp */,
 				7C4705AD12EF584C00369E51 /* AddonInstaller.h */,
 				18B49FF41152BFA5001AF8A6 /* AddonManager.cpp */,
@@ -8691,6 +8695,8 @@
 				436B38F3106628850049AB3B /* EndianSwap.h */,
 				DF529BAC1741697B00523FB4 /* Environment.cpp */,
 				DF529BAD1741697B00523FB4 /* Environment.h */,
+				2A5531F91D4A0FEB00BDCE99 /* EventStream.h */,
+				2A5531FA1D4A0FEB00BDCE99 /* EventStreamDetail.h */,
 				E36C29E90DA72486001F0C9D /* Fanart.cpp */,
 				6E97BDC30DA2B620003A2A89 /* Fanart.h */,
 				F5F244641110DC6B009126C6 /* FileOperationJob.cpp */,

project/VS2010Express/XBMC.vcxproj
@@ -2329,6 +2329,8 @@ copy "..\Win32BuildSetup\dependencies\python27.dll" "$(TargetDir)"</Command>
     <ClInclude Include="..\..\xbmc\utils\Crc32.h" />
     <ClInclude Include="..\..\xbmc\utils\DatabaseUtils.h" />
     <ClInclude Include="..\..\xbmc\utils\EndianSwap.h" />
+    <ClInclude Include="..\..\xbmc\utils\EventStream.h" />
+    <ClInclude Include="..\..\xbmc\utils\EventStreamDetail.h" />
     <ClInclude Include="..\..\xbmc\utils\Fanart.h" />
     <ClInclude Include="..\..\xbmc\utils\FileOperationJob.h" />
     <ClInclude Include="..\..\xbmc\utils\FileUtils.h" />
@@ -2562,6 +2564,7 @@ copy "..\Win32BuildSetup\dependencies\python27.dll" "$(TargetDir)"</Command>
     <ClInclude Include="..\..\xbmc\FileSystem\VideoDatabaseDirectory\QueryParams.h" />
     <ClInclude Include="..\..\xbmc\addons\Addon.h" />
     <ClInclude Include="..\..\xbmc\addons\AddonDll.h" />
+    <ClInclude Include="..\..\xbmc\addons\AddonEvents.h" />
     <ClInclude Include="..\..\xbmc\addons\AddonManager.h" />
     <ClInclude Include="..\..\xbmc\addons\AddonStatusHandler.h" />
     <ClInclude Include="..\..\xbmc\addons\AudioEncoder.h" />

project/VS2010Express/XBMC.vcxproj.filters
@@ -3865,6 +3865,9 @@
     <ClInclude Include="..\..\xbmc\addons\AddonDll.h">
       <Filter>addons</Filter>
     </ClInclude>
+    <ClInclude Include="..\..\xbmc\addons\AddonEvents.h">
+      <Filter>addons</Filter>
+    </ClInclude>
     <ClInclude Include="..\..\xbmc\addons\AddonManager.h">
       <Filter>addons</Filter>
     </ClInclude>
@@ -4540,6 +4543,12 @@
       <Filter>utils</Filter>
     </ClInclude>
     <ClInclude Include="..\..\xbmc\utils\EndianSwap.h">
+      <Filter>utils</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\xbmc\utils\EventStream.h">
+      <Filter>utils</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\xbmc\utils\EventStreamDetail.h">
       <Filter>utils</Filter>
     </ClInclude>
     <ClInclude Include="..\..\xbmc\utils\Fanart.h">

xbmc/addons/AddonEvents.h
@@ -17,33 +17,37 @@
  *  <http://www.gnu.org/licenses/>.
  *
  */
-
 #pragma once
 
-#include "utils/Observer.h"
-#include "Addon.h"
-#include "AddonEvents.h"
-#include "threads/CriticalSection.h"
-#include <map>
-#include <vector>
+#include <string>
 
-namespace ADDON {
-
-class CBinaryAddonCache
+namespace ADDON
 {
-public:
-  virtual ~CBinaryAddonCache();
-  void Init();
-  void Deinit();
-  void GetAddons(VECADDONS& addons, const TYPE& type);
+  struct AddonEvent
+  {
+    virtual ~AddonEvent() {};
+  };
 
-protected:
-  void Update();
-  void OnEvent(const AddonEvent& event);
-  
-  CCriticalSection m_critSection;
-  std::multimap<TYPE, VECADDONS> m_addons;
-  std::vector<TYPE> m_addonsToCache;
+  namespace AddonEvents
+  {
+    struct Enabled : AddonEvent
+    {
+      std::string id;
+      Enabled(std::string id) : id(std::move(id)) {}
+    };
+
+    struct Disabled : AddonEvent
+    {
+      std::string id;
+      Disabled(std::string id) : id(std::move(id)) {}
+    };
+
+    struct MetadataChanged : AddonEvent
+    {
+      std::string id;
+      MetadataChanged(std::string id) : id(std::move(id)) {}
+    };
+
+    struct InstalledChanged : AddonEvent {};
+  };
 };
-
-}

xbmc/addons/AddonManager.cpp
@@ -736,6 +736,8 @@ bool CAddonMgr::FindAddonsAndNotify()
 
   NotifyObservers(ObservableMessageAddons);
 
+  m_events.Publish(AddonEvents::InstalledChanged());
+
   return true;
 }
 
@@ -780,6 +782,18 @@ bool CAddonMgr::IsBlacklisted(const std::string& id) const
   return m_updateBlacklist.find(id) != m_updateBlacklist.end();
 }
 
+void CAddonMgr::UpdateLastUsed(const std::string& id)
+{
+  auto time = CDateTime::GetCurrentDateTime();
+  CJobManager::GetInstance().Submit([this, id, time](){
+    {
+      CSingleLock lock(m_critSection);
+      m_database.SetLastUsed(id, time);
+    }
+    m_events.Publish(AddonEvents::MetadataChanged(id));
+  });
+}
+
 static void ResolveDependencies(const std::string& addonId, std::vector<std::string>& needed, std::vector<std::string>& missing)
 {
   if (std::find(needed.begin(), needed.end(), addonId) != needed.end())
@@ -809,12 +823,14 @@ bool CAddonMgr::DisableAddon(const std::string& id)
   if (!m_disabled.insert(id).second)
     return false;
 
+  //success
+  ADDON::OnDisabled(id);
+
   AddonPtr addon;
   if (GetAddon(id, addon, ADDON_UNKNOWN, false) && addon != NULL)
     CEventLog::GetInstance().Add(EventPtr(new CAddonManagementEvent(addon, 24141)));
 
-  //success
-  ADDON::OnDisabled(id);
+  m_events.Publish(AddonEvents::Disabled(id));
   return true;
 }
 
@@ -833,11 +849,14 @@ bool CAddonMgr::EnableSingle(const std::string& id)
     CEventLog::GetInstance().Add(EventPtr(new CAddonManagementEvent(addon, 24064)));
 
   CLog::Log(LOGDEBUG, "CAddonMgr: enabled %s", addon->ID().c_str());
+  m_events.Publish(AddonEvents::Enabled(id));
   return true;
 }
 
 bool CAddonMgr::EnableAddon(const std::string& id)
 {
+  if (id.empty() || !IsAddonInstalled(id))
+    return false;
   std::vector<std::string> needed;
   std::vector<std::string> missing;
   ResolveDependencies(id, needed, missing);
@@ -846,6 +865,7 @@ bool CAddonMgr::EnableAddon(const std::string& id)
         "correctly", dep.c_str(), id.c_str());
   for (auto it = needed.rbegin(); it != needed.rend(); ++it)
     EnableSingle(*it);
+
   return true;
 }
 

xbmc/addons/AddonManager.h
@@ -19,14 +19,17 @@
  *
  */
 #include "Addon.h"
+#include "AddonDatabase.h"
+#include "AddonEvents.h"
+#include "Repository.h"
 #include "threads/CriticalSection.h"
 #include "utils/Observer.h"
+#include "utils/EventStream.h"
 #include <string>
 #include <vector>
 #include <map>
 #include <deque>
-#include "AddonDatabase.h"
-#include "Repository.h"
+
 
 class DllLibCPluff;
 extern "C"
@@ -75,6 +78,7 @@ namespace ADDON
     CAddonMgr const& operator=(CAddonMgr const&);
     virtual ~CAddonMgr();
 
+    CEventStream<AddonEvent>& Events() { return m_events; }
 
     IAddonMgrCallback* GetCallbackForType(TYPE type);
     bool RegisterAddonMgrCallback(TYPE type, IAddonMgrCallback* cb);
@@ -182,6 +186,8 @@ namespace ADDON
     bool RemoveFromUpdateBlacklist(const std::string& id);
     bool IsBlacklisted(const std::string& id) const;
 
+    void UpdateLastUsed(const std::string& id);
+
     /* libcpluff */
     std::string GetExtValue(cp_cfg_element_t *base, const char *path) const;
 
@@ -266,6 +272,7 @@ namespace ADDON
     static std::map<TYPE, IAddonMgrCallback*> m_managers;
     CCriticalSection m_critSection;
     CAddonDatabase m_database;
+    CEventSource<AddonEvent> m_events;
     std::set<std::string> m_systemAddons;
     std::set<std::string> m_optionalAddons;
   };

commit 4db5ba9aa1919a01c44fa71354cadfcf864813eb

listproviders: fix segfault introduced by 2702a31f6dc1e9b6d9aaab56bc52f0825675d951

xbmc/listproviders/DirectoryProvider.cpp
@@ -294,7 +294,13 @@ void CDirectoryProvider::Reset(bool immediately /* = false */)
     m_currentSort.sortOrder = SortOrderAscending;
     m_currentLimit = 0;
     m_updateState = OK;
-    RegisterListProvider();
+
+    if (m_isAnnounced)
+    {
+      m_isAnnounced = false;
+      CAnnouncementManager::GetInstance().RemoveAnnouncer(this);
+      ADDON::CAddonMgr::GetInstance().Events().Unsubscribe(this);
+    }
   }
 }
 
@@ -374,35 +380,21 @@ bool CDirectoryProvider::IsUpdating() const
   return m_jobID || m_updateState == DONE || m_updateState == INVALIDATED;
 }
 
-void CDirectoryProvider::RegisterListProvider()
+bool CDirectoryProvider::UpdateURL()
 {
   CSingleLock lock(m_section);
+  std::string value(m_url.GetLabel(m_parentID, false));
+  if (value == m_currentUrl)
+    return false;
+
+  m_currentUrl = value;
+
   if (!m_isAnnounced)
   {
     m_isAnnounced = true;
     CAnnouncementManager::GetInstance().AddAnnouncer(this);
     ADDON::CAddonMgr::GetInstance().Events().Subscribe(this, &CDirectoryProvider::OnEvent);
   }
-  else if (m_isAnnounced)
-  {
-    m_isAnnounced = false;
-    CAnnouncementManager::GetInstance().RemoveAnnouncer(this);
-    ADDON::CAddonMgr::GetInstance().Events().Unsubscribe(this);
-  }
-}
-
-bool CDirectoryProvider::UpdateURL()
-{
-  {
-    CSingleLock lock(m_section);
-    std::string value(m_url.GetLabel(m_parentID, false));
-    if (value == m_currentUrl)
-      return false;
-
-    m_currentUrl = value;
-  }
-
-  RegisterListProvider();
   return true;
 }
 

xbmc/listproviders/DirectoryProvider.h
@@ -84,7 +84,6 @@ private:
   std::vector<InfoTagType> m_itemTypes;
   CCriticalSection m_section;
 
-  void RegisterListProvider();
   bool UpdateURL();
   bool UpdateLimit();
   bool UpdateSort();