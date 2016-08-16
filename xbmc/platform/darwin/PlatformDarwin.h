32704cd98ed9b8339f5a157251f73855cf70fcde
Memphiz <memphis@machzwo.de>
2016-08-16 21:02:54 +0200

    [PlatformDarwin] - added derived CPlatform version for darwin

---
+++ xbmc/platform/darwin/PlatformDarwin.h
@@ -0,0 +1,35 @@
+#pragma once
+
+/*
+ *      Copyright (C) 2016 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "platform/Platform.h"
+
+class CPlatformDarwin : public CPlatform
+{
+  public:
+    /**\brief C'tor */
+    CPlatformDarwin();
+  
+    /**\brief D'tor */
+    virtual ~CPlatformDarwin();
+  
+    void Init() override;
+};
