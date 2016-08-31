9ac3196c7323f9ed8fca9242afeeeec2cfc59f73
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-08-31 16:56:50 +0200

    [PVR] Recordings window: fix/improve user experience in case of empty recordings list.

--- xbmc/pvr/recordings/PVRRecordings.cpp
+++ xbmc/pvr/recordings/PVRRecordings.cpp
@@ -314,11 +314,17 @@ bool CPVRRecordings::GetDirectory(const std::string& strPath, CFileItemList &ite
 
       items.Add(pFileItem);
     }
-
-    return true;
   }
 
-  return false;
+  if (items.IsEmpty())
+  {
+    // Note: Do not change the ".." label. It has very special meaning/logic.
+    //       CFileItem::IsParentFolder() and and other code depends on this.
+    const CFileItemPtr item(new CFileItem(".."));
+    items.Add(item);
+  }
+
+  return recPath.IsValid();
 }
 
 void CPVRRecordings::GetAll(CFileItemList &items, bool bDeleted)
