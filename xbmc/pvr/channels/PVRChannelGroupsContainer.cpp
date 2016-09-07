c0207d5e6a7357aa394e9b07bfe9c5f2d703f367
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-09-07 23:44:25 +0200

    [Estuary][PVR] Fix home screen channel widget to display only channels that actually have been played before.

--- xbmc/pvr/channels/PVRChannelGroupsContainer.cpp
+++ xbmc/pvr/channels/PVRChannelGroupsContainer.cpp
@@ -191,6 +191,8 @@ bool CPVRChannelGroupsContainer::GetDirectory(const std::string& strPath, CFileI
       group = GetGroupAllTV();
     if (group)
       group->GetMembers(results, !StringUtils::EndsWithNoCase(fileName, ".hidden"));
+
+    FilterDirectory(url, results);
     return true;
   }
   else if (StringUtils::StartsWith(fileName, "channels/radio/"))
@@ -202,10 +204,43 @@ bool CPVRChannelGroupsContainer::GetDirectory(const std::string& strPath, CFileI
       group = GetGroupAllRadio();
     if (group)
       group->GetMembers(results, !StringUtils::EndsWithNoCase(fileName, ".hidden"));
+
+    FilterDirectory(url, results);
     return true;
   }
 
   return false;
+}
+
+bool CPVRChannelGroupsContainer::FilterDirectory(const CURL &url, CFileItemList &results) const
+{
+  if (!results.IsEmpty())
+  {
+    if (url.HasOption("view"))
+    {
+      const std::string view(url.GetOption("view"));
+      if (view == "lastplayed")
+      {
+        // remove channels never played so far
+        for (int i = 0; i < results.Size(); ++i)
+        {
+          const CPVRChannelPtr channel(results.Get(i)->GetPVRChannelInfoTag());
+          time_t lastWatched = channel->LastWatched();
+          if (!lastWatched)
+          {
+            results.Remove(i);
+            --i;
+          }
+        }
+      }
+      else
+      {
+        CLog::Log(LOGERROR, "CPVRChannelGroupsContainer - %s - unsupported value '%s' for url parameter 'view'", __FUNCTION__, view.c_str());
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 CPVRChannelGroupPtr CPVRChannelGroupsContainer::GetSelectedGroup(bool bRadio) const
