6f5b2216fa83b0587cd2e4640e0638f305826f6f
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-09-19 19:01:32 +0200

    [PVR][guiinfo] Optimize implementation of System.HasPVRAddon. Also fixes possible deadlock on PVR startup.

--- xbmc/addons/BinaryAddonCache.cpp
+++ xbmc/addons/BinaryAddonCache.cpp
@@ -32,7 +32,7 @@ CBinaryAddonCache::~CBinaryAddonCache()
 
 void CBinaryAddonCache::Init()
 {
-  m_addonsToCache = {ADDON_AUDIODECODER, ADDON_INPUTSTREAM};
+  m_addonsToCache = {ADDON_AUDIODECODER, ADDON_INPUTSTREAM, ADDON_PVRDLL};
   CAddonMgr::GetInstance().Events().Subscribe(this, &CBinaryAddonCache::OnEvent);
   Update();
 }
