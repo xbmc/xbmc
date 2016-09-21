b84ddd9fcadda469e1c0d354f995842e3c64e843
Stephan Raue <stephan@openelec.tv>
2016-09-21 23:23:18 +0200

    [cmake] RPi: to be consistent with AMLogic add support for -DENABLE_RPI=<value> - rename MMAL_FOUND to RPI_FOUND - remove CORE_SYSTEM_NAME=rbpi and use RPI_FOUND - use RPI_FOUND for OMXLIB_FOUND and OMX_FOUND - use linux platform support for 'rbpi'

--- project/cmake/modules/FindRPI.cmake
+++ project/cmake/modules/FindRPI.cmake
@@ -1,15 +1,15 @@
-# - Try to find MMAL
+# - Try to find RPI
 # Once done this will define
 #
-# MMAL_FOUND - system has MMAL
-# MMAL_INCLUDE_DIRS - the MMAL include directory
-# MMAL_LIBRARIES - The MMAL libraries
+# RPI_FOUND - system has RPI
+# RPI_INCLUDE_DIRS - the RPI include directory
+# RPI_LIBRARIES - The RPI libraries
 
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(MMAL mmal QUIET)
+  pkg_check_modules(RPI mmal QUIET)
 endif()
 
-if(NOT MMAL_FOUND)
+if(NOT RPI_FOUND)
   find_path(MMAL_INCLUDE_DIRS interface/mmal/mmal.h)
   find_library(MMAL_LIBRARY  mmal)
   find_library(MMALCORE_LIBRARY mmal_core)
@@ -22,15 +22,15 @@ if(NOT MMAL_FOUND)
   find_library(VCSM_LIBRARY vcsm)
   find_library(CONTAINER_LIBRARY containers)
 
-  set(MMAL_LIBRARIES ${MMAL_LIBRARY} ${MMALCORE_LIBRARY} ${MMALUTIL_LIBRARY}
+  set(RPI_LIBRARIES ${MMAL_LIBRARY} ${MMALCORE_LIBRARY} ${MMALUTIL_LIBRARY}
                      ${MMALCLIENT_LIBRARY} ${MMALCOMPONENT_LIBRARY}
                      ${BCM_LIBRARY} ${VCHIQ_LIBRARY} ${VCOS_LIBRARY} ${VCSM_LIBRARY} ${CONTAINER_LIBRARY}
-      CACHE STRING "mmal libraries" FORCE)
+      CACHE STRING "RPi support libraries" FORCE)
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MMAL DEFAULT_MSG MMAL_LIBRARIES MMAL_INCLUDE_DIRS)
+find_package_handle_standard_args(RPI DEFAULT_MSG RPI_LIBRARIES MMAL_INCLUDE_DIRS)
 
-list(APPEND MMAL_DEFINITIONS -DHAVE_MMAL=1 -DHAS_MMAL=1)
+list(APPEND RPI_DEFINITIONS -DHAS_MMAL=1 -DHAS_OMXPLAYER -DHAVE_OMXLIB)
 
 mark_as_advanced(MMAL_INCLUDE_DIRS MMAL_LIBRARIES MMAL_DEFINITIONS)
