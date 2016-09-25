019fb6be5e8a21a6ade7324e82d85fdb5ba7168a
Memphiz <memphis@machzwo.de>
2016-09-25 21:12:58 +0200

    [PlatformRbpi] - added PlatformRbpi override and implemented InitUniqueHardwareIdentifier (by using the serialnumber from /proc/cpuinfo)

---
+++ xbmc/platform/overrides/rbpi/PlatformRbpi.cpp
@@ -0,0 +1,72 @@
+/*
+ *      Copyright (C) 2016 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "PlatformRbpi.h"
+#include <stdlib.h>
+#include <string.h>
+#include "utils/log.h"
+#include "utils/md5.h"
+
+CPlatform* CPlatform::CreateInstance()
+{
+    return new CPlatformRbpi();
+}
+
+void CPlatformRbpi::Init()
+{
+  // call base init
+  CPlatform::Init();
+  // add platform specific init stuff here
+}
+
+void CPlatformRbpi::InitUniqueHardwareIdentifier()
+{
+  FILE *f = fopen("/proc/cpuinfo", "r");
+  if (f)
+  {
+    char line[256];
+    while (fgets(line, sizeof line, f))
+    {
+      if (strncmp(line, "Serial", 6) == 0)
+      {
+        char *colon = strchr(line, ':');
+        if (colon)
+        {
+          m_uuid = colon + 2;
+#if defined(_DEBUG)
+          CLog::Log(LOGDEBUG, "HardwareUUID (nomd5): %s", m_uuid.c_str());
+#endif
+        }
+      }
+    }
+    fclose(f);
+  }
+
+  // fallback to base implementation if needed - paranoia
+  if (m_uuid == NoValidUUID)
+  {
+    CPlatform::InitUniqueHardwareIdentifier();
+  }
+  else
+  {
+    m_uuid = XBMC::XBMC_MD5::GetMD5(m_uuid);
+    CLog::Log(LOGNOTICE, "HardwareUUID: %s", m_uuid.c_str());
+  }
+}
