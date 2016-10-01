699affa50c0b21072acc02af4ccb21c9ae5fe939
Pär Björklund <per@ohmy.nu>
2016-10-01 17:42:33 +0200

    Update this to use Visual Studio 14.
    This file had apparently been forgotten about and was still
    trying to use Visual Studio 2013 toolset.

--- tools/windows/prepare-binary-addons-dev.bat
+++ tools/windows/prepare-binary-addons-dev.bat
@@ -18,7 +18,7 @@ FOR %%b IN (%*) DO (
 SETLOCAL DisableDelayedExpansion
 
 rem set Visual C++ build environment
-call "%VS120COMNTOOLS%..\..\VC\bin\vcvars32.bat"
+call "%VS140COMNTOOLS%..\..\VC\bin\vcvars32.bat"
 
 SET WORKDIR=%WORKSPACE%
 
@@ -76,7 +76,7 @@ IF "%addon%" NEQ "" (
 )
 
 rem execute cmake to generate Visual Studio 12 project files
-cmake "%ADDONS_PATH%" -G "Visual Studio 12" ^
+cmake "%ADDONS_PATH%" -G "Visual Studio 14" ^
       -DCMAKE_BUILD_TYPE=Debug ^
       -DCMAKE_USER_MAKE_RULES_OVERRIDE="%SCRIPTS_PATH%/CFlagOverrides.cmake" ^
       -DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX="%SCRIPTS_PATH%/CXXFlagOverrides.cmake" ^
