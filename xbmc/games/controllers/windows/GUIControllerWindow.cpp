5ca9830a22346adb189fe7cb41536485b7ee6529
Garrett Brown <themagnificentmrb@gmail.com>
2016-10-23 14:35:46 -0700

    Fade the current controller when mapping its buttons

--- xbmc/games/controllers/windows/GUIControllerWindow.cpp
+++ xbmc/games/controllers/windows/GUIControllerWindow.cpp
@@ -26,6 +26,8 @@
 #include "addons/GUIWindowAddonBrowser.h"
 #include "addons/IAddon.h"
 #include "addons/AddonManager.h"
+#include "guilib/GUIButtonControl.h"
+#include "guilib/GUIControl.h"
 #include "guilib/GUIMessage.h"
 #include "guilib/WindowIDs.h"
 
@@ -54,6 +56,39 @@ CGUIControllerWindow::~CGUIControllerWindow(void)
 {
   delete m_controllerList;
   delete m_featureList;
+}
+
+void CGUIControllerWindow::DoProcess(unsigned int currentTime, CDirtyRegionList &dirtyregions)
+{
+  /*
+   * Apply the faded focus texture to the current controller when unfocused
+   */
+
+  CGUIControl* control = nullptr; // The controller button
+  bool bAlphaFaded = false; // True if the controller button has been focused and faded this frame
+
+  if (m_controllerList && m_controllerList->GetFocusedController() >= 0)
+  {
+    control = GetFirstFocusableControl(CONTROL_CONTROLLER_BUTTONS_START + m_controllerList->GetFocusedController());
+    if (control && !control->HasFocus())
+    {
+      if (control->GetControlType() == CGUIControl::GUICONTROL_BUTTON)
+      {
+        control->SetFocus(true);
+        static_cast<CGUIButtonControl*>(control)->SetAlpha(0x80);
+        bAlphaFaded = true;
+      }
+    }
+  }
+
+  CGUIDialog::DoProcess(currentTime, dirtyregions);
+
+  if (control && bAlphaFaded)
+  {
+    control->SetFocus(false);
+    if (control->GetControlType() == CGUIControl::GUICONTROL_BUTTON)
+      static_cast<CGUIButtonControl*>(control)->SetAlpha(0xFF);
+  }
 }
 
 bool CGUIControllerWindow::OnMessage(CGUIMessage& message)
