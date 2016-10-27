8f83105eab5981d139de76835bbf01d6648d11b4
ronie <ronie@users.noreply.github.com>
2016-10-28 01:22:56 +0200

    [linux] use appname instead of hardcoding kodi

--- project/cmake/scripts/linux/ExtraTargets.cmake
+++ project/cmake/scripts/linux/ExtraTargets.cmake
@@ -3,7 +3,7 @@ if(ENABLE_X11 AND X_FOUND AND XRANDR_FOUND)
   find_package(X QUIET)
   find_package(XRandR QUIET)
   add_executable(${APP_NAME_LC}-xrandr ${CORE_SOURCE_DIR}/xbmc-xrandr.c)
-  target_link_libraries(kodi-xrandr ${SYSTEM_LDFLAGS} ${X_LIBRARIES} m ${XRANDR_LIBRARIES})
+  target_link_libraries(${APP_NAME_LC}-xrandr ${SYSTEM_LDFLAGS} ${X_LIBRARIES} m ${XRANDR_LIBRARIES})
 endif()
 
 # WiiRemote
