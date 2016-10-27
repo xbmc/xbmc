38e691aa8f5ed0be4c03c2b0454dfc32a06b736c
Martijn Kaijser <martijn@xbmc.org>
2016-10-27 18:11:36 +0200

    Revert "[xbmc][win]Build addons with debug info on Windows"

--- project/Win32BuildSetup/copy-pdb.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Recurse -Include *.pdb -Exclude vc140.pdb,'kodi-test.pdb',gtest.pdb,CMakeCCompilerId.pdb,CMakeCXXCompilerId.pdb,example.pdb ..\.. |
-foreach {
-  if (-not $_.DirectoryName.EndsWith('PDB')) {
-    Copy-Item $_.FullName (join-path PDB $_.Name)
-  }
-}
