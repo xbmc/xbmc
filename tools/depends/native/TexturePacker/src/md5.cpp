304f9c038aafbbd0bd9bf28aa68a8fb9767589fd
Arne Morten Kvarving <cptspiff@gmail.com>
2016-11-23 22:59:20 +0100

    register is deprecated and does nothing but throw a warning with c++11

--- tools/depends/native/TexturePacker/src/md5.cpp
+++ tools/depends/native/TexturePacker/src/md5.cpp
@@ -59,7 +59,7 @@ byteSwap(uint32_t *buf, unsigned words)
 static void
 MD5Transform(uint32_t buf[4], uint32_t const in[16])
 {
-	register uint32_t a, b, c, d;
+	uint32_t a, b, c, d;
 
 	a = buf[0];
 	b = buf[1];
