30ab48f91dac122045b9a964dfbb77900fdf844d
Alwin Esch <alwin.esch@web.de>
2016-12-01 13:42:04 +0100

    [addon] remove not needed header comment in CScreenSaver
    The comment on code has confused me a bit and is not really required on so small code.
    To prevent misunderstand becomes it removed.

--- xbmc/addons/ScreenSaver.h
+++ xbmc/addons/ScreenSaver.h
@@ -36,7 +36,6 @@ public:
   virtual ~CScreenSaver() {}
   virtual bool IsInUse() const;
 
-  // Things that MUST be supplied by the child classes
   bool CreateScreenSaver();
   void Start();
   void Render();
