a4c25b0f60b74c705c8ddcc605a10ba0d699dc06
b-pass <b-pass@users.noreply.github.com>
2016-12-18 15:56:15 -0500

    Avoid deadlock by Posting AlarmClock commands

--- xbmc/utils/AlarmClock.cpp
+++ xbmc/utils/AlarmClock.cpp
@@ -125,7 +125,7 @@ void CAlarmClock::Stop(const std::string& strName, bool bSilent /* false */)
   }
   else
   {
-    CApplicationMessenger::GetInstance().SendMsg(TMSG_EXECUTE_BUILT_IN, -1, -1, nullptr, iter->second.m_strCommand);
+    CApplicationMessenger::GetInstance().PostMsg(TMSG_EXECUTE_BUILT_IN, -1, -1, nullptr, iter->second.m_strCommand);
     if (iter->second.m_loop)
     {
       iter->second.watch.Reset();
