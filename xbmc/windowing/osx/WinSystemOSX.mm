xbmc/xbmc

commit 4b9126649940de22e89619cbbe8ed5d64c5411a9

[WinSystem/OSX] - ensuer the window origin is reset after resizing - fixes windowed mode appearing on the lower bottom of the screen while ignoring the former origin

xbmc/windowing/osx/WinSystemOSX.mm
@@ -1012,6 +1012,7 @@ bool CWinSystemOSX::SetFullScreen(bool fullScreen, RESOLUTION_INFO& res, bool bl
   ShowHideNSWindow([last_view window], needtoshowme);
   // need to make sure SDL tracks any window size changes
   ResizeWindowInternal(m_nWidth, m_nHeight, -1, -1, last_view);
+  [[last_view window] setFrameOrigin:last_window_origin];
   HandlePossibleRefreshrateChange();
 
   return true;

commit d8e4469bdc9c713be027a1e5f4808e1bd8cc0524

[osx/windowing] - fix regression introduced in 4b9126649940de22e89619cbbe8ed5d64c5411a9 - thx to fernetmenta - fixes trac #17160

xbmc/windowing/osx/WinSystemOSX.mm
@@ -1012,7 +1012,11 @@ bool CWinSystemOSX::SetFullScreen(bool fullScreen, RESOLUTION_INFO& res, bool bl
   ShowHideNSWindow([last_view window], needtoshowme);
   // need to make sure SDL tracks any window size changes
   ResizeWindowInternal(m_nWidth, m_nHeight, -1, -1, last_view);
-  [[last_view window] setFrameOrigin:last_window_origin];
+  // restore origin once again when going to windowed mode
+  if (!fullScreen)
+  {
+    [[last_view window] setFrameOrigin:last_window_origin];
+  }
   HandlePossibleRefreshrateChange();
 
   return true;