57c04ea976de5e088a0651feb64aafac3c42a701
Rainer Hochecker <fernetmenta@online.de>
2017-01-02 14:30:03 +0100

    VideoPlayer: refactor CPullupCorrection

--- xbmc/cores/VideoPlayer/DVDTSCorrection.h
+++ xbmc/cores/VideoPlayer/DVDTSCorrection.h
@@ -50,13 +50,9 @@ class CPullupCorrection
     double GetDiff(int diffnr);      //gets diffs from now to the past
 
     void GetPattern(std::vector<double>& pattern);     //gets the current pattern
-    void GetDifftypes(std::vector<double>& difftypes); //gets the difftypes from the ringbuffer
 
     static bool MatchDiff(double diff1, double diff2); //checks if two diffs match by MAXERR
     static bool MatchDifftype(int* diffs1, int* diffs2, int nrdiffs); //checks if the difftypes match
-
-    //builds a pattern of timestamps in the ringbuffer
-    void BuildPattern(std::vector<double>& pattern, int patternlength);
 
     //checks if the current pattern matches with the saved m_pattern with offset m_patternpos
     bool CheckPattern(std::vector<double>& pattern);
