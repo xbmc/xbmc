66a436addd59da8e8381eb9ffbe368781eece92c
Rainer Hochecker <fernetmenta@online.de>
2017-01-31 19:27:37 +0100

    VideoPlayer: VideoReferenceClock to the right place

--- xbmc/windowing/X11/WinSystemX11.cpp
+++ xbmc/windowing/X11/WinSystemX11.cpp
@@ -39,6 +39,8 @@
 #include "windowing/WindowingFactory.h"
 #include "CompileInfo.h"
 #include "messaging/ApplicationMessenger.h"
+#include "VideoSyncDRM.h"
+#include "VideoSyncGLX.h"
 #include <X11/Xatom.h>
 #include <X11/extensions/Xrandr.h>
 
@@ -1069,4 +1071,13 @@ void CWinSystemX11::UpdateCrtc()
   g_graphicsContext.SetFPS(fps);
 }
 
+std::unique_ptr<CVideoSync> CWinSystemX11::GetVideoSync(void *clock)
+{
+#if defined(HAS_GLX)
+  std::unique_ptr<CVideoSync> pVSync(new CVideoSyncGLX(clock));
+#else
+  std::unique_ptr<CVideoSync> pVSync(new CVideoSyncDRM(clock));
+#endif
+  return pVSync;
+}
 
