9c4d3b64ea6dbd3d54143feede75a0471e0cdaef
notspiff <cptspiff@gmail.com>
2017-02-01 11:13:03 +0100

    added: allow using slider controls in visibility conditions

--- xbmc/guilib/GUISliderControl.h
+++ xbmc/guilib/GUISliderControl.h
@@ -86,6 +86,7 @@ public:
   void SetIntInterval(int iInterval);
   void SetFloatInterval(float fInterval);
   void SetType(int iType) { m_iType = iType; };
+  int GetType() const { return m_iType; }
   virtual std::string GetDescription() const;
   void SetTextValue(const std::string &textValue) { m_textValue = textValue; };
   void SetAction(const std::string &action);
