acdf649f44a2fd4b72cd6f1a5cbd7eef313d91c6
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2017-02-04 18:26:45 +0100

    [PVR] Avoid deadlocks by obtaining extended progress dialog instance pointer early.

--- xbmc/pvr/PVRManager.h
+++ xbmc/pvr/PVRManager.h
@@ -36,6 +36,7 @@
 #include <memory>
 #include <vector>
 
+class CGUIDialogExtendedProgressBar;
 class CGUIDialogProgressBarHandle;
 class CStopWatch;
 class CAction;
@@ -608,6 +609,13 @@ private:
      */
     void PublishEvent(PVREvent state);
 
+    /*!
+     * @brief Show an extended progress bar dialog.
+     * @param strTitle the title for the dialog.
+     * @return the handle that can be used to control the progress dialog.
+     */
+    CGUIDialogProgressBarHandle* ShowProgressDialog(const std::string &strTitle) const;
+
   protected:
     /*!
      * @brief Start the PVRManager, which loads all PVR data and starts some threads to update the PVR data.
@@ -700,6 +708,7 @@ private:
     bool                            m_bFirstStart;                 /*!< true when the PVR manager was started first, false otherwise */
     bool                            m_bIsSwitchingChannels;        /*!< true while switching channels */
     bool                            m_bEpgsCreated;                /*!< true if epg data for channels has been created */
+    CGUIDialogExtendedProgressBar * m_progressBar;                 /*!< extended progress dialog instance pointer */
     CGUIDialogProgressBarHandle *   m_progressHandle;              /*!< progress dialog that is displayed while the pvrmanager is loading */
 
     CCriticalSection                m_managerStateMutex;
