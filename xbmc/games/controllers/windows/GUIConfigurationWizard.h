8d7022b65675c691630987616c6f3511aab697c1
Garrett Brown <themagnificentmrb@gmail.com>
2017-02-08 12:32:21 -0800

    [controller dialog] Ignore delayed axes

--- xbmc/games/controllers/windows/GUIConfigurationWizard.h
+++ xbmc/games/controllers/windows/GUIConfigurationWizard.h
@@ -61,6 +61,7 @@ namespace GAME
                               JOYSTICK::IActionMap* actionMap,
                               const JOYSTICK::CDriverPrimitive& primitive) override;
     virtual void OnEventFrame(const JOYSTICK::IButtonMap* buttonMap, bool bMotion) override;
+    virtual void OnLateAxis(const JOYSTICK::IButtonMap* buttonMap, unsigned int axisIndex) override;
 
     // implementation of IKeyboardHandler
     virtual bool OnKeyPress(const CKey& key) override;
