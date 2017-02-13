c5f4cf2b657f673f707cde690e7c9af061ac6dbf
Boris <foxbob@mail.ru>
2017-02-13 13:53:18 +0200

    patch

--- xbmc/input/InputManager.cpp
+++ xbmc/input/InputManager.cpp
@@ -511,6 +511,18 @@ bool CInputManager::OnKey(const CKey& key)
           useKeyboard = true;
       }
     }
+#if defined(TARGET_VUPLUS) || defined(TARGET_VUPLUS_ARM)
+    switch(key.GetVKey()) {
+      case 0x0d:
+      case 0x80:
+      case 0x81:
+      case 0x82:
+      case 0x83:
+        if (iWin == WINDOW_DIALOG_KEYBOARD)
+          useKeyboard = false;
+        break;
+    }
+#endif
     if (useKeyboard)
     {
       // use the virtualkeyboard section of the keymap, and send keyboard-specific or navigation
