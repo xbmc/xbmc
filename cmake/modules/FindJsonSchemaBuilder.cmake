8f9343ffa3f71dd3078deeb1240aa267a73ac380
Rechi <Rechi@users.noreply.github.com>
2017-02-17 19:27:34 +0100

    [cmake][windows] use precompiled JsonSchemaBuilder if cross compiling

--- cmake/modules/FindJsonSchemaBuilder.cmake
+++ cmake/modules/FindJsonSchemaBuilder.cmake
@@ -10,8 +10,13 @@
 if(NOT TARGET JsonSchemaBuilder::JsonSchemaBuilder)
   if(KODI_DEPENDSBUILD OR CMAKE_CROSSCOMPILING)
     add_executable(JsonSchemaBuilder::JsonSchemaBuilder IMPORTED GLOBAL)
-    set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
-                                                               IMPORTED_LOCATION "${NATIVEPREFIX}/bin/JsonSchemaBuilder")
+    if(CORE_SYSTEM_NAME STREQUAL windows OR CORE_SYSTEM_NAME STREQUAL windowsstore)
+      set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
+                                                                 IMPORTED_LOCATION "${CMAKE_SOURCE_DIR}/project/BuildDependencies/bin/json-rpc/JsonSchemaBuilder")
+    else()
+      set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
+                                                                 IMPORTED_LOCATION "${NATIVEPREFIX}/bin/JsonSchemaBuilder")
+    endif()
     set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES FOLDER Tools)
   else()
     add_subdirectory(${CMAKE_SOURCE_DIR}/tools/depends/native/JsonSchemaBuilder build/jsonschemabuilder)
