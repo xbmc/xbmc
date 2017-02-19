dcb0a8d37bd1e2ec03a9e0cdf375ee7feedede49
Boris <foxbob@mail.ru>
2017-02-19 18:29:43 +0200

    Update PictureInfoLoader.cpp

--- xbmc/pictures/PictureInfoLoader.cpp
+++ xbmc/pictures/PictureInfoLoader.cpp
@@ -20,7 +20,6 @@
 
 #include "PictureInfoLoader.h"
 #include "PictureInfoTag.h"
-#include "ServiceBroker.h"
 #include "settings/Settings.h"
 #include "FileItem.h"
 
@@ -44,7 +43,6 @@ void CPictureInfoLoader::OnLoaderStart()
   m_mapFileItems->SetFastLookup(true);
 
   m_tagReads = 0;
-  m_loadTags = CServiceBroker::GetSettings().GetBool(CSettings::SETTING_PICTURES_USETAGS);
 
   if (m_pProgressCallback)
     m_pProgressCallback->SetProgressMax(m_pVecItems->GetFileCount());
@@ -89,11 +87,8 @@ bool CPictureInfoLoader::LoadItemLookup(CFileItem* pItem)
   if (pItem->HasPictureInfoTag())
     return false;
 
-  if (m_loadTags)
-  { // Nothing found, load tag from file
-    pItem->GetPictureInfoTag()->Load(pItem->GetPath());
-    m_tagReads++;
-  }
+  pItem->GetPictureInfoTag()->Load(pItem->GetPath());
+  m_tagReads++;
 
   return true;
 }
