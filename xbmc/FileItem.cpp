f903b894353e12eba6a4531418a6c1eb9fe6a73c
phil65 <philipptemminghoff@gmx.de>
2017-02-20 22:22:09 +0100

    [cleanup] remove some old xbox references

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -1262,7 +1262,6 @@ void CFileItem::FillInDefaultIcon()
   //   default picture for videos
   //   default picture for shortcuts
   //   default picture for playlists
-  //   or the icon embedded in an .xbe
   //
   // for folders
   //   for .. folders the default picture for parent folder
