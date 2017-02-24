2698e848c8f04c59cb7f2d9352060eca7f35f7c7
Rechi <Rechi@users.noreply.github.com>
2017-02-24 16:02:14 +0100

    [cmake|win] don't link with yajl.lib explicitly as it is already added by core_require_dep

--- cmake/scripts/windows/ArchSetup.cmake
+++ cmake/scripts/windows/ArchSetup.cmake
@@ -49,7 +49,7 @@ link_directories(${CMAKE_SOURCE_DIR}/lib/win32/ffmpeg/bin
 
 # Additional libraries
 list(APPEND DEPLIBS d3d11.lib DInput8.lib DSound.lib winmm.lib Mpr.lib Iphlpapi.lib WS2_32.lib
-                    PowrProf.lib setupapi.lib dwmapi.lib yajl.lib dxguid.lib DelayImp.lib)
+                    PowrProf.lib setupapi.lib dwmapi.lib dxguid.lib DelayImp.lib)
 
 # NODEFAULTLIB option
 set(_nodefaultlibs_RELEASE libcmt)
