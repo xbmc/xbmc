xbmc/xbmc

commit 06a781a1b4d1ee715887940e6c915a9c214a1fdc

[coverity] CID 1373316: String not null terminated.

xbmc/guilib/XBTFReader.cpp
@@ -38,7 +38,7 @@ static bool ReadString(FILE* file, char* str, size_t max_length)
   if (file == nullptr || str == nullptr || max_length <= 0)
     return false;
 
-  return (fread(str, max_length, 1, file) == 1);
+  return (fgets(str, max_length, file) != nullptr);
 }
 
 static bool ReadUInt32(FILE* file, uint32_t& value)
@@ -94,7 +94,7 @@ bool CXBTFReader::Open(const std::string& path)
     return false;
 
   // read the magic word
-  char magic[4];
+  char magic[5];
   if (!ReadString(m_file, magic, sizeof(magic)))
     return false;
 
@@ -102,7 +102,7 @@ bool CXBTFReader::Open(const std::string& path)
     return false;
 
   // read the version
-  char version[1];
+  char version[2];
   if (!ReadString(m_file, version, sizeof(version)))
     return false;
 

commit 12eab09aeedc13e35424ac7bb1fa189e2f3beff9

[Fix] XBTFReader: fix regression introduced by 06a781a1b4d1ee715887940e6c915a9c214a1fdc.

xbmc/guilib/XBTFReader.cpp
@@ -38,7 +38,7 @@ static bool ReadString(FILE* file, char* str, size_t max_length)
   if (file == nullptr || str == nullptr || max_length <= 0)
     return false;
 
-  return (fgets(str, max_length, file) != nullptr);
+  return (fread(str, max_length, 1, file) == 1);
 }
 
 static bool ReadUInt32(FILE* file, uint32_t& value)
@@ -94,7 +94,7 @@ bool CXBTFReader::Open(const std::string& path)
     return false;
 
   // read the magic word
-  char magic[5];
+  char magic[4];
   if (!ReadString(m_file, magic, sizeof(magic)))
     return false;
 
@@ -102,7 +102,7 @@ bool CXBTFReader::Open(const std::string& path)
     return false;
 
   // read the version
-  char version[2];
+  char version[1];
   if (!ReadString(m_file, version, sizeof(version)))
     return false;
 
@@ -119,9 +119,10 @@ bool CXBTFReader::Open(const std::string& path)
     uint32_t u32;
     uint64_t u64;
 
-    char path[CXBTFFile::MaximumPathLength];
+    // one extra char to null terminate the string with the following memset
+    char path[CXBTFFile::MaximumPathLength + 1];
     memset(path, 0, sizeof(path));
-    if (!ReadString(m_file, path, sizeof(path)))
+    if (!ReadString(m_file, path, sizeof(path) - 1))
       return false;
     xbtfFile.SetPath(path);
 