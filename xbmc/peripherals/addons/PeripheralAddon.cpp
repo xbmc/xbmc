7ff9411e8e613878a855dd1c233e79aade49e3a0
Garrett Brown <themagnificentmrb@gmail.com>
2017-03-03 07:53:32 -0800

    [Peripherals] Improve logging for button presses

--- xbmc/peripherals/addons/PeripheralAddon.cpp
+++ xbmc/peripherals/addons/PeripheralAddon.cpp
@@ -463,19 +463,13 @@ bool CPeripheralAddon::ProcessEvents(void)
           case PERIPHERAL_EVENT_TYPE_DRIVER_BUTTON:
           {
             const bool bPressed = (event.ButtonState() == JOYSTICK_STATE_BUTTON_PRESSED);
-            CLog::Log(LOGDEBUG, "Button [ %u ] on %s %s", event.DriverIndex(),
-                      joystickDevice->DeviceName().c_str(), bPressed ? "pressed" : "released");
-            if (joystickDevice->OnButtonMotion(event.DriverIndex(), bPressed))
-              CLog::Log(LOGDEBUG, "Joystick button event handled");
+            joystickDevice->OnButtonMotion(event.DriverIndex(), bPressed);
             break;
           }
           case PERIPHERAL_EVENT_TYPE_DRIVER_HAT:
           {
             const HAT_STATE state = CPeripheralAddonTranslator::TranslateHatState(event.HatState());
-            CLog::Log(LOGDEBUG, "Hat [ %u ] on %s %s", event.DriverIndex(),
-                      joystickDevice->DeviceName().c_str(), CJoystickTranslator::HatStateToString(state));
-            if (joystickDevice->OnHatMotion(event.DriverIndex(), state))
-              CLog::Log(LOGDEBUG, "Joystick hat event handled");
+            joystickDevice->OnHatMotion(event.DriverIndex(), state);
             break;
           }
           case PERIPHERAL_EVENT_TYPE_DRIVER_AXIS:
