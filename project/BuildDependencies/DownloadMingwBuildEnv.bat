73823b2879c8b62d351026fb601a1b8bb5d6666b
TimGales <tgales@tgaconnect.com>
2017-03-11 07:54:32 -0500

    Removed echo statment

--- project/BuildDependencies/DownloadMingwBuildEnv.bat
+++ project/BuildDependencies/DownloadMingwBuildEnv.bat
@@ -19,7 +19,7 @@
 ::    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ::-------------------------------------------------------------------------------------
 
-@echo off
+
 title msys2
 
 set msysver=20160205
