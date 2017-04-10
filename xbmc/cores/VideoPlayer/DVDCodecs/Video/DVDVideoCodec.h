2cabf65f29031b7e18e5a8f26c534e017b6c9090
Rainer Hochecker <fernetmenta@online.de>
2017-04-10 19:30:05 +0200

    VideoPlayer: add video buffers

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodec.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodec.h
@@ -23,6 +23,7 @@
 #include "system.h"
 #include "cores/VideoPlayer/VideoRenderers/RenderFormats.h"
 #include "cores/VideoPlayer/Process/ProcessInfo.h"
+#include "cores/VideoPlayer/Process/VideoBuffer.h"
 #include "cores/VideoPlayer/DVDDemuxers/DVDDemuxPacket.h"
 #include "DVDResource.h"
 
@@ -47,14 +48,18 @@ class CSetting;
 // should be entirely filled by all codecs
 struct VideoPicture
 {
+  ~VideoPicture()
+  {
+    if (videoBuffer)
+    {
+      videoBuffer->Release();
+    }
+  }
+
+  CVideoBuffer *videoBuffer = nullptr;
+
   double pts; // timestamp in seconds, used in the CVideoPlayer class to keep track of pts
   double dts;
-
-  uint8_t* data[4];   // [4] = alpha channel, currently not used
-  int iLineSize[4];   // [4] = alpha channel, currently not used
-
-  void *hwPic;
-
   unsigned int iFlags;
 
   double iRepeatPicture;
@@ -81,9 +86,7 @@ struct VideoPicture
 
 #define DVP_FLAG_TOP_FIELD_FIRST    0x00000001
 #define DVP_FLAG_REPEAT_TOP_FIELD   0x00000002  //< Set to indicate that the top field should be repeated
-#define DVP_FLAG_ALLOCATED          0x00000004  //< Set to indicate that this has allocated data
 #define DVP_FLAG_INTERLACED         0x00000008  //< Set to indicate that this frame is interlaced
-
 #define DVP_FLAG_DROPPED            0x00000010  //< indicate that this picture has been dropped in decoder stage, will have no data
 
 #define DVD_CODEC_CTRL_SKIPDEINT    0x01000000  //< request to skip a deinterlacing cycle, if possible
