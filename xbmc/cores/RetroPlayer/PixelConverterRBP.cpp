794085384095b443b677f1e6597138beba7770de
popcornmix <popcornmix@gmail.com>
2017-04-10 13:19:57 +0100

    PixelConverterRBP: adapt to VP updates

--- xbmc/cores/RetroPlayer/PixelConverterRBP.cpp
+++ xbmc/cores/RetroPlayer/PixelConverterRBP.cpp
@@ -137,7 +137,7 @@ bool CPixelConverterRBP::Decode(const uint8_t* pData, unsigned int size)
     return false;
   }
 
-  MMAL::CMMALYUVBuffer *omvb = (MMAL::CMMALYUVBuffer *)m_buf->MMALBuffer;
+  MMAL::CMMALYUVBuffer *omvb = static_cast<MMAL::CMMALYUVBuffer*>(m_buf->hwPic);
 
   const int stride = size / m_height;
 
@@ -155,9 +155,9 @@ void CPixelConverterRBP::GetPicture(VideoPicture& dvdVideoPicture)
 {
   CPixelConverter::GetPicture(dvdVideoPicture);
 
-  dvdVideoPicture.MMALBuffer = m_buf->MMALBuffer;
+  dvdVideoPicture.hwPic = m_buf->hwPic;
 
-  MMAL::CMMALYUVBuffer *omvb = (MMAL::CMMALYUVBuffer *)m_buf->MMALBuffer;
+  MMAL::CMMALYUVBuffer *omvb = static_cast<MMAL::CMMALYUVBuffer*>(m_buf->hwPic);
 
   // need to flush ARM cache so GPU can see it
   omvb->gmem->Flush();
@@ -195,7 +195,7 @@ VideoPicture* CPixelConverterRBP::AllocatePicture(int iWidth, int iHeight)
 
   if (pPicture)
   {
-    pPicture->MMALBuffer = omvb;
+    pPicture->hwPic = static_cast<void*>(omvb);
     pPicture->iWidth = iWidth;
     pPicture->iHeight = iHeight;
   }
@@ -207,9 +207,11 @@ void CPixelConverterRBP::FreePicture(VideoPicture* pPicture)
 {
   if (pPicture)
   {
-    if (pPicture->MMALBuffer)
-      pPicture->MMALBuffer->Release();
-
+    if (pPicture->hwPic)
+    {
+      MMAL::CMMALYUVBuffer *omvb = static_cast<MMAL::CMMALYUVBuffer*>(m_buf->hwPic);
+      omvb->Release();
+    }
     delete pPicture;
   }
   else
