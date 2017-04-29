39497880d5cf7b7da465a0f61b1de4b342ddda11
Rechi <Rechi@users.noreply.github.com>
2017-04-29 16:54:44 +0200

    [buildsteps][windows] make-addons

--- tools/buildsteps/windows/make-addons.bat
+++ tools/buildsteps/windows/make-addons.bat
@@ -23,19 +23,9 @@ FOR %%b IN (%*) DO (
 )
 SETLOCAL DisableDelayedExpansion
 
-rem set Visual C++ build environment
-call "%VS140COMNTOOLS%..\..\VC\bin\amd64_x86\vcvarsamd64_x86.bat" || call "%VS140COMNTOOLS%..\..\VC\bin\vcvars32.bat"
-
-SET WORKDIR=%base_dir%
-
-IF "%WORKDIR%" == "" (
-  rem resolve the relative path
-  SETLOCAL EnableDelayedExpansion
-  PUSHD ..\..\..
-  SET WORKDIR=!CD!
-  POPD
-  SETLOCAL DisableDelayedExpansion
-)
+PUSHD %~dp0\..\..\..
+SET WORKDIR=%CD%
+POPD
 
 rem setup some paths that we need later
 SET CUR_PATH=%CD%
