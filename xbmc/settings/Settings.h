2b1402b876dd8d5f377cbcac6a293ed04863e246
chewitt <github@chrishewitt.net>
2017-05-16 09:29:22 +0100

    smbclient: cleanup smbclient configuration

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -277,6 +277,7 @@ public:
   static const std::string SETTING_SERVICES_AIRPLAYVIDEOSUPPORT;
   static const std::string SETTING_SMB_WINSSERVER;
   static const std::string SETTING_SMB_WORKGROUP;
+  static const std::string SETTING_SMB_MAXPROTOCOL;
   static const std::string SETTING_VIDEOSCREEN_MONITOR;
   static const std::string SETTING_VIDEOSCREEN_SCREEN;
   static const std::string SETTING_VIDEOSCREEN_RESOLUTION;
