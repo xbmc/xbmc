e4877f55efe0b310a2033e4bfa6d3116b84a6a39
Lukas Rusak <lorusak@gmail.com>
2017-05-24 14:42:40 -0700

    [cmake] fix FindEGL.cmake defines and adjust system.h

--- xbmc/system.h
+++ xbmc/system.h
@@ -149,10 +149,6 @@
 #define HAS_FILESYSTEM_SFTP
 #endif
 
-#if defined(HAVE_X11)
-#define HAS_EGL
-#endif
-
 /****************************************
  * Additional platform specific includes
  ****************************************/
@@ -192,10 +188,6 @@
 #if defined(TARGET_ANDROID)
 #undef HAS_LINUX_EVENTS
 #undef HAS_LIRC
-#endif
-
-#ifdef HAVE_LIBEGL
-#define HAS_EGL
 #endif
 
 // GLES2.0 detected. Dont use GL!
