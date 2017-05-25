246449eaff7722c12c946a12385b7fdb1c075b09
Rainer Hochecker <fernetmenta@online.de>
2017-05-25 19:37:07 +0200

    VideoPlayer: drop render formats

--- xbmc/cores/RetroPlayer/RetroPlayerVideo.cpp
+++ xbmc/cores/RetroPlayer/RetroPlayerVideo.cpp
@@ -148,18 +148,18 @@ bool CRetroPlayerVideo::Configure(VideoPicture& picture)
 
     m_bConfigured = m_renderManager.Configure(picture, static_cast<float>(m_framerate), flags, m_orientation, buffers);
 
-    if (m_bConfigured)
-    {
-      // Update process info
-      AVPixelFormat pixfmt = static_cast<AVPixelFormat>(CDVDCodecUtils::PixfmtFromEFormat(picture.format));
-      if (pixfmt != AV_PIX_FMT_NONE)
-      {
-        //! @todo
-        //m_processInfo.SetVideoPixelFormat(CDVDVideoCodecFFmpeg::GetPixelFormatName(pixfmt));
-      }
-      m_processInfo.SetVideoDimensions(picture.iWidth, picture.iHeight);
-      m_processInfo.SetVideoFps(static_cast<float>(m_framerate));
-    }
+//    if (m_bConfigured)
+//    {
+//      // Update process info
+//      AVPixelFormat pixfmt = static_cast<AVPixelFormat>(CDVDCodecUtils::PixfmtFromEFormat(picture.format));
+//      if (pixfmt != AV_PIX_FMT_NONE)
+//      {
+//        //! @todo
+//        //m_processInfo.SetVideoPixelFormat(CDVDVideoCodecFFmpeg::GetPixelFormatName(pixfmt));
+//      }
+//      m_processInfo.SetVideoDimensions(picture.iWidth, picture.iHeight);
+//      m_processInfo.SetVideoFps(static_cast<float>(m_framerate));
+//    }
   }
 
   return m_bConfigured;
--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
@@ -229,8 +229,7 @@ protected:
   std::deque<int> m_queued;
   std::deque<int> m_discard;
 
-  ERenderFormat m_format;
-  void *m_hwPic = nullptr;
+  std::unique_ptr<VideoPicture> m_pConfigPicture;
   unsigned int m_width, m_height, m_dwidth, m_dheight;
   unsigned int m_flags = 0;
   float m_fps;
