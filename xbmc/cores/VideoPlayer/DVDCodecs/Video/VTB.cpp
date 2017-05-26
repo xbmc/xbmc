4544c70c148c0b514ab9fd32657bbda8002442b3
Rainer Hochecker <fernetmenta@online.de>
2017-05-26 20:26:00 +0200

    VideoPlayer: OSX adapt to video buffers

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/VTB.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/VTB.cpp
@@ -28,6 +28,7 @@
 #include "utils/BitstreamConverter.h"
 #include "utils/BitstreamReader.h"
 #include "settings/Settings.h"
+#include "threads/SingleLock.h"
 #include "ServiceBroker.h"
 
 extern "C" {
@@ -36,14 +37,119 @@ extern "C" {
 
 using namespace VTB;
 
+//------------------------------------------------------------------------------
+// Video Buffers
+//------------------------------------------------------------------------------
+
+CVideoBufferVTB::CVideoBufferVTB(IVideoBufferPool &pool, int id)
+: CVideoBuffer(id)
+{
+  m_pFrame = av_frame_alloc();
+}
+
+CVideoBufferVTB::~CVideoBufferVTB()
+{
+  av_frame_free(&m_pFrame);
+}
+
+void CVideoBufferVTB::SetRef(AVFrame *frame)
+{
+  av_frame_unref(m_pFrame);
+  av_frame_ref(m_pFrame, frame);
+  m_pbRef = (CVPixelBufferRef)m_pFrame->data[3];
+}
+
+void CVideoBufferVTB::Unref()
+{
+  av_frame_unref(m_pFrame);
+}
+
+CVPixelBufferRef CVideoBufferVTB::GetPB()
+{
+  return m_pbRef;
+}
+
+//------------------------------------------------------------------------------
+
+class VTB::CVideoBufferPoolVTB : public IVideoBufferPool
+{
+public:
+  ~CVideoBufferPoolVTB();
+  virtual void Return(int id) override;
+  virtual CVideoBuffer* Get() override;
+
+protected:
+  CCriticalSection m_critSection;
+  std::vector<CVideoBufferVTB*> m_all;
+  std::deque<int> m_used;
+  std::deque<int> m_free;
+};
+
+CVideoBufferPoolVTB::~CVideoBufferPoolVTB()
+{
+  for (auto buf : m_all)
+  {
+    delete buf;
+  }
+}
+
+CVideoBuffer* CVideoBufferPoolVTB::Get()
+{
+  CSingleLock lock(m_critSection);
+
+  CVideoBufferVTB *buf = nullptr;
+  if (!m_free.empty())
+  {
+    int idx = m_free.front();
+    m_free.pop_front();
+    m_used.push_back(idx);
+    buf = m_all[idx];
+  }
+  else
+  {
+    int id = m_all.size();
+    buf = new CVideoBufferVTB(*this, id);
+    m_all.push_back(buf);
+    m_used.push_back(id);
+  }
+
+  buf->Acquire(GetPtr());
+  return buf;
+}
+
+void CVideoBufferPoolVTB::Return(int id)
+{
+  CSingleLock lock(m_critSection);
+
+  m_all[id]->Unref();
+  auto it = m_used.begin();
+  while (it != m_used.end())
+  {
+    if (*it == id)
+    {
+      m_used.erase(it);
+      break;
+    }
+    else
+      ++it;
+  }
+  m_free.push_back(id);
+}
+
+//------------------------------------------------------------------------------
+// main class
+//------------------------------------------------------------------------------
 
 CDecoder::CDecoder(CProcessInfo& processInfo) : m_processInfo(processInfo)
 {
   m_avctx = nullptr;
+  m_videoBufferPool = std::make_shared<CVideoBufferPoolVTB>();
 }
 
 CDecoder::~CDecoder()
 {
+  if (m_renderBuffer)
+    m_renderBuffer->Release();
   Close();
 }
 
@@ -109,7 +215,10 @@ CDVDVideoCodec::VCReturn CDecoder::Decode(AVCodecContext* avctx, AVFrame* frame)
 
   if(frame)
   {
-    m_renderPicture = (CVPixelBufferRef)frame->data[3];
+    if (m_renderBuffer)
+      m_renderBuffer->Release();
+    m_renderBuffer = dynamic_cast<CVideoBufferVTB*>(m_videoBufferPool->Get());
+    m_renderBuffer->SetRef(frame);
     return CDVDVideoCodec::VC_PICTURE;
   }
   else
@@ -120,8 +229,11 @@ bool CDecoder::GetPicture(AVCodecContext* avctx, VideoPicture* picture)
 {
   ((ICallbackHWAccel*)avctx->opaque)->GetPictureCommon(picture);
 
-  // picture->pixFormat = AV_PIX_FMT_NONE;
-  //picture->hwPic = m_renderPicture;
+  if (picture->videoBuffer)
+    picture->videoBuffer->Release();
+
+  picture->videoBuffer = m_renderBuffer;
+  picture->videoBuffer->Acquire();
   return true;
 }
 
