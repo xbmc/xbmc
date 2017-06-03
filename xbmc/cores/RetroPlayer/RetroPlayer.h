44134d2c49f0af61c030460bca11972dd0ff49d8
Garrett Brown <themagnificentmrb@gmail.com>
2017-06-02 17:20:08 -0700

    RetroPlayer: Update for video buffers

--- xbmc/cores/RetroPlayer/RetroPlayer.h
+++ xbmc/cores/RetroPlayer/RetroPlayer.h
@@ -143,8 +143,10 @@ namespace RETRO
     virtual void VideoParamsChange() override { }
     virtual void GetDebugInfo(std::string &audio, std::string &video, std::string &general) override { }
     virtual void UpdateClockSync(bool enabled) override;
-    //virtual void UpdateRenderInfo(CRenderInfo &info) override;
+    virtual void UpdateRenderInfo(CRenderInfo &info) override;
     virtual void UpdateRenderBuffers(int queued, int discard, int free) override {}
+    virtual void UpdareGuiRender(bool gui) override;
+    virtual void UpdareVideoRender(bool video) override;
 
   private:
     /*!
