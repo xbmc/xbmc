4cdcb4ea3f24ca42884782eb57cd24c4d015ea5f
Garrett Brown <themagnificentmrb@gmail.com>
2017-06-05 13:37:22 -0700

    RetroPlayer: Fix spelling error

--- xbmc/cores/RetroPlayer/RetroPlayer.h
+++ xbmc/cores/RetroPlayer/RetroPlayer.h
@@ -140,8 +140,8 @@ namespace GAME
     virtual void UpdateClockSync(bool enabled) override;
     virtual void UpdateRenderInfo(CRenderInfo &info) override;
     virtual void UpdateRenderBuffers(int queued, int discard, int free) override {}
-    virtual void UpdareGuiRender(bool gui) override;
-    virtual void UpdareVideoRender(bool video) override;
+    virtual void UpdateGuiRender(bool gui) override;
+    virtual void UpdateVideoRender(bool video) override;
 
   private:
     /**
