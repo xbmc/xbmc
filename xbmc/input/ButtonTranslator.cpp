ae6c2af42c698d0d2ee299f81b3f66e6a8934c28
Garrett Brown <themagnificentmrb@gmail.com>
2017-06-09 15:31:41 -0700

    ButtonTranslator: Move app command translation to new class CAppTranslator

--- xbmc/input/ButtonTranslator.cpp
+++ xbmc/input/ButtonTranslator.cpp
@@ -24,6 +24,7 @@
 #include <utility>
 
 #include "ActionTranslator.h"
+#include "AppTranslator.h"
 #include "CustomControllerTranslator.h"
 #include "IRTranslator.h"
 #include "MouseTranslator.h"
@@ -42,42 +43,6 @@
 #include "utils/XBMCTinyXML.h"
 
 using namespace XFILE;
-
-typedef struct
-{
-  const char* name;
-  int action;
-} ActionMapping;
-
-#ifdef TARGET_WINDOWS
-static const ActionMapping appcommands[] =
-{
-    { "browser_back"             , APPCOMMAND_BROWSER_BACKWARD },
-    { "browser_forward"          , APPCOMMAND_BROWSER_FORWARD },
-    { "browser_refresh"          , APPCOMMAND_BROWSER_REFRESH },
-    { "browser_stop"             , APPCOMMAND_BROWSER_STOP },
-    { "browser_search"           , APPCOMMAND_BROWSER_SEARCH },
-    { "browser_favorites"        , APPCOMMAND_BROWSER_FAVORITES },
-    { "browser_home"             , APPCOMMAND_BROWSER_HOME },
-    { "volume_mute"              , APPCOMMAND_VOLUME_MUTE },
-    { "volume_down"              , APPCOMMAND_VOLUME_DOWN },
-    { "volume_up"                , APPCOMMAND_VOLUME_UP },
-    { "next_track"               , APPCOMMAND_MEDIA_NEXTTRACK },
-    { "prev_track"               , APPCOMMAND_MEDIA_PREVIOUSTRACK },
-    { "stop"                     , APPCOMMAND_MEDIA_STOP },
-    { "play_pause"               , APPCOMMAND_MEDIA_PLAY_PAUSE },
-    { "launch_mail"              , APPCOMMAND_LAUNCH_MAIL },
-    { "launch_media_select"      , APPCOMMAND_LAUNCH_MEDIA_SELECT },
-    { "launch_app1"              , APPCOMMAND_LAUNCH_APP1 },
-    { "launch_app2"              , APPCOMMAND_LAUNCH_APP2 },
-    { "play"                     , APPCOMMAND_MEDIA_PLAY },
-    { "pause"                    , APPCOMMAND_MEDIA_PAUSE },
-    { "fastforward"              , APPCOMMAND_MEDIA_FAST_FORWARD },
-    { "rewind"                   , APPCOMMAND_MEDIA_REWIND },
-    { "channelup"                , APPCOMMAND_MEDIA_CHANNEL_UP },
-    { "channeldown"              , APPCOMMAND_MEDIA_CHANNEL_DOWN }
-};
-#endif
 
 CButtonTranslator& CButtonTranslator::GetInstance()
 {
@@ -516,7 +481,7 @@ void CButtonTranslator::MapWindowActions(TiXmlNode *pWindow, int windowID)
         else if (type == "mouse")
             buttonCode = CMouseTranslator::TranslateMouseCommand(pButton);
         else if (type == "appcommand")
-            buttonCode = TranslateAppCommand(pButton->Value());
+            buttonCode = CAppTranslator::TranslateAppCommand(pButton->Value());
         else if (type == "joystick")
         {
           std::string controllerId = DEFAULT_CONTROLLER_ID;
@@ -691,22 +656,6 @@ uint32_t CButtonTranslator::TranslateKeyboardButton(TiXmlElement *pButton)
   }
 
   return button_id;
-}
-
-uint32_t CButtonTranslator::TranslateAppCommand(const char *szButton)
-{
-#ifdef TARGET_WINDOWS
-  std::string strAppCommand = szButton;
-  StringUtils::ToLower(strAppCommand);
-
-  for (int i = 0; i < ARRAY_SIZE(appcommands); i++)
-    if (strAppCommand == appcommands[i].name)
-      return appcommands[i].action | KEY_APPCOMMAND;
-
-  CLog::Log(LOGERROR, "%s: Can't find appcommand %s", __FUNCTION__, szButton);
-#endif
-
-  return 0;
 }
 
 void CButtonTranslator::Clear()
