48556f4e388e5a9b9577c0107e3d169020ee53d3
Philipp Kerling <pkerling@casix.org>
2017-06-23 16:16:34 +0200

    Add OSScreenSaver implementation for OSX

---
+++ xbmc/windowing/osx/OSScreenSaverOSX.cpp
@@ -0,0 +1,47 @@
+/*
+ *      Copyright (C) 2017 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "OSScreenSaverOSX.h"
+
+#include <CoreFoundation/CoreFoundation.h>
+
+COSScreenSaverOSX::COSScreenSaverOSX()
+{
+}
+
+void COSScreenSaverOSX::Inhibit()
+{
+  // see Technical Q&A QA1340
+  if (m_assertionID == 0)
+  {
+    CFStringRef reasonForActivity= CFSTR("XBMC requested disable system screen saver");
+    IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep,
+      kIOPMAssertionLevelOn, reasonForActivity, &m_assertionID);
+  }
+}
+
+void COSScreenSaverOSX::Uninhibit()
+{
+  if (m_assertionID != 0)
+  {
+    IOPMAssertionRelease(m_assertionID);
+    m_assertionID = 0;
+  }
+}
