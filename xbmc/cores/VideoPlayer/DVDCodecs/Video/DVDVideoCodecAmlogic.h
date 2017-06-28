5fe3d560ef9761c08e5762b897c99caa7b681d37
peak3d <pfau@peak3d.de>
2017-06-28 17:36:19 +0200

    AMLVideoBufferPool

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.h
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.h
@@ -22,6 +22,7 @@
 #include "DVDVideoCodec.h"
 #include "DVDStreamInfo.h"
 #include "threads/CriticalSection.h"
+#include "cores/VideoPlayer/Process/VideoBuffer.h"
 
 #include <set>
 #include <atomic>
@@ -34,32 +35,37 @@ class CBitstreamConverter;
 
 class CDVDVideoCodecAmlogic;
 
-class CDVDAmlogicInfo
+class CAMLVideoBuffer : public CVideoBuffer
 {
 public:
-  CDVDAmlogicInfo(CDVDVideoCodecAmlogic *codec, CAMLCodec *amlcodec, int omxPts, int amlDuration, uint32_t bufferIndex);
-
-  // reference counting
-  CDVDAmlogicInfo* Retain();
-  long             Release();
-
-  CAMLCodec *getAmlCodec() const;
-  int GetOmxPts() const { return m_omxPts; }
-  int GetAmlDuration() const { return m_amlDuration; }
-  uint32_t GetBufferIndex() const { return m_bufferIndex; };
-  void invalidate();
-  void SetRendered() { m_rendered = true; };
-  bool IsRendered() { return m_rendered; };
-
-protected:
-  long m_refs;
-  CCriticalSection    m_section;
+  CAMLVideoBuffer(int id) : CVideoBuffer(id) {};
+  void Set(CDVDVideoCodecAmlogic *codec, std::shared_ptr<CAMLCodec> amlcodec, int omxPts, int amlDuration, uint32_t bufferIndex)
+  {
+    m_codec = codec;
+    m_amlCodec = amlcodec;
+    m_omxPts = omxPts;
+    m_amlDuration = amlDuration;
+    m_bufferIndex = bufferIndex;
+  }
 
   CDVDVideoCodecAmlogic* m_codec;
-  CAMLCodec* m_amlCodec;
+  std::shared_ptr<CAMLCodec> m_amlCodec;
   int m_omxPts, m_amlDuration;
   uint32_t m_bufferIndex;
-  bool m_rendered;
+};
+
+class CAMLVideoBufferPool : public IVideoBufferPool
+{
+public:
+  virtual ~CAMLVideoBufferPool();
+
+  virtual CVideoBuffer* Get() override;
+  virtual void Return(int id) override;
+
+private:
+  CCriticalSection m_criticalSection;;
+  std::vector<CAMLVideoBuffer*> m_videoBuffers;
+  std::vector<int> m_freeBuffers;
 };
 
 class CDVDVideoCodecAmlogic : public CDVDVideoCodec
@@ -84,10 +90,10 @@ protected:
   void            FrameQueuePop(void);
   void            FrameQueuePush(double dts, double pts);
   void            FrameRateTracking(uint8_t *pData, int iSize, double dts, double pts);
-  void            RemoveInfo(CDVDAmlogicInfo* info);
+  //void            RemoveInfo(CDVDAmlogicInfo* info);
 
-  CAMLCodec      *m_Codec;
-  std::set<CDVDAmlogicInfo*> m_inflight;
+  std::shared_ptr<CAMLCodec> m_Codec;
+
   const char     *m_pFormatName;
   VideoPicture m_videobuffer;
   bool            m_opened;
@@ -107,6 +113,6 @@ protected:
   CBitstreamParser *m_bitparser;
   CBitstreamConverter *m_bitstream;
 private:
-  CCriticalSection    m_secure;
+  std::shared_ptr<CAMLVideoBufferPool> m_videoBufferPool;
   static std::atomic<bool> m_InstanceGuard;
 };
