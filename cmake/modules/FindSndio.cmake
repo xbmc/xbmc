3b8def617f2645a0d4c9af85b5909ec7ab3030e5
wsnipex <wsnipex@a1.net>
2017-06-29 14:23:25 +0200

    [cmake] fix FindSndio module

--- cmake/modules/FindSndio.cmake
+++ cmake/modules/FindSndio.cmake
@@ -1,34 +1,41 @@
-# sndio check, based on FindAlsa.cmake
+#
+# FindSndio
+# ---------
+# Finds the Sndio Library
+#
+# This will will define the following variables:
+#
+# SNDIO_FOUND - system has sndio
+# SNDIO_INCLUDE_DIRS - sndio include directory
+# SNDIO_DEFINITIONS - sndio definitions
+#
+# and the following imported targets::
+#
+#  Sndio::Sndio    - the sndio library
 #
 
-# Copyright (c) 2006, David Faure, <faure@kde.org>
-# Copyright (c) 2007, Matthias Kretz <kretz@kde.org>
-# Copyright (c) 2009, Jacob Meuser <jakemsr@sdf.lonestar.org>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
-include(CheckIncludeFiles)
-include(CheckIncludeFileCXX)
-include(CheckLibraryExists)
-
-# Already done by toplevel
+find_path(SNDIO_INCLUDE_DIR sndio.h)
 find_library(SNDIO_LIBRARY sndio)
-set(SNDIO_LIBRARY_DIR "")
-if(SNDIO_LIBRARY)
-   get_filename_component(SNDIO_LIBRARY_DIR ${SNDIO_LIBRARY} PATH)
-endif(SNDIO_LIBRARY)
 
-check_library_exists(sndio sio_open "${SNDIO_LIBRARY_DIR}" HAVE_SNDIO)
-if(HAVE_SNDIO)
-  message(STATUS "Found sndio: ${SNDIO_LIBRARY}")
-  set(SNDIO_DEFINITIONS -DHAVE_SNDIO=1)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Sndio
+                                  REQUIRED_VARS SNDIO_LIBRARY SNDIO_INCLUDE_DIR)
+
+if(SNDIO_FOUND)
+  set(SNDIO_INCLUDE_DIRS ${SNDIO_INCLUDE_DIR})
   set(SNDIO_LIBRARIES ${SNDIO_LIBRARY})
-else(HAVE_SNDIO)
-    message(STATUS "sndio not found")
-endif(HAVE_SNDIO)
-set(SNDIO_FOUND ${HAVE_SNDIO})
+  set(SNDIO_DEFINITIONS -DHAVE_SNDIO=1)
 
-find_path(SNDIO_INCLUDES sndio.h)
+  if(NOT TARGET Sndio::Sndio)
+    add_library(Sndio::Sndio UNKNOWN IMPORTED)
+    set_target_properties(Sndio::Sndio PROPERTIES
+                                       IMPORTED_LOCATION "${SNDIO_LIBRARY}"
+                                       INTERFACE_INCLUDE_DIRECTORIES "${SNDIO_INCLUDE_DIR}")
+    set_target_properties(Sndio::Sndio PROPERTIES
+                                       INTERFACE_COMPILE_DEFINITIONS -DHAVE_SNDIO=1)
+  endif()
+endif()
 
-mark_as_advanced(SNDIO_INCLUDES SNDIO_LIBRARY)
+
+mark_as_advanced(SNDIO_INCLUDE_DIR SNDIO_LIBRARY)
