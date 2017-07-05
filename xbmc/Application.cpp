xbmc/xbmc

commit 26349e1fb91b3f27214492924b8e4fae50c29c6c

Move CInputManager into service manager

xbmc/Application.cpp
@@ -94,6 +94,7 @@
 #include "utils/CPUInfo.h"
 #include "utils/log.h"
 #include "utils/SeekHandler.h"
+#include "ServiceBroker.h"
 
 #include "input/KeyboardLayoutManager.h"
 
@@ -350,7 +351,7 @@ bool CApplication::OnEvent(XBMC_Event& newEvent)
       g_application.OnAction(CAction(ACTION_MOUSE_MOVE, 0, static_cast<float>(newEvent.focus.x), static_cast<float>(newEvent.focus.y), 0, 0));
       break;
     default:
-      return CInputManager::GetInstance().OnEvent(newEvent);
+      return CServiceBroker::GetInputManager().OnEvent(newEvent);
   }
   return true;
 }
@@ -644,9 +645,6 @@ bool CApplication::Create()
   m_replayGainSettings.iNoGainPreAmp = m_ServiceManager->GetSettings().GetInt(CSettings::SETTING_MUSICPLAYER_REPLAYGAINNOGAINPREAMP);
   m_replayGainSettings.bAvoidClipping = m_ServiceManager->GetSettings().GetBool(CSettings::SETTING_MUSICPLAYER_REPLAYGAINAVOIDCLIPPING);
 
-  // Create the Mouse, Keyboard and Remote
-  CInputManager::GetInstance().InitializeInputs();
-
   // load the keyboard layouts
   if (!CKeyboardLayoutManager::GetInstance().Load())
   {
@@ -736,7 +734,7 @@ bool CApplication::CreateGUI()
 
   // The key mappings may already have been loaded by a peripheral
   CLog::Log(LOGINFO, "load keymapping");
-  if (!CButtonTranslator::GetInstance().Load())
+  if (!CServiceBroker::GetInputManager().LoadKeymaps())
     return false;
 
   RESOLUTION_INFO info = g_graphicsContext.GetResInfo();
@@ -1923,7 +1921,7 @@ bool CApplication::OnAppCommand(const CAction &action)
   uint32_t appcmd = action.GetID();
   CKey key(appcmd | KEY_APPCOMMAND, (unsigned int) 0);
   int iWin = g_windowManager.GetActiveWindow() & WINDOW_ID_MASK;
-  CAction appcmdaction = CButtonTranslator::GetInstance().GetAction(iWin, key);
+  CAction appcmdaction = CServiceBroker::GetInputManager().GetAction(iWin, key);
 
   // If we couldn't find an action return false to indicate we have not
   // handled this appcommand
@@ -1962,8 +1960,7 @@ bool CApplication::OnAction(const CAction &action)
   }
 
   if (action.IsMouse())
-    CInputManager::GetInstance().SetMouseActive(true);
-
+    CServiceBroker::GetInputManager().SetMouseActive(true);
 
   if (action.GetID() == ACTION_CREATE_EPISODE_BOOKMARK)
   {
@@ -2023,10 +2020,7 @@ bool CApplication::OnAction(const CAction &action)
 
   // reload keymaps
   if (action.GetID() == ACTION_RELOAD_KEYMAPS)
-  {
-    CButtonTranslator::GetInstance().Clear();
-    CButtonTranslator::GetInstance().Load();
-  }
+    CServiceBroker::GetInputManager().ReloadKeymaps();
 
   // show info : Shows the current video or song information
   if (action.GetID() == ACTION_SHOW_INFO)
@@ -2671,7 +2665,7 @@ void CApplication::FrameMove(bool processEvents, bool processGUI)
     }
     CWinEvents::MessagePump();
 
-    CInputManager::GetInstance().Process(g_windowManager.GetActiveWindowID(), frameTime);
+    CServiceBroker::GetInputManager().Process(g_windowManager.GetActiveWindowID(), frameTime);
 
     if (processGUI && m_renderGUI)
     {
@@ -2765,7 +2759,7 @@ bool CApplication::Cleanup()
     g_LangCodeExpander.Clear();
     g_charsetConverter.clear();
     g_directoryCache.Clear();
-    CButtonTranslator::GetInstance().Clear();
+    //CServiceBroker::GetInputManager().ClearKeymaps(); //! @todo
 #ifdef HAS_EVENT_SERVER
     CEventServer::RemoveInstance();
 #endif
@@ -2905,7 +2899,7 @@ void CApplication::Stop(int exitCode)
     m_ServiceManager->DestroyAudioEngine();
 
     CLog::Log(LOGNOTICE, "closing down remote control service");
-    CInputManager::GetInstance().DisableRemoteControl();
+    CServiceBroker::GetInputManager().DisableRemoteControl();
 
     // unregister ffmpeg lock manager call back
     av_lockmgr_register(NULL);

xbmc/AppParamParser.cpp
@@ -56,13 +56,13 @@ void CAppParamParser::Parse(const char* const* argv, int nArgs)
         {
           if ((argv[next][0] != '-') && (argv[next][0] == '/'))
           {
-            CInputManager::GetInstance().SetRemoteControlName(argv[next]);
+            CServiceBroker::GetInputManager().SetRemoteControlName(argv[next]);
             i++;
           }
         }
       }
       else if (strnicmp(argv[i], "-n", 2) == 0 || strnicmp(argv[i], "--nolirc", 8) == 0)
-        CInputManager::GetInstance().DisableRemoteControl();
+        CServiceBroker::GetInputManager().DisableRemoteControl();
 
       if (stricmp(argv[i], "-d") == 0)
       {

xbmc/ServiceBroker.cpp
@@ -105,6 +105,11 @@ ADDON::CServiceAddonManager& CServiceBroker::GetServiceAddons()
   return g_application.m_ServiceManager->GetServiceAddons();
 }
 
+CInputManager& CServiceBroker::GetInputManager()
+{
+  return g_application.m_ServiceManager->GetInputManager();
+}
+
 bool CServiceBroker::IsBinaryAddonCacheUp()
 {
   return g_application.m_ServiceManager->init_level > 1;

xbmc/ServiceBroker.h
@@ -53,6 +53,7 @@ class CDataCacheCore;
 class CSettings;
 class IAE;
 class CFavouritesService;
+class CInputManager;
 
 namespace KODI
 {
@@ -86,5 +87,6 @@ public:
   static PERIPHERALS::CPeripherals& GetPeripherals();
   static CFavouritesService& GetFavouritesService();
   static ADDON::CServiceAddonManager& GetServiceAddons();
+  static CInputManager& GetInputManager();
   static bool IsBinaryAddonCacheUp();
 };

xbmc/ServiceManager.cpp
@@ -31,6 +31,7 @@
 #include "PlayListPlayer.h"
 #include "profiles/ProfilesManager.h"
 #include "utils/log.h"
+#include "input/InputManager.h"
 #include "interfaces/AnnouncementManager.h"
 #include "interfaces/generic/ScriptInvocationManager.h"
 #include "interfaces/python/XBPython.h"
@@ -41,7 +42,8 @@ using namespace KODI;
 
 CServiceManager::CServiceManager() :
   m_gameServices(new GAME::CGameServices),
-  m_peripherals(new PERIPHERALS::CPeripherals)
+  m_peripherals(new PERIPHERALS::CPeripherals),
+  m_inputManager(new CInputManager)
 {
 }
 
@@ -100,6 +102,8 @@ bool CServiceManager::Init2()
   m_contextMenuManager.reset(new CContextMenuManager(*m_addonMgr.get()));
   m_serviceAddons.reset(new ADDON::CServiceAddonManager(*m_addonMgr));
 
+  m_inputManager->InitializeInputs();
+
   init_level = 2;
   return true;
 }
@@ -149,6 +153,7 @@ void CServiceManager::Deinit()
   m_serviceAddons.reset();
   m_gameServices->Deinit();
   m_peripherals.reset();
+  //m_inputManager->Deinitialize(); //! @todo
   m_contextMenuManager.reset();
   m_favouritesService.reset();
   m_vfsAddonCache.reset();
@@ -252,6 +257,11 @@ PERIPHERALS::CPeripherals& CServiceManager::GetPeripherals()
 CFavouritesService& CServiceManager::GetFavouritesService()
 {
   return *m_favouritesService;
+}
+
+CInputManager& CServiceManager::GetInputManager()
+{
+  return *m_inputManager;
 }
 
 // deleters for unique_ptr

xbmc/ServiceManager.h
@@ -72,6 +72,8 @@ namespace PERIPHERALS
   class CPeripherals;
 }
 
+class CInputManager;
+
 class CServiceManager
 {
 public:
@@ -109,6 +111,7 @@ public:
 
   CSettings& GetSettings();
   CFavouritesService& GetFavouritesService();
+  CInputManager &GetInputManager();
 
 protected:
   struct delete_dataCacheCore
@@ -150,4 +153,5 @@ protected:
   std::unique_ptr<KODI::GAME::CGameServices> m_gameServices;
   std::unique_ptr<PERIPHERALS::CPeripherals> m_peripherals;
   std::unique_ptr<CFavouritesService, delete_favouritesService> m_favouritesService;
+  std::unique_ptr<CInputManager> m_inputManager;
 };

xbmc/cores/ExternalPlayer/ExternalPlayer.cpp
@@ -461,13 +461,13 @@ bool CExternalPlayer::ExecuteAppLinux(const char* strSwitches)
 {
   CLog::Log(LOGNOTICE, "%s: %s", __FUNCTION__, strSwitches);
 
-  bool remoteUsed = CInputManager::GetInstance().IsRemoteControlEnabled();
-  CInputManager::GetInstance().DisableRemoteControl();
+  bool remoteUsed = CServiceBroker::GetInputManager().IsRemoteControlEnabled();
+  CServiceBroker::GetInputManager().DisableRemoteControl();
 
   int ret = system(strSwitches);
 
   if (remoteUsed)
-    CInputManager::GetInstance().EnableRemoteControl();
+    CServiceBroker::GetInputManager().EnableRemoteControl();
 
   if (ret != 0)
   {

xbmc/games/addons/GameClient.cpp
@@ -39,6 +39,7 @@
 #include "guilib/GUIWindowManager.h"
 #include "guilib/WindowIDs.h"
 #include "input/joysticks/JoystickTypes.h"
+#include "input/InputManager.h"
 #include "messaging/ApplicationMessenger.h"
 #include "peripherals/Peripherals.h"
 #include "profiles/ProfilesManager.h"
@@ -881,7 +882,7 @@ bool CGameClient::SetRumble(unsigned int port, const std::string& feature, float
 
 void CGameClient::OpenKeyboard(void)
 {
-  m_keyboard.reset(new CGameClientKeyboard(this, &m_struct.toAddon));
+  m_keyboard.reset(new CGameClientKeyboard(this, &m_struct.toAddon, &CServiceBroker::GetInputManager()));
 }
 
 void CGameClient::CloseKeyboard(void)
@@ -891,7 +892,7 @@ void CGameClient::CloseKeyboard(void)
 
 void CGameClient::OpenMouse(void)
 {
-  m_mouse.reset(new CGameClientMouse(this, &m_struct.toAddon));
+  m_mouse.reset(new CGameClientMouse(this, &m_struct.toAddon, &CServiceBroker::GetInputManager()));
 
   CSingleLock lock(m_critSection);
 

commit a7d2a4f8169d554dca790a41d86ba53764921d6c

Fix crash on startup
This fixes a crash on startup introduced in commit 26349e1, "Move
CInputManager into service manager" from PR 12366.

xbmc/Application.cpp
@@ -252,6 +252,7 @@ CApplication::CApplication(void)
   , m_iScreenSaveLock(0)
   , m_bPlaybackStarting(false)
   , m_ePlayState(PLAY_STATE_NONE)
+  , m_ServiceManager(new CServiceManager)
   , m_confirmSkinChange(true)
   , m_ignoreSkinSettingChanges(false)
   , m_saveSkinOnUnloading(true)
@@ -417,7 +418,6 @@ bool CApplication::Create()
   // Grab a handle to our thread to be used later in identifying the render thread.
   m_threadID = CThread::GetCurrentThreadId();
 
-  m_ServiceManager.reset(new CServiceManager());
   if (!m_ServiceManager->Init1())
   {
     return false;