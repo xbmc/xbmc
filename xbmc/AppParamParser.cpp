xbmc/xbmc

commit 26349e1fb91b3f27214492924b8e4fae50c29c6c

Move CInputManager into service manager

xbmc/AppParamParser.cpp
@@ -56,13 +56,13 @@ void CAppParamParser::Parse(const char* const* argv, int nArgs)
         {
           if ((argv[next][0] != '-') && (argv[next][0] == '/'))
           {
-            CInputManager::GetInstance().SetRemoteControlName(argv[next]);
+            CServiceBroker::GetInputManager().SetRemoteControlName(argv[next]);
             i++;
           }
         }
       }
       else if (strnicmp(argv[i], "-n", 2) == 0 || strnicmp(argv[i], "--nolirc", 8) == 0)
-        CInputManager::GetInstance().DisableRemoteControl();
+        CServiceBroker::GetInputManager().DisableRemoteControl();
 
       if (stricmp(argv[i], "-d") == 0)
       {

xbmc/Application.cpp
@@ -94,6 +94,7 @@
 #include "utils/CPUInfo.h"
 #include "utils/log.h"
 #include "utils/SeekHandler.h"
+#include "ServiceBroker.h"
 
 #include "input/KeyboardLayoutManager.h"
 
@@ -350,7 +351,7 @@ bool CApplication::OnEvent(XBMC_Event& newEvent)
       g_application.OnAction(CAction(ACTION_MOUSE_MOVE, 0, static_cast<float>(newEvent.focus.x), static_cast<float>(newEvent.focus.y), 0, 0));
       break;
     default:
-      return CInputManager::GetInstance().OnEvent(newEvent);
+      return CServiceBroker::GetInputManager().OnEvent(newEvent);
   }
   return true;
 }
@@ -644,9 +645,6 @@ bool CApplication::Create()
   m_replayGainSettings.iNoGainPreAmp = m_ServiceManager->GetSettings().GetInt(CSettings::SETTING_MUSICPLAYER_REPLAYGAINNOGAINPREAMP);
   m_replayGainSettings.bAvoidClipping = m_ServiceManager->GetSettings().GetBool(CSettings::SETTING_MUSICPLAYER_REPLAYGAINAVOIDCLIPPING);
 
-  // Create the Mouse, Keyboard and Remote
-  CInputManager::GetInstance().InitializeInputs();
-
   // load the keyboard layouts
   if (!CKeyboardLayoutManager::GetInstance().Load())
   {
@@ -736,7 +734,7 @@ bool CApplication::CreateGUI()
 
   // The key mappings may already have been loaded by a peripheral
   CLog::Log(LOGINFO, "load keymapping");
-  if (!CButtonTranslator::GetInstance().Load())
+  if (!CServiceBroker::GetInputManager().LoadKeymaps())
     return false;
 
   RESOLUTION_INFO info = g_graphicsContext.GetResInfo();
@@ -1923,7 +1921,7 @@ bool CApplication::OnAppCommand(const CAction &action)
   uint32_t appcmd = action.GetID();
   CKey key(appcmd | KEY_APPCOMMAND, (unsigned int) 0);
   int iWin = g_windowManager.GetActiveWindow() & WINDOW_ID_MASK;
-  CAction appcmdaction = CButtonTranslator::GetInstance().GetAction(iWin, key);
+  CAction appcmdaction = CServiceBroker::GetInputManager().GetAction(iWin, key);
 
   // If we couldn't find an action return false to indicate we have not
   // handled this appcommand
@@ -1962,8 +1960,7 @@ bool CApplication::OnAction(const CAction &action)
   }
 
   if (action.IsMouse())
-    CInputManager::GetInstance().SetMouseActive(true);
-
+    CServiceBroker::GetInputManager().SetMouseActive(true);
 
   if (action.GetID() == ACTION_CREATE_EPISODE_BOOKMARK)
   {
@@ -2023,10 +2020,7 @@ bool CApplication::OnAction(const CAction &action)
 
   // reload keymaps
   if (action.GetID() == ACTION_RELOAD_KEYMAPS)
-  {
-    CButtonTranslator::GetInstance().Clear();
-    CButtonTranslator::GetInstance().Load();
-  }
+    CServiceBroker::GetInputManager().ReloadKeymaps();
 
   // show info : Shows the current video or song information
   if (action.GetID() == ACTION_SHOW_INFO)
@@ -2671,7 +2665,7 @@ void CApplication::FrameMove(bool processEvents, bool processGUI)
     }
     CWinEvents::MessagePump();
 
-    CInputManager::GetInstance().Process(g_windowManager.GetActiveWindowID(), frameTime);
+    CServiceBroker::GetInputManager().Process(g_windowManager.GetActiveWindowID(), frameTime);
 
     if (processGUI && m_renderGUI)
     {
@@ -2765,7 +2759,7 @@ bool CApplication::Cleanup()
     g_LangCodeExpander.Clear();
     g_charsetConverter.clear();
     g_directoryCache.Clear();
-    CButtonTranslator::GetInstance().Clear();
+    //CServiceBroker::GetInputManager().ClearKeymaps(); //! @todo
 #ifdef HAS_EVENT_SERVER
     CEventServer::RemoveInstance();
 #endif
@@ -2905,7 +2899,7 @@ void CApplication::Stop(int exitCode)
     m_ServiceManager->DestroyAudioEngine();
 
     CLog::Log(LOGNOTICE, "closing down remote control service");
-    CInputManager::GetInstance().DisableRemoteControl();
+    CServiceBroker::GetInputManager().DisableRemoteControl();
 
     // unregister ffmpeg lock manager call back
     av_lockmgr_register(NULL);

xbmc/ServiceManager.cpp
@@ -31,6 +31,7 @@
 #include "PlayListPlayer.h"
 #include "profiles/ProfilesManager.h"
 #include "utils/log.h"
+#include "input/InputManager.h"
 #include "interfaces/AnnouncementManager.h"
 #include "interfaces/generic/ScriptInvocationManager.h"
 #include "interfaces/python/XBPython.h"
@@ -41,7 +42,8 @@ using namespace KODI;
 
 CServiceManager::CServiceManager() :
   m_gameServices(new GAME::CGameServices),
-  m_peripherals(new PERIPHERALS::CPeripherals)
+  m_peripherals(new PERIPHERALS::CPeripherals),
+  m_inputManager(new CInputManager)
 {
 }
 
@@ -100,6 +102,8 @@ bool CServiceManager::Init2()
   m_contextMenuManager.reset(new CContextMenuManager(*m_addonMgr.get()));
   m_serviceAddons.reset(new ADDON::CServiceAddonManager(*m_addonMgr));
 
+  m_inputManager->InitializeInputs();
+
   init_level = 2;
   return true;
 }
@@ -149,6 +153,7 @@ void CServiceManager::Deinit()
   m_serviceAddons.reset();
   m_gameServices->Deinit();
   m_peripherals.reset();
+  //m_inputManager->Deinitialize(); //! @todo
   m_contextMenuManager.reset();
   m_favouritesService.reset();
   m_vfsAddonCache.reset();
@@ -252,6 +257,11 @@ PERIPHERALS::CPeripherals& CServiceManager::GetPeripherals()
 CFavouritesService& CServiceManager::GetFavouritesService()
 {
   return *m_favouritesService;
+}
+
+CInputManager& CServiceManager::GetInputManager()
+{
+  return *m_inputManager;
 }
 
 // deleters for unique_ptr

xbmc/ServiceManager.h
@@ -72,6 +72,8 @@ namespace PERIPHERALS
   class CPeripherals;
 }
 
+class CInputManager;
+
 class CServiceManager
 {
 public:
@@ -109,6 +111,7 @@ public:
 
   CSettings& GetSettings();
   CFavouritesService& GetFavouritesService();
+  CInputManager &GetInputManager();
 
 protected:
   struct delete_dataCacheCore
@@ -150,4 +153,5 @@ protected:
   std::unique_ptr<KODI::GAME::CGameServices> m_gameServices;
   std::unique_ptr<PERIPHERALS::CPeripherals> m_peripherals;
   std::unique_ptr<CFavouritesService, delete_favouritesService> m_favouritesService;
+  std::unique_ptr<CInputManager> m_inputManager;
 };

xbmc/input/InputManager.cpp
@@ -23,6 +23,8 @@
 #include "Application.h"
 #include "ServiceBroker.h"
 #include "InputManager.h"
+#include "IRTranslator.h"
+#include "KeymapEnvironment.h"
 #include "input/keyboard/IKeyboardHandler.h"
 #include "input/mouse/generic/MouseInputHandling.h"
 #include "input/mouse/IMouseDriverHandler.h"
@@ -74,6 +76,9 @@ using namespace KODI;
 using namespace MESSAGING;
 
 CInputManager::CInputManager() :
+  m_keymapEnvironment(new CKeymapEnvironment),
+  m_buttonTranslator(new CButtonTranslator),
+  m_irTranslator(new CIRTranslator),
   m_mouseButtonMap(new MOUSE::CMouseWindowingButtonMap),
   m_keyboardEasterEgg(new KEYBOARD::CKeyboardEasterEgg)
 {
@@ -83,12 +88,6 @@ CInputManager::CInputManager() :
 CInputManager::~CInputManager()
 {
   UnregisterKeyboardHandler(m_keyboardEasterEgg.get());
-}
-
-CInputManager& CInputManager::GetInstance()
-{
-  static CInputManager inputManager;
-  return inputManager;
 }
 
 void CInputManager::InitializeInputs()
@@ -150,7 +149,7 @@ bool CInputManager::ProcessMouse(int windowId)
 
   // Retrieve the corresponding action
   CKey key(mousekey, (unsigned int)0);
-  CAction mouseaction = CButtonTranslator::GetInstance().GetAction(windowId, key);
+  CAction mouseaction = m_buttonTranslator->GetAction(windowId, key);
 
   // Deactivate mouse if non-mouse action
   if (!mouseaction.IsMouse())
@@ -234,7 +233,7 @@ bool CInputManager::ProcessEventServer(int windowId, float frameTime)
         std::string actionName;
         
         // Translate using custom controller translator.
-        if (CButtonTranslator::GetInstance().TranslateCustomControllerString(windowId, strMapName, wKeyID, actionID, actionName))
+        if (m_buttonTranslator->TranslateCustomControllerString(windowId, strMapName, wKeyID, actionID, actionName))
         {
           // break screensaver
           g_application.ResetSystemIdleTimer();
@@ -364,7 +363,7 @@ bool CInputManager::OnEvent(XBMC_Event& newEvent)
       // Do not repeat long presses
       break;
     }
-    if (!CButtonTranslator::GetInstance().HasLongpressMapping(g_windowManager.GetActiveWindowID(), key))
+    if (!m_buttonTranslator->HasLongpressMapping(g_windowManager.GetActiveWindowID(), key))
     {
       m_LastKey.Reset();
       OnKey(key);
@@ -442,7 +441,7 @@ bool CInputManager::OnEvent(XBMC_Event& newEvent)
     else
     {
       int iWin = g_windowManager.GetActiveWindowID();
-      CButtonTranslator::GetInstance().TranslateTouchAction(iWin, newEvent.touch.action, newEvent.touch.pointers, actionId, actionString);
+      m_buttonTranslator->TranslateTouchAction(iWin, newEvent.touch.action, newEvent.touch.pointers, actionId, actionString);
     }
 
     if (actionId <= 0)
@@ -496,7 +495,7 @@ bool CInputManager::OnKey(const CKey& key)
 
   // this will be checked for certain keycodes that need
   // special handling if the screensaver is active
-  CAction action = CButtonTranslator::GetInstance().GetAction(iWin, key);
+  CAction action = m_buttonTranslator->GetAction(iWin, key);
 
   // a key has been pressed.
   // reset Idle Timer
@@ -559,7 +558,7 @@ bool CInputManager::OnKey(const CKey& key)
     {
       // use the virtualkeyboard section of the keymap, and send keyboard-specific or navigation
       // actions through if that's what they are
-      CAction action = CButtonTranslator::GetInstance().GetAction(WINDOW_DIALOG_KEYBOARD, key);
+      CAction action = m_buttonTranslator->GetAction(WINDOW_DIALOG_KEYBOARD, key);
       if (!(action.GetID() == ACTION_MOVE_LEFT ||
         action.GetID() == ACTION_MOVE_RIGHT ||
         action.GetID() == ACTION_MOVE_UP ||
@@ -571,7 +570,7 @@ bool CInputManager::OnKey(const CKey& key)
         action.GetID() == ACTION_VOICE_RECOGNIZE))
       {
         // the action isn't plain navigation - check for a keyboard-specific keymap
-        action = CButtonTranslator::GetInstance().GetAction(WINDOW_DIALOG_KEYBOARD, key, false);
+        action = m_buttonTranslator->GetAction(WINDOW_DIALOG_KEYBOARD, key, false);
         if (!(action.GetID() >= REMOTE_0 && action.GetID() <= REMOTE_9) ||
             action.GetID() == ACTION_BACKSPACE ||
             action.GetID() == ACTION_SHIFT ||
@@ -620,10 +619,10 @@ bool CInputManager::OnKey(const CKey& key)
     if (key.GetFromService())
     {
       if (key.GetButtonCode() != KEY_INVALID)
-        action = CButtonTranslator::GetInstance().GetAction(iWin, key);
+        action = m_buttonTranslator->GetAction(iWin, key);
     }
     else
-      action = CButtonTranslator::GetInstance().GetAction(iWin, key);
+      action = m_buttonTranslator->GetAction(iWin, key);
   }
   if (!key.IsAnalogButton())
     CLog::LogF(LOGDEBUG, "%s pressed, action is %s", m_Keyboard.GetKeyName((int)key.GetButtonCode()).c_str(), action.GetName().c_str());
@@ -866,6 +865,63 @@ bool CInputManager::OnAction(const CAction& action)
   }
 
   return false;
+}
+
+bool CInputManager::LoadKeymaps()
+{
+  if (!m_buttonTranslator->Load())
+    return false;
+
+  m_irTranslator->Load();
+
+  return true;
+}
+
+bool CInputManager::ReloadKeymaps()
+{
+  //! @todo
+  ClearKeymaps();
+
+  if (!LoadKeymaps())
+    return false;
+
+  return true;
+}
+
+void CInputManager::ClearKeymaps()
+{
+  m_buttonTranslator->Clear();
+  m_irTranslator->Clear();
+}
+
+void CInputManager::AddKeymap(const std::string &keymap)
+{
+  m_buttonTranslator->AddDevice(keymap);
+}
+
+void CInputManager::RemoveKeymap(const std::string &keymap)
+{
+  m_buttonTranslator->RemoveDevice(keymap);
+}
+
+CAction CInputManager::GetAction(int window, const CKey &key, bool fallback /* = true */)
+{
+  return m_buttonTranslator->GetAction(window, key, fallback);
+}
+
+CAction CInputManager::GetGlobalAction(const CKey &key)
+{
+  return m_buttonTranslator->GetGlobalAction(key);
+}
+
+std::vector<const IWindowKeymap*> CInputManager::GetJoystickKeymaps() const
+{
+  return m_buttonTranslator->JoystickKeymaps();
+}
+
+int CInputManager::TranslateLircRemoteString(const std::string &szDevice, const std::string &szButton)
+{
+  return m_irTranslator->TranslateButton(szDevice, szButton);
 }
 
 void CInputManager::RegisterKeyboardHandler(KEYBOARD::IKeyboardHandler* handler)

commit b1592173baa7e53008bcda960b7c8994f677646b

AppParamParser: Fix crash on startup
This fixes a crash on startup introduced in commit 26349e1, "Move
CInputManager into Service Manager" from PR 12366.

xbmc/AppParamParser.cpp
@@ -56,13 +56,13 @@ void CAppParamParser::Parse(const char* const* argv, int nArgs)
         {
           if ((argv[next][0] != '-') && (argv[next][0] == '/'))
           {
-            CServiceBroker::GetInputManager().SetRemoteControlName(argv[next]);
+            m_remoteControlName = argv[next];
             i++;
           }
         }
       }
       else if (strnicmp(argv[i], "-n", 2) == 0 || strnicmp(argv[i], "--nolirc", 8) == 0)
-        CServiceBroker::GetInputManager().DisableRemoteControl();
+        m_remoteControlEnabled = false;
 
       if (stricmp(argv[i], "-d") == 0)
       {

xbmc/AppParamParser.h
@@ -25,7 +25,10 @@ class CAppParamParser
   public:
     CAppParamParser();
     void Parse(const char* const* argv, int nArgs);
-    CFileItemList m_playlist;
+
+    const CFileItemList &Playlist() const { return m_playlist; }
+    bool RemoteControlEnabled() const { return m_remoteControlEnabled; }
+    const std::string &RemoteControlName() const { return m_remoteControlName; }
 
   private:
     bool m_testmode;
@@ -33,4 +36,8 @@ class CAppParamParser
     void DisplayHelp();
     void DisplayVersion();
     void EnableDebugMode();
+
+    CFileItemList m_playlist;
+    bool m_remoteControlEnabled = true;
+    std::string m_remoteControlName;
 };

xbmc/Application.cpp
@@ -412,7 +412,7 @@ bool CApplication::SetupNetwork()
   return m_network != NULL;
 }
 
-bool CApplication::Create()
+bool CApplication::Create(const CAppParamParser &params)
 {
   // Grab a handle to our thread to be used later in identifying the render thread.
   m_threadID = CThread::GetCurrentThreadId();
@@ -622,7 +622,7 @@ bool CApplication::Create()
   // initialize the addon database (must be before the addon manager is init'd)
   CDatabaseManager::GetInstance().Initialize(true);
 
-  if (!m_ServiceManager->InitStageTwo())
+  if (!m_ServiceManager->InitStageTwo(params))
   {
     return false;
   }

xbmc/Application.h
@@ -135,7 +135,7 @@ public:
   void FrameMove(bool processEvents, bool processGUI = true) override;
   void Render() override;
   virtual void Preflight();
-  bool Create();
+  bool Create(const CAppParamParser &params);
   bool Cleanup() override;
 
   bool CreateGUI();

xbmc/PlayListPlayer.cpp
@@ -618,7 +618,7 @@ void CPlayListPlayer::ReShuffle(int iPlaylist, int iPosition)
   }
 }
 
-void CPlayListPlayer::Add(int iPlaylist, CPlayList& playlist)
+void CPlayListPlayer::Add(int iPlaylist, const CPlayList& playlist)
 {
   if (iPlaylist != PLAYLIST_MUSIC && iPlaylist != PLAYLIST_VIDEO)
     return;
@@ -640,7 +640,7 @@ void CPlayListPlayer::Add(int iPlaylist, const CFileItemPtr &pItem)
     ReShuffle(iPlaylist, iSize);
 }
 
-void CPlayListPlayer::Add(int iPlaylist, CFileItemList& items)
+void CPlayListPlayer::Add(int iPlaylist, const CFileItemList& items)
 {
   if (iPlaylist != PLAYLIST_MUSIC && iPlaylist != PLAYLIST_VIDEO)
     return;
@@ -651,7 +651,7 @@ void CPlayListPlayer::Add(int iPlaylist, CFileItemList& items)
     ReShuffle(iPlaylist, iSize);
 }
 
-void CPlayListPlayer::Insert(int iPlaylist, CPlayList& playlist, int iIndex)
+void CPlayListPlayer::Insert(int iPlaylist, const CPlayList& playlist, int iIndex)
 {
   if (iPlaylist != PLAYLIST_MUSIC && iPlaylist != PLAYLIST_VIDEO)
     return;
@@ -677,7 +677,7 @@ void CPlayListPlayer::Insert(int iPlaylist, const CFileItemPtr &pItem, int iInde
     m_iCurrentSong++;
 }
 
-void CPlayListPlayer::Insert(int iPlaylist, CFileItemList& items, int iIndex)
+void CPlayListPlayer::Insert(int iPlaylist, const CFileItemList& items, int iIndex)
 {
   if (iPlaylist != PLAYLIST_MUSIC && iPlaylist != PLAYLIST_VIDEO)
     return;

xbmc/PlayListPlayer.h
@@ -169,12 +169,12 @@ public:
   REPEAT_STATE GetRepeat(int iPlaylist) const;
 
   // add items via the playlist player
-  void Add(int iPlaylist, CPlayList& playlist);
+  void Add(int iPlaylist, const CPlayList& playlist);
   void Add(int iPlaylist, const CFileItemPtr &pItem);
-  void Add(int iPlaylist, CFileItemList& items);
-  void Insert(int iPlaylist, CPlayList& playlist, int iIndex);
+  void Add(int iPlaylist, const CFileItemList& items);
+  void Insert(int iPlaylist, const CPlayList& playlist, int iIndex);
   void Insert(int iPlaylist, const CFileItemPtr &pItem, int iIndex);
-  void Insert(int iPlaylist, CFileItemList& items, int iIndex);
+  void Insert(int iPlaylist, const CFileItemList& items, int iIndex);
   void Remove(int iPlaylist, int iPosition);
   void Swap(int iPlaylist, int indexItem1, int indexItem2);
 

xbmc/ServiceManager.cpp
@@ -67,7 +67,7 @@ bool CServiceManager::InitStageOne()
   return true;
 }
 
-bool CServiceManager::InitStageTwo()
+bool CServiceManager::InitStageTwo(const CAppParamParser &params)
 {
   m_Platform.reset(CPlatform::CreateInstance());
   m_Platform->Init();
@@ -105,7 +105,7 @@ bool CServiceManager::InitStageTwo()
 
   m_controllerManager.reset(new GAME::CControllerManager);
 
-  m_inputManager.reset(new CInputManager);
+  m_inputManager.reset(new CInputManager(params));
   m_inputManager->InitializeInputs();
 
   init_level = 2;

xbmc/ServiceManager.h
@@ -23,6 +23,8 @@
 #include <memory>
 #include "platform/Platform.h"
 
+class CAppParamParser;
+
 namespace ADDON {
 class CAddonMgr;
 class CBinaryAddonManager;
@@ -83,7 +85,7 @@ public:
   ~CServiceManager();
 
   bool InitStageOne();
-  bool InitStageTwo();
+  bool InitStageTwo(const CAppParamParser &params);
   bool CreateAudioEngine();
   bool DestroyAudioEngine();
   bool StartAudioEngine();

xbmc/XBApplicationEx.cpp
@@ -34,6 +34,7 @@
 #ifdef TARGET_POSIX
 #include "linux/XTimeUtils.h"
 #endif
+#include "AppParamParser.h"
 
 // Put this here for easy enable and disable
 #ifndef _DEBUG
@@ -62,7 +63,7 @@ VOID CXBApplicationEx::Destroy()
 }
 
 /* Function that runs the application */
-INT CXBApplicationEx::Run(CFileItemList &playlist)
+INT CXBApplicationEx::Run(const CAppParamParser &params)
 {
   CLog::Log(LOGNOTICE, "Running the application..." );
 
@@ -70,9 +71,9 @@ INT CXBApplicationEx::Run(CFileItemList &playlist)
   unsigned int frameTime = 0;
   const unsigned int noRenderFrameTime = 15;  // Simulates ~66fps
 
-  if (playlist.Size() > 0)
+  if (params.Playlist().Size() > 0)
   {
-    CServiceBroker::GetPlaylistPlayer().Add(0, playlist);
+    CServiceBroker::GetPlaylistPlayer().Add(0, params.Playlist());
     CServiceBroker::GetPlaylistPlayer().SetCurrentPlaylist(0);
     KODI::MESSAGING::CApplicationMessenger::GetInstance().PostMsg(TMSG_PLAYLISTPLAYER_PLAY, -1);
   }

xbmc/XBApplicationEx.h
@@ -23,7 +23,7 @@
 
 #include "guilib/IWindowManagerCallback.h"
 
-class CFileItemList;
+class CAppParamParser;
 
 // Do not change the numbering, external scripts depend on them
 enum {
@@ -51,7 +51,7 @@ public:
   virtual void SetRenderGUI(bool renderGUI) {};
 
 public:
-  INT Run(CFileItemList &playlist);
+  INT Run(const CAppParamParser &params);
   VOID Destroy();
 
 private:

xbmc/input/InputManager.cpp
@@ -62,6 +62,7 @@
 #include "utils/StringUtils.h"
 #include "Util.h"
 #include "settings/Settings.h"
+#include "AppParamParser.h"
 
 #ifdef HAS_PERFORMANCE_SAMPLE
 #include "utils/PerformanceSample.h"
@@ -78,7 +79,7 @@ using EVENTSERVER::CEventServer;
 using namespace KODI;
 using namespace MESSAGING;
 
-CInputManager::CInputManager() :
+CInputManager::CInputManager(const CAppParamParser &params) :
   m_keymapEnvironment(new CKeymapEnvironment),
   m_buttonTranslator(new CButtonTranslator),
   m_irTranslator(new CIRTranslator),
@@ -93,6 +94,12 @@ CInputManager::CInputManager() :
   m_buttonTranslator->RegisterMapper("joystick", m_joystickTranslator.get());
 
   RegisterKeyboardHandler(m_keyboardEasterEgg.get());
+
+  if (!params.RemoteControlName().empty())
+    SetRemoteControlName(params.RemoteControlName());
+
+  if (!params.RemoteControlEnabled())
+    DisableRemoteControl();
 }
 
 CInputManager::~CInputManager()

xbmc/input/InputManager.h
@@ -42,6 +42,7 @@
 #include "threads/CriticalSection.h"
 #include "utils/Observer.h"
 
+class CAppParamParser;
 class CButtonTranslator;
 class CCustomControllerTranslator;
 class CIRTranslator;
@@ -86,7 +87,7 @@ class CInputManager : public ISettingCallback,
                       public Observable
 {
 public:
-  CInputManager();
+  CInputManager(const CAppParamParser &params);
   CInputManager(const CInputManager&) = delete;
   CInputManager const& operator=(CInputManager const&) = delete;
   ~CInputManager() override;

xbmc/platform/android/activity/XBMCApp.cpp
@@ -417,7 +417,7 @@ void CXBMCApp::run()
   android_printf(" => running XBMC_Run...");
   try
   {
-    status = XBMC_Run(true, appParamParser.m_playlist);
+    status = XBMC_Run(true, appParamParser);
     android_printf(" => XBMC_Run finished with %d", status);
   }
   catch(...)

xbmc/platform/darwin/ios/IOSEAGLView.mm
@@ -24,7 +24,6 @@
 
 #include "system.h"
 #include "settings/AdvancedSettings.h"
-#include "FileItem.h"
 #include "Application.h"
 #include "messaging/ApplicationMessenger.h"
 #include "windowing/WindowingFactory.h"
@@ -32,6 +31,7 @@
 #include "utils/TimeUtils.h"
 #include "Util.h"
 #include "platform/XbmcContext.h"
+#include "AppParamParser.h"
 
 #import <QuartzCore/QuartzCore.h>
 
@@ -395,7 +395,7 @@ using namespace KODI::MESSAGING;
   setlocale(LC_NUMERIC, "C");
 
   g_application.Preflight();
-  if (!g_application.Create())
+  if (!g_application.Create(CAppParamParser()))
   {
     readyToRun = false;
     ELOG(@"%sUnable to create application", __PRETTY_FUNCTION__);
@@ -423,8 +423,7 @@ using namespace KODI::MESSAGING;
     try
     {
       CCocoaAutoPool innerpool;
-      CFileItemList playlist;
-      g_application.Run(playlist);
+      g_application.Run(CAppParamParser());
     }
     catch(...)
     {

xbmc/platform/posix/main.cpp
@@ -112,5 +112,5 @@ int main(int argc, char* argv[])
   CAppParamParser appParamParser;
   appParamParser.Parse(argv, argc);
   
-  return XBMC_Run(true, appParamParser.m_playlist);
+  return XBMC_Run(true, appParamParser);
 }

xbmc/platform/win32/WinMain.cpp
@@ -132,9 +132,8 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR commandLine, INT)
     CAppParamParser appParamParser;
     appParamParser.Parse(argv, argc);
     // Create and run the app
-    status = XBMC_Run(true, appParamParser.m_playlist);
+    status = XBMC_Run(true, appParamParser);
   }
-  
 
   for (int i = 0; i < argc; ++i)
     delete[] argv[i];

xbmc/platform/xbmc.cpp
@@ -37,7 +37,7 @@
 #include "platform/MessagePrinter.h"
 #include "utils/log.h"
 
-extern "C" int XBMC_Run(bool renderGUI, CFileItemList &playlist)
+extern "C" int XBMC_Run(bool renderGUI, const CAppParamParser &params)
 {
   int status = -1;
 
@@ -46,7 +46,7 @@ extern "C" int XBMC_Run(bool renderGUI, CFileItemList &playlist)
     g_advancedSettings.Initialize();
   }
 
-  if (!g_application.Create())
+  if (!g_application.Create(params))
   {
     CMessagePrinter::DisplayError("ERROR: Unable to create application. Exiting");
     return status;
@@ -85,7 +85,7 @@ extern "C" int XBMC_Run(bool renderGUI, CFileItemList &playlist)
 
   try
   {
-    status = g_application.Run(playlist);
+    status = g_application.Run(params);
   }
 #ifdef TARGET_WINDOWS
   catch (const XbmcCommons::UncheckedException &e)

xbmc/platform/xbmc.h
@@ -20,7 +20,7 @@
 
 #pragma once
 
-class CFileItemList;
+class CAppParamParser;
 
-extern "C" int XBMC_Run(bool renderGUI, CFileItemList &playlist);
+extern "C" int XBMC_Run(bool renderGUI, const CAppParamParser &params);
 

xbmc/playlists/PlayList.cpp
@@ -123,19 +123,19 @@ void CPlayList::Add(const CFileItemPtr &item)
   Add(item, -1, -1);
 }
 
-void CPlayList::Add(CPlayList& playlist)
+void CPlayList::Add(const CPlayList& playlist)
 {
   for (int i = 0; i < (int)playlist.size(); i++)
     Add(playlist[i], -1, -1);
 }
 
-void CPlayList::Add(CFileItemList& items)
+void CPlayList::Add(const CFileItemList& items)
 {
   for (int i = 0; i < (int)items.Size(); i++)
     Add(items[i]);
 }
 
-void CPlayList::Insert(CPlayList& playlist, int iPosition /* = -1 */)
+void CPlayList::Insert(const CPlayList& playlist, int iPosition /* = -1 */)
 {
   // out of bounds so just add to the end
   int iSize = size();
@@ -151,7 +151,7 @@ void CPlayList::Insert(CPlayList& playlist, int iPosition /* = -1 */)
   }
 }
 
-void CPlayList::Insert(CFileItemList& items, int iPosition /* = -1 */)
+void CPlayList::Insert(const CFileItemList& items, int iPosition /* = -1 */)
 {
   // out of bounds so just add to the end
   int iSize = size();

xbmc/playlists/PlayList.h
@@ -36,13 +36,13 @@ public:
   virtual bool LoadData(const std::string& strData);
   virtual void Save(const std::string& strFileName) const {};
 
-  void Add(CPlayList& playlist);
+  void Add(const CPlayList& playlist);
   void Add(const CFileItemPtr &pItem);
-  void Add(CFileItemList& items);
+  void Add(const CFileItemList& items);
 
   // for Party Mode
-  void Insert(CPlayList& playlist, int iPosition = -1);
-  void Insert(CFileItemList& items, int iPosition = -1);
+  void Insert(const CPlayList& playlist, int iPosition = -1);
+  void Insert(const CFileItemList& items, int iPosition = -1);
   void Insert(const CFileItemPtr& item, int iPosition = -1);
 
   int FindOrder(int iOrder) const;

xbmc/test/TestBasicEnvironment.cpp
@@ -37,6 +37,7 @@
 #include "addons/BinaryAddonCache.h"
 #include "interfaces/python/XBPython.h"
 #include "pvr/PVRManager.h"
+#include "AppParamParser.h"
 
 #if defined(TARGET_WINDOWS) || defined(TARGET_WIN10)
 #include "platform/win32/WIN32Util.h"
@@ -122,7 +123,7 @@ void TestBasicEnvironment::SetUp()
   g_application.m_ServiceManager->CreateAudioEngine();
   CServiceBroker::GetSettings().Initialize();
 
-  if (!g_application.m_ServiceManager->InitStageTwo()
+  if (!g_application.m_ServiceManager->InitStageTwo(CAppParamParser()))
     exit(1);
 
   g_application.m_ServiceManager->StartAudioEngine();