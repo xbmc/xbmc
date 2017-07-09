oz23/kodi

VideoPlayer: create a state for gui/video render

xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -5205,16 +5205,6 @@ bool CVideoPlayer::IsRenderingVideo()
   return m_renderManager.IsConfigured();
 }
 
-bool CVideoPlayer::IsRenderingGuiLayer()
-{
-  return m_renderManager.IsGuiLayer();
-}
-
-bool CVideoPlayer::IsRenderingVideoLayer()
-{
-  return m_renderManager.IsVideoLayer();
-}
-
 bool CVideoPlayer::Supports(EINTERLACEMETHOD method)
 {
   if (!m_processInfo)
@@ -5284,6 +5274,16 @@ void CVideoPlayer::UpdateRenderInfo(CRenderInfo &info)
 void CVideoPlayer::UpdateRenderBuffers(int queued, int discard, int free)
 {
   m_processInfo->UpdateRenderBuffers(queued, discard, free);
+}
+
+void CVideoPlayer::UpdareGuiRender(bool gui)
+{
+  m_processInfo->SetGuiRender(gui);
+}
+
+void CVideoPlayer::UpdareVideoRender(bool video)
+{
+  m_processInfo->SetVideoRender(video);
 }
 
 // IDispResource interface

xbmc/cores/VideoPlayer/VideoPlayer.h
@@ -370,15 +370,13 @@ public:
 
   virtual bool SwitchChannel(const PVR::CPVRChannelPtr &channel);
 
-  virtual void FrameMove();
-  virtual void Render(bool clear, uint32_t alpha = 255, bool gui = true);
-  virtual void FlushRenderer();
-  virtual void SetRenderViewMode(int mode);
-  float GetRenderAspectRatio();
-  virtual void TriggerUpdateResolution();
-  virtual bool IsRenderingVideo();
-  virtual bool IsRenderingGuiLayer();
-  virtual bool IsRenderingVideoLayer();
+  virtual void FrameMove() override;
+  virtual void Render(bool clear, uint32_t alpha = 255, bool gui = true) override;
+  virtual void FlushRenderer() override;
+  virtual void SetRenderViewMode(int mode) override;
+  virtual float GetRenderAspectRatio() override;
+  virtual void TriggerUpdateResolution() override;
+  virtual bool IsRenderingVideo() override;
   virtual bool Supports(EINTERLACEMETHOD method) override;
   virtual EINTERLACEMETHOD GetDeinterlacingMethodDefault() override;
   virtual bool Supports(ESCALINGMETHOD method) override;
@@ -402,14 +400,16 @@ public:
 protected:
   friend class CSelectionStreams;
 
-  virtual void OnStartup();
-  virtual void OnExit();
-  virtual void Process();
+  virtual void OnStartup() override;
+  virtual void OnExit() override;
+  virtual void Process() override;
   virtual void VideoParamsChange() override;
   virtual void GetDebugInfo(std::string &audio, std::string &video, std::string &general) override;
   virtual void UpdateClockSync(bool enabled) override;
   virtual void UpdateRenderInfo(CRenderInfo &info) override;
   virtual void UpdateRenderBuffers(int queued, int discard, int free) override;
+  virtual void UpdareGuiRender(bool gui) override;
+  virtual void UpdareVideoRender(bool video) override;
 
   void CreatePlayers();
   void DestroyPlayers();

xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
@@ -56,6 +56,8 @@ protected:
   virtual void UpdateClockSync(bool enabled) = 0;
   virtual void UpdateRenderInfo(CRenderInfo &info) = 0;
   virtual void UpdateRenderBuffers(int queued, int discard, int free) = 0;
+  virtual void UpdareGuiRender(bool gui) = 0;
+  virtual void UpdareVideoRender(bool video) = 0;
 };
 
 class CRenderManager
@@ -70,7 +72,6 @@ public:
   void FrameMove();
   void FrameWait(int ms);
   void Render(bool clear, DWORD flags = 0, DWORD alpha = 255, bool gui = true);
-  bool IsGuiLayer();
   bool IsVideoLayer();
   RESOLUTION GetResolution();
   void UpdateResolution();
@@ -160,6 +161,7 @@ protected:
 
   void PrepareNextRender();
   bool IsPresenting();
+  bool IsGuiLayer();
 
   bool Configure();
   void CreateRenderer();

xbmc/xbmc

xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -5221,16 +5221,6 @@ bool CVideoPlayer::IsRenderingVideo()
   return m_renderManager.IsConfigured();
 }
 
-bool CVideoPlayer::IsRenderingGuiLayer()
-{
-  return m_renderManager.IsGuiLayer();
-}
-
-bool CVideoPlayer::IsRenderingVideoLayer()
-{
-  return m_renderManager.IsVideoLayer();
-}
-
 bool CVideoPlayer::Supports(EINTERLACEMETHOD method)
 {
   if (!m_processInfo)
@@ -5300,6 +5290,16 @@ void CVideoPlayer::UpdateRenderInfo(CRenderInfo &info)
 void CVideoPlayer::UpdateRenderBuffers(int queued, int discard, int free)
 {
   m_processInfo->UpdateRenderBuffers(queued, discard, free);
+}
+
+void CVideoPlayer::UpdateGuiRender(bool gui)
+{
+  m_processInfo->SetGuiRender(gui);
+}
+
+void CVideoPlayer::UpdateVideoRender(bool video)
+{
+  m_processInfo->SetVideoRender(video);
 }
 
 // IDispResource interface

xbmc/cores/VideoPlayer/VideoPlayer.h
@@ -371,15 +371,13 @@ public:
 
   virtual bool SwitchChannel(const PVR::CPVRChannelPtr &channel);
 
-  virtual void FrameMove();
-  virtual void Render(bool clear, uint32_t alpha = 255, bool gui = true);
-  virtual void FlushRenderer();
-  virtual void SetRenderViewMode(int mode);
-  float GetRenderAspectRatio();
-  virtual void TriggerUpdateResolution();
-  virtual bool IsRenderingVideo();
-  virtual bool IsRenderingGuiLayer();
-  virtual bool IsRenderingVideoLayer();
+  virtual void FrameMove() override;
+  virtual void Render(bool clear, uint32_t alpha = 255, bool gui = true) override;
+  virtual void FlushRenderer() override;
+  virtual void SetRenderViewMode(int mode) override;
+  virtual float GetRenderAspectRatio() override;
+  virtual void TriggerUpdateResolution() override;
+  virtual bool IsRenderingVideo() override;
   virtual bool Supports(EINTERLACEMETHOD method) override;
   virtual EINTERLACEMETHOD GetDeinterlacingMethodDefault() override;
   virtual bool Supports(ESCALINGMETHOD method) override;
@@ -403,14 +401,16 @@ public:
 protected:
   friend class CSelectionStreams;
 
-  virtual void OnStartup();
-  virtual void OnExit();
-  virtual void Process();
+  virtual void OnStartup() override;
+  virtual void OnExit() override;
+  virtual void Process() override;
   virtual void VideoParamsChange() override;
   virtual void GetDebugInfo(std::string &audio, std::string &video, std::string &general) override;
   virtual void UpdateClockSync(bool enabled) override;
   virtual void UpdateRenderInfo(CRenderInfo &info) override;
   virtual void UpdateRenderBuffers(int queued, int discard, int free) override;
+  virtual void UpdateGuiRender(bool gui) override;
+  virtual void UpdateVideoRender(bool video) override;
 
   void CreatePlayers();
   void DestroyPlayers();

xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.h
@@ -56,6 +56,8 @@ protected:
   virtual void UpdateClockSync(bool enabled) = 0;
   virtual void UpdateRenderInfo(CRenderInfo &info) = 0;
   virtual void UpdateRenderBuffers(int queued, int discard, int free) = 0;
+  virtual void UpdateGuiRender(bool gui) = 0;
+  virtual void UpdateVideoRender(bool video) = 0;
 };
 
 class CRenderManager
@@ -70,7 +72,6 @@ public:
   void FrameMove();
   void FrameWait(int ms);
   void Render(bool clear, DWORD flags = 0, DWORD alpha = 255, bool gui = true);
-  bool IsGuiLayer();
   bool IsVideoLayer();
   RESOLUTION GetResolution();
   void UpdateResolution();
@@ -160,6 +161,7 @@ protected:
 
   void PrepareNextRender();
   bool IsPresenting();
+  bool IsGuiLayer();
 
   bool Configure();
   void CreateRenderer();