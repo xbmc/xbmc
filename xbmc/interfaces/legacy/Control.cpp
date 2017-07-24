8b0533a9e2e0a86cc1c0d79e62e6d2a5805d20bd
Roman Miroshnychenko <roman1972@gmail.com>
2017-07-24 17:05:34 +0300

    Add xbmcgui.Control.isVisible method
    This method allows to check Control's visible state from Python.

--- xbmc/interfaces/legacy/Control.cpp
+++ xbmc/interfaces/legacy/Control.cpp
@@ -704,6 +704,16 @@ namespace XBMCAddon
         pGUIControl->SetVisible(visible);
     }
 
+    bool Control::isVisible()
+    {
+      DelayedCallGuard dcguard(languageHook);
+      XBMCAddonUtils::GuiLock(languageHook, false);
+      if (pGUIControl)
+        return pGUIControl->IsVisible();
+      else
+        return false;
+    }
+
     void Control::setVisibleCondition(const char* visible, bool allowHiddenFocus)
     {
       DelayedCallGuard dcguard(languageHook);
