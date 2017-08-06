2e433311c0245a38ed101a9e384511071f458c5a
Rechi <Rechi@users.noreply.github.com>
2017-08-06 23:23:46 +0200

    [cleanup][android] silence overloaded virtual warning

--- xbmc/filesystem/AndroidAppDirectory.h
+++ xbmc/filesystem/AndroidAppDirectory.h
@@ -34,7 +34,7 @@ public:
   virtual bool GetDirectory(const CURL& url, CFileItemList &items);
   virtual bool Exists(const CURL& url) { return true; };
   virtual bool AllowAll() const { return true; };
-  virtual DIR_CACHE_TYPE GetCacheType(const std::string& strPath) const { return DIR_CACHE_NEVER; }
+  virtual DIR_CACHE_TYPE GetCacheType(const CURL& url) const { return DIR_CACHE_NEVER; }
 };
 }
 #endif
