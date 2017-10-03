cb830e96808be25a01fc68e344cfb1703eb9ac98
Arne Morten Kvarving <cptspiff@gmail.com>
2017-10-03 19:57:16 +0200

    fixed: append a -temp to the temp file folder
    add-on ids sometimes end with the same name as a file folder extension.
    this lead to infinite loops. this is a simple workaround

--- xbmc/addons/interfaces/General.cpp
+++ xbmc/addons/interfaces/General.cpp
@@ -312,9 +312,9 @@ char* Interface_General::get_temp_path(void* kodiBase)
     return nullptr;
   }
 
-  const std::string tempPath = URIUtils::AddFileToFolder(CServiceBroker::GetBinaryAddonManager().GetTempAddonBasePath(), addon->ID());
-  if (!XFILE::CDirectory::Exists(tempPath))
-    XFILE::CDirectory::Create(tempPath);
+  std::string tempPath = URIUtils::AddFileToFolder(CServiceBroker::GetBinaryAddonManager().GetTempAddonBasePath(), addon->ID());
+  tempPath += "-temp";
+  XFILE::CDirectory::Create(tempPath);
 
   return strdup(CSpecialProtocol::TranslatePath(tempPath).c_str());
 }
