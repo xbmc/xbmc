98f2e119f2ece9c31ac138f7ec07a451166a9df1
Garrett Brown <themagnificentmrb@gmail.com>
2017-10-06 11:04:38 -0700

    Check an 8-year-old FIXME off the list
    Fixed in 9d0df95c.

--- xbmc/rendering/gles/RenderSystemGLES.cpp
+++ xbmc/rendering/gles/RenderSystemGLES.cpp
@@ -459,7 +459,6 @@ void CRenderSystemGLES::GetViewPort(CRect& viewPort)
   viewPort.y2 = viewPort.y1 + m_viewPort[3];
 }
 
-// FIXME make me const so that I can accept temporary objects
 void CRenderSystemGLES::SetViewPort(const CRect& viewPort)
 {
   if (!m_bRenderCreated)
