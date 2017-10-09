10b50f5a755f6bc9f0130f41c354a9fefeb07e04
Lukas Rusak <lorusak@gmail.com>
2017-10-09 14:26:20 -0700

    [cmake] adjust json-rpc build method

--- xbmc/interfaces/json-rpc/schema/GenerateAddonXml.cmake
+++
@@ -1,5 +0,0 @@
-file(STRINGS ${CMAKE_SOURCE_DIR}/xbmc/interfaces/json-rpc/schema/version.txt jsonrpc_version)
-
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml)
-configure_file(${CMAKE_SOURCE_DIR}/addons/xbmc.json/addon.xml.in
-               ${CORE_BINARY_DIR}/addons/xbmc.json/addon.xml @ONLY)
