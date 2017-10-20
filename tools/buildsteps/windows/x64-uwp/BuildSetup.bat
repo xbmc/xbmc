9c1d2f26940aa6b397293bc68bfe463c98dde175
Anton Fedchin <anightik@gmail.com>
2017-10-20 12:54:36 +0300

    [win10] buildsteps: do not make nsis installer for uwp + cosmetics

--- tools/buildsteps/windows/x64-uwp/BuildSetup.bat
+++ tools/buildsteps/windows/x64-uwp/BuildSetup.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 SET cmakeGenerator=Visual Studio 14 Win64
-SET TARGET_ARCHITECTURE=x86
-SET cmakeProps=-DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0.14393.0
+SET TARGET_ARCHITECTURE=x64
 
 rem set Visual C++ build environment for binary addons
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" amd64 store 10.0.14393.0
 
+SET cmakeProps=-DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=%UCRTVersion%
 PUSHD %~dp0\..
 CALL BuildSetup.bat %*
 POPD
