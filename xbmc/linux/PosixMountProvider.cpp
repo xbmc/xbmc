c3c975f4fe10222d643bc9b02476caf4a5ddf3bb
Memphiz <memphis@machzwo.de>
2017-10-28 09:26:21 +0200

    [ios] - there is no "system" call allowed when compiling with ios11 SDK.

--- xbmc/linux/PosixMountProvider.cpp
+++ xbmc/linux/PosixMountProvider.cpp
@@ -131,6 +131,8 @@ std::vector<std::string> CPosixMountProvider::GetDiskUsage()
 
 bool CPosixMountProvider::Eject(const std::string& mountpath)
 {
+
+#if !defined(TARGET_DARWIN_IOS)
   // just go ahead and try to umount the disk
   // if it does umount, life is good, if not, no loss.
   std::string cmd = "umount \"" + mountpath + "\"";
@@ -138,6 +140,7 @@ bool CPosixMountProvider::Eject(const std::string& mountpath)
 
   if (status == 0)
     return true;
+#endif
 
   return false;
 }
