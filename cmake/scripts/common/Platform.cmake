a5d2febf632093c4d3b19a7d600a0f5aea3a430c
wsnipex <wsnipex@a1.net>
2017-10-29 20:53:35 +0100

    [cmake] linux: name kodi binary according to platform name
    and automatically select kodi-wayland binary on wayland compositors

--- cmake/scripts/common/Platform.cmake
+++ cmake/scripts/common/Platform.cmake
@@ -6,9 +6,12 @@ if(CORE_SYSTEM_NAME STREQUAL linux)
   # Set default CORE_PLATFORM_NAME to X11
   # This is overridden by user setting -DCORE_PLATFORM_NAME=<platform>
   set(_DEFAULT_PLATFORM X11)
+  option(ENABLE_APP_AUTONAME    "Enable renaming the binary according to windowing?" ON)
 else()
   string(TOLOWER ${CORE_SYSTEM_NAME} _DEFAULT_PLATFORM)
 endif()
+
+set(APP_BINARY_SUFFIX ".bin")
 
 #
 # Note: please do not use CORE_PLATFORM_NAME in any checks,
@@ -23,10 +26,12 @@ string(TOLOWER ${CORE_PLATFORM_NAME} CORE_PLATFORM_NAME_LC)
 list(APPEND final_message "Platform: ${CORE_PLATFORM_NAME}")
 if(EXISTS ${CMAKE_SOURCE_DIR}/cmake/platform/${CORE_SYSTEM_NAME}/${CORE_PLATFORM_NAME_LC}.cmake)
   include(${CMAKE_SOURCE_DIR}/cmake/platform/${CORE_SYSTEM_NAME}/${CORE_PLATFORM_NAME_LC}.cmake)
+  if(ENABLE_APP_AUTONAME)
+    set(APP_BINARY_SUFFIX "-${CORE_PLATFORM_NAME_LC}")
+  endif()
 else()
   file(GLOB _platformnames RELATIVE ${CMAKE_SOURCE_DIR}/cmake/platform/${CORE_SYSTEM_NAME}/
                                     ${CMAKE_SOURCE_DIR}/cmake/platform/${CORE_SYSTEM_NAME}/*.cmake)
   string(REPLACE ".cmake" " " _platformnames ${_platformnames})
   message(FATAL_ERROR "invalid CORE_PLATFORM_NAME: ${CORE_PLATFORM_NAME_LC}\nValid platforms: ${_platformnames}")
 endif()
-
