127b232a9835724809fd3dcd3e136ebc1ea891ad
fritsch <Peter.Fruehberger@gmail.com>
2017-11-25 17:42:38 +0100

    Versions: Bump PVR and inputstream to pickup DemuxPacket changes

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/versions.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/versions.h
@@ -103,8 +103,8 @@
 #define ADDON_INSTANCE_VERSION_IMAGEDECODER_XML_ID    "kodi.binary.instance.imagedecoder"
 #define ADDON_INSTANCE_VERSION_IMAGEDECODER_DEPENDS   "addon-instance/ImageDecoder.h"
 
-#define ADDON_INSTANCE_VERSION_INPUTSTREAM            "2.0.4"
-#define ADDON_INSTANCE_VERSION_INPUTSTREAM_MIN        "2.0.4"
+#define ADDON_INSTANCE_VERSION_INPUTSTREAM            "2.0.5"
+#define ADDON_INSTANCE_VERSION_INPUTSTREAM_MIN        "2.0.5"
 #define ADDON_INSTANCE_VERSION_INPUTSTREAM_XML_ID     "kodi.binary.instance.inputstream"
 #define ADDON_INSTANCE_VERSION_INPUTSTREAM_DEPENDS    "addon-instance/Inputstream.h"
 
@@ -114,8 +114,8 @@
 #define ADDON_INSTANCE_VERSION_PERIPHERAL_DEPENDS     "addon-instance/Peripheral.h" \
                                                       "addon-instance/PeripheralUtils.h"
 
-#define ADDON_INSTANCE_VERSION_PVR                    "5.7.1"
-#define ADDON_INSTANCE_VERSION_PVR_MIN                "5.7.0"
+#define ADDON_INSTANCE_VERSION_PVR                    "5.7.2"
+#define ADDON_INSTANCE_VERSION_PVR_MIN                "5.7.2"
 #define ADDON_INSTANCE_VERSION_PVR_XML_ID             "kodi.binary.instance.pvr"
 #define ADDON_INSTANCE_VERSION_PVR_DEPENDS            "xbmc_pvr_dll.h" \
                                                       "xbmc_pvr_types.h" \
