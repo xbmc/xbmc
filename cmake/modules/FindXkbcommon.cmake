a7bb932a9281d14c4461e1afbd29b96af53e2588
Philipp Kerling <pkerling@casix.org>
2018-01-04 13:32:46 +0100

    [cmake] Require pkgconfig for libxkbcommon
    Since we will require a minimum libxkbcommon version, just finding the
    headers is not enough and configuring will fail anyway if xkbcommon
    was not found through pkgconfig.

--- cmake/modules/FindXkbcommon.cmake
+++ cmake/modules/FindXkbcommon.cmake
@@ -9,27 +9,13 @@
 # XKBCOMMON_LIBRARIES    - the libxkbcommon libraries
 # XKBCOMMON_DEFINITIONS  - the libxkbcommon definitions
 
-
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules (PC_XKBCOMMON xkbcommon QUIET)
-endif()
-
-find_path(XKBCOMMON_INCLUDE_DIR NAMES xkbcommon/xkbcommon.h
-                                PATHS ${PC_XKBCOMMON_INCLUDE_DIRS})
-
-find_library(XKBCOMMON_LIBRARY NAMES xkbcommon
-                               PATHS ${PC_XKBCOMMON_LIBRARIES} ${PC_XKBCOMMON_LIBRARY_DIRS})
+pkg_check_modules (XKBCOMMON xkbcommon)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (Xkbcommon
   REQUIRED_VARS
-  XKBCOMMON_INCLUDE_DIR
-  XKBCOMMON_LIBRARY)
+  XKBCOMMON_FOUND)
 
-if (XKBCOMMON_FOUND)
-  set(XKBCOMMON_LIBRARIES ${XKBCOMMON_LIBRARY})
-  set(XKBCOMMON_INCLUDE_DIRS ${PC_XKBCOMMON_INCLUDE_DIRS})
-  set(XKBCOMMON_DEFINITIONS -DHAVE_XKBCOMMON=1)
-endif()
-
-mark_as_advanced (XKBCOMMON_LIBRARY XKBCOMMON_INCLUDE_DIR)
+set(XKBCOMMON_DEFINITIONS -DHAVE_XKBCOMMON=1)
+set(XKBCOMMON_LIBRARIES ${XKBCOMMON_LDFLAGS})
+set(XKBCOMMON_INCLUDE_DIRS ${XKBCOMMON_INCLUDEDIR})
