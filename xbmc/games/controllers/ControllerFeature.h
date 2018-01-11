448601ad4c28d6e87c15f2a4b9248e114f1eb8ea
Garrett Brown <themagnificentmrb@gmail.com>
2018-01-11 13:18:28 -0800

    Controllers: Modernize CControllerFeature

--- xbmc/games/controllers/ControllerFeature.h
+++ xbmc/games/controllers/ControllerFeature.h
@@ -34,7 +34,7 @@ namespace GAME
 class CControllerFeature
 {
 public:
-  CControllerFeature(void) { Reset(); }
+  CControllerFeature() = default;
   CControllerFeature(const CControllerFeature& other) { *this = other; }
 
   void Reset(void);
@@ -43,10 +43,14 @@ public:
 
   JOYSTICK::FEATURE_TYPE Type(void) const { return m_type; }
   JOYSTICK::FEATURE_CATEGORY Category(void) const { return m_category; }
-  std::string            CategoryLabel(void) const;
-  const std::string&     Name(void) const       { return m_strName; }
-  std::string            Label(void) const;
-  int                    LabelID(void) const    { return m_labelId; }
+  const std::string &Name(void) const { return m_strName; }
+
+  // GUI properties
+  std::string Label(void) const;
+  int LabelID(void) const { return m_labelId; }
+  std::string CategoryLabel(void) const;
+
+  // Input properties
   JOYSTICK::INPUT_TYPE InputType(void) const { return m_inputType; }
 
   bool Deserialize(const TiXmlElement* pElement,
@@ -55,13 +59,13 @@ public:
                    int categoryLabelId);
 
 private:
-  const CController *m_controller; // To get the controller ID for translating labels
-  JOYSTICK::FEATURE_TYPE m_type;
-  JOYSTICK::FEATURE_CATEGORY m_category;
-  int m_categoryLabelId;
-  std::string            m_strName;
-  int                    m_labelId;
-  JOYSTICK::INPUT_TYPE m_inputType;
+  const CController *m_controller = nullptr; // Used for translating addon-specific labels
+  JOYSTICK::FEATURE_TYPE m_type = JOYSTICK::FEATURE_TYPE::UNKNOWN;
+  JOYSTICK::FEATURE_CATEGORY m_category = JOYSTICK::FEATURE_CATEGORY::UNKNOWN;
+  int m_categoryLabelId = -1;
+  std::string m_strName;
+  int m_labelId = -1;
+  JOYSTICK::INPUT_TYPE m_inputType = JOYSTICK::INPUT_TYPE::UNKNOWN;
 };
 
 }
