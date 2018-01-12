8f9bf7b1dcf65f9e08b78e3b5b79092e6b99910c
popcornmix <popcornmix@gmail.com>
2018-01-12 14:56:05 +0000

    Application: Set volume after opening file
    Initial volume was broken in omxplayer after #13301

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -3203,9 +3203,9 @@ bool CApplication::PlayFile(CFileItem item, const std::string& player, bool bRes
       CLog::LogF(LOGDEBUG,"Ignored %d playback thread messages", dMsgCount);
   }
 
+  m_appPlayer.OpenFile(item, options, player, *this);
   m_appPlayer.SetVolume(m_volumeLevel);
   m_appPlayer.SetMute(m_muted);
-  m_appPlayer.OpenFile(item, options, player, *this);
 
 #if !defined(TARGET_POSIX)
   g_audioManager.Enable(false);
