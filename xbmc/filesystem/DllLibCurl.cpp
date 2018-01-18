b6f32bb37a568a284a25dd4b63068bf900402489
Pär Björklund <per.bjorklund@gmail.com>
2018-01-18 18:59:54 +0100

    Fix inconsistent formatting

--- xbmc/filesystem/DllLibCurl.cpp
+++ xbmc/filesystem/DllLibCurl.cpp
@@ -20,16 +20,16 @@
 
 #include "DllLibCurl.h"
 
-#include "threads/SystemClock.h"
 #include "system.h"
 #include "threads/SingleLock.h"
+#include "threads/SystemClock.h"
 #include "utils/log.h"
 
 #include <assert.h>
 
 #ifdef HAVE_OPENSSL
-#include "threads/Thread.h"
 #include "openssl/crypto.h"
+#include "threads/Thread.h"
 
 static CCriticalSection** m_sslLockArray = NULL;
 
@@ -38,21 +38,18 @@ extern "C"
 {
 #endif
 
-void ssl_lock_callback(int mode, int type, char *file, int line)
-{
-  if (!m_sslLockArray)
-    return;
+  void ssl_lock_callback(int mode, int type, char* file, int line)
+  {
+    if (!m_sslLockArray)
+      return;
 
-  if (mode & CRYPTO_LOCK)
-    m_sslLockArray[type]->lock();
-  else
-    m_sslLockArray[type]->unlock();
-}
+    if (mode & CRYPTO_LOCK)
+      m_sslLockArray[type]->lock();
+    else
+      m_sslLockArray[type]->unlock();
+  }
 
-unsigned long ssl_thread_id(void)
-{
-  return (unsigned long)CThread::GetCurrentThreadId();
-}
+  unsigned long ssl_thread_id() { return static_cast<unsigned long>(CThread::GetCurrentThreadId()); }
 
 #ifdef __cplusplus
 }
@@ -62,10 +59,6 @@ unsigned long ssl_thread_id(void)
 
 namespace XCURL
 {
-#define CURL CURL_HANDLE
-#include <curl/curl.h>
-#undef CURL
-
 CURLcode DllLibCurl::global_init(long flags)
 {
   return curl_global_init(flags);
@@ -76,100 +69,103 @@ void DllLibCurl::global_cleanup()
   curl_global_cleanup();
 }
 
-CURL_HANDLE * DllLibCurl::easy_init()
+CURL_HANDLE* DllLibCurl::easy_init()
 {
   return curl_easy_init();
 }
 
-CURLcode DllLibCurl::easy_perform(CURL_HANDLE * handle)
+CURLcode DllLibCurl::easy_perform(CURL_HANDLE* handle)
 {
   return curl_easy_perform(handle);
 }
 
-CURLcode DllLibCurl::easy_pause(CURL_HANDLE * handle, int bitmask)
+CURLcode DllLibCurl::easy_pause(CURL_HANDLE* handle, int bitmask)
 {
   return curl_easy_pause(handle, bitmask);
 }
 
-void DllLibCurl::easy_reset(CURL_HANDLE * handle)
+void DllLibCurl::easy_reset(CURL_HANDLE* handle)
 {
   curl_easy_reset(handle);
 }
 
-void DllLibCurl::easy_cleanup(CURL_HANDLE * handle)
+void DllLibCurl::easy_cleanup(CURL_HANDLE* handle)
 {
   curl_easy_cleanup(handle);
 }
 
-CURL_HANDLE * DllLibCurl::easy_duphandle(CURL_HANDLE * handle)
+CURL_HANDLE* DllLibCurl::easy_duphandle(CURL_HANDLE* handle)
 {
   return curl_easy_duphandle(handle);
 }
 
-CURLM * DllLibCurl::multi_init()
+CURLM* DllLibCurl::multi_init()
 {
   return curl_multi_init();
 }
 
-CURLMcode DllLibCurl::multi_add_handle(CURLM * multi_handle, CURL_HANDLE * easy_handle)
+CURLMcode DllLibCurl::multi_add_handle(CURLM* multi_handle, CURL_HANDLE* easy_handle)
 {
   return curl_multi_add_handle(multi_handle, easy_handle);
 }
 
-CURLMcode DllLibCurl::multi_perform(CURLM * multi_handle, int * running_handles)
+CURLMcode DllLibCurl::multi_perform(CURLM* multi_handle, int* running_handles)
 {
   return curl_multi_perform(multi_handle, running_handles);
 }
 
-CURLMcode DllLibCurl::multi_remove_handle(CURLM * multi_handle, CURL_HANDLE * easy_handle)
+CURLMcode DllLibCurl::multi_remove_handle(CURLM* multi_handle, CURL_HANDLE* easy_handle)
 {
   return curl_multi_remove_handle(multi_handle, easy_handle);
 }
 
-CURLMcode DllLibCurl::multi_fdset(CURLM * multi_handle, fd_set * read_fd_set, fd_set * write_fd_set, fd_set * exc_fd_set, int * max_fd)
+CURLMcode DllLibCurl::multi_fdset(
+    CURLM* multi_handle, fd_set* read_fd_set, fd_set* write_fd_set, fd_set* exc_fd_set, int* max_fd)
 {
   return curl_multi_fdset(multi_handle, read_fd_set, write_fd_set, exc_fd_set, max_fd);
 }
 
-CURLMcode DllLibCurl::multi_timeout(CURLM * multi_handle, long * timeout)
+CURLMcode DllLibCurl::multi_timeout(CURLM* multi_handle, long* timeout)
 {
   return curl_multi_timeout(multi_handle, timeout);
 }
 
-CURLMsg * DllLibCurl::multi_info_read(CURLM * multi_handle, int * msgs_in_queue)
+CURLMsg* DllLibCurl::multi_info_read(CURLM* multi_handle, int* msgs_in_queue)
 {
   return curl_multi_info_read(multi_handle, msgs_in_queue);
 }
 
-CURLMcode DllLibCurl::multi_cleanup(CURLM * handle)
+CURLMcode DllLibCurl::multi_cleanup(CURLM* handle)
 {
   return curl_multi_cleanup(handle);
 }
 
-curl_slist * DllLibCurl::slist_append(curl_slist *list, const char *to_append)
+curl_slist* DllLibCurl::slist_append(curl_slist* list, const char* to_append)
 {
   return curl_slist_append(list, to_append);
 }
 
-void DllLibCurl::slist_free_all(curl_slist *list)
+void DllLibCurl::slist_free_all(curl_slist* list)
 {
   curl_slist_free_all(list);
 }
 
-const char * DllLibCurl::easy_strerror(CURLcode code)
+const char* DllLibCurl::easy_strerror(CURLcode code)
 {
   return curl_easy_strerror(code);
 }
+
 #if defined(HAS_CURL_STATIC)
-void DllLibCurl::crypto_set_id_callback(unsigned long(*cb)(void))
+void DllLibCurl::crypto_set_id_callback(unsigned long (*cb)())
 {
   curl_crypto_set_id_callback(cb);
 }
-void DllLibCurl::crypto_set_locking_callback(void(*cb)(int, int, const char *, int))
+void DllLibCurl::crypto_set_locking_callback(void (*cb)(int, int, const char*, int))
 {
   curl_crypto_set_locking_callback(cb);
 }
 #endif
+
 DllLibCurlGlobal::DllLibCurlGlobal()
 {
   /* we handle this ourself */
@@ -181,9 +177,9 @@ DllLibCurlGlobal::DllLibCurlGlobal()
 #if defined(HAS_CURL_STATIC)
   // Initialize ssl locking array
   m_sslLockArray = new CCriticalSection*[CRYPTO_num_locks()];
-  for (int i=0; i<CRYPTO_num_locks(); i++)
-    m_sslLockArray[i] = new CCriticalSection;  
- 
+  for (int i = 0; i < CRYPTO_num_locks(); i++)
+    m_sslLockArray[i] = new CCriticalSection;
+
   crypto_set_id_callback((unsigned long (*)())ssl_thread_id);
   crypto_set_locking_callback((void (*)(int, int, const char*, int))ssl_lock_callback);
 #endif
@@ -198,7 +194,7 @@ DllLibCurlGlobal::~DllLibCurlGlobal()
   // Cleanup ssl locking array
   curl_crypto_set_id_callback(NULL);
   curl_crypto_set_locking_callback(NULL);
-  for (int i=0; i<CRYPTO_num_locks(); i++)
+  for (int i = 0; i < CRYPTO_num_locks(); i++)
     delete m_sslLockArray[i];
 
   delete[] m_sslLockArray;
@@ -212,17 +208,19 @@ void DllLibCurlGlobal::CheckIdle()
   const unsigned int idletime = 30000;
 
   VEC_CURLSESSIONS::iterator it = m_sessions.begin();
-  while(it != m_sessions.end())
+  while (it != m_sessions.end())
   {
-    if( !it->m_busy && (XbmcThreads::SystemClockMillis() - it->m_idletimestamp) > idletime )
+    if (!it->m_busy && (XbmcThreads::SystemClockMillis() - it->m_idletimestamp) > idletime)
     {
-      CLog::Log(LOGINFO, "%s - Closing session to %s://%s (easy=%p, multi=%p)\n", __FUNCTION__, it->m_protocol.c_str(), it->m_hostname.c_str(), (void*)it->m_easy, (void*)it->m_multi);
+      CLog::Log(LOGINFO, "%s - Closing session to %s://%s (easy=%p, multi=%p)\n", __FUNCTION__,
+                it->m_protocol.c_str(), it->m_hostname.c_str(), static_cast<void*>(it->m_easy),
+                static_cast<void*>(it->m_multi));
 
-      if(it->m_multi && it->m_easy)
+      if (it->m_multi && it->m_easy)
         multi_remove_handle(it->m_multi, it->m_easy);
-      if(it->m_easy)
+      if (it->m_easy)
         easy_cleanup(it->m_easy);
-      if(it->m_multi)
+      if (it->m_multi)
         multi_cleanup(it->m_multi);
 
       it = m_sessions.erase(it);
@@ -232,33 +230,36 @@ void DllLibCurlGlobal::CheckIdle()
   }
 }
 
-void DllLibCurlGlobal::easy_acquire(const char *protocol, const char *hostname, CURL_HANDLE** easy_handle, CURLM** multi_handle)
+void DllLibCurlGlobal::easy_acquire(const char* protocol,
+                                    const char* hostname,
+                                    CURL_HANDLE** easy_handle,
+                                    CURLM** multi_handle)
 {
   assert(easy_handle != NULL);
 
   CSingleLock lock(m_critSection);
 
   VEC_CURLSESSIONS::iterator it;
-  for(it = m_sessions.begin(); it != m_sessions.end(); ++it)
+  for (it = m_sessions.begin(); it != m_sessions.end(); ++it)
   {
-    if( !it->m_busy )
+    if (!it->m_busy)
     {
       /* allow reuse of requester is trying to connect to same host */
       /* curl will take care of any differences in username/password */
-      if( it->m_protocol.compare(protocol) == 0 && it->m_hostname.compare(hostname) == 0)
+      if (it->m_protocol.compare(protocol) == 0 && it->m_hostname.compare(hostname) == 0)
       {
         it->m_busy = true;
-        if(easy_handle)
+        if (easy_handle)
         {
-          if(!it->m_easy)
+          if (!it->m_easy)
             it->m_easy = easy_init();
 
           *easy_handle = it->m_easy;
         }
 
-        if(multi_handle)
+        if (multi_handle)
         {
-          if(!it->m_multi)
+          if (!it->m_multi)
             it->m_multi = multi_init();
 
           *multi_handle = it->m_multi;
@@ -274,13 +275,13 @@ void DllLibCurlGlobal::easy_acquire(const char *protocol, const char *hostname, 
   session.m_protocol = protocol;
   session.m_hostname = hostname;
 
-  if(easy_handle)
+  if (easy_handle)
   {
     session.m_easy = easy_init();
     *easy_handle = session.m_easy;
   }
 
-  if(multi_handle)
+  if (multi_handle)
   {
     session.m_multi = multi_init();
     *multi_handle = session.m_multi;
@@ -288,11 +289,9 @@ void DllLibCurlGlobal::easy_acquire(const char *protocol, const char *hostname, 
 
   m_sessions.push_back(session);
 
-
   CLog::Log(LOGINFO, "%s - Created session to %s://%s\n", __FUNCTION__, protocol, hostname);
 
   return;
-
 }
 
 void DllLibCurlGlobal::easy_release(CURL_HANDLE** easy_handle, CURLM** multi_handle)
@@ -300,24 +299,24 @@ void DllLibCurlGlobal::easy_release(CURL_HANDLE** easy_handle, CURLM** multi_han
   CSingleLock lock(m_critSection);
 
   CURL_HANDLE* easy = NULL;
-  CURLM*       multi = NULL;
+  CURLM* multi = NULL;
 
-  if(easy_handle)
+  if (easy_handle)
   {
     easy = *easy_handle;
     *easy_handle = NULL;
   }
 
-  if(multi_handle)
+  if (multi_handle)
   {
     multi = *multi_handle;
     *multi_handle = NULL;
   }
 
   VEC_CURLSESSIONS::iterator it;
-  for(it = m_sessions.begin(); it != m_sessions.end(); ++it)
+  for (it = m_sessions.begin(); it != m_sessions.end(); ++it)
   {
-    if( it->m_easy == easy && (multi == NULL || it->m_multi == multi) )
+    if (it->m_easy == easy && (multi == NULL || it->m_multi == multi))
     {
       /* reset session so next caller doesn't reuse options, only connections */
       /* will reset verbose too so it won't print that it closed connections on cleanup*/
@@ -334,9 +333,9 @@ CURL_HANDLE* DllLibCurlGlobal::easy_duphandle(CURL_HANDLE* easy_handle)
   CSingleLock lock(m_critSection);
 
   VEC_CURLSESSIONS::iterator it;
-  for(it = m_sessions.begin(); it != m_sessions.end(); ++it)
+  for (it = m_sessions.begin(); it != m_sessions.end(); ++it)
   {
-    if( it->m_easy == easy_handle )
+    if (it->m_easy == easy_handle)
     {
       SSession session = *it;
       session.m_easy = DllLibCurl::easy_duphandle(easy_handle);
@@ -347,28 +346,31 @@ CURL_HANDLE* DllLibCurlGlobal::easy_duphandle(CURL_HANDLE* easy_handle)
   return DllLibCurl::easy_duphandle(easy_handle);
 }
 
-void DllLibCurlGlobal::easy_duplicate(CURL_HANDLE* easy, CURLM* multi, CURL_HANDLE** easy_out, CURLM** multi_out)
+void DllLibCurlGlobal::easy_duplicate(CURL_HANDLE* easy,
+                                      CURLM* multi,
+                                      CURL_HANDLE** easy_out,
+                                      CURLM** multi_out)
 {
   CSingleLock lock(m_critSection);
 
-  if(easy_out && easy)
+  if (easy_out && easy)
     *easy_out = DllLibCurl::easy_duphandle(easy);
 
-  if(multi_out && multi)
+  if (multi_out && multi)
     *multi_out = DllLibCurl::multi_init();
 
   VEC_CURLSESSIONS::iterator it;
-  for(it = m_sessions.begin(); it != m_sessions.end(); ++it)
+  for (it = m_sessions.begin(); it != m_sessions.end(); ++it)
   {
-    if( it->m_easy == easy )
+    if (it->m_easy == easy)
     {
       SSession session = *it;
-      if(easy_out && easy)
+      if (easy_out && easy)
         session.m_easy = *easy_out;
       else
         session.m_easy = NULL;
 
-      if(multi_out && multi)
+      if (multi_out && multi)
         session.m_multi = *multi_out;
       else
         session.m_multi = NULL;
@@ -379,4 +381,4 @@ void DllLibCurlGlobal::easy_duplicate(CURL_HANDLE* easy, CURLM* multi, CURL_HAND
   }
   return;
 }
-}
+} // namespace XCURL
--- xbmc/filesystem/DllLibCurl.h
+++ xbmc/filesystem/DllLibCurl.h
@@ -21,84 +21,92 @@
 
 #include "threads/CriticalSection.h"
 #include <stdio.h>
+#include <string>
 #include <type_traits>
 #include <vector>
 
 /* put types of curl in namespace to avoid namespace pollution */
 namespace XCURL
 {
-  #define CURL CURL_HANDLE
-  #include <curl/curl.h>
-  #undef CURL
+#define CURL CURL_HANDLE
+#include <curl/curl.h>
+#undef CURL
 
-  class DllLibCurl
+class DllLibCurl
+{
+public:
+  CURLcode global_init(long flags);
+  void global_cleanup();
+  CURL_HANDLE* easy_init();
+  template<typename... Args>
+  CURLcode easy_setopt(CURL_HANDLE* handle, CURLoption option, Args... args)
   {
-  public:
-    CURLcode global_init(long flags);
-    void global_cleanup();
-    CURL_HANDLE * easy_init();
-    template<typename... Args>
-    CURLcode easy_setopt(CURL_HANDLE *handle, CURLoption option, Args... args)
-    {
-      return curl_easy_setopt(handle, option, std::forward<Args>(args)...);
-    }
-    CURLcode easy_perform(CURL_HANDLE * handle );
-    CURLcode easy_pause(CURL_HANDLE * handle, int bitmask );
-    void easy_reset(CURL_HANDLE * handle);
-    template<typename... Args>
-    CURLcode easy_getinfo(CURL_HANDLE *curl, CURLINFO info, Args... args)
-    {
-      return curl_easy_getinfo(curl, info, std::forward<Args>(args)...);
-    }
-    void easy_cleanup(CURL_HANDLE * handle );
-    virtual CURL_HANDLE *easy_duphandle(CURL_HANDLE *handle );
-    CURLM * multi_init(void);
-    CURLMcode multi_add_handle(CURLM *multi_handle, CURL_HANDLE *easy_handle);
-    CURLMcode multi_perform(CURLM *multi_handle, int *running_handles);
-    CURLMcode multi_remove_handle(CURLM *multi_handle, CURL_HANDLE *easy_handle);
-    CURLMcode multi_fdset(CURLM *multi_handle, fd_set *read_fd_set, fd_set *write_fd_set, fd_set *exc_fd_set, int *max_fd);
-    CURLMcode multi_timeout(CURLM *multi_handle, long *timeout);
-    CURLMsg*  multi_info_read(CURLM *multi_handle, int *msgs_in_queue);
-    CURLMcode multi_cleanup(CURLM * handle );
-    struct curl_slist* slist_append(struct curl_slist *list, const char *to_append);
-    void  slist_free_all(struct curl_slist *list);
-    const char* easy_strerror(CURLcode code);
+    return curl_easy_setopt(handle, option, std::forward<Args>(args)...);
+  }
+  CURLcode easy_perform(CURL_HANDLE* handle);
+  CURLcode easy_pause(CURL_HANDLE* handle, int bitmask);
+  void easy_reset(CURL_HANDLE* handle);
+  template<typename... Args>
+  CURLcode easy_getinfo(CURL_HANDLE* curl, CURLINFO info, Args... args)
+  {
+    return curl_easy_getinfo(curl, info, std::forward<Args>(args)...);
+  }
+  void easy_cleanup(CURL_HANDLE* handle);
+  virtual CURL_HANDLE* easy_duphandle(CURL_HANDLE* handle);
+  CURLM* multi_init(void);
+  CURLMcode multi_add_handle(CURLM* multi_handle, CURL_HANDLE* easy_handle);
+  CURLMcode multi_perform(CURLM* multi_handle, int* running_handles);
+  CURLMcode multi_remove_handle(CURLM* multi_handle, CURL_HANDLE* easy_handle);
+  CURLMcode multi_fdset(CURLM* multi_handle,
+                        fd_set* read_fd_set,
+                        fd_set* write_fd_set,
+                        fd_set* exc_fd_set,
+                        int* max_fd);
+  CURLMcode multi_timeout(CURLM* multi_handle, long* timeout);
+  CURLMsg* multi_info_read(CURLM* multi_handle, int* msgs_in_queue);
+  CURLMcode multi_cleanup(CURLM* handle);
+  struct curl_slist* slist_append(struct curl_slist* list, const char* to_append);
+  void slist_free_all(struct curl_slist* list);
+  const char* easy_strerror(CURLcode code);
 #if defined(HAS_CURL_STATIC)
-    void crypto_set_id_callback(unsigned long (*cb)(void));
-    void crypto_set_locking_callback(void (*cb)(int, int, const char*, int));
+  void crypto_set_id_callback(unsigned long (*cb)(void));
+  void crypto_set_locking_callback(void (*cb)(int, int, const char*, int));
 #endif
-  };
+};
 
-  class DllLibCurlGlobal : public DllLibCurl
+class DllLibCurlGlobal : public DllLibCurl
+{
+public:
+  DllLibCurlGlobal();
+  ~DllLibCurlGlobal();
+  /* extend interface with buffered functions */
+  void easy_acquire(const char* protocol,
+                    const char* hostname,
+                    CURL_HANDLE** easy_handle,
+                    CURLM** multi_handle);
+  void easy_release(CURL_HANDLE** easy_handle, CURLM** multi_handle);
+  void easy_duplicate(CURL_HANDLE* easy, CURLM* multi, CURL_HANDLE** easy_out, CURLM** multi_out);
+  CURL_HANDLE* easy_duphandle(CURL_HANDLE* easy_handle) override;
+  void CheckIdle();
+
+  /* overloaded load and unload with reference counter */
+
+  /* structure holding a session info */
+  typedef struct SSession
   {
-  public:
-    DllLibCurlGlobal();
-    ~DllLibCurlGlobal();
-    /* extend interface with buffered functions */
-    void easy_acquire(const char *protocol, const char *hostname, CURL_HANDLE** easy_handle, CURLM** multi_handle);
-    void easy_release(CURL_HANDLE** easy_handle, CURLM** multi_handle);
-    void easy_duplicate(CURL_HANDLE* easy, CURLM* multi, CURL_HANDLE** easy_out, CURLM** multi_out);
-    CURL_HANDLE* easy_duphandle(CURL_HANDLE* easy_handle) override;
-    void CheckIdle();
+    unsigned int m_idletimestamp; // timestamp of when this object when idle
+    std::string m_protocol;
+    std::string m_hostname;
+    bool m_busy;
+    CURL_HANDLE* m_easy;
+    CURLM* m_multi;
+  } SSession;
 
-    /* overloaded load and unload with reference counter */
+  typedef std::vector<SSession> VEC_CURLSESSIONS;
 
-    /* structure holding a session info */
-    typedef struct SSession
-    {
-      unsigned int  m_idletimestamp;  // timestamp of when this object when idle
-      std::string   m_protocol;
-      std::string   m_hostname;
-      bool          m_busy;
-      CURL_HANDLE*  m_easy;
-      CURLM*        m_multi;
-    } SSession;
-
-    typedef std::vector<SSession> VEC_CURLSESSIONS;
-
-    VEC_CURLSESSIONS m_sessions;
-    CCriticalSection m_critSection;
-  };
-}
+  VEC_CURLSESSIONS m_sessions;
+  CCriticalSection m_critSection;
+};
+} // namespace XCURL
 
 extern XCURL::DllLibCurlGlobal g_curlInterface;
