a3d15450310d4ae7bca3155ad6a64b1ae0e5d6e2
Rechi <Rechi@users.noreply.github.com>
2018-02-07 12:00:00 +0100

    [cleanup][PlatformDefs] remove unused symbols
    [cleanup][PlatformDefs] remove HWND

--- xbmc/platform/linux/PlatformDefs.h
+++ xbmc/platform/linux/PlatformDefs.h
@@ -22,8 +22,6 @@
 
 #ifdef TARGET_POSIX
 
-#define LINE_ENDING "\n"
-
 #ifndef _LARGEFILE64_SOURCE
 #define _LARGEFILE64_SOURCE
 #endif
@@ -67,19 +65,11 @@
 #define PIXEL_RSHIFT 8
 #define PIXEL_GSHIFT 16
 #define PIXEL_BSHIFT 24
-#define AMASK 0x000000ff
-#define RMASK 0x0000ff00
-#define GMASK 0x00ff0000
-#define BMASK 0xff000000
 #else
 #define PIXEL_ASHIFT 24
 #define PIXEL_RSHIFT 16
 #define PIXEL_GSHIFT 8
 #define PIXEL_BSHIFT 0
-#define AMASK 0xff000000
-#define RMASK 0x00ff0000
-#define GMASK 0x0000ff00
-#define BMASK 0x000000ff
 #endif
 
 #include <stdint.h>
@@ -108,35 +98,11 @@
 #endif
 #endif
 	
-#ifndef PRIx64
-#ifdef TARGET_WINDOWS
-#define PRIx64 "I64x"
-#else
-#if __WORDSIZE == 64
-#define PRIx64 "lx"
-#else
-#define PRIx64 "llx"
-#endif
-#endif
-#endif
-
 #ifndef PRIdS
 #define PRIdS "zd"
 #endif
 
-#ifndef PRIuS
-#define PRIuS "zu"
-#endif
-
 #ifdef TARGET_POSIX
-
-#ifndef INSTALL_PATH
-#define INSTALL_PATH    "/usr/share/xbmc"
-#endif
-
-#ifndef BIN_INSTALL_PATH
-#define BIN_INSTALL_PATH "/usr/lib/xbmc"
-#endif
 
 #define CONST   const
 #define FALSE   0
@@ -149,37 +115,16 @@
 #define strcmpi strcasecmp
 #define strnicmp  strncasecmp
 #define _atoi64(x) atoll(x)
-#define CopyMemory(dst,src,size) memmove(dst, src, size)
 #define ZeroMemory(dst,size) memset(dst, 0, size)
 
 #define VOID    void
-#define __int8    char
-#define __int16   short
 #define __int32   int
 #define __int64   long long
-#define __uint64  unsigned long long
 
 #define __stdcall
 #define __cdecl
-#define WINBASEAPI
-#define NTAPI       __stdcall
-#define CALLBACK    __stdcall
 #define WINAPI      __stdcall
-#define WINAPIV     __cdecl
-#if !defined(TARGET_DARWIN) && !defined(TARGET_FREEBSD)
 #undef APIENTRY
-#define APIENTRY    WINAPI
-#else
-#undef APIENTRY
-#define APIENTRY
-#endif
-#define APIPRIVATE  __stdcall
-#define IN
-#define OUT
-#define OPTIONAL
-#define _declspec(X)
-#define __declspec(X)
-
 struct CXHandle; // forward declaration
 typedef CXHandle* HANDLE;
 
@@ -193,7 +138,6 @@ typedef char        CHAR;
 typedef wchar_t     WCHAR;
 typedef int         INT;
 typedef unsigned int  UINT;
-// typedef int INT32;              // unused; must match Xmd.h but why bother
 typedef long long     INT64;
 typedef unsigned long long    UINT64;
 typedef long        LONG;
@@ -203,19 +147,13 @@ typedef UInt32          ULONG;
 #else
 typedef unsigned long   ULONG;
 #endif
-typedef size_t        SIZE_T;
 typedef void*         PVOID;
 typedef void*         LPVOID;
-//typedef PVOID         HANDLE;
 #define INVALID_HANDLE_VALUE     ((HANDLE)~0U)
-typedef HANDLE        HDC;
-typedef void*       HWND;
 typedef BYTE*       LPBYTE;
 typedef DWORD*        LPDWORD;
 typedef CONST CHAR*   LPCSTR;
 typedef CONST WCHAR*    LPCWSTR;
-typedef CHAR*     LPTSTR;
-typedef WCHAR         *PWSTR,      *LPWSTR,    *NWPSTR;
 typedef CHAR            *PSTR,       *LPSTR,     *NPSTR;
 typedef LONG        *PLONG, *LPLONG;
 #ifdef UNICODE
@@ -224,17 +162,9 @@ typedef LPCWSTR       LPCTSTR;
 typedef LPCSTR      LPCTSTR;
 #endif
 typedef unsigned __int64 ULONGLONG;
-typedef unsigned long   ULONG_PTR;
-typedef ULONG_PTR     DWORD_PTR;
 typedef intptr_t (*FARPROC)(void);
 
 #define MAXWORD   0xffff
-
-typedef DWORD LCID;
-typedef WORD* LPWORD;
-typedef CHAR* LPCHAR;
-typedef CHAR* PCHAR;
-typedef const void* LPCVOID;
 
 typedef union _LARGE_INTEGER
 {
@@ -253,7 +183,6 @@ typedef union _LARGE_INTEGER
   ULONGLONG QuadPart;
 } ULARGE_INTEGER, *PULARGE_INTEGER;
 
-#define MAKELONG(low,high)     ((LONG)(((WORD)((DWORD_PTR)(low) & 0xFFFF)) | ((DWORD)((WORD)((DWORD_PTR)(high) & 0xFFFF))) << 16))
 LONGLONG Int32x32To64(LONG Multiplier, LONG Multiplicand);
 
 void OutputDebugString(LPCTSTR lpOutputString);
@@ -297,27 +226,15 @@ typedef struct _TIME_ZONE_INFORMATION {
 #define THREAD_PRIORITY_NORMAL          0
 #define THREAD_PRIORITY_HIGHEST         THREAD_BASE_PRIORITY_MAX
 #define THREAD_PRIORITY_ABOVE_NORMAL    (THREAD_PRIORITY_HIGHEST-1)
-#define THREAD_PRIORITY_ERROR_RETURN    (0x7fffffff)
-#define THREAD_PRIORITY_TIME_CRITICAL   THREAD_BASE_PRIORITY_LOWRT
-#define THREAD_PRIORITY_IDLE            THREAD_BASE_PRIORITY_IDLE
 
 // Network
 #define SOCKADDR_IN struct sockaddr_in
-#define IN_ADDR struct in_addr
 #define SOCKET_ERROR (-1)
 #define INVALID_SOCKET (~0)
 #define closesocket(s)  close(s)
 #define ioctlsocket(s, f, v) ioctl(s, f, v)
 #define WSAGetLastError() (errno)
-#define WSASetLastError(e) (errno = e)
 #define WSAECONNRESET ECONNRESET
-#define WSAHOST_NOT_FOUND ENOENT
-#define WSAETIMEDOUT  ETIMEDOUT
-#define WSAEADDRINUSE EADDRINUSE
-#define WSAECANCELLED EINTR
-#define WSAECONNREFUSED ECONNREFUSED
-#define WSAECONNABORTED ECONNABORTED
-#define WSAETIMEDOUT ETIMEDOUT
 
 typedef int SOCKET;
 
@@ -326,7 +243,6 @@ typedef int (*LPTHREAD_START_ROUTINE)(void *);
 
 // File
 #define O_BINARY 0
-#define O_TEXT   0
 #define _O_TRUNC O_TRUNC
 #define _O_RDONLY O_RDONLY
 #define _O_WRONLY O_WRONLY
@@ -377,8 +293,6 @@ typedef struct _WIN32_FIND_DATA
     CHAR      cAlternateFileName[14];
 } WIN32_FIND_DATA, *PWIN32_FIND_DATA, *LPWIN32_FIND_DATA;
 
-#define LPWIN32_FIND_DATAA LPWIN32_FIND_DATA
-
 #define FILE_ATTRIBUTE_DIRECTORY           0x00000010
 
 typedef struct _SECURITY_ATTRIBUTES {
@@ -395,8 +309,6 @@ typedef struct _SECURITY_ATTRIBUTES {
 #define _S_IFDIR  S_IFDIR
 #define MAX_PATH PATH_MAX
 
-#define _stat stat
-
 // Memory
 typedef struct _MEMORYSTATUSEX
 {
@@ -411,30 +323,9 @@ typedef struct _MEMORYSTATUSEX
   uint64_t ullAvailVirtual;
 } MEMORYSTATUSEX, *LPMEMORYSTATUSEX;
 
-// Basic D3D stuff
-typedef struct _RECT {
-  LONG left;
-  LONG top;
-  LONG right;
-  LONG bottom;
-} RECT, *PRECT;
-
-// Misc stuff found in the code, not really important
-#define PAGE_READWRITE    0x04
-#define MAXULONG_PTR    ((ULONG) 0xffffffff)
-
 // CreateFile defines
-#define FILE_FLAG_WRITE_THROUGH         0x80000000
-#define FILE_FLAG_OVERLAPPED            0x40000000
 #define FILE_FLAG_NO_BUFFERING          0x20000000
-#define FILE_FLAG_RANDOM_ACCESS         0x10000000
-#define FILE_FLAG_SEQUENTIAL_SCAN       0x08000000
 #define FILE_FLAG_DELETE_ON_CLOSE       0x04000000
-#define FILE_FLAG_BACKUP_SEMANTICS      0x02000000
-#define FILE_FLAG_POSIX_SEMANTICS       0x01000000
-#define FILE_FLAG_OPEN_REPARSE_POINT    0x00200000
-#define FILE_FLAG_OPEN_NO_RECALL        0x00100000
-#define FILE_FLAG_FIRST_PIPE_INSTANCE   0x00080000
 
 #define CREATE_NEW          1
 #define CREATE_ALWAYS       2
@@ -442,21 +333,8 @@ typedef struct _RECT {
 #define OPEN_ALWAYS         4
 #define TRUNCATE_EXISTING   5
 
-#define FILE_ATTRIBUTE_NORMAL 0x00000080
-#define FILE_ATTRIBUTE_READONLY 0x00000001
-#define FILE_ATTRIBUTE_HIDDEN 0x00000002
-#define FILE_ATTRIBUTE_SYSTEM 0x00000004
-#define FILE_ATTRIBUTE_DIRECTORY  0x00000010
-
 #define FILE_READ_DATA   ( 0x0001 )
 #define FILE_WRITE_DATA  ( 0x0002 )
-#define FILE_APPEND_DATA ( 0x0004 )
-
-#define GENERIC_READ  FILE_READ_DATA
-#define GENERIC_WRITE FILE_WRITE_DATA
-#define FILE_SHARE_READ                  0x00000001
-#define FILE_SHARE_WRITE                 0x00000002
-#define FILE_SHARE_DELETE                0x00000004
 
 #endif
 
