ed45fcf3b43e0eb998f8dd619e5630adec05e069
Rechi <Rechi@users.noreply.github.com>
2018-02-13 15:40:00 +0100

    [cleanup] FreeBSD 8 is EOL since 2015

--- xbmc/freebsd/FreeBSDGNUReplacements.h
+++ xbmc/freebsd/FreeBSDGNUReplacements.h
@@ -34,13 +34,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#if __FreeBSD_version < 800067
-
-size_t strnlen(const char *s, size_t n);
-ssize_t getdelim(char **lineptr, size_t *n, int delimiter, FILE *stream);
-
-#endif
-
 int strverscmp(const char *s1, const char *s2);
 
 #ifdef __cplusplus
