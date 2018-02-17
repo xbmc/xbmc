4a602574528ab0a256fe2d2fbf3190cafa65ddeb
Rechi <Rechi@users.noreply.github.com>
2018-02-17 10:00:00 +0100

    [cleanup] move GET_[ARGB] defines to system_gl.h

--- xbmc/system.h
+++ xbmc/system.h
@@ -52,9 +52,3 @@
 #define SAFE_DELETE(p)       do { delete (p);     (p)=NULL; } while (0)
 #define SAFE_DELETE_ARRAY(p) do { delete[] (p);   (p)=NULL; } while (0)
 #define SAFE_RELEASE(p)      do { if(p) { (p)->Release(); (p)=NULL; } } while (0)
-
-// Useful pixel colour manipulation macros
-#define GET_A(color)            ((color >> 24) & 0xFF)
-#define GET_R(color)            ((color >> 16) & 0xFF)
-#define GET_G(color)            ((color >>  8) & 0xFF)
-#define GET_B(color)            ((color >>  0) & 0xFF)
