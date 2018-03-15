e3a1a1b5a9cb01bb79a8f9b4348c48dff135272c
Philipp Kerling <pkerling@casix.org>
2018-03-15 12:14:56 +0100

    Add OpenSSL-based digest calculation utility

---
+++ xbmc/utils/Digest.cpp
@@ -0,0 +1,173 @@
+/*
+ *      Copyright (C) 2018 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "Digest.h"
+#include "StringUtils.h"
+
+#include <openssl/evp.h>
+
+#include <array>
+#include <stdexcept>
+
+namespace KODI
+{
+namespace UTILITY
+{
+
+namespace
+{
+
+EVP_MD const * TypeToEVPMD(CDigest::Type type)
+{
+  switch (type)
+  {
+    case CDigest::Type::MD5:
+      return EVP_md5();
+    case CDigest::Type::SHA1:
+      return EVP_sha1();
+    case CDigest::Type::SHA256:
+      return EVP_sha256();
+    case CDigest::Type::SHA512:
+      return EVP_sha512();
+    default:
+      throw std::invalid_argument("Unknown digest type");
+  }
+}
+
+}
+
+std::string CDigest::TypeToString(Type type)
+{
+  switch (type)
+  {
+    case Type::MD5:
+      return "md5";
+    case Type::SHA1:
+      return "sha1";
+    case Type::SHA256:
+      return "sha256";
+    case Type::SHA512:
+      return "sha512";
+    default:
+      throw std::invalid_argument("Unknown digest type");
+  }
+}
+
+CDigest::Type CDigest::TypeFromString(std::string const& type)
+{
+  std::string typeLower{type};
+  StringUtils::ToLower(typeLower);
+  if (type == "md5")
+  {
+    return Type::MD5;
+  }
+  else if (type == "sha1")
+  {
+    return Type::SHA1;
+  }
+  else if (type == "sha256")
+  {
+    return Type::SHA256;
+  }
+  else if (type == "sha512")
+  {
+    return Type::SHA512;
+  }
+  else
+  {
+    throw std::invalid_argument(std::string("Unknown digest type \"") + type + "\"");
+  }
+}
+
+void CDigest::MdCtxDeleter::operator()(EVP_MD_CTX* context)
+{
+  EVP_MD_CTX_destroy(context);
+}
+
+CDigest::CDigest(Type type)
+: m_context{EVP_MD_CTX_create()}, m_md(TypeToEVPMD(type))
+{
+  if (1 != EVP_DigestInit_ex(m_context.get(), m_md, nullptr))
+  {
+    throw std::runtime_error("EVP_DigestInit_ex failed");
+  }
+}
+
+void CDigest::Update(std::string const& data)
+{
+  Update(data.c_str(), data.size());
+}
+
+void CDigest::Update(void const* data, std::size_t size)
+{
+  if (m_finalized)
+  {
+    throw std::logic_error("Finalized digest cannot be updated any more");
+  }
+
+  if (1 != EVP_DigestUpdate(m_context.get(), data, size))
+  {
+    throw std::runtime_error("EVP_DigestUpdate failed");
+  }
+}
+
+std::string CDigest::FinalizeRaw()
+{
+  if (m_finalized)
+  {
+    throw std::logic_error("Digest can only be finalized once");
+  }
+
+  m_finalized = true;
+
+  std::array<unsigned char, 64> digest;
+  std::size_t size = EVP_MD_size(m_md);
+  if (size > digest.size())
+  {
+    throw std::runtime_error("Digest unexpectedly long");
+  }
+  if (1 != EVP_DigestFinal_ex(m_context.get(), digest.data(), nullptr))
+  {
+    throw std::runtime_error("EVP_DigestFinal_ex failed");
+  }
+  return {reinterpret_cast<char*> (digest.data()), size};
+}
+
+std::string CDigest::Finalize()
+{
+  return StringUtils::ToHexadecimal(FinalizeRaw());
+}
+
+std::string CDigest::Calculate(Type type, std::string const& data)
+{
+  CDigest digest{type};
+  digest.Update(data);
+  return digest.Finalize();
+}
+
+std::string CDigest::Calculate(Type type, void const* data, std::size_t size)
+{
+  CDigest digest{type};
+  digest.Update(data, size);
+  return digest.Finalize();
+}
+
+}
+}
---
+++ xbmc/utils/test/TestDigest.cpp
@@ -0,0 +1,71 @@
+/*
+ *      Copyright (C) 2018 Team Kodi
+ *      http://kodi.tv
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "utils/Digest.h"
+
+#include "gtest/gtest.h"
+
+using KODI::UTILITY::CDigest;
+
+TEST(TestDigest, Digest_Empty)
+{
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::MD5, "").c_str(), "d41d8cd98f00b204e9800998ecf8427e");
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::MD5, nullptr, 0).c_str(), "d41d8cd98f00b204e9800998ecf8427e");
+  {
+    CDigest digest{CDigest::Type::MD5};
+    EXPECT_STREQ(digest.Finalize().c_str(), "d41d8cd98f00b204e9800998ecf8427e");
+  }
+  {
+    CDigest digest{CDigest::Type::MD5};
+    digest.Update("");
+    digest.Update(nullptr, 0);
+    EXPECT_STREQ(digest.Finalize().c_str(), "d41d8cd98f00b204e9800998ecf8427e");
+  }
+}
+
+TEST(TestDigest, Digest_Basic)
+{
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::MD5, "asdf").c_str(), "912ec803b2ce49e4a541068d495ab570");
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::MD5, "asdf", 4).c_str(), "912ec803b2ce49e4a541068d495ab570");
+  {
+    CDigest digest{CDigest::Type::MD5};
+    digest.Update("as");
+    digest.Update("df", 2);
+    EXPECT_STREQ(digest.Finalize().c_str(), "912ec803b2ce49e4a541068d495ab570");
+  }
+}
+
+TEST(TestDigest, Digest_SHA1)
+{
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA1, "").c_str(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA1, "asdf").c_str(), "3da541559918a808c2402bba5012f6c60b27661c");
+}
+
+TEST(TestDigest, Digest_SHA256)
+{
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA256, "").c_str(), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA256, "asdf").c_str(), "f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b");
+}
+
+TEST(TestDigest, Digest_SHA512)
+{
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA512, "").c_str(), "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e");
+  EXPECT_STREQ(CDigest::Calculate(CDigest::Type::SHA512, "asdf").c_str(), "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429080fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1");
+}
