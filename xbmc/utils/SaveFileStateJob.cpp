c701bc6199b4cd2ac036644f4edf594cd4f418db
David Gamache <contact@gamache.de>
2018-04-07 23:00:00 +0200

    Do not invoke UpdateFileList() when calling SaveFileState()
    Set param2 of the GUIMessage to 0 so that only the one item gets
    updated, not the whole list.

--- xbmc/utils/SaveFileStateJob.cpp
+++ xbmc/utils/SaveFileStateJob.cpp
@@ -170,7 +170,7 @@ void CSaveFileState::DoWork(CFileItem& item,
           CFileItemPtr msgItem(new CFileItem(item));
           if (item.HasProperty("original_listitem_url"))
             msgItem->SetPath(item.GetProperty("original_listitem_url").asString());
-          CGUIMessage message(GUI_MSG_NOTIFY_ALL, CServiceBroker::GetGUI()->GetWindowManager().GetActiveWindow(), 0, GUI_MSG_UPDATE_ITEM, 1, msgItem); // 1 to update the listing as well
+          CGUIMessage message(GUI_MSG_NOTIFY_ALL, CServiceBroker::GetGUI()->GetWindowManager().GetActiveWindow(), 0, GUI_MSG_UPDATE_ITEM, 0, msgItem);
           CServiceBroker::GetGUI()->GetWindowManager().SendThreadMessage(message);
         }
       }
