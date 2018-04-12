ef4b154b1a6a5e1109a5d2a0b7c9d4ee233bd6d5
Anton Fedchin <anightik@gmail.com>
2018-04-12 10:00:00 +0200

    rendering: directx - added 10bit output

--- xbmc/rendering/dx/RenderSystemDX.cpp
+++ xbmc/rendering/dx/RenderSystemDX.cpp
@@ -162,7 +162,8 @@ void CRenderSystemDX::CheckInterlacedStereoView()
       || RENDER_STEREO_MODE_CHECKERBOARD == stereoMode))
   {
     const auto outputSize = m_deviceResources->GetOutputSize();
-    if (!m_rightEyeTex.Create(outputSize.Width, outputSize.Height, 1, D3D11_USAGE_DEFAULT, DXGI_FORMAT_B8G8R8A8_UNORM))
+    DXGI_FORMAT texFormat = m_deviceResources->GetBackBuffer()->GetFormat();
+    if (!m_rightEyeTex.Create(outputSize.Width, outputSize.Height, 1, D3D11_USAGE_DEFAULT, texFormat))
     {
       CLog::Log(LOGERROR, "%s - Failed to create right eye buffer.", __FUNCTION__);
       CServiceBroker::GetWinSystem()->GetGfxContext().SetStereoMode(RENDER_STEREO_MODE_SPLIT_HORIZONTAL); // try fallback to split horizontal
