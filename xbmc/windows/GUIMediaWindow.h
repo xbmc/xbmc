52680372c20b12b35acd10522e9e4679e3a09395
Rainer Hochecker <fernetmenta@online.de>
2018-05-01 11:02:35 +0200

    add busy dialog to GUIMediaWindow

--- xbmc/windows/GUIMediaWindow.h
+++ xbmc/windows/GUIMediaWindow.h
@@ -164,8 +164,8 @@ protected:
   void UpdateFileList();
   virtual void OnDeleteItem(int iItem);
   void OnRenameItem(int iItem);
-
   bool WaitForNetwork() const;
+  bool GetDirectoryItems(CURL &url, CFileItemList &items, bool useDir);
 
   /*! \brief Translate the folder to start in from the given quick path
    \param url the folder the user wants
@@ -209,4 +209,5 @@ protected:
    \sa Update
    */
   std::string m_strFilterPath;
+  bool m_useBusyDialog = false;
 };
