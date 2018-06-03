2450560e83c45635c666f28f41c6892adee035a8
Anton Fedchin <anightik@gmail.com>
2018-06-03 12:36:02 +0300

    [d3d11] screenshot: fix screenshot in case of 10bit back buffer

--- xbmc/utils/Screenshot.cpp
+++ xbmc/utils/Screenshot.cpp
@@ -88,32 +88,22 @@ bool CScreenshotSurface::capture()
   deviceResources->FinishCommandList();
 
   ComPtr<ID3D11DeviceContext> pImdContext = deviceResources->GetImmediateContext();
-  ComPtr<ID3D11DeviceContext> pContext = deviceResources->GetD3DContext();
   ComPtr<ID3D11Device> pDevice = deviceResources->GetD3DDevice();
-
-  ComPtr<ID3D11RenderTargetView> pRTView = nullptr;
-  pContext->OMGetRenderTargets(1, pRTView.GetAddressOf(), nullptr);
-  if (pRTView == nullptr)
-    return false;
-
-  ComPtr<ID3D11Resource> pRTResource = nullptr;
-  pRTView->GetResource(pRTResource.GetAddressOf());
-
-  ComPtr<ID3D11Texture2D> pCopyTexture = nullptr;
-  ComPtr<ID3D11Texture2D> pRTTexture = nullptr;
-  if (FAILED(pRTResource.As(&pRTTexture)))
+  CD3DTexture* backbuffer = deviceResources->GetBackBuffer();
+  if (!backbuffer)
     return false;
 
   D3D11_TEXTURE2D_DESC desc = { 0 };
-  pRTTexture->GetDesc(&desc);
+  backbuffer->GetDesc(&desc);
   desc.Usage = D3D11_USAGE_STAGING;
   desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
   desc.BindFlags = 0;
 
-  if (SUCCEEDED(pDevice->CreateTexture2D(&desc, nullptr, pCopyTexture.GetAddressOf())))
+  ComPtr<ID3D11Texture2D> pCopyTexture = nullptr;
+  if (SUCCEEDED(pDevice->CreateTexture2D(&desc, nullptr, &pCopyTexture)))
   {
     // take copy
-    pImdContext->CopyResource(pCopyTexture.Get(), pRTTexture.Get());
+    pImdContext->CopyResource(pCopyTexture.Get(), backbuffer->Get());
 
     D3D11_MAPPED_SUBRESOURCE res;
     if (SUCCEEDED(pImdContext->Map(pCopyTexture.Get(), 0, D3D11_MAP_READ, 0, &res)))
@@ -122,7 +112,33 @@ bool CScreenshotSurface::capture()
       m_height = desc.Height;
       m_stride = res.RowPitch;
       m_buffer = new unsigned char[m_height * m_stride];
-      memcpy(m_buffer, res.pData, m_height * m_stride);
+      if (desc.Format == DXGI_FORMAT_R10G10B10A2_UNORM)
+      {
+        // convert R10G10B10A2 -> B8G8R8A8
+        for (int y = 0; y < m_height; y++)
+        {
+          uint32_t* pixels10 = reinterpret_cast<uint32_t*>(static_cast<uint8_t*>(res.pData) + y * res.RowPitch);
+          uint8_t* pixels8 = m_buffer + y * m_stride;
+
+          for (int x = 0; x < m_width; x++, pixels10++, pixels8 += 4)
+          {
+            // actual bit per channel is A2B10G10R10
+            uint32_t pixel = *pixels10;
+            // R
+            pixels8[2] = static_cast<uint8_t>((pixel & 0x3FF) * 255 / 1023);
+            // G
+            pixel >>= 10;
+            pixels8[1] = static_cast<uint8_t>((pixel & 0x3FF) * 255 / 1023);
+            // B
+            pixel >>= 10;
+            pixels8[0] = static_cast<uint8_t>((pixel & 0x3FF) * 255 / 1023);
+            // A
+            pixels8[3] = 0xFF;
+          }
+        }
+      }
+      else
+        memcpy(m_buffer, res.pData, m_height * m_stride);
       pImdContext->Unmap(pCopyTexture.Get(), 0);
     }
     else
