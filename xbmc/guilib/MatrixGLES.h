e842ec7ad488660341b42bacbc72ac0393ace1c5
Max Kellermann <max.kellermann@gmail.com>
2018-06-08 19:02:05 +0200

    guilib/MatrixGLES: add GLfloat... constructor
    Unlike the old array constructor, this one can be `constexpr` because
    it doesn't use `memcpy()`.

--- xbmc/guilib/MatrixGLES.h
+++ xbmc/guilib/MatrixGLES.h
@@ -27,6 +27,13 @@ class CMatrixGL
 {
 public:
   CMatrixGL() = default;
+
+  constexpr CMatrixGL(GLfloat x0, GLfloat x1, GLfloat x2, GLfloat x3,
+                      GLfloat x4, GLfloat x5, GLfloat x6, GLfloat x7,
+                      GLfloat x8, GLfloat x9, GLfloat x10, GLfloat x11,
+                      GLfloat x12, GLfloat x13, GLfloat x14, GLfloat x15)
+    :m_pMatrix{x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15} {}
+
   operator const float*() const                { return m_pMatrix; }
 
   void LoadIdentity();
