38571b1b4bc13e9e8c68aa7f0a16e489344c249d
h.udo <hudokkow@gmail.com>
2018-06-12 21:25:13 +0100

    [kissfft] BSD-3-Clause SPDX license identifier
    See https://github.com/mborgerding/kissfft/pull/4 for reference

--- xbmc/contrib/kissfft/kiss_fftr.h
+++ xbmc/contrib/kissfft/kiss_fftr.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (c) 2003-2004, Mark Borgerding. All rights reserved.
+ *  This file is part of KISS FFT - https://github.com/mborgerding/kissfft
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  See COPYING file for more information.
+ */
+
 #ifndef KISS_FTR_H
 #define KISS_FTR_H
 
