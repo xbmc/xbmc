ab99ae8a1ecd5e55e1acc440cbce374730c84180
h.udo <hudokkow@gmail.com>
2018-06-12 20:51:34 +0100

    [xbmc] BSD-4-Clause SPDX license identifier

--- xbmc/platform/win32/WIN32Util.cpp
+++ xbmc/platform/win32/WIN32Util.cpp
@@ -624,47 +624,19 @@ extern "C"
 }
 
 extern "C" {
-  /*
-   * Ported from NetBSD to Windows by Ron Koenderink, 2007
-   */
-
-  /*  $NetBSD: strptime.c,v 1.25 2005/11/29 03:12:00 christos Exp $  */
-
-  /*-
-   * Copyright (c) 1997, 1998, 2005 The NetBSD Foundation, Inc.
-   * All rights reserved.
-   *
-   * This code was contributed to The NetBSD Foundation by Klaus Klein.
-   * Heavily optimised by David Laight
-   *
-   * Redistribution and use in source and binary forms, with or without
-   * modification, are permitted provided that the following conditions
-   * are met:
-   * 1. Redistributions of source code must retain the above copyright
-   *    notice, this list of conditions and the following disclaimer.
-   * 2. Redistributions in binary form must reproduce the above copyright
-   *    notice, this list of conditions and the following disclaimer in the
-   *    documentation and/or other materials provided with the distribution.
-   * 3. All advertising materials mentioning features or use of this software
-   *    must display the following acknowledgement:
-   *        This product includes software developed by the NetBSD
-   *        Foundation, Inc. and its contributors.
-   * 4. Neither the name of The NetBSD Foundation nor the names of its
-   *    contributors may be used to endorse or promote products derived
-   *    from this software without specific prior written permission.
-   *
-   * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
-   * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-   * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-   * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
-   * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-   * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-   * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-   * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-   * POSSIBILITY OF SUCH DAMAGE.
-   */
+/*
+ *  Copyright (c) 1997, 1998, 2005 The NetBSD Foundation, Inc.
+ *  All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-4-Clause
+ *  See LICENSES/README.md for more information.
+ *
+ *  Ported from NetBSD to Windows by Ron Koenderink, 2007
+ *  $NetBSD: strptime.c,v 1.25 2005/11/29 03:12:00 christos Exp $
+ *
+ *  This code was contributed to The NetBSD Foundation by Klaus Klein.
+ *  Heavily optimised by David Laight
+ */
 
   #if !defined(TARGET_WINDOWS)
   #include <sys/cdefs.h>
