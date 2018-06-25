b76a92317230c950a858d7ab5396c09a3401a004
Lukas Rusak <lorusak@gmail.com>
2018-06-25 14:09:40 -0700

    windowing/gbm: plane selection rework
    This rework now allows us to find a dedicated plane for video
    and a dedicated plane for the gui. Before we would swap the gui
    back and forth between planes depending if a video was playing.
    That behaviour isn't needed and now we can run the gui on the overlay
    full time.
    
    We have to track the fb format and use an alpha format when a
    video is playing and a non-alpha format when no video is playing.
    When the format changes the fb is validated and destroyed so a new
    fb can be created.
    
    This also allows us to add dependencies to the primary plane. For example
    in the DRMPRIME renderer we can disable it if no primary plane is available.
    
    We check for three formats currently:
    1) YUV420 -> for RPi
    2) YUYV   -> for i.MX6
    3) NV12   -> for basically everything else
    
    more formats can be added in the future if needed.

--- xbmc/windowing/gbm/DRMUtils.h
+++ xbmc/windowing/gbm/DRMUtils.h
@@ -20,6 +20,7 @@
 
 #pragma once
 
+#include <drm_fourcc.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 #include <gbm.h>
@@ -27,6 +28,12 @@
 
 #include "windowing/Resolution.h"
 #include "GBMUtils.h"
+
+enum EPLANETYPE
+{
+  VIDEO_PLANE,
+  GUI_PLANE
+};
 
 struct drm_object
 {
@@ -39,7 +46,7 @@ struct drm_object
 struct plane : drm_object
 {
   drmModePlanePtr plane = nullptr;
-  uint32_t format;
+  uint32_t format = DRM_FORMAT_XRGB8888;
 };
 
 struct connector : drm_object
@@ -61,6 +68,7 @@ struct drm_fb
 {
   struct gbm_bo *bo = nullptr;
   uint32_t fb_id;
+  uint32_t format;
 };
 
 class CDRMUtils
@@ -106,6 +114,9 @@ protected:
   int m_height = 0;
 
 private:
+  static bool SupportsFormat(drmModePlanePtr plane, uint32_t format);
+  drmModePlanePtr FindPlane(drmModePlaneResPtr resources, int crtc_index, int type);
+
   bool GetResources();
   bool FindConnector();
   bool FindEncoder();
