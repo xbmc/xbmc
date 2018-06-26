c5c32eda7621af6336f6a070282c9b3a9f1cdc9e
enen92 <92enen@gmail.com>
2018-06-26 11:28:36 +0100

    [Utils/Color] Add static color definitions

--- xbmc/utils/Color.h
+++ xbmc/utils/Color.h
@@ -27,4 +27,17 @@ namespace UTILS
 
   typedef uint32_t Color;
 
+namespace COLOR
+{
+  static const Color NONE = 0x00000000;
+  static const Color BLACK = 0xFF000000;
+  static const Color YELLOW = 0xFFFFFF00;
+  static const Color WHITE = 0xFFFFFFFF;
+  static const Color LIGHTGREY = 0xFFE5E5E5;
+  static const Color GREY = 0xFFC0C0C0;
+  static const Color BLUE = 0xFF0099FF;
+  static const Color BRIGHTGREEN = 0xFF00FF00;
+  static const Color YELLOWGREEN = 0xFFCCFF00;
+  static const Color CYAN = 0xFF00FFFF;
+} // namespace COLOR
 } // namespace UTILS
