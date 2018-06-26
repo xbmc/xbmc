4182fecefafae2f32448eb55beada9fad4b79232
enen92 <92enen@gmail.com>
2018-06-26 23:05:11 +0100

    [VideoRenderers] Do not use a background for DebugRenderer

--- xbmc/cores/VideoPlayer/VideoRenderers/DebugRenderer.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/DebugRenderer.cpp
@@ -121,7 +121,7 @@ void CDebugRenderer::CRenderer::Render(int idx)
 
     COverlayText *text = dynamic_cast<COverlayText*>(o);
     if (text)
-      text->PrepareRender("arial.ttf", 1, 16, 0, m_font, m_fontBorder);
+      text->PrepareRender("arial.ttf", 1, 16, 0, m_font, m_fontBorder, UTILS::COLOR::NONE);
 
     RESOLUTION_INFO res = CServiceBroker::GetWinSystem()->GetGfxContext().GetResInfo(CServiceBroker::GetWinSystem()->GetGfxContext().GetVideoResolution());
 
