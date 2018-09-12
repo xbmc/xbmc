fab31888f3e19c04c2ccff448d2f60958a1bdd40
Rechi <Rechi@users.noreply.github.com>
2018-09-12 12:00:00 +0200

    [modernize][playlists] range-based loops

--- xbmc/playlists/SmartPlayList.cpp
+++ xbmc/playlists/SmartPlayList.cpp
@@ -115,8 +115,6 @@ static const translateField fields[] = {
   { "source",            FieldSource,                  CDatabaseQueryRule::TEXT_FIELD,     NULL,                                 true,  39030 }
 };
 
-static const size_t NUM_FIELDS = sizeof(fields) / sizeof(translateField);
-
 typedef struct
 {
   std::string name;
@@ -140,23 +138,21 @@ static const group groups[] = { { "",           FieldUnknown,   false,    571 },
                                 { "tags",       FieldTag,       false,  20459 },
                               };
 
-static const size_t NUM_GROUPS = sizeof(groups) / sizeof(group);
-
 #define RULE_VALUE_SEPARATOR  " / "
 
 CSmartPlaylistRule::CSmartPlaylistRule() = default;
 
 int CSmartPlaylistRule::TranslateField(const char *field) const
 {
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
-    if (StringUtils::EqualsNoCase(field, fields[i].string)) return fields[i].field;
+  for (const translateField& f : fields)
+    if (StringUtils::EqualsNoCase(field, f.string)) return f.field;
   return FieldNone;
 }
 
 std::string CSmartPlaylistRule::TranslateField(int field) const
 {
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
-    if (field == fields[i].field) return fields[i].string;
+  for (const translateField& f : fields)
+    if (field == f.field) return f.string;
   return "none";
 }
 
@@ -176,10 +172,10 @@ std::string CSmartPlaylistRule::TranslateOrder(SortBy order)
 
 Field CSmartPlaylistRule::TranslateGroup(const char *group)
 {
-  for (unsigned int i = 0; i < NUM_GROUPS; i++)
+  for (const auto & i : groups)
   {
-    if (StringUtils::EqualsNoCase(group, groups[i].name))
-      return groups[i].field;
+    if (StringUtils::EqualsNoCase(group, i.name))
+      return i.field;
   }
 
   return FieldUnknown;
@@ -187,10 +183,10 @@ Field CSmartPlaylistRule::TranslateGroup(const char *group)
 
 std::string CSmartPlaylistRule::TranslateGroup(Field group)
 {
-  for (unsigned int i = 0; i < NUM_GROUPS; i++)
+  for (const auto & i : groups)
   {
-    if (group == groups[i].field)
-      return groups[i].name;
+    if (group == i.field)
+      return i.name;
   }
 
   return "";
@@ -198,22 +194,22 @@ std::string CSmartPlaylistRule::TranslateGroup(Field group)
 
 std::string CSmartPlaylistRule::GetLocalizedField(int field)
 {
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
-    if (field == fields[i].field) return g_localizeStrings.Get(fields[i].localizedString);
+  for (const translateField& f : fields)
+    if (field == f.field) return g_localizeStrings.Get(f.localizedString);
   return g_localizeStrings.Get(16018);
 }
 
 CDatabaseQueryRule::FIELD_TYPE CSmartPlaylistRule::GetFieldType(int field) const
 {
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
-    if (field == fields[i].field) return fields[i].type;
+  for (const translateField& f : fields)
+    if (field == f.field) return f.type;
   return TEXT_FIELD;
 }
 
 bool CSmartPlaylistRule::IsFieldBrowseable(int field)
 {
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
-    if (field == fields[i].field) return fields[i].browseable;
+  for (const translateField& f : fields)
+    if (field == f.field) return f.browseable;
 
   return false;
 }
@@ -227,11 +223,11 @@ bool CSmartPlaylistRule::Validate(const std::string &input, void *data)
 
   // check if there's a validator for this rule
   StringValidation::Validator validator = NULL;
-  for (unsigned int i = 0; i < NUM_FIELDS; i++)
+  for (const translateField& field : fields)
   {
-    if (rule->m_field == fields[i].field)
+    if (rule->m_field == field.field)
     {
-        validator = fields[i].validator;
+        validator = field.validator;
         break;
     }
   }
@@ -651,10 +647,10 @@ std::vector<Field> CSmartPlaylistRule::GetGroups(const std::string &type)
 
 std::string CSmartPlaylistRule::GetLocalizedGroup(Field group)
 {
-  for (unsigned int i = 0; i < NUM_GROUPS; i++)
+  for (const auto & i : groups)
   {
-    if (group == groups[i].field)
-      return g_localizeStrings.Get(groups[i].localizedString);
+    if (group == i.field)
+      return g_localizeStrings.Get(i.localizedString);
   }
 
   return g_localizeStrings.Get(groups[0].localizedString);
@@ -662,10 +658,10 @@ std::string CSmartPlaylistRule::GetLocalizedGroup(Field group)
 
 bool CSmartPlaylistRule::CanGroupMix(Field group)
 {
-  for (unsigned int i = 0; i < NUM_GROUPS; i++)
+  for (const auto & i : groups)
   {
-    if (group == groups[i].field)
-      return groups[i].canMix;
+    if (group == i.field)
+      return i.canMix;
   }
 
   return false;
@@ -1455,10 +1451,10 @@ void CSmartPlaylist::GetAvailableFields(const std::string &type, std::vector<std
   std::vector<Field> typeFields = CSmartPlaylistRule::GetFields(type);
   for (std::vector<Field>::const_iterator field = typeFields.begin(); field != typeFields.end(); ++field)
   {
-    for (unsigned int i = 0; i < NUM_FIELDS; i++)
+    for (const translateField& i : fields)
     {
-      if (*field == fields[i].field)
-        fieldList.push_back(fields[i].string);
+      if (*field == i.field)
+        fieldList.push_back(i.string);
     }
   }
 }
