1aa7e759bfd53ed2782f9d8920892b0885016fa5
Lukas Rusak <lorusak@gmail.com>
2018-10-15 23:30:18 -0700

    windowing/gbm: reset libinput early to avoid shutdown races

--- xbmc/windowing/gbm/WinSystemGbm.cpp
+++ xbmc/windowing/gbm/WinSystemGbm.cpp
@@ -113,6 +113,9 @@ bool CWinSystemGbm::DestroyWindowSystem()
   m_GBM->DestroyDevice();
 
   CLog::Log(LOGDEBUG, "CWinSystemGbm::%s - deinitialized DRM", __FUNCTION__);
+
+  m_libinput.reset();
+
   return true;
 }
 
