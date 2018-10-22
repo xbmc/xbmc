c96b2773afe290a9dac54ff4c8e0744be801f1a2
Garrett Brown <themagnificentmrb@gmail.com>
2018-10-21 17:36:19 -0700

    VideoPlayer: Fix warning
    Warning was:
    
    Member 'demuxerId' was not initialized in this constructor

--- xbmc/cores/VideoPlayer/Interface/Addon/DemuxPacket.h
+++ xbmc/cores/VideoPlayer/Interface/Addon/DemuxPacket.h
@@ -24,7 +24,7 @@ typedef struct DemuxPacket
   uint8_t *pData = nullptr;
   int iSize = 0;
   int iStreamId = -1;
-  int64_t demuxerId; // id of the demuxer that created the packet
+  int64_t demuxerId = -1; // id of the demuxer that created the packet
   int iGroupId = -1; // the group this data belongs to, used to group data from different streams together
 
   void *pSideData = nullptr;
