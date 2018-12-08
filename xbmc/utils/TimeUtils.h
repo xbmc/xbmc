d8a94b0454bc96fbad650b478aab1183eeab291c
enen92 <92enen@gmail.com>
2018-12-08 02:48:10 +0000

    [docs] cosmetic changes to timeutils header file

--- xbmc/utils/TimeUtils.h
+++ xbmc/utils/TimeUtils.h
@@ -20,8 +20,18 @@ int64_t CurrentHostFrequency(void);
 class CTimeUtils
 {
 public:
-  static void UpdateFrameTime(bool flip); ///< update the frame time.  Not threadsafe
-  static unsigned int GetFrameTime(); ///< returns the frame time in MS.  Not threadsafe
+  
+  /*!
+   * @brief Update the time frame
+   * @note Not threadsafe
+   */
+  static void UpdateFrameTime(bool flip);
+  
+  /*!
+   * @brief Returns the frame time in MS
+   * @note Not threadsafe
+   */
+  static unsigned int GetFrameTime();
   static CDateTime GetLocalTime(time_t time);
   
   /*!
