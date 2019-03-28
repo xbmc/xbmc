af8457a90f53c8057170b94c4f4e3f45dd9cb5c7
Memphiz <memphis@machzwo.de>
2019-03-28 20:58:49 +0100

    [gui] - add IMsgHandler interface

---
+++ xbmc/guilib/IMsgHandler.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (C) 2005-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "GUIMessage.h"
+
+class IMsgHandler
+{
+public:
+  virtual bool ProcessMsgHooks(CGUIMessage& message) = 0;
+  virtual ~IMsgHandler() = default;
+};
