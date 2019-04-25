48d4dd4074b456d1a44e18dd75420b37aa1fadf9
wsnipex <wsnipex@a1.net>
2019-04-25 15:52:15 +0200

    [cmake] make sure CXX11 is also always set for addons

---
+++ cmake/scripts/common/CompilerSettings.cmake
@@ -0,0 +1,6 @@
+# Languages and global compiler settings
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp")
+
