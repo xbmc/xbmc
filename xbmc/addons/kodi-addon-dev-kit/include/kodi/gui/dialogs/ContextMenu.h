446f057ae315a7ed175ae9e87936277368fd0cf7
Alwin Esch <alwin.esch@web.de>
2019-07-24 23:17:47 +0200

    [addons] fix binary addon compiler warnings (win32) on GUI function calls
    Where size_t to int.

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/gui/dialogs/ContextMenu.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/gui/dialogs/ContextMenu.h
@@ -69,7 +69,7 @@ namespace dialogs
     inline int Show(const std::string& heading, const std::vector<std::string>& entries)
     {
       using namespace ::kodi::addon;
-      unsigned int size = entries.size();
+      unsigned int size = static_cast<unsigned int>(entries.size());
       const char** cEntries = static_cast<const char**>(malloc(size*sizeof(const char**)));
       for (unsigned int i = 0; i < size; ++i)
       {
@@ -116,7 +116,7 @@ namespace dialogs
     inline int Show(const std::string& heading, const std::vector<std::pair<std::string, std::string>>& entries)
     {
       using namespace ::kodi::addon;
-      unsigned int size = entries.size();
+      unsigned int size = static_cast<unsigned int>(entries.size());
       const char** cEntries = static_cast<const char**>(malloc(size*sizeof(const char**)));
       for (unsigned int i = 0; i < size; ++i)
       {
@@ -163,7 +163,7 @@ namespace dialogs
     inline int Show(const std::string& heading, const std::vector<std::pair<int, std::string>>& entries)
     {
       using namespace ::kodi::addon;
-      unsigned int size = entries.size();
+      unsigned int size = static_cast<unsigned int>(entries.size());
       const char** cEntries = static_cast<const char**>(malloc(size*sizeof(const char**)));
       for (unsigned int i = 0; i < size; ++i)
       {
