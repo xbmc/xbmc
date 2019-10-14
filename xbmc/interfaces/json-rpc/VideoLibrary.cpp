d07ee3cc8c544b117320c68b50a31fcf67171932
Elan Ruusamäe <glen@pld-linux.org>
2019-10-14 18:26:19 +0300

    VideoLibrary: check boolean option values

--- xbmc/interfaces/json-rpc/VideoLibrary.cpp
+++ xbmc/interfaces/json-rpc/VideoLibrary.cpp
@@ -886,11 +886,11 @@ JSONRPC_STATUS CVideoLibrary::Export(const std::string &method, ITransportLayer 
   else
   {
     cmd = "exportlibrary2(video, separate, dummy";
-    if (parameterObject["options"].isMember("images"))
+    if (parameterObject["options"].asBoolean("images"))
       cmd += ", artwork";
-    if (parameterObject["options"].isMember("overwrite"))
+    if (parameterObject["options"].asBoolean("overwrite"))
       cmd += ", overwrite";
-    if (parameterObject["options"].isMember("actorthumbs"))
+    if (parameterObject["options"].asBoolean("actorthumbs"))
       cmd += ", actorthumbs";
     cmd += ")";
   }
