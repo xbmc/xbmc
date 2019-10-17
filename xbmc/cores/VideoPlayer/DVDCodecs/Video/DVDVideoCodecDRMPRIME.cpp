7831f12228e1911efcdbcce2d6c8ce65f1b78c57
Jonas Karlman <jonas@kwiboo.se>
2019-10-17 21:49:44 +0000

    DVDVideoCodecDRMPRIME: do not set DVP_FLAG_DROPPED
    Removing this fixed an issue observed while experimenting with VA-API,
    lets remove this for now.

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecDRMPRIME.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecDRMPRIME.cpp
@@ -281,7 +281,6 @@ void CDVDVideoCodecDRMPRIME::SetPictureParams(VideoPicture* pVideoPicture)
   pVideoPicture->iFlags = 0;
   pVideoPicture->iFlags |= m_pFrame->interlaced_frame ? DVP_FLAG_INTERLACED : 0;
   pVideoPicture->iFlags |= m_pFrame->top_field_first ? DVP_FLAG_TOP_FIELD_FIRST : 0;
-  pVideoPicture->iFlags |= m_pFrame->data[0] ? 0 : DVP_FLAG_DROPPED;
 
   int64_t pts = m_pFrame->pts;
   if (pts == AV_NOPTS_VALUE)
