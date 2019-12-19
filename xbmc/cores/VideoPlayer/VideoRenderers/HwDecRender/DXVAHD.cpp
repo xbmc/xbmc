412a3765642d63bed427e279b71aeeef44d0e1cc
thexai <58434170+thexai@users.noreply.github.com>
2019-12-19 01:12:19 +0100

    Added support for HLG (Hybrid Log-Gamma) and Rec.2020 for Live TV

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/DXVAHD.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/DXVAHD.cpp
@@ -369,6 +369,9 @@ DXGI_COLOR_SPACE_TYPE CProcessorHD::GetDXGIColorSpace(CRenderBuffer* view, bool 
       // DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_TOPLEFT_P2020
       return DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_LEFT_P2020;
 
+    if (view->color_transfer == AVCOL_TRC_ARIB_STD_B67) // HLG
+      return DXGI_COLOR_SPACE_YCBCR_STUDIO_GHLG_TOPLEFT_P2020;
+
     if (view->full_range)
       return DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P2020;
 
@@ -505,18 +508,26 @@ bool CProcessorHD::Render(CRect src, CRect dst, ID3D11Resource* target, CRenderB
   if (SUCCEEDED(m_pVideoContext.As(&videoCtx1)))
   {
     const DXGI_COLOR_SPACE_TYPE source_color = GetDXGIColorSpace(views[2], m_bSupportHDR10);
-    DXGI_COLOR_SPACE_TYPE target_color;
+    DXGI_COLOR_SPACE_TYPE target_color = DX::Windowing()->UseLimitedColor()
+                                             ? DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P709
+                                             : DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
 
-    if (DX::DeviceResources::Get()->Is10BitSwapchain() && CRendererBase::IsStreamHDR10(views[2]))
+    if (DX::DeviceResources::Get()->Is10BitSwapchain())
     {
-      target_color = DX::Windowing()->UseLimitedColor()
-                         ? DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020
-                         : DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020;
-    }
-    else
-    {
-      target_color = DX::Windowing()->UseLimitedColor() ? DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P709
-                                                        : DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
+      if ((views[2]->color_transfer == AVCOL_TRC_SMPTE2084 ||
+           views[2]->color_transfer == AVCOL_TRC_ARIB_STD_B67) &&
+          views[2]->primaries == AVCOL_PRI_BT2020)
+      {
+        target_color = DX::Windowing()->UseLimitedColor()
+                           ? DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020
+                           : DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020;
+      }
+      else if (views[2]->primaries == AVCOL_PRI_BT2020)
+      {
+        target_color = DX::Windowing()->UseLimitedColor()
+                           ? DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P2020
+                           : DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P2020;
+      }
     }
 
     videoCtx1->VideoProcessorSetStreamColorSpace1(m_pVideoProcessor.Get(), DEFAULT_STREAM_INDEX,
@@ -525,26 +536,23 @@ bool CProcessorHD::Render(CRect src, CRect dst, ID3D11Resource* target, CRenderB
     // makes target available for processing in shaders
     videoCtx1->VideoProcessorSetOutputShaderUsage(m_pVideoProcessor.Get(), 1);
 
-    if (m_bSupportHDR10 && (target_color == DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020 ||
-                            target_color == DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020))
+    if (m_bSupportHDR10 && views[2]->color_transfer == AVCOL_TRC_SMPTE2084 &&
+        views[2]->primaries == AVCOL_PRI_BT2020)
     {
-      if (CRendererBase::IsStreamHDR10(views[2]))
+      ComPtr<ID3D11VideoContext2> videoCtx2;
+      if (SUCCEEDED(m_pVideoContext.As(&videoCtx2)))
       {
-        ComPtr<ID3D11VideoContext2> videoCtx2;
-        if (SUCCEEDED(m_pVideoContext.As(&videoCtx2)))
-        {
-          // Passes stream SEI HDR metadata to VideoProcessor (refresh changes during playback)
-          DXGI_HDR_METADATA_HDR10 hdr10Stream = CRendererBase::GetDXGIHDR10MetaData(views[2]);
-          videoCtx2->VideoProcessorSetStreamHDRMetaData(
-              m_pVideoProcessor.Get(), DEFAULT_STREAM_INDEX, DXGI_HDR_METADATA_TYPE_HDR10,
-              sizeof(hdr10Stream), &hdr10Stream);
+        // Passes stream SEI HDR metadata to VideoProcessor (refresh changes during playback)
+        DXGI_HDR_METADATA_HDR10 hdr10Stream = CRendererBase::GetDXGIHDR10MetaData(views[2]);
+        videoCtx2->VideoProcessorSetStreamHDRMetaData(m_pVideoProcessor.Get(), DEFAULT_STREAM_INDEX,
+                                                      DXGI_HDR_METADATA_TYPE_HDR10,
+                                                      sizeof(hdr10Stream), &hdr10Stream);
 
-          // Passes Display HDR parameters (EDID) to VideoProcessor
-          DXGI_HDR_METADATA_HDR10 hdr10Display = DX::DeviceResources::Get()->GetHdr10Display();
-          videoCtx2->VideoProcessorSetOutputHDRMetaData(m_pVideoProcessor.Get(),
-                                                        DXGI_HDR_METADATA_TYPE_HDR10,
-                                                        sizeof(hdr10Display), &hdr10Display);
-        }
+        // Passes Display HDR parameters (EDID) to VideoProcessor
+        DXGI_HDR_METADATA_HDR10 hdr10Display = DX::DeviceResources::Get()->GetHdr10Display();
+        videoCtx2->VideoProcessorSetOutputHDRMetaData(m_pVideoProcessor.Get(),
+                                                      DXGI_HDR_METADATA_TYPE_HDR10,
+                                                      sizeof(hdr10Display), &hdr10Display);
       }
     }
   }
