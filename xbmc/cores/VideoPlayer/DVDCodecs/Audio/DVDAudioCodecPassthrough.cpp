c712930de281d8caaf94a1c8713b7106185642b2
Anssi Hannula <anssi@kodi.tv>
2019-12-26 02:20:47 +0200

    Fix cosmetic style issues introduced by TrueHD fix
    Commit 0e76bd058fbd8e3 ("Fix crashes due to missing TrueHD overrun
    checks") introduced several style issues (too long lines and missing
    whitespace).
    
    Fix those.

--- xbmc/cores/VideoPlayer/DVDCodecs/Audio/DVDAudioCodecPassthrough.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Audio/DVDAudioCodecPassthrough.cpp
@@ -177,10 +177,12 @@ bool CDVDAudioCodecPassthrough::AddData(const DemuxPacket &packet)
     if (!m_trueHDoffset)
       memset(m_trueHDBuffer.get(), 0, TRUEHD_BUF_SIZE);
 
-    if (m_dataSize > 2560-2)
+    if (m_dataSize > 2560 - 2)
     {
-      CLog::Log(LOGERROR, "CDVDAudioCodecPassthrough::AddData - truncating TrueHD frame of %u bytes", m_dataSize);
-      m_dataSize = 2560-2;
+      CLog::Log(LOGERROR,
+                "CDVDAudioCodecPassthrough::AddData - truncating TrueHD frame of %u bytes",
+                m_dataSize);
+      m_dataSize = 2560 - 2;
     }
     memcpy(&(m_trueHDBuffer.get())[m_trueHDoffset], m_buffer, m_dataSize);
     uint8_t highByte = (m_dataSize >> 8) & 0xFF;
