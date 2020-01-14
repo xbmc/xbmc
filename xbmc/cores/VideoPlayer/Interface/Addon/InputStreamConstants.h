7c30bc44b5d7f0db6c168c46b6cd496493a9b5e5
Alwin Esch <alwin.esch@web.de>
2020-01-14 01:41:05 +0100

    [videoplayer] cleanup Inputstream constants documentation
    To have in one line by so big text, makes it hard to read and looks a bit ugly.

--- xbmc/cores/VideoPlayer/Interface/Addon/InputStreamConstants.h
+++ xbmc/cores/VideoPlayer/Interface/Addon/InputStreamConstants.h
@@ -8,6 +8,23 @@
 
 #pragma once
 
-#define STREAM_PROPERTY_INPUTSTREAMCLASS  "inputstreamclass" /*!< @brief the name of the inputstream add-on that should be used by Kodi to play the stream denoted by STREAM_PROPERTY_STREAMURL. Leave blank to use Kodi's built-in playing capabilities or to allow ffmpeg to handle directly set to STREAM_PROPERTY_VALUE_INPUTSTREAMFFMPEG. */
-#define STREAM_PROPERTY_ISREALTIMESTREAM "isrealtimestream" /*!< @brief "true" to denote that the stream that should be played is a realtime stream. Any other value indicates that this is not a realtime stream.*/
-#define STREAM_PROPERTY_VALUE_INPUTSTREAMFFMPEG  "inputstream.ffmpeg" /*!< @brief special value for STREAM_PROPERTY_INPUTSTREAMCLASS to use ffmpeg to directly play a stream URL. */
+/*!
+ * @brief the name of the inputstream add-on that should be used by Kodi to
+ * play the stream denoted by STREAM_PROPERTY_STREAMURL. Leave blank to use
+ * Kodi's built-in playing capabilities or to allow ffmpeg to handle directly
+ * set to STREAM_PROPERTY_VALUE_INPUTSTREAMFFMPEG.
+ */
+#define STREAM_PROPERTY_INPUTSTREAMCLASS "inputstreamclass"
+
+/*!
+ * @brief "true" to denote that the stream that should be played is a
+ * realtime stream. Any other value indicates that this is not a realtime
+ * stream.
+ */
+#define STREAM_PROPERTY_ISREALTIMESTREAM "isrealtimestream"
+
+/*!
+ * @brief special value for STREAM_PROPERTY_INPUTSTREAMCLASS to use
+ * ffmpeg to directly play a stream URL.
+ */
+#define STREAM_PROPERTY_VALUE_INPUTSTREAMFFMPEG "inputstream.ffmpeg"
