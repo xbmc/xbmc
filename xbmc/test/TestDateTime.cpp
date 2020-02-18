b47b97e48347112c2d5361f9818ed53ce97dba06
Lukas Rusak <lorusak@gmail.com>
2020-02-17 22:40:56 -0800

    [test] fix TestDateTime to work with std::chrono

--- xbmc/test/TestDateTime.cpp
+++ xbmc/test/TestDateTime.cpp
@@ -14,6 +14,10 @@
 #include <array>
 #include <iostream>
 
+#define USE_OS_TZDB 0
+#define HAS_REMOTE_API 0
+#include <date/date.h>
+#include <date/tz.h>
 #include <gtest/gtest.h>
 
 class TestDateTime : public testing::Test
@@ -31,24 +35,6 @@ TEST_F(TestDateTime, DateTimeOperators)
   EXPECT_TRUE(dateTime1 < dateTime2);
   EXPECT_FALSE(dateTime1 > dateTime2);
   EXPECT_FALSE(dateTime1 == dateTime2);
-}
-
-TEST_F(TestDateTime, FileTimeOperators)
-{
-  CDateTime dateTime1(1991, 5, 14, 12, 34, 56);
-  CDateTime dateTime2(1991, 5, 14, 12, 34, 57);
-
-  KODI::TIME::FileTime fileTime1;
-  KODI::TIME::FileTime fileTime2;
-
-  dateTime1.GetAsTimeStamp(fileTime1);
-  dateTime2.GetAsTimeStamp(fileTime2);
-
-  CDateTime dateTime3(fileTime1);
-
-  EXPECT_TRUE(dateTime3 < fileTime2);
-  EXPECT_FALSE(dateTime3 > fileTime2);
-  EXPECT_FALSE(dateTime3 == fileTime2);
 }
 
 TEST_F(TestDateTime, SystemTimeOperators)
@@ -162,7 +148,7 @@ TEST_F(TestDateTime, MonthStringToMonthNum)
 }
 
 // this method is broken as SetFromDBDate() will return true
-TEST_F(TestDateTime, DISABLED_SetFromDateString)
+TEST_F(TestDateTime, SetFromDateString)
 {
   CDateTime dateTime;
   EXPECT_TRUE(dateTime.SetFromDateString("tuesday may 14, 1991"));
@@ -191,13 +177,7 @@ TEST_F(TestDateTime, SetFromDBDate)
   EXPECT_EQ(dateTime.GetDay(), 2);
 }
 
-// disabled on osx and freebsd as their mktime functions
-// don't work for dates before 1900
-#if defined(TARGET_DARWIN_OSX) || defined(TARGET_FREEBSD)
-TEST_F(TestDateTime, DISABLED_SetFromDBTime)
-#else
 TEST_F(TestDateTime, SetFromDBTime)
-#endif
 {
   CDateTime dateTime1;
   EXPECT_TRUE(dateTime1.SetFromDBTime("12:34"));
@@ -238,10 +218,8 @@ TEST_F(TestDateTime, SetFromW3CDate)
 
 TEST_F(TestDateTime, SetFromW3CDateTime)
 {
-  CDateTimeSpan bias = CDateTime::GetTimezoneBias();
   CDateTime dateTime;
   dateTime.SetFromDBDateTime("1994-11-05 13:15:30");
-  dateTime += bias;
   std::string dateTimeStr = dateTime.GetAsDBDate() + "T" + dateTime.GetAsDBTime() + "Z";
 
   CDateTime dateTime1;
@@ -265,11 +243,8 @@ TEST_F(TestDateTime, SetFromW3CDateTime)
 
 TEST_F(TestDateTime, SetFromUTCDateTime)
 {
-  CDateTimeSpan bias = CDateTime::GetTimezoneBias();
-
   CDateTime dateTime1;
   dateTime1.SetFromDBDateTime("1991-05-14 12:34:56");
-  dateTime1 += bias;
 
   CDateTime dateTime2;
   EXPECT_TRUE(dateTime2.SetFromUTCDateTime(dateTime1));
@@ -280,7 +255,7 @@ TEST_F(TestDateTime, SetFromUTCDateTime)
   EXPECT_EQ(dateTime2.GetMinute(), 34);
   EXPECT_EQ(dateTime2.GetSecond(), 56);
 
-  const time_t time = 674224496 + bias.GetSecondsTotal();
+  const time_t time = 674224496;
 
   CDateTime dateTime3;
   EXPECT_TRUE(dateTime3.SetFromUTCDateTime(time));
@@ -326,18 +301,14 @@ TEST_F(TestDateTime, SetDateTime)
   EXPECT_EQ(dateTime2.GetMinute(), 0);
   EXPECT_EQ(dateTime2.GetSecond(), 0);
 
-// disabled on osx and freebsd as their mktime functions
-// don't work for dates before 1900
-#if !defined(TARGET_DARWIN_OSX) && !defined(TARGET_FREEBSD)
   CDateTime dateTime3;
   EXPECT_TRUE(dateTime3.SetTime(12, 34, 56));
-  EXPECT_EQ(dateTime3.GetYear(), 1601);
+  EXPECT_EQ(dateTime3.GetYear(), 1970);
   EXPECT_EQ(dateTime3.GetMonth(), 1);
   EXPECT_EQ(dateTime3.GetDay(), 1);
   EXPECT_EQ(dateTime3.GetHour(), 12);
   EXPECT_EQ(dateTime3.GetMinute(), 34);
   EXPECT_EQ(dateTime3.GetSecond(), 56);
-#endif
 }
 
 TEST_F(TestDateTime, GetAsStrings)
@@ -352,22 +323,20 @@ TEST_F(TestDateTime, GetAsStrings)
   EXPECT_EQ(dateTime.GetAsW3CDate(), "1991-05-14");
 }
 
-// disabled because we have no way to validate these values
-// GetTimezoneBias() always returns a positive value so
-// there is no way to detect the direction of the offset
-TEST_F(TestDateTime, DISABLED_GetAsStringsWithBias)
+TEST_F(TestDateTime, GetAsStringsWithBias)
 {
-  CDateTimeSpan bias = CDateTime::GetTimezoneBias();
-
   CDateTime dateTime;
   dateTime.SetDateTime(1991, 05, 14, 12, 34, 56);
 
-  CDateTime dateTimeWithBias(dateTime);
-  dateTimeWithBias += bias;
+  std::cout << dateTime.GetAsRFC1123DateTime() << std::endl;
+  std::cout << dateTime.GetAsW3CDateTime(false) << std::endl;
+  std::cout << dateTime.GetAsW3CDateTime(true) << std::endl;
 
-  EXPECT_EQ(dateTime.GetAsRFC1123DateTime(), "Tue, 14 May 1991 20:34:56 GMT");
-  EXPECT_EQ(dateTime.GetAsW3CDateTime(false), "1991-05-14T12:34:56+08:00");
-  EXPECT_EQ(dateTime.GetAsW3CDateTime(true), "1991-05-14T20:34:56Z");
+  auto zone = date::make_zoned(date::current_zone(), dateTime.GetAsTimePoint());
+
+  EXPECT_EQ(dateTime.GetAsRFC1123DateTime(), "Tue, 14 May 1991 12:34:56 GMT");
+  EXPECT_EQ(dateTime.GetAsW3CDateTime(false), "1991-05-14T05:34:56" + date::format("%Ez", zone));
+  EXPECT_EQ(dateTime.GetAsW3CDateTime(true), "1991-05-14T12:34:56Z");
 }
 
 TEST_F(TestDateTime, GetAsLocalized)
@@ -663,31 +632,13 @@ TEST_F(TestDateTime, GetAsTm)
   }
 }
 
-// Disabled pending std::chrono and std::date changes.
-TEST_F(TestDateTime, DISABLED_GetAsTimeStamp)
-{
-  CDateTimeSpan bias = CDateTime::GetTimezoneBias();
-
-  CDateTime dateTime;
-  dateTime.SetDateTime(1991, 05, 14, 12, 34, 56);
-
-  KODI::TIME::FileTime fileTime;
-  dateTime.GetAsTimeStamp(fileTime);
-  dateTime += bias;
-
-  EXPECT_TRUE(dateTime == fileTime);
-}
-
 TEST_F(TestDateTime, GetAsUTCDateTime)
 {
-  CDateTimeSpan bias = CDateTime::GetTimezoneBias();
-
   CDateTime dateTime1;
   dateTime1.SetDateTime(1991, 05, 14, 12, 34, 56);
 
   CDateTime dateTime2;
   dateTime2 = dateTime1.GetAsUTCDateTime();
-  dateTime2 -= bias;
 
   EXPECT_EQ(dateTime2.GetYear(), 1991);
   EXPECT_EQ(dateTime2.GetMonth(), 5);
@@ -697,20 +648,14 @@ TEST_F(TestDateTime, GetAsUTCDateTime)
   EXPECT_EQ(dateTime2.GetSecond(), 56);
 }
 
-// disabled on osx and freebsd as their mktime functions
-// don't work for dates before 1900
-#if defined(TARGET_DARWIN_OSX) || defined(TARGET_FREEBSD)
-TEST_F(TestDateTime, DISABLED_Reset)
-#else
 TEST_F(TestDateTime, Reset)
-#endif
 {
   CDateTime dateTime;
   dateTime.SetDateTime(1991, 05, 14, 12, 34, 56);
 
   dateTime.Reset();
 
-  EXPECT_EQ(dateTime.GetYear(), 1601);
+  EXPECT_EQ(dateTime.GetYear(), 1970);
   EXPECT_EQ(dateTime.GetMonth(), 1);
   EXPECT_EQ(dateTime.GetDay(), 1);
   EXPECT_EQ(dateTime.GetHour(), 0);
