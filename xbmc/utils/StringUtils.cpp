36f3af62b8e861e5130896065eb93a5298e0975f
Lukas Rusak <lorusak@gmail.com>
2020-03-06 15:12:49 -0800

    StringUtils: allow CompareNoCase to compare first n bytes of s1

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -408,21 +408,24 @@ bool StringUtils::EqualsNoCase(const char *s1, const char *s2)
   return true;
 }
 
-int StringUtils::CompareNoCase(const std::string &str1, const std::string &str2)
+int StringUtils::CompareNoCase(const std::string& str1, const std::string& str2, size_t n /* = 0 */)
 {
-  return CompareNoCase(str1.c_str(), str2.c_str());
+  return CompareNoCase(str1.c_str(), str2.c_str(), n);
 }
 
-int StringUtils::CompareNoCase(const char *s1, const char *s2)
+int StringUtils::CompareNoCase(const char* s1, const char* s2, size_t n /* = 0 */)
 {
   char c2; // we need only one char outside the loop
+  size_t index = 0;
   do
   {
     const char c1 = *s1++; // const local variable should help compiler to optimize
     c2 = *s2++;
+    index++;
     if (c1 != c2 && ::tolower(c1) != ::tolower(c2)) // This includes the possibility that one of the characters is the null-terminator, which implies a string mismatch.
       return ::tolower(c1) - ::tolower(c2);
-  } while (c2 != '\0'); // At this point, we know c1 == c2, so there's no need to test them both.
+  } while (c2 != '\0' &&
+           index != n); // At this point, we know c1 == c2, so there's no need to test them both.
   return 0;
 }
 
