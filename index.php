1ae967663f61ca7501f20974cd560c2715fcb121
I-Cat <58572646+I-Cat@users.noreply.github.com>
2020-03-26 21:03:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <!DOCTYPE html > <head> <title> My Hit counter Test</title> </head> <body bgcolor = "#99CC99" > <h1> this is my Test </h1> </body> </html>
+<?php
+$hits = intval(file_get_contents('counter.txt'));
+file_put_contents('counter.txt',$hits+1);
+echo $hits;
+?>
