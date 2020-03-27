12da9d143ce9871a5e790befca919e4cc4fca625
Alwin Esch <alwin.esch@web.de>
2020-03-27 23:53:20 +0100

    [addons][filesystem] "C" ABI cleanups
    This add a 'extern "C"' and the #ifdef to separate between C and C++.
    In API change this nothing, only done to confirm correct "C" and allow
    "C" only in future.

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/Network.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/Network.h
@@ -10,24 +10,32 @@
 
 #include "AddonBase.h"
 
-/*
- * For interface between add-on and kodi.
- *
- * This structure defines the addresses of functions stored inside Kodi which
- * are then available for the add-on to call
- *
- * All function pointers there are used by the C++ interface functions below.
- * You find the set of them on xbmc/addons/interfaces/General.cpp
- *
- * Note: For add-on development itself this is not needed
- */
-typedef struct AddonToKodiFuncTable_kodi_network
+#ifdef __cplusplus
+extern "C"
 {
-  bool (*wake_on_lan)(void* kodiBase, const char *mac);
-  char* (*get_ip_address)(void* kodiBase);
-  char* (*dns_lookup)(void* kodiBase, const char* url, bool* ret);
-  char* (*url_encode)(void* kodiBase, const char* url);
-} AddonToKodiFuncTable_kodi_network;
+#endif /* __cplusplus */
+
+  /*
+   * For interface between add-on and kodi.
+   *
+   * This structure defines the addresses of functions stored inside Kodi which
+   * are then available for the add-on to call
+   *
+   * All function pointers there are used by the C++ interface functions below.
+   * You find the set of them on xbmc/addons/interfaces/General.cpp
+   *
+   * Note: For add-on development itself this is not needed
+   */
+  typedef struct AddonToKodiFuncTable_kodi_network
+  {
+    bool (*wake_on_lan)(KODI_HANDLE kodiBase, const char *mac);
+    char* (*get_ip_address)(KODI_HANDLE kodiBase);
+    char* (*dns_lookup)(KODI_HANDLE kodiBase, const char* url, bool* ret);
+    char* (*url_encode)(KODI_HANDLE kodiBase, const char* url);
+  } AddonToKodiFuncTable_kodi_network;
+
+#ifdef __cplusplus
+} /* extern "C" */
 
 //==============================================================================
 ///
@@ -174,3 +182,5 @@ namespace network
 
 } /* namespace network */
 } /* namespace kodi */
+
+#endif /* __cplusplus */
