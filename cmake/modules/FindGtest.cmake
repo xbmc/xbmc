8708bce6562f9ad75a7171ba10e42a314549b367
Rechi <Rechi@users.noreply.github.com>
2020-04-17 08:55:13 +0100

    [cmake] disable gtest auto fallback to internal build

--- cmake/modules/FindGtest.cmake
+++ cmake/modules/FindGtest.cmake
@@ -9,23 +9,6 @@
 # GTEST_INCLUDE_DIRS - the gtest include directories
 # GTEST_LIBRARIES - the gtest libraries
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_GTEST gtest>=1.10.0 QUIET)
-endif()
-
-find_library(GTEST_LIBRARY NAMES gtest
-                           PATHS ${PC_GTEST_LIBDIR})
-
-find_path(GTEST_INCLUDE_DIR NAMES gtest/gtest.h
-                            PATHS ${PC_GTEST_INCLUDEDIR})
-
-set(GTEST_VERSION ${PC_GTEST_VERSION})
-
-if (NOT GTEST_LIBRARY AND NOT GTEST_INCLUDE_DIR AND NOT GTEST_VERSION)
-  set(ENABLE_INTERNAL_GTEST ON)
-  message(STATUS "libgtest not found, falling back to internal build")
-endif()
-
 if(ENABLE_INTERNAL_GTEST)
   include(ExternalProject)
 
@@ -54,15 +37,37 @@ if(ENABLE_INTERNAL_GTEST)
                       DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/download
                       PREFIX ${CORE_BUILD_DIR}/gtest
                       INSTALL_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}
-                      CMAKE_ARGS -DBUILD_GMOCK=OFF -DINSTALL_GTEST=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_INSTALL_LIBDIR=lib
+                      CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DBUILD_GMOCK=OFF -DINSTALL_GTEST=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_INSTALL_LIBDIR=lib
                       BUILD_BYPRODUCTS ${GTEST_LIBRARY})
 
-  set_target_properties(gtest PROPERTIES FOLDER "External Projects")
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Gtest
+                                    REQUIRED_VARS GTEST_LIBRARY GTEST_INCLUDE_DIR
+                                    VERSION_VAR GTEST_VERSION)
+
+  set(GTEST_LIBRARIES ${GTEST_LIBRARY})
+  set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR})
+else()
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_GTEST gtest>=1.10.0 QUIET)
+  endif()
+
+  find_library(GTEST_LIBRARY NAMES gtest
+                             PATHS ${PC_GTEST_LIBDIR})
+
+  find_path(GTEST_INCLUDE_DIR NAMES gtest/gtest.h
+                              PATHS ${PC_GTEST_INCLUDEDIR})
+
+  set(GTEST_VERSION ${PC_GTEST_VERSION})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Gtest
+                                    REQUIRED_VARS GTEST_LIBRARY GTEST_INCLUDE_DIR
+                                    VERSION_VAR GTEST_VERSION)
+
+  if(GTEST_FOUND)
+    set(GTEST_LIBRARIES ${GTEST_LIBRARY})
+    set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR})
+  endif()
+  mark_as_advanced(GTEST_INCLUDE_DIR GTEST_LIBRARY)
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Gtest
-                                  REQUIRED_VARS GTEST_LIBRARY GTEST_INCLUDE_DIR
-                                  VERSION_VAR GTEST_VERSION)
-
-mark_as_advanced(GTEST_INCLUDE_DIR GTEST_LIBRARY)
