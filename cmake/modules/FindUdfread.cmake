cfaebd834f0a85beec64e47a0f0e28b342dfb7c2
Lukas Rusak <lorusak@gmail.com>
2020-04-30 17:17:57 -0700

    [cmake] udfread allow building with cmake depends

--- cmake/modules/FindUdfread.cmake
+++ cmake/modules/FindUdfread.cmake
@@ -23,43 +23,14 @@ find_library(UDFREAD_LIBRARY NAMES udfread libudfread
 set(UDFREAD_VERSION ${PC_UDFREAD_VERSION})
 
 if(ENABLE_INTERNAL_UDFREAD)
-  include(ExternalProject)
+  include(${WITH_KODI_DEPENDS}/packages/libudfread/package.cmake)
+  set(UDFREAD_VERSION ${PKG_VERSION})
+  add_depends_for_targets("HOST")
 
-  # Extract version
-  file(STRINGS ${CMAKE_SOURCE_DIR}/tools/depends/target/libudfread/UDFREAD-VERSION VER)
+  add_custom_target(udfread ALL DEPENDS libudfread-host)
 
-  string(REGEX MATCH "VERSION=[^ ]*$.*" UDFREAD_VER "${VER}")
-  list(GET UDFREAD_VER 0 UDFREAD_VER)
-  string(SUBSTRING "${UDFREAD_VER}" 8 -1 UDFREAD_VER)
-
-  # allow user to override the download URL with a local tarball
-  # needed for offline build envs
-  if(UDFREAD_URL)
-    get_filename_component(UDFREAD_URL "${UDFREAD_URL}" ABSOLUTE)
-  else()
-    set(UDFREAD_URL http://mirrors.kodi.tv/build-deps/sources/libudfread-${UDFREAD_VER}.tar.gz)
-  endif()
-
-  if(VERBOSE)
-    message(STATUS "UDFREAD_URL: ${UDFREAD_URL}")
-  endif()
-
-  set(UDFREAD_LIBRARY ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/lib/libudfread.a)
-  set(UDFREAD_INCLUDE_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/include)
-  set(UDFREAD_VERSION ${UDFREAD_VER})
-
-  externalproject_add(udfread
-                      URL ${UDFREAD_URL}
-                      DOWNLOAD_NAME libudfread-${UDFREAD_VER}.tar.gz
-                      DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}/download
-                      PREFIX ${CORE_BUILD_DIR}/libudfread
-                      CONFIGURE_COMMAND autoreconf -vif &&
-                                        ./configure
-                                        --enable-static
-                                        --disable-shared
-                                        --prefix=${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}
-                      BUILD_BYPRODUCTS ${UDFREAD_LIBRARY}
-                      BUILD_IN_SOURCE 1)
+  set(UDFREAD_LIBRARY ${INSTALL_PREFIX_HOST}/lib/libudfread.a)
+  set(UDFREAD_INCLUDE_DIR ${INSTALL_PREFIX_HOST}/include)
 
   set_target_properties(udfread PROPERTIES FOLDER "External Projects")
 endif()
