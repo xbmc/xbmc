8f7052edc6ab7a7ce90d516f54aa0db607620aa8
Alwin Esch <alwin.esch@web.de>
2020-05-09 01:07:24 +0200

    [addons][filesystem] separate "C" and "C++" parts of headers
    By them becomes the "C" part placed in own header to see better what language
    is where and to make easier for other languages.

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/versions.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/versions.h
@@ -59,6 +59,7 @@
 #define ADDON_GLOBAL_VERSION_FILESYSTEM_MIN           "1.0.3"
 #define ADDON_GLOBAL_VERSION_FILESYSTEM_XML_ID        "kodi.binary.global.filesystem"
 #define ADDON_GLOBAL_VERSION_FILESYSTEM_DEPENDS       "Filesystem.h" \
+                                                      "c-api/filesystem.h" \
                                                       "gui/gl/Shader.h"
 
 #define ADDON_GLOBAL_VERSION_NETWORK                  "1.0.1"
