8c98f4650be7fc943dadc508bd1fe3c11991ee72
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2020-05-11 12:20:23 +0800

    Include CMake Scripts for ICUNDK

---
+++ cmake/modules/FindICUNDK.cmake
@@ -0,0 +1,19 @@
+# FindICUNDK
+# --------
+# Finds the ICUNDK library
+#
+# This will define the following variables::
+#
+# ICUNDK_FOUND - system has ICUNDK
+# ICUNDK_INCLUDE_DIRS - the UCINDK include directory
+# ICUNDK_LIBRARIES - the ICUNDK libraries
+
+find_path(ICUNDK_INCLUDE_DIR NAMES unicode/utypes.h)
+find_library(ICUNDK_LIBRARY NAMES libicundk.a)
+find_package_handle_standard_args(ICUNDK
+                                  REQUIRED_VARS ICUNDK_INCLUDE_DIR ICUNDK_LIBRARY)
+
+if (ICUNDK_FOUND)
+  set(ICUNDK_INCLUDE_DIRS "${ICUNDK_INCLUDE_DIR}")
+  set(ICUNDK_LIBRARIES "${ICUNDK_LIBRARY}")
+endif()
