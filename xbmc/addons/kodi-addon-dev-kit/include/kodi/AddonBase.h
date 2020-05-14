5a09c65ae003fcf168b18fea3d0832758b107cd6
Alwin Esch <alwin.esch@web.de>
2020-05-14 13:49:31 +0200

    [addons][base] move all "C" define's from AddonBase.h to addon_base.h
    This moves it from C++ header to new added right "C" header.

--- xbmc/addons/kodi-addon-dev-kit/include/kodi/AddonBase.h
+++ xbmc/addons/kodi-addon-dev-kit/include/kodi/AddonBase.h
@@ -20,48 +20,6 @@
 #include <string>
 #include <vector>
 
-#ifndef TARGET_WINDOWS
-#ifndef __cdecl
-#define __cdecl
-#endif
-#ifndef __declspec
-#define __declspec(X)
-#endif
-#endif
-
-#undef ATTRIBUTE_PACKED
-#undef PRAGMA_PACK_BEGIN
-#undef PRAGMA_PACK_END
-
-#if defined(__GNUC__)
-  #define ATTRIBUTE_PACKED __attribute__ ((packed))
-  #define PRAGMA_PACK 0
-  #define ATTRIBUTE_HIDDEN __attribute__ ((visibility ("hidden")))
-#endif
-
-#if !defined(ATTRIBUTE_PACKED)
-  #define ATTRIBUTE_PACKED
-  #define PRAGMA_PACK 1
-#endif
-
-#if !defined(ATTRIBUTE_HIDDEN)
-  #define ATTRIBUTE_HIDDEN
-#endif
-
-#ifdef _MSC_VER
-    #define ATTRIBUTE_FORCEINLINE __forceinline
-#elif defined(__GNUC__)
-    #define ATTRIBUTE_FORCEINLINE inline __attribute__((__always_inline__))
-#elif defined(__CLANG__)
-    #if __has_attribute(__always_inline__)
-        #define ATTRIBUTE_FORCEINLINE inline __attribute__((__always_inline__))
-    #else
-        #define ATTRIBUTE_FORCEINLINE inline
-    #endif
-#else
-    #define ATTRIBUTE_FORCEINLINE inline
-#endif
-
 #include "versions.h"
 
 namespace kodi { namespace addon { class CAddonBase; }}
