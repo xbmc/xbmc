acae4ab7fd0a8fa939f5e034b6e17dc3e25a529e
howie-f <rftc@gmx.de>
2020-06-29 21:04:27 +0200

    [addons] deny addon-updates from private repos if they exist in official repos

--- xbmc/CompileInfo.h
+++ xbmc/CompileInfo.h
@@ -18,8 +18,9 @@ public:
   static const char* GetPackage();
   static const char* GetClass();
   static const char* GetAppName();
-  static const char *GetSuffix();  // Git "Tag", e.g. alpha1
-  static const char* GetSCMID();   // Git Revision
+  static const char* GetSuffix(); // Git "Tag", e.g. alpha1
+  static const char* GetSCMID(); // Git Revision
+  static std::string GetOfficialAddonRepos();
   static const char* GetCopyrightYears();
   static std::string GetBuildDate();
   static const char* GetVersionCode();
