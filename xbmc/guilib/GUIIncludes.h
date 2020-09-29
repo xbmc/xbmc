767c0f26e0bffc4ca6a7360697b95e6c201a3b9a
fuzzard <fuzzard@kodi.tv>
2020-09-29 19:23:18 +1000

    [guilib/guiincludes] fix warning - func arg order different
    Function 'InsertNested' argument order different: declaration 'controls, node, include' definition 'controls, include, node'

--- xbmc/guilib/GUIIncludes.h
+++ xbmc/guilib/GUIIncludes.h
@@ -110,7 +110,7 @@ private:
   void ResolveExpressions(TiXmlElement *node);
 
   typedef std::map<std::string, std::string> Params;
-  static void InsertNested(TiXmlElement *controls, TiXmlElement *node, TiXmlElement *include);
+  static void InsertNested(TiXmlElement* controls, TiXmlElement* include, TiXmlElement* node);
   static bool GetParameters(const TiXmlElement *include, const char *valueAttribute, Params& params);
   static void ResolveParametersForNode(TiXmlElement *node, const Params& params);
   static ResolveParamsResult ResolveParameters(const std::string& strInput, std::string& strOutput, const Params& params);
