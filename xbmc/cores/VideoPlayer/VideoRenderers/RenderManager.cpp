129b5c6bb0bf9f0f4b30c1ce7706f5b227649126
Lukas Rusak <lorusak@gmail.com>
2020-09-28 17:06:08 -0700

    VideoPlayer: CRenderManager: improve logging of renderer being used

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -232,7 +232,7 @@ bool CRenderManager::Configure()
 
     m_renderState = STATE_CONFIGURED;
 
-    CLog::Log(LOGDEBUG, "CRenderManager::Configure - {}", m_QueueSize);
+    CLog::Log(LOGDEBUG, "CRenderManager::{} - queue size: {}", __FUNCTION__, m_QueueSize);
   }
   else
     m_renderState = STATE_UNCONFIGURED;
@@ -483,10 +483,12 @@ void CRenderManager::CreateRenderer()
       m_pRenderer = VIDEOPLAYER::CRendererFactory::CreateRenderer(id, buffer);
       if (m_pRenderer)
       {
+        CLog::Log(LOGDEBUG, "CRenderManager::{} - using renderer: {}", __FUNCTION__, id);
         return;
       }
     }
     m_pRenderer = VIDEOPLAYER::CRendererFactory::CreateRenderer("default", buffer);
+    CLog::Log(LOGDEBUG, "CRenderManager::{} - using default renderer", __FUNCTION__);
   }
 }
 
@@ -494,7 +496,7 @@ void CRenderManager::DeleteRenderer()
 {
   if (m_pRenderer)
   {
-    CLog::Log(LOGDEBUG, "{} - deleting renderer", __FUNCTION__);
+    CLog::Log(LOGDEBUG, "CRenderManager::{} - deleting renderer", __FUNCTION__);
 
     delete m_pRenderer;
     m_pRenderer = NULL;
