161b685077acb8f1e43057542ec23d6dcdcf8913
fuzzard <fuzzard@kodi.tv>
2020-10-02 07:15:38 +1000

    [toolchain] set cxx std only for core
    Dont propagate cxx std to binary addon toolchains

--- cmake/scripts/common/CompilerSettings.cmake
+++ cmake/scripts/common/CompilerSettings.cmake
@@ -1,5 +1,4 @@
 # Languages and global compiler settings
-set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp")
