2b0753e10408c1a2b2607c40b3f1d46f0bcc33dd
Rechi <Rechi@users.noreply.github.com>
2020-10-05 01:48:47 +0200

    [cmake] add cppcheck support

---
+++ cmake/modules/FindCppcheck.cmake
@@ -0,0 +1,32 @@
+#.rst:
+# FindCppcheck
+# ------------
+# Finds cppcheck and sets it up to run along with the compiler for C and CXX.
+
+find_program(CPPCHECK_EXECUTABLE cppcheck)
+
+if(CPPCHECK_EXECUTABLE)
+  execute_process(COMMAND "${CPPCHECK_EXECUTABLE}" --version
+                  OUTPUT_VARIABLE CPPCHECK_VERSION
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REGEX REPLACE "Cppcheck (.*)" "\\1" CPPCHECK_VERSION "${CPPCHECK_VERSION}")
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Cppcheck REQUIRED_VARS CPPCHECK_EXECUTABLE
+                                  VERSION_VAR CPPCHECK_VERSION)
+
+if(CPPCHECK_FOUND)
+  # CMake < 3.16 treats Objective-C (OBJC) files as C files and Objective-C++ (OBJCXX) files as C++ files,
+  # but cppcheck doesn't support Objective-C and Objective-C++.
+  # CMake >= 3.16 added support for Objective-C and Objective-C++ language,
+  # but doesn't support OBJC and OBJCXX for <LANG>_CLANG_TIDY.
+  file(WRITE "${CMAKE_BINARY_DIR}/cppcheck" "case \"$@\" in *.m|*.mm) exit 0; esac\nexec \"${CPPCHECK_EXECUTABLE}\" --quiet --relative-paths=\"${CMAKE_SOURCE_DIR}\" \"$@\"\n")
+  execute_process(COMMAND chmod +x "${CMAKE_BINARY_DIR}/cppcheck")
+
+  # Supports Unix Makefiles and Ninja
+  set(CMAKE_C_CPPCHECK "${CMAKE_BINARY_DIR}/cppcheck" PARENT_SCOPE)
+  set(CMAKE_CXX_CPPCHECK "${CMAKE_BINARY_DIR}/cppcheck" PARENT_SCOPE)
+endif()
+
+mark_as_advanced(CPPCHECK_EXECUTABLE)
