4a531e692a436e03cea18809970bbd2dfb82f6e2
Alwin Esch <alwin.esch@web.de>
2020-10-18 15:06:43 +0200

    [addons][inputstream] include InputStreamConstants.h inside addon headers dir
    This add the header to the kodi-dev-kit folder to have them all
    together and prepared for a independent dev kit, further to confirm
    more the "C" use between Kodi and addon.

--- xbmc/addons/kodi-dev-kit/include/kodi/addon-instance/Inputstream.h
+++ xbmc/addons/kodi-dev-kit/include/kodi/addon-instance/Inputstream.h
@@ -11,6 +11,7 @@
 #include "../AddonBase.h"
 #include "../StreamCrypto.h"
 #include "../c-api/addon-instance/inputstream.h"
+#include "inputstream/StreamConstants.h"
 #include "inputstream/TimingConstants.h"
 
 #ifdef __cplusplus
