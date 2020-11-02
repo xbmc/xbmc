bdf022fc7b08d0beaa43e8781cc9a2abaaea76e0
freakynl <freaky@bananateam.nl>
2020-11-02 23:33:27 +0100

    Got some remarks that it should go below 0.5 so set the lower limit to 0.5

--- xbmc/settings/AdvancedSettings.cpp
+++ xbmc/settings/AdvancedSettings.cpp
@@ -700,7 +700,7 @@ void CAdvancedSettings::ParseSettingsFile(const std::string &file)
 
     //0 = disable fps detect, 1 = only detect on timestamps with uniform spacing, 2 detect on all timestamps
     XMLUtils::GetInt(pElement, "fpsdetect", m_videoFpsDetect, 0, 2);
-    XMLUtils::GetFloat(pElement, "mintempo", m_minTempo, 0.3, 0.9);
+    XMLUtils::GetFloat(pElement, "mintempo", m_minTempo, 0.5, 0.9);
     XMLUtils::GetFloat(pElement, "maxtempo", m_maxTempo, 1.5, 2.1);
     XMLUtils::GetBoolean(pElement, "preferstereostream", m_videoPreferStereoStream);
 
