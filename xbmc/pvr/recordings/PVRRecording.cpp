2b0c81f7c305b744d8d6da91142126b2195dbeb9
Vasyl Gello <vasek.gello@gmail.com>
2020-11-03 22:49:09 +0200

    Compensate the representation change
    ... from local time by default to UTC time by default.
    
    Namely, 'CDateTime::SetFromUTCDateTime' converted the UTC time
    to local time in the implementation of CDateTime before
    'std::chrono'. Now this conversion has gone, and to avoid
    displaying PVR times in UTC, 'GetAsLocalDateTime' is added
    where necessary.
    
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

--- xbmc/pvr/recordings/PVRRecording.cpp
+++ xbmc/pvr/recordings/PVRRecording.cpp
@@ -527,7 +527,7 @@ void CPVRRecording::UpdatePath()
 const CDateTime& CPVRRecording::RecordingTimeAsLocalTime() const
 {
   static CDateTime tmp;
-  tmp.SetFromUTCDateTime(m_recordingTime);
+  tmp.SetFromUTCDateTime(m_recordingTime.GetAsLocalDateTime());
 
   return tmp;
 }
@@ -541,7 +541,7 @@ CDateTime CPVRRecording::EndTimeAsUTC() const
 CDateTime CPVRRecording::EndTimeAsLocalTime() const
 {
   CDateTime ret;
-  ret.SetFromUTCDateTime(EndTimeAsUTC());
+  ret.SetFromUTCDateTime(EndTimeAsUTC().GetAsLocalDateTime());
   return ret;
 }
 
