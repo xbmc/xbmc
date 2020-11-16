2d4618cb53871b41ec3911563bac27a3a5710003
Vasyl Gello <vasek.gello@gmail.com>
2020-11-16 13:58:31 +0200

    [EventClients] Fix examples and libs
     * Provide correct icon paths and types depending on file existence
     * Fix C++ and Java EventClient examples

--- tools/EventClients/examples/java/XBMCDemoClient1.java
+++ tools/EventClients/examples/java/XBMCDemoClient1.java
@@ -1,5 +1,6 @@
 package org.xbmc.eventclient.demo;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
@@ -18,10 +19,27 @@ public class XBMCDemoClient1 {
 	 * @param args
 	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
-		InetAddress host = Inet4Address.getByAddress(new byte[] { (byte)192, (byte)168, 0, 20 } );
+		InetAddress host = Inet4Address.getByAddress(new byte[] { (byte)127, 0, 0, 1 } );
 
 		Thread.sleep(20000);
-		XBMCClient oXBMCClient = new XBMCClient(host, 9777, "My Client", "/usr/share/xbmc/media/icon.png");
+
+		String iconFile = "/usr/share/xbmc/media/icon.png";
+
+		if (! new File(iconFile).exists()) {
+			iconFile = "../../icons/icon.png";
+
+			if (! new File(iconFile).exists()) {
+				iconFile = "";
+			}
+		}
+
+		XBMCClient oXBMCClient = null;
+
+		if (iconFile != "") {
+			oXBMCClient = new XBMCClient(host, 9777, "My Client", iconFile);
+		} else {
+			oXBMCClient = new XBMCClient(host, 9777, "My Client");
+		}
 
 		Thread.sleep(7000);
 
