780239a41ad078416f6677446a7e0779076db821
pkubaj <pkubaj@users.noreply.github.com>
2020-12-15 22:01:52 +0000

    Fix build with clang
    clang also defines __GNUC__ and can't build this code.

--- xbmc/utils/EndianSwap.h
+++ xbmc/utils/EndianSwap.h
@@ -23,7 +23,7 @@
 extern "C" {
 #endif
 
-#if defined(__GNUC__) && (defined(__powerpc__) || defined(__ppc__))
+#if defined(__GNUC__) && (defined(__powerpc__) || defined(__ppc__)) && !defined(__clang__)
 static __inline__ uint16_t Endian_Swap16(uint16_t x)
 {
         uint16_t result;
