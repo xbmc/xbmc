f3712d3088ff0a5a8cbcb8951ee0c05d8885bbc3
Matthias Reichl <hias@horus.com>
2020-12-18 22:11:01 +0100

    windowing/gbm: add null checks before accessing settings
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- xbmc/windowing/gbm/WinSystemGbm.cpp
+++ xbmc/windowing/gbm/WinSystemGbm.cpp
@@ -77,14 +77,21 @@ bool CWinSystemGbm::InitWindowSystem()
     return false;
   }
 
-  CServiceBroker::GetSettingsComponent()
-      ->GetSettings()
-      ->GetSetting(CSettings::SETTING_VIDEOSCREEN_LIMITEDRANGE)
-      ->SetVisible(true);
-  CServiceBroker::GetSettingsComponent()
-      ->GetSettings()
-      ->GetSetting("videoscreen.limitguisize")
-      ->SetVisible(true);
+  auto settingsComponent = CServiceBroker::GetSettingsComponent();
+  if (!settingsComponent)
+    return false;
+
+  auto settings = settingsComponent->GetSettings();
+  if (!settings)
+    return false;
+
+  auto setting = settings->GetSetting(CSettings::SETTING_VIDEOSCREEN_LIMITEDRANGE);
+  if (setting)
+    setting->SetVisible(true);
+
+  setting = settings->GetSetting("videoscreen.limitguisize");
+  if (setting)
+    setting->SetVisible(true);
 
   CLog::Log(LOGDEBUG, "CWinSystemGbm::%s - initialized DRM", __FUNCTION__);
   return CWinSystemBase::InitWindowSystem();
