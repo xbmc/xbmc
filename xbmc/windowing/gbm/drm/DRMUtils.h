01a3d35e31ae00f51d7f11529dfc36ae01b3180e
Lukas Rusak <lorusak@gmail.com>
2021-09-16 12:43:37 -0700

    CDRMUtils: remove duplicate method

--- xbmc/windowing/gbm/drm/DRMUtils.h
+++ xbmc/windowing/gbm/drm/DRMUtils.h
@@ -59,7 +59,6 @@ public:
 
   static uint32_t FourCCWithAlpha(uint32_t fourcc);
   static uint32_t FourCCWithoutAlpha(uint32_t fourcc);
-  static std::string FourCCToString(uint32_t fourcc);
 
 protected:
   bool OpenDrm(bool needConnector);
