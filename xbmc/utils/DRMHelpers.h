6c72f278f068b565fe1d3ee28e08fe4465024bd2
Lukas Rusak <lorusak@gmail.com>
2021-09-16 12:33:53 -0700

    linux: add new DRMHELPER namespace utilites

---
+++ xbmc/utils/DRMHelpers.h
@@ -0,0 +1,18 @@
+/*
+ *  Copyright (C) 2005-2021 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include <string>
+
+namespace DRMHELPERS
+{
+
+std::string FourCCToString(uint32_t fourcc);
+
+} // namespace DRMHELPERS
