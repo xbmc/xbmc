694ffcad285a04795705c7b0da7100051c9163f6
fuzzard <fuzzard@kodi.tv>
2021-09-20 17:46:42 +1000

    [osx] relocate SDLMain.mm
    Relocate SDLMain to explicit SDL folder

--- cmake/platform/osx/osx.cmake
+++ cmake/platform/osx/osx.cmake
@@ -9,7 +9,7 @@ endif()
 if(NOT APP_WINDOW_SYSTEM OR APP_WINDOW_SYSTEM STREQUAL sdl)
   list(APPEND SYSTEM_DEFINES -DHAS_SDL)
   list(APPEND PLATFORM_REQUIRED_DEPS Sdl)
-  list(APPEND CORE_MAIN_SOURCE ${CMAKE_SOURCE_DIR}/xbmc/platform/darwin/osx/SDLMain.mm
+  list(APPEND CORE_MAIN_SOURCE ${CMAKE_SOURCE_DIR}/xbmc/platform/darwin/osx/SDL/SDLMain.mm
                                ${CMAKE_SOURCE_DIR}/xbmc/platform/darwin/osx/SDLMain.h)
 else()
   message(SEND_ERROR "Currently only SDL windowing is supported. Please set APP_WINDOW_SYSTEM to \"sdl\"")
