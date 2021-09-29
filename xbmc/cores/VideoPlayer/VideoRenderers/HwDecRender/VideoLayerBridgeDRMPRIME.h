3dd111c84c7089ddd5ed1cdd183ed72b0b01af3b
Lukas Rusak <lorusak@gmail.com>
2021-09-28 23:34:03 -0700

    CWinSystemGbm: add ability to set HDR output

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/VideoLayerBridgeDRMPRIME.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/VideoLayerBridgeDRMPRIME.h
@@ -15,34 +15,6 @@
 
 #include <drm_mode.h>
 
-#ifndef HAVE_HDR_OUTPUT_METADATA
-// HDR structs is copied from linux include/linux/hdmi.h
-struct hdr_metadata_infoframe
-{
-  uint8_t eotf;
-  uint8_t metadata_type;
-  struct
-  {
-    uint16_t x, y;
-  } display_primaries[3];
-  struct
-  {
-    uint16_t x, y;
-  } white_point;
-  uint16_t max_display_mastering_luminance;
-  uint16_t min_display_mastering_luminance;
-  uint16_t max_cll;
-  uint16_t max_fall;
-};
-struct hdr_output_metadata
-{
-  uint32_t metadata_type;
-  union {
-    struct hdr_metadata_infoframe hdmi_metadata_type1;
-  };
-};
-#endif
-
 namespace KODI
 {
 namespace WINDOWING
@@ -78,7 +50,4 @@ private:
 
   CVideoBufferDRMPRIME* m_buffer = nullptr;
   CVideoBufferDRMPRIME* m_prev_buffer = nullptr;
-
-  uint32_t m_hdr_blob_id = 0;
-  struct hdr_output_metadata m_hdr_metadata = {};
 };
