90f4f8844abf200346429263ae026b8f34a54905
Lukas Rusak <lorusak@gmail.com>
2021-10-04 21:52:54 -0700

    SQuads: initialize member variables

--- xbmc/cores/VideoPlayer/VideoRenderers/OverlayRendererUtil.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/OverlayRendererUtil.h
@@ -29,15 +29,8 @@ namespace OVERLAY {
 
   struct SQuads
   {
-    SQuads()
-    {
-      data = {};
-      quad = {};
-      size_x = 0;
-      size_y = 0;
-    }
-    int      size_x;
-    int      size_y;
+    int size_x{0};
+    int size_y{0};
     std::vector<uint8_t> data;
     std::vector<SQuad> quad;
   };
