02322a789c7989e114d448685799f87bff3dda49
Alwin Esch <alwin.esch@web.de>
2021-10-14 17:24:23 +0200

    [addons][audiodecoder] replace oggstream/nsfstream and do with audiodecoder addons
    Furthermore, oggstream has been removed from MusicInfoTagLoaderFactory.cpp, as it is not really used by ogg files and since it was moved to external audio decoder add-ons it has never been used (since the associated code is no longer available).

--- xbmc/music/tags/MusicInfoTagLoaderFactory.cpp
+++ xbmc/music/tags/MusicInfoTagLoaderFactory.cpp
@@ -58,21 +58,14 @@ IMusicInfoTagLoader* CMusicInfoTagLoaderFactory::CreateLoader(const CFileItem& i
     }
   }
 
-  if (strExtension == "aac" ||
-      strExtension == "ape" || strExtension == "mac" ||
-      strExtension == "mp3" ||
-      strExtension == "wma" ||
-      strExtension == "flac" ||
+  if (strExtension == "aac" || strExtension == "ape" || strExtension == "mac" ||
+      strExtension == "mp3" || strExtension == "wma" || strExtension == "flac" ||
       strExtension == "m4a" || strExtension == "mp4" || strExtension == "m4b" ||
-      strExtension == "m4v" ||
-      strExtension == "mpc" || strExtension == "mpp" || strExtension == "mp+" ||
-      strExtension == "ogg" || strExtension == "oga" || strExtension == "oggstream" ||
-      strExtension == "opus" ||
-      strExtension == "aif" || strExtension == "aiff" ||
-      strExtension == "wav" ||
-      strExtension == "mod" ||
-      strExtension == "s3m" || strExtension == "it" || strExtension == "xm" ||
-      strExtension == "wv")
+      strExtension == "m4v" || strExtension == "mpc" || strExtension == "mpp" ||
+      strExtension == "mp+" || strExtension == "ogg" || strExtension == "oga" ||
+      strExtension == "opus" || strExtension == "aif" || strExtension == "aiff" ||
+      strExtension == "wav" || strExtension == "mod" || strExtension == "s3m" ||
+      strExtension == "it" || strExtension == "xm" || strExtension == "wv")
   {
     CTagLoaderTagLib *pTagLoader = new CTagLoaderTagLib();
     return pTagLoader;
