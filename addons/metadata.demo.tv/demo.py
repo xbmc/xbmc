e5f265e7f957ec9ce3d51b587acffbc46344f1b9
CastagnaIT <gottardo.stefano.83@gmail.com>
2021-10-18 18:10:47 +0200

    [addons][python] Updated metadata.demo.tv scraper code

--- addons/metadata.demo.tv/demo.py
+++ addons/metadata.demo.tv/demo.py
@@ -1,192 +1,188 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2005-2021 Team Kodi
+    This file is part of Kodi - kodi.tv
+    SPDX-License-Identifier: GPL-2.0-or-later
+    See LICENSES/README.md for more information.
+"""
+import sys
+import urllib.parse
 
-import xbmcplugin,xbmcgui,xbmc,xbmcaddon
-import os,sys,urllib
+import xbmcgui
+import xbmcplugin
+import xbmc
+
 
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-
-        return param
+    param_string = sys.argv[2][1:]
+    if param_string:
+        return dict(urllib.parse.parse_qsl(param_string))
+    return {}
 
 
-params=get_params()
-
-action=urllib.unquote_plus(params["action"])
+params = get_params()
+plugin_handle = int(sys.argv[1])
+action = params.get('action')
 
 if action == 'find':
-    year = 0
-    title=urllib.unquote_plus(params["title"])
-    try:
-        year=int(urllib.unquote_plus(params["year"]))
-    except:
-        pass
+    title = params['title']
+    year = params.get('year', 'not specified')
+    xbmc.log(f'Find movie with title "{title}" from year {year}', xbmc.LOGDEBUG)
 
-    print('Find TV show with title %s from year %i' %(title, int(year)))
-    liz=xbmcgui.ListItem('Demo show 1', thumbnailImage='DefaultVideo.png', offscreen=True)
+    liz = xbmcgui.ListItem('Demo show 1', offscreen=True)
+    liz.setArt({'thumb': 'DefaultVideo.png'})
     liz.setProperty('relevance', '0.5')
-    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="/path/to/show", listitem=liz, isFolder=True)
-    liz=xbmcgui.ListItem('Demo show 2', thumbnailImage='DefaultVideo.png', offscreen=True)
+    xbmcplugin.addDirectoryItem(handle=plugin_handle, url='/path/to/show', listitem=liz, isFolder=True)
+    liz = xbmcgui.ListItem('Demo show 2', offscreen=True)
+    liz.setArt({'thumb': 'DefaultVideo.png'})
     liz.setProperty('relevance', '0.3')
-    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="/path/to/show2", listitem=liz, isFolder=True)
+    xbmcplugin.addDirectoryItem(handle=plugin_handle, url='/path/to/show2', listitem=liz, isFolder=True)
+
 elif action == 'getdetails':
-    url=urllib.unquote_plus(params["url"])
+    url = params['url']
     if url == '/path/to/show':
-        liz=xbmcgui.ListItem('Demo show 1', offscreen=True)
-        liz.setInfo('video',
-                    {'title': 'Demo show 1',
-                     'originaltitle': 'Demo shåvv 1',
-                     'sorttitle': '2',
-                     'userrating': 5,
-                     'plotoutline': 'Outline yo',
-                     'plot': 'Plot yo',
-                     'tagline': 'Tag yo',
-                     'duration': 110,
-                     'mpaa': 'T',
-                     'trailer': '/home/akva/fluffy/bunnies.mkv',
-                     'genre': ['Action', 'Comedy'],
-                     'country': ['Norway', 'Sweden', 'China'],
-                     'credits': ['None', 'Want', 'To Admit It'],
-                     'director': ['spiff', 'spiff2'],
-                     'studio': ['Studio1', 'Studio2'],
-                     'dateadded': '2016-01-01',
-                     'premiered': '2015-01-01',
-                     'aired': '2007-01-01',
-                     'status': 'Cancelled',
-                     'episodeguide': '/path/to/show/guide',
-                     'tag': ['Family', 'Mom <3']
-                    })
-        liz.setRating("imdb", 9, 100000, True )
-        liz.setRating("tvdb", 8.9, 1000)
-        liz.setUniqueIDs({ 'imdb': 'tt8938399', 'tvdb' : '9837493' }, 'tvdb')
-        liz.addSeason(1, 'Horrible')
-        liz.addSeason(2, 'Crap')
-        liz.setCast([{'name': 'spiff', 'role': 'himself', 'thumbnail': '/home/akva/Pictures/fish.jpg', 'order': 2},
-                    {'name': 'monkey', 'role': 'orange', 'thumbnail': '/home/akva/Pictures/coffee.jpg', 'order': 1}])
-        liz.addAvailableArtwork('DefaultBackFanart.png', 'banner')
-        liz.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png', 'poster')
-        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'}, 
-                                {'image': '/home/akva/Pictures/hawaii-shirt.png', 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
-        xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=liz)
+        xbmc.log('Get tv show details callback', xbmc.LOGDEBUG)
+        liz = xbmcgui.ListItem('Demo show 1', offscreen=True)
+        tags = liz.getVideoInfoTag()
+        tags.setTitle('Demo show 1')
+        tags.setOriginalTitle('Demo shåvv 1')
+        tags.setSortTitle('2')
+        tags.setUserRating(5)
+        tags.setPlotOutline('Outline yo')
+        tags.setPlot('Plot yo')
+        tags.setTagLine('Tag yo')
+        tags.setDuration(110)
+        tags.setMpaa('T')
+        tags.setTrailer('/home/akva/fluffy/bunnies.mkv')
+        tags.setGenres(['Action', 'Comedy'])
+        tags.setWriters(['None', 'Want', 'To Admit It'])
+        tags.setDirectors(['Director 1', 'Director 2'])
+        tags.setStudios(['Studio1', 'Studio2'])
+        tags.setDateAdded('2016-01-01')
+        tags.setPremiered('2015-01-01')
+        tags.setFirstAired('2007-01-01')
+        tags.setTvShowStatus('Cancelled')
+        tags.setEpisodeGuide('/path/to/show/guide')
+        tags.setTagLine('Family / Mom <3')
+        tags.setRatings({'imdb': (9, 100000), 'tvdb': (8.9, 1000)}, defaultRating='imdb')
+        tags.setUniqueIDs({'imdb': 'tt8938399', 'tmdb': '9837493'}, defaultUniqueID='tvdb')
+        tags.addSeason(1, 'Beautiful')
+        tags.addSeason(2, 'Sun')
+        tags.setCast([xbmc.Actor('spiff', 'himself', order=2, thumbnail='/home/akva/Pictures/fish.jpg'),
+                      xbmc.Actor('monkey', 'orange', order=1, thumbnail='/home/akva/Pictures/coffee.jpg')])
+        tags.addAvailableArtwork('DefaultBackFanart.png', 'banner')
+        tags.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png', 'poster')
+        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'},
+                                {'image': '/home/akva/Pictures/hawaii-shirt.png',
+                                 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
+        xbmcplugin.setResolvedUrl(handle=plugin_handle, succeeded=True, listitem=liz)
+
 elif action == 'getepisodelist':
-    url=urllib.unquote_plus(params["url"])
-    print('in here yo ' + url)
+    url = params['url']
+    xbmc.log(f'Get episode list callback "{url}"', xbmc.LOGDEBUG)
     if url == '/path/to/show/guide':
-        liz=xbmcgui.ListItem('Demo Episode 1x1', offscreen=True)
-        liz.setInfo('video',
-                    {'title': 'Demo Episode 1',
-                     'season': 1,
-                     'episode': 1,
-                     'aired': '2015-01-01'
-                     })
-        liz.addAvailableArtwork('/path/to/episode1','banner')
-        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="/path/to/episode1", listitem=liz, isFolder=False)
-        liz=xbmcgui.ListItem('Demo Episode 2x2', offscreen=True)
-        liz.setInfo('video',
-                    {'title': 'Demo Episode 2',
-                     'season': 2,
-                     'episode': 2,
-                     'aired': '2014-01-01'
-                     })
-        liz.addAvailableArtwork('/path/to/episode2','banner')
-        #liz.setProperty('video.sub_episode', '1')
-        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="/path/to/episode2", listitem=liz, isFolder=False)
+        liz = xbmcgui.ListItem('Demo Episode 1x1', offscreen=True)
+        tags = liz.getVideoInfoTag()
+        tags.setTitle('Demo Episode 1')
+        tags.setSeason(1)
+        tags.setEpisode(1)
+        tags.setFirstAired('2015-01-01')
+        tags.addAvailableArtwork('/path/to/episode1', 'banner')
+        xbmcplugin.addDirectoryItem(handle=plugin_handle, url="/path/to/episode1", listitem=liz, isFolder=False)
+
+        liz = xbmcgui.ListItem('Demo Episode 2x2', offscreen=True)
+        tags = liz.getVideoInfoTag()
+        tags.setTitle('Demo Episode 2')
+        tags.setSeason(2)
+        tags.setEpisode(2)
+        tags.setFirstAired('2014-01-01')
+        tags.addAvailableArtwork('/path/to/episode2', 'banner')
+        xbmcplugin.addDirectoryItem(handle=plugin_handle, url="/path/to/episode1", listitem=liz, isFolder=False)
+
 elif action == 'getepisodedetails':
-    url=urllib.unquote_plus(params["url"])
+    url = params['url']
     if url == '/path/to/episode1':
-        liz=xbmcgui.ListItem('Demo Episode 1', offscreen=True)
-        liz.setInfo('video',
-                    {'title': 'Demo Episode 1',
-                     'originaltitle': 'Demo æpisod 1x1',
-                     'sorttitle': '2',
-                     'episode': 1,
-                     'season': 1,
-                     'userrating': 5,
-                     'plotoutline': 'Outline yo',
-                     'plot': 'Plot yo',
-                     'tagline': 'Tag yo',
-                     'duration': 110,
-                     'mpaa': 'T',
-                     'trailer': '/home/akva/fluffy/unicorns.mkv',
-                     'genre': ['Action', 'Comedy'],
-                     'country': ['Norway', 'Sweden', 'China'],
-                     'credits': ['None', 'Want', 'To Admit It'],
-                     'director': ['spiff', 'spiff2'],
-                     'studio': ['Studio1', 'Studio2'],
-                     'dateadded': '2016-01-01',
-                     'premiered': '2015-01-01',
-                     'aired': '2007-01-01',
-                     'tag': ['Family', 'Dad <3']
-                    })
-        liz.setRating("imdb", 9, 100000, True )
-        liz.setRating("tvdb", 8.9, 1000)
-        liz.setUniqueIDs({ 'tvdb': '3894', 'imdb' : 'tt384940' }, 'tvdb')
-        liz.addSeason(1, 'Horrible')
-        liz.addSeason(2, 'Crap')
-        liz.setCast([{'name': 'spiff', 'role': 'himself', 'thumbnail': '/home/akva/Pictures/fish.jpg', 'order': 2},
-                    {'name': 'monkey', 'role': 'orange', 'thumbnail': '/home/akva/Pictures/coffee.jpg', 'order': 1}])
-        liz.addAvailableArtwork('DefaultBackFanart.png', 'banner')
-        liz.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png', 'poster')
-        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'}, 
-                                {'image': '/home/akva/Pictures/hawaii-shirt.png', 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
-        xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=liz)
+        xbmc.log('Get episode 1 details callback', xbmc.LOGDEBUG)
+        liz = xbmcgui.ListItem('Demo Episode 1', offscreen=True)
+        tags = liz.getVideoInfoTag()
+        tags.setTitle('Demo Episode 1')
+        tags.setOriginalTitle('Demo æpisod 1x1')
+        tags.setSeason(1)
+        tags.setEpisode(1)
+        tags.setUserRating(5)
+        tags.setPlotOutline('Outline yo')
+        tags.setPlot('Plot yo')
+        tags.setTagLine('Tag yo')
+        tags.setDuration(110)
+        tags.setMpaa('T')
+        tags.setTrailer('/home/akva/fluffy/unicorns.mkv')
+        tags.setGenres(['Action', 'Comedy'])
+        tags.setCountries(['Norway', 'Sweden', 'China'])
+        tags.setWriters(['None', 'Want', 'To Admit It'])
+        tags.setDirectors(['Director 1', 'Director 2'])
+        tags.setStudios(['Studio1', 'Studio2'])
+        tags.setDateAdded('2016-01-01')
+        tags.setPremiered('2015-01-01')
+        tags.setFirstAired('2007-01-01')
+        tags.setTagLine('Family / Dad <3')
+        tags.setRatings({'imdb': (9, 100000), 'tvdb': (8.9, 1000)}, defaultRating='imdb')
+        tags.setUniqueIDs({'tvdb': '3894', 'imdb': 'tt384940'}, defaultUniqueID='tvdb')
+        tags.addSeason(1, 'Beautiful')
+        tags.addSeason(2, 'Sun')
+        tags.setCast([xbmc.Actor('spiff', 'himself', order=2, thumbnail='/home/akva/Pictures/fish.jpg'),
+                      xbmc.Actor('monkey', 'orange', order=1, thumbnail='/home/akva/Pictures/coffee.jpg')])
+        tags.addAvailableArtwork('DefaultBackFanart.png', 'banner')
+        tags.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png', 'poster')
+        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'},
+                                {'image': '/home/akva/Pictures/hawaii-shirt.png',
+                                 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
+        xbmcplugin.setResolvedUrl(handle=plugin_handle, succeeded=True, listitem=liz)
+
     elif url == '/path/to/episode2':
-        liz=xbmcgui.ListItem('Demo Episode 2', offscreen=True)
-        liz.setInfo('video',
-                    {'title': 'Demo Episode 2',
-                     'originaltitle': 'Demo æpisod 2x2',
-                     'sorttitle': '1',
-                     'episode': 2,
-                     'season': 2,
-                     'userrating': 8,
-                     'plotoutline': 'Outline yo',
-                     'plot': 'Plot yo',
-                     'tagline': 'Tag yo',
-                     'duration': 110,
-                     'mpaa': 'T',
-                     'trailer': '/home/akva/fluffy/puppies.mkv',
-                     'genre': ['Action', 'Comedy'],
-                     'country': ['Norway', 'Sweden', 'China'],
-                     'credits': ['None', 'Want', 'To Admit It'],
-                     'director': ['spiff', 'spiff2'],
-                     'studio': ['Studio1', 'Studio2'],
-                     'dateadded': '2016-01-01',
-                     'premiered': '2015-01-01',
-                     'aired': '2007-01-01',
-                     'tag': ['Something', 'Else']
-                    })
-        liz.setRating("imdb", 7, 25457, True )
-        liz.setRating("tvdb", 8.1, 5478)
-        liz.setUniqueIDs({ 'tvdb': '3894', 'imdb' : 'tt384940' })
-        liz.addSeason(1, 'Horrible')
-        liz.addSeason(2, 'Crap')
-        liz.setCast([{'name': 'spiff', 'role': 'himself', 'thumbnail': '/home/akva/Pictures/fish.jpg', 'order': 2},
-                    {'name': 'monkey', 'role': 'orange', 'thumbnail': '/home/akva/Pictures/coffee.jpg', 'order': 1}])
-        liz.addAvailableArtwork('DefaultBackFanart.png','banner')
-        liz.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png','poster')
-        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'}, 
-                                {'image': '/home/akva/Pictures/hawaii-shirt.png', 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
-        xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=liz)
-elif action == 'nfourl': 
-    nfo=urllib.unquote_plus(params["nfo"]) 
-    print 'Find url from nfo file' 
-    liz=xbmcgui.ListItem('Demo show 1', offscreen=True) 
-    xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url="/path/to/show", listitem=liz, isFolder=True) 
- 
+        xbmc.log('Get episode 2 details callback', xbmc.LOGDEBUG)
+        liz = xbmcgui.ListItem('Demo Episode 2', offscreen=True)
+        tags = liz.getVideoInfoTag()
+        tags.setTitle('Demo Episode 2')
+        tags.setOriginalTitle('Demo æpisod 2x2')
+        tags.setSortTitle('1')
+        tags.setSeason(2)
+        tags.setEpisode(2)
+        tags.setUserRating(8)
+        tags.setPlotOutline('Outline yo')
+        tags.setPlot('Plot yo')
+        tags.setTagLine('Tag yo')
+        tags.setDuration(110)
+        tags.setMpaa('T')
+        tags.setTrailer('/home/akva/fluffy/puppies.mkv')
+        tags.setGenres(['Action', 'Comedy'])
+        tags.setCountries(['Norway', 'Sweden', 'China'])
+        tags.setWriters(['None', 'Want', 'To Admit It'])
+        tags.setDirectors(['Director 1', 'Director 2'])
+        tags.setStudios(['Studio1', 'Studio2'])
+        tags.setDateAdded('2016-01-01')
+        tags.setPremiered('2015-01-01')
+        tags.setFirstAired('2007-01-01')
+        tags.setTagLine('Something / Else')
+        tags.setRatings({'imdb': (7, 25457), 'tvdb': (8.1, 5478)}, defaultRating='imdb')
+        tags.setUniqueIDs({'tvdb': '3894', 'imdb': 'tt384940'}, defaultUniqueID='tvdb')
+        tags.addSeason(1, 'Beautiful')
+        tags.addSeason(2, 'Sun')
+        tags.setCast([xbmc.Actor('spiff', 'himself', order=2, thumbnail='/home/akva/Pictures/fish.jpg'),
+                      xbmc.Actor('monkey', 'orange', order=1, thumbnail='/home/akva/Pictures/coffee.jpg')])
+        tags.addAvailableArtwork('DefaultBackFanart.png', 'banner')
+        tags.addAvailableArtwork('/home/akva/Pictures/hawaii-shirt.png', 'poster')
+        liz.setAvailableFanart([{'image': 'DefaultBackFanart.png', 'preview': 'DefaultBackFanart.png'},
+                                {'image': '/home/akva/Pictures/hawaii-shirt.png',
+                                 'preview': '/home/akva/Pictures/hawaii-shirt.png'}])
+        xbmcplugin.setResolvedUrl(handle=plugin_handle, succeeded=True, listitem=liz)
 
+elif action == 'nfourl':
+    nfo = params['nfo']
+    xbmc.log('Find url from nfo file', xbmc.LOGDEBUG)
+    liz = xbmcgui.ListItem('Demo show 1', offscreen=True)
+    xbmcplugin.addDirectoryItem(handle=plugin_handle, url="/path/to/show", listitem=liz, isFolder=True)
 
+elif action is not None:
+    xbmc.log(f'Action "{action}" not implemented', xbmc.LOGDEBUG)
 
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
+xbmcplugin.endOfDirectory(plugin_handle)
