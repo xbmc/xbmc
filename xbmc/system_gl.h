921382ea58ca0bae1789d35a7575dd07544c1108
Lukas Rusak <lorusak@gmail.com>
2021-10-18 19:55:11 -0700

    [clang-format] format system_gl.h

--- xbmc/system_gl.h
+++ xbmc/system_gl.h
@@ -9,37 +9,37 @@
 #pragma once
 
 #ifdef HAS_GL
-  // always define GL_GLEXT_PROTOTYPES before include gl headers
-  #if !defined(GL_GLEXT_PROTOTYPES)
-    #define GL_GLEXT_PROTOTYPES
-  #endif
-  #if defined(TARGET_LINUX)
-    #include <GL/gl.h>
-    #include <GL/glu.h>
-    #include <GL/glext.h>
-  #elif defined(TARGET_FREEBSD)
-    #include <GL/gl.h>
-    #include <GL/glu.h>
-  #elif defined(TARGET_DARWIN)
-    #include <OpenGL/gl3.h>
-    #include <OpenGL/glu.h>
-    #include <OpenGL/gl3ext.h>
-  #endif
+// always define GL_GLEXT_PROTOTYPES before include gl headers
+#if !defined(GL_GLEXT_PROTOTYPES)
+#define GL_GLEXT_PROTOTYPES
+#endif
+#if defined(TARGET_LINUX)
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GL/glu.h>
+#elif defined(TARGET_FREEBSD)
+#include <GL/gl.h>
+#include <GL/glu.h>
+#elif defined(TARGET_DARWIN)
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+#include <OpenGL/glu.h>
+#endif
 #elif HAS_GLES >= 2
-  #if defined(TARGET_DARWIN)
-    #include <OpenGLES/ES2/gl.h>
-    #include <OpenGLES/ES2/glext.h>
-  #else
-    #include <GLES2/gl2.h>
-    #include <GLES2/gl2ext.h>
-  #endif
-  #if HAS_GLES == 3
-    #include <GLES3/gl3.h>
-  #endif
+#if defined(TARGET_DARWIN)
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+#else
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#endif
+#if HAS_GLES == 3
+#include <GLES3/gl3.h>
+#endif
 #endif
 
 // Useful pixel colour manipulation macros
-#define GET_A(color)            ((color >> 24) & 0xFF)
-#define GET_R(color)            ((color >> 16) & 0xFF)
-#define GET_G(color)            ((color >>  8) & 0xFF)
-#define GET_B(color)            ((color >>  0) & 0xFF)
+#define GET_A(color) ((color >> 24) & 0xFF)
+#define GET_R(color) ((color >> 16) & 0xFF)
+#define GET_G(color) ((color >> 8) & 0xFF)
+#define GET_B(color) ((color >> 0) & 0xFF)
