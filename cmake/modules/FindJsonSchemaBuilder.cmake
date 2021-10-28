33212fe5478c7ae71fa88ee1f6e9e85150c036d6
Vasyl Gello <vasek.gello@gmail.com>
2021-10-28 15:02:52 +0000

    Rename installable TexturePacker and JsonSchemaBuilder
    ... to kodi-TexturePacker and kodi-JsonSchemaBuilder.
    
    These names are less generic and will less likely cause conflicts
    in /usr/bin.
    
    Signed-off-by: Vasyl Gello <vasek.gello@gmail.com>

--- cmake/modules/FindJsonSchemaBuilder.cmake
+++ cmake/modules/FindJsonSchemaBuilder.cmake
@@ -13,17 +13,26 @@
 
 if(NOT TARGET JsonSchemaBuilder::JsonSchemaBuilder)
   if(KODI_DEPENDSBUILD)
+    get_filename_component(_jsbpath "${NATIVEPREFIX}/bin" ABSOLUTE)
+    find_program(JSONSCHEMABUILDER_EXECUTABLE NAMES "${APP_NAME_LC}-JsonSchemaBuilder" JsonSchemaBuilder
+                                   HINTS ${_jsbpath})
+
     add_executable(JsonSchemaBuilder::JsonSchemaBuilder IMPORTED GLOBAL)
     set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
-                                                       IMPORTED_LOCATION "${NATIVEPREFIX}/bin/JsonSchemaBuilder")
+                                                       IMPORTED_LOCATION "${JSONSCHEMABUILDER_EXECUTABLE}")
   elseif(CORE_SYSTEM_NAME STREQUAL windowsstore)
+    get_filename_component(_jsbpath "${DEPENDENCIES_DIR}/bin/json-rpc" ABSOLUTE)
+    find_program(JSONSCHEMABUILDER_EXECUTABLE NAMES "${APP_NAME_LC}-JsonSchemaBuilder" JsonSchemaBuilder
+                                              HINTS ${_jsbpath})
+
     add_executable(JsonSchemaBuilder::JsonSchemaBuilder IMPORTED GLOBAL)
     set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
-                                                       IMPORTED_LOCATION "${DEPENDENCIES_DIR}/bin/json-rpc/JsonSchemaBuilder")
+                                                       IMPORTED_LOCATION "${JSONSCHEMABUILDER_EXECUTABLE}")
   else()
     if(WITH_JSONSCHEMABUILDER)
       get_filename_component(_jsbpath ${WITH_JSONSCHEMABUILDER} ABSOLUTE)
-      find_program(JSONSCHEMABUILDER_EXECUTABLE JsonSchemaBuilder PATHS ${_jsbpath})
+      find_program(JSONSCHEMABUILDER_EXECUTABLE NAMES "${APP_NAME_LC}-JsonSchemaBuilder" JsonSchemaBuilder
+                                                PATHS ${_jsbpath})
 
       include(FindPackageHandleStandardArgs)
       find_package_handle_standard_args(JsonSchemaBuilder DEFAULT_MSG JSONSCHEMABUILDER_EXECUTABLE)
