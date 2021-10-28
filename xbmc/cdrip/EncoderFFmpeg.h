a9171509a2bfc27100a0c7bddfd419e84e8b406e
Alwin Esch <alwin.esch@web.de>
2021-10-28 17:33:59 +0200

    [addons][audioencoder] cleanup CEncoderFFmpeg
    This cleans up the class to be more in line with current Kodi standards.

--- xbmc/cdrip/EncoderFFmpeg.h
+++ xbmc/cdrip/EncoderFFmpeg.h
@@ -20,7 +20,7 @@ extern "C"
 class CEncoderFFmpeg : public IEncoder
 {
 public:
-  CEncoderFFmpeg();
+  CEncoderFFmpeg() = default;
   ~CEncoderFFmpeg() override = default;
 
   bool Init(AddonToKodiFuncTable_AudioEncoder& callbacks) override;
@@ -31,11 +31,12 @@ private:
   static int avio_write_callback(void* opaque, uint8_t* buf, int buf_size);
   static int64_t avio_seek_callback(void* opaque, int64_t offset, int whence);
   void SetTag(const std::string& tag, const std::string& value);
+  bool WriteFrame();
 
-  AVFormatContext* m_Format;
-  AVCodecContext* m_CodecCtx;
-  SwrContext* m_SwrCtx;
-  AVStream* m_Stream;
+  AVFormatContext* m_Format{nullptr};
+  AVCodecContext* m_CodecCtx{nullptr};
+  SwrContext* m_SwrCtx{nullptr};
+  AVStream* m_Stream{nullptr};
   AVSampleFormat m_InFormat;
   AVSampleFormat m_OutFormat;
 
@@ -49,15 +50,13 @@ private:
 
   unsigned int m_NeededFrames;
   unsigned int m_NeededBytes;
-  uint8_t* m_Buffer;
-  unsigned int m_BufferSize = 0;
-  AVFrame* m_BufferFrame;
-  uint8_t* m_ResampledBuffer;
-  unsigned int m_ResampledBufferSize = 0;
-  AVFrame* m_ResampledFrame;
-  bool m_NeedConversion = false;
+  uint8_t* m_Buffer{nullptr};
+  unsigned int m_BufferSize{0};
+  AVFrame* m_BufferFrame{nullptr};
+  uint8_t* m_ResampledBuffer{nullptr};
+  unsigned int m_ResampledBufferSize{0};
+  AVFrame* m_ResampledFrame{nullptr};
+  bool m_NeedConversion{false};
 
-  AddonToKodiFuncTable_AudioEncoder m_callbacks;
-
-  bool WriteFrame();
+  AddonToKodiFuncTable_AudioEncoder m_callbacks{};
 };
