6186a7e42c96166d7a4041a621cd875f759990be
Alwin Esch <alwin.esch@web.de>
2021-10-28 20:20:00 +0200

    [addons][audioencoder] Clean already used doxygen documentation parts
    This clears up the already existing doxygen documentations in order to have a better overview and to correspond to a style.

--- xbmc/cdrip/CDDARipJob.h
+++ xbmc/cdrip/CDDARipJob.h
@@ -26,15 +26,18 @@ class CEncoder;
 class CCDDARipJob : public CJob
 {
 public:
-  //! \brief Construct a ripper job
-  //! \param input The input file url
-  //! \param output The output file url
-  //! \param tag The music tag to attach to track
-  //! \param encoder The encoder to use. See Encoder.h
-  //! \param eject Should we eject tray on finish?
-  //! \param rate The sample rate of the input
-  //! \param channels Number of audio channels in input
-  //! \param bps The bits per sample for input
+  /*!
+   * \brief Construct a ripper job
+   *
+   * \param[in] input The input file url
+   * \param[in] output The output file url
+   * \param[in] tag The music tag to attach to track
+   * \param[in] encoder The encoder to use. See Encoder.h
+   * \param[in] eject Should we eject tray on finish?
+   * \param[in] rate The sample rate of the input
+   * \param[in] channels Number of audio channels in input
+   * \param[in] bps The bits per sample for input
+   */
   CCDDARipJob(const std::string& input,
               const std::string& output,
               const MUSIC_INFO::CMusicInfoTag& tag,
@@ -52,20 +55,27 @@ public:
   std::string GetOutput() const { return m_output; }
 
 protected:
-  //! \brief Setup the audio encoder
+  /*!
+   * \brief Setup the audio encoder
+   */
   std::unique_ptr<CEncoder> SetupEncoder(XFILE::CFile& reader);
 
-  //! \brief Helper used if output is a remote url
+  /*!
+   * \brief Helper used if output is a remote url
+   */
   std::string SetupTempFile();
 
-  //! \brief Rip a chunk of audio
-  //! \param reader The input reader
-  //! \param encoder The audio encoder
-  //! \param percent The percentage completed on return
-  //! \return 0 (CDDARIP_OK) if everything went okay, or
-  //!         a positive error code from the reader, or
-  //!         -1 if the encoder failed
-  //! \sa CCDDARipper::GetData, CEncoder::Encode
+  /*!
+   * \brief Rip a chunk of audio
+   *
+   * \param[in] reader The input reader
+   * \param[in] encoder The audio encoder
+   * \param[out] percent The percentage completed on return
+   * \return 0 (CDDARIP_OK) if everything went okay, or
+   *         a positive error code from the reader, or
+   *         -1 if the encoder failed
+   * \sa CCDDARipper::GetData, CEncoder::Encode
+   */
   int RipChunk(XFILE::CFile& reader, const std::unique_ptr<CEncoder>& encoder, int& percent);
 
   unsigned int m_rate; //< The sample rate of the input file
