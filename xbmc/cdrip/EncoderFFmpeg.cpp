86f5ed3f2814bcbf535ef57459106aacb1e4b026
Alwin Esch <alwin.esch@web.de>
2021-10-28 17:33:59 +0200

    [addons][audioencoder] cleanup CEncoderFFmpeg
    This cleans up the class to be more in line with current Kodi standards.

--- xbmc/cdrip/EncoderFFmpeg.cpp
+++ xbmc/cdrip/EncoderFFmpeg.cpp
@@ -29,21 +29,6 @@
 #define AV_PKT_FLAG_KEY PKT_FLAG_KEY
 #endif
 
-using namespace ADDON;
-
-CEncoderFFmpeg::CEncoderFFmpeg()
-  : m_Format(NULL),
-    m_CodecCtx(NULL),
-    m_SwrCtx(NULL),
-    m_Stream(NULL),
-    m_Buffer(NULL),
-    m_BufferFrame(NULL),
-    m_ResampledBuffer(NULL),
-    m_ResampledFrame(NULL)
-{
-  memset(&m_callbacks, 0, sizeof(m_callbacks));
-}
-
 bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
 {
   if (!callbacks.kodiInstance || !callbacks.write || !callbacks.seek)
@@ -52,10 +37,10 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
   m_callbacks = callbacks;
 
   std::string filename = URIUtils::GetFileName(m_strFile);
-  if (avformat_alloc_output_context2(&m_Format, NULL, NULL, filename.c_str()))
+  if (avformat_alloc_output_context2(&m_Format, nullptr, nullptr, filename.c_str()))
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Unable to guess the output format for the file {}",
-              filename);
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Unable to guess the output format for the file {}",
+              __func__, filename);
     return false;
   }
 
@@ -64,31 +49,32 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
 
   if (!codec)
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Unable to find a suitable FFmpeg encoder");
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Unable to find a suitable FFmpeg encoder", __func__);
     return false;
   }
 
-  m_Format->pb = avio_alloc_context(m_BCBuffer, sizeof(m_BCBuffer), AVIO_FLAG_WRITE, this, NULL,
+  m_Format->pb = avio_alloc_context(m_BCBuffer, sizeof(m_BCBuffer), AVIO_FLAG_WRITE, this, nullptr,
                                     avio_write_callback, avio_seek_callback);
   if (!m_Format->pb)
   {
     av_freep(&m_Format);
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to allocate ByteIOContext");
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to allocate ByteIOContext", __func__);
     return false;
   }
 
-  AddonPtr addon;
+  ADDON::AddonPtr addon;
   std::string addonId = CServiceBroker::GetSettingsComponent()->GetSettings()->GetString(
       CSettings::SETTING_AUDIOCDS_ENCODER);
   bool success = CServiceBroker::GetAddonMgr().GetAddon(addonId, addon, ADDON::ADDON_UNKNOWN,
                                                         ADDON::OnlyEnabled::YES);
   if (success && addon)
   {
-    m_Format->bit_rate = (128 + 32 * strtol(addon->GetSetting("bitrate").c_str(), NULL, 10)) * 1000;
+    m_Format->bit_rate =
+        (128 + 32 * strtol(addon->GetSetting("bitrate").c_str(), nullptr, 10)) * 1000;
   }
   else
   {
-    CLog::Log(LOGERROR, "{} - Could not get addon: {}", __FUNCTION__, addonId);
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Could not get addon: {}", __func__, addonId);
   }
 
   /* add a stream to it */
@@ -97,7 +83,7 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
   {
     av_freep(&m_Format->pb);
     av_freep(&m_Format);
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to allocate AVStream context");
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to allocate AVStream context", __func__);
     return false;
   }
 
@@ -143,9 +129,9 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
 
   m_NeedConversion = (m_OutFormat != m_InFormat);
 
-  if (m_OutFormat <= AV_SAMPLE_FMT_NONE || avcodec_open2(m_CodecCtx, codec, NULL))
+  if (m_OutFormat <= AV_SAMPLE_FMT_NONE || avcodec_open2(m_CodecCtx, codec, nullptr))
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to open the codec {}",
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to open the codec {}", __func__,
               codec->long_name ? codec->long_name : codec->name);
     av_freep(&m_Stream);
     av_freep(&m_Format->pb);
@@ -155,14 +141,14 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
 
   /* calculate how many bytes we need per frame */
   m_NeededFrames = m_CodecCtx->frame_size;
-  m_NeededBytes = av_samples_get_buffer_size(NULL, m_iInChannels, m_NeededFrames, m_InFormat, 0);
+  m_NeededBytes = av_samples_get_buffer_size(nullptr, m_iInChannels, m_NeededFrames, m_InFormat, 0);
   m_Buffer = (uint8_t*)av_malloc(m_NeededBytes);
   m_BufferSize = 0;
 
   m_BufferFrame = av_frame_alloc();
   if (!m_BufferFrame || !m_Buffer)
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to allocate necessary buffers");
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to allocate necessary buffers", __func__);
     av_frame_free(&m_BufferFrame);
     av_freep(&m_Buffer);
     av_freep(&m_Stream);
@@ -179,12 +165,12 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
 
   if (m_NeedConversion)
   {
-    m_SwrCtx = swr_alloc_set_opts(NULL, m_CodecCtx->channel_layout, m_OutFormat,
+    m_SwrCtx = swr_alloc_set_opts(nullptr, m_CodecCtx->channel_layout, m_OutFormat,
                                   m_CodecCtx->sample_rate, m_CodecCtx->channel_layout, m_InFormat,
-                                  m_CodecCtx->sample_rate, 0, NULL);
+                                  m_CodecCtx->sample_rate, 0, nullptr);
     if (!m_SwrCtx || swr_init(m_SwrCtx) < 0)
     {
-      CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to initialize the resampler");
+      CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to initialize the resampler", __func__);
       av_frame_free(&m_BufferFrame);
       av_freep(&m_Buffer);
       av_freep(&m_Stream);
@@ -194,12 +180,13 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
     }
 
     m_ResampledBufferSize =
-        av_samples_get_buffer_size(NULL, m_iInChannels, m_NeededFrames, m_OutFormat, 0);
+        av_samples_get_buffer_size(nullptr, m_iInChannels, m_NeededFrames, m_OutFormat, 0);
     m_ResampledBuffer = (uint8_t*)av_malloc(m_ResampledBufferSize);
     m_ResampledFrame = av_frame_alloc();
     if (!m_ResampledBuffer || !m_ResampledFrame)
     {
-      CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to allocate a frame for resampling");
+      CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to allocate a frame for resampling",
+                __func__);
       av_frame_free(&m_ResampledFrame);
       av_freep(&m_ResampledBuffer);
       swr_free(&m_SwrCtx);
@@ -226,9 +213,9 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
   SetTag("encoder", CSysInfo::GetAppName() + " FFmpeg Encoder");
 
   /* write the header */
-  if (avformat_write_header(m_Format, NULL) != 0)
+  if (avformat_write_header(m_Format, nullptr) != 0)
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::Init - Failed to write the header");
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Failed to write the header", __func__);
     av_frame_free(&m_ResampledFrame);
     av_freep(&m_ResampledBuffer);
     swr_free(&m_SwrCtx);
@@ -240,7 +227,8 @@ bool CEncoderFFmpeg::Init(AddonToKodiFuncTable_AudioEncoder& callbacks)
     return false;
   }
 
-  CLog::Log(LOGDEBUG, "CEncoderFFmpeg::Init - Successfully initialized with muxer {} and codec {}",
+  CLog::Log(LOGDEBUG, "CEncoderFFmpeg::{} - Successfully initialized with muxer {} and codec {}",
+            __func__,
             m_Format->oformat->long_name ? m_Format->oformat->long_name : m_Format->oformat->name,
             codec->long_name ? codec->long_name : codec->name);
 
@@ -300,7 +288,7 @@ bool CEncoderFFmpeg::WriteFrame()
   AVPacket* pkt = av_packet_alloc();
   if (!pkt)
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - av_packet_alloc failed: {}", __FUNCTION__,
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - av_packet_alloc failed: {}", __func__,
               strerror(errno));
     return false;
   }
@@ -311,7 +299,7 @@ bool CEncoderFFmpeg::WriteFrame()
     if (swr_convert(m_SwrCtx, m_ResampledFrame->extended_data, m_NeededFrames,
                     const_cast<const uint8_t**>(m_BufferFrame->extended_data), m_NeededFrames) < 0)
     {
-      CLog::Log(LOGERROR, "CEncoderFFmpeg::WriteFrame - Error resampling audio");
+      CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Error resampling audio", __func__);
       av_packet_free(&pkt);
       return false;
     }
@@ -326,7 +314,7 @@ bool CEncoderFFmpeg::WriteFrame()
 
   if (encoded < 0)
   {
-    CLog::Log(LOGERROR, "CEncoderFFmpeg::WriteFrame - Error encoding audio: {}", encoded);
+    CLog::Log(LOGERROR, "CEncoderFFmpeg::{} - Error encoding audio: {}", __func__, encoded);
     av_packet_free(&pkt);
     return false;
   }
@@ -335,7 +323,7 @@ bool CEncoderFFmpeg::WriteFrame()
   {
     if (av_write_frame(m_Format, pkt) < 0)
     {
-      CLog::Log(LOGERROR, "CEncoderFFMmpeg::WriteFrame - Failed to write the frame data");
+      CLog::Log(LOGERROR, "CEncoderFFMmpeg::{} - Failed to write the frame data", __func__);
       av_packet_free(&pkt);
       return false;
     }
