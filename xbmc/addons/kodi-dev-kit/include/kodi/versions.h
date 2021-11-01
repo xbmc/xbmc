fbe59719c009d1f207f1de9aad6eecf0ffc58d92
Alwin Esch <alwin.esch@web.de>
2021-11-01 03:53:37 +0100

    [addons][games] increase API version to 2.1.1
    Min version stays the same and not breaks the addons.

--- xbmc/addons/kodi-dev-kit/include/kodi/versions.h
+++ xbmc/addons/kodi-dev-kit/include/kodi/versions.h
@@ -97,7 +97,7 @@
 #define ADDON_INSTANCE_VERSION_AUDIOENCODER_DEPENDS   "c-api/addon-instance/audio_encoder.h" \
                                                       "addon-instance/AudioEncoder.h"
 
-#define ADDON_INSTANCE_VERSION_GAME                   "2.1.0"
+#define ADDON_INSTANCE_VERSION_GAME                   "2.1.1"
 #define ADDON_INSTANCE_VERSION_GAME_MIN               "2.1.0"
 #define ADDON_INSTANCE_VERSION_GAME_XML_ID            "kodi.binary.instance.game"
 #define ADDON_INSTANCE_VERSION_GAME_DEPENDS           "addon-instance/Game.h"
