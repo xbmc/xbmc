1a121568dd1891c69326be4d7d9ad4d10d90a651
Lukas Rusak <lorusak@gmail.com>
2021-11-05 19:44:46 -0700

    CRendererVAAPI: rename to GL and GLES specific classes

--- xbmc/windowing/gbm/OptionalsReg.h
+++ xbmc/windowing/gbm/OptionalsReg.h
@@ -25,8 +25,12 @@ CVaapiProxy* VaapiProxyCreate(int fd);
 void VaapiProxyDelete(CVaapiProxy *proxy);
 void VaapiProxyConfig(CVaapiProxy *proxy, void *eglDpy);
 void VAAPIRegister(CVaapiProxy *winSystem, bool deepColor);
-void VAAPIRegisterRender(CVaapiProxy *winSystem, bool &general, bool &deepColor);
-
+#if defined(HAS_GL)
+void VAAPIRegisterRenderGL(CVaapiProxy* winSystem, bool& general, bool& deepColor);
+#endif
+#if defined(HAS_GLES)
+void VAAPIRegisterRenderGLES(CVaapiProxy* winSystem, bool& general, bool& deepColor);
+#endif
 }
 }
 }
