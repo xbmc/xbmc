xbmc/xbmc

commit 61662026a99e0792e06b86b50efd9a8b162a76fe

Texture: Let factory methods return std::unique_ptr<CTexture>

xbmc/guilib/Texture.cpp
@@ -7,23 +7,32 @@
  */
 
 #include "Texture.h"
-#include "ServiceBroker.h"
-#include "utils/log.h"
-#include "utils/URIUtils.h"
+
 #include "DDSImage.h"
+#include "ServiceBroker.h"
+#include "URL.h"
+#include "commons/ilog.h"
 #include "filesystem/File.h"
 #include "filesystem/ResourceFile.h"
 #include "filesystem/XbtFile.h"
+#include "guilib/iimage.h"
+#include "guilib/imagefactory.h"
+#include "utils/URIUtils.h"
+#include "utils/log.h"
 #if defined(TARGET_DARWIN_EMBEDDED)
 #include <ImageIO/ImageIO.h>
 #include "filesystem/File.h"
 #endif
 #if defined(TARGET_ANDROID)
-#include "URL.h"
 #include "platform/android/filesystem/AndroidAppFile.h"
 #endif
 #include "rendering/RenderSystem.h"
 #include "utils/MemUtils.h"
+
+#include <algorithm>
+#include <cstring>
+#include <exception>
+#include <utility>
 
 /************************************************************************/
 /*                                                                      */
@@ -180,11 +189,11 @@ void CTexture::ClampToEdge()
   }
 }
 
-CTexture* CTexture::LoadFromFile(const std::string& texturePath,
-                                 unsigned int idealWidth,
-                                 unsigned int idealHeight,
-                                 bool requirePixels,
-                                 const std::string& strMimeType)
+std::unique_ptr<CTexture> CTexture::LoadFromFile(const std::string& texturePath,
+                                                 unsigned int idealWidth,
+                                                 unsigned int idealHeight,
+                                                 bool requirePixels,
+                                                 const std::string& strMimeType)
 {
 #if defined(TARGET_ANDROID)
   CURL url(texturePath);
@@ -201,31 +210,28 @@ CTexture* CTexture::LoadFromFile(const std::string& texturePath,
       if (!inputBuffSize)
         return NULL;
 
-      CTexture* texture = CTexture::CreateTexture();
+      std::unique_ptr<CTexture> texture = CTexture::CreateTexture();
       texture->LoadFromMemory(width, height, width*4, XB_FMT_RGBA8, true, inputBuff);
-      delete [] inputBuff;
       return texture;
     }
   }
 #endif
-  CTexture* texture = CTexture::CreateTexture();
+  std::unique_ptr<CTexture> texture = CTexture::CreateTexture();
   if (texture->LoadFromFileInternal(texturePath, idealWidth, idealHeight, requirePixels, strMimeType))
     return texture;
-  delete texture;
-  return NULL;
+  return {};
 }
 
-CTexture* CTexture::LoadFromFileInMemory(unsigned char* buffer,
-                                         size_t bufferSize,
-                                         const std::string& mimeType,
-                                         unsigned int idealWidth,
-                                         unsigned int idealHeight)
+std::unique_ptr<CTexture> CTexture::LoadFromFileInMemory(unsigned char* buffer,
+                                                         size_t bufferSize,
+                                                         const std::string& mimeType,
+                                                         unsigned int idealWidth,
+                                                         unsigned int idealHeight)
 {
-  CTexture* texture = CTexture::CreateTexture();
+  std::unique_ptr<CTexture> texture = CTexture::CreateTexture();
   if (texture->LoadFromFileInMem(buffer, bufferSize, mimeType, idealWidth, idealHeight))
     return texture;
-  delete texture;
-  return NULL;
+  return {};
 }
 
 bool CTexture::LoadFromFileInternal(const std::string& texturePath,

xbmc/guilib/GUITextureD3D.cpp
@@ -36,7 +36,7 @@ CGUITextureD3D* CGUITextureD3D::Clone() const
 
 void CGUITextureD3D::Begin(UTILS::COLOR::Color color)
 {
-  CTexture* texture = m_texture.m_textures[m_currentFrame];
+  CTexture* texture = m_texture.m_textures[m_currentFrame].get();
   texture->LoadToGPU();
 
   if (m_diffuse.size())
@@ -113,14 +113,14 @@ void CGUITextureD3D::Draw(float *x, float *y, float *z, const CRect &texture, co
   }
   verts[3].color = xcolor;
 
-  CDXTexture* tex = (CDXTexture *)m_texture.m_textures[m_currentFrame];
+  CDXTexture* tex = static_cast<CDXTexture*>(m_texture.m_textures[m_currentFrame].get());
   CGUIShaderDX* pGUIShader = DX::Windowing()->GetGUIShader();
 
   pGUIShader->Begin(m_diffuse.size() ? SHADER_METHOD_RENDER_MULTI_TEXTURE_BLEND : SHADER_METHOD_RENDER_TEXTURE_BLEND);
 
   if (m_diffuse.size())
   {
-    CDXTexture* diff = (CDXTexture *)m_diffuse.m_textures[0];
+    CDXTexture* diff = static_cast<CDXTexture*>(m_diffuse.m_textures[0].get());
     ID3D11ShaderResourceView* resource[] = { tex->GetShaderResource(), diff->GetShaderResource() };
     pGUIShader->SetShaderViews(ARRAYSIZE(resource), resource);
   }

xbmc/guilib/GUITextureGLES.cpp
@@ -39,7 +39,7 @@ CGUITextureGLES* CGUITextureGLES::Clone() const
 
 void CGUITextureGLES::Begin(UTILS::COLOR::Color color)
 {
-  CTexture* texture = m_texture.m_textures[m_currentFrame];
+  CTexture* texture = m_texture.m_textures[m_currentFrame].get();
   texture->LoadToGPU();
   if (m_diffuse.size())
     m_diffuse.m_textures[0]->LoadToGPU();

xbmc/guilib/Texture.h
@@ -8,8 +8,14 @@
 
 #pragma once
 
-#include "XBTF.h"
-#include "guilib/imagefactory.h"
+#include "guilib/TextureFormats.h"
+
+#include <cstddef>
+#include <memory>
+#include <string>
+
+class IImage;
+
 
 #pragma pack(1)
 struct COLOR {unsigned char b,g,r,x;};	// Windows GDI expects 4bytes per color
@@ -32,9 +38,9 @@ public:
   CTexture(unsigned int width = 0, unsigned int height = 0, unsigned int format = XB_FMT_A8R8G8B8);
   virtual ~CTexture();
 
-  static CTexture* CreateTexture(unsigned int width = 0,
-                                 unsigned int height = 0,
-                                 unsigned int format = XB_FMT_A8R8G8B8);
+  static std::unique_ptr<CTexture> CreateTexture(unsigned int width = 0,
+                                                 unsigned int height = 0,
+                                                 unsigned int format = XB_FMT_A8R8G8B8);
 
   /*! \brief Load a texture from a file
    Loads a texture from a file, restricting in size if needed based on maxHeight and maxWidth.
@@ -43,13 +49,13 @@ public:
    \param idealWidth the ideal width of the texture (defaults to 0, no ideal width).
    \param idealHeight the ideal height of the texture (defaults to 0, no ideal height).
    \param strMimeType mimetype of the given texture if available (defaults to empty)
-   \return a CTexture pointer to the created texture - NULL if the texture failed to load.
+   \return a CTexture std::unique_ptr to the created texture - nullptr if the texture failed to load.
    */
-  static CTexture* LoadFromFile(const std::string& texturePath,
-                                unsigned int idealWidth = 0,
-                                unsigned int idealHeight = 0,
-                                bool requirePixels = false,
-                                const std::string& strMimeType = "");
+  static std::unique_ptr<CTexture> LoadFromFile(const std::string& texturePath,
+                                                unsigned int idealWidth = 0,
+                                                unsigned int idealHeight = 0,
+                                                bool requirePixels = false,
+                                                const std::string& strMimeType = "");
 
   /*! \brief Load a texture from a file in memory
    Loads a texture from a file in memory, restricting in size if needed based on maxHeight and maxWidth.
@@ -59,13 +65,13 @@ public:
    \param mimeType the mime type of the file in buffer.
    \param idealWidth the ideal width of the texture (defaults to 0, no ideal width).
    \param idealHeight the ideal height of the texture (defaults to 0, no ideal height).
-   \return a CTexture pointer to the created texture - NULL if the texture failed to load.
+   \return a CTexture std::unique_ptr to the created texture - nullptr if the texture failed to load.
    */
-  static CTexture* LoadFromFileInMemory(unsigned char* buffer,
-                                        size_t bufferSize,
-                                        const std::string& mimeType,
-                                        unsigned int idealWidth = 0,
-                                        unsigned int idealHeight = 0);
+  static std::unique_ptr<CTexture> LoadFromFileInMemory(unsigned char* buffer,
+                                                        size_t bufferSize,
+                                                        const std::string& mimeType,
+                                                        unsigned int idealWidth = 0,
+                                                        unsigned int idealHeight = 0);
 
   bool LoadFromMemory(unsigned int width, unsigned int height, unsigned int pitch, unsigned int format, bool hasAlpha, const unsigned char* pixels);
   bool LoadPaletted(unsigned int width, unsigned int height, unsigned int pitch, unsigned int format, const unsigned char *pixels, const COLOR *palette);

xbmc/guilib/TextureDX.cpp
@@ -11,9 +11,13 @@
 #include "utils/MemUtils.h"
 #include "utils/log.h"
 
-CTexture* CTexture::CreateTexture(unsigned int width, unsigned int height, unsigned int format)
+#include <memory>
+
+std::unique_ptr<CTexture> CTexture::CreateTexture(unsigned int width,
+                                                  unsigned int height,
+                                                  unsigned int format)
 {
-  return new CDXTexture(width, height, format);
+  return std::make_unique<CDXTexture>(width, height, format);
 }
 
 CDXTexture::CDXTexture(unsigned int width, unsigned int height, unsigned int format)

xbmc/guilib/TextureGL.cpp
@@ -16,9 +16,13 @@
 #include "utils/MemUtils.h"
 #include "utils/log.h"
 
-CTexture* CTexture::CreateTexture(unsigned int width, unsigned int height, unsigned int format)
+#include <memory>
+
+std::unique_ptr<CTexture> CTexture::CreateTexture(unsigned int width,
+                                                  unsigned int height,
+                                                  unsigned int format)
 {
-  return new CGLTexture(width, height, format);
+  return std::make_unique<CGLTexture>(width, height, format);
 }
 
 CGLTexture::CGLTexture(unsigned int width, unsigned int height, unsigned int format)

commit 7dae6615a20985504c71d5ab485379794cee0f1a

Texture: Fix memory leak on Android
Was introduced in 61662026a9.

xbmc/guilib/Texture.cpp
@@ -212,6 +212,7 @@ std::unique_ptr<CTexture> CTexture::LoadFromFile(const std::string& texturePath,
 
       std::unique_ptr<CTexture> texture = CTexture::CreateTexture();
       texture->LoadFromMemory(width, height, width*4, XB_FMT_RGBA8, true, inputBuff);
+      delete[] inputBuff;
       return texture;
     }
   }