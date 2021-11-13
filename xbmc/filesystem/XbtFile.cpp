xbmc/xbmc

commit c0ece1436fa8198622f1e159ef81aa6e4cf11704

XbtFile: Adapt to changes in CTextureBundleXBT

xbmc/filesystem/XbtFile.cpp
@@ -16,9 +16,11 @@
 #include "utils/StringUtils.h"
 
 #include <algorithm>
-#include <string.h>
-
-#include <lzo/lzo1x.h>
+#include <climits>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <utility>
 
 namespace XFILE
 {
@@ -63,17 +65,14 @@ bool CXbtFile::Open(const CURL& url)
   m_positionWithinFrame = 0;
   m_positionTotal = 0;
 
-  m_unpackedFrames.assign(frames.size(), nullptr);
+  m_unpackedFrames.resize(frames.size());
 
   return true;
 }
 
 void CXbtFile::Close()
 {
-  for (const auto& unpackedFrame : m_unpackedFrames)
-    delete [] unpackedFrame;
   m_unpackedFrames.clear();
-
   m_frameIndex = 0;
   m_positionWithinFrame = 0;
   m_positionTotal = 0;
@@ -166,17 +165,18 @@ ssize_t CXbtFile::Read(void* lpBuf, size_t uiBufSize)
     const CXBTFFrame& frame = frames[m_frameIndex];
 
     // check if we have already unpacked the current frame
-    if (m_unpackedFrames[m_frameIndex] == nullptr)
+    if (!m_unpackedFrames[m_frameIndex].empty())
     {
       // unpack the data from the current frame
-      uint8_t* unpackedFrame = CTextureBundleXBT::UnpackFrame(*m_xbtfReader.get(), frame);
-      if (unpackedFrame == nullptr)
+      std::vector<uint8_t> unpackedFrame =
+          CTextureBundleXBT::UnpackFrame(*m_xbtfReader.get(), frame);
+      if (unpackedFrame.empty())
       {
         Close();
         return -1;
       }
 
-      m_unpackedFrames[m_frameIndex] = unpackedFrame;
+      m_unpackedFrames[m_frameIndex] = std::move(unpackedFrame);
     }
 
     // determine how many bytes we need to copy from the current frame
@@ -186,7 +186,7 @@ ssize_t CXbtFile::Read(void* lpBuf, size_t uiBufSize)
       bytesToCopy = std::min(remaining, static_cast<size_t>(remainingBytesInFrame));
 
     // copy the data
-    memcpy(lpBuf, m_unpackedFrames[m_frameIndex] + m_positionWithinFrame, bytesToCopy);
+    memcpy(lpBuf, m_unpackedFrames[m_frameIndex].data() + m_positionWithinFrame, bytesToCopy);
     m_positionWithinFrame += bytesToCopy;
     m_positionTotal += bytesToCopy;
     remaining -= bytesToCopy;
@@ -245,17 +245,18 @@ int64_t CXbtFile::Seek(int64_t iFilePosition, int iWhence)
     const CXBTFFrame& frame = frames[m_frameIndex];
 
     // check if we have already unpacked the current frame
-    if (m_unpackedFrames[m_frameIndex] == nullptr)
+    if (m_unpackedFrames[m_frameIndex].empty())
     {
       // unpack the data from the current frame
-      uint8_t* unpackedFrame = CTextureBundleXBT::UnpackFrame(*m_xbtfReader.get(), frame);
-      if (unpackedFrame == nullptr)
+      std::vector<uint8_t> unpackedFrame =
+          CTextureBundleXBT::UnpackFrame(*m_xbtfReader.get(), frame);
+      if (unpackedFrame.empty())
       {
         Close();
         return -1;
       }
 
-      m_unpackedFrames[m_frameIndex] = unpackedFrame;
+      m_unpackedFrames[m_frameIndex] = std::move(unpackedFrame);
     }
 
     int64_t remainingBytesToSeek = newPosition - m_positionTotal;

xbmc/filesystem/XbtFile.h
@@ -13,7 +13,11 @@
 #include "guilib/XBTF.h"
 #include "guilib/XBTFReader.h"
 
+#include <cstdint>
+#include <cstdio>
 #include <vector>
+
+#include "PlatformDefs.h"
 
 namespace XFILE
 {
@@ -58,6 +62,6 @@ private:
   uint64_t m_positionWithinFrame = 0;
   int64_t m_positionTotal = 0;
 
-  std::vector<uint8_t*> m_unpackedFrames;
+  std::vector<std::vector<uint8_t>> m_unpackedFrames;
 };
 }

commit e9aeb3fc6ae6d4cfdf5f0f6a8187b7f91a16497c

XbtFile: Fix logic error
Was introduced in c0ece1436f.

xbmc/filesystem/XbtFile.cpp
@@ -165,7 +165,7 @@ ssize_t CXbtFile::Read(void* lpBuf, size_t uiBufSize)
     const CXBTFFrame& frame = frames[m_frameIndex];
 
     // check if we have already unpacked the current frame
-    if (!m_unpackedFrames[m_frameIndex].empty())
+    if (m_unpackedFrames[m_frameIndex].empty())
     {
       // unpack the data from the current frame
       std::vector<uint8_t> unpackedFrame =