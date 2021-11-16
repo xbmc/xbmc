99e8983066dc0349a2f14c187b915aea037bc7a1
Garrett Brown <themagnificentmrb@gmail.com>
2021-11-16 06:40:28 -0800

    Controllers: Add dialog for controller ports

---
+++ xbmc/games/controllers/windows/GUIPortDefines.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (C) 2021 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+// Dialog title
+#define CONTROL_PORT_DIALOG_LABEL 2
+
+// GUI control IDs
+#define CONTROL_PORT_LIST 3
+
+// GUI button IDs
+#define CONTROL_CLOSE_BUTTON 18
+#define CONTROL_RESET_BUTTON 19
+
+// Skin XML file
+#define PORT_DIALOG_XML "DialogGameControllers.xml"
