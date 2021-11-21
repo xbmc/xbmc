xbmc/xbmc

commit 6a5cd633229a7861f23d56a28f909ae68a7dc232

[GL/GLES] use enum class for shader methods

xbmc/cores/RetroPlayer/rendering/RenderContext.cpp
@@ -68,24 +68,42 @@ bool CRenderContext::IsExtSupported(const char* extension)
 #if defined(HAS_GL) || defined(HAS_GLES)
 namespace
 {
-static ESHADERMETHOD TranslateShaderMethod(GL_SHADER_METHOD method)
+
+#ifdef HAS_GL
+static ShaderMethodGL TranslatShaderMethodGL(GL_SHADER_METHOD method)
 {
   switch (method)
   {
     case GL_SHADER_METHOD::DEFAULT:
-      return SM_DEFAULT;
+      return ShaderMethodGL::SM_DEFAULT;
     case GL_SHADER_METHOD::TEXTURE:
-      return SM_TEXTURE;
-#if defined(HAS_GLES)
-    case GL_SHADER_METHOD::TEXTURE_NOALPHA:
-      return SM_TEXTURE_NOALPHA;
-#endif
+      return ShaderMethodGL::SM_TEXTURE;
     default:
       break;
   }
 
-  return SM_DEFAULT;
+  return ShaderMethodGL::SM_DEFAULT;
 }
+#endif
+#ifdef HAS_GLES
+static ShaderMethodGLES TranslatShaderMethodGLES(GL_SHADER_METHOD method)
+{
+  switch (method)
+  {
+    case GL_SHADER_METHOD::DEFAULT:
+      return ShaderMethodGLES::SM_DEFAULT;
+    case GL_SHADER_METHOD::TEXTURE:
+      return ShaderMethodGLES::SM_TEXTURE;
+    case GL_SHADER_METHOD::TEXTURE_NOALPHA:
+      return ShaderMethodGLES::SM_TEXTURE_NOALPHA;
+    default:
+      break;
+  }
+
+  return ShaderMethodGLES::SM_DEFAULT;
+}
+#endif
+
 } // namespace
 #endif
 
@@ -94,11 +112,11 @@ void CRenderContext::EnableGUIShader(GL_SHADER_METHOD method)
 #if defined(HAS_GL)
   CRenderSystemGL* rendering = dynamic_cast<CRenderSystemGL*>(m_rendering);
   if (rendering != nullptr)
-    rendering->EnableShader(TranslateShaderMethod(method));
+    rendering->EnableShader(TranslatShaderMethodGL(method));
 #elif HAS_GLES >= 2
   CRenderSystemGLES* renderingGLES = dynamic_cast<CRenderSystemGLES*>(m_rendering);
   if (renderingGLES != nullptr)
-    renderingGLES->EnableGUIShader(TranslateShaderMethod(method));
+    renderingGLES->EnableGUIShader(TranslatShaderMethodGLES(method));
 #endif
 }
 

xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererDRMPRIMEGLES.cpp
@@ -219,7 +219,7 @@ void CRendererDRMPRIMEGLES::DrawBlackBars()
   if (!renderSystem)
     return;
 
-  renderSystem->EnableGUIShader(SM_DEFAULT);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_DEFAULT);
   GLint posLoc = renderSystem->GUIShaderGetPos();
   GLint uniCol = renderSystem->GUIShaderGetUniCol();
 
@@ -311,7 +311,7 @@ void CRendererDRMPRIMEGLES::Render(unsigned int flags, int index)
 
   glBindTexture(GL_TEXTURE_EXTERNAL_OES, buf.texture.GetTexture());
 
-  renderSystem->EnableGUIShader(SM_TEXTURE_RGBA_OES);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_TEXTURE_RGBA_OES);
 
   GLubyte idx[4] = {0, 1, 3, 2}; // Determines order of triangle strip
   GLuint vertexVBO;

xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererMediaCodec.cpp
@@ -122,7 +122,7 @@ bool CRendererMediaCodec::RenderHook(int index)
 
   if (m_currentField != FIELD_FULL)
   {
-    renderSystem->EnableGUIShader(SM_TEXTURE_RGBA_BOB_OES);
+    renderSystem->EnableGUIShader(ShaderMethodGLES::SM_TEXTURE_RGBA_BOB_OES);
     GLint   fieldLoc = renderSystem->GUIShaderGetField();
     GLint   stepLoc = renderSystem->GUIShaderGetStep();
 
@@ -134,7 +134,7 @@ bool CRendererMediaCodec::RenderHook(int index)
     glUniform1f(stepLoc, 1.0f / (float)plane.texheight);
   }
   else
-    renderSystem->EnableGUIShader(SM_TEXTURE_RGBA_OES);
+    renderSystem->EnableGUIShader(ShaderMethodGLES::SM_TEXTURE_RGBA_OES);
 
   GLint   contrastLoc = renderSystem->GUIShaderGetContrast();
   glUniform1f(contrastLoc, m_videoSettings.m_Contrast * 0.02f);

xbmc/cores/VideoPlayer/VideoRenderers/LinuxRendererGL.cpp
@@ -550,7 +550,7 @@ void CLinuxRendererGL::DrawBlackBars()
   Svertex vertices[24];
   GLubyte count = 0;
 
-  m_renderSystem->EnableShader(SM_DEFAULT);
+  m_renderSystem->EnableShader(ShaderMethodGL::SM_DEFAULT);
   GLint posLoc = m_renderSystem->ShaderGetPos();
   GLint uniCol = m_renderSystem->ShaderGetUniCol();
 

xbmc/cores/VideoPlayer/VideoRenderers/LinuxRendererGLES.cpp
@@ -406,7 +406,7 @@ void CLinuxRendererGLES::DrawBlackBars()
   if (!renderSystem)
     return;
 
-  renderSystem->EnableGUIShader(SM_DEFAULT);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_DEFAULT);
   GLint posLoc = renderSystem->GUIShaderGetPos();
   GLint uniCol = renderSystem->GUIShaderGetUniCol();
 

xbmc/cores/VideoPlayer/VideoRenderers/OverlayRendererGL.cpp
@@ -359,7 +359,7 @@ void COverlayGlyphGL::Render(SRenderState& state)
 
 #ifdef HAS_GL
   CRenderSystemGL* renderSystem = dynamic_cast<CRenderSystemGL*>(CServiceBroker::GetRenderSystem());
-  renderSystem->EnableShader(SM_FONTS);
+  renderSystem->EnableShader(ShaderMethodGL::SM_FONTS);
   GLint posLoc  = renderSystem->ShaderGetPos();
   GLint colLoc  = renderSystem->ShaderGetCol();
   GLint tex0Loc = renderSystem->ShaderGetCoord0();
@@ -408,7 +408,7 @@ void COverlayGlyphGL::Render(SRenderState& state)
 
 #else
   CRenderSystemGLES* renderSystem = dynamic_cast<CRenderSystemGLES*>(CServiceBroker::GetRenderSystem());
-  renderSystem->EnableGUIShader(SM_FONTS);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_FONTS);
   GLint posLoc  = renderSystem->GUIShaderGetPos();
   GLint colLoc  = renderSystem->GUIShaderGetCol();
   GLint tex0Loc = renderSystem->GUIShaderGetCoord0();
@@ -497,9 +497,9 @@ void COverlayTextureGL::Render(SRenderState& state)
   int glslMajor, glslMinor;
   renderSystem->GetGLSLVersion(glslMajor, glslMinor);
   if (glslMajor >= 2 || (glslMajor == 1 && glslMinor >= 50))
-    renderSystem->EnableShader(SM_TEXTURE_LIM);
+    renderSystem->EnableShader(ShaderMethodGL::SM_TEXTURE_LIM);
   else
-    renderSystem->EnableShader(SM_TEXTURE);
+    renderSystem->EnableShader(ShaderMethodGL::SM_TEXTURE);
 
   GLint posLoc = renderSystem->ShaderGetPos();
   GLint tex0Loc = renderSystem->ShaderGetCoord0();
@@ -572,7 +572,7 @@ void COverlayTextureGL::Render(SRenderState& state)
 
 #else
   CRenderSystemGLES* renderSystem = dynamic_cast<CRenderSystemGLES*>(CServiceBroker::GetRenderSystem());
-  renderSystem->EnableGUIShader(SM_TEXTURE);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_TEXTURE);
   GLint posLoc = renderSystem->GUIShaderGetPos();
   GLint colLoc = renderSystem->GUIShaderGetCol();
   GLint tex0Loc = renderSystem->GUIShaderGetCoord0();

xbmc/guilib/GUIFontTTFGL.cpp
@@ -120,7 +120,7 @@ void CGUIFontTTFGL::LastEnd()
 {
 #ifdef HAS_GL
   CRenderSystemGL* renderSystem = dynamic_cast<CRenderSystemGL*>(CServiceBroker::GetRenderSystem());
-  renderSystem->EnableShader(SM_FONTS);
+  renderSystem->EnableShader(ShaderMethodGL::SM_FONTS);
 
   GLint posLoc = renderSystem->ShaderGetPos();
   GLint colLoc = renderSystem->ShaderGetCol();
@@ -174,7 +174,7 @@ void CGUIFontTTFGL::LastEnd()
 #else
   // GLES 2.0 version.
   CRenderSystemGLES* renderSystem = dynamic_cast<CRenderSystemGLES*>(CServiceBroker::GetRenderSystem());
-  renderSystem->EnableGUIShader(SM_FONTS);
+  renderSystem->EnableGUIShader(ShaderMethodGLES::SM_FONTS);
 
   GLint posLoc  = renderSystem->GUIShaderGetPos();
   GLint colLoc  = renderSystem->GUIShaderGetCol();

xbmc/guilib/GUITextureGL.cpp
@@ -57,11 +57,11 @@ void CGUITextureGL::Begin(UTILS::COLOR::Color color)
   {
     if (m_col[0] == 255 && m_col[1] == 255 && m_col[2] == 255 && m_col[3] == 255 )
     {
-      m_renderSystem->EnableShader(SM_MULTI);
+      m_renderSystem->EnableShader(ShaderMethodGL::SM_MULTI);
     }
     else
     {
-      m_renderSystem->EnableShader(SM_MULTI_BLENDCOLOR);
+      m_renderSystem->EnableShader(ShaderMethodGL::SM_MULTI_BLENDCOLOR);
     }
 
     hasAlpha |= m_diffuse.m_textures[0]->HasAlpha();
@@ -72,11 +72,11 @@ void CGUITextureGL::Begin(UTILS::COLOR::Color color)
   {
     if (m_col[0] == 255 && m_col[1] == 255 && m_col[2] == 255 && m_col[3] == 255)
     {
-      m_renderSystem->EnableShader(SM_TEXTURE_NOBLEND);
+      m_renderSystem->EnableShader(ShaderMethodGL::SM_TEXTURE_NOBLEND);
     }
     else
     {
-      m_renderSystem->EnableShader(SM_TEXTURE);
+      m_renderSystem->EnableShader(ShaderMethodGL::SM_TEXTURE);
     }
   }
 
@@ -274,9 +274,9 @@ void CGUITexture::DrawQuad(const CRect& rect,
   }vertex[4];
 
   if (texture)
-    renderSystem->EnableShader(SM_TEXTURE);
+    renderSystem->EnableShader(ShaderMethodGL::SM_TEXTURE);
   else
-    renderSystem->EnableShader(SM_DEFAULT);
+    renderSystem->EnableShader(ShaderMethodGL::SM_DEFAULT);
 
   GLint posLoc = renderSystem->ShaderGetPos();
   GLint tex0Loc = renderSystem->ShaderGetCoord0();

commit 7039be01bd5a3b52406e1dda35eddeaada29f728

CRenderContext: fix spelling error introduced in 6a5cd63

xbmc/cores/RetroPlayer/rendering/RenderContext.cpp
@@ -70,7 +70,7 @@ namespace
 {
 
 #ifdef HAS_GL
-static ShaderMethodGL TranslatShaderMethodGL(GL_SHADER_METHOD method)
+static ShaderMethodGL TranslateShaderMethodGL(GL_SHADER_METHOD method)
 {
   switch (method)
   {
@@ -86,7 +86,7 @@ static ShaderMethodGL TranslatShaderMethodGL(GL_SHADER_METHOD method)
 }
 #endif
 #ifdef HAS_GLES
-static ShaderMethodGLES TranslatShaderMethodGLES(GL_SHADER_METHOD method)
+static ShaderMethodGLES TranslateShaderMethodGLES(GL_SHADER_METHOD method)
 {
   switch (method)
   {
@@ -112,11 +112,11 @@ void CRenderContext::EnableGUIShader(GL_SHADER_METHOD method)
 #if defined(HAS_GL)
   CRenderSystemGL* rendering = dynamic_cast<CRenderSystemGL*>(m_rendering);
   if (rendering != nullptr)
-    rendering->EnableShader(TranslatShaderMethodGL(method));
+    rendering->EnableShader(TranslateShaderMethodGL(method));
 #elif HAS_GLES >= 2
   CRenderSystemGLES* renderingGLES = dynamic_cast<CRenderSystemGLES*>(m_rendering);
   if (renderingGLES != nullptr)
-    renderingGLES->EnableGUIShader(TranslatShaderMethodGLES(method));
+    renderingGLES->EnableGUIShader(TranslateShaderMethodGLES(method));
 #endif
 }
 