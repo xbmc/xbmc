ced2dafa93a9c984e6bbf001506d46affb4386fb
fuzzard <fuzzard@kodi.tv>
2021-12-05 18:40:57 +1000

    [tools/depends][target] update meson-cross-file due to deprecation warnings
    Update meson cross file to remove following deprecation warnings
    
    DEPRECATION: The [paths] section is deprecated, use the [built-in options] section instead.
    DEPRECATION: c_args in the [properties] section of the machine file is deprecated, use the [built-in options] section.
    DEPRECATION: c_link_args in the [properties] section of the machine file is deprecated, use the [built-in options] section.
    DEPRECATION: cpp_args in the [properties] section of the machine file is deprecated, use the [built-in options] section.
    DEPRECATION: cpp_link_args in the [properties] section of the machine file is deprecated, use the [built-in options] section.
    
    This change brings a requirement of meson 0.56.0 to build kodi as per
    
    https://mesonbuild.com/Machine-files.html#meson-builtin-options

--- tools/depends/target/meson-cross-setup.sh
+++ tools/depends/target/meson-cross-setup.sh
@@ -15,13 +15,16 @@ cpu = '$CPU'
 endian = 'little'
 
 [properties]
+pkg_config_libdir = '$PREFIX/lib/pkgconfig'
+
+[built-in options]
 $($NATIVEPREFIX/bin/python3 -c "print('c_args = {}'.format([x for x in '$CFLAGS'.split() if x not in ['-g', '-gdwarf-2']]))")
 $($NATIVEPREFIX/bin/python3 -c "print('c_link_args = {}'.format([x for x in '$LDFLAGS'.split()]))")
 $($NATIVEPREFIX/bin/python3 -c "print('cpp_args = {}'.format([x for x in '$CXXFLAGS'.split() if x not in ['-g', '-gdwarf-2']]))")
 $($NATIVEPREFIX/bin/python3 -c "print('cpp_link_args = {}'.format([x for x in '$LDFLAGS'.split()]))")
-
-[paths]
+default_library = 'static'
 prefix = '$PREFIX'
 libdir = 'lib'
 bindir = 'bin'
+includedir = 'include'
 EOF
