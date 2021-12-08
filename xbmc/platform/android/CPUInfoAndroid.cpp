7678678c0dc73058214baa5d9fbcdf141da118a4
thexai <58434170+thexai@users.noreply.github.com>
2021-12-08 10:24:29 +0100

    [Android][Fix] System Info displays empty/incorrect CPU info values

--- xbmc/platform/android/CPUInfoAndroid.cpp
+++ xbmc/platform/android/CPUInfoAndroid.cpp
@@ -8,9 +8,13 @@
 
 #include "CPUInfoAndroid.h"
 
+#include "URL.h"
+#include "utils/StringUtils.h"
 #include "utils/Temperature.h"
 
 #include "platform/android/activity/AndroidFeatures.h"
+
+#include <array>
 
 std::shared_ptr<CCPUInfo> CCPUInfo::GetCPUInfo()
 {
@@ -19,6 +23,28 @@ std::shared_ptr<CCPUInfo> CCPUInfo::GetCPUInfo()
 
 CCPUInfoAndroid::CCPUInfoAndroid()
 {
+  m_posixFile = std::make_unique<CPosixFile>();
+
+  if (m_posixFile && m_posixFile->Open(CURL("/proc/cpuinfo")))
+  {
+    std::array<char, 2048> buffer = {};
+
+    if (0 < m_posixFile->Read(buffer.data(), buffer.size()))
+    {
+      for (const auto& line : StringUtils::Split(buffer.data(), '\n'))
+      {
+        if (line.find("vendor_id") != std::string::npos)
+          m_cpuVendor = line.substr(line.find(':') + 2);
+        else if (line.find("model name") != std::string::npos)
+          m_cpuModel = line.substr(line.find(':') + 2);
+        else if (line.find("BogoMIPS") != std::string::npos)
+          m_cpuBogoMips = line.substr(line.find(':') + 2);
+      }
+    }
+
+    m_posixFile->Close();
+  }
+
   m_cpuCount = CAndroidFeatures::GetCPUCount();
 
   for (int i = 0; i < m_cpuCount; i++)
@@ -30,4 +56,24 @@ CCPUInfoAndroid::CCPUInfoAndroid()
 
   if (CAndroidFeatures::HasNeon())
     m_cpuFeatures |= CPU_FEATURE_NEON;
+}
+
+float CCPUInfoAndroid::GetCPUFrequency()
+{
+  float freq = 0.f;
+
+  if (!m_posixFile)
+    return freq;
+
+  if (m_posixFile->Open(CURL("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")))
+  {
+    std::array<char, 32> buffer = {};
+
+    if (0 < m_posixFile->Read(buffer.data(), buffer.size()))
+      freq = std::atof(buffer.data()) / 1000;
+
+    m_posixFile->Close();
+  }
+
+  return freq;
 }
