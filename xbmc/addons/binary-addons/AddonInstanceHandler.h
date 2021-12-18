958c70d6771ad21a0380b5653101fcad2547e88f
Alwin Esch <alwin.esch@web.de>
2021-12-18 01:19:37 +0100

    [addons] clang clean up about AddonInstanceHandler.[cpp|h]
    To have more clean on comming changes

--- xbmc/addons/binary-addons/AddonInstanceHandler.h
+++ xbmc/addons/binary-addons/AddonInstanceHandler.h
@@ -17,45 +17,44 @@
 namespace ADDON
 {
 
-  class IAddonInstanceHandler
-  {
-  public:
-    IAddonInstanceHandler(ADDON_TYPE type,
-                          const AddonInfoPtr& addonInfo,
-                          KODI_HANDLE parentInstance = nullptr,
-                          const std::string& instanceID = "");
-    virtual ~IAddonInstanceHandler();
+class IAddonInstanceHandler
+{
+public:
+  IAddonInstanceHandler(ADDON_TYPE type,
+                        const AddonInfoPtr& addonInfo,
+                        KODI_HANDLE parentInstance = nullptr,
+                        const std::string& instanceID = "");
+  virtual ~IAddonInstanceHandler();
 
-    ADDON_TYPE UsedType() const { return m_type; }
-    const std::string& InstanceID() { return m_instanceId; }
+  ADDON_TYPE UsedType() const { return m_type; }
+  const std::string& InstanceID() { return m_instanceId; }
 
-    std::string ID() const;
-    std::string Name() const;
-    std::string Author() const;
-    std::string Icon() const;
-    std::string Path() const;
-    std::string Profile() const;
-    AddonVersion Version() const;
+  std::string ID() const;
+  std::string Name() const;
+  std::string Author() const;
+  std::string Icon() const;
+  std::string Path() const;
+  std::string Profile() const;
+  AddonVersion Version() const;
 
-    ADDON_STATUS CreateInstance(KODI_HANDLE instance);
-    void DestroyInstance();
-    const AddonDllPtr& Addon() const { return m_addon; }
-    AddonInfoPtr GetAddonInfo() const { return m_addonInfo; }
+  ADDON_STATUS CreateInstance(KODI_HANDLE instance);
+  void DestroyInstance();
+  const AddonDllPtr& Addon() const { return m_addon; }
+  AddonInfoPtr GetAddonInfo() const { return m_addonInfo; }
 
-    virtual void OnPreInstall() {}
-    virtual void OnPostInstall(bool update, bool modal) {}
-    virtual void OnPreUnInstall() {}
-    virtual void OnPostUnInstall() {}
+  virtual void OnPreInstall() {}
+  virtual void OnPostInstall(bool update, bool modal) {}
+  virtual void OnPreUnInstall() {}
+  virtual void OnPostUnInstall() {}
 
-  private:
-    ADDON_TYPE m_type;
-    std::string m_instanceId;
-    KODI_HANDLE m_parentInstance;
-    AddonInfoPtr m_addonInfo;
-    BinaryAddonBasePtr m_addonBase;
-    AddonDllPtr m_addon;
-    static CCriticalSection m_cdSec;
-  };
+private:
+  ADDON_TYPE m_type;
+  std::string m_instanceId;
+  KODI_HANDLE m_parentInstance;
+  AddonInfoPtr m_addonInfo;
+  BinaryAddonBasePtr m_addonBase;
+  AddonDllPtr m_addon;
+  static CCriticalSection m_cdSec;
+};
 
 } /* namespace ADDON */
-
