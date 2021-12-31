8a5174a9fd6391935e718e939d2731ee6f6c1fcc
Lukas Rusak <lorusak@gmail.com>
2021-12-31 15:29:39 -0800

    TestEndTime: add new test and template CommonTests

--- xbmc/threads/test/TestEndTime.cpp
+++ xbmc/threads/test/TestEndTime.cpp
@@ -15,23 +15,24 @@ using namespace std::chrono_literals;
 namespace
 {
 
-void CommonTests(XbmcThreads::EndTime<>& endTime)
+template<typename T = std::chrono::milliseconds>
+void CommonTests(XbmcThreads::EndTime<T>& endTime)
 {
   EXPECT_EQ(100ms, endTime.GetInitialTimeoutValue());
-  EXPECT_LT(0ms, endTime.GetStartTime().time_since_epoch());
+  EXPECT_LT(T::zero(), endTime.GetStartTime().time_since_epoch());
 
   EXPECT_FALSE(endTime.IsTimePast());
-  EXPECT_LT(0ms, endTime.GetTimeLeft());
+  EXPECT_LT(T::zero(), endTime.GetTimeLeft());
 
   std::this_thread::sleep_for(100ms);
 
   EXPECT_TRUE(endTime.IsTimePast());
-  EXPECT_EQ(0ms, endTime.GetTimeLeft());
+  EXPECT_EQ(T::zero(), endTime.GetTimeLeft());
 
   endTime.SetInfinite();
-  EXPECT_EQ(std::chrono::milliseconds::max(), endTime.GetInitialTimeoutValue());
+  EXPECT_EQ(T::max(), endTime.GetInitialTimeoutValue());
   endTime.SetExpired();
-  EXPECT_EQ(0ms, endTime.GetInitialTimeoutValue());
+  EXPECT_EQ(T::zero(), endTime.GetInitialTimeoutValue());
 }
 
 } // namespace
@@ -47,6 +48,13 @@ TEST(TestEndTime, DefaultConstructor)
 TEST(TestEndTime, ExplicitConstructor)
 {
   XbmcThreads::EndTime<> endTime(100ms);
+
+  CommonTests(endTime);
+}
+
+TEST(TestEndTime, DoubleMicroSeconds)
+{
+  XbmcThreads::EndTime<std::chrono::duration<double, std::micro>> endTime(100ms);
 
   CommonTests(endTime);
 }
