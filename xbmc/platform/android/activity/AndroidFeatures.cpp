eb536bd0b13c28711c987553631c321cbb430c41
thexai <58434170+thexai@users.noreply.github.com>
2021-12-31 11:30:09 +0100

    [android] Fix: misdetection of CPU feature NEON on ARM64 platforms

--- xbmc/platform/android/activity/AndroidFeatures.cpp
+++ xbmc/platform/android/activity/AndroidFeatures.cpp
@@ -15,8 +15,13 @@
 
 bool CAndroidFeatures::HasNeon()
 {
+  // All ARMv8-based devices support Neon - https://developer.android.com/ndk/guides/cpu-arm-neon
+  if (android_getCpuFamily() == ANDROID_CPU_FAMILY_ARM64)
+    return true;
+
   if (android_getCpuFamily() == ANDROID_CPU_FAMILY_ARM)
     return ((android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0);
+
   return false;
 }
 
