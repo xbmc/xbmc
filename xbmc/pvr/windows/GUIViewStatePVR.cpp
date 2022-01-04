f323fd2d21aed73de5203f3d680e0eef1183456e
phunkyfish <phunkyfish@gmail.com>
2022-01-04 10:34:28 +0000

    [pvr] fix comment so it correctly represents the size sort method for recordings

--- xbmc/pvr/windows/GUIViewStatePVR.cpp
+++ xbmc/pvr/windows/GUIViewStatePVR.cpp
@@ -50,7 +50,10 @@ CGUIViewStateWindowPVRRecordings::CGUIViewStateWindowPVRRecordings(const int win
   AddSortMethod(SortByTime,  180, LABEL_MASKS("%L", "%D", "%L", ""));   // "Duration" : Filename, Duration | Foldername, empty
   AddSortMethod(SortByFile,  561, LABEL_MASKS("%L", "%d", "%L", ""));   // "File"     : Filename, DateTime | Foldername, empty
   if (CServiceBroker::GetPVRManager().Clients()->AnyClientSupportingRecordingsSize())
-    AddSortMethod(SortBySize,  553, LABEL_MASKS("%L", "%I", "%L", "%I")); // "Size"   : Filename, DateTime | Foldername, empty
+  {
+    // "Size" : Filename, Size | Foldername, Size
+    AddSortMethod(SortBySize, 553, LABEL_MASKS("%L", "%I", "%L", "%I"));
+  }
 
   SetSortMethod(CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()
                 ->m_PVRDefaultSortOrder);
