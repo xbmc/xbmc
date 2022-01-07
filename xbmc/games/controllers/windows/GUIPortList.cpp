ab5234d34d99a9bf8abfd5707ae2dbbafc15315a
Garrett Brown <themagnificentmrb@gmail.com>
2022-01-07 13:55:16 -0800

    Port dialog: Fix controller selection

--- xbmc/games/controllers/windows/GUIPortList.cpp
+++ xbmc/games/controllers/windows/GUIPortList.cpp
@@ -263,9 +263,9 @@ void CGUIPortList::OnControllerSelected(const CPortNode& port, const ControllerP
   const bool bConnected = static_cast<bool>(controller);
 
   // Update the game client
-  const bool bSuccess =
-      bConnected ? m_gameClient->Input().DisconnectController(port.GetAddress())
-                 : m_gameClient->Input().ConnectController(port.GetAddress(), controller);
+  const bool bSuccess = bConnected
+                            ? m_gameClient->Input().ConnectController(port.GetAddress(), controller)
+                            : m_gameClient->Input().DisconnectController(port.GetAddress());
 
   if (bSuccess)
   {
