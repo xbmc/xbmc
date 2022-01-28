57ff0da2594ed717ea1b0978bcc2d6719ecf2764
Glenn Guy <glennguy83@gmail.com>
2022-01-28 22:12:49 +1100

    [Android] pass pattern encryption and mode in crypto info

--- xbmc/addons/kodi-dev-kit/include/kodi/versions.h
+++ xbmc/addons/kodi-dev-kit/include/kodi/versions.h
@@ -108,8 +108,8 @@
 #define ADDON_INSTANCE_VERSION_IMAGEDECODER_DEPENDS   "c-api/addon-instance/imagedecoder.h" \
                                                       "addon-instance/ImageDecoder.h"
 
-#define ADDON_INSTANCE_VERSION_INPUTSTREAM            "3.0.2"
-#define ADDON_INSTANCE_VERSION_INPUTSTREAM_MIN        "3.0.1"
+#define ADDON_INSTANCE_VERSION_INPUTSTREAM            "3.1.0"
+#define ADDON_INSTANCE_VERSION_INPUTSTREAM_MIN        "3.1.0"
 #define ADDON_INSTANCE_VERSION_INPUTSTREAM_XML_ID     "kodi.binary.instance.inputstream"
 #define ADDON_INSTANCE_VERSION_INPUTSTREAM_DEPENDS    "c-api/addon-instance/inputstream.h" \
                                                       "c-api/addon-instance/inputstream/demux_packet.h" \
