b2a2008c582d749bb3bae32834a7145eb2cc0935
Lukas Rusak <lorusak@gmail.com>
2022-02-11 22:42:12 -0800

    clang-format: run on RecursiveMutex.h

--- xbmc/threads/platform/RecursiveMutex.h
+++ xbmc/threads/platform/RecursiveMutex.h
@@ -14,38 +14,33 @@
 #include <pthread.h>
 namespace XbmcThreads
 {
-  class CRecursiveMutex
-  {
-    pthread_mutex_t m_mutex;
+class CRecursiveMutex
+{
+  pthread_mutex_t m_mutex;
 
-    // implementation is in threads/platform/pthreads/ThreadImpl.cpp
-    static pthread_mutexattr_t* getRecursiveAttr();
+  // implementation is in threads/platform/pthreads/ThreadImpl.cpp
+  static pthread_mutexattr_t* getRecursiveAttr();
 
-  public:
+public:
+  CRecursiveMutex(const CRecursiveMutex&) = delete;
+  CRecursiveMutex& operator=(const CRecursiveMutex&) = delete;
 
-    CRecursiveMutex(const CRecursiveMutex&) = delete;
-    CRecursiveMutex& operator=(const CRecursiveMutex&) = delete;
+  inline CRecursiveMutex() { pthread_mutex_init(&m_mutex, getRecursiveAttr()); }
 
-    inline CRecursiveMutex() { pthread_mutex_init(&m_mutex,getRecursiveAttr()); }
+  inline ~CRecursiveMutex() { pthread_mutex_destroy(&m_mutex); }
 
-    inline ~CRecursiveMutex() { pthread_mutex_destroy(&m_mutex); }
+  inline void lock() { pthread_mutex_lock(&m_mutex); }
 
-    inline void lock() { pthread_mutex_lock(&m_mutex); }
+  inline void unlock() { pthread_mutex_unlock(&m_mutex); }
 
-    inline void unlock() { pthread_mutex_unlock(&m_mutex); }
+  inline bool try_lock() { return (pthread_mutex_trylock(&m_mutex) == 0); }
 
-    inline bool try_lock() { return (pthread_mutex_trylock(&m_mutex) == 0); }
-
-    inline std::recursive_mutex::native_handle_type  native_handle()
-    {
-      return &m_mutex;
-    }
-  };
-}
+  inline std::recursive_mutex::native_handle_type native_handle() { return &m_mutex; }
+};
+} // namespace XbmcThreads
 #elif (defined TARGET_WINDOWS)
 namespace XbmcThreads
 {
-  typedef std::recursive_mutex CRecursiveMutex;
+typedef std::recursive_mutex CRecursiveMutex;
 }
 #endif
-
