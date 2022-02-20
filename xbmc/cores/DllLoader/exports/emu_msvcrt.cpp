0282b2e6d05d9adbdf064b6fb7fd4e0489331017
enen92 <92enen@gmail.com>
2022-02-20 17:21:47 +0000

    [Linux][Discs][DllLoader] Always check getmntent result

--- xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
+++ xbmc/cores/DllLoader/exports/emu_msvcrt.cpp
@@ -2026,21 +2026,24 @@ extern "C"
 
   struct mntent *dll_getmntent(FILE *fp)
   {
-    if (fp == NULL)
-      return NULL;
+    if (!fp)
+      return nullptr;
 
+#if defined(TARGET_LINUX)
+    struct mntent* mountPoint = getmntent(fp);
+    if (mountPoint)
+      return mountPoint;
+
+    // warn if this is a kodi vfs file not associated with a mountpoint
     CFile* pFile = g_emuFileWrapper.GetFileXbmcByStream(fp);
     if (pFile)
     {
-      CLog::Log(LOGERROR, "{} - getmntent is not implemented for our virtual filesystem",
-                __FUNCTION__);
-      return NULL;
+      CLog::LogF(LOGWARNING, "getmntent is not implemented for our virtual filesystem");
     }
-#if defined(TARGET_LINUX)
-    return getmntent(fp);
+    return nullptr;
 #else
-    CLog::Log(LOGWARNING, "{} - unimplemented function called", __FUNCTION__);
-    return NULL;
+    CLog::LogF(LOGWARNING, "Unimplemented function called");
+    return nullptr;
 #endif
   }
 
