1b6f51c840df4f667c9aea84b2ab672d0dde37bc
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-03-04 00:12:39 +0100

    [Android] Refactor CXBMCApp: Make member m_playback_state non-static.

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -110,8 +110,6 @@ using namespace std::chrono_literals;
 
 std::unique_ptr<CXBMCApp> CXBMCApp::m_appinstance;
 
-uint32_t CXBMCApp::m_playback_state = PLAYBACK_STATE_STOPPED;
-
 CXBMCApp::CXBMCApp(ANativeActivity* nativeActivity, IInputHandler& inputHandler)
   : CJNIMainActivity(nativeActivity),
     CJNIBroadcastReceiver(CJNIContext::getPackageName() + ".XBMCBroadcastReceiver"),
