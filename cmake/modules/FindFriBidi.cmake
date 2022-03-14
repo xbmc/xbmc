af0820f4b6681e262a5d98e93c973e3e47920491
Rechi <Rechi@users.noreply.github.com>
2022-03-13 18:37:06 -0700

    [cmake] declare directories of external libraries as system headers
    This allows the compiler and checker tools to ignore warnings in the library headers.

--- cmake/modules/FindFriBidi.cmake
+++ cmake/modules/FindFriBidi.cmake
@@ -33,15 +33,18 @@ find_package_handle_standard_args(FriBidi
 if(FRIBIDI_FOUND)
   set(FRIBIDI_LIBRARIES ${FRIBIDI_LIBRARY})
   set(FRIBIDI_INCLUDE_DIRS ${FRIBIDI_INCLUDE_DIR})
-  if(PC_FRIBIDI_CFLAGS)
-    set(FRIBIDI_DEFINITIONS ${PC_FRIBIDI_CFLAGS})
+  if(PC_FRIBIDI_INCLUDE_DIRS)
+    list(APPEND FRIBIDI_INCLUDE_DIRS ${PC_FRIBIDI_INCLUDE_DIRS})
+  endif()
+  if(PC_FRIBIDI_CFLAGS_OTHER)
+    set(FRIBIDI_DEFINITIONS ${PC_FRIBIDI_CFLAGS_OTHER})
   endif()
 
   if(NOT TARGET FriBidi::FriBidi)
     add_library(FriBidi::FriBidi UNKNOWN IMPORTED)
     set_target_properties(FriBidi::FriBidi PROPERTIES
                                            IMPORTED_LOCATION "${FRIBIDI_LIBRARY}"
-                                           INTERFACE_INCLUDE_DIRECTORIES "${FRIBIDI_INCLUDE_DIR}"
+                                           INTERFACE_INCLUDE_DIRECTORIES "${FRIBIDI_INCLUDE_DIRS}"
                                            INTERFACE_COMPILE_OPTIONS "${FRIBIDI_DEFINITIONS}")
   endif()
 endif()
