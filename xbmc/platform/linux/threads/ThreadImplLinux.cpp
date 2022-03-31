xbmc/xbmc

commit a5d17f691212e93d0a1eeadfd3b8fe19d1237fee

CThreadImplLinux: use gettid for android and linux

xbmc/platform/linux/threads/ThreadImplLinux.cpp
@@ -15,12 +15,7 @@
 #include <mutex>
 
 #include <sys/resource.h>
-
-#if defined(TARGET_ANDROID)
 #include <unistd.h>
-#else
-#include <sys/syscall.h>
-#endif
 
 namespace
 {
@@ -48,15 +43,6 @@ int ThreadPriorityToNativePriority(const ThreadPriority& priority)
 }
 
 } // namespace
-
-static pid_t GetCurrentThreadPid_()
-{
-#if defined(TARGET_ANDROID)
-  return gettid();
-#else
-  return syscall(SYS_gettid);
-#endif
-}
 
 // We need to return what the best number than can be passed
 // to SetPriority is. It will basically be relative to the
@@ -101,7 +87,7 @@ std::unique_ptr<IThreadImpl> IThreadImpl::CreateThreadImpl(std::thread::native_h
 }
 
 CThreadImplLinux::CThreadImplLinux(std::thread::native_handle_type handle)
-  : IThreadImpl(handle), m_threadID(GetCurrentThreadPid_())
+  : IThreadImpl(handle), m_threadID(gettid())
 {
 }
 

commit ebe92872214756c50f0bd5342c1964141f7803b7

CThreadImplLinux: fix build with uClibc
Fixes build error introduced by
https://github.com/xbmc/xbmc/commit/a5d17f691212e93d0a1eeadfd3b8fe19d1237fee

xbmc/platform/linux/threads/ThreadImplLinux.cpp:103:37: error:
  ‘gettid’ was not declared in this scope; did you mean ‘getgid’?
  103 |   : IThreadImpl(handle), m_threadID(gettid())

xbmc/platform/linux/threads/ThreadImplLinux.cpp
@@ -17,6 +17,11 @@
 #include <sys/resource.h>
 #include <unistd.h>
 
+#if !defined(gettid)
+#include <sys/syscall.h>
+#define gettid() syscall(SYS_gettid)
+#endif
+
 namespace
 {
 