dbda4aa65d3040b98ceba50ac94a6129274375cd
Arne Morten Kvarving <spiff@kodi.tv>
2022-04-12 21:27:58 +0200

    GUIMessage: replace defines by constexpr const int

--- xbmc/guilib/GUIMessage.h
+++ xbmc/guilib/GUIMessage.h
@@ -13,159 +13,159 @@
 \brief
 */
 
-#define GUI_MSG_WINDOW_INIT     1   // initialize window
-#define GUI_MSG_WINDOW_DEINIT   2   // deinit window
-#define GUI_MSG_WINDOW_RESET    27  // reset window to initial state
+constexpr const int GUI_MSG_WINDOW_INIT    = 1;  // initialize window
+constexpr const int GUI_MSG_WINDOW_DEINIT  = 2;  // deinit window
+constexpr const int GUI_MSG_WINDOW_RESET   = 27; // reset window to initial state
 
-#define GUI_MSG_SETFOCUS        3   // set focus to control param1=up/down/left/right
-#define GUI_MSG_LOSTFOCUS       4   // control lost focus
+constexpr const int GUI_MSG_SETFOCUS       = 3;  // set focus to control param1=up/down/left/right
+constexpr const int GUI_MSG_LOSTFOCUS      = 4;  // control lost focus
 
-#define GUI_MSG_CLICKED         5   // control has been clicked
+constexpr const int GUI_MSG_CLICKED        = 5;  // control has been clicked
 
-#define GUI_MSG_VISIBLE         6   // set control visible
-#define GUI_MSG_HIDDEN          7   // set control hidden
+constexpr const int GUI_MSG_VISIBLE        = 6;  // set control visible
+constexpr const int GUI_MSG_HIDDEN         = 7;   // set control hidden
 
-#define GUI_MSG_ENABLED         8   // enable control
-#define GUI_MSG_DISABLED        9   // disable control
+constexpr const int GUI_MSG_ENABLED        = 8;   // enable control
+constexpr const int GUI_MSG_DISABLED       = 9;   // disable control
 
-#define GUI_MSG_SET_SELECTED   10   // control = selected
-#define GUI_MSG_SET_DESELECTED 11   // control = not selected
+constexpr const int GUI_MSG_SET_SELECTED   = 10;   // control = selected
+constexpr const int GUI_MSG_SET_DESELECTED = 11;   // control = not selected
 
-#define GUI_MSG_LABEL_ADD      12   // add label control (for controls supporting more then 1 label)
+constexpr const int GUI_MSG_LABEL_ADD      = 12;   // add label control (for controls supporting more then 1 label)
 
-#define GUI_MSG_LABEL_SET      13  // set the label of a control
+constexpr const int GUI_MSG_LABEL_SET      = 13;  // set the label of a control
 
-#define GUI_MSG_LABEL_RESET    14  // clear all labels of a control // add label control (for controls supporting more then 1 label)
+constexpr const int GUI_MSG_LABEL_RESET    = 14;  // clear all labels of a control // add label control (for controls supporting more then 1 label)
 
-#define GUI_MSG_ITEM_SELECTED  15  // ask control 2 return the selected item
-#define GUI_MSG_ITEM_SELECT   16  // ask control 2 select a specific item
-#define GUI_MSG_LABEL2_SET   17
-#define GUI_MSG_SHOWRANGE      18
+constexpr const int GUI_MSG_ITEM_SELECTED  = 15;  // ask control 2 return the selected item
+constexpr const int GUI_MSG_ITEM_SELECT    = 16;  // ask control 2 select a specific item
+constexpr const int GUI_MSG_LABEL2_SET     = 17;
+constexpr const int GUI_MSG_SHOWRANGE      = 18;
 
-#define GUI_MSG_FULLSCREEN  19  // should go to fullscreen window (vis or video)
-#define GUI_MSG_EXECUTE    20  // user has clicked on a button with <execute> tag
+constexpr const int GUI_MSG_FULLSCREEN     = 19;  // should go to fullscreen window (vis or video)
+constexpr const int GUI_MSG_EXECUTE        = 20;  // user has clicked on a button with <execute> tag
 
-#define GUI_MSG_NOTIFY_ALL    21  // message will be send to all active and inactive(!) windows, all active modal and modeless dialogs
+constexpr const int GUI_MSG_NOTIFY_ALL     = 21;  // message will be send to all active and inactive(!) windows, all active modal and modeless dialogs
                                   // dwParam1 must contain an additional message the windows should react on
 
-#define GUI_MSG_REFRESH_THUMBS 22 // message is sent to all windows to refresh all thumbs
+constexpr const int GUI_MSG_REFRESH_THUMBS = 22; // message is sent to all windows to refresh all thumbs
 
-#define GUI_MSG_MOVE          23 // message is sent to the window from the base control class when it's
+constexpr const int GUI_MSG_MOVE           = 23; // message is sent to the window from the base control class when it's
                                  // been asked to move.  dwParam1 contains direction.
 
-#define GUI_MSG_LABEL_BIND     24   // bind label control (for controls supporting more then 1 label)
+constexpr const int GUI_MSG_LABEL_BIND     = 24;   // bind label control (for controls supporting more then 1 label)
 
-#define GUI_MSG_FOCUSED     26  // a control has become focused
+constexpr const int GUI_MSG_FOCUSED        = 26;  // a control has become focused
 
-#define GUI_MSG_PAGE_CHANGE 28  // a page control has changed the page number
+constexpr const int GUI_MSG_PAGE_CHANGE    = 28;  // a page control has changed the page number
 
-#define GUI_MSG_REFRESH_LIST 29 // message sent to all listing controls telling them to refresh their item layouts
+constexpr const int GUI_MSG_REFRESH_LIST   = 29; // message sent to all listing controls telling them to refresh their item layouts
 
-#define GUI_MSG_PAGE_UP      30 // page up
-#define GUI_MSG_PAGE_DOWN    31 // page down
-#define GUI_MSG_MOVE_OFFSET  32 // Instruct the control to MoveUp or MoveDown by offset amount
+constexpr const int GUI_MSG_PAGE_UP      = 30; // page up
+constexpr const int GUI_MSG_PAGE_DOWN    = 31; // page down
+constexpr const int GUI_MSG_MOVE_OFFSET  = 32; // Instruct the control to MoveUp or MoveDown by offset amount
 
-#define GUI_MSG_SET_TYPE     33 ///< Instruct a control to set it's type appropriately
+constexpr const int GUI_MSG_SET_TYPE     = 33; ///< Instruct a control to set it's type appropriately
 
 /*!
  \brief Message indicating the window has been resized
  Any controls that keep stored sizing information based on aspect ratio or window size should
  recalculate sizing information
  */
-#define GUI_MSG_WINDOW_RESIZE  34
+constexpr const int GUI_MSG_WINDOW_RESIZE  = 34;
 
 /*!
  \brief Message indicating loss of renderer, prior to reset
  Any controls that keep shared resources should free them on receipt of this message, as the renderer
  is about to be reset.
  */
-#define GUI_MSG_RENDERER_LOST  35
+constexpr const int GUI_MSG_RENDERER_LOST = 35;
 
 /*!
  \brief Message indicating regain of renderer, after reset
  Any controls that keep shared resources may reallocate them now that the renderer is back
  */
-#define GUI_MSG_RENDERER_RESET 36
+constexpr const int GUI_MSG_RENDERER_RESET = 36;
 
 /*!
  \brief A control wishes to have (or release) exclusive access to mouse actions
  */
-#define GUI_MSG_EXCLUSIVE_MOUSE 37
+constexpr const int GUI_MSG_EXCLUSIVE_MOUSE = 37;
 
 /*!
  \brief A request for supported gestures is made
  */
-#define GUI_MSG_GESTURE_NOTIFY  38
+constexpr const int GUI_MSG_GESTURE_NOTIFY = 38;
 
 /*!
  \brief A request to add a control
  */
-#define GUI_MSG_ADD_CONTROL     39
+constexpr const int GUI_MSG_ADD_CONTROL = 39;
 
 /*!
  \brief A request to remove a control
  */
-#define GUI_MSG_REMOVE_CONTROL  40
+constexpr const int GUI_MSG_REMOVE_CONTROL = 40;
 
 /*!
  \brief A request to unfocus all currently focused controls
  */
-#define GUI_MSG_UNFOCUS_ALL 41
+constexpr const int GUI_MSG_UNFOCUS_ALL = 41;
 
-#define GUI_MSG_SET_TEXT        42
+constexpr const int GUI_MSG_SET_TEXT    = 42;
 
-#define GUI_MSG_WINDOW_LOAD 43
+constexpr const int GUI_MSG_WINDOW_LOAD = 43;
 
-#define GUI_MSG_VALIDITY_CHANGED  44
+constexpr const int GUI_MSG_VALIDITY_CHANGED = 44;
 
 /*!
  \brief Check whether a button is selected
  */
-#define GUI_MSG_IS_SELECTED    45
+constexpr const int GUI_MSG_IS_SELECTED  = 45;
 
 /*!
  \brief Bind a set of labels to a spin (or similar) control
  */
-#define GUI_MSG_SET_LABELS     46
+constexpr const int GUI_MSG_SET_LABELS = 46;
 
 /*!
  \brief Set the filename for an image control
  */
-#define GUI_MSG_SET_FILENAME   47
+constexpr const int GUI_MSG_SET_FILENAME = 47;
 
 /*!
  \brief Get the filename of an image control
  */
 
-#define GUI_MSG_GET_FILENAME   48
+constexpr const int GUI_MSG_GET_FILENAME  = 48;
 
 /*!
  \brief The user interface is ready for usage
  */
-#define GUI_MSG_UI_READY       49
+constexpr const int GUI_MSG_UI_READY = 49;
 
  /*!
  \brief Called every 500ms to allow time dependent updates
  */
-#define GUI_MSG_REFRESH_TIMER  50
+constexpr const int GUI_MSG_REFRESH_TIMER = 50;
 
  /*!
  \brief Called if state has changed which could lead to GUI changes
  */
-#define GUI_MSG_STATE_CHANGED  51
+constexpr const int GUI_MSG_STATE_CHANGED = 51;
 
 /*!
  \brief Called when a subtitle download has finished
  */
-#define GUI_MSG_SUBTITLE_DOWNLOADED  52
+constexpr const int GUI_MSG_SUBTITLE_DOWNLOADED = 52;
 
 
-#define GUI_MSG_USER         1000
+constexpr const int GUI_MSG_USER = 1000;
 
 /*!
 \brief Complete to get codingtable page
 */
-#define GUI_MSG_CODINGTABLE_LOOKUP_COMPLETED 65000
+constexpr const int GUI_MSG_CODINGTABLE_LOOKUP_COMPLETED = 65000;
 
 /*!
  \ingroup winmsg
