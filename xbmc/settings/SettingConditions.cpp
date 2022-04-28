98f1144fa22d23e09cb1f909d8aa07b9ab8bfeca
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-04-28 17:32:07 +0200

    [application][settings] Remove circular dependency CApplication<->CSettings.

--- xbmc/settings/SettingConditions.cpp
+++ xbmc/settings/SettingConditions.cpp
@@ -9,7 +9,6 @@
 #include "SettingConditions.h"
 
 #include "AppParams.h"
-#include "Application.h"
 #include "LockType.h"
 #include "Util.h"
 #include "addons/AddonManager.h"
@@ -338,14 +337,6 @@ bool LessThanOrEqual(const std::string& condition,
   return lhs <= rhs;
 }
 
-bool IsPlaying(const std::string& condition,
-               const std::string& value,
-               const SettingConstPtr& setting,
-               void* data)
-{
-  return g_application.GetAppPlayer().IsPlaying();
-}
-
 const CProfileManager *CSettingConditions::m_profileManager = nullptr;
 std::set<std::string> CSettingConditions::m_simpleConditions;
 std::map<std::string, SettingConditionCheck> CSettingConditions::m_complexConditions;
@@ -464,7 +455,6 @@ void CSettingConditions::Initialize()
   m_complexConditions.insert(std::pair<std::string, SettingConditionCheck>("gte",                           GreaterThanOrEqual));
   m_complexConditions.insert(std::pair<std::string, SettingConditionCheck>("lt",                            LessThan));
   m_complexConditions.insert(std::pair<std::string, SettingConditionCheck>("lte",                           LessThanOrEqual));
-  m_complexConditions.insert(std::pair<std::string, SettingConditionCheck>("isplaying", IsPlaying));
 }
 
 void CSettingConditions::Deinitialize()
