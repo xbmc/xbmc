c9a50f006d2db466e52b4e67eba16e2b30eafdb6
fuzzard <fuzzard@kodi.tv>
2022-05-09 14:05:20 +1000

    Android SDK 30 and All files access permission

--- cmake/platform/android/android.cmake
+++ cmake/platform/android/android.cmake
@@ -2,6 +2,6 @@ set(PLATFORM_REQUIRED_DEPS LibAndroidJNI OpenGLES EGL Zip)
 set(APP_RENDER_SYSTEM gles)
 
 # Store SDK compile version
-set(TARGET_SDK 29)
+set(TARGET_SDK 30)
 # Minimum supported SDK version
 set(TARGET_MINSDK 21)
