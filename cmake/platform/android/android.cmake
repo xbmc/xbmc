d23080c4849ee344551f69f3c8b5e78a5c5b617e
fuzzard <fuzzard@kodi.tv>
2022-05-21 08:02:18 +1000

    [android] SDK30 bump and allow conditional use of MANAGE_EXTERNAL_STORAGE
    This utilises the ability to target Google Play store or not to conditionally
    add MANAGE_EXTERNAL_STORAGE for non play store apk's.

--- cmake/platform/android/android.cmake
+++ cmake/platform/android/android.cmake
@@ -2,12 +2,13 @@ set(PLATFORM_REQUIRED_DEPS LibAndroidJNI OpenGLES EGL Zip)
 set(APP_RENDER_SYSTEM gles)
 
 # Store SDK compile version
-set(TARGET_SDK 29)
+set(TARGET_SDK 30)
 # Minimum supported SDK version
 set(TARGET_MINSDK 21)
 
 if(DEFINED ENV{ANDROIDSTORE} AND ("$ENV{ANDROIDSTORE}" STREQUAL "GOOGLE"))
   set(PLAYSTORE_STATE "true")
 else()
+  set(PERMISSION.MANAGE_EXTERNAL_STORAGE "<uses-permission android:name=\"android.permission.MANAGE_EXTERNAL_STORAGE\" />")
   set(PLAYSTORE_STATE "false")
 endif()
