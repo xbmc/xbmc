cc2190974538791c885cc32bc833461f14d7b4f4
fuzzard <fuzzard@kodi.tv>
2022-05-22 17:38:20 +1000

    [android] Add Play Store labeling to logs/sysinfo for playstore apks

--- cmake/platform/android/android.cmake
+++ cmake/platform/android/android.cmake
@@ -8,6 +8,7 @@ set(TARGET_MINSDK 21)
 
 if(DEFINED ENV{ANDROIDSTORE} AND ("$ENV{ANDROIDSTORE}" STREQUAL "GOOGLE"))
   set(PLAYSTORE_STATE "true")
+  set(INFO_STORE_DISPLAYNAME " Google Play Store")
 else()
   set(PERMISSION.MANAGE_EXTERNAL_STORAGE "<uses-permission android:name=\"android.permission.MANAGE_EXTERNAL_STORAGE\" />")
   set(PLAYSTORE_STATE "false")
