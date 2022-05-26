29451ad5cc8b5ab644783814decb057acbba4124
fuzzard <fuzzard@kodi.tv>
2022-05-26 17:59:49 +1000

    [cmake] Crossguid use CrossGUID::CrossGUID target for depends
    Implement CrossGUID::CrossGUID target to allow adding dependency target for
    ENABLE_INTERNAL_CROSSGUID.

--- cmake/modules/FindCrossGUID.cmake
+++ cmake/modules/FindCrossGUID.cmake
@@ -11,7 +11,7 @@
 #
 # and the following imported targets::
 #
-#   crossguid   - The CrossGUID library
+#   CrossGUID::CrossGUID   - The CrossGUID library
 
 if(ENABLE_INTERNAL_CROSSGUID)
   include(cmake/scripts/common/ModuleHelpers.cmake)
@@ -79,13 +79,6 @@ if(CROSSGUID_FOUND)
     list(APPEND CROSSGUID_DEFINITIONS -DHAVE_NEW_CROSSGUID)
   endif()
 
-  if(NOT TARGET crossguid)
-    add_library(crossguid UNKNOWN IMPORTED)
-    set_target_properties(crossguid PROPERTIES
-                                    IMPORTED_LOCATION "${CROSSGUID_LIBRARY}"
-                                    INTERFACE_INCLUDE_DIRECTORIES "${CROSSGUID_INCLUDE_DIR}")
-  endif()
-
   if(UNIX AND NOT (APPLE OR ANDROID))
     # Suppress mismatch warning, see https://cmake.org/cmake/help/latest/module/FindPackageHandleStandardArgs.html
     set(FPHSA_NAME_MISMATCHED 1)
@@ -95,6 +88,25 @@ if(CROSSGUID_FOUND)
     list(APPEND CROSSGUID_LIBRARIES ${UUID_LIBRARIES})
   endif()
 
-  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP crossguid)
+  if(NOT TARGET CrossGUID::CrossGUID)
+    add_library(CrossGUID::CrossGUID UNKNOWN IMPORTED)
+    if(CROSSGUID_LIBRARY_RELEASE)
+      set_target_properties(CrossGUID::CrossGUID PROPERTIES
+                                                 IMPORTED_CONFIGURATIONS RELEASE
+                                                 IMPORTED_LOCATION "${CROSSGUID_LIBRARY_RELEASE}")
+    endif()
+    if(CROSSGUID_LIBRARY_DEBUG)
+      set_target_properties(CrossGUID::CrossGUID PROPERTIES
+                                                 IMPORTED_CONFIGURATIONS DEBUG
+                                                 IMPORTED_LOCATION "${CROSSGUID_LIBRARY_DEBUG}")
+    endif()
+    set_target_properties(CrossGUID::CrossGUID PROPERTIES
+                                               INTERFACE_INCLUDE_DIRECTORIES "${CROSSGUID_INCLUDE_DIRS}")
+  endif()
+  if(TARGET crossguid)
+    add_dependencies(CrossGUID::CrossGUID crossguid)
+  endif()
+  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP CrossGUID::CrossGUID)
+
 endif()
 mark_as_advanced(CROSSGUID_INCLUDE_DIR CROSSGUID_LIBRARY)
