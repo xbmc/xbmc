04c47876ae2a1a5220c664697ca1fe58f848600c
fuzzard <fuzzard@kodi.tv>
2022-05-31 15:41:16 +1000

    [cmake] pass cmake_prefix_path through to internaldep build function
    Prepend depends_path to cmake_prefix_path for find_package/program/etc calls to
    search our depends_path first
    Pass through cmake_prefix_path to internal build functions for cmake based build systems

--- cmake/scripts/common/PathSetup.cmake
+++ cmake/scripts/common/PathSetup.cmake
@@ -5,3 +5,9 @@ include(cmake/scripts/${CORE_SYSTEM_NAME}/PathSetup.cmake)
 if(NOT DEPENDS_PATH)
   set(DEPENDS_PATH "${CMAKE_BINARY_DIR}/${CORE_BUILD_DIR}")
 endif()
+
+# If a platform sets a Depends_path for libs, prepend to cmake prefix path
+# for when searching for libs (eg find_package)
+if(DEPENDS_PATH)
+  list(PREPEND CMAKE_PREFIX_PATH ${DEPENDS_PATH})
+endif()
