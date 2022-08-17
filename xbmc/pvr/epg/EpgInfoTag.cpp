f83d8661f725eaae515283db19bec66f3aa0af43
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-08-17 16:15:19 +0200

    [pvr] Optimize CPVREpgInfoTag ctor. Calculate genres vector only on demand.

--- xbmc/pvr/epg/EpgInfoTag.cpp
+++ xbmc/pvr/epg/EpgInfoTag.cpp
@@ -65,7 +65,9 @@ CPVREpgInfoTag::CPVREpgInfoTag(const EPG_TAG& data,
                                int iClientId,
                                const std::shared_ptr<CPVREpgChannelData>& channelData,
                                int iEpgID)
-  : m_iParentalRating(data.iParentalRating),
+  : m_iGenreType(data.iGenreType),
+    m_iGenreSubType(data.iGenreSubType),
+    m_iParentalRating(data.iParentalRating),
     m_iStarRating(data.iStarRating),
     m_iSeriesNumber(data.iSeriesNumber),
     m_iEpisodeNumber(data.iEpisodeNumber),
@@ -103,11 +105,11 @@ CPVREpgInfoTag::CPVREpgInfoTag(const EPG_TAG& data,
     m_channelData = std::make_shared<CPVREpgChannelData>(iClientId, data.iUniqueChannelId);
   }
 
-  SetGenre(data.iGenreType, data.iGenreSubType, data.strGenreDescription);
-
   // explicit NULL check, because there is no implicit NULL constructor for std::string
   if (data.strTitle)
     m_strTitle = data.strTitle;
+  if (data.strGenreDescription)
+    m_strGenreDescription = data.strGenreDescription;
   if (data.strPlotOutline)
     m_strPlotOutline = data.strPlotOutline;
   if (data.strPlot)
@@ -177,7 +179,7 @@ void CPVREpgInfoTag::Serialize(CVariant& value) const
   value["writer"] = DeTokenize(m_writers);
   value["year"] = m_iYear;
   value["imdbnumber"] = m_strIMDBNumber;
-  value["genre"] = m_genre;
+  value["genre"] = Genre();
   value["filenameandpath"] = Path();
   value["starttime"] = m_startTime.IsValid() ? m_startTime.GetAsDBDateTime() : StringUtils::Empty;
   value["endtime"] = m_endTime.IsValid() ? m_endTime.GetAsDBDateTime() : StringUtils::Empty;
@@ -372,7 +374,8 @@ const std::string CPVREpgInfoTag::GetWritersLabel() const
 
 const std::string CPVREpgInfoTag::GetGenresLabel() const
 {
-  return StringUtils::Join(m_genre, CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_videoItemSeparator);
+  return StringUtils::Join(
+      Genre(), CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_videoItemSeparator);
 }
 
 int CPVREpgInfoTag::Year() const
@@ -385,29 +388,6 @@ std::string CPVREpgInfoTag::IMDBNumber() const
   return m_strIMDBNumber;
 }
 
-void CPVREpgInfoTag::SetGenre(int iGenreType, int iGenreSubType, const char* strGenre)
-{
-  if (m_iGenreType != iGenreType || m_iGenreSubType != iGenreSubType)
-  {
-    m_iGenreType = iGenreType;
-    m_iGenreSubType = iGenreSubType;
-    if ((iGenreType == EPG_GENRE_USE_STRING || iGenreSubType == EPG_GENRE_USE_STRING) && (strGenre != NULL) && (strlen(strGenre) > 0))
-    {
-      /* Type and sub type are both not given. No EPG color coding possible unless sub type is used to specify
-       * EPG_GENRE_USE_STRING leaving type available for genre category, use the provided genre description for the text. */
-      m_genre = Tokenize(strGenre);
-    }
-  }
-
-  if (m_genre.empty())
-  {
-    // Determine the genre description from the type and subtype IDs.
-    m_genre = StringUtils::Split(
-        CPVREpg::ConvertGenreIdToString(iGenreType, iGenreSubType),
-        CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_videoItemSeparator);
-  }
-}
-
 int CPVREpgInfoTag::GenreType() const
 {
   return m_iGenreType;
@@ -418,8 +398,30 @@ int CPVREpgInfoTag::GenreSubType() const
   return m_iGenreSubType;
 }
 
+std::string CPVREpgInfoTag::GenreDescription() const
+{
+  return m_strGenreDescription;
+}
+
 const std::vector<std::string> CPVREpgInfoTag::Genre() const
 {
+  if (m_genre.empty())
+  {
+    if ((m_iGenreType == EPG_GENRE_USE_STRING || m_iGenreSubType == EPG_GENRE_USE_STRING) &&
+        !m_strGenreDescription.empty())
+    {
+      // Type and sub type are both not given. No EPG color coding possible unless sub type is
+      // used to specify EPG_GENRE_USE_STRING leaving type available for genre category, use the
+      // provided genre description for the text.
+      m_genre = Tokenize(m_strGenreDescription);
+    }
+
+    if (m_genre.empty())
+    {
+      // Determine the genre from the type and subtype IDs.
+      m_genre = Tokenize(CPVREpg::ConvertGenreIdToString(m_iGenreType, m_iGenreSubType));
+    }
+  }
   return m_genre;
 }
 
@@ -493,7 +495,8 @@ bool CPVREpgInfoTag::Update(const CPVREpgInfoTag& tag, bool bUpdateBroadcastId /
        m_iYear != tag.m_iYear || m_strIMDBNumber != tag.m_strIMDBNumber ||
        m_startTime != tag.m_startTime || m_endTime != tag.m_endTime ||
        m_iGenreType != tag.m_iGenreType || m_iGenreSubType != tag.m_iGenreSubType ||
-       m_firstAired != tag.m_firstAired || m_iParentalRating != tag.m_iParentalRating ||
+       m_strGenreDescription != tag.m_strGenreDescription || m_firstAired != tag.m_firstAired ||
+       m_iParentalRating != tag.m_iParentalRating ||
        m_strParentalRatingCode != tag.m_strParentalRatingCode ||
        m_iStarRating != tag.m_iStarRating || m_iEpisodeNumber != tag.m_iEpisodeNumber ||
        m_iEpisodePart != tag.m_iEpisodePart || m_iSeriesNumber != tag.m_iSeriesNumber ||
@@ -523,20 +526,11 @@ bool CPVREpgInfoTag::Update(const CPVREpgInfoTag& tag, bool bUpdateBroadcastId /
     m_endTime = tag.m_endTime;
     m_iGenreType = tag.m_iGenreType;
     m_iGenreSubType = tag.m_iGenreSubType;
+    m_strGenreDescription = tag.m_strGenreDescription;
+    m_genre = tag.m_genre;
     m_iEpgID = tag.m_iEpgID;
     m_iFlags = tag.m_iFlags;
     m_strSeriesLink = tag.m_strSeriesLink;
-
-    if (m_iGenreType == EPG_GENRE_USE_STRING || m_iGenreSubType == EPG_GENRE_USE_STRING)
-    {
-      /* No type/subtype. Use the provided description */
-      m_genre = tag.m_genre;
-    }
-    else
-    {
-      /* Determine genre description by type/subtype */
-      m_genre = StringUtils::Split(CPVREpg::ConvertGenreIdToString(tag.m_iGenreType, tag.m_iGenreSubType), CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_videoItemSeparator);
-    }
     m_firstAired = tag.m_firstAired;
     m_iParentalRating = tag.m_iParentalRating;
     m_strParentalRatingCode = tag.m_strParentalRatingCode;
