ec392f4fadf5476f55fb1845d8155767c0de9617
Rechi <Rechi@users.noreply.github.com>
2022-08-26 19:45:37 +0100

    [cppcheck] enable performance checks

--- cmake/modules/buildtools/FindCppcheck.cmake
+++ cmake/modules/buildtools/FindCppcheck.cmake
@@ -21,7 +21,7 @@ if(CPPCHECK_FOUND)
   # but cppcheck doesn't support Objective-C and Objective-C++.
   # CMake >= 3.16 added support for Objective-C and Objective-C++ language,
   # but doesn't support OBJC and OBJCXX for <LANG>_CLANG_TIDY.
-  file(WRITE "${CMAKE_BINARY_DIR}/cppcheck" "case \"$@\" in *.m|*.mm) exit 0; esac\nexec \"${CPPCHECK_EXECUTABLE}\" --quiet --relative-paths=\"${CMAKE_SOURCE_DIR}\" \"$@\"\n")
+  file(WRITE "${CMAKE_BINARY_DIR}/cppcheck" "case \"$@\" in *.m|*.mm) exit 0; esac\nexec \"${CPPCHECK_EXECUTABLE}\" --enable=performance --quiet --relative-paths=\"${CMAKE_SOURCE_DIR}\" \"$@\"\n")
   execute_process(COMMAND chmod +x "${CMAKE_BINARY_DIR}/cppcheck")
 
   # Supports Unix Makefiles and Ninja
