1c68cd5197dce3ea7c4bb6bf39a3343b97bc6bd1
Garrett Brown <themagnificentmrb@gmail.com>
2022-09-09 16:45:42 -0700

    Games: Add caption to game info tag

--- xbmc/interfaces/legacy/InfoTagGame.h
+++ xbmc/interfaces/legacy/InfoTagGame.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "AddonClass.h"
+#include "games/tags/GameInfoTag.h"
 
 namespace KODI
 {
@@ -234,6 +235,23 @@ public:
 #ifdef DOXYGEN_SHOULD_USE_THIS
   ///
   /// \ingroup python_InfoTagGame
+  /// @brief \python_func{ getCaption() }
+  /// Gets a rich presence evaluation of the game's state.
+  ///
+  /// @return [string] overview
+  ///
+  ///
+  ///-----------------------------------------------------------------------
+  /// @python_v20 New function added.
+  ///
+  getCaption();
+#else
+  String getCaption();
+#endif
+
+#ifdef DOXYGEN_SHOULD_USE_THIS
+  ///
+  /// \ingroup python_InfoTagGame
   /// @brief \python_func{ setTitle(title) }
   /// Sets the title of the game.
   ///
@@ -367,6 +385,23 @@ public:
   void setGameClient(const String& gameClient);
 #endif
 
+#ifdef DOXYGEN_SHOULD_USE_THIS
+  ///
+  /// \ingroup python_InfoTagGame
+  /// @brief \python_func{ setCaption(gameClient) }
+  /// Sets the caption of the game.
+  ///
+  /// @param caption         string - caption.
+  ///
+  ///
+  ///-----------------------------------------------------------------------
+  /// @python_v20 New function added.
+  ///
+  setCaption(...);
+#else
+  void setCaption(const String& caption);
+#endif
+
 #ifndef SWIG
   static void setTitleRaw(KODI::GAME::CGameInfoTag* infoTag, const String& title);
   static void setPlatformRaw(KODI::GAME::CGameInfoTag* infoTag, const String& platform);
@@ -376,6 +411,7 @@ public:
   static void setOverviewRaw(KODI::GAME::CGameInfoTag* infoTag, const String& overview);
   static void setYearRaw(KODI::GAME::CGameInfoTag* infoTag, unsigned int year);
   static void setGameClientRaw(KODI::GAME::CGameInfoTag* infoTag, const String& gameClient);
+  static void setCaptionRaw(KODI::GAME::CGameInfoTag* infoTag, const String& caption);
 #endif
 };
 
