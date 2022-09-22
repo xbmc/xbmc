1c47217ccaca542cf7951f436bed2e2ddc11f9d2
chrisism <cwjungerius@gmail.com>
2022-09-22 23:07:30 +0200

    Android: Add action and category parameter to StartActivity builtin

--- xbmc/platform/android/activity/XBMCApp.h
+++ xbmc/platform/android/activity/XBMCApp.h
@@ -164,7 +164,9 @@ public:
                             const std::string& dataType = std::string(),
                             const std::string& dataURI = std::string(),
                             const std::string& flags = std::string(),
-                            const std::string& extras = std::string());
+                            const std::string& extras = std::string(),
+                            const std::string& action = std::string(),
+                            const std::string& category = std::string());
   std::vector<androidPackage> GetApplications() const;
 
   /*!
