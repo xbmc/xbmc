ba7d0185f2ff962df80387bdf3b08bdcc12f5d4e
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-09-22 23:59:16 +0200

    [PVR] Refactor CPVRGUIActions step 8: Move database related actions to own class.

--- xbmc/pvr/guilib/PVRGUIActions.h
+++ xbmc/pvr/guilib/PVRGUIActions.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "pvr/guilib/PVRGUIActionsChannels.h"
+#include "pvr/guilib/PVRGUIActionsDatabase.h"
 #include "pvr/guilib/PVRGUIActionsEPG.h"
 #include "pvr/guilib/PVRGUIActionsParentalControl.h"
 #include "pvr/guilib/PVRGUIActionsPlayback.h"
@@ -26,6 +27,7 @@ class CFileItem;
 namespace PVR
 {
 class CPVRGUIActions : public CPVRGUIActionsChannels,
+                       public CPVRGUIActionsDatabase,
                        public CPVRGUIActionsEPG,
                        public CPVRGUIActionsParentalControl,
                        public CPVRGUIActionsPlayback,
@@ -42,13 +44,6 @@ public:
    * @return true on success, false otherwise.
    */
   bool ProcessSettingsMenuHooks();
-
-  /*!
-   * @brief Reset the TV database to it's initial state and delete all the data.
-   * @param bResetEPGOnly True to only reset the EPG database, false to reset both PVR and EPG database.
-   * @return true on success, false otherwise.
-   */
-  bool ResetPVRDatabase(bool bResetEPGOnly);
 
   /*!
    * @brief Get the currently selected item path; used across several windows/dialogs to share item selection.
