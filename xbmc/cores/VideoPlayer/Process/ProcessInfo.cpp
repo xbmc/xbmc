1009f588b9de406fe48acdb759af6b3048746429
Arne Morten Kvarving <spiff@kodi.tv>
2022-09-23 14:55:22 +0200

    ProcessInfo: mark some methods const

--- xbmc/cores/VideoPlayer/Process/ProcessInfo.cpp
+++ xbmc/cores/VideoPlayer/Process/ProcessInfo.cpp
@@ -269,7 +269,7 @@ void CProcessInfo::UpdateDeinterlacingMethods(std::list<EINTERLACEMETHOD> &metho
     m_deintMethods.push_front(EINTERLACEMETHOD::VS_INTERLACEMETHOD_NONE);
 }
 
-bool CProcessInfo::Supports(EINTERLACEMETHOD method)
+bool CProcessInfo::Supports(EINTERLACEMETHOD method) const
 {
   std::unique_lock<CCriticalSection> lock(m_videoCodecSection);
 
@@ -287,7 +287,7 @@ void CProcessInfo::SetDeinterlacingMethodDefault(EINTERLACEMETHOD method)
   m_deintMethodDefault = method;
 }
 
-EINTERLACEMETHOD CProcessInfo::GetDeinterlacingMethodDefault()
+EINTERLACEMETHOD CProcessInfo::GetDeinterlacingMethodDefault() const
 {
   std::unique_lock<CCriticalSection> lock(m_videoCodecSection);
 
