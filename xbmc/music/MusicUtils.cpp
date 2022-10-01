fedd983362cff24a52596667d65f7381d0614534
Arne Morten Kvarving <spiff@kodi.tv>
2022-10-01 12:17:33 +0200

    MusicUtils.h: avoid include of FileItem.h

--- xbmc/music/MusicUtils.cpp
+++ xbmc/music/MusicUtils.cpp
@@ -183,7 +183,7 @@ namespace MUSIC_UTILS
     }
   };
 
-  void UpdateArtJob(const CFileItemPtr& pItem,
+  void UpdateArtJob(const std::shared_ptr<CFileItem>& pItem,
                     const std::string& strType,
                     const std::string& strArt)
   {
@@ -339,7 +339,7 @@ namespace MUSIC_UTILS
     return -1;
   }
 
-  void UpdateSongRatingJob(const CFileItemPtr& pItem, int userrating)
+  void UpdateSongRatingJob(const std::shared_ptr<CFileItem>& pItem, int userrating)
   {
     // Asynchronously update the song user rating in music library
     const CMusicInfoTag *tag = pItem->GetMusicInfoTag();
