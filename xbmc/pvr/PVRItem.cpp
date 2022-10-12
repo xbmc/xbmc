0e675d89f5cd6efd839dae51e2d929970175f005
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-10-12 11:37:33 +0200

    [PVR] clang-format: pvr/PVRItem.(cpp|h)

--- xbmc/pvr/PVRItem.cpp
+++ xbmc/pvr/PVRItem.cpp
@@ -25,138 +25,143 @@
 
 namespace PVR
 {
-  std::shared_ptr<CPVREpgInfoTag> CPVRItem::GetEpgInfoTag() const
+std::shared_ptr<CPVREpgInfoTag> CPVRItem::GetEpgInfoTag() const
+{
+  if (m_item->IsEPG())
   {
-    if (m_item->IsEPG())
-    {
-      return m_item->GetEPGInfoTag();
-    }
-    else if (m_item->IsPVRChannel())
-    {
-      return m_item->GetPVRChannelInfoTag()->GetEPGNow();
-    }
-    else if (m_item->IsPVRTimer())
-    {
-      return m_item->GetPVRTimerInfoTag()->GetEpgInfoTag();
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return {};
+    return m_item->GetEPGInfoTag();
   }
+  else if (m_item->IsPVRChannel())
+  {
+    return m_item->GetPVRChannelInfoTag()->GetEPGNow();
+  }
+  else if (m_item->IsPVRTimer())
+  {
+    return m_item->GetPVRTimerInfoTag()->GetEpgInfoTag();
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return {};
+}
 
-  std::shared_ptr<CPVREpgInfoTag> CPVRItem::GetNextEpgInfoTag() const
+std::shared_ptr<CPVREpgInfoTag> CPVRItem::GetNextEpgInfoTag() const
+{
+  if (m_item->IsEPG())
   {
-    if (m_item->IsEPG())
-    {
-      const std::shared_ptr<CPVRChannel> channel = CServiceBroker::GetPVRManager().ChannelGroups()->GetChannelForEpgTag(m_item->GetEPGInfoTag());
-      if (channel)
-        return channel->GetEPGNext();
-    }
-    else if (m_item->IsPVRChannel())
-    {
-      return m_item->GetPVRChannelInfoTag()->GetEPGNext();
-    }
-    else if (m_item->IsPVRTimer())
-    {
-      const std::shared_ptr<CPVRChannel> channel = m_item->GetPVRTimerInfoTag()->Channel();
-      if (channel)
-        return channel->GetEPGNext();
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return {};
+    const std::shared_ptr<CPVRChannel> channel =
+        CServiceBroker::GetPVRManager().ChannelGroups()->GetChannelForEpgTag(
+            m_item->GetEPGInfoTag());
+    if (channel)
+      return channel->GetEPGNext();
   }
+  else if (m_item->IsPVRChannel())
+  {
+    return m_item->GetPVRChannelInfoTag()->GetEPGNext();
+  }
+  else if (m_item->IsPVRTimer())
+  {
+    const std::shared_ptr<CPVRChannel> channel = m_item->GetPVRTimerInfoTag()->Channel();
+    if (channel)
+      return channel->GetEPGNext();
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return {};
+}
 
-  std::shared_ptr<CPVRChannel> CPVRItem::GetChannel() const
+std::shared_ptr<CPVRChannel> CPVRItem::GetChannel() const
+{
+  if (m_item->IsPVRChannel())
   {
-    if (m_item->IsPVRChannel())
-    {
-      return m_item->GetPVRChannelInfoTag();
-    }
-    else if (m_item->IsEPG())
-    {
-      return CServiceBroker::GetPVRManager().ChannelGroups()->GetChannelForEpgTag(m_item->GetEPGInfoTag());
-    }
-    else if (m_item->IsPVRTimer())
-    {
-      return m_item->GetPVRTimerInfoTag()->Channel();
-    }
-    else if (m_item->IsPVRRecording())
-    {
-      return m_item->GetPVRRecordingInfoTag()->Channel();
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return {};
+    return m_item->GetPVRChannelInfoTag();
   }
+  else if (m_item->IsEPG())
+  {
+    return CServiceBroker::GetPVRManager().ChannelGroups()->GetChannelForEpgTag(
+        m_item->GetEPGInfoTag());
+  }
+  else if (m_item->IsPVRTimer())
+  {
+    return m_item->GetPVRTimerInfoTag()->Channel();
+  }
+  else if (m_item->IsPVRRecording())
+  {
+    return m_item->GetPVRRecordingInfoTag()->Channel();
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return {};
+}
 
-  std::shared_ptr<CPVRTimerInfoTag> CPVRItem::GetTimerInfoTag() const
+std::shared_ptr<CPVRTimerInfoTag> CPVRItem::GetTimerInfoTag() const
+{
+  if (m_item->IsPVRTimer())
   {
-    if (m_item->IsPVRTimer())
-    {
-      return m_item->GetPVRTimerInfoTag();
-    }
-    else if (m_item->IsEPG())
-    {
-      return CServiceBroker::GetPVRManager().Timers()->GetTimerForEpgTag(m_item->GetEPGInfoTag());
-    }
-    else if (m_item->IsPVRChannel())
-    {
-      return CServiceBroker::GetPVRManager().Timers()->GetActiveTimerForChannel(m_item->GetPVRChannelInfoTag());
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return {};
+    return m_item->GetPVRTimerInfoTag();
   }
+  else if (m_item->IsEPG())
+  {
+    return CServiceBroker::GetPVRManager().Timers()->GetTimerForEpgTag(m_item->GetEPGInfoTag());
+  }
+  else if (m_item->IsPVRChannel())
+  {
+    return CServiceBroker::GetPVRManager().Timers()->GetActiveTimerForChannel(
+        m_item->GetPVRChannelInfoTag());
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return {};
+}
 
-  std::shared_ptr<CPVRRecording> CPVRItem::GetRecording() const
+std::shared_ptr<CPVRRecording> CPVRItem::GetRecording() const
+{
+  if (m_item->IsPVRRecording())
   {
-    if (m_item->IsPVRRecording())
-    {
-      return m_item->GetPVRRecordingInfoTag();
-    }
-    else if (m_item->IsEPG())
-    {
-      return CServiceBroker::GetPVRManager().Recordings()->GetRecordingForEpgTag(m_item->GetEPGInfoTag());
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return {};
+    return m_item->GetPVRRecordingInfoTag();
   }
+  else if (m_item->IsEPG())
+  {
+    return CServiceBroker::GetPVRManager().Recordings()->GetRecordingForEpgTag(
+        m_item->GetEPGInfoTag());
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return {};
+}
 
-  bool CPVRItem::IsRadio() const
+bool CPVRItem::IsRadio() const
+{
+  if (m_item->IsPVRChannel())
   {
-    if (m_item->IsPVRChannel())
-    {
-      return m_item->GetPVRChannelInfoTag()->IsRadio();
-    }
-    else if (m_item->IsEPG())
-    {
-      return m_item->GetEPGInfoTag()->IsRadio();
-    }
-    else if (m_item->IsPVRRecording())
-    {
-      return m_item->GetPVRRecordingInfoTag()->IsRadio();
-    }
-    else if (m_item->IsPVRTimer())
-    {
-      return m_item->GetPVRTimerInfoTag()->IsRadio();
-    }
-    else if (URIUtils::IsPVR(m_item->GetDynPath()))
-    {
-      CLog::LogF(LOGERROR, "Unsupported item type!");
-    }
-    return false;
+    return m_item->GetPVRChannelInfoTag()->IsRadio();
   }
+  else if (m_item->IsEPG())
+  {
+    return m_item->GetEPGInfoTag()->IsRadio();
+  }
+  else if (m_item->IsPVRRecording())
+  {
+    return m_item->GetPVRRecordingInfoTag()->IsRadio();
+  }
+  else if (m_item->IsPVRTimer())
+  {
+    return m_item->GetPVRTimerInfoTag()->IsRadio();
+  }
+  else if (URIUtils::IsPVR(m_item->GetDynPath()))
+  {
+    CLog::LogF(LOGERROR, "Unsupported item type!");
+  }
+  return false;
+}
 
 } // namespace PVR
