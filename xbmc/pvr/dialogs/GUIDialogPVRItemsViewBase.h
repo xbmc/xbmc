8fde39ab98b9ed5d79f60c5723b5b713067431b4
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2022-10-13 14:44:54 +0200

    [PVR] clang-format: pvr/dialogs/GUIDialogPVRItemsViewBase.(cpp|h)

--- xbmc/pvr/dialogs/GUIDialogPVRItemsViewBase.h
+++ xbmc/pvr/dialogs/GUIDialogPVRItemsViewBase.h
@@ -18,30 +18,30 @@ class CFileItemList;
 
 namespace PVR
 {
-  class CGUIDialogPVRItemsViewBase : public CGUIDialog
-  {
-  public:
-    CGUIDialogPVRItemsViewBase() = delete;
-    CGUIDialogPVRItemsViewBase(int id, const std::string& xmlFile);
-    ~CGUIDialogPVRItemsViewBase() override = default;
+class CGUIDialogPVRItemsViewBase : public CGUIDialog
+{
+public:
+  CGUIDialogPVRItemsViewBase() = delete;
+  CGUIDialogPVRItemsViewBase(int id, const std::string& xmlFile);
+  ~CGUIDialogPVRItemsViewBase() override = default;
 
-    void OnWindowLoaded() override;
-    void OnWindowUnload() override;
-    bool OnAction(const CAction& action) override;
+  void OnWindowLoaded() override;
+  void OnWindowUnload() override;
+  bool OnAction(const CAction& action) override;
 
-  protected:
-    void Init();
+protected:
+  void Init();
 
-    void OnInitWindow() override;
-    void OnDeinitWindow(int nextWindowID) override;
-    CGUIControl* GetFirstFocusableControl(int id) override;
+  void OnInitWindow() override;
+  void OnDeinitWindow(int nextWindowID) override;
+  CGUIControl* GetFirstFocusableControl(int id) override;
 
-    std::unique_ptr<CFileItemList> m_vecItems;
-    CGUIViewControl m_viewControl;
+  std::unique_ptr<CFileItemList> m_vecItems;
+  CGUIViewControl m_viewControl;
 
-  private:
-    void Clear();
-    void ShowInfo(int itemIdx);
-    bool ContextMenu(int iItemIdx);
-  };
-}
+private:
+  void Clear();
+  void ShowInfo(int itemIdx);
+  bool ContextMenu(int iItemIdx);
+};
+} // namespace PVR
