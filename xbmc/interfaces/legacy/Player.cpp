a0841e9f5e7f328a97caf5da5366e82cbb4d3065
Garrett Brown <themagnificentmrb@gmail.com>
2022-10-14 11:44:43 -0700

    Games: Add game info tag to player interface API

--- xbmc/interfaces/legacy/Player.cpp
+++ xbmc/interfaces/legacy/Player.cpp
@@ -356,6 +356,12 @@ namespace XBMCAddon
       return getAppPlayer()->IsPlayingRDS();
     }
 
+    bool Player::isPlayingGame()
+    {
+      XBMC_TRACE;
+      return getAppPlayer()->IsPlayingGame();
+    }
+
     bool Player::isExternalPlayer()
     {
       XBMC_TRACE;
@@ -415,6 +421,20 @@ namespace XBMCAddon
 
       CGUIMessage msg(GUI_MSG_NOTIFY_ALL, 0, 0, GUI_MSG_UPDATE_ITEM, 0, item->item);
       CServiceBroker::GetGUI()->GetWindowManager().SendMessage(msg);
+    }
+
+    InfoTagGame* Player::getGameInfoTag()
+    {
+      XBMC_TRACE;
+      if (!getAppPlayer()->IsPlayingGame())
+        throw PlayerException("Kodi is not playing any game file");
+
+      const KODI::GAME::CGameInfoTag* game =
+          CServiceBroker::GetGUI()->GetInfoManager().GetCurrentGameTag();
+      if (game)
+        return new InfoTagGame(game);
+
+      return new InfoTagGame();
     }
 
     InfoTagRadioRDS* Player::getRadioRDSInfoTag()
