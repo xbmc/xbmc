8b69f86f914d81267d0aaa00801a7c53165ff09b
Frank Riley <fhriley@gmail.com>
2023-01-12 17:47:17 -0700

    Fix infinite loops in CPluginFile. Fixes #22448
    Signed-off-by: Frank Riley <fhriley@gmail.com>

--- xbmc/filesystem/PluginFile.cpp
+++ xbmc/filesystem/PluginFile.cpp
@@ -18,9 +18,39 @@ CPluginFile::CPluginFile(void) : COverrideFile(false)
 
 CPluginFile::~CPluginFile(void) = default;
 
+bool CPluginFile::Open(const CURL& url)
+{
+  return false;
+}
+
 bool CPluginFile::Exists(const CURL& url)
 {
   return true;
+}
+
+int CPluginFile::Stat(const CURL& url, struct __stat64* buffer)
+{
+  return -1;
+}
+
+int CPluginFile::Stat(struct __stat64* buffer)
+{
+  return -1;
+}
+
+bool CPluginFile::OpenForWrite(const CURL& url, bool bOverWrite)
+{
+  return false;
+}
+
+bool CPluginFile::Delete(const CURL& url)
+{
+  return false;
+}
+
+bool CPluginFile::Rename(const CURL& url, const CURL& urlnew)
+{
+  return false;
 }
 
 std::string CPluginFile::TranslatePath(const CURL& url)
