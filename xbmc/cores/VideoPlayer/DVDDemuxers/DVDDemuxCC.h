a7950077272ff4731282beb269fbd6b47192fe71
Miguel Borges de Freitas <92enen@gmail.com>
2023-01-28 11:35:49 +0000

    Revert "[Video][Subtitles] Use ffmpeg A53 sidedata instead of custom demuxer"
    This reverts commit 0b559dc2d50432c47a27c75623ed20aa808d0689.

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxCC.h
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxCC.h
@@ -33,7 +33,7 @@ public:
   std::vector<CDemuxStream*> GetStreams() const override;
   int GetNrOfStreams() const override;
 
-  DemuxPacket* Process(CCaptionBlock* captionBlock);
+  DemuxPacket* Read(DemuxPacket *packet);
   static void Handler(int service, void *userdata);
 
 protected:
