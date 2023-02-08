56fa4bbca787d86d09875291a76d48a2c4c07fab
Jose Luis Marti <joseluis.marti@gmail.com>
2023-02-08 16:11:56 +0100

    Bump Android SDK 33

--- cmake/platform/android/android.cmake
+++ cmake/platform/android/android.cmake
@@ -2,6 +2,6 @@ set(PLATFORM_REQUIRED_DEPS LibAndroidJNI OpenGLES EGL LibZip)
 set(APP_RENDER_SYSTEM gles)
 
 # Store SDK compile version
-set(TARGET_SDK 31)
+set(TARGET_SDK 33)
 # Minimum supported SDK version
 set(TARGET_MINSDK 21)
