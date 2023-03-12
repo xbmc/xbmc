0b1b3662d39f7423d022a598758fdefec8423553
Lukas Rusak <lorusak@gmail.com>
2023-03-11 19:32:09 -0800

    DXVA: CDecoder: replace removed av_mallocz_array with av_calloc
    ref: https://ffmpeg.org/doxygen/5.0/group__lavu__mem__funcs.html\#ga6627f140c3f70847bc6d9690a2fd001f
    
    Signed-off-by: Lukas Rusak <lorusak@gmail.com>

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DXVA.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DXVA.cpp
@@ -1032,7 +1032,8 @@ CDecoder::CDecoder(CProcessInfo& processInfo)
   m_event.Set();
   m_avD3D11Context = av_d3d11va_alloc_context();
   m_avD3D11Context->cfg = reinterpret_cast<D3D11_VIDEO_DECODER_CONFIG*>(av_mallocz(sizeof(D3D11_VIDEO_DECODER_CONFIG)));
-  m_avD3D11Context->surface = reinterpret_cast<ID3D11VideoDecoderOutputView**>(av_mallocz_array(32, sizeof(ID3D11VideoDecoderOutputView*)));
+  m_avD3D11Context->surface = reinterpret_cast<ID3D11VideoDecoderOutputView**>(
+      av_calloc(32, sizeof(ID3D11VideoDecoderOutputView*)));
   m_bufferPool.reset();
 
   DX::Windowing()->Register(this);
