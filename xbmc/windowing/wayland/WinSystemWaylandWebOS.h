0ad02e4159aafd100794cf169dcbdaeeba968f78
Stephan Sundermann <stephansundermann@gmail.com>
2023-03-13 21:44:42 +0100

    webos: Make webos-foreign optional for compability

--- xbmc/windowing/wayland/WinSystemWaylandWebOS.h
+++ xbmc/windowing/wayland/WinSystemWaylandWebOS.h
@@ -21,8 +21,25 @@ class CWinSystemWaylandWebOS : public CWinSystemWayland
 
 public:
   bool InitWindowSystem() override;
+
+  /**
+   * Gets the exported window name. May return an empty string on non wayland-webos-foreign devices (pre webOS 5)
+   * @return Exported window name
+   */
   std::string GetExportedWindowName();
+
+  /**
+   * Sets up the an exported window for display. The display engine will merge the exported window with the UI layer.
+   * Therefore any UI element must be transparent for the exported window to punch through. Not available on non
+   * wayland-webos-foreign devices (pre webOS 5)
+   * @param srcWidth Exported window width
+   * @param srcHeight Exported window height
+   * @param dstWidth Destination width
+   * @param dstHeight Destination width
+   * @return True on success, else false
+   */
   bool SetExportedWindow(int32_t srcWidth, int32_t srcHeight, int32_t dstWidth, int32_t dstHeight);
+
   IShellSurface* CreateShellSurface(const std::string& name) override;
   bool CreateNewWindow(const std::string& name, bool fullScreen, RESOLUTION_INFO& res) override;
   ~CWinSystemWaylandWebOS() noexcept override;
