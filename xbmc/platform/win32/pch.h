c5a2f90fe5f223dcbe2c210a74249b1dbac3cacf
thexai <58434170+thexai@users.noreply.github.com>
2023-04-08 20:40:49 +0200

    [UWP][Build] Changes and fixes to able build with recent Windows SDK's
    Tested with Windows SDK 10.0.22000.0 (Windows 11)

--- xbmc/platform/win32/pch.h
+++ xbmc/platform/win32/pch.h
@@ -44,13 +44,14 @@
 #include <winrt/Windows.ApplicationModel.Core.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.Storage.h>
 #include <winrt/Windows.System.h>
 #include <winrt/Windows.System.Threading.h>
 #include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.UI.Input.h>
 #include <winrt/Windows.UI.ViewManagement.h>
-WINRT_WARNING_PUSH
 #endif
 // anything below here should be headers that very rarely (hopefully never)
 // change yet are included almost everywhere.
