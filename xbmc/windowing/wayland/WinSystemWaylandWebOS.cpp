15c812bf32aaf719c7df2f03c9d1ba9f24223b2f
Stephan Sundermann <stephansundermann@gmail.com>
2023-04-12 16:34:30 +0200

    webos: Cleanup

--- xbmc/windowing/wayland/WinSystemWaylandWebOS.cpp
+++ xbmc/windowing/wayland/WinSystemWaylandWebOS.cpp
@@ -23,7 +23,8 @@ namespace KODI::WINDOWING::WAYLAND
 
 bool CWinSystemWaylandWebOS::InitWindowSystem()
 {
-  auto ok = CWinSystemWayland::InitWindowSystem();
+  if (!CWinSystemWayland::InitWindowSystem())
+    return false;
 
   CDVDVideoCodecStarfish::Register();
   CRendererStarfish::Register();
@@ -35,14 +36,15 @@ bool CWinSystemWaylandWebOS::InitWindowSystem()
   m_webosRegistry->RequestSingleton(m_webosForeign, 1, 2, false);
   m_webosRegistry->Bind();
 
-  return ok;
+  return true;
 }
 
 bool CWinSystemWaylandWebOS::CreateNewWindow(const std::string& name,
                                              bool fullScreen,
                                              RESOLUTION_INFO& res)
 {
-  auto ok = CWinSystemWayland::CreateNewWindow(name, fullScreen, res);
+  if (!CWinSystemWayland::CreateNewWindow(name, fullScreen, res))
+    return false;
 
   if (m_webosForeign)
   {
@@ -51,12 +53,14 @@ bool CWinSystemWaylandWebOS::CreateNewWindow(const std::string& name,
         static_cast<uint32_t>(wayland::webos_foreign_webos_exported_type::video_object));
     m_exportedSurface.on_window_id_assigned() = [this](std::string window_id,
                                                        uint32_t exported_type) {
-      CLog::Log(LOGDEBUG, "Wayland foreign video surface exported {}", window_id);
+      CLog::Log(LOGDEBUG,
+                "CreateNewWindow:CDVDVideoCodecStarfish: Foreign video surface exported {}",
+                window_id);
       this->m_exportedWindowName = window_id;
     };
   }
 
-  return ok;
+  return true;
 }
 
 CWinSystemWaylandWebOS::~CWinSystemWaylandWebOS() noexcept
@@ -85,14 +89,13 @@ bool CWinSystemWaylandWebOS::SetExportedWindow(CRect orig, CRect src, CRect dest
 {
   if (m_webosForeign)
   {
-    CLog::Log(LOGINFO,
-              "CWinSystemWaylandWebOS::SetExportedWindow orig {} {} {} {} src {} {} {} {} -> dest "
-              "{} {} {} {}",
-              orig.x1, orig.y1, orig.x2, orig.y2, src.x1, src.y1, src.x2, src.y2, dest.x1, dest.y1,
-              dest.x2, dest.y2);
-    auto origRegion = m_compositor.create_region();
-    auto srcRegion = m_compositor.create_region();
-    auto dstRegion = m_compositor.create_region();
+    CLog::LogF(LOGINFO,
+               "CWinSystemWaylandWebOS: orig {} {} {} {} src {} {} {} {} -> dest {} {} {} {}",
+               orig.x1, orig.y1, orig.x2, orig.y2, src.x1, src.y1, src.x2, src.y2, dest.x1, dest.y1,
+               dest.x2, dest.y2);
+    wayland::region_t origRegion = m_compositor.create_region();
+    wayland::region_t srcRegion = m_compositor.create_region();
+    wayland::region_t dstRegion = m_compositor.create_region();
     origRegion.add(static_cast<int>(orig.x1), static_cast<int>(orig.y1),
                    static_cast<int>(orig.Width()), static_cast<int>(orig.Height()));
     srcRegion.add(static_cast<int>(src.x1), static_cast<int>(src.y1), static_cast<int>(src.Width()),
