b804a58d0ef9ba8d25fbd1d823ae7716faf48a76
Ryan Rector <rmrector@gmail.com>
2023-04-23 23:30:22 -0600

    Move pulling embedded music images into the cache...
    to the new loader interface and factory. This is just moving some code
    around, this is still the same functionality called at the same point
    during execution.

---
+++ xbmc/music/MusicEmbeddedImageFileLoader.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2023 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "MusicEmbeddedImageFileLoader.h"
+
+#include "FileItem.h"
+#include "guilib/Texture.h"
+#include "music/tags/ImusicInfoTagLoader.h"
+#include "music/tags/MusicInfoTag.h"
+#include "music/tags/MusicInfoTagLoaderFactory.h"
+#include "utils/EmbeddedArt.h"
+
+using namespace MUSIC_INFO;
+
+bool CMusicEmbeddedImageFileLoader::CanLoad(std::string specialType) const
+{
+  return specialType == "music";
+}
+
+namespace
+{
+bool GetEmbeddedThumb(const std::string& path, EmbeddedArt& art)
+{
+  CFileItem item(path, false);
+  std::unique_ptr<IMusicInfoTagLoader> loader(CMusicInfoTagLoaderFactory::CreateLoader(item));
+  CMusicInfoTag tag;
+  if (loader)
+    loader->Load(path, tag, &art);
+
+  return !art.Empty();
+}
+} // namespace
+
+std::unique_ptr<CTexture> CMusicEmbeddedImageFileLoader::Load(std::string specialType,
+                                                              std::string filePath,
+                                                              unsigned int preferredWidth,
+                                                              unsigned int preferredHeight) const
+{
+  EmbeddedArt art;
+  if (GetEmbeddedThumb(filePath, art))
+    return CTexture::LoadFromFileInMemory(art.m_data.data(), art.m_size, art.m_mime, preferredWidth,
+                                          preferredHeight);
+  return nullptr;
+}
--- xbmc/music/MusicThumbLoader.cpp
+++ xbmc/music/MusicThumbLoader.cpp
@@ -12,7 +12,6 @@
 #include "TextureDatabase.h"
 #include "music/infoscanner/MusicInfoScanner.h"
 #include "music/tags/MusicInfoTag.h"
-#include "music/tags/MusicInfoTagLoaderFactory.h"
 #include "utils/StringUtils.h"
 #include "video/VideoThumbLoader.h"
 
@@ -371,15 +370,4 @@ bool CMusicThumbLoader::FillLibraryArt(CFileItem &item)
 
   item.SetProperty("libraryartfilled", true);
   return artfound;
-}
-
-bool CMusicThumbLoader::GetEmbeddedThumb(const std::string &path, EmbeddedArt &art)
-{
-  CFileItem item(path, false);
-  std::unique_ptr<IMusicInfoTagLoader> pLoader (CMusicInfoTagLoaderFactory::CreateLoader(item));
-  CMusicInfoTag tag;
-  if (nullptr != pLoader)
-    pLoader->Load(path, tag, &art);
-
-  return !art.Empty();
 }
