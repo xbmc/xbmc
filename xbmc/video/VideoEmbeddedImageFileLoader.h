bb501849b089f1b82c7738ce34afec9f45c98401
Ryan Rector <rmrector@gmail.com>
2023-04-23 23:11:14 -0600

    Move pulling embedded video images into the cache...
    to the new loader interface and factory, posters and fanart. This is
    just moving some code around, this is still the same functionality
    called at the same point during execution.

---
+++ xbmc/video/VideoEmbeddedImageFileLoader.h
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) 2023 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "imagefiles/SpecialImageFileLoader.h"
+
+namespace VIDEO
+{
+
+class CVideoEmbeddedImageFileLoader : public IMAGE_FILES::ISpecialImageFileLoader
+{
+public:
+  CVideoEmbeddedImageFileLoader() = default;
+  ~CVideoEmbeddedImageFileLoader() override = default;
+
+  bool CanLoad(std::string specialType) const override;
+  std::unique_ptr<CTexture> Load(std::string specialType,
+                                 std::string filePath,
+                                 unsigned int preferredWidth,
+                                 unsigned int preferredHeight) const override;
+};
+
+} // namespace VIDEO
