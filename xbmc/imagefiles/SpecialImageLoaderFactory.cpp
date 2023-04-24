a367f5fd1729c49feed876124b2d700a2647b3bf
Ryan Rector <rmrector@gmail.com>
2023-04-23 22:35:04 -0600

    Introduce an interface for pulling all images into cache
    A factory around them, and the expected consumer. This commit adds no
    functional change, just a bit of wiring.

---
+++ xbmc/imagefiles/SpecialImageLoaderFactory.cpp
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2023 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "SpecialImageLoaderFactory.h"
+
+#include "guilib/Texture.h"
+
+using namespace IMAGE_FILES;
+
+CSpecialImageLoaderFactory::CSpecialImageLoaderFactory()
+{
+}
+
+std::unique_ptr<CTexture> CSpecialImageLoaderFactory::Load(std::string specialType,
+                                                           std::string filePath,
+                                                           unsigned int preferredWidth,
+                                                           unsigned int preferredHeight) const
+{
+  if (specialType.empty())
+    return {};
+  for (auto& loader : m_specialImageLoaders)
+  {
+    if (loader->CanLoad(specialType))
+    {
+      auto val = loader->Load(specialType, filePath, preferredWidth, preferredHeight);
+      if (val)
+        return val;
+    }
+  }
+  return {};
+}
