10d3afdd7ec1e424e876c125356904e3095471c9
Lukas Rusak <lorusak@gmail.com>
2023-05-01 19:29:34 -0700

    Revert "[cmake] glx: find glx library instead of gl"
    This reverts commit 26fe222b503a89b973c537bea37b140a40e8359e.

--- cmake/modules/FindGLX.cmake
+++ cmake/modules/FindGLX.cmake
@@ -20,7 +20,7 @@ endif()
 
 find_path(GLX_INCLUDE_DIR NAMES GL/glx.h
                           PATHS ${PC_GLX_INCLUDEDIR})
-find_library(GLX_LIBRARY NAMES GLX
+find_library(GLX_LIBRARY NAMES GL
                          PATHS ${PC_GLX_LIBDIR})
 
 include(FindPackageHandleStandardArgs)
