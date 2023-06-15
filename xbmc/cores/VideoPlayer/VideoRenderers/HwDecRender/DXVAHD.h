04d9d7afd2c88eb3a496d38fba1fcdabb9cee854
CrystalP <crystalp@kodi.tv>
2023-06-15 13:25:53 -0400

    [Windows][dxva] CProcessorHD refactor: moved handling of the d3d enumerator interfaces to the new CEnumeratorHD class - unifies the static and non-static code paths - reduces duplication

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/DXVAHD.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/DXVAHD.h
@@ -10,6 +10,7 @@
 
 #include "DVDCodecs/Video/DVDVideoCodecFFmpeg.h"
 #include "DVDCodecs/Video/DXVA.h"
+#include "VideoRenderers/HwDecRender/DXVAEnumeratorHD.h"
 #include "VideoRenderers/Windows/RendererBase.h"
 #include "guilib/D3DResource.h"
 #include "utils/Geometry.h"
@@ -22,35 +23,7 @@ class CRenderBuffer;
 
 namespace DXVA {
 
-// ProcAmp filters d3d11 filters
-struct ProcAmpFilter
-{
-  D3D11_VIDEO_PROCESSOR_FILTER filter;
-  D3D11_VIDEO_PROCESSOR_FILTER_CAPS cap;
-  const char* name;
-};
-
-// clang-format off
-const ProcAmpFilter PROCAMP_FILTERS[] = {
-    {D3D11_VIDEO_PROCESSOR_FILTER_BRIGHTNESS,
-    D3D11_VIDEO_PROCESSOR_FILTER_CAPS_BRIGHTNESS, "Brightness"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_CONTRAST,
-    D3D11_VIDEO_PROCESSOR_FILTER_CAPS_CONTRAST, "Contrast"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_HUE,
-    D3D11_VIDEO_PROCESSOR_FILTER_CAPS_HUE, "Hue"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_SATURATION,
-    D3D11_VIDEO_PROCESSOR_FILTER_CAPS_SATURATION, "Saturation"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_NOISE_REDUCTION,
-    D3D11_VIDEO_PROCESSOR_FILTER_CAPS_NOISE_REDUCTION, "Noise Reduction"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_EDGE_ENHANCEMENT,
-     D3D11_VIDEO_PROCESSOR_FILTER_CAPS_EDGE_ENHANCEMENT, "Edge Enhancement"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_ANAMORPHIC_SCALING,
-     D3D11_VIDEO_PROCESSOR_FILTER_CAPS_ANAMORPHIC_SCALING, "Anamorphic Scaling"},
-    {D3D11_VIDEO_PROCESSOR_FILTER_STEREO_ADJUSTMENT,
-     D3D11_VIDEO_PROCESSOR_FILTER_CAPS_STEREO_ADJUSTMENT, "Stereo Adjustment"}};
-// clang-format on
-
-const size_t NUM_FILTERS = ARRAYSIZE(PROCAMP_FILTERS);
+using namespace Microsoft::WRL;
 
 struct DXGIColorSpaceArgs
 {
@@ -88,7 +61,7 @@ public:
   bool Open(UINT width, UINT height, const VideoPicture& picture);
   void Close();
   bool Render(CRect src, CRect dst, ID3D11Resource* target, CRenderBuffer **views, DWORD flags, UINT frameIdx, UINT rotation, float contrast, float brightness);
-  uint8_t PastRefs() const { return m_max_back_refs; }
+  uint8_t PastRefs() const { return std::min(m_procCaps.m_rateCaps.PastFrames, 4u); }
   bool IsFormatSupported(DXGI_FORMAT format, D3D11_VIDEO_PROCESSOR_FORMAT_SUPPORT support) const;
   /*!
    * \brief Evaluate if the DXVA processor supports converting between two formats and color spaces.
@@ -119,8 +92,6 @@ public:
     UnInit();
   }
 
-  static bool IsBT2020Supported();
-  static bool IsPQ10PassthroughSupported(const DXGI_FORMAT dxgi_format);
   static bool IsSuperResolutionSuitable(const VideoPicture& picture);
   void TryEnableVideoSuperResolution();
   bool IsVideoSuperResolutionEnabled() const { return m_superResolutionEnabled; }
@@ -165,33 +136,18 @@ protected:
 
   uint32_t m_width = 0;
   uint32_t m_height = 0;
-  uint8_t  m_max_back_refs = 0;
-  uint8_t  m_max_fwd_refs = 0;
-  uint32_t m_procIndex = 0;
-  D3D11_VIDEO_PROCESSOR_CAPS m_vcaps = {};
-  D3D11_VIDEO_PROCESSOR_RATE_CONVERSION_CAPS m_rateCaps = {};
-  bool m_bSupportHLG = false;
-  bool m_HDR10Left{false};
-  bool m_BT2020Left{false};
-  bool m_hasMetadataHDR10Support{false};
 
-  struct ProcAmpInfo
-  {
-    bool bSupported;
-    D3D11_VIDEO_PROCESSOR_FILTER_RANGE Range;
-  };
-  ProcAmpInfo m_Filters[NUM_FILTERS]{};
-  Microsoft::WRL::ComPtr<ID3D11VideoDevice> m_pVideoDevice;
-  Microsoft::WRL::ComPtr<ID3D11VideoContext> m_pVideoContext;
-  Microsoft::WRL::ComPtr<ID3D11VideoProcessorEnumerator> m_pEnumerator;
-  Microsoft::WRL::ComPtr<ID3D11VideoProcessorEnumerator1> m_pEnumerator1;
-  Microsoft::WRL::ComPtr<ID3D11VideoProcessor> m_pVideoProcessor;
+  ComPtr<ID3D11VideoDevice> m_pVideoDevice;
+  ComPtr<ID3D11VideoContext> m_pVideoContext;
+  ComPtr<ID3D11VideoProcessor> m_pVideoProcessor;
+  std::unique_ptr<CEnumeratorHD> m_enumerator;
 
   AVColorPrimaries m_color_primaries{AVCOL_PRI_UNSPECIFIED};
   AVColorTransferCharacteristic m_color_transfer{AVCOL_TRC_UNSPECIFIED};
+  ProcessorCapabilities m_procCaps{};
+  DXGI_FORMAT m_input_dxgi_format{DXGI_FORMAT_UNKNOWN};
 
   bool m_forced8bit{false};
   bool m_superResolutionEnabled{false};
 };
-
 };
