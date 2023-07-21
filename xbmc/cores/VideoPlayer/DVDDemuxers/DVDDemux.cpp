15fe4ccdadf4b4998a680e7bfc8d03b5ebd02a42
CastagnaIT <gottardo.stefano.83@gmail.com>
2023-07-21 17:55:05 +0200

    [DVDDemux][cleanup] applied clang format

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemux.cpp
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemux.cpp
@@ -13,59 +13,59 @@ std::string CDemuxStreamAudio::GetStreamType()
   std::string strInfo;
   switch (codec)
   {
-  case AV_CODEC_ID_AC3:
-    strInfo = "AC3 ";
-    break;
-  case AV_CODEC_ID_EAC3:
-    strInfo = "DD+ ";
-    break;
-  case AV_CODEC_ID_DTS:
-  {
-    switch (profile)
+    case AV_CODEC_ID_AC3:
+      strInfo = "AC3 ";
+      break;
+    case AV_CODEC_ID_EAC3:
+      strInfo = "DD+ ";
+      break;
+    case AV_CODEC_ID_DTS:
     {
-    case FF_PROFILE_DTS_HD_MA:
-      strInfo = "DTS-HD MA ";
-      break;
-    case FF_PROFILE_DTS_HD_HRA:
-      strInfo = "DTS-HD HRA ";
-      break;
-    default:
-      strInfo = "DTS ";
+      switch (profile)
+      {
+        case FF_PROFILE_DTS_HD_MA:
+          strInfo = "DTS-HD MA ";
+          break;
+        case FF_PROFILE_DTS_HD_HRA:
+          strInfo = "DTS-HD HRA ";
+          break;
+        default:
+          strInfo = "DTS ";
+          break;
+      }
       break;
     }
-    break;
-  }
-  case AV_CODEC_ID_MP2:
-    strInfo = "MP2 ";
-    break;
-  case AV_CODEC_ID_MP3:
-    strInfo = "MP3 ";
-    break;
-  case AV_CODEC_ID_TRUEHD:
-    strInfo = "TrueHD ";
-    break;
-  case AV_CODEC_ID_AAC:
-    strInfo = "AAC ";
-    break;
-  case AV_CODEC_ID_ALAC:
-    strInfo = "ALAC ";
-    break;
-  case AV_CODEC_ID_FLAC:
-    strInfo = "FLAC ";
-    break;
-  case AV_CODEC_ID_OPUS:
-    strInfo = "Opus ";
-    break;
-  case AV_CODEC_ID_VORBIS:
-    strInfo = "Vorbis ";
-    break;
-  case AV_CODEC_ID_PCM_BLURAY:
-  case AV_CODEC_ID_PCM_DVD:
-    strInfo = "PCM ";
-    break;
-  default:
-    strInfo = "";
-    break;
+    case AV_CODEC_ID_MP2:
+      strInfo = "MP2 ";
+      break;
+    case AV_CODEC_ID_MP3:
+      strInfo = "MP3 ";
+      break;
+    case AV_CODEC_ID_TRUEHD:
+      strInfo = "TrueHD ";
+      break;
+    case AV_CODEC_ID_AAC:
+      strInfo = "AAC ";
+      break;
+    case AV_CODEC_ID_ALAC:
+      strInfo = "ALAC ";
+      break;
+    case AV_CODEC_ID_FLAC:
+      strInfo = "FLAC ";
+      break;
+    case AV_CODEC_ID_OPUS:
+      strInfo = "Opus ";
+      break;
+    case AV_CODEC_ID_VORBIS:
+      strInfo = "Vorbis ";
+      break;
+    case AV_CODEC_ID_PCM_BLURAY:
+    case AV_CODEC_ID_PCM_DVD:
+      strInfo = "PCM ";
+      break;
+    default:
+      strInfo = "";
+      break;
   }
 
   strInfo += m_channelLayoutName;
@@ -79,7 +79,8 @@ int CDVDDemux::GetNrOfStreams(StreamType streamType)
 
   for (auto pStream : GetStreams())
   {
-    if (pStream && pStream->type == streamType) iCounter++;
+    if (pStream && pStream->type == streamType)
+      iCounter++;
   }
 
   return iCounter;
