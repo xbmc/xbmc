44e72a4dd734f89a4ffe60faaa3f89bf5477a4f3
Frank Howie <howie-f@kodi.tv>
2023-07-27 22:19:58 +0200

    [cleanup] change deprecated 'CAddonMgr::IsCompatible(const IAddon& addon)'
    this member takes a 'const std::shared_ptr<IAddon>&' now
    
    Signed-off-by: Frank Howie <howie-f@kodi.tv>

--- xbmc/addons/AddonManager.cpp
+++ xbmc/addons/AddonManager.cpp
@@ -901,7 +901,7 @@ bool CAddonMgr::EnableSingle(const std::string& id)
 
   auto eventLog = CServiceBroker::GetEventLog();
 
-  if (!IsCompatible(*addon))
+  if (!IsCompatible(addon))
   {
     CLog::Log(LOGERROR, "Add-on '{}' is not compatible with Kodi", addon->ID());
     if (eventLog)
@@ -1111,9 +1111,9 @@ void CAddonMgr::PublishInstanceRemoved(const std::string& addonId, AddonInstance
   m_events.Publish(AddonEvents::InstanceRemoved(addonId, instanceId));
 }
 
-bool CAddonMgr::IsCompatible(const IAddon& addon) const
+bool CAddonMgr::IsCompatible(const std::shared_ptr<const IAddon>& addon) const
 {
-  for (const auto& dependency : addon.GetDependencies())
+  for (const auto& dependency : addon->GetDependencies())
   {
     if (!dependency.optional)
     {
@@ -1122,10 +1122,10 @@ bool CAddonMgr::IsCompatible(const IAddon& addon) const
       if (StringUtils::StartsWith(dependency.id, "xbmc.") ||
           StringUtils::StartsWith(dependency.id, "kodi."))
       {
-        AddonPtr addon;
-        bool haveAddon =
-            GetAddon(dependency.id, addon, AddonType::UNKNOWN, OnlyEnabled::CHOICE_YES);
-        if (!haveAddon || !addon->MeetsVersion(dependency.versionMin, dependency.version))
+        std::shared_ptr<IAddon> dep;
+        const bool haveDependency =
+            GetAddon(dependency.id, dep, AddonType::UNKNOWN, OnlyEnabled::CHOICE_YES);
+        if (!haveDependency || !dep->MeetsVersion(dependency.versionMin, dependency.version))
           return false;
       }
     }
