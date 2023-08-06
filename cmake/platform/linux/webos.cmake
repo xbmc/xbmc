1468675dcda22803b3e09f82cb5fc48a9da2b668
Stephan Sundermann <stephansundermann@gmail.com>
2023-08-06 22:12:38 +0200

    [webOS] Add PlayerFactory library

--- cmake/platform/linux/webos.cmake
+++ cmake/platform/linux/webos.cmake
@@ -4,7 +4,7 @@ include(${CMAKE_SOURCE_DIR}/cmake/platform/${CORE_SYSTEM_NAME}/wayland.cmake)
 # saves reworking other assumptions for linux windowing as the platform name.
 list(APPEND CORE_PLATFORM_NAME_LC wayland)
 
-list(APPEND PLATFORM_REQUIRED_DEPS WaylandProtocolsWebOS PlayerAPIs WebOSHelpers)
+list(APPEND PLATFORM_REQUIRED_DEPS WaylandProtocolsWebOS PlayerAPIs PlayerFactory WebOSHelpers)
 list(APPEND PLATFORM_GLOBAL_TARGET_DEPS generate-wayland-webos-protocols)
 list(APPEND ARCH_DEFINES -DTARGET_WEBOS)
 set(ENABLE_PULSEAUDIO OFF CACHE BOOL "" FORCE)
