8eeceb66805c4075b61937846147807ed404bc25
ksooo <3226626+ksooo@users.noreply.github.com>
2023-08-07 10:24:00 +0200

    [video] Do not fallback to play the item (which is the default onclick implementation) in case video info could not be displayed.

--- xbmc/video/windows/GUIWindowVideoNav.h
+++ xbmc/video/windows/GUIWindowVideoNav.h
@@ -37,7 +37,7 @@ public:
   bool OnAction(const CAction &action) override;
   bool OnMessage(CGUIMessage& message) override;
 
-  void OnItemInfo(const CFileItem& fileItem, ADDON::ScraperPtr &info) override;
+  bool OnItemInfo(const CFileItem& fileItem, ADDON::ScraperPtr& info) override;
 
 protected:
   bool ApplyWatchedFilter(CFileItemList &items);
