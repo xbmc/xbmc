8c8d1ce64a7188f72aff74bbcefca47d817282a6
Glenn Guy <glennguy@users.noreply.github.com>
2023-08-14 14:20:09 +0200

    [Android] support ClearKey crypto type

--- xbmc/cores/VideoPlayer/DVDInputStreams/InputStreamAddon.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/InputStreamAddon.cpp
@@ -539,10 +539,9 @@ KODI_HANDLE CInputStreamAddon::cb_get_stream_transfer(KODI_HANDLE handle,
       stream->m_cryptoSession.keySystem < STREAM_CRYPTO_KEY_SYSTEM_COUNT)
   {
     static const CryptoSessionSystem map[] = {
-        CRYPTO_SESSION_SYSTEM_NONE,
-        CRYPTO_SESSION_SYSTEM_WIDEVINE,
-        CRYPTO_SESSION_SYSTEM_PLAYREADY,
-        CRYPTO_SESSION_SYSTEM_WISEPLAY,
+        CRYPTO_SESSION_SYSTEM_NONE,      CRYPTO_SESSION_SYSTEM_WIDEVINE,
+        CRYPTO_SESSION_SYSTEM_PLAYREADY, CRYPTO_SESSION_SYSTEM_WISEPLAY,
+        CRYPTO_SESSION_SYSTEM_CLEARKEY,
     };
     demuxStream->cryptoSession = std::shared_ptr<DemuxCryptoSession>(
         new DemuxCryptoSession(map[stream->m_cryptoSession.keySystem],
