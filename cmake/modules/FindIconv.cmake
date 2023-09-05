8ddd6bba9c177bb3b26003ae977b918a0ea1aa04
fuzzard <fuzzard@kodi.tv>
2023-09-05 17:59:10 +1000

    [cmake] FindIconv migrate to full TARGET usage

--- cmake/modules/FindIconv.cmake
+++ cmake/modules/FindIconv.cmake
@@ -3,42 +3,36 @@
 # --------
 # Finds the ICONV library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# ICONV_FOUND - system has ICONV
-# ICONV_INCLUDE_DIRS - the ICONV include directory
-# ICONV_LIBRARIES - the ICONV libraries
-#
-# and the following imported targets::
-#
-#   ICONV::ICONV   - The ICONV library
+#   ICONV::ICONV - The ICONV library
 
-find_path(ICONV_INCLUDE_DIR NAMES iconv.h)
+if(NOT TARGET ICONV::ICONV)
+  find_path(ICONV_INCLUDE_DIR NAMES iconv.h
+                              HINTS ${DEPENDS_PATH}/include
+                              NO_CACHE)
 
-find_library(ICONV_LIBRARY NAMES iconv libiconv c)
+  find_library(ICONV_LIBRARY NAMES iconv libiconv c
+                             HINTS ${DEPENDS_PATH}/lib
+                             NO_CACHE)
 
-set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARY})
-check_function_exists(iconv HAVE_ICONV_FUNCTION)
-if(NOT HAVE_ICONV_FUNCTION)
-  check_function_exists(libiconv HAVE_LIBICONV_FUNCTION2)
-  set(HAVE_ICONV_FUNCTION ${HAVE_LIBICONV_FUNCTION2})
-  unset(HAVE_LIBICONV_FUNCTION2)
-endif()
+  set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARY})
+  check_function_exists(iconv HAVE_ICONV_FUNCTION)
+  if(NOT HAVE_ICONV_FUNCTION)
+    check_function_exists(libiconv HAVE_LIBICONV_FUNCTION2)
+    set(HAVE_ICONV_FUNCTION ${HAVE_LIBICONV_FUNCTION2})
+    unset(HAVE_LIBICONV_FUNCTION2)
+  endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Iconv
-                                  REQUIRED_VARS ICONV_LIBRARY ICONV_INCLUDE_DIR HAVE_ICONV_FUNCTION)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Iconv
+                                    REQUIRED_VARS ICONV_LIBRARY ICONV_INCLUDE_DIR HAVE_ICONV_FUNCTION)
 
-if(ICONV_FOUND)
-  set(ICONV_LIBRARIES ${ICONV_LIBRARY})
-  set(ICONV_INCLUDE_DIRS ${ICONV_INCLUDE_DIR})
-
-  if(NOT TARGET ICONV::ICONV)
+  if(ICONV_FOUND)
     add_library(ICONV::ICONV UNKNOWN IMPORTED)
     set_target_properties(ICONV::ICONV PROPERTIES
-                                     IMPORTED_LOCATION "${ICONV_LIBRARY}"
-                                     INTERFACE_INCLUDE_DIRECTORIES "${ICONV_INCLUDE_DIR}")
+                                       IMPORTED_LOCATION "${ICONV_LIBRARY}"
+                                       INTERFACE_INCLUDE_DIRECTORIES "${ICONV_INCLUDE_DIR}")
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP ICONV::ICONV)
   endif()
 endif()
-
-mark_as_advanced(ICONV_INCLUDE_DIR ICONV_LIBRARY HAVE_ICONV_FUNCTION)
