2eca856d938ec04d858e00eaf8f492d98713ba9f
fuzzard <fuzzard@kodi.tv>
2023-09-05 15:35:22 +1000

    [cmake] FindLibUSB migrate to full TARGET usage

--- cmake/modules/FindLibUSB.cmake
+++ cmake/modules/FindLibUSB.cmake
@@ -3,43 +3,36 @@
 # ----------
 # Finds the USB library
 #
-# This will define the following variables::
-#
-# LIBUSB_FOUND - system has LibUSB
-# LIBUSB_INCLUDE_DIRS - the USB include directory
-# LIBUSB_LIBRARIES - the USB libraries
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   LibUSB::LibUSB   - The USB library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_LIBUSB libusb QUIET)
-endif()
+if(NOT TARGET LibUSB::LibUSB)
+  find_package(PkgConfig)
 
-find_path(LIBUSB_INCLUDE_DIR usb.h
-                             PATHS ${PC_LIBUSB_INCLUDEDIR})
-find_library(LIBUSB_LIBRARY NAMES usb
-                            PATHS ${PC_LIBUSB_INCLUDEDIR})
-set(LIBUSB_VERSION ${PC_LIBUSB_VERSION})
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_LIBUSB libusb QUIET)
+  endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibUSB
-                                  REQUIRED_VARS LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR
-                                  VERSION_VAR LIBUSB_VERSION)
+  find_path(LIBUSB_INCLUDE_DIR usb.h
+                               PATHS ${PC_LIBUSB_INCLUDEDIR}
+                               NO_CACHE)
+  find_library(LIBUSB_LIBRARY NAMES usb
+                              PATHS ${PC_LIBUSB_INCLUDEDIR}
+                              NO_CACHE)
+  set(LIBUSB_VERSION ${PC_LIBUSB_VERSION})
 
-if(LIBUSB_FOUND)
-  set(LIBUSB_INCLUDE_DIRS ${LIBUSB_INCLUDE_DIR})
-  set(LIBUSB_LIBRARIES ${LIBUSB_LIBRARY})
-  set(LIBUSB_DEFINITIONS -DUSE_LIBUSB=1)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibUSB
+                                    REQUIRED_VARS LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR
+                                    VERSION_VAR LIBUSB_VERSION)
 
-  if(NOT TARGET LibUSB::LibUSB)
+  if(LIBUSB_FOUND)
     add_library(LibUSB::LibUSB UNKNOWN IMPORTED)
     set_target_properties(LibUSB::LibUSB PROPERTIES
                                          IMPORTED_LOCATION "${LIBUSB_LIBRARY}"
                                          INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIR}"
                                          INTERFACE_COMPILE_DEFINITIONS USE_LIBUSB=1)
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP LibUSB::LibUSB)
   endif()
 endif()
-
-mark_as_advanced(USB_INCLUDE_DIR USB_LIBRARY)
