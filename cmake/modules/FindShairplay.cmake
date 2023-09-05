edd99bebee86f9ea11249f124883693de3f352ae
fuzzard <fuzzard@kodi.tv>
2023-09-05 15:48:31 +1000

    [cmake] FindShairplay migrate to full TARGET usage

--- cmake/modules/FindShairplay.cmake
+++ cmake/modules/FindShairplay.cmake
@@ -3,55 +3,42 @@
 # -------------
 # Finds the Shairplay library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# SHAIRPLAY_FOUND - system has Shairplay
-# SHAIRPLAY_INCLUDE_DIRS - the Shairplay include directory
-# SHAIRPLAY_LIBRARIES - the Shairplay libraries
-# SHAIRPLAY_DEFINITIONS - the Shairplay compile definitions
-#
-# and the following imported targets::
-#
-#   Shairplay::Shairplay   - The Shairplay library
+#   Shairplay::Shairplay - The Shairplay library
 
-find_path(SHAIRPLAY_INCLUDE_DIR shairplay/raop.h)
+if(NOT TARGET Shairplay::Shairplay)
 
-include(FindPackageHandleStandardArgs)
-find_library(SHAIRPLAY_LIBRARY NAMES shairplay libshairplay)
+  find_path(SHAIRPLAY_INCLUDE_DIR shairplay/raop.h NO_CACHE)
 
-if(SHAIRPLAY_INCLUDE_DIR AND SHAIRPLAY_LIBRARY)
-  include(CheckCSourceCompiles)
-  set(CMAKE_REQUIRED_INCLUDES ${SHAIRPLAY_INCLUDE_DIR})
-  set(CMAKE_REQUIRED_LIBRARIES ${SHAIRPLAY_LIBRARIES})
-  check_c_source_compiles("#include <shairplay/raop.h>
+  include(FindPackageHandleStandardArgs)
+  find_library(SHAIRPLAY_LIBRARY NAMES shairplay libshairplay
+                                 NO_CACHE)
 
-                           int main()
-                           {
-                             struct raop_callbacks_s foo;
-                             foo.cls;
-                             return 0;
-                           }
-                          " HAVE_SHAIRPLAY_CALLBACK_CLS)
-endif()
+  if(SHAIRPLAY_INCLUDE_DIR AND SHAIRPLAY_LIBRARY)
+    include(CheckCSourceCompiles)
+    set(CMAKE_REQUIRED_INCLUDES ${SHAIRPLAY_INCLUDE_DIR})
+    set(CMAKE_REQUIRED_LIBRARIES ${SHAIRPLAY_LIBRARIES})
+    check_c_source_compiles("#include <shairplay/raop.h>
 
-find_package_handle_standard_args(Shairplay
-                                  REQUIRED_VARS SHAIRPLAY_LIBRARY SHAIRPLAY_INCLUDE_DIR HAVE_SHAIRPLAY_CALLBACK_CLS)
+                             int main()
+                             {
+                               struct raop_callbacks_s foo;
+                               foo.cls;
+                               return 0;
+                             }
+                            " HAVE_SHAIRPLAY_CALLBACK_CLS)
+  endif()
 
-if(SHAIRPLAY_FOUND)
-  set(SHAIRPLAY_LIBRARIES ${SHAIRPLAY_LIBRARY})
-  set(SHAIRPLAY_INCLUDE_DIRS ${SHAIRPLAY_INCLUDE_DIR})
-  set(SHAIRPLAY_DEFINITIONS -DHAS_AIRTUNES=1)
+  find_package_handle_standard_args(Shairplay
+                                    REQUIRED_VARS SHAIRPLAY_LIBRARY SHAIRPLAY_INCLUDE_DIR HAVE_SHAIRPLAY_CALLBACK_CLS)
 
-  if(NOT TARGET Shairplay::Shairplay)
+  if(SHAIRPLAY_FOUND)
     add_library(Shairplay::Shairplay UNKNOWN IMPORTED)
-    if(SHAIRPLAY_LIBRARY)
-      set_target_properties(Shairplay::Shairplay PROPERTIES
-                                                 IMPORTED_LOCATION "${SHAIRPLAY_LIBRARY}")
-    endif()
     set_target_properties(Shairplay::Shairplay PROPERTIES
+                                               IMPORTED_LOCATION "${SHAIRPLAY_LIBRARY}"
                                                INTERFACE_INCLUDE_DIRECTORIES "${SHAIRPLAY_INCLUDE_DIR}"
                                                INTERFACE_COMPILE_DEFINITIONS HAS_AIRTUNES=1)
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP Shairplay::Shairplay)
   endif()
 endif()
-
-mark_as_advanced(SHAIRPLAY_INCLUDE_DIR SHAIRPLAY_LIBRARY)
