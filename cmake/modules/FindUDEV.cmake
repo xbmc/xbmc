12c5e4479f9a8679425aa0270cadc659ee7bef64
fuzzard <fuzzard@kodi.tv>
2023-09-05 15:32:02 +1000

    [cmake] FindUDEV migrate to full TARGET usage

--- cmake/modules/FindUDEV.cmake
+++ cmake/modules/FindUDEV.cmake
@@ -3,45 +3,36 @@
 # -------
 # Finds the UDEV library
 #
-# This will define the following variables::
-#
-# UDEV_FOUND - system has UDEV
-# UDEV_INCLUDE_DIRS - the UDEV include directory
-# UDEV_LIBRARIES - the UDEV libraries
-# UDEV_DEFINITIONS - the UDEV definitions
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   UDEV::UDEV   - The UDEV library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_UDEV libudev QUIET)
-endif()
+if(NOT TARGET UDEV::UDEV)
+  find_package(PkgConfig)
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_UDEV libudev QUIET)
+  endif()
 
-find_path(UDEV_INCLUDE_DIR NAMES libudev.h
-                           PATHS ${PC_UDEV_INCLUDEDIR})
-find_library(UDEV_LIBRARY NAMES udev
-                          PATHS ${PC_UDEV_LIBDIR})
+  find_path(UDEV_INCLUDE_DIR NAMES libudev.h
+                             PATHS ${PC_UDEV_INCLUDEDIR}
+                             NO_CACHE)
+  find_library(UDEV_LIBRARY NAMES udev
+                            PATHS ${PC_UDEV_LIBDIR}
+                            NO_CACHE)
 
-set(UDEV_VERSION ${PC_UDEV_VERSION})
+  set(UDEV_VERSION ${PC_UDEV_VERSION})
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(UDEV
-                                  REQUIRED_VARS UDEV_LIBRARY UDEV_INCLUDE_DIR
-                                  VERSION_VAR UDEV_VERSION)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(UDEV
+                                    REQUIRED_VARS UDEV_LIBRARY UDEV_INCLUDE_DIR
+                                    VERSION_VAR UDEV_VERSION)
 
-if(UDEV_FOUND)
-  set(UDEV_LIBRARIES ${UDEV_LIBRARY})
-  set(UDEV_INCLUDE_DIRS ${UDEV_INCLUDE_DIR})
-  set(UDEV_DEFINITIONS -DHAVE_LIBUDEV=1)
-
-  if(NOT TARGET UDEV::UDEV)
+  if(UDEV_FOUND)
     add_library(UDEV::UDEV UNKNOWN IMPORTED)
     set_target_properties(UDEV::UDEV PROPERTIES
                                    IMPORTED_LOCATION "${UDEV_LIBRARY}"
                                    INTERFACE_INCLUDE_DIRECTORIES "${UDEV_INCLUDE_DIR}"
                                    INTERFACE_COMPILE_DEFINITIONS HAVE_LIBUDEV=1)
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP UDEV::UDEV)
   endif()
 endif()
-
-mark_as_advanced(UDEV_INCLUDE_DIR UDEV_LIBRARY)
