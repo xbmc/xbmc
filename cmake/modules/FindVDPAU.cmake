ea75b4036148682af7572a3e4baf28e78e5c1e46
fuzzard <fuzzard@kodi.tv>
2023-09-05 17:10:53 +1000

    [cmake] FindVDPAU migrate to full TARGET usage

--- cmake/modules/FindVDPAU.cmake
+++ cmake/modules/FindVDPAU.cmake
@@ -3,45 +3,36 @@
 # ---------
 # Finds the VDPAU library
 #
-# This will define the following variables::
-#
-# VDPAU_FOUND - system has VDPAU
-# VDPAU_INCLUDE_DIRS - the VDPAU include directory
-# VDPAU_LIBRARIES - the VDPAU libraries
-# VDPAU_DEFINITIONS - the VDPAU definitions
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   VDPAU::VDPAU   - The VDPAU library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_VDPAU vdpau QUIET)
-endif()
+if(NOT TARGET VDPAU::VDPAU)
+  find_package(PkgConfig)
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_VDPAU vdpau QUIET)
+  endif()
 
-find_path(VDPAU_INCLUDE_DIR NAMES vdpau/vdpau.h vdpau/vdpau_x11.h
-                            PATHS ${PC_VDPAU_INCLUDEDIR})
-find_library(VDPAU_LIBRARY NAMES vdpau
-                           PATHS ${PC_VDPAU_LIBDIR})
+  find_path(VDPAU_INCLUDE_DIR NAMES vdpau/vdpau.h vdpau/vdpau_x11.h
+                              PATHS ${PC_VDPAU_INCLUDEDIR}
+                              NO_CACHE)
+  find_library(VDPAU_LIBRARY NAMES vdpau
+                             PATHS ${PC_VDPAU_LIBDIR}
+                             NO_CACHE)
 
-set(VDPAU_VERSION ${PC_VDPAU_VERSION})
+  set(VDPAU_VERSION ${PC_VDPAU_VERSION})
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(VDPAU
-                                  REQUIRED_VARS VDPAU_LIBRARY VDPAU_INCLUDE_DIR
-                                  VERSION_VAR VDPAU_VERSION)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(VDPAU
+                                    REQUIRED_VARS VDPAU_LIBRARY VDPAU_INCLUDE_DIR
+                                    VERSION_VAR VDPAU_VERSION)
 
-if(VDPAU_FOUND)
-  set(VDPAU_INCLUDE_DIRS ${VDPAU_INCLUDE_DIR})
-  set(VDPAU_LIBRARIES ${VDPAU_LIBRARY})
-  set(VDPAU_DEFINITIONS -DHAVE_LIBVDPAU=1)
-
-  if(NOT TARGET VDPAU::VDPAU)
+  if(VDPAU_FOUND)
     add_library(VDPAU::VDPAU UNKNOWN IMPORTED)
     set_target_properties(VDPAU::VDPAU PROPERTIES
                                        IMPORTED_LOCATION "${VDPAU_LIBRARY}"
                                        INTERFACE_INCLUDE_DIRECTORIES "${VDPAU_INCLUDE_DIR}"
                                        INTERFACE_COMPILE_DEFINITIONS HAVE_LIBVDPAU=1)
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP VDPAU::VDPAU)
   endif()
 endif()
-
-mark_as_advanced(VDPAU_INCLUDE_DIR VDPAU_LIBRARY)
