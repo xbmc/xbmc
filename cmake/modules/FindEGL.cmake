94d914f4976aaf370ee6a065e7e35ee5264f0b2c
fuzzard <fuzzard@kodi.tv>
2023-09-05 16:54:18 +1000

    [cmake] FindEGL migrate to TARGET usage

--- cmake/modules/FindEGL.cmake
+++ cmake/modules/FindEGL.cmake
@@ -3,52 +3,45 @@
 # -------
 # Finds the EGL library
 #
-# This will define the following variables::
-#
-# EGL_FOUND - system has EGL
-# EGL_INCLUDE_DIRS - the EGL include directory
-# EGL_LIBRARIES - the EGL libraries
-# EGL_DEFINITIONS - the EGL definitions
-# HAVE_EGLEXTANGLE - if eglext_angle.h exists else use eglextchromium.h
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   EGL::EGL   - The EGL library
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_EGL egl QUIET)
-endif()
-
-find_path(EGL_INCLUDE_DIR EGL/egl.h
-                          PATHS ${PC_EGL_INCLUDEDIR})
-
-find_library(EGL_LIBRARY NAMES EGL egl
-                         PATHS ${PC_EGL_LIBDIR})
-
-set(EGL_VERSION ${PC_EGL_VERSION})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EGL
-                                  REQUIRED_VARS EGL_LIBRARY EGL_INCLUDE_DIR
-                                  VERSION_VAR EGL_VERSION)
-
-if(EGL_FOUND)
-  set(EGL_LIBRARIES ${EGL_LIBRARY})
-  set(EGL_INCLUDE_DIRS ${EGL_INCLUDE_DIR})
-  set(EGL_DEFINITIONS -DHAS_EGL=1)
-  include(CheckIncludeFiles)
-  check_include_files("EGL/egl.h;EGL/eglext.h;EGL/eglext_angle.h" HAVE_EGLEXTANGLE)
-  if(HAVE_EGLEXTANGLE)
-    list(APPEND EGL_DEFINITIONS "-DHAVE_EGLEXTANGLE=1")
+if(NOT TARGET EGL::EGL)
+  find_package(PkgConfig)
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_EGL egl QUIET)
   endif()
 
-  if(NOT TARGET EGL::EGL)
+  find_path(EGL_INCLUDE_DIR EGL/egl.h
+                            PATHS ${PC_EGL_INCLUDEDIR}
+                            NO_CACHE)
+
+  find_library(EGL_LIBRARY NAMES EGL egl
+                           PATHS ${PC_EGL_LIBDIR}
+                           NO_CACHE)
+
+  set(EGL_VERSION ${PC_EGL_VERSION})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(EGL
+                                    REQUIRED_VARS EGL_LIBRARY EGL_INCLUDE_DIR
+                                    VERSION_VAR EGL_VERSION)
+
+  if(EGL_FOUND)
+    include(CheckIncludeFiles)
+    check_include_files("EGL/egl.h;EGL/eglext.h;EGL/eglext_angle.h" HAVE_EGLEXTANGLE)
+
     add_library(EGL::EGL UNKNOWN IMPORTED)
     set_target_properties(EGL::EGL PROPERTIES
                                    IMPORTED_LOCATION "${EGL_LIBRARY}"
                                    INTERFACE_INCLUDE_DIRECTORIES "${EGL_INCLUDE_DIR}"
                                    INTERFACE_COMPILE_DEFINITIONS HAS_EGL=1)
+
+    if(HAVE_EGLEXTANGLE)
+      set_property(TARGET EGL::EGL APPEND PROPERTY
+                                          INTERFACE_COMPILE_DEFINITIONS HAVE_EGLEXTANGLE=1)
+    endif()
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP EGL::EGL)
   endif()
 endif()
-
-mark_as_advanced(EGL_INCLUDE_DIR EGL_LIBRARY)
