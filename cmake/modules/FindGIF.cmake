3364f91280d3fe377fd716901e2ea072ec3e4dd5
fuzzard <fuzzard@kodi.tv>
2023-09-05 17:55:31 +1000

    [cmake] FindGIF migrate fully to TARGET usage

--- cmake/modules/FindGIF.cmake
+++ cmake/modules/FindGIF.cmake
@@ -3,38 +3,23 @@
 # -------
 # Finds the libgif library
 #
-# This will define the following variables::
-#
-# GIF_FOUND - system has libgif
-# GIF_INCLUDE_DIRS - the libgif include directory
-# GIF_LIBRARIES - the libgif libraries
-#
-# and the following imported targets::
+# This will define the following target:
 #
 #   GIF::GIF   - The libgif library
 
-find_path(GIF_INCLUDE_DIR gif_lib.h)
+if(NOT TARGET GIF::GIF)
+  find_library(GIF_LIBRARY NAMES gif NO_CACHE)
+  find_path(GIF_INCLUDE_DIR gif_lib.h NO_CACHE)
 
-include(FindPackageHandleStandardArgs)
-find_library(GIF_LIBRARY NAMES gif)
-find_package_handle_standard_args(GIF
-                                  REQUIRED_VARS GIF_LIBRARY GIF_INCLUDE_DIR)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(GIF
+                                    REQUIRED_VARS GIF_LIBRARY GIF_INCLUDE_DIR)
 
-if(GIF_FOUND)
-  set(GIF_LIBRARIES ${GIF_LIBRARY})
-  set(GIF_INCLUDE_DIRS ${GIF_INCLUDE_DIR})
-  set(GIF_DEFINITIONS -DHAVE_LIBGIF=1)
-
-  if(NOT TARGET GIF::GIF)
+  if(GIF_FOUND)
     add_library(GIF::GIF UNKNOWN IMPORTED)
-    if(GIF_LIBRARY)
-      set_target_properties(GIF::GIF PROPERTIES
-                                     IMPORTED_LOCATION "${GIF_LIBRARY}")
-    endif()
     set_target_properties(GIF::GIF PROPERTIES
+                                   IMPORTED_LOCATION "${GIF_LIBRARY}"
                                    INTERFACE_INCLUDE_DIRECTORIES "${GIF_INCLUDE_DIR}"
                                    INTERFACE_COMPILE_DEFINITIONS HAVE_LIBGIF=1)
   endif()
 endif()
-
-mark_as_advanced(GIF_INCLUDE_DIR GIF_LIBRARY)
