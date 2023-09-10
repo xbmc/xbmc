b377e5d06b277373ff5d3bb748d191f7368cc3e6
fuzzard <fuzzard@kodi.tv>
2023-09-10 13:29:08 +1000

    [cmake] FindWaylandProtocolsWebOS simplify find module
    Remove manual pkgconfig file from dependency build. It hardcodes things, and isnt
    needed for our purposes as all.

--- cmake/modules/FindWaylandProtocolsWebOS.cmake
+++ cmake/modules/FindWaylandProtocolsWebOS.cmake
@@ -4,24 +4,15 @@
 #
 # This will define the following variables::
 #
-# WAYLANDPROTOCOLSWEBOS_FOUND        -  systm has wayland-webos-client
 # WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR -  directory containing the additional webOS Wayland protocols
 #                                       from the webos-wayland-extensions package
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_WAYLAND_WEBOS_CLIENT wayland-webos-client>=1.0.0)
-endif()
 
-find_path(WAYLAND_PROTOCOLS_WEBOS_PROTOCOLDIR NAMES wayland-webos/webos-shell.xml
-                                              PATHS ${DEPENDS_PATH}/share)
+find_path(WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR NAMES webos-shell.xml
+                                             PATH_SUFFIXES wayland-webos
+                                             PATHS ${DEPENDS_PATH}/share
+                                             REQUIRED)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(WaylandProtocolsWebOS
-                                  REQUIRED_VARS WAYLAND_PROTOCOLS_WEBOS_PROTOCOLDIR
-                                  VERSION_VAR WAYLAND_WEBOS_SERVER_VERSION)
-
-if(WAYLANDPROTOCOLSWEBOS_FOUND)
-  # Promote to cache variables so all code can access it
-  set(WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR "${WAYLAND_PROTOCOLS_WEBOS_PROTOCOLDIR}/wayland-webos" CACHE INTERNAL "")
-endif()
+include(FindPackageMessage)
+find_package_message(WaylandProtocolsWebOS "Found WaylandProtocols-WebOS: ${WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR}" "[${WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR}]")
 
 mark_as_advanced(WAYLANDPROTOCOLSWEBOS_PROTOCOLSDIR)
