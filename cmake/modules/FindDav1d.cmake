e10ac1b9e41690aac608eca76205853915740684
fuzzard <fuzzard@kodi.tv>
2023-09-11 07:00:08 +1000

    [cmake] FindDav1d migrate to full TARGET usage

--- cmake/modules/FindDav1d.cmake
+++ cmake/modules/FindDav1d.cmake
@@ -3,60 +3,67 @@
 # --------
 # Finds the dav1d library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# DAV1D_FOUND - system has dav1d
-# DAV1D_INCLUDE_DIRS - the dav1d include directories
-# DAV1D_LIBRARIES - the dav1d libraries
+#   dav1d::dav1d   - The dav1d library
 
-if(ENABLE_INTERNAL_DAV1D)
-  include(cmake/scripts/common/ModuleHelpers.cmake)
+if(NOT TARGET dav1d::dav1d)
+  if(ENABLE_INTERNAL_DAV1D)
+    include(cmake/scripts/common/ModuleHelpers.cmake)
 
-  set(MODULE_LC dav1d)
+    set(MODULE_LC dav1d)
 
-  SETUP_BUILD_VARS()
+    SETUP_BUILD_VARS()
 
-  set(DAV1D_VERSION ${${MODULE}_VER})
+    set(DAV1D_VERSION ${${MODULE}_VER})
 
-  find_program(NINJA_EXECUTABLE ninja REQUIRED)
-  find_program(MESON_EXECUTABLE meson REQUIRED)
+    find_program(NINJA_EXECUTABLE ninja REQUIRED)
+    find_program(MESON_EXECUTABLE meson REQUIRED)
 
-  set(CONFIGURE_COMMAND ${MESON_EXECUTABLE}
-                        --buildtype=release
-                        --default-library=static
-                        --prefix=${DEPENDS_PATH}
-                        --libdir=lib
-                        -Denable_asm=true
-                        -Denable_tools=false
-                        -Denable_examples=false
-                        -Denable_tests=false
-                        ../dav1d)
-  set(BUILD_COMMAND ${NINJA_EXECUTABLE})
-  set(INSTALL_COMMAND ${NINJA_EXECUTABLE} install)
+    set(CONFIGURE_COMMAND ${MESON_EXECUTABLE}
+                          --buildtype=release
+                          --default-library=static
+                          --prefix=${DEPENDS_PATH}
+                          --libdir=lib
+                          -Denable_asm=true
+                          -Denable_tools=false
+                          -Denable_examples=false
+                          -Denable_tests=false
+                          ../dav1d)
+    set(BUILD_COMMAND ${NINJA_EXECUTABLE})
+    set(INSTALL_COMMAND ${NINJA_EXECUTABLE} install)
 
-  BUILD_DEP_TARGET()
-else()
-  if(PKG_CONFIG_FOUND)
-    pkg_check_modules(PC_DAV1D dav1d QUIET)
+    BUILD_DEP_TARGET()
+  else()
+    if(PKG_CONFIG_FOUND)
+      pkg_check_modules(PC_DAV1D dav1d QUIET)
+    endif()
+
+    find_library(DAV1D_LIBRARY NAMES dav1d libdav1d
+                               PATHS ${PC_DAV1D_LIBDIR}
+                               NO_CACHE)
+
+    find_path(DAV1D_INCLUDE_DIR NAMES dav1d/dav1d.h
+                                PATHS ${PC_DAV1D_INCLUDEDIR}
+                                NO_CACHE)
+
+    set(DAV1D_VERSION ${PC_DAV1D_VERSION})
   endif()
 
-  find_library(DAV1D_LIBRARY NAMES dav1d libdav1d
-                             PATHS ${PC_DAV1D_LIBDIR})
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Dav1d
+                                    REQUIRED_VARS DAV1D_LIBRARY DAV1D_INCLUDE_DIR
+                                    VERSION_VAR DAV1D_VERSION)
 
-  find_path(DAV1D_INCLUDE_DIR NAMES dav1d/dav1d.h
-                              PATHS ${PC_DAV1D_INCLUDEDIR})
+  if(DAV1D_FOUND)
+    add_library(dav1d::dav1d UNKNOWN IMPORTED)
+    set_target_properties(dav1d::dav1d PROPERTIES
+                                             IMPORTED_LOCATION "${DAV1D_LIBRARY}"
+                                             INTERFACE_INCLUDE_DIRECTORIES "${DAV1D_INCLUDE_DIR}")
+    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP dav1d::dav1d)
 
-  set(DAV1D_VERSION ${PC_DAV1D_VERSION})
+    if(TARGET dav1d)
+      add_dependencies(dav1d::dav1d dav1d)
+    endif()
+  endif()
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Dav1d
-                                  REQUIRED_VARS DAV1D_LIBRARY DAV1D_INCLUDE_DIR
-                                  VERSION_VAR DAV1D_VERSION)
-
-if(DAV1D_FOUND)
-  set(DAV1D_INCLUDE_DIRS ${DAV1D_INCLUDE_DIR})
-  set(DAV1D_LIBRARIES ${DAV1D_LIBRARY})
-endif()
-
-mark_as_advanced(DAV1D_INCLUDE_DIR DAV1D_LIBRARY)
