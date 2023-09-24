d61ad115d724e11fb3df0b1007e2a4e4a9a03259
fuzzard <fuzzard@kodi.tv>
2023-09-24 13:08:14 +1000

    [cmake] FindTagLib add to multigen build_internal_depends target

--- cmake/modules/FindTagLib.cmake
+++ cmake/modules/FindTagLib.cmake
@@ -112,19 +112,23 @@ if(NOT TARGET TagLib::TagLib)
 
     if(TARGET taglib)
       add_dependencies(TagLib::TagLib taglib)
-    else()
-      # Add internal build target when a Multi Config Generator is used
-      # We cant add a dependency based off a generator expression for targeted build types,
-      # https://gitlab.kitware.com/cmake/cmake/-/issues/19467
-      # therefore if the find heuristics only find the library, we add the internal build 
-      # target to the project to allow user to manually trigger for any build type they need
-      # in case only a specific build type is actually available (eg Release found, Debug Required)
-      # This is mainly targeted for windows who required different runtime libs for different
-      # types, and they arent compatible
-      if(_multiconfig_generator)
+    endif()
+
+    # Add internal build target when a Multi Config Generator is used
+    # We cant add a dependency based off a generator expression for targeted build types,
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/19467
+    # therefore if the find heuristics only find the library, we add the internal build
+    # target to the project to allow user to manually trigger for any build type they need
+    # in case only a specific build type is actually available (eg Release found, Debug Required)
+    # This is mainly targeted for windows who required different runtime libs for different
+    # types, and they arent compatible
+    if(_multiconfig_generator)
+      if(NOT TARGET taglib)
         buildTagLib()
       endif()
+      add_dependencies(build_internal_depends taglib)
     endif()
+
 
     set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP TagLib::TagLib)
   else()
