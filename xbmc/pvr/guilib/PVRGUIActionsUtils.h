b220e19c5b8d69e21886cd9c3d1ce4ddccef2740
ksooo <3226626+ksooo@users.noreply.github.com>
2023-10-14 15:21:01 +0200

    [fileitem][PVR] CFileItem::LoadDetails: Factor out loading of PVR items.

--- xbmc/pvr/guilib/PVRGUIActionsUtils.h
+++ xbmc/pvr/guilib/PVRGUIActionsUtils.h
@@ -10,6 +10,8 @@
 
 #include "pvr/IPVRComponent.h"
 
+#include <memory>
+
 class CFileItem;
 
 namespace PVR
@@ -33,6 +35,13 @@ public:
      * @return True on success, false otherwise.
      */
   bool OnInfo(const CFileItem& item);
+
+  /*!
+   * @brief Load item details (create recording info tag etc.).
+   * @param item The item.
+   * @return Loaded item on success, nullptr otherwise.
+   */
+  std::shared_ptr<CFileItem> LoadItem(const CFileItem& item);
 
 private:
   CPVRGUIActionsUtils(const CPVRGUIActionsUtils&) = delete;
