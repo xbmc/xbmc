5b48bc1ef7f74423c0a16803ba2fa05105fedfa0
ksooo <3226626+ksooo@users.noreply.github.com>
2023-10-30 18:09:32 +0100

    [PVR] Recording info dialog's play button now respects default play action setting.

---
+++ xbmc/pvr/guilib/PVRGUIRecordingsPlayActionProcessor.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2023 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "FileItem.h"
+#include "ServiceBroker.h"
+#include "pvr/PVRManager.h"
+#include "pvr/guilib/PVRGUIActionsPlayback.h"
+#include "video/guilib/VideoPlayActionProcessor.h"
+
+namespace PVR
+{
+class CGUIPVRRecordingsPlayActionProcessor : public VIDEO::GUILIB::CVideoPlayActionProcessorBase
+{
+public:
+  explicit CGUIPVRRecordingsPlayActionProcessor(CFileItem& item)
+    : CVideoPlayActionProcessorBase(item)
+  {
+  }
+
+protected:
+  bool OnResumeSelected() override
+  {
+    m_item.SetStartOffset(STARTOFFSET_RESUME);
+    Play();
+    return true;
+  }
+
+  bool OnPlaySelected() override
+  {
+    Play();
+    return true;
+  }
+
+private:
+  void Play()
+  {
+    CServiceBroker::GetPVRManager().Get<PVR::GUI::Playback>().PlayRecording(
+        m_item, false /* no resume check */);
+  }
+};
+} // namespace PVR
