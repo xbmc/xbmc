25902229253318ece22e48d52cdd089984317282
thexai <58434170+thexai@users.noreply.github.com>
2023-11-04 12:01:37 +0100

    DVDInputStreamBluray: Fix missing file flags on creation

--- xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -1242,7 +1242,8 @@ void CDVDInputStreamBluray::SetupPlayerSettings()
 
 bool CDVDInputStreamBluray::OpenStream(CFileItem &item)
 {
-  m_pstream = std::make_unique<CDVDInputStreamFile>(item, 0);
+  m_pstream = std::make_unique<CDVDInputStreamFile>(item, READ_TRUNCATED | READ_BITRATE |
+                                                              READ_CHUNKED | READ_NO_CACHE);
 
   if (!m_pstream->Open())
   {
