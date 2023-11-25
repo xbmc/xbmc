d65fd0ab34796e0154c92f6b26ca58882487baab
Miguel Borges de Freitas <92enen@gmail.com>
2023-11-25 17:56:50 +0000

    [GUIDialogPictureInfo] Use array instead of map

--- xbmc/pictures/GUIDialogPictureInfo.cpp
+++ xbmc/pictures/GUIDialogPictureInfo.cpp
@@ -16,70 +16,70 @@
 #include "guilib/LocalizeStrings.h"
 #include "guilib/guiinfo/GUIInfoLabels.h"
 #include "input/Key.h"
-#include "utils/Map.h"
+
+#include <array>
 
 namespace
 {
 constexpr unsigned int CONTROL_PICTURE_INFO = 5;
 
-constexpr auto slideShowInfoTranslationMap =
-    make_map<uint32_t, uint32_t>({{SLIDESHOW_FILE_NAME, 21800},
-                                  {SLIDESHOW_FILE_PATH, 21801},
-                                  {SLIDESHOW_FILE_SIZE, 21802},
-                                  {SLIDESHOW_FILE_DATE, 21803},
-                                  {SLIDESHOW_INDEX, 21804},
-                                  {SLIDESHOW_RESOLUTION, 21805},
-                                  {SLIDESHOW_COMMENT, 21806},
-                                  {SLIDESHOW_EXIF_DATE_TIME, 21820},
-                                  {SLIDESHOW_EXIF_DESCRIPTION, 21821},
-                                  {SLIDESHOW_EXIF_CAMERA_MAKE, 21822},
-                                  {SLIDESHOW_EXIF_CAMERA_MODEL, 21823},
-                                  {SLIDESHOW_EXIF_COLORSPACE, 21825},
-                                  {SLIDESHOW_EXIF_COMMENT, 21824},
-                                  {SLIDESHOW_EXIF_SOFTWARE, 13419},
-                                  {SLIDESHOW_EXIF_APERTURE, 21826},
-                                  {SLIDESHOW_EXIF_FOCAL_LENGTH, 21827},
-                                  {SLIDESHOW_EXIF_FOCUS_DIST, 21828},
-                                  {SLIDESHOW_EXIF_EXPOSURE, 21829},
-                                  {SLIDESHOW_EXIF_EXPOSURE_TIME, 21830},
-                                  {SLIDESHOW_EXIF_EXPOSURE_BIAS, 21831},
-                                  {SLIDESHOW_EXIF_EXPOSURE_MODE, 21832},
-                                  {SLIDESHOW_EXIF_FLASH_USED, 21833},
-                                  {SLIDESHOW_EXIF_WHITE_BALANCE, 21834},
-                                  {SLIDESHOW_EXIF_LIGHT_SOURCE, 21835},
-                                  {SLIDESHOW_EXIF_METERING_MODE, 21836},
-                                  {SLIDESHOW_EXIF_ISO_EQUIV, 21837},
-                                  {SLIDESHOW_EXIF_DIGITAL_ZOOM, 21838},
-                                  {SLIDESHOW_EXIF_CCD_WIDTH, 21839},
-                                  {SLIDESHOW_EXIF_GPS_LATITUDE, 21840},
-                                  {SLIDESHOW_EXIF_GPS_LONGITUDE, 21841},
-                                  {SLIDESHOW_EXIF_GPS_ALTITUDE, 21842},
-                                  {SLIDESHOW_EXIF_ORIENTATION, 21843},
-                                  {SLIDESHOW_EXIF_XPCOMMENT, 21844},
-                                  {SLIDESHOW_IPTC_SUBLOCATION, 21857},
-                                  {SLIDESHOW_IPTC_IMAGETYPE, 21858},
-                                  {SLIDESHOW_IPTC_TIMECREATED, 21859},
-                                  {SLIDESHOW_IPTC_SUP_CATEGORIES, 21860},
-                                  {SLIDESHOW_IPTC_KEYWORDS, 21861},
-                                  {SLIDESHOW_IPTC_CAPTION, 21862},
-                                  {SLIDESHOW_IPTC_AUTHOR, 21863},
-                                  {SLIDESHOW_IPTC_HEADLINE, 21864},
-                                  {SLIDESHOW_IPTC_SPEC_INSTR, 21865},
-                                  {SLIDESHOW_IPTC_CATEGORY, 21866},
-                                  {SLIDESHOW_IPTC_BYLINE, 21867},
-                                  {SLIDESHOW_IPTC_BYLINE_TITLE, 21868},
-                                  {SLIDESHOW_IPTC_CREDIT, 21869},
-                                  {SLIDESHOW_IPTC_SOURCE, 21870},
-                                  {SLIDESHOW_IPTC_COPYRIGHT_NOTICE, 21871},
-                                  {SLIDESHOW_IPTC_OBJECT_NAME, 21872},
-                                  {SLIDESHOW_IPTC_CITY, 21873},
-                                  {SLIDESHOW_IPTC_STATE, 21874},
-                                  {SLIDESHOW_IPTC_COUNTRY, 21875},
-                                  {SLIDESHOW_IPTC_TX_REFERENCE, 21876},
-                                  {SLIDESHOW_IPTC_DATE, 21877},
-                                  {SLIDESHOW_IPTC_URGENCY, 21878},
-                                  {SLIDESHOW_IPTC_COUNTRY_CODE, 21879},
-                                  {SLIDESHOW_IPTC_REF_SERVICE, 21880}});
+constexpr std::array slideShowInfoTranslationList{std::pair{SLIDESHOW_FILE_NAME, 21800},
+                                                  std::pair{SLIDESHOW_FILE_PATH, 21801},
+                                                  std::pair{SLIDESHOW_FILE_SIZE, 21802},
+                                                  std::pair{SLIDESHOW_FILE_DATE, 21803},
+                                                  std::pair{SLIDESHOW_INDEX, 21804},
+                                                  std::pair{SLIDESHOW_RESOLUTION, 21805},
+                                                  std::pair{SLIDESHOW_COMMENT, 21806},
+                                                  std::pair{SLIDESHOW_EXIF_DATE_TIME, 21820},
+                                                  std::pair{SLIDESHOW_EXIF_DESCRIPTION, 21821},
+                                                  std::pair{SLIDESHOW_EXIF_CAMERA_MAKE, 21822},
+                                                  std::pair{SLIDESHOW_EXIF_CAMERA_MODEL, 21823},
+                                                  std::pair{SLIDESHOW_EXIF_COLORSPACE, 21825},
+                                                  std::pair{SLIDESHOW_EXIF_COMMENT, 21824},
+                                                  std::pair{SLIDESHOW_EXIF_SOFTWARE, 13419},
+                                                  std::pair{SLIDESHOW_EXIF_APERTURE, 21826},
+                                                  std::pair{SLIDESHOW_EXIF_FOCAL_LENGTH, 21827},
+                                                  std::pair{SLIDESHOW_EXIF_FOCUS_DIST, 21828},
+                                                  std::pair{SLIDESHOW_EXIF_EXPOSURE, 21829},
+                                                  std::pair{SLIDESHOW_EXIF_EXPOSURE_TIME, 21830},
+                                                  std::pair{SLIDESHOW_EXIF_EXPOSURE_BIAS, 21831},
+                                                  std::pair{SLIDESHOW_EXIF_EXPOSURE_MODE, 21832},
+                                                  std::pair{SLIDESHOW_EXIF_FLASH_USED, 21833},
+                                                  std::pair{SLIDESHOW_EXIF_WHITE_BALANCE, 21834},
+                                                  std::pair{SLIDESHOW_EXIF_LIGHT_SOURCE, 21835},
+                                                  std::pair{SLIDESHOW_EXIF_METERING_MODE, 21836},
+                                                  std::pair{SLIDESHOW_EXIF_ISO_EQUIV, 21837},
+                                                  std::pair{SLIDESHOW_EXIF_DIGITAL_ZOOM, 21838},
+                                                  std::pair{SLIDESHOW_EXIF_CCD_WIDTH, 21839},
+                                                  std::pair{SLIDESHOW_EXIF_GPS_LATITUDE, 21840},
+                                                  std::pair{SLIDESHOW_EXIF_GPS_LONGITUDE, 21841},
+                                                  std::pair{SLIDESHOW_EXIF_GPS_ALTITUDE, 21842},
+                                                  std::pair{SLIDESHOW_EXIF_ORIENTATION, 21843},
+                                                  std::pair{SLIDESHOW_EXIF_XPCOMMENT, 21844},
+                                                  std::pair{SLIDESHOW_IPTC_SUBLOCATION, 21857},
+                                                  std::pair{SLIDESHOW_IPTC_IMAGETYPE, 21858},
+                                                  std::pair{SLIDESHOW_IPTC_TIMECREATED, 21859},
+                                                  std::pair{SLIDESHOW_IPTC_SUP_CATEGORIES, 21860},
+                                                  std::pair{SLIDESHOW_IPTC_KEYWORDS, 21861},
+                                                  std::pair{SLIDESHOW_IPTC_CAPTION, 21862},
+                                                  std::pair{SLIDESHOW_IPTC_AUTHOR, 21863},
+                                                  std::pair{SLIDESHOW_IPTC_HEADLINE, 21864},
+                                                  std::pair{SLIDESHOW_IPTC_SPEC_INSTR, 21865},
+                                                  std::pair{SLIDESHOW_IPTC_CATEGORY, 21866},
+                                                  std::pair{SLIDESHOW_IPTC_BYLINE, 21867},
+                                                  std::pair{SLIDESHOW_IPTC_BYLINE_TITLE, 21868},
+                                                  std::pair{SLIDESHOW_IPTC_CREDIT, 21869},
+                                                  std::pair{SLIDESHOW_IPTC_SOURCE, 21870},
+                                                  std::pair{SLIDESHOW_IPTC_COPYRIGHT_NOTICE, 21871},
+                                                  std::pair{SLIDESHOW_IPTC_OBJECT_NAME, 21872},
+                                                  std::pair{SLIDESHOW_IPTC_CITY, 21873},
+                                                  std::pair{SLIDESHOW_IPTC_STATE, 21874},
+                                                  std::pair{SLIDESHOW_IPTC_COUNTRY, 21875},
+                                                  std::pair{SLIDESHOW_IPTC_TX_REFERENCE, 21876},
+                                                  std::pair{SLIDESHOW_IPTC_DATE, 21877},
+                                                  std::pair{SLIDESHOW_IPTC_URGENCY, 21878},
+                                                  std::pair{SLIDESHOW_IPTC_COUNTRY_CODE, 21879},
+                                                  std::pair{SLIDESHOW_IPTC_REF_SERVICE, 21880}};
 } // namespace
 
 CGUIDialogPictureInfo::CGUIDialogPictureInfo(void)
@@ -140,14 +140,13 @@ void CGUIDialogPictureInfo::UpdatePictureInfo()
   CGUIMessage msgReset(GUI_MSG_LABEL_RESET, GetID(), CONTROL_PICTURE_INFO);
   OnMessage(msgReset);
   m_pictureInfo->Clear();
-  for (auto it = slideShowInfoTranslationMap.cbegin(); it != slideShowInfoTranslationMap.cend();
-       ++it)
+  for (const auto& [info, code] : slideShowInfoTranslationList)
   {
     const std::string picInfo =
-        CServiceBroker::GetGUI()->GetInfoManager().GetLabel(it->first, INFO::DEFAULT_CONTEXT);
+        CServiceBroker::GetGUI()->GetInfoManager().GetLabel(info, INFO::DEFAULT_CONTEXT);
     if (!picInfo.empty())
     {
-      auto item{std::make_shared<CFileItem>(g_localizeStrings.Get(it->second))};
+      auto item{std::make_shared<CFileItem>(g_localizeStrings.Get(code))};
       item->SetLabel2(picInfo);
       m_pictureInfo->Add(item);
     }
