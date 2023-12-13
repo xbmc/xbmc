xbmc/xbmc

commit fe6a7b1ed0de937b0b39c7c756dec0ab8c04f6a6

[utils] RssReader/Manager migrate to tinyxml2

xbmc/utils/RssReader.cpp
@@ -129,7 +129,6 @@ void CRssReader::Process()
 
     int nRetries = 3;
     CURL url(strUrl);
-    std::string fileCharset;
 
     // we wait for the network to come up
     if ((url.IsProtocol("http") || url.IsProtocol("https")) &&
@@ -164,7 +163,6 @@ void CRssReader::Process()
         {
           if (http.Get(strUrl, strXML))
           {
-            fileCharset = http.GetProperty(XFILE::FILE_PROPERTY_CONTENT_CHARSET);
             CLog::Log(LOGDEBUG, "Got rss feed: {}", strUrl);
             break;
           }
@@ -192,7 +190,7 @@ void CRssReader::Process()
         iStart = strXML.find("<content:encoded>");
       }
 
-      if (Parse(strXML, iFeed, fileCharset))
+      if (Parse(strXML, iFeed))
         CLog::Log(LOGDEBUG, "Parsed rss feed: {}", strUrl);
     }
   }
@@ -241,11 +239,11 @@ void CRssReader::AddString(std::wstring aString, int aColour, int iFeed)
     m_strColors[iFeed] += aString;
 }
 
-void CRssReader::GetNewsItems(TiXmlElement* channelXmlNode, int iFeed)
+void CRssReader::GetNewsItems(tinyxml2::XMLNode* channelXmlNode, int iFeed)
 {
   HTML::CHTMLUtil html;
 
-  TiXmlElement * itemNode = channelXmlNode->FirstChildElement("item");
+  auto* itemNode = channelXmlNode->FirstChildElement("item");
   std::map<std::string, std::wstring> mTagElements;
   typedef std::pair<std::string, std::wstring> StrPair;
   std::list<std::string>::iterator i;
@@ -256,19 +254,19 @@ void CRssReader::GetNewsItems(TiXmlElement* channelXmlNode, int iFeed)
   if (m_tagSet.empty())
     AddTag("title");
 
-  while (itemNode != nullptr)
+  while (itemNode)
   {
-    TiXmlNode* childNode = itemNode->FirstChild();
+    auto* childNode = itemNode->FirstChild();
     mTagElements.clear();
-    while (childNode != nullptr)
+    while (childNode)
     {
-      std::string strName = childNode->ValueStr();
+      std::string strName = childNode->Value();
 
       for (i = m_tagSet.begin(); i != m_tagSet.end(); ++i)
       {
         if (!childNode->NoChildren() && *i == strName)
         {
-          std::string htmlText = childNode->FirstChild()->ValueStr();
+          std::string htmlText = childNode->FirstChild()->Value();
 
           // This usually happens in right-to-left languages where they want to
           // specify in the RSS body that the text should be RTL.
@@ -276,7 +274,7 @@ void CRssReader::GetNewsItems(TiXmlElement* channelXmlNode, int iFeed)
           //  <div dir="RTL">��� ����: ���� �� �����</div>
           // </title>
           if (htmlText == "div" || htmlText == "span")
-            htmlText = childNode->FirstChild()->FirstChild()->ValueStr();
+            htmlText = childNode->FirstChild()->FirstChild()->Value();
 
           std::wstring unicodeText, unicodeText2;
 
@@ -307,39 +305,32 @@ void CRssReader::GetNewsItems(TiXmlElement* channelXmlNode, int iFeed)
   }
 }
 
-bool CRssReader::Parse(const std::string& data, int iFeed, const std::string& charset)
+bool CRssReader::Parse(const std::string& data, int iFeed)
 {
   m_xml.Clear();
-  m_xml.Parse(data, charset);
-
-  CLog::Log(LOGDEBUG, "RSS feed encoding: {}", m_xml.GetUsedCharset());
+  m_xml.Parse(data);
 
   return Parse(iFeed);
 }
 
 bool CRssReader::Parse(int iFeed)
 {
-  TiXmlElement* rootXmlNode = m_xml.RootElement();
+  auto* rootXmlNode = m_xml.RootElement();
 
   if (!rootXmlNode)
     return false;
 
-  TiXmlElement* rssXmlNode = NULL;
-
-  std::string strValue = rootXmlNode->ValueStr();
-  if (strValue.find("rss") != std::string::npos ||
-      strValue.find("rdf") != std::string::npos)
-    rssXmlNode = rootXmlNode;
-  else
+  std::string strValue = rootXmlNode->Value();
+  if (strValue.find("rss") == std::string::npos || strValue.find("rdf") == std::string::npos)
   {
     // Unable to find root <rss> or <rdf> node
     return false;
   }
 
-  TiXmlElement* channelXmlNode = rssXmlNode->FirstChildElement("channel");
+  auto* channelXmlNode = rootXmlNode->FirstChildElement("channel");
   if (channelXmlNode)
   {
-    TiXmlElement* titleNode = channelXmlNode->FirstChildElement("title");
+    auto* titleNode = channelXmlNode->FirstChildElement("title");
     if (titleNode && !titleNode->NoChildren())
     {
       std::string strChannel = titleNode->FirstChild()->Value();
@@ -354,7 +345,7 @@ bool CRssReader::Parse(int iFeed)
     GetNewsItems(channelXmlNode,iFeed);
   }
 
-  GetNewsItems(rssXmlNode,iFeed);
+  GetNewsItems(rootXmlNode, iFeed);
 
   // avoid trailing ' - '
   if (m_strFeed[iFeed].size() > 3 && m_strFeed[iFeed].substr(m_strFeed[iFeed].size() - 3) == L" - ")

xbmc/utils/RssManager.cpp
@@ -20,6 +20,7 @@
 #include "utils/FileUtils.h"
 #include "utils/RssReader.h"
 #include "utils/StringUtils.h"
+#include "utils/XBMCTinyXML2.h"
 #include "utils/log.h"
 
 #include <mutex>
@@ -101,58 +102,57 @@ bool CRssManager::Load()
   if (!CFileUtils::Exists(rssXML))
     return false;
 
-  CXBMCTinyXML rssDoc;
+  CXBMCTinyXML2 rssDoc;
   if (!rssDoc.LoadFile(rssXML))
   {
-    CLog::Log(LOGERROR, "CRssManager: error loading {}, Line {}\n{}", rssXML, rssDoc.ErrorRow(),
-              rssDoc.ErrorDesc());
+    CLog::Log(LOGERROR, "CRssManager: error loading {}, Line {}\n{}", rssXML, rssDoc.ErrorLineNum(),
+              rssDoc.ErrorStr());
     return false;
   }
 
-  const TiXmlElement *pRootElement = rssDoc.RootElement();
-  if (pRootElement == NULL || !StringUtils::EqualsNoCase(pRootElement->ValueStr(), "rssfeeds"))
+  auto* rootElement = rssDoc.RootElement();
+  if (!rootElement || !StringUtils::EqualsNoCase(rootElement->Value(), "rssfeeds"))
   {
     CLog::Log(LOGERROR, "CRssManager: error loading {}, no <rssfeeds> node", rssXML);
     return false;
   }
 
   m_mapRssUrls.clear();
-  const TiXmlElement* pSet = pRootElement->FirstChildElement("set");
-  while (pSet != NULL)
+  auto* setElement = rootElement->FirstChildElement("set");
+  while (setElement)
   {
     int iId;
-    if (pSet->QueryIntAttribute("id", &iId) == TIXML_SUCCESS)
+    if (setElement->QueryIntAttribute("id", &iId) == tinyxml2::XML_SUCCESS)
     {
       RssSet set;
-      set.rtl = pSet->Attribute("rtl") != NULL &&
-                StringUtils::CompareNoCase(pSet->Attribute("rtl"), "true") == 0;
-      const TiXmlElement* pFeed = pSet->FirstChildElement("feed");
-      while (pFeed != NULL)
+      set.rtl = setElement->Attribute("rtl") != nullptr &&
+                StringUtils::CompareNoCase(setElement->Attribute("rtl"), "true") == 0;
+      auto* feedElement = setElement->FirstChildElement("feed");
+      while (feedElement)
       {
-        int iInterval;
-        if (pFeed->QueryIntAttribute("updateinterval", &iInterval) != TIXML_SUCCESS)
+        int iInterval = 30; // default to 30 min
+        if (feedElement->QueryIntAttribute("updateinterval", &iInterval) != tinyxml2::XML_SUCCESS)
         {
-          iInterval = 30; // default to 30 min
           CLog::Log(LOGDEBUG, "CRssManager: no interval set, default to 30!");
         }
 
-        if (pFeed->FirstChild() != NULL)
+        if (feedElement->FirstChild())
         {
-          //! @todo UTF-8: Do these URLs need to be converted to UTF-8?
-          //!              What about the xml encoding?
-          std::string strUrl = pFeed->FirstChild()->ValueStr();
+          std::string strUrl = feedElement->FirstChild()->Value();
           set.url.push_back(strUrl);
           set.interval.push_back(iInterval);
         }
-        pFeed = pFeed->NextSiblingElement("feed");
+        feedElement = feedElement->NextSiblingElement("feed");
       }
 
       m_mapRssUrls.insert(std::make_pair(iId,set));
     }
     else
+    {
       CLog::Log(LOGERROR, "CRssManager: found rss url set with no id in RssFeeds.xml, ignored");
+    }
 
-    pSet = pSet->NextSiblingElement("set");
+    setElement = setElement->NextSiblingElement("set");
   }
 
   return true;

xbmc/utils/RssReader.h
@@ -11,7 +11,7 @@
 #include "threads/CriticalSection.h"
 #include "threads/Thread.h"
 #include "utils/IRssObserver.h"
-#include "utils/XBMCTinyXML.h"
+#include "utils/XBMCTinyXML2.h"
 
 #include <list>
 #include <string>
@@ -29,7 +29,7 @@ public:
   ~CRssReader() override;
 
   void Create(IRssObserver* aObserver, const std::vector<std::string>& aUrl, const std::vector<int>& times, int spacesBetweenFeeds, bool rtl);
-  bool Parse(const std::string& data, int iFeed, const std::string& charset);
+  bool Parse(const std::string& data, int iFeed);
   void getFeed(vecText &text);
   void AddTag(const std::string &addTag);
   void AddToQueue(int iAdd);
@@ -42,7 +42,7 @@ public:
 private:
   void Process() override;
   bool Parse(int iFeed);
-  void GetNewsItems(TiXmlElement* channelXmlNode, int iFeed);
+  void GetNewsItems(tinyxml2::XMLNode* channelXmlNode, int iFeed);
   void AddString(std::wstring aString, int aColour, int iFeed);
   void UpdateFeed();
   void OnExit() override;
@@ -55,7 +55,7 @@ private:
   std::vector<KODI::TIME::SystemTime*> m_vecTimeStamps;
   std::vector<int> m_vecUpdateTimes;
   int m_spacesBetweenFeeds;
-  CXBMCTinyXML m_xml;
+  CXBMCTinyXML2 m_xml;
   std::list<std::string> m_tagSet;
   std::vector<std::string> m_vecUrls;
   std::vector<int> m_vecQueue;

commit 7e2b1b29d451c2547242b81511efe2e08628323c

RssReader: Fix logic error introduced in fe6a7b1ed0
Fixes #24229.

xbmc/utils/RssReader.cpp
@@ -321,7 +321,7 @@ bool CRssReader::Parse(int iFeed)
     return false;
 
   std::string strValue = rootXmlNode->Value();
-  if (strValue.find("rss") == std::string::npos || strValue.find("rdf") == std::string::npos)
+  if (strValue.find("rss") == std::string::npos && strValue.find("rdf") == std::string::npos)
   {
     // Unable to find root <rss> or <rdf> node
     return false;