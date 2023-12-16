7113acfa3a4f9ef6639a3435e7d50a183749f14f
Pedro Câmara <camara-pedro@hotmail.com>
2023-12-16 23:10:47 +0000

    Fix edge case when downloading subtitles for http(s) streams

--- xbmc/video/dialogs/GUIDialogSubtitles.cpp
+++ xbmc/video/dialogs/GUIDialogSubtitles.cpp
@@ -548,7 +548,7 @@ void CGUIDialogSubtitles::OnDownloadComplete(const CFileItemList *items, const s
   std::vector<std::string> vecFiles;
 
   std::string strCurrentFilePath;
-  if (StringUtils::StartsWith(strCurrentFilePath, "http://"))
+  if (StringUtils::StartsWith(strCurrentFile, "http://") || StringUtils::StartsWith(strCurrentFile, "https://"))
   {
     strCurrentFile = "TempSubtitle";
     vecFiles.push_back(strCurrentFile);
