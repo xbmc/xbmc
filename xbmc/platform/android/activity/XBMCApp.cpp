4768e26fabb555456407b9f546825224a5f93490
hugbug <hugbug@users.noreply.github.com>
2023-12-31 13:44:08 +0100

    [Android] Avoid unnecessary resets of audio engine
    Do reset only on wake-up after sleep. Do not reset on resolution or refresh rate changes on begin of playback.

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -1333,10 +1333,11 @@ void CXBMCApp::onReceive(CJNIIntent intent)
       if (winSystem && dynamic_cast<CWinSystemAndroid*>(winSystem))
         dynamic_cast<CWinSystemAndroid*>(winSystem)->SetHdmiState(hdmiPlugged);
     }
-    if (hdmiPlugged)
+    if (hdmiPlugged && m_wakeUp)
     {
       CLog::Log(LOGDEBUG, "CXBMCApp::{}: Reset audio engine", __FUNCTION__);
       CServiceBroker::GetActiveAE()->DeviceChange();
+      m_wakeUp = false;
     }
   }
   else if (action == CJNIIntent::ACTION_SCREEN_ON)
@@ -1359,6 +1360,8 @@ void CXBMCApp::onReceive(CJNIIntent intent)
       const auto appPower = components.GetComponent<CApplicationPowerHandling>();
       appPower->WakeUpScreenSaverAndDPMS();
     }
+
+    m_wakeUp = true;
   }
   else if (action == CJNIIntent::ACTION_SCREEN_OFF)
   {
