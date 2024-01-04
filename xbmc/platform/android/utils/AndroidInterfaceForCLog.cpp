c679401d4a428f904c733ac5faaafbc5627d9545
hugbug <hugbug@users.noreply.github.com>
2024-01-04 17:37:46 +0100

    [Android] Enable logging of debug messages...
    ... for our app even if it is disabled system-wide.

--- xbmc/platform/android/utils/AndroidInterfaceForCLog.cpp
+++ xbmc/platform/android/utils/AndroidInterfaceForCLog.cpp
@@ -10,11 +10,32 @@
 
 #include "CompileInfo.h"
 
+#include <dlfcn.h>
 #include <spdlog/sinks/android_sink.h>
 #include <spdlog/sinks/dist_sink.h>
 
+// On some Android platforms debug logging is deactivated.
+// We try to activate debug logging for our app via function "__android_log_set_minimum_priority" from "/system/lib/liblog.so".
+// The function is defined in API level 30 (Android 11) as:
+//   int32_t __android_log_set_minimum_priority(int32_t priority);
+void ActivateAndroidDebugLogging()
+{
+  void* libHandle = dlopen("liblog.so", RTLD_LAZY);
+  if (libHandle)
+  {
+    void* funcPtr = dlsym(libHandle, "__android_log_set_minimum_priority");
+    if (funcPtr)
+    {
+      typedef int32_t (*android_log_set_minimum_priority_func)(int32_t);
+      reinterpret_cast<android_log_set_minimum_priority_func>(funcPtr)(ANDROID_LOG_DEBUG);
+    }
+    dlclose(libHandle);
+  }
+}
+
 std::unique_ptr<IPlatformLog> IPlatformLog::CreatePlatformLog()
 {
+  ActivateAndroidDebugLogging();
   return std::make_unique<CAndroidInterfaceForCLog>();
 }
 
