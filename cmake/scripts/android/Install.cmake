fdef39a321bdce2cb924882edaad4c52831a5b28
fuzzard <fuzzard@kodi.tv>
2024-01-13 13:20:51 +1000

    [Android] Remove special versioning for Android arch types
    With AAB bundles being the new requirement for Play store submissions, the bundle for
    multiple architectures no longer requires a unique version for each architecture.
    Remove this version increment for aarch64 from the build process.

--- cmake/scripts/android/Install.cmake
+++ cmake/scripts/android/Install.cmake
@@ -28,9 +28,6 @@ unset(APP_VERSION_CODE_LIST)
 math(EXPR APP_VERSION_CODE_ANDROID "(${major} * 100 + ${minor}) * 1000 + ${patch}")
 unset(major)
 unset(minor)
-if(ARCH STREQUAL aarch64 AND patch LESS 999)
-  math(EXPR APP_VERSION_CODE_ANDROID "${APP_VERSION_CODE_ANDROID} + 1")
-endif()
 unset(patch)
 
 set(package_files strings.xml
