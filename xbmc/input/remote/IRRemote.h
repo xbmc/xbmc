2bf543b53c734bdc977de247999d392848649ffa
Garrett Brown <themagnificentmrb@gmail.com>
2024-01-14 19:34:38 -0800

    [Remote] Remove unused struct

--- xbmc/input/remote/IRRemote.h
+++ xbmc/input/remote/IRRemote.h
@@ -89,18 +89,3 @@
 // Reserved 256 -> ...
 // Key.h
 // KEY_BUTTON_*
-
-typedef struct _XINPUT_IR_REMOTE
-{
-  unsigned char wButtons;
-  unsigned char region; // just a guess
-
-  //! Some value that is changing while a button is pressed... could be the
-  //! state of the buffer
-  unsigned char counter;
-
-  //! If > 0: first event triggered after a button was pressed on the remote
-  //! If 0: not first event
-  unsigned char firstEvent;
-
-} XINPUT_IR_REMOTE, *PIR_REMOTE;
