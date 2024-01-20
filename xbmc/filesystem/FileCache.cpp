bad29939851d91ae3c89e81c68a51fc2ce503447
thexai <58434170+thexai@users.noreply.github.com>
2024-01-20 11:16:11 +0100

    [FileCache] adjust Read Factor algorithm
    Fixes cache not fills with high bitrate files and "low" read factors.

--- xbmc/filesystem/FileCache.cpp
+++ xbmc/filesystem/FileCache.cpp
@@ -24,7 +24,6 @@
 
 #include <algorithm>
 #include <cassert>
-#include <chrono>
 #include <inttypes.h>
 #include <memory>
 
@@ -33,7 +32,6 @@
 #endif
 
 using namespace XFILE;
-using namespace std::chrono_literals;
 
 class CWriteRate
 {
@@ -300,7 +298,7 @@ void CFileCache::Process()
       if (limiter.Rate(m_writePos) < m_writeRate * readFactor)
         break;
 
-      if (m_seekEvent.Wait(100ms))
+      if (m_seekEvent.Wait(m_processWait))
       {
         if (!m_bStop)
           m_seekEvent.Set();
@@ -618,6 +616,18 @@ int CFileCache::IoControl(EIoControl request, void* param)
   if (request == IOCTRL_CACHE_SETRATE)
   {
     m_writeRate = *static_cast<uint32_t*>(param);
+
+    const double mBits = m_writeRate / 1024.0 / 1024.0 * 8.0; // Mbit/s
+
+    // calculates wait time inversely proportional to the bitrate
+    // and limited between 30 - 100 ms
+    const int wait = std::clamp(static_cast<int>(110.0 - mBits), 30, 100);
+
+    m_processWait = std::chrono::milliseconds(wait);
+
+    CLog::Log(LOGDEBUG,
+              "CFileCache::IoControl - setting maxRate to {:.2f} Mbit/s with processWait of {} ms",
+              mBits, wait);
     return 0;
   }
 
