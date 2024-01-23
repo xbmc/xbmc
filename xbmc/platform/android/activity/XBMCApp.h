beaf676a0b49902c255313eca341b39a4d463a02
hugbug <hugbug@users.noreply.github.com>
2024-01-23 15:36:31 +0100

    [Android] Exec wake-up routine only after hdmi handshake
    When device wakes up after sleep the wake-up jobs are executed. They include reinitialisation of audio engine. To avoid errors in audio engine we wait for hdmi handshake to complete before executing wake-up jobs.

--- xbmc/platform/android/activity/XBMCApp.h
+++ xbmc/platform/android/activity/XBMCApp.h
@@ -256,6 +256,7 @@ private:
   bool m_headsetPlugged{false};
   bool m_hdmiSource{false};
   bool m_wakeUp{false};
+  bool m_aeReset{false};
   IInputDeviceCallbacks* m_inputDeviceCallbacks{nullptr};
   IInputDeviceEventHandler* m_inputDeviceEventHandler{nullptr};
   bool m_hasReqVisible{false};
@@ -277,6 +278,9 @@ private:
 
   bool XBMC_DestroyDisplay();
   bool XBMC_SetupDisplay();
+
+  void OnSleep();
+  void OnWakeup();
 
   uint32_t m_playback_state{0};
   int64_t m_frameTimeNanos{0};
