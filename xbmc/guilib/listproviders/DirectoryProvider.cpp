9893aae45166f973edc6c6d7586465e5c64e2690
ksooo <3226626+ksooo@users.noreply.github.com>
2024-02-03 19:03:54 +0100

    [favourites][listproviders] Fix select action not to show the choose video version dialog for movies with multiple versions but to directly play the version.

--- xbmc/guilib/listproviders/DirectoryProvider.cpp
+++ xbmc/guilib/listproviders/DirectoryProvider.cpp
@@ -563,6 +563,9 @@ bool CDirectoryProvider::OnClick(const CGUIListItemPtr& item)
   // video select action setting is for files only, except exec func is playmedia...
   if (targetItem.HasVideoInfoTag() && (!targetItem.m_bIsFolder || isPlayMedia))
   {
+    // play the given/default video version, even if multiple versions are available
+    targetItem.SetProperty("has_resolved_video_asset", true);
+
     CVideoSelectActionProcessor proc{*this, std::make_shared<CFileItem>(targetItem)};
     if (proc.ProcessDefaultAction())
       return true;
