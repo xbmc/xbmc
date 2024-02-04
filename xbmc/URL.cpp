108eca01af57d9c1423b53078fbbce03fef47a6d
Andrey Filipenkov <decapitator@ukr.net>
2024-02-04 21:52:39 +0300

    don't try to set protocol options on a NFS URL

--- xbmc/URL.cpp
+++ xbmc/URL.cpp
@@ -365,6 +365,8 @@ void CURL::SetOptions(std::string strOptions)
 
 void CURL::SetProtocolOptions(std::string strOptions)
 {
+  if (AreProtocolOptionsProhibited())
+    return;
   m_strProtocolOptions.clear();
   m_protocolOptions.Clear();
   if (strOptions.length() > 0)
@@ -799,6 +801,8 @@ std::string CURL::GetProtocolOption(const std::string &key) const
 
 void CURL::SetProtocolOption(const std::string &key, const std::string &value)
 {
+  if (AreProtocolOptionsProhibited())
+    return;
   m_protocolOptions.AddOption(key, value);
   m_strProtocolOptions = m_protocolOptions.GetOptionsString(false);
 }
@@ -807,4 +811,9 @@ void CURL::RemoveProtocolOption(const std::string &key)
 {
   m_protocolOptions.RemoveOption(key);
   m_strProtocolOptions = m_protocolOptions.GetOptionsString(false);
+}
+
+bool CURL::AreProtocolOptionsProhibited() const
+{
+  return IsProtocol("nfs");
 }
