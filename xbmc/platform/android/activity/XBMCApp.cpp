32f33872329ae4afecdd16769ddad2e1634319d8
hugbug <hugbug@users.noreply.github.com>
2024-02-04 13:12:55 +0100

    [Android] Check if ACTION_HDMI_AUDIO_PLUG works
    The wakeup-routine now relies on ACTION_HDMI_AUDIO_PLUG only when this intent is really supported by device.

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -1333,6 +1333,7 @@ void CXBMCApp::onReceive(CJNIIntent intent)
   }
   else if (action == CJNIAudioManager::ACTION_HDMI_AUDIO_PLUG)
   {
+    m_supportsHdmiAudioPlug = true;
     const bool hdmiPlugged = (intent.getIntExtra(CJNIAudioManager::EXTRA_AUDIO_PLUG_STATE, 0) != 0);
     android_printf("-- HDMI is plugged in: %s", hdmiPlugged ? "yes" : "no");
     if (g_application.IsInitialized())
@@ -1361,7 +1362,7 @@ void CXBMCApp::onReceive(CJNIIntent intent)
     // screen but it is actually sent in response to changes in the overall interactive state of
     // the device.
     CLog::Log(LOGINFO, "Got device wakeup intent");
-    if (m_hdmiSource)
+    if (m_supportsHdmiAudioPlug)
       // wake-up sequence continues in ACTION_HDMI_AUDIO_PLUG intent
       m_wakeUp = true;
     else
