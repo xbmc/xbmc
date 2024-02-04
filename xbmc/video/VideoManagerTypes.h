6c02f3f174d1af5813865845b34d14061b4acf18
CrystalP <crystalp@kodi.tv>
2024-02-04 18:13:29 -0500

    [video] fill GUIListItem properties with streamdetails for the hybrid movie/folder items

--- xbmc/video/VideoManagerTypes.h
+++ xbmc/video/VideoManagerTypes.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <string>
+
 enum class VideoAssetTypeOwner
 {
   UNKNOWN = -1,
@@ -33,6 +35,7 @@ static constexpr int VIDEO_VERSION_ID_BEGIN = 40400;
 static constexpr int VIDEO_VERSION_ID_END = 40800;
 static constexpr int VIDEO_VERSION_ID_DEFAULT = VIDEO_VERSION_ID_BEGIN;
 static constexpr int VIDEO_VERSION_ID_ALL = 0;
+static const std::string VIDEODB_PATH_VERSION_ID_ALL{"videodb://movies/videoversions/0"};
 
 struct VideoAssetInfo
 {
