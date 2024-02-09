26aeb6b582e11aadc53a0868a9bce65ce5bc411f
enen92 <92enen@gmail.com>
2024-02-09 13:24:50 +0000

    Teletext: Fix crash due to invalid FTC_Node

--- xbmc/video/Teletext.h
+++ xbmc/video/Teletext.h
@@ -193,7 +193,7 @@ private:
   FTC_SBit            m_sBit;             /*  "       "   "  */
   FT_Face             m_Face;             /*  "       "   "  */
   /*! An opaque handle to a cache node object. Each cache node is reference-counted. */
-  FTC_Node m_anode;
+  FTC_Node m_anode{nullptr};
   FTC_ImageTypeRec    m_TypeTTF;          /*  "       "   "  */
   int                 m_Ascender;         /*  "       "   "  */
 
