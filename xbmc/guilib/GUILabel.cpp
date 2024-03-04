a1537b43df8b6732f0b50e215e79eacf5063be37
CastagnaIT <gottardo.stefano.83@gmail.com>
2024-03-04 11:09:33 +0100

    [guilib][GUILabel] Fix align Center x with overflow mode

--- xbmc/guilib/GUILabel.cpp
+++ xbmc/guilib/GUILabel.cpp
@@ -137,6 +137,12 @@ void CGUILabel::Render()
 
       if (m_label.align & XBFONT_RIGHT)
         align |= XBFONT_RIGHT;
+
+      if (m_label.align & XBFONT_CENTER_X)
+      {
+        posX += m_renderRect.Width() * 0.5f; // hack for centered multiline text, same of above
+        align |= XBFONT_CENTER_X;
+      }
     }
     m_textLayout.Render(posX, posY, m_label.angle, color, m_label.shadowColor, align, m_overflowType == OVER_FLOW_CLIP ? m_textLayout.GetTextWidth() : m_renderRect.Width(), renderSolid);
   }
