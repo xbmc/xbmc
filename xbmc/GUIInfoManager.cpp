921eccfbde2d9fa62eb7029ed7a4c989ff91ff07
Philipp Kerling <yol@casix.org>
2024-03-25 20:42:22 +0100

    c++20: Fix deprecated call to shared_ptr::unique

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -11245,7 +11245,7 @@ void CGUIInfoManager::Clear()
   {
     swapList.clear();
     for (auto &item : m_bools)
-      if (!item.unique())
+      if (item.use_count() > 1)
         swapList.insert(item);
     m_bools.swap(swapList);
   } while (swapList.size() != m_bools.size());
