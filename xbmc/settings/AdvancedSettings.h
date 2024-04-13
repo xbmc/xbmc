8be5519701fedc6ce861339942040ae06cbc0020
Stephan Sundermann <stephansundermann@gmail.com>
2024-04-13 10:31:36 +0200

    [settings] Remove VideoPlayerignoredtsinwav advanced setting

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -121,7 +121,6 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
 
     std::string m_audioDefaultPlayer;
     float m_audioPlayCountMinimumPercent;
-    bool m_VideoPlayerIgnoreDTSinWAV;
     float m_limiterHold;
     float m_limiterRelease;
 
