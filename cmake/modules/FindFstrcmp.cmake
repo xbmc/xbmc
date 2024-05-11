070643ebbd67817032b8a66879a39def106d3ec4
fuzzard <fuzzard@kodi.tv>
2024-05-11 22:14:30 +1000

    [cmake][modules] FindFstrcmp cleanup and use core_target_link_libraries

--- cmake/modules/FindFstrcmp.cmake
+++ cmake/modules/FindFstrcmp.cmake
@@ -5,10 +5,10 @@
 #
 # This will define the following target:
 #
-#   fstrcmp::fstrcmp   - The fstrcmp library
+#   ${APP_NAME_LC}::Fstrcmp   - The fstrcmp library
 #
 
-if(NOT TARGET fstrcmp::fstrcmp)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   if(ENABLE_INTERNAL_FSTRCMP)
     find_program(LIBTOOL libtool REQUIRED)
     include(cmake/scripts/common/ModuleHelpers.cmake)
@@ -28,19 +28,17 @@ if(NOT TARGET fstrcmp::fstrcmp)
     BUILD_DEP_TARGET()
   else()
     find_package(PkgConfig)
-    if(PKG_CONFIG_FOUND)
+    if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWS_STORE))
       pkg_check_modules(PC_FSTRCMP fstrcmp QUIET)
     endif()
 
     find_path(FSTRCMP_INCLUDE_DIR NAMES fstrcmp.h
                                   HINTS ${DEPENDS_PATH}/include ${PC_FSTRCMP_INCLUDEDIR}
-                                  ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
-                                  NO_CACHE)
+                                  ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
 
     find_library(FSTRCMP_LIBRARY NAMES fstrcmp
                                  HINTS ${DEPENDS_PATH}/lib ${PC_FSTRCMP_LIBDIR}
-                                 ${${CORE_PLATFORM_LC}_SEARCH_CONFIG}
-                                 NO_CACHE)
+                                 ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
 
     set(FSTRCMP_VER ${PC_FSTRCMP_VERSION})
   endif()
@@ -50,14 +48,18 @@ if(NOT TARGET fstrcmp::fstrcmp)
                                     REQUIRED_VARS FSTRCMP_LIBRARY FSTRCMP_INCLUDE_DIR
                                     VERSION_VAR FSTRCMP_VER)
 
-  add_library(fstrcmp::fstrcmp UNKNOWN IMPORTED)
-  set_target_properties(fstrcmp::fstrcmp PROPERTIES
-                                         IMPORTED_LOCATION "${FSTRCMP_LIBRARY}"
-                                         INTERFACE_INCLUDE_DIRECTORIES "${FSTRCMP_INCLUDE_DIR}")
+  if(FSTRCMP_FOUND)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${FSTRCMP_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${FSTRCMP_INCLUDE_DIR}")
 
-  if(TARGET fstrcmp)
-    add_dependencies(fstrcmp::fstrcmp fstrcmp)
+    if(TARGET fstrcmp)
+      add_dependencies(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} fstrcmp)
+    endif()
+  else()
+    if(Fstrcmp_FIND_REQUIRED)
+      message(FATAL_ERROR "Fstrcmp libraries were not found. You may want to use -DENABLE_INTERNAL_FSTRCMP=ON")
+    endif()
   endif()
-
-  set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP fstrcmp::fstrcmp)
 endif()
