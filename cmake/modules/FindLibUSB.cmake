a945a7fb01000ca60581baf1b1fe49cc163f7279
fuzzard <fuzzard@kodi.tv>
2024-05-11 22:43:14 +1000

    [cmake][modules] FindLibUSB cleanup and use core_target_link_libraries

--- cmake/modules/FindLibUSB.cmake
+++ cmake/modules/FindLibUSB.cmake
@@ -5,9 +5,9 @@
 #
 # This will define the following target:
 #
-#   LibUSB::LibUSB   - The USB library
+#   ${APP_NAME_LC}::LibUSB   - The USB library
 
-if(NOT TARGET LibUSB::LibUSB)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
 
   if(PKG_CONFIG_FOUND)
@@ -15,11 +15,9 @@ if(NOT TARGET LibUSB::LibUSB)
   endif()
 
   find_path(LIBUSB_INCLUDE_DIR usb.h
-                               HINTS ${PC_LIBUSB_INCLUDEDIR}
-                               NO_CACHE)
+                               HINTS ${PC_LIBUSB_INCLUDEDIR})
   find_library(LIBUSB_LIBRARY NAMES usb
-                              HINTS ${PC_LIBUSB_INCLUDEDIR}
-                              NO_CACHE)
+                              HINTS ${PC_LIBUSB_INCLUDEDIR})
   set(LIBUSB_VERSION ${PC_LIBUSB_VERSION})
 
   include(FindPackageHandleStandardArgs)
@@ -28,11 +26,10 @@ if(NOT TARGET LibUSB::LibUSB)
                                     VERSION_VAR LIBUSB_VERSION)
 
   if(LIBUSB_FOUND)
-    add_library(LibUSB::LibUSB UNKNOWN IMPORTED)
-    set_target_properties(LibUSB::LibUSB PROPERTIES
-                                         IMPORTED_LOCATION "${LIBUSB_LIBRARY}"
-                                         INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIR}"
-                                         INTERFACE_COMPILE_DEFINITIONS USE_LIBUSB=1)
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP LibUSB::LibUSB)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${LIBUSB_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAVE_LIBUSB)
   endif()
 endif()
