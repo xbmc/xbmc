0b50c6e830110ba570c47b3674da5ad860da5719
fuzzard <fuzzard@kodi.tv>
2024-05-11 21:26:54 +1000

    [cmake][modules] FindCAP cleanup and use core_target_link_libraries

--- cmake/modules/FindCAP.cmake
+++ cmake/modules/FindCAP.cmake
@@ -5,20 +5,18 @@
 #
 # This will define the following target:
 #
-# CAP::CAP - The LibCap library
+# ${APP_NAME_LC}::CAP - The LibCap library
 
-if(NOT TARGET CAP::CAP)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
   if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_CAP libcap QUIET)
   endif()
 
   find_path(CAP_INCLUDE_DIR NAMES sys/capability.h
-                            HINTS ${PC_CAP_INCLUDEDIR}
-                            NO_CACHE)
+                            HINTS ${PC_CAP_INCLUDEDIR})
   find_library(CAP_LIBRARY NAMES cap libcap
-                           HINTS ${PC_CAP_LIBDIR}
-                           NO_CACHE)
+                           HINTS ${PC_CAP_LIBDIR})
 
   set(CAP_VERSION ${PC_CAP_VERSION})
 
@@ -28,11 +26,10 @@ if(NOT TARGET CAP::CAP)
                                     VERSION_VAR CAP_VERSION)
 
   if(CAP_FOUND)
-    add_library(CAP::CAP UNKNOWN IMPORTED)
-    set_target_properties(CAP::CAP PROPERTIES
-                                   IMPORTED_LOCATION "${CAP_LIBRARY}"
-                                   INTERFACE_INCLUDE_DIRECTORIES "${CAP_INCLUDE_DIR}"
-                                   INTERFACE_COMPILE_DEFINITIONS HAVE_LIBCAP=1)
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP CAP::CAP)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${CAP_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${CAP_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAVE_LIBCAP)
   endif()
 endif()
