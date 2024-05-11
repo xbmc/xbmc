fb21af3703d5e5282291af23218ed2645b16292a
fuzzard <fuzzard@kodi.tv>
2024-05-11 21:14:34 +1000

    [cmake][modules] FindAcbAPI cleanup and use core_target_link_libraries

--- cmake/modules/FindAcbAPI.cmake
+++ cmake/modules/FindAcbAPI.cmake
@@ -5,9 +5,9 @@
 #
 # This will define the following target:
 #
-#   ACBAPI::ACBAPI   - The acbAPI library
+#   ${APP_NAME_LC}::AcbAPI   - The acbAPI library
 
-if(NOT TARGET ACBAPI::ACBAPI)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
   if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_ACBAPI libAcbAPI QUIET)
@@ -28,15 +28,18 @@ if(NOT TARGET ACBAPI::ACBAPI)
                                     VERSION_VAR ACBAPI_VERSION)
 
   if(ACBAPI_FOUND)
-    add_library(ACBAPI::ACBAPI UNKNOWN IMPORTED)
-    set_target_properties(ACBAPI::ACBAPI PROPERTIES
-                                         IMPORTED_LOCATION "${ACBAPI_LIBRARY}"
-                                         INTERFACE_INCLUDE_DIRECTORIES "${ACBAPI_INCLUDE_DIR}")
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP ACBAPI::ACBAPI)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${ACBAPI_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${ACBAPI_INCLUDE_DIR}")
 
     # creates an empty library to install on webOS 5+ devices
     file(TOUCH dummy.c)
     add_library(AcbAPI SHARED dummy.c)
     set_target_properties(AcbAPI PROPERTIES VERSION 1.0.0 SOVERSION 1)
+  else()
+    if(AcbAPI_FIND_REQUIRED)
+      message(FATAL_ERROR "AcbAPI libraries were not found.")
+    endif()
   endif()
 endif()
