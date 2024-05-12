d2721eecbcd2200f8db4aca6fb00494e1427ae63
fuzzard <fuzzard@kodi.tv>
2024-05-12 12:19:40 +1000

    [cmake][modules] FindMDNS cleanup and use core_target_link_libraries

--- cmake/modules/FindMDNS.cmake
+++ cmake/modules/FindMDNS.cmake
@@ -5,31 +5,27 @@
 #
 # This will define the following target:
 #
-#   MDNS::MDNS   - The mDNSlibrary
+#   ${APP_NAME_LC}::MDNS   - The mDNSlibrary
 
-if(NOT TARGET MDNS::MDNS)
-  find_path(MDNS_INCLUDE_DIR NAMES dmDnsEmbedded.h dns_sd.h
-                             NO_CACHE)
-  find_library(MDNS_LIBRARY NAMES mDNSEmbedded dnssd
-                            NO_CACHE)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
+  find_path(MDNS_INCLUDE_DIR NAMES dmDnsEmbedded.h dns_sd.h)
+  find_library(MDNS_LIBRARY NAMES mDNSEmbedded dnssd)
 
-  find_path(MDNS_EMBEDDED_INCLUDE_DIR NAMES mDnsEmbedded.h
-                                      NO_CACHE)
+  find_path(MDNS_EMBEDDED_INCLUDE_DIR NAMES mDnsEmbedded.h)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(MDNS
                                     REQUIRED_VARS MDNS_LIBRARY MDNS_INCLUDE_DIR)
 
   if(MDNS_FOUND)
-    add_library(MDNS::MDNS UNKNOWN IMPORTED)
-    set_target_properties(MDNS::MDNS PROPERTIES
-                                     IMPORTED_LOCATION "${MDNS_LIBRARY}"
-                                     INTERFACE_INCLUDE_DIRECTORIES "${MDNS_INCLUDE_DIR}"
-                                     INTERFACE_COMPILE_DEFINITIONS "HAS_MDNS=1;HAS_ZEROCONF=1")
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${MDNS_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${MDNS_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS "HAS_MDNS;HAS_ZEROCONF")
     if(MDNS_EMBEDDED_INCLUDE_DIR)
-      set_property(TARGET MDNS::MDNS APPEND PROPERTY
-                                            INTERFACE_COMPILE_DEFINITIONS HAS_MDNS_EMBEDDED=1)
+      set_property(TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} APPEND PROPERTY
+                                                                            INTERFACE_COMPILE_DEFINITIONS HAS_MDNS_EMBEDDED)
     endif()
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP MDNS::MDNS)
   endif()
 endif()
