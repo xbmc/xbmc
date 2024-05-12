74427b2686f1e734372bd1f64be5934bd9748bc0
fuzzard <fuzzard@kodi.tv>
2024-05-12 13:31:32 +1000

    [cmake][modules] FindPlayerAPIs cleanup and use core_target_link_libraries

--- cmake/modules/FindPlayerAPIs.cmake
+++ cmake/modules/FindPlayerAPIs.cmake
@@ -5,29 +5,26 @@
 #
 # This will define the following target:
 #
-#   PLAYERAPIS::PLAYERAPIS   - The playerAPIs library
+#   ${APP_NAME_LC}::PlayerAPIs   - The playerAPIs library
 
-if(NOT TARGET PLAYERAPIS::PLAYERAPIS)
-
-  if(PlayerAPIs_FIND_VERSION)
-    if(PlayerAPIs_FIND_VERSION_EXACT)
-      set(PlayerAPIs_FIND_SPEC "=${PlayerAPIs_FIND_VERSION_COMPLETE}")
-    else()
-      set(PlayerAPIs_FIND_SPEC ">=${PlayerAPIs_FIND_VERSION_COMPLETE}")
-    endif()
-  endif()
-
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
   if(PKG_CONFIG_FOUND)
+    if(PlayerAPIs_FIND_VERSION)
+      if(PlayerAPIs_FIND_VERSION_EXACT)
+        set(PlayerAPIs_FIND_SPEC "=${PlayerAPIs_FIND_VERSION_COMPLETE}")
+      else()
+        set(PlayerAPIs_FIND_SPEC ">=${PlayerAPIs_FIND_VERSION_COMPLETE}")
+      endif()
+    endif()
+
     pkg_check_modules(PC_PLAYERAPIS libplayerAPIs${PlayerAPIs_FIND_SPEC} QUIET)
   endif()
 
   find_path(PLAYERAPIS_INCLUDE_DIR NAMES starfish-media-pipeline/StarfishMediaAPIs.h
-                                   HINTS ${PC_PLAYERAPIS_INCLUDEDIR}
-                                   NO_CACHE)
+                                   HINTS ${PC_PLAYERAPIS_INCLUDEDIR})
   find_library(PLAYERAPIS_LIBRARY NAMES playerAPIs
-                                  HINTS ${PC_PLAYERAPIS_LIBDIR}
-                                  NO_CACHE)
+                                  HINTS ${PC_PLAYERAPIS_LIBDIR})
 
   set(PLAYERAPIS_VERSION ${PC_PLAYERAPIS_VERSION})
 
@@ -37,10 +34,13 @@ if(NOT TARGET PLAYERAPIS::PLAYERAPIS)
                                     VERSION_VAR PLAYERAPIS_VERSION)
 
   if(PLAYERAPIS_FOUND)
-    add_library(PLAYERAPIS::PLAYERAPIS UNKNOWN IMPORTED)
-    set_target_properties(PLAYERAPIS::PLAYERAPIS PROPERTIES
-                                                 IMPORTED_LOCATION "${PLAYERAPIS_LIBRARY}"
-                                                 INTERFACE_INCLUDE_DIRECTORIES "${PLAYERAPIS_INCLUDE_DIR}")
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP PLAYERAPIS::PLAYERAPIS)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${PLAYERAPIS_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${PLAYERAPIS_INCLUDE_DIR}")
+  else()
+    if(PlayerAPIs_FIND_REQUIRED)
+      message(FATAL_ERROR "PlayerAPIs library not found.")
+    endif()
   endif()
 endif()
