33b4f017cf8e18e6704e2ccd85e59fdd51f09058
fuzzard <fuzzard@kodi.tv>
2024-05-12 12:55:51 +1000

    [cmake][modules] FindOpenGl cleanup and use core_target_link_libraries

--- cmake/modules/FindOpenGl.cmake
+++ cmake/modules/FindOpenGl.cmake
@@ -5,9 +5,9 @@
 #
 # This will define the following target:
 #
-#   OpenGL::GL - The OpenGL library
+#   ${APP_NAME_LC}::OpenGl - The OpenGL library
 
-if(NOT TARGET OpenGL::GL)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
   if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_OPENGL gl QUIET)
@@ -15,22 +15,19 @@ if(NOT TARGET OpenGL::GL)
 
   find_library(OPENGL_gl_LIBRARY NAMES GL OpenGL
                                  HINTS ${PC_OPENGL_gl_LIBDIR} ${CMAKE_OSX_SYSROOT}/System/Library
-                                 PATH_SUFFIXES Frameworks
-                                 NO_CACHE)
+                                 PATH_SUFFIXES Frameworks)
   find_path(OPENGL_INCLUDE_DIR NAMES GL/gl.h gl.h
-                               HINTS ${PC_OPENGL_gl_INCLUDEDIR} ${OPENGL_gl_LIBRARY}/Headers
-                               NO_CACHE)
+                               HINTS ${PC_OPENGL_gl_INCLUDEDIR} ${OPENGL_gl_LIBRARY}/Headers)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(OpenGl
                                     REQUIRED_VARS OPENGL_gl_LIBRARY OPENGL_INCLUDE_DIR)
 
   if(OPENGL_FOUND)
-    add_library(OpenGL::GL UNKNOWN IMPORTED)
-    set_target_properties(OpenGL::GL PROPERTIES
-                                     IMPORTED_LOCATION "${OPENGL_gl_LIBRARY}"
-                                     INTERFACE_INCLUDE_DIRECTORIES "${OPENGL_INCLUDE_DIR}"
-                                     INTERFACE_COMPILE_DEFINITIONS HAS_GL=1)
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP OpenGL::GL)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${OPENGL_gl_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${OPENGL_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAS_GL)
   endif()
 endif()
