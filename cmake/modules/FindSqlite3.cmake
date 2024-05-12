f134ce5459da0c8aab81db831e33115632f0a659
fuzzard <fuzzard@kodi.tv>
2024-05-12 14:45:34 +1000

    [cmake][modules] FindSqlite3 cleanup and use core_target_link_libraries

--- cmake/modules/FindSqlite3.cmake
+++ cmake/modules/FindSqlite3.cmake
@@ -5,23 +5,23 @@
 #
 # This will define the following target:
 #
-#   SQLite3::SQLite3 - The SQLite3 library
+#   ${APP_NAME_LC}::Sqlite3 - The SQLite3 library
 #
 
-if(NOT TARGET SQLite3::SQLite3)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
-  if(PKG_CONFIG_FOUND)
+  if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWS_STORE))
     pkg_check_modules(PC_SQLITE3 sqlite3 QUIET)
+
+    set(SQLITE3_VERSION ${PC_SQLITE3_VERSION})
   endif()
 
   find_path(SQLITE3_INCLUDE_DIR NAMES sqlite3.h
-                                HINTS ${PC_SQLITE3_INCLUDEDIR}
-                                NO_CACHE)
+                             HINTS ${DEPENDS_PATH}/include ${PC_SQLITE3_INCLUDEDIR}
+                             ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
   find_library(SQLITE3_LIBRARY NAMES sqlite3
-                               HINTS ${PC_SQLITE3_LIBDIR}
-                               NO_CACHE)
-
-  set(SQLITE3_VERSION ${PC_SQLITE3_VERSION})
+                               HINTS ${DEPENDS_PATH}/lib ${PC_SQLITE3_LIBDIR}
+                               ${${CORE_PLATFORM_LC}_SEARCH_CONFIG})
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Sqlite3
@@ -29,11 +29,13 @@ if(NOT TARGET SQLite3::SQLite3)
                                     VERSION_VAR SQLITE3_VERSION)
 
   if(Sqlite3_FOUND)
-    add_library(SQLite3::SQLite3 UNKNOWN IMPORTED)
-    set_target_properties(SQLite3::SQLite3 PROPERTIES
-                                           IMPORTED_LOCATION "${SQLITE3_LIBRARY}"
-                                           INTERFACE_INCLUDE_DIRECTORIES "${SQLITE3_INCLUDE_DIR}")
-
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP SQLite3::SQLite3)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${SQLITE3_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${SQLITE3_INCLUDE_DIR}")
+  else()
+    if(Sqlite3_FIND_REQUIRED)
+      message(FATAL_ERROR "SQLite3 library not found.")
+    endif()
   endif()
 endif()
