dcec96dc89606dc5efcdd5e0830c493a197862f8
fuzzard <fuzzard@kodi.tv>
2024-05-12 15:37:48 +1000

    [cmake][modules] FindVDPAU cleanup and use core_target_link_libraries

--- cmake/modules/FindVDPAU.cmake
+++ cmake/modules/FindVDPAU.cmake
@@ -5,20 +5,18 @@
 #
 # This will define the following target:
 #
-#   VDPAU::VDPAU   - The VDPAU library
+#   ${APP_NAME_LC}::VDPAU   - The VDPAU library
 
-if(NOT TARGET VDPAU::VDPAU)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
   if(PKG_CONFIG_FOUND)
     pkg_check_modules(PC_VDPAU vdpau QUIET)
   endif()
 
   find_path(VDPAU_INCLUDE_DIR NAMES vdpau/vdpau.h vdpau/vdpau_x11.h
-                              HINTS ${PC_VDPAU_INCLUDEDIR}
-                              NO_CACHE)
+                              HINTS ${PC_VDPAU_INCLUDEDIR})
   find_library(VDPAU_LIBRARY NAMES vdpau
-                             HINTS ${PC_VDPAU_LIBDIR}
-                             NO_CACHE)
+                             HINTS ${PC_VDPAU_LIBDIR})
 
   set(VDPAU_VERSION ${PC_VDPAU_VERSION})
 
@@ -28,11 +26,10 @@ if(NOT TARGET VDPAU::VDPAU)
                                     VERSION_VAR VDPAU_VERSION)
 
   if(VDPAU_FOUND)
-    add_library(VDPAU::VDPAU UNKNOWN IMPORTED)
-    set_target_properties(VDPAU::VDPAU PROPERTIES
-                                       IMPORTED_LOCATION "${VDPAU_LIBRARY}"
-                                       INTERFACE_INCLUDE_DIRECTORIES "${VDPAU_INCLUDE_DIR}"
-                                       INTERFACE_COMPILE_DEFINITIONS HAVE_LIBVDPAU=1)
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP VDPAU::VDPAU)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${VDPAU_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${VDPAU_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAVE_LIBVDPAU)
   endif()
 endif()
