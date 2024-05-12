7236765e06325461589584e5b7e074681224a76e
fuzzard <fuzzard@kodi.tv>
2024-05-12 13:36:10 +1000

    [cmake][modules] FindPlist cleanup and use core_target_link_libraries

--- cmake/modules/FindPlist.cmake
+++ cmake/modules/FindPlist.cmake
@@ -5,23 +5,22 @@
 #
 # This will define the following target:
 #
-#   Plist::Plist - The Plist library
+#   ${APP_NAME_LC}::Plist - The Plist library
 
-if(NOT TARGET Plist::Plist)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(PkgConfig)
-  if(PKG_CONFIG_FOUND)
+  if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWS_STORE))
     pkg_search_module(PC_PLIST libplist-2.0 libplist QUIET)
   endif()
 
   find_path(PLIST_INCLUDE_DIR plist/plist.h
-                              HINTS ${PC_PLIST_INCLUDEDIR}
-                              NO_CACHE)
+                              HINTS ${DEPENDS_PATH}/include ${PC_PLIST_INCLUDEDIR}
+                              ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
+  find_library(PLIST_LIBRARY NAMES plist-2.0 plist libplist-2.0 libplist
+                             HINTS ${DEPENDS_PATH}/lib ${PC_PLIST_LIBDIR}
+                             ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
 
   set(PLIST_VERSION ${PC_PLIST_VERSION})
-
-  find_library(PLIST_LIBRARY NAMES plist-2.0 plist libplist-2.0 libplist
-                                   HINTS ${PC_PLIST_LIBDIR}
-                                   NO_CACHE)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Plist
@@ -29,11 +28,10 @@ if(NOT TARGET Plist::Plist)
                                     VERSION_VAR PLIST_VERSION)
 
   if(PLIST_FOUND)
-    add_library(Plist::Plist UNKNOWN IMPORTED)
-    set_target_properties(Plist::Plist PROPERTIES
-                                       IMPORTED_LOCATION "${PLIST_LIBRARY}"
-                                       INTERFACE_INCLUDE_DIRECTORIES "${PLIST_INCLUDE_DIR}"
-                                       INTERFACE_COMPILE_DEFINITIONS HAS_AIRPLAY=1)
-    set_property(GLOBAL APPEND PROPERTY INTERNAL_DEPS_PROP Plist::Plist)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${PLIST_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${PLIST_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAS_AIRPLAY)
   endif()
 endif()
