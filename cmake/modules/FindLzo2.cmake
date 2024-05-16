edc5e4387d14b4068c80440bfe3fc3761024e075
fuzzard <fuzzard@kodi.tv>
2024-05-16 18:00:04 +1000

    [cmake][modules] FindLzo2 cleanup and use core_target_link_libraries

--- cmake/modules/FindLzo2.cmake
+++ cmake/modules/FindLzo2.cmake
@@ -3,35 +3,33 @@
 # --------
 # Finds the Lzo2 library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# LZO2_FOUND - system has Lzo2
-# LZO2_INCLUDE_DIRS - the Lzo2 include directory
-# LZO2_LIBRARIES - the Lzo2 libraries
-#
-# and the following imported targets::
-#
-#   Lzo2::Lzo2   - The Lzo2 library
+#   ${APP_NAME_LC}::Lzo2   - The Lzo2 library
 
-find_path(LZO2_INCLUDE_DIR NAMES lzo1x.h
-                           PATH_SUFFIXES lzo)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
 
-find_library(LZO2_LIBRARY NAMES lzo2 liblzo2)
+  find_path(LZO2_INCLUDE_DIR NAMES lzo1x.h
+                             PATH_SUFFIXES lzo
+                             HINTS ${DEPENDS_PATH}/include
+                             ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Lzo2
-                                  REQUIRED_VARS LZO2_LIBRARY LZO2_INCLUDE_DIR)
+  find_library(LZO2_LIBRARY NAMES lzo2 liblzo2
+                            HINTS ${DEPENDS_PATH}/lib
+                            ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
 
-if(LZO2_FOUND)
-  set(LZO2_LIBRARIES ${LZO2_LIBRARY})
-  set(LZO2_INCLUDE_DIRS ${LZO2_INCLUDE_DIR})
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Lzo2
+                                    REQUIRED_VARS LZO2_LIBRARY LZO2_INCLUDE_DIR)
 
-  if(NOT TARGET Lzo2::Lzo2)
-    add_library(Lzo2::Lzo2 UNKNOWN IMPORTED)
-    set_target_properties(Lzo2::Lzo2 PROPERTIES
-                                     IMPORTED_LOCATION "${LZO2_LIBRARY}"
-                                     INTERFACE_INCLUDE_DIRECTORIES "${LZO2_INCLUDE_DIR}")
+  if(LZO2_FOUND)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${LZO2_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${LZO2_INCLUDE_DIR}")
+  else()
+    if(LibLzo2_FIND_REQUIRED)
+      message(FATAL_ERROR "Lzo2 library was not found.")
+    endif()
   endif()
 endif()
-
-mark_as_advanced(LZO2_INCLUDE_DIR LZO2_LIBRARY)
