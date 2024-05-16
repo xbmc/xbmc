f369d5887d5d0cd84cd42a2f673c901e0b657ec4
fuzzard <fuzzard@kodi.tv>
2024-05-16 17:50:47 +1000

    [cmake][modules] FindLibDovi update to target usage

--- cmake/modules/FindLibDovi.cmake
+++ cmake/modules/FindLibDovi.cmake
@@ -2,33 +2,32 @@
 # -------
 # Finds the libdovi library
 #
-# This will define the following variables::
+# This will define the following target:
 #
-# LIBDOVI_FOUND - system has libdovi
-# LIBDOVI_INCLUDE_DIRS - the libdovi include directories
-# LIBDOVI_LIBRARIES - the libdovi libraries
-# LIBDOVI_DEFINITIONS - the libdovi compile definitions
+#   ${APP_NAME_LC}::LibDovi   - The libDovi library
 
-find_package(PkgConfig)
+if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
 
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_LIBDOVI libdovi QUIET)
+  find_package(PkgConfig)
+
+  if(PKG_CONFIG_FOUND)
+    pkg_check_modules(PC_LIBDOVI libdovi QUIET)
+  endif()
+
+  find_library(LIBDOVI_LIBRARY NAMES dovi libdovi
+                               HINTS ${PC_LIBDOVI_LIBDIR})
+  find_path(LIBDOVI_INCLUDE_DIR NAMES libdovi/rpu_parser.h
+                                HINTS ${PC_LIBDOVI_INCLUDEDIR})
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(LibDovi
+                                    REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR)
+
+  if(LIBDOVI_FOUND)
+    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
+    set_target_properties(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
+                                                                     IMPORTED_LOCATION "${LIBDOVI_LIBRARY}"
+                                                                     INTERFACE_INCLUDE_DIRECTORIES "${LIBDOVI_INCLUDE_DIR}"
+                                                                     INTERFACE_COMPILE_DEFINITIONS HAVE_LIBDOVI)
+  endif()
 endif()
-
-find_library(LIBDOVI_LIBRARY NAMES dovi libdovi
-                             HINTS ${PC_LIBDOVI_LIBDIR}
-)
-find_path(LIBDOVI_INCLUDE_DIR NAMES libdovi/rpu_parser.h
-                              HINTS ${PC_LIBDOVI_INCLUDEDIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibDovi
-                                  REQUIRED_VARS LIBDOVI_LIBRARY LIBDOVI_INCLUDE_DIR)
-
-if(LIBDOVI_FOUND)
-  set(LIBDOVI_INCLUDE_DIRS ${LIBDOVI_INCLUDE_DIR})
-  set(LIBDOVI_LIBRARIES ${LIBDOVI_LIBRARY})
-  set(LIBDOVI_DEFINITIONS -DHAVE_LIBDOVI=1)
-endif()
-
-mark_as_advanced(LIBDOVI_INCLUDE_DIR LIBDOVI_LIBRARY)
