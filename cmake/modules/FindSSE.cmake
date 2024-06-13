597196c55f285af27f697fce8c899f529b2da461
Frank Howie <howie-f@kodi.tv>
2024-06-13 15:46:18 +0200

    [cmake] use dmesg and pipe to check SSE capabilities
    enables SSE check in a jailed FreeBSD builder

--- cmake/modules/FindSSE.cmake
+++ cmake/modules/FindSSE.cmake
@@ -42,7 +42,8 @@ if(CMAKE_SYSTEM_NAME MATCHES "Linux")
    endif()
 elseif(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
    if(CPU MATCHES "amd64" OR CPU MATCHES "i.86")
-     execute_process(COMMAND grep Features /var/run/dmesg.boot OUTPUT_VARIABLE CPUINFO)
+     execute_process(COMMAND dmesg
+                     COMMAND grep Features OUTPUT_VARIABLE CPUINFO)
 
      string(REGEX REPLACE "^.*(SSE).*$" "\\1" _SSE_THERE "${CPUINFO}")
      string(COMPARE EQUAL "SSE" "${_SSE_THERE}" _SSE_TRUE)
