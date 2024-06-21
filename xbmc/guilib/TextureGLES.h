61331b0813c67e2990a6dc44696768b4a5b9eb94
sarbes <sarbes@kodi.tv>
2024-06-21 15:16:54 +0200

    GLES: Add Client sync to texture uploads

--- xbmc/guilib/TextureGLES.h
+++ xbmc/guilib/TextureGLES.h
@@ -21,6 +21,7 @@ public:
   void CreateTextureObject() override;
   void DestroyTextureObject() override;
   void LoadToGPU() override;
+  void SyncGPU() override;
   void BindToUnit(unsigned int unit) override;
 
 protected:
