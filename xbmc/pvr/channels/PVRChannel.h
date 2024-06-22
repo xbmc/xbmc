c49b45a03ebbe87d4040574b56aa65087220f847
ksooo <3226626+ksooo@users.noreply.github.com>
2024-06-22 15:40:00 +0200

    Cleanup: Fix typos in PVRChannel.h

--- xbmc/pvr/channels/PVRChannel.h
+++ xbmc/pvr/channels/PVRChannel.h
@@ -93,7 +93,7 @@ public:
   /*!
    * @brief Set the identifier for this channel.
    * @param iDatabaseId The new channel ID
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetChannelID(int iDatabaseId);
 
@@ -114,7 +114,7 @@ public:
    * The EPG of hidden channels won't be updated.
    * @param bIsHidden The new setting.
    * @param bIsUserSetIcon true if user changed the hidden flag via GUI, false otherwise.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetHidden(bool bIsHidden, bool bIsUserSetHidden = false);
 
@@ -129,7 +129,7 @@ public:
    * Set to true to lock this channel. Set to false to unlock it.
    * Locked channels need can only be viewed if parental PIN entered.
    * @param bIsLocked The new setting.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetLocked(bool bIsLocked);
 
@@ -191,7 +191,7 @@ public:
    * @brief Set the path to the icon for this channel.
    * @param strIconPath The new path.
    * @param bIsUserSetIcon true if user changed the icon via GUI, false otherwise.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetIconPath(const std::string& strIconPath, bool bIsUserSetIcon = false);
 
@@ -204,7 +204,7 @@ public:
    * @brief Set the name for this channel used by XBMC.
    * @param strChannelName The new channel name.
    * @param bIsUserSetName whether the change was triggered by the user directly
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetChannelName(const std::string& strChannelName, bool bIsUserSetName = false);
 
@@ -217,7 +217,7 @@ public:
    * @brief Set the last time the channel has been watched and the channel group used to watch.
    * @param lastWatched The new last watched time value.
    * @param groupId the id of the group used to watch the channel.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetLastWatched(time_t lastWatched, int groupId);
 
@@ -229,7 +229,7 @@ public:
   /*!
    * @brief Set the date and time the channel was added to the TV database.
    * @param dateTimeAdded The date and time.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetDateTimeAdded(const CDateTime& dateTimeAdded);
 
@@ -267,7 +267,7 @@ public:
   /*!
    * @brief Set the identifier of the client that serves this channel.
    * @param iClientId The new ID.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetClientID(int iClientId);
 
@@ -415,7 +415,7 @@ public:
   /*!
    * @brief Set to true if an EPG should be used for this channel. Set to false otherwise.
    * @param bEPGEnabled The new value.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetEPGEnabled(bool bEPGEnabled);
 
@@ -436,7 +436,7 @@ public:
    * Set to "client" to load the EPG from the backend
    *
    * @param strScraper The new scraper name.
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetEPGScraper(const std::string& strScraper);
 
@@ -512,7 +512,7 @@ private:
   /*!
    * @brief Set the client provider Uid for this channel
    * @param iClientProviderUid The provider Uid for this channel
-   * @return True if the something changed, false otherwise.
+   * @return True if something changed, false otherwise.
    */
   bool SetClientProviderUid(int iClientProviderUid);
 
