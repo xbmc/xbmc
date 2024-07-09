29f33e3d7d85364ce86d252790ed0fc4b4006e9f
fuzzard <fuzzard@kodi.tv>
2024-07-09 20:42:16 +1000

    [cmake] Fix LibDVD* for all platforms
    Oops, broke this pretty bad.
    Fix macros using incorrect arguments.
    Fix target usage of additional libs.
    Dont use generator expression in a variable

--- cmake/modules/FindLibDvd.cmake
+++ cmake/modules/FindLibDvd.cmake
@@ -9,16 +9,19 @@
 if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   find_package(LibDvdNav MODULE REQUIRED)
 
-  set(_dvdlibs LibDvdNav::LibDvdNav
-               $<$<TARGET_EXISTS:LibDvdCSS::LibDvdCSS>:LibDvdCSS::LibDvdCSS>>)
-
   if(CORE_SYSTEM_NAME MATCHES windows)
     set(LIBDVD_TARGET_DIR .)
-    copy_file_to_buildtree($<TARGET_FILE:LibDvdNav::LibDvdNav> DIRECTORY ${LIBDVD_TARGET_DIR})
+    copy_file_to_buildtree(${DEPENDS_PATH}/bin/libdvdnav.dll DIRECTORY ${LIBDVD_TARGET_DIR})
   else()
+    set(_dvdlibs LibDvdRead::LibDvdRead)
+
+    if(TARGET LibDvdCSS::LibDvdCSS)
+      list(APPEND _dvdlibs LibDvdCSS::LibDvdCSS)
+    endif()
+
     # link a shared dvdnav library that includes the whole archives of dvdread and dvdcss as well
     # the quotes around _dvdlibs are on purpose, since we want to pass a list to the function that will be unpacked automatically
-    core_link_library(LibDvdNav::LibDvdNav system/players/VideoPlayer/libdvdnav libdvdnav archives "${_dvdlibs}")
+    core_link_library(LibDvdNav::LibDvdNav system/players/VideoPlayer/libdvdnav archives "${_dvdlibs}")
   endif()
 
   add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} INTERFACE IMPORTED)
