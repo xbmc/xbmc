9efe6a87c312874662d69630be67383d8b22df0e
78andyp <drandypowell@live.co.uk>
2024-07-14 00:34:45 +0100

    Jenkins

--- xbmc/video/VideoDatabase.h
+++ xbmc/video/VideoDatabase.h
@@ -596,7 +596,8 @@ public:
   int SetDetailsForMusicVideo(CVideoInfoTag& details,
                               const std::map<std::string, std::string>& artwork,
                               int idMVideo = -1);
-  void SetStreamDetailsForFile(const CStreamDetails& details, const std::string& strFileNameAndPath);
+  void SetStreamDetailsForFile(const CStreamDetails& details,
+                               const std::string& strFileNameAndPath);
   /*!
    * \brief Clear any existing stream details and add the new provided details to a file.
    * \param[in] details New stream details
