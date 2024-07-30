9b643c70bfca3b20faed73e21609b31d8fa23d33
Markus Härer <markus.haerer@gmx.net>
2024-07-31 01:59:25 +0200

    StringUtils: Modernize `IsNaturalNumber` and `IsInteger`
    - Add `[[nodiscard]]`
    - Add `noexcept`
    - Use `std::string_view`

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -1457,7 +1457,7 @@ std::string StringUtils::MillisecondsToTimeString(std::chrono::milliseconds mill
   return strTimeString;
 }
 
-bool StringUtils::IsNaturalNumber(const std::string& str)
+bool StringUtils::IsNaturalNumber(std::string_view str) noexcept
 {
   size_t i = 0, n = 0;
   // allow whitespace,digits,whitespace
@@ -1472,7 +1472,7 @@ bool StringUtils::IsNaturalNumber(const std::string& str)
   return i == str.size() && n > 0;
 }
 
-bool StringUtils::IsInteger(const std::string& str)
+bool StringUtils::IsInteger(std::string_view str) noexcept
 {
   size_t i = 0, n = 0;
   // allow whitespace,-,digits,whitespace
