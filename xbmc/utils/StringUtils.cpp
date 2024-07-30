7b31b11d4fa0c7ebc586ee4df4cbf7060f74087e
Markus Härer <markus.haerer@gmx.net>
2024-07-31 01:42:27 +0200

    StringUtils: Make `RemoveCRLF` `noexcept`

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -1510,8 +1510,7 @@ int StringUtils::asciixdigitvalue(char chr)
   return -1;
 }
 
-
-void StringUtils::RemoveCRLF(std::string& strLine)
+void StringUtils::RemoveCRLF(std::string& strLine) noexcept
 {
   StringUtils::TrimRight(strLine, "\n\r");
 }
