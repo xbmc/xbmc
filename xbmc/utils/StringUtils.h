d61c65465a4364d24677526b50e743d3820f83a2
Markus Härer <markus.haerer@gmx.net>
2024-08-03 22:03:08 +0200

    StringUtils: Add `[[nodiscard]]` to `ValidateUUID`

--- xbmc/utils/StringUtils.h
+++ xbmc/utils/StringUtils.h
@@ -361,7 +361,7 @@ public:
   [[nodiscard]] static std::string ISODateToLocalizedDate(std::string_view strIsoDate);
   static void WordToDigits(std::string& word) noexcept;
   [[nodiscard]] static std::string CreateUUID();
-  static bool ValidateUUID(const std::string &uuid); // NB only validates syntax
+  [[nodiscard]] static bool ValidateUUID(const std::string& uuid); // NB only validates syntax
   static double CompareFuzzy(const std::string &left, const std::string &right);
   static int FindBestMatch(const std::string &str, const std::vector<std::string> &strings, double &matchscore);
   static bool ContainsKeyword(const std::string &str, const std::vector<std::string> &keywords);
