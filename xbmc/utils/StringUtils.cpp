0d93ad75b71a9b54e8c1e84ab78facad2405af9d
Markus Härer <markus.haerer@gmx.net>
2024-08-03 21:23:46 +0200

    StringUtils: Add `[[nodiscard]]` to `CreateUUID`

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -1698,7 +1698,7 @@ std::string StringUtils::CreateUUID()
   auto guid = guidGenerator.newGuid();
 
   std::stringstream strGuid; strGuid << guid;
-  return strGuid.str();
+  return std::move(strGuid).str();
 #endif
 }
 
