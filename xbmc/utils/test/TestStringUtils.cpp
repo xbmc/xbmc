b0e7868acaa2f298640d820d3d933326a7c093c8
Markus Härer <markus.haerer@gmx.net>
2024-08-05 01:22:47 +0200

    TestStringUtils: Add missing tests

--- xbmc/utils/test/TestStringUtils.cpp
+++ xbmc/utils/test/TestStringUtils.cpp
@@ -9,6 +9,9 @@
 #include "utils/StringUtils.h"
 
 #include <algorithm>
+#include <limits>
+#include <string>
+#include <string_view>
 
 #include <gtest/gtest.h>
 enum class ECG
@@ -136,6 +139,11 @@ TEST(TestStringUtils, EqualsNoCase)
   EXPECT_TRUE(StringUtils::EqualsNoCase(refstr, "tEsT"));
 }
 
+TEST(TestStringUtils, ReturnDigits)
+{
+  EXPECT_EQ(123456, StringUtils::ReturnDigits("H1el2lo 3Wor4ld56"));
+}
+
 TEST(TestStringUtils, Left)
 {
   std::string refstr, varstr;
@@ -209,6 +217,10 @@ TEST(TestStringUtils, Trim)
   std::string varstr = " test test   ";
   StringUtils::Trim(varstr);
   EXPECT_STREQ(refstr.c_str(), varstr.c_str());
+
+  varstr = "ababtest testabab";
+  StringUtils::Trim(varstr, "ab");
+  EXPECT_STREQ(refstr.c_str(), varstr.c_str());
 }
 
 TEST(TestStringUtils, TrimLeft)
@@ -217,6 +229,11 @@ TEST(TestStringUtils, TrimLeft)
 
   std::string varstr = " test test   ";
   StringUtils::TrimLeft(varstr);
+  EXPECT_STREQ(refstr.c_str(), varstr.c_str());
+
+  refstr = "test testabab";
+  varstr = "ababtest testabab";
+  StringUtils::TrimLeft(varstr, "ab");
   EXPECT_STREQ(refstr.c_str(), varstr.c_str());
 }
 
@@ -227,6 +244,27 @@ TEST(TestStringUtils, TrimRight)
   std::string varstr = " test test   ";
   StringUtils::TrimRight(varstr);
   EXPECT_STREQ(refstr.c_str(), varstr.c_str());
+
+  refstr = "ababtest test";
+  varstr = "ababtest testabab";
+  StringUtils::TrimRight(varstr, "ab");
+  EXPECT_STREQ(refstr.c_str(), varstr.c_str());
+}
+
+TEST(TestStringUtils, RemoveDuplicatedSpacesAndTabs)
+{
+  const std::string refstr = " test test ";
+
+  std::string varstr = "\t\ttest  test \t";
+  StringUtils::RemoveDuplicatedSpacesAndTabs(varstr);
+  EXPECT_STREQ(refstr.c_str(), varstr.c_str());
+}
+
+TEST(TestStringUtils, ReplaceSpecialCharactersWithSpace)
+{
+  const std::string input("a .-_+b,!'c\"\tde/\\f*?g#$%h&@(i)[j]{k}");
+  const std::string output = StringUtils::ReplaceSpecialCharactersWithSpace(input);
+  EXPECT_STREQ(output.c_str(), "a b c de f g h i j k ");
 }
 
 TEST(TestStringUtils, Replace)
@@ -341,6 +379,27 @@ TEST(TestStringUtils, Split)
   EXPECT_STREQ("a bc  d ef ghi ", StringUtils::Split("a bc  d ef ghi ", 'z').at(0).c_str());
 }
 
+TEST(TestStringUtils, SplitMulti)
+{
+  const std::vector<std::string> input{"aaa::bbb", "cc:c##ddd::eee"};
+  const std::vector<std::string> delims{"::", "##"};
+
+  EXPECT_EQ(std::vector<std::string>({"aaa", "bbb", "cc:c", "ddd", "eee"}),
+            StringUtils::SplitMulti(input, delims));
+  EXPECT_EQ(std::vector<std::string>({"aaa::bbb", "cc:c##ddd::eee"}),
+            StringUtils::SplitMulti(input, delims, 1));
+  EXPECT_EQ(std::vector<std::string>({"aaa::bbb", "cc:c##ddd::eee"}),
+            StringUtils::SplitMulti(input, delims, 2));
+  EXPECT_EQ(std::vector<std::string>({"aaa", "bbb", "cc:c##ddd::eee"}),
+            StringUtils::SplitMulti(input, delims, 3));
+  EXPECT_EQ(std::vector<std::string>({"aaa", "bbb", "cc:c##ddd", "eee"}),
+            StringUtils::SplitMulti(input, delims, 4));
+  EXPECT_EQ(std::vector<std::string>({"aaa", "bbb", "cc:c", "ddd", "eee"}),
+            StringUtils::SplitMulti(input, delims, 5));
+  EXPECT_EQ(std::vector<std::string>({"aaa", "bbb", "cc:c", "ddd", "eee"}),
+            StringUtils::SplitMulti(input, delims, 6));
+}
+
 TEST(TestStringUtils, FindNumber)
 {
   EXPECT_EQ(3, StringUtils::FindNumber("aabcaadeaa", "aa"));
@@ -398,6 +457,12 @@ TEST(TestStringUtils, SecondsToTimeString)
   ref = "21:30:55";
   var = StringUtils::SecondsToTimeString(77455);
   EXPECT_STREQ(ref.c_str(), var.c_str());
+}
+
+TEST(TestStringUtils, MillisecondsToTimeString)
+{
+  EXPECT_STREQ(StringUtils::MillisecondsToTimeString(std::chrono::milliseconds(53254549)).c_str(),
+               "14:47:34.549");
 }
 
 TEST(TestStringUtils, IsNaturalNumber)
@@ -551,6 +616,38 @@ TEST(TestStringUtils, FindBestMatch)
   EXPECT_EQ(refdouble, vardouble);
 }
 
+TEST(TestStringUtils, ContainsKeyword)
+{
+  using namespace std::literals::string_literals;
+  const auto str = "alpha beta gamma delta"s;
+  EXPECT_TRUE(StringUtils::ContainsKeyword(str, std::vector{"alpha"s}));
+  EXPECT_TRUE(StringUtils::ContainsKeyword(str, std::vector{"beta"s}));
+  EXPECT_TRUE(StringUtils::ContainsKeyword(str, std::vector{"delta"s, "alpha"s}));
+  EXPECT_TRUE(StringUtils::ContainsKeyword(str, std::vector{"other"s, "beta"s}));
+  EXPECT_TRUE(StringUtils::ContainsKeyword(str, std::vector{"mm"s}));
+  EXPECT_FALSE(StringUtils::ContainsKeyword(str, std::vector{"epsilon"s}));
+  EXPECT_FALSE(StringUtils::ContainsKeyword(str, std::vector{"alphaa"s}));
+  EXPECT_FALSE(StringUtils::ContainsKeyword(str, std::vector{"alpha gamma"s}));
+}
+
+TEST(TestStringUtils, BinaryStringToString)
+{
+  EXPECT_STREQ("abc", StringUtils::BinaryStringToString("\\97\\98\\99").c_str());
+  EXPECT_STREQ("aabbcc", StringUtils::BinaryStringToString("a\\97\\98b\\99c").c_str());
+}
+
+TEST(TestStringUtils, ToHexadecimal)
+{
+  EXPECT_STREQ("", StringUtils::ToHexadecimal("").c_str());
+  EXPECT_STREQ("616263", StringUtils::ToHexadecimal("abc").c_str());
+  std::string a{"a\0b\n", 4};
+  EXPECT_STREQ("6100620a", StringUtils::ToHexadecimal(a).c_str());
+  std::string nul{"\0", 1};
+  EXPECT_STREQ("00", StringUtils::ToHexadecimal(nul).c_str());
+  std::string ff{"\xFF", 1};
+  EXPECT_STREQ("ff", StringUtils::ToHexadecimal(ff).c_str());
+}
+
 TEST(TestStringUtils, Paramify)
 {
   const char *input = "some, very \\ odd \"string\"";
@@ -560,20 +657,66 @@ TEST(TestStringUtils, Paramify)
   EXPECT_STREQ(ref, result.c_str());
 }
 
-TEST(TestStringUtils, sortstringbyname)
+TEST(TestStringUtils, DeParamify)
 {
-  std::vector<std::string> strarray;
-  strarray.emplace_back("B");
-  strarray.emplace_back("c");
-  strarray.emplace_back("a");
-  std::sort(strarray.begin(), strarray.end(), sortstringbyname());
-
-  EXPECT_STREQ("a", strarray[0].c_str());
-  EXPECT_STREQ("B", strarray[1].c_str());
-  EXPECT_STREQ("c", strarray[2].c_str());
+  std::string input("\"some, very \\\\ odd \\\"string\\\"\"");
+  const std::string result = StringUtils::DeParamify(input);
+  EXPECT_STREQ("some, very \\ odd \"string\"", result.c_str());
 }
 
-TEST(TestStringUtils, FileSizeFormat)
+TEST(TestStringUtils, Tokenize)
+{
+  const std::string input("aaa,bbb;ccc,;ddd;");
+  const std::string delims(",;.");
+  const std::vector<std::string> result = StringUtils::Tokenize(input, delims);
+  EXPECT_EQ(4, result.size());
+  EXPECT_STREQ("aaa", result[0].c_str());
+  EXPECT_STREQ("bbb", result[1].c_str());
+  EXPECT_STREQ("ccc", result[2].c_str());
+  EXPECT_STREQ("ddd", result[3].c_str());
+}
+
+TEST(TestStringUtils, ToUint32)
+{
+  EXPECT_EQ(0, StringUtils::ToUint32(""));
+  EXPECT_EQ(0, StringUtils::ToUint32("abc"));
+  EXPECT_EQ(0, StringUtils::ToUint32("abc123"));
+  EXPECT_EQ(0, StringUtils::ToUint32("0"));
+  EXPECT_EQ(0, StringUtils::ToUint32("-0"));
+  EXPECT_EQ(127536, StringUtils::ToUint32("127536"));
+  EXPECT_EQ(127536, StringUtils::ToUint32("127536abc"));
+  EXPECT_EQ(std::numeric_limits<uint32_t>::max(), StringUtils::ToUint32("-1"));
+  EXPECT_EQ(std::numeric_limits<uint32_t>::max(), StringUtils::ToUint32("4294967295"));
+  EXPECT_EQ(std::numeric_limits<uint32_t>::max(), StringUtils::ToUint32("999999999999"));
+}
+
+TEST(TestStringUtils, ToUint64)
+{
+  EXPECT_EQ(0, StringUtils::ToUint64(""));
+  EXPECT_EQ(0, StringUtils::ToUint64("abc"));
+  EXPECT_EQ(0, StringUtils::ToUint64("abc123"));
+  EXPECT_EQ(0, StringUtils::ToUint64("0"));
+  EXPECT_EQ(0, StringUtils::ToUint64("-0"));
+  EXPECT_EQ(127536, StringUtils::ToUint64("127536"));
+  EXPECT_EQ(127536, StringUtils::ToUint64("127536abc"));
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(), StringUtils::ToUint64("-1"));
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(), StringUtils::ToUint64("18446744073709551615"));
+  EXPECT_EQ(std::numeric_limits<uint64_t>::max(),
+            StringUtils::ToUint64("9999999999999999999999999999"));
+}
+
+TEST(TestStringUtils, ToFloat)
+{
+  EXPECT_FLOAT_EQ(0.0, StringUtils::ToFloat(""));
+  EXPECT_FLOAT_EQ(0.0, StringUtils::ToFloat("abc"));
+  EXPECT_FLOAT_EQ(0.0, StringUtils::ToFloat("abc1.0"));
+  EXPECT_FLOAT_EQ(0.0, StringUtils::ToFloat("0.0"));
+  EXPECT_FLOAT_EQ(0.0, StringUtils::ToFloat("-0.0"));
+  EXPECT_FLOAT_EQ(1.5, StringUtils::ToFloat("1.5"));
+  EXPECT_FLOAT_EQ(-1.5, StringUtils::ToFloat("-1.5"));
+}
+
+TEST(TestStringUtils, FormatFileSize)
 {
   EXPECT_STREQ("0B", StringUtils::FormatFileSize(0).c_str());
 
@@ -596,14 +739,23 @@ TEST(TestStringUtils, FileSizeFormat)
   EXPECT_STREQ("5432PB", StringUtils::FormatFileSize(6115888293969133568).c_str());
 }
 
-TEST(TestStringUtils, ToHexadecimal)
+TEST(TestStringUtils, Contains)
 {
-  EXPECT_STREQ("", StringUtils::ToHexadecimal("").c_str());
-  EXPECT_STREQ("616263", StringUtils::ToHexadecimal("abc").c_str());
-  std::string a{"a\0b\n", 4};
-  EXPECT_STREQ("6100620a", StringUtils::ToHexadecimal(a).c_str());
-  std::string nul{"\0", 1};
-  EXPECT_STREQ("00", StringUtils::ToHexadecimal(nul).c_str());
-  std::string ff{"\xFF", 1};
-  EXPECT_STREQ("ff", StringUtils::ToHexadecimal(ff).c_str());
+  EXPECT_TRUE(StringUtils::Contains("abcDEFghi", "dEf"));
+  EXPECT_FALSE(StringUtils::Contains("abcDEFghi", "dEf", false));
+  EXPECT_FALSE(StringUtils::Contains("abcdefg", "cdfg"));
+  EXPECT_FALSE(StringUtils::Contains("abcdefg", "cdfg", false));
+}
+
+TEST(TestStringUtils, sortstringbyname)
+{
+  std::vector<std::string> strarray;
+  strarray.emplace_back("B");
+  strarray.emplace_back("c");
+  strarray.emplace_back("a");
+  std::sort(strarray.begin(), strarray.end(), sortstringbyname());
+
+  EXPECT_STREQ("a", strarray[0].c_str());
+  EXPECT_STREQ("B", strarray[1].c_str());
+  EXPECT_STREQ("c", strarray[2].c_str());
 }
