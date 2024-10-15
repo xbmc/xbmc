0144d5d9472e3f9c603bd3a66af912e75ed50c65
CrystalP <crystalp@kodi.tv>
2024-10-15 19:38:55 -0400

    [Windows] Synchronize dxerr.h/dxerr.cpp with upstream
    - minimized the differences with upstream
    - disabled clang formatting to minimizes diffs with upstream
    - kept the changes required for successful build on desktop and uwp
    - kept the typo fixes
    - removed codes and descriptions for DirectInput and Direct3D9
    - DirectSound messages moved to WinErrorUtils

--- xbmc/platform/win32/dxerr.h
+++ xbmc/platform/win32/dxerr.h
@@ -7,17 +7,18 @@
  *
  *  SPDX-License-Identifier: MIT
  *  See LICENSES/README.md for more information.
+ *
+ *  source https://github.com/microsoft/DXUT/blob/main/Core/dxerr.h
+ *  last synchronized 2024-10-15
  */
+
+// clang-format off
 
 // This version only supports UNICODE.
 
 #pragma once
 
-#if !defined(NOMINMAX)
-#define NOMINMAX
-#endif
-
-#include <windows.h>
+#include <Windows.h>
 #include <sal.h>
 
 #ifdef __cplusplus
@@ -44,15 +45,15 @@ void WINAPI DXGetErrorDescriptionW( _In_ HRESULT hr, _Out_cap_(count) WCHAR* des
 //
 //  Desc:  Outputs a formatted error message to the debug stream
 //
-//  Args:  WCHAR* strFile   The current file, typically passed in using the
+//  Args:  WCHAR* strFile   The current file, typically passed in using the 
 //                         __FILEW__ macro.
-//         DWORD dwLine    The current line number, typically passed in using the
+//         DWORD dwLine    The current line number, typically passed in using the 
 //                         __LINE__ macro.
 //         HRESULT hr      An HRESULT that will be traced to the debug stream.
 //         CHAR* strMsg    A string that will be traced to the debug stream (may be NULL)
 //         BOOL bPopMsgBox If TRUE, then a message box will popup also containing the passed info.
 //
-//  Return: The hr that was passed in.
+//  Return: The hr that was passed in.  
 //--------------------------------------------------------------------------------------
 HRESULT WINAPI DXTraceW( _In_z_ const WCHAR* strFile, _In_ DWORD dwLine, _In_ HRESULT hr, _In_opt_ const WCHAR* strMsg, _In_ bool bPopMsgBox );
 
@@ -64,9 +65,17 @@ HRESULT WINAPI DXTraceW( _In_z_ const WCHAR* strFile, _In_ DWORD dwLine, _In_ HR
 //
 //--------------------------------------------------------------------------------------
 #if defined(DEBUG) || defined(_DEBUG)
+#ifdef _MSC_VER
 #define DXTRACE_MSG(str)              DXTrace( __FILEW__, (DWORD)__LINE__, 0, str, false )
 #define DXTRACE_ERR(str,hr)           DXTrace( __FILEW__, (DWORD)__LINE__, hr, str, false )
 #define DXTRACE_ERR_MSGBOX(str,hr)    DXTrace( __FILEW__, (DWORD)__LINE__, hr, str, true )
+#else
+#define DXUT_PASTE(x, y) x##y
+#define DXUT_MAKEWIDE(x) DXUT_PASTE(L,x)
+#define DXTRACE_MSG(str)              DXTrace( DXUT_MAKEWIDE(__FILE__), (DWORD)__LINE__, 0, str, false )
+#define DXTRACE_ERR(str,hr)           DXTrace( DXUT_MAKEWIDE(__FILE__), (DWORD)__LINE__, hr, str, false )
+#define DXTRACE_ERR_MSGBOX(str,hr)    DXTrace( DXUT_MAKEWIDE(__FILE__), (DWORD)__LINE__, hr, str, true )
+#endif
 #else
 #define DXTRACE_MSG(str)              (0L)
 #define DXTRACE_ERR(str,hr)           (hr)
