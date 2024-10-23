eb704d21c520fef13682823c5287b8635b0e6d83
Yu Xiao <1918256943@qq.com>
2024-10-23 10:08:44 +0800

    SqliteDataset: Fix memory leak in mysql_vmprintf()

--- xbmc/dbwrappers/mysqldataset.cpp
+++ xbmc/dbwrappers/mysqldataset.cpp
@@ -1634,7 +1634,10 @@ std::string MysqlDatabase::mysql_vmprintf(const char* zFormat, va_list ap)
 
   mysqlStrAccumInit(&acc, zBase, sizeof(zBase), MYSQL_MAX_LENGTH);
   mysqlVXPrintf(&acc, 0, zFormat, ap);
-  return mysqlStrAccumFinish(&acc);
+  std::string strResult = mysqlStrAccumFinish(&acc);
+  // Free acc.zText to avoid memory leak.
+  mysqlStrAccumReset(&acc);
+  return strResult;
 }
 
 //************* MysqlDataset implementation ***************
