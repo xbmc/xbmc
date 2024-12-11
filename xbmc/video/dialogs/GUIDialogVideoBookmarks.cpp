2647511618b2a151f45d3941a9e151b4bec48199
CrystalP <crystalp@kodi.tv>
2024-12-11 09:33:57 -0500

    [guilib] Catch database open errors in bookmarks dialog

--- xbmc/video/dialogs/GUIDialogVideoBookmarks.cpp
+++ xbmc/video/dialogs/GUIDialogVideoBookmarks.cpp
@@ -216,7 +216,9 @@ void CGUIDialogVideoBookmarks::Delete(int item)
 void CGUIDialogVideoBookmarks::Delete(const CBookmark& bm)
 {
   CVideoDatabase videoDatabase;
-  videoDatabase.Open();
+  if (!videoDatabase.Open())
+    return;
+
   const std::string path{g_application.CurrentFileItem().GetDynPath()};
   videoDatabase.ClearBookMarkOfFile(path, bm, bm.type);
   videoDatabase.Close();
@@ -233,7 +235,9 @@ void CGUIDialogVideoBookmarks::OnRefreshList()
   m_filePath = g_application.CurrentFileItem().GetDynPath();
 
   CVideoDatabase videoDatabase;
-  videoDatabase.Open();
+  if (!videoDatabase.Open())
+    return;
+
   videoDatabase.GetBookMarksForFile(m_filePath, m_bookmarks);
   videoDatabase.GetBookMarksForFile(m_filePath, m_bookmarks, CBookmark::EPISODE, true);
   videoDatabase.Close();
@@ -317,7 +321,8 @@ void CGUIDialogVideoBookmarks::OnRefreshList()
 void CGUIDialogVideoBookmarks::Update()
 {
   CVideoDatabase videoDatabase;
-  videoDatabase.Open();
+  if (!videoDatabase.Open())
+    return;
 
   if (g_application.CurrentFileItem().HasVideoInfoTag() && g_application.CurrentFileItem().GetVideoInfoTag()->m_iEpisode > -1)
   {
@@ -377,7 +382,9 @@ void CGUIDialogVideoBookmarks::GotoBookmark(int item)
 void CGUIDialogVideoBookmarks::ClearBookmarks()
 {
   CVideoDatabase videoDatabase;
-  videoDatabase.Open();
+  if (!videoDatabase.Open())
+    return;
+
   const std::string path{g_application.CurrentFileItem().GetDynPath()};
   videoDatabase.ClearBookMarksOfFile(path, CBookmark::STANDARD);
   videoDatabase.ClearBookMarksOfFile(path, CBookmark::RESUME);
@@ -388,7 +395,6 @@ void CGUIDialogVideoBookmarks::ClearBookmarks()
 
 bool CGUIDialogVideoBookmarks::AddBookmark(CVideoInfoTag* tag)
 {
-  CVideoDatabase videoDatabase;
   CBookmark bookmark;
   bookmark.timeInSeconds = (int)g_application.GetTime();
   bookmark.totalTimeInSeconds = (int)g_application.GetTotalTime();
@@ -486,7 +492,10 @@ bool CGUIDialogVideoBookmarks::AddBookmark(CVideoInfoTag* tag)
 
   free(pixels);
 
-  videoDatabase.Open();
+  CVideoDatabase videoDatabase;
+  if (!videoDatabase.Open())
+    return false;
+
   if (tag)
     videoDatabase.AddBookMarkForEpisode(*tag, bookmark);
   else
@@ -525,7 +534,9 @@ bool CGUIDialogVideoBookmarks::AddEpisodeBookmark()
 {
   std::vector<CVideoInfoTag> episodes;
   CVideoDatabase videoDatabase;
-  videoDatabase.Open();
+  if (!videoDatabase.Open())
+    return false;
+
   videoDatabase.GetEpisodesByFile(g_application.CurrentFile(), episodes);
   videoDatabase.Close();
   if (!episodes.empty())
@@ -573,7 +584,8 @@ bool CGUIDialogVideoBookmarks::OnAddEpisodeBookmark()
   if (g_application.CurrentFileItem().HasVideoInfoTag() && g_application.CurrentFileItem().GetVideoInfoTag()->m_iEpisode > -1)
   {
     CVideoDatabase videoDatabase;
-    videoDatabase.Open();
+    if (!videoDatabase.Open())
+      return bReturn;
     std::vector<CVideoInfoTag> episodes;
     videoDatabase.GetEpisodesByFile(g_application.CurrentFile(),episodes);
     if (episodes.size() > 1)
