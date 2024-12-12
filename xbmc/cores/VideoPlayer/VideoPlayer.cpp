772f9b3996400840b60d6ca285849d18fe3606ed
thexai <58434170+thexai@users.noreply.github.com>
2024-12-12 17:13:06 +0100

    [VideoPlayer] fix conditions for default enable forced PGS/VobSub subtitles

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -973,6 +973,12 @@ void CVideoPlayer::OpenDefaultStreams(bool reset)
         visible = false;
       else if(stream.flags & StreamFlags::FLAG_FORCED)
         visible = true;
+
+      // It's necessary keep the stream open, even if the subtitle track is not visible, to analyze
+      // possible forced PGS / VobSub in same language as audio, otherwise the stream is closed.
+      if (!visible && !g_LangCodeExpander.CompareISO639Codes(stream.language, as.language))
+        valid = false;
+
       break;
     }
   }
@@ -2933,6 +2939,12 @@ void CVideoPlayer::HandleMessages()
         SetEnableStream(m_CurrentSubtitle, false);
 
       SetSubtitleVisibleInternal(isVisible);
+
+      const auto& ss = m_SelectionStreams.Get(STREAM_SUBTITLE, GetSubtitle());
+      const auto& as = m_SelectionStreams.Get(STREAM_AUDIO, GetAudioStream());
+
+      if (!isVisible && !g_LangCodeExpander.CompareISO639Codes(ss.language, as.language))
+        CloseStream(m_CurrentSubtitle, false);
     }
     else if (pMsg->IsType(CDVDMsg::PLAYER_SET_PROGRAM))
     {
