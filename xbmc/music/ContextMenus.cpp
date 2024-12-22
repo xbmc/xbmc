1e53a0af1910018db27e44e2dc8d3ccd6364292b
ksooo <3226626+ksooo@users.noreply.github.com>
2024-12-22 17:47:43 +0100

    [music] Add CONTEXTMENU::CMusicInfo to support items with media type 'music' and non-empty music items with empty media type.

--- xbmc/music/ContextMenus.cpp
+++ xbmc/music/ContextMenus.cpp
@@ -14,6 +14,7 @@
 #include "cores/playercorefactory/PlayerCoreFactory.h"
 #include "guilib/GUIComponent.h"
 #include "guilib/GUIWindowManager.h"
+#include "music/MusicFileItemClassify.h"
 #include "music/MusicUtils.h"
 #include "music/dialogs/GUIDialogMusicInfo.h"
 #include "playlists/PlayListTypes.h"
@@ -44,6 +45,18 @@ bool CMusicInfoBase::Execute(const std::shared_ptr<CFileItem>& item) const
 {
   CGUIDialogMusicInfo::ShowFor(item.get());
   return true;
+}
+
+bool CMusicInfo::IsVisible(const CFileItem& item) const
+{
+  if (CMusicInfoBase::IsVisible(item))
+    return true;
+
+  if (item.m_bIsFolder)
+    return false;
+
+  const auto* tag{item.GetMusicInfoTag()};
+  return tag && tag->GetType() == MediaTypeNone && !tag->GetTitle().empty() && MUSIC::IsAudio(item);
 }
 
 bool CMusicBrowse::IsVisible(const CFileItem& item) const
