d816dde0794a080f8fe61feb08598c0349223b9a
78andyp <drandypowell@live.co.uk>
2024-12-29 21:58:07 +0000

    [Backport][VideoInfoScanner] Fix #25662 - save all tv show and episode actor images in tv show .actors folder.

--- xbmc/video/VideoDatabase.h
+++ xbmc/video/VideoDatabase.h
@@ -904,7 +904,11 @@ public:
   void UpdateFileDateAdded(CVideoInfoTag& details);
 
   void ExportToXML(const std::string &path, bool singleFile = true, bool images=false, bool actorThumbs=false, bool overwrite=false);
-  void ExportActorThumbs(const std::string &path, const CVideoInfoTag& tag, bool singleFiles, bool overwrite=false);
+  void ExportActorThumbs(const std::string& path,
+                         const CVideoInfoTag& tag,
+                         bool singleFiles,
+                         bool overwrite = false,
+                         const std::string& tvshowDir = "") const;
   void ImportFromXML(const std::string &path);
   void DumpToDummyFiles(const std::string &path);
   bool ImportArtFromXML(const TiXmlNode *node, std::map<std::string, std::string> &artwork);
