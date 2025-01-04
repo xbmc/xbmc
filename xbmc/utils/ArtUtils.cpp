627b2629b750dd7f44fd88e8ca808940b8ae608a
78andyp <drandypowell@live.co.uk>
2025-01-04 20:37:52 +0000

    Import multi-episode files (including blurays).
    Ensures the bluray:// paths are correctly translated to/from real file paths.
    
    Also remove redundant code in CNfoFile as episodes now have individual .nfo files.

--- xbmc/utils/ArtUtils.cpp
+++ xbmc/utils/ArtUtils.cpp
@@ -204,7 +204,9 @@ std::string GetLocalArt(const CFileItem& item, const std::string& artFile, bool 
   return "";
 }
 
-std::string GetLocalArtBaseFilename(const CFileItem& item, bool& useFolder)
+std::string GetLocalArtBaseFilename(const CFileItem& item,
+                                    bool& useFolder,
+                                    UseSeasonAndEpisode useSeasonAndEpisode /* = no */)
 {
   std::string strFile;
   if (item.IsStack())
@@ -228,9 +230,24 @@ std::string GetLocalArtBaseFilename(const CFileItem& item, bool& useFolder)
     strFile = CMultiPathDirectory::GetFirstPath(item.GetPath());
 
   if (item.IsOpticalMediaFile())
-  { // optical media files should be treated like folders
-    useFolder = true;
+  {
+    // Optical media files should be treated like folders
+    useFolder = true; // ByRef so changes behaviour in GetLocalArt()
     strFile = item.GetLocalMetadataPath();
+  }
+
+  if (useSeasonAndEpisode == UseSeasonAndEpisode::YES && !URIUtils::GetFileName(file).empty() &&
+      item.HasVideoInfoTag())
+  {
+    // Note this is an exception to the optical media rule above - episode art files will be stored alongside the episode .nfo
+    const CVideoInfoTag* tag{item.GetVideoInfoTag()};
+    if (tag->m_iSeason > -1 && tag->m_iEpisode > -1)
+    {
+      std::string baseFile{file};
+      URIUtils::RemoveExtension(baseFile);
+      strFile = fmt::format("{}-S{:02}E{:02}{}", baseFile, tag->m_iSeason, tag->m_iEpisode,
+                            URIUtils::GetExtension(file));
+    }
   }
   else if (URIUtils::IsBlurayPath(file))
   {
