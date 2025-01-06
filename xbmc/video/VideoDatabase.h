d8747d086c95e8ff2748e739271ff47405765c8a
78andyp <drandypowell@live.co.uk>
2025-01-06 20:32:41 +0000

    Add ability to additional playlists from Bluray source and add them as additional video versions.
    Uses the simple menu dialog and select versions dialog and then creates a new videoversion table entry.

--- xbmc/video/VideoDatabase.h
+++ xbmc/video/VideoDatabase.h
@@ -1116,6 +1116,11 @@ public:
                              int dbIdTarget,
                              int idVideoVersion,
                              VideoAssetType assetType);
+  bool AddVideoVersion(VideoDbContentType itemType,
+                       int dbIdSource,
+                       int idFile,
+                       int idVideoVersion,
+                       VideoAssetType assetType);
   void SetDefaultVideoVersion(VideoDbContentType itemType, int dbId, int idFile);
   void SetVideoVersion(int idFile, int idVideoVersion);
   int AddVideoVersionType(const std::string& typeVideoVersion,
