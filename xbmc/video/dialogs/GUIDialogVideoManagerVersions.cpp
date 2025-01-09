66a1bc1ef05b1e44987d8ca7d1b6db33800af681
78andyp <drandypowell@live.co.uk>
2025-01-09 18:59:45 +0000

    Get stream details at playlist selection.
    Get streamdetails from BLURAY_TITLE_INFO during playlist selection. These may not be completely accurate so ensure these are then updated when the playlist is actually played.

--- xbmc/video/dialogs/GUIDialogVideoManagerVersions.cpp
+++ xbmc/video/dialogs/GUIDialogVideoManagerVersions.cpp
@@ -347,8 +347,22 @@ bool CGUIDialogVideoManagerVersions::ChoosePlaylist(const std::shared_ptr<CFileI
       bool videoDbSuccess{false};
       m_database.BeginTransaction();
       if (replaceExistingFile == ReplaceExistingFile::YES)
+      {
         videoDbSuccess = m_database.SetFileForMedia(item->GetDynPath(), item->GetVideoContentType(),
                                                     idMovie, item->GetVideoInfoTag()->m_iFileId);
+        if (videoDbSuccess)
+        {
+          m_database.SetStreamDetailsForFile(item->GetVideoInfoTag()->m_streamDetails,
+                                             item->GetDynPath());
+
+          // Notify all windows to update the file item
+          std::shared_ptr<CFileItem> oldItem{item};
+          oldItem->SetPath(oldPath);
+          CGUIMessage msg{GUI_MSG_NOTIFY_ALL,        0,      0, GUI_MSG_UPDATE_ITEM,
+                          GUI_MSG_FLAG_FORCE_UPDATE, oldItem};
+          CServiceBroker::GetGUI()->GetWindowManager().SendMessage(msg);
+        }
+      }
       else
       {
         // Choose a video version for the video
@@ -360,6 +374,7 @@ bool CGUIDialogVideoManagerVersions::ChoosePlaylist(const std::shared_ptr<CFileI
         if (idFile > 0)
         {
           videoDbSuccess = true;
+          m_database.SetStreamDetailsForFileId(item->GetVideoInfoTag()->m_streamDetails, idFile);
           if (m_database.AddVideoVersion(item->GetVideoContentType(), idMovie, idFile,
                                          idVideoVersion, VideoAssetType::VERSION) < 0)
             return false;
