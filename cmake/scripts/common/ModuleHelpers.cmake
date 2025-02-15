5e6e0e0a9dc145f919b98424c63fc67dccc3c0b7
fuzzard <fuzzard@kodi.tv>
2025-02-15 19:56:54 +1000

    [cmake] Modulehelper dont append DEP_BUILDENV when _LIB_TYPE native

--- cmake/scripts/common/ModuleHelpers.cmake
+++ cmake/scripts/common/ModuleHelpers.cmake
@@ -320,7 +320,10 @@ macro(BUILD_DEP_TARGET)
   endif()
 
   if(CONFIGURE_COMMAND)
-    if(NOT CMAKE_ARGS AND DEP_BUILDENV)
+    # DEP_BUILDENV is potentially populated in a toolchain file. We dont want to use it
+    # for host tool builds, so make sure to check _LIB_TYPE
+    if(NOT CMAKE_ARGS AND
+      (DEP_BUILDENV AND NOT ${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}_LIB_TYPE STREQUAL "native"))
       # DEP_BUILDENV only used for non cmake externalproject_add builds
       # iterate through CONFIGURE_COMMAND looking for multiple COMMAND, we need to
       # add DEP_BUILDENV for each distinct COMMAND
