857b61b64f1302af5df8a8dca86f3312151ef205
78andyp <drandypowell@live.co.uk>
2025-02-24 14:09:29 +0000

    Add tests.

--- xbmc/test/TestFileItem.cpp
+++ xbmc/test/TestFileItem.cpp
@@ -56,20 +56,32 @@ TEST_P(TestFileItemBasePath, GetBaseMoviePath)
   EXPECT_EQ(compare, path);
 }
 
-const TestFileData BaseMovies[] = {{ "c:\\dir\\filename.avi", false, "c:\\dir\\filename.avi" },
-                                   { "c:\\dir\\filename.avi", true,  "c:\\dir\\" },
-                                   { "/dir/filename.avi", false, "/dir/filename.avi" },
-                                   { "/dir/filename.avi", true,  "/dir/" },
-                                   { "smb://somepath/file.avi", false, "smb://somepath/file.avi" },
-                                   { "smb://somepath/file.avi", true, "smb://somepath/" },
-                                   { "stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi", false, "stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi" },
-                                   { "stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi", true,  "/path/to/movie_name/" },
-                                   { "/home/user/TV Shows/Dexter/S1/1x01.avi", false, "/home/user/TV Shows/Dexter/S1/1x01.avi" },
-                                   { "/home/user/TV Shows/Dexter/S1/1x01.avi", true, "/home/user/TV Shows/Dexter/S1/" },
-                                   { "zip://g%3a%5cmultimedia%5cmovies%5cSphere%2ezip/Sphere.avi", true, "g:\\multimedia\\movies\\" },
-                                   { "/home/user/movies/movie_name/video_ts/VIDEO_TS.IFO", false, "/home/user/movies/movie_name/" },
-                                   { "/home/user/movies/movie_name/video_ts/VIDEO_TS.IFO", true, "/home/user/movies/movie_name/" },
-                                   { "/home/user/movies/movie_name/BDMV/index.bdmv", false, "/home/user/movies/movie_name/" },
-                                   { "/home/user/movies/movie_name/BDMV/index.bdmv", true, "/home/user/movies/movie_name/" }};
+const TestFileData BaseMovies[] = {
+    {"c:\\dir\\filename.avi", false, "c:\\dir\\filename.avi"},
+    {"c:\\dir\\filename.avi", true, "c:\\dir\\"},
+    {"/dir/filename.avi", false, "/dir/filename.avi"},
+    {"/dir/filename.avi", true, "/dir/"},
+    {"smb://somepath/file.avi", false, "smb://somepath/file.avi"},
+    {"smb://somepath/file.avi", true, "smb://somepath/"},
+    {"stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi",
+     false,
+     "stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi"},
+    {"stack:///path/to/movie_name/cd1/some_file1.avi , /path/to/movie_name/cd2/some_file2.avi",
+     true, "/path/to/movie_name/"},
+    {"/home/user/TV Shows/Dexter/S1/1x01.avi", false, "/home/user/TV Shows/Dexter/S1/1x01.avi"},
+    {"/home/user/TV Shows/Dexter/S1/1x01.avi", true, "/home/user/TV Shows/Dexter/S1/"},
+    {"zip://g%3a%5cmultimedia%5cmovies%5cSphere%2ezip/Sphere.avi", true,
+     "g:\\multimedia\\movies\\"},
+    {"/home/user/movies/movie_name/video_ts/VIDEO_TS.IFO", false, "/home/user/movies/movie_name/"},
+    {"/home/user/movies/movie_name/video_ts/VIDEO_TS.IFO", true, "/home/user/movies/movie_name/"},
+    {"/home/user/movies/movie_name/BDMV/index.bdmv", false, "/home/user/movies/movie_name/"},
+    {"/home/user/movies/movie_name/BDMV/index.bdmv", true, "/home/user/movies/movie_name/"},
+    {"/home/user/movies/movie_name/disc 1/BDMV/index.bdmv", false, "/home/user/movies/movie_name/"},
+    {"/home/user/movies/movie_name/disc 1/BDMV/index.bdmv", true, "/home/user/movies/movie_name/"},
+    {"bluray://udf%3a%2f%2fsmb%253a%252f%252fsomepath%252fmovie.iso%2f/BDMV/PLAYLIST/00800.mpls",
+     true, "smb://somepath/"},
+    {"bluray://smb%3a%2f%2fsomepath%2fDisc%201%2f/BDMV/PLAYLIST/00800.mpls", true,
+     "smb://somepath/"},
+    {"zip://smb%3a%2f%2fsomepath%2fmovie.zip/BDMV/index.bdmv", true, "smb://somepath/"}};
 
 INSTANTIATE_TEST_SUITE_P(BaseNameMovies, TestFileItemBasePath, ValuesIn(BaseMovies));
