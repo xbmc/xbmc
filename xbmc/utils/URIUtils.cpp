dbf1a07cb237686a57bee260b999b65b8587e0b1
78andyp <drandypowell@live.co.uk>
2025-02-24 00:20:52 +0000

    Fix GetMovieName() and GetMovieBasePath() to better handle optical media, bluray:// paths and Disc n subdirectories.

--- xbmc/utils/URIUtils.cpp
+++ xbmc/utils/URIUtils.cpp
@@ -17,6 +17,7 @@
 #include "filesystem/MultiPathDirectory.h"
 #include "filesystem/SpecialProtocol.h"
 #include "filesystem/StackDirectory.h"
+#include "guilib/LocalizeStrings.h"
 #include "network/DNSNameCache.h"
 #include "network/Network.h"
 #include "pvr/channels/PVRChannelsPath.h"
@@ -579,6 +580,16 @@ std::string URIUtils::GetBlurayPath(const std::string& path)
   }
 
   return newPath;
+}
+
+std::string URIUtils::GetTrailingPartNumberRegex()
+{
+  // Build regex inserting local specific spelling of disc (xxx)
+  // \/?:cd|dvd|xxx|dis[ck][ _.-]*([0-9]+)$
+  std::string localeDiscStr{StringUtils::Format("{} ", g_localizeStrings.Get(427))};
+  if (!localeDiscStr.empty())
+    localeDiscStr += "|";
+  return {R"([\\\/](?:cd|dvd|)" + localeDiscStr + R"(dis[ck])[ _.-]*([0-9]+)$)"};
 }
 
 std::string URLEncodePath(const std::string& strPath)
