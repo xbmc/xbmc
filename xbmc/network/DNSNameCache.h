d1f239db5d4f659b64e71ddd884271afe668dbb7
Markus Härer <markus.haerer@gmx.net>
2025-03-13 22:29:00 +0100

    DNSNameCache: Add Doxygen comments

--- xbmc/network/DNSNameCache.h
+++ xbmc/network/DNSNameCache.h
@@ -18,9 +18,39 @@
 class CDNSNameCache final
 {
 public:
+  /*!
+   * \brief Add the IP for a hostname to the cache. From the time of adding it stays in the cache for \ref TTL
+   *
+   * \param strHostName The hostname
+   * \param strIpAddress The IP for that hostname. Can be IPv4 or IPv6
+   */
   static void Add(const std::string& strHostName, const std::string& strIpAddress);
+  /*!
+   * \brief Add the IP for a hostname to the cache indefinitely
+   *
+   * \param strHostName The hostname
+   * \param strIpAddress The IP for that hostname. Can be IPv4 or IPv6
+   */
   static void AddPermanent(const std::string& strHostName, const std::string& strIpAddress);
+  /*!
+   * \brief Get an IP for a hostname from the cache
+   *
+   * \param strHostName The hostname to look up
+   * \param[out] strIpAddress Contains the IP for the hostname if the info is in the cache, otherwise unchanged
+   *
+   * \return true if the IP is cached
+   */
   static bool GetCached(const std::string& strHostName, std::string& strIpAddress);
+  /*!
+   * \brief Get the IP for the hostname from the cache or query it form the DNS
+   *
+   * If a successful DNS query was performed the result is added to the cache for the duration of \ref TTL
+   *
+   * \param strHostName The hostname to look up
+   * \param[out] strIpAddress Contains the IP for the hostname if the info can be provided, otherwise unchanged
+   *
+   * \return true if the IP is cached or the DNS query was successful
+   */
   static bool Lookup(const std::string& strHostName, std::string& strIpAddress);
 
 private:
