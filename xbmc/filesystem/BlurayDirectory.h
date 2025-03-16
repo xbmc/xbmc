4dcb23f2a054b5f95748a76a331a0b78ff525c5c
78andyp <drandypowell@live.co.uk>
2025-03-16 12:36:41 +0000

    Implements cache of bluray disc and playlist information.

--- xbmc/filesystem/BlurayDirectory.h
+++ xbmc/filesystem/BlurayDirectory.h
@@ -14,6 +14,8 @@
 
 #include <memory>
 
+#include <libbluray/bluray.h>
+
 class CFileItem;
 class CFileItemList;
 
@@ -26,7 +28,7 @@ namespace XFILE
 class CBlurayDirectory : public IDirectory
 {
 public:
-  CBlurayDirectory() = default;
+  CBlurayDirectory();
   ~CBlurayDirectory() override;
   bool GetDirectory(const CURL& url, CFileItemList& items) override;
 
@@ -43,12 +45,18 @@ private:
   };
 
   void Dispose();
-  std::string GetDiscInfoString(DiscInfo info) const;
+  std::string GetDiscInfoString(DiscInfo info);
   bool GetPlaylists(GetTitlesJob job, CFileItemList& items, SortTitlesJob sort) const;
   void GetPlaylists(ClipMap& clips, PlaylistMap& playlists) const;
   int GetMainPlaylistFromDisc() const;
-  std::shared_ptr<CFileItem> GetFileItem(const BLURAY_TITLE_INFO* title,
+  std::shared_ptr<CFileItem> GetFileItem(const BLURAY_TITLE_INFO& title,
                                          const std::string& label) const;
+
+  std::string GetCachePath() const;
+  const BLURAY_DISC_INFO* GetDiscInfo() const;
+  int GetNumberOfTitlesFromDisc() const;
+  bool GetPlaylistInfoFromDisc(unsigned int playlist, BLURAY_TITLE_INFO& p) const;
+  bool GetTitleInfoFromDisc(unsigned int title, BLURAY_TITLE_INFO& p) const;
 
   CURL m_url;
   std::string m_realPath;
---
+++ xbmc/filesystem/BlurayDiscCache.h
@@ -0,0 +1,62 @@
+/*
+ *  Copyright (C) 2005-2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "DiscDirectoryHelper.h"
+#include "threads/CriticalSection.h"
+
+#include <map>
+
+#include <libbluray/bluray.h>
+
+struct Disc
+{
+  std::map<unsigned int, BLURAY_TITLE_INFO> m_playlists;
+  std::map<unsigned int, unsigned int> m_titleMap;
+
+  bool m_mapsSet{false};
+  PlaylistMap m_playlistmap;
+  ClipMap m_clipmap;
+};
+
+using CacheMapEntry = std::pair<std::string, Disc>;
+using CacheMap = std::map<std::string, Disc>;
+
+namespace XFILE
+{
+class CBlurayDiscCache
+{
+public:
+  CBlurayDiscCache();
+  virtual ~CBlurayDiscCache();
+  void Clear();
+
+  CacheMap::iterator SetDisc(const std::string& path);
+  void SetTitleInfo(const std::string& path,
+                    unsigned int title,
+                    const BLURAY_TITLE_INFO* playlistInfo);
+  void SetPlaylistInfo(const std::string& path,
+                       unsigned int playlist,
+                       const BLURAY_TITLE_INFO* playlistInfo);
+  void SetMaps(const std::string& path, const PlaylistMap& playlistmap, const ClipMap& clipmap);
+
+  bool GetPlaylistInfo(const std::string& path,
+                       unsigned int playlist,
+                       BLURAY_TITLE_INFO& playlistInfo);
+  bool GetTitleInfo(const std::string& path, unsigned int title, BLURAY_TITLE_INFO& playlistInfo);
+  bool GetMaps(const std::string& path, PlaylistMap& playlistmap, ClipMap& clipmap);
+
+  void ClearDisc(const std::string& path);
+
+protected:
+  CacheMap m_cache;
+
+  mutable CCriticalSection m_cs;
+};
+} // namespace XFILE
