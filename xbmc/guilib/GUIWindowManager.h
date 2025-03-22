f5cf4b7e0b51d8e80393d71d13dc4a5b4cdd5456
ksooo <kai.sommerfeld@gmx.com>
2025-03-22 18:58:12 +0100

    [guilib] CGUIWindowManager::SendThreadMessage: make param message a const reference.

--- xbmc/guilib/GUIWindowManager.h
+++ xbmc/guilib/GUIWindowManager.h
@@ -192,7 +192,7 @@ public:
    */
   int GetTopmostModalDialog(bool ignoreClosing = false) const;
 
-  void SendThreadMessage(CGUIMessage& message, int window = 0);
+  void SendThreadMessage(const CGUIMessage& message, int window = 0);
   void DispatchThreadMessages();
   // method to removed queued messages with message id in the requested message id list.
   // pMessageIDList: point to first integer of a 0 ends integer array.
