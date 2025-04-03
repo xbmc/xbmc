ddd5c8496174c3ac335d446cabc3f22f0e89ac7d
Portisch <hugo.portisch@yahoo.de>
2025-04-03 11:01:23 +0200

    Peripheral: call also adapter OnSettingChanged

--- xbmc/peripherals/dialogs/GUIDialogPeripheralSettings.cpp
+++ xbmc/peripherals/dialogs/GUIDialogPeripheralSettings.cpp
@@ -124,6 +124,8 @@ void CGUIDialogPeripheralSettings::OnSettingChanged(const std::shared_ptr<const 
     if (controller)
       peripheral->SetControllerProfile(controller);
   }
+
+  peripheral->OnSettingChanged(settingId);
 }
 
 bool CGUIDialogPeripheralSettings::Save()
