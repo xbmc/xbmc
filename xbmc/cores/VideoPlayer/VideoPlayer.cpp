673ea414aecf6266073d6a1d7ccdd9287df356b8
CrystalP <crystalp@kodi.tv>
2025-04-18 15:33:24 -0400

    [VideoPlayer] Use shared function for DVD audio channel layout

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -558,12 +558,14 @@ void CSelectionStreams::Update(const std::shared_ptr<CDVDInputStream>& input,
       {
         s.codecDesc = aStream->GetStreamType();
         s.channels = aStream->iChannels;
+        s.channelLayout = aStream->m_channelLayoutName;
         s.bitrate = aStream->iBitRate;
       }
       else
       {
         s.codecDesc = info.codecDesc;
         s.channels = info.channels;
+        s.channelLayout = info.channelLayout;
       }
       s.language = g_LangCodeExpander.ConvertToISO6392B(info.language);
       s.flags = info.flags;
