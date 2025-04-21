f664dc2bfd37ee6184f7ce16ae48c57df1ba5f52
fuzzard <fuzzard@kodi.tv>
2025-04-21 10:33:02 +1000

    [tools/depends][target] Bump smctemp 0.6.0
    smctemp had an api break with 0.4.0. We now require the version to be newer
    than 0.4.0

--- cmake/modules/FindSmctemp.cmake
+++ cmake/modules/FindSmctemp.cmake
@@ -9,10 +9,24 @@
 
 if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
 
+  set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC smctemp)
+
+  SETUP_FIND_SPECS()
+
   find_path(SMCTEMP_INCLUDE_DIR NAMES smctemp.h
                                 HINTS ${DEPENDS_PATH}/include)
-  find_library(SMCTEMP_LIBRARY NAMES smctemp
-                               HINTS ${DEPENDS_PATH}/lib)
+
+  if(EXISTS ${SMCTEMP_INCLUDE_DIR}/smctemp.h)
+    file(STRINGS ${SMCTEMP_INCLUDE_DIR}/smctemp.h _smc_version_str
+         REGEX ".*kVersion\[\] = \"[0-9.]+\".*")
+    string(REGEX REPLACE ".*kVersion.*[\"](.*)[\"].*" "\\1" SMCTEMP_VERSION ${_smc_version_str})
+    unset(_smc_version_str)
+
+    if(SMCTEMP_VERSION VERSION_GREATER_EQUAL ${CONFIG_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC})
+      find_library(SMCTEMP_LIBRARY NAMES smctemp
+                                   HINTS ${DEPENDS_PATH}/lib)
+    endif()
+  endif()
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(Smctemp
