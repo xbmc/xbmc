6d8eb3660a7a3a4be894008d779361b0e9c9521b
cpm <162544069+cpm-code@users.noreply.github.com>
2025-04-27 17:45:17 +0800

    AgedMap: find closest match timestamp from stream_type_stream inconsistent resulting in the pts being adjusted in xbmc but with slightly different value to original pts - thus need to find closest here.

--- xbmc/utils/AgedMap.h
+++ xbmc/utils/AgedMap.h
@@ -2,6 +2,7 @@
 
 #include <unordered_map>
 #include <deque>
+#include <limits>
 
 template<typename K, typename V, size_t MaxSize = 512>
 class AgedMap {
@@ -38,12 +39,27 @@ void AgedMap<K, V, MaxSize>::insert(K key, V value) {
 
 template<typename K, typename V, size_t MaxSize>
 auto AgedMap<K, V, MaxSize>::findOrLatest(const K& key) const {
-  auto it = map.find(key);
+  auto exact = map.find(key);
 
-  if (it != map.end())
-    return it;
-  else if (hasEntries)
-    return map.find(mostRecentKey);
-  else
-    return map.end();
+  if (exact != map.end()) return exact;
+  if (!hasEntries) return map.end();
+
+  auto closest = map.end();
+  typename std::unordered_map<K, V>::size_type minDistance =
+      std::numeric_limits<typename std::unordered_map<K, V>::size_type>::max();
+
+  for (auto it = map.begin(); it != map.end(); ++it) {
+    typename std::unordered_map<K, V>::size_type distance;
+    if (it->first > key)
+      distance = it->first - key;
+    else
+      distance = key - it->first;
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      closest = it;
+    }
+  }
+
+  return closest;
 }
