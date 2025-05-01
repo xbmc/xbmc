78andyp/scrape_disc

Fix retrieval of basepath.
Looking at the existing code basepath is different to the actual path of the media file, it is where the actual physical file resides (eg. for an archive:// path the basepath is the path in which the archive file itself resides not the media contained therein which the archive:// path+file points to, and for BD file structure its the movie directory not the BDMV directory with index.BDMV).

xbmc/utils/URIUtils.cpp
@@ -13,6 +13,7 @@
 #include "ServiceBroker.h"
 #include "StringUtils.h"
 #include "URL.h"
+#include "filesystem/BlurayFile.h"
 #include "filesystem/MultiPathDirectory.h"
 #include "filesystem/SpecialProtocol.h"
 #include "filesystem/StackDirectory.h"
@@ -440,11 +441,16 @@ std::string URIUtils::GetBasePath(const std::string& strPath)
     strCheck = CStackDirectory::GetFirstStackedFile(strPath);
 
   std::string strDirectory = GetDirectory(strCheck);
+
   if (IsInRAR(strCheck))
   {
     std::string strPath=strDirectory;
     GetParentPath(strPath, strDirectory);
   }
+
+  if (IsBlurayPath(strCheck))
+    strDirectory = CBlurayFile::GetBasePath(CURL(strPath));
+
   if (IsStack(strPath))
   {
     strCheck = strDirectory;
@@ -452,6 +458,7 @@ std::string URIUtils::GetBasePath(const std::string& strPath)
     if (GetFileName(strCheck).size() == 3 && StringUtils::StartsWithNoCase(GetFileName(strCheck), "cd"))
       strDirectory = GetDirectory(strCheck);
   }
+
   return strDirectory;
 }
 

xbmc/xbmc

xbmc/utils/URIUtils.cpp
@@ -13,6 +13,7 @@
 #include "ServiceBroker.h"
 #include "StringUtils.h"
 #include "URL.h"
+#include "filesystem/BlurayDirectory.h"
 #include "filesystem/MultiPathDirectory.h"
 #include "filesystem/SpecialProtocol.h"
 #include "filesystem/StackDirectory.h"
@@ -454,11 +455,16 @@ std::string URIUtils::GetBasePath(const std::string& strPath)
     strCheck = CStackDirectory::GetFirstStackedFile(strPath);
 
   std::string strDirectory = GetDirectory(strCheck);
+
   if (IsInRAR(strCheck))
   {
     std::string strPath=strDirectory;
     GetParentPath(strPath, strDirectory);
   }
+
+  if (IsBlurayPath(strCheck))
+    strDirectory = CBlurayDirectory::GetBasePath(CURL(strPath));
+
   if (IsStack(strPath))
   {
     strCheck = strDirectory;
@@ -466,6 +472,7 @@ std::string URIUtils::GetBasePath(const std::string& strPath)
     if (GetFileName(strCheck).size() == 3 && StringUtils::StartsWithNoCase(GetFileName(strCheck), "cd"))
       strDirectory = GetDirectory(strCheck);
   }
+
   return strDirectory;
 }
 