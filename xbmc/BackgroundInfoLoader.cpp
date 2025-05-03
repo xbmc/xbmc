7b09be5131221c4401a9f50587d230b8deafe148
Arne Morten Kvarving <spiff@kodi.tv>
2025-05-03 12:18:36 +0200

    BackgroundInfoLoader: use range for

--- xbmc/BackgroundInfoLoader.cpp
+++ xbmc/BackgroundInfoLoader.cpp
@@ -40,10 +40,8 @@ void CBackgroundInfoLoader::Run()
       OnLoaderStart();
 
       // Stage 1: All "fast" stuff we have already cached
-      for (std::vector<CFileItemPtr>::const_iterator iter = m_vecItems.begin(); iter != m_vecItems.end(); ++iter)
+      for (const auto& pItem : m_vecItems)
       {
-        const CFileItemPtr& pItem = *iter;
-
         // Ask the callback if we should abort
         if ((m_pProgressCallback && m_pProgressCallback->Abort()) || m_bStop)
           break;
@@ -62,10 +60,8 @@ void CBackgroundInfoLoader::Run()
       }
 
       // Stage 2: All "slow" stuff that we need to lookup
-      for (std::vector<CFileItemPtr>::const_iterator iter = m_vecItems.begin(); iter != m_vecItems.end(); ++iter)
+      for (const auto& pItem : m_vecItems)
       {
-        const CFileItemPtr& pItem = *iter;
-
         // Ask the callback if we should abort
         if ((m_pProgressCallback && m_pProgressCallback->Abort()) || m_bStop)
           break;
