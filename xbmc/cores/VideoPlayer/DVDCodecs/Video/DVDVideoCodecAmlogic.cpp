093d5a47c7ea2a36b44a3a3bb772e8d7f8d7ae23
cpm <162544069+cpm-code@users.noreply.github.com>
2025-05-03 15:20:05 +0800

    DVDVideoCodecAmlogic: Do not reset stream type stream unless speed is 1 (minimize black frames - always requires one reset to being pts inline when reverting to speed 1)

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAmlogic.cpp
@@ -604,16 +604,17 @@ bool CDVDVideoCodecAmlogic::AddData(const DemuxPacket &packet)
 
 void CDVDVideoCodecAmlogic::Reset(void)
 {
-  if ((m_dataCacheCore.GetSpeed() == 1.0f) || (m_hints.codec == AV_CODEC_ID_H264))
+  if (m_Codec->IsStreamTypeStream())
   {
-    if (m_Codec->IsStreamTypeStream()) // Cannot just do reset, needs fuller open and close to have correct pts.
+    if (m_dataCacheCore.GetSpeed() == 1.0f)
     {
+      // Cannot just do reset, needs fuller open and close to have correct pts.
       m_Codec->CloseDecoder(true);
       m_Codec->OpenDecoder(true);
     }
-    else
-      m_Codec->Reset();
   }
+  else
+    m_Codec->Reset();
 
   ClearBitstreamCommon();
 
