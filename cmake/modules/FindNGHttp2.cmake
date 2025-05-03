f69b2f221eeb02e017268bc05a6975d627fca02c
fuzzard <fuzzard@kodi.tv>
2025-05-03 19:30:01 +1000

    [cmake] FindNGHttp2 use new modulehelper macros

--- cmake/modules/FindNGHttp2.cmake
+++ cmake/modules/FindNGHttp2.cmake
@@ -36,23 +36,23 @@ if(NOT TARGET LIBRARY::NGHttp2)
 
   # Search for cmake config. Suitable for all platforms including windows
   # nghttp uses a non standard config name, so we have to supply CONFIGS
-  find_package(nghttp2 ${CONFIG_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} CONFIG ${SEARCH_QUIET}
-                       CONFIGS nghttp2-targets.cmake
-                       HINTS ${DEPENDS_PATH}/lib/cmake
-                       ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
+  find_package(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} ${CONFIG_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} CONFIG ${SEARCH_QUIET}
+                                                         CONFIGS nghttp2-targets.cmake
+                                                         HINTS ${DEPENDS_PATH}/lib/cmake
+                                                         ${${CORE_PLATFORM_NAME_LC}_SEARCH_CONFIG})
 
   # cmake config may not be available (eg Debian libnghttp2-dev package)
   # fallback to pkgconfig for non windows platforms
-  if(NOT nghttp2_FOUND)
+  if(NOT ${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
     find_package(PkgConfig ${SEARCH_QUIET})
 
     if(PKG_CONFIG_FOUND AND NOT (WIN32 OR WINDOWSSTORE))
-      pkg_check_modules(nghttp2 libnghttp2${PC_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} ${SEARCH_QUIET} IMPORTED_TARGET)
+      pkg_check_modules(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} libnghttp2${PC_${CMAKE_FIND_PACKAGE_NAME}_FIND_SPEC} ${SEARCH_QUIET} IMPORTED_TARGET)
     endif()
   endif()
 
   # Check for existing Nghttp2. If version >= NGHTTP2-VERSION file version, dont build
-  if(nghttp2_VERSION VERSION_LESS ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER} AND NGHttp2_FIND_REQUIRED)
+  if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_VERSION VERSION_LESS ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER} AND NGHttp2_FIND_REQUIRED)
     # Build lib
     buildlibnghttp2()
   else()
@@ -71,40 +71,44 @@ if(NOT TARGET LIBRARY::NGHttp2)
       # we only do this because we use find_package_handle_standard_args for config time output
       # and it isnt capable of handling TARGETS, so we have to extract the info
       get_target_property(_NGHTTP2_CONFIGURATIONS ${_target} IMPORTED_CONFIGURATIONS)
-      foreach(_nghttp2_config IN LISTS _NGHTTP2_CONFIGURATIONS)
-        # Some non standard config (eg None on Debian)
-        # Just set to RELEASE var so select_library_configurations can continue to work its magic
-        string(TOUPPER ${_nghttp2_config} _nghttp2_config_UPPER)
-        if((NOT ${_nghttp2_config_UPPER} STREQUAL "RELEASE") AND
-           (NOT ${_nghttp2_config_UPPER} STREQUAL "DEBUG"))
-          get_target_property(NGHTTP2_LIBRARY_RELEASE ${_target} IMPORTED_LOCATION_${_nghttp2_config_UPPER})
-        else()
-          get_target_property(NGHTTP2_LIBRARY_${_nghttp2_config_UPPER} ${_target} IMPORTED_LOCATION_${_nghttp2_config_UPPER})
-        endif()
-      endforeach()
+      if(_NGHTTP2_CONFIGURATIONS)
+        foreach(_nghttp2_config IN LISTS _NGHTTP2_CONFIGURATIONS)
+          # Some non standard config (eg None on Debian)
+          # Just set to RELEASE var so select_library_configurations can continue to work its magic
+          string(TOUPPER ${_nghttp2_config} _nghttp2_config_UPPER)
+          if((NOT ${_nghttp2_config_UPPER} STREQUAL "RELEASE") AND
+             (NOT ${_nghttp2_config_UPPER} STREQUAL "DEBUG"))
+            get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE ${_target} IMPORTED_LOCATION_${_nghttp2_config_UPPER})
+          else()
+            get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_${_nghttp2_config_UPPER} ${_target} IMPORTED_LOCATION_${_nghttp2_config_UPPER})
+          endif()
+        endforeach()
+      else()
+        get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE ${_target} IMPORTED_LOCATION)
+      endif()
 
       get_target_property(NGHTTP2_INCLUDE_DIR ${_target} INTERFACE_INCLUDE_DIRECTORIES)
-    elseif(TARGET PkgConfig::nghttp2)
+    elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
       # First item is the full path of the library file found
       # pkg_check_modules does not populate a variable of the found library explicitly
-      list(GET nghttp2_LINK_LIBRARIES 0 NGHTTP2_LIBRARY_RELEASE)
+      list(GET ${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_LINK_LIBRARIES 0 ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY_RELEASE)
 
-      get_target_property(NGHTTP2_INCLUDE_DIR PkgConfig::nghttp2 INTERFACE_INCLUDE_DIRECTORIES)
+      get_target_property(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} INTERFACE_INCLUDE_DIRECTORIES)
 
-      set(NGHTTP2_VERSION ${nghttp2_VERSION})
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION ${${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_VERSION})
     endif()
   endif()
 
   include(SelectLibraryConfigurations)
-  select_library_configurations(NGHTTP2)
-  unset(NGHTTP2_LIBRARIES)
+  select_library_configurations(${${CMAKE_FIND_PACKAGE_NAME}_MODULE})
+  unset(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARIES)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(NGHttp2
-                                    REQUIRED_VARS NGHTTP2_LIBRARY NGHTTP2_INCLUDE_DIR
-                                    VERSION_VAR NGHTTP2_VERSION)
+                                    REQUIRED_VARS ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LIBRARY ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_INCLUDE_DIR
+                                    VERSION_VAR ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VERSION)
 
-  if(NGHTTP2_FOUND)
+  if(NGHttp2_FOUND)
 
     if((TARGET nghttp2::nghttp2 OR TARGET nghttp2::nghttp2_static) AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
       # We have a preference for the static lib when needed, however provide support 
@@ -117,30 +121,12 @@ if(NOT TARGET LIBRARY::NGHttp2)
 
       # This is a kodi target name
       add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ALIAS ${_target})
-    elseif(TARGET PkgConfig::nghttp2 AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
-      add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::nghttp2)
+    elseif(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
     else()
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_TYPE LIBRARY)
+      SETUP_BUILD_TARGET()
 
-      add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} UNKNOWN IMPORTED)
-  
-      set_target_properties(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                INTERFACE_COMPILE_DEFINITIONS "NGHTTP2_STATICLIB"
-                                                                INTERFACE_INCLUDE_DIRECTORIES "${NGHTTP2_INCLUDE_DIR}")
-
-      if(NGHTTP2_LIBRARY_RELEASE)
-        set_target_properties(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                  IMPORTED_CONFIGURATIONS RELEASE
-                                                                  IMPORTED_LOCATION_RELEASE "${NGHTTP2_LIBRARY_RELEASE}")
-      endif()
-      if(NGHTTP2_LIBRARY_DEBUG)
-        set_target_properties(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES
-                                                                  IMPORTED_LOCATION_DEBUG "${NGHTTP2_LIBRARY_DEBUG}")
-        set_property(TARGET LIBRARY::${CMAKE_FIND_PACKAGE_NAME} APPEND PROPERTY
-                                                                       IMPORTED_CONFIGURATIONS DEBUG)
-      endif()
-    endif()
-
-    if(TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
       add_dependencies(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
 
       # We are building as a requirement, so set LIB_BUILD property to allow calling
