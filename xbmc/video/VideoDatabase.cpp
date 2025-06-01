445292eca16f94d14495108775a3fc2cdc54633d
78andyp <99039295+78andyp@users.noreply.github.com>
2025-06-01 14:30:14 +0100

    Fix scraper cache needs content type to be part of key otherwise content type is not updated if two sources using the same scraper with different content are scanned.

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -9241,7 +9241,7 @@ ScraperPtr CVideoDatabase::GetScraperForPath(const std::string& strPath,
   {
     if (scraperCache)
     {
-      auto key = scraperID + pathSettings;
+      auto key = scraperID + pathSettings + TranslateContent(content);
       if (auto iter = scraperCache->find(key); iter != scraperCache->end())
         return std::make_pair(true, iter->second);
     }
@@ -9256,7 +9256,7 @@ ScraperPtr CVideoDatabase::GetScraperForPath(const std::string& strPath,
       scraper->SetPathSettings(content, pathSettings);
       if (scraperCache)
       {
-        auto key = scraperID + pathSettings;
+        auto key = scraperID + pathSettings + TranslateContent(content);
         scraperCache->emplace(std::move(key), scraper);
       }
     }
