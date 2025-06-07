4c069709f63c3ae0310d79d4ec0548087eb57bad
fuzzard <fuzzard@kodi.tv>
2025-06-08 07:18:38 +1000

    fixup! [cmake] FindLzo2 remove FPHSA

--- cmake/modules/FindLzo2.cmake
+++ cmake/modules/FindLzo2.cmake
@@ -21,7 +21,12 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   SEARCH_EXISTING_PACKAGES()
 
   if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
-    add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+    if(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+    elseif(TARGET lzo2::lzo2)
+      # Kodi target - windows prebuilt lib
+      add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS lzo2::lzo2)
+    endif()
   else()
     if(LibLzo2_FIND_REQUIRED)
       message(FATAL_ERROR "Lzo2 library was not found.")
