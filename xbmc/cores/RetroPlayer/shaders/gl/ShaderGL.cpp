74e5577289d25f5a1ce879b8faf356d652dabf4a
KOPRajs <zdenek.koprivik@post.cz>
2025-06-10 16:00:24 +0200

    RetroPlayer: Rename GL shader array buffers for consistency

--- xbmc/cores/RetroPlayer/shaders/gl/ShaderGL.cpp
+++ xbmc/cores/RetroPlayer/shaders/gl/ShaderGL.cpp
@@ -27,9 +27,9 @@ CShaderGL::CShaderGL(RETRO::CRenderContext& context)
 
 CShaderGL::~CShaderGL()
 {
-  glDeleteBuffers(1, &EBO);
-  glDeleteBuffers(3, VBO);
-  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &m_shaderIndexVBO);
+  glDeleteBuffers(3, m_shaderVertexVBO);
+  glDeleteVertexArrays(1, &m_shaderVAO);
 }
 
 bool CShaderGL::Create(std::string shaderSource,
@@ -128,9 +128,9 @@ bool CShaderGL::Create(std::string shaderSource,
 
   GetUniformLocs();
 
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(3, VBO);
-  glGenBuffers(1, &EBO);
+  glGenVertexArrays(1, &m_shaderVAO);
+  glGenBuffers(3, m_shaderVertexVBO);
+  glGenBuffers(1, &m_shaderIndexVBO);
   return true;
 }
 
@@ -142,23 +142,23 @@ void CShaderGL::Render(IShaderTexture& source, IShaderTexture& target)
 
   SetShaderParameters(sourceGL.GetTexture());
 
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+  glBindVertexArray(m_shaderVAO);
+  glBindBuffer(GL_ARRAY_BUFFER, m_shaderVertexVBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(m_VertexCoords), m_VertexCoords, GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+  glBindBuffer(GL_ARRAY_BUFFER, m_shaderVertexVBO[1]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(m_colors), m_colors, GL_STATIC_DRAW);
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+  glBindBuffer(GL_ARRAY_BUFFER, m_shaderVertexVBO[2]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(m_TexCoords), m_TexCoords, GL_STATIC_DRAW);
   glEnableVertexAttribArray(2);
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_shaderIndexVBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_indices), m_indices, GL_STATIC_DRAW);
 
   glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, 0);
