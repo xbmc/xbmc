3db192270e57c576ae9340d3cf59c34f295b4f03
Damien Ruscoe <damien.ruscoe@gmail.com>
2025-06-14 23:14:50 +0100

    [CURL] Reduce the number of constructions of CURL objects.
    CURL objects are very expensive to create. The constructor calls
    `CURL::Parse` on every construction of a new object which analyses the
    string and parses it into many smaller strings; causing many
    allocations. Cache the `CURL` object on the `FileItem` object and
    invalidate the cache each time the `FileItem` path is modified.

--- xbmc/FileItemList.cpp
+++ xbmc/FileItemList.cpp
@@ -91,7 +91,7 @@ void CFileItemList::SetFastLookup(bool fastLookup)
     m_map.clear();
     for (const auto& pItem : m_items)
     {
-      m_map.emplace(m_ignoreURLOptions ? CURL(pItem->GetPath()).GetWithoutOptions()
+      m_map.emplace(m_ignoreURLOptions ? pItem->GetURL().GetWithoutOptions()
                                        : pItem->GetPath(),
                     pItem);
     }
@@ -143,7 +143,7 @@ void CFileItemList::Add(CFileItemPtr pItem)
   std::unique_lock lock(m_lock);
   if (m_fastLookup)
     m_map.emplace(
-        m_ignoreURLOptions ? CURL(pItem->GetPath()).GetWithoutOptions() : pItem->GetPath(), pItem);
+        m_ignoreURLOptions ? pItem->GetURL().GetWithoutOptions() : pItem->GetPath(), pItem);
   m_items.emplace_back(std::move(pItem));
 }
 
@@ -152,7 +152,7 @@ void CFileItemList::Add(CFileItem&& item)
   std::unique_lock lock(m_lock);
   auto ptr = std::make_shared<CFileItem>(std::move(item));
   if (m_fastLookup)
-    m_map.emplace(m_ignoreURLOptions ? CURL(ptr->GetPath()).GetWithoutOptions() : ptr->GetPath(),
+    m_map.emplace(m_ignoreURLOptions ? ptr->GetURL().GetWithoutOptions() : ptr->GetPath(),
                   ptr);
   m_items.emplace_back(std::move(ptr));
 }
@@ -172,7 +172,7 @@ void CFileItemList::AddFront(const CFileItemPtr& pItem, int itemPosition)
   if (m_fastLookup)
   {
     m_map.emplace(
-        m_ignoreURLOptions ? CURL(pItem->GetPath()).GetWithoutOptions() : pItem->GetPath(), pItem);
+        m_ignoreURLOptions ? pItem->GetURL().GetWithoutOptions() : pItem->GetPath(), pItem);
   }
 }
 
@@ -186,7 +186,7 @@ void CFileItemList::Remove(CFileItem* pItem)
     m_items.erase(it);
     if (m_fastLookup)
     {
-      m_map.erase(m_ignoreURLOptions ? CURL(pItem->GetPath()).GetWithoutOptions()
+      m_map.erase(m_ignoreURLOptions ? pItem->GetURL().GetWithoutOptions()
                                      : pItem->GetPath());
     }
   }
@@ -207,7 +207,7 @@ void CFileItemList::Remove(int iItem)
     CFileItemPtr pItem = *(m_items.begin() + iItem);
     if (m_fastLookup)
     {
-      m_map.erase(m_ignoreURLOptions ? CURL(pItem->GetPath()).GetWithoutOptions()
+      m_map.erase(m_ignoreURLOptions ? pItem->GetURL().GetWithoutOptions()
                                      : pItem->GetPath());
     }
     m_items.erase(m_items.begin() + iItem);
