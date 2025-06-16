eigendude/kodi

[Mouse] Separate input handling into absolute/relative functions

xbmc/games/addons/input/GameClientMouse.h
@@ -51,7 +51,11 @@ public:
 
   // implementation of IMouseInputHandler
   std::string ControllerID() const override;
-  bool OnMotion(const std::string& relpointer, int dx, int dy) override;
+  bool OnPosition(const std::string& relpointer, int distanceX, int distanceY) override
+  {
+    return false;
+  }
+  bool OnMotion(const std::string& relpointer, int differenceX, int differenceY) override;
   bool OnButtonPress(const std::string& button) override;
   void OnButtonRelease(const std::string& button) override;
   void OnInputFrame() override;

xbmc/games/agents/input/AgentMouse.h
@@ -40,6 +40,10 @@ public:
 
   // Implementation of IMouseInputHandler
   std::string ControllerID() const override;
+  bool OnPosition(const MOUSE::PointerName& relpointer, int distanceX, int distanceY) override
+  {
+    return false;
+  }
   bool OnMotion(const MOUSE::PointerName& relpointer, int differenceX, int differenceY) override;
   bool OnButtonPress(const MOUSE::ButtonName& button) override;
   void OnButtonRelease(const MOUSE::ButtonName& button) override;

xbmc/input/mouse/interfaces/IMouseInputHandler.h
@@ -34,18 +34,32 @@ public:
   virtual std::string ControllerID(void) const = 0;
 
   /*!
-   * \brief A relative pointer has moved
+   * \brief A relative pointer has moved to a new absolute position
    *
-   * \param relpointer   The name of the relative pointer being moved
-   * \param dx           The relative x coordinate of motion
-   * \param dy           The relative y coordinate of motion
+   * \param relpointer The name of the relative pointer being moved
+   * \param distanceX The absolute x coordinate of motion
+   * \param distanceY The absolute y coordinate of motion
    *
    * The mouse uses a left-handed (graphics) cartesian coordinate system.
    * Positive X is right, positive Y is down.
    *
    * \return True if the event was handled, otherwise false
    */
-  virtual bool OnMotion(const PointerName& relpointer, int dx, int dy) = 0;
+  virtual bool OnPosition(const PointerName& relpointer, int distanceX, int distanceY) = 0;
+
+  /*!
+   * \brief A relative pointer has moved a relative distance
+   *
+   * \param relpointer The name of the relative pointer being moved
+   * \param differenceX The relative x coordinate of motion
+   * \param differenceY The relative y coordinate of motion
+   *
+   * The mouse uses a left-handed (graphics) cartesian coordinate system.
+   * Positive X is right, positive Y is down.
+   *
+   * \return True if the event was handled, otherwise false
+   */
+  virtual bool OnMotion(const PointerName& relpointer, int differenceX, int differenceY) = 0;
 
   /*!
    * \brief A mouse button has been pressed

xbmc/xbmc

xbmc/games/addons/input/GameClientMouse.h
@@ -51,7 +51,11 @@ public:
 
   // implementation of IMouseInputHandler
   std::string ControllerID() const override;
-  bool OnMotion(const std::string& relpointer, int dx, int dy) override;
+  bool OnPosition(const std::string& relpointer, int positionX, int positionY) override
+  {
+    return false;
+  }
+  bool OnMotion(const std::string& relpointer, int differenceX, int differenceY) override;
   bool OnButtonPress(const std::string& button) override;
   void OnButtonRelease(const std::string& button) override;
   void OnInputFrame() override;

xbmc/games/agents/input/AgentMouse.h
@@ -40,6 +40,10 @@ public:
 
   // Implementation of IMouseInputHandler
   std::string ControllerID() const override;
+  bool OnPosition(const MOUSE::PointerName& relpointer, int positionX, int positionY) override
+  {
+    return false;
+  }
   bool OnMotion(const MOUSE::PointerName& relpointer, int differenceX, int differenceY) override;
   bool OnButtonPress(const MOUSE::ButtonName& button) override;
   void OnButtonRelease(const MOUSE::ButtonName& button) override;

xbmc/input/mouse/interfaces/IMouseInputHandler.h
@@ -34,18 +34,32 @@ public:
   virtual std::string ControllerID(void) const = 0;
 
   /*!
-   * \brief A relative pointer has moved
+   * \brief A relative pointer has moved to a new absolute position
    *
-   * \param relpointer   The name of the relative pointer being moved
-   * \param dx           The relative x coordinate of motion
-   * \param dy           The relative y coordinate of motion
+   * \param relpointer The name of the relative pointer being moved
+   * \param positionX The absolute x coordinate of motion
+   * \param positionY The absolute y coordinate of motion
    *
    * The mouse uses a left-handed (graphics) cartesian coordinate system.
    * Positive X is right, positive Y is down.
    *
    * \return True if the event was handled, otherwise false
    */
-  virtual bool OnMotion(const PointerName& relpointer, int dx, int dy) = 0;
+  virtual bool OnPosition(const PointerName& relpointer, int positionX, int positionY) = 0;
+
+  /*!
+   * \brief A relative pointer has moved a relative distance
+   *
+   * \param relpointer The name of the relative pointer being moved
+   * \param differenceX The relative x coordinate of motion
+   * \param differenceY The relative y coordinate of motion
+   *
+   * The mouse uses a left-handed (graphics) cartesian coordinate system.
+   * Positive X is right, positive Y is down.
+   *
+   * \return True if the event was handled, otherwise false
+   */
+  virtual bool OnMotion(const PointerName& relpointer, int differenceX, int differenceY) = 0;
 
   /*!
    * \brief A mouse button has been pressed