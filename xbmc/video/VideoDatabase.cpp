738a8aa39280db5d6463fdd679c8d428b2d59088
CrystalP <crystalp@kodi.tv>
2025-07-03 16:14:52 -0400

    [VideoDB] Index to match show by title and date faster, with DB version bump

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -302,6 +302,9 @@ void CVideoDatabase::CreateAnalytics()
 
   m_pDS->exec(PrepareSQL("CREATE INDEX ix_movie_title ON movie (c%02d(255))", VIDEODB_ID_TITLE));
 
+  m_pDS->exec(PrepareSQL("CREATE INDEX ix_tvshow_title ON tvshow (c%02d(255), c%02d(10))",
+                         VIDEODB_ID_TV_TITLE, VIDEODB_ID_TV_PREMIERED));
+
   CreateLinkIndex("tag");
   CreateForeignLinkIndex("director", "actor");
   CreateForeignLinkIndex("writer", "actor");
@@ -7011,7 +7014,7 @@ void CVideoDatabase::UpdateTables(int iVersion)
 
 int CVideoDatabase::GetSchemaVersion() const
 {
-  return 136;
+  return 137;
 }
 
 bool CVideoDatabase::LookupByFolders(const std::string &path, bool shows)
