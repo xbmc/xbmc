4b8f56c2c5e080bfec78b8e99d22adf70bd879f0
ksooo <kai.sommerfeld@gmx.com>
2025-08-02 08:39:02 +0200

    [profiles] CProfileManager::LoadProfile: Fix conditions for not loading a profile. Allow reload of currently active profile (data refresh).

--- xbmc/profiles/ProfileManager.cpp
+++ xbmc/profiles/ProfileManager.cpp
@@ -274,6 +274,15 @@ void CProfileManager::PrepareLoadProfile(unsigned int profileIndex)
 
 bool CProfileManager::LoadProfile(unsigned int index)
 {
+  std::unique_lock lock(m_critical);
+
+  // check if the index is valid or not
+  if (index >= m_profiles.size())
+  {
+    CLog::LogF(LOGERROR, "Profile not loaded. Invalid profile id {}", index);
+    return false;
+  }
+
   PrepareLoadProfile(index);
 
   if (index == 0 && IsMasterProfile())
@@ -287,15 +296,6 @@ bool CProfileManager::LoadProfile(unsigned int index)
 
     return true;
   }
-
-  std::unique_lock lock(m_critical);
-  // check if the index is valid or not
-  if (index >= m_profiles.size())
-    return false;
-
-  // check if the profile is already active
-  if (m_currentProfile == index)
-    return true;
 
   // save any settings of the currently used skin but only if the (master)
   // profile hasn't just been loaded as a temporary profile for login
