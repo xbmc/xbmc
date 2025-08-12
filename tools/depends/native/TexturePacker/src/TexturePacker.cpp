568efb3bbe6cde4cd3b0769d49083f776345b267
Rechi <Rechi@users.noreply.github.com>
2025-08-12 03:31:24 +0100

    [clang-tidy] readability-container-size-empty

--- tools/depends/native/TexturePacker/src/TexturePacker.cpp
+++ tools/depends/native/TexturePacker/src/TexturePacker.cpp
@@ -149,7 +149,7 @@ void TexturePacker::CreateSkeletonHeader(CXBTFWriter& xbtfWriter,
         if (dp->d_type == DT_DIR || stat_p.st_mode & S_IFDIR)
         {
           std::string tmpPath = relativePath;
-          if (tmpPath.size() > 0)
+          if (!tmpPath.empty())
           {
             tmpPath += "/";
           }
@@ -160,7 +160,7 @@ void TexturePacker::CreateSkeletonHeader(CXBTFWriter& xbtfWriter,
         else if (decoderManager.IsSupportedGraphicsFile(dp->d_name))
         {
           std::string fileName = "";
-          if (relativePath.size() > 0)
+          if (!relativePath.empty())
           {
             fileName += relativePath;
             fileName += "/";
