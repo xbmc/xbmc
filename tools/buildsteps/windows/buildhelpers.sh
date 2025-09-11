029892ece0b6f753272619bfb5a3d5fcc130fbf7
fuzzard <fuzzard@kodi.tv>
2025-09-11 20:28:16 +1000

    [windows] msys buildhelpers extract ARCHIVE from VERSION file
    dont hardcode archive name for ffmpeg build on windows. Extract and use
    the archive name in the VERSION file in tools/depends

--- tools/buildsteps/windows/buildhelpers.sh
+++ tools/buildsteps/windows/buildhelpers.sh
@@ -147,7 +147,11 @@ do_loaddeps() {
   LIBNAME=$(grep "LIBNAME=" $file | sed 's/LIBNAME=//g;s/#.*$//g;/^$/d')
   VERSION=$(grep "VERSION=" $file | sed 's/VERSION=//g;s/#.*$//g;/^$/d')
   SHA512=$(grep "SHA512=" $file | sed 's/SHA512=//g;s/#.*$//g;/^$/d')
-  ARCHIVE=$LIBNAME-$VERSION.tar.gz
+  ARCHIVE=$(grep "ARCHIVE=" $file | sed 's/ARCHIVE=//g;s/#.*$//g;/^$/d')
+
+  # replace variables in ARCHIVE string if used.
+  ARCHIVE=$(echo "$ARCHIVE" | sed "s/\$(LIBNAME)/$LIBNAME/g")
+  ARCHIVE=$(echo "$ARCHIVE" | sed "s/\$(VERSION)/$VERSION/g")
 
   BASE_URL=$(grep "BASE_URL=" $file | sed 's/BASE_URL=//g;s/#.*$//g;/^$/d')
   if [ -z "$BASE_URL" ]; then
