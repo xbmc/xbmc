e1e9fd3e7990644536db247ed4ff3bbfce6d62fc
CrystalP <crystalp@kodi.tv>
2025-09-23 21:58:36 -0400

    [i18n] Add tests for ISO639-3

---
+++ xbmc/utils/i18n/test/TestISO639_3.cpp
@@ -0,0 +1,60 @@
+/*
+ *  Copyright (C) 2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "utils/i18n/TableISO639.h"
+#include "utils/i18n/TableISO639_3.h"
+
+#include <gtest/gtest.h>
+
+TEST(TestTableISO639_3, LookupByCode1)
+{
+  uint32_t longCode = StringToLongCode("aaa");
+  std::optional<std::string> result;
+  result = CTableISO639_3::LookupByCode(longCode);
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(*result, "Ghotuo");
+
+  longCode = StringToLongCode("boh");
+  result = CTableISO639_3::LookupByCode(longCode);
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(*result, "Boma");
+
+  // value in the range reserved for private use - will never match
+  longCode = StringToLongCode("qaa");
+  result = CTableISO639_3::LookupByCode(longCode);
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(TestTableISO639_3, LookupByCode2)
+{
+  std::optional<std::string> result;
+
+  result = CTableISO639_3::LookupByCode("aaa");
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(*result, "Ghotuo");
+
+  result = CTableISO639_3::LookupByCode("boh");
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(*result, "Boma");
+
+  // value in the range reserved for private use - will never match
+  result = CTableISO639_3::LookupByCode("qaa");
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(TestTableISO639_3, LookupByName)
+{
+  std::optional<std::string> result;
+
+  result = CTableISO639_3::LookupByName("Puyuma");
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(*result, "pyu");
+
+  result = CTableISO639_3::LookupByName("not a language");
+  EXPECT_FALSE(result.has_value());
+}
