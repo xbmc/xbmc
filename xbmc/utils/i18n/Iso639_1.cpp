fc8c25e739850a64f38fc10f0a1ab0d93cc595d7
CrystalP <crystalp@kodi.tv>
2025-10-04 10:37:57 -0400

    [i18n] Refactor ISO 639-1 support as a class

---
+++ xbmc/utils/i18n/Iso639_1.cpp
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (C) 2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "utils/i18n/Iso639_1.h"
+
+#include "utils/StringUtils.h"
+#include "utils/i18n/Iso639_1_Table.h"
+#include "utils/i18n/TableISO639.h"
+
+#include <algorithm>
+
+using namespace KODI::UTILS::I18N;
+
+std::optional<std::string> CIso639_1::LookupByCode(std::string_view code)
+{
+  const uint32_t longCode = StringToLongCode(code);
+  return LookupByCode(longCode);
+}
+
+std::optional<std::string> CIso639_1::LookupByCode(uint32_t longCode)
+{
+  {
+    auto it = std::ranges::lower_bound(TableISO639_1ByCode, longCode, {}, &LCENTRY::code);
+    if (it != TableISO639_1ByCode.end() && longCode == it->code)
+      return std::string{it->name};
+  }
+  {
+    auto it = std::ranges::lower_bound(TableISO639_1_DeprByCode, longCode, {}, &LCENTRY::code);
+    if (it != TableISO639_1_DeprByCode.end() && longCode == it->code)
+      return std::string{it->name};
+  }
+  return std::nullopt;
+}
+
+std::optional<std::string> CIso639_1::LookupByName(std::string_view name)
+{
+  {
+    auto it = std::ranges::lower_bound(
+        TableISO639_1ByName, name, [](std::string_view a, std::string_view b)
+        { return StringUtils::CompareNoCase(a, b) < 0; }, &LCENTRY::name);
+    if (it != TableISO639_1ByName.end() && StringUtils::EqualsNoCase(it->name, name))
+      return LongCodeToString(it->code);
+  }
+  {
+    auto it = std::ranges::lower_bound(
+        TableISO639_1_DeprByName, name, [](std::string_view a, std::string_view b)
+        { return StringUtils::CompareNoCase(a, b) < 0; }, &LCENTRY::name);
+    if (it != TableISO639_1_DeprByName.end() && StringUtils::EqualsNoCase(it->name, name))
+      return LongCodeToString(it->code);
+  }
+  return std::nullopt;
+}
+
+bool CIso639_1::ListLanguages(std::map<std::string, std::string>& langMap)
+{
+  std::ranges::transform(TableISO639_1ByName, std::inserter(langMap, langMap.end()),
+                         [](const LCENTRY& e)
+                         { return std::make_pair(LongCodeToString(e.code), std::string{e.name}); });
+
+  std::ranges::transform(
+      TableISO639_1_DeprByName, std::inserter(langMap, langMap.end()), [](const LCENTRY& e)
+      { return std::make_pair(LongCodeToString(e.code), std::string{e.name} + " (Deprecated)"); });
+
+  return true;
+}
---
+++ xbmc/utils/i18n/Iso639_1.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2025 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <map>
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace KODI::UTILS::I18N
+{
+class CIso639_1
+{
+public:
+  CIso639_1() = delete;
+
+  /*!
+   * \brief Retrieve the name of the language for the provided ISO 639-1 code.
+   * \param[in] code alpha-2 ISO 639-1 code
+   * \return English name of the language, nullopt if no language by that code exists.
+   */
+  static std::optional<std::string> LookupByCode(std::string_view code);
+
+  /*!
+   * \brief Retrieve the name of the language for the provided ISO 639-1 code.
+   * \param[in] code alpha-2 ISO 639-1 code, coded as a 32 bit unsigned integer
+   * \return English name of the language, nullopt if no language by that code exists.
+   */
+  static std::optional<std::string> LookupByCode(uint32_t longCode);
+
+  /*!
+   * \brief Retrieve the code of the provided language name.
+   * \param[in] name English language name
+   * \return alpha-2 code of the language, nullopt if no language by that name exists.
+   */
+  static std::optional<std::string> LookupByName(std::string_view name);
+
+  /*!
+   * \brief Provide a list of defined ISO 639-1 languages
+   * \param[in] langMap map to add languages to
+   * \return true for success, false otherwise
+   */
+  static bool ListLanguages(std::map<std::string, std::string>& langMap);
+};
+} // namespace KODI::UTILS::I18N
