a70690f0d4e6a0564b410d0e534eb7f53a5b1203
Portisch <hugo.portisch@yahoo.de>
2025-12-03 10:02:14 +0100

    ActiveAE: remove double resample check

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEBuffer.cpp
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEBuffer.cpp
@@ -210,8 +210,7 @@ bool CActiveAEBufferPoolResample::ResampleBuffers(int64_t timestamp)
   {
     if (m_changeResampler)
     {
-      if (m_changeResampler)
-        ChangeResampler();
+      ChangeResampler();
       return true;
     }
     while(!m_inputSamples.empty())
