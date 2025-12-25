719d6050253b8e0368ae4f90994a64db11f33c0c
78andyp <78andyp@kodi.tv>
2025-12-25 19:40:35 +0000

    [URIUtils][URL] Fix inconsistent handling of DOS/POSIX paths.

--- xbmc/utils/URIUtils.cpp
+++ xbmc/utils/URIUtils.cpp
@@ -567,12 +567,10 @@ bool URIUtils::GetParentPath(const std::string& strPath, std::string& strParent)
   }
 
   size_t iPos = strFile.rfind('/');
-#ifndef TARGET_POSIX
   if (iPos == std::string::npos)
   {
     iPos = strFile.rfind('\\');
   }
-#endif
   if (iPos == std::string::npos)
   {
     url.SetFileName("");
@@ -1547,6 +1545,14 @@ bool URIUtils::IsDOSPath(const std::string &path)
 
   // windows network drives
   if (path.size() > 1 && path[0] == '\\' && path[1] == '\\')
+    return true;
+
+  return false;
+}
+
+bool URIUtils::IsPOSIXPath(const std::string& path)
+{
+  if (path.size() > 1 && path[0] == '/' && isalpha(path[1]))
     return true;
 
   return false;
