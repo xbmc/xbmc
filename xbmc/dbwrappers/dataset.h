75c1d8cce3845fef97dd3dd86a1740a77be4731a
fuzzard <fuzzard@kodi.tv>
2025-12-25 21:52:21 +1000

    [mysql/mariadb] Advanced option enable server tls verification
    mariadb-c-connector changes the default behaviour to force TLS server verification
    DEFAULT_SSL_VERIFY_SERVER_CERT. This creates an advanced setting <verifyserver> to
    enable/disable server verification.
    
    <verifyserver>1</verifyserver>
    
    The default behaviour will be to disable the server verification. If enabled, it requires
    the user to provide combinations of cert/ca/capath, so the addition of the extra field
    shouldnt be that much of an issue for those using those additional fields.

--- xbmc/dbwrappers/dataset.h
+++ xbmc/dbwrappers/dataset.h
@@ -52,6 +52,7 @@ class Database
 protected:
   bool active{false};
   bool compression{false};
+  bool verifyserver{false}; // tls server verifications
   std::string error; // Error description
   std::string host;
   std::string port;
@@ -105,6 +106,7 @@ public:
                          const char* newCApath,
                          const char* newCiphers,
                          unsigned int newConnectTimeout,
+                         bool newVerifyServer,
                          bool newCompression)
   {
     key = newKey;
@@ -113,6 +115,7 @@ public:
     capath = newCApath;
     ciphers = newCiphers;
     connect_timeout = newConnectTimeout;
+    verifyserver = newVerifyServer;
     compression = newCompression;
   }
 
@@ -134,6 +137,7 @@ public:
                           const char* newCA = nullptr,
                           const char* newCApath = nullptr,
                           const char* newCiphers = nullptr,
+                          bool newVerifyServer = false,
                           bool newCompression = false);
   virtual void disconnect() { active = false; }
   virtual int postconnect() { return DB_COMMAND_OK; }
