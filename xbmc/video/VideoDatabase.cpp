f5c54442c12261e834ddde05de1699dc49aa573f
78andyp <78andyp@kodi.tv>
2025-12-29 18:18:19 +0000

    [VideoDatabase] Fix Information -> Refresh crashes on stacks.

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -8328,6 +8328,8 @@ ScraperPtr CVideoDatabase::GetScraperForPath(const std::string& strPath,
 
     if (URIUtils::IsMultiPath(strPath))
       strPath2 = CMultiPathDirectory::GetFirstPath(strPath);
+    else if (URIUtils::IsStack(strPath))
+      strPath2 = CStackDirectory::GetFirstStackedFile(strPath);
     else
       strPath2 = strPath;
 
