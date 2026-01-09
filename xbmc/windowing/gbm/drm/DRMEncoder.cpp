db52e9027532b1699ded738e651dab26c9fc2b78
boogie <boogiepop@gmx.com>
2026-01-09 01:50:39 +0100

    CDRMEncoder: Refactor GetPossibleCrtcs
    Now GetPossibleCrtcs returns the possible CDRMCrtcs vector for the given
    encoder with a possiblity to have one crtc preferred

--- xbmc/windowing/gbm/drm/DRMEncoder.cpp
+++ xbmc/windowing/gbm/drm/DRMEncoder.cpp
@@ -21,3 +21,24 @@ CDRMEncoder::CDRMEncoder(int fd, uint32_t encoder)
   if (!m_encoder)
     throw std::runtime_error("drmModeGetEncoder failed: " + std::string{strerror(errno)});
 }
+
+std::vector<CDRMCrtc*> CDRMEncoder::GetPossibleCrtcs(
+    const std::vector<std::unique_ptr<CDRMCrtc>>& crtcs, CDRMCrtc* preferred) const
+{
+  std::vector<CDRMCrtc*> possible;
+
+  for (auto& crtc : crtcs)
+  {
+    // Check bitmask
+    if (!(m_encoder->possible_crtcs & (1 << crtc->GetOffset())))
+      continue;
+
+    // Use raw pointer for the result
+    if (preferred && preferred->GetId() == crtc->GetId())
+      possible.insert(possible.begin(), crtc.get()); // Put preferred at front
+    else
+      possible.push_back(crtc.get());
+  }
+
+  return possible;
+}
