979adadab129575b2ca9f30c8b4b0b46c704cd01
Marc Aymerich Gubern <maymerichgubern@maymeric-ltmn7tx.internal.salesforce.com>
2026-01-15 18:11:54 +0000

    Revert "CShoutcastFile::Process: Send Player.OnPropertyChanged for radio stream metadata updates"
    This reverts commit 6b3c9f06d06107f13400e5523190aafbf48be45e.

--- xbmc/filesystem/ShoutcastFile.cpp
+++ xbmc/filesystem/ShoutcastFile.cpp
@@ -17,13 +17,9 @@
 #include "FileItem.h"
 #include "ServiceBroker.h"
 #include "URL.h"
-#include "application/ApplicationComponents.h"
-#include "application/ApplicationPlayer.h"
 #include "filesystem/CurlFile.h"
-#include "interfaces/AnnouncementManager.h"
 #include "messaging/ApplicationMessenger.h"
 #include "music/tags/MusicInfoTag.h"
-#include "PlayListPlayer.h"
 #include "settings/AdvancedSettings.h"
 #include "settings/SettingsComponent.h"
 #include "threads/SingleLock.h"
@@ -33,13 +29,11 @@
 #include "utils/RegExp.h"
 #include "utils/StringUtils.h"
 #include "utils/UrlOptions.h"
-#include "utils/Variant.h"
 
 #include <climits>
 #include <memory>
 #include <mutex>
 
-using namespace KODI;
 using namespace XFILE;
 using namespace MUSIC_INFO;
 using namespace std::chrono_literals;
@@ -358,24 +352,10 @@ void CShoutcastFile::Process()
         }
         else
         {
-          const TagInfo& tagInfo = front;
-          CFileItem* item = new CFileItem(*tagInfo.second); // will be deleted by msg receiver
-          CFileItemPtr itemPtr = std::make_shared<CFileItem>(*tagInfo.second);
+          CFileItem* item = new CFileItem(*front.second); // will be deleted by msg receiver
           m_tags.pop();
           CServiceBroker::GetAppMessenger()->PostMsg(TMSG_UPDATE_CURRENT_ITEM, 1, -1,
                                                      static_cast<void*>(item));
-
-          // Announce property change for radio stream metadata updates
-          const auto& components = CServiceBroker::GetAppComponents();
-          const auto appPlayer = components.GetComponent<CApplicationPlayer>();
-          if (appPlayer->IsPlayingAudio() &&
-              CServiceBroker::GetPlaylistPlayer().GetCurrentPlaylist() == PLAYLIST::Id::TYPE_MUSIC)
-          {
-            CVariant data;
-            data["player"]["playerid"] = static_cast<int>(PLAYLIST::Id::TYPE_MUSIC);
-            CServiceBroker::GetAnnouncementManager()->Announce(ANNOUNCEMENT::Player, "OnPropertyChanged",
-                                                               itemPtr, data);
-          }
         }
       }
     }
