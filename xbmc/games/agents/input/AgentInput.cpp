7a86552f456d6752356a99c45bec3c966876b057
Arne Morten Kvarving <spiff@kodi.tv>
2026-01-17 19:46:12 +0100

    use std::ranges::copy
    the constrained algorithm better expresses intent
    and is less error prone

--- xbmc/games/agents/input/AgentInput.cpp
+++ xbmc/games/agents/input/AgentInput.cpp
@@ -184,7 +184,7 @@ std::vector<std::shared_ptr<const CAgentController>> CAgentInput::GetControllers
   std::lock_guard<std::mutex> lock(m_controllerMutex);
 
   std::vector<std::shared_ptr<const CAgentController>> controllers{m_controllers.size()};
-  std::copy(m_controllers.begin(), m_controllers.end(), controllers.begin());
+  std::ranges::copy(m_controllers, controllers.begin());
 
   return controllers;
 }
