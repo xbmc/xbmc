1302038fe19637e8db20fcf32bdff6630096c81c
CrystalP <crystalp@kodi.tv>
2026-02-01 10:32:36 -0500

    [Windows] Have the Win API update the Wide/Multi conversion result string in-place

--- xbmc/platform/win32/CharsetConverter.cpp
+++ xbmc/platform/win32/CharsetConverter.cpp
@@ -8,8 +8,8 @@
 
 #include "CharsetConverter.h"
 
+#include <cassert>
 #include <limits>
-#include <memory>
 
 #include <Windows.h>
 
@@ -21,19 +21,30 @@ namespace WINDOWS
 {
 std::string FromW(const wchar_t* str, size_t length)
 {
+  std::string newStr;
+
   if (length == 0 || length > std::numeric_limits<int>::max())
-    return std::string();
+    return newStr;
 
-  int result = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, str, length, nullptr, 0, nullptr, nullptr);
-  if (result == 0)
-    return std::string();
+  const int result1 =
+      WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, str, length, nullptr, 0, nullptr, nullptr);
+  if (result1 == 0)
+    return newStr;
 
-  auto newStr = std::make_unique<char[]>(result);
-  result = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, str, length, newStr.get(), result, nullptr, nullptr);
-  if (result == 0)
-    return std::string();
+  newStr.resize(result1, '\0');
+  const int result2 = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, str, length, newStr.data(),
+                                          result1, nullptr, nullptr);
+  assert(result1 == result2);
 
-  return std::string(newStr.get(), result);
+  if (result1 != result2)
+  {
+    if (result2 < result1) // less data written than expected is recoverable though still a problem
+      newStr.resize(result2, '\0');
+    else
+      newStr.clear();
+  }
+
+  return newStr;
 }
 
 std::string FromW(std::wstring_view str)
@@ -43,20 +54,29 @@ std::string FromW(std::wstring_view str)
 
 std::wstring ToW(const char* str, size_t length)
 {
+  std::wstring newStr;
+
   if (length == 0 || length > std::numeric_limits<int>::max())
-    return std::wstring();
+    return newStr;
 
-  int result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str, length, nullptr, 0);
-  if (result == 0)
-    return std::wstring();
+  const int result1 = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str, length, nullptr, 0);
+  if (result1 == 0)
+    return newStr;
 
-  auto newStr = std::make_unique<wchar_t[]>(result);
-  result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str, length, newStr.get(), result);
+  newStr.resize(result1, L'\0');
+  const int result2 =
+      MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str, length, newStr.data(), result1);
+  assert(result1 == result2);
 
-  if (result == 0)
-    return std::wstring();
+  if (result1 != result2)
+  {
+    if (result2 < result1) // less data written than expected is recoverable though still a problem
+      newStr.resize(result2, L'\0');
+    else
+      newStr.clear();
+  }
 
-  return std::wstring(newStr.get(), result);
+  return newStr;
 }
 
 std::wstring ToW(std::string_view str)
