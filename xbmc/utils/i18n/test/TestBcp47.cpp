e9eba247943e9c5666b59b6dadb0687307194089
CrystalP <crystalp@kodi.tv>
2026-02-07 22:18:06 -0500

    [i18n] Refactor BCP47 Tag tests

--- xbmc/utils/i18n/test/TestBcp47.cpp
+++ xbmc/utils/i18n/test/TestBcp47.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2025 Team Kodi
+ *  Copyright (C) 2025-2026 Team Kodi
  *  This file is part of Kodi - https://kodi.tv
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
@@ -90,6 +90,63 @@ TEST_P(Bcp47ParseTagTester, ParseTag)
 
 INSTANTIATE_TEST_SUITE_P(TestI18nBcp47, Bcp47ParseTagTester, testing::ValuesIn(ParseBcp47Tests));
 
+struct TestBcp47ValidateTag
+{
+  std::string input;
+  bool expectedIsValid{true};
+};
+
+std::ostream& operator<<(std::ostream& os, const TestBcp47ValidateTag& rhs)
+{
+  return os << rhs.input;
+}
+
+// clang-format off
+const TestBcp47ValidateTag ValidityBcp47Tests[] = {
+  {"zz", false}, // undefined
+  // Language subtags
+  {"en", true}, // ISO 639-1
+  {"eng", false}, // ISO 639-2 that has an equivalent ISO 639-1
+  {"tib", false}, // ISO 639-2/B that has an equivalent ISO 639-1
+  {"bod", false}, // ISO 639-2/T that has an equivalent ISO 639-1
+  {"ady", true}, // ISO 639-2 without ISO 639-1 equivalent
+  {"qaa", true}, // Private use ISO 639-2
+  // Extlang subtags
+  {"ar-aao", true},
+  {"en-abc-def", false}, // Multiple extlangs are rejected whether they exist in registry or not
+  //! @todo add test for inexistent extlang after implementation of the registry
+  // Region subtags
+  {"en-GB", true}, // ISO 3166-1
+  {"en-AA", true}, // Private use
+  {"es-419", false}, // UN M.49 - support to be added with IANA language subtags registry
+  // Variant subtags
+  {"en-variant", true},
+  {"en-variant-variant", false},
+  // Extensions subtags
+  {"ab-a-bcdefghi-jk", true}, // extension with multiple segments
+  {"ab-b-ab-a-cd", true}, // multiple extensions in non-alphabetical order are OK
+  {"ab-a-bc-a-de", false}, // duplicate extensions not allowed
+};
+// clang-format on
+
+class Bcp47ValidateTagTester : public testing::Test,
+                               public testing::WithParamInterface<TestBcp47ValidateTag>
+{
+};
+
+TEST_P(Bcp47ValidateTagTester, ParseTag)
+{
+  auto& param = GetParam();
+  auto actual = CBcp47::ParseTag(param.input);
+
+  EXPECT_TRUE(actual.has_value());
+  EXPECT_EQ(param.expectedIsValid, actual.value().IsValid());
+}
+
+INSTANTIATE_TEST_SUITE_P(TestI18nBcp47,
+                         Bcp47ValidateTagTester,
+                         testing::ValuesIn(ValidityBcp47Tests));
+
 TEST(TestI18nBcp47, Canonicalize)
 {
   // ISO 639-1 code
@@ -101,89 +158,4 @@ TEST(TestI18nBcp47, Canonicalize)
   tag = CBcp47::ParseTag("ab-d-ef-g-hi-a-bc");
   tag->Canonicalize();
   EXPECT_EQ(tag->Format(), "ab-a-bc-d-ef-g-hi");
-}
-
-TEST(TestI18nBcp47, ValidateLanguage)
-{
-  // ISO 639-1
-  auto tag = CBcp47::ParseTag("en");
-  EXPECT_TRUE(tag->IsValid());
-
-  // ISO 639-2 that has an equivalent ISO 639-1
-  tag = CBcp47::ParseTag("eng");
-  EXPECT_FALSE(tag->IsValid());
-
-  // ISO 639-2/B that has an equivalent ISO 639-1
-  tag = CBcp47::ParseTag("tib");
-  EXPECT_FALSE(tag->IsValid());
-
-  // ISO 639-2/T that has an equivalent ISO 639-1
-  tag = CBcp47::ParseTag("bod");
-  EXPECT_FALSE(tag->IsValid());
-
-  // ISO 639-2 without ISO 639-1 equivalent
-  tag = CBcp47::ParseTag("ady");
-  EXPECT_TRUE(tag->IsValid());
-
-  // Private use ISO 639-2
-  tag = CBcp47::ParseTag("qaa");
-  EXPECT_TRUE(tag->IsValid());
-}
-
-TEST(TestI18nBcp47, ValidateRegion)
-{
-  // ISO 3166-1
-  auto tag = CBcp47::ParseTag("en-GB");
-  EXPECT_TRUE(tag->IsValid());
-
-  // Private use
-  tag = CBcp47::ParseTag("en-AA");
-  EXPECT_TRUE(tag->IsValid());
-
-  // UN M.49 - support to be added with IANA language subtags registry
-  tag = CBcp47::ParseTag("es-419");
-  EXPECT_FALSE(tag->IsValid());
-}
-
-TEST(TestI18nBcp47, ValidateVariants)
-{
-  auto tag = CBcp47::ParseTag("en-variant");
-  EXPECT_TRUE(tag->IsValid());
-
-  tag = CBcp47::ParseTag("en");
-  EXPECT_TRUE(tag->IsValid());
-
-  tag = CBcp47::ParseTag("en-variant-variant");
-  EXPECT_FALSE(tag->IsValid());
-}
-
-TEST(TestI18nBcp47, ValidateExtensions)
-{
-  // extension with multiple segments
-  auto tag = CBcp47::ParseTag("ab-a-bcdefghi-jk");
-  EXPECT_TRUE(tag->IsValid());
-
-  // multiple extensions in non-alphabetical order are OK
-  tag = CBcp47::ParseTag("ab-b-ab-a-cd");
-  EXPECT_TRUE(tag->IsValid());
-
-  // no extension
-  tag = CBcp47::ParseTag("ab");
-  EXPECT_TRUE(tag->IsValid());
-
-  // duplicate extensions not allowed
-  tag = CBcp47::ParseTag("ab-a-bc-a-de");
-  EXPECT_FALSE(tag->IsValid());
-}
-
-TEST(TestI18nBcp47, ValidateExtLang)
-{
-  auto tag = CBcp47::ParseTag("ar-aao");
-  EXPECT_TRUE(tag->IsValid());
-
-  // Multiple extlangs are rejected whether they exist in registry or not
-  tag = CBcp47::ParseTag("en-abc-def");
-  EXPECT_FALSE(tag->IsValid());
-
-  //! @todo add test for inexistent extlang after implementation of the registry
 }
