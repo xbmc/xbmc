2de37b35d4f032b99b0f90699cee029ac0a04279
CrystalP <crystalp@kodi.tv>
2026-02-15 19:51:23 -0500

    [Windows] use https in mirrors url

--- tools/buildsteps/windows/download-dependencies.bat
+++ tools/buildsteps/windows/download-dependencies.bat
@@ -20,7 +20,7 @@ ECHO TARGETPLATFORM: %TARGETPLATFORM%
 ECHO NATIVEPLATFORM: %NATIVEPLATFORM%
 
 REM If KODI_MIRROR is not set externally to this script, set it to the default mirror URL
-IF "%KODI_MIRROR%" == "" SET KODI_MIRROR=http://mirrors.kodi.tv
+IF "%KODI_MIRROR%" == "" SET KODI_MIRROR=https://mirrors.kodi.tv
 echo Downloading from mirror %KODI_MIRROR%
 
 REM Locate the BuildDependencies directory, based on the path of this script
@@ -55,9 +55,9 @@ IF NOT EXIST %FORMED_OK_FLAG% (
   ECHO.
   ECHO I tried to get the packages from %KODI_MIRROR%;
   ECHO if this download mirror seems to be having problems, try choosing another from
-  ECHO the list on http://mirrors.kodi.tv/timestamp.txt?mirrorlist, and setting %%KODI_MIRROR%% to
+  ECHO the list on https://mirrors.kodi.tv/timestamp.txt?mirrorlist, and setting %%KODI_MIRROR%% to
   ECHO point to it, like so:
-  ECHO   C:\^> SET KODI_MIRROR=http://example.com/pub/xbmc/
+  ECHO   C:\^> SET KODI_MIRROR=https://example.com/pub/xbmc/
   ECHO.
   ECHO Then, rerun this script.
   
