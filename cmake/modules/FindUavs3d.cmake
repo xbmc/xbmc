66c9f5096f2ba06e17bb8cd4c8c63dfe8ae787d9
fuzzard <fuzzard@kodi.tv>
2026-03-08 08:09:36 +1000

    [cmake] Add FindUavs3d module
    LIBRARY for ffmpeg avs3 support

---
+++ cmake/modules/FindUavs3d.cmake
@@ -0,0 +1,79 @@
+#.rst:
+# FindUavs3d
+# ----------
+# Finds the uavs3d library
+#
+# This will define the following target:
+#
+#   LIBRARY::Uavs3d   - The uavs3d library
+#
+
+if(NOT TARGET LIBRARY::${CMAKE_FIND_PACKAGE_NAME})
+
+  include(cmake/scripts/common/ModuleHelpers.cmake)
+
+  macro(buildmacroUavs3d)
+  
+    set(patches "${CMAKE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/01-all-enable_prog.patch"
+                "${CMAKE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/02-all-arm_removeflags.patch"
+                "${CMAKE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/03-all-version.h.patch"
+                "${CMAKE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/04-all-pthread.patch")
+
+    generate_patchcommand("${patches}")
+    unset(patches)
+
+    set(CMAKE_ARGS -DCOMPILE_10BIT=ON
+                   ${EXTRA_ARGS})
+  
+    BUILD_DEP_TARGET()
+
+    if(NOT (WIN32 OR WINDOWS_STORE))
+      list(APPEND ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LINK_LIBRARIES "m")
+    endif()
+
+    if(CMAKE_USE_PTHREADS_INIT)
+      add_dependencies(${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME} Threads::Threads)
+      list(APPEND ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LINK_LIBRARIES "Threads::Threads")
+    endif()
+
+  endmacro()
+
+  set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC uavs3d)
+
+  SETUP_BUILD_VARS()
+
+  SETUP_FIND_SPECS()
+
+  SEARCH_EXISTING_PACKAGES()
+
+  if(("${${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_VERSION}" VERSION_LESS ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER} AND ENABLE_INTERNAL_UAVS3D) OR
+     (((CORE_SYSTEM_NAME STREQUAL linux AND NOT "webos" IN_LIST CORE_PLATFORM_NAME_LC) OR CORE_SYSTEM_NAME STREQUAL freebsd) AND ENABLE_INTERNAL_UAVS3D))
+    message(STATUS "Building ${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}: \(version \"${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER}\"\)")
+    cmake_language(EVAL CODE "
+      buildmacro${CMAKE_FIND_PACKAGE_NAME}()
+    ")
+  endif()
+
+  if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
+    if(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+    else()
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_TYPE LIBRARY)
+
+      SETUP_BUILD_TARGET()
+
+      add_dependencies(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+
+      # We are building as a requirement, so set LIB_BUILD property to allow calling
+      # modules to know we will be building, and they will want to rebuild as well.
+      # Property must be set on actual TARGET and not the ALIAS
+      set_target_properties(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES LIB_BUILD ON)
+    endif()
+
+    ADD_MULTICONFIG_BUILDMACRO()
+  else()
+    if(Uavs3d_FIND_REQUIRED)
+      message(FATAL_ERROR "libuavs3d not found. You may want to try -DENABLE_INTERNAL_UAVS3D=ON")
+    endif()
+  endif()
+endif()
