916ccdaea6c59376e51f403751eb92241558087f
Matthias Reichl <hias@horus.com>
2026-03-07 15:11:04 +0100

    Input: fix keyname logging
    GetKeyName got somewhat broken in the last 15 years, fix this
    so that keys are properly logged:
    
    Log the full 16-bit code (as it would appear in key id="..." mappings)
    for unknown keys instead of only the lower 8 bits and display the
    obc code only for valid keycodes (255 or less).
    
    Also handle the special KEY_UNICODE and KEY_INVALID values.

--- xbmc/input/keyboard/KeyboardStat.h
+++ xbmc/input/keyboard/KeyboardStat.h
@@ -50,7 +50,7 @@ public:
   void ProcessKeyDown(XBMC_keysym& keysym);
   void ProcessKeyUp(void);
 
-  std::string GetKeyName(int KeyID);
+  std::string GetKeyName(uint32_t keyid);
 
 private:
   static bool LookupSymAndUnicodePeripherals(XBMC_keysym& keysym, uint16_t* key, char* unicode);
