77db846751302d80081cc999367683f23b9481fd
shaqer1 <shaq@alumni.purdue.edu>
2026-03-29 23:57:47 +0000

    Android: Fix issue 28009 by storing extracted APK assets in filesDir and validating cache contents before startup

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -1657,7 +1657,7 @@ void CXBMCApp::SetupEnv()
   StringUtils::ToLower(appName);
   std::string className = CCompileInfo::GetPackage();
 
-  std::string cacheDir = getCacheDir().getAbsolutePath();
+  std::string cacheDir = getFilesDir().getAbsolutePath();
   std::string xbmcTemp = CJNISystem::getProperty("xbmc.temp", "");
   if (!xbmcTemp.empty())
   {
