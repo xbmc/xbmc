4455d48bc5362166f85f6136eafbc8f9f35915da
reardonia <2925104+reardonia@users.noreply.github.com>
2026-04-20 19:32:48 +0300

    CDRMObject:  Add CDRMPropertyBlob RAII wrapper

--- xbmc/windowing/gbm/drm/DRMObject.h
+++ xbmc/windowing/gbm/drm/DRMObject.h
@@ -8,11 +8,16 @@
 
 #pragma once
 
+#include "utils/log.h"
+
+#include <cerrno>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <memory>
 #include <optional>
 #include <string_view>
+#include <utility>
 #include <vector>
 
 #include <xf86drmMode.h>
@@ -71,6 +76,73 @@ protected:
 private:
   uint32_t m_id{0};
   uint32_t m_type{0};
+};
+
+/*!
+ * \brief RAII wrapper around a DRM property blob.
+ *
+ * Owns both the drm fd and the blob id, and destroys the blob on
+ * scope exit. Move-only.
+ *
+ * Lifetime note: an atomic commit that references the blob id must
+ * complete BEFORE this object is destroyed; DRM requires the blob to
+ * be alive for the duration of the commit. Declare at a scope (local
+ * or member) that ends AFTER drmModeAtomicCommit returns.
+ */
+class CDRMPropertyBlob
+{
+public:
+  CDRMPropertyBlob() = default;
+
+  CDRMPropertyBlob(int fd, const void* data, std::size_t size) : m_fd(fd)
+  {
+    if (drmModeCreatePropertyBlob(fd, data, size, &m_id) != 0)
+      m_id = 0;
+  }
+
+  ~CDRMPropertyBlob() { Reset(); }
+
+  CDRMPropertyBlob(const CDRMPropertyBlob&) = delete;
+  CDRMPropertyBlob& operator=(const CDRMPropertyBlob&) = delete;
+
+  CDRMPropertyBlob(CDRMPropertyBlob&& other) noexcept
+    : m_fd(other.m_fd),
+      m_id(std::exchange(other.m_id, 0))
+  {
+  }
+
+  CDRMPropertyBlob& operator=(CDRMPropertyBlob&& other) noexcept
+  {
+    if (this != &other)
+    {
+      Reset();
+      m_fd = other.m_fd;
+      m_id = std::exchange(other.m_id, 0);
+    }
+    return *this;
+  }
+
+  /*! \brief Blob id to pass to AddProperty / atomic commit. */
+  std::uint32_t Get() const { return m_id; }
+
+  /*! \brief True if the blob was successfully created and not yet released. */
+  bool IsValid() const { return m_id != 0; }
+
+  /*!
+   * \brief Destroy the owned blob (if any) and return to the null state.
+   *
+   * Safe to call on a default-constructed or already-released instance.
+   */
+  void Reset()
+  {
+    if (m_id != 0 && drmModeDestroyPropertyBlob(m_fd, m_id) != 0)
+      CLog::LogF(LOGERROR, "failed to destroy property blob: {}", std::strerror(errno));
+    m_id = 0;
+  }
+
+private:
+  int m_fd{-1};
+  std::uint32_t m_id{0};
 };
 
 } // namespace GBM
