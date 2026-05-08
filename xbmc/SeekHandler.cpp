5eea26b4774e7953b24f5a45b54c909241638ed9
ShortyCM <39855412+ShortyCM@users.noreply.github.com>
2026-05-08 15:36:36 -0700

    Apply formatting cleanup

--- xbmc/SeekHandler.cpp
+++ xbmc/SeekHandler.cpp
@@ -293,8 +293,8 @@ bool CSeekHandler::OnAction(const CAction &action)
         if (fps > 1.0f)
         {
           const int64_t frameTimeMs{static_cast<int64_t>(std::llround(1000.0 / fps))};
-          const int64_t targetTimeMs{
-              std::max(appPlayer->GetMinTime(), appPlayer->GetTime() - std::max<int64_t>(1, frameTimeMs))};
+          const int64_t targetTimeMs{std::max(
+              appPlayer->GetMinTime(), appPlayer->GetTime() - std::max<int64_t>(1, frameTimeMs))};
           appPlayer->SeekTime(targetTimeMs);
           return true;
         }
