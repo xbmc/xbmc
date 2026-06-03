174a16b672ab0778d8e2a590e9b82512ea93ecde
78andyp <drandypowell@live.co.uk>
2026-06-03 14:12:49 +0100

    [Profile] A custom advancedsettings.xml is now created in the profile directory if there is a remote database in the current profile - these details are copied over and name for the music/video database is given as -mymusic/myvideos.
    When creating a profile with a remote database - Media Info effectively doesn't do anything. The same mymovies and mymusic tables are used for each profile - this can be overcome with the tag in advancedsettings.xml but this isn't obvious.

--- xbmc/profiles/ProfileManager.cpp
+++ xbmc/profiles/ProfileManager.cpp
@@ -21,7 +21,6 @@
 #include "addons/Service.h" //! @todo Remove me
 #include "addons/Skin.h"
 #include "application/Application.h" //! @todo Remove me
-#include "application/ApplicationComponents.h"
 #include "application/ApplicationPowerHandling.h"
 #include "dialogs/GUIDialogKaiToast.h"
 #include "events/EventLog.h"
@@ -52,7 +51,6 @@
 #include "utils/FileUtils.h"
 #include "utils/StringUtils.h"
 #include "utils/URIUtils.h"
-#include "utils/Variant.h"
 #include "utils/XMLUtils.h"
 #include "utils/log.h"
 #include "video/VideoLibraryQueue.h" //! @todo Remove me
@@ -298,6 +296,10 @@ bool CProfileManager::LoadProfile(unsigned int index)
     return true;
   }
 
+  // check if the profile is already active
+  if (m_currentProfile == index && !m_profiles.at(index).needsRefresh())
+    return true;
+
   // save any settings of the currently used skin but only if the (master)
   // profile hasn't just been loaded as a temporary profile for login
   auto skin = CServiceBroker::GetGUI()->GetSkinInfo();
@@ -312,6 +314,7 @@ bool CProfileManager::LoadProfile(unsigned int index)
 
   SetCurrentProfileId(index);
   m_previousProfileLoadedForLogin = false;
+  m_profiles.at(index).SetNeedsRefresh(false);
 
   // load the new settings
   if (!settings->Load())
