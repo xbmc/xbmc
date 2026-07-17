bbb7bf36e67048f750ffe81f8c3021c4095d560d
Kevin Goffe <HitcherUK@gmail.com>
2026-07-17 11:19:19 +0100

    [guilib] Add ResetGroupList builtin to reset grouplist focus position

--- xbmc/interfaces/builtins/GUIBuiltins.cpp
+++ xbmc/interfaces/builtins/GUIBuiltins.cpp
@@ -16,6 +16,7 @@
 #include "dialogs/GUIDialogNumeric.h"
 #include "filesystem/Directory.h"
 #include "guilib/GUIComponent.h"
+#include "guilib/GUIControlGroupList.h"
 #include "guilib/GUIWindowManager.h"
 #include "guilib/StereoscopicsManager.h"
 #include "input/WindowTranslator.h"
@@ -35,10 +36,68 @@
 #include "utils/log.h"
 #include "windows/GUIMediaWindow.h"
 
+#include <charconv>
+
 using namespace KODI;
 
 namespace
 {
+/*! \brief Reset a grouplist control's focus to its first item.
+ *  \param params The parameters.
+ *  \details params[0] = The control ID of the grouplist to reset.
+ */
+static int ResetGroupList(const std::vector<std::string>& params)
+{
+  CGUIComponent* gui = CServiceBroker::GetGUI();
+  if (gui == nullptr)
+  {
+    CLog::Log(LOGWARNING, "ResetGroupList: No GUI present");
+    return 0;
+  }
+
+  CGUIWindow* window =
+      gui->GetWindowManager().GetWindow(gui->GetWindowManager().GetActiveWindowOrDialog());
+  if (window == nullptr)
+  {
+    CLog::Log(LOGWARNING, "ResetGroupList: No active window or dialog found");
+    return 0;
+  }
+
+  int controlId = 0;
+  const auto& param = params[0];
+  const auto result = std::from_chars(param.data(), param.data() + param.size(), controlId);
+  if (result.ec != std::errc{})
+  {
+    CLog::Log(LOGWARNING, "ResetGroupList: Invalid control ID '{}'", params[0]);
+    return 0;
+  }
+
+  CGUIControl* control = window->GetControl(controlId);
+  if (control == nullptr)
+  {
+    CLog::Log(LOGWARNING, "ResetGroupList: Control {} not found", params[0]);
+    return 0;
+  }
+
+  if (control->GetControlType() != CGUIControl::GUICONTROL_GROUPLIST)
+  {
+    CLog::Log(LOGWARNING, "ResetGroupList: Control {} is not a grouplist", params[0]);
+    return 0;
+  }
+
+  if (static_cast<CGUIControlGroupList*>(control)->ResetFocusToFirstItem())
+  {
+    CLog::Log(LOGDEBUG, "ResetGroupList: Reset grouplist {} to first item", params[0]);
+    return 0;
+  }
+
+  CLog::Log(
+      LOGWARNING,
+      "ResetGroupList: Unable to reset grouplist {} - no visible and focusable controls found",
+      params[0]);
+  return 1;
+}
+
 /*! \brief Execute a GUI action.
  *  \param params The parameters.
  *  \details params[0] = Action to execute.
@@ -581,9 +640,17 @@ static int ToggleDirty(const std::vector<std::string>&)
 ///     ,
 ///     makes dirty regions visible for debugging proposes.
 ///   }
+///   \table_row2_l{
+///     <b>`Control.ResetGroupList(id)`</b>
+///     ,
+///     Resets a grouplist control's focus to its first item and scrolls back to the top.
+///     @param[in] id                    The control ID of the grouplist to reset.
+///     \skinning_v22
+///   }
 ///  \table_end
 ///
 
+// clang-format off
 CBuiltins::CommandMap CGUIBuiltins::GetOperations() const
 {
   return {
@@ -602,6 +669,8 @@ CBuiltins::CommandMap CGUIBuiltins::GetOperations() const
            {"setproperty",                    {"Sets a window property for the current focused window/dialog (key,value)", 2, SetProperty}},
            {"setstereomode",                  {"Changes the stereo mode of the GUI. Params can be: toggle, next, previous, select, tomono or any of the supported stereomodes (off, split_vertical, split_horizontal, row_interleaved, hardware_based, anaglyph_cyan_red, anaglyph_green_magenta, anaglyph_yellow_blue, monoscopic)", 1, SetStereoMode}},
            {"takescreenshot",                 {"Takes a Screenshot", 0, Screenshot}},
-           {"toggledirtyregionvisualization", {"Enables/disables dirty-region visualization", 0, ToggleDirty}}
+           {"toggledirtyregionvisualization", {"Enables/disables dirty-region visualization", 0, ToggleDirty}},
+           {"control.resetgrouplist",         {"Resets a grouplist control's focus to its first item", 1, ResetGroupList}}
          };
 }
+// clang-format on
