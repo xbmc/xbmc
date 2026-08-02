76bf2b02e3258249cabed4171b6b977f23ce3e9c
Keith Herrington <keith@hadm.net>
2026-08-02 13:44:56 -0700

    Test iOS and tvOS deb packaging

--- tools/darwin/packaging/darwin_embedded/test/TestDebPackaging.cmake
+++ tools/darwin/packaging/darwin_embedded/test/TestDebPackaging.cmake
@@ -1,13 +1,14 @@
 cmake_minimum_required(VERSION 3.15)
 
-if(NOT KODI_SOURCE_DIR OR NOT TEST_BINARY_DIR OR NOT DPKG_DEB_EXECUTABLE)
+if(NOT KODI_SOURCE_DIR OR NOT TEST_BINARY_DIR OR NOT DPKG_DEB_EXECUTABLE OR
+   NOT TEST_PLATFORM OR NOT TEST_BUNDLE_IDENTIFIER OR NOT TEST_ARCHITECTURE)
   message(FATAL_ERROR
-    "KODI_SOURCE_DIR, TEST_BINARY_DIR and DPKG_DEB_EXECUTABLE are required")
+    "Kodi source, test binary, dpkg-deb and platform settings are required")
 endif()
 
-set(_test_root "${TEST_BINARY_DIR}/darwin-embedded-deb")
+set(_test_root "${TEST_BINARY_DIR}/darwin-embedded-deb-${TEST_PLATFORM}")
 set(_packaging_dir "${_test_root}/tools/darwin/packaging/darwin_embedded")
-set(_app_dir "${_test_root}/build/Debug-iphoneos/Kodi.app")
+set(_app_dir "${_test_root}/build/Debug-${TEST_PLATFORM}/Kodi.app")
 file(REMOVE_RECURSE "${_test_root}")
 file(MAKE_DIRECTORY "${_packaging_dir}" "${_app_dir}")
 file(WRITE "${_app_dir}/payload.txt" "packaged\n")
@@ -19,8 +20,8 @@ set(APP_VERSION_MAJOR 22)
 set(APP_VERSION_MINOR 0)
 set(APP_VERSION_TAG_LC alpha1)
 set(APP_WEBSITE "https://kodi.tv")
-set(PLATFORM iphoneos)
-set(PLATFORM_BUNDLE_IDENTIFIER org.xbmc.kodi-ios)
+set(PLATFORM "${TEST_PLATFORM}")
+set(PLATFORM_BUNDLE_IDENTIFIER "${TEST_BUNDLE_IDENTIFIER}")
 
 configure_file(
   "${KODI_SOURCE_DIR}/tools/darwin/packaging/darwin_embedded/mkdeb-darwin_embedded.sh.in"
@@ -38,7 +39,7 @@ if(NOT _package_result EQUAL 0)
 endif()
 
 set(_package
-  "${_packaging_dir}/org.xbmc.kodi-ios64_22.0-0~alpha1_iphoneos-arm.deb")
+  "${_packaging_dir}/${TEST_BUNDLE_IDENTIFIER}64_22.0-0~alpha1_${TEST_PLATFORM}-arm.deb")
 if(NOT EXISTS "${_package}")
   message(FATAL_ERROR "Expected package was not created: ${_package}")
 endif()
@@ -55,16 +56,25 @@ if(NOT _contents MATCHES "Applications/Kodi\\.app/payload\\.txt")
   message(FATAL_ERROR "Package does not contain the app payload:\n${_contents}")
 endif()
 
-execute_process(
-  COMMAND "${DPKG_DEB_EXECUTABLE}" --field "${_package}"
-          Package Version Architecture
-  RESULT_VARIABLE _metadata_result
-  OUTPUT_VARIABLE _metadata
-  ERROR_VARIABLE _metadata_error)
-if(NOT _metadata_result EQUAL 0)
-  message(FATAL_ERROR "Could not inspect package metadata: ${_metadata_error}")
-endif()
-if(NOT _metadata MATCHES
-   "Package: org\\.xbmc\\.kodi-ios64.*Version: 22\\.0-0~alpha1.*Architecture: iphoneos-arm")
-  message(FATAL_ERROR "Package metadata is incorrect:\n${_metadata}")
-endif()
+foreach(_field IN ITEMS Package Version Architecture)
+  execute_process(
+    COMMAND "${DPKG_DEB_EXECUTABLE}" --field "${_package}" "${_field}"
+    RESULT_VARIABLE _metadata_result
+    OUTPUT_VARIABLE _metadata
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_VARIABLE _metadata_error)
+  if(NOT _metadata_result EQUAL 0)
+    message(FATAL_ERROR "Could not inspect ${_field}: ${_metadata_error}")
+  endif()
+  if(_field STREQUAL Package)
+    set(_expected_metadata "${TEST_BUNDLE_IDENTIFIER}64")
+  elseif(_field STREQUAL Version)
+    set(_expected_metadata "22.0-0~alpha1")
+  else()
+    set(_expected_metadata "${TEST_ARCHITECTURE}")
+  endif()
+  if(NOT _metadata STREQUAL _expected_metadata)
+    message(FATAL_ERROR
+      "Package ${_field} is '${_metadata}', expected '${_expected_metadata}'")
+  endif()
+endforeach()
