f0c5a071ec3ba893b0a9e00e9ba4a72506fc080b
CrystalP <crystalp@kodi.tv>
2026-08-04 15:17:16 -0400

    [NfoUtils] Define version and upgrade function per tag type

--- xbmc/NfoUtils.cpp
+++ xbmc/NfoUtils.cpp
@@ -17,9 +17,44 @@ namespace
 {
 constexpr int VERSION_LEGACY = 0; // nfo has no version attribute and predates version support
 
+// Current nfo versions
+constexpr int VERSION_MOVIE = 0;
+constexpr int VERSION_TVSHOW = 0;
+constexpr int VERSION_EPISODEDETAILS = 0;
+constexpr int VERSION_MUSICVIDEOS = 0;
+
 constexpr int CurrentNfoVersion(std::string_view tag)
 {
+  if (tag == "movie")
+    return VERSION_MOVIE;
+  if (tag == "tvshow")
+    return VERSION_TVSHOW;
+  if (tag == "episodedetails")
+    return VERSION_EPISODEDETAILS;
+  if (tag == "musicvideos")
+    return VERSION_EPISODEDETAILS;
+
   return VERSION_LEGACY;
+}
+
+bool UpgradeMovie(TiXmlElement* root, int currentVersion)
+{
+  return true;
+}
+
+bool UpgradeTvShow(TiXmlElement* root, int currentVersion)
+{
+  return true;
+}
+
+bool UpgradeEpisodeDetails(TiXmlElement* root, int currentVersion)
+{
+  return true;
+}
+
+bool UpgradeMusicVideos(TiXmlElement* root, int currentVersion)
+{
+  return true;
 }
 } // namespace
 
@@ -47,7 +82,14 @@ bool CNfoUtils::Upgrade(TiXmlElement* root)
 
     root->SetAttribute("version", targetVersion);
 
-    // Upgrade code goes here
+    if (type == "movie")
+      return UpgradeMovie(root, version);
+    if (type == "tvshow")
+      return UpgradeTvShow(root, version);
+    if (type == "episodedetails")
+      return UpgradeEpisodeDetails(root, version);
+    if (type == "musicvideos")
+      return UpgradeMusicVideos(root, version);
   }
 
   return true;
