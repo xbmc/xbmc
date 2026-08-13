cc5b53a7a454f987512a8ce052f9de3ca21a87eb
78andyp <drandypowell@live.co.uk>
2026-08-13 13:54:20 +0100

    [Video][Database] Store the movie set sort title
    Add a strSortSet column to the sets table and movie_view so the set sort
    title is kept in the library and can be changed via UpdateVideoSortTitle.

--- xbmc/video/VideoDatabase.h
+++ xbmc/video/VideoDatabase.h
@@ -261,6 +261,13 @@ public:
   void UpdateMovieTitle(int idMovie,
                         const std::string& strNewMovieTitle,
                         VideoDbContentType iType = VideoDbContentType::MOVIES);
+
+  /*! \brief Set the sort title of a movie, tvshow or movie set.
+   \param[in] idDb the dbId of the item
+   \param[in] strNewSortTitle the new sort title
+   \param[in] iType the content type of the item
+   \return true on success.
+   */
   bool UpdateVideoSortTitle(int idDb,
                             const std::string& strNewSortTitle,
                             VideoDbContentType iType = VideoDbContentType::MOVIES);
@@ -958,9 +965,21 @@ public:
   \return The dbId of the season.
   */
   int AddSeason(int showID, int season, const std::string& name = "", const std::string& plot = "");
+
+  /*! \brief Add a movie set, or update it if it already exists.
+   \param[in] strSet the (possibly user defined) title of the set
+   \param[in] strOverview the overview of the set
+   \param[in] strOriginalSet the title of the set as given by the scraper. Used to identify an
+              existing set, and defaults to strSet when empty.
+   \param[in] strSortSet the title used to sort the set. An empty value never clears an existing
+              sort title, so callers without one to offer can leave it out.
+   \param[in] updateOverview whether an existing set's overview should be replaced
+   \return the dbId of the set, or -1 on failure.
+   */
   int AddSet(const std::string& strSet,
              const std::string& strOverview = "",
              const std::string& strOriginalSet = "",
+             const std::string& strSortSet = "",
              const bool updateOverview = true);
   void ClearMovieSet(int idMovie);
   void SetMovieSet(int idMovie, int idSet);
