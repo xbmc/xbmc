633abfbf11c2c40519cc2081e9b3124df6924dcc
CrystalP <crystalp@kodi.tv>
2026-08-16 17:25:14 -0400

    [SettingsMigration] Generalize the single setting conversion as a template

--- xbmc/settings/lib/SettingsMigrationPrimitive.cpp
+++ xbmc/settings/lib/SettingsMigrationPrimitive.cpp
@@ -9,14 +9,6 @@
 #include "SettingsMigrationPrimitive.h"
 
 #include "Setting.h"
-#include "settings/SettingsValueXmlSerializer.h"
-#include "settings/lib/SettingsManager.h"
-#include "utils/XMLUtils.h"
-#include "utils/log.h"
-
-#include <memory>
-#include <stdexcept>
-#include <string>
 
 namespace KODI::SETTINGS
 {
@@ -25,54 +17,12 @@ SettingConversionResult ConvertSettingBoolToInt(TiXmlElement* root,
                                                 std::string_view newSettingId,
                                                 const SettingBoolToIntMapping& mapping)
 {
-  if (TiXmlElement* elem = CSettingsManager::LocateSetting(root, oldSettingId); elem != nullptr)
-  {
-    //! maybe future @todo: strategy - read/write settings without dependency on CSetting* classes?
-    auto oldSetting = std::make_shared<CSettingBool>(oldSettingId, nullptr);
-    if (oldSetting == nullptr)
-      return SettingConversionResult::FAILURE;
-
-    const std::string oldValue = elem->FirstChild() ? elem->FirstChild()->ValueStr() : "";
-
-    if (!oldSetting->FromString(oldValue))
-    {
-      CLog::Log(LOGWARNING,
-                "Settings conversion: unable to load the value of the old "
-                "setting \"{}\": \"{}\". "
-                "The new setting \"{}\" will have its default value.",
-                oldSettingId, oldValue, newSettingId);
-      return SettingConversionResult::INVALID;
-    }
-
-    // Map to int setting values
-    const int newValue = oldSetting->GetValue() ? mapping.m_true : mapping.m_false;
-
-    // Prepare a new setting
-    auto newSetting = std::make_shared<CSettingInt>(newSettingId, nullptr);
-    if (newSetting == nullptr)
-      return SettingConversionResult::FAILURE;
-
-    newSetting->SetDefault(mapping.m_default);
-    newSetting->SetValue(newValue);
-
-    // Add the new setting and remove the old one
-    // The new setting doesn't have to be in the same place in the file as the old one.
-    if (!CSettingsValueXmlSerializer::SerializeSetting(root, newSetting) ||
-        !XMLUtils::RemoveNode(elem))
-      return SettingConversionResult::FAILURE;
-
-    CLog::LogF(LOGDEBUG,
-               "Successful conversion of old setting \"{}\" / \"{}\" to new "
-               "setting \"{}\" / \"{}\".",
-               oldSettingId, oldValue, newSettingId, newValue);
-    return SettingConversionResult::CONVERTED;
-  }
-
-  CLog::LogF(LOGDEBUG,
-             "Old setting \"{}\" not found. The new setting \"{}\" will have "
-             "its default value.",
-             oldSettingId, newSettingId);
-
-  return SettingConversionResult::NOT_PRESENT;
+  return impl::ConvertSingleSetting<CSettingBool, CSettingInt>(
+      root, oldSettingId, newSettingId,
+      [&mapping](bool oldValue)
+      {
+        const int newValue = oldValue ? mapping.m_true : mapping.m_false;
+        return std::pair{newValue, mapping.m_default};
+      });
 }
 } // namespace KODI::SETTINGS
