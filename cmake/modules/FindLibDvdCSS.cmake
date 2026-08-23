xbmc/xbmc

commit 9370ea763c1cd2d47f85be897dee2bb1750115a2

[dependencies] Bump libdvdcss 1.5.0
Update to meson build system
Bring in xbmc patches from https://github.com/xbmc/libdvdcss/tree/Omega

cmake/modules/FindLibDvdCSS.cmake
@@ -5,82 +5,117 @@
 #
 # This will define the following target:
 #
-#   LibDvdCSS::LibDvdCSS   - The LibDvdCSS library
+#   LIBRARY::LibDvdCSS   - The LibDvdCSS library
 
-if(NOT TARGET LibDvdCSS::LibDvdCSS)
+if(NOT TARGET LIBRARY::${CMAKE_FIND_PACKAGE_NAME})
+
   include(cmake/scripts/common/ModuleHelpers.cmake)
+
+  macro(buildmacroLibDvdCSS)
+
+    find_package(Meson REQUIRED)
+    find_package(Ninja REQUIRED)
+
+    set(patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/01-all-libc_read_improvement.patch")
+
+    if(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
+      list(APPEND patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/02-darwinembedded-enablebuild.patch")
+    endif()
+
+    if("webos" IN_LIST CORE_PLATFORM_NAME_LC)
+      # PATH_MAX not defined in limits.h. Just match windows size libdvdcss uses.
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_C_FLAGS "-DPATH_MAX=2048")
+    endif()
+
+    if(WIN32 OR WINDOWS_STORE)
+      if(WINDOWS_STORE)
+        list(APPEND patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/03-win-uwpfixes.patch")
+      endif()
+
+      list(APPEND patches "${CORE_SOURCE_DIR}/tools/depends/target/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}/04-win-open_wrapforce.patch")
+
+      create_module_dev_env()
+    endif()
+
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_libType static)
+
+    generate_patchcommand("${patches}")
+    unset(patches)
+
+    if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.26)
+      set(configure_env_mod ${CMAKE_COMMAND} -E env --modify NINJA=set:${NINJA_EXECUTABLE}
+                                                    ${additional_env_mod})
+    endif()
+
+    # generate meson cross file for build target
+    generate_mesoncrossfile()
+
+    if(EXISTS ${DEPENDS_PATH}/share/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}-cross-file.meson)
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}_CROSS_FILE --cross-file=${DEPENDS_PATH}/share/${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}-cross-file.meson)
+    elseif(EXISTS ${DEPENDS_PATH}/share/cross-file.meson)
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}_CROSS_FILE --cross-file=${DEPENDS_PATH}/share/cross-file.meson)
+    endif()
+
+    # We set a bunch of env vars to assist meson finding cmake packages for dependencies
+    # If we do not do this, it may find system libs outside of DEPENDS_PATH, or not find anything at all
+    set(CONFIGURE_COMMAND ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_dev_env}
+                          ${configure_env_mod}
+                          ${MESON_EXECUTABLE} setup ./build
+                          --cmake-prefix-path=['${DEPENDS_PATH}/lib/cmake']
+                          --prefix=${DEPENDS_PATH}
+                          --libdir=lib
+                          --buildtype=release
+                          -Ddefault_library=static
+                          -Denable_docs=false
+                          -Denable_examples=false
+                          ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_EXTRAS}
+                          ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}_CROSS_FILE})
+
+    set(BUILD_COMMAND ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_dev_env}
+                      ${build_env_mod}
+                      ${NINJA_EXECUTABLE} -C ./build)
+    set(INSTALL_COMMAND ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_dev_env}
+                        ${build_env_mod}
+                        ${NINJA_EXECUTABLE} -C ./build install)
+    set(BUILD_IN_SOURCE 1)
+
+    BUILD_DEP_TARGET()
+
+  endmacro()
 
   set(${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC libdvdcss)
 
   SETUP_BUILD_VARS()
 
-  # Legacy support for lowercase user provided URL override
-  if(libdvdcss_URL)
-    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_URL ${libdvdcss_URL})
+  SETUP_FIND_SPECS()
+
+  SEARCH_EXISTING_PACKAGES()
+
+  if((${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_VERSION VERSION_LESS ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER} AND ENABLE_DVDCSS) OR
+     (((CORE_SYSTEM_NAME STREQUAL linux AND NOT "webos" IN_LIST CORE_PLATFORM_NAME_LC) OR CORE_SYSTEM_NAME STREQUAL freebsd) AND ENABLE_DVDCSS))
+    message(STATUS "Building ${${CMAKE_FIND_PACKAGE_NAME}_MODULE_LC}: \(version \"${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER}\"\)")
+
+    cmake_language(EVAL CODE "
+      buildmacro${CMAKE_FIND_PACKAGE_NAME}()
+    ")
   endif()
-
-  set(LIBDVDCSS_VERSION ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_VER})
-
-  set(HOST_ARCH ${ARCH})
-  if(CORE_SYSTEM_NAME STREQUAL android)
-    if(ARCH STREQUAL arm)
-      set(HOST_ARCH arm-linux-androideabi)
-    elseif(ARCH STREQUAL i486-linux)
-      set(HOST_ARCH i686-linux-android)
-    elseif()
-      set(HOST_ARCH ${ARCH}-linux-android)
-    endif()
-  elseif(CORE_SYSTEM_NAME STREQUAL windowsstore)
-    set(LIBDVD_ADDITIONAL_ARGS "-DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}" "-DCMAKE_SYSTEM_VERSION=${CMAKE_SYSTEM_VERSION}")
-    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_CXX_FLAGS "/Zc:twoPhase-")
-  endif()
-
-  if(APPLE)
-    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LINK_LIBRARIES "-framework CoreFoundation")
-    if(NOT CORE_SYSTEM_NAME STREQUAL darwin_embedded)
-      list(APPEND ${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LINK_LIBRARIES "-framework IOKit")
-    endif()
-    string(REPLACE ";" " " LIBDVDCSS_FLAGS "${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_LINK_LIBRARIES}")
-  endif()
-
-  if(CORE_SYSTEM_NAME MATCHES windows)
-    set(CMAKE_ARGS -DDUMMY_DEFINE=ON
-                   -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-                   ${LIBDVD_ADDITIONAL_ARGS})
-  else()
-    find_program(AUTORECONF autoreconf REQUIRED)
-    if (CMAKE_HOST_SYSTEM_NAME MATCHES "(Free|Net|Open)BSD")
-      find_program(MAKE_EXECUTABLE gmake)
-    endif()
-    find_program(MAKE_EXECUTABLE make REQUIRED)
-
-    set(CONFIGURE_COMMAND ${AUTORECONF} -vif
-                  COMMAND ac_cv_path_GIT= ./configure
-                          --target=${HOST_ARCH}
-                          --host=${HOST_ARCH}
-                          --disable-doc
-                          --enable-static
-                          --disable-shared
-                          --with-pic
-                          --prefix=${DEPENDS_PATH}
-                          --libdir=${DEPENDS_PATH}/lib
-                          "CC=${CMAKE_C_COMPILER}"
-                          "CFLAGS=${CMAKE_C_FLAGS}"
-                          "LDFLAGS=${CMAKE_EXE_LINKER_FLAGS} ${LIBDVDCSS_FLAGS}")
-    set(BUILD_COMMAND ${MAKE_EXECUTABLE})
-    set(INSTALL_COMMAND ${MAKE_EXECUTABLE} install)
-    set(BUILD_IN_SOURCE 1)
-  endif()
-
-  BUILD_DEP_TARGET()
 
   if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
-    SETUP_BUILD_TARGET()
+    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_TYPE LIBRARY)
 
-    set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAVE_DVDCSS_DVDCSS_H)
-    ADD_TARGET_COMPILE_DEFINITION()
+    if(TARGET PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME} AND NOT TARGET ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      add_library(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
+    else()
+      SETUP_BUILD_TARGET()
+      add_dependencies(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
 
-    add_dependencies(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ${${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_BUILD_NAME})
+      # We are building as a requirement, so set LIB_BUILD property to allow calling
+      # modules to know we will be building, and they will want to rebuild as well.
+      # Property must be set on actual TARGET and not the ALIAS
+      set_target_properties(LIBRARY::${CMAKE_FIND_PACKAGE_NAME} PROPERTIES LIB_BUILD ON)
+    endif()
+
+    ADD_MULTICONFIG_BUILDMACRO()
   else()
     if(LibDvdCSS_FIND_REQUIRED)
       message(FATAL_ERROR "Libdvdcss not found. Possibly remove ENABLE_DVDCSS.")

cmake/modules/FindLibDvd.cmake
@@ -15,8 +15,8 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   else()
     set(_dvdlibs ${APP_NAME_LC}::LibDvdRead)
 
-    if(TARGET ${APP_NAME_LC}::LibDvdCSS)
-      list(APPEND _dvdlibs ${APP_NAME_LC}::LibDvdCSS)
+    if(TARGET LIBRARY::LibDvdCSS)
+      list(APPEND _dvdlibs LIBRARY::LibDvdCSS)
     endif()
 
     # link a shared dvdnav library that includes the whole archives of dvdread and dvdcss as well

tools/depends/target/libdvdcss/01-all-libc_read_improvement.patch
@@ -1,0 +1,30 @@
+--- a/src/device.c
++++ b/src/device.c
+@@ -636,13 +636,21 @@ static int libc_read ( dvdcss_t dvdcss, void *p_buffer, int i_blocks )
+     off_t i_size, i_ret, i_ret_blocks;
+ 
+     i_size = (off_t)i_blocks * (off_t)DVDCSS_BLOCK_SIZE;
+-    i_ret = read( dvdcss->i_fd, p_buffer, i_size );
+-
+-    if( i_ret < 0 )
+-    {
+-        print_error( dvdcss, "read error" );
++    i_ret = 0;
++    while (i_ret < i_size)
++    {
++      off_t i_r;
++      i_r = read(dvdcss->i_fd, ((char*)p_buffer) + i_ret, i_size - i_ret);
++      if (i_r < 0)
++      {
++        print_error(dvdcss, "read error");
+         dvdcss->i_pos = -1;
+-        return i_ret;
++        return i_r;
++      }
++      if (i_r == 0)
++        break;
++
++      i_ret += i_r;
+     }
+ 
+     i_ret_blocks = i_ret / DVDCSS_BLOCK_SIZE;

tools/depends/target/libdvdcss/02-darwinembedded-enablebuild.patch
@@ -1,0 +1,153 @@
+--- a/meson.build
++++ b/meson.build
+@@ -68,7 +68,9 @@
+    # Windows is supported as long as winioctl.h is found
+ elif host_machine.system() == 'darwin'
+     add_project_arguments(cc.get_supported_arguments('-no-cpp-precomp'), language: 'c')
+-    cdata.set('DARWIN_DVD_IOCTL', 1)
+-    extra_dependencies += dependency('appleframeworks', modules: ['CoreFoundation', 'IOKit'])
++    if cc.has_header('IOKit/storage/IODVDMediaBSDClient.h', args: test_args)
++      cdata.set('DARWIN_DVD_IOCTL', 1)
++      extra_dependencies += dependency('appleframeworks', modules: ['CoreFoundation', 'IOKit'])
++    endif
+ elif host_machine.system() == 'sunos' \
+      and cc.has_header('sys/scsi/scsi_types.h', args: test_args) \
+--- a/src/device.c
++++ b/src/device.c
+@@ -123,6 +123,8 @@ int dvdcss_use_ioctls( dvdcss_t dvdcss )
+     if( ulMode & OPEN_FLAGS_DASD )
+         return 1;
+ 
++    return 0;
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
+     return 0;
+ #else
+     struct stat fileinfo;
+--- a/src/ioctl.c
++++ b/src/ioctl.c
+@@ -223,10 +223,11 @@
+                         p_buffer, sizeof(p_buffer), &ulDataLen);
+ 
+     *pi_copyright = p_buffer[ 4 ];
+-
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++    *pi_copyright = 0;
++    i_ret = 0;
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+-
+ #endif
+     return i_ret;
+ }
+@@ -356,7 +357,8 @@
+     }
+ 
+     memcpy( p_key, p_buffer + 4, DVD_DISCKEY_SIZE );
+-
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -487,6 +489,8 @@
+ 
+     memcpy( p_key, p_buffer + 5, DVD_KEY_SIZE );
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -584,6 +588,8 @@
+ 
+     *pi_agid = p_buffer[ 7 ] >> 6;
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -692,6 +698,8 @@
+ 
+     memcpy( p_challenge, p_buffer + 4, DVD_CHALLENGE_SIZE );
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -802,6 +810,8 @@
+ 
+     *pi_asf = p_buffer[ 7 ] & 1;
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -905,6 +915,8 @@
+ 
+     memcpy( p_key, p_buffer + 4, DVD_KEY_SIZE );
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -988,6 +1000,9 @@
+     i_ret = DosDevIOCtl(i_fd, IOCTL_CDROMDISK, CDROMDISK_EXECMD,
+                         &sdc, sizeof(sdc), &ulParamLen,
+                         NULL, 0, &ulDataLen);
++
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -1075,7 +1090,8 @@
+     i_ret = ioctl( h_dvd, DKIOCDVDREADSTRUCTURE, &dvd );
+     if (i_ret == 0)
+         memcpy(p_buffer, dvd.buffer, sizeof(dvd.bufferLength));
+-
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++    i_ret = 0;
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -1177,7 +1193,8 @@
+         memcpy( p_mkb_pack, p_buffer + 4, CPRM_MKB_PACK_SIZE );
+     }
+     return i_ret;
+-
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++    i_ret = 0;
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -1288,6 +1305,8 @@
+                          &sdc, sizeof(sdc), &ulParamLen,
+                          p_buffer, sizeof(p_buffer), &ulDataLen );
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -1397,6 +1416,8 @@
+                          &sdc, sizeof(sdc), &ulParamLen,
+                          p_buffer, sizeof(p_buffer), &ulDataLen );
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 
+@@ -1524,6 +1545,8 @@
+     *p_mask = p_buffer[ 5 ];
+     *p_scheme = p_buffer[ 6 ];
+ 
++#elif defined(__APPLE__) && (defined(__arm__) || defined(__aarch64__))
++// nothing
+ #else
+ #   error "DVD ioctls are unavailable on this system"
+ 

commit fb9832a9f6ca174a955e5dcd5cb9a5b4a8b7ebb3

[cmake][modules][webos] Fix libdvdcss build against glibc 2.12 sysroot
libdvdcss 1.5.0 builds with meson using c_std=c17 and relies on
-D_DEFAULT_SOURCE to expose strdup(). The webOS SDK sysroot ships
glibc 2.12, which predates _DEFAULT_SOURCE (added in glibc 2.19), so
that define is a silent no-op there.

Because -std=c17 (rather than gnu17) defines __STRICT_ANSI__, glibc
2.12's features.h skips its _BSD_SOURCE/_SVID_SOURCE defaults. strdup()
in that string.h is gated on __USE_SVID || __USE_BSD ||
__USE_XOPEN_EXTENDED || __USE_XOPEN2K8, none of which end up set, so the
declaration is hidden and GCC 15 fails the build:

  ../src/device.c:343:34: error: implicit declaration of function
  'strdup'; did you mean 'strcmp'? [-Wimplicit-function-declaration]
  ../src/libdvdcss.c:548:39: error: implicit declaration of function
  'strdup'; did you mean 'strcmp'? [-Wimplicit-function-declaration]

Define _BSD_SOURCE explicitly for webOS to restore the declaration.
This keeps the flag scoped to the one platform that needs it, since
_BSD_SOURCE is deprecated on glibc >= 2.20.

Regression from 9370ea763c ([dependencies] Bump libdvdcss 1.5.0); the
previous autotools build did not compile in strict-ANSI mode.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

cmake/modules/FindLibDvdCSS.cmake
@@ -24,7 +24,10 @@ if(NOT TARGET LIBRARY::${CMAKE_FIND_PACKAGE_NAME})
 
     if("webos" IN_LIST CORE_PLATFORM_NAME_LC)
       # PATH_MAX not defined in limits.h. Just match windows size libdvdcss uses.
-      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_C_FLAGS "-DPATH_MAX=2048")
+      # webos sysroot is glibc 2.12, which predates _DEFAULT_SOURCE (glibc 2.19). libdvdcss
+      # builds with -std=c17, so __STRICT_ANSI__ suppresses the _BSD_SOURCE/_SVID_SOURCE
+      # defaults and strdup is never declared. Define _BSD_SOURCE explicitly to restore it.
+      set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_C_FLAGS "-DPATH_MAX=2048 -D_BSD_SOURCE")
     endif()
 
     if(WIN32 OR WINDOWS_STORE)