ad879814a6751348b26ed8290fdd822cb42c388e
Andrey Filipenkov <decapitator@ukr.net>
2026-09-13 10:03:45 +0300

    [depends][libmicrohttpd][apple] remove no longer needed explicit `mhd_cv_have_func_timespec_get`

--- cmake/modules/FindMicroHttpd.cmake
+++ cmake/modules/FindMicroHttpd.cmake
@@ -33,13 +33,6 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
       endif()
       find_program(MAKE_EXECUTABLE make REQUIRED)
 
-      if(CORE_SYSTEM_NAME STREQUAL darwin_embedded)
-        # blanket disable timespec_get use for darwin_embedded. timespec_get was introduced in
-        # __API_AVAILABLE(macosx(10.15), ios(13.0), tvos(13.0), watchos(6.0)) but older platforms
-        # are failing to run.
-        set(EXTRA_ARGS mhd_cv_have_func_timespec_get=no)
-      endif()
-
       set(CONFIGURE_COMMAND ./configure --prefix ${DEPENDS_PATH}
                                         --disable-shared
                                         --disable-doc
