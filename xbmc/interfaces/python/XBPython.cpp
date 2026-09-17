bd860b62837e23019fdd5ab2ea22f2a22fce3110
Andrey Filipenkov <decapitator@ukr.net>
2026-09-17 21:00:21 +0300

    [apple] slight code improvement

--- xbmc/interfaces/python/XBPython.cpp
+++ xbmc/interfaces/python/XBPython.cpp
@@ -53,12 +53,11 @@ XBPython::XBPython()
   {
     // using external python, its build looking for xxx/lib/python3.8
     // so point it to frameworks which is where python3.8 is located
-    setenv("PYTHONHOME", CSpecialProtocol::TranslatePath("special://frameworks").c_str(), 1);
-    setenv("PYTHONPATH", CSpecialProtocol::TranslatePath("special://frameworks").c_str(), 1);
-    CLog::Log(LOGDEBUG, "PYTHONHOME -> {}",
-              CSpecialProtocol::TranslatePath("special://frameworks"));
-    CLog::Log(LOGDEBUG, "PYTHONPATH -> {}",
-              CSpecialProtocol::TranslatePath("special://frameworks"));
+    const auto pythonRootPath = CSpecialProtocol::TranslatePath("special://frameworks");
+    setenv("PYTHONHOME", pythonRootPath.c_str(), 1);
+    setenv("PYTHONPATH", pythonRootPath.c_str(), 1);
+    CLog::Log(LOGDEBUG, "PYTHONHOME -> {}", pythonRootPath);
+    CLog::Log(LOGDEBUG, "PYTHONPATH -> {}", pythonRootPath);
   }
 #endif
 
