00e9ec4374d04fae2f90b2f4da86150b1269a6de
Martin Ellis <malard@gmail.com>
2026-09-17 07:37:38 +0100

    Update xbmc/utils/i18n/Iso639_1.h
    Co-authored-by: CrystalP <crystalp@kodi.tv>

--- xbmc/utils/i18n/Iso639_1.h
+++ xbmc/utils/i18n/Iso639_1.h
@@ -43,8 +43,8 @@ public:
   static std::optional<std::string> LookupByName(std::string_view name);
 
   /*!
-   * \brief Provide the languages offered as a choice: active ISO 639-1 codes and Kodi's
-   *        additions, excluding codes ISO 639-1 has withdrawn.
+   * \brief Provide a list of active ISO 639-1 codes and Kodi's additions.
+   *        Deprecated language codes are excluded.
    * \param[in] langMap map to add languages to
    * \return true for success, false otherwise
    */
