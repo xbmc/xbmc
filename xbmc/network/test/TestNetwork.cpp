0b83e87569723d722a165e59c3f51919925835ff
Garrett Brown <themagnificentmrb@gmail.com>
2026-09-22 16:53:25 -0700

    [network] Restore PingHost test on Linux
    Remove the permission probe and skip now that Linux PingHost handles
    restricted ping sockets. Always verify loopback and unreachable hosts.

--- xbmc/network/test/TestNetwork.cpp
+++ xbmc/network/test/TestNetwork.cpp
@@ -12,13 +12,6 @@
 #include <arpa/inet.h>
 #include <gtest/gtest.h>
 
-#if defined(TARGET_LINUX) && !defined(TARGET_ANDROID)
-#include <errno.h>
-
-#include <sys/socket.h>
-#include <unistd.h>
-#endif
-
 class TestNetwork : public testing::Test
 {
 public:
@@ -42,21 +35,6 @@ private:
 
 TEST_F(TestNetwork, PingHost)
 {
-#if defined(TARGET_LINUX) && !defined(TARGET_ANDROID)
-  // CNetworkLinux::PingHost() uses an unprivileged Linux "ping socket"
-  // (SOCK_DGRAM + IPPROTO_ICMP), which the kernel refuses unless the
-  // calling process's group is within net.ipv4.ping_group_range - disabled
-  // by default on most distributions (see icmp(7)).
-  int probe = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
-  if (probe < 0 && (errno == EACCES || errno == EPERM))
-  {
-    GTEST_SKIP() << "Unprivileged ICMP ping sockets are not permitted on this host (see "
-                    "net.ipv4.ping_group_range in icmp(7))";
-  }
-  if (probe >= 0)
-    close(probe);
-#endif
-
   EXPECT_TRUE(PingHost("127.0.0.1"));
   EXPECT_FALSE(PingHost("10.254.254.254"));
 }
